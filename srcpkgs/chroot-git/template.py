7c6f310631a1a58f0b28e30accdc1763ad6217a6
Daniel Kolesa <daniel@octaforge.org>
2021-06-08 03:17:35 +0200

    chroot-git: port

--- srcpkgs/chroot-git/template.py
+++ srcpkgs/chroot-git/template.py
@@ -1,39 +1,54 @@
-# Template file for 'chroot-git'
-pkgname=chroot-git
-version=2.31.1
-revision=1
-bootstrap=yes
-wrksrc="git-${version}"
-build_style=gnu-configure
-configure_args="--without-curl --without-openssl
- --without-python --without-expat --without-tcltk
- ac_cv_lib_curl_curl_global_init=no ac_cv_lib_expat_XML_ParserCreate=no"
-make_check_target=test
-makedepends="zlib-devel"
-short_desc="GIT Tree History Storage Tool -- for xbps-src use"
-maintainer="Enno Boland <gottox@voidlinux.org>"
-license="GPL-2.0-only"
-homepage="https://git-scm.com/"
-distfiles="https://www.kernel.org/pub/software/scm/git/git-${version}.tar.xz"
-checksum=9f61417a44d5b954a5012b6f34e526a3336dcf5dd720e2bb7ada92ad8b3d6680
+pkgname = "chroot-git"
+version = "2.31.1"
+revision = 1
+bootstrap = True
+wrksrc = f"git-{version}"
+build_style = "gnu_configure"
+configure_args = [
+    "--without-curl", "--without-openssl",
+    "--without-python", "--without-expat",
+    "--without-tcltk",
+    "ac_cv_lib_curl_curl_global_init=no",
+    "ac_cv_lib_expat_XML_ParserCreate=no"
+]
+make_check_target = "test"
+makedepends = ["zlib-devel"]
+short_desc = "GIT Tree History Storage Tool -- for xbps-src use"
+maintainer = "Enno Boland <gottox@voidlinux.org>"
+license = "GPL-2.0-only"
+homepage = "https://git-scm.com/"
+distfiles = [
+    f"https://www.kernel.org/pub/software/scm/git/git-{version}.tar.xz"
+]
+checksum = ["9f61417a44d5b954a5012b6f34e526a3336dcf5dd720e2bb7ada92ad8b3d6680"]
 
-if [ "$CHROOT_READY" ]; then
-	hostmakedepends="perl tar"
-else
-	configure_args+=" --with-zlib=${XBPS_MASTERDIR}/usr"
-fi
+if not bootstrapping:
+    hostmakedepends = ["perl", "tar"]
 
-post_configure() {
-	cat <<-EOF >config.mak
-	CC_LD_DYNPATH=-L
-	NO_INSTALL_HARDLINKS=Yes
-	NO_GETTEXT=Yes
-	EOF
-}
+def pre_configure(self):
+    from cbuild.core import paths
+    if self.bootstrapping:
+        self.configure_args += [
+            "--with-zlib", paths.masterdir() / "usr"
+        ]
 
-do_install() {
-	# remove unneeded stuff.
-	make DESTDIR=${wrksrc}/build-tmp install
+def post_configure(self):
+    with open(self.abs_wrksrc / "config.mak", "w") as f:
+        f.write("""
+CC_LD_DYNPATH=-L
+NO_INSTALL_HARDLINKS=Yes
+NO_GETTEXT=Yes
+""")
 
-	vbin ${wrksrc}/build-tmp/usr/bin/git chroot-git
-}
+def do_install(self):
+    import os
+
+    self.make.install([
+        "DESTDIR=" + str(self.chroot_wrksrc / "build-tmp")
+    ], default_args = False)
+    # remove unneeded stuff
+    os.rename(
+        self.abs_wrksrc / "build-tmp/usr/bin/git",
+        self.abs_wrksrc / "build-tmp/usr/bin/chroot-git"
+    )
+    self.install_bin("build-tmp/usr/bin/chroot-git")
