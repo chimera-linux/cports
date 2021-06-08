7b833280f116cf24da8e078cd5b6184248ab3559
Daniel Kolesa <daniel@octaforge.org>
2021-06-08 03:24:31 +0200

    chroot-grep: port

---
+++ srcpkgs/chroot-grep/template.py
@@ -0,0 +1,24 @@
+pkgname = "chroot-grep"
+version = "3.3"
+revision = 1
+wrksrc = f"grep-{version}"
+bootstrap = True
+build_style = "gnu_configure"
+configure_args = [
+    "--disable-perl-regexp", "--disable-nls", "ac_cv_path_GREP=grep"
+]
+short_desc = "The GNU grep utility - for use with xbps-src"
+maintainer = "Enno Boland <gottox@voidlinux.org>"
+license = "GPL-3.0-or-later"
+homepage = "http://www.gnu.org/software/grep/"
+
+from cbuild import sites
+
+distfiles = [f"{sites.gnu}/grep/grep-{version}.tar.xz"]
+checksum = ["b960541c499619efd6afe1fa795402e4733c8e11ebf9fafccc0bb4bccdc5b514"]
+conflicts = ["grep>=0"]
+provides = [f"grep-{version}_{revision}"]
+
+def post_install(self):
+    self.rmtree("usr/share/info")
+    self.rmtree("usr/share/man")
