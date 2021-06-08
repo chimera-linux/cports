74985b35f240296661f5827d5044596979aa9e64
Daniel Kolesa <daniel@octaforge.org>
2021-06-09 01:07:10 +0200

    zstd: port

---
+++ srcpkgs/zstd/template.py
@@ -0,0 +1,45 @@
+pkgname = "zstd"
+version = "1.5.0"
+revision = 1
+bootstrap = True
+makedepends = ["zlib-devel", "liblzma-devel", "liblz4-devel"]
+checkdepends = ["gtest-devel", "tar"]
+short_desc = "Fast real-time compression algorithm - CLI tool"
+maintainer = "Orphaned <orphan@voidlinux.org>"
+license = "BSD-3-Clause, GPL-2.0-or-later"
+homepage = "http://www.zstd.net"
+distfiles = [f"https://github.com/facebook/zstd/releases/download/v{version}/zstd-{version}.tar.gz"]
+checksum = ["5194fbfa781fcf45b98c5e849651aa7b3b0a008c6b72d4a0db760f3002291e94"]
+
+def do_build(self):
+    from cbuild.util import make
+    self.make = make.Make(self)
+    self.make.invoke("lib-mt")
+    self.make.build(["-C", "contrib/pzstd"])
+
+def do_install(self):
+    self.make.install(["PREFIX=/usr"])
+    self.make.install(["-C", "contrib/pzstd", "PREFIX=/usr"])
+    self.install_license("LICENSE")
+
+@subpackage("libzstd")
+def _lib(self):
+    self.short_desc = "Fast real-time compression algorithm"
+
+    def install():
+        self.take("usr/lib/*.so.*")
+
+    return install
+
+@subpackage("libzstd-devel")
+def _devel(self):
+    self.short_desc = "Fast real-time compression algorithm - development files"
+    self.depends = [f"libzstd-{version}_{revision}"]
+
+    def install():
+        self.take("usr/include")
+        self.take("usr/lib/pkgconfig")
+        self.take("usr/lib/*.so")
+        self.take("usr/lib/*.a")
+
+    return install
