e49b4d23e8c38780349239c11d0dd11e314ec602
Daniel Kolesa <daniel@octaforge.org>
2021-06-11 00:28:43 +0200

    base-chroot: use bsdgrep

--- srcpkgs/base-chroot/template.py
+++ srcpkgs/base-chroot/template.py
@@ -1,6 +1,6 @@
 pkgname = "base-chroot"
 version = "0.66"
-revision = 3
+revision = 4
 bootstrap = True
 short_desc = "Minimal set of packages required for chroot with xbps-src"
 maintainer = "Juan RP <xtraeme@gmail.com>"
@@ -10,7 +10,7 @@ homepage = "http://www.voidlinux.org"
 depends = [
     "musl-devel", "base-files", "binutils", "gcc", "patch", "sed", "findutils",
     "diffutils", "make", "gzip", "coreutils", "file", "bsdtar", "xbps",
-    "ncurses", "chroot-bash", "chroot-grep", "chroot-gawk", "chroot-git",
+    "ncurses", "bsdgrep", "chroot-bash", "chroot-gawk", "chroot-git",
     "chroot-util-linux"
 ]
 
