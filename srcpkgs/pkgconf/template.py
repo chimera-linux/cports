422ab6677a9356b9e6f7093edcf1397a053c3839
Daniel Kolesa <daniel@octaforge.org>
2021-06-10 03:10:33 +0200

    pkgconf: drop alternatives

--- srcpkgs/pkgconf/template.py
+++ srcpkgs/pkgconf/template.py
@@ -13,19 +13,11 @@ changelog = "https://raw.githubusercontent.com/pkgconf/pkgconf/master/NEWS"
 distfiles = [f"https://distfiles.dereferenced.org/pkgconf/pkgconf-{version}.tar.xz"]
 checksum = ["b846aea51cf696c3392a0ae58bef93e2e72f8e7073ca6ad1ed8b01c85871f9c0"]
 
-alternatives = [
-    ("pkg-config", "pkg-config", "/usr/bin/pkgconf"),
-    ("pkg-config", "pkg-config.1", "/usr/share/man/man1/pkgconf.1"),
-    ("pkg-config", "pkg.m4", "/usr/share/aclocal/pkg.m4.pkgconf")
-]
-
 def post_install(self):
     self.install_license("COPYING")
 
     import shutil
-
     shutil.rmtree(self.destdir / "usr/include")
-    shutil.move(
-        self.destdir / "usr/share/aclocal/pkg.m4",
-        self.destdir / "usr/share/aclocal/pkg.m4.pkgconf"
-    )
+
+    self.install_link("pkgconf", "usr/bin/pkg-config")
+    self.install_link("pkgconf.1", "usr/share/man/man1/pkg-config.1")
