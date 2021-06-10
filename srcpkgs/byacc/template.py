b8f2dfead05e69ad2e07d78ee2d0e623575cf668
Daniel Kolesa <daniel@octaforge.org>
2021-06-10 03:11:55 +0200

    byacc: drop alternatives

--- srcpkgs/byacc/template.py
+++ srcpkgs/byacc/template.py
@@ -10,10 +10,10 @@ homepage = "http://invisible-island.net/byacc/"
 distfiles = [f"ftp://ftp.invisible-island.net/{pkgname}/{pkgname}-{version}.tgz"]
 checksum = ["d7d31dae72cb973482ef7f975609ae401ccc12ee3fb168b67a69526c60afe43e"]
 
-alternatives = [
-    ("yacc", "yacc", "/usr/bin/byacc"),
-    ("yacc", "yacc.1", "/usr/share/man/man1/byacc.1")
-]
-
 def post_install(self):
     self.install_license("README", "LICENSE")
+
+    import shutil
+
+    self.install_link("byacc", "usr/bin/yacc")
+    self.install_link("byacc.1", "usr/share/man/man1/yacc.1")
