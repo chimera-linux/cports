013a10ef026d98e1ece5e74a33214cf2daf0bf45
Daniel Kolesa <daniel@octaforge.org>
2021-06-10 02:29:33 +0200

    musl: update to 1.2.2

--- srcpkgs/musl/template.py
+++ srcpkgs/musl/template.py
@@ -1,7 +1,6 @@
 pkgname = "musl"
-reverts = ["1.2.0_1"]
-version = "1.1.24"
-revision = 7
+version = "1.2.2"
+revision = 1
 archs = "*-musl"
 bootstrap = True
 build_style = "gnu_configure"
@@ -11,7 +10,7 @@ maintainer = "Enno Boland <gottox@voidlinux.org>"
 license = "MIT"
 homepage = "http://www.musl-libc.org/"
 distfiles = [f"http://www.musl-libc.org/releases/musl-{version}.tar.gz"]
-checksum = ["1370c9a812b2cf2a7d92802510cca0058cc37e66a7bedd70051f0a34015022a3"]
+checksum = ["9b969322012d796dc23dda27a35866034fa67d8fb67e0e2c45c913c3d43219dd"]
 
 shlib_provides = ["libc.so"]
 
