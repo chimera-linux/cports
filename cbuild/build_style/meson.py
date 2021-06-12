22ed74180255f7c27d8bef0d9bf85604f4a20c16
Daniel Kolesa <daniel@octaforge.org>
2021-06-12 19:08:43 +0200

    bsdutils: new package + add meson build_style

---
+++ cbuild/build_style/meson.py
@@ -0,0 +1,48 @@
+from cbuild.util import make
+
+def init_configure(self):
+    self.make = make.Make(self, wrksrc = "build")
+
+def do_configure(self):
+    self.do(
+        "meson", [
+            "--prefix=/usr",
+            "--libdir=/usr/lib",
+            "--libexecdir=/usr/libexec",
+            "--bindir=/usr/bin",
+            "--sbindir=/usr/bin",
+            "--includedir=/usr/include",
+            "--datadir=/usr/share",
+            "--mandir=/usr/share/man",
+            "--infodir=/usr/share/info",
+            "--sysconfdir=/etc",
+            "--localstatedir=/var",
+            "--sharedstatedir=/var/lib",
+            "--buildtype=plain",
+            "--auto-features=auto",
+            "--wrap-mode=nodownload",
+            "-Db_ndebug=true",
+            "-Db_staticpic=true"
+        ] + self.configure_args + [".", "build"], build = True
+    )
+
+def do_build(self):
+    self.make.build()
+
+def do_check(self):
+    pass
+
+def do_install(self):
+    self.make.install(default_args = False, env = {
+        "DESTDIR": str(self.chroot_destdir)
+    })
+
+def use(tmpl):
+    tmpl.build_style = "meson"
+    tmpl.init_configure = init_configure
+    tmpl.do_configure = do_configure
+    tmpl.do_build = do_build
+    tmpl.do_check = do_check
+    tmpl.do_install = do_install
+    tmpl.make_build_target = "all"
+    tmpl.make_cmd = "ninja"
---
+++ srcpkgs/bsdutils/template.py
@@ -0,0 +1,20 @@
+pkgname = "bsdutils"
+_commit="459414732b474a5c451771d2ccffb34d2224a4e0"
+version = "0.0.1"
+revision = 1
+wrksrc = f"bsdutils-{_commit}"
+bootstrap = True
+build_style = "meson"
+makedepends = [
+    "acl-devel", "ncurses-devel", "libedit-devel", "openssl-devel",
+    "musl-fts-devel", "musl-rpmatch-devel"
+]
+short_desc = "Alternative to GNU coreutils from FreeBSD"
+maintainer = "q66 <daniel@octaforge.org>"
+license = "BSD-2-Clause"
+homepage = "https://github.com/chimera-linux/bsdgrep"
+distfiles = [f"https://github.com/chimera-linux/bsdutils/archive/{_commit}.tar.gz"]
+checksum = ["fb007a201c8bcc2c164cfdb7ddb0821bfc629d2ed4fdd921f5495f8245af1884"]
+
+if not current.bootstrapping:
+    hostmakedepends = ["flex", "byacc", "meson", "pkgconf"]
