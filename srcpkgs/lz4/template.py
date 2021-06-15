75a090ba98e580a2d26895991c9288adb113334d
Daniel Kolesa <daniel@octaforge.org>
2021-06-15 03:57:38 +0200

    lz4: replace cursed build system

--- srcpkgs/lz4/template.py
+++ srcpkgs/lz4/template.py
@@ -3,9 +3,6 @@ version = "1.9.3"
 revision = 1
 bootstrap = True
 build_style = "gnu_makefile"
-make_cmd = "gmake"
-make_check_target = "test"
-checkdepends = ["python3"]
 short_desc = "LZ4 compression utilities"
 maintainer = "Alessio Sergi <al3hex@gmail.com>"
 license = "BSD-2-Clause, GPL-2.0-or-later"
@@ -13,6 +10,10 @@ homepage = "https://lz4.github.io/lz4"
 changelog = "https://raw.githubusercontent.com/lz4/lz4/dev/NEWS"
 distfiles = [f"https://github.com/lz4/lz4/archive/v{version}.tar.gz"]
 checksum = ["030644df4611007ff7dc962d981f390361e6c97a34e5cbc393ddfbe019ffe2c1"]
+
+def post_patch(self):
+    import shutil
+    shutil.copy(self.files_path / "Makefile", self.abs_wrksrc)
 
 def post_install(self):
     self.install_license("lib/LICENSE")
