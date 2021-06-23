d5423aa9d76f5dcff9ff77fbe893cbde3443d5f4
Daniel Kolesa <daniel@octaforge.org>
2021-06-23 20:07:40 +0200

    pax-utils: new package

---
+++ srcpkgs/pax-utils/template.py
@@ -0,0 +1,18 @@
+pkgname = "pax-utils"
+version = "1.3.2"
+revision = 1
+build_style = "gnu_configure"
+configure_args = ["--with-caps"]
+hostmakedepends = ["pkgconf"]
+makedepends = ["libcap-devel"]
+short_desc = "PaX aware and related utilities for ELF binaries"
+maintainer = "q66 <daniel@octaforge.org>"
+license = "GPL-2.0-only"
+homepage = "https://cgit.gentoo.org/proj/pax-utils.git"
+distfiles = [f"https://gentoo.osuosl.org/distfiles/{pkgname}-{version}.tar.xz"]
+checksum = ["02eba0c305ad349ad6ff1f30edae793061ce95680fd5bdee0e14caf731dee1e7"]
+
+def post_install(self):
+    # bash scripts
+    (self.destdir / "usr/bin/lddtree").unlink(missing_ok = True)
+    (self.destdir / "usr/bin/symtree").unlink(missing_ok = True)
