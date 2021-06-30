2e7f02ea7a21eae3f9367772ba6a1b8da3f04f6a
Daniel Kolesa <daniel@octaforge.org>
2021-07-01 01:06:02 +0200

    bsdm4: new package

---
+++ srcpkgs/bsdm4/template.py
@@ -0,0 +1,21 @@
+pkgname = "bsdm4"
+version = "0.99.0"
+revision = 1
+bootstrap = True
+build_style = "gnu_makefile"
+make_build_args = ["YACC=byacc"]
+short_desc = "The m4(1) utility from FreeBSD"
+maintainer = "q66 <daniel@octaforge.org>"
+license = "BSD-2-Clause"
+homepage = "https://github.com/chimera-linux/bsdm4"
+distfiles = [f"https://github.com/chimera-linux/bsdm4/archive/refs/tags/v{version}.tar.gz"]
+checksum = ["79169016a8fdfbea2284b5e576807e0e32e46ae98561b57888b7898bf45350c0"]
+
+if not current.bootstrapping:
+    hostmakedepends = ["byacc"]
+
+def post_extract(self):
+    import shutil
+    # pre-bootstrapped copies to avoid dependency cycle with flex
+    shutil.copy(self.files_path / "tokenizer.c", self.abs_wrksrc)
+    shutil.copy(self.files_path / "tokenizer.h", self.abs_wrksrc)
