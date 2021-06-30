5a6b46e2888e26a11ca3fb0e8c6f84d7f514875c
Daniel Kolesa <daniel@octaforge.org>
2021-06-30 22:33:40 +0200

    bsdutils: update

--- srcpkgs/bsdutils/template.py
+++ srcpkgs/bsdutils/template.py
@@ -1,20 +1,20 @@
 pkgname = "bsdutils"
-_commit="4f1cb254ad5549ddcaf2cf5cb2ceb5c84b839e52"
+_commit="cd3f2427f59d40851d0781ae5e9c1cd479ffade4"
 version = "0.0.1"
-revision = 2
+revision = 3
 wrksrc = f"bsdutils-{_commit}"
 bootstrap = True
 build_style = "meson"
 makedepends = [
     "acl-devel", "ncurses-devel", "libedit-devel", "openssl-devel",
-    "musl-fts-devel", "musl-rpmatch-devel"
+    "musl-fts-devel", "musl-rpmatch-devel", "libxo-devel"
 ]
 short_desc = "Alternative to GNU coreutils from FreeBSD"
 maintainer = "q66 <daniel@octaforge.org>"
 license = "BSD-2-Clause"
 homepage = "https://github.com/chimera-linux/bsdutils"
 distfiles = [f"https://github.com/chimera-linux/bsdutils/archive/{_commit}.tar.gz"]
-checksum = ["d464de8b3111aa794b43c08ef142d63ba46947bae481e07abb10b6cbfcba8ccb"]
+checksum = ["cb03da705357ee2b24a55f3ccae9943749e1f39bdc9f574972842637f9dfa988"]
 
 if not current.bootstrapping:
     hostmakedepends = ["flex", "byacc", "meson", "pkgconf"]
