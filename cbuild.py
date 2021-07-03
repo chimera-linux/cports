10b4ca503fdb95ec029f3fe879243159bf756cc7
Daniel Kolesa <daniel@octaforge.org>
2021-07-03 05:46:21 +0200

    cbuild: clean up main program a bit

--- cbuild.py
+++ cbuild.py
@@ -9,20 +9,52 @@ import importlib
 import traceback
 import configparser
 
-from os import path
-
-from cbuild.core import paths
-
-paths.init(path.dirname(__file__), "masterdir", "hostdir")
-
 # start from a sane directory
-os.chdir(path.dirname(__file__))
+os.chdir(os.path.dirname(__file__))
 
-from cbuild.util import make
-from cbuild.core import chroot, logger, template, build
-from cbuild import cpu
+# ensure files are created with sane permissions
+os.umask(0o022)
 
-from cbuild.apk import sign
+# we should always be able to import modules from here
+sys.path.append(os.path.dirname(__file__))
+
+def do_exit(signum, stack):
+    raise Exception("cbuild: interrupted!")
+
+# exit handler
+signal.signal(signal.SIGINT, do_exit)
+signal.signal(signal.SIGTERM, do_exit)
+
+# program checks
+if not shutil.which("bwrap"):
+    sys.exit("Required program not found: bwrap")
+
+# global options
+
+opt_gen_dbg   = False
+opt_skipexist = False
+opt_makejobs  = 1
+opt_nocolor   = "NO_COLOR" in os.environ
+opt_signkey   = None
+opt_force     = False
+
+# parse config file and set the global options from it
+
+global_cfg = configparser.ConfigParser()
+global_cfg.read("etc/config.ini")
+
+if "general" in global_cfg:
+    gencfg = global_cfg["general"]
+
+    opt_gen_dbg  = gencfg.getboolean("build_dbg", fallback = False)
+    opt_makejobs = gencfg.getint("jobs", fallback = 1)
+
+if "signing" in global_cfg:
+    signcfg = global_cfg["signing"]
+
+    opt_signkey = signcfg.get("key", fallback = None)
+
+# parse command line arguments
 
 parser = argparse.ArgumentParser(description = "Chimera Linux build system.")
 
@@ -49,40 +81,40 @@ parser.add_argument(
 )
 parser.add_argument("command", nargs = "+", help = "The command to issue.")
 
-args = parser.parse_args()
+cmdline = parser.parse_args()
 
-cmd = args.command
+# command line args override config file
 
-opt_gen_dbg = False
+if cmdline.jobs:
+    opt_makejobs = int(cmdline.jobs)
 
-# read global configuration
-
-global_cfg = configparser.ConfigParser()
-global_cfg.read("etc/config.ini")
-
-if "general" in global_cfg:
-    gencfg = global_cfg["general"]
-    make.set_jobs(gencfg.getint("jobs", fallback = 1))
-    opt_gen_dbg = gencfg.getboolean("build_dbg", fallback = False)
-
-signkey = None
-
-if "signing" in global_cfg:
-    signcfg = global_cfg["signing"]
-    signkey = signcfg.get("key", fallback = None)
-
-# set args options
-
-if args.jobs:
-    make.set_jobs(int(args.jobs))
-
-if args.build_dbg:
+if cmdline.build_dbg:
     opt_gen_dbg = True
 
-# ensure files are created with sane permissions
-os.umask(0o022)
+if cmdline.no_color:
+    opt_nocolor = True
 
-logger.init(not args.no_color and not "NO_COLOR" in os.environ)
+if cmdline.force:
+    opt_force = True
+
+if cmdline.skip_if_exists:
+    opt_skipexist = True
+
+# set global config bits as needed
+
+from cbuild.core import paths
+
+# init paths early, modules rely on it
+paths.init(os.path.dirname(__file__), "masterdir", "hostdir")
+
+from cbuild.util import make
+from cbuild.core import chroot, logger, template, build
+from cbuild.apk import sign
+from cbuild import cpu
+
+make.set_jobs(opt_makejobs)
+
+logger.init(not opt_nocolor)
 
 # check masterdir and while at it perform arch checks
 chroot.chroot_check()
@@ -90,10 +122,7 @@ chroot.chroot_check()
 # fix up environment
 os.environ["CBUILD_ARCH"] = cpu.host()
 os.environ["PATH"] = os.environ["PATH"] + ":" + \
-    path.join(paths.masterdir(), "usr/bin")
-
-# we should always be able to import modules from here
-sys.path.append(path.dirname(__file__))
+    str(paths.masterdir() / "usr/bin")
 
 # create necessary directories
 os.makedirs(paths.masterdir(), exist_ok = True)
@@ -101,27 +130,17 @@ os.makedirs(paths.hostdir(), exist_ok = True)
 os.makedirs(paths.repository(), exist_ok = True)
 os.makedirs(paths.sources(), exist_ok = True)
 
-if not shutil.which("bwrap"):
-    sys.exit("Required program not found: bwrap")
-
-def do_exit(signum, stack):
-    raise Exception("cbuild: interrupted!")
-
-# exit handler
-signal.signal(signal.SIGINT, do_exit)
-signal.signal(signal.SIGTERM, do_exit)
-
 def binary_bootstrap(tgt):
-    if len(cmd) <= 1:
+    if len(cmdline.command) <= 1:
         chroot.install(cpu.host())
     else:
-        chroot.install(cmd[1])
+        chroot.install(cmdline.command[1])
 
 def bootstrap(tgt):
     rp = template.read_pkg("base-chroot", False, True, False, False, None)
     chroot.initdb()
     chroot.repo_sync()
-    build.build(tgt, rp, {}, signkey)
+    build.build(tgt, rp, {}, opt_signkey)
     shutil.rmtree(paths.masterdir())
     chroot.install(cpu.host())
 
@@ -129,15 +148,15 @@ def bootstrap_update(tgt):
     chroot.update()
 
 def do_keygen(tgt):
-    if len(cmd) >= 3:
-        keyn, keysize = cmd[1], int(cmd[2])
-    elif len(cmd) >= 2:
-        keyn, keysize = cmd[1], 2048
+    if len(cmdline.command) >= 3:
+        keyn, keysize = cmdline.command[1], int(cmdline.command[2])
+    elif len(cmdline.command) >= 2:
+        keyn, keysize = cmdline.command[1], 2048
     else:
         keyn, keysize = None, 2048
 
     if not keyn or len(keyn) == 0:
-        keyn = signkey
+        keyn = opt_signkey
 
     sign.keygen(keyn, keysize)
 
@@ -171,15 +190,15 @@ def do_remove_autodeps(tgt):
     chroot.remove_autodeps(None)
 
 def do_pkg(tgt):
-    pkgn = cmd[1] if len(cmd) >= 1 else None
+    pkgn = cmdline.command[1] if len(cmdline.command) >= 1 else None
     rp = template.read_pkg(
-        pkgn, args.force, False, args.skip_if_exists, opt_gen_dbg, None
+        pkgn, opt_force, False, opt_skipexist, opt_gen_dbg, None
     )
     # don't remove builddir/destdir
     chroot.repo_sync()
     chroot.update(do_clean = False)
     chroot.remove_autodeps(False)
-    build.build(tgt, rp, {}, signkey)
+    build.build(tgt, rp, {}, opt_signkey)
 
 def do_bad(tgt):
     logger.get().out_red("cbuild: invalid target " + tgt)
@@ -205,7 +224,7 @@ try:
         "check": do_pkg,
         "install": do_pkg,
         "pkg": do_pkg
-    }).get(cmd[0], do_bad)(cmd[0])
+    }).get(cmdline.command[0], do_bad)(cmdline.command[0])
 except template.SkipPackage:
     pass
 except:
