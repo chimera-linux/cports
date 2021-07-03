25225a575c0127c4e71477863819fee3c60b32cf
Daniel Kolesa <daniel@octaforge.org>
2021-07-03 19:20:05 +0200

    cbuild: fix bootstrap profile

--- cbuild/build_profiles/bootstrap.py
+++ cbuild/build_profiles/bootstrap.py
@@ -1,5 +1,5 @@
 # putting stuff in cflags/cxxflags has annoying warnings but it works around
 # potentially broken build systems not accounting for ldflags
 CBUILD_TARGET_CFLAGS = ["-O2", "-pipe", "-rtlib=compiler-rt"]
-CBUILD_TARGET_CXXFLAGS = CBUILD_CFLAGS + ["-stdlib=libc++", "-unwindlib=libunwind"]
+CBUILD_TARGET_CXXFLAGS = CBUILD_TARGET_CFLAGS + ["-stdlib=libc++", "-unwindlib=libunwind"]
 CBUILD_TARGET_LDFLAGS = ["-fuse-ld=lld"]
