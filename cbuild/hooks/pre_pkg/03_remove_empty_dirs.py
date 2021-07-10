a95e2e2d49655a6877a7248ee8c9bee528698a04
Daniel Kolesa <q66@chimera-linux.org>
2021-07-10 02:54:20 +0200

    cbuild: fix 03_remove_empty_dirs

--- cbuild/hooks/pre_pkg/03_remove_empty_dirs.py
+++ cbuild/hooks/pre_pkg/03_remove_empty_dirs.py
@@ -3,13 +3,18 @@ def clean_empty(pkg, dpath):
 
     for f in dpath.iterdir():
         if f.is_dir() and not f.is_symlink():
-            clean_empty(pkg, f)
-        empty = False
+            if not clean_empty(pkg, f):
+                empty = False
+        else:
+            empty = False
 
     if empty and dpath != pkg.destdir:
         pstr = str(dpath.relative_to(pkg.destdir))
         pkg.log_warn(f"removed empty directory: {pstr}")
         dpath.rmdir()
+        return True
+
+    return False
 
 def invoke(pkg):
     clean_empty(pkg, pkg.destdir)
