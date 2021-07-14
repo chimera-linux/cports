2dd50295a3d6346581792ec1b7cb40d12f2dd398
Daniel Kolesa <q66@chimera-linux.org>
2021-07-14 03:18:24 +0200

    chroot-util-linux: switch to gmake for now to fix issues

--- srcpkgs/chroot-util-linux/template.py
+++ srcpkgs/chroot-util-linux/template.py
@@ -16,6 +16,7 @@ configure_args = [
     "--disable-runuser", "--disable-setpriv", "--disable-libsmartcols",
     "--without-readline", "scanf_cv_alloc_modifier=as"
 ]
+make_cmd = "gmake"
 makedepends = ["zlib-devel"]
 short_desc = "Miscellaneous linux utilities -- for xbps-src use"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -24,6 +25,9 @@ homepage = "http://userweb.kernel.org/~kzak/util-linux-ng"
 
 from cbuild import sites
 
+if not current.bootstrapping:
+	hostmakedepends = ["gmake"]
+
 distfiles = [
     f"{sites.kernel}/utils/util-linux/v{_mver}/util-linux-{version}.tar.xz"
 ]
@@ -31,12 +35,7 @@ checksum = ["86e6707a379c7ff5489c218cfaf1e3464b0b95acf7817db0bc5f179e356a67b2"]
 conflicts = ["util-linux"]
 
 def do_build(self):
-    # there's something broken about the build system that sometimes
-    # fails during the first pass regarding manpage generation
-    try:
-        self.make.build()
-    except:
-        self.make.build()
+    self.make.build()
 
 def post_install(self):
     # Remove unused stuff
