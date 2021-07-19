2d1e3e97dcf0bfbb8ba47e103dffb93168d168ca
Daniel Kolesa <q66@chimera-linux.org>
2021-07-19 03:05:45 +0200

    bmake: fix cross build

--- srcpkgs/bmake/template.py
+++ srcpkgs/bmake/template.py
@@ -13,18 +13,25 @@ checksum = ["47e551293843d504fe4197413aa6d7ee003090669ac200932ff40e9ccb4658aa"]
 def do_build(self):
     import os
     os.makedirs(self.abs_wrksrc / "build", exist_ok = True)
+    eargs = []
+    if self.cross_build:
+        eargs = ["--host=" + self.build_profile.short_triplet]
     self.do(
-        self.chroot_wrksrc / "boot-strap", ["--prefix=/usr", "op=build"],
+        self.chroot_wrksrc / "boot-strap",
+        eargs + ["--prefix=/usr", "op=build"],
         wrksrc = "build"
     )
 
 def do_install(self):
     import shutil
+    eargs = []
+    if self.cross_build:
+        eargs = ["BMAKE=make"]
     self.do(
         self.chroot_wrksrc / "boot-strap", [
             "--prefix=/usr", "--install-destdir=" + str(self.chroot_destdir),
             "op=install"
-        ],
+        ] + eargs,
         wrksrc = "build"
     )
     shutil.rmtree(self.destdir / "usr/share/man")
