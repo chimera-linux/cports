374bc1bef634e13bf2f7929ad34804d96d15bddf
yopito <pierre.bourgin@free.fr>
2021-07-31 23:55:11 +0200

    cbuild: need some str() convertion back
    partial revert of 9ce7f5d959eabc6f436bb72c92370318eb076690

--- cbuild/core/profile.py
+++ cbuild/core/profile.py
@@ -173,9 +173,9 @@ class Profile:
 
         # bootstrap
         if not self._triplet:
-            bflags = ["-isystem", paths.masterdir() / "usr/include"]
+            bflags = ["-isystem", str(paths.masterdir() / "usr/include")]
         elif self.cross:
-            bflags = ["--sysroot", self.sysroot]
+            bflags = ["--sysroot", str(self.sysroot)]
         else:
             bflags = []
 
@@ -196,9 +196,9 @@ class Profile:
 
         # bootstrap
         if not self._triplet:
-            bflags = ["-isystem", paths.masterdir() / "usr/include"]
+            bflags = ["-isystem", str(paths.masterdir() / "usr/include")]
         elif self.cross:
-            bflags = ["--sysroot", self.sysroot]
+            bflags = ["--sysroot", str(self.sysroot)]
         else:
             bflags = []
 
@@ -219,9 +219,9 @@ class Profile:
 
         # bootstrap
         if not self._triplet:
-            bflags = ["-isystem", paths.masterdir() / "usr/include"]
+            bflags = ["-isystem", str(paths.masterdir() / "usr/include")]
         elif self.cross:
-            bflags = ["--sysroot", self.sysroot]
+            bflags = ["--sysroot", str(self.sysroot)]
         else:
             bflags = []
 
@@ -245,7 +245,7 @@ class Profile:
                 "-Wl,-rpath-link=" + str(paths.masterdir() / "usr/lib")
             ]
         elif self.cross:
-            bflags = ["--sysroot", self.sysroot]
+            bflags = ["--sysroot", str(self.sysroot)]
         else:
             bflags = []
 
