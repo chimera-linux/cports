d2872e2cf06c35dab74bc6efb56721dafafe08dc
Daniel Kolesa <q66@chimera-linux.org>
2021-10-11 01:56:52 +0200

    main/bsdsed: lint

--- main/bsdsed/template.py
+++ main/bsdsed/template.py
@@ -2,11 +2,11 @@ pkgname = "bsdsed"
 pkgver = "0.99.2"
 pkgrel = 0
 build_style = "makefile"
-pkgdesc = "The sed(1) utility from FreeBSD"
+pkgdesc = "FreeBSD sed(1) utility"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/bsdsed"
 source = f"https://github.com/chimera-linux/bsdsed/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "4e2e5df15c3f9c0594f4ba1b9d243c5e7aa87abac8721716635bb872eef46229"
-
-options = ["bootstrap", "!check", "!lint"]
+# no test suite
+options = ["bootstrap", "!check"]
