908dce2539af0195b25ed28a801af09fe977ed5d
Daniel Kolesa <q66@chimera-linux.org>
2021-10-11 01:48:39 +0200

    main/bsddiff: enable lint option

--- main/bsddiff/template.py
+++ main/bsddiff/template.py
@@ -8,8 +8,8 @@ license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/bsdutils"
 source = f"https://github.com/chimera-linux/{pkgname}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "9505436bc26b7a9ba7efed7e67194f1fc21ff3b3b4c968277c96d3da25676ca1"
-
-options = ["bootstrap", "!check", "!lint"]
+# no test suite
+options = ["bootstrap", "!check"]
 
 if not current.bootstrapping:
     hostmakedepends = ["meson"]
