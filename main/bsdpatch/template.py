4c4b210ff3d01be446b51aea18919613742e5361
Daniel Kolesa <q66@chimera-linux.org>
2021-10-11 01:56:16 +0200

    main/bsdpatch: lint

--- main/bsdpatch/template.py
+++ main/bsdpatch/template.py
@@ -2,11 +2,11 @@ pkgname = "bsdpatch"
 pkgver = "0.99.1"
 pkgrel = 0
 build_style = "makefile"
-pkgdesc = "The patch(1) utility from FreeBSD"
+pkgdesc = "FreeBSD patch(1) utility"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/bsdpatch"
 source = f"https://github.com/chimera-linux/bsdpatch/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "ad031c86b23ed776697f77f1a3348cd7129835965d4ee9966bc50e65c97703e8"
-
-options = ["bootstrap", "!check", "!lint"]
+# no test suite
+options = ["bootstrap", "!check"]
