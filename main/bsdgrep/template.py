9a5742bd7ddd0dceaf0d84931d45fc03994e7aa8
Daniel Kolesa <q66@chimera-linux.org>
2021-10-11 01:49:57 +0200

    main/bsdgrep: enable lnit option

--- main/bsdgrep/template.py
+++ main/bsdgrep/template.py
@@ -9,5 +9,5 @@ license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/bsdgrep"
 source = f"https://github.com/chimera-linux/bsdgrep/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "839a505c5d5f6d53069e1863ed264d33120db01df29a606441a67cd40ce032ea"
-
-options = ["bootstrap", "!check", "!lint"]
+# no test suite
+options = ["bootstrap", "!check"]
