2a1fa5c14ce96328927910a3e77c28a357fafcea
Daniel Kolesa <q66@chimera-linux.org>
2021-10-11 00:01:58 +0200

    main/acl: clean up template, partially fix test suite

--- main/acl/template.py
+++ main/acl/template.py
@@ -7,33 +7,25 @@ configure_args = [
     f"--libexecdir=/usr/lib"
 ]
 makedepends = ["attr-devel"]
+checkdepends = ["perl"]
 pkgdesc = "Access Control List filesystem support"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://savannah.nongnu.org/projects/acl"
 source = f"$(NONGNU_SITE)/acl/acl-{pkgver}.tar.gz"
 sha256 = "760c61c68901b37fdd5eefeeaf4c0c7a26bdfdd8ac747a1edff1ce0e243c11af"
-options = ["bootstrap", "!check", "!lint"]
+# test suite makes assumptions about a GNU environment
+options = ["bootstrap", "!check"]
 
 if not current.bootstrapping:
     hostmakedepends = ["pkgconf"]
 
 @subpackage("acl-devel")
 def _devel(self):
-    self.depends = ["attr-devel", f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.depends += ["attr-devel"]
 
-    return [
-        "usr/include",
-        "usr/lib/*.a",
-        "usr/lib/*.so",
-        "usr/lib/pkgconfig",
-        "usr/share/man/man[235]",
-        "usr/share/doc"
-    ]
+    return self.default_devel(extra = ["usr/share/man/man5"])
 
 @subpackage("acl-progs")
 def _progs(self):
-    return [
-        "usr/bin",
-        "usr/share"
-    ]
+    return self.default_progs(extra = ["usr/share"])
