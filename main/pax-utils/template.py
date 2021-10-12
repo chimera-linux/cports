97bde8f724785befb8c7b9b33e4fc9ccd5f79de0
Daniel Kolesa <q66@chimera-linux.org>
2021-10-12 03:59:04 +0200

    main/pax-utils: lint

--- main/pax-utils/template.py
+++ main/pax-utils/template.py
@@ -11,8 +11,8 @@ license = "GPL-2.0-only"
 url = "https://cgit.gentoo.org/proj/pax-utils.git"
 source = f"https://gentoo.osuosl.org/sources/{pkgname}-{pkgver}.tar.xz"
 sha256 = "02eba0c305ad349ad6ff1f30edae793061ce95680fd5bdee0e14caf731dee1e7"
-
-options = ["!check", "!lint"]
+# weird test suite and needs bash etc
+options = ["!check"]
 
 def post_install(self):
     # bash scripts
