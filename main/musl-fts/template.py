10efa5d3c501d52314f67950dbe075576587c71d
Daniel Kolesa <q66@chimera-linux.org>
2021-10-12 23:21:55 +0200

    main/musl-fts: cleanup, lint

--- main/musl-fts/template.py
+++ main/musl-fts/template.py
@@ -9,8 +9,8 @@ license = "BSD-3-Clause"
 url = "https://github.com/chimera-linux/musl-fts"
 source = f"https://github.com/chimera-linux/{pkgname}/archive/refs/tags/v{pkgver}-mk2.tar.gz"
 sha256 = "1f65612b523e7040dbd9d5579a2eca97ede79c2ff3f91db7ccc288263e60da50"
-
-options = ["bootstrap", "!check", "!lint"]
+# no test suite
+options = ["bootstrap", "!check"]
 
 if not current.bootstrapping:
     hostmakedepends = ["pkgconf"]
@@ -20,9 +20,4 @@ def post_install(self):
 
 @subpackage("musl-fts-devel")
 def _devel(self):
-    self.depends = [f"{pkgname}={pkgver}-r{pkgrel}"]
-
-    return [
-        "usr/share/man", "usr/include", "usr/lib/*.a", "usr/lib/*.so",
-        "usr/lib/pkgconfig"
-    ]
+    return self.default_devel(man = True)
