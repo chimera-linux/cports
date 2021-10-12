7fbf792770229f166badfbc268bf4dc5de6a3c6a
Daniel Kolesa <q66@chimera-linux.org>
2021-10-12 04:27:19 +0200

    main/libcap: lint, run tests

--- main/libcap/template.py
+++ main/libcap/template.py
@@ -2,7 +2,6 @@ pkgname = "libcap"
 pkgver = "2.49"
 pkgrel = 0
 build_style = "makefile"
-hostmakedepends = ["pkgconf", "gmake", "perl"]
 make_cmd = "gmake"
 make_build_args = ["GOLANG=no"]
 make_install_args = [
@@ -12,37 +11,29 @@ make_install_args = [
     "exec_prefix=/usr",
     "RAISE_SETFCAP=no",
 ]
+make_check_target = "test"
 make_use_env = True
+hostmakedepends = ["pkgconf", "gmake", "perl"]
 makedepends = ["attr-devel"]
 pkgdesc = "POSIX.1e capabilities suite"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
-url = "http://sites.google.com/site/fullycapable/"
+url = "http://sites.google.com/site/fullycapable"
 source = f"$(KERNEL_SITE)/libs/security/linux-privs/libcap2/{pkgname}-{pkgver}.tar.xz"
 sha256 = "e98bc4d93645082ec787730b0fd1a712b38882465c505777de17c338831ee181"
 
-options = ["!check", "!lint"]
-
 def init_configure(self):
-    self.make_build_args += [
+    eargs = [
         "CC=" + self.get_tool("CC"),
         "BUILD_CC=" + self.get_tool("CC", target = "host"),
     ]
+    self.make_build_args += eargs
+    self.make_check_args += eargs
 
 @subpackage("libcap-devel")
 def _devel(self):
-    self.depends = [f"{pkgname}={pkgver}-r{pkgrel}"]
-
-    return [
-        "usr/include",
-        "usr/lib/libcap.a",
-        "usr/lib/libpsx.a",
-        "usr/lib/libcap.so",
-        "usr/lib/libpsx.so",
-        "usr/lib/pkgconfig",
-        "usr/share/man/man3",
-    ]
+    return self.default_devel(man = True)
 
 @subpackage("libcap-progs")
 def _progs(self):
-    return ["usr/bin", "usr/share"]
+    return self.default_progs(extra = ["usr/share"])
