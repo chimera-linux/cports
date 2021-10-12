f530a151c4125f4008de64d19057cde48d13d676
Daniel Kolesa <q66@chimera-linux.org>
2021-10-12 04:32:10 +0200

    main/file: run tests, lint, cleanup

--- main/file/template.py
+++ main/file/template.py
@@ -10,11 +10,10 @@ makedepends = ["zlib-devel"]
 pkgdesc = "File type identification utility"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
-url = "http://www.darwinsys.com/file/"
-source = f"https://astron.com/pub/file/file-{pkgver}.tar.gz"
+url = "http://www.darwinsys.com/file"
+source = f"https://astron.com/pub/{pkgname}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "167321f43c148a553f68a0ea7f579821ef3b11c27b8cbe158e4df897e4a5dd57"
-
-options = ["bootstrap", "!check", "!lint"]
+options = ["bootstrap"]
 
 if not current.bootstrapping:
     hostmakedepends = ["pkgconf"]
@@ -29,21 +28,14 @@ def post_install(self):
 def _libmagic(self):
     self.pkgdesc = "File type identification library"
 
-    return [
-        "usr/lib/*.so.*",
+    return self.default_libs(extra = [
         "usr/share/misc",
         "usr/share/man/man4",
-    ]
+    ])
 
 @subpackage("file-devel")
 def _devel(self):
-    self.depends = makedepends + [f"libmagic={pkgver}-r{pkgrel}"]
+    self.depends += makedepends
     self.pkgdesc = "File type identification library (development files)"
 
-    return [
-        "usr/include",
-        "usr/lib/*.a",
-        "usr/lib/*.so",
-        "usr/lib/pkgconfig",
-        "usr/share/man/man3",
-    ]
+    return self.default_devel(man = True)
