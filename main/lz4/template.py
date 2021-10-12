874f05739f2830997a9ac30b093bc502ce0c185a
Daniel Kolesa <q66@chimera-linux.org>
2021-10-12 05:20:21 +0200

    main/lz4: lint/check, run tests with -j1

--- main/lz4/template.py
+++ main/lz4/template.py
@@ -2,16 +2,15 @@ pkgname = "lz4"
 pkgver = "1.9.3"
 pkgrel = 0
 build_style = "makefile"
+make_cmd = "gmake"
+make_check_args = ["-j1"]
 pkgdesc = "LZ4 compression utilities"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause AND GPL-2.0-or-later"
 url = "https://lz4.github.io/lz4"
 source = f"https://github.com/lz4/lz4/archive/v{pkgver}.tar.gz"
 sha256 = "030644df4611007ff7dc962d981f390361e6c97a34e5cbc393ddfbe019ffe2c1"
-
-options = ["bootstrap", "!check", "!lint"]
-
-make_cmd = "gmake"
+options = ["bootstrap"]
 
 if not current.bootstrapping:
     hostmakedepends = ["pkgconf", "gmake"]
@@ -23,16 +22,10 @@ def post_install(self):
 def _lib(self):
     self.pkgdesc = "LZ4 compression library"
 
-    return ["usr/lib/*.so.*"]
+    return self.default_libs()
 
 @subpackage("liblz4-devel")
 def _devel(self):
     self.short_decs = "LZ4 compression library (development files)"
-    self.depends = [f"liblz4={pkgver}-r{pkgrel}"]
 
-    return [
-        "usr/include",
-        "usr/lib/*.a",
-        "usr/lib/*.so",
-        "usr/lib/pkgconfig",
-    ]
+    return self.default_devel()
