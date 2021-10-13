95585135220a5935bbd0c467d740eba01387c971
Daniel Kolesa <q66@chimera-linux.org>
2021-10-13 05:02:35 +0200

    main/python-setuptools: lint, check

--- main/python-setuptools/template.py
+++ main/python-setuptools/template.py
@@ -11,8 +11,6 @@ url = "https://github.com/pypa/setuptools"
 source = f"$(PYPI_SITE)/s/setuptools/setuptools-{pkgver}.tar.gz"
 sha256 = "401cbf33a7bf817d08014d51560fc003b895c4cdc1a5b521ad2969e928a07535"
 
-options = ["!check", "!lint"]
-
 env = {
     "SETUPTOOLS_INSTALL_WINDOWS_SPECIFIC_FILES": "0",
     "SETUPTOOLS_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT": "1"
--- src/cbuild/build_style/python_module.py
+++ src/cbuild/build_style/python_module.py
@@ -1,12 +1,26 @@
+from cbuild.core import chroot
+
 def do_build(self):
-    self.do("python", ["setup.py", "build"] + self.make_build_args)
+    self.do("python3", ["setup.py", "build"] + self.make_build_args)
 
 def do_check(self):
-    pass
+    if chroot.enter(
+        "python3", ["-c", "import pytest"], capture_out = True,
+        ro_root = True, ro_build = True, unshare_all = True
+    ).returncode == 0:
+        self.do(
+            "python3",
+            ["-m", "pytest"] + self.make_check_args + [self.make_check_target]
+        )
+    else:
+        self.do(
+            "python3",
+            ["setup.py", self.make_check_target] + self.make_check_args
+        )
 
 def do_install(self):
     self.do(
-        "python", [
+        "python3", [
             "setup.py", "install", "--prefix=/usr",
             "--root=" + str(self.chroot_destdir)
         ] + self.make_install_args
