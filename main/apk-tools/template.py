c0780ae381ac2a90be96ab841dbcb1529a4aa6d9
Daniel Kolesa <q66@chimera-linux.org>
2021-10-13 23:07:12 +0200

    main/apk-tools: update to 2.12.7

--- main/apk-tools/template.py
+++ main/apk-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "apk-tools"
-pkgver = "2.12.5"
+pkgver = "2.12.7"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -13,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "http://git.alpinelinux.org/cgit/apk-tools"
 source = f"http://git.alpinelinux.org/cgit/{pkgname}/snapshot/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "a3cbabbcd3072f197b19f85e13e526b8b769d1e537f8156457b1779bcc9300fe"
+sha256 = "269831b60d0008d3f4420293971ebbe951b04ee72f8359f2cc3ee89e649b1705"
 tool_flags = {
     "CFLAGS": ["-Wno-error"]
 }
