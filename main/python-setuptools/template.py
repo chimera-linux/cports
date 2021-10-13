3ebb8db1a45250cb0f7bf9794921e6e16db7128c
Daniel Kolesa <q66@chimera-linux.org>
2021-10-13 22:55:48 +0200

    main/python-setuptools: update to 58.1.0

--- main/python-setuptools/template.py
+++ main/python-setuptools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-setuptools"
-pkgver = "57.0.0"
+pkgver = "58.1.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-devel"]
@@ -9,8 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pypa/setuptools"
 source = f"$(PYPI_SITE)/s/setuptools/setuptools-{pkgver}.tar.gz"
-sha256 = "401cbf33a7bf817d08014d51560fc003b895c4cdc1a5b521ad2969e928a07535"
-
+sha256 = "5de67252090e08d25f240f07d80310f778a5a46cdcf9ea9855662630ac8547b2"
 env = {
     "SETUPTOOLS_INSTALL_WINDOWS_SPECIFIC_FILES": "0",
     "SETUPTOOLS_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT": "1"
