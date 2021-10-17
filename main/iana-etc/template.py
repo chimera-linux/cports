f6fa50a940dc27c891dfb78f52f8c827c48689da
Daniel Kolesa <q66@chimera-linux.org>
2021-10-17 05:49:35 +0200

    main/iana-etc: new package (20211004)

---
+++ main/iana-etc/template.py
@@ -0,0 +1,13 @@
+pkgname = "iana-etc"
+pkgver = "20211004"
+pkgrel = 0
+pkgdesc = "Unix /etc/services and /etc/protocols files"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:none"
+url = f"https://www.iana.org/protocols"
+# no tests
+options = ["!check"]
+
+def do_install(self):
+    self.install_file(self.files_path / "protocols", "etc")
+    self.install_file(self.files_path / "services", "etc")
