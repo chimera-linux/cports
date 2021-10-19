a6833bea248f43895d86b96a68f5cb44d980f947
Daniel Kolesa <q66@chimera-linux.org>
2021-10-19 04:47:37 +0200

    main/base-full: depend on nano

--- main/base-full/template.py
+++ main/base-full/template.py
@@ -3,7 +3,7 @@ pkgver = "0.1"
 pkgrel = 0
 build_style = "meta"
 depends = [
-    "base-core"
+    "base-core", "nano"
 ]
 pkgdesc = "Chimera base package for bare metal and virtual machines"
 maintainer = "q66 <q66@chimera-linux.org>"
