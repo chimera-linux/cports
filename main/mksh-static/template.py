417849c036388f445d169d3d47bc0ca7af7dbe63
Daniel Kolesa <q66@chimera-linux.org>
2021-10-19 23:45:08 +0200

    main/mksh-static: register shells

--- main/mksh-static/template.py
+++ main/mksh-static/template.py
@@ -28,3 +28,7 @@ def do_install(self):
     self.install_bin("mksh.static")
 
     self.install_license(self.sources_path / "TaC-mksh.txt")
+
+    # register shell
+    self.install_dir("etc/shells.d")
+    self.install_link("/usr/bin/mksh.static", "etc/shells.d/mksh.static")
