9edefa819200ff7fcc353f3134d5bb247c71097c
Daniel Kolesa <q66@chimera-linux.org>
2021-10-19 04:30:38 +0200

    main/base-full: new package (0.1)

---
+++ main/base-full/template.py
@@ -0,0 +1,20 @@
+pkgname = "base-full"
+pkgver = "0.1"
+pkgrel = 0
+build_style = "meta"
+depends = [
+    "base-core"
+]
+pkgdesc = "Chimera base package for bare metal and virtual machines"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:meta"
+url = "https://chimera-linux.org"
+
+# TODO:
+#
+# - usbutils
+# - ethtool
+# - elogind
+# - wpa_supplicant or iwd
+# - firmware (wifi etc)
+# - kernel
