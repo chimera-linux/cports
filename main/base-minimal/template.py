801a219677514ad7c8e99933c61462eaaf46dc40
Daniel Kolesa <q66@chimera-linux.org>
2021-10-19 04:17:56 +0200

    main/base-minimal: new package (0.1)

---
+++ main/base-minimal/template.py
@@ -0,0 +1,16 @@
+pkgname = "base-minimal"
+pkgver = "0.1"
+pkgrel = 0
+build_style = "meta"
+depends = [
+    "base-files", "musl", "apk-tools", "bsdutils", "bsddiff", "bsdgrep",
+    "bsdsed", "bsded", "bsdgzip", "bsdtar", "dash", "awk", "util-linux",
+    "shadow", "procps-ng", "iana-etc", "tzdata",
+]
+pkgdesc = "Minimal set of packages for a Chimera system"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:meta"
+url = "https://chimera-linux.org"
+
+# TODO: a text editor? (other than ed)
+# TODO: dinit-chimera
