bdfc2a0e7eeeab001408baca162adcde426f679e
Daniel Kolesa <q66@chimera-linux.org>
2021-10-19 04:40:10 +0200

    main/nano: new package (5.9)

---
+++ main/nano/template.py
@@ -0,0 +1,16 @@
+pkgname = "nano"
+_mver = "5"
+pkgver = f"{_mver}.9"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--enable-utf8"]
+makedepends = ["ncurses-devel", "file-devel", "linux-headers"]
+pkgdesc = "GNU nano text editor"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-3.0-or-later"
+url = "https://www.nano-editor.org"
+source = f"https://www.nano-editor.org/dist/v{_mver}/nano-{pkgver}.tar.xz"
+sha256 = "757db8cda4bb2873599e47783af463e3b547a627b0cabb30ea7bf71fb4c24937"
+
+def post_install(self):
+    self.install_file("syntax/nanorc.nanorc", "usr/share/examples/nano")
