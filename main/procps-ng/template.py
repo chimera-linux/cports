b3e19e12d8da10642743be7c95cfde23706a5e88
Daniel Kolesa <q66@chimera-linux.org>
2021-10-19 04:11:28 +0200

    main/procps-ng: new package (3.3.17)

---
+++ main/procps-ng/template.py
@@ -0,0 +1,27 @@
+pkgname = "procps-ng"
+pkgver = "3.3.17"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--disable-kill", "--enable-watch8bit", "--disable-modern-top",
+    "--without-systemd", "--disable-dependency-tracking"
+]
+make_cmd = "gmake"
+hostmakedepends = ["pkgconf", "gmake"]
+makedepends = ["ncurses-devel"]
+checkdepends = ["dejagnu"]
+pkgdesc = "Utilities for monitoring your system and its processes"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
+url = "https://gitlab.com/procps-ng/procps"
+source = f"$(SOURCEFORGE_SITE)/{pkgname}/Production/{pkgname}-{pkgver}.tar.xz"
+sha256 = "4518b3e7aafd34ec07d0063d250fd474999b20b200218c3ae56f5d2113f141b4"
+# missing checkdepends
+options = ["!check"]
+
+def post_install(self):
+    self.install_file(self.files_path / "sysctl.conf", "etc")
+
+@subpackage("procps-ng-devel")
+def _devel(self):
+    return self.default_devel(man = True)
