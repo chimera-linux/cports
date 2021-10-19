c307e66301cc585fa95fe19d5c5c62cf92722709
Daniel Kolesa <q66@chimera-linux.org>
2021-10-19 04:28:05 +0200

    main/base-core: new package (0.1)

---
+++ main/base-core/template.py
@@ -0,0 +1,21 @@
+pkgname = "base-core"
+pkgver = "0.1"
+pkgrel = 0
+build_style = "meta"
+depends = [
+    "base-minimal", "ncurses", "mksh", "file", "less", "eudev", "kmod"
+]
+pkgdesc = "Common Chimera packages for most deployments"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:meta"
+url = "https://chimera-linux.org"
+
+# TODO:
+#
+# - man-pages, mandoc
+# - filesystem tools (e2fsprogs, xfsprogs, btrfs-progs, f2fs-tools, dosfstools)
+# - kbd
+# - pciutils
+# - iproute2, iputils, iw, traceroute, (dhcpcd?)
+# - openssh
+# - sudo or doas
