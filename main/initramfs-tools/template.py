fe84ed62616c3da8dbdd1e096872405a25d4606e
Daniel Kolesa <q66@chimera-linux.org>
2021-10-22 03:12:32 +0200

    main/initramfs-tools: new package (0.140)

---
+++ main/initramfs-tools/template.py
@@ -0,0 +1,13 @@
+pkgname = "initramfs-tools"
+pkgver = "0.140"
+pkgrel = 0
+build_style = "makefile"
+depends = ["klibc-progs", "bsdtar", "kmod", "debianutils"]
+pkgdesc = "Generic modular initramfs generator"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = f"https://salsa.debian.org/kernel-team/initramfs-tools"
+source = f"{url}/-/archive/v{pkgver}/{pkgname}-v{pkgver}.tar.gz"
+sha256 = "f46ae0e490a8a46975afd23a09202dee86756ebad1c8b795e862470a47dfac6b"
+# no tests
+options = ["!check"]
