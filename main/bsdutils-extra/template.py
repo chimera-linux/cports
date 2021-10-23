18cf0eb3fe92844449137892491c55ab0903d5cb
Daniel Kolesa <q66@chimera-linux.org>
2021-10-23 15:08:45 +0200

    main/bsdutils-extra: incorporate fixes

--- main/bsdutils-extra/template.py
+++ main/bsdutils-extra/template.py
@@ -1,5 +1,5 @@
 pkgname = "bsdutils-extra"
-_commit="66541d372e13de2e236d5d3a385e4ea6359bf421"
+_commit="abc84ae47425bcebfbbd4430cbf53ed480bfedb0"
 pkgver = "0.0.1"
 pkgrel = 0
 build_style = "meson"
@@ -11,6 +11,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/bsdutils-extra"
 source = f"https://github.com/chimera-linux/bsdutils-extra/archive/{_commit}.tar.gz"
-sha256 = "9025cf42e5a9a4e6f4c1b0f2444ab7a9e60c683c28577df88f521af6476dceb6"
+sha256 = "9d7a9b35e138fbe0cba83dea68da9936c0a8ab304514148dd618ed8b4ebfc637"
 # no test suite
 options = ["bootstrap", "!check"]
