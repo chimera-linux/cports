b75bd813fdcbd153723d1f9336aa52a01980d3f1
Daniel Kolesa <q66@chimera-linux.org>
2021-10-27 03:42:49 +0200

    main/pax-utils: install lddtree/symtree and depend on bash

--- main/pax-utils/template.py
+++ main/pax-utils/template.py
@@ -2,19 +2,17 @@ pkgname = "pax-utils"
 pkgver = "1.3.2"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_args = ["--with-caps"]
-hostmakedepends = ["pkgconf"]
+configure_args = ["--with-caps", "--without-python"]
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "pkgconf"]
 makedepends = ["libcap-devel"]
+checkdepends = ["bash", "python"]
+depends = ["bash"]
 pkgdesc = "PaX aware and related utilities for ELF binaries"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://cgit.gentoo.org/proj/pax-utils.git"
 source = f"https://gentoo.osuosl.org/distfiles/{pkgname}-{pkgver}.tar.xz"
 sha256 = "02eba0c305ad349ad6ff1f30edae793061ce95680fd5bdee0e14caf731dee1e7"
-# weird test suite and needs bash etc
+# needs python elftools
 options = ["!check"]
-
-def post_install(self):
-    # bash scripts
-    self.rm(self.destdir / "usr/bin/lddtree", force = True)
-    self.rm(self.destdir / "usr/bin/symtree", force = True)
