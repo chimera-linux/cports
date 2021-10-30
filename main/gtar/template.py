51c478b32eb75c850f21b39e49cf1674de0d3245
Daniel Kolesa <q66@chimera-linux.org>
2021-10-30 19:30:31 +0200

    main/gtar: new package (1.34)

---
+++ main/gtar/template.py
@@ -0,0 +1,20 @@
+pkgname = "gtar"
+pkgver = "1.34"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--program-prefix=g",
+    "gl_cv_struct_dirent_d_ino=yes",
+]
+makedepends = ["acl-devel"]
+pkgdesc = "GNU tape archiver"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-3.0-or-later"
+url = "http://www.gnu.org/software/tar"
+source = f"$(GNU_SITE)/tar/tar-{pkgver}.tar.xz"
+sha256 = "63bebd26879c5e1eea4352f0d03c991f966aeb3ddeb3c7445c902568d5411d28"
+# FIXME: some tests do not seem to like program-prefix
+options = ["!check"]
+
+def post_install(self):
+    self.rm(self.destdir / "usr/share/info", recursive = True)
