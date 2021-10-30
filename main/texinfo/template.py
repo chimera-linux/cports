6c75e7cbf1e9fe87a7faf31b3af28b949c4c525d
Daniel Kolesa <q66@chimera-linux.org>
2021-10-31 01:31:26 +0200

    main/texinfo: new package (6.7)

---
+++ main/texinfo/template.py
@@ -0,0 +1,17 @@
+pkgname = "texinfo"
+pkgver = "6.7"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--disable-static", "--disable-perl-xs"
+]
+hostmakedepends = ["perl", "ncurses-devel"]
+makedepends = ["ncurses-devel"]
+depends = ["bsdgzip", "perl"]
+triggers = ["/usr/share/info"]
+pkgdesc = "GNU Documentation System"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-3.0-or-later"
+url = "http://www.gnu.org/software/texinfo"
+source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "988403c1542d15ad044600b909997ba3079b10e03224c61188117f3676b02caa"
--- src/cbuild/hooks/post_install/01_remove_misc.py
+++ src/cbuild/hooks/post_install/01_remove_misc.py
@@ -1,5 +1,3 @@
 def invoke(pkg):
     (pkg.destdir / "usr/lib/charset.alias").unlink(missing_ok = True)
-
-    if pkg.pkgname != "texinfo":
-        (pkg.destdir / "usr/share/info/dir").unlink(missing_ok = True)
+    (pkg.destdir / "usr/share/info/dir").unlink(missing_ok = True)
