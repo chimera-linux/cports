583f62c6c03c25f8dc7d2e57915eab043ce73670
Daniel Kolesa <q66@chimera-linux.org>
2021-10-31 05:15:52 +0100

    main/base-full: depend on opendoas

--- main/base-full/template.py
+++ main/base-full/template.py
@@ -3,7 +3,7 @@ pkgver = "0.1"
 pkgrel = 0
 build_style = "meta"
 depends = [
-    "base-core", "nano", "initramfs-tools"
+    "base-core", "nano", "initramfs-tools", "opendoas"
 ]
 pkgdesc = "Chimera base package for bare metal and virtual machines"
 maintainer = "q66 <q66@chimera-linux.org>"
