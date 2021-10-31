6c86cc7da1f0295d7926d7aac6b8f224f5532280
Daniel Kolesa <q66@chimera-linux.org>
2021-11-01 00:46:12 +0100

    main/libusb: new package (1.0.24)

---
+++ main/libusb/template.py
@@ -0,0 +1,17 @@
+pkgname = "libusb"
+pkgver = "1.0.24"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+makedepends = ["eudev-devel", "linux-headers"]
+pkgdesc = "Library for userspace USB device access"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.1-or-later"
+url = "https://libusb.info"
+source = f"https://github.com/{pkgname}/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "7efd2685f7b327326dcfb85cee426d9b871fd70e22caa15bb68d595ce2a2b12a"
+
+@subpackage("libusb-devel")
+def _devel(self):
+    self.depends = ["eudev-devel"]
+    return self.default_devel()
