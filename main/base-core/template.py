709428bd970eff447e677f135fb2901e6317d1e0
Daniel Kolesa <q66@chimera-linux.org>
2021-10-31 18:59:31 +0100

    main/base-core: more dependencies

--- main/base-core/template.py
+++ main/base-core/template.py
@@ -3,8 +3,9 @@ pkgver = "0.1"
 pkgrel = 0
 build_style = "meta"
 depends = [
-    "base-minimal", "ncurses", "mksh", "file", "less", "eudev", "kmod", "kbd",
-    "mandoc", "man-pages", "e2fsprogs", "xfsprogs", "btrfs-progs",
+    "base-minimal", "ncurses", "mksh", "file", "less", "eudev", "kmod",
+    "kbd", "mandoc", "man-pages", "e2fsprogs", "xfsprogs", "btrfs-progs",
+    "dosfstools", "iputils", "iproute2",
 ]
 pkgdesc = "Common Chimera packages for most deployments"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -13,7 +14,6 @@ url = "https://chimera-linux.org"
 
 # TODO:
 #
-# - filesystem tools (f2fs-tools, dosfstools)
 # - pciutils
-# - iproute2, iputils, iw, traceroute, (dhcpcd?)
+# - iw, traceroute, (dhcpcd?)
 # - openssh
