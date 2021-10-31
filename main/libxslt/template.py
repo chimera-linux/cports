794dc7cecd4ec69a498c10483a840934e0970baf
Daniel Kolesa <q66@chimera-linux.org>
2021-10-31 07:16:09 +0100

    main/libxslt: new package (1.1.34)

---
+++ main/libxslt/template.py
@@ -0,0 +1,30 @@
+pkgname = "libxslt"
+pkgver = "1.1.34"
+pkgrel = 0
+build_style = "gnu_configure"
+# FIXME: crypto needs libgcrypt, maybe also enable python?
+configure_args = ["--without-crypto", "--without-python"]
+hostmakedepends = [
+    "docbook-xml", "docbook-xsl-nons", "pkgconf"
+]
+makedepends = ["libxml2-devel"]
+pkgdesc = "GNOME XSLT parser library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "http://xmlsoft.org/XSLT"
+source = f"http://xmlsoft.org/sources/{pkgname}-{pkgver}.tar.gz"
+sha256 = "98b1bd46d6792925ad2dfe9a87452ea2adebf69dcb9919ffd55bf926a7f93f7f"
+# test code seemingly incompatible with current libxml2
+options = ["!cross", "!check"]
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libxslt-devel")
+def _devel(self):
+    return self.default_devel(man = True)
+
+@subpackage("xsltproc")
+def _xsltproc(self):
+    self.pkgdesc = "XSLT 1.0 command line processor"
+    return self.default_progs(man = True)
