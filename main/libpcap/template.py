79a955f7b74bd593bb2ef48299c383d2014ffaf3
Daniel Kolesa <q66@chimera-linux.org>
2021-11-01 01:00:01 +0100

    main/libpcap: new package (1.10.1)

---
+++ main/libpcap/template.py
@@ -0,0 +1,43 @@
+pkgname = "libpcap"
+pkgver = "1.10.1"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-ipv6", "--with-libnl", "--with-pcap=linux", "--enable-usb",
+]
+hostmakedepends = ["pkgconf", "flex"]
+makedepends = ["libnl3-devel", "libusb-devel", "linux-headers"]
+pkgdesc = "System-independent interface for user-level packet capture"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-3-Clause"
+url = "https://www.tcpdump.org"
+source = f"{url}/release/{pkgname}-{pkgver}.tar.gz"
+sha256 = "ed285f4accaf05344f90975757b3dbfe772ba41d1c401c2648b7fa45b711bdd4"
+# no check target
+options = ["!check"]
+
+def init_configure(self):
+    incp = self.profile().sysroot / "usr/include/libnl3"
+    self.tool_flags["CFLAGS"] = [f"-I{incp}"]
+
+def post_install(self):
+    self.install_license("LICENSE")
+    for f in (self.destdir / "usr/share/man/man3").glob("*.3pcap"):
+        ff = f.with_name(f.name.removesuffix("pcap"))
+        if f.is_symlink():
+            tp = f.readlink()
+            f.unlink()
+            self.ln_s(tp.with_name(tp.name.removesuffix("pcap")), ff)
+        else:
+            self.mv(f, ff)
+
+@subpackage("libpcap-devel")
+def _devel(self):
+    self.depends += ["libnl3-devel"]
+
+    return self.default_devel(man = True, extra = [
+        "usr/bin",
+        "usr/share/man/man[15]",
+        "usr/share/man/man7/pcap-linktype.7",
+        "usr/share/man/man7/pcap-tstamp.7",
+    ])
