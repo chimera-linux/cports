ad1e04d87532bbc70222367f3e775947d2996035
Daniel Kolesa <q66@chimera-linux.org>
2021-11-01 05:22:24 +0100

    main/dhcpcd: new package (9.4.1)

---
+++ main/dhcpcd/template.py
@@ -0,0 +1,27 @@
+pkgname = "dhcpcd"
+pkgver = "9.4.1"
+pkgrel = 0
+build_style = "configure"
+configure_args = [
+    "--prefix=/usr",
+    "--sbindir=/usr/bin",
+    "--sysconfdir=/etc",
+    "--rundir=/run/dhcpcd",
+    # FIXME: needs a user, plus fixing up the seccomp on some platforms
+    #"--privsepuser=_dhcpcd",
+    #"--enable-privsep",
+]
+make_check_target = "test"
+hostmakedepends = ["pkgconf"]
+makedepends = ["eudev-devel", "linux-headers"]
+pkgdesc = "RFC2131 compliant DHCP client"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-2-Clause"
+url = "https://roy.marples.name/projects/dhcpcd"
+source = f"https://roy.marples.name/downloads/{pkgname}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "819357634efed1ea5cf44ec01b24d3d3f8852fec8b4249925dcc5667c54e376c"
+
+# TODO: add services
+
+def post_install(self):
+    self.install_license("LICENSE")
