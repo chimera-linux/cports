983211e15b3a07994ca8749f7907bdc87413efbf
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 00:09:33 +0100

    main/xorg-util-macros: new package (1.19.3)

---
+++ main/xorg-util-macros/template.py
@@ -0,0 +1,14 @@
+pkgname = "xorg-util-macros"
+pkgver = "1.19.3"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+pkgdesc = "X.org autotools macros"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://gitlab.freedesktop.org/xorg/util/macros"
+source = f"$(XORG_SITE)/util/util-macros-{pkgver}.tar.bz2"
+sha256 = "0f812e6e9d2786ba8f54b960ee563c0663ddbe2434bf24ff193f5feab1f31971"
+
+def post_install(self):
+    self.install_license("COPYING")
