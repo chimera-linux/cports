c97662dce1e8ae8e5640bcb7432676b95853942d
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 00:26:03 +0100

    main/xtrans: new package (1.4.0)

---
+++ main/xtrans/template.py
@@ -0,0 +1,14 @@
+pkgname = "xtrans"
+pkgver = "1.4.0"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+pkgdesc = "Network API translation layer to insulate X"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "377c4491593c417946efcd2c7600d1e62639f7a8bbca391887e2c4679807d773"
+
+def post_install(self):
+    self.install_license("COPYING")
