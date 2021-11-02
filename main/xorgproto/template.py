bb77122c64cc796facc1e17a094dd1a61743f877
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 00:23:53 +0100

    main/xorgproto: new package (2021.4)

---
+++ main/xorgproto/template.py
@@ -0,0 +1,36 @@
+pkgname = "xorgproto"
+pkgver = "2021.4"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--enable-legacy"]
+hostmakedepends = ["pkgconf", "xorg-util-macros"]
+pkgdesc = "Combined X.Org X11 protocol headers"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/proto/{pkgname}-{pkgver}.tar.gz"
+sha256 = "9de0babd3d8cb16b0c1c47b8389a52f3e1326bb0bc9a9ab34a9500778448a2bd"
+# we don't want dependencies on all the pkg-config stuff
+options = ["!scanrundeps"]
+
+def post_install(self):
+    for f in self.cwd.glob("COPYING-*"):
+        self.install_license(f)
+
+    for f in (self.destdir / "usr/include/X11/extensions").glob("apple*"):
+        f.unlink()
+    for f in (self.destdir / "usr/include/X11/extensions").glob("windows*"):
+        f.unlink()
+
+    self.rm(
+        self.destdir / f"usr/share/licenses/{pkgname}/COPYING-applewmproto"
+    )
+    self.rm(
+        self.destdir / f"usr/share/licenses/{pkgname}/COPYING-windowswmproto"
+    )
+
+    self.rm(self.destdir / "usr/share/pkgconfig/applewmproto.pc")
+    self.rm(self.destdir / "usr/share/pkgconfig/windowswmproto.pc")
+
+    # provided by libx11-devel
+    self.rm(self.destdir / "usr/include/X11/extensions/XKBgeom.h")
