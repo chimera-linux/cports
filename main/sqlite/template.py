6f7c26cf8d806115c2cf7ee2db9990ee26ce23e3
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 05:55:35 +0100

    main/sqlite: new package (3.36.0)

---
+++ main/sqlite/template.py
@@ -0,0 +1,37 @@
+pkgname = "sqlite"
+pkgver = "3.36.0"
+_amalg = "3360000"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-threadsafe", "--enable-dynamic-extensions", "--enable-fts5"
+]
+hostmakedepends = ["pkgconf"]
+makedepends = ["libedit-devel", "zlib-devel"]
+pkgdesc = "SQL Database Engine in a C library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:none"
+url = "https://sqlite.org"
+source = f"https://sqlite.org/2021/sqlite-autoconf-{_amalg}.tar.gz"
+sha256 = "bd90c3eb96bee996206b83be7065c9ce19aef38c3f4fb53073ada0d0b69bbce3"
+options = ["!parallel"]
+
+_cflags = [
+    "-DSQLITE_ENABLE_DBSTAT_VTAB", "-DSQLITE_ENABLE_COLUMN_METADATA",
+    "-DSQLITE_ENABLE_UNLOCK_NOTIFY", "-DSQLITE_SECURE_DELETE",
+    "-DSQLITE_ENABLE_JSON1", "-DSQLITE_ENABLE_FTS3",
+    "-DSQLITE_ENABLE_FTS3_PARENTHESIS", "-DSQLITE_ENABLE_FTS4",
+    "-DSQLITE_ENABLE_FTS3_TOKENIZER=1", "-DSQLITE_ENABLE_BATCH_ATOMIC_WRITE=1",
+    "-DSQLITE_ENABLE_DESERIALIZE",
+
+    "-DHAVE_FDATASYNC"
+]
+
+if current.profile().endian == "big":
+    _cflags += ["-DSHA3_BYTEORDER=4321", "-DSQLITE_BYTEORDER=4321"]
+else:
+    _cflags += ["-DSHA3_BYTEORDER=1234", "-DSQLITE_BYTEORDER=1234"]
+
+@subpackage("sqlite-devel")
+def _devel(self):
+    return self.default_devel()
