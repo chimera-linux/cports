3d0a75c5037ae1ab03c5368f8e0142bcad6ab127
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 02:15:24 +0100

    main/wayland: new package (1.19.0)

---
+++ main/wayland/template.py
@@ -0,0 +1,37 @@
+pkgname = "wayland"
+pkgver = "1.19.0"
+pkgrel = 0
+build_style = "meson"
+# "Tests must not be built with NDEBUG defined, they rely on assert()."
+configure_args = ["-Ddocumentation=false", "-Db_ndebug=false"]
+hostmakedepends = ["meson", "pkgconf", "flex"]
+makedepends = [
+    "libexpat-devel", "libffi-devel", "libfl-devel", "libxml2-devel"
+]
+pkgdesc = "Wayland compositor infrastructure"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://wayland.freedesktop.org"
+source = f"{url}/releases/{pkgname}-{pkgver}.tar.xz"
+sha256 = "baccd902300d354581cd5ad3cc49daa4921d55fb416a5883e218750fef166d15"
+
+if current.cross_build:
+    hostmakedepends += ["wayland-progs"]
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("wayland-progs")
+def _progs(self):
+    return self.default_progs(extra = [
+        "usr/share/aclocal/wayland-scanner.m4",
+        "usr/share/wayland/wayland-scanner.mk",
+    ])
+
+@subpackage("wayland-devel")
+def _devel(self):
+    self.depends += [
+        f"wayland-progs={pkgver}-r{pkgrel}",
+        "libffi-devel"
+    ]
+    return self.default_devel(extra = ["usr/share"])
