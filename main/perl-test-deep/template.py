99510adc38695a574925f4b7b61b7c7022fd787f
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 22:33:02 +0100

    main/perl-test-deep: new package (1.130)

---
+++ main/perl-test-deep/template.py
@@ -0,0 +1,13 @@
+pkgname = "perl-test-deep"
+pkgver = "1.130"
+pkgrel = 0
+build_style = "perl_module"
+hostmakedepends = ["gmake", "perl"]
+makedepends = ["perl"]
+depends = ["perl"]
+pkgdesc = "Extremely flexible deep comparison"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
+url = "https://metacpan.org/release/Test-Deep"
+source = f"$(CPAN_SITE)/Test/Test-Deep-{pkgver}.tar.gz"
+sha256 = "4064f494f5f62587d0ae501ca439105821ee5846c687dc6503233f55300a7c56"
