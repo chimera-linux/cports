7731d49111b03b8ddca0037af0bc6400d3b03a50
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 01:04:05 +0100

    main/libx11: new package (1.7.2)

---
+++ main/libx11/template.py
@@ -0,0 +1,28 @@
+pkgname = "libx11"
+pkgver = "1.7.2"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-ipv6",
+    "--enable-xlocaledir",
+    "--enable-malloc0returnsnull",
+    "--enable-static",
+    "--without-xmlto",
+]
+hostmakedepends = ["pkgconf", "xorg-util-macros"]
+makedepends = ["xorgproto", "xtrans", "libxcb-devel"]
+pkgdesc = "Base X libraries from Xorg"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/lib/libX11-{pkgver}.tar.bz2"
+sha256 = "1cfa35e37aaabbe4792e9bb690468efefbfbf6b147d9c69d6f90d13c3092ea6c"
+# broken for now
+options = ["!cross"]
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libx11-devel")
+def _devel(self):
+    return self.default_devel(man = True)
