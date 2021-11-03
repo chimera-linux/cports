74f6dad82700692c2d561a0da759e8ba419e7438
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 05:28:47 +0100

    main/fotconfig: new package (2.13.1)

---
+++ main/fontconfig/template.py
@@ -0,0 +1,30 @@
+pkgname = "fontconfig"
+pkgver = "2.13.1"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-static", "--enable-docs",
+    f"--with-cache-dir=/var/cache/{pkgname}",
+]
+make_cmd = "gmake"
+hostmakedepends = ["pkgconf", "gperf", "gmake"]
+makedepends = ["libexpat-devel", "freetype-devel", "libuuid-devel"]
+pkgdesc = "Library for configuring and customizing font access"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://www.fontconfig.org"
+source = f"$(FREEDESKTOP_SITE)/{pkgname}/release/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "f655dd2a986d7aa97e052261b36aa67b0a64989496361eca8d604e6414006741"
+# leaves junk in pc file
+options = ["!cross"]
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("fontconfig-devel")
+def _devel(self):
+    return self.default_devel(man = True)
+
+@subpackage("fontconfig-doc")
+def _doc(self):
+    return self.default_doc()
