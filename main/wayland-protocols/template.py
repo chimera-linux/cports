b57bd9d32e0d414eec3316e3e57ae3f8316ecf41
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 02:42:42 +0100

    main/wayland-protocols: new package (1.23)

---
+++ main/wayland-protocols/template.py
@@ -0,0 +1,15 @@
+pkgname = "wayland-protocols"
+pkgver = "1.23"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf", "wayland-progs"]
+makedepends = ["wayland-devel"]
+pkgdesc = "Wayland compositor protocols"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://wayland.freedesktop.org"
+source = f"https://github.com/wayland-project/{pkgname}/archive/{pkgver}.tar.gz"
+sha256 = "1ffd6f90eb247ff79de50ac10490ed03100572fb571cebef4df9ec74a271b2af"
+
+def post_install(self):
+    self.install_license("COPYING")
