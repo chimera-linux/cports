9046189a358a38edf1cc4546bd23bf7d42d401fc
Daniel Kolesa <q66@chimera-linux.org>
2021-11-04 00:51:20 +0100

    main/libxvmc: new package (1.0.12)

---
+++ main/libxvmc/template.py
@@ -0,0 +1,22 @@
+pkgname = "libxvmc"
+pkgver = "1.0.12"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--enable-malloc0returnsnull"]
+hostmakedepends = ["pkgconf"]
+makedepends = ["xorgproto", "libx11-devel", "libxext-devel", "libxv-devel"]
+pkgdesc = "XvMC extension library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/lib/libXvMC-{pkgver}.tar.bz2"
+sha256 = "6b3da7977b3f7eaf4f0ac6470ab1e562298d82c4e79077765787963ab7966dcd"
+
+def post_install(self):
+    self.install_license("COPYING")
+    # in xorgproto
+    self.rm(self.destdir / "usr/include/X11/extensions/vldXvMC.h")
+
+@subpackage("libxvmc-devel")
+def _devel(self):
+    return self.default_devel(man = True, extra = ["usr/share/doc"])
