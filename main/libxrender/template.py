1ad8082358443d315c37aea6b05f05ae8be0ea3f
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 02:02:45 +0100

    main/libxrender: new package (0.9.10)

---
+++ main/libxrender/template.py
@@ -0,0 +1,20 @@
+pkgname = "libxrender"
+pkgver = "0.9.10"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--enable-malloc0returnsnull"]
+hostmakedepends = ["pkgconf"]
+makedepends = ["xorgproto", "libxext-devel"]
+pkgdesc = "X Render library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/lib/libXrender-{pkgver}.tar.bz2"
+sha256 = "c06d5979f86e64cabbde57c223938db0b939dff49fdb5a793a1d3d0396650949"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libxrender-devel")
+def _devel(self):
+    return self.default_devel()
