2b471348382872b0e3e96ae28bf31ffd5fce34b8
Daniel Kolesa <q66@chimera-linux.org>
2021-11-03 05:00:06 +0100

    main/libxscrnsaver: new package (1.2.3)

---
+++ main/libxscrnsaver/template.py
@@ -0,0 +1,20 @@
+pkgname = "libxscrnsaver"
+pkgver = "1.2.3"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--enable-malloc0returnsnull"]
+hostmakedepends = ["pkgconf"]
+makedepends = ["xorgproto", "libx11-devel", "libxext-devel"]
+pkgdesc = "X Screensaver Library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/lib/libXScrnSaver-{pkgver}.tar.bz2"
+sha256 = "f917075a1b7b5a38d67a8b0238eaab14acd2557679835b154cf2bca576e89bf8"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libxscrnsaver-devel")
+def _devel(self):
+    return self.default_devel(extra = ["usr/share"])
