27565c227d5784b756b9daff3d1c845e5c5da96e
Daniel Kolesa <q66@chimera-linux.org>
2021-11-04 03:53:58 +0100

    main/libxshmfence: new package (1.3)

---
+++ main/libxshmfence/template.py
@@ -0,0 +1,20 @@
+pkgname = "libxshmfence"
+pkgver = "1.3"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--with-shared-memory-dir=/dev/shm"]
+hostmakedepends = ["pkgconf"]
+makedepends = ["xorgproto"]
+pkgdesc = "X SyncFence synchronization primitive"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/lib/libxshmfence-{pkgver}.tar.bz2"
+sha256 = "b884300d26a14961a076fbebc762a39831cb75f92bed5ccf9836345b459220c7"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libxshmfence-devel")
+def _devel(self):
+    return self.default_devel(man = True)
