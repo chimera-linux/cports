5d5621c730fc4fe165aa8a501a3fcbc9eb1f5cbb
Daniel Kolesa <q66@chimera-linux.org>
2021-11-04 04:20:49 +0100

    main/libvdpau: new package (1.4)

---
+++ main/libvdpau/template.py
@@ -0,0 +1,20 @@
+pkgname = "libvdpau"
+pkgver = "1.4"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
+makedepends = ["libxext-devel", "xorgproto"]
+pkgdesc = "Video Decode and Presentation API for UNIX"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://freedesktop.org/wiki/Software/VDPAU"
+source = f"https://gitlab.freedesktop.org/vdpau/{pkgname}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "240dd8f9ec08de707529917677827ba12e13b9fc299eeb3af13ea05e7fc74aba"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libvdpau-devel")
+def _devel(self):
+    self.depends += makedepends
+    return self.default_devel(man = True)
