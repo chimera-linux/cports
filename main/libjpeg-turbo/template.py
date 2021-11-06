7474927becc5e438b76f7d7edb0d31c15df92a5a
Daniel Kolesa <q66@chimera-linux.org>
2021-11-06 05:48:05 +0100

    main/libjpeg-turbo: new package (2.1.1)

---
+++ main/libjpeg-turbo/template.py
@@ -0,0 +1,31 @@
+pkgname = "libjpeg-turbo"
+pkgver = "2.1.1"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DWITH_JPEG8=1", "-DCMAKE_INSTALL_LIBDIR=/usr/lib"]
+hostmakedepends = ["cmake", "ninja", "pkgconf", "yasm"]
+pkgdesc = "Derivative of libjpeg which uses SIMD instructions"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "IJG AND BSD-3-Clause AND Zlib"
+url = "https://libjpeg-turbo.org"
+source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "b76aaedefb71ba882cbad4e9275b30c2ae493e3195be0a099425b5c6b99bd510"
+# FIXME: one test segfaults, let's see when we have a debugger
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("LICENSE.md")
+
+    self.install_file("jpegint.h", "usr/include")
+    self.install_file("transupp.h", "usr/include")
+
+    self.rm(self.destdir / "usr/share/doc", recursive = True)
+    self.rm(self.destdir / "usr/bin/tjbench")
+
+@subpackage("libjpeg-turbo-devel")
+def _devel(self):
+    return self.default_devel()
+
+@subpackage("libjpeg-turbo-progs")
+def _progs(self):
+    return self.default_progs(man = True)
