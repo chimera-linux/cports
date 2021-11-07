8c16bcd6db5e1f51cade435d488e2eca9811b425
Daniel Kolesa <q66@chimera-linux.org>
2021-11-07 01:07:55 +0100

    main/base-cbuild: switch back to dynamic apk
    If we use static apk we cannot preload libs, and thus fakeroot
    cannot be used.

--- main/base-cbuild/template.py
+++ main/base-cbuild/template.py
@@ -9,18 +9,12 @@ url = "https://chimera-linux.org"
 
 depends = [
     "base-files", "musl-devel", "elftoolchain", "llvm", "clang", "lld",
-    "apk-tools-static", "bsdutils-extra", "bsdgrep", "bsdgzip", "bsdpatch",
+    "apk-tools", "bsdutils-extra", "bsdgrep", "bsdgzip", "bsdpatch",
     "bsdsed", "bsddiff", "bmake", "bsdtar", "dash", "mksh-static", "awk",
     "ncurses", "util-linux-cbuild", "tzdata", "fakeroot",
 ]
-# provide a low version so it does not take over
-provides = ["apk-tools=0.0.1-r0"]
 
 options = ["bootstrap", "brokenlinks"]
 
 if current.stage > 1:
     depends += ["ccache"]
-
-def do_install(self):
-    self.install_dir("usr/bin")
-    self.install_link("apk.static", "usr/bin/apk")
