20d19ad658b9b53dff2f95833743aabc017e1b8b
Daniel Kolesa <q66@chimera-linux.org>
2021-11-07 19:24:00 +0100

    main/python-six: new package (1.16.0)

---
+++ main/python-six/template.py
@@ -0,0 +1,18 @@
+pkgname = "python-six"
+pkgver = "1.16.0"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+checkdepends = ["python-pytest"]
+depends = ["python"]
+pkgdesc = "Python compatibility utilities"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://github.com/benjaminp/six"
+source = f"$(PYPI_SITE)/s/six/six-{pkgver}.tar.gz"
+sha256 = "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+# pytest not packaged yet
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("LICENSE")
