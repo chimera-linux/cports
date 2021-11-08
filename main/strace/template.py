c819d9aed5de0270699ebef6d7ae500f77c38894
Daniel Kolesa <q66@chimera-linux.org>
2021-11-08 05:34:03 +0100

    main/strace: new package (5.14)

---
+++ main/strace/template.py
@@ -0,0 +1,23 @@
+pkgname = "strace"
+pkgver = "5.14"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--without-libunwind", "--disable-mpers", "--disable-gcc-Werror",
+    "--disable-dependency-tracking",
+]
+make_cmd = "gmake"
+# there's over a thousand tests and a ~50 of them
+# fail due to various reasons, mostly harmless ones
+make_check_args = [
+    "TESTS=bpf.gen epoll_pwait.gen getcpu.gen open.gen read-write.gen "
+    "readlink.gen seccomp-filter.gen mmap ioctl caps readv"
+]
+hostmakedepends = ["gmake"]
+makedepends = ["linux-headers"]
+pkgdesc = "System call tracer"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.1-or-later"
+url = "https://strace.io"
+source = f"https://github.com/{pkgname}/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "901bee6db5e17debad4530dd9ffb4dc9a96c4a656edbe1c3141b7cb307b11e73"
