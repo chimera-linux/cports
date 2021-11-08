90af83b9b980fa3a558355f93560eae7c0d3932a
Daniel Kolesa <q66@chimera-linux.org>
2021-11-08 21:07:32 +0100

    main/automake: fix hardlinks

--- main/automake/template.py
+++ main/automake/template.py
@@ -1,5 +1,6 @@
 pkgname = "automake"
-pkgver = "1.16.5"
+_mver = "1.16"
+pkgver = f"{_mver}.5"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["perl", "autoconf"]
@@ -11,3 +12,11 @@ license = "GPL-2.0-or-later"
 url = "https://www.gnu.org/software/automake"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "07bd24ad08a64bc17250ce09ec56e921d6343903943e99ccf63bbf0705e34605"
+
+def post_install(self):
+    fp = self.destdir / "usr/bin/aclocal"
+    fp.unlink()
+    fp.symlink_to(f"aclocal-{_mver}")
+    fp = self.destdir / "usr/bin/automake"
+    fp.unlink()
+    fp.symlink_to(f"automake-{_mver}")
