cc6726f7672205424cb73f21f1881d8fee21a857
Daniel Kolesa <q66@chimera-linux.org>
2021-11-12 00:41:41 +0100

    main/python-attrs: new package (21.2.0)

---
+++ main/python-attrs/template.py
@@ -0,0 +1,18 @@
+pkgname = "python-attrs"
+pkgver = "21.2.0"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+checkdepends = ["python-pytest"] # and other stuff, but does not matter
+depends = ["python"]
+pkgdesc = "Attributes without boilerplate"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://attrs.readthedocs.io"
+source = f"$(PYPI_SITE)/a/attrs/attrs-{pkgver}.tar.gz"
+sha256 = "ef6aaac3ca6cd92904cdd0d83f629a15f18053ec84e6432106f7a4d04ae4f5fb"
+# dependency of pytest
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("LICENSE")
