292eed552a62c2c3da78cf9b9887fbd3b09edd01
Daniel Kolesa <q66@chimera-linux.org>
2021-11-12 00:45:39 +0100

    main/python-iniconfig: new package (1.1.1)

---
+++ main/python-iniconfig/template.py
@@ -0,0 +1,15 @@
+pkgname = "python-iniconfig"
+pkgver = "1.1.1"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools_scm"]
+checkdepends = ["python-pytest", "python-py"]
+depends = ["python"]
+pkgdesc = "Brain-dead simple config-ini parsing"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://github.com/RonnyPfannschmidt/iniconfig"
+source = f"$(PYPI_SITE)/i/iniconfig/iniconfig-{pkgver}.tar.gz"
+sha256 = "bc3af051d7d14b2ee5ef9969666def0cd1a000e121eaea580d4a313df4b37f32"
+# dependency of pytest
+options = ["!check"]
