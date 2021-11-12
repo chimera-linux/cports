3b7381beaa20ccce5ecb2ede2fe832145b249c38
Daniel Kolesa <q66@chimera-linux.org>
2021-11-12 03:49:34 +0100

    main/python-sphinxcontrib-applehelp: new package (1.0.2)

---
+++ main/python-sphinxcontrib-applehelp/template.py
@@ -0,0 +1,18 @@
+pkgname = "python-sphinxcontrib-applehelp"
+pkgver = "1.0.2"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+checkdepends = ["python-sphinx"]
+depends = ["python"]
+pkgdesc = "Sphinx extension which outputs Apple help book"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-2-Clause"
+url = "http://sphinx-doc.org"
+source = f"$(PYPI_SITE)/s/sphinxcontrib-applehelp/sphinxcontrib-applehelp-{pkgver}.tar.gz"
+sha256 = "a072735ec80e7675e3f432fcae8610ecf509c5f1869d17e2eecff44389cdbc58"
+# circular checkdepends
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("LICENSE")
