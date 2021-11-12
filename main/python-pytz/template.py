a41ec7ea008cfd01f63759f9c5a69de37d9c5ed6
Daniel Kolesa <q66@chimera-linux.org>
2021-11-12 01:18:42 +0100

    main/python-pytz: new package (2021.3)

---
+++ main/python-pytz/template.py
@@ -0,0 +1,24 @@
+pkgname = "python-pytz"
+pkgver = "2021.3"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+checkdepends = ["python-pytest"]
+depends = ["python", "tzdata"]
+pkgdesc = "Python timezone library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://pythonhosted.org/pytz"
+source = f"$(PYPI_SITE)/p/pytz/pytz-{pkgver}.tar.gz"
+sha256 = "acad2d8b20a1af07d4e4c9d2e9285c5ed9104354062f275f3fcd88dcef4f1326"
+# dependency of pytest
+options = ["!check", "brokenlinks"]
+
+def post_install(self):
+    for f in (self.destdir / "usr/lib").glob(
+        "python*/site-packages/pytz/zoneinfo"
+    ):
+        self.rm(f, recursive = True)
+        f.symlink_to("/usr/share/zoneinfo")
+
+    self.install_license("LICENSE.txt")
