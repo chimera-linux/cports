64edd08e229fc3e0456187a9f20640576c20c8fa
Daniel Kolesa <q66@chimera-linux.org>
2021-11-12 04:16:20 +0100

    main/python-docutils: downgrade to 0.17.1
    0.18 is not compatible with sphinx yet

--- main/python-docutils/template.py
+++ main/python-docutils/template.py
@@ -1,5 +1,6 @@
+# 0.18 is not compatible with sphinx yet
 pkgname = "python-docutils"
-pkgver = "0.18"
+pkgver = "0.17.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -10,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:none AND BSD-2-Clause AND GPL-3.0-or-later AND Python-2.0"
 url = "http://docutils.sourceforge.net"
 source = f"$(PYPI_SITE)/d/docutils/docutils-{pkgver}.tar.gz"
-sha256 = "c1d5dab2b11d16397406a282e53953fe495a46d69ae329f55aa98a5c4e3c5fbb"
+sha256 = "686577d2e4c32380bb50cbb22f575ed742d58168cee37e99117a854bcd88f125"
 
 def do_check(self):
     self.do("python", ["alltests.py"], wrksrc = "test")
