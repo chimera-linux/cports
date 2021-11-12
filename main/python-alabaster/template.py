e595e217c2a7467b24802f58188dbf1634f0ac88
Daniel Kolesa <q66@chimera-linux.org>
2021-11-12 01:24:33 +0100

    main/python-alabaster: new package (0.7.12)

---
+++ main/python-alabaster/template.py
@@ -0,0 +1,15 @@
+pkgname = "python-alabaster"
+pkgver = "0.7.12"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+depends = ["python"]
+pkgdesc = "Configurable sidebar-enabled Sphinx theme"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-3-Clause"
+url = "https://alabaster.readthedocs.io"
+source = f"$(PYPI_SITE)/a/alabaster/alabaster-{pkgver}.tar.gz"
+sha256 = "a661d72d58e6ea8a57f7a86e37d86716863ee5e92788398526d58b26a4e4dc02"
+
+def post_install(self):
+    self.install_license("LICENSE")
