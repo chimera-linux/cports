74fb959e7aa20baffa150e4543d22a6f96b2dcd7
Daniel Kolesa <q66@chimera-linux.org>
2021-11-12 03:55:32 +0100

    main/python-sphinxcontrib-htmlhelp: new package (2.0.0)

---
+++ main/python-sphinxcontrib-htmlhelp/template.py
@@ -0,0 +1,18 @@
+pkgname = "python-sphinxcontrib-htmlhelp"
+pkgver = "2.0.0"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+checkdepends = ["python-sphinx"]
+depends = ["python"]
+pkgdesc = "Sphinx extension which outputs HTML document"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-2-Clause"
+url = "http://sphinx-doc.org"
+source = f"$(PYPI_SITE)/s/sphinxcontrib-htmlhelp/sphinxcontrib-htmlhelp-{pkgver}.tar.gz"
+sha256 = "f5f8bb2d0d629f398bf47d0d69c07bc13b65f75a81ad9e2f71a63d4b7a2f6db2"
+# circular checkdepends
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("LICENSE")
