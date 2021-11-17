8bd4df5e1b4d2b82647b6a46b4ad2f0660edaf13
Daniel Kolesa <q66@chimera-linux.org>
2021-11-17 04:03:42 +0100

    main/vulkan-headers: update to 1.2.199

--- main/vulkan-headers/template.py
+++ main/vulkan-headers/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-headers"
-pkgver = "1.2.182"
+pkgver = "1.2.199"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja"]
@@ -8,6 +8,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://www.khronos.org/vulkan"
 source = f"https://github.com/KhronosGroup/Vulkan-Headers/archive/v{pkgver}.tar.gz"
-sha256 = "38d1c953de7bb2d839556226851feeb690f0d23bc22ac46c823dcb66c97bfdc8"
+sha256 = "c4b860e267312364aa0b36e457e5ac8c114fe0957f216668c9da9211ef4643d0"
 # no test suite
 options = ["!check"]
---
+++ main/vulkan-headers/update.py
@@ -0,0 +1,1 @@
+pkgname = "Vulkan-Headers"
