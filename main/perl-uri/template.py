070cb6e4d668492bd5000bc9c40e47e101021e45
Daniel Kolesa <q66@chimera-linux.org>
2021-11-17 23:16:09 +0100

    main/perl-uri: update to 5.10

--- main/perl-uri/template.py
+++ main/perl-uri/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-uri"
-pkgver = "5.09"
+pkgver = "5.10"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["gmake", "perl"]
@@ -10,6 +10,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/release/URI"
 source = f"$(CPAN_SITE)/URI/URI-{pkgver}.tar.gz"
-sha256 = "03e63ada499d2645c435a57551f041f3943970492baa3b3338246dab6f1fae0a"
+sha256 = "16325d5e308c7b7ab623d1bf944e1354c5f2245afcfadb8eed1e2cae9a0bd0b5"
 # missing checkdepends
 options = ["!check"]
