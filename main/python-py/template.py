3e6e9a36c04be0168afc6b59e0a526a28c7726d5
Daniel Kolesa <q66@chimera-linux.org>
2021-11-17 05:18:45 +0100

    main/python-py: update to 1.11.0

--- main/python-py/template.py
+++ main/python-py/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-py"
-pkgver = "1.10.0"
+pkgver = "1.11.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools_scm"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pytest-dev/py"
 source = f"$(PYPI_SITE)/p/py/py-{pkgver}.tar.gz"
-sha256 = "21b81bda15b66ef5e1a777a21c4dcd9c20ad3efd0b3f817e7a809035269e1bd3"
+sha256 = "51c75c4126074b472f746a24399ad32f6053d1b34b68d2fa41e558e6f4a98719"
 # dependency of pytest
 options = ["!check"]
 
