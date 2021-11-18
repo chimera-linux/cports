dbd2c735a48cf9d1a44a75ab97c670e02737969a
Daniel Kolesa <q66@chimera-linux.org>
2021-11-18 22:24:27 +0100

    main/glslang: update to 11.7.0

--- main/glslang/template.py
+++ main/glslang/template.py
@@ -1,6 +1,6 @@
-# libraries are unversioned (abi breakage, rebuild shaderc on updates)
+# note: some libs are unversioned
 pkgname = "glslang"
-pkgver = "8.13.3743"
+pkgver = "11.7.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/KhronosGroup/glslang"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "639ebec56f1a7402f2fa094469a5ddea1eceecfaf2e9efe361376a0f73a7ee2f"
+sha256 = "b6c83864c3606678d11675114fa5f358c519fe1dad9a781802bcc87fb8fa32d5"
 # missing checkdepends
 options = ["!check"]
 
@@ -20,6 +20,7 @@ def _devel(self):
     self.depends = [f"{pkgname}={pkgver}-r{pkgrel}"]
     return [
         "usr/include",
+        "usr/lib/libglslang.so",
         "usr/lib/*.a",
         "usr/lib/cmake",
     ]
