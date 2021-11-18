8260b8aa78535ed77cc1295801a67ed17560d74a
Daniel Kolesa <q66@chimera-linux.org>
2021-11-18 23:33:27 +0100

    main/libssh2: update to 1.10.0

--- main/libssh2/template.py
+++ main/libssh2/template.py
@@ -1,5 +1,5 @@
 pkgname = "libssh2"
-pkgver = "1.9.0"
+pkgver = "1.10.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [f"--with-libssl-prefix={self.profile().sysroot / 'usr'}"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://www.libssh2.org"
 source = f"https://www.libssh2.org/download/{pkgname}-{pkgver}.tar.gz"
-sha256 = "d5fb8bd563305fd1074dda90bd053fb2d29fc4bce048d182f96eaa466dfadafd"
+sha256 = "2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51"
 
 def post_install(self):
     self.install_license("COPYING")
