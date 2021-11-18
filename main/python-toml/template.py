d0d3bbb1596b8536baeba9e2ec4b5e49e7ecef5a
Daniel Kolesa <q66@chimera-linux.org>
2021-11-18 05:32:20 +0100

    main/python-toml: fix url

--- main/python-toml/template.py
+++ main/python-toml/template.py
@@ -7,7 +7,7 @@ depends = ["python"]
 pkgdesc = "Python library for TOML"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
-url = "https://github.com/RonnyPfannschmidt/iniconfig"
+url = "https://github.com/uiri/toml"
 source = f"$(PYPI_SITE)/t/toml/toml-{pkgver}.tar.gz"
 sha256 = "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
 # needs pytest, is a dependency of pytest
