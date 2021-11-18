d510ea82a8e2365b0db6a4ac54c59ea78dc44f6c
Daniel Kolesa <q66@chimera-linux.org>
2021-11-18 22:48:01 +0100

    main/ccache: update to 4.5.1

--- main/ccache/template.py
+++ main/ccache/template.py
@@ -1,17 +1,17 @@
 pkgname = "ccache"
-pkgver = "4.2.1"
+pkgver = "4.5.1"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DENABLE_TESTING=OFF"]
+configure_args = ["-DENABLE_TESTING=OFF", "-DREDIS_STORAGE_BACKEND=OFF"]
 make_check_target = "check"
-hostmakedepends = ["cmake", "ninja", "perl"]
+hostmakedepends = ["cmake", "ninja", "perl", "asciidoc"]
 makedepends = ["libzstd-devel", "zlib-devel"]
 pkgdesc = "Fast C/C++ compiler cache"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://ccache.samba.org"
 source = f"https://github.com/ccache/ccache/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "9d6ba1cdefdc690401f404b747d81a9a1802b17af4235815866b7620d980477e"
+sha256 = "51186ebe0326365f4e6131e1caa8911de7da4aa6718efc00680322d63a759517"
 # test suite needs bash
 options = ["!check"]
 
