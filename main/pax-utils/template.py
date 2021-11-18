ee07a57780abcf564c08284653c1ce0e1a9dd2aa
Daniel Kolesa <q66@chimera-linux.org>
2021-11-18 06:10:36 +0100

    main/pax-utils: update to 1.3.3

--- main/pax-utils/template.py
+++ main/pax-utils/template.py
@@ -1,5 +1,5 @@
 pkgname = "pax-utils"
-pkgver = "1.3.2"
+pkgver = "1.3.3"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-caps", "--without-python"]
@@ -13,6 +13,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://cgit.gentoo.org/proj/pax-utils.git"
 source = f"https://gentoo.osuosl.org/distfiles/{pkgname}-{pkgver}.tar.xz"
-sha256 = "02eba0c305ad349ad6ff1f30edae793061ce95680fd5bdee0e14caf731dee1e7"
+sha256 = "eeca7fbd98bc66bead4a77000c2025d9f17ea8201b84245882406ce00b9b6b14"
 # needs python elftools
 options = ["!check"]
