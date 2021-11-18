5dcbcec72e4a9c04cd8e6a149b1dd4f4d23319f7
Daniel Kolesa <q66@chimera-linux.org>
2021-11-18 05:35:39 +0100

    main/python-idna: update to 3.3

--- main/python-idna/template.py
+++ main/python-idna/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-idna"
-pkgver = "3.2"
+pkgver = "3.3"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/kjd/idna"
 source = f"$(PYPI_SITE)/i/idna/idna-{pkgver}.tar.gz"
-sha256 = "467fbad99067910785144ce333826c71fb0e63a425657295239737f7ecd125f3"
+sha256 = "9d643ff0a55b762d5cdb124b8eaa99c66322e2157b69160bc32796e824360e6d"
 
 def post_install(self):
     self.install_license("LICENSE.md")
