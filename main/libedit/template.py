0cbe5064e7e6063f02dfab417216a91eb36436c4
Daniel Kolesa <q66@chimera-linux.org>
2021-11-18 23:17:03 +0100

    main/libedit: update to 20210910.3.1

--- main/libedit/template.py
+++ main/libedit/template.py
@@ -1,5 +1,5 @@
 pkgname = "libedit"
-_datever = "20210522"
+_datever = "20210910"
 _distver = 3.1
 pkgver = f"{_datever}.{_distver}"
 pkgrel = 0
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://www.thrysoee.dk/editline"
 source = f"http://thrysoee.dk/editline/{pkgname}-{_datever}-{_distver}.tar.gz"
-sha256 = "0220bc2047e927c0c1984ef5f7b4eb2a9469a5b7bf12ba573ca3b23ca02bbb6f"
+sha256 = "6792a6a992050762edcca28ff3318cdb7de37dccf7bc30db59fcd7017eed13c5"
 options = ["bootstrap"]
 
 def post_install(self):
