755d734a4fa4def9d990bc2e185763a108fb3b44
Daniel Kolesa <q66@chimera-linux.org>
2021-11-18 05:34:02 +0100

    main/python-packaging: update to 21.3

--- main/python-packaging/template.py
+++ main/python-packaging/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-packaging"
-pkgver = "21.2"
+pkgver = "21.3"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -8,9 +8,9 @@ depends = ["python-pyparsing"]
 pkgdesc = "Easily build and distribute Python packages"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0 OR BSD-2-Clause"
-url = "https://github.com/pypa/setuptools"
+url = "https://github.com/pypa/packaging"
 source = f"$(PYPI_SITE)/p/packaging/packaging-{pkgver}.tar.gz"
-sha256 = "096d689d78ca690e4cd8a89568ba06d07ca097e3306a4381635073ca91479966"
+sha256 = "dd47c42927d89ab911e606518907cc2d3a1f38bbd026385970643f9c5b8ecfeb"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
