5fbb9e9056d2a4e5e417baa3b780235c0c723507
yopito <pierre.bourgin@free.fr>
2021-11-25 00:08:25 +0100

    main/dinit: crossbuild support, enhance checks.

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -1,19 +1,23 @@
+# XXX build: static binaries since init system ?
 pkgname = "dinit"
 pkgver = "0.12.0"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
-make_install_args = ["SBINDIR=/usr/bin"]
 hostmakedepends = ["gmake", "bsdm4"]
 pkgdesc = "Service manager and init system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
-url = f"https://github.com/davmac314/{pkgname}"
-source = f"{url}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
+url = "https://davmac.org/projects/dinit"
+source = f"https://github.com/davmac314/dinit/releases/download/v{pkgver}/dinit-{pkgver}.tar.xz"
 sha256 = "d5f9afe7005da7c08224dddcf2b63f37a6c4120b7493bed4669ef362cde1b544"
 tool_flags = {"CXXFLAGS": ["-fno-rtti"], "LDFLAGS": ["-fno-rtti"]}
 
-def init_configure(self):
-    eflags = ["CXXOPTS=" + self.get_cxxflags(shell = True)]
-    self.make_build_args += eflags
-    self.make_check_args += eflags
+def post_patch(self):
+    # static mconfig file instead of generated one
+    self.cp(self.files_path / "mconfig", self.cwd)
+
+def do_check(self):
+    self.make.invoke("check")
+    # integration tests
+    self.make.invoke("check-igr")
