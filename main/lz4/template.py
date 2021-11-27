70df3a88e263bd3e79f875260ad8faf2305773c0
Daniel Kolesa <q66@chimera-linux.org>
2021-11-27 21:29:15 +0100

    main/lz4: fix build
    The build system is a mess, and we need to be selective about
    what gets passed in on command line.

--- main/lz4/template.py
+++ main/lz4/template.py
@@ -4,6 +4,7 @@ pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
 make_check_args = ["-j1"]
+make_use_env = True
 hostmakedepends = ["pkgconf", "gmake"]
 pkgdesc = "LZ4 compression utilities"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -12,6 +13,12 @@ url = "https://lz4.github.io/lz4"
 source = f"https://github.com/lz4/lz4/archive/v{pkgver}.tar.gz"
 sha256 = "030644df4611007ff7dc962d981f390361e6c97a34e5cbc393ddfbe019ffe2c1"
 options = ["bootstrap"]
+
+def init_configure(self):
+    self.make_build_args += [
+        "CFLAGS=" + self.get_cflags(shell = True),
+        "LDFLAGS=" + self.get_ldflags(shell = True),
+    ]
 
 def post_install(self):
     self.install_license("lib/LICENSE")
