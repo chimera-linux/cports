92658483b6b1721181b16e7771570f4e7e7e403a
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 05:49:46 +0100

    main/acl: enable LTO

--- main/acl/template.py
+++ main/acl/template.py
@@ -16,7 +16,11 @@ url = "https://savannah.nongnu.org/projects/acl"
 source = f"$(NONGNU_SITE)/acl/acl-{pkgver}.tar.gz"
 sha256 = "760c61c68901b37fdd5eefeeaf4c0c7a26bdfdd8ac747a1edff1ce0e243c11af"
 # test suite makes assumptions about a GNU environment
-options = ["bootstrap", "!check"]
+options = ["bootstrap", "!check", "lto"]
+
+@subpackage("acl-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("acl-devel")
 def _devel(self):
