b22e10efc6d1bfe0ec119c6f9fef57500861ebc5
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 06:53:38 +0100

    main/bsdgzip: enable lTO

--- main/bsdgzip/template.py
+++ main/bsdgzip/template.py
@@ -11,7 +11,7 @@ url = "https://github.com/chimera-linux/bsdgzip"
 source = f"https://github.com/chimera-linux/{pkgname}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "61b24e5573694b28f1266f38bc6859ba72eb35e48dc95bfbe37572ca120f9fe4"
 # no test suite
-options = ["bootstrap", "!check"]
+options = ["bootstrap", "!check", "lto"]
 
 def post_install(self):
     self.rm(self.destdir / "usr/bin/zless")
