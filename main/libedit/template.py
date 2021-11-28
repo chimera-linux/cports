34283455e939157dc6c8b62d716f1ba116104ba1
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 07:50:54 +0100

    main/libedit: enable LTO

--- main/libedit/template.py
+++ main/libedit/template.py
@@ -12,10 +12,14 @@ license = "BSD-3-Clause"
 url = "http://www.thrysoee.dk/editline"
 source = f"http://thrysoee.dk/editline/{pkgname}-{_datever}-{_distver}.tar.gz"
 sha256 = "6792a6a992050762edcca28ff3318cdb7de37dccf7bc30db59fcd7017eed13c5"
-options = ["bootstrap"]
+options = ["bootstrap", "lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libedit-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libedit-devel")
 def _devel(self):
