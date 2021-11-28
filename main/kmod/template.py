aae06f622ef70d3af4516da26a39488c623fa72b
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 07:39:16 +0100

    main/kmod: enable LTO

--- main/kmod/template.py
+++ main/kmod/template.py
@@ -18,7 +18,7 @@ url = "https://git.kernel.org/pub/scm/utils/kernel/kmod/kmod.git"
 source = f"$(KERNEL_SITE)/utils/kernel/kmod/kmod-{pkgver}.tar.xz"
 sha256 = "0b80eea7aa184ac6fd20cafa2a1fdf290ffecc70869a797079e2cc5c6225a52a"
 # broken testsuite build system
-options = ["!check"]
+options = ["!check", "lto"]
 
 def post_install(self):
     self.install_file(
