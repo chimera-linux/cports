e8dfb06a3fe0dd531d6edff8b010fe6748c4425d
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 06:08:21 +0100

    main/openssl: enable LTO

--- main/openssl/template.py
+++ main/openssl/template.py
@@ -16,7 +16,7 @@ license = "OpenSSL"
 url = "https://www.openssl.org"
 source = f"https://www.openssl.org/source/openssl-{pkgver}.tar.gz"
 sha256 = "0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1"
-options = ["bootstrap"]
+options = ["bootstrap", "lto"]
 
 if self.stage > 0:
     makedepends = ["linux-headers"]
@@ -63,6 +63,10 @@ def _crehash(self):
         self.depends.append("perl")
 
     return ["usr/bin/c_rehash"]
+
+@subpackage("openssl-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("openssl-devel")
 def _devel(self):
