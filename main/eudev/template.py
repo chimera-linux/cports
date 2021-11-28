76b9f33e90766bb50dc6360f73e4438a35df7718
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 08:05:20 +0100

    main/eudev: enable LTO

--- main/eudev/template.py
+++ main/eudev/template.py
@@ -15,6 +15,7 @@ license = "GPL-2.0-or-later"
 url = "https://github.com/gentoo/eudev"
 source = f"https://dev.gentoo.org/~blueness/eudev/eudev-{pkgver}.tar.gz"
 sha256 = "87bb028d470fd1b85169349b44c55d5b733733dc2d50ddf1196e026725ead034"
+options = ["lto"]
 
 def post_install(self):
     # initramfs-tools
@@ -38,6 +39,10 @@ def post_install(self):
         self.files_path / "udevd.wrapper", "usr/libexec", mode = 0o755
     )
     self.install_service(self.files_path / "udevd")
+
+@subpackage("eudev-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("eudev-devel")
 def _devel(self):
