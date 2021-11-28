86554c6b76b3c1e5a07571ed871879bcd2c7741f
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 05:59:23 +0100

    main/bzip2: enable LTO

--- main/bzip2/template.py
+++ main/bzip2/template.py
@@ -8,7 +8,7 @@ url = "https://sourceware.org/bzip2"
 source = f"https://sourceware.org/pub/bzip2/bzip2-{pkgver}.tar.gz"
 sha256 = "ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269"
 tool_flags = {"CFLAGS": ["-fPIC"]}
-options = ["bootstrap"]
+options = ["bootstrap", "lto"]
 
 def init_build(self):
     from cbuild.util import make
@@ -59,6 +59,12 @@ def _lib(self):
     self.pkgdesc = "Bzip2-format compression library"
 
     return self.default_libs()
+
+@subpackage("libbz2-static")
+def _static(self):
+    self.pkgdesc = "Bzip2-format compression library (static library)"
+
+    return self.default_static()
 
 @subpackage("libbz2-devel")
 def _devel(self):
