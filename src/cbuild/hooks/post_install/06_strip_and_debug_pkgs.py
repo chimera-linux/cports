d7d5a8adaef73528b659cafce2f96a6d9c7ad10f
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 03:34:05 +0100

    cbuild: only enable the LTO strip skipping for stage2+
    During stages 0 and 1 there is no effect to the lto option, so
    eliminating the stripping is wrong.

--- src/cbuild/hooks/post_install/06_strip_and_debug_pkgs.py
+++ src/cbuild/hooks/post_install/06_strip_and_debug_pkgs.py
@@ -93,7 +93,8 @@ def invoke(pkg):
         # strip static library, only if not LTO or when forced
         if not vt:
             v.chmod(0o644)
-            if not pkg.rparent.options["lto"] or pkg.options["ltostrip"]:
+            if not pkg.rparent.options["lto"] or pkg.options["ltostrip"] \
+                or pkg.stage < 2:
                 try:
                     pkg.rparent.do(strip_path, "--strip-debug", cfile)
                 except:
