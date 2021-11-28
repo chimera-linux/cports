c39d160ba8bf25756e828e77a0f5d7cb087bfa3f
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 07:52:46 +0100

    main/libexpat: enable LTO

--- main/libexpat/template.py
+++ main/libexpat/template.py
@@ -10,9 +10,14 @@ license = "MIT"
 url = "https://libexpat.github.io"
 source = f"https://github.com/{pkgname}/{pkgname}/releases/download/R_{pkgver.replace('.', '_')}/expat-{pkgver}.tar.xz"
 sha256 = "cf032d0dba9b928636548e32b327a2d66b1aab63c4f4a13dd132c2d1d2f2fb6a"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libexpat-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libexpat-devel")
 def _devel(self):
