22d519b67626eb8da4c10695187ccd4ed37f29e5
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 06:50:29 +0100

    main/bsdsed: enable LTO

--- main/bsdsed/template.py
+++ main/bsdsed/template.py
@@ -8,4 +8,4 @@ license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/bsdsed"
 source = f"https://github.com/chimera-linux/bsdsed/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "4e2e5df15c3f9c0594f4ba1b9d243c5e7aa87abac8721716635bb872eef46229"
-options = ["bootstrap"]
+options = ["bootstrap", "lto"]
