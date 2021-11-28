3080c699d4877059f87e486165721cf3e9c9062c
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 22:37:24 +0100

    main/mandoc: disable LTO (fails)

--- main/mandoc/template.py
+++ main/mandoc/template.py
@@ -15,6 +15,8 @@ license = "ISC"
 url = "http://mandoc.bsd.lv"
 source = f"{url}/snapshots/{pkgname}-{pkgver}.tar.gz"
 sha256 = "8bf0d570f01e70a6e124884088870cbed7537f36328d512909eb10cd53179d9c"
+# ld: error: undefined symbol: mchars_alloc
+options = ["!lto"]
 
 def pre_configure(self):
     with open(self.cwd / "configure.local", "w") as cf:
