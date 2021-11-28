352b12941dcd547bcc87d7de6bd874c10c7e6d9f
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 06:52:12 +0100

    main/musl-fts: never LTO (no point)

--- main/musl-fts/template.py
+++ main/musl-fts/template.py
@@ -10,8 +10,8 @@ license = "BSD-3-Clause"
 url = "https://github.com/chimera-linux/musl-fts"
 source = f"https://github.com/chimera-linux/{pkgname}/archive/refs/tags/v{pkgver}-mk2.tar.gz"
 sha256 = "1f65612b523e7040dbd9d5579a2eca97ede79c2ff3f91db7ccc288263e60da50"
-# no test suite
-options = ["bootstrap", "!check"]
+# no test suite; also no point in LTOing this
+options = ["bootstrap", "!check", "!lto"]
 
 def post_install(self):
     self.install_license("COPYING")
