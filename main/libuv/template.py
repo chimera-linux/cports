f19a96c1e673b33645155720aacb8adf705adb06
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 23:09:00 +0100

    main/libuv: enable LTO

--- main/libuv/template.py
+++ main/libuv/template.py
@@ -9,12 +9,17 @@ license = "MIT"
 url = "https://libuv.org"
 source = f"https://github.com/{pkgname}/{pkgname}/archive/v{pkgver}.tar.gz"
 sha256 = "371e5419708f6aaeb8656671f89400b92a9bba6443369af1bb70bcd6e4b3c764"
+options = ["lto"]
 
 def pre_configure(self):
     self.do(self.chroot_cwd / "autogen.sh")
 
 def post_install(self):
     self.install_license("LICENSE")
+
+@subpackage("libuv-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libuv-devel")
 def _devel(self):
