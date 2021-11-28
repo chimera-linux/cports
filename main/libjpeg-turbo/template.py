90d49f45e3269b9cd1af56412c5094927afba4d9
Daniel Kolesa <q66@chimera-linux.org>
2021-11-28 23:35:39 +0100

    main/libjpeg-turbo: enable LTO, reenable tests, disable altivec
    It seems the altivec SIMD is the source of test segfaulting (with
    or without LTO, does not seem to affect it)

--- main/libjpeg-turbo/template.py
+++ main/libjpeg-turbo/template.py
@@ -10,8 +10,12 @@ license = "IJG AND BSD-3-Clause AND Zlib"
 url = "https://libjpeg-turbo.org"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "b76aaedefb71ba882cbad4e9275b30c2ae493e3195be0a099425b5c6b99bd510"
-# FIXME: one test segfaults, let's see when we have a debugger
-options = ["!check"]
+options = ["lto"]
+
+# tests segfault with altivec simd
+match self.profile().arch:
+    case "ppc64le" | "ppc64":
+        configure_args += ["-DWITH_SIMD=FALSE"]
 
 def post_install(self):
     self.install_license("LICENSE.md")
@@ -21,6 +25,10 @@ def post_install(self):
 
     self.rm(self.destdir / "usr/share/doc", recursive = True)
     self.rm(self.destdir / "usr/bin/tjbench")
+
+@subpackage("libjpeg-turbo-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libjpeg-turbo-devel")
 def _devel(self):
