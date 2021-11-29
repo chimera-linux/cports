8ab5c9e00a08be28089317afe8f5d15e8d0faa91
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 02:41:55 +0100

    main/libxkbfile: enable LTO

--- main/libxkbfile/template.py
+++ main/libxkbfile/template.py
@@ -10,9 +10,14 @@ license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "758dbdaa20add2db4902df0b1b7c936564b7376c02a0acd1f2a331bd334b38c7"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libxkbfile-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libxkbfile-devel")
 def _devel(self):
