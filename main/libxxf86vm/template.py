43bc3a7f3c10c0ecd7df24aba393c57a4dc8349d
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 02:39:23 +0100

    main/libxxf86vm: enable LTO

--- main/libxxf86vm/template.py
+++ main/libxxf86vm/template.py
@@ -11,9 +11,14 @@ license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXxf86vm-{pkgver}.tar.bz2"
 sha256 = "afee27f93c5f31c0ad582852c0fb36d50e4de7cd585fcf655e278a633d85cd57"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libxxf86vm-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libxxf86vm-devel")
 def _devel(self):
