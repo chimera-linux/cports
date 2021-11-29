9b1881045ab1358b699e872bc435ef0838d2a6dc
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 04:16:53 +0100

    main/wayland-protocols: enable LTO

--- main/wayland-protocols/template.py
+++ main/wayland-protocols/template.py
@@ -10,6 +10,7 @@ license = "MIT"
 url = "https://wayland.freedesktop.org"
 source = f"https://github.com/wayland-project/{pkgname}/archive/{pkgver}.tar.gz"
 sha256 = "1ffd6f90eb247ff79de50ac10490ed03100572fb571cebef4df9ec74a271b2af"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
