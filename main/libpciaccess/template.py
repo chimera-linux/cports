dd06ef3fe5421705c8b70e9235c9185428f3559b
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 03:17:46 +0100

    main/libpciaccess: enable LTO

--- main/libpciaccess/template.py
+++ main/libpciaccess/template.py
@@ -9,9 +9,14 @@ license = "MIT"
 url = "http://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "214c9d0d884fdd7375ec8da8dcb91a8d3169f263294c9a90c575bf1938b9f489"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libpciaccess-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libpciaccess-devel")
 def _devel(self):
