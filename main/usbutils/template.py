94b3641c860a5b1f2287e0aeb1ecc88a68a7ec61
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 04:08:59 +0100

    main/usbutils: enable LTO

--- main/usbutils/template.py
+++ main/usbutils/template.py
@@ -11,6 +11,7 @@ license = "GPL-2.0-only"
 url = "http://linux-usb.sourceforge.net"
 source = f"$(KERNEL_SITE)/utils/usb/{pkgname}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "3a079cfad60560227b67192482d7813bf96326fcbb66c04254839715f276fc69"
+options = ["lto"]
 
 def pre_configure(self):
     self.do("autoreconf", "-if")
