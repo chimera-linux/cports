6921df412041afcd334985d081684afe56dd57c5
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 02:54:00 +0100

    main/libxscrnsaver: enable LTO

--- main/libxscrnsaver/template.py
+++ main/libxscrnsaver/template.py
@@ -11,9 +11,14 @@ license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXScrnSaver-{pkgver}.tar.bz2"
 sha256 = "f917075a1b7b5a38d67a8b0238eaab14acd2557679835b154cf2bca576e89bf8"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libxscrnsaver-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libxscrnsaver-devel")
 def _devel(self):
