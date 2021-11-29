c4f5cdaeebea3d4f1226d30cbed10f91373023a3
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 21:53:51 +0100

    main/libspiro: enable LTO

--- main/libspiro/template.py
+++ main/libspiro/template.py
@@ -9,9 +9,14 @@ license = "GPL-3.0-or-later"
 url = "https://github.com/fontforge/libspiro"
 source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "00be530b5c0ea9274baadf6c05521f0b192d4c3c1db636ac8b08efd44aaea8f5"
+options = ["lto"]
 
 def pre_configure(self):
     self.do("autoreconf", "-if")
+
+@subpackage("libspiro-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libspiro-devel")
 def _devel(self):
