5f87c69f85eeeed95eef9fe1fc036c2acc635eb7
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 21:53:20 +0100

    main/fuse: disable LTO

--- main/fuse/template.py
+++ main/fuse/template.py
@@ -13,6 +13,8 @@ url = "https://github.com/libfuse/libfuse"
 source = f"{url}/releases/download/{pkgname}-{pkgver}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "b2e283485d47404ac896dd0bb7f7ba81e1470838e677e45f659804c3a3b69666"
 suid_files = ["usr/bin/fusermount3"]
+# ld: error: default version symbol fuse_loop_mt@@FUSE_3.2 must be defined
+options = ["!lto"]
 
 def do_check(self):
     self.do("python", "-m", "pytest", "test/", wrksrc = self.make_dir)
@@ -20,6 +22,10 @@ def do_check(self):
 def post_install(self):
     self.chmod(self.destdir / "usr/bin/fusermount3", 0o4755)
     self.rm(self.destdir / "etc/init.d/fuse3")
+
+@subpackage("fuse-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("fuse-devel")
 def _devel(self):
