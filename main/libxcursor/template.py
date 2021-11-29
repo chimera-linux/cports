f5812f5eea1da811b0825bf4da9cc9454ef4bcd8
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 02:52:01 +0100

    main/libxcursor: enable LTO

--- main/libxcursor/template.py
+++ main/libxcursor/template.py
@@ -10,9 +10,14 @@ license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXcursor-{pkgver}.tar.bz2"
 sha256 = "3ad3e9f8251094af6fe8cb4afcf63e28df504d46bfa5a5529db74a505d628782"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libxcursor-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libxcursor-devel")
 def _devel(self):
