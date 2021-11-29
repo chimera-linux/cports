3186e5592cdc1392c88bd56ae63fe92feabbed6f
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 02:44:44 +0100

    main/libxrender: enable LTO

--- main/libxrender/template.py
+++ main/libxrender/template.py
@@ -11,9 +11,14 @@ license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXrender-{pkgver}.tar.bz2"
 sha256 = "c06d5979f86e64cabbde57c223938db0b939dff49fdb5a793a1d3d0396650949"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libxrender-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libxrender-devel")
 def _devel(self):
