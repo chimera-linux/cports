3fefe499680b39fd212fa5e8536234e81120aab8
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 03:33:11 +0100

    main/harfbuzz: enable LTO

--- main/harfbuzz/template.py
+++ main/harfbuzz/template.py
@@ -23,6 +23,7 @@ license = "MIT"
 url = "http://www.freedesktop.org/wiki/Software/HarfBuzz"
 source = f"https://github.com/{pkgname}/{pkgname}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "f3f3247bdeabf36765acc237a5f651e651e4e9706582b9cc2cf6c9b8102dfa93"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
@@ -34,6 +35,10 @@ def _lib(self):
     return self.default_libs(extra = [
         "usr/lib/girepository-1.0"
     ])
+
+@subpackage("harfbuzz-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("harfbuzz-devel")
 def _devel(self):
