853ea1ddd9e6d3cee4de11db50ebe820db4d9d27
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 02:40:13 +0100

    main/libxshmfence: enable LTO

--- main/libxshmfence/template.py
+++ main/libxshmfence/template.py
@@ -11,9 +11,14 @@ license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libxshmfence-{pkgver}.tar.bz2"
 sha256 = "b884300d26a14961a076fbebc762a39831cb75f92bed5ccf9836345b459220c7"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libxshmfence-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libxshmfence-devel")
 def _devel(self):
