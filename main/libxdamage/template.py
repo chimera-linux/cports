8b78c7c34a09e9e6f7d703687e2c98af950db106
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 02:52:53 +0100

    main/libxdamage: enable LTO

--- main/libxdamage/template.py
+++ main/libxdamage/template.py
@@ -10,9 +10,14 @@ license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXdamage-{pkgver}.tar.bz2"
 sha256 = "b734068643cac3b5f3d2c8279dd366b5bf28c7219d9e9d8717e1383995e0ea45"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libxdamage-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libxdamage-devel")
 def _devel(self):
