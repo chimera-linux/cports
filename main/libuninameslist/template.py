4bd384e5293ffd0de2ae7eb5e832e419e855edf7
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 21:53:59 +0100

    main/libuninameslist: enable LTO

--- main/libuninameslist/template.py
+++ main/libuninameslist/template.py
@@ -9,12 +9,17 @@ license = "BSD-3-Clause"
 url = "https://github.com/fontforge/libuninameslist"
 source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "c089c6164f2cef361c3419a07408be72d6b58d6ef224ec226724a9fa93c0d46e"
+options = ["lto"]
 
 def pre_configure(self):
     self.do("autoreconf", "-if")
 
 def post_install(self):
     self.install_license("LICENSE")
+
+@subpackage("libuninameslist-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libuninameslist-devel")
 def _devel(self):
