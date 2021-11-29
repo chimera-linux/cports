b60ceaf4be4f9f7679e804f61d832decd9ea8b2e
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 03:35:59 +0100

    main/freetype: enable LTO

--- main/freetype/template.py
+++ main/freetype/template.py
@@ -22,11 +22,15 @@ url = "https://freetype.org"
 source = f"$(NONGNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "8bee39bd3968c4804b70614a0a3ad597299ad0e824bc8aad5ce8aaf48067bde7"
 # data files missing
-options = ["!check"]
+options = ["!check", "lto"]
 
 def post_install(self):
     self.install_license("LICENSE.TXT")
     self.install_license("docs/FTL.TXT")
+
+@subpackage("freetype-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("freetype-devel")
 def _devel(self):
