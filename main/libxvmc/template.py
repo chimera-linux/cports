2d0c6f78b5ae1ac871800844148654bc2f2ae45b
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 02:48:32 +0100

    main/libxvmc: enable LTO

--- main/libxvmc/template.py
+++ main/libxvmc/template.py
@@ -11,11 +11,16 @@ license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXvMC-{pkgver}.tar.bz2"
 sha256 = "6b3da7977b3f7eaf4f0ac6470ab1e562298d82c4e79077765787963ab7966dcd"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
     # in xorgproto
     self.rm(self.destdir / "usr/include/X11/extensions/vldXvMC.h")
+
+@subpackage("libxvmc-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libxvmc-devel")
 def _devel(self):
