97bdd5755933667e04f2b594b3a364fc49e5aec0
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 02:43:36 +0100

    main/xkeyboard-config: enable LTO

--- main/xkeyboard-config/template.py
+++ main/xkeyboard-config/template.py
@@ -15,6 +15,7 @@ license = "MIT"
 url = "https://www.freedesktop.org/wiki/Software/XKeyboardConfig"
 source = f"$(XORG_SITE)/data/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "b321d27686ee7e6610ffe7b56e28d5bbf60625a1f595124cd320c0caa717b8ce"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
