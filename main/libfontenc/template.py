bc98756b59b04e17f55020c17f4e3cc56f90e72f
Daniel Kolesa <q66@chimera-linux.org>
2021-11-29 03:37:20 +0100

    main/libfontenc: enable LTO

--- main/libfontenc/template.py
+++ main/libfontenc/template.py
@@ -11,9 +11,14 @@ license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "2cfcce810ddd48f2e5dc658d28c1808e86dcf303eaff16728b9aa3dbc0092079"
+options = ["lto"]
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("libfontenc-static")
+def _static(self):
+    return self.default_static()
 
 @subpackage("libfontenc-devel")
 def _devel(self):
