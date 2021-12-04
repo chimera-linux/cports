5b63937caf809f2f6a728aefe4c94dab665d3cd1
Daniel Kolesa <q66@chimera-linux.org>
2021-12-04 15:03:05 +0100

    main/numactl: new package (2.0.14)

---
+++ main/numactl/template.py
@@ -0,0 +1,36 @@
+pkgname = "numactl"
+pkgver = "2.0.14"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = ["automake", "libtool", "pkgconf", "gmake"]
+makedepends = ["linux-headers"]
+checkdepends = ["bash"]
+pkgdesc = "Simple NUMA policy support"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.1-only"
+url = "https://github.com/numactl/numactl"
+source = f"{url}/archive/v{pkgver}.tar.gz"
+sha256 = "1ee27abd07ff6ba140aaf9bc6379b37825e54496e01d6f7343330cf1a4487035"
+# some tests fail because of gnuisms in testsuite
+options = ["!check"]
+
+def pre_configure(self):
+    self.do("autoreconf", "-if")
+
+def post_install(self):
+    self.rm(self.destdir / "usr/share/man/man2", recursive = True)
+
+@subpackage("libnuma")
+def _lib(self):
+    self.pkgdesc = f"{pkgdesc} (runtime library)"
+
+    return self.default_libs()
+
+@subpackage("libnuma-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("libnuma-devel")
+def _devel(self):
+    return self.default_devel(man = True)
