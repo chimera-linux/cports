7b02ecbcf14800edcecf613eec45e055b7573f94
Daniel Kolesa <q66@chimera-linux.org>
2021-12-04 03:11:27 +0100

    main/font-alias: new package (1.0.4)

---
+++ main/font-alias/template.py
@@ -0,0 +1,14 @@
+pkgname = "font-alias"
+pkgver = "1.0.4"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--with-fontrootdir=/usr/share/fonts"]
+pkgdesc = "Standard aliases for X11 PCF fonts"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/font/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "f3111ae8bf2e980f5f56af400e8eefe5fc9f4207f4a412ea79637fd66c945276"
+
+def post_install(self):
+    self.install_license("COPYING")
