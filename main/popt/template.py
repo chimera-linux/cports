467c0d048bee4997555bcd3eab38d1b0cbcb1bca
Daniel Kolesa <q66@chimera-linux.org>
2021-12-05 03:54:01 +0100

    main/popt: new package (1.18)

---
+++ main/popt/template.py
@@ -0,0 +1,26 @@
+pkgname = "popt"
+pkgver = "1.18"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf", "automake", "libtool", "gettext-tiny"]
+makedepends = ["gettext-tiny-devel"]
+pkgdesc = "Command line option parsing library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://rpm.org"
+source = f"http://ftp.rpm.org/popt/releases/popt-1.x/popt-{pkgver}.tar.gz"
+sha256 = "5159bc03a20b28ce363aa96765f37df99ea4d8850b1ece17d1e6ad5c24fdc5d1"
+
+def pre_configure(self):
+    self.do(self.chroot_cwd / "autogen.sh")
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("popt-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("popt-devel")
+def _devel(self):
+    return self.default_devel(man = True)
