a1d600f8967ce8eeb64f79f9627d98b6975a064c
Daniel Kolesa <q66@chimera-linux.org>
2021-12-05 23:50:52 +0100

    main/libmodplug: new package (0.8.9.0)

---
+++ main/libmodplug/template.py
@@ -0,0 +1,20 @@
+pkgname = "libmodplug"
+pkgver = "0.8.9.0"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--enable-static"]
+hostmakedepends = ["pkgconf"]
+pkgdesc = "MOD playing library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:none"
+url = "http://modplug-xmms.sourceforge.net"
+source = f"$(SOURCEFORGE_SITE)/modplug-xmms/{pkgname}-{pkgver}.tar.gz"
+sha256 = "457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de"
+
+@subpackage("libmodplug-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("libmodplug-devel")
+def _devel(self):
+    return self.default_devel(man = True)
