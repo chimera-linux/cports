49869b248ece295f0eabfbcca9bb508fd7779956
Daniel Kolesa <q66@chimera-linux.org>
2021-12-05 23:42:55 +0100

    main/opusfile: mew package (0.12)

---
+++ main/opusfile/template.py
@@ -0,0 +1,24 @@
+pkgname = "opusfile"
+pkgver = "0.12"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+makedepends = ["libogg-devel", "opus-devel", "openssl-devel"]
+pkgdesc = "Library for opening, seeking, and decoding .opus files"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-3-Clause"
+url = "https://www.opus-codec.org"
+source = f"http://downloads.xiph.org/releases/opus/{pkgname}-{pkgver}.tar.gz"
+sha256 = "118d8601c12dd6a44f52423e68ca9083cc9f2bfe72da7a8c1acb22a80ae3550b"
+
+def post_install(self):
+    self.rm(self.destdir / "usr/share", recursive = True)
+    self.install_license("COPYING")
+
+@subpackage("opusfile-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("opusfile-devel")
+def _devel(self):
+    return self.default_devel(man = True)
