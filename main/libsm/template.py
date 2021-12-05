116bc989304c1e100fea7024bb5898037a563f2a
Daniel Kolesa <q66@chimera-linux.org>
2021-12-05 23:07:07 +0100

    main/libsm: new package (1.2.3)

---
+++ main/libsm/template.py
@@ -0,0 +1,23 @@
+pkgname = "libsm"
+pkgver = "1.2.3"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+makedepends = ["libice-devel", "libuuid-devel", "xtrans"]
+pkgdesc = "X session management library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/lib/libSM-{pkgver}.tar.bz2"
+sha256 = "2d264499dcb05f56438dee12a1b4b71d76736ce7ba7aa6efbf15ebb113769cbb"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libsm-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("libsm-devel")
+def _devel(self):
+    return self.default_devel(man = True)
