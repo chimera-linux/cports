f3b245a3c7e5aea164bb8a1f5345b774545bf938
Daniel Kolesa <q66@chimera-linux.org>
2021-12-05 23:56:23 +0100

    main/smpeg: new package (2.0.0)

---
+++ main/smpeg/template.py
@@ -0,0 +1,23 @@
+pkgname = "smpeg"
+pkgver = "2.0.0"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "pkgconf"]
+makedepends = ["sdl-devel"]
+pkgdesc = "SDL image loading library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.1-or-later"
+url = "https://icculus.org/smpeg"
+source = f"https://www.libsdl.org/projects/smpeg/release/smpeg2-{pkgver}.tar.gz"
+sha256 = "979a65b211744a44fa641a9b6e4d64e64a12ff703ae776bafe3c4c4cd85494b3"
+# no check target
+options = ["!check"]
+
+@subpackage("smpeg-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("smpeg-devel")
+def _devel(self):
+    return self.default_devel()
