041bd2c6470ac4964f84139a511800716a9d07e6
Daniel Kolesa <q66@chimera-linux.org>
2021-12-06 04:33:59 +0100

    main/graphene: new package (1.10.6)

---
+++ main/graphene/template.py
@@ -0,0 +1,39 @@
+pkgname = "graphene"
+_mver = "1.10"
+pkgver = f"{_mver}.6"
+pkgrel = 0
+build_style = "meson"
+configure_args = [
+    "-Dinstalled_tests=false", "-Dgcc_vector=false", "-Dintrospection=enabled"
+]
+hostmakedepends = ["meson", "pkgconf", "gobject-introspection"]
+makedepends = [
+    "libglib-devel"
+]
+pkgdesc = "Thin layer of graphic data types"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://github.com/ebassi/graphene"
+source = f"$(GNOME_SITE)/{pkgname}/{_mver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "80ae57723e4608e6875626a88aaa6f56dd25df75024bd16e9d77e718c3560b25"
+
+# TODO: possibly use SSE intrinsics on ppc64(le)
+# for now we gotta fall back to scalar as clang's vector
+# intrinsics are different from gcc's and not compatible
+
+match self.profile().arch:
+    case "x86_64":
+        configure_args += ["-Dsse2=true"]
+    case "aarch64":
+        configure_args += ["-Darm_neon=true"]
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
+
+@subpackage("graphene-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("graphene-devel")
+def _devel(self):
+    return self.default_devel(extra = ["usr/lib/graphene-1.0"])
