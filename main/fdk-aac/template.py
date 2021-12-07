25d56b9d1583f4d53c5e9047b567d26d41ec5a2f
Daniel Kolesa <q66@chimera-linux.org>
2021-12-07 01:07:14 +0100

    main/fdk-aac: new package (2.0.2)

---
+++ main/fdk-aac/template.py
@@ -0,0 +1,22 @@
+pkgname = "fdk-aac"
+pkgver = "2.0.2"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+pkgdesc = "Fraunhofer FDK AAC library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:FDK-AAC"
+url = "http://www.iis.fraunhofer.de/en/ff/amm/impl/fdkaaccodec.html"
+source = f"$(SOURCEFORGE_SITE)/opencore-amr/{pkgname}-{pkgver}.tar.gz"
+sha256 = "c9e8630cf9d433f3cead74906a1520d2223f89bcd3fa9254861017440b8eb22f"
+
+def post_install(self):
+    self.install_license("NOTICE")
+
+@subpackage("fdk-aac-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("fdk-aac-devel")
+def _devel(self):
+    return self.default_devel(man = True)
