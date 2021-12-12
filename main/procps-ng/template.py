84174822b0a6dfa49895f885148a953b2d1c1b32
Daniel Kolesa <q66@chimera-linux.org>
2021-12-12 04:42:03 +0100

    main/procps-ng: enable check

--- main/procps-ng/template.py
+++ main/procps-ng/template.py
@@ -16,8 +16,6 @@ license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://gitlab.com/procps-ng/procps"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/Production/{pkgname}-{pkgver}.tar.xz"
 sha256 = "4518b3e7aafd34ec07d0063d250fd474999b20b200218c3ae56f5d2113f141b4"
-# missing checkdepends
-options = ["!check"]
 
 def post_install(self):
     self.install_file(self.files_path / "sysctl.conf", "etc")
