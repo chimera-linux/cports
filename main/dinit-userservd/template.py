e5b6052e18db1ebf295f8fd6045340637a6cc8b8
Daniel Kolesa <q66@chimera-linux.org>
2021-12-12 22:45:38 +0100

    main/dinit-userservd: pull in updates

--- main/dinit-userservd/template.py
+++ main/dinit-userservd/template.py
@@ -1,6 +1,6 @@
 pkgname = "dinit-userservd"
 pkgver = "0.1.0"
-_commit = "4427f4614320af3259e1197d7c5be6ade897339b"
+_commit = "0c8198d51076b7f32eff601efbccd71279b9eacc"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/dinit-userservd"
 source = f"https://github.com/chimera-linux/{pkgname}/archive/{_commit}.tar.gz"
-sha256 = "f7b4b646153aabc8ad865e2b3633dbc04e662b1e9fbb1ef92506e1f81a7e658b"
+sha256 = "dfc9d24a15db9b0a159a6c79c78df93480708995f1b289fd503829295c9ba555"
 
 @subpackage("dinit-userservd-dbus")
 def _dbus(self):
