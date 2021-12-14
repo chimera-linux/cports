c88c97d3e973be21ce41e2a126ee8b7cc8381c7e
Daniel Kolesa <q66@chimera-linux.org>
2021-12-14 02:14:24 +0100

    main/libepoxy: new package (1.5.9)

---
+++ main/libepoxy/template.py
@@ -0,0 +1,23 @@
+pkgname = "libepoxy"
+pkgver = "1.5.9"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf", "xorg-util-macros"]
+makedepends = ["mesa-devel"]
+pkgdesc = "Library for handling OpenGL function pointers"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://github.com/anholt/libepoxy"
+source = f"{url}/archive/{pkgver}.tar.gz"
+sha256 = "ee8048d20179a2e86156ac842ddb6428732d9cd7a2cfc2eca905165bf24887a2"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libepoxy-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("libepoxy-devel")
+def _devel(self):
+    return self.default_devel()
