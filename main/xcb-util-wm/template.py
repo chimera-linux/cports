8ec2fcd66dee47435f46a353584d8df66ad2a587
Daniel Kolesa <q66@chimera-linux.org>
2021-12-14 05:08:29 +0100

    main/xcb-util-wm: new package (0.4.1)

---
+++ main/xcb-util-wm/template.py
@@ -0,0 +1,23 @@
+pkgname = "xcb-util-wm"
+pkgver = "0.4.1"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf", "bsdm4"]
+makedepends = ["xcb-util-devel"]
+pkgdesc = "XCB utilities library - window manager helpers for ICCCM"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xcb.freedesktop.org"
+source = f"{url}/dist/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "28bf8179640eaa89276d2b0f1ce4285103d136be6c98262b6151aaee1d3c2a3f"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("xcb-util-wm-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("xcb-util-wm-devel")
+def _devel(self):
+    return self.default_devel(man = True)
