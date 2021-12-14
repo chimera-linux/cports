a72891dcf8e7a58073b379153e2d7d40fb281eb1
Daniel Kolesa <q66@chimera-linux.org>
2021-12-14 04:47:26 +0100

    main/libxxf86dga: new package (1.1.5)

---
+++ main/libxxf86dga/template.py
@@ -0,0 +1,24 @@
+pkgname = "libxxf86dga"
+pkgver = "1.1.5"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--enable-malloc0returnsnull"]
+hostmakedepends = ["pkgconf"]
+makedepends = ["xorgproto", "libxext-devel"]
+pkgdesc = "XFree86-DGA X extension library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/lib/libXxf86dga-{pkgver}.tar.bz2"
+sha256 = "2b98bc5f506c6140d4eddd3990842d30f5dae733b64f198a504f07461bdb7203"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libxxf86dga-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("libxxf86dga-devel")
+def _devel(self):
+    return self.default_devel(man = True)
