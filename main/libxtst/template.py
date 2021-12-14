b4455b4a9df0890e12236b5e8f3619afc900acd9
Daniel Kolesa <q66@chimera-linux.org>
2021-12-14 04:41:08 +0100

    main/libxtst: new package (1.2.3)

---
+++ main/libxtst/template.py
@@ -0,0 +1,23 @@
+pkgname = "libxtst"
+pkgver = "1.2.3"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+makedepends = ["xorgproto", "libxext-devel", "libxi-devel"]
+pkgdesc = "X Tst library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/lib/libXtst-{pkgver}.tar.bz2"
+sha256 = "4655498a1b8e844e3d6f21f3b2c4e2b571effb5fd83199d428a6ba7ea4bf5204"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libxtst-static")
+def _static(self):
+    return self.default_static()
+
+@subpackage("libxtst-devel")
+def _devel(self):
+    return self.default_devel(man = True)
