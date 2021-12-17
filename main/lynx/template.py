d53b9f89bb6fd0b1277ac5f5e8419e5a3c1e0575
Daniel Kolesa <q66@chimera-linux.org>
2021-12-17 02:22:48 +0100

    main/lynx: new package (2.9.0_pre10)

---
+++ main/lynx/template.py
@@ -0,0 +1,20 @@
+pkgname = "lynx"
+pkgver = "2.9.0_pre10"
+_uver = "2.9.0dev.10"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-widec", "--enable-ipv6", "--with-zlib", "--with-bzlib",
+    "--with-ssl"
+]
+hostmakedepends = ["pkgconf"]
+makedepends = [
+    "zlib-devel", "libbz2-devel", "ncurses-devel", "openssl-devel"
+]
+pkgdesc = "Text web browser"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "http://lynx.invisible-island.net"
+source = f"http://invisible-mirror.net/archives/{pkgname}/tarballs/{pkgname}{_uver}.tar.bz2"
+sha256 = "898ac82bcfcbd4b20ea39afdf66fd659b8773c7549623b0f8802bf392a41a912"
+options = ["!cross"]
