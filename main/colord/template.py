c5ba0a030118fab354f9be756fae282b7354633e
Daniel Kolesa <q66@chimera-linux.org>
2021-12-18 23:51:14 +0100

    main/colord: new package(1.4.5)

---
+++ main/colord/template.py
@@ -0,0 +1,65 @@
+pkgname = "colord"
+pkgver = "1.4.5"
+pkgrel = 0
+build_style = "meson"
+# manpages fail to generate
+# TODO: enable sane
+configure_args = [
+    "-Ddaemon_user=_colord",
+    "-Dsystemd=false",
+    "-Dargyllcms_sensor=false",
+    "-Ddocs=false",
+    "-Dman=false",
+    "-Dsane=false",
+    "-Dvapi=true",
+]
+hostmakedepends = [
+    "meson", "pkgconf", "gettext-tiny", "glib-devel",
+    "gobject-introspection", "vala"
+]
+makedepends = [
+    "bash-completion", "lcms2-devel", "libgudev-devel", "libgusb-devel",
+    "polkit-devel", "sqlite-devel",
+]
+pkgdesc = "Color management daemon"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-only"
+url = "https://www.freedesktop.org/software/colord"
+source = f"{url}/releases/{pkgname}-{pkgver}.tar.xz"
+sha256 = "b774ea443d239f4a2ee1853bd678426e669ddeda413dcb71cea1638c4d6c5e17"
+file_modes = {
+    "var/lib/colord": ("_colord:0", "_colord:0", 0o755),
+    "var/lib/colord/icc": ("_colord:0", "_colord:0", 0o755),
+}
+# assertion failed (cd_edid_get_vendor_name (edid) == "LG"): (NULL == "LG")
+options = ["!check"]
+
+if self.profile().cross:
+    hostmakdepends.append("colord")
+
+system_users = [
+    {
+        "name": "_colord",
+        "id": None,
+        "home": "/var/lib/colord",
+    }
+]
+
+def post_install(self):
+    self.install_service(self.files_path / "colord")
+
+    self.install_dir("var/lib/colord/icc")
+    (self.destdir / "var/lib/colord/icc/.empty").touch(mode = 0o644)
+
+@subpackage("libcolord")
+def _lib(self):
+    self.pkgdesc = f"{pkgdesc} (shared library)"
+
+    return [
+        "usr/lib/girepository-1.0",
+        "usr/lib/*.so.*",
+    ]
+
+@subpackage("colord-devel")
+def _devel(self):
+    return self.default_devel(man = True)
