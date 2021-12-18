aee9f236ed45ee5c78dfab77b919bba6cc451ff9
Daniel Kolesa <q66@chimera-linux.org>
2021-12-19 00:25:08 +0100

    main/adwaita-icon-theme: new package (41.0)

---
+++ main/adwaita-icon-theme/template.py
@@ -0,0 +1,12 @@
+pkgname = "adwaita-icon-theme"
+pkgver = "41.0"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "pkgconf"]
+pkgdesc = "Icon theme for GTK+"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-3.0-or-later OR CC-BY-SA-3.0"
+url = f"https://gitlab.gnome.org/GNOME/adwaita-icon-theme"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "ef5339d8c35fcad5d10481b70480803f0fa20b3d3cbc339238fcaceeaee01eba"
