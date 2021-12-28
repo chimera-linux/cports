381c7fe819686b78039090d5afa625e9174b0306
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 13:38:43 +0100

    main/bzip2: make -static automatic

--- main/bzip2/template.py
+++ main/bzip2/template.py
@@ -60,12 +60,6 @@ def _lib(self):
 
     return self.default_libs()
 
-@subpackage("libbz2-static")
-def _static(self):
-    self.pkgdesc = "Bzip2-format compression library (static library)"
-
-    return self.default_static()
-
 @subpackage("libbz2-devel")
 def _devel(self):
     self.pkgdesc = "Bzip2-format compression library (development files)"
