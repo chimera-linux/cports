b8efd3c1ae08f54b3081fd8c082b1fa4e63e36e0
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 02:47:56 +0100

    main/fdk-aac: make -static automatic

--- main/fdk-aac/template.py
+++ main/fdk-aac/template.py
@@ -13,10 +13,6 @@ sha256 = "c9e8630cf9d433f3cead74906a1520d2223f89bcd3fa9254861017440b8eb22f"
 def post_install(self):
     self.install_license("NOTICE")
 
-@subpackage("fdk-aac-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("fdk-aac-devel")
 def _devel(self):
     return self.default_devel()
