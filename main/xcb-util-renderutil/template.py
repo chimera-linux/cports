ebee427a0d952db294b43d4b837361936847034b
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 15:34:47 +0100

    main/xcb-util-renderutil: make -static automatic

--- main/xcb-util-renderutil/template.py
+++ main/xcb-util-renderutil/template.py
@@ -14,10 +14,6 @@ sha256 = "c6e97e48fb1286d6394dddb1c1732f00227c70bd1bedb7d1acabefdd340bea5b"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("xcb-util-renderutil-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("xcb-util-renderutil-devel")
 def _devel(self):
     return self.default_devel()
