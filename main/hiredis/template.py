3c8972214dd03bd4c1df2899126d4ee32f4e5be9
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 22:04:28 +0100

    main/hiredis: new package (1.0.2)

---
+++ main/hiredis/template.py
@@ -0,0 +1,33 @@
+pkgname = "hiredis"
+pkgver = "1.0.2"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_build_args = ["USE_SSL=1"]
+make_install_args = ["USE_SSL=1"]
+hostmakedepends = ["pkgconf", "gmake"]
+makedepends = ["openssl-devel"]
+pkgdesc = "Minimalistic C client library for Redis"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-3-Clause"
+url = "https://github.com/redis/hiredis"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "e0ab696e2f07deb4252dda45b703d09854e53b9703c7d52182ce5a22616c3819"
+# needs redis
+options = ["!check"]
+
+def init_configure(self):
+    self.make_build_args += ["DEBUG=" + self.get_cflags(shell = True)]
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("hiredis-ssl")
+def _ssl(self):
+    self.pkgdesc = f"{pkgdesc} (hiredis_ssl library)"
+
+    return ["usr/lib/libhiredis_ssl.so.*"]
+
+@subpackage("hiredis-devel")
+def _devel(self):
+    return self.default_devel()
