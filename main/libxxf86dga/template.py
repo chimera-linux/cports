f3642b0d58b79f4c456fd26c0c1d43459efa9638
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:55:57 +0100

    main/libxxf86dga: make -static automatic

--- main/libxxf86dga/template.py
+++ main/libxxf86dga/template.py
@@ -15,10 +15,6 @@ sha256 = "2b98bc5f506c6140d4eddd3990842d30f5dae733b64f198a504f07461bdb7203"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("libxxf86dga-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("libxxf86dga-devel")
 def _devel(self):
     return self.default_devel()
