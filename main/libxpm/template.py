9ec298c7bd9ec1ca70cb70fb04e83c2644885e6a
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:49:07 +0100

    main/libxpm: make -static automatic

--- main/libxpm/template.py
+++ main/libxpm/template.py
@@ -14,10 +14,6 @@ sha256 = "9cd1da57588b6cb71450eff2273ef6b657537a9ac4d02d0014228845b935ac25"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("libxpm-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("libxpm-devel")
 def _devel(self):
     return self.default_devel()
