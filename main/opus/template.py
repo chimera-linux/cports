2813d92f10f8a428ecfb65c79e9a310769bfe938
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 15:04:59 +0100

    main/opus: make -static automatic

--- main/opus/template.py
+++ main/opus/template.py
@@ -15,10 +15,6 @@ sha256 = "65b58e1e25b2a114157014736a3d9dfeaad8d41be1c8179866f144a2fb44ff9d"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("opus-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("opus-devel")
 def _devel(self):
     return self.default_devel()
