42cd80eca18f380ac3861729344acb7e2bb6d988
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:27:10 +0100

    main/libspiro: make -static automatic

--- main/libspiro/template.py
+++ main/libspiro/template.py
@@ -13,10 +13,6 @@ sha256 = "00be530b5c0ea9274baadf6c05521f0b192d4c3c1db636ac8b08efd44aaea8f5"
 def pre_configure(self):
     self.do("autoreconf", "-if")
 
-@subpackage("libspiro-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("libspiro-devel")
 def _devel(self):
     return self.default_devel()
