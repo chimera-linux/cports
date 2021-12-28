eb7154c91878321b0e98b2f9143810c5540111d6
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 15:37:04 +0100

    main/xcb-util-wm: make -static automatic

--- main/xcb-util-wm/template.py
+++ main/xcb-util-wm/template.py
@@ -14,10 +14,6 @@ sha256 = "28bf8179640eaa89276d2b0f1ce4285103d136be6c98262b6151aaee1d3c2a3f"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("xcb-util-wm-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("xcb-util-wm-devel")
 def _devel(self):
     return self.default_devel()
