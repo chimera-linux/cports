7d73bfc409b6b5c47efd1b6160e6d0bfc769177d
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:48:41 +0100

    main/libxmu: make -static automatic

--- main/libxmu/template.py
+++ main/libxmu/template.py
@@ -14,10 +14,6 @@ sha256 = "9c343225e7c3dc0904f2122b562278da5fed639b1b5e880d25111561bac5b731"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("libxmu-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("libxmu-devel")
 def _devel(self):
     return self.default_devel()
