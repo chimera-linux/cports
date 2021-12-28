c6786428752c73cbbd46edc3bcbf810c532b1979
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 15:04:06 +0100

    main/nettle: make -static automatic

--- main/nettle/template.py
+++ main/nettle/template.py
@@ -13,10 +13,6 @@ url = "https://www.lysator.liu.se/~nisse/nettle"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "661f5eb03f048a3b924c3a8ad2515d4068e40f67e774e8a26827658007e3bcf0"
 
-@subpackage("nettle-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("nettle-devel")
 def _devel(self):
     return self.default_devel(extra = ["usr/share/info"])
