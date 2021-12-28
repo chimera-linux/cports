2c76b5776be4b6b47ed1f89088d7a4d154cdd7d3
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:37:31 +0100

    main/libxcursor: make -static automatic

--- main/libxcursor/template.py
+++ main/libxcursor/template.py
@@ -14,10 +14,6 @@ sha256 = "3ad3e9f8251094af6fe8cb4afcf63e28df504d46bfa5a5529db74a505d628782"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("libxcursor-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("libxcursor-devel")
 def _devel(self):
     return self.default_devel()
