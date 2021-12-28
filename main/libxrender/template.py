768fb97453d71eb2409a8efc328263517831bfbe
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:50:58 +0100

    main/libxrender: make -static automatic

--- main/libxrender/template.py
+++ main/libxrender/template.py
@@ -15,10 +15,6 @@ sha256 = "c06d5979f86e64cabbde57c223938db0b939dff49fdb5a793a1d3d0396650949"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("libxrender-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("libxrender-devel")
 def _devel(self):
     return self.default_devel()
