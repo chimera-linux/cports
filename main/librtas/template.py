98b52dbf92b90df47d3836ebff9ddad35f87507b
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:15:57 +0100

    main/librtas: make -static automatic

--- main/librtas/template.py
+++ main/librtas/template.py
@@ -15,10 +15,6 @@ sha256 = "b47b2a6f140347ac265e2c66ddf68293f6cdcc7c0c9a78c6e21ff52846465415"
 def pre_configure(self):
     self.do(self.chroot_cwd / "autogen.sh")
 
-@subpackage("librtas-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("librtas-devel")
 def _devel(self):
     return self.default_devel()
