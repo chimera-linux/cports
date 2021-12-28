61a748b8e5c8cc92824423c34aec8ebc9efddf2e
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:49:33 +0100

    main/libxpresent: make -static automatic

--- main/libxpresent/template.py
+++ main/libxpresent/template.py
@@ -14,10 +14,6 @@ sha256 = "c11ae015141a9afbe10f4f2b8ee00b11adca6373dc1b9808d7c6c138b2da7b8a"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("libxpresent-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("libxpresent-devel")
 def _devel(self):
     return self.default_devel()
