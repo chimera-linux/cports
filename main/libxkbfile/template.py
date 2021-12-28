c066410adb4e58e5bdc0c172a8d9e1fd39649b5a
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:45:18 +0100

    main/libxkbfile: make -static automatic

--- main/libxkbfile/template.py
+++ main/libxkbfile/template.py
@@ -14,10 +14,6 @@ sha256 = "758dbdaa20add2db4902df0b1b7c936564b7376c02a0acd1f2a331bd334b38c7"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("libxkbfile-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("libxkbfile-devel")
 def _devel(self):
     return self.default_devel()
