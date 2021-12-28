6aa855917fea4cb1c0ec8703fdc0d38ec7678dc2
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:56:46 +0100

    main/libxxf86vm: make -static automatic

--- main/libxxf86vm/template.py
+++ main/libxxf86vm/template.py
@@ -15,10 +15,6 @@ sha256 = "afee27f93c5f31c0ad582852c0fb36d50e4de7cd585fcf655e278a633d85cd57"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("libxxf86vm-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("libxxf86vm-devel")
 def _devel(self):
     return self.default_devel()
