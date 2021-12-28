119e29bf91a08541ec712c9a8c25c5fbebe877f3
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 15:08:49 +0100

    main/orc: make -static automatic

--- main/orc/template.py
+++ main/orc/template.py
@@ -19,10 +19,6 @@ sha256 = "a66e3d8f2b7e65178d786a01ef61f2a0a0b4d0b8370de7ce134ba73da4af18f0"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("orc-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("orc-devel")
 def _devel(self):
     return self.default_devel(extra = ["usr/share/gtk-doc"])
