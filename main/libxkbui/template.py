3af94e80588c54d2b5df3ef01a8e76d867bc7a6e
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 14:45:42 +0100

    main/libxkbui: make -static automatic

--- main/libxkbui/template.py
+++ main/libxkbui/template.py
@@ -14,10 +14,6 @@ sha256 = "20c23101d63234ee5f6d696dfa069b29c6c58e39eff433bcd7705b50b3ffa214"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("libxkbui-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("libxkbui-devel")
 def _devel(self):
     return self.default_devel()
