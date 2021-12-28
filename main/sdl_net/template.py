fd2b9b086dba061f9268f8b7a1ab2a2a2d4b3fc9
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 15:18:30 +0100

    main/sdl_net: make -static automatic

--- main/sdl_net/template.py
+++ main/sdl_net/template.py
@@ -17,10 +17,6 @@ options = ["!check"]
 def post_install(self):
     self.install_license("COPYING.txt")
 
-@subpackage("sdl_net-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("sdl_net-devel")
 def _devel(self):
     return self.default_devel()
