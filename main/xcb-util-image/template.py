fd4f2b957854edd1cc3187894332e47d618616c8
Daniel Kolesa <q66@chimera-linux.org>
2021-12-28 15:33:48 +0100

    main/xcb-util-image: make -static automatic

--- main/xcb-util-image/template.py
+++ main/xcb-util-image/template.py
@@ -14,10 +14,6 @@ sha256 = "2db96a37d78831d643538dd1b595d7d712e04bdccf8896a5e18ce0f398ea2ffc"
 def post_install(self):
     self.install_license("COPYING")
 
-@subpackage("xcb-util-image-static")
-def _static(self):
-    return self.default_static()
-
 @subpackage("xcb-util-image-devel")
 def _devel(self):
     return self.default_devel()
