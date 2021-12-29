fc252ec84157820c9177200df46e1224e68ebdca
Daniel Kolesa <q66@chimera-linux.org>
2021-12-29 22:03:53 +0100

    main/efl: enable rsvg

--- main/efl/template.py
+++ main/efl/template.py
@@ -8,11 +8,11 @@ configure_args = [
     "-Dembedded-lz4=false",
     "-Dcrypto=openssl",
     "-Decore-imf-loaders-disabler=ibus,scim",
-    # FIXMEs:
-    # enable pdf, ps, raw: libpoppler, libspectre, libraw
-    # enable jp2k: libopenjpeg2
-    # maybe enable avif
-    "-Devas-loaders-disabler=avif,rsvg,json,pdf,ps",
+    # TODO: maybe enable avif
+    # ps needs libspectre and thus ghostscript (AGPL)
+    # pdf needs poppler, which is GPL and probably leaks
+    # into the EFL distribution so we don't want that
+    "-Devas-loaders-disabler=avif,json,pdf,ps",
     "-Dlua-interpreter=lua",
     "-Dbindings=cxx",
     "-Dopengl=es-egl",
@@ -43,9 +43,9 @@ makedepends = [
     "xcb-util-renderutil-devel", "xorgproto", "liblz4-devel", "zlib-devel",
     "fontconfig-devel", "fribidi-devel", "harfbuzz-devel", "freetype-devel",
     "libjpeg-turbo-devel", "libpng-devel", "giflib-devel", "libtiff-devel",
-    "libwebp-devel", "openjpeg-devel", "libraw-devel", "libpulse-devel",
-    "libsndfile-devel", "gstreamer-devel", "gst-plugins-base-devel",
-    "glib-devel", "avahi-devel", "lua5.1-devel",
+    "libwebp-devel", "openjpeg-devel", "libraw-devel", "librsvg-devel",
+    "libpulse-devel", "libsndfile-devel", "gstreamer-devel",
+    "gst-plugins-base-devel", "glib-devel", "avahi-devel", "lua5.1-devel",
 ]
 checkdepends = ["dbus", "xvfb-run", "check-devel"]
 pkgdesc = "Enlightenment Foundation Libraries"
