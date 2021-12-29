34dd0594650e1fdae4078135d1a36ffa42cd8192
Daniel Kolesa <q66@chimera-linux.org>
2021-12-29 21:50:51 +0100

    main/efl: enable raw support

--- main/efl/template.py
+++ main/efl/template.py
@@ -12,7 +12,7 @@ configure_args = [
     # enable pdf, ps, raw: libpoppler, libspectre, libraw
     # enable jp2k: libopenjpeg2
     # maybe enable avif
-    "-Devas-loaders-disabler=avif,rsvg,json,pdf,ps,raw",
+    "-Devas-loaders-disabler=avif,rsvg,json,pdf,ps",
     "-Dlua-interpreter=lua",
     "-Dbindings=cxx",
     "-Dopengl=es-egl",
@@ -43,9 +43,9 @@ makedepends = [
     "xcb-util-renderutil-devel", "xorgproto", "liblz4-devel", "zlib-devel",
     "fontconfig-devel", "fribidi-devel", "harfbuzz-devel", "freetype-devel",
     "libjpeg-turbo-devel", "libpng-devel", "giflib-devel", "libtiff-devel",
-    "libwebp-devel", "openjpeg-devel", "libpulse-devel", "libsndfile-devel",
-    "gstreamer-devel", "gst-plugins-base-devel", "glib-devel", "avahi-devel",
-    "lua5.1-devel",
+    "libwebp-devel", "openjpeg-devel", "libraw-devel", "libpulse-devel",
+    "libsndfile-devel", "gstreamer-devel", "gst-plugins-base-devel",
+    "glib-devel", "avahi-devel", "lua5.1-devel",
 ]
 checkdepends = ["dbus", "xvfb-run", "check-devel"]
 pkgdesc = "Enlightenment Foundation Libraries"
