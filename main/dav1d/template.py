511b196fd7b4b34ace7f05db51466ac9b3830d3d
Daniel Kolesa <q66@chimera-linux.org>
2021-12-30 15:49:04 +0100

    main/dav1d: new package (0.9.2)

---
+++ main/dav1d/template.py
@@ -0,0 +1,23 @@
+pkgname = "dav1d"
+pkgver = "0.9.2"
+pkgrel = 0
+build_style = "meson"
+configure_args = [
+    "-Denable_tests=true", "-Denable_asm=true",
+    "-Denable_tools=true", "-Dfuzzing_engine=none",
+]
+hostmakedepends = ["meson", "pkgconf", "nasm"]
+pkgdesc = "Small and fast AV1 decoder"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-2-Clause"
+url = "https://code.videolan.org/videolan/dav1d"
+source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "59a5fc9cc5d8ea780ad71ede6d589ed33fb5179d87780dcf80a00ee854952935"
+
+@subpackage("dav1d-devel")
+def _devel(self):
+    return self.default_devel()
+
+@subpackage("dav1d-progs")
+def _progs(self):
+    return self.default_progs()
