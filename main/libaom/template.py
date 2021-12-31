92a4f97c1ce1681ed81bcb6bd5616d621cd1098d
Daniel Kolesa <q66@chimera-linux.org>
2021-12-31 03:40:50 +0100

    main/libaom: new package (3.2.0)

---
+++ main/libaom/template.py
@@ -0,0 +1,33 @@
+pkgname = "libaom"
+pkgver = "3.2.0"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DENABLE_TESTS=OFF"]
+hostmakedepends = [
+    "cmake", "ninja", "pkgconf", "perl", "python", "yasm", "doxygen"
+]
+makedepends = ["linux-headers"]
+pkgdesc = "Reference implementation of the AV1 codec"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-2-Clause"
+url = "https://aomedia.org"
+source = [f"https://aomedia.googlesource.com/aom/+archive/v{pkgver}.tar.gz"]
+sha256 = ["5d1919c6ccd8811f344a89150a89234404e8273734cb0bd91e48b045d3226439"]
+tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=2097152"]}
+# requires a testdata download, tests take long
+options = ["!check"]
+
+match self.profile().arch:
+    case "ppc64":
+        configure_args += ["-DENABLE_VSX=0"]
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+@subpackage("libaom-devel")
+def _devel(self):
+    return self.default_devel()
+
+@subpackage("libaom-progs")
+def _progs(self):
+    return self.default_progs()
