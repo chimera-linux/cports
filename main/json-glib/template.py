67fb48e5c01a0c02d907478e64bfdafd54040017
Daniel Kolesa <q66@chimera-linux.org>
2022-01-05 18:14:00 +0100

    main/json-glib: new package (1.6.6)

---
+++ main/json-glib/template.py
@@ -0,0 +1,17 @@
+pkgname = "json-glib"
+pkgver = "1.6.6"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dintrospection=enabled"]
+hostmakedepends = ["meson", "pkgconf", "glib-devel", "gobject-introspection"]
+makedepends = ["libglib-devel"]
+pkgdesc = "JSON parser for glib-based projects"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.1-or-later"
+url = "https://wiki.gnome.org/action/show/Projects/JsonGlib"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "96ec98be7a91f6dde33636720e3da2ff6ecbb90e76ccaa49497f31a6855a490e"
+
+@subpackage("json-glib-devel")
+def _devel(self):
+    return self.default_devel()
