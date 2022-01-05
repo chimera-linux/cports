1655b99e2b6eea485561ab8154ca380ce2cf4695
Daniel Kolesa <q66@chimera-linux.org>
2022-01-05 17:54:15 +0100

    main/xdg-dbus-proxy: new package (0.1.2)

---
+++ main/xdg-dbus-proxy/template.py
@@ -0,0 +1,14 @@
+pkgname = "xdg-dbus-proxy"
+pkgver = "0.1.2"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--enable-man"]
+hostmakedepends = ["pkgconf", "xsltproc", "docbook-xsl-nons"]
+makedepends = ["libglib-devel"]
+checkdepends = ["dbus"]
+pkgdesc = "Filtering proxy for D-Bus connections"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/flatpak/xdg-dbus-proxy"
+source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "1749d6f9f46dcc9edc87725641cf56cf91dcad1b01707891ea0850c1000c520f"
