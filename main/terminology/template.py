238e9d0196363a68cb018fb738d012327d93d0bb
Daniel Kolesa <q66@chimera-linux.org>
2022-01-05 06:43:45 +0100

    main/terminology: update to 1.12.1

--- main/terminology/template.py
+++ main/terminology/template.py
@@ -1,5 +1,5 @@
 pkgname = "terminology"
-pkgver = "1.11.0"
+pkgver = "1.12.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "gettext-tiny"]
@@ -9,7 +9,7 @@ pkgdesc = "EFL-based terminal emulator"
 license = "BSD-2-Clause"
 url = "https://enlightenment.org"
 source = f"http://download.enlightenment.org/rel/apps/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "4fd884bd2ffbbc86d87163063074fbd969be04b17bb8d7e23cd1f6708fd86a2d"
+sha256 = "f8ced9584c2e9ae87452ce7425fd25b2d3e122c7489785d2917890215c6b5aa9"
 
 def post_install(self):
     self.install_license("COPYING")
