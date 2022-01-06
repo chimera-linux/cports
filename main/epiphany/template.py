dd27c809a1844410651bc14701ccfde9d2c14b48
Daniel Kolesa <q66@chimera-linux.org>
2022-01-06 06:13:12 +0100

    main/epiphany: new package (41.3)

---
+++ main/epiphany/template.py
@@ -0,0 +1,20 @@
+pkgname = "epiphany"
+pkgver = "41.3"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dunit_tests=disabled", "-Dsoup2=disabled"]
+hostmakedepends = ["meson", "pkgconf", "glib-devel", "gettext-tiny", "itstool"]
+makedepends = [
+    "webkitgtk-devel", "cairo-devel", "gcr-devel", "gdk-pixbuf-devel",
+    "libglib-devel", "gsettings-desktop-schemas-devel", "gtk+3-devel",
+    "nettle-devel", "json-glib-devel", "libarchive-devel", "libdazzle-devel",
+    "libhandy-devel", "libsecret-devel", "libxml2-devel", "libportal-devel",
+    "libsoup-devel", "sqlite-devel", "iso-codes",
+]
+depends = ["hicolor-icon-theme", "iso-codes"]
+pkgdesc = "GNOME web browser"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-3.0-or-later"
+url = "https://wiki.gnome.org/Apps/Web"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "ba01268ee54f318dfdac2e01eba38a3fc96940c1cbf640ed2613ae29f8bcb9ad"
