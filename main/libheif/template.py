27685bccf0f92d1bf021386eccb363d6a15133d0
Daniel Kolesa <q66@chimera-linux.org>
2022-01-12 05:50:56 +0100

    main/libheif: new package (1.12.0)

---
+++ main/libheif/template.py
@@ -0,0 +1,27 @@
+pkgname = "libheif"
+pkgver = "1.12.0"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--disable-option-checking"]
+hostmakedepends = ["pkgconf", "automake", "libtool"]
+makedepends = [
+    "libde265-devel", "x265-devel", "libaom-devel", "dav1d-devel",
+    "libjpeg-turbo-devel", "libpng-devel"
+]
+pkgdesc = "HEIF and AVIF file format decoder and encoder"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-3.0-or-later"
+url = "http://www.libheif.org"
+source = f"https://github.com/strukturag/{pkgname}/archive/v{pkgver}.tar.gz"
+sha256 = "086145b0d990182a033b0011caadb1b642da84f39ab83aa66d005610650b3c65"
+
+def pre_configure(self):
+    self.do(self.chroot_cwd / "autogen.sh")
+
+@subpackage("libheif-devel")
+def _devel(self):
+    return self.default_devel()
+
+@subpackage("libheif-progs")
+def _progs(self):
+    return self.default_progs()
