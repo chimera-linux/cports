df508a018fc659ef131a8756da6d41a6b27d3fa6
yopito <pierre.bourgin@free.fr>
2022-01-12 09:00:07 +0100

    main/libaom: fix sha256's distfile

--- main/libaom/template.py
+++ main/libaom/template.py
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://aomedia.org"
 source = [f"https://aomedia.googlesource.com/aom/+archive/v{pkgver}.tar.gz"]
-sha256 = ["5d1919c6ccd8811f344a89150a89234404e8273734cb0bd91e48b045d3226439"]
+sha256 = ["3d618b65eb601c348a10ef375dc6a72fe4c94d702a0ca4be1b268ffcf9c46609"]
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=2097152"]}
 # requires a testdata download, tests take long
 options = ["!check"]
