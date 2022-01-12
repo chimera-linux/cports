acd2eb4fed2ccea7b002a28a9cc80a85f9ff95cd
Daniel Kolesa <q66@chimera-linux.org>
2022-01-12 21:27:47 +0100

    main/libaom: switch to custom source location

--- main/libaom/template.py
+++ main/libaom/template.py
@@ -11,7 +11,8 @@ pkgdesc = "Reference implementation of the AV1 codec"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://aomedia.org"
-source = [f"https://aomedia.googlesource.com/aom/+archive/v{pkgver}.tar.gz"]
+# there are no official tarballs and no deterministic autotarballs
+source = [f"https://ftp.octaforge.org/q66/random/{pkgname}-{pkgver}.tar.gz"]
 sha256 = ["5d1919c6ccd8811f344a89150a89234404e8273734cb0bd91e48b045d3226439"]
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=2097152"]}
 # requires a testdata download, tests take long
