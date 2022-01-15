5cfaf57b3de1b3bbca7dfbbb4d270f79cf64e702
Daniel Kolesa <q66@chimera-linux.org>
2022-01-15 16:55:57 +0100

    main/gnutls: move back from contrib, add remaining deps
    This is currently required by CUPS, which we need in main in order
    to have reasonable printing support in gtk, bluez and other stuff.

--- main/gnutls/template.py
+++ main/gnutls/template.py
@@ -2,18 +2,19 @@ pkgname = "gnutls"
 pkgver = "3.7.2"
 pkgrel = 0
 build_style = "gnu_configure"
-# FIXME: do not use included libunistring, add libidn2, trousers, unbound
 configure_args = [
-    "--with-zlib", "--with-nettle-mini", "--with-included-unistring",
-    "--disable-guile", "--disable-static",
+    "--with-zlib", "--with-nettle-mini", "--disable-guile", "--disable-static",
     "--disable-valgrind-tests", "--disable-rpath",
     "--with-default-trust-store-file=/etc/ssl/certs/ca-certificates.crt",
 ]
 hostmakedepends = ["pkgconf", "gettext-tiny"]
 makedepends = [
-    "nettle-devel", "libtasn1-devel", "zlib-devel", "lzo-devel",
-    "libgpg-error-devel", "libgcrypt-devel", "p11-kit-devel",
+    "nettle-devel", "libtasn1-devel", "libidn2-devel",  "libgcrypt-devel",
+    "libgpg-error-devel", "libunistring-devel", "zlib-devel", "lzo-devel",
+    "p11-kit-devel", "unbound-devel", "trousers-devel",
 ]
+# dlopened
+depends = ["libtspi"]
 pkgdesc = "GNU Transport Layer Security library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
@@ -25,6 +26,8 @@ options = ["!check"]
 
 @subpackage("gnutls-devel")
 def _devel(self):
+    self.depends += ["trousers-devel"]
+
     return self.default_devel(extra = ["usr/share/info"])
 
 @subpackage("gnutls-progs")
