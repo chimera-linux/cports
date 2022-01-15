309c4f7d3ee5b2888eb4817c82d48e06c9ebdd63
Daniel Kolesa <q66@chimera-linux.org>
2022-01-15 22:21:56 +0100

    main/glib-networking: enable both openssl and gnutls backends
    And split them up into subpackages that are installed whenever
    their backend library is installed. When installed, GnuTLS backend
    will take preference over the OpenSSL one (since it's more fully
    featured and that's how things are set up).

--- main/glib-networking/template.py
+++ main/glib-networking/template.py
@@ -3,14 +3,14 @@ pkgver = "2.70.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
-    "-Dgnutls=disabled", "-Dopenssl=enabled", "-Dlibproxy=enabled",
+    "-Dgnutls=enabled", "-Dopenssl=enabled", "-Dlibproxy=enabled",
     "-Dgnome_proxy=enabled"
 ]
 hostmakedepends = [
     "meson", "pkgconf", "ca-certificates", "gettext-tiny"
 ]
 makedepends = [
-    "openssl-devel", "gsettings-desktop-schemas-devel",
+    "openssl-devel", "gnutls-devel", "gsettings-desktop-schemas-devel",
     "libglib-devel", "libproxy-devel"
 ]
 depends = ["gsettings-desktop-schemas"]
@@ -24,3 +24,19 @@ sha256 = "2a16bfc2d271ccd3266e3fb462bc8a4103c02e81bbb339aa92d6fb060592d7bc"
 
 def post_install(self):
     self.rm(self.destdir / "usr/lib/systemd", recursive = True)
+
+@subpackage("glib-networking-openssl")
+def _gnutls(self):
+    self.pkgdesc = f"{pkgdesc} (OpenSSL backend)"
+    # autoinstall if openssl is installed
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}", "openssl"]
+
+    return ["usr/lib/gio/modules/libgioopenssl.so"]
+
+@subpackage("glib-networking-gnutls")
+def _gnutls(self):
+    self.pkgdesc = f"{pkgdesc} (GnuTLS backend)"
+    # autoinstall if gnutls is installed
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}", "gnutls"]
+
+    return ["usr/lib/gio/modules/libgiognutls.so"]
