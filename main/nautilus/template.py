042e6695cffc20e8ee36249f69b4912631d2fe70
Daniel Kolesa <q66@chimera-linux.org>
2022-01-18 02:17:47 +0100

    main/nautilus: new package (41.2)

---
+++ main/nautilus/template.py
@@ -0,0 +1,33 @@
+pkgname = "nautilus"
+pkgver = "41.2"
+pkgrel = 0
+build_style = "meson"
+configure_args = [
+    "-Dextensions=false", # TODO: needs libgexiv2
+    "-Dtests=headless",
+]
+hostmakedepends = [
+    "meson", "pkgconf", "glib-devel", "gettext-tiny", "gobject-introspection",
+]
+makedepends = [
+    "libglib-devel", "gnome-desktop-devel", "gnome-autoar-devel",
+    "libhandy-devel", "gtk+3-devel", "libportal-devel", "tracker-devel",
+    "libxml2-devel",
+]
+depends = ["hicolor-icon-theme", "tracker", "tracker-miners"]
+checkdepends = ["dbus", "tracker", "tracker-miners", "python-gobject"]
+pkgdesc = "GNOME file manager"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
+url = "https://wiki.gnome.org/Apps/Files"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "872285b1c40a6ee418ce295ed115f5427da70907d822d95bcf51675d5498822b"
+options = ["!cross"]
+
+@subpackage("nautilus-devel")
+def _devel(self):
+    return self.default_devel()
+
+@subpackage("nautilus-libs")
+def _libs(self):
+    return self.default_libs()
