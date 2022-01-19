5f9adc7adbff8e5ce8b69c4e96b59fd16bbda0c3
Daniel Kolesa <q66@chimera-linux.org>
2022-01-19 01:07:36 +0100

    main/libnftnl: new package (1.2.1)

---
+++ main/libnftnl/template.py
@@ -0,0 +1,22 @@
+pkgname = "libnftnl"
+pkgver = "1.2.1"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_env = {"MAKE": "gmake"}
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = ["gmake", "pkgconf"]
+makedepends = ["linux-headers", "libmnl-devel"]
+pkgdesc = "Low-level netlink API to nf_tables"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://www.netfilter.org/projects/libnftnl"
+source = f"{url}/files/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "7508a5c414fab13e3cb3ce8262d0ce4f02c1590a8e4f8628ab497b5b4585937c"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libnftnl-devel")
+def _devel(self):
+    return self.default_devel()
