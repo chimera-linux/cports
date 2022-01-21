25e54906dd7d756c07d98a39f5a86e79d7a79de8
Daniel Kolesa <q66@chimera-linux.org>
2022-01-21 04:58:35 +0100

    main/mpfr: new package (4.1.0)

---
+++ main/mpfr/template.py
@@ -0,0 +1,19 @@
+pkgname = "mpfr"
+pkgver = "4.1.0"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--enable-thread-safe"]
+hostmakedepends = ["pkgconf"]
+makedepends = ["gmp-devel"]
+pkgdesc = "Library for multiple-precision floating-point computations"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-3.0-or-later"
+url = "https://www.mpfr.org"
+source = f"{url}/{pkgname}-{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "0c98a3f1732ff6ca4ea690552079da9c597872d30e96ec28414ee23c95558a7f"
+
+@subpackage("mpfr-devel")
+def _devel(self):
+    self.depends += ["gmp-devel"]
+
+    return self.default_devel()
