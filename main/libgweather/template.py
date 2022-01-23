4d7f8b5f96146bd041c40db644f8e4a38167685c
Daniel Kolesa <q66@chimera-linux.org>
2022-01-23 19:44:40 +0100

    main/libgweather: new package (3.91.0)

---
+++ main/libgweather/template.py
@@ -0,0 +1,29 @@
+pkgname = "libgweather"
+# use devel snapshot for now for libsoup3 support
+pkgver = "3.91.0"
+pkgrel = 0
+build_style = "meson"
+configure_args = [
+    "-Dsoup2=false", "-Dgtk_doc=false", "-Denable_vala=true",
+    "-Dzoneinfo_dir=/usr/share/zoneinfo",
+]
+hostmakedepends = [
+    "meson", "pkgconf", "gobject-introspection", "glib-devel",
+    "gettext-tiny", "vala", "python-gobject-devel"
+]
+makedepends = [
+    "geocode-glib-devel", "gtk+3-devel", "libsoup-devel"
+]
+depends = ["tzdata"]
+pkgdesc = "GNOME Weather information access library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.1-or-later"
+url = "https://wiki.gnome.org/Projects/LibGWeather"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "725b3eb34fc6d21edc80b6e684728b3088b1fa144f0a2a4d3e9605c7a8f3dcf8"
+# needs network access
+options = ["!check"]
+
+@subpackage("libgweather-devel")
+def _devel(self):
+    return self.default_devel()
