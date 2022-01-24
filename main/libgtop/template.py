bf91a6d99215cb219844b14608f25d256e1548ef
Daniel Kolesa <q66@chimera-linux.org>
2022-01-24 05:37:52 +0100

    main/libgtop: new pakage (2.40.0)

---
+++ main/libgtop/template.py
@@ -0,0 +1,18 @@
+pkgname = "libgtop"
+pkgver = "2.40.0"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--with-libgtop-smp", "--enable-introspection"]
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "pkgconf", "perl", "gobject-introspection"]
+makedepends = ["libglib-devel", "libxau-devel", "linux-headers"]
+pkgdesc = "GNOME library to retrieve system information"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://gitlab.gnome.org/GNOME/libgtop"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "78f3274c0c79c434c03655c1b35edf7b95ec0421430897fb1345a98a265ed2d4"
+
+@subpackage("libgtop-devel")
+def _devel(self):
+    return self.default_devel()
