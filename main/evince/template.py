6d579a3cc0ade5c86cd14536d36a0a79e3339e7a
Daniel Kolesa <q66@chimera-linux.org>
2022-01-25 23:33:07 +0100

    main/evince: new package (41.3)

---
+++ main/evince/template.py
@@ -0,0 +1,35 @@
+pkgname = "evince"
+pkgver = "41.3"
+pkgrel = 0
+build_style = "meson"
+# TODO: xps, dvi, gspell, djvu
+configure_args = [
+    "-Dintrospection=true", "-Dgtk_doc=false",
+    "-Dcomics=enabled", "-Dps=enabled", "-Dxps=disabled", "-Ddvi=disabled",
+    "-Dt1lib=disabled", "-Dgspell=disabled", "-Ddjvu=disabled",
+]
+hostmakedepends = [
+    "meson", "pkgconf", "gobject-introspection", "glib-devel", "itstool",
+    "gettext-tiny", "perl-xml-parser", "adwaita-icon-theme",
+]
+makedepends = [
+    "gtk+3-devel", "libglib-devel", "libhandy-devel", "nautilus-devel",
+    "dbus-devel", "libsecret-devel", "gstreamer-devel", "libspectre-devel",
+    "libarchive-devel", "libpoppler-glib-devel", "gst-plugins-base-devel",
+    "gsettings-desktop-schemas-devel", "libtiff-devel", "adwaita-icon-theme",
+]
+depends = ["hicolor-icon-theme"]
+pkgdesc = "GNOME document viewer"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://wiki.gnome.org/Apps/Evince"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "3346b01f9bdc8f2d5ffea92f110a090c64a3624942b5b543aad4592a9de33bb0"
+
+@subpackage("evince-libs")
+def _libs(self):
+    return self.default_libs()
+
+@subpackage("evince-devel")
+def _devel(self):
+    return self.default_devel()
