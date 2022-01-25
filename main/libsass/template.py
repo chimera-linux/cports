75f2f0bdc7662da599c30c0c4808415c52e704b3
Daniel Kolesa <q66@chimera-linux.org>
2022-01-25 03:16:15 +0100

    main/libsass: new package (3.6.5)

---
+++ main/libsass/template.py
@@ -0,0 +1,26 @@
+pkgname = "libsass"
+pkgver = "3.6.5"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = ["pkgconf", "gmake", "automake", "libtool"]
+pkgdesc = "C implementation of the Sass CSS preprocessor"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://www.sass-lang.com/libsass"
+source = f"https://github.com/sass/{pkgname}/archive/{pkgver}.tar.gz"
+sha256 = "89d8f2c46ae2b1b826b58ce7dde966a176bac41975b82e84ad46b01a55080582"
+
+def pre_configure(self):
+    # otherwise the .pc file will have 'na' version
+    with open(self.cwd / "VERSION", "w") as vf:
+        vf.write(pkgver)
+    # rebuild configure afterwards
+    self.do("autoreconf", "-if")
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+@subpackage("libsass-devel")
+def _devel(self):
+    return self.default_devel()
