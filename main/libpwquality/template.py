3ba862db727533d1f841fd6a8bed1d197e8c3087
Daniel Kolesa <q66@chimera-linux.org>
2022-01-25 04:39:52 +0100

    main/libpwquality: new package (1.4.4)

---
+++ main/libpwquality/template.py
@@ -0,0 +1,32 @@
+pkgname = "libpwquality"
+pkgver = "1.4.4"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--disable-static", "--enable-pam", "--with-securedir=/usr/lib/security",
+]
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = ["gmake", "pkgconf", "gettext-tiny-devel", "python-devel"]
+makedepends = ["cracklib-devel", "linux-pam-devel", "python-devel"]
+pkgdesc = "Library for password quality checking"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-3-Clause OR GPL-2.0-or-later"
+url = "https://github.com/libpwquality/libpwquality"
+source = f"{url}/releases/download/{pkgname}-{pkgver}/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "d43baf23dc6887fe8f8e9b75cabaabc5f4bbbaa0f9eff44278d276141752a545"
+options = ["!cross"]
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libpwquality-devel")
+def _devel(self):
+    return self.default_devel()
+
+@subpackage("libpwquality-python")
+def _python(self):
+    self.pkgdesc = f"{pkgdesc} (Python bindings)"
+    self.depends += ["cracklib-devel"]
+
+    return ["usr/lib/python*"]
