e2c8209a9229ba1e4e7e293be6caf2c3ad21d532
Daniel Kolesa <q66@chimera-linux.org>
2022-01-26 00:10:23 +0100

    main/gnome-screenshot: new package (41.0)

---
+++ main/gnome-screenshot/template.py
@@ -0,0 +1,15 @@
+pkgname = "gnome-screenshot"
+pkgver = "41.0"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf", "gettext-tiny", "glib-devel"]
+makedepends = [
+    "libcanberra-devel", "libglib-devel", "libhandy-devel", "gtk+3-devel",
+    "libxext-devel", "libx11-devel"
+]
+pkgdesc = "Optional extensions for GNOME shell"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://gitlab.gnome.org/GNOME/gnome-screenshot"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "4adb7dec926428f74263d5796673cf142e4720b6e768f5468a8d0933f98c9597"
