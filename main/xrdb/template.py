4c9adedad788cd54ef489356884c0dbdee721931
Daniel Kolesa <q66@chimera-linux.org>
2022-01-25 05:19:53 +0100

    main/xrdb: new package (1.2.1)

---
+++ main/xrdb/template.py
@@ -0,0 +1,16 @@
+pkgname = "xrdb"
+pkgver = "1.2.1"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--with-cpp=/usr/bin/clang-cpp,/usr/bin/cpp"]
+hostmakedepends = ["pkgconf"]
+makedepends = ["libx11-devel", "libxmu-devel"]
+pkgdesc = "X server resource database utility"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "4f5d031c214ffb88a42ae7528492abde1178f5146351ceb3c05f3b8d5abee8b4"
+
+def post_install(self):
+    self.install_license("COPYING")
