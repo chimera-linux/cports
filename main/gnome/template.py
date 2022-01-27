880bb6461c39d89bfdd342e6c2c8dbedfa0bb2aa
Daniel Kolesa <q66@chimera-linux.org>
2022-01-27 03:00:20 +0100

    main/gnome: new package (41)
    This one is incomplete.

---
+++ main/gnome/template.py
@@ -0,0 +1,39 @@
+pkgname = "gnome"
+pkgver = "41"
+pkgrel = 0
+build_style = "meta"
+depends = [
+    f"gnome-core>={pkgver}",
+    #"baobab",
+    #"eog",
+    "epiphany",
+    "evince",
+    #"gedit",
+    #"gnome-calculator",
+    #"gnome-calendar",
+    #"gnome-characters",
+    #"gnome-clocks",
+    #"gnome-contacts",
+    #"gnome-connections",
+    #"gnome-disk-utility",
+    #"gnome-font-viewer",
+    #"gnome-maps",
+    #"gnome-music",
+    #"gnome-remote-desktop",
+    "gnome-screenshot",
+    "gnome-shell-extensions",
+    #"gnome-system-monitor",
+    "gnome-terminal",
+    #"gnome-user-docs",
+    #"gnome-user-share",
+    #"gnome-weather",
+    #"orca",
+    #"phodav",
+    #"simple-scan",
+    "sushi",
+    #"totem",
+]
+pkgdesc = "GNOME desktop environment (session and apps)"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:meta"
+url = "https://www.gnome.org"
