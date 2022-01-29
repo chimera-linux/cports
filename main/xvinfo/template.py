5ff134fc02274b052d52d1e876a68f9d368b5341
Daniel Kolesa <q66@chimera-linux.org>
2022-01-29 04:11:10 +0100

    main/xvinfo: new package (1.1.4)

---
+++ main/xvinfo/template.py
@@ -0,0 +1,15 @@
+pkgname = "xvinfo"
+pkgver = "1.1.4"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+makedepends = ["libxv-devel"]
+pkgdesc = "X video capabilities query utility"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "0353220d6606077ba42363db65f50410759f9815352f77adc799e2adfa76e73f"
+
+def post_install(self):
+    self.install_license("COPYING")
