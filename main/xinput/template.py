6e6740220bd109e81cd3c8be5d215edfd4849be0
Daniel Kolesa <q66@chimera-linux.org>
2022-01-29 03:49:07 +0100

    main/xinput: new package (1.6.3)

---
+++ main/xinput/template.py
@@ -0,0 +1,17 @@
+pkgname = "xinput"
+pkgver = "1.6.3"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+makedepends = [
+    "libxext-devel", "libxi-devel", "libxrandr-devel", "libxinerama-devel"
+]
+pkgdesc = "X input device configuration utility"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "35a281dd3b9b22ea85e39869bb7670ba78955d5fec17c6ef7165d61e5aeb66ed"
+
+def post_install(self):
+    self.install_license("COPYING")
