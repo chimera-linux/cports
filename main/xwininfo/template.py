d37f435d3b975e4b7b16fbadb958b4becc1fbd1a
Daniel Kolesa <q66@chimera-linux.org>
2022-01-29 04:14:07 +0100

    main/xwininfo: new package (1.1.5)

---
+++ main/xwininfo/template.py
@@ -0,0 +1,15 @@
+pkgname = "xwininfo"
+pkgver = "1.1.5"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+makedepends = ["libx11-devel"]
+pkgdesc = "Query information about X windows"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "7a405441dfc476666c744f5fcd1bc8a75abf8b5b1d85db7b88b370982365080e"
+
+def post_install(self):
+    self.install_license("COPYING")
