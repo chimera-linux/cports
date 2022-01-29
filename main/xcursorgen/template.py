21025a67a873fe1789b15ffc7d95f210863b8cd6
Daniel Kolesa <q66@chimera-linux.org>
2022-01-29 03:35:23 +0100

    main/xcursorgen: new package (1.0.7)

---
+++ main/xcursorgen/template.py
@@ -0,0 +1,18 @@
+pkgname = "xcursorgen"
+pkgver = "1.0.7"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf"]
+makedepends = [
+    "libx11-devel", "libxcursor-devel", "libxrender-devel",
+    "libxfixes-devel", "libpng-devel"
+]
+pkgdesc = "X cursor generator"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://xorg.freedesktop.org"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "35b6f844b24f1776e9006c880a745728800764dbe3b327a128772b4610d8eb3d"
+
+def post_install(self):
+    self.install_license("COPYING")
