78254fd118b66bea8ed6e99c3dccd77c4717e9eb
Daniel Kolesa <q66@chimera-linux.org>
2022-02-11 04:56:17 +0100

    main/gstreamer: update to 1.20.0

--- main/gstreamer/template.py
+++ main/gstreamer/template.py
@@ -1,5 +1,5 @@
 pkgname = "gstreamer"
-pkgver = "1.18.5"
+pkgver = "1.20.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -21,9 +21,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "55862232a63459bbf56abebde3085ca9aec211b478e891dacea4d6df8cafe80a"
-# musl related failures
-options = ["!check"]
+sha256 = "edf4bffff85591d4fff7b21bb9ed7f0feabc123ac4a4eff29e73cbce454f9db7"
 
 @subpackage("gstreamer-devel")
 def _devel(self):
