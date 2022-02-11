35d0f9ce2a72105d6577bc2051f0eac38ca9f253
Daniel Kolesa <q66@chimera-linux.org>
2022-02-11 05:23:10 +0100

    main/gst-libav: new package (1.20.0)

--- main/gst-libav/template.py
+++ main/gst-libav/template.py
@@ -1,5 +1,5 @@
 pkgname = "gst-libav"
-pkgver = "1.18.5"
+pkgver = "1.20.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddefault_library=shared"]
@@ -13,4 +13,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "822e008a910e9dd13aedbdd8dc63fedef4040c0ee2e927bab3112e9de693a548"
+sha256 = "5eee5ed8d5082a31b500448e41535c722ee30cd5f8224f32982bbaba2eedef17"
