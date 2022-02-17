6bd71f41e352545bf1196b9ea77963a44a9f173a
Daniel Kolesa <q66@chimera-linux.org>
2022-02-17 04:58:50 +0100

    main/dinit-userservd: remove now obsolete dbus launcher

--- main/dinit-userservd/template.py
+++ main/dinit-userservd/template.py
@@ -12,10 +12,5 @@ url = "https://github.com/chimera-linux/dinit-userservd"
 source = f"https://github.com/chimera-linux/{pkgname}/archive/{_commit}.tar.gz"
 sha256 = "dfc9d24a15db9b0a159a6c79c78df93480708995f1b289fd503829295c9ba555"
 
-@subpackage("dinit-userservd-dbus")
-def _dbus(self):
-    self.pkgdesc = f"{pkgdesc} (dbus session runner)"
-    self.depends = ["dbus"]
-    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}", "dbus"]
-
-    return ["usr/bin/dinit-run-dbus"]
+def post_install(self):
+    (self.destdir / "usr/bin/dinit-run-dbus").unlink()
