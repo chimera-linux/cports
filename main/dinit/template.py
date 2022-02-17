fa6060ce16fe1c1ef6fdbbfb24ade4dc7cb0fd05
Daniel Kolesa <q66@chimera-linux.org>
2022-02-17 04:57:34 +0100

    main/dinit: update to git snapshot, add setenv functionality
    This allows exporting environment variables into the activation
    environment of dinit services using dinitctl. That means e.g. the
    dbus user service can export DBUS_SESSION_BUS_ADDRESS into the
    environment and every user service after it can automatically
    be aware of that environment variable.
    
    This means user services can now freely rely on session info,
    without any silly wrappers. That enables various things adjacent
    to graphical environment/session to be managed via services,
    similarly to systemd.

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -1,6 +1,8 @@
 pkgname = "dinit"
-pkgver = "0.12.0"
+pkgver = "0.12.0_git20220215"
 pkgrel = 0
+# pin to a git commit for the time being
+_commit = "49f4b5f3daea3174fcb78d4fd50354c24399f30a"
 build_style = "makefile"
 make_cmd = "gmake"
 make_check_args = ["check-igr"] # additional target
@@ -9,14 +11,9 @@ pkgdesc = "Service manager and init system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = f"https://davmac.org/projects/dinit"
-source = f"https://github.com/davmac314/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "d5f9afe7005da7c08224dddcf2b63f37a6c4120b7493bed4669ef362cde1b544"
+source = f"https://github.com/davmac314/{pkgname}/archive/{_commit}.tar.gz"
+sha256 = "49526385c1b1a7174fbdd8278915d1f5942e6052c65c5b9297e2a043914bc569"
 
 def post_patch(self):
     self.cp(self.files_path / "mconfig", self.cwd)
     (self.cwd / "mconfig").touch() # mtime
-
-def post_install(self):
-    # dinit does not install this link by itself right now
-    self.install_link("shutdown", "usr/bin/poweroff")
-    self.install_link("shutdown.8", "usr/share/man/man8/poweroff.8")
