82d61d75ef0572055d9b00a1cf75ff3356aa29fc
Daniel Kolesa <q66@chimera-linux.org>
2022-02-18 13:30:12 +0100

    main/dinit: update to latest upstream commit
    The setenv work has been merged so we don't need a local patch
    anymore.

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -1,8 +1,8 @@
 pkgname = "dinit"
-pkgver = "0.12.0_git20220215"
+pkgver = "0.12.0_git20220218"
 pkgrel = 0
 # pin to a git commit for the time being
-_commit = "49f4b5f3daea3174fcb78d4fd50354c24399f30a"
+_commit = "c76a688694e8c109b2b76638bb53e345b6154c19"
 build_style = "makefile"
 make_cmd = "gmake"
 make_check_args = ["check-igr"] # additional target
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = f"https://davmac.org/projects/dinit"
 source = f"https://github.com/davmac314/{pkgname}/archive/{_commit}.tar.gz"
-sha256 = "49526385c1b1a7174fbdd8278915d1f5942e6052c65c5b9297e2a043914bc569"
+sha256 = "f40875fd68b1fb465af3e37f6d8fdbb3eefaf78a642ac31f5cd39f8164d370e9"
 
 def post_patch(self):
     self.cp(self.files_path / "mconfig", self.cwd)
