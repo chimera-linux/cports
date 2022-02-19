13908516a732e18d1be406f09bbb780b56596415
Wesley Moore <wes@wezm.net>
2022-02-19 17:49:18 +1000

    contrib/libxdg-basedir: new package (1.2.3)
    Two tests are disabled as they fail unless the configure script is
    called with a relative path. cbuild calls it with an absolute path which
    causes the test to build an expected path with two absolute paths
    concatenated together ($wd and $td).

---
+++ contrib/libxdg-basedir/template.py
@@ -0,0 +1,18 @@
+pkgname = "libxdg-basedir"
+pkgver = "1.2.3"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["automake", "libtool", "pkgconf"]
+pkgdesc = "Implementation of the XDG Base Directory specifications"
+maintainer = "Wesley Moore <wes@wezm.net>"
+license = "MIT"
+url = "https://github.com/devnev/libxdg-basedir"
+source = f"https://github.com/devnev/{pkgname}/archive/{pkgname}-{pkgver}.tar.gz"
+sha256 = "ff30c60161f7043df4dcc6e7cdea8e064e382aa06c73dcc3d1885c7d2c77451d"
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("libxdg-basedir-devel")
+def _devel(self):
+    return self.default_devel()
