e541f05e6582aa50eac2c857a87f81dd7157e70c
Daniel Kolesa <q66@chimera-linux.org>
2022-02-25 03:30:10 +0100

    main/rust-bootstrap: update to 1.59.0

--- main/rust-bootstrap/template.py
+++ main/rust-bootstrap/template.py
@@ -1,5 +1,5 @@
 pkgname = "rust-bootstrap"
-pkgver = "1.57.0"
+pkgver = "1.59.0"
 pkgrel = 0
 # satisfy revdeps
 makedepends = ["zlib", "ncurses-libs"]
@@ -9,7 +9,7 @@ pkgdesc = "Rust programming language bootstrap toolchain"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT OR Apache-2.0"
 url = "https://rust-lang.org"
-_urlb = "https://ftp.octaforge.org/q66/random/rust-chimera"
+_urlb = "https://ftp.octaforge.org/chimera/distfiles"
 source = [
     f"{_urlb}/rustc-{pkgver}-{self.profile().triplet}.tar.xz",
     f"{_urlb}/rust-std-{pkgver}-{self.profile().triplet}.tar.xz"
@@ -19,18 +19,18 @@ options = ["!strip"]
 match self.profile().arch:
     case "aarch64":
         sha256 = [
-            "c9382d56949eb427648850b8595ac39b4542822b5b4b223a067a202fb57f6968",
-            "4e11271df34353e139f18e05573b75be4be80bd14c8315edfa271c61ea25ec42",
+            "3c2ed1312f4f68c304389f83670f83ca85aca51374953d3b1af91f642ceaa7cf",
+            "0b2359ef5971f5c5e07dc454b3e01360d90d26be066ef9990a22850494b364e2",
         ]
     case "ppc64le":
         sha256 = [
-            "f4960ca2bcc9f147b2e8f45e4e9d65ab1bd40979f3f3ff4914b52597c526546d",
-            "618c5a93f37ec8c2a5e10e57fb5d259e17aa2acef3ede9426cfebbb5eb5022a7",
+            "edb7581e9f3412a169be84aefd26817b98c25033e871dc68defdd60bdc0ed414",
+            "92a1e3eb69204bda7a41ec74a6a56264607af098b64793d9a31cba0c159cd751",
         ]
     case "x86_64":
         sha256 = [
-            "0add5ffc9271925ed470aa3de6c7fea39dbf4e7d2f6be55b410c69297bd0ec09",
-            "0177e357ee80541c8abd737fdf0af15c7df7d06f8758de8f6fb0af3a884a4a8b",
+            "93730fe852e681d058e6169ec7c74ba1a5d58edbed092cc61c0752fbf7771f1f",
+            "525b12f2603918d21018a7606e22e95f55380c3001c572c3718016d03e93f635",
         ]
     case _:
         broken = f"not yet built for {self.profile().arch}"
