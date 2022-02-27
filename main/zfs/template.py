9bdf80861540552ef999ebcdd5c55a5209a63586
Daniel Kolesa <q66@chimera-linux.org>
2022-02-27 05:35:00 +0100

    main/zfs: new package (2.1.2)

---
+++ main/zfs/template.py
@@ -0,0 +1,80 @@
+# don't forget to update files/ckms.ini when bumping
+pkgname = "zfs"
+pkgver = "2.1.2"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--with-config=user", "--with-mounthelperdir=/usr/bin",
+    "--with-udevdir=/usr/lib/udev", "--with-udevruledir=/usr/lib/udev/rules.d",
+    "--with-dracutdir=/usr/lib/dracut", "--with-tirpc",
+]
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "pkgconf", "automake", "libtool", "python"]
+makedepends = [
+    "libuuid-devel", "libblkid-devel", "linux-pam-devel", "libtirpc-devel",
+    "attr-devel", "openssl-devel", "eudev-devel", "zlib-devel",
+    "linux-headers",
+]
+pkgdesc = "OpenZFS for Linux"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "CDDL-1.0"
+url = "https://openzfs.github.io/openzfs-docs"
+source = f"https://github.com/openzfs/{pkgname}/releases/download/{pkgname}-{pkgver}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "258cf1d17a1f668a3b99b61eaf14be06c614df42503db0319bef1b9fc4c8b9e7"
+
+def post_patch(self):
+    # clean up for ckms
+    for f in self.patches_path.iterdir():
+        self.rm(f.name)
+
+def pre_configure(self):
+    self.do("autoreconf", "-if")
+    # compress source tree for ckms
+    fn = f"{pkgname}-{pkgver}.tar"
+    self.do("tar", "cf", fn, "--exclude", fn, ".")
+
+def post_install(self):
+    self.install_license("COPYRIGHT")
+    self.install_license("LICENSE")
+    self.install_license("NOTICE")
+
+    self.rm(self.destdir / "usr/share/zfs/zfs-tests", recursive = True)
+    self.rm(self.destdir / "etc/init.d", recursive = True)
+    self.rm(self.destdir / "etc/default", recursive = True)
+    self.rm(self.destdir / "etc/zfs/zfs-functions")
+    self.rm(self.destdir / "usr/share/pam-configs/zfs_key")
+    self.rm(self.destdir / "usr/share/man/man8/zfs-mount-generator.8")
+
+    # install ckms source tree
+    srcp = f"usr/src/{pkgname}-{pkgver}"
+    self.install_dir(srcp)
+    self.do(
+        "tar", "xf",
+        self.chroot_builddir / self.wrksrc / f"{pkgname}-{pkgver}.tar",
+        wrksrc = self.chroot_destdir / srcp
+    )
+    self.install_file(self.files_path / "ckms.ini", srcp)
+
+    self.install_service(self.files_path / "zed")
+
+@subpackage("zfs-dracut")
+def _dracut(self):
+    self.pkgdesc = f"{pkgdesc} (dracut module)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}", "dracut"]
+    self.depends = [f"{pkgname}={pkgver}-r{pkgrel}"]
+
+    return ["usr/lib/dracut"]
+
+@subpackage("zfs-devel")
+def _devel(self):
+    return self.default_devel()
+
+@subpackage("zfs-ckms")
+def _ckms(self):
+    self.pkgdesc = f"{pkgdesc} (kernel sources)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}", "ckms"]
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}", "ckms", "perl", "python", "gmake"
+    ]
+
+    return ["usr/src"]
