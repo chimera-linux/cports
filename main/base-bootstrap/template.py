750de67d15bbbd0419148d40c1b9618b618638c3
yopito <pierre.bourgin@free.fr>
2022-03-12 20:24:12 +0100

    main/base-bootstrap: is a bootstrap package

--- main/base-bootstrap/template.py
+++ main/base-bootstrap/template.py
@@ -7,3 +7,5 @@ pkgdesc = "Scriptless base metapackage for bootstrapping systems"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:meta"
 url = "https://chimera-linux.org"
+
+options = ["bootstrap"]
