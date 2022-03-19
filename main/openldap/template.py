b4bc07ddbc6ba102f0d873d6a3a8c4c4b8a1e4be
Daniel Kolesa <q66@chimera-linux.org>
2022-03-19 05:42:50 +0100

    main/openldap: new package (2.6.1)

---
+++ main/openldap/template.py
@@ -0,0 +1,66 @@
+# TODO: slapd service
+pkgname = "openldap"
+pkgver = "2.6.1"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-slapd", "--enable-syslog", "--enable-crypt",
+    "--enable-dynamic", "--enable-spasswd", "--enable-rlookups",
+    "--enable-modules", "--enable-backends=mod", "--enable-overlays=mod",
+    "--disable-sql", "--disable-wt", "--disable-static", "--disable-debug",
+    "--disable-versioning", "--with-tls=openssl", "--with-cyrus-sasl",
+]
+make_cmd = "gmake"
+hostmakedepends = [
+    "gmake", "pkgconf", "automake", "libtool", "groff",
+]
+makedepends = [
+    "openssl-devel", "libsasl-devel", "libltdl-devel", "libevent-devel",
+    "libsodium-devel",
+]
+depends = [f"openldap-progs={pkgver}-r{pkgrel}"]
+pkgdesc = "FOSS implementation of the Lightweight Directory Access Protocol"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "OLDAP-2.0"
+url = "https://www.openldap.org"
+source = f"{url}/software/download/OpenLDAP/openldap-release/{pkgname}-{pkgver}.tgz"
+sha256 = "9d576ea6962d7db8a2e2808574e8c257c15aef55f403a1fb5a0faf35de70e6f3"
+file_modes = {
+    "etc/openldap/slapd.conf": ("root", "ldap", 0o640),
+    "etc/openldap/slapd.ldif": ("root", "ldap", 0o640),
+    "etc/openldap/slapd.d": ("ldap", "ldap", 0o700),
+    "var/lib/openldap": ("ldap", "ldap", 0o700),
+}
+# test suite takes ages
+options = ["!cross", "!check"]
+
+system_users = [
+    {
+        "name": "ldap",
+        "id": 83,
+        "home": "/var/lib/openldap",
+    }
+]
+
+def pre_configure(self):
+    self.do("autoreconf", "-if")
+
+def post_install(self):
+    self.install_dir("etc/openldap/slapd.d", mode = 0o700, empty = True)
+    self.install_dir("var/lib/openldap", mode = 0o700, empty = True)
+    self.install_license("LICENSE")
+
+@subpackage("openldap-libs")
+def _lib(self):
+    return self.default_libs()
+
+@subpackage("openldap-devel")
+def _devel(self):
+    return self.default_devel()
+
+@subpackage("openldap-progs")
+def _progs(self):
+    return [
+        "usr/bin/ldap*",
+        "usr/share/man/man1/ldap*.1",
+    ]
