5901701ac4fcf1d16c5aa12a6069860173a512b6
Daniel Kolesa <q66@chimera-linux.org>
2022-03-24 05:39:38 +0100

    main/meson: install completions

--- main/meson/template.py
+++ main/meson/template.py
@@ -10,7 +10,18 @@ license = "Apache-2.0"
 url = "https://mesonbuild.com"
 source = f"https://github.com/mesonbuild/{pkgname}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "9c884434469471f3fe0cbbceb9b9ea0c8047f19e792940e1df6595741aae251b"
-# checkdepends not available yet
+# meson is early in our bootstrap path but has a million checkdepends
 options = ["!check"]
 
-# FIXME: tests, install completions etc.
+def post_install(self):
+    # vim syntax
+    self.install_dir("usr/share/vim/vimfiles")
+    for f in ["ftdetect", "ftplugin", "indent", "syntax"]:
+        self.install_files(
+            f"data/syntax-highlighting/vim/{f}", "usr/share/vim/vimfiles"
+        )
+    with self.pushd("data/shell-completions"):
+        # bashcomp
+        self.install_file("bash/meson", "usr/share/bash-completion/completions")
+        # zshcomp
+        self.install_file("zsh/_meson", "usr/share/zsh/site-functions")
