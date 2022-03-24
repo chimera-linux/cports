367ce67a35803fb9064b09168b9cf02574158bbd
Daniel Kolesa <q66@chimera-linux.org>
2022-03-24 04:25:24 +0100

    main/gtkmm: enable tests

--- main/gtkmm/template.py
+++ main/gtkmm/template.py
@@ -2,19 +2,19 @@ pkgname = "gtkmm"
 pkgver = "4.4.0"
 pkgrel = 0
 build_style = "meson"
+make_check_wrapper = ["xvfb-run"]
 hostmakedepends = ["meson", "pkgconf", "glib-devel"]
 makedepends = [
     "gtk4-devel", "cairomm-devel", "pangomm-devel",
     "gdk-pixbuf-devel", "libepoxy-devel"
 ]
+checkdepends = ["xserver-xorg-xvfb"]
 pkgdesc = "C++ bindings for Gtk4"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gtkmm.org"
 source = f"$(GNOME_SITE)/gtkmm/{pkgver[:-2]}/gtkmm-{pkgver}.tar.xz"
 sha256 = "2eb464326096e6a40c82e9cd074164d8103fb5e07865679c0a649e4174700dda"
-# XXX: xvfb-run
-options = ["!check"]
 
 @subpackage("gtkmm-devel")
 def _devel(self):
