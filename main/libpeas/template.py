5b1f82e75f23453371b45fb649505fc7984d3bc7
Daniel Kolesa <q66@chimera-linux.org>
2022-03-24 04:27:35 +0100

    main/libpeas: enable tests

--- main/libpeas/template.py
+++ main/libpeas/template.py
@@ -3,6 +3,7 @@ pkgver = "1.30.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddemos=false", "-Dvapi=true"]
+make_check_wrapper = ["xvfb-run"]
 hostmakedepends = [
     "meson", "pkgconf", "glib-devel", "gettext-tiny", "vala",
     "gobject-introspection", "python"
@@ -12,14 +13,13 @@ makedepends = [
     "glade3-devel"
 ]
 depends = ["python-gobject", "hicolor-icon-theme"]
+checkdepends = ["xserver-xorg-xvfb"]
 pkgdesc = "G0bject application plugin library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Projects/Libpeas"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "0bf5562e9bfc0382a9dcb81f64340787542568762a3a367d9d90f6185898b9a3"
-# no xvfb-run
-options = ["!check"]
 
 @subpackage("libpeas-devel")
 def _devel(self):
