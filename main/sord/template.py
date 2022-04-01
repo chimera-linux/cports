55c4851cbd141f246f8e5794177f9ddfc407aa8a
Daniel Kolesa <q66@chimera-linux.org>
2022-04-01 03:49:04 +0200

    main/sord: new package (0.16.8)

---
+++ main/sord/template.py
@@ -0,0 +1,25 @@
+pkgname = "sord"
+pkgver = "0.16.8"
+pkgrel = 0
+build_style = "waf"
+hostmakedepends = ["python", "pkgconf"]
+makedepends = ["serd-devel", "pcre-devel"]
+pkgdesc = "C library for storing RDF data in memory"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "ISC"
+url = "https://drobilla.net/software/sord.html"
+source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "7c289d2eaabf82fa6ac219107ce632d704672dcfb966e1a7ff0bbc4ce93f5e14"
+# FIXME check
+options = ["!cross", "!check"]
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("sord-devel")
+def _devel(self):
+    return self.default_devel()
+
+@subpackage("sord-progs")
+def _progs(self):
+    return self.default_progs()
