137b4f2bcbd0e67508c8124db371b68af1376f7e
Daniel Kolesa <q66@chimera-linux.org>
2022-04-02 20:17:19 +0200

    main/flite: new package (2.2)

---
+++ main/flite/template.py
@@ -0,0 +1,26 @@
+pkgname = "flite"
+pkgver = "2.2"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--enable-shared", "--with-audio=pulseaudio"]
+make_cmd = "gmake"
+make_dir = "."
+make_check_target = ""
+make_check_args = ["-C", "testsuite"]
+hostmakedepends = ["gmake"]
+makedepends = ["libpulse-devel"]
+pkgdesc = "Lightweight speech synthesis engine"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-4-Clause"
+url = "http://www.festvox.org/flite"
+source = f"https://github.com/festvox/{pkgname}/archive/v{pkgver}.tar.gz"
+sha256 = "ab1555fe5adc3f99f1d4a1a0eb1596d329fd6d74f1464a0097c81f53c0cf9e5c"
+# testsuite needs alsa lib
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("COPYING")
+
+@subpackage("flite-devel")
+def _devel(self):
+    return self.default_devel()
