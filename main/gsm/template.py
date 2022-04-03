58598c0e5462be1afaabd178e9e2de297782d2c0
Daniel Kolesa <q66@chimera-linux.org>
2022-04-03 05:31:48 +0200

    main/gsm: new package (1.0.19)

---
+++ main/gsm/template.py
@@ -0,0 +1,59 @@
+pkgname = "gsm"
+pkgver = "1.0.19"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_check_target = "tst"
+make_use_env = True
+hostmakedepends = ["pkgconf", "gmake"]
+pkgdesc = "GSM 06.10 lossy speech compression"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "TU-Berlin-2.0"
+url = "http://www.quut.com/gsm"
+source = f"{url}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "4903652f68a8c04d0041f0d19b1eb713ddcd2aa011c5e595b3b8bca2755270f6"
+# racey mess of a build system
+options = ["!parallel", "!lto"]
+
+def init_configure(self):
+    self._margs = [
+        "CC=" + self.get_tool("CC"),
+        "LD=" + self.get_tool("CC"),
+        "AR=" + self.get_tool("AR"),
+        "CCFLAGS=" + self.get_cflags(shell = True) + \
+            " -c -DNeedFunctionPrototypes=1"
+    ]
+
+def do_build(self):
+    self.make.build(self._margs)
+
+def do_check(self):
+    self.make.check(self._margs)
+
+def do_install(self):
+    self.install_dir("usr/bin")
+    self.install_dir("usr/lib")
+    self.install_dir("usr/include/gsm")
+    self.install_dir("usr/share/man/man3")
+    self.install_dir("usr/share/man/man1")
+
+    self.make.install([
+        "INSTALL_ROOT=" + str(self.chroot_destdir / "usr"),
+        "GSM_INSTALL_INC=" + str(self.chroot_destdir / "usr/include/gsm"),
+        "GSM_INSTALL_MAN=" + str(self.chroot_destdir / "usr/share/man/man3"),
+        "TOAST_INSTALL_MAN=" + str(self.chroot_destdir / "usr/share/man/man1"),
+    ])
+
+    self.install_file(
+        "lib/libgsm.so", "usr/lib", name = f"libgsm.so.{pkgver}", mode = 0o755
+    )
+    self.install_link(f"libgsm.so.{pkgver}", "usr/lib/libgsm.so.1")
+    self.install_link(f"libgsm.so.{pkgver}", "usr/lib/libgsm.so")
+
+    self.install_link("gsm/gsm.h", "usr/include/gsm.h")
+
+    self.install_license("COPYRIGHT")
+
+@subpackage("gsm-devel")
+def _devel(self):
+    return self.default_devel()
