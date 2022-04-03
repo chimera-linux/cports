350e70b4f8c6e45c067bb24852c10b73b13cca4a
Daniel Kolesa <q66@chimera-linux.org>
2022-04-03 04:38:19 +0200

    main/libsrtp: new package (2.4.2)

---
+++ main/libsrtp/template.py
@@ -0,0 +1,20 @@
+pkgname = "libsrtp"
+pkgver = "2.4.2"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dcrypto-library=openssl"]
+hostmakedepends = ["meson", "pkgconf"]
+makedepends = ["openssl-devel"]
+pkgdesc = f"Library for Secure Real-Time Transport Protocol"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-3-Clause"
+url = "https://github.com/cisco/libsrtp"
+source = f"{url}/archive/v{pkgver}.tar.gz"
+sha256 = "3b1bcb14ebda572b04b9bdf07574a449c84cb924905414e4d94e62837d22b628"
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+@subpackage("libsrtp-devel")
+def _devel(self):
+    return self.default_devel()
