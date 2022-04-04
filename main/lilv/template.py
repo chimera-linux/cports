cf85b4fea4b0ed862ae150085b9d8a040448902c
Daniel Kolesa <q66@chimera-linux.org>
2022-04-04 03:00:57 +0200

    main/lilv: enable check

--- main/lilv/template.py
+++ main/lilv/template.py
@@ -2,7 +2,7 @@ pkgname = "lilv"
 pkgver = "0.24.12"
 pkgrel = 0
 build_style = "waf"
-configure_args = ["--dyn-manifest"]
+configure_args = ["--test", "--dyn-manifest"]
 hostmakedepends = ["python", "pkgconf"]
 makedepends = [
     "libsndfile-devel", "python-devel", "serd-devel", "sord-devel",
@@ -14,8 +14,7 @@ license = "ISC"
 url = "https://drobilla.net/software/lilv.html"
 source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "26a37790890c9c1f838203b47f5b2320334fe92c02a4d26ebbe2669dbd769061"
-# FIXME check
-options = ["!cross", "!check"]
+options = ["!cross"]
 
 def post_install(self):
     self.install_license("COPYING")
