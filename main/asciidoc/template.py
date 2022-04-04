ee04b393893fd9739ba5f01429bd7399c9af207c
Daniel Kolesa <q66@chimera-linux.org>
2022-04-04 02:37:17 +0200

    main/asciidoc: update to 10.1.4

--- main/asciidoc/template.py
+++ main/asciidoc/template.py
@@ -1,5 +1,5 @@
 pkgname = "asciidoc"
-pkgver = "10.0.2"
+pkgver = "10.1.4"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools", "docbook-xsl-nons", "xsltproc"]
@@ -9,8 +9,8 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://asciidoc.org"
 source = f"$(PYPI_SITE)/a/asciidoc/asciidoc-{pkgver}.tar.gz"
-sha256 = "1800699c579038bcf68e760e9358304b69a19ef04c9bf0b4faa76b729dcf7dbf"
-# needs source-highlight, FIXME
+sha256 = "e614fe7cd6ff096d2aea06733ecfb6b15411b2a0f4aef8267964a32c7f7472ac"
+# apparently only supports tox now and that's useless
 options = ["!check"]
 
 def post_install(self):
