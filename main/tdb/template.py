4b1f75e389f8b2de14516103dcde0ff0fb5c9c25
Daniel Kolesa <q66@chimera-linux.org>
2022-04-04 02:45:30 +0200

    main/tdb: enable check

--- main/tdb/template.py
+++ main/tdb/template.py
@@ -19,8 +19,7 @@ license = "LGPL-3.0-or-later"
 url = "https://tdb.samba.org"
 source = f"https://download.samba.org/pub/{pkgname}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "d6892bd8befe04a77642a1dd56e4a879349bf1cf5b2c0bf5fb841061938def0b"
-# FIXME check
-options = ["!cross", "!check"]
+options = ["!cross"]
 
 @subpackage("tdb-devel")
 def _devel(self):
