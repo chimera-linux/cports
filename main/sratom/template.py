44192301c6f5bdae655d52b72d2146f96b2522f6
Daniel Kolesa <q66@chimera-linux.org>
2022-04-04 03:02:55 +0200

    main/sratom: enable check

--- main/sratom/template.py
+++ main/sratom/template.py
@@ -2,6 +2,7 @@ pkgname = "sratom"
 pkgver = "0.6.8"
 pkgrel = 0
 build_style = "waf"
+configure_args = ["--test"]
 hostmakedepends = ["python", "pkgconf"]
 makedepends = ["serd-devel", "sord-devel", "lv2"]
 pkgdesc = "Library for serializing LV2 atoms to/from RDF"
@@ -10,8 +11,7 @@ license = "ISC"
 url = "https://drobilla.net/software/sratom.html"
 source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "3acb32b1adc5a2b7facdade2e0818bcd6c71f23f84a1ebc17815bb7a0d2d02df"
-# FIXME check
-options = ["!cross", "!check"]
+options = ["!cross"]
 
 def post_install(self):
     self.install_license("COPYING")
