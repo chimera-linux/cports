efafa4e6a865be23510e760bd855aa0d1bf0400b
Daniel Kolesa <q66@chimera-linux.org>
2022-04-04 02:58:55 +0200

    main/sord: enable check

--- main/sord/template.py
+++ main/sord/template.py
@@ -2,6 +2,7 @@ pkgname = "sord"
 pkgver = "0.16.8"
 pkgrel = 0
 build_style = "waf"
+configure_args = ["--test"]
 hostmakedepends = ["python", "pkgconf"]
 makedepends = ["serd-devel", "pcre-devel"]
 pkgdesc = "C library for storing RDF data in memory"
@@ -10,8 +11,7 @@ license = "ISC"
 url = "https://drobilla.net/software/sord.html"
 source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "7c289d2eaabf82fa6ac219107ce632d704672dcfb966e1a7ff0bbc4ce93f5e14"
-# FIXME check
-options = ["!cross", "!check"]
+options = ["!cross"]
 
 def post_install(self):
     self.install_license("COPYING")
