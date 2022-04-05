fc2269490998fcfbd1283786a9955f7bde8e35ce
Daniel Kolesa <q66@chimera-linux.org>
2022-04-05 03:32:25 +0200

    main/at-spi2-core: update to 2.44.0

--- main/at-spi2-core/template.py
+++ main/at-spi2-core/template.py
@@ -1,5 +1,5 @@
 pkgname = "at-spi2-core"
-pkgver = "2.42.0"
+pkgver = "2.44.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -15,9 +15,9 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/at-spi2-core"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "4b5da10e94fa3c6195f95222438f63a0234b99ef9df772c7640e82baeaa6e386"
+sha256 = "7eee3cf285b089060fd6b6e51b3eb2cacf752cca3a082c7f4c2c5ab841e51353"
 # non-trivial dbus setup
-options = ["!check"]
+options = ["!check", "!cross"]
 
 def post_install(self):
     self.rm(self.destdir / "usr/lib/systemd", recursive = True)
