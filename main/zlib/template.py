ee4fcc4d964d010ac7421af013d410700528c2be
Daniel Kolesa <q66@chimera-linux.org>
2022-04-05 04:22:12 +0200

    main/zlib: update to 1.2.12

--- main/zlib/template.py
+++ main/zlib/template.py
@@ -1,5 +1,5 @@
 pkgname = "zlib"
-pkgver = "1.2.11"
+pkgver = "1.2.12"
 pkgrel = 0
 build_style = "configure"
 hostmakedepends = ["pkgconf"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Zlib"
 url = "http://www.zlib.net"
 source = f"{url}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
+sha256 = "91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
 tool_flags = {"CFLAGS": ["-fPIC"]}
 options = ["bootstrap"]
 
