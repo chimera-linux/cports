56573c9f3275a1a7fa7b668756356154a134b80a
Daniel Kolesa <q66@chimera-linux.org>
2022-04-05 23:01:30 +0200

    main/xserver-xorg-input-libinput: update to 1.2.1

--- main/xserver-xorg-input-libinput/template.py
+++ main/xserver-xorg-input-libinput/template.py
@@ -1,5 +1,5 @@
 pkgname = "xserver-xorg-input-libinput"
-pkgver = "1.2.0"
+pkgver = "1.2.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -9,8 +9,8 @@ pkgdesc = "Generic input driver for X.org server based on libinput"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/driver/xf86-input-libinput-{pkgver}.tar.bz2"
-sha256 = "f80da3c514fe1cbf57fa1b1bd6ff97f6b0a1f87466ad89247bac59cd0a5869f6"
+source = f"$(XORG_SITE)/driver/xf86-input-libinput-{pkgver}.tar.gz"
+sha256 = "ac37b3fd4685025d8a1864ee361b3d6c4e50689c9a5b86786ea1fa3eb997f3e5"
 
 def post_install(self):
     self.install_license("COPYING")
