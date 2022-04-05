9a39b554aaf35d6b3400cf9171b9def6e2794da7
Daniel Kolesa <q66@chimera-linux.org>
2022-04-05 03:27:50 +0200

    main/accountsservice: update to 22.08.8

--- main/accountsservice/template.py
+++ main/accountsservice/template.py
@@ -1,12 +1,13 @@
 pkgname = "accountsservice"
-pkgver = "0.6.55"
+pkgver = "22.08.8"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dsystemdsystemunitdir=no", "-Dintrospection=true", "-Delogind=true",
 ]
 hostmakedepends = [
-    "meson", "pkgconf", "gobject-introspection", "glib-devel", "polkit"
+    "meson", "pkgconf", "gobject-introspection", "glib-devel", "polkit",
+    "gettext-tiny", "vala",
 ]
 makedepends = ["polkit-devel", "elogind-devel", "dbus-devel"]
 pkgdesc = "D-Bus service for accessing user accounts"
@@ -14,7 +15,8 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.freedesktop.org/wiki/Software/AccountsService"
 source = f"$(FREEDESKTOP_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "ff2b2419a7e06bd9cb335ffe391c7409b49a0f0130b890bd54692a3986699c9b"
+sha256 = "909997a76919fe7dc138a9a01cea70bd622d5a932dbc9fb13010113023a7a391"
+options = ["!cross"]
 
 def post_install(self):
     self.install_dir("var/lib/AccountsService/users", empty = True)
