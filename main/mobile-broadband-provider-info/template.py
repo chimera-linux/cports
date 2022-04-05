2834ba76729a69b3644914fa3d26c8f5f7d9191f
Daniel Kolesa <q66@chimera-linux.org>
2022-04-06 00:34:32 +0200

    main/mobile-broadband-provider-info: update to 20220315

--- main/mobile-broadband-provider-info/template.py
+++ main/mobile-broadband-provider-info/template.py
@@ -1,5 +1,5 @@
 pkgname = "mobile-broadband-provider-info"
-pkgver = "20210805"
+pkgver = "20220315"
 pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:none"
 url = "https://gitlab.gnome.org/GNOME/mobile-broadband-provider-info"
 source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "3daf09b538a0ef0c47c65603c41862e9a6179853e495092c02bfce14c8fe287f"
+sha256 = "0776c8396f46487c31569d0e68a0d839f42ef1882db5bb0461a59880812ba9cf"
 
 def pre_configure(self):
     self.do("autoreconf", "-if")
