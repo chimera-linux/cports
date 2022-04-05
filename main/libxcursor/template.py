e8e10e5e8c3678c32e0eab013baa112f44619722
Daniel Kolesa <q66@chimera-linux.org>
2022-04-05 04:39:25 +0200

    main/libxcusor: update to 1.2.1

--- main/libxcursor/template.py
+++ main/libxcursor/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxcursor"
-pkgver = "1.2.0"
+pkgver = "1.2.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "Client-side cursor loading library for X"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libXcursor-{pkgver}.tar.bz2"
-sha256 = "3ad3e9f8251094af6fe8cb4afcf63e28df504d46bfa5a5529db74a505d628782"
+source = f"$(XORG_SITE)/lib/libXcursor-{pkgver}.tar.gz"
+sha256 = "77f96b9ad0a3c422cfa826afabaf1e02b9bfbfc8908c5fa1a45094faad074b98"
 
 def post_install(self):
     self.install_license("COPYING")
