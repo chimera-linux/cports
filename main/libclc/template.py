029cfc434f2a23053d15e9749f8591fbf5da490c
Daniel Kolesa <q66@chimera-linux.org>
2022-04-07 06:16:11 +0200

    main/libclc: update to 14.0.0

--- main/libclc/template.py
+++ main/libclc/template.py
@@ -1,6 +1,7 @@
 pkgname = "libclc"
-pkgver = "13.0.0"
+pkgver = "14.0.0"
 pkgrel = 0
+build_wrksrc = f"{pkgname}-{pkgver}.src"
 build_style = "cmake"
 configure_args = ["-DCMAKE_BUILD_TYPE=Release"]
 hostmakedepends = [
@@ -12,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://libclc.llvm.org"
 source = f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{pkgver}/{pkgname}-{pkgver}.src.tar.xz"
-sha256 = "7dc0131f05842a8ed29997362de260c82e188aa0639e482683ece8b47cca21b2"
+sha256 = "39fe8fbc112d335b483023c7e9dec2eac7e0be7062c5b088774de935d7c7cdb8"
 # external-calls-clspv broken
 options = ["!check"]
 
