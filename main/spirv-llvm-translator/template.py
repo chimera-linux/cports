b9fe078ff44cbc19fe31f59a38f22b5729913c86
Daniel Kolesa <q66@chimera-linux.org>
2022-04-07 06:14:26 +0200

    main/spirv-llvm-translator: update to 14.0.0_pre0

--- main/spirv-llvm-translator/template.py
+++ main/spirv-llvm-translator/template.py
@@ -1,6 +1,7 @@
 pkgname = "spirv-llvm-translator"
-pkgver = "13.0.0"
+pkgver = "14.0.0_pre0"
 pkgrel = 0
+_commit = "e95eb30ace4954a3a7e8e17a3cc22f7382d4a47e"
 build_style = "cmake"
 configure_args = [
     "-Wno-dev",
@@ -19,8 +20,8 @@ pkgdesc = "API and commands for processing SPIR-V modules"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "NCSA"
 url = "https://github.com/KhronosGroup/SPIRV-LLVM-Translator"
-source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "b416c06525c8724be628327565956c418755fbb471b4fe23d040ca56e1a79061"
+source = f"{url}/archive/{_commit}.tar.gz"
+sha256 = "00ef6119686965c224317aa26c2c4efa8d72907399e852d69b009cfb993fb861"
 # tests disabled
 options = ["!check"]
 
