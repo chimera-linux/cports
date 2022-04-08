65bd7ccc92274f1ac9a6cdafb54b19708ed020d7
Daniel Kolesa <q66@chimera-linux.org>
2022-04-08 23:13:39 +0200

    main/libplacebo: update to 4.192.1

--- main/libplacebo/template.py
+++ main/libplacebo/template.py
@@ -1,5 +1,5 @@
 pkgname = "libplacebo"
-pkgver = "4.157.0"
+pkgver = "4.192.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -18,7 +18,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://code.videolan.org/videolan/libplacebo"
 source = f"{url}/-/archive/v{pkgver}/{pkgname}-v{pkgver}.tar.gz"
-sha256 = "8ee7773fb7813520b6b1e5f8f207cdf0071a7cf48ca0ce871e956ae6f4d0d538"
+sha256 = "e6c5805cf4d955b5941dd12b00fe157b61e77995bc1786b9a86df0ca99a0edc4"
 
 @subpackage("libplacebo-devel")
 def _devel(self):
