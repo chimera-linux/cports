d4c939f44e8e385b8e18a3518b617c0cd1133148
Daniel Kolesa <q66@chimera-linux.org>
2022-04-08 22:32:10 +0200

    main/rubberband: update to 2.0.2

--- main/rubberband/template.py
+++ main/rubberband/template.py
@@ -1,5 +1,5 @@
 pkgname = "rubberband"
-pkgver = "2.0.0"
+pkgver = "2.0.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://breakfastquay.com/rubberband"
 source = f"https://breakfastquay.com/files/releases/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "eccbf0545496ce3386a2433ceec31e6576a76ed6884310e4b465003bfe260286"
+sha256 = "b9eac027e797789ae99611c9eaeaf1c3a44cc804f9c8a0441a0d1d26f3d6bdf9"
 
 @subpackage("rubberband-progs")
 def _progs(self):
