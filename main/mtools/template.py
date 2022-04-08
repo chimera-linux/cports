03b5274ae5db6a940c35b04b38230352945d752e
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 01:16:55 +0200

    main/mtools: update to 4.0.38

--- main/mtools/template.py
+++ main/mtools/template.py
@@ -1,5 +1,5 @@
 pkgname = "mtools"
-pkgver = "4.0.37"
+pkgver = "4.0.38"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["texinfo"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/mtools"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "799b197e23e47b61259628810b27790efb7a1fe36037ef1da8a27b0ae4fa8342"
+sha256 = "7b94485f486e7df08cca68b00a164a13cd38f4c63cb8684d188759ee7bc5e729"
 
 def post_install(self):
     self.install_file("mtools.conf", "etc")
