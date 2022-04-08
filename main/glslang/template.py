f9ae75de145295125f488276a790df1e71248b6e
Daniel Kolesa <q66@chimera-linux.org>
2022-04-08 23:08:57 +0200

    main/glslang: update to 11.9.0

--- main/glslang/template.py
+++ main/glslang/template.py
@@ -1,6 +1,6 @@
 # note: some libs are unversioned (rebuild shaderc on updates)
 pkgname = "glslang"
-pkgver = "11.7.0"
+pkgver = "11.9.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/KhronosGroup/glslang"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "b6c83864c3606678d11675114fa5f358c519fe1dad9a781802bcc87fb8fa32d5"
+sha256 = "d5744adba19eef9ad3d73f524226b39fec559d94cb582cd442e3c5de930004b2"
 # missing checkdepends
 options = ["!check"]
 
