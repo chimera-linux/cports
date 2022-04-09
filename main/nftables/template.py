cd04e5c469f686660f52d6c816ee3ac5d1ef6ed2
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 06:03:46 +0200

    main/nftables: update to 1.0.2

--- main/nftables/template.py
+++ main/nftables/template.py
@@ -1,5 +1,5 @@
 pkgname = "nftables"
-pkgver = "1.0.1"
+pkgver = "1.0.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "http://netfilter.org/projects/nftables"
 source = f"{url}/files/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "3ceeba625818e81a0be293e9dd486c3ef799ebd92165270f1e57e9a201efa423"
+sha256 = "0b28a36ffcf4567b841de7bd3f37918b1fed27859eb48bdec51e1f7a83954c02"
 
 def post_install(self):
     self.install_service(self.files_path / "nftables")
