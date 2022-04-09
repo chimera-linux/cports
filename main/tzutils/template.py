84b79e7e711713074e4c3659f5679e2b9eb395e0
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 05:52:10 +0200

    main/tzutils: update to 2022a

--- main/tzutils/template.py
+++ main/tzutils/template.py
@@ -1,16 +1,16 @@
 pkgname = "tzutils"
-pkgver = "2021e"
+pkgver = "2022a"
 pkgrel = 0
 build_style = "makefile"
 make_build_args = ["TZDIR=/usr/share/zoneinfo", "KSHELL=/bin/sh"]
-checkdepends = ["groff", "perl"]
+checkdepends = ["groff", "perl", "curl"]
 pkgdesc = "Time zone and daylight-saving time utilities"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:none AND BSD-3-Clause"
 url = "http://www.iana.org/time-zones"
 source = f"https://github.com/eggert/tz/archive/{pkgver}.tar.gz"
-sha256 = "11908a7f18530ca3013c8458d902a54cdd3382276bdd56891db074b1af4a26b8"
-# missing checkdepends
+sha256 = "e9d82a851a15bb5db2cbaae2c3fc633743ad9edc069e3738c5e8908978064ed8"
+# needs network access
 options = ["!check"]
 
 def do_install(self):
