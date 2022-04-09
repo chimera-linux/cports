bcf8b1518bc307aeed437fe8067b93e6cadc74f4
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 22:05:08 +0200

    main/duktape: update to 2.7.0

--- main/duktape/template.py
+++ main/duktape/template.py
@@ -1,28 +1,22 @@
 pkgname = "duktape"
-pkgver = "2.6.0"
+pkgver = "2.7.0"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
 make_build_args = ["-f", "Makefile.sharedlibrary"]
-make_install_args = ["-f", "Makefile.sharedlibrary"]
+make_install_args = ["-f", "Makefile.sharedlibrary", "INSTALL_PREFIX=/usr"]
 hostmakedepends = ["gmake", "pkgconf"]
 pkgdesc = "Embeddeable JavaScript engine"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://duktape.org"
 source = f"https://github.com/svaarala/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "96f4a05a6c84590e53b18c59bb776aaba80a205afbbd92b82be609ba7fe75fa7"
+sha256 = "90f8d2fa8b5567c6899830ddef2c03f3c27960b11aca222fa17aa7ac613c2890"
 # no check target
 options = ["!check"]
 
-def init_configure(self):
-    self.make_install_args += [
-        f"INSTALL_PREFIX={self.chroot_destdir / 'usr'}"
-    ]
-
 def post_install(self):
     self.install_license("LICENSE.txt")
-    self.install_file(self.files_path / "duktape.pc", "usr/lib/pkgconfig")
 
 @subpackage("duktape-devel")
 def _devel(self):
