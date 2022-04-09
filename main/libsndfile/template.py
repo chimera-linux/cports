7decd1939f1a72511a25f6204f3b2ed39e346f3c
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 20:31:30 +0200

    main/libsndfile: update to 1.1.0

--- main/libsndfile/template.py
+++ main/libsndfile/template.py
@@ -1,18 +1,18 @@
 pkgname = "libsndfile"
-pkgver = "1.0.31"
+pkgver = "1.1.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "python"]
 makedepends = [
     "libvorbis-devel", "flac-devel", "opus-devel", "sqlite-devel",
-    "linux-headers",
+    "mpg123-devel", "lame-devel", "linux-headers",
 ]
 pkgdesc = "C library for reading and writing files containing sampled sound"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://libsndfile.github.io/libsndfile"
-source = f"https://github.com/{pkgname}/{pkgname}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "a8cfb1c09ea6e90eff4ca87322d4168cdbe5035cb48717b40bf77e751cc02163"
+source = f"https://github.com/{pkgname}/{pkgname}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "0f98e101c0f7c850a71225fb5feaf33b106227b3d331333ddc9bacee190bcf41"
 
 @subpackage("libsndfile-progs")
 def _progs(self):
