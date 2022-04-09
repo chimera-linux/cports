4073700e5431491b3d694258e27515a3be7a1e0b
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 06:18:58 +0200

    main/jasper: update to 3.0.3

--- main/jasper/template.py
+++ main/jasper/template.py
@@ -1,7 +1,8 @@
 pkgname = "jasper"
-pkgver = "2.0.33"
+pkgver = "3.0.3"
 pkgrel = 0
 build_style = "cmake"
+make_dir = "jbuild" # build is taken
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = ["libjpeg-turbo-devel"]
 pkgdesc = "Reference implementation of the JPEG-2000 codec"
@@ -9,10 +10,10 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "JasPer-2.0"
 url = "https://ece.engr.uvic.ca/~frodo/jasper"
 source = f"https://github.com/jasper-software/{pkgname}/releases/download/version-{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "28d28290cc2eaf70c8756d391ed8bcc8ab809a895b9a67ea6e89da23a611801a"
+sha256 = "7c2ae6e10f0e4988277aba9d6d15cbf4f73576e9372c1749366e565b68c76eae"
 
 def post_install(self):
-    self.install_license("LICENSE")
+    self.install_license("LICENSE.txt")
 
 @subpackage("jasper-devel")
 def _devel(self):
