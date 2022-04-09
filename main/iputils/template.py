ed016be829ac404831b73c82ffbf7f964c0dde15
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 01:45:23 +0200

    main/iputils: update to 20211215

--- main/iputils/template.py
+++ main/iputils/template.py
@@ -1,11 +1,10 @@
 pkgname = "iputils"
-pkgver = "20210722"
+pkgver = "20211215"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-DNO_SETCAP_OR_SUID=true",
     "-DUSE_IDN=false", 
-    "-DBUILD_TFTPD=false",
     "-DBUILD_NINFOD=false"
 ]
 hostmakedepends = [
@@ -18,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause AND GPL-2.0-or-later"
 url = "https://github.com/iputils/iputils"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "6d1a44b0682d3d4b64586dbaebe61dd61ae16d6e2f4dc0c43336d0e47a9db323"
+sha256 = "b6f67fc705490673ff4471d006221b4a2f1b1180b929d9fefd771352621ccedf"
 # operation not permitted (sandbox, unshared network)
 options = ["!check"]
 
