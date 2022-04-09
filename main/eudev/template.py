bf39cf8586a7f4657bdb8b129de68c81badd41bc
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 18:51:15 +0200

    main/eudev: new upstream, update to 3.2.11

--- main/eudev/template.py
+++ main/eudev/template.py
@@ -1,5 +1,5 @@
 pkgname = "eudev"
-pkgver = "3.2.10"
+pkgver = "3.2.11"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -12,9 +12,9 @@ triggers = ["/usr/lib/udev/rules.d"]
 pkgdesc = "Standalone implementation of systemd-udev"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
-url = "https://github.com/gentoo/eudev"
-source = f"https://dev.gentoo.org/~blueness/eudev/eudev-{pkgver}.tar.gz"
-sha256 = "87bb028d470fd1b85169349b44c55d5b733733dc2d50ddf1196e026725ead034"
+url = "https://github.com/eudev-project/eudev"
+source = f"https://github.com/eudev-project/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "19847cafec67897da855fde56f9dc7d92e21c50e450aa79068a7e704ed44558b"
 options = ["!splitudev"]
 
 def post_install(self):
