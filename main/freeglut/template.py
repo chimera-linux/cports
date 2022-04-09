0f2b060c07d184f1f551a628bf91ebd8869a0572
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 05:35:53 +0200

    main/freeglut: update to 3.2.2

--- main/freeglut/template.py
+++ main/freeglut/template.py
@@ -1,5 +1,5 @@
 pkgname = "freeglut"
-pkgver = "3.2.1"
+pkgver = "3.2.2"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://freeglut.sourceforge.net"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "d4000e02102acaf259998c870e25214739d1f16f67f99cb35e4f46841399da68"
+sha256 = "c5944a082df0bba96b5756dddb1f75d0cd72ce27b5395c6c1dde85c2ff297a50"
 tool_flags = {"CFLAGS": ["-fcommon"]}
 # no tests
 options = ["!check"]
