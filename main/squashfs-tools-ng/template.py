367757bf53c5ef694f7cd7b3b4a1424a4c7fbd4a
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 01:31:34 +0200

    main/squashfs-tools-ng: update to 1.1.4

--- main/squashfs-tools-ng/template.py
+++ main/squashfs-tools-ng/template.py
@@ -1,5 +1,5 @@
 pkgname = "squashfs-tools-ng"
-pkgver = "1.1.3"
+pkgver = "1.1.4"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-or-later AND GPL-3.0-or-later"
 url = "https://infraroot.at/projects/squashfs-tools-ng/index.html"
 source = f"https://infraroot.at/pub/squashfs/{pkgname}-{pkgver}.tar.xz"
-sha256 = "abce0fcf9a8ae1c3352e4e5e87e1b077f54411da517332ea83b5e7ce948dd70d"
+sha256 = "6f3f7864f17b250453df31fe3925ce7d1430cf6b1d514112641d734fe3c8f61a"
 
 @subpackage("libsquashfs")
 def _libmagic(self):
