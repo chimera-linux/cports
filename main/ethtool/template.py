a9fe0732b3a5db291c635f6d7ffa6f821de436a0
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 01:04:12 +0200

    main/ethtool: update to 5.17

--- main/ethtool/template.py
+++ main/ethtool/template.py
@@ -1,5 +1,5 @@
 pkgname = "ethtool"
-pkgver = "5.15"
+pkgver = "5.17"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -9,4 +9,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "http://www.kernel.org/pub/software/network/ethtool"
 source = f"{url}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "686fd6110389d49c2a120f00c3cd5dfe43debada8e021e4270d74bbe452a116d"
+sha256 = "64ab914b9c6b45047245d91f40b8760b2728992a9e5af22717c644238e889133"
