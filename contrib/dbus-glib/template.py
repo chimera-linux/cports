36ed5ab59246b33653e319cef4b18025f8f98084
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 02:36:47 +0200

    contrib/dbus-glib: update to 0.112

--- contrib/dbus-glib/template.py
+++ contrib/dbus-glib/template.py
@@ -1,5 +1,5 @@
 pkgname = "dbus-glib"
-pkgver = "0.110"
+pkgver = "0.112"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://www.freedesktop.org/wiki/Software/DBusBindings"
 source = f"http://dbus.freedesktop.org/releases/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "7ce4760cf66c69148f6bd6c92feaabb8812dee30846b24cd0f7395c436d7e825"
+sha256 = "7d550dccdfcd286e33895501829ed971eeb65c614e73aadb4a08aeef719b143a"
 options = ["!cross"]
 
 @subpackage("dbus-glib-devel")
