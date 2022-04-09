95571f169fb4d5d6d506c7821f023173160d81d5
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 01:28:08 +0200

    main/setxkbmap: update to 1.3.3

--- main/setxkbmap/template.py
+++ main/setxkbmap/template.py
@@ -1,15 +1,15 @@
 pkgname = "setxkbmap"
-pkgver = "1.3.2"
+pkgver = "1.3.3"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
-makedepends = ["libx11-devel", "libxkbfile-devel"]
+makedepends = ["libx11-devel", "libxkbfile-devel", "libxrandr-devel"]
 pkgdesc = "Sets the X keyboard layout"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "8ff27486442725e50b02d7049152f51d125ecad71b7ce503cfa09d5d8ceeb9f5"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
+sha256 = "51ba28edf93a464a7444b53b154fd5e93dedd1e9bbcc85b636f4cf56986c4842"
 
 def post_install(self):
     self.install_license("COPYING")
