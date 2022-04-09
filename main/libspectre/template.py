8bd19995781b8c6ebd0bcc3412b53dc89d4cb5ce
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 20:34:53 +0200

    main/libspectre: update to 0.2.10

--- main/libspectre/template.py
+++ main/libspectre/template.py
@@ -1,5 +1,5 @@
 pkgname = "libspectre"
-pkgver = "0.2.9"
+pkgver = "0.2.10"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-static"]
@@ -13,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://www.freedesktop.org/wiki/Software/libspectre"
 source = f"http://libspectre.freedesktop.org/releases/{pkgname}-{pkgver}.tar.gz"
-sha256 = "49ae9c52b5af81b405455c19fe24089d701761da2c45d22164a99576ceedfbed"
+sha256 = "cf60b2a80f6bfc9a6b110e18f08309040ceaa755210bf94c465a969da7524d07"
 
 @subpackage("libspectre-devel")
 def _devel(self):
