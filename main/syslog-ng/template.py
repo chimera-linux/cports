d53abd7415d6db980a4e8b49aa876642f77d674c
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 01:35:17 +0200

    main/syslog-ng: update to 3.36.1

--- main/syslog-ng/template.py
+++ main/syslog-ng/template.py
@@ -1,5 +1,5 @@
 pkgname = "syslog-ng"
-pkgver = "3.35.1"
+pkgver = "3.36.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -42,7 +42,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later AND GPL-2.0-or-later"
 url = "https://www.syslog-ng.com/products/open-source-log-management"
 source = f"https://github.com/{pkgname}/{pkgname}/releases/download/{pkgname}-{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "1d0238b06b3e5987c859e5b529ecee738f75bacff04b149398b1fe8cbb121e53"
+sha256 = "90a25c9767fe749db50f118ddfc92ec71399763d2ecd5ad4f11ff5eea049e60b"
 # tests need https://github.com/Snaipe/Criterion
 options = ["!check"]
 
