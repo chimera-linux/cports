d031c6b370e0fcb4caa779bcf7398ebe4746a2ca
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 20:01:37 +0200

    main/libqmi: update to 1.30.4

--- main/libqmi/template.py
+++ main/libqmi/template.py
@@ -1,5 +1,5 @@
 pkgname = "libqmi"
-pkgver = "1.30.2"
+pkgver = "1.30.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://www.freedesktop.org/wiki/Software/libqmi"
 source = f"$(FREEDESKTOP_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "be01ece0ea2c2194cbea5744bf5aaf06c04ba5fb7ec7887a13116c76d114fedd"
+sha256 = "00d7da30a4f8d1185f37cba289cfaf1dfcd04a58f2f76d6acfdf5b85312d6ed6"
 
 @subpackage("libqmi-devel")
 def _devel(self):
