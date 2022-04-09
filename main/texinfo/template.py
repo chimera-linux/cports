c67c1b0cb8263529523d74f083d4686087827ecd
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 05:48:45 +0200

    main/texinfo: update to 6.8

--- main/texinfo/template.py
+++ main/texinfo/template.py
@@ -1,5 +1,5 @@
 pkgname = "texinfo"
-pkgver = "6.7"
+pkgver = "6.8"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -14,4 +14,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/texinfo"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "988403c1542d15ad044600b909997ba3079b10e03224c61188117f3676b02caa"
+sha256 = "8eb753ed28bca21f8f56c1a180362aed789229bd62fff58bf8368e9beb59fec4"
