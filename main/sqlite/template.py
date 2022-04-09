a522a4cce4acf668a006bbec05a6cfdc8e5da162
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 01:19:33 +0200

    main/sqlite: update to 3.38.2

--- main/sqlite/template.py
+++ main/sqlite/template.py
@@ -1,6 +1,6 @@
 pkgname = "sqlite"
-pkgver = "3.36.0"
-_amalg = "3360000"
+pkgver = "3.38.2"
+_amalg = "3380200"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -12,17 +12,16 @@ pkgdesc = "SQL Database Engine in a C library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:none"
 url = "https://sqlite.org"
-source = f"https://sqlite.org/2021/sqlite-autoconf-{_amalg}.tar.gz"
-sha256 = "bd90c3eb96bee996206b83be7065c9ce19aef38c3f4fb53073ada0d0b69bbce3"
+source = f"https://sqlite.org/2022/sqlite-autoconf-{_amalg}.tar.gz"
+sha256 = "e7974aa1430bad690a5e9f79a6ee5c8492ada8269dc675875ad0fb747d7cada4"
 options = ["!parallel"]
 
 _cflags = [
     "-DSQLITE_ENABLE_DBSTAT_VTAB", "-DSQLITE_ENABLE_COLUMN_METADATA",
     "-DSQLITE_ENABLE_UNLOCK_NOTIFY", "-DSQLITE_SECURE_DELETE",
-    "-DSQLITE_ENABLE_JSON1", "-DSQLITE_ENABLE_FTS3",
-    "-DSQLITE_ENABLE_FTS3_PARENTHESIS", "-DSQLITE_ENABLE_FTS4",
-    "-DSQLITE_ENABLE_FTS3_TOKENIZER=1", "-DSQLITE_ENABLE_BATCH_ATOMIC_WRITE=1",
-    "-DSQLITE_ENABLE_DESERIALIZE",
+    "-DSQLITE_ENABLE_FTS3", "-DSQLITE_ENABLE_FTS3_PARENTHESIS",
+    "-DSQLITE_ENABLE_FTS4", "-DSQLITE_ENABLE_FTS3_TOKENIZER=1",
+    "-DSQLITE_ENABLE_BATCH_ATOMIC_WRITE=1", "-DSQLITE_ENABLE_DESERIALIZE",
 
     "-DHAVE_FDATASYNC"
 ]
