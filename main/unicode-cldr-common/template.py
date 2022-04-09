d26283613765c73a446563cd7d5eef9256e548a9
Daniel Kolesa <q66@chimera-linux.org>
2022-04-09 19:35:37 +0200

    main/unicode-cldr-common: update to 41.0

--- main/unicode-cldr-common/template.py
+++ main/unicode-cldr-common/template.py
@@ -1,12 +1,12 @@
 pkgname = "unicode-cldr-common"
-pkgver = "40.0"
+pkgver = "41.0"
 pkgrel = 0
 pkgdesc = "Common data from Unicode CLDR"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Unicode-DFS-2016"
 url = "https://cldr.unicode.org"
 source = f"https://github.com/unicode-org/cldr/releases/download/release-{pkgver[:-2]}/cldr-common-{pkgver}.zip"
-sha256 = "8d03c1ba6a3e33280e3959a34fe37d0a7002a4b6ac40c6570a69f7bbc25e6756"
+sha256 = "823c6170c41e2de2c229574e8a436332d25f1c9723409867fe721e00bc92d853"
 
 def do_install(self):
     self.install_dir("usr/share/unicode/cldr")
