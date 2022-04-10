08902e72dc23f5b6077931454a3b8043d954b2cd
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 06:31:29 +0200

    main/simple-scan: update to 42.0

--- main/simple-scan/template.py
+++ main/simple-scan/template.py
@@ -1,5 +1,5 @@
 pkgname = "simple-scan"
-pkgver = "40.7"
+pkgver = "42.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -16,4 +16,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/simple-scan"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "7c551852cb5af7d34aa989f8ad5ede3cbe31828cf8dd5aec2b2b6fdcd1ac3d53"
+sha256 = "ac1f857afd0bc8897dd2045023ad7c5713e5ceefca56b0b3cc5e9a4795329586"
