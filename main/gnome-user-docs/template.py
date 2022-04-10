388b4e500afd4e69946bec30c874473ca3964ec2
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 08:08:15 +0200

    main/gnome-user-docs: update to 42.0

--- main/gnome-user-docs/template.py
+++ main/gnome-user-docs/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-user-docs"
-pkgver = "41.1"
+pkgver = "42.0"
 pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
@@ -13,5 +13,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "CC-BY-3.0"
 url = "https://gitlab.gnome.org/GNOME/gnome-user-docs"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "55ee3df577d1717fc152fc2aeb89d0af7a9eca866c8974c675f60bd630888c48"
+sha256 = "b2b27da04100bbdd2f4f0e5bc38f2d056d95fa3656720163f6436a3167d5e905"
 options = ["!splitdoc"]
