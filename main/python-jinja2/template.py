aa183d4c3bf99ecd2664443055556dfc71241931
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 06:15:18 +0200

    main/python-jinja2: update to 3.1.1

--- main/python-jinja2/template.py
+++ main/python-jinja2/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-jinja2"
-pkgver = "3.0.3"
+pkgver = "3.1.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://jinja.pocoo.org"
 source = f"$(PYPI_SITE)/J/Jinja2/Jinja2-{pkgver}.tar.gz"
-sha256 = "611bb273cd68f3b993fabdc4064fc858c5b47a973cb5aa7999ec1ba405c87cd7"
+sha256 = "640bed4bb501cbd17194b3cace1dc2126f5b619cf068a726b98192a0fde74ae9"
 # dependency of pytest
 options = ["!check"]
 
