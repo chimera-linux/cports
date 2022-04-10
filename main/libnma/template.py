27d87f1c64e4df7f772163d3bed792c99362fc43
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 07:09:19 +0200

    main/libnma: enable gtk4

--- main/libnma/template.py
+++ main/libnma/template.py
@@ -2,13 +2,15 @@ pkgname = "libnma"
 pkgver = "1.8.38"
 pkgrel = 0
 build_style = "meson"
-configure_args = ["-Dgtk_doc=false"]
+configure_args = [
+    "-Dgtk_doc=false", "-Dlibnma_gtk4=true",
+]
 hostmakedepends = [
     "meson", "pkgconf", "gobject-introspection", "vala", "glib-devel",
     "gettext-tiny",
 ]
 makedepends = [
-    "networkmanager-devel", "gcr-devel", "gtk+3-devel",
+    "networkmanager-devel", "gcr-devel", "gtk+3-devel", "gtk4-devel",
     "mobile-broadband-provider-info", "iso-codes",
 ]
 depends = ["networkmanager", "iso-codes"]
@@ -18,6 +20,7 @@ license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libnma"
 source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "241c7202c88af3a83331830c210958866b15c858fab3733bf663837d14d40640"
+options = ["!cross"]
 
 @subpackage("libnma-devel")
 def _devel(self):
