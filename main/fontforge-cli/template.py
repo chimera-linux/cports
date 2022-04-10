f32bca5de560c7962f4bee4b288a4b25453ab706
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 02:15:33 +0200

    main/fontforge-cli: update to 20220308

--- main/fontforge-cli/template.py
+++ main/fontforge-cli/template.py
@@ -1,5 +1,5 @@
 pkgname = "fontforge-cli"
-pkgver = "20201107"
+pkgver = "20220308"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DENABLE_GUI=OFF"]
@@ -7,15 +7,15 @@ hostmakedepends = ["cmake", "ninja", "gettext-tiny", "pkgconf"]
 makedepends = [
     "python-devel", "libedit-devel", "zlib-devel", "giflib-devel",
     "libpng-devel", "libjpeg-turbo-devel", "libtiff-devel", "libxml2-devel",
-    "freetype-devel", "libspiro-devel", "libuninameslist-devel",
+    "freetype-devel", "libspiro-devel", "libuninameslist-devel", "woff2-devel",
 ]
 depends = ["!fontforge"] # conflicts with gui version
-pkgdesc = "Create and modify PostScript, TrueType and SVG fonts - CLI version"
+pkgdesc = "Create and modify PostScript, TrueType and SVG fonts (CLI version)"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://fontforge.github.io/en-US"
 source = f"https://github.com/fontforge/fontforge/archive/{pkgver}.tar.gz"
-sha256 = "274f8c8cbd7b6a1c77d2a1c03d4d6cd3c9319db62be8b8c88fabbf597f7e863c"
+sha256 = "58bbc759eb102263be835e6c006b1c16b508ba3d0252acd5389062826764f7a5"
 
 def post_install(self):
     for f in (self.cwd / "contrib/cidmap").glob("Adobe-*.cidmap"):
