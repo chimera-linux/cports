cc7291ab2ec24f3a7f7afeedb71bda2ae6c6e4a9
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 07:22:20 +0200

    main/gnome-shell: update to 42.0

--- main/gnome-shell/template.py
+++ main/gnome-shell/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-shell"
-pkgver = "41.3"
+pkgver = "42.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -15,7 +15,7 @@ makedepends = [
     "gnome-control-center-devel", "evolution-data-server-devel",
     "gsettings-desktop-schemas-devel", "startup-notification-devel",
     "mutter-devel", "at-spi2-atk-devel", "mutter-devel", "gjs-devel",
-    "gcr-devel", "gtk+3-devel", "libxml2-devel", "ibus-devel",
+    "gcr-devel", "gtk4-devel", "libxml2-devel", "ibus-devel",
     "gnome-bluetooth-devel", "gstreamer-devel", "pipewire-devel",
     "libpulse-devel", "gnome-desktop-devel", "elogind-devel",
     "polkit-devel", "networkmanager-devel", "gnome-autoar-devel",
@@ -30,6 +30,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Projects/GnomeShell"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "1e3dfa2a0be49454182b4ace77f11d10d3f5b988ef0fcb732b7313573949ded1"
+sha256 = "33d404fb3c93b9de42984f0110a2969d6a4a7247c27fe7f5e24c67ecfec75094"
 # tests need libmutter-test
 options = ["!check"]
