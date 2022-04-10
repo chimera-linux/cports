596f8aa43aabccdf150ae85174cc039354fe5e75
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 06:17:03 +0200

    main/python-pygments: update to 2.11.2

--- main/python-pygments/template.py
+++ main/python-pygments/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pygments"
-pkgver = "2.10.0"
+pkgver = "2.11.2"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://pygments.org"
 source = f"$(PYPI_SITE)/P/Pygments/Pygments-{pkgver}.tar.gz"
-sha256 = "f398865f7eb6874156579fdf36bc840a03cab64d1cde9e93d68f46a425ec52c6"
+sha256 = "4e426f72023d88d03b2fa258de560726ce890ff3b630f88c21cbb8b2503b8c6a"
 # dependency of pytest
 options = ["!check"]
 
