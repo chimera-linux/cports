eade9648296bac583e287608ba8ad7b763ba7b65
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 08:21:06 +0200

    main/eog: update to 42.0

--- main/eog/template.py
+++ main/eog/template.py
@@ -1,5 +1,5 @@
 pkgname = "eog"
-pkgver = "41.1"
+pkgver = "42.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=true"]
@@ -8,9 +8,10 @@ hostmakedepends = [
     "itstool",
 ]
 makedepends = [
-    "libglib-devel", "gtk+3-devel", "gnome-desktop-devel", "libpeas-devel",
-    "gdk-pixbuf-devel", "libexif-devel", "exempi-devel", "lcms2-devel",
-    "libportal-devel", "libjpeg-turbo-devel", "librsvg-devel",
+    "libglib-devel", "gtk+3-devel", "libhandy-devel", "libpeas-devel",
+    "gnome-desktop-devel", "gdk-pixbuf-devel", "libexif-devel",
+    "exempi-devel", "lcms2-devel", "libportal-devel", "librsvg-devel",
+    "libjpeg-turbo-devel",
 ]
 depends = ["hicolor-icon-theme", "shared-mime-info"]
 provides = ["so:libeog.so=0"]
@@ -19,7 +20,8 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/EyeOfGnome"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "86e1b9ba39dacf74226afa457ab983b41253b89f617bf54139cad0892d02d8a9"
+sha256 = "fb35bfb5167a42121f59a7b9b7ac0d75bbd0517b9afd6d91831e84d35735ddf8"
+options = ["!cross"]
 
 @subpackage("eog-devel")
 def _devel(self):
