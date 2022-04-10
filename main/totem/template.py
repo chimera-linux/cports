942e60b1b14d1a3036e4ada206858f3ec7721973
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 08:14:17 +0200

    main/totem: update to 42.0

--- main/totem/template.py
+++ main/totem/template.py
@@ -1,8 +1,5 @@
 pkgname = "totem"
-pkgver = "41_alpha0"
-# use a snapshot for now to avoid clutter-gtk
-_commit = "b0cd071c7cbc85aaa8ff0cde02b821084d6eedda"
-_libgd = "cc90107531640bcba6c3c58e5cf6aec94d498763"
+pkgver = "42.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -25,22 +22,10 @@ pkgdesc = "GNOME media player"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/Videos"
-source = [
-    f"https://gitlab.gnome.org/GNOME/{pkgname}/-/archive/{_commit}.tar.gz",
-    f"https://gitlab.gnome.org/GNOME/libgd/-/archive/{_libgd}.tar.gz",
-]
-sha256 = [
-    "d069e2d8e38016bd3248dd8a7cb2dffc8eb08c9f5b5eee2d0acb11cb60112ec5",
-    "f068de749a30695cd361fa6406c71421caf66f976008e7385dcd80fead6f46ad",
-]
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "4af0491ddb95df8b33aee399d3a50f9c7ab17de88d3af63356567cf88f57e6ab"
 # needs graphical environment
 options = ["!check"]
-
-def post_extract(self):
-    for f in (self.cwd / f"totem-{_commit}").iterdir():
-        self.mv(f, ".")
-    self.rm("subprojects/libgd", recursive = True)
-    self.mv(f"libgd-{_libgd}", "subprojects/libgd")
 
 @subpackage("totem-libs")
 def _libs(self):
