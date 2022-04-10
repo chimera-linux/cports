13a230f9e3dc11277c947dc09cf0a6b5954f94d4
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 08:06:33 +0200

    main/gnome-font-viewer: update to 42.0

--- main/gnome-font-viewer/template.py
+++ main/gnome-font-viewer/template.py
@@ -1,12 +1,12 @@
 pkgname = "gnome-font-viewer"
-pkgver = "41.0"
+pkgver = "42.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "meson", "pkgconf", "glib-devel", "gettext-tiny",
 ]
 makedepends = [
-    "gtk+3-devel", "libglib-devel", "libhandy-devel", "fontconfig-devel",
+    "gtk4-devel", "libadwaita-devel", "libglib-devel", "fontconfig-devel",
     "harfbuzz-devel", "freetype-devel", "gnome-desktop-devel",
 ]
 pkgdesc = "Font viewer for GNOME"
@@ -14,4 +14,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-font-viewer"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "5dd410331be070e4e034397f2754980e073851d50a2119f2fbf96adc6fe2e876"
+sha256 = "cfa2b8dfff21a105a1a021dadfa213f13627e6a179a77c7b59fdcedaca848dcc"
