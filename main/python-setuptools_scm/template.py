10d3c7a46668c6dde541917857a1a9791cc76fa0
Daniel Kolesa <q66@chimera-linux.org>
2022-04-10 05:35:07 +0200

    main/python-setuptools_scm: update to 6.4.2

--- main/python-setuptools_scm/template.py
+++ main/python-setuptools_scm/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-setuptools_scm"
-pkgver = "6.3.2"
+pkgver = "6.4.2"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools", "python-tomli"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pypa/setuptools_scm"
 source = f"$(PYPI_SITE)/s/setuptools_scm/setuptools_scm-{pkgver}.tar.gz"
-sha256 = "a49aa8081eeb3514eb9728fa5040f2eaa962d6c6f4ec9c32f6c1fba88f88a0f2"
+sha256 = "6833ac65c6ed9711a4d5d2266f8024cfa07c533a0e55f4c12f6eff280a5a9e30"
 # tests fail when the package is not installed
 options = ["!check"]
 
