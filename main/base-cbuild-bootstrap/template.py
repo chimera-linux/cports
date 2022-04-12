3dc5e2fb98e1c949b0f698944c4901d2ce0d2105
Daniel Kolesa <q66@chimera-linux.org>
2022-04-13 00:56:46 +0200

    main/base-cbuild-bootstrap: new package
    This is a complete metapackage that has everything one needs to
    have in a Chimera system to rebootstrap it from source code.

---
+++ main/base-cbuild-bootstrap/template.py
@@ -0,0 +1,17 @@
+pkgname = "base-cbuild-bootstrap"
+pkgver = "0.1"
+pkgrel = 0
+build_style = "meta"
+depends = [
+    "base-cbuild-host", # all the host tools for cbuild itself
+    # toolchain base
+    "clang", "lld", "elftoolchain", "linux-headers",
+    # stage 0 tooling
+    "bsdpatch", "bsdm4", "pkgconf", "byacc", "flex", "perl",
+    # stage 0 build systems
+    "cmake", "meson", "bmake", "gmake", "ninja",
+]
+pkgdesc = "Everything one needs to build Chimera from source"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:meta"
+url = "https://chimera-linux.org"
