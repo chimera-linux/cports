c9a1133a1b74bfbc2c83230f87abb7dfb5bc340d
Daniel Kolesa <q66@chimera-linux.org>
2022-05-05 03:04:18 +0200

    main/spirv-headers: update to 1.3.211.0
    This changes versioning scheme (the new version is lower). The
    versioning scheme now matches the other pieces of the spirv sdk.

--- main/spirv-headers/template.py
+++ main/spirv-headers/template.py
@@ -1,9 +1,5 @@
 pkgname = "spirv-headers"
-# a recent snapshot is needed, latest tagged release is not good enough
-# for spirv-llvm-translator and without it we cannot build libclc
-_commit = "eddd4dfc930f1374a70797460240a501c7d333f7"
-# actually not the real version
-pkgver = "1.5.4"
+pkgver = "1.3.211.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -11,8 +7,8 @@ pkgdesc = "Machine-readable files for the SPIR-V Registry"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:spirv-headers"
 url = "https://github.com/KhronosGroup/SPIRV-Headers"
-source = f"{url}/archive/{_commit}.tar.gz"
-sha256 = "845c29d7f44bd6c5bb7ddefba93deec888ce03439d3b8a1c1388e34fd7c13944"
+source = f"{url}/archive/refs/tags/sdk-{pkgver}.tar.gz"
+sha256 = "30a78e61bd812c75e09fdc7a319af206b1044536326bc3e85fea818376a12568"
 # no test suite
 options = ["!check"]
 
