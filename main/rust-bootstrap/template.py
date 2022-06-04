947493e2993c3833ae9ef9e682b9a27563e281e9
Daniel Kolesa <q66@chimera-linux.org>
2022-06-05 00:22:57 +0200

    main/rust-bootstrap: update to 1.61.0

--- main/rust-bootstrap/template.py
+++ main/rust-bootstrap/template.py
@@ -1,5 +1,5 @@
 pkgname = "rust-bootstrap"
-pkgver = "1.60.0"
+pkgver = "1.61.0"
 pkgrel = 0
 # satisfy revdeps
 makedepends = ["zlib", "ncurses-libs"]
@@ -19,13 +19,13 @@ options = ["!strip"]
 match self.profile().arch:
     case "ppc64le":
         sha256 = [
-            "b23bc823f0208049554df1f526c740684f36a6e33d79977114b361e6cb5fd244",
-            "3a4644b7487732b581bff1da539f64f99f25ec2b6b85d2f7183333faea167999",
+            "1f334527f4067729e8e4f5605507269d3b9638d86a9e5d6ab96d02c3bd3592aa",
+            "0aed1fa0d893baad2ea46fc4cfc875166e1edb4e227b5c2bc290d680114efbbf",
         ]
     case "x86_64":
         sha256 = [
-            "990f6bdb44956444cc8a280be41f59bf3147775f08cbc69bf2fbbf40a0a90178",
-            "1bf6b7bf3eb2e2b0aec2e39e36a2db383bf05e6d095455d4fdfd33d1044a8e38",
+            "48a8475d2436815b320ffe336f6805ad2f5f2424ac571f1491d1080ce749d5cc",
+            "78353b58110a64b4ceb55a08c233bca2fb0eab717a6f281b9e9e768d2119908a",
         ]
     case _:
         broken = f"not yet built for {self.profile().arch}"
