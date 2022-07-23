d77e1e530e29eb666134f8de4939f1c0a69778f1
Daniel Kolesa <q66@chimera-linux.org>
2022-07-23 18:45:43 +0200

    main/dinit-userservd: update
    This mainly brings some robustness fixes - userservd will now
    wait for any old dinit process formerly belonging to the session
    to die before starting a new one (can happen if somebody logs in,
    logs out, and then logs in again before the old dinit has any
    opportunity to finish quitting), and it will not rely solely on
    SIGTERM behaving when it comes to shutting down dinit instances;
    a timeout is set up, and a SIGKILL is attempted after that
    expires.

--- main/dinit-userservd/template.py
+++ main/dinit-userservd/template.py
@@ -1,6 +1,6 @@
 pkgname = "dinit-userservd"
 pkgver = "0.1.0_git20220723"
-_commit = "f7a220026eee5547fc871a4dec66b8962a413004"
+_commit = "dc8c5da96b6dd07e2ad904bda9635476b02dfb70"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -10,4 +10,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/dinit-userservd"
 source = f"https://github.com/chimera-linux/{pkgname}/archive/{_commit}.tar.gz"
-sha256 = "054ac0cb5cc6a25798281cb143c03d2af07475352c845fcc04908619e235a6e0"
+sha256 = "69208eb923b9419b7be6c1d0a2218003d518de0e0ee693ca2e82d5f281e4e7e3"
