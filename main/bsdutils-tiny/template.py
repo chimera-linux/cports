4b0e1cab10f5d8cd9fe8edea2da822f15e6a16f7
Daniel Kolesa <q66@chimera-linux.org>
2022-07-29 00:20:10 +0200

    main/bsdutils-tiny: update

--- main/bsdutils-tiny/template.py
+++ main/bsdutils-tiny/template.py
@@ -1,28 +1,32 @@
 pkgname = "bsdutils-tiny"
-_commit="d4182ca7a0afd33a199f433af049800a944c93a6"
+_commit="309e666117edfbad300181a3c298233420e99ece"
 pkgver = "0.0.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
     "--bindir=libexec/bsdutils-tiny",
     "--sbindir=libexec/bsdutils-tiny",
+    "--libexecdir=libexec/bsdutils-tiny/libexec",
     "-Dstatic_fts=true",
     "-Dstatic_rpmatch=true",
     "-Dcolor_ls=false",
     "-Dlibcrypto=disabled",
     "-Dlibedit=disabled",
+    "-Dzlib=disabled",
+    "-Dlzma=disabled",
+    "-Dbzip2=disabled",
 ]
 hostmakedepends = ["flex", "byacc", "meson", "pkgconf"]
 makedepends = [
     "musl-fts-devel", "musl-rpmatch-devel", "libxo-tiny-devel",
-    "linux-headers",
+    "linux-headers", "musl-bsd-headers",
 ]
 pkgdesc = "FreeBSD userland utilities (tiny version)"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/bsdutils"
 source = f"https://github.com/chimera-linux/bsdutils/archive/{_commit}.tar.gz"
-sha256 = "5a4f7be8fca0139f2b58eb7c2ed8fb15426c60598368af7449c7611502d35e60"
+sha256 = "49c1e9fe032ea3684b4097a4a501b310f8fe0663f36e6ee56278537636cbcaf4"
 
 def post_install(self):
     # drop manpages
