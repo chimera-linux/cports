29222bbe747877ce7a98d502b110d63d0451b924
Daniel Kolesa <q66@chimera-linux.org>
2022-07-29 19:42:15 +0200

    main/libexpat: fix check

--- main/libexpat/template.py
+++ main/libexpat/template.py
@@ -4,6 +4,7 @@ pkgrel = 0
 build_style = "gnu_configure"
 make_check_args = ["-j1"]
 hostmakedepends = ["pkgconf"]
+checkdepends = ["mksh"]
 pkgdesc = "XML parser library written in C"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
