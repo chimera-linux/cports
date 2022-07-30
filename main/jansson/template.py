6018a2955ef7af1fde5c2216968e355032402817
Daniel Kolesa <q66@chimera-linux.org>
2022-07-30 03:43:12 +0200

    main/jansson: switch to release tarball and remove autoreconf

--- main/jansson/template.py
+++ main/jansson/template.py
@@ -2,16 +2,13 @@ pkgname = "jansson"
 pkgver = "2.14"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf", "automake", "libtool"]
+hostmakedepends = ["pkgconf"]
 pkgdesc = "Library for encoding, decoding and manipulating JSON data"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.digip.org/jansson"
-source = f"https://github.com/akheron/{pkgname}/archive/v{pkgver}.tar.gz"
-sha256 = "c739578bf6b764aa0752db9a2fdadcfe921c78f1228c7ec0bb47fa804c55d17b"
-
-def pre_configure(self):
-    self.do("autoreconf", "-if")
+source = f"https://github.com/akheron/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "5798d010e41cf8d76b66236cfb2f2543c8d082181d16bc3085ab49538d4b9929"
 
 def post_install(self):
     self.install_license("LICENSE")
