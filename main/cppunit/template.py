77ebebf05389a2942eee1f8ddea490384a5257c8
Daniel Kolesa <q66@chimera-linux.org>
2022-07-30 03:40:37 +0200

    main/cppunit: no need to autoreconf

--- main/cppunit/template.py
+++ main/cppunit/template.py
@@ -4,16 +4,13 @@ pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-werror"]
 make_cmd = "gmake"
-hostmakedepends = ["gmake", "pkgconf", "automake", "libtool"]
+hostmakedepends = ["gmake", "pkgconf"]
 pkgdesc = "C++ unit testing framework"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "http://cppunit.sourceforge.net"
 source = f"https://dev-www.libreoffice.org/src/{pkgname}-{pkgver}.tar.gz"
 sha256 = "89c5c6665337f56fd2db36bc3805a5619709d51fb136e51937072f63fcc717a7"
-
-def pre_configure(self):
-    self.do("autoreconf", "-if")
 
 @subpackage("cppunit-devel")
 def _devel(self):
