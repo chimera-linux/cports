580168c4c1f9d5762223426afdb93b81c734f522
Daniel Kolesa <q66@chimera-linux.org>
2022-08-03 00:09:44 +0200

    main/chimerautils: fix patch(1)

--- main/chimerautils-tiny/template.py
+++ main/chimerautils-tiny/template.py
@@ -1,5 +1,5 @@
 pkgname = "chimerautils-tiny"
-_commit="670908b6c79f60c7e48d682b3e03996994513cbb"
+_commit="a1310e9f9c0345875ef7c8bccafe278e16becefc"
 pkgver = "0.0.1"
 pkgrel = 0
 build_style = "meson"
@@ -26,7 +26,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/chimerautils"
 source = f"https://github.com/chimera-linux/chimerautils/archive/{_commit}.tar.gz"
-sha256 = "dbdfd753b22bd19fc3f214f9aead12c603b114636466b3c3aff09b93c485bc18"
+sha256 = "1b13f1f8f02983b5c58739228cf4c1c8e04cfd12ed0fdceb281aaa81a1d25bb8"
 
 def post_install(self):
     # drop manpages
