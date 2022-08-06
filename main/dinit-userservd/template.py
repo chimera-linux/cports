461c7099e94c3cb57c47b0ff051cb39fbfa31d42
Daniel Kolesa <q66@chimera-linux.org>
2022-08-06 12:58:11 +0200

    main/dinit-userservd: update
    This pulls in some minor upstream changes wrt pipe queuing.

--- main/dinit-userservd/template.py
+++ main/dinit-userservd/template.py
@@ -1,6 +1,6 @@
 pkgname = "dinit-userservd"
 pkgver = "0.1.0_git20220723"
-_commit = "dc8c5da96b6dd07e2ad904bda9635476b02dfb70"
+_commit = "209454fceded25308042bed5397842c5436fd40e"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -10,4 +10,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/dinit-userservd"
 source = f"https://github.com/chimera-linux/{pkgname}/archive/{_commit}.tar.gz"
-sha256 = "69208eb923b9419b7be6c1d0a2218003d518de0e0ee693ca2e82d5f281e4e7e3"
+sha256 = "6a7acce0399fe6326dccb7d6c9c1895189938ed8aac0f0c6ba55fb3dbf65083d"
