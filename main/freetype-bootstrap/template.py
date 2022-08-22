b29ec00fe26be42cb72b2335f5737fba4871f3a5
Daniel Kolesa <q66@chimera-linux.org>
2022-08-23 01:27:25 +0200

    main/freetype-bootstrap: update to 2.12.1

--- main/freetype-bootstrap/template.py
+++ main/freetype-bootstrap/template.py
@@ -2,7 +2,7 @@
 # needs); after that we can build real freetype with harfbuzz support and get
 # better hinting - do not actually use this package in a regular system
 pkgname = "freetype-bootstrap"
-pkgver = "2.12.0"
+pkgver = "2.12.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -25,7 +25,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "FTL OR GPL-2.0-or-later"
 url = "https://freetype.org"
 source = f"$(NONGNU_SITE)/freetype/freetype-{pkgver}.tar.xz"
-sha256 = "ef5c336aacc1a079ff9262d6308d6c2a066dd4d2a905301c4adda9b354399033"
+sha256 = "4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f"
 options = ["!lto", "!scanshlibs"]
 
 def post_install(self):
