f4c04d409ed1d5d2f7f83d91c07a856673985a74
Daniel Kolesa <q66@chimera-linux.org>
2022-08-24 02:41:54 +0200

    main/dinit-chimera: add an xsession scriptlet

---
+++ main/dinit-chimera/files/01dinit-env
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# we want to make these available in the user service activation environment
+dinitctl setenv DISPLAY XAUTHORITY > /dev/null 2>&1 || :
--- main/dinit-chimera/template.py
+++ main/dinit-chimera/template.py
@@ -20,3 +20,17 @@ def post_install(self):
     self.install_file(self.files_path / "locale.conf", "etc")
     # init symlink
     self.install_link("dinit", "usr/bin/init")
+    # x11 support
+    self.install_dir("etc/X11/Xsession.d")
+    self.install_file(
+        self.files_path / "01dinit-env", "etc/X11/Xsession.d", mode = 0o755
+    )
+
+@subpackage("dinit-chimera-x11")
+def _x11(self):
+    self.pkgdesc = f"{pkgdesc} (X11 support)"
+    self.depends = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}", "xinit"]
+    return [
+        "etc/X11/Xsession.d",
+    ]
