963fddaf7e1ec40db9a076fd10a525ebb7f9c257
Daniel Kolesa <q66@chimera-linux.org>
2022-08-25 01:15:59 +0200

    contrib/atkmm: update to 2.36.2

--- contrib/atkmm/template.py
+++ contrib/atkmm/template.py
@@ -1,5 +1,5 @@
 pkgname = "atkmm"
-pkgver = "2.36.1"
+pkgver = "2.36.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gtkmm.org"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "e11324bfed1b6e330a02db25cecc145dca03fb0dff47f0710c85e317687da458"
+sha256 = "6f62dd99f746985e573605937577ccfc944368f606a71ca46342d70e1cdae079"
 
 @subpackage("atkmm-devel")
 def _devel(self):
