751975fe615734dfd0055785e5c8d3e51a08db3e
Daniel Kolesa <q66@chimera-linux.org>
2022-08-25 01:30:32 +0200

    main/bluez: update to 5.65

--- main/bluez/template.py
+++ main/bluez/template.py
@@ -1,5 +1,5 @@
 pkgname = "bluez"
-pkgver = "5.64"
+pkgver = "5.65"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -29,7 +29,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "http://www.bluez.org"
 source = f"$(KERNEL_SITE)/bluetooth/{pkgname}-{pkgver}.tar.xz"
-sha256 = "ae437e65b6b3070c198bc5b0109fe9cdeb9eaa387380e2072f9de65fe8a1de34"
+sha256 = "2565a4d48354b576e6ad92e25b54ed66808296581c8abb80587051f9993d96d4"
 tool_flags = {"CFLAGS": ["-Wno-deprecated-declarations"]}
 system_groups = ["bluetooth"]
 
