33d52fd6fedeed94f119e84e5517c83fcd0d5112
Daniel Kolesa <q66@chimera-linux.org>
2022-08-25 02:01:15 +0200

    main/dialog: update to 1.3.20220728

--- main/dialog/template.py
+++ main/dialog/template.py
@@ -1,5 +1,5 @@
 pkgname = "dialog"
-pkgver = "1.3.20220117"
+pkgver = "1.3.20220728"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-ncursesw", "--disable-nls"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-only"
 url = "https://invisible-island.net/dialog"
 source = f"https://invisible-mirror.net/archives/{pkgname}/{pkgname}-{pkgver.replace('.2022', '-2022')}.tgz"
-sha256 = "754cb6bf7dc6a9ac5c1f80c13caa4d976e30a5a6e8b46f17b3bb9b080c31041f"
+sha256 = "54418973d559a461b00695fafe68df62f2bc73d506b436821d77ca3df454190b"
 
 def post_install(self):
     self.rm(self.destdir / "usr/lib", force = True, recursive = True)
