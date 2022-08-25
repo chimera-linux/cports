fb47daa7d74b7b67cc338abc52625f344480ae43
Daniel Kolesa <q66@chimera-linux.org>
2022-08-25 02:41:49 +0200

    main/gcr: update to 3.41.1

--- main/gcr/template.py
+++ main/gcr/template.py
@@ -1,9 +1,10 @@
 pkgname = "gcr"
-pkgver = "3.41.0"
+pkgver = "3.41.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
-    "-Dgpg_path=/usr/bin/gpg", "-Dsystemd=disabled", "-Dssh_agent=false"
+    "-Dgpg_path=/usr/bin/gpg", "-Dsystemd=disabled", "-Dssh_agent=false",
+    "-Dgtk_doc=false",
 ]
 hostmakedepends = [
     "meson", "pkgconf", "glib-devel", "gtk-doc-tools", "gettext-tiny-devel",
@@ -18,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gcr"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "0909fc49ea8ad4832d27564ff2fd1dc666696e2a07c73941c48829e60572da01"
+sha256 = "bb7128a3c2febbfee9c03b90d77d498d0ceb237b0789802d60185c71c4bea24f"
 # needs x11
 options = ["!check"]
 
