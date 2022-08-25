b64c553a7bfa14f454ad390d0c1c1cc9e2329c44
Daniel Kolesa <q66@chimera-linux.org>
2022-08-25 22:30:36 +0200

    main/grilo: update to 0.3.15

--- main/grilo/template.py
+++ main/grilo/template.py
@@ -1,10 +1,10 @@
 pkgname = "grilo"
-pkgver = "0.3.14"
+pkgver = "0.3.15"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Denable-introspection=true", "-Denable-vala=true",
-    "-Denable-gtk-doc=false",
+    "-Denable-gtk-doc=false", "-Dsoup3=true",
 ]
 hostmakedepends = [
     "meson", "pkgconf", "gobject-introspection", "glib-devel",
@@ -19,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Projects/Grilo"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-3]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "0369d0b00bb0f59ba5f7aea8cfc665f38df14a5b4182d28c7c1e2cd15b518743"
+sha256 = "f352acf73665669934270636fede66b52da6801fe20f638c4048ab2678577b2d"
 
 @subpackage("grilo-devel")
 def _devel(self):
