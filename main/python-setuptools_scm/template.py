1d21a39ab5763f6996d546c3e65e05ca655880f3
Daniel Kolesa <q66@chimera-linux.org>
2022-08-25 23:29:46 +0200

    main/python-setuptools_scm: update to 7.0.5

--- main/python-setuptools_scm/template.py
+++ main/python-setuptools_scm/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-setuptools_scm"
-pkgver = "6.4.2"
+pkgver = "7.0.5"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools", "python-tomli"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pypa/setuptools_scm"
 source = f"$(PYPI_SITE)/s/setuptools_scm/setuptools_scm-{pkgver}.tar.gz"
-sha256 = "6833ac65c6ed9711a4d5d2266f8024cfa07c533a0e55f4c12f6eff280a5a9e30"
+sha256 = "031e13af771d6f892b941adb6ea04545bbf91ebc5ce68c78aaf3fff6e1fb4844"
 # tests fail when the package is not installed
 options = ["!check"]
 
