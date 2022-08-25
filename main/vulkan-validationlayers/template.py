a268451bf2380d6469d27ee189b534aed0256d0b
Daniel Kolesa <q66@chimera-linux.org>
2022-08-25 04:13:32 +0200

    main/vulkan-validationlayers: update to 1.3.225

--- main/vulkan-validationlayers/template.py
+++ main/vulkan-validationlayers/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-validationlayers"
-pkgver = "1.3.211"
+pkgver = "1.3.225"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -19,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://www.khronos.org/vulkan"
 source = f"https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/v{pkgver}.tar.gz"
-sha256 = "d748df9a37979cd9fefd342c88a71825fefa25624d8d6326383aac44e66bd5c1"
+sha256 = "36831d17b2d7bd0e8b75e9e6f935687e7820d23be0abc89099b21e3b15cf9c20"
 # no test suite
 options = ["!cross"]
 
