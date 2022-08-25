821befe5412af46ec4737e7c3f98e4d36fff884c
Daniel Kolesa <q66@chimera-linux.org>
2022-08-25 23:14:51 +0200

    main/jasper: update to 3.0.6

--- main/jasper/template.py
+++ main/jasper/template.py
@@ -1,5 +1,5 @@
 pkgname = "jasper"
-pkgver = "3.0.3"
+pkgver = "3.0.6"
 pkgrel = 0
 build_style = "cmake"
 make_dir = "jbuild" # build is taken
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "JasPer-2.0"
 url = "https://ece.engr.uvic.ca/~frodo/jasper"
 source = f"https://github.com/jasper-software/{pkgname}/releases/download/version-{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "7c2ae6e10f0e4988277aba9d6d15cbf4f73576e9372c1749366e565b68c76eae"
+sha256 = "169be004d91f6940c649a4f854ada2755d4f35f62b0555ce9e1219c778cffc09"
 
 def post_install(self):
     self.install_license("LICENSE.txt")
