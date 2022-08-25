e3db1edbce8ff684242d065939af7d4a6361188c
Daniel Kolesa <q66@chimera-linux.org>
2022-08-25 03:44:17 +0200

    main/shaderc: update to 2022.2

--- main/shaderc/template.py
+++ main/shaderc/template.py
@@ -1,5 +1,5 @@
 pkgname = "shaderc"
-pkgver = "2022.1"
+pkgver = "2022.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DSHADERC_SKIP_TESTS=ON", "-DSHADERC_SKIP_EXAMPLES=ON"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/google/shaderc"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "592c0ba07706a940f95a24073d7f3d050daa0839f24019434a7b2776e5620ace"
+sha256 = "517d36937c406858164673db696dc1d9c7be7ef0960fbf2965bfef768f46b8c0"
 tool_flags = {
     "CXXFLAGS": [f"-I{self.profile().sysroot / 'usr/include/glslang'}"]
 }
