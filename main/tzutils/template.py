1a564efa50258dea24ca2123e01391c0c2183429
Daniel Kolesa <q66@chimera-linux.org>
2022-08-25 23:32:34 +0200

    main/tzutils: update to 2022c

--- main/tzutils/template.py
+++ main/tzutils/template.py
@@ -1,5 +1,5 @@
 pkgname = "tzutils"
-pkgver = "2022a"
+pkgver = "2022c"
 pkgrel = 0
 build_style = "makefile"
 make_build_args = ["TZDIR=/usr/share/zoneinfo", "KSHELL=/bin/sh"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:none AND BSD-3-Clause"
 url = "http://www.iana.org/time-zones"
 source = f"https://github.com/eggert/tz/archive/{pkgver}.tar.gz"
-sha256 = "e9d82a851a15bb5db2cbaae2c3fc633743ad9edc069e3738c5e8908978064ed8"
+sha256 = "067f0f4bee8e509b3eca502c5bd7c8f98073ab49b6c05654a157cbffd07aa3a4"
 # needs network access
 options = ["!check"]
 
