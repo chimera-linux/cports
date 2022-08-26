597d6f6ae9dac303e8115a9d86e666732ab15984
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 02:48:47 +0200

    main/libuninameslist: update to 20220701

--- main/libuninameslist/template.py
+++ main/libuninameslist/template.py
@@ -1,17 +1,14 @@
 pkgname = "libuninameslist"
-pkgver = "20211114"
+pkgver = "20220701"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf", "automake", "libtool"]
+hostmakedepends = ["pkgconf"]
 pkgdesc = "Library of Unicode names and annotation data"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/fontforge/libuninameslist"
-source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "c089c6164f2cef361c3419a07408be72d6b58d6ef224ec226724a9fa93c0d46e"
-
-def pre_configure(self):
-    self.do("autoreconf", "-if")
+source = f"{url}/releases/download/{pkgver}/{pkgname}-dist-{pkgver}.tar.gz"
+sha256 = "3a59cbc626d653e3ab74802da809e56d72cce4017cf5677310c7599eae994cef"
 
 def post_install(self):
     self.install_license("LICENSE")
