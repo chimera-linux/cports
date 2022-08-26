daaa45a00f2cfc11c7157649dfacaa3b4cf4f276
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 04:15:25 +0200

    main/nano: update to 6.4

--- main/nano/template.py
+++ main/nano/template.py
@@ -1,5 +1,5 @@
 pkgname = "nano"
-pkgver = "6.2"
+pkgver = "6.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-utf8"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.nano-editor.org"
 source = f"https://www.nano-editor.org/dist/v{pkgver[0]}/nano-{pkgver}.tar.xz"
-sha256 = "2bca1804bead6aaf4ad791f756e4749bb55ed860eec105a97fba864bc6a77cb3"
+sha256 = "4199ae8ca78a7796de56de1a41b821dc47912c0307e9816b56cc317df34661c0"
 
 def post_install(self):
     self.install_file("syntax/nanorc.nanorc", "usr/share/examples/nano")
