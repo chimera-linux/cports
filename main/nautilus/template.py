a8cc5d410f6f770cff84db3d71d7579a570a5dd4
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 04:20:39 +0200

    main/nautilus: 42.2

--- main/nautilus/template.py
+++ main/nautilus/template.py
@@ -1,5 +1,5 @@
 pkgname = "nautilus"
-pkgver = "42.0"
+pkgver = "42.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dtests=headless"]
@@ -19,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Apps/Files"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "3c904f33ba2d2a078892bee2af71484cd608923cd78ec7e8a1517bc2155113d5"
+sha256 = "99212d2eb75996f181728ad04a2e2d86f2577b064e68a34c8b81a7037df4ccb2"
 options = ["!cross"]
 
 @subpackage("nautilus-devel")
