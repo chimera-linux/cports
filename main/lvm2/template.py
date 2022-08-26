e941b7175730d5d08aba2b1c1f301dd4890fc823
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 03:17:00 +0200

    main/lvm2: update to 2.03.16

--- main/lvm2/template.py
+++ main/lvm2/template.py
@@ -1,5 +1,5 @@
 pkgname = "lvm2"
-pkgver = "2.03.15"
+pkgver = "2.03.16"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -36,7 +36,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND LGPL-2.1-only"
 url = "https://sourceware.org/lvm2"
 source = f"https://mirrors.kernel.org/sourceware/{pkgname}/releases/LVM2.{pkgver}.tgz"
-sha256 = "935283a51ee17abd752a545a0ed1cf4edc993359265bc9e562edf81500edc99e"
+sha256 = "e661ece15b5d88d8abe39a4c1e1db2f43e1896f019948bb98b0e15d777680786"
 # the tests are full of scary gnuisms + don't work rootless
 options = ["!check"]
 # otherwise we're in for a world of pain
