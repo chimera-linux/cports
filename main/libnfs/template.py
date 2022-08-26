73fd033e64cfc82787c833c9ae6db5b2b6c82ded
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 02:13:29 +0200

    main/libnfs: update to 5.0.2

--- main/libnfs/template.py
+++ main/libnfs/template.py
@@ -1,5 +1,5 @@
 pkgname = "libnfs"
-pkgver = "5.0.1"
+pkgver = "5.0.2"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "libtool"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/sahlberg/libnfs"
 source = f"{url}/archive/{pkgname}-{pkgver}.tar.gz"
-sha256 = "7ef445410b42f36b9bad426608b53ccb9ccca4101e545c383f564c11db672ca8"
+sha256 = "637e56643b19da9fba98f06847788c4dad308b723156a64748041035dcdf9bd3"
 
 def pre_configure(self):
     self.do("autoreconf", "-if")
