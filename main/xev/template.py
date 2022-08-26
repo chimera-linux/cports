c6f2b8505f947a53af027f4a7e2c10757d1d3bc8
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 21:45:58 +0200

    main/xev: update to 1.2.5

--- main/xev/template.py
+++ main/xev/template.py
@@ -1,5 +1,5 @@
 pkgname = "xev"
-pkgver = "1.2.4"
+pkgver = "1.2.5"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "Display X events"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "d700e08bfe751ed2dbf802baa204b056d0e49348b6eb3c6f9cb035d8ae4885e2"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
+sha256 = "a948974ede621a8402ed9ea64f1ec83992285aa4fbb9d40b52985156c61a358a"
 
 def post_install(self):
     self.install_license("COPYING")
