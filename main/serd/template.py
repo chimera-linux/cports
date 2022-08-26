7fe39b4484862633b17cdc7e98b66be91464b0ef
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 03:05:50 +0200

    main/serd: update to 0.30.14

--- main/serd/template.py
+++ main/serd/template.py
@@ -1,16 +1,14 @@
 pkgname = "serd"
-pkgver = "0.30.10"
+pkgver = "0.30.14"
 pkgrel = 0
-build_style = "waf"
-configure_args = ["--test"]
-hostmakedepends = ["python", "pkgconf"]
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
 pkgdesc = "C library for RDF syntax"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "ISC"
 url = "https://drobilla.net/software/serd.html"
-source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "affa80deec78921f86335e6fc3f18b80aefecf424f6a5755e9f2fa0eb0710edf"
-options = ["!cross"]
+source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.xz"
+sha256 = "a14137d47b11d6ad431e78da341ca9737998d9eaccf6a49263d4c8d79fd856e3"
 
 def post_install(self):
     self.install_license("COPYING")
