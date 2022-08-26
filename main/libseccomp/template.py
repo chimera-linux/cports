8ca637b075c43be76871f230167bff4b1eb5b523
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 02:28:07 +0200

    main/libseccomp: update to 2.5.4

--- main/libseccomp/template.py
+++ main/libseccomp/template.py
@@ -1,5 +1,5 @@
 pkgname = "libseccomp"
-pkgver = "2.5.3"
+pkgver = "2.5.4"
 pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/seccomp/libseccomp"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "1e337ae9d8bab26641b17669a9871eaa10d4f3b474aaa4885d64b691a04614e3"
+sha256 = "96bbadb4384716272a6d2be82801dc564f7aab345febfe9b698b70fc606e3f75"
 # prevent a bunch of pain
 exec_wrappers = [
     ("/usr/bin/gsed", "sed")
