920d61b25db9d3cddfc27891f505b5b091bb1d03
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 02:17:07 +0200

    main/libnl: update to 3.7.0

--- main/libnl/template.py
+++ main/libnl/template.py
@@ -1,5 +1,5 @@
 pkgname = "libnl"
-pkgver = "3.5.0"
+pkgver = "3.7.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-static"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-only"
 url = "https://github.com/thom311/libnl"
 source = f"{url}/releases/download/libnl{pkgver.replace('.', '_')}/libnl-{pkgver}.tar.gz"
-sha256 = "352133ec9545da76f77e70ccb48c9d7e5324d67f6474744647a7ed382b5e05fa"
+sha256 = "9fe43ccbeeea72c653bdcf8c93332583135cda46a79507bfd0a483bb57f65939"
 
 @subpackage("libnl-devel")
 def _devel(self):
