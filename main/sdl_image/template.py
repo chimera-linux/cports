508132d6ce47d2d053be24313aeea095e1650fc8
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 20:06:10 +0200

    main/sdl_image: update to 2.6.2

--- main/sdl_image/template.py
+++ main/sdl_image/template.py
@@ -1,5 +1,5 @@
 pkgname = "sdl_image"
-pkgver = "2.0.5"
+pkgver = "2.6.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -7,21 +7,27 @@ configure_args = [
     "--disable-png-shared",
     "--disable-webp-shared",
     "--disable-tif-shared",
+    # defaulting to stb is stupid because the separate libraries are faster
+    # and better while being installed on pretty much every system anyway
+    "--disable-stb-image",
 ]
 make_cmd = "gmake"
 hostmakedepends = ["gmake", "pkgconf"]
-makedepends = ["libpng-devel", "libtiff-devel", "libwebp-devel", "sdl-devel"]
+makedepends = [
+    "libpng-devel", "libtiff-devel", "libwebp-devel",
+    "libavif-devel", "sdl-devel"
+]
 pkgdesc = "SDL image loading library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Zlib"
 url = "https://libsdl.org/projects/SDL_image"
 source = f"{url}/release/SDL2_image-{pkgver}.tar.gz"
-sha256 = "bdd5f6e026682f7d7e1be0b6051b209da2f402a2dd8bd1c4bd9c25ad263108d0"
+sha256 = "48355fb4d8d00bac639cd1c4f4a7661c4afef2c212af60b340e06b7059814777"
 # no check target
 options = ["!check"]
 
 def post_install(self):
-    self.install_license("COPYING.txt")
+    self.install_license("LICENSE.txt")
 
 @subpackage("sdl_image-devel")
 def _devel(self):
