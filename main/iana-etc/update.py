770eead1b2c4319a14f00a055ebe4da575b2a039
yopito <pierre.bourgin@free.fr>
2022-08-26 12:04:07 +0200

    main/iana-etc: fix update-check

--- main/iana-etc/update.py
+++ main/iana-etc/update.py
@@ -1,7 +1,6 @@
 url = "https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xml"
-_pver = self.template.pkgver
-pkgver = f"{_pver[0:4]}-{_pver[4:6]}-{_pver[6:]}"
 pattern = "<updated>([-\d]+)(?=</updated>)"
 
 def fetch_versions(self, src):
-    return map(lambda v: v.replace("-", ""), self.fetch_versions(src))
+    # keep only global last updated date
+    return map(lambda v: v.replace("-", ""), self.fetch_versions(src)[:1])
