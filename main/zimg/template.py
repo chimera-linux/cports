bb7d1ddbcff05bf2fcac6b9a8831709314ce90e1
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 22:08:54 +0200

    main/zimg: update to 3.0.4

--- main/zimg/template.py
+++ main/zimg/template.py
@@ -1,5 +1,5 @@
 pkgname = "zimg"
-pkgver = "3.0.3"
+pkgver = "3.0.4"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "libtool"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "WTFPL"
 url = "https://github.com/sekrit-twc/zimg"
 source = f"{url}/archive/release-{pkgver}.tar.gz"
-sha256 = "5e002992bfe8b9d2867fdc9266dc84faca46f0bfd931acc2ae0124972b6170a7"
+sha256 = "219d1bc6b7fde1355d72c9b406ebd730a4aed9c21da779660f0a4c851243e32f"
 
 def pre_configure(self):
     self.do(self.chroot_cwd / "autogen.sh")
