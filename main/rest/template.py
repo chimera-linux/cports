f8dd5c80b891603b41c7a95569f367a760a17aaf
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 05:09:31 +0200

    main/rest: update to 0.9.1

--- main/rest/template.py
+++ main/rest/template.py
@@ -1,23 +1,25 @@
 pkgname = "rest"
-pkgver = "0.9.0"
+pkgver = "0.9.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
-    "-Dca_certificates=true", "-Dsoup2=false", "-Dgtk_doc=false",
+    "-Dsoup2=false", "-Dgtk_doc=false", "-Dexamples=false",
+    "-Dca_certificates=true",
     "-Dca_certificates_path=/etc/ssl/certs/ca-certificates.crt"
 ]
 hostmakedepends = [
     "meson", "pkgconf", "gobject-introspection", "glib-devel", "vala",
+    "gettext-tiny",
 ]
 makedepends = [
-    "libglib-devel", "libsoup-devel", "json-glib-devel", "libxml2-devel"
+    "libglib-devel", "libsoup-devel", "json-glib-devel", "libxml2-devel",
 ]
 pkgdesc = "GNOME RESTful library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/librest"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "85b2bc9341128139539b53ee53f0533310bc96392fd645863a040410b81ebe66"
+sha256 = "9266a5c10ece383e193dfb7ffb07b509cc1f51521ab8dad76af96ed14212c2e3"
 
 @subpackage("rest-devel")
 def _devel(self):
