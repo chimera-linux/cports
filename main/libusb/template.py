74b71c79f4086a7a901ce5cee9083a593aa6bd45
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 02:49:34 +0200

    main/libusb: update to 1.0.26

--- main/libusb/template.py
+++ main/libusb/template.py
@@ -1,5 +1,5 @@
 pkgname = "libusb"
-pkgver = "1.0.25"
+pkgver = "1.0.26"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://libusb.info"
 source = f"https://github.com/{pkgname}/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "8a28ef197a797ebac2702f095e81975e2b02b2eeff2774fa909c78a74ef50849"
+sha256 = "12ce7a61fc9854d1d2a1ffe095f7b5fac19ddba095c259e6067a46500381b5a5"
 
 @subpackage("libusb-devel")
 def _devel(self):
