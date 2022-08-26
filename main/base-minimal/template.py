077f0839fc32d2d121ff7637b7b6a7da387b4770
Daniel Kolesa <q66@chimera-linux.org>
2022-08-27 01:46:34 +0200

    main/base-minimal: add chimera-repo-main dependency

--- main/base-minimal/template.py
+++ main/base-minimal/template.py
@@ -5,7 +5,7 @@ build_style = "meta"
 depends = [
     "chimerautils", "base-shells", "apk-tools", "awk",
     "bsdtar", "util-linux", "shadow", "procps-ng",
-    "iana-etc", "tzdata", "dinit-chimera"
+    "iana-etc", "tzdata", "dinit-chimera", "chimera-repo-main",
 ]
 pkgdesc = "Minimal set of packages for a Chimera system"
 maintainer = "q66 <q66@chimera-linux.org>"
