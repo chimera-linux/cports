3c8173e8a85f24af5c912856880910e5258db5ef
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 05:48:53 +0200

    main/sbc: update to 2.0

--- main/sbc/template.py
+++ main/sbc/template.py
@@ -1,5 +1,5 @@
 pkgname = "sbc"
-pkgver = "1.5"
+pkgver = "2.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-pie"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "http://www.bluez.org"
 source = f"$(KERNEL_SITE)/bluetooth/{pkgname}-{pkgver}.tar.xz"
-sha256 = "0cbad69823a99e8421fe0700e8cf9eeb8fa0c1ad28e8dbc2182b3353507931d2"
+sha256 = "8f12368e1dbbf55e14536520473cfb338c84b392939cc9b64298360fd4a07992"
 
 @subpackage("sbc-devel")
 def _devel(self):
