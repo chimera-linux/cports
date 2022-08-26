ea6bf806afe73714435dc3219a0a6197b1f53735
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 03:09:47 +0200

    main/sratom: update to 0.6.12

--- main/sratom/template.py
+++ main/sratom/template.py
@@ -1,17 +1,15 @@
 pkgname = "sratom"
-pkgver = "0.6.8"
+pkgver = "0.6.12"
 pkgrel = 0
-build_style = "waf"
-configure_args = ["--test"]
-hostmakedepends = ["python", "pkgconf"]
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
 makedepends = ["serd-devel", "sord-devel", "lv2"]
 pkgdesc = "Library for serializing LV2 atoms to/from RDF"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "ISC"
 url = "https://drobilla.net/software/sratom.html"
-source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "3acb32b1adc5a2b7facdade2e0818bcd6c71f23f84a1ebc17815bb7a0d2d02df"
-options = ["!cross"]
+source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.xz"
+sha256 = "349933ce75ee4b467f0d620defa5b2139a2194c16dbf11a837b5fa800c1a0c83"
 
 def post_install(self):
     self.install_license("COPYING")
