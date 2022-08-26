0bd6ce195734e4adb13e318b8b4acd103368bd3e
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 05:08:13 +0200

    main/powerpc-utils: update to 1.3.10

--- main/powerpc-utils/template.py
+++ main/powerpc-utils/template.py
@@ -1,5 +1,5 @@
 pkgname = "powerpc-utils"
-pkgver = "1.3.9"
+pkgver = "1.3.10"
 pkgrel = 0
 archs = ["ppc*"]
 build_style = "gnu_configure"
@@ -18,7 +18,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/ibm-power-utilities/powerpc-utils"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "afe5fd7ebabe50a629f3a1efa79afffdc1f7cfaf0c82407490f20b2073732f9a"
+sha256 = "d64d9016a3e63a1e44c6e0833742cf964ae6bb1c6a9c7f0c7c5748aa335dc3db"
 # too many places that use PATH_MAX without limits.h include
 tool_flags = {"CFLAGS": ["-DPATH_MAX=4096"]}
 
