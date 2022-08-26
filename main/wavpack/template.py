e38b23fa6b7213694d6b9f68c7e1c082dfdfa706
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 21:17:44 +0200

    main/wavpack: update to 5.5.0

--- main/wavpack/template.py
+++ main/wavpack/template.py
@@ -1,5 +1,5 @@
 pkgname = "wavpack"
-pkgver = "5.4.0"
+pkgver = "5.5.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://www.wavpack.com"
 source = f"https://github.com/dbry/WavPack/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "4bde6a6b2a86614a6bd2579e60dcc974e2c8f93608d2281110a717c1b3c28b79"
+sha256 = "ef749d98df46925bc2916993e601cc7ee9114d99653e63e0e304f031ba73b8e6"
 
 def post_install(self):
     self.install_license("COPYING")
