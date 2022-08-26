bbb37f44dfb7d9c42f47552f57df6565af4e05a5
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 04:37:27 +0200

    main/openal-soft: update to 1.22.2

--- main/openal-soft/template.py
+++ main/openal-soft/template.py
@@ -1,19 +1,19 @@
 pkgname = "openal-soft"
-pkgver = "1.21.1"
+pkgver = "1.22.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DALSOFT_EXAMPLES=OFF"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = [
-    "zlib-devel", "libpulse-devel", "pipewire-jack-devel", "sdl-devel",
-    "ffmpeg-devel",
+    "zlib-devel", "libpulse-devel", "pipewire-devel", "pipewire-jack-devel",
+    "sdl-devel", "ffmpeg-devel",
 ]
 pkgdesc = "Cross-platform 3D audio API"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://openal-soft.org"
 source = f"{url}/openal-releases/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "c8ad767e9a3230df66756a21cc8ebf218a9d47288f2514014832204e666af5d8"
+sha256 = "ae94cc95cda76b7cc6e92e38c2531af82148e76d3d88ce996e2928a1ea7c3d20"
 # no test target
 options = ["!check"]
 
