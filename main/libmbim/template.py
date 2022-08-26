6c5c8bb470f45b2be358e7cf98e3c27d5a30ac2d
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 02:11:55 +0200

    main/libmbim: update to 1.26.4

--- main/libmbim/template.py
+++ main/libmbim/template.py
@@ -1,5 +1,5 @@
 pkgname = "libmbim"
-pkgver = "1.26.2"
+pkgver = "1.26.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-static", "--enable-introspection"]
@@ -13,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://www.freedesktop.org/wiki/Software/libmbim"
 source = f"$(FREEDESKTOP_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "10c77bf5b5eb8c92ba80e9b519923ad9b898362bc8e1928e2bc9a17eeba649af"
+sha256 = "f688cec4c4586a17575f5e327448ce62f2000ef6a07c9e4589873d4a68568ad9"
 
 @subpackage("libmbim-devel")
 def _devel(self):
