f7ca03250447178a4d95c05285e0a25e4928bc75
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 02:38:43 +0200

    main/libspiro: update to 20220722

--- main/libspiro/template.py
+++ main/libspiro/template.py
@@ -1,17 +1,14 @@
 pkgname = "libspiro"
-pkgver = "20200505"
+pkgver = "20220722"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf", "automake", "libtool"]
+hostmakedepends = ["pkgconf"]
 pkgdesc = "Simplifies the drawing of beautiful curves"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/fontforge/libspiro"
-source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "00be530b5c0ea9274baadf6c05521f0b192d4c3c1db636ac8b08efd44aaea8f5"
-
-def pre_configure(self):
-    self.do("autoreconf", "-if")
+source = f"{url}/releases/download/{pkgver}/{pkgname}-dist-{pkgver}.tar.gz"
+sha256 = "3b8e54473f3d4d99c014f2630e62f966f5f4e25c28ca59b63d30bd8e9b7593f5"
 
 @subpackage("libspiro-devel")
 def _devel(self):
