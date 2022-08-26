11ea5c37e1e3e66e289361b234282fdcf1675d46
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 02:12:49 +0200

    main/libmtp: update to 1.1.20

--- main/libmtp/template.py
+++ main/libmtp/template.py
@@ -1,5 +1,5 @@
 pkgname = "libmtp"
-pkgver = "1.1.19"
+pkgver = "1.1.20"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-udev=/usr/lib/udev", "--with-udev-group=plugdev"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "http://libmtp.sourceforge.net"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "deb4af6f63f5e71215cfa7fb961795262920b4ec6cb4b627f55b30b18aa33228"
+sha256 = "c9191dac2f5744cf402e08641610b271f73ac21a3c802734ec2cedb2c6bc56d0"
 options = ["!cross"]
 
 @subpackage("libmtp-devel")
