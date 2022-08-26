c40fb2d80dcf2b6c09de9588f536a4a5e3082192
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 02:54:22 +0200

    main/libxcb: update to 1.15

--- main/libxcb/template.py
+++ main/libxcb/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxcb"
-pkgver = "1.14"
+pkgver = "1.15"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-build-docs", "--enable-xinput", "--enable-xkb"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xcb.freedesktop.org"
 source = f"{url}/dist/{pkgname}-{pkgver}.tar.xz"
-sha256 = "a55ed6db98d43469801262d81dc2572ed124edc3db31059d4e9916eb9f844c34"
+sha256 = "cc38744f817cf6814c847e2df37fcb8997357d72fa4bcbc228ae0fe47219a059"
 
 def post_install(self):
     self.install_license("COPYING")
