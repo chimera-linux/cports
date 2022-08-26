0c1b0faceddd80b78a648c014b82b0bf4b66c290
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 21:51:30 +0200

    main/xrefresh: update to 1.0.7

--- main/xrefresh/template.py
+++ main/xrefresh/template.py
@@ -1,5 +1,5 @@
 pkgname = "xrefresh"
-pkgver = "1.0.6"
+pkgver = "1.0.7"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "Refresh all or a part of an X screen"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "287dfb9bb7e8d780d07e672e3252150850869cb550958ed5f8401f0835cd6353"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
+sha256 = "f2817920f119bd9146ed3cde223b8a4ab17cb72da4ece7bddde35e18b31aa337"
 
 def post_install(self):
     self.install_license("COPYING")
