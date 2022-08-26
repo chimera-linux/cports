66e313b44b8f6928a59e28242de7e80c636e5714
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 21:50:50 +0200

    main/xpr: update to 1.1.0

--- main/xpr/template.py
+++ main/xpr/template.py
@@ -1,5 +1,5 @@
 pkgname = "xpr"
-pkgver = "1.0.5"
+pkgver = "1.1.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "Print an X window dump"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "b936e0f1f8d63f5ad097a0bbdce2e67c34f5d49b7263f32a832ff62d394d1e1f"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
+sha256 = "fabd02fb1a52358d521f1be7422738bc8c9b511a8d82a163888f628db6f6cb18"
 
 def post_install(self):
     self.install_license("COPYING")
