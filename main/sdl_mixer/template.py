6445f7c40793d738d6a2e098d03bb1d96a1bc6b8
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 20:12:54 +0200

    main/sdl_mixer: update to 2.6.2

--- main/sdl_mixer/template.py
+++ main/sdl_mixer/template.py
@@ -1,14 +1,20 @@
 pkgname = "sdl_mixer"
-pkgver = "2.0.4"
+pkgver = "2.6.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
+    # use external libraries, disable bundled crap
+    "--enable-music-flac-libflac",
+    "--enable-music-mp3-mpg123",
+    "--enable-music-ogg-vorbis",
+    "--disable-music-flac-drflac",
+    "--disable-music-mp3-drmp3",
+    "--disable-music-ogg-stb",
     "--disable-music-mod-modplug-shared",
-    "--disable-music-mod-mikmod-shared",
     "--disable-music-midi-fluidsynth-shared",
     "--disable-music-mp3-mpg123-shared",
-    "--disable-music-ogg-shared",
-    "--disable-music-flac-shared",
+    "--disable-music-ogg-vorbis-shared",
+    "--disable-music-flac-libflac-shared",
     "--disable-music-opus-shared",
     "--disable-music-midi-timidity",
 ]
@@ -19,7 +25,6 @@ makedepends = [
     "flac-devel",
     "libvorbis-devel",
     "opusfile-devel",
-    "libmikmod-devel",
     "libmodplug-devel",
     "fluidsynth-devel",
     "smpeg-devel",
@@ -30,12 +35,12 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Zlib"
 url = "https://libsdl.org/projects/SDL_mixer"
 source = f"{url}/release/SDL2_mixer-{pkgver}.tar.gz"
-sha256 = "b4cf5a382c061cd75081cf246c2aa2f9df8db04bdda8dcdc6b6cca55bede2419"
+sha256 = "8cdea810366decba3c33d32b8071bccd1c309b2499a54946d92b48e6922aa371"
 # no check target
 options = ["!check"]
 
 def post_install(self):
-    self.install_license("COPYING.txt")
+    self.install_license("LICENSE.txt")
 
 @subpackage("sdl_mixer-devel")
 def _devel(self):
