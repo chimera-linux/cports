eb5f11c8391ff017dc3515cb41852b8343d41917
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 04:25:49 +0200

    main/nghttp2: update to 1.49.0

--- main/nghttp2/template.py
+++ main/nghttp2/template.py
@@ -1,5 +1,5 @@
 pkgname = "nghttp2"
-pkgver = "1.47.0"
+pkgver = "1.49.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-python-bindings"]
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://nghttp2.org"
 source = f"https://github.com/tatsuhiro-t/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "68271951324554c34501b85190f22f2221056db69f493afc3bbac8e7be21e7cc"
+sha256 = "b0cfd492bbf0b131c472e8f6501c9f4ee82b51b68130f47b278c0b7c9848a66e"
 
 def post_install(self):
     self.install_license("COPYING")
