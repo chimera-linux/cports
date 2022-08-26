e3e08e08617b7863c5d5747f072b90ca90bf983c
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 02:06:32 +0200

    main/libgsf: update to 1.14.50

--- main/libgsf/template.py
+++ main/libgsf/template.py
@@ -1,5 +1,5 @@
 pkgname = "libgsf"
-pkgver = "1.14.49"
+pkgver = "1.14.50"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-introspection"]
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libgsf"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-3]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "e9ebe36688f010c9e6e40c8903f3732948deb8aca032578d07d0751bd82cf857"
+sha256 = "6e6c20d0778339069d583c0d63759d297e817ea10d0d897ebbe965f16e2e8e52"
 
 @subpackage("libgsf-devel")
 def _devel(self):
