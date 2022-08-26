d6bdcc6dd0c1377bc885a038ec6021e3fd09019a
Daniel Kolesa <q66@chimera-linux.org>
2022-08-26 03:06:56 +0200

    main/sord: update to 0.16.12

--- main/sord/template.py
+++ main/sord/template.py
@@ -1,17 +1,15 @@
 pkgname = "sord"
-pkgver = "0.16.8"
+pkgver = "0.16.12"
 pkgrel = 0
-build_style = "waf"
-configure_args = ["--test"]
-hostmakedepends = ["python", "pkgconf"]
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
 makedepends = ["serd-devel", "pcre-devel"]
 pkgdesc = "C library for storing RDF data in memory"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "ISC"
 url = "https://drobilla.net/software/sord.html"
-source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "7c289d2eaabf82fa6ac219107ce632d704672dcfb966e1a7ff0bbc4ce93f5e14"
-options = ["!cross"]
+source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.xz"
+sha256 = "fde269893cb24b2ab7b75708d7a349c6e760c47a0d967aeca5b1c651294ff9f2"
 
 def post_install(self):
     self.install_license("COPYING")
