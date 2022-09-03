ae8bacbeca991e27326bfc0649ae88f21745be2f
Daniel Kolesa <q66@chimera-linux.org>
2022-09-03 14:20:25 +0200

    main/exempi: regenerate autotools to fix up aarch64

--- main/exempi/template.py
+++ main/exempi/template.py
@@ -3,7 +3,7 @@ pkgver = "2.6.2"
 pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
-hostmakedepends = ["pkgconf", "gmake"]
+hostmakedepends = ["pkgconf", "gmake", "automake", "libtool"]
 makedepends = ["boost-devel", "libexpat-devel", "zlib-devel"]
 pkgdesc = "Library for easy parsing of XMP metadata"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -11,6 +11,10 @@ license = "BSD-3-Clause"
 url = "https://libopenraw.freedesktop.org/exempi"
 source = f"https://libopenraw.freedesktop.org/download/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "4d17d4c93df2a95da3e3172c45b7a5bf317dd31dafd1c7a340169728c7089d1d"
+
+# fix up libtool crap for aarch64
+def pre_configure(self):
+    self.do("autoreconf", "-if")
 
 def post_install(self):
     self.install_license("COPYING")
