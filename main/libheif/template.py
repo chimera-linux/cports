c9d8d59ac28eb1dee6c72099efb7a33b4fd26725
Daniel Kolesa <q66@chimera-linux.org>
2022-09-03 11:55:49 +0200

    main/libheif, main/libde265: drop aarch64 atomics workaround
    This was really libtool's fault, and  we now patched libtool.
    Both projects generate their autotools stuff during build.

--- main/libheif/template.py
+++ main/libheif/template.py
@@ -15,14 +15,6 @@ url = "http://www.libheif.org"
 source = f"https://github.com/strukturag/{pkgname}/archive/v{pkgver}.tar.gz"
 sha256 = "086145b0d990182a033b0011caadb1b642da84f39ab83aa66d005610650b3c65"
 
-if self.profile().arch == "aarch64":
-    # fails to resolve some symbols from compiler-rt builtins
-    # e.g. __aarch64_ldadd8_acq_rel
-    tool_flags = {
-        "CFLAGS": ["-mno-outline-atomics"],
-        "CXXFLAGS": ["-mno-outline-atomics"]
-    }
-
 def pre_configure(self):
     self.do(self.chroot_cwd / "autogen.sh")
 
