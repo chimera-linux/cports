85480f45e85649682a03bdc698fe13dc23a11555
q66 <daniel@octaforge.org>
2022-09-03 23:43:06 +0000

    main/linux-pbp: new package (5.15.64)
    This is a kernel for the Pinebook Pro. It is essentially vanilla
    5.15 but with a specialized dotconfig and a bunch of usability
    patches on top. Similar boards such as RockPro64 should be usable
    with the vanilla kernel.

---
+++ main/linux-pbp/template.py
@@ -0,0 +1,62 @@
+pkgname = "linux-pbp"
+pkgver = "5.15.64"
+pkgrel = 0
+archs = ["aarch64"]
+make_dir = "build"
+hostmakedepends = ["base-kernel-devel"]
+depends = ["base-kernel"]
+pkgdesc = "Linux kernel for Pinebook Pro (5.15.x)"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-only"
+url = "https://kernel.org"
+source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
+sha256 = "c6a1d38c6fa3798341372d5cf0088ae806ccdc827e31ecbff8988e097ba5de50"
+# no meaningful checking to be done
+options = [
+    "!check", "!debug", "!strip", "!scanrundeps", "!scanshlibs",
+    "!lto", "textrels", "foreignelf" # vdso32
+]
+
+if self.profile().cross:
+    broken = "linux-devel does not come out right"
+
+def do_configure(self):
+    self.cp(self.files_path / "config", self.cwd)
+
+    epoch = self.source_date_epoch or 0
+    args = []
+
+    if self.profile().cross:
+        args += [f"CROSS_COMPILE={self.profile().triplet}"]
+
+    self.do(
+        "chimera-buildkernel",
+        "prepare",
+        f"ARCH=arm64",
+        f"CONFIG_FILE={self.chroot_cwd}/config",
+        f"OBJDIR={self.make_dir}",
+        f"JOBS={self.make_jobs}",
+        f"LOCALVERSION=-{pkgrel}-pbp",
+        f"EPOCH={epoch}",
+        *args
+    )
+
+def do_build(self):
+    self.do("chimera-buildkernel", "build")
+
+def do_install(self):
+    self.do("chimera-buildkernel", "install", self.chroot_destdir)
+
+@subpackage("linux-pbp-devel")
+def _devel(self):
+    self.depends += ["clang"]
+    self.options = ["foreignelf", "!scanshlibs"]
+    return ["usr/src", "usr/lib/modules/*/build"]
+
+@subpackage("linux-pbp-dbg")
+def _dbg(self):
+    self.pkgdesc += " (debug files)"
+    self.options = [
+        "!scanrundeps", "!strip", "!scanshlibs", "foreignelf", "textrels"
+    ]
+    return ["usr/lib/debug", "boot/System.map-*"]
