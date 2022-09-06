63a64ef4e9bd101eab176ef393d67b0d993adf50
q66 <daniel@octaforge.org>
2022-09-06 20:38:50 +0000

    main/base-rpi: new package (0.1)

---
+++ main/base-rpi/template.py
@@ -0,0 +1,18 @@
+pkgname = "base-rpi"
+pkgver = "0.1"
+pkgrel = 0
+archs = ["aarch64"]
+depends = ["firmware-rpi", "linux-rpi", "base-kernel"]
+pkgdesc = "Chimera base package for Raspberry Pi devices"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:none"
+url = "https://chimera-linux.org"
+
+def do_install(self):
+    self.install_file(
+        self.files_path / "71-raspberrypi.rules", "usr/lib/udev/rules.d"
+    )
+    # kernel hook
+    self.install_file(
+        self.files_path / "99-rpi-kernel.sh", "etc/kernel.d", mode = 0o755
+    )
