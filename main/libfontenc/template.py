4d464206e2763fc4a8a1faf56a1a95524d877a5b
Daniel Kolesa <q66@chimera-linux.org>
2022-09-11 22:54:40 +0200

    main/libfontenc: update to 1.1.6

--- main/libfontenc/template.py
+++ main/libfontenc/template.py
@@ -1,5 +1,5 @@
 pkgname = "libfontenc"
-pkgver = "1.1.4"
+pkgver = "1.1.6"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-encodingsdir=/usr/share/fonts/encodings"]
@@ -9,8 +9,8 @@ pkgdesc = "Fontenc Library from X.org"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "2cfcce810ddd48f2e5dc658d28c1808e86dcf303eaff16728b9aa3dbc0092079"
+source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.gz"
+sha256 = "c103543a47ce5c0200fb1867f32df5e754a7c3ef575bf1fe72187117eac22a53"
 
 def post_install(self):
     self.install_license("COPYING")
