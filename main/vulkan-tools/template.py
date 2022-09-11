7611b310305f258791c80242382d4c5551ee58c8
Daniel Kolesa <q66@chimera-linux.org>
2022-09-11 23:35:11 +0200

    main/vulkan-tools: update to 1.3.227

--- main/vulkan-tools/template.py
+++ main/vulkan-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-tools"
-pkgver = "1.3.225"
+pkgver = "1.3.227"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -17,6 +17,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://www.khronos.org/vulkan"
 source = f"https://github.com/KhronosGroup/Vulkan-Tools/archive/v{pkgver}.tar.gz"
-sha256 = "f30dfedb2a5630b981ee93627f4ec6be9cef95d6b8527340437207276bd0d18e"
+sha256 = "dbba74f6a4b3a4288276543ab692dd3a8298d9e37ed6c5f594e4ea1717052920"
 # no test suite
 options = ["!cross", "!check"]
