ee17a97361cdb677c6afdcab6a086f6911ecb643
Daniel Kolesa <q66@chimera-linux.org>
2022-09-11 23:12:18 +0200

    main/libxft: update to 2.3.6

--- main/libxft/template.py
+++ main/libxft/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxft"
-pkgver = "2.3.4"
+pkgver = "2.3.6"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -10,8 +10,8 @@ pkgdesc = "X font library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libXft-{pkgver}.tar.bz2"
-sha256 = "57dedaab20914002146bdae0cb0c769ba3f75214c4c91bd2613d6ef79fc9abdd"
+source = f"$(XORG_SITE)/lib/libXft-{pkgver}.tar.gz"
+sha256 = "b7e59f69e0bbabe9438088775f7e5a7c16a572e58b11f9722519385d38192df5"
 
 def post_install(self):
     self.install_license("COPYING")
