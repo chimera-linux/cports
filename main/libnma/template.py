65123b6218472c8b1191ff662c700409c15498d9
Daniel Kolesa <q66@chimera-linux.org>
2022-09-11 23:03:02 +0200

    main/libnma: update to 1.10.2

--- main/libnma/template.py
+++ main/libnma/template.py
@@ -1,5 +1,5 @@
 pkgname = "libnma"
-pkgver = "1.8.40"
+pkgver = "1.10.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -19,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libnma"
 source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "ff205dca5cfde13d7b749cff041b3764ae7c6195a4bc68fc9d28d168baf698e2"
+sha256 = "8357965caf9c267230af851aad069bac2d5de9ca9bc6051401ddbbdb741523b5"
 options = ["!cross"]
 
 @subpackage("libnma-devel")
