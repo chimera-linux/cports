455487791201fea288aa078e904d65413ad2b2b8
Daniel Kolesa <q66@chimera-linux.org>
2022-09-11 22:40:38 +0200

    main/less: update to 608

--- main/less/template.py
+++ main/less/template.py
@@ -1,5 +1,5 @@
 pkgname = "less"
-pkgver = "590"
+pkgver = "608"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-regex=posix"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:less OR GPL-3.0-or-later"
 url = "http://www.greenwoodsoftware.com/less"
 source = f"http://www.greenwoodsoftware.com/less/less-{pkgver}.tar.gz"
-sha256 = "6aadf54be8bf57d0e2999a3c5d67b1de63808bb90deb8f77b028eafae3a08e10"
+sha256 = "a69abe2e0a126777e021d3b73aa3222e1b261f10e64624d41ec079685a6ac209"
 
 def post_install(self):
     self.install_file(self.files_path / "lesspipe.sh", "usr/bin", mode = 0o755)
