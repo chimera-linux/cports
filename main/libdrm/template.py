028d9de3acf366779f793c7fe9346ed01ff3f1be
Daniel Kolesa <q66@chimera-linux.org>
2022-09-11 22:53:34 +0200

    main/libdrm: update to 2.4.113

--- main/libdrm/template.py
+++ main/libdrm/template.py
@@ -1,10 +1,10 @@
 pkgname = "libdrm"
-pkgver = "2.4.112"
+pkgver = "2.4.113"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dudev=true",
-    "-Dvalgrind=false"
+    "-Dvalgrind=disabled"
 ]
 hostmakedepends = ["meson", "pkgconf"]
 makedepends = ["eudev-devel", "libpciaccess-devel", "linux-headers"]
@@ -13,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://dri.freedesktop.org"
 source = f"https://gitlab.freedesktop.org/mesa/drm/-/archive/{pkgname}-{pkgver}/drm-{pkgname}-{pkgver}.tar.gz"
-sha256 = "d9d38a989e22c8c6aad6ecc27db25de2fe685bf0e2c27855f8135ea4ca1cdddb"
+sha256 = "f2969eaca119766485ce7e274ffab73a3d30d74bd98ce7855d7478c6fba765ec"
 
 # stuff like radeon autodetects, most arm stuff is by default false
 match self.profile().arch:
