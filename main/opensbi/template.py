e1ef104a577bbbf7aa163a2f0d97aed3291c44b3
Daniel Kolesa <q66@chimera-linux.org>
2022-09-29 23:43:49 +0200

    main/opensbi: strip binaries, disable payload

--- main/opensbi/template.py
+++ main/opensbi/template.py
@@ -3,7 +3,9 @@ pkgver = "1.1"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
-make_build_args = ["PLATFORM=generic", "PLATFORM_RISCV_XLEN=64", "LLVM=1"]
+make_build_args = [
+    "PLATFORM=generic", "FW_PAYLOAD=n", "PLATFORM_RISCV_XLEN=64", "LLVM=1"
+]
 make_use_env = True
 hostmakedepends = ["gmake", "bash"]
 pkgdesc = "RISC-V Open Source Supervisor Binary Interface"
@@ -13,13 +15,19 @@ url = "https://github.com/riscv-software-src/opensbi"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "d183cb890130983a4f01e75fc03ee4f7ea0e16a7923b8af9c6dff7deb2fedaec"
 # no test suite
-options = ["!check"]
+options = ["!check", "!lto", "!strip", "!debug", "foreignelf"]
 
 def do_install(self):
     instp = "build/platform/generic/firmware"
     destp = "usr/lib/opensbi/generic"
     for f in ["dynamic", "jump"]:
-        self.install_file(f"{instp}/fw_{f}.bin", destp)
-        self.install_file(f"{instp}/fw_{f}.elf", destp)
+        self.install_file(f"{instp}/fw_{f}.bin", destp, mode = 0o644)
+        self.install_file(f"{instp}/fw_{f}.elf", destp, mode = 0o755)
+        self.do(
+            "/usr/bin/llvm-strip",
+            "--remove-section=.comment",
+            "--remove-section=.note",
+            self.chroot_destdir / destp / f"fw_{f}.elf"
+        )
 
     self.install_license("COPYING.BSD")
