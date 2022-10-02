f828abddc55f58bd7035030e4e061851662b05c4
Daniel Kolesa <q66@chimera-linux.org>
2022-10-02 23:39:31 +0200

    main/startup-notification: disable tests
    The test code is broken.

--- main/startup-notification/template.py
+++ main/startup-notification/template.py
@@ -11,6 +11,8 @@ license = "LGPL-2.1-only"
 url = "https://www.freedesktop.org/wiki/Software/startup-notification"
 source = f"$(FREEDESKTOP_SITE)/{pkgname}/releases/{pkgname}-{pkgver}.tar.gz"
 sha256 = "3c391f7e930c583095045cd2d10eb73a64f085c7fde9d260f2652c7cb3cfbe4a"
+# the unit test code is broken (passing char * to int args)
+options = ["!check"]
 
 @subpackage("startup-notification-devel")
 def _devel(self):
