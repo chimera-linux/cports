00a8fe5bfde88bc1b7fbe161f424647f81b5d2e3
Daniel Kolesa <q66@chimera-linux.org>
2022-10-05 11:24:00 +0200

    contrib/u-boot-qemu-riscv64_smode: new package (2022.10)

---
+++ contrib/u-boot-qemu-riscv64_smode/template.py
@@ -0,0 +1,20 @@
+pkgname = "u-boot-qemu-riscv64_smode"
+pkgver = "2022.10"
+pkgrel = 0
+build_style = "u_boot"
+hostmakedepends = [
+    "gmake", "gcc-riscv64-unknown-elf", "flex", "bison",
+    "dtc", "python", "openssl-devel"
+]
+pkgdesc = "U-Boot for qemu-riscv64 supervisor mode"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-only AND BSD-3-Clause"
+url = "https://www.denx.de/wiki/U-Boot"
+source = f"https://ftp.denx.de/pub/u-boot/u-boot-{pkgver}.tar.bz2"
+sha256 = "50b4482a505bc281ba8470c399a3c26e145e29b23500bc35c50debd7fa46bdf8"
+env = {
+    "U_BOOT_TRIPLET": "riscv64-unknown-elf",
+    "U_BOOT_TARGETS": "u-boot",
+}
+# not relevant
+options = ["!strip", "!check", "!lto", "!debug", "foreignelf"]
