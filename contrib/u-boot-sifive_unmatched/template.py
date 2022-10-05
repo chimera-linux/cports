b83595a5410a9faa1b44f7d8c525d5f6ce562d75
Daniel Kolesa <q66@chimera-linux.org>
2022-10-05 11:55:19 +0200

    contrib/u-boot-sifive_unmatched: fix build

--- contrib/u-boot-sifive_unmatched/template.py
+++ contrib/u-boot-sifive_unmatched/template.py
@@ -3,9 +3,10 @@ pkgver = "2022.10"
 pkgrel = 0
 archs = ["riscv64"]
 build_style = "u_boot"
+make_build_args = ["OPENSBI=/usr/lib/opensbi/generic/fw_dynamic.bin"]
 hostmakedepends = [
-    "gmake", "gcc-riscv64-unknown-elf", "flex", "bison",
-    "dtc", "python", "openssl-devel"
+    "gmake", "gcc-riscv64-unknown-elf", "flex", "bison", "dtc", "swig",
+    "opensbi", "python-devel", "openssl-devel", "python-setuptools",
 ]
 pkgdesc = "U-Boot for HiFive Unmatched boards"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -15,7 +16,7 @@ source = f"https://ftp.denx.de/pub/u-boot/u-boot-{pkgver}.tar.bz2"
 sha256 = "50b4482a505bc281ba8470c399a3c26e145e29b23500bc35c50debd7fa46bdf8"
 env = {
     "U_BOOT_TRIPLET": "riscv64-unknown-elf",
-    "U_BOOT_TARGETS": "u-boot",
+    "U_BOOT_TARGETS": "spl/u-boot-spl.bin u-boot.itb",
 }
 # not relevant
 options = ["!strip", "!check", "!lto", "!debug", "foreignelf"]
