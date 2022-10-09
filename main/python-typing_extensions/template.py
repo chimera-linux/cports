3be2fd5bcd46703120e9bc4c9a5e378d2c86ec23
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 01:49:20 +0200

    main/python-typing_extensions: update to 4.4.0

--- main/python-typing_extensions/template.py
+++ main/python-typing_extensions/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-typing_extensions"
-pkgver = "4.3.0"
+pkgver = "4.4.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-pip", "python-flit_core"]
@@ -9,6 +9,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Python-2.0"
 url = "https://github.com/python/typing"
 source = f"$(PYPI_SITE)/t/typing_extensions/typing_extensions-{pkgver}.tar.gz"
-sha256 = "e6d2677a32f47fc7eb2795db1dd15c1f34eff616bcaf2cfb5e997f854fa1c4a6"
+sha256 = "1511434bb92bf8dd198c12b1cc812e800d4181cfcb867674e0f8279cc93087aa"
 # in early path
 options = ["!check"]
