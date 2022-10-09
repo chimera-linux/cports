b34b2b6f07fabd579cb1eb94538db49cc9800681
Daniel Kolesa <q66@chimera-linux.org>
2022-10-09 23:10:56 +0200

    main/opencl-headers: update to 2022.09.30

--- main/opencl-headers/template.py
+++ main/opencl-headers/template.py
@@ -1,12 +1,12 @@
 pkgname = "opencl-headers"
-pkgver = "2022.05.18"
+pkgver = "2022.09.30"
 pkgrel = 0
 pkgdesc = "OpenCL header files"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/KhronosGroup/OpenCL-Headers"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "88a1177853b279eaf574e2aafad26a84be1a6f615ab1b00c20d5af2ace95c42e"
+sha256 = "0ae857ecb28af95a420c800b21ed2d0f437503e104f841ab8db249df5f4fbe5c"
 # no test suite
 options = ["!check"]
 
