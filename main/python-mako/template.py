3c2a6af685d4c44c445ecb18f4d69ccd4b64ed90
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 01:52:09 +0200

    main/python-mako: update to 1.2.3

--- main/python-mako/template.py
+++ main/python-mako/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-mako"
-pkgver = "1.2.2"
+pkgver = "1.2.3"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.makotemplates.org"
 source = f"$(PYPI_SITE)/M/Mako/Mako-{pkgver}.tar.gz"
-sha256 = "3724869b363ba630a272a5f89f68c070352137b8fd1757650017b7e06fda163f"
+sha256 = "7fde96466fcfeedb0eed94f187f20b23d85e4cb41444be0e542e2c8c65c396cd"
 # tests failing with 3.10 for now, should be harmless
 options = ["!check"]
 
