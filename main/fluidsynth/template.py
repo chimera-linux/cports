1ceee84b37f056a7733d04c2fc27112a6165d6f2
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 00:28:19 +0200

    main/fluidsynth: update to 2.3.0

--- main/fluidsynth/template.py
+++ main/fluidsynth/template.py
@@ -1,5 +1,5 @@
 pkgname = "fluidsynth"
-pkgver = "2.2.9"
+pkgver = "2.3.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -8,15 +8,15 @@ configure_args = [
 make_check_target = "check"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = [
-    "glib-devel", "pipewire-jack-devel", "libpulse-devel",
-    "libsndfile-devel", "libedit-devel", "linux-headers",
+    "glib-devel", "pipewire-devel", "pipewire-jack-devel", "libpulse-devel",
+    "sdl-devel", "libsndfile-devel", "libedit-devel", "linux-headers",
 ]
 pkgdesc = "Software synthesizer based on the SoundFont 2 specifications"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.fluidsynth.org"
 source = f"https://github.com/FluidSynth/{pkgname}/archive/v{pkgver}.tar.gz"
-sha256 = "bc62494ec2554fdcfc01512a2580f12fc1e1b01ce37a18b370dd7902af7a8159"
+sha256 = "1df5a1afb91acf3b945b7fdb89ac0d99877622161d9b5155533da59113eaaa20"
 
 @subpackage("libfluidsynth")
 def _lib(self):
