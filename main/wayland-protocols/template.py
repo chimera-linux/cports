20fa0401dba5d72ab84075e529874f72a58b622a
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 22:42:43 +0200

    main/wayland-protocols: update to 1.27

--- main/wayland-protocols/template.py
+++ main/wayland-protocols/template.py
@@ -1,5 +1,5 @@
 pkgname = "wayland-protocols"
-pkgver = "1.26"
+pkgver = "1.27"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "wayland-progs"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://wayland.freedesktop.org"
 source = f"https://github.com/wayland-project/{pkgname}/archive/{pkgver}.tar.gz"
-sha256 = "fe56386f436a84e97c3b6a61b76306f205a64425900f247ad0048174b9c32d4d"
+sha256 = "6dd6ee86478adf4347f3b8b4f3da62dbe9e44912c9cef21cf99abfe692313df4"
 
 def post_install(self):
     self.install_license("COPYING")
