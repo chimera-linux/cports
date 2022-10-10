3f1d35793404aac9cb420075e74326e80e7ae852
Daniel Kolesa <q66@chimera-linux.org>
2022-10-11 01:51:27 +0200

    main/nautilus: update to 43.0

--- main/nautilus/template.py
+++ main/nautilus/template.py
@@ -1,16 +1,17 @@
 pkgname = "nautilus"
-pkgver = "42.2"
+pkgver = "43.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dtests=headless"]
 hostmakedepends = [
     "meson", "pkgconf", "glib-devel", "gettext-tiny", "gobject-introspection",
-    "cmake",
+    "cmake", "desktop-file-utils",
 ]
 makedepends = [
     "libglib-devel", "gnome-desktop-devel", "gnome-autoar-devel",
-    "libhandy-devel", "gtk+3-devel", "libportal-devel", "tracker-devel",
+    "libadwaita-devel", "gtk4-devel", "libportal-devel", "tracker-devel",
     "libxml2-devel", "gexiv2-devel", "gst-plugins-base-devel",
+    "libcloudproviders-devel",
 ]
 depends = ["hicolor-icon-theme", "tracker", "tracker-miners"]
 checkdepends = ["dbus", "tracker", "tracker-miners", "python-gobject"]
@@ -19,7 +20,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Apps/Files"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "99212d2eb75996f181728ad04a2e2d86f2577b064e68a34c8b81a7037df4ccb2"
+sha256 = "3cf54fac0a8abee090e155417f7b16f63ea0ac0c264ef4f54574af345801a911"
 options = ["!cross"]
 
 @subpackage("nautilus-devel")
