edca17028def0576c083d2e8e105e39000b0c39f
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 02:42:45 +0200

    main/libxxf86vm: update to 1.1.5

--- main/libxxf86vm/template.py
+++ main/libxxf86vm/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxxf86vm"
-pkgver = "1.1.4"
+pkgver = "1.1.5"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-malloc0returnsnull"]
@@ -9,8 +9,8 @@ pkgdesc = "XFree86-VidMode X extension library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libXxf86vm-{pkgver}.tar.bz2"
-sha256 = "afee27f93c5f31c0ad582852c0fb36d50e4de7cd585fcf655e278a633d85cd57"
+source = f"$(XORG_SITE)/lib/libXxf86vm-{pkgver}.tar.gz"
+sha256 = "f3f1c29fef8accb0adbd854900c03c6c42f1804f2bc1e4f3ad7b2e1f3b878128"
 
 def post_install(self):
     self.install_license("COPYING")
