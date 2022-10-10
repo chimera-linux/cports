6d9a3726aa0817e4e766b90fbaa3d005f69f6e9c
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 05:06:15 +0200

    main/libgusb: update to 0.4.1

--- main/libgusb/template.py
+++ main/libgusb/template.py
@@ -1,18 +1,19 @@
 pkgname = "libgusb"
-pkgver = "0.3.10"
+pkgver = "0.4.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocs=false", "-Dintrospection=true"]
 hostmakedepends = ["meson", "pkgconf", "vala", "gobject-introspection"]
 makedepends = [
-    "libglib-devel", "libusb-devel", "libgudev-devel", "vala-devel", "usbutils"
+    "libglib-devel", "libusb-devel", "libgudev-devel", "json-glib-devel",
+    "vala-devel", "usbutils"
 ]
 pkgdesc = "GLib wrapper around libusb"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/hughsie/libgusb"
 source = f"http://people.freedesktop.org/~hughsient/releases/{pkgname}-{pkgver}.tar.xz"
-sha256 = "0eb0b9ab0f8bba0c59631c809c37b616ef34eb3c8e000b0b9b71cf11e4931bdc"
+sha256 = "39ee01dab6a75f28de7c317e25d24159f511e1bf8e7465e275a0fbc483a48b63"
 # no access to usb in container
 options = ["!check", "!cross"]
 
