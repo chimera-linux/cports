3b8fb427e5ee6f9d3666f94a9076c527acd677f4
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 03:41:07 +0200

    main/libgpg-error: update to 1.46

--- main/libgpg-error/template.py
+++ main/libgpg-error/template.py
@@ -1,5 +1,5 @@
 pkgname = "libgpg-error"
-pkgver = "1.45"
+pkgver = "1.46"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gnupg.org"
 source = f"{url}/ftp/gcrypt/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "570f8ee4fb4bff7b7495cff920c275002aea2147e9a1d220c068213267f80a26"
+sha256 = "b7e11a64246bbe5ef37748de43b245abd72cfcd53c9ae5e7fc5ca59f1c81268d"
 # needs qemu and patching
 options = ["!cross"]
 
