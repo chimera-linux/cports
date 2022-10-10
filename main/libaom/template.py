8083eea8325d538b6134662fc3f73b38d5a2478d
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 04:13:24 +0200

    main/libaom: update to 3.5.0

--- main/libaom/template.py
+++ main/libaom/template.py
@@ -1,5 +1,5 @@
 pkgname = "libaom"
-pkgver = "3.4.0"
+pkgver = "3.5.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DENABLE_TESTS=OFF"]
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://aomedia.org"
 source = [f"https://storage.googleapis.com/aom-releases/{pkgname}-{pkgver}.tar.gz"]
-sha256 = ["bd754b58c3fa69f3ffd29da77de591bd9c26970e3b18537951336d6c0252e354"]
+sha256 = ["d37dbee372e2430a7efde813984ae6d78bdf1fc4080ebe32457c9115408b0738"]
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=2097152"]}
 # requires a testdata download, tests take long
 options = ["!check"]
