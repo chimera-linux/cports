41459c243b17c306c9ea6a127c5dfb2c12ff62d3
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 04:54:45 +0200

    main/cairomm: update to 1.16.2

--- main/cairomm/template.py
+++ main/cairomm/template.py
@@ -1,5 +1,5 @@
 pkgname = "cairomm"
-pkgver = "1.16.1"
+pkgver = "1.16.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dboost-shared=true"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.cairographics.org/cairomm"
 source = f"http://cairographics.org/releases/{pkgname}-{pkgver}.tar.xz"
-sha256 = "6f6060d8e98dd4b8acfee2295fddbdd38cf487c07c26aad8d1a83bb9bff4a2c6"
+sha256 = "6a63bf98a97dda2b0f55e34d1b5f3fb909ef8b70f9b8d382cb1ff3978e7dc13f"
 
 @subpackage("cairomm-devel")
 def _devel(self):
