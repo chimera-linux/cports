ef6e29b74db16b97e8fef4c3b58ad1c77288434f
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 23:39:02 +0200

    main/gnome-initial-setup: update to 43.0

--- main/gnome-initial-setup/template.py
+++ main/gnome-initial-setup/template.py
@@ -1,10 +1,8 @@
 pkgname = "gnome-initial-setup"
-pkgver = "42.2"
+pkgver = "43.0"
 pkgrel = 0
 build_style = "meson"
-configure_args = [
-    "-Dparental_controls=disabled", "-Dcheese=disabled", "-Dsystemd=false",
-]
+configure_args = ["-Dparental_controls=disabled", "-Dsystemd=false",]
 hostmakedepends = [
     "meson", "pkgconf", "glib-devel", "gettext-tiny"
 ]
@@ -12,14 +10,14 @@ makedepends = [
     "ibus-devel", "networkmanager-devel", "accountsservice-devel",
     "polkit-devel", "gnome-desktop-devel", "gsettings-desktop-schemas-devel",
     "fontconfig-devel", "libgweather-devel", "gnome-online-accounts-devel",
-    "gtk+3-devel", "libglib-devel", "webkitgtk-devel", "geocode-glib-devel",
-    "geoclue-devel", "pango-devel", "rest-devel", "json-glib-devel",
-    "heimdal-devel", "libsecret-devel", "libpwquality-devel", "gdm-devel",
-    "libnma-devel", "libhandy-devel",
+    "gtk+3-devel", "gtk4-devel", "libglib-devel", "webkitgtk4-devel",
+    "geocode-glib-devel", "geoclue-devel", "pango-devel", "rest-devel",
+    "json-glib-devel", "heimdal-devel", "libsecret-devel", "gdm-devel",
+    "libpwquality-devel", "libnma-devel", "libadwaita-devel",
 ]
 pkgdesc = "GNOME initial setup"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Design/OS/InitialSetup"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "4d83d98b249abe15bb2b3e7e7aead9c47f848bca7b6a091abc908b76541abe7b"
+sha256 = "029ecb81933f404fa4f6ca55bee47b03ef890e84462bf145f6ee09477e1a3955"
