aaa69decae169140c13734840efd0e15952c562a
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 03:56:25 +0200

    main/font-unifont-bdf: update to 15.0.01

--- main/font-unifont-bdf/template.py
+++ main/font-unifont-bdf/template.py
@@ -1,5 +1,5 @@
 pkgname = "font-unifont-bdf"
-pkgver = "14.0.04"
+pkgver = "15.0.01"
 pkgrel = 0
 depends = ["font-util"]
 pkgdesc = "GNU Unifont Glyphs - BDF"
@@ -7,7 +7,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "http://unifoundry.com/unifont/index.html"
 source = f"http://unifoundry.com/pub/unifont/unifont-{pkgver}/font-builds/unifont-{pkgver}.bdf.gz"
-sha256 = "0bad2b8a46fcc4864c7b4cee4072653a58b9f36e2f54a5a395c7d6dc97766526"
+sha256 = "1fddba900a36b8a067bf2177b05c4a2482a0f7ca1545cf531c03509f47ce1590"
 
 def do_install(self):
     self.install_file(
