762bca0f735b35d1de8b983d22db02ddfa64ffc7
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 22:36:55 +0200

    main/weston: update to 11.0.0

--- main/weston/template.py
+++ main/weston/template.py
@@ -1,12 +1,11 @@
 pkgname = "weston"
-pkgver = "10.0.2"
+pkgver = "11.0.0"
 pkgrel = 0
 build_style = "meson"
-# pipewire requires 0.2 in this release
 configure_args = [
     "-Dsystemd=false", "-Dlauncher-logind=false", "-Dlauncher-libseat=true",
-    "-Dpipewire=false", "-Dremoting=false", "-Dbackend-rdp=false",
-    "-Dbackend-drm-screencast-vaapi=true", "-Dcolor-management-colord=true",
+    "-Dpipewire=true", "-Dremoting=false", "-Dbackend-rdp=false",
+    "-Dbackend-drm-screencast-vaapi=true", "-Dcolor-management-lcms=true",
     "-Dtest-junit-xml=false", "-Db_ndebug=false",
     "-Ddefault_library=shared",
 ]
@@ -14,11 +13,11 @@ make_check_env = {"XDG_RUNTIME_DIR": "/tmp"}
 hostmakedepends = ["meson", "pkgconf", "wayland-progs", "wayland-protocols"]
 makedepends = [
     "libpng-devel", "libwebp-devel", "lcms2-devel", "pixman-devel",
-    "mesa-devel", "pango-devel", "cairo-devel", "mtdev-devel",
+    "mesa-devel", "pango-devel", "cairo-devel", "mtdev-devel", "glu-devel",
     "libinput-devel", "libxcb-devel", "libxcursor-devel", "libxkbcommon-devel",
-    "wayland-devel", "wayland-protocols", "libdrm-devel",
-    "linux-pam-devel", "eudev-devel", "dbus-devel",
-    "colord-devel", "glu-devel", "libva-devel", "libseat-devel",
+    "wayland-devel", "wayland-protocols", "libdrm-devel", "linux-pam-devel",
+    "eudev-devel", "dbus-devel", "libva-devel", "pipewire-devel",
+    "libseat-devel",
 ]
 checkdepends = ["mesa-dri", "xwayland"]
 pkgdesc = "Reference implementation of a Wayland compositor"
@@ -26,7 +25,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://wayland.freedesktop.org"
 source = f"https://gitlab.freedesktop.org/wayland/{pkgname}/-/releases/{pkgver}/downloads/{pkgname}-{pkgver}.tar.xz"
-sha256 = "89646ca0d9f8d413c2767e5c3828eaa3fa149c2a105b3729a6894fa7cf1549e7"
+sha256 = "a6138d4dc9554560ac304312df456019f4be025ec79130f05fb5f2e41c091e1d"
 
 def post_install(self):
     self.install_license("COPYING")
@@ -45,15 +44,9 @@ def _xwayland(self):
 
     return ["usr/lib/libweston*/xwayland.so"]
 
-@subpackage("weston-colord")
-def _colord(self):
-    self.pkgdesc = f"{pkgdesc} (colord plugin)"
-
-    return ["usr/lib/weston/cms-colord.so"]
-
 @subpackage("weston-libs")
 def _lib(self):
-    return self.default_libs(extra = [f"usr/lib/libweston-10"])
+    return self.default_libs(extra = [f"usr/lib/libweston-11"])
 
 @subpackage("weston-devel")
 def _devel(self):
