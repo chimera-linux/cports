b0958f3f83dd4221c0a65b9394879d4c14f84116
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 23:21:33 +0200

    main/gnome-desktop: update to 43

--- main/gnome-desktop/template.py
+++ main/gnome-desktop/template.py
@@ -1,13 +1,8 @@
 pkgname = "gnome-desktop"
-pkgver = "42.4"
+pkgver = "43"
 pkgrel = 0
 build_style = "meson"
-configure_args = [
-    "-Dgnome_distributor=Chimera",
-    "-Dudev=enabled",
-    "-Dsystemd=disabled",
-    "-Ddate_in_gnome_version=false",
-]
+configure_args = ["-Dudev=enabled", "-Dsystemd=disabled",]
 hostmakedepends = [
     "meson", "pkgconf", "gobject-introspection", "glib-devel",
     "gettext-tiny", "docbook-xml", "itstool",
@@ -24,8 +19,8 @@ pkgdesc = "GNOME desktop management utilities"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-desktop"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "1ce2c9d5067969dbe0b282ea5a9acfb8698751f03cd07e2c730240f85dc9ad25"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "3d6e153317486157596aa3802f87676414c570738f450a94a041fe8835420a69"
 # needs graphical environment
 options = ["!check"]
 
