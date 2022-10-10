1485335d2be08e615665c45e0b760650558f6109
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 05:27:33 +0200

    main/gtkmm: update to 4.8.0

--- main/gtkmm/template.py
+++ main/gtkmm/template.py
@@ -1,5 +1,5 @@
 pkgname = "gtkmm"
-pkgver = "4.6.1"
+pkgver = "4.8.0"
 pkgrel = 0
 build_style = "meson"
 make_check_wrapper = ["xvfb-run"]
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gtkmm.org"
 source = f"$(GNOME_SITE)/gtkmm/{pkgver[:-2]}/gtkmm-{pkgver}.tar.xz"
-sha256 = "0d5efeca9ec64fdd530bb8226c6310ac99549b3dd9604d6e367639791af3d1e0"
+sha256 = "c82786d46e2b07346b6397ca7f1929d952f4922fa5c9db3dee08498b9a136cf5"
 
 @subpackage("gtkmm-devel")
 def _devel(self):
