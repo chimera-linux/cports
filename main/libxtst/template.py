4917c49ec7735ee42cca42a8caef86827f08bd90
Daniel Kolesa <q66@chimera-linux.org>
2022-10-10 02:42:04 +0200

    main/libxtst: update to 1.2.4

--- main/libxtst/template.py
+++ main/libxtst/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxtst"
-pkgver = "1.2.3"
+pkgver = "1.2.4"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "X Tst library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libXtst-{pkgver}.tar.bz2"
-sha256 = "4655498a1b8e844e3d6f21f3b2c4e2b571effb5fd83199d428a6ba7ea4bf5204"
+source = f"$(XORG_SITE)/lib/libXtst-{pkgver}.tar.gz"
+sha256 = "01366506aeb033f6dffca5326af85f670746b0cabbfd092aabefb046cf48c445"
 
 def post_install(self):
     self.install_license("COPYING")
