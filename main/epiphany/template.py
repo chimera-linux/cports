548d74715ded0b6cb529144d1a9d3f7ba78ed26e
Daniel Kolesa <q66@chimera-linux.org>
2022-10-11 02:13:21 +0200

    main/epiphany: update to 43.0

--- main/epiphany/template.py
+++ main/epiphany/template.py
@@ -1,9 +1,12 @@
 pkgname = "epiphany"
-pkgver = "42.4"
+pkgver = "43.0"
 pkgrel = 0
 build_style = "meson"
-configure_args = ["-Dunit_tests=disabled", "-Dsoup2=disabled"]
-hostmakedepends = ["meson", "pkgconf", "glib-devel", "gettext-tiny", "itstool"]
+configure_args = ["-Dunit_tests=disabled"]
+hostmakedepends = [
+    "meson", "pkgconf", "glib-devel", "gettext-tiny", "itstool",
+    "desktop-file-utils",
+]
 makedepends = [
     "webkitgtk-devel", "cairo-devel", "gcr-devel", "gdk-pixbuf-devel",
     "libglib-devel", "gsettings-desktop-schemas-devel", "gtk+3-devel",
@@ -17,4 +20,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://wiki.gnome.org/Apps/Web"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "370938ad2920eeb28bc2435944776b7ba55a0e2ede65836f79818cfb7e8f0860"
+sha256 = "b66d499f9ee72696d83cf844125377181a954554a4bb3785b73293380ac0c227"
