57ffa3bd992376816fbe0ce30b235431c118c129
Daniel Kolesa <q66@chimera-linux.org>
2022-10-11 02:08:00 +0200

    main/gnome-disk-utility: update to 43.0

--- main/gnome-disk-utility/template.py
+++ main/gnome-disk-utility/template.py
@@ -1,21 +1,21 @@
 pkgname = "gnome-disk-utility"
-pkgver = "42.0"
+pkgver = "43.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dlogind=libelogind"]
 hostmakedepends = [
     "meson", "pkgconf", "glib-devel", "gettext-tiny", "xsltproc",
-    "docbook-xsl-nons",
+    "docbook-xsl-nons", "desktop-file-utils",
 ]
 makedepends = [
     "libdvdread-devel", "libglib-devel", "gtk+3-devel", "libhandy-devel",
     "liblzma-devel", "libnotify-devel", "libsecret-devel", "udisks-devel",
     "libpwquality-devel", "elogind-devel", "libcanberra-devel",
 ]
-depends = ["hicolor-icon-theme"]
+depends = ["udisks", "hicolor-icon-theme"]
 pkgdesc = "GNOME disk drive and media management"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/Disks"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "1b6564454d67426322cb3bfc5a5558653bfc7dfeea2ae0825b1d08629f01090b"
+sha256 = "7afca9805a6b92db6933dd7efcec4af8386c01bbc1f871e2dae4def7e192a2c5"
