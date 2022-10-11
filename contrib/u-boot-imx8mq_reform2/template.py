c8826a58b244b8a913122ee1c13b05e6802b36be
q66 <daniel@octaforge.org>
2022-10-11 16:33:05 +0000

    contrib/u-boot-imx8mq_reform2: switch to binary dist

--- contrib/u-boot-imx8mq_reform2/template.py
+++ contrib/u-boot-imx8mq_reform2/template.py
@@ -1,26 +1,23 @@
 pkgname = "u-boot-imx8mq_reform2"
 pkgver = "2018.07"
 pkgrel = 0
-_commit = "4d563c4525e1500c0633abfb2b90ba8ad846dd91"
 archs = ["aarch64"]
-build_style = "u_boot"
-make_build_args = ["u-boot.bin", "flash.bin"]
-hostmakedepends = [
-    "gmake", "gcc-aarch64-none-elf", "flex", "bison",
-    "dtc", "python", "openssl-devel"
-]
 pkgdesc = "U-Boot for MNT Reform 2"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND BSD-3-Clause"
 url = "https://source.mnt.re/reform/reform-boundary-uboot"
-source = f"{url}/-/archive/{_commit}.tar.gz"
-sha256 = "deb9c9c9e4ae8ac71798144f55add574c5b84bbdaa70e52827065977fd95e08d"
-env = {
-    "U_BOOT_TRIPLET": "aarch64-none-elf",
-    "U_BOOT_TARGETS": "spl/u-boot-spl.bin u-boot.itb flash.bin",
-}
+source = f"https://repo.chimera-linux.org/distfiles/{pkgname}-{pkgver}.tar.gz"
+sha256 = "d8699b465c8d09549aee622e3a42d4101e765abfe4f3f0be54a45a3d878a152a"
 # not relevant
 options = ["!strip", "!check", "!lto", "!debug"]
 
-def do_configure(self):
-    self.cp("mntreform-config", ".config")
+def do_install(self):
+    for x in self.cwd.iterdir():
+        if x.is_dir():
+            continue
+        self.install_file(x, "usr/lib/u-boot/imx8mq_reform2")
+    # licenses
+    for f in (self.cwd / "Licenses").iterdir():
+        self.install_license(f"Licenses/{f.name}")
+    # readme
+    self.install_file("README.txt", f"usr/share/doc/{self.pkgname}")
