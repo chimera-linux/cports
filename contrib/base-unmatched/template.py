52d9682ca4e33976fb971dc1b605c4a371e701d2
Daniel Kolesa <q66@chimera-linux.org>
2022-10-22 18:44:30 +0200

    contrib/base-unmatched: switch to common u-boot-menu

--- contrib/base-unmatched/template.py
+++ contrib/base-unmatched/template.py
@@ -2,18 +2,15 @@ pkgname = "base-unmatched"
 pkgver = "0.1"
 pkgrel = 0
 archs = ["riscv64"]
-depends = ["linux", "u-boot-sifive_unmatched"]
+depends = ["linux", "u-boot-sifive_unmatched", "u-boot-menu"]
 pkgdesc = "Chimera base package for HiFive Unmatched"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:none"
 url = "https://chimera-linux.org"
 
 def do_install(self):
-    # kernel hook
-    self.install_file(
-        self.files_path / "99-unmatched-kernel.sh", "etc/kernel.d", mode = 0o755
-    )
-    # cmdline
-    self.install_file(self.files_path / "unmatched-cmdline", "etc/default")
+    # u-boot-menu
+    self.install_file(self.files_path / "u-boot-cmdline", "etc/default")
+    self.install_file(self.files_path / "u-boot-fdt", "etc/default")
     # agetty service
     self.install_service(self.files_path / "agetty-ttySIF0")
