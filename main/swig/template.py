a0cda832a3e1df3f6ee85b5efd1481834844352c
Daniel Kolesa <q66@chimera-linux.org>
2022-10-27 03:55:59 +0200

    main/swig: update to 4.1.0 and switch to pcre2

--- main/swig/template.py
+++ main/swig/template.py
@@ -1,14 +1,14 @@
 pkgname = "swig"
-pkgver = "4.0.2"
+pkgver = "4.1.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["byacc"]
-makedepends = ["zlib-devel", "pcre-devel"]
+makedepends = ["zlib-devel", "pcre2-devel"]
 pkgdesc = "Simplified Wrapper and Interface Generator"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.swig.org"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "d53be9730d8d58a16bf0cbd1f8ac0c0c3e1090573168bfa151b01eb47fa906fc"
+sha256 = "d6a9a8094e78f7cfb6f80a73cc271e1fe388c8638ed22668622c2c646df5bb3d"
 # broken check target?
 options = ["!check"]
