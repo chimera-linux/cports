d75e616d85a433f01f8397870c9a8c70f82ef321
Daniel Kolesa <q66@chimera-linux.org>
2022-11-01 20:18:51 +0100

    main/fortify-headers: update to avoid some clang errors

--- main/fortify-headers/template.py
+++ main/fortify-headers/template.py
@@ -1,14 +1,14 @@
 pkgname = "fortify-headers"
 pkgver = "1.1.99"
 pkgrel = 0
-_gitrev = "6700b691e0e5c3b75225c01c437a06bd598a8343"
+_gitrev = "55ae3986e7c54efdbcb4b3d9e5834ed4826d81f7"
 build_style = "makefile"
 pkgdesc = "Standalone fortify implementation"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "0BSD"
 url = "https://github.com/chimera-linux/fortify-headers"
 source = f"{url}/archive/{_gitrev}.tar.gz"
-sha256 = "7f5ee9d564d6ebfd1a4413a76f9a60b5b7176e5fe19baf3f3b2536449d26f2e0"
+sha256 = "c7eb4e1b442dfcf9f85862851bd5dd3f43b7f879a7755820be4e04560d3ce2c0"
 # no test suite
 options = ["bootstrap", "!check"]
 
