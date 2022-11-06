eb7d931265a895848e2c5e2e8d574ca69ed6a9db
Daniel Kolesa <q66@chimera-linux.org>
2022-11-07 00:04:59 +0100

    main/python-sphinx: update to 5.3.0

--- main/python-sphinx/template.py
+++ main/python-sphinx/template.py
@@ -1,11 +1,11 @@
 pkgname = "python-sphinx"
-pkgver = "5.2.3"
+pkgver = "5.3.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-setuptools", "python-flit_core", "python-pip"]
 checkdepends = ["python-pytest", "python-markupsafe"]
 depends = [
-    "python-jinja2", "python-docutils", "python-pygments",
+    "python", "python-jinja2", "python-docutils", "python-pygments",
     "python-snowballstemmer", "python-babel", "python-alabaster",
     "python-imagesize", "python-requests", "python-packaging",
     "python-sphinxcontrib-applehelp", "python-sphinxcontrib-devhelp",
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://sphinx-doc.org"
 source = f"$(PYPI_SITE)/S/Sphinx/Sphinx-{pkgver}.tar.gz"
-sha256 = "5b10cb1022dac8c035f75767799c39217a05fc0fe2d6fe5597560d38e44f0363"
+sha256 = "51026de0a9ff9fc13c05d74913ad66047e104f56a129ff73e174eb5c3ee794b5"
 # dependency of pytest, missing other checkdepends
 options = ["!check"]
 
