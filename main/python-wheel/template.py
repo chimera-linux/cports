58ea5cdc34cc3058ccbfe0961c5a1ef9b6ab255b
Daniel Kolesa <q66@chimera-linux.org>
2022-11-06 23:44:39 +0100

    main/python-wheel: update to 0.38.2

--- main/python-wheel/template.py
+++ main/python-wheel/template.py
@@ -1,15 +1,15 @@
 pkgname = "python-wheel"
-pkgver = "0.37.1"
+pkgver = "0.38.2"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
-depends = ["python-setuptools"]
+depends = ["python", "python-setuptools"]
 pkgdesc = "Built-in package format for Python"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pypa/wheel"
 source = f"$(PYPI_SITE)/w/wheel/wheel-{pkgver}.tar.gz"
-sha256 = "e9a504e793efbca1b8e0e9cb979a249cf4a0a7b5b8c9e8b65a5e39d49529c1c4"
+sha256 = "3d492ef22379a156ec923d2a77051cedfd4df4b667864e9e41ba83f0b70b7149"
 # TODO
 options = ["!check"]
 
