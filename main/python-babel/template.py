e7b3c3b721f833c16a81f7ec7cd3765a2f33c92c
Daniel Kolesa <q66@chimera-linux.org>
2022-11-06 23:32:23 +0100

    main/python-babel: update to 2.11.0

--- main/python-babel/template.py
+++ main/python-babel/template.py
@@ -1,16 +1,16 @@
 pkgname = "python-babel"
-pkgver = "2.10.3"
+pkgver = "2.11.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
 checkdepends = ["python-pytz"]
-depends = ["python-setuptools", "python-pytz"]
+depends = ["python", "python-setuptools", "python-pytz"]
 pkgdesc = "Tools for internationalizing Python applications"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://babel.pocoo.org"
 source = f"$(PYPI_SITE)/B/Babel/Babel-{pkgver}.tar.gz"
-sha256 = "7614553711ee97490f732126dc077f8d0ae084ebc6a96e23db1482afabdb2c51"
+sha256 = "5ef4b3226b0180dedded4229651c8b0e1a3a6a2837d45a073272f313e4cf97f6"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
