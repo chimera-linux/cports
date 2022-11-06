328fa30bd379b35bcaae3e08fa7ad9a9fd5905f9
Daniel Kolesa <q66@chimera-linux.org>
2022-11-06 23:36:55 +0100

    main/python-flit_core: update to 3.8.0

--- main/python-flit_core/template.py
+++ main/python-flit_core/template.py
@@ -1,17 +1,17 @@
 pkgname = "python-flit_core"
-pkgver = "3.7.1"
+pkgver = "3.8.0"
 pkgrel = 0
 build_wrksrc = f"flit_core"
 build_style = "python_pep517"
 hostmakedepends = ["python-pip"]
 checkdepends = ["python-pytest", "python-tomli"]
-depends = ["python-tomli"]
+depends = ["python", "python-tomli"]
 pkgdesc = "Simplified packaging of Python modules (PEP 517 backend)"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://flit.readthedocs.io"
 source = f"$(PYPI_SITE)/f/flit/flit-{pkgver}.tar.gz"
-sha256 = "3c9bd9c140515bfe62dd938c6610d10d6efb9e35cc647fc614fe5fb3a5036682"
+sha256 = "d0f2a8f4bd45dc794befbf5839ecc0fd3830d65a57bd52b5997542fac5d5e937"
 # missing checkdepends
 options = ["!check"]
 
