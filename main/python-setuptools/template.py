13bf57c768191bde6f317a08bbacad65a23033b7
Daniel Kolesa <q66@chimera-linux.org>
2022-11-06 23:13:00 +0100

    main/python-setuptools: update to 65.5.1

--- main/python-setuptools/template.py
+++ main/python-setuptools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-setuptools"
-pkgver = "65.4.1"
+pkgver = "65.5.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-devel"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pypa/setuptools"
 source = f"$(PYPI_SITE)/s/setuptools/setuptools-{pkgver}.tar.gz"
-sha256 = "3050e338e5871e70c72983072fe34f6032ae1cdeeeb67338199c2f74e083a80e"
+sha256 = "e197a19aa8ec9722928f2206f8de752def0e4c9fc6953527360d1c36d94ddb2f"
 env = {
     "SETUPTOOLS_INSTALL_WINDOWS_SPECIFIC_FILES": "0",
     "SETUPTOOLS_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT": "1"
