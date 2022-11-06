414ea05b414f364b08ff5def5af2c3b168c0c2d6
Daniel Kolesa <q66@chimera-linux.org>
2022-11-06 23:34:00 +0100

    main/python-pip: update to 22.3.1

--- main/python-pip/template.py
+++ main/python-pip/template.py
@@ -1,15 +1,15 @@
 pkgname = "python-pip"
-pkgver = "22.2.2"
+pkgver = "22.3.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
-depends = ["python-setuptools"]
+depends = ["python", "python-setuptools"]
 pkgdesc = "Python package manager"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pip.pypa.io"
 source = f"$(PYPI_SITE)/p/pip/pip-{pkgver}.tar.gz"
-sha256 = "3fd1929db052f056d7a998439176d3333fa1b3f6c1ad881de1885c0717608a4b"
+sha256 = "65fd48317359f3af8e593943e6ae1506b66325085ea64b706a998c6e83eeaf38"
 # unpackaged dependencies
 options = ["!check"]
 
