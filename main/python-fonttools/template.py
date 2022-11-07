adb71e80983514fd5fe05c9d7eea936931c3bfa2
Daniel Kolesa <q66@chimera-linux.org>
2022-11-07 02:04:45 +0100

    main/python-fonttools: update to 4.38.0

--- main/python-fonttools/template.py
+++ main/python-fonttools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-fonttools"
-pkgver = "4.37.4"
+pkgver = "4.38.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT AND OFL-1.1 AND BSD-3-Clause AND Apache-2.0"
 url = "https://github.com/fonttools/fonttools"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "811389458954ff6fe3a503b11c00eccc202019b9dfe42c85de59365a45e7855d"
+sha256 = "764c569221fb437d0c3937d19d3e38e137d324f225fb30e54ef0aaa2d6f7a289"
 # unpackaged deps
 options = ["!check"]
 
