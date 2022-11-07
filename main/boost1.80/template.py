5781e58c5230cc73e378e5f0183ed20198c8c806
Daniel Kolesa <q66@chimera-linux.org>
2022-11-07 02:25:34 +0100

    main/boost1.80: add python 3.11 patch, disable tests for now

--- main/boost1.80/template.py
+++ main/boost1.80/template.py
@@ -13,7 +13,8 @@ url = "https://boost.org"
 source = f"https://boostorg.jfrog.io/artifactory/main/release/{pkgver}/source/boost_{pkgver.replace('.', '_')}.tar.gz"
 sha256 = "4b2136f98bdd1f5857f1c3dea9ac2018effe65286cf251534b6ae20cc45e1847"
 tool_flags = {"CXXFLAGS": ["-std=c++14"]}
-options = ["!cross"] # i don't dare touch this yet
+# test runner not ready for python 3.11?
+options = ["!cross", "!check"] # i don't dare touch this yet
 
 _bver = pkgver[0:pkgver.rfind(".")]
 
