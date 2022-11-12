4e09d12e560114a5a761892e5e6c2afb7caee6ef
Daniel Kolesa <q66@chimera-linux.org>
2022-11-12 21:10:36 +0100

    main/dinit-userservd: update to 0.92.0
    This release implements session lingering (which lets one have
    user services keep running even after last logout) and also
    now always manages the runtime directory (with elogind having
    the functionality patched out)

--- main/dinit-userservd/template.py
+++ main/dinit-userservd/template.py
@@ -1,20 +1,23 @@
 pkgname = "dinit-userservd"
-pkgver = "0.91.0"
+pkgver = "0.92.0"
 pkgrel = 0
 build_style = "meson"
-hostmakedepends = ["meson", "pkgconf"]
+configure_args = ["-Dmanage_rundir=true"]
+hostmakedepends = ["meson", "pkgconf", "scdoc"]
 makedepends = ["linux-pam-devel"]
 pkgdesc = "Dinit user instance manager"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/dinit-userservd"
 source = f"https://github.com/chimera-linux/{pkgname}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "350d4a83fd77ce870a813bbf49bbbd69aa358b16d293bada8c53ec8dc1f6b6f6"
+sha256 = "af77af35c5d91c4cbfbabdb2dd015ec78a5b9c355501e3aea124e4b14efcd6c2"
 options = ["brokenlinks", "!splitdinit"]
 
 def post_install(self):
     # just make sure it exists
     self.install_dir("usr/lib/dinit.d/user/boot.d", empty = True)
+    # linger
+    self.install_dir("var/lib/dinit-userservd/linger", empty = True)
     # also default systemwide link
     self.install_dir("usr/lib/dinit.d/boot.d")
     self.install_link(
