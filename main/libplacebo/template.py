f1238669424d62f34383882e5b1daa9ecf087357
Daniel Kolesa <q66@chimera-linux.org>
2022-11-13 06:11:58 +0100

    main/libplacebo: update to 5.229.1

--- main/libplacebo/template.py
+++ main/libplacebo/template.py
@@ -1,24 +1,25 @@
 pkgname = "libplacebo"
-pkgver = "4.208.0"
+pkgver = "5.229.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
-    "-Dshaderc=enabled", "-Dvulkan=enabled",
+    "-Dshaderc=enabled", "-Dglslang=enabled", "-Dvulkan=enabled",
     "-Dopengl=enabled", "-Dlcms=enabled",
 ]
 hostmakedepends = [
-    "meson", "pkgconf", "python-mako", "vulkan-headers"
+    "meson", "pkgconf", "python-jinja2", "python-glad", "python-markupsafe",
+    "vulkan-headers"
 ]
 makedepends = [
-    "shaderc-devel", "vulkan-headers", "vulkan-loader",
-    "lcms2-devel", "libepoxy-devel"
+    "shaderc-devel", "glslang-devel", "vulkan-headers", "vulkan-loader",
+    "lcms2-devel",
 ]
 pkgdesc = "Reusable library for GPU-accelerated video/image rendering"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://code.videolan.org/videolan/libplacebo"
 source = f"{url}/-/archive/v{pkgver}/{pkgname}-v{pkgver}.tar.gz"
-sha256 = "7b3c857934ee3d30f743e43d7f0606e10950806661ea0ea385f8a1f06cbab854"
+sha256 = "fc021fc68376b92511977b5bd32340fc575dc36af5c471cc095b5b7e3fa581d4"
 
 @subpackage("libplacebo-devel")
 def _devel(self):
