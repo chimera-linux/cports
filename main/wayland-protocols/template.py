12ea8a8c5a83adf9923c961062dd77da21cb475f
Daniel Kolesa <q66@chimera-linux.org>
2022-11-13 05:55:26 +0100

    main/wayland-protocols: update to 1.28

--- main/wayland-protocols/template.py
+++ main/wayland-protocols/template.py
@@ -1,5 +1,5 @@
 pkgname = "wayland-protocols"
-pkgver = "1.27"
+pkgver = "1.28"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "wayland-progs"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://wayland.freedesktop.org"
 source = f"https://github.com/wayland-project/{pkgname}/archive/{pkgver}.tar.gz"
-sha256 = "6dd6ee86478adf4347f3b8b4f3da62dbe9e44912c9cef21cf99abfe692313df4"
+sha256 = "914dfa09e7e866e913b27d2d9bda0e20e728c7b1c831fd3db71538d9f99a4869"
 
 def post_install(self):
     self.install_license("COPYING")
