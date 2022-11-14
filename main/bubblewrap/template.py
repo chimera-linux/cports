fe1eb2842d5800a57642f00eadfba24b5de14957
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 02:59:14 +0100

    main/bubblewrap: update to 0.7.0

--- main/bubblewrap/template.py
+++ main/bubblewrap/template.py
@@ -1,5 +1,5 @@
 pkgname = "bubblewrap"
-pkgver = "0.6.2"
+pkgver = "0.7.0"
 pkgrel = 0
 build_style = "gnu_configure"
 make_check_args = ["-j1"]
@@ -11,5 +11,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
 url = "https://github.com/containers/bubblewrap"
 source = f"https://github.com/containers/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "8a0ec802d1b3e956c5bb0a40a81c9ce0b055a31bf30a8efa547433603b8af20b"
+sha256 = "764ab7100bd037ea53d440d362e099d7a425966bc62d1f00ab26b8fbb882a9dc"
 tool_flags = {"CFLAGS": ["-Wno-error,-Wformat-nonliteral"]}
