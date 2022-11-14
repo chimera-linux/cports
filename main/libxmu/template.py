89835cb5c03fe75924ad37a50846ffceeb2633ab
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 20:31:52 +0100

    main/libxmu: update to 1.1.4

--- main/libxmu/template.py
+++ main/libxmu/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxmu"
-pkgver = "1.1.3"
+pkgver = "1.1.4"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "X Miscellaneous Utilities library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libXmu-{pkgver}.tar.bz2"
-sha256 = "9c343225e7c3dc0904f2122b562278da5fed639b1b5e880d25111561bac5b731"
+source = f"$(XORG_SITE)/lib/libXmu-{pkgver}.tar.gz"
+sha256 = "3091d711cdc1d8ea0f545a13b90d1464c3c3ab64778fd121f0d789b277a80289"
 
 def post_install(self):
     self.install_license("COPYING")
