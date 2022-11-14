e20e5e1d3ea536fa720efbdb25ddbe93dbed9824
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 20:35:16 +0100

    main/xcb-util-image: update to 0.4.1

--- main/xcb-util-image/template.py
+++ main/xcb-util-image/template.py
@@ -1,5 +1,5 @@
 pkgname = "xcb-util-image"
-pkgver = "0.4.0"
+pkgver = "0.4.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "XCB utilities library - XImage and XShmImage"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xcb.freedesktop.org"
-source = f"{url}/dist/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "2db96a37d78831d643538dd1b595d7d712e04bdccf8896a5e18ce0f398ea2ffc"
+source = f"{url}/dist/{pkgname}-{pkgver}.tar.gz"
+sha256 = "0ebd4cf809043fdeb4f980d58cdcf2b527035018924f8c14da76d1c81001293b"
 
 def post_install(self):
     self.install_license("COPYING")
