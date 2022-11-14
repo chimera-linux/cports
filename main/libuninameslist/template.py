4c11f318fe8332a8dcb73be0f6c6554bdb882aac
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 22:46:14 +0100

    main/libuninameslist: update to 20221022

--- main/libuninameslist/template.py
+++ main/libuninameslist/template.py
@@ -1,5 +1,5 @@
 pkgname = "libuninameslist"
-pkgver = "20220701"
+pkgver = "20221022"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/fontforge/libuninameslist"
 source = f"{url}/releases/download/{pkgver}/{pkgname}-dist-{pkgver}.tar.gz"
-sha256 = "3a59cbc626d653e3ab74802da809e56d72cce4017cf5677310c7599eae994cef"
+sha256 = "92c833936d653b2f205fb5e7ac82818311824dabdc7abdc2e81f07c3a0ea39bb"
 
 def post_install(self):
     self.install_license("LICENSE")
