1d32ed8d1d9ac25ee3d86edf40a6dba233271372
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 22:46:55 +0100

    main/libunistring: update to 1.1

--- main/libunistring/template.py
+++ main/libunistring/template.py
@@ -1,5 +1,5 @@
 pkgname = "libunistring"
-pkgver = "1.0"
+pkgver = "1.1"
 pkgrel = 0
 build_style = "gnu_configure"
 pkgdesc = "Library for manipulating Unicode strings"
@@ -7,7 +7,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-or-later"
 url = "http://www.gnu.org/software/libunistring"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "3c0184c0e492d7c208ce31d25dd1d2c58f0c3ed6cbbe032c5b248cddad318544"
+sha256 = "a2252beeec830ac444b9f68d6b38ad883db19919db35b52222cf827c385bdb6a"
 
 @subpackage("libunistring-devel")
 def _devel(self):
