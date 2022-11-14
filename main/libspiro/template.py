345f093b12864578c866429a97bfac500e5740cb
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 22:58:32 +0100

    main/libspiro: update to 20221101

--- main/libspiro/template.py
+++ main/libspiro/template.py
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/fontforge/libspiro"
 source = f"{url}/releases/download/{pkgver}/{pkgname}-dist-{pkgver}.tar.gz"
-sha256 = "3b8e54473f3d4d99c014f2630e62f966f5f4e25c28ca59b63d30bd8e9b7593f5"
+sha256 = "5984fb5af3e4e1f927f3a74850b705a711fb86284802a5e6170b09786440e8be"
 
 @subpackage("libspiro-devel")
 def _devel(self):
