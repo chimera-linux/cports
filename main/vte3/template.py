31fa6b0feed306bf8e6b9de89d56e73ba370754f
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 04:20:05 +0100

    main/vte3: update to 0.70.1

--- main/vte3/template.py
+++ main/vte3/template.py
@@ -1,5 +1,5 @@
 pkgname = "vte3"
-pkgver = "0.70.0"
+pkgver = "0.70.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -19,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/Terminal/VTE"
 source = f"$(GNOME_SITE)/vte/{pkgver[:-2]}/vte-{pkgver}.tar.xz"
-sha256 = "93e0dd4a1bc2a7a1a62da64160a274cce456976ea1567d98591da96e2d265ae6"
+sha256 = "1f4601cbfea5302b96902208c8f185e5b18b259b5358bc93cf392bf59871c5b6"
 # assert in meson
 options = ["!lto", "!cross"]
 
