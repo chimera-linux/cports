7c149a4a7ab12e627de3684e176784c55d2f0177
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 23:52:45 +0100

    cbuild: refactor fetch not to use deprecated api + fix by_sha256
    The previous code forgot about the case when sources already
    existed with the right checksum, but weren't in by_sha256.

--- src/cbuild/hooks/do_fetch/000_sources.py
+++ src/cbuild/hooks/do_fetch/000_sources.py
@@ -3,31 +3,34 @@ import os
 import hashlib
 from urllib import request
 
-def get_cksum(fname, dfile, pkg):
+def get_cksum(dfile, pkg):
     return hashlib.sha256(dfile.read_bytes()).hexdigest()
 
-def verify_cksum(fname, dfile, cksum, pkg):
-    pkg.log(f"verifying sha256sums for source '{fname}'... ", "")
-    filesum = get_cksum(fname, dfile, pkg)
+def make_link(dfile, cksum):
+    shapath = paths.sources() / "by_sha256"
+    linkpath = shapath / f"{cksum}_{dfile.name}"
+    if not linkpath.is_file():
+        shapath.mkdir(parents = True, exist_ok = True)
+        dfile.link_to(linkpath)
+
+def verify_cksum(dfile, cksum, pkg):
+    pkg.log(f"verifying sha256sums for source '{dfile.name}'... ", "")
+    filesum = get_cksum(dfile, pkg)
     if cksum != filesum:
         pkg.logger.out_plain("")
-        pkg.logger.out_red(f"SHA256 mismatch for '{fname}':\n{filesum}")
+        pkg.logger.out_red(f"SHA256 mismatch for '{dfile.name}':\n{filesum}")
         return False
     else:
-        shapath = paths.sources() / "by_sha256"
-        linkpath = shapath / f"{cksum}_{fname}"
-        if not linkpath.is_file():
-            shapath.mkdir(parents = True, exist_ok = True)
-            dfile.link_to(linkpath)
+        make_link(dfile, cksum)
         pkg.logger.out_plain("OK.")
         return True
 
-def link_cksum(fname, dfile, cksum, pkg):
+def link_cksum(dfile, cksum, pkg):
     shapath = paths.sources() / "by_sha256"
-    linkpath = shapath / f"{cksum}_{fname}"
+    linkpath = shapath / f"{cksum}_{dfile.name}"
     if len(cksum) > 0 and linkpath.is_file():
         linkpath.link_to(dfile)
-        pkg.log(f"using known source '{fname}'")
+        pkg.log(f"using known source '{dfile.name}'")
 
 def get_nameurl(d):
     if isinstance(d, tuple):
@@ -62,8 +65,9 @@ def invoke(pkg):
         url, fname = get_nameurl(d)
         dfile = srcdir / fname
         if dfile.is_file():
-            filesum = get_cksum(fname, dfile, pkg)
+            filesum = get_cksum(dfile, pkg)
             if ck == filesum:
+                make_link(dfile, filesum)
                 dfgood += 1
             else:
                 ino = dfile.stat().st_ino
@@ -78,20 +82,21 @@ def invoke(pkg):
         url, fname = get_nameurl(d)
         dfile = srcdir / fname
         if not dfile.is_file():
-            link_cksum(fname, dfile, ck, pkg)
+            link_cksum(dfile, ck, pkg)
         if not dfile.is_file():
             pkg.log(f"fetching source '{fname}'...")
             try:
-                opener = request.URLopener()
-                # some servers block the default user agent, e.g. netfilter
-                opener.addheader("User-Agent", "cbuild-fetch/4.20.69")
-                fname =  opener.retrieve(url, str(dfile))[0]
-                fname = os.path.basename(fname)
+                rq = request.Request(url, data = None, headers = {
+                    "User-Agent": "cbuild-fetch/4.20.69"
+                })
+                rqf = request.urlopen(rq)
+                with open(dfile, "wb") as df:
+                    df.write(rqf.read())
             except Exception as e:
-                pkg.log_warn(f"error fetching '{fname}': {e}")
+                pkg.log_warn(f"error fetching '{dfile.name}': {e}")
         if not dfile.is_file():
-            pkg.error(f"failed to fetch '{fname}'")
-        if not verify_cksum(fname, dfile, ck, pkg):
+            pkg.error(f"failed to fetch '{dfile.name}'")
+        if not verify_cksum(dfile, ck, pkg):
             errors += 1
 
     if errors > 0:
