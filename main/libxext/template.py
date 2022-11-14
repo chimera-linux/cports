0e335cee2f1d56564d767fdf7239f14b0765c30d
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 20:29:16 +0100

    main/libxext: update to 1.3.5

--- main/libxext/template.py
+++ main/libxext/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxext"
-pkgver = "1.3.4"
+pkgver = "1.3.5"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-malloc0returnsnull"]
@@ -9,8 +9,8 @@ pkgdesc = "X extension library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libXext-{pkgver}.tar.bz2"
-sha256 = "59ad6fcce98deaecc14d39a672cf218ca37aba617c9a0f691cac3bcd28edf82b"
+source = f"$(XORG_SITE)/lib/libXext-{pkgver}.tar.gz"
+sha256 = "1a3dcda154f803be0285b46c9338515804b874b5ccc7a2b769ab7fd76f1035bd"
 
 def post_install(self):
     self.install_license("COPYING")
