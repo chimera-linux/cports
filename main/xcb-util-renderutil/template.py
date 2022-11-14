644dadcc3f3e32adb62ad6887b1ff6adb171e8a1
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 20:36:25 +0100

    main/xcb-util-renderutil: update to 0.3.10

--- main/xcb-util-renderutil/template.py
+++ main/xcb-util-renderutil/template.py
@@ -1,5 +1,5 @@
 pkgname = "xcb-util-renderutil"
-pkgver = "0.3.9"
+pkgver = "0.3.10"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "XCB utilities library - Render extension convenience functions"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xcb.freedesktop.org"
-source = f"{url}/dist/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "c6e97e48fb1286d6394dddb1c1732f00227c70bd1bedb7d1acabefdd340bea5b"
+source = f"{url}/dist/{pkgname}-{pkgver}.tar.gz"
+sha256 = "e04143c48e1644c5e074243fa293d88f99005b3c50d1d54358954404e635128a"
 
 def post_install(self):
     self.install_license("COPYING")
