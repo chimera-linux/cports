86c720f92e6d39a0f60113b64b1acea87b78142f
Daniel Kolesa <q66@chimera-linux.org>
2022-11-15 00:30:14 +0100

    main/libavif: update to 0.11.1

--- main/libavif/template.py
+++ main/libavif/template.py
@@ -1,11 +1,12 @@
 pkgname = "libavif"
-pkgver = "0.10.1"
+pkgver = "0.11.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DAVIF_BUILD_APPS=ON", "-DAVIF_BUILD_GDK_PIXBUF=ON",
     "-DAVIF_CODEC_DAV1D=ON", "-DAVIF_CODEC_AOM=ON",
-    "-DAVIF_BUILD_TESTS=ON", "-DAVIF_ENABLE_WERROR=OFF",
+    "-DAVIF_BUILD_TESTS=OFF", "-DAVIF_ENABLE_WERROR=OFF",
+    "-DAVIF_ENABLE_GTEST=OFF",
 ]
 make_check_target = "avif_test_all"
 hostmakedepends = ["cmake", "ninja", "pkgconf", "gdk-pixbuf-devel"]
@@ -18,8 +19,8 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/AOMediaCodec/libavif"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "66e82854ceb84a3e542bc140a343bc90e56c68f3ecb4fff63e636c136ed9a05e"
-# doesn't pass with current dependencies
+sha256 = "0eb49965562a0e5e5de58389650d434cff32af84c34185b6c9b7b2fccae06d4e"
+# doesn't pass with current dependencies, needs gtest
 options = ["!check"]
 
 def post_install(self):
