47216e85dff3ce0e7e8b0fb307750371e99e838a
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 21:50:30 +0100

    main/perl-io-socket-ssl: update to 2.076

--- main/perl-io-socket-ssl/template.py
+++ main/perl-io-socket-ssl/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-io-socket-ssl"
-pkgver = "2.075"
+pkgver = "2.076"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["gmake", "perl"]
@@ -10,6 +10,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/release/IO-Socket-SSL"
 source = f"$(CPAN_SITE)/IO/IO-Socket-SSL-{pkgver}.tar.gz"
-sha256 = "c30ee2220b1e181a968ebbc81861d0cadf334b001377a44105ae5a8637ddae8c"
+sha256 = "bdd148d9feaef1220251676d7053698fcf446c9850d706fe2e1c90ff232ed874"
 # missing checkdepends
 options = ["!check"]
