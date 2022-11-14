06f95e9614ed938883eaeb3d17216d7155d8fb57
Daniel Kolesa <q66@chimera-linux.org>
2022-11-14 20:39:00 +0100

    main/pixman: update to 0.42.2

--- main/pixman/template.py
+++ main/pixman/template.py
@@ -1,5 +1,5 @@
 pkgname = "pixman"
-pkgver = "0.40.0"
+pkgver = "0.42.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pixman.org"
 source = f"https://www.cairographics.org/releases/{pkgname}-{pkgver}.tar.gz"
-sha256 = "6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc"
+sha256 = "ea1480efada2fd948bc75366f7c349e1c96d3297d09a3fe62626e38e234a625e"
 # needs a lot larger stack than musl default
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=2097152"]}
 
