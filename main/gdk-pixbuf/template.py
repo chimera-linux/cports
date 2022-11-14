cf94454ec78aea65cf020db3e9e0dbf933665cd1
Daniel Kolesa <q66@chimera-linux.org>
2022-11-15 00:25:14 +0100

    main/gdk-pixbuf: update to 2.42.10

--- main/gdk-pixbuf/template.py
+++ main/gdk-pixbuf/template.py
@@ -1,5 +1,5 @@
 pkgname = "gdk-pixbuf"
-pkgver = "2.42.9"
+pkgver = "2.42.10"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -18,8 +18,8 @@ pkgdesc = "Image loading library for GTK"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Projects/GdkPixbuf"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "28f7958e7bf29a32d4e963556d241d0a41a6786582ff6a5ad11665e0347fc962"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-3]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "ee9b6c75d13ba096907a2e3c6b27b61bcd17f5c7ebeab5a5b439d2f2e39fe44b"
 
 @subpackage("gdk-pixbuf-devel")
 def _devel(self):
