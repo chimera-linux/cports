fe4b39102054bcb46cd22252373651a4a52ea338
Daniel Kolesa <q66@chimera-linux.org>
2022-11-15 01:32:51 +0100

    main/gstreamer: update to 1.20.4

--- main/gstreamer/template.py
+++ main/gstreamer/template.py
@@ -1,5 +1,5 @@
 pkgname = "gstreamer"
-pkgver = "1.20.3"
+pkgver = "1.20.4"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -21,7 +21,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "607daf64bbbd5fb18af9d17e21c0d22c4d702fffe83b23cb22d1b1af2ca23a2a"
+sha256 = "67c1edf8c3c339cda5dde85f4f7b953bb9607c2d13ae970e2491c5c4c055ef5f"
 options = ["!cross"]
 
 @subpackage("gstreamer-devel")
