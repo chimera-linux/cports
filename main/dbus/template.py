7a0747c7b88f39dda8e3d1c5f692123ecd6a86ba
Daniel Kolesa <q66@chimera-linux.org>
2022-11-20 18:02:11 +0100

    main/dbus: fix up services, drop elogind dep
    The only practical purpose logind support has is that dbus can
    check if the user is logged in on a seat, which is used for the
    at-console policy stuff in dbus. However, there are very few
    things that use at-console, and it's generally deprecated and
    a bad idea, so we won't be missing it.

--- main/dbus/template.py
+++ main/dbus/template.py
@@ -19,9 +19,8 @@ configure_args = [
 make_cmd = "gmake"
 hostmakedepends = ["gmake", "gperf", "pkgconf", "xmlto"]
 makedepends = [
-    "elogind-devel", "libexpat-devel", "libx11-devel", "libcap-devel"
+    "libexpat-devel", "libx11-devel", "libcap-devel"
 ]
-install_if = ["elogind", f"dbus-libs={pkgver}-r{pkgrel}"]
 pkgdesc = "Message bus system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
