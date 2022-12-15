51976b3695ff3a13fe08b02ccc9a74287d6fdb33
Daniel Kolesa <q66@chimera-linux.org>
2022-12-15 04:17:34 +0100

    main/slang: strip down

--- main/slang/template.py
+++ main/slang/template.py
@@ -4,11 +4,7 @@ pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
 make_dir = "."
-hostmakedepends = ["gmake", "pkgconf", "ncurses-devel"]
-makedepends = [
-    "ncurses-devel", "zlib-devel", "pcre-devel",
-    "libpng-devel", "oniguruma-devel"
-]
+hostmakedepends = ["gmake", "pkgconf"]
 pkgdesc = "S-Lang programming library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
@@ -19,11 +15,6 @@ sha256 = "f9145054ae131973c61208ea82486d5dd10e3c5cdad23b7c4a0617743c8f5a18"
 options = ["!parallel"]
 
 def init_configure(self):
-    sroot = str(self.profile().sysroot / "usr")
-    self.configure_args += [
-        f"--with-z={sroot}", f"--with-pcre={sroot}",
-        f"--with-png={sroot}", f"--with-onig={sroot}"
-    ]
     # force it to use CFLAGS too during linking
     self.configure_env = {
         "LDFLAGS": self.get_cflags(shell = True) + " " + \
@@ -31,7 +22,14 @@ def init_configure(self):
     }
 
 def post_install(self):
-    self.install_license("COPYING")
+    # clear up unnecessary junk, maybe ship in contrib?
+    self.rm(self.destdir / "etc", recursive = True)
+    self.rm(self.destdir / "usr/bin", recursive = True)
+    self.rm(self.destdir / "usr/lib/slang", recursive = True)
+    # documents largely just slsh
+    self.rm(self.destdir / "usr/share/doc", recursive = True)
+    self.rm(self.destdir / "usr/share/man/man1/slsh.1")
+    self.rm(self.destdir / "usr/share/slsh", recursive = True)
 
 @subpackage("slang-devel")
 def _devel(self):
