43adaf959bd3b61c8e6171c6efd97bdd246023d5
q66 <q66@chimera-linux.org>
2022-12-19 22:23:36 +0000

    main/linux-rpi: update to 6.1.0

--- main/linux-rpi/template.py
+++ main/linux-rpi/template.py
@@ -1,54 +1,48 @@
+# update linux-rpi-zfs-bin when bumping
 pkgname = "linux-rpi"
-pkgver = "5.15.78"
+pkgver = "6.1.0"
 pkgrel = 0
 archs = ["aarch64"]
 make_dir = "build"
 # necessary for efistub
 make_env = {"CBUILD_BYPASS_STRIP_WRAPPER": "1"}
-_commit = "46937443a928d407705747a54f6533b013ddbc4d"
+_commit = "b91c411ff7d4a35970d0100ac4257bcf9afd7e12"
 hostmakedepends = ["base-kernel-devel"]
 depends = ["base-kernel"]
-pkgdesc = "Linux kernel for Raspberry Pi 3 and 4 (5.15.x)"
+provides = ["linux"]
+pkgdesc = "Linux kernel for Raspberry Pi 3 and 4 (6.1.x)"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://github.com/raspberrypi/linux"
 source = f"{url}/archive/{_commit}.tar.gz"
-sha256 = "26dd8cef9f082689b8c8ce25ee4e788d8b616044d3b9776518faf8c4b998a265"
+sha256 = "be0be442c494005896b8a293b9763ecd85743252cd13ece40c50b82a2f375a2a"
 # no meaningful checking to be done
 options = [
     "!check", "!debug", "!strip", "!scanrundeps", "!scanshlibs",
     "!lto", "textrels", "foreignelf" # vdso32
 ]
 
+_flavor = "rpi"
+
 if self.profile().cross:
     broken = "linux-devel does not come out right"
 
+def init_configure(self):
+    # generate scriptlets for packaging, just hooking to base-kernel helpers
+    from cbuild.util import linux
+    linux.generate_scriptlets(self, _flavor)
+
 def do_configure(self):
-    self.cp(self.files_path / "config", self.cwd)
-
-    epoch = self.source_date_epoch or 0
-    args = []
-
-    if self.profile().cross:
-        args += [f"CROSS_COMPILE={self.profile().triplet}"]
-
-    self.do(
-        "chimera-buildkernel",
-        "prepare",
-        f"ARCH=arm64",
-        f"CONFIG_FILE={self.chroot_cwd}/config",
-        f"OBJDIR={self.make_dir}",
-        f"JOBS={self.make_jobs}",
-        f"LOCALVERSION=-{pkgrel}-rpi",
-        f"EPOCH={epoch}",
-        *args
-    )
+    from cbuild.util import linux
+    linux.configure(self, _flavor)
 
 def do_build(self):
-    self.do("chimera-buildkernel", "build", env = make_env)
+    from cbuild.util import linux
+    linux.build(self, _flavor)
 
 def do_install(self):
-    self.do("chimera-buildkernel", "install", self.chroot_destdir)
+    from cbuild.util import linux
+    linux.install(self, _flavor)
 
 @subpackage("linux-rpi-devel")
 def _devel(self):
