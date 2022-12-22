cc096eee2e95427b11d45d57df283728d6215ef2
Daniel Kolesa <q66@chimera-linux.org>
2022-12-22 01:33:01 +0100

    main/*-cross: disable ubsan
    This mirrors the native packages.

--- main/musl-cross/template.py
+++ main/musl-cross/template.py
@@ -13,8 +13,8 @@ license = "MIT"
 url = "http://www.musl-libc.org"
 source = f"http://www.musl-libc.org/releases/musl-{pkgver}.tar.gz"
 sha256 = "7d5b0b6062521e4627e099e4c9dc8248d32a30285e959b7eecaa780cf8cfd4a4"
-# segfaults otherwise
-hardening = ["!scp"]
+# mirrors musl
+hardening = ["!scp", "!int"]
 # crosstoolchain
 options = ["!cross", "!check", "!lto", "brokenlinks"]
 
