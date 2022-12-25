4d5a5f4e98aa99fd484882a34bb8acb8cdea1e21
Daniel Kolesa <q66@chimera-linux.org>
2022-12-25 02:31:01 +0100

    main/opensbi: adjust hardening

--- main/opensbi/template.py
+++ main/opensbi/template.py
@@ -14,6 +14,7 @@ license = "BSD-2-Clause"
 url = "https://github.com/riscv-software-src/opensbi"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "d183cb890130983a4f01e75fc03ee4f7ea0e16a7923b8af9c6dff7deb2fedaec"
+hardening = ["!vis", "!int"]
 # no test suite
 options = ["!check", "!lto", "!strip", "!debug", "foreignelf"]
 
@@ -31,6 +32,3 @@ def do_install(self):
         )
 
     self.install_license("COPYING.BSD")
-
-# FIXME visibility
-hardening = ["!vis"]
