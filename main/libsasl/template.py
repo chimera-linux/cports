22ea7f6154794a8d8e6ac64d32f275d1e312285b
q66 <q66@chimera-linux.org>
2023-01-01 22:17:06 +0000

    main/libsasl: drop lmdb database

--- main/libsasl/template.py
+++ main/libsasl/template.py
@@ -8,17 +8,18 @@ configure_args = [
     "--enable-ntlm", "--with-configdir=/etc/sasl2:/etc/sasl:/usr/lib/sasl2",
     "--disable-otp", "--disable-srp", "--disable-srp-setpass",
     "--disable-krb4", "--with-devrandom=/dev/random",
-    "--with-dblib=lmdb",
+    "--with-dblib=none",
 ]
 make_cmd = "gmake"
 hostmakedepends = ["gmake", "pkgconf", "automake", "libtool"]
-makedepends = ["lmdb-devel"]
 pkgdesc = "Cyrus SASL (runtime library)"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause-Attribution"
 url = "https://www.cyrusimap.org/sasl"
 source = f"https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-{pkgver}/cyrus-sasl-{pkgver}.tar.gz"
 sha256 = "7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"
+# does not mark api visibility
+hardening = ["!vis"]
 options = ["!cross"]
 
 def pre_configure(self):
@@ -34,6 +35,3 @@ def post_install(self):
 @subpackage("libsasl-devel")
 def _devel(self):
     return self.default_devel()
-
-# FIXME visibility
-hardening = ["!vis"]
