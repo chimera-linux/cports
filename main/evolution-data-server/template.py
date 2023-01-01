3c63ea395c4435e0cf8c41eb2f25ff1c516c2c5d
q66 <q66@chimera-linux.org>
2023-01-01 22:36:51 +0000

    main/evolution-data-server: update to 3.46.2 + disable openldap
    Also reenable tests.

--- main/evolution-data-server/template.py
+++ main/evolution-data-server/template.py
@@ -1,5 +1,5 @@
 pkgname = "evolution-data-server"
-pkgver = "3.46.1"
+pkgver = "3.46.2"
 pkgrel = 0
 build_style = "cmake"
 # TODO: libgdata
@@ -7,6 +7,7 @@ configure_args = [
     "-DENABLE_GOOGLE=OFF", "-DWITH_LIBDB=OFF",
     "-DSYSCONF_INSTALL_DIR=/etc", "-DENABLE_INTROSPECTION=ON",
     "-DENABLE_VALA_BINDINGS=ON",
+    "-DWITH_OPENLDAP=OFF", # don't depend on shit software
 ]
 hostmakedepends = [
     "cmake", "ninja", "pkgconf", "flex", "glib-devel", "gperf",
@@ -17,7 +18,7 @@ makedepends = [
     "webkitgtk-devel", "webkitgtk4-devel", "libsecret-devel", "nss-devel",
     "nspr-devel", "gnome-online-accounts-devel", "sqlite-devel",
     "libgweather-devel", "libsoup-devel", "json-glib-devel",
-    "vala-devel", "openldap-devel",
+    "vala-devel",
 ]
 checkdepends = ["dbus"]
 pkgdesc = "Centralized access to appointments and contacts"
@@ -25,11 +26,12 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/evolution-data-server"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "c55e72cff4190b42e63dd6eabc6dce48a1a1f79040531f1af6d51c1efa4aa6eb"
+sha256 = "f9bd52008b7e535d816920cfcec70a0e5562bde28af71f996e203ff6a86e46e0"
 # internally passes some stuff that only goes to linker
 tool_flags = {"CFLAGS": ["-Wno-unused-command-line-argument"]}
-# fail test-book-client-custom-summary
-options = ["!cross", "!check"]
+# no visibility markers
+hardening = ["!vis"]
+options = ["!cross"]
 
 def post_install(self):
     self.rm(self.destdir / "usr/lib/systemd", recursive = True)
@@ -37,6 +39,3 @@ def post_install(self):
 @subpackage("evolution-data-server-devel")
 def _devel(self):
     return self.default_devel()
-
-# FIXME visibility
-hardening = ["!vis"]
