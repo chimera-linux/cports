87ce347749325677ac92af8bf1220eb9fbf66be1
q66 <q66@chimera-linux.org>
2023-01-01 17:06:14 +0000

    main/less: fix and enable cfi

--- main/less/template.py
+++ main/less/template.py
@@ -21,6 +21,3 @@ def post_install(self):
     self.install_link("zless", "usr/bin/xzless")
     self.install_link("zless", "usr/bin/lzless")
     self.install_link("zless", "usr/bin/zstdless")
-
-# FIXME visibility
-hardening = ["!vis"]
