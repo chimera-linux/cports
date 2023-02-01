683b121e71bbb63c93e8651577d89b51a5e8479d
Louis Hampton <louis@switch-ev.com>
2023-02-01 12:35:38 +0000

    contrib/psmisc: update to 23.6

--- contrib/psmisc/template.py
+++ contrib/psmisc/template.py
@@ -1,5 +1,5 @@
 pkgname = "psmisc"
-pkgver = "23.5"
+pkgver = "23.6"
 pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
@@ -11,10 +11,6 @@ maintainer = "roastveg <louis@hamptonsoftworks.com>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.com/psmisc/psmisc"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "dc37ecc2f7e4a90a94956accc6e1c77adb71316b7c9cbd39b26738db0c3ae58b"
+sha256 = "257dde06159a4c49223d06f1cccbeb68933a4514fc8f1d77c64b54f0d108822a"
 tool_flags = {"LDFLAGS": ["-lgnu_getopt"], "CFLAGS": ["-Dgetopt_long_only=gnu_getopt_long_only"]}
 hardening = ["vis", "cfi"]
-
-def pre_check(self):
-    # ERROR: global config file /builddir/psmisc-23.5/testsuite/global-conf.exp not found.
-    (self.cwd / "testsuite/global-conf.exp").touch()
