4024ba95302e20a45fe653acce9f116620845820
q66 <q66@chimera-linux.org>
2023-02-19 18:25:02 +0000

    main/ugetopt: fix manpage name

--- main/ugetopt/template.py
+++ main/ugetopt/template.py
@@ -8,6 +8,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/chimera-linux/ugetopt"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "01900060d4fcb93cfb7b9940abc81a8fdd04ad4dc1e5696d365cc19557243ee2"
+sha256 = "19c15c87bc3573fbbf8bdb7dda567c74b233d3fac8c6eb16af7d544f0bf470e4"
 hardening = ["vis", "cfi"]
 options = ["bootstrap"]
