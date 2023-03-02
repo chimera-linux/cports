efa7836f8e2ad98fbafc3d41746676aa416ab166
Daniel Kolesa <q66@chimera-linux.org>
2023-03-02 17:41:31 +0100

    main/llvm: also disable int for ppc64le
    It seems otherwise we trigger something in LLVM PowerPC codegen
    while building e.g. graphviz.

--- main/llvm/template.py
+++ main/llvm/template.py
@@ -53,10 +53,12 @@ debug_level = 1
 # rpath and fakeroot effectively overrides rpath, so disable that
 options = ["bootstrap", "!check", "!installroot"]
 
-if self.profile().arch == "aarch64" or self.profile().arch == "riscv64":
-    # disable ubsan integer checks on aarch64 and riscv64
+if self.profile().arch != "x86_64":
+    # disable ubsan integer checks on aarch64/ppc64/riscv64
     # when lto-linking lld with ubsan'd clang/lld, it causes
     # that to crash in 'AArch64 Instruction Selection'
+    # (on ppc64le, 'PowerPC Instruction Selection' e.g.
+    # when building graphviz)
     # while on riscv it prevents rust from building
     hardening = ["!int"]
 
