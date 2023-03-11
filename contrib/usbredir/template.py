f11e70d96122a97a48ffbd108300a90353509e07
eater <=@eater.me>
2023-03-11 23:24:44 +0100

    contrib/usbredir: new package (0.13.0)

---
+++ contrib/usbredir/template.py
@@ -0,0 +1,17 @@
+pkgname = "usbredir"
+pkgver = "0.13.0"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
+makedepends = ["libusb-devel", "glib-devel"]
+pkgdesc = "Protocol for redirection USB traffic"
+maintainer = "eater <=@eater.me>"
+license = "GPL-2.0"
+url = "https://gitlab.freedesktop.org/spice/usbredir"
+source = f"https://www.spice-space.org/download/usbredir/usbredir-{pkgver}.tar.xz"
+sha256 = "4ba6faa02c0ae6deeb4c53883d66ab54b3a5899bead42ce4ded9568b9a7dc46e"
+
+
+@subpackage("usbredir-devel")
+def _devel(self):
+    return self.default_devel()
