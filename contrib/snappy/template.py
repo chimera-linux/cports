d81f74549b8eff749f9089c93581491a121a014e
eater <=@eater.me>
2023-03-11 23:21:48 +0100

    contrib/snappy: new package (1.1.10)

---
+++ contrib/snappy/template.py
@@ -0,0 +1,21 @@
+pkgname = "snappy"
+pkgver = "1.1.10"
+pkgrel = 0
+build_style = "cmake"
+# tests depend on gtest
+configure_args = ["-DBUILD_SHARED_LIBS=1", "-DSNAPPY_BUILD_TESTS=0", "-DSNAPPY_BUILD_BENCHMARKS=0"]
+hostmakedepends = ["cmake", "ninja"]
+pkgdesc = "Fast compressor/decompressor"
+maintainer = "eater <=@eater.me>"
+license = "BSD-3-Clause"
+url = "https://google.github.io/snappy"
+source = f"https://github.com/google/snappy/archive/{pkgver}.tar.gz"
+sha256 = "49d831bffcc5f3d01482340fe5af59852ca2fe76c3e05df0e67203ebbe0f1d90"
+tool_flags = {'CXXFLAGS': ['-Wno-sign-compare']}
+# tests depend on gtest
+options = ["!check"]
+
+
+@subpackage("snappy-devel")
+def _devel(self):
+    return self.default_devel()
