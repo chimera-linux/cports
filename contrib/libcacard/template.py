b7a1a4dd2d9e04f83668414f5015452afa68c74a
eater <=@eater.me>
2023-03-11 23:17:18 +0100

    contrib/libcacard: new package (2.8.1)

---
+++ contrib/libcacard/template.py
@@ -0,0 +1,16 @@
+pkgname = "libcacard"
+pkgver = "2.8.1"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
+makedepends = ["glib-devel", "nss-devel", "pcsc-lite-devel"]
+pkgdesc = "Common Access Card library"
+maintainer = "eater <=@eater.me>"
+license = "LGPL-2.1-or-later"
+url = "https://gitlab.freedesktop.org/spice/libcacard"
+source = f"https://www.spice-space.org/download/libcacard/libcacard-{pkgver}.tar.xz"
+sha256 = "fbbf4de8cb7db5bdff5ecb672ff0dbe6939fb9f344b900d51ba6295329a332e7"
+
+@subpackage("libcacard-devel")
+def _devel(self):
+    return self.default_devel()
