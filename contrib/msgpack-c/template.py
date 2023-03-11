6af8338ca0f0e03f1a9b03c508034d85b0add4f2
yopito <pierre.bourgin@free.fr>
2023-03-11 22:48:39 +0100

    contrib/msgpack-c: new package (6.0.0)

---
+++ contrib/msgpack-c/template.py
@@ -0,0 +1,16 @@
+pkgname = "msgpack-c"
+pkgver = "6.0.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = ["zlib-devel"]
+checkdepends = ["gtest-devel"]
+pkgdesc = "Binary-based efficient object serialization library (C support)"
+license = "Apache-2.0"
+url = "https://msgpack.org"
+source = f"https://github.com/msgpack/msgpack-c/archive/c-{pkgver}.tar.gz"
+sha256 = "af6f3cf25edb220aa2140b09bb5bdd73ddf00938194bd94ebe5c92090cccb466"
+
+@subpackage("msgpack-c-devel")
+def _devel(self):
+    return self.default_devel()
