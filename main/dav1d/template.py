751b47eaff503b6637daf1a2d94ce5b006c7ab93
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 18:56:11 +0100

    main/dav1d: update to 1.1.0

--- main/dav1d/template.py
+++ main/dav1d/template.py
@@ -1,5 +1,5 @@
 pkgname = "dav1d"
-pkgver = "1.0.0"
+pkgver = "1.1.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://code.videolan.org/videolan/dav1d"
 source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "047b8229511a82b5718a1d34c86c067b078efd02f602986d2ed09b23182ec136"
+sha256 = "b163791a587c083803a3db2cd18b4fbaf7fb865b47d038c4869ffef7722b6b16"
 # FIXME cfi, int
 hardening = ["vis", "!cfi", "!int"]
 
