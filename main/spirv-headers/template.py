766994bc1c48fe80e9a9615aa3a9bf035b800f03
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 15:33:48 +0100

    main/spirv-headers: update to 1.3.239.0

--- main/spirv-headers/template.py
+++ main/spirv-headers/template.py
@@ -1,5 +1,5 @@
 pkgname = "spirv-headers"
-pkgver = "1.3.231.1"
+pkgver = "1.3.239.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:spirv-headers"
 url = "https://github.com/KhronosGroup/SPIRV-Headers"
 source = f"{url}/archive/refs/tags/sdk-{pkgver}.tar.gz"
-sha256 = "fc340700b005e9a2adc98475b5afbbabd1bc931f789a2afd02d54ebc22522af3"
+sha256 = "fdaf6670e311cd1c08ae90bf813e89dd31630205bc60030ffd25fb0af39b51fe"
 # no test suite
 options = ["!check"]
 
