96f597029632c2b148816aaa448a829779ef033d
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 16:05:46 +0100

    main/cmake: update to 2.36.0

--- main/cmake/template.py
+++ main/cmake/template.py
@@ -1,5 +1,5 @@
 pkgname = "cmake"
-pkgver = "3.24.3"
+pkgver = "3.26.0"
 pkgrel = 0
 build_style = "configure"
 configure_args = [
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://cmake.org"
 source = f"https://www.cmake.org/files/v{pkgver[:-2]}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "b53aa10fa82bff84ccdb59065927b72d3bee49f4d86261249fc0984b3b367291"
+sha256 = "4256613188857e95700621f7cdaaeb954f3546a9249e942bc2f9b3c26e381365"
 # prevent cmake self-bootstrap false positive nonsense
 tool_flags = {
     "CXXFLAGS": ["-Wno-unused-command-line-argument"],
