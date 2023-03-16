2219dda54e8ba60626d94b4bd9dd6f8e4bfe7b80
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 16:21:04 +0100

    main/ncurses: update to 6.4

--- main/ncurses/template.py
+++ main/ncurses/template.py
@@ -1,5 +1,5 @@
 pkgname = "ncurses"
-pkgver = "6.3"
+pkgver = "6.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -23,7 +23,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://www.gnu.org/software/ncurses"
 source = f"$(GNU_SITE)/ncurses/{pkgname}-{pkgver}.tar.gz"
-sha256 = "97fc51ac2b085d4cde31ef4d2c3122c21abc217e9090a43a30fc5ec21684e059"
+sha256 = "6931283d9ac87c5073f30b6290c4c75f21632bb4fc3603ac8100812bed248159"
 tool_flags = {"CFLAGS": ["-fPIC"],}
 # FIXME int; prevents some chroots from working
 hardening = ["!int"]
