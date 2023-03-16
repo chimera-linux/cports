7a8b92e803ee9e2e958e29a75e31f28d6be9a1ef
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 01:42:20 +0100

    main/zenity: update to 3.44.0

--- main/zenity/template.py
+++ main/zenity/template.py
@@ -1,5 +1,5 @@
 pkgname = "zenity"
-pkgver = "3.43.0"
+pkgver = "3.44.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "gettext-tiny", "itstool"]
@@ -12,4 +12,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Projects/Zenity"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "b0d7ca1e0c1868fa18f05c210260d8a7be1f08ee13b7f5cfdbab9b61fa16f833"
+sha256 = "c15582301ed90b9d42ce521dbccf99a989f22f12041bdd5279c6636da99ebf65"
