61251a869934c116b3f84af3b7930c1379293787
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 16:17:26 +0100

    main/byacc: update to 20230219

--- main/byacc/template.py
+++ main/byacc/template.py
@@ -1,5 +1,5 @@
 pkgname = "byacc"
-pkgver = "20221106"
+pkgver = "20230219"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--program-transform=s,^,b,"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:byacc"
 url = "http://invisible-island.net/byacc"
 source = f"https://invisible-island.net/archives/{pkgname}/{pkgname}-{pkgver}.tgz"
-sha256 = "a899be227bbcac9cf7700f7dbb5a8494688f1f9f0617b510762daeace47b9d12"
+sha256 = "36b972a6d4ae97584dd186925fbbc397d26cb20632a76c2f52ac7653cd081b58"
 hardening = ["vis", "cfi"]
 
 def post_install(self):
