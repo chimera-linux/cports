5ab4f0502e43a4543f9136c8d664a2ed26958dce
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 01:58:17 +0100

    main/wayland-protocols: update to 1.31

--- main/wayland-protocols/template.py
+++ main/wayland-protocols/template.py
@@ -1,5 +1,5 @@
 pkgname = "wayland-protocols"
-pkgver = "1.29"
+pkgver = "1.31"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "wayland-progs"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://wayland.freedesktop.org"
 source = f"https://github.com/wayland-project/{pkgname}/archive/{pkgver}.tar.gz"
-sha256 = "4a85786ae69cd6d53bbe9278572f3c3d6ea342875ea444960edb6089237c3a18"
+sha256 = "04d3f66eca99d638ec8dbfdfdf79334290e22028f7d0b04c7034d9ef18a3248a"
 
 def post_install(self):
     self.install_license("COPYING")
