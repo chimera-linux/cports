7e525bc6130ea2b87f870eba673d2748ebeb9d58
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 18:41:54 +0100

    main/libbluray: update to 1.3.4

--- main/libbluray/template.py
+++ main/libbluray/template.py
@@ -1,5 +1,5 @@
 pkgname = "libbluray"
-pkgver = "1.3.3"
+pkgver = "1.3.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-optimizations", "--disable-bdjava-jar"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.videolan.org/developers/libbluray.html"
 source = f"https://download.videolan.org/pub/videolan/{pkgname}/{pkgver}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "58ff52cdcee64c55dcc3c777a1c39fb41abd951b927978e4d2b6811b9193a488"
+sha256 = "478ffd68a0f5dde8ef6ca989b7f035b5a0a22c599142e5cd3ff7b03bbebe5f2b"
 
 @subpackage("libbluray-devel")
 def _devel(self):
