e779ba303212426217f9f3e6547bcba09e5007cb
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 02:04:04 +0100

    main/libxdamage: update to 1.1.6

--- main/libxdamage/template.py
+++ main/libxdamage/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxdamage"
-pkgver = "1.1.5"
+pkgver = "1.1.6"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "Xdamage extension Library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libXdamage-{pkgver}.tar.bz2"
-sha256 = "b734068643cac3b5f3d2c8279dd366b5bf28c7219d9e9d8717e1383995e0ea45"
+source = f"$(XORG_SITE)/lib/libXdamage-{pkgver}.tar.gz"
+sha256 = "2afcc139eb6eb926ffe344494b1fc023da25def42874496e6e6d3aa8acef8595"
 
 def post_install(self):
     self.install_license("COPYING")
