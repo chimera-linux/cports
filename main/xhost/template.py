694d5c820b86a7c672734daafcf20e0393fc4ce7
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 02:35:57 +0100

    main/xhost: update to 1.0.9

--- main/xhost/template.py
+++ main/xhost/template.py
@@ -1,15 +1,15 @@
 pkgname = "xhost"
-pkgver = "1.0.8"
+pkgver = "1.0.9"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["pkgconf", "gettext-tiny-devel"]
 makedepends = ["libx11-devel", "libxmu-devel"]
 pkgdesc = "X server access control program"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "a2dc3c579e13674947395ef8ccc1b3763f89012a216c2cc6277096489aadc396"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
+sha256 = "ca850367593fcddc4bff16de7ea1598aa4f6817daf5a803a1258dff5e337f7c3"
 hardening = ["vis", "cfi"]
 
 def post_install(self):
