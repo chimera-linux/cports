8f223b956a460ce03d581c2fe620d79df3f7798f
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 15:59:55 +0100

    main/texinfo: update to 7.0.2

--- main/texinfo/template.py
+++ main/texinfo/template.py
@@ -1,5 +1,5 @@
 pkgname = "texinfo"
-pkgver = "7.0"
+pkgver = "7.0.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -14,6 +14,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/texinfo"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "20744b82531ce7a04d8cee34b07143ad59777612c3695d5855f29fba40fbe3e0"
+sha256 = "f211ec3261383e1a89e4555a93b9d017fe807b9c3992fb2dff4871dae6da54ad"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
