7192259d208ff7fda433edfeaefc68d60c378a9a
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 19:26:26 +0100

    main/libsm: update to 1.2.4

--- main/libsm/template.py
+++ main/libsm/template.py
@@ -1,5 +1,5 @@
 pkgname = "libsm"
-pkgver = "1.2.3"
+pkgver = "1.2.4"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "X session management library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libSM-{pkgver}.tar.bz2"
-sha256 = "2d264499dcb05f56438dee12a1b4b71d76736ce7ba7aa6efbf15ebb113769cbb"
+source = f"$(XORG_SITE)/lib/libSM-{pkgver}.tar.gz"
+sha256 = "51464ce1abce323d5b6707ceecf8468617106e1a8a98522f8342db06fd024c15"
 
 def post_install(self):
     self.install_license("COPYING")
