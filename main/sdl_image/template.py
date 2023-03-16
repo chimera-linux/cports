343e004c83dedf763e71c340a6fa01ea0b595299
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 20:25:08 +0100

    main/sdl_image: update to 2.6.3

--- main/sdl_image/template.py
+++ main/sdl_image/template.py
@@ -1,5 +1,5 @@
 pkgname = "sdl_image"
-pkgver = "2.6.2"
+pkgver = "2.6.3"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -22,7 +22,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Zlib"
 url = "https://libsdl.org/projects/SDL_image"
 source = f"{url}/release/SDL2_image-{pkgver}.tar.gz"
-sha256 = "48355fb4d8d00bac639cd1c4f4a7661c4afef2c212af60b340e06b7059814777"
+sha256 = "931c9be5bf1d7c8fae9b7dc157828b7eee874e23c7f24b44ba7eff6b4836312c"
 # no check target
 options = ["!check"]
 
