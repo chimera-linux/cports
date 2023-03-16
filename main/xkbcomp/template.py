bf0ae8000a2d2b6f5c7f76d622316a6f84ce5661
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 02:30:17 +0100

    main/xkbcomp: update to 1.4.6

--- main/xkbcomp/template.py
+++ main/xkbcomp/template.py
@@ -1,5 +1,5 @@
 pkgname = "xkbcomp"
-pkgver = "1.4.5"
+pkgver = "1.4.6"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "bison"]
@@ -8,8 +8,8 @@ pkgdesc = "XKBD keymap compiler"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "6851086c4244b6fd0cc562880d8ff193fb2bbf1e141c73632e10731b31d4b05e"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
+sha256 = "b216a2c8c0eab83f3dc4a3d5ee2bdf7827b30e49c8907035d0f222138eca0987"
 hardening = ["vis", "cfi"]
 
 def post_install(self):
