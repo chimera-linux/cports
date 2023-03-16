0b2e8899e3c8f4f8ed21dc9c0a21377d31ff5e44
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 01:53:17 +0100

    main/python-setuptools: update to 67.6.0

--- main/python-setuptools/template.py
+++ main/python-setuptools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-setuptools"
-pkgver = "65.5.1"
+pkgver = "67.6.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-devel"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pypa/setuptools"
 source = f"$(PYPI_SITE)/s/setuptools/setuptools-{pkgver}.tar.gz"
-sha256 = "e197a19aa8ec9722928f2206f8de752def0e4c9fc6953527360d1c36d94ddb2f"
+sha256 = "2ee892cd5f29f3373097f5a814697e397cf3ce313616df0af11231e2ad118077"
 env = {
     "SETUPTOOLS_INSTALL_WINDOWS_SPECIFIC_FILES": "0",
     "SETUPTOOLS_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT": "1"
