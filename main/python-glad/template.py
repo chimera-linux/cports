8f0d15f892d3de2c2bf6a790cedcc71fed04f4e9
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 15:55:22 +0100

    main/python-glad: update to 2.0.4

--- main/python-glad/template.py
+++ main/python-glad/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-glad"
-pkgver = "2.0.2"
+pkgver = "2.0.4"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools", "python-jinja2"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://glad.dav1d.de"
 source = f"https://github.com/Dav1dde/glad/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0326d59b1ccf8ccae1fface5bfb71bec1bb41fbe2341e722b31f6acac3068aeb"
+sha256 = "02629644c242dcc27c58222bd2c001d5e2f3765dbbcfd796542308bddebab401"
 # unpackaged checkdepends
 options = ["!check"]
 
