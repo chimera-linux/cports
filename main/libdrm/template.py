d519f447eb015af090becb6380d1a6b428b47242
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 18:46:01 +0100

    main/libdrm: update to 2.4.115

--- main/libdrm/template.py
+++ main/libdrm/template.py
@@ -1,5 +1,5 @@
 pkgname = "libdrm"
-pkgver = "2.4.114"
+pkgver = "2.4.115"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -13,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://dri.freedesktop.org"
 source = f"https://gitlab.freedesktop.org/mesa/drm/-/archive/{pkgname}-{pkgver}/drm-{pkgname}-{pkgver}.tar.gz"
-sha256 = "919ccacc5d9211840b9433d16ee825b1bc7108e7d63e05557969a17c3532d764"
+sha256 = "fd8b7c6e4e40d13c32ab67ace2cb6c71e08c9ca53124ff33ccadd74f604117e3"
 # FIXME cfi int
 hardening = ["vis", "!cfi", "!int"]
 
