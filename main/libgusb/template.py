1281cdb3c8b00ef750d85a5ebeccbe5eba15ee4e
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 18:52:08 +0100

    main/libgusb: update to 0.4.5

--- main/libgusb/template.py
+++ main/libgusb/template.py
@@ -1,5 +1,5 @@
 pkgname = "libgusb"
-pkgver = "0.4.2"
+pkgver = "0.4.5"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocs=false", "-Dintrospection=true"]
@@ -12,8 +12,8 @@ pkgdesc = "GLib wrapper around libusb"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/hughsie/libgusb"
-source = f"http://people.freedesktop.org/~hughsient/releases/{pkgname}-{pkgver}.tar.xz"
-sha256 = "02d3a992a0cd16c46a346439334417617cd7cd5b2ccc5fe0fe998e9ffb8d5d8a"
+source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "bc8c6328289f057c8f73b07c1ba6251de96029787309f2672ca252ca90ace1b2"
 # no access to usb in container
 options = ["!check", "!cross"]
 
