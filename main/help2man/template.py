ed0100b0990380554cf767ec98796050c3767fd5
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 15:31:35 +0100

    main/help2man: update to 1.49.3

--- main/help2man/template.py
+++ main/help2man/template.py
@@ -1,5 +1,5 @@
 pkgname = "help2man"
-pkgver = "1.49.2"
+pkgver = "1.49.3"
 pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/help2man"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "9e2e0e213a7e0a36244eed6204d902b6504602a578b6ecd15268b1454deadd36"
+sha256 = "4d7e4fdef2eca6afe07a2682151cea78781e0a4e8f9622142d9f70c083a2fd4f"
 hardening = ["vis", "cfi"]
 # no test suite
 options = ["!check"]
