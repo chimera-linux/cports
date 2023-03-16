f7b4ae6fd1c1473399e7127915613d59cc423dfa
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 18:59:21 +0100

    main/libpng: update to 1.6.39

--- main/libpng/template.py
+++ main/libpng/template.py
@@ -1,5 +1,5 @@
 pkgname = "libpng"
-pkgver = "1.6.38"
+pkgver = "1.6.39"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Libpng"
 url = "http://www.libpng.org/pub/png/libpng.html"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "b3683e8b8111ebf6f1ac004ebb6b0c975cd310ec469d98364388e9cedbfa68be"
+sha256 = "1f4696ce70b4ee5f85f1e1623dc1229b210029fa4b7aee573df3e2ba7b036937"
 
 def post_install(self):
     self.install_license("LICENSE")
