43ecf6c3d613e312298737d941e60fba28626c01
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 14:43:00 +0100

    main/rust-bootstrap: update to 1.68.0

--- main/rust-bootstrap/template.py
+++ main/rust-bootstrap/template.py
@@ -1,5 +1,5 @@
 pkgname = "rust-bootstrap"
-pkgver = "1.67.0"
+pkgver = "1.68.0"
 pkgrel = 0
 # satisfy revdeps
 makedepends = ["zlib", "ncurses-libs"]
@@ -19,23 +19,23 @@ options = ["!strip"]
 match self.profile().arch:
     case "aarch64":
         sha256 = [
-            "b28df0b866452d92c2b4e0473c4b14485b6135450a5a40ff8d20b59f38a73f2e",
-            "466aa4f5d04f829ad876a01d1a864f75f668bce6f55f3c1321951a3e655f1906",
+            "bb5b6c0ec01906c1217b02dfe45fe9b39cf45104a53de0de5bc47d092d4034e1",
+            "0bd0d4ce5f1413060207ef76b5c255520b9887c00cde3ae8d5c92a80166344ad",
         ]
     case "ppc64le":
         sha256 = [
-            "7cf7dda7b6a0a7c5c3360a778489257e7e8f0914feb8bd8763f8171dffaa9d9a",
-            "2d7f80a60b9f3a399ba708782019ba2bd3df8e98ea3c846ce57dee172364d432",
+            "59a39cc5ecae69c995346dc04bb6e03ac770299fba873870ead705f7fcdd1051",
+            "10cda47222eb1aa865feac7a3d8bbc5227a730075b2dd820a5ee611d9596f4ad",
         ]
     case "riscv64":
         sha256 = [
-            "eb7cbf08fdcf5d097983f4449b0f86bd59e503cee2e21cf21a9cb11ee2e8dab1",
-            "35c304e7ba96b8245050ae40b8799d747e30f40c87108004078e71ee4c658006",
+            "1a6864b42d5178a40f15b71c7d233312b328699e7e26aefd4c0c6228f1bc99f4",
+            "6549ec1fd47ae22acfab637c1fda6286c39180ee5fe84bb0b7aa3e81fe946379",
         ]
     case "x86_64":
         sha256 = [
-            "610a6df0d9f23e0cd1fc9cfcdb67479d8518d422527255205c9699240ba8339e",
-            "d010e4bd8c9acc568391ad89e43e14a863ce4a550ff600a96b34f95f6b365adf",
+            "374af4f128be70a4468df1faa765f6bdda10f9e066655d6f87985e11e6fa991b",
+            "e4817030c3d7f13e82154f83e335bcf3363a9085db95d3354f2eb614e47f1058",
         ]
     case _:
         broken = f"not yet built for {self.profile().arch}"
