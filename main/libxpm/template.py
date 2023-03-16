180c91d468a1288ffd7492dc02811288f4564274
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 02:22:30 +0100

    main/libxpm: update to 3.5.15

--- main/libxpm/template.py
+++ main/libxpm/template.py
@@ -1,15 +1,16 @@
 pkgname = "libxpm"
-pkgver = "3.5.13"
+pkgver = "3.5.15"
 pkgrel = 0
 build_style = "gnu_configure"
+configure_args = ["--disable-open-zfile"]
 hostmakedepends = ["pkgconf", "gettext-tiny"]
 makedepends = ["xorgproto", "libsm-devel", "libxext-devel", "libxt-devel"]
 pkgdesc = "X PixMap library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libXpm-{pkgver}.tar.bz2"
-sha256 = "9cd1da57588b6cb71450eff2273ef6b657537a9ac4d02d0014228845b935ac25"
+source = f"$(XORG_SITE)/lib/libXpm-{pkgver}.tar.gz"
+sha256 = "2a9bd419e31270593e59e744136ee2375ae817322447928d2abb6225560776f9"
 
 def post_install(self):
     self.install_license("COPYING")
