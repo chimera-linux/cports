65beabee4616e2ca0baea2e3f8e3e6223e12b3e7
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 02:38:45 +0100

    main/xserver-xorg-core: update to 21.1.7

--- main/xserver-xorg-core/template.py
+++ main/xserver-xorg-core/template.py
@@ -1,5 +1,5 @@
 pkgname = "xserver-xorg-core"
-pkgver = "21.1.4"
+pkgver = "21.1.7"
 pkgrel = 0
 build_style = "meson"
 _fontroot = "/usr/share/fonts"
@@ -35,7 +35,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT AND BSD-3-Clause"
 url = "https://xorg.freedesktop.org"
 source = f"https://gitlab.freedesktop.org/xorg/xserver/-/archive/xorg-server-{pkgver}/xserver-xorg-server-{pkgver}.tar.gz"
-sha256 = "63874e04941fae33f837a3456b006602778e37f804a4d53a64a41c047f867401"
+sha256 = "de29023ea57a905794820030c4d40fd5c88d8f2e91a5fe8f3a5bddddac1ccae3"
 tool_flags = {
     "CFLAGS": ["-D_GNU_SOURCE", "-D__uid_t=uid_t", "-D__gid_t=gid_t"],
     "LDFLAGS": ["-Wl,-z,lazy"] # must be set for modules to work
