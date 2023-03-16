0758877d51f095e814624685c682f82dc9fdb4fd
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 19:42:29 +0100

    main/libtiff: update to 4.5.0

--- main/libtiff/template.py
+++ main/libtiff/template.py
@@ -1,5 +1,5 @@
 pkgname = "libtiff"
-pkgver = "4.4.0"
+pkgver = "4.5.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-cxx", "--without-x"]
@@ -15,13 +15,13 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "libtiff"
 url = "http://libtiff.maptools.org"
 source = f"http://download.osgeo.org/{pkgname}/tiff-{pkgver}.tar.gz"
-sha256 = "917223b37538959aca3b790d2d73aa6e626b688e02dcda272aec24c2f498abed"
+sha256 = "c7a1d9296649233979fa3eacffef3fa024d73d05d589cb622727b5b08c423464"
 
 def post_install(self):
     for f in (self.destdir / "usr/share/man/man3").glob("*.3tiff"):
         self.mv(f, f.with_suffix(".3"))
 
-    self.install_license("COPYRIGHT")
+    self.install_license("LICENSE.md")
 
 @subpackage("libtiff-devel")
 def _devel(self):
