aace11f10eff2675ff86cf8d9f173c6cbf75dde6
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 02:00:05 +0100

    main/libx11: update to 1.8.4

--- main/libx11/template.py
+++ main/libx11/template.py
@@ -1,5 +1,5 @@
 pkgname = "libx11"
-pkgver = "1.8.2"
+pkgver = "1.8.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libX11-{pkgver}.tar.gz"
-sha256 = "f1bc56187bee0f830e1179ac5068ac93b78c51ace94eb27702ffb2efd116587b"
+sha256 = "efd3a3a43c1f177edc2c205bedb0719b6648203595e54c0b83a32576aeaca7cd"
 # FIXME int (_XkbReadGetIndicatorMapReply)
 hardening = ["!int"]
 # broken for now
