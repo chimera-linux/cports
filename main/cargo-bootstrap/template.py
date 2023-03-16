7fdeca3c256e1cfac8b9d11ede3a6171da769e8e
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 15:02:57 +0100

    main/cargo-bootstrap: update to 1.68.0

--- main/cargo-bootstrap/template.py
+++ main/cargo-bootstrap/template.py
@@ -1,5 +1,5 @@
 pkgname = "cargo-bootstrap"
-pkgver = "1.67.0"
+pkgver = "1.68.0"
 pkgrel = 0
 # satisfy runtime dependencies
 hostmakedepends = ["curl"]
@@ -15,13 +15,13 @@ options = ["!strip"]
 
 match self.profile().arch:
     case "aarch64":
-        sha256 = "715f44918bacd9246e551669a0bcb8f15740a0c2fbe801e63a3bf928760441e0"
+        sha256 = "a8359051529362575b095929fc6d6052fe6f1cfc2973a9d2b76d8a53848b12e3"
     case "ppc64le":
-        sha256 = "66fd87b8b6c5addb3738275645aa623f262b9259910a3536be484fe6f1fb4b60"
+        sha256 = "42f636faa0416aec81e1694c2ed266bed88979c466aab1da899ab5e06fb3e850"
     case "riscv64":
-        sha256 = "22ed6386d7347976cfd451732f909e91094d1912ce9350bd82efc3392e7772bf"
+        sha256 = "6188917978f0c58bffb4e1914e0dd1d1ad2b798a34a0ab0cff0087c087504fdf"
     case "x86_64":
-        sha256 = "00604ce0c4b1f3b7ca15324d50a4cd809511a9fed1f5e44cff655c56aa221b3e"
+        sha256 = "712f4cd05e7b52bee95fd89e18778f2e8a430ad6ccff7d21b6a5376b077efad0"
     case _:
         broken = f"not yet built for {self.profile().arch}"
 
