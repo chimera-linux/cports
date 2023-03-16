70c4f7e870bc58fa756e5c6028182d3e9fbaf8fa
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 18:46:57 +0100

    main/libfontenc: update to 1.1.7

--- main/libfontenc/template.py
+++ main/libfontenc/template.py
@@ -1,5 +1,5 @@
 pkgname = "libfontenc"
-pkgver = "1.1.6"
+pkgver = "1.1.7"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-encodingsdir=/usr/share/fonts/encodings"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.gz"
-sha256 = "c103543a47ce5c0200fb1867f32df5e754a7c3ef575bf1fe72187117eac22a53"
+sha256 = "5e5f210329823f08f97bfe9fd5b4105070c789bc5aef88ce01d86d8203d4aa9f"
 
 def post_install(self):
     self.install_license("COPYING")
