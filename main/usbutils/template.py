add045adf1ddbf5ffd0c5c5e8c8398b3551e2a9a
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 18:50:28 +0100

    main/usbutils: update to 015

--- main/usbutils/template.py
+++ main/usbutils/template.py
@@ -1,5 +1,5 @@
 pkgname = "usbutils"
-pkgver = "014"
+pkgver = "015"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "libtool", "pkgconf"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "http://linux-usb.sourceforge.net"
 source = f"$(KERNEL_SITE)/utils/usb/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "3a079cfad60560227b67192482d7813bf96326fcbb66c04254839715f276fc69"
+sha256 = "c3b451bb1f4ff9f6356cac5a6956a9ac8e85d81651af56a29e689f94fa6fda6e"
 hardening = ["vis", "cfi"]
 
 def pre_configure(self):
