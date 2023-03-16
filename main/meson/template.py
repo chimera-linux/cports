1661dc74409aaba5f44a8a5bca550fefe1b0f8cd
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 01:54:35 +0100

    main/meson: update to 1.0.1

--- main/meson/template.py
+++ main/meson/template.py
@@ -1,5 +1,5 @@
 pkgname = "meson"
-pkgver = "0.64.0"
+pkgver = "1.0.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-devel", "python-setuptools"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://mesonbuild.com"
 source = f"https://github.com/mesonbuild/{pkgname}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "c5e27e091c2a35b9049e152a6535045ebbd057253aeb67856de6ecbb7b917bab"
+sha256 = "d926b730de6f518728cc7c57bc5e701667bae0c3522f9e369427b2cc7839d3c1"
 # meson is early in our bootstrap path but has a million checkdepends
 options = ["!check"]
 
