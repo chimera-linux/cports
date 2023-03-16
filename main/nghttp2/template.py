6581346b6f971a8373e748828c1f600415dd824d
Daniel Kolesa <q66@chimera-linux.org>
2023-03-16 19:32:27 +0100

    main/nghttp2: update to 1.52.0

--- main/nghttp2/template.py
+++ main/nghttp2/template.py
@@ -1,5 +1,5 @@
 pkgname = "nghttp2"
-pkgver = "1.51.0"
+pkgver = "1.52.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-python-bindings"]
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://nghttp2.org"
 source = f"https://github.com/tatsuhiro-t/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "66aa76d97c143f42295405a31413e5e7d157968dad9f957bb4b015b598882e6b"
+sha256 = "3ea9f0439e60469ad4d39cb349938684ffb929dd7e8e06a7bffe9f9d21f8ba7d"
 # FIXME cfi; reproduces in e.g. libsoup
 hardening = ["vis", "!cfi"]
 
