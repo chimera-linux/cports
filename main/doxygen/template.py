3d5a4a4b44105ba490788a8a13755a9fdc1e39eb
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 05:08:52 +0100

    main/doxygen: update to 1.9.6

--- main/doxygen/template.py
+++ main/doxygen/template.py
@@ -1,5 +1,5 @@
 pkgname = "doxygen"
-pkgver = "1.9.5"
+pkgver = "1.9.6"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "perl", "python", "flex", "bison"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://doxygen.nl"
 source = f"{url}/files/{pkgname}-{pkgver}.src.tar.gz"
-sha256 = "55b454b35d998229a96f3d5485d57a0a517ce2b78d025efb79d57b5a2e4b2eec"
+sha256 = "297f8ba484265ed3ebd3ff3fe7734eb349a77e4f95c8be52ed9977f51dea49df"
 hardening = ["vis", "cfi"]
 
 def post_extract(self):
