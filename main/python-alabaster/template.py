e679f5c9353b3675aa29863413fbe875abcd9e86
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 20:48:19 +0100

    main/python-alabaster: update to 0.7.13

--- main/python-alabaster/template.py
+++ main/python-alabaster/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-alabaster"
-pkgver = "0.7.12"
+pkgver = "0.7.13"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://alabaster.readthedocs.io"
 source = f"$(PYPI_SITE)/a/alabaster/alabaster-{pkgver}.tar.gz"
-sha256 = "a661d72d58e6ea8a57f7a86e37d86716863ee5e92788398526d58b26a4e4dc02"
+sha256 = "a27a4a084d5e690e16e01e03ad2b2e552c61a65469419b907243193de1a84ae2"
 
 def post_install(self):
     self.install_license("LICENSE")
