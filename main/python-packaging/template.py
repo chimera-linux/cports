7c00ddafa8c17c2b101cb550fa54ffaeb9013d3c
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 21:02:57 +0100

    main/python-packaging: update to 23.0

--- main/python-packaging/template.py
+++ main/python-packaging/template.py
@@ -1,8 +1,8 @@
 pkgname = "python-packaging"
-pkgver = "21.3"
+pkgver = "23.0"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools"]
+build_style = "python_pep517"
+hostmakedepends = ["python-pip", "python-flit_core"]
 checkdepends = ["python-pytest"]
 depends = ["python", "python-pyparsing"]
 pkgdesc = "Easily build and distribute Python packages"
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0 OR BSD-2-Clause"
 url = "https://github.com/pypa/packaging"
 source = f"$(PYPI_SITE)/p/packaging/packaging-{pkgver}.tar.gz"
-sha256 = "dd47c42927d89ab911e606518907cc2d3a1f38bbd026385970643f9c5b8ecfeb"
+sha256 = "b6ad297f8907de0fa2fe1ccbd26fdaf387f5f47c7275fedf8cce89f99446cf97"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
