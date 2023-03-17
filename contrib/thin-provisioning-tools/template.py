b9286b2209e1545509de8f180ff66092928ca4af
Daniel Kolesa <q66@chimera-linux.org>
2023-03-18 00:14:23 +0100

    contrib/thin-provisioning-tools: update to 1.0.3

--- contrib/thin-provisioning-tools/template.py
+++ contrib/thin-provisioning-tools/template.py
@@ -1,29 +1,20 @@
 pkgname = "thin-provisioning-tools"
-pkgver = "0.9.0"
+pkgver = "1.0.3"
 pkgrel = 0
-build_style = "gnu_configure"
-configure_args = ["--enable-testing"]
-make_cmd = "gmake"
-make_dir = "." # otherwise tests will not build
-make_check_target = "test"
-# gawk needed for manpage generation
-hostmakedepends = ["gmake", "pkgconf", "automake", "libtool", "gawk"]
-makedepends = ["boost-devel", "libexpat-devel", "libaio-devel"]
+build_style = "cargo"
+hostmakedepends = ["cargo", "gmake", "gawk"]
+makedepends = ["rust"]
 pkgdesc = "Tools for manipulating the metadata of dm-thin targets"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/jthornber/thin-provisioning-tools"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "a2508d9933ed8a3f6c8d302280d838d416668a1d914a83c4bd0fb01eaf0676e8"
-hardening = ["vis", "cfi"]
-# needs gtest...
+sha256 = "8c912be0f7a1cdc40f37fd4226a1794a91fa31667231d4e88338f32cf84b88b6"
+# too long
 options = ["!check"]
 
-def init_configure(self):
-    self.make_install_args += [
-        f"BINDIR={self.chroot_destdir}/usr/bin",
-        f"MANDIR={self.chroot_destdir}/usr/share/man",
-    ]
-
-def pre_configure(self):
-    self.do("autoreconf", "-if")
+def do_install(self):
+    self.do(
+        "gmake", "DESTDIR=" + str(self.chroot_destdir),
+        "RUST_TARGET=" + self.profile().triplet, "install"
+    )
