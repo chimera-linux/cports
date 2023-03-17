70f33764e9bd01d2d8fadbd1d0c302c9215074f3
Daniel Kolesa <q66@chimera-linux.org>
2023-03-18 00:35:27 +0100

    main/gedit: update to 44.2

--- main/gedit/template.py
+++ main/gedit/template.py
@@ -1,23 +1,25 @@
 pkgname = "gedit"
-pkgver = "42.2"
+pkgver = "44.2"
 pkgrel = 0
 build_style = "meson"
+configure_args = ["-Dgtk_doc=false"]
 hostmakedepends = [
     "meson", "pkgconf", "glib-devel", "gettext-tiny", "vala",
     "gobject-introspection", "perl", "itstool",
 ]
 makedepends = [
     "libglib-devel", "gtk+3-devel", "gtksourceview4-devel", "libpeas-devel",
-    "gspell-devel", "libxml2-devel", "python-gobject-devel",
+    "gspell-devel", "libxml2-devel", "python-gobject-devel", "tepl-devel",
+    "amtk-devel",
 ]
 depends = ["gsettings-desktop-schemas", "iso-codes", "python-gobject"]
-provides = ["so:libgedit-41.0.so=0"]
+provides = ["so:libgedit-44.so=0"]
 pkgdesc = "GNOME text editor"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/Gedit"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "3c6229111f0ac066ae44964920791d1265f5bbb56b0bd949a69b7b1261fc8fca"
+sha256 = "3bbb1b3775d4c277daf54aaab44b0eb83a4eb1f09f0391800041c9e56893ec11"
 
 def post_install(self):
     from cbuild.util import python
