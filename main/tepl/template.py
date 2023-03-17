13a76994180101f778d17cba334a7aade3a7e330
Daniel Kolesa <q66@chimera-linux.org>
2023-03-18 00:31:50 +0100

    main/tepl: new package (6.4.0)

---
+++ main/tepl/template.py
@@ -0,0 +1,25 @@
+pkgname = "tepl"
+pkgver = "6.4.0"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dgtk_doc=false"]
+hostmakedepends = [
+    "meson", "pkgconf", "gobject-introspection", "glib-devel",
+    "gettext-tiny",
+]
+makedepends = [
+    "libglib-devel", "gtk+3-devel", "amtk-devel", "gtksourceview4-devel",
+    "libxml2-devel", "uchardet-devel", "gsettings-desktop-schemas-devel",
+]
+pkgdesc = "Text editor product line"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-3.0-or-later"
+url = "https://gitlab.gnome.org/swilmet/tepl"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "5e56b20669d0cf05fa1d64b58c8c342c59158122dc518100d093d59df9b87321"
+# needs graphical environment
+options = ["!check", "!cross"]
+
+@subpackage("tepl-devel")
+def _devel(self):
+    return self.default_devel()
