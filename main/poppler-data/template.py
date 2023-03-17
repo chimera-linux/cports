0bb67f8516cf4bd081cc9a5a8505d883a3ab855b
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 05:45:40 +0100

    main/poppler-data: update to 0.4.12

--- main/poppler-data/template.py
+++ main/poppler-data/template.py
@@ -1,5 +1,5 @@
 pkgname = "poppler-data"
-pkgver = "0.4.11"
+pkgver = "0.4.12"
 pkgrel = 0
 build_style = "makefile"
 make_install_args = ["prefix=/usr"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause AND (GPL-2.0-only OR GPL-3.0-only)"
 url = "https://poppler.freedesktop.org"
 source = f"{url}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "2cec05cd1bb03af98a8b06a1e22f6e6e1a65b1e2f3816cb3069bb0874825f08c"
+sha256 = "c835b640a40ce357e1b83666aabd95edffa24ddddd49b8daff63adb851cdab74"
 # no test suite
 options = ["!check"]
 
