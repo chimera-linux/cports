7157287833952ca70552dae4ee643cc1986eba59
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 22:03:41 +0100

    main/lynx: update to 2.9.0_pre12

--- main/lynx/template.py
+++ main/lynx/template.py
@@ -1,5 +1,5 @@
 pkgname = "lynx"
-pkgver = "2.9.0_pre10"
+pkgver = "2.9.0_pre12"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -15,6 +15,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "http://lynx.invisible-island.net"
 source = f"http://invisible-mirror.net/archives/{pkgname}/tarballs/{pkgname}{pkgver.replace('_pre', 'dev.')}.tar.bz2"
-sha256 = "898ac82bcfcbd4b20ea39afdf66fd659b8773c7549623b0f8802bf392a41a912"
+sha256 = "a6455b159d00776d8ec1051285c972dc1f0c552d0571a0cff02a23ec146ee8e5"
 hardening = ["vis", "cfi"]
 options = ["!cross"]
