32f3d8389e4987378aa9750c2cd9592a6d029971
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 20:51:32 +0100

    main/python-babel: update to 2.12.1

--- main/python-babel/template.py
+++ main/python-babel/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-babel"
-pkgver = "2.11.0"
+pkgver = "2.12.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://babel.pocoo.org"
 source = f"$(PYPI_SITE)/B/Babel/Babel-{pkgver}.tar.gz"
-sha256 = "5ef4b3226b0180dedded4229651c8b0e1a3a6a2837d45a073272f313e4cf97f6"
+sha256 = "cc2d99999cd01d44420ae725a21c9e3711b3aadc7976d6147f622d8581963455"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
