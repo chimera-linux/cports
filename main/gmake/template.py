3f5035458c471d96f918ee5d7a6e4d21940ab900
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 19:05:03 +0100

    main/gmake: update to 4.4.1

--- main/gmake/template.py
+++ main/gmake/template.py
@@ -1,5 +1,5 @@
 pkgname = "gmake"
-pkgver = "4.4"
+pkgver = "4.4.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--without-guile", "--program-prefix=g"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/make"
 source = f"$(GNU_SITE)/make/make-{pkgver}.tar.lz"
-sha256 = "48d0fc0b2a04bb50f2911c16da65723285f7f4804c74fc5a2124a3df6c5f78c4"
+sha256 = "8814ba072182b605d156d7589c19a43b89fc58ea479b9355146160946f8cf6e9"
 hardening = ["!cfi"] # needs figuring out hidden visibility first
 # perl needs gmake to build, so tests introduce a cycle
 options = ["!check"]
