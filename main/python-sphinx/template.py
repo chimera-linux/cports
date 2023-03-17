fe45b95ca71024b3a688b4454d72fbe6f4fdee5e
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 21:38:17 +0100

    main/python-sphinx: update to 6.1.3

--- main/python-sphinx/template.py
+++ main/python-sphinx/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-sphinx"
-pkgver = "5.3.0"
+pkgver = "6.1.3"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-setuptools", "python-flit_core", "python-pip"]
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://sphinx-doc.org"
 source = f"$(PYPI_SITE)/S/Sphinx/Sphinx-{pkgver}.tar.gz"
-sha256 = "51026de0a9ff9fc13c05d74913ad66047e104f56a129ff73e174eb5c3ee794b5"
+sha256 = "0dac3b698538ffef41716cf97ba26c1c7788dba73ce6f150c1ff5b4720786dd2"
 # dependency of pytest, missing other checkdepends
 options = ["!check"]
 
