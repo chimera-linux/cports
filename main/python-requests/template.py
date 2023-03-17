6d886e7060e776ed937f64dfcf3ff12a780369a2
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 21:12:04 +0100

    main/python-requests: update to 2.28.2

--- main/python-requests/template.py
+++ main/python-requests/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-requests"
-pkgver = "2.28.1"
+pkgver = "2.28.2"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -12,6 +12,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://python-requests.org"
 source = f"$(PYPI_SITE)/r/requests/requests-{pkgver}.tar.gz"
-sha256 = "7c5599b102feddaa661c826c56ab4fee28bfd17f5abca1ebbe3e7f19d7c97983"
+sha256 = "98b1b2782e3c6c4904938b84c0eb932721069dfdb9134313beff7c83c2df24bf"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
