73aa1f6b543f963fa6c4b27b748b362b98d0580c
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 18:49:00 +0100

    main/iputils: update to 20221126

--- main/iputils/template.py
+++ main/iputils/template.py
@@ -1,11 +1,10 @@
 pkgname = "iputils"
-pkgver = "20211215"
+pkgver = "20221126"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-DNO_SETCAP_OR_SUID=true",
     "-DUSE_IDN=false", 
-    "-DBUILD_NINFOD=false"
 ]
 hostmakedepends = [
     "meson", "pkgconf", "xsltproc", "docbook-xsl", "libcap-progs", "iproute2"
@@ -17,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause AND GPL-2.0-or-later"
 url = "https://github.com/iputils/iputils"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "b6f67fc705490673ff4471d006221b4a2f1b1180b929d9fefd771352621ccedf"
+sha256 = "745ea711fe06d5c57d470d21acce3c3ab866eb6afb69379a16c6d60b89bd4311"
 hardening = ["vis", "cfi"]
 # operation not permitted (sandbox, unshared network)
 options = ["!check"]
