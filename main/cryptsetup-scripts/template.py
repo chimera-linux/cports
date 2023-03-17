e9b09d08121000a71644e1fc5dd1d6d760280b98
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 19:56:37 +0100

    main/cryptsetup-scripts: update to 2.6.1.3

--- main/cryptsetup-scripts/template.py
+++ main/cryptsetup-scripts/template.py
@@ -1,6 +1,6 @@
 pkgname = "cryptsetup-scripts"
-_debver = "6"
-_cver = "2.5.0"
+_debver = "3"
+_cver = "2.6.1"
 pkgver = f"{_cver}.{_debver}"
 pkgrel = 0
 build_style = "makefile"
@@ -15,6 +15,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://salsa.debian.org/cryptsetup-team/cryptsetup"
 source = f"{url}/-/archive/debian/2%25{_cver}-{_debver}/cryptsetup-debian-2%25{_cver}-{_debver}.tar.gz"
-sha256 = "e9c53c79347ee597b8218d7ec505ae5b6175e9fbe9f27c11945da30b1a00a3cc"
+sha256 = "511f41fd6aed8134911c5547da29f25457aba2fb78eb25f2dc8278d8d68f7cfe"
 # no test suite
 options = ["!check"]
