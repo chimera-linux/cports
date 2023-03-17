a212aadc5a8dde1751f8eafea644227649e033f4
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 19:03:57 +0100

    main/userspace-rcu: update to 0.14.0

--- main/userspace-rcu/template.py
+++ main/userspace-rcu/template.py
@@ -1,5 +1,5 @@
 pkgname = "userspace-rcu"
-pkgver = "0.13.2"
+pkgver = "0.14.0"
 pkgrel = 0
 build_style = "gnu_configure"
 make_check_args = ["-j1"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://liburcu.org"
 source = f"https://www.lttng.org/files/urcu/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "1213fd9f1b0b74da7de2bb74335b76098db9738fec5d3cdc07c0c524f34fc032"
+sha256 = "ca43bf261d4d392cff20dfae440836603bf009fce24fdc9b2697d837a2239d4f"
 tool_flags = {"CFLAGS": ["-DLITTLE_ENDIAN=4321", "-DBIG_ENDIAN=1234"]}
 # XXX: tests pass when run outside the suite...
 options = ["!check"]
