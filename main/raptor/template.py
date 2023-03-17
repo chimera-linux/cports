0d6b531288d61883273dd06f99724803acd89081
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 06:21:37 +0100

    main/raptor: update to 2.0.16

--- main/raptor/template.py
+++ main/raptor/template.py
@@ -1,5 +1,5 @@
 pkgname = "raptor"
-pkgver = "2.0.15"
+pkgver = "2.0.16"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-yajl=no"]
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0 OR LGPL-2.1-or-later OR GPL-2.0-or-later"
 url = "https://librdf.org/raptor"
 source = f"https://librdf.org/dist/source/raptor2-{pkgver}.tar.gz"
-sha256 = "ada7f0ba54787b33485d090d3d2680533520cd4426d2f7fb4782dd4a6a1480ed"
+sha256 = "089db78d7ac982354bdbf39d973baf09581e6904ac4c92a98c5caadb3de44680"
 
 @subpackage("raptor-devel")
 def _devel(self):
