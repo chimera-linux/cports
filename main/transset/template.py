dc786980f51a8fd9d3db5282e8a7fec0d0022a2f
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 20:31:26 +0100

    main/transset: update to 1.0.3

--- main/transset/template.py
+++ main/transset/template.py
@@ -1,5 +1,5 @@
 pkgname = "transset"
-pkgver = "1.0.2"
+pkgver = "1.0.3"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf"]
@@ -8,8 +8,8 @@ pkgdesc = "Sets the transparency of an X window"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "4bac142ee7cfda574893b2f6b4e413dacd88a130c6dca5be1a9958e7c1451b21"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
+sha256 = "adba0da81dacdebe5275ec0117dd08685e4f2f31afa0391f423e54906d0fb824"
 hardening = ["vis", "cfi"]
 
 def post_install(self):
