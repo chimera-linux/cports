c9fe6f0c238ba8e985f2f38e08ea0f3fe486cd6c
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 20:56:53 +0100

    main/python-charset-normalizer: update to 3.1.0

--- main/python-charset-normalizer/template.py
+++ main/python-charset-normalizer/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-charset-normalizer"
-pkgver = "3.0.0"
+pkgver = "3.1.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://charset-normalizer.readthedocs.io"
 source = f"https://github.com/Ousret/charset_normalizer/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "7988228e24f8e3fdca28dde813ea3e8bbefb8ecea609a3c34230689fe50b054d"
+sha256 = "0ca96d2ffef13e7062c53df8619e97b910343d1e14b4b92ee71bc61d2d18ced4"
 # dependency of pytest
 options = ["!check"]
 
