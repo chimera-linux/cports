60b05abfec4997f52c3709bb35c2e2c141ed061b
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 18:57:21 +0100

    main/libvisual: update to 0.4.1

--- main/libvisual/template.py
+++ main/libvisual/template.py
@@ -1,7 +1,8 @@
 pkgname = "libvisual"
-pkgver = "0.4.0"
+pkgver = "0.4.1"
 pkgrel = 0
 build_style = "gnu_configure"
+configure_args = ["--disable-examples"]
 make_cmd = "gmake"
 # must be used to overwrite generated junk that messes up build
 make_dir = "."
@@ -11,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "http://sourceforge.net/projects/libvisual"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "0b4dfdb87125e129567752089e3c8b54cefed601eef169d2533d8659da8dc1d7"
+sha256 = "aa12877417f76d3642d9f4c723302380d833175639d63a55641d01928a5ddb7d"
 
 @subpackage("libvisual-devel")
 def _devel(self):
