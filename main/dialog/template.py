22d86ad8e535f36ef8094a92665b03c37390ff57
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 04:55:48 +0100

    main/dialog: update to 1.3.20230209

--- main/dialog/template.py
+++ main/dialog/template.py
@@ -1,5 +1,5 @@
 pkgname = "dialog"
-pkgver = "1.3.20220728"
+pkgver = "1.3.20230209"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-ncursesw", "--disable-nls"]
@@ -8,8 +8,8 @@ pkgdesc = "Tool to display dialog boxes from shell scripts"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-only"
 url = "https://invisible-island.net/dialog"
-source = f"https://invisible-mirror.net/archives/{pkgname}/{pkgname}-{pkgver.replace('.2022', '-2022')}.tgz"
-sha256 = "54418973d559a461b00695fafe68df62f2bc73d506b436821d77ca3df454190b"
+source = f"https://invisible-mirror.net/archives/{pkgname}/{pkgname}-{pkgver.replace('.2023', '-2023')}.tgz"
+sha256 = "0c26282305264be2217f335f3798f48b1dce3cf12c5a076bf231cadf77a6d6a8"
 hardening = ["vis", "cfi"]
 
 def post_install(self):
