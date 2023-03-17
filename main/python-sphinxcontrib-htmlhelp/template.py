459bf409a1bc7fdee0fc1b979010ba878aa9f79e
Daniel Kolesa <q66@chimera-linux.org>
2023-03-17 21:37:04 +0100

    main/python-sphinxcontrib-htmlhelp: update to 2.0.1

--- main/python-sphinxcontrib-htmlhelp/template.py
+++ main/python-sphinxcontrib-htmlhelp/template.py
@@ -1,8 +1,9 @@
 pkgname = "python-sphinxcontrib-htmlhelp"
-pkgver = "2.0.0"
+pkgver = "2.0.1"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools"]
+build_style = "python_pep517"
+make_install_target = f"sphinxcontrib_htmlhelp-{pkgver}-*-*-*.whl"
+hostmakedepends = ["python-pip", "python-flit_core"]
 checkdepends = ["python-sphinx"]
 depends = ["python"]
 pkgdesc = "Sphinx extension which outputs HTML document"
@@ -10,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "http://sphinx-doc.org"
 source = f"$(PYPI_SITE)/s/sphinxcontrib-htmlhelp/sphinxcontrib-htmlhelp-{pkgver}.tar.gz"
-sha256 = "f5f8bb2d0d629f398bf47d0d69c07bc13b65f75a81ad9e2f71a63d4b7a2f6db2"
+sha256 = "0cbdd302815330058422b98a113195c9249825d681e18f11e8b1f78a2f11efff"
 # circular checkdepends
 options = ["!check"]
 
