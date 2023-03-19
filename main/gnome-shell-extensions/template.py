0753edc6b7ccbf936091f17bb7cdc27b48df3e82
Daniel Kolesa <q66@chimera-linux.org>
2023-03-19 18:35:39 +0100

    main/gnome-shell-extensions: update to 44.0

--- main/gnome-shell-extensions/template.py
+++ main/gnome-shell-extensions/template.py
@@ -1,12 +1,12 @@
 pkgname = "gnome-shell-extensions"
-pkgver = "43.1"
+pkgver = "44.0"
 pkgrel = 0
 build_style = "meson"
-hostmakedepends = ["meson", "pkgconf", "gettext-tiny"]
+hostmakedepends = ["meson", "pkgconf", "gettext-tiny", "glib-devel"]
 depends = [f"gnome-shell~{pkgver[:-2]}", "nautilus", "gnome-menus"]
 pkgdesc = "Optional extensions for GNOME shell"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Projects/GnomeShell/Extensions"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "adde04bd946a13113557e4c34c890bbe9077505a6a3f0755f17bea1c6e4a2d17"
+sha256 = "8c345e72f31a1e37f55063e0b15997301b01194ed698772fd87aeb50cbb101ab"
