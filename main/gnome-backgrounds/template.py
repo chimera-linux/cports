fee96f3a99b6394d9a0a74406f199198655a3524
Daniel Kolesa <q66@chimera-linux.org>
2023-03-19 18:46:30 +0100

    main/gnome-backgrounds: update to 44.0_beta1

--- main/gnome-backgrounds/template.py
+++ main/gnome-backgrounds/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-backgrounds"
-pkgver = "43.1"
+pkgver = "44.0_beta1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson"]
@@ -7,5 +7,5 @@ pkgdesc = "Backgrounds for GNOME desktop"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND CC-BY-2.0 AND CC-BY-SA-2.0 AND CC-BY-SA-3.0"
 url = "https://gitlab.gnome.org/GNOME/gnome-backgrounds"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "8e459cad38a7574683a55e5e77690e658c6f9feaeec9cb82039ab25ba2bca05d"
+source = f"$(GNOME_SITE)/{pkgname}/44/{pkgname}-44.beta.tar.xz"
+sha256 = "5013d343882afbce2bdf91a88b07b0b0d906d66de363cf94386fe868b6e0e9c0"
