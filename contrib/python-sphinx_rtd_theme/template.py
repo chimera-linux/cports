181d85c2c28767717f82ebf22e1b02083fc1f579
eater <=@eater.me>
2023-03-19 23:30:59 +0100

    contrib/python-sphinx_rtd_theme: new package (1.2.0)

---
+++ contrib/python-sphinx_rtd_theme/template.py
@@ -0,0 +1,14 @@
+pkgname = "python-sphinx_rtd_theme"
+pkgver = "1.2.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = ["python-setuptools", "python-pip", "python-wheel"]
+depends = ["python", "python-sphinxcontrib-jquery"]
+pkgdesc = "Sphinx theme for readthedocs.org"
+maintainer = "eater <=@eater.me>"
+license = "MIT"
+url = "https://github.com/readthedocs/sphinx_rtd_theme"
+source = f"https://github.com/readthedocs/sphinx_rtd_theme/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "6f79aaccdf2a6019f6fb7486b22c2b3a37bd8571845ae3105b39486d8b77024e"
+# No tests available
+options = ["!check"]
