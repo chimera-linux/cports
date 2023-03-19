94d7146b68a17aecddca1b49db82284bfbfc34b9
Daniel Kolesa <q66@chimera-linux.org>
2023-03-19 03:55:01 +0100

    main/evince: update to 44.0

--- main/evince/template.py
+++ main/evince/template.py
@@ -1,5 +1,5 @@
 pkgname = "evince"
-pkgver = "43.1"
+pkgver = "44.0"
 pkgrel = 0
 build_style = "meson"
 # dvi needs kpathsea, which is in texlive
@@ -11,6 +11,7 @@ configure_args = [
 hostmakedepends = [
     "meson", "pkgconf", "gobject-introspection", "glib-devel", "itstool",
     "gettext-tiny", "perl-xml-parser", "adwaita-icon-theme",
+    "desktop-file-utils",
 ]
 makedepends = [
     "gtk+3-devel", "libglib-devel", "libhandy-devel", #"nautilus-devel",
@@ -25,7 +26,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/Evince"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "6d75ca62b73bfbb600f718a098103dc6b813f9050b9594be929e29b4589d2335"
+sha256 = "339ee9e005dd7823a13fe21c71c2ec6d2c4cb74548026e4741eee7b2703e09da"
 
 @subpackage("evince-libs")
 def _libs(self):
