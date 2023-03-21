c197ceb0587685bb87b5d74e6ab8b3fc5eb3c7eb
Daniel Kolesa <q66@chimera-linux.org>
2023-03-21 03:06:47 +0100

    main/mpg123: update to 1.31.3

--- main/mpg123/template.py
+++ main/mpg123/template.py
@@ -1,5 +1,5 @@
 pkgname = "mpg123"
-pkgver = "1.31.2"
+pkgver = "1.31.3"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-only"
 url = "https://www.mpg123.org"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "b17f22905e31f43b6b401dfdf6a71ed11bb7d056f68db449d70b9f9ae839c7de"
+sha256 = "1ca77d3a69a5ff845b7a0536f783fee554e1041139a6b978f6afe14f5814ad1a"
 
 def _genlib(libn, descn, iif):
     @subpackage(f"mpg123-output-{libn}")
