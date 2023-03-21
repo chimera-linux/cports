e3ab34844dda9e736e08e53c8387e7fe555c3e7a
Daniel Kolesa <q66@chimera-linux.org>
2023-03-21 02:56:43 +0100

    main/baobab: update to 44.0

--- main/baobab/template.py
+++ main/baobab/template.py
@@ -1,9 +1,10 @@
 pkgname = "baobab"
-pkgver = "43.0"
+pkgver = "44.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
-    "meson", "pkgconf", "glib-devel", "gettext-tiny", "vala", "itstool"
+    "meson", "pkgconf", "glib-devel", "gettext-tiny", "vala", "itstool",
+    "desktop-file-utils",
 ]
 makedepends = [
     "gtk4-devel", "glib-devel", "libadwaita-devel",
@@ -14,6 +15,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/action/show/Apps/DiskUsageAnalyzer"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "52c6864118f5697f5a5736882dcda27db22e7220bc492838deecc699246cdb26"
+sha256 = "845b63bb9123d74568c8126c571bbc74273483ff920179a2cf1eddbbefa1bfc0"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
