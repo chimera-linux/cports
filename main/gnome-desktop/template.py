b4709516d7a1ce8084f85087c92c2253455352fb
Daniel Kolesa <q66@chimera-linux.org>
2023-03-21 03:04:13 +0100

    main/gnome-desktop: update to 44.0

--- main/gnome-desktop/template.py
+++ main/gnome-desktop/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-desktop"
-pkgver = "43.2"
+pkgver = "44.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dudev=enabled", "-Dsystemd=disabled",]
@@ -20,7 +20,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-desktop"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "a0b9ab20d28a63df6ce7e91eabb5e3af86630bf522b5a8997c04971bd551d730"
+sha256 = "42c773745d84ba14bc1cf1c4c6f4606148803a5cd337941c63964795f3c59d42"
 # needs graphical environment
 options = ["!check"]
 
