444a2b2da7357f41f36b744a1dfa2cd8c33c1a4c
Daniel Kolesa <q66@chimera-linux.org>
2023-03-21 03:05:06 +0100

    main/gnome-font-viewer: update to 44.0

--- main/gnome-font-viewer/template.py
+++ main/gnome-font-viewer/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-font-viewer"
-pkgver = "43.0"
+pkgver = "44.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -14,6 +14,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-font-viewer"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "81c6bffb06d5332346e00eaecaec1bdcfd617c51dfd95bcd058d6c76c76dd2b9"
+sha256 = "a1511df30b228cc2ef1175dd9d2b93438ea912e25913404b263cf3d457bb9f97"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
