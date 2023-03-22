da55db6bc1384e9520b39484cda969a40205be19
eater <=@eater.me>
2023-03-22 18:40:20 +0100

    contrib/weechat: new package (3.8)
    Closes https://github.com/chimera-linux/cports/pull/157

---
+++ contrib/weechat/template.py
@@ -0,0 +1,59 @@
+pkgname = "weechat"
+pkgver = "3.8"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    # no guile available
+    "-DENABLE_GUILE=False",
+    # no php available
+    "-DENABLE_PHP=False",
+    # no v8 available
+    "-DENABLE_JAVASCRIPT=False",
+    # no, aspell available
+    "-DENABLE_ENCHANT=True",
+    # missing dependencies
+    "-DENABLE_TESTS=False",
+    "-DENABLE_MAN=False",
+    "-DENABLE_DOC=False",
+]
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = [
+    # core deps
+    "libgcrypt-devel", "gnutls-devel", "libzstd-devel", "libcurl-devel",
+    "ncurses-devel",
+    # perl plugin
+    "perl",
+    # lua plugin
+    "lua5.4-devel",
+    # pyhton plugin
+    "python-devel",
+    # relay plugin
+    "zlib-devel",
+    # ruby plugin
+    "ruby-devel",
+    # spell plugin
+    "enchant-devel",
+    # tcl plugin
+    "tcl-devel",
+]
+pkgdesc = "Extensible chat client"
+maintainer = "eater <=@eater.me>"
+license = "GPL-3.0-or-later"
+url = "https://weechat.org"
+source = f"https://weechat.org/files/src/weechat-{pkgver}.tar.gz"
+sha256 = "d9d27fac127c724564cf28c6179fa6ecc79a61f9dad09a3b251500f2b0755409"
+
+@subpackage("weechat-devel")
+def _devel(self):
+    return self.default_devel()
+
+def _plugin(name):
+    @subpackage(f"weechat-{name}")
+    def _plg(self):
+        self.pkgdesc = f"{pkgdesc} ({name.capitalize()} plugin)"
+        self.depends = [f"{pkgname}={pkgver}-r{pkgrel}"]
+
+        return [f"usr/lib/weechat/plugins/{name}.so"]
+
+for p in ["lua", "python", "ruby", "tcl", "perl"]:
+    _plugin(p)
