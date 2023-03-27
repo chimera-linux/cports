9ba7caca6cdcbef16f8ca59e77802bfa7aeade2b
Jami Kettunen <jami.kettunen@protonmail.com>
2023-03-27 03:32:12 +0300

    contrib/unudhcpd: new package (0.2.1)
    Closes https://github.com/chimera-linux/cports/pull/160

---
+++ contrib/unudhcpd/template.py
@@ -0,0 +1,11 @@
+pkgname = "unudhcpd"
+pkgver = "0.2.1"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson"]
+pkgdesc = "Very basic DHCP server that only issues 1 IP address to any client"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-3.0-or-later"
+url = "https://gitlab.com/postmarketOS/unudhcpd"
+source = f"{url}/-/archive/{pkgver}.tar.gz"
+sha256 = "d9ac8e2739b0953156115a029b0dd79a5ca916c8f5ad1c7006f207e79f1d13c0"
