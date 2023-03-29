faced7dc14309d0d3ea4e7dd442475ae8bd09e94
Daniel Kolesa <q66@chimera-linux.org>
2023-03-29 19:42:35 +0200

    main/base-minimal: remove dependency on shadow
    This is now pulled in through dinit-chimera (sulogin) and nyagetty
    (login). It also makes it more flexible as potential alternative
    implementations could be used (thanks to virtual packages).

--- main/base-minimal/template.py
+++ main/base-minimal/template.py
@@ -2,9 +2,7 @@ pkgname = "base-minimal"
 pkgver = "0.1"
 pkgrel = 0
 build_style = "meta"
-depends = [
-    "base-bootstrap", "dinit-chimera", "bsdtar", "shadow", "nyagetty"
-]
+depends = ["base-bootstrap", "dinit-chimera", "bsdtar", "nyagetty"]
 pkgdesc = "Minimal set of packages for a bootable Chimera system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:meta"
