bddd499350d7165004232a9f8e4257293a30b6b0
q66 <q66@chimera-linux.org>
2023-03-30 16:50:46 +0000

    main/snooze: enable cfi

--- main/snooze/template.py
+++ main/snooze/template.py
@@ -8,5 +8,6 @@ license = "custom:none"
 url = "https://github.com/leahneukirchen/snooze"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "d63fde85d9333188bed5996baabd833eaa00842ce117443ffbf8719c094be414"
+hardening = ["vis", "cfi"]
 # no tests
 options = ["!check"]
