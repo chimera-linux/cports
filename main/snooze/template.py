97d34da55351a353dc6409a53718b4afe722fd0e
q66 <q66@chimera-linux.org>
2023-03-30 16:43:12 +0000

    main/snooze: new package (0.5)
    Services will later be provided in dinit-chimera.

---
+++ main/snooze/template.py
@@ -0,0 +1,12 @@
+pkgname = "snooze"
+pkgver = "0.5"
+pkgrel = 0
+build_style = "makefile"
+pkgdesc = "Run a command at a particular time"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:none"
+url = "https://github.com/leahneukirchen/snooze"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "d63fde85d9333188bed5996baabd833eaa00842ce117443ffbf8719c094be414"
+# no tests
+options = ["!check"]
