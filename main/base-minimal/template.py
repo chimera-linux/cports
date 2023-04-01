018591dd01856fedb7e99925d02d1d648390df2f
Daniel Kolesa <q66@chimera-linux.org>
2023-04-01 18:37:11 +0200

    main/base-minimal: depend on chimera-install-scripts
    These are useful for bootstrapping chroots and stuff in regular
    systems as well, and they have no dependencies not already present
    in base-minimal, and take only a few kilobytes of space, so
    include them by default.

--- main/base-minimal/template.py
+++ main/base-minimal/template.py
@@ -2,7 +2,10 @@ pkgname = "base-minimal"
 pkgver = "0.1"
 pkgrel = 0
 build_style = "meta"
-depends = ["base-bootstrap", "dinit-chimera", "bsdtar", "nyagetty"]
+depends = [
+    "base-bootstrap", "dinit-chimera", "bsdtar", "nyagetty",
+    "chimera-install-scripts",
+]
 pkgdesc = "Minimal set of packages for a bootable Chimera system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:meta"
