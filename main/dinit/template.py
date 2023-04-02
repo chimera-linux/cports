a6178652fa2abae897037a1425b68f6fb89ebef4
Daniel Kolesa <q66@chimera-linux.org>
2023-04-03 01:41:36 +0200

    main/dinit: update
    This adds support for is-active and is-failed to dinitctl.

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -1,6 +1,6 @@
 pkgname = "dinit"
 pkgver = "0.16.999"
-_commit = "3f70f79e36a6e2a5edf70738eea953497e25aae4"
+_commit = "42228a5e14833b28a2078e6ad2e5e3596cbdaff8"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--syscontrolsocket=/run/dinitctl"]
@@ -14,7 +14,7 @@ license = "Apache-2.0"
 url = f"https://davmac.org/projects/dinit"
 #source = f"https://github.com/davmac314/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
 source = f"https://github.com/davmac314/{pkgname}/archive/{_commit}.tar.gz"
-sha256 = "65b02ba823584843d2545febb2e121cd9e5184cce59882315293c648d299f4b2"
+sha256 = "72bec3a7cd4ddb1d372ca358bf70f19e6798dfd10fdebe79403ac8906f9a6f5c"
 hardening = ["vis", "cfi"]
 
 tool_flags = {
