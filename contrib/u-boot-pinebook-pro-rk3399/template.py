0eca6a2824a1348eec613ef8637d833974a9e75b
Daniel Kolesa <q66@chimera-linux.org>
2023-04-04 21:48:08 +0200

    contrib/u-boot-pinebook-pro-rk3399: update to 2023.04

--- contrib/u-boot-pinebook-pro-rk3399/template.py
+++ contrib/u-boot-pinebook-pro-rk3399/template.py
@@ -1,7 +1,5 @@
-# CAUTION: 2022.07 has SD card issues in Linux (the SD
-# card does not init and we fall back to initramfs shell)
 pkgname = "u-boot-pinebook-pro-rk3399"
-pkgver = "2022.04"
+pkgver = "2023.04"
 pkgrel = 0
 archs = ["aarch64"]
 build_style = "u_boot"
@@ -9,8 +7,8 @@ make_build_args = [
     "BL31=" + str(self.profile().sysroot / "usr/lib/trusted-firmware-a/rk3399/bl31.elf"),
 ]
 hostmakedepends = [
-    "gmake", "gcc-aarch64-none-elf", "flex", "bison",
-    "dtc", "python", "openssl-devel"
+    "gmake", "gcc-aarch64-none-elf", "flex", "bison", "dtc", "swig",
+    "python-devel", "openssl-devel", "python-setuptools", "python-pyelftools",
 ]
 makedepends = ["atf-rk3399-bl31"]
 pkgdesc = "U-Boot for Pinebook Pro"
@@ -18,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND BSD-3-Clause"
 url = "https://www.denx.de/wiki/U-Boot"
 source = f"https://ftp.denx.de/pub/u-boot/u-boot-{pkgver}.tar.bz2"
-sha256 = "68e065413926778e276ec3abd28bb32fa82abaa4a6898d570c1f48fbdb08bcd0"
+sha256 = "e31cac91545ff41b71cec5d8c22afd695645cd6e2a442ccdacacd60534069341"
 env = {
     "U_BOOT_TRIPLET": "aarch64-none-elf",
     "U_BOOT_TARGETS": "idbloader.img u-boot.itb",
