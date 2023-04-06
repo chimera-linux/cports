ce8a2c6f88da7c895390596d8ae4790f947e20cd
Daniel Kolesa <q66@chimera-linux.org>
2023-04-06 06:13:44 +0200

    main/fonts-source-sans-otf: update to 3.052

--- main/fonts-source-sans-otf/template.py
+++ main/fonts-source-sans-otf/template.py
@@ -1,13 +1,13 @@
 pkgname = "fonts-source-sans-otf"
-pkgver = "3.046"
+pkgver = "3.052"
 pkgrel = 0
 pkgdesc = "Sans serif font family for UI environments"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "OFL-1.1"
 url = "https://adobe-fonts.github.io/source-sans"
 source = f"https://github.com/adobe-fonts/source-sans/releases/download/{pkgver}R/OTF-source-sans-{pkgver}R.zip"
-sha256 = "ed03cdb943892e60ebb7f63f1ec8d826722923f7f65dd9f01fe99ce7a04b8cb8"
+sha256 = "a4ebbdea20b08ccbd7bf3665a9462454eefdd01d9a6307129d3b3d4672981074"
 
 def do_install(self):
-    for f in self.cwd.glob("*.otf"):
+    for f in (self.cwd / "OTF").glob("*.otf"):
         self.install_file(f, "usr/share/fonts/source-sans")
