5c5e8c3ce6e837e1a8aa71efd7109d246b1a7e4c
Daniel Kolesa <q66@chimera-linux.org>
2023-04-06 03:28:05 +0200

    main/ppp: update to 2.5.0

--- main/ppp/template.py
+++ main/ppp/template.py
@@ -1,40 +1,25 @@
 # fix up networkmanager when updating this (versioned .so paths)
 pkgname = "ppp"
-pkgver = "2.4.9"
+pkgver = "2.5.0"
 pkgrel = 0
-build_style = "configure"
-configure_args = ["--prefix=/usr"]
+build_style = "gnu_configure"
+configure_args = [
+    "--prefix=/usr",
+    "--with-logfile-dir=/var/log/ppp",
+    "--with-runtime-dir=/run/pppd",
+]
 make_cmd = "gmake"
 make_build_args = ["CBCP=y"]
-hostmakedepends = ["gmake"]
+hostmakedepends = ["gmake", "pkgconf"]
 makedepends = ["libpcap-devel", "openssl-devel", "linux-headers"]
 pkgdesc = "PPP (Point-to-Point Protocol) daemon"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause AND LGPL-2.0-or-later AND GPL-2.0-or-later"
 url = "https://ppp.samba.org"
 source = f"https://ftp.samba.org/pub/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "f938b35eccde533ea800b15a7445b2f1137da7f88e32a16898d02dee8adc058d"
+sha256 = "5cae0e8075f8a1755f16ca290eb44e6b3545d3f292af4da65ecffe897de636ff"
 # no check target
 options = ["!check"]
-
-def init_configure(self):
-    cfl = self.get_cflags(shell = True)
-    ldfl = self.get_ldflags(shell = True)
-
-    self.configure_args += [
-        "--cc=" + self.get_tool("CC"),
-        "--cflags=" + cfl
-    ]
-    self.make_build_args += [
-        "LDFLAGS=" + cfl + " " + ldfl,
-        "CBUILD_SYSROOT=" + str(self.profile().sysroot)
-    ]
-    self.make_install_args += [
-        "INSTROOT=" + str(self.chroot_destdir),
-        "DESTDIR=" + str(self.chroot_destdir / "usr"),
-        "BINDIR=" + str(self.chroot_destdir / "usr/bin"),
-        "CBUILD_SYSROOT=" + str(self.profile().sysroot)
-    ]
 
 def post_install(self):
     self.install_file("include/net/ppp_defs.h", "usr/include/net")
@@ -43,6 +28,7 @@ def post_install(self):
     for f in (self.destdir / f"usr/lib/pppd/{pkgver}").glob("*.so"):
         f.chmod(0o755)
 
+    self.rm(self.destdir / "etc/ppp/options")
     self.install_file(self.files_path / "options", "etc/ppp", mode = 0o644)
     self.install_file(self.files_path / "ip-up", "etc/ppp", mode = 0o755)
     self.install_file(self.files_path / "ip-down", "etc/ppp", mode = 0o755)
@@ -68,9 +54,6 @@ def post_install(self):
     self.install_man("scripts/pon.1")
     self.install_bin("scripts/poff")
     self.install_bin("scripts/plog")
-
-    self.install_file("etc.ppp/pap-secrets", "etc/ppp", mode = 0o600)
-    self.install_file("etc.ppp/chap-secrets", "etc/ppp", mode = 0o600)
 
 @subpackage("ppp-devel")
 def _devel(self):
