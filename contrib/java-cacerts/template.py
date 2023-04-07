43efc04d8bf515918d474ec222b16c3e7a1bf8c6
q66 <q66@chimera-linux.org>
2023-04-07 01:37:22 +0000

    contrib/java-cacerts: new package (1.0)

---
+++ contrib/java-cacerts/template.py
@@ -0,0 +1,16 @@
+pkgname = "java-cacerts"
+pkgver = "1.0"
+pkgrel = 0
+depends = ["p11-kit", "ca-certificates"]
+pkgdesc = "Script to update Java cacerts store"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:none"
+url = "https://chimera-linux.org"
+# no tests
+options = ["!check"]
+
+def do_install(self):
+    self.install_file(
+        self.files_path / "java-cacerts", "etc/ca-certificates/update.d",
+        mode = 0o755
+    )
