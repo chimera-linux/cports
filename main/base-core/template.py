a38cb3b92dcdae21bf57486214f528a6b1da1369
q66 <q66@chimera-linux.org>
2023-04-13 17:39:25 +0000

    main/base-core: depend on chimerautils-extra

--- main/base-core/template.py
+++ main/base-core/template.py
@@ -3,10 +3,10 @@ pkgver = "0.1"
 pkgrel = 0
 build_style = "meta"
 depends = [
-    "base-minimal", "base-man", "base-kernel", "dmesg", "fstrim",
-    "lscpu", "file", "less", "kbd", "man-pages", "mkfs", "e2fsprogs",
-    "xfsprogs", "btrfs-progs", "dosfstools", "iputils", "iproute2",
-    "traceroute", "iw", "pciutils",
+    "base-minimal", "base-man", "base-kernel", "chimerautils-extra",
+    "dmesg", "fstrim", "lscpu", "file", "less", "kbd", "man-pages",
+    "mkfs", "e2fsprogs", "xfsprogs", "btrfs-progs", "dosfstools",
+    "iputils", "iproute2", "traceroute", "iw", "pciutils",
 ]
 pkgdesc = "Common Chimera packages for most deployments"
 maintainer = "q66 <q66@chimera-linux.org>"
