e0f6c2eff69c7e6f567aa436a5c32043d0209d3c
Daniel Kolesa <q66@chimera-linux.org>
2023-04-14 23:36:24 +0200

    main/python-packaging: update to 23.1

--- main/python-packaging/template.py
+++ main/python-packaging/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-packaging"
-pkgver = "23.0"
+pkgver = "23.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-pip", "python-flit_core"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0 OR BSD-2-Clause"
 url = "https://github.com/pypa/packaging"
 source = f"$(PYPI_SITE)/p/packaging/packaging-{pkgver}.tar.gz"
-sha256 = "b6ad297f8907de0fa2fe1ccbd26fdaf387f5f47c7275fedf8cce89f99446cf97"
+sha256 = "a392980d2b6cffa644431898be54b0045151319d1e7ec34f0cfed48767dd334f"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
