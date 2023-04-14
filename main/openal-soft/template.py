e2e2d5d8dbf5a68c21a5679787cd762d79389dbc
Daniel Kolesa <q66@chimera-linux.org>
2023-04-15 00:06:52 +0200

    main/openal-soft: update to 1.23.1

--- main/openal-soft/template.py
+++ main/openal-soft/template.py
@@ -1,5 +1,5 @@
 pkgname = "openal-soft"
-pkgver = "1.23.0"
+pkgver = "1.23.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DALSOFT_EXAMPLES=OFF"]
@@ -13,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://openal-soft.org"
 source = f"{url}/openal-releases/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "057dcf96c3cdfcf40159800a93f57740fe79c2956f76247bee10e436b6657183"
+sha256 = "796f4b89134c4e57270b7f0d755f0fa3435b90da437b745160a49bd41c845b21"
 # no test target
 options = ["!check"]
 
