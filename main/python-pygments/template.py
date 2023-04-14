3f75453e44ddaaa8a4c43619144f4ddc039738ef
Daniel Kolesa <q66@chimera-linux.org>
2023-04-14 23:35:37 +0200

    main/python-pygments: update to 2.15.0

--- main/python-pygments/template.py
+++ main/python-pygments/template.py
@@ -1,15 +1,16 @@
 pkgname = "python-pygments"
-pkgver = "2.14.0"
+pkgver = "2.15.0"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools"]
+build_style = "python_pep517"
+make_install_target = f"Pygments-{pkgver}-*-*-*.whl"
+hostmakedepends = ["python-pip", "python-flit_core", "python-wheel"]
 depends = ["python"]
 pkgdesc = "Generic syntax highlighter written in Python"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://pygments.org"
 source = f"$(PYPI_SITE)/P/Pygments/Pygments-{pkgver}.tar.gz"
-sha256 = "b3ed06a9e8ac9a9aae5a6f5dbe78a8a58655d17b43b93c078f094ddc476ae297"
+sha256 = "f7e36cffc4c517fbc252861b9a6e4644ca0e5abadf9a113c72d1358ad09b9500"
 # dependency of pytest
 options = ["!check"]
 
