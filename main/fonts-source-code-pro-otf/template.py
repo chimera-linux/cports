708c217e4de7394fd71ba4346830906de4c78654
Daniel Kolesa <q66@chimera-linux.org>
2023-04-15 00:01:05 +0200

    main/fonts-source-code-pro-otf: update to 1.026

--- main/fonts-source-code-pro-otf/template.py
+++ main/fonts-source-code-pro-otf/template.py
@@ -1,14 +1,14 @@
 pkgname = "fonts-source-code-pro-otf"
-pkgver = "2.040"
-_itver = "1.060"
-_vver = "1.024"
+pkgver = "2.042"
+_itver = "1.062"
+_vver = "1.026"
 pkgrel = 0
 pkgdesc = "Sans serif font family for UI environments"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "OFL-1.1"
 url = "https://adobe-fonts.github.io/source-code-pro"
-source = f"https://github.com/adobe-fonts/source-code-pro/releases/download/{pkgver}R-u/{_itver}R-i/{_vver}R-vf/OTF-source-code-pro-{pkgver}R-u_{_itver}R-i_{_vver}Rvf.zip"
-sha256 = "98e0edc528b80fd58c7847b5829f151559b10bbc8bc56c3845b4833a14e20721"
+source = f"https://github.com/adobe-fonts/source-code-pro/releases/download/{pkgver}R-u/{_itver}R-i/{_vver}R-vf/OTF-source-code-pro-{pkgver}R-u_{_itver}R-i.zip"
+sha256 = "754a2e3ebb945ae905d720ac5896b3b34acc9546dd6551ef9536869788629dae"
 
 def do_install(self):
     for f in self.cwd.glob("*.otf"):
