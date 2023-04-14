3e503061eca8a47119d2c2c0316ce1836ddc892f
Daniel Kolesa <q66@chimera-linux.org>
2023-04-14 15:38:52 +0200

    main/iana-etc: remove

--- main/iana-etc/update.py
+++
@@ -1,6 +0,0 @@
-url = "https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xml"
-pattern = "<updated>([-\d]+)(?=</updated>)"
-
-def fetch_versions(self, src):
-    # keep only global last updated date
-    return map(lambda v: v.replace("-", ""), self.fetch_versions(src)[:1])
