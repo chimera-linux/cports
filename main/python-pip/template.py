296feba78f33b73bb893ec97ddf08c1fb7a60f88
Daniel Kolesa <q66@chimera-linux.org>
2023-04-15 15:42:16 +0200

    main/python-pip: update to 23.1

--- main/python-pip/template.py
+++ main/python-pip/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pip"
-pkgver = "23.0.1"
+pkgver = "23.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pip.pypa.io"
 source = f"$(PYPI_SITE)/p/pip/pip-{pkgver}.tar.gz"
-sha256 = "cd015ea1bfb0fcef59d8a286c1f8bebcb983f6317719d415dc5351efb7cd7024"
+sha256 = "408539897ee535dbfb83a153f7bc4d620f990d8bd44a52a986efc0b4d330d34a"
 # unpackaged dependencies
 options = ["!check"]
 
