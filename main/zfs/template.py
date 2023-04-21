94a26927af838e43d1febf7b0158d3f76622d84f
Daniel Kolesa <q66@chimera-linux.org>
2023-04-21 21:35:07 +0200

    main/zfs: update to 2.1.11

--- main/zfs/template.py
+++ main/zfs/template.py
@@ -1,7 +1,7 @@
 # don't forget to update files/ckms.ini when bumping
 # also update linux-*-zfs-bin
 pkgname = "zfs"
-pkgver = "2.1.10"
+pkgver = "2.1.11"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -21,7 +21,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "CDDL-1.0"
 url = "https://openzfs.github.io/openzfs-docs"
 source = f"https://github.com/openzfs/{pkgname}/releases/download/{pkgname}-{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "f3abb83a1a4c0cd4f064f3c6a7cc133f800a76dd40b3fec1d15a31ad08471ab9"
+sha256 = "a54fe4e854d0a207584f1799a80e165eae66bc30dc8e8c96a1f99ed9d4d8ceb2"
 hardening = ["!cfi"] # TODO
 
 def post_patch(self):
