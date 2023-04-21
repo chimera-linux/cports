2980de1f52c474f9c438622c4aab270b76990030
Daniel Kolesa <q66@chimera-linux.org>
2023-04-21 21:49:48 +0200

    main/linux-rpi-zfs-bin: update to 2.1.11.6.1.23

--- main/linux-rpi-zfs-bin/template.py
+++ main/linux-rpi-zfs-bin/template.py
@@ -1,6 +1,6 @@
 pkgname = "linux-rpi-zfs-bin"
 _kernver = "6.1.23"
-_zfsver = "2.1.10"
+_zfsver = "2.1.11"
 pkgver = f"{_zfsver}.{_kernver}"
 pkgrel = 0
 hostmakedepends = ["gmake", "perl", "python", "ckms"]
