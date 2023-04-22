2ebaeb3e95206556cf3a360a67a7ec8181b79d64
Daniel Kolesa <q66@chimera-linux.org>
2023-04-23 00:01:59 +0200

    main/gnome-keyring: switch to gcr4

--- main/gnome-keyring/template.py
+++ main/gnome-keyring/template.py
@@ -9,11 +9,12 @@ make_cmd = "gmake"
 make_check_args = ["-j1"]
 make_check_wrapper = ["xvfb-run"]
 hostmakedepends = [
-    "gmake", "pkgconf", "docbook-xsl-nons", "glib-devel", "libtasn1-progs",
-    "xsltproc", "openssh"
+    "gmake", "pkgconf", "docbook-xsl-nons", "glib-devel", "python",
+    "libtasn1-progs", "xsltproc", "openssh", "automake", "libtool",
+    "gettext-tiny-devel",
 ]
 makedepends = [
-    "gcr-devel", "glib-devel", "linux-pam-devel", "libgcrypt-devel"
+    "gcr4-devel", "glib-devel", "linux-pam-devel", "libgcrypt-devel"
 ]
 checkdepends = ["xserver-xorg-xvfb", "dbus-x11"]
 depends = ["dconf"]
@@ -23,3 +24,6 @@ license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-keyring"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "c7f4d040cc76a6b7fe67e08ef9106911c3c80d40fc88cbfc8e2684a4c946e3e6"
+
+def pre_configure(self):
+    self.do("autoreconf", "-if")
