34c54d3b5d065590c088d0cc248b5f11755babc9
Daniel Kolesa <q66@chimera-linux.org>
2023-05-01 22:22:18 +0200

    main/gtksourceview: disable lto
    It seems that LTO prevents mouse scrolling from working in
    gnome-text-editor for some reason.

--- main/gtksourceview/template.py
+++ main/gtksourceview/template.py
@@ -19,7 +19,8 @@ url = "https://wiki.gnome.org/Projects/GtkSourceView"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "110dd4c20def21886fbf777298fe0ef8cc2ad6023b8f36c7424411a414818933"
 # needs graphical environment
-options = ["!check", "!cross"]
+# lto results in broken mouse scrolling in gnome-text-editor
+options = ["!check", "!cross", "!lto"]
 
 @subpackage("gtksourceview-devel")
 def _devel(self):
