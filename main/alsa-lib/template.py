4710697547926c6cbe277a25de13d8f9ea05511b
q66 <q66@chimera-linux.org>
2023-05-09 21:25:21 +0000

    main/alsa-lib: update to 1.2.9

--- main/alsa-lib/template.py
+++ main/alsa-lib/template.py
@@ -1,5 +1,5 @@
 pkgname = "alsa-lib"
-pkgver = "1.2.8"
+pkgver = "1.2.9"
 pkgrel = 0
 build_style = "gnu_configure"
 # build a stripped down alsa lib; things should never use it directly other
@@ -15,12 +15,13 @@ configure_args = [
 ]
 hostmakedepends = ["pkgconf"]
 makedepends = ["linux-headers"]
+depends = ["alsa-ucm-conf"]
 pkgdesc = "Advanced Linux Sound Architecture library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.alsa-project.org"
 source = f"{url}/files/pub/lib/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "1ab01b74e33425ca99c2e36c0844fd6888273193bd898240fe8f93accbcbf347"
+sha256 = "dc9c643fdc4ccfd0572cc685858dd41e08afb583f30460b317e4188275f615b2"
 # tests require stuff we disable
 options = ["!check"]
 
