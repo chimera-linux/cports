a3e6e5d7ff8944e2579ceeac431b9daa79257330
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 15:05:49 +0200

    main/python-pyasn1: new package (0.5.0)

---
+++ main/python-pyasn1/template.py
@@ -0,0 +1,15 @@
+pkgname = "python-pyasn1"
+pkgver = "0.5.0"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+depends = ["python"]
+pkgdesc = "Python ASN.1 library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-2-Clause"
+url = "https://github.com/etingof/pyasn1"
+source = f"$(PYPI_SITE)/p/pyasn1/pyasn1-{pkgver}.tar.gz"
+sha256 = "97b7290ca68e62a832558ec3976f15cbf911bf5d7c7039d8b861c2a0ece69fde"
+
+def post_install(self):
+    self.install_license("LICENSE.rst")
