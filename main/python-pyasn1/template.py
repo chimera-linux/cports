bf84a6e3ae9cc6532d41b1dc22a8a64c8019558a
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 17:06:43 +0200

    main/python-pyasn1: downgrade to 0.4.8
    The modules are not compatible with 0.5 yet.

--- main/python-pyasn1/template.py
+++ main/python-pyasn1/template.py
@@ -1,5 +1,6 @@
+# XXX: don't update to 0.5.x unless python-pyasn1-modules is compatible
 pkgname = "python-pyasn1"
-pkgver = "0.5.0"
+pkgver = "0.4.8"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -9,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/etingof/pyasn1"
 source = f"$(PYPI_SITE)/p/pyasn1/pyasn1-{pkgver}.tar.gz"
-sha256 = "97b7290ca68e62a832558ec3976f15cbf911bf5d7c7039d8b861c2a0ece69fde"
+sha256 = "aef77c9fb94a3ac588e87841208bdec464471d9871bd5050a287cc9a475cd0ba"
 
 def post_install(self):
     self.install_license("LICENSE.rst")
