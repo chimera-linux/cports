d0041f0f17adc937ec7b5cbcb1f4f5bd762cd7a8
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 14:52:33 +0200

    main/python-priority: new package (2.0.0)

---
+++ main/python-priority/template.py
@@ -0,0 +1,18 @@
+pkgname = "python-priority"
+pkgver = "2.0.0"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+depends = ["python"]
+checkdepends = ["python-pytest", "python-hypothesis"]
+pkgdesc = "Python implementation of HTTP/2 priority"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://github.com/python-hyper/priority"
+source = f"$(PYPI_SITE)/p/priority/priority-{pkgver}.tar.gz"
+sha256 = "c965d54f1b8d0d0b19479db3924c7c36cf672dbf2aec92d43fbdaf4492ba18c0"
+# unpackaged checkdepends
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("LICENSE")
