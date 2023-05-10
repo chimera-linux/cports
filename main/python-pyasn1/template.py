f283ea4eee590dfe205edd2cffc4a51eaf97df7c
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 17:41:12 +0200

    main/python-pyasn1: update to 0.5.0

--- main/python-pyasn1/template.py
+++ main/python-pyasn1/template.py
@@ -1,6 +1,5 @@
-# XXX: don't update to 0.5.x unless python-pyasn1-modules is compatible
 pkgname = "python-pyasn1"
-pkgver = "0.4.8"
+pkgver = "0.5.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -8,9 +7,9 @@ depends = ["python"]
 pkgdesc = "Python ASN.1 library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
-url = "https://github.com/etingof/pyasn1"
+url = "https://pyasn1.readthedocs.io/en/latest/contents.html"
 source = f"$(PYPI_SITE)/p/pyasn1/pyasn1-{pkgver}.tar.gz"
-sha256 = "aef77c9fb94a3ac588e87841208bdec464471d9871bd5050a287cc9a475cd0ba"
+sha256 = "97b7290ca68e62a832558ec3976f15cbf911bf5d7c7039d8b861c2a0ece69fde"
 
 def post_install(self):
     self.install_license("LICENSE.rst")
