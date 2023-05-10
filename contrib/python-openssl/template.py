fa3dd330ec6028362ea16fcdd1dc7de7ed85185e
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 04:37:18 +0200

    contrib/python-openssl: new package (23.1.1)

---
+++ contrib/python-openssl/template.py
@@ -0,0 +1,17 @@
+pkgname = "python-openssl"
+pkgver = "23.1.1"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+depends = ["python-cryptography"]
+checkdepends = [
+    "python-pytest", "python-flaky", "python-pretend", "python-cryptography"
+]
+pkgdesc = "Python interface to OpenSSL"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "Apache-2.0"
+url = "https://pyopenssl.org"
+source = f"$(PYPI_SITE)/p/pyOpenSSL/pyOpenSSL-{pkgver}.tar.gz"
+sha256 = "841498b9bec61623b1b6c47ebbc02367c07d60e0e195f19790817f10cc8db0b7"
+# unpackaged checkdepends
+options = ["!check"]
