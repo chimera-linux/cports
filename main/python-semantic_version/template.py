99743856a8de2e546bd7c768907da231d1613285
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 04:17:03 +0200

    main/python-semantic_version: new package (2.10.0)

---
+++ main/python-semantic_version/template.py
@@ -0,0 +1,17 @@
+pkgname = "python-semantic_version"
+pkgver = "2.10.0"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+depends = ["python"]
+pkgdesc = "Semantic version comparison for Python"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-2-Clause"
+url = "https://github.com/rbarrois/python-semanticversion"
+source = f"$(PYPI_SITE)/s/semantic-version/semantic_version-{pkgver}.tar.gz"
+sha256 = "bdabb6d336998cbb378d4b9db3a4b56a1e3235701dc05ea2690d9a997ed5041c"
+# needs django?
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("LICENSE")
