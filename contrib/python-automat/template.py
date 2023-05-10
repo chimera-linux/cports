96e4f71b67958e874e6ec1195db16d76387d372c
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 14:34:10 +0200

    contrib/python-automat: new package (22.10.0)

---
+++ contrib/python-automat/template.py
@@ -0,0 +1,20 @@
+pkgname = "python-automat"
+pkgver = "22.10.0"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = [
+    "python-setuptools", "python-setuptools_scm", "python-wheel"
+]
+depends = ["python-setuptools", "python-attrs", "python-six"]
+checkdepends = ["python-pytest", "python-graphviz"] + depends
+pkgdesc = "Finite state machines for Python"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://github.com/glyph/Automat"
+source = f"$(PYPI_SITE)/A/Automat/Automat-{pkgver}.tar.gz"
+sha256 = "e56beb84edad19dcc11d30e8d9b895f75deeb5ef5e96b84a467066b3b84bb04e"
+# unpackaged checkdepends
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("LICENSE")
