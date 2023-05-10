0f44c2058032736162bc5daef3416ccbd4137869
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 19:24:35 +0200

    contrib/intltool: new package (0.51.0)

---
+++ contrib/intltool/template.py
@@ -0,0 +1,14 @@
+pkgname = "intltool"
+pkgver = "0.51.0"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "perl-xml-parser"]
+makedepends = ["perl-xml-parser", "gettext-tiny"]
+depends = list(makedepends)
+pkgdesc = "Internationalization tools"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://edge.launchpad.net/intltool"
+source = f"http://launchpad.net/intltool/trunk/{pkgver}/+download/{pkgname}-{pkgver}.tar.gz"
+sha256 = "67c74d94196b153b774ab9f89b2fa6c6ba79352407037c8c14d5aeb334e959cd"
