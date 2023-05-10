0d2cafa61551a109a74710c94eea8fc6fbb164a4
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 16:17:51 +0200

    main/python-pyxdg: new package (0.28)

---
+++ main/python-pyxdg/template.py
@@ -0,0 +1,12 @@
+pkgname = "python-pyxdg"
+pkgver = "0.28"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+depends = ["python"]
+pkgdesc = "Freedesktop.org standards module for Python"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.0-only"
+url = "https://freedesktop.org/Software/pyxdg"
+source = f"$(PYPI_SITE)/p/pyxdg/pyxdg-{pkgver}.tar.gz"
+sha256 = "3267bb3074e934df202af2ee0868575484108581e6f3cb006af1da35395e88b4"
