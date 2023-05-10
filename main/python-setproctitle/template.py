4cdc204c2e914783d548c967a842c1f564fdfd75
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 16:21:50 +0200

    main/python-setproctitle: new package (1.3.2)

---
+++ main/python-setproctitle/template.py
@@ -0,0 +1,19 @@
+pkgname = "python-setproctitle"
+pkgver = "1.3.2"
+pkgrel = 0
+build_style = "python_module"
+make_check_target = "tests"
+hostmakedepends = ["python-setuptools"]
+depends = ["python"]
+checkdepends = ["python-pytest"]
+pkgdesc = "Python module for process title customization"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-3-Clause"
+url = "https://github.com/dvarrazzo/py-setproctitle"
+source = f"$(PYPI_SITE)/s/setproctitle/setproctitle-{pkgver}.tar.gz"
+sha256 = "b9fb97907c830d260fa0658ed58afd48a86b2b88aac521135c352ff7fd3477fd"
+# can't find itself
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("COPYRIGHT")
