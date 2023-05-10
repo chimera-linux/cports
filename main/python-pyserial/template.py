263b554696357369712a6ab09b855060aef521e8
Daniel Kolesa <q66@chimera-linux.org>
2023-05-10 14:48:55 +0200

    main/python-pyserial: new package (3.5)

---
+++ main/python-pyserial/template.py
@@ -0,0 +1,16 @@
+pkgname = "python-pyserial"
+pkgver = "3.5"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+depends = ["python"]
+checkdepends = ["python-pytest"]
+pkgdesc = "Python serial port module"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-3-Clause"
+url = "https://github.com/pyserial/pyserial"
+source = f"$(PYPI_SITE)/p/pyserial/pyserial-{pkgver}.tar.gz"
+sha256 = "3c77e014170dfffbd816e6ffc205e9842efb10be9f58ec16d3e8675b4925cddb"
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
