5c688d9fd650716fff846199f123107e66a9727c
Daniel Kolesa <q66@chimera-linux.org>
2023-05-11 13:22:16 +0200

    main/libgee: new package (0.20.6)

---
+++ main/libgee/template.py
@@ -0,0 +1,17 @@
+pkgname = "libgee"
+pkgver = "0.20.6"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--disable-static"]
+hostmakedepends = ["pkgconf", "vala"]
+makedepends = ["glib-devel"]
+pkgdesc = "GObject collection library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.1-or-later"
+url = "https://wiki.gnome.org/Libgee"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "1bf834f5e10d60cc6124d74ed3c1dd38da646787fbf7872220b8b4068e476d4d"
+
+@subpackage("libgee-devel")
+def _devel(self):
+    return self.default_devel()
