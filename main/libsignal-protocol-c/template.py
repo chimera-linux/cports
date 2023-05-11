8a5298858112a0c418e2fd5fc13200f372bd38a2
Daniel Kolesa <q66@chimera-linux.org>
2023-05-11 13:31:33 +0200

    main/libsignal-protocol-c: new package (2.3.3)

---
+++ main/libsignal-protocol-c/template.py
@@ -0,0 +1,20 @@
+pkgname = "libsignal-protocol-c"
+pkgver = "2.3.3"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    "-DBUILD_SHARED_LIBS=ON", "-DBUILD_TESTING=ON",
+]
+hostmakedepends = ["cmake", "pkgconf", "ninja", "openssl-devel"]
+makedepends = ["openssl-devel"]
+checkdepends = ["check-devel"]
+pkgdesc = "Signal protocol C library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-3.0-or-later"
+url = "https://www.whispersystems.org"
+source = f"https://github.com/signalapp/{pkgname}/archive/v{pkgver}.tar.gz"
+sha256 = "c22e7690546e24d46210ca92dd808f17c3102e1344cd2f9a370136a96d22319d"
+
+@subpackage("libsignal-protocol-c-devel")
+def _devel(self):
+    return self.default_devel()
