993935f050c9aa6d71682127b99fc6b8f7e8db2a
Daniel Kolesa <q66@chimera-linux.org>
2023-05-12 01:54:26 +0200

    contrib/mypaint-brushes: new package (2.0.2)

---
+++ contrib/mypaint-brushes/template.py
@@ -0,0 +1,18 @@
+pkgname = "mypaint-brushes"
+pkgver = "2.0.2"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "automake", "pkgconf"]
+pkgdesc = "Brushes for libmypaint"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "CC0-1.0"
+url = "https://github.com/mypaint/mypaint-brushes"
+source = f"{url}/archive/v{pkgver}.tar.gz"
+sha256 = "01032550dd817bb0f8e85d83a632ed2e50bc16e0735630839e6c508f02f800ac"
+
+def pre_configure(self):
+    self.do(self.chroot_cwd / "autogen.sh")
+
+def post_install(self):
+    self.install_license("COPYING")
