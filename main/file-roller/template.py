7609f0db893701ee8367909c98b90a407c258900
Daniel Kolesa <q66@chimera-linux.org>
2023-05-11 13:05:17 +0200

    main/file-roller: new package (44_alpha)

---
+++ main/file-roller/template.py
@@ -0,0 +1,21 @@
+pkgname = "file-roller"
+pkgver = "44_alpha"
+pkgrel = 0
+build_style = "meson"
+_commit = "49405db32edd55606d7f19056e425b7c9293119a"
+hostmakedepends = [
+    "meson", "pkgconf", "gettext-tiny", "glib-devel", "desktop-file-utils",
+    "itstool", "gtk-update-icon-cache"
+]
+makedepends = [
+    "gtk4-devel", "glib-devel", "libarchive-devel", "libnotify-devel",
+    "nautilus-devel", "libadwaita-devel", "libportal-devel",
+    "json-glib-devel",
+]
+depends = ["hicolor-icon-theme", "desktop-file-utils"]
+pkgdesc = "GNOME archiver frontend"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://wiki.gnome.org/Apps/FileRoller"
+source = f"https://gitlab.gnome.org/GNOME/{pkgname}/-/archive/{_commit}.tar.gz"
+sha256 = "4a71ea19c268f95ec561c100945d7ac4ccc92bdae097fd4c6defcb4088f54d3f"
