ada62939ae6805d4fd7a2560e23e30f9424a9a0f
Daniel Kolesa <q66@chimera-linux.org>
2023-05-16 00:04:26 +0200

    contrib/unshield: new package (1.5.1)

---
+++ contrib/unshield/template.py
@@ -0,0 +1,25 @@
+pkgname = "unshield"
+pkgver = "1.5.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = ["zlib-devel"]
+pkgdesc = "Utility for InstallShield CAB archive extraction"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://github.com/twogood/unshield"
+source = f"{url}/archive/{pkgver}.tar.gz"
+sha256 = "34cd97ff1e6f764436d71676e3d6842dc7bd8e2dd5014068da5c560fe4661f60"
+# runner not integrated
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+@subpackage("unshield-libs")
+def _libs(self):
+    return self.default_libs()
+
+@subpackage("unshield-devel")
+def _devel(self):
+    return self.default_devel()
