c8d8834f458ec68f4a0cc29907ba6c199528efc8
Daniel Kolesa <q66@chimera-linux.org>
2023-05-15 18:59:23 +0200

    contrib/libb2: new package (0.98.1)

---
+++ contrib/libb2/template.py
@@ -0,0 +1,18 @@
+pkgname = "libb2"
+pkgver = "0.98.1"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["pkgconf", "automake", "libtool"]
+pkgdesc = "BLAKE2 hash library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "CC0-1.0"
+url = "https://www.blake2.net"
+source = f"https://github.com/BLAKE2/{pkgname}/archive/v{pkgver}.tar.gz"
+sha256 = "10053dbc2fa342516b780a6bbf6e7b2a2360b8d49c5ac426936bf3df82526732"
+
+def pre_configure(self):
+    self.do("autoreconf", "-if")
+
+@subpackage("libb2-devel")
+def _devel(self):
+    return self.default_devel()
