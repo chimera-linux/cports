e1f22ed4d29f119eb570219332a593967092f502
eater <=@eater.me>
2023-05-16 05:07:36 +0200

    contrib/mtr: new package (0.95)
    Closes https://github.com/chimera-linux/cports/pull/174

---
+++ contrib/mtr/template.py
@@ -0,0 +1,17 @@
+pkgname = "mtr"
+pkgver = "0.95"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--without-gtk"]
+configure_gen = ["./bootstrap.sh"]
+hostmakedepends = ["automake", "pkgconf", "libcap-progs"]
+makedepends = ["ncurses-devel", "libcap-devel"]
+depends = ["libcap-progs"]
+pkgdesc = "Network diagnostic tool"
+maintainer = "eater <=@eater.me>"
+license = "GPL-3.0-only AND BSD-3-Clause"
+url = "https://www.bitwizard.nl/mtr"
+source = f"https://github.com/traviscross/{pkgname}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "12490fb660ba5fb34df8c06a0f62b4f9cbd11a584fc3f6eceda0a99124e8596f"
+# tries to reach the internet
+options = ["!check"]
