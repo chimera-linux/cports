61a9c806f85433aa48d152c8e22e6b403cdccfdd
Daniel Kolesa <q66@chimera-linux.org>
2023-05-18 00:31:20 +0200

    contrib/gsl: new package (2.7.1)

---
+++ contrib/gsl/template.py
@@ -0,0 +1,18 @@
+pkgname = "gsl"
+pkgver = "2.7.1"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = ["pkgconf", "gmake"]
+pkgdesc = "GNU Scientific Library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-3.0-or-later"
+url = "http://www.gnu.org/software/gsl/gsl.html"
+source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "dcb0fbd43048832b757ff9942691a8dd70026d5da0ff85601e52687f6deeb34b"
+# FIXME fails tests
+hardening = ["!int"]
+
+@subpackage("gsl-devel")
+def _devel(self):
+    return self.default_devel()
