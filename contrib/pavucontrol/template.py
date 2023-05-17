8675266e08efb1206221b77e132d7cd9c4f994b2
Daniel Kolesa <q66@chimera-linux.org>
2023-05-17 21:58:43 +0200

    contrib/pavucontrol: new package (5.0)

---
+++ contrib/pavucontrol/template.py
@@ -0,0 +1,17 @@
+pkgname = "pavucontrol"
+pkgver = "5.0"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "pkgconf", "intltool"]
+makedepends = [
+    "gtkmm3.0-devel", "libcanberra-devel", "gtk+3-devel",
+    "libpulse-devel", "json-glib-devel"
+]
+depends = ["desktop-file-utils"]
+pkgdesc = "PulseAudio volume control"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://freedesktop.org/software/pulseaudio/pavucontrol"
+source = f"$(FREEDESKTOP_SITE)/pulseaudio/{pkgname}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "ce2b72c3b5f1a70ad0df19dd81750f9455bd20870d1d3a36d20536af2e8f4e7a"
