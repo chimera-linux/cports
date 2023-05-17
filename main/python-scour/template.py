fb2396bf95d4d508fd4ff52e059b3a40cfc59dd8
Daniel Kolesa <q66@chimera-linux.org>
2023-05-17 23:55:39 +0200

    main/python-scour: new package (0.38.2)

---
+++ main/python-scour/template.py
@@ -0,0 +1,13 @@
+pkgname = "python-scour"
+pkgver = "0.38.2"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+depends = ["python-setuptools", "python-six"]
+checkdepends = ["python-six"]
+pkgdesc = "Python SVG scrubber"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "Apache-2.0"
+url = "https://github.com/scour-project/scour"
+source = f"$(PYPI_SITE)/s/scour/scour-{pkgver}.tar.gz"
+sha256 = "6881ec26660c130c5ecd996ac6f6b03939dd574198f50773f2508b81a68e0daf"
