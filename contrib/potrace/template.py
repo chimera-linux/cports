61b874c5dfbae1e5542a04f848fd33e43f560388
Daniel Kolesa <q66@chimera-linux.org>
2023-05-17 23:52:58 +0200

    contrib/potrace: new package (1.16)

---
+++ contrib/potrace/template.py
@@ -0,0 +1,17 @@
+pkgname = "potrace"
+pkgver = "1.16"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--with-libpotrace"]
+hostmakedepends = ["pkgconf"]
+makedepends = ["zlib-devel"]
+pkgdesc = "Bitmap to vector tracer"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://potrace.sourceforge.net"
+source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "be8248a17dedd6ccbaab2fcc45835bb0502d062e40fbded3bc56028ce5eb7acc"
+
+@subpackage("potrace-devel")
+def _devel(self):
+    return self.default_devel()
