7a83cc7c6cca764a591a0394394b9f3dd39c225b
Daniel Kolesa <q66@chimera-linux.org>
2023-05-17 23:57:26 +0200

    main/python-appdirs: new package (1.4.4)

---
+++ main/python-appdirs/template.py
@@ -0,0 +1,15 @@
+pkgname = "python-appdirs"
+pkgver = "1.4.4"
+pkgrel = 0
+build_style = "python_module"
+hostmakedepends = ["python-setuptools"]
+depends = ["python"]
+pkgdesc = "Platform-specific directory module for Python"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://github.com/ActiveState/appdirs"
+source = f"$(PYPI_SITE)/a/appdirs/appdirs-{pkgver}.tar.gz"
+sha256 = "7d5d0167b2b1ba821647616af46a749d1c653740dd0d2415100fe26e27afdf41"
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
