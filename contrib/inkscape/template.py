68769a46b7c741e68c357687b09f44b723492682
Daniel Kolesa <q66@chimera-linux.org>
2023-05-18 01:01:43 +0200

    contrib/inkscape: new package (1.2.2)

---
+++ contrib/inkscape/template.py
@@ -0,0 +1,29 @@
+pkgname = "inkscape"
+pkgver = "1.2.2"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DBUILD_TESTING=OFF"]
+hostmakedepends = [
+    "cmake", "ninja", "pkgconf", "perl", "gettext-tiny", "glib-devel",
+]
+# TODO: imagemagick once we have it
+makedepends = [
+    "gtk+3-devel", "boost-devel", "gc-devel", "gtkmm3.0-devel", "gspell-devel",
+    "lcms2-devel", "libjpeg-turbo-devel", "libsoup-devel", "libxml2-devel",
+    "libxslt-devel", "libpoppler-glib-devel", "popt-devel", "potrace-devel",
+    "gsl-devel", "double-conversion-devel", "libomp-devel", "python-devel",
+    "libedit-readline-devel",
+]
+depends = [
+    "desktop-file-utils", "python-scour", "python-appdirs", "python-lxml",
+    "python-pillow",
+]
+pkgdesc = "Vector graphics editor"
+license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
+url = "https://inkscape.org"
+source = f"https://media.inkscape.org/dl/resources/file/{pkgname}-{pkgver}.tar.xz"
+sha256 = "a0c7fd0d03c0a21535e648ef301dcf80dd7cfc1f3545e51065fbf1ba3ee8a5c4"
+# TODO
+hardening = ["!int"]
+# long, heavy, etc
+options = ["!check"]
