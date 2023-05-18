0eb4ffc6007c64e59383aa1a15e0a14ceaeb3f78
Daniel Kolesa <q66@chimera-linux.org>
2023-05-18 18:44:45 +0200

    main/dtc: switch to meson
    This also somehow fixes the test suite on ppc64le, and allows
    qemu to work (without crashing on libfdt API usage). I have no
    idea why, probably some flags applied by the makefile. In any
    case, with meson it all passes and everything works.

--- main/dtc/template.py
+++ main/dtc/template.py
@@ -1,22 +1,16 @@
 pkgname = "dtc"
 pkgver = "1.7.0"
 pkgrel = 0
-build_style = "makefile"
-make_cmd = "gmake"
-make_build_args = ["NO_PYTHON=1"]
-make_install_args = make_build_args
-hostmakedepends = ["gmake", "flex", "bison", "pkgconf"]
+build_style = "meson"
+configure_args = ["-Dpython=disabled", "-Ddefault_library=shared"]
+hostmakedepends = ["meson", "ninja", "flex", "bison", "pkgconf"]
 makedepends = ["libyaml-devel"]
 pkgdesc = "Device Tree Compiler"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://git.kernel.org/pub/scm/utils/dtc/dtc.git"
-source = f"https://www.kernel.org/pub/software/utils/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "29edce3d302a15563d8663198bbc398c5a0554765c83830d0d4c0409d21a16c4"
-
-if self.profile().arch == "ppc64le":
-    # weird crashes
-    options = ["!check"]
+source = f"https://git.kernel.org/pub/scm/utils/dtc/dtc.git/snapshot/dtc-e8364666d5acc985c434fb574e92c5206d9a8d6b.tar.gz"
+sha256 = "9871d96b26c495561b64256bef17a749856ab1fb78b1d5e99a27ba9fe6d7bfb0"
 
 @subpackage("dtc-devel")
 def _devel(self):
