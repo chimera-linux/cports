9699041b483be5069a4546766efcc9d651a3d7fe
Daniel Kolesa <q66@chimera-linux.org>
2023-05-18 21:39:12 +0200

    main/less: update to 633

--- main/less/template.py
+++ main/less/template.py
@@ -1,16 +1,20 @@
 pkgname = "less"
-pkgver = "608"
+pkgver = "633"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-regex=posix"]
+make_cmd = "gmake"
+hostmakedepends = ["gmake"]
 makedepends = ["ncurses-devel"]
 pkgdesc = "Pager program similar to more(1)"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:less OR GPL-3.0-or-later"
 url = "http://www.greenwoodsoftware.com/less"
 source = f"http://www.greenwoodsoftware.com/less/less-{pkgver}.tar.gz"
-sha256 = "a69abe2e0a126777e021d3b73aa3222e1b261f10e64624d41ec079685a6ac209"
+sha256 = "2f201d64b828b88af36dfe6cfdba3e0819ece2e446ebe6224813209aaefed04f"
 hardening = ["vis", "cfi"]
+# test suite not shipped
+options = ["!check"]
 
 def post_install(self):
     self.install_file(self.files_path / "lesspipe.sh", "usr/bin", mode = 0o755)
