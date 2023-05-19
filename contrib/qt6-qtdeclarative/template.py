05805526abdc1002963c7abc84da88d4c3053c63
yopito <pierre.bourgin@free.fr>
2023-05-19 08:54:56 +0200

    contrib/qt6-qtdeclarative: update to 6.5.1, enable check

--- contrib/qt6-qtdeclarative/template.py
+++ contrib/qt6-qtdeclarative/template.py
@@ -1,10 +1,8 @@
 pkgname = "qt6-qtdeclarative"
-pkgver = "6.5.0"
+pkgver = "6.5.1"
 pkgrel = 0
 build_style = "cmake"
-configure_args = [
-    "-DQT_BUILD_TESTS=ON",
-]
+configure_args = ["-DQT_BUILD_TESTS=ON"]
 hostmakedepends = [
     "cmake",
     "ninja",
@@ -16,17 +14,76 @@ hostmakedepends = [
 ]
 makedepends = ["qt6-qtbase-devel", "qt6-qtshadertools-devel"]
 pkgdesc = "Qt6 declarative component"
+maintainer = "q66 <q66@chimera-linux.org>"
 license = (
     "LGPL-2.1-only AND LGPL-3.0-only AND GPL-3.0-only WITH Qt-GPL-exception-1.0"
 )
 url = "https://www.qt.io"
 source = f"https://download.qt.io/official_releases/qt/{pkgver[:-2]}/{pkgver}/submodules/qtdeclarative-everywhere-src-{pkgver}.tar.xz"
-sha256 = "f7d631cd8ebc1491dad0f30f1b5326ae812bee4ad706e61157816a82bf588c97"
+sha256 = "201148f9614a65d169a85cd0d0fbd414603498da6ff330c4e0835ecd962dce79"
 debug_level = 1  # defatten, especially with LTO
 # FIXME
 hardening = ["!int"]
 # TODO
-options = ["!check", "!cross"]
+options = ["!cross"]
+
+
+def do_check(self):
+    excl_list = [
+        "test_qml_app_deployment",  # missing /usr/lib/cmake/Qt6Quick/Qt6QuickConfig.cmake
+        "module_includes",  # Could NOT find Qt6 (missing: Qt6_DIR)
+        "cmake_tooling_imports",  # missing /usr/lib/cmake/Qt6Qml/Qt6QmlConfig.cmake
+        "empty_qmldir",  # missing /usr/lib/cmake/Qt6Qml/Qt6QmlConfig.cmake
+        "qmlquery",  # missing /usr/lib/cmake/Qt6Qml/Qt6QmlConfig.cmake
+        "qtquickcompiler",  # missing /usr/lib/cmake/Qt6Qml/Qt6QmlConfig.cmake
+        "cmake_test_common_import_path",  # missing /usr/lib/cmake/Qt6Qml/Qt6QmlConfig.cmake
+        "tst_qjsengine", # tst_QJSEngine::interrupt(tail call / jit) 'jsEngine.isInterrupted()' returned FALSE. () - maybe ppc64le only
+        "tst_qqmlapplicationengine",  # tst_qqmlapplicationengine::application(delayed quit) 'QString(testStdErr).endsWith(QString(expectedStdErr))' returned FALSE.
+        "tst_qqmljsscope",  # missing builtins.qmltypes, jsroot.qmltypes
+        "tst_qdebugmessageservice",  # Could not launch app  "/usr/lib/qt6/bin/qml"
+        "tst_qqmldebugtranslationclient",  # Could not launch app  "/usr/lib/qt6/bin/qml"
+        "tst_qqmldebugjs",  # Could not launch app  "/usr/lib/qt6/bin/qmlscene"
+        "tst_qqmlinspector",  # Could not launch app  "/usr/lib/qt6/bin/qml"
+        "tst_qqmlprofilerservice",  # Could not launch app  "/usr/lib/qt6/bin/qmlscene"
+        "tst_qqmlenginedebuginspectorintegration",  # Could not launch app  "/usr/lib/qt6/bin/qml"
+        "tst_qqmlenginecontrol",  # Could not launch app  "/usr/lib/qt6/bin/qmlscene"
+        "tst_qqmldebuggingenabler",  # Could not launch app  "/usr/lib/qt6/bin/qmlscene"
+        "tst_qqmldebugprocess",  # Timeout while waiting for QML debugging messages
+        "tst_qqmlpreview",  # Could not launch app  "/usr/lib/qt6/bin/qml"
+        "tst_qmlformat",  # qmlformat executable not found (looked for /usr/lib/qt6/bin/qmlformat)
+        "tst_qmlimportscanner",  # qmlimportscanner executable not found (looked for /usr/lib/qt6/libexec/qmlimportscanner)
+        "tst_qmllint",  # qmllint executable not found (looked for /usr/lib/qt6/bin/qmllint)
+        "tst_qmltc_qprocess",  # qmltc executable not found (looked for /usr/lib/qt6/bin/qmltc)
+        "tst_qmlplugindump",  # qmlplugindump executable not found (looked for /usr/lib/qt6/bin/qmlplugindump)
+        "tst_qml",  # tst_qml::initTestCase() 'QFileInfo(qmlPath).exists()' returned FALSE. ()
+        "tst_qqmlextensionplugin",  # tst_qqmlextensionplugin::iidCheck() ASSERT failure in QTest::fetchData(): "Test data requested, but no testdata available"
+        "text",  # test failed
+        "tst_qmldomitem",  # Error: Could not find builtins.qmltypes file
+        "tst_dom_all",  # Error: Could not find builtins.qmltypes file
+        "tst_basic",  # test failed
+        "tst_fusion",  # test failed
+        "tst_imagine",  # XXX
+        "tst_material",  # XXX
+        "tst_universal",  # XXX
+        "tst_qquickiconimage",  # execution failed with exit code Segmentation fault
+        "tst_qquickfiledialogimpl",  # XXX
+        "tst_qquickfolderdialogimpl",  # test failed
+    ]
+    self.do(
+        "ctest",
+        f"-j{self.make_jobs}",
+        "-E",
+        "(" + "|".join(excl_list) + ")",
+        wrksrc=self.make_dir,
+        env={
+            "QT_QPA_PLATFORM": "offscreen",
+            "CTEST_OUTPUT_ON_FAILURE": "True",
+            # qml stuff is not yet installed
+            "QML2_IMPORT_PATH": str(
+                self.chroot_cwd / f"{self.make_dir}/lib/qt6/qml"
+            ),
+        },
+    )
 
 
 def post_install(self):
