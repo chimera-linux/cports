3c13e35668cc6b4aa0d1a5c070599bc7ef05dcbd
yopito <pierre.bourgin@free.fr>
2023-05-19 08:48:30 +0200

    contrib/qt6-qtsvg: update to 6.5.1, enable check

--- contrib/qt6-qtsvg/template.py
+++ contrib/qt6-qtsvg/template.py
@@ -1,24 +1,22 @@
 pkgname = "qt6-qtsvg"
-pkgver = "6.5.0"
+pkgver = "6.5.1"
 pkgrel = 0
 build_style = "cmake"
-configure_args = [
-    "-DQT_BUILD_TESTS=ON",
-]
+configure_args = ["-DQT_BUILD_TESTS=ON"]
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
 hostmakedepends = ["cmake", "ninja", "pkgconf", "perl", "qt6-qtbase"]
 makedepends = ["qt6-qtbase-devel"]
 pkgdesc = "Qt6 SVG component"
+maintainer = "q66 <q66@chimera-linux.org>"
 license = (
     "LGPL-2.1-only AND LGPL-3.0-only AND GPL-3.0-only WITH Qt-GPL-exception-1.0"
 )
 url = "https://www.qt.io"
 source = f"https://download.qt.io/official_releases/qt/{pkgver[:-2]}/{pkgver}/submodules/qtsvg-everywhere-src-{pkgver}.tar.xz"
-sha256 = "64ca7e61f44d51e28bcbb4e0509299b53a9a7e38879e00a7fe91643196067a4f"
+sha256 = "d58d29491d44f0f59b684686a9898fec0e6c4fb7c09d9393b4e9c211fe9608ef"
 debug_level = 1  # defatten, especially with LTO
 # FIXME
 hardening = ["!int"]
-# TODO
-options = ["!check"]
 
 
 def post_install(self):
