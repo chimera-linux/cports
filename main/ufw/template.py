0d5fd4b12a7358582ba61a5ee840339186b4d0bd
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 00:37:47 +0200

    main/ufw: update to 0.36.2

--- main/ufw/template.py
+++ main/ufw/template.py
@@ -1,5 +1,5 @@
 pkgname = "ufw"
-pkgver = "0.36.1"
+pkgver = "0.36.2"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools", "gmake", "iptables"]
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-only"
 url = "https://launchpad.net/ufw"
 source = f"{url}/{pkgver[:-2]}/{pkgver}/+download/{pkgname}-{pkgver}.tar.gz"
-sha256 = "1c57e78fbf2970f0cc9c56ea87a231e6d83d825e55b9e31e2c88b91b0ea03c8c"
+sha256 = "2a57a99eecef6b44db3537ed2520b30bae3759f8465456e22e404cd643838bf5"
 # needs itself installed
 options = ["!check"]
 
