b3028c2bb4f57633b5193f2f01d809eb699095a2
Daniel Kolesa <q66@chimera-linux.org>
2023-05-31 04:28:26 +0200

    main/byacc: update to 20230521

--- main/byacc/template.py
+++ main/byacc/template.py
@@ -1,8 +1,9 @@
 pkgname = "byacc"
-pkgver = "20230219"
+pkgver = "20230521"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--program-transform=s,^,b,"]
+configure_gen = []
 pkgdesc = "Berkeley yacc, a LALR(1) parser generator"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:byacc"
@@ -10,7 +11,7 @@ url = "http://invisible-island.net/byacc"
 source = (
     f"https://invisible-island.net/archives/{pkgname}/{pkgname}-{pkgver}.tgz"
 )
-sha256 = "36b972a6d4ae97584dd186925fbbc397d26cb20632a76c2f52ac7653cd081b58"
+sha256 = "5ad915a7d5833aa38a5e31bd077505666029c35e365dff8569fe4598eaa9fef2"
 hardening = ["vis", "cfi"]
 
 
@@ -19,6 +20,3 @@ def post_install(self):
     self.install_license("LICENSE")
     self.install_link("byacc", "usr/bin/yacc")
     self.install_link("byacc.1", "usr/share/man/man1/yacc.1")
-
-
-configure_gen = []
