166a77b4e3fa4fd04fe9ca5e17e9f8faed20232c
Daniel Kolesa <q66@chimera-linux.org>
2023-05-31 18:09:41 +0200

    main/xdpyinfo: update to 1.3.4

--- main/xdpyinfo/template.py
+++ main/xdpyinfo/template.py
@@ -1,9 +1,9 @@
 pkgname = "xdpyinfo"
-pkgver = "1.3.3"
+pkgver = "1.3.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--without-dmx"]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["pkgconf", "automake", "libtool", "xorg-util-macros"]
 makedepends = [
     "libxext-devel",
     "libxtst-devel",
@@ -18,12 +18,9 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
-sha256 = "2ae7b8213ea839b8376843477496276e8d69550c48bff081e16376539fc27c5a"
+sha256 = "fbd1e18885f67332b330fecd83592af25ad42d21457aaabfbd31a5a97388652a"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
