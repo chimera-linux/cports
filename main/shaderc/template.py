c27fac28515de8c242b8208905c2df9d7130db53
Daniel Kolesa <q66@chimera-linux.org>
2023-05-31 04:22:34 +0200

    main/shaderc: update to 2023.4

--- main/shaderc/template.py
+++ main/shaderc/template.py
@@ -1,6 +1,6 @@
 pkgname = "shaderc"
-pkgver = "2023.3"
-pkgrel = 1
+pkgver = "2023.4"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DSHADERC_SKIP_TESTS=ON", "-DSHADERC_SKIP_EXAMPLES=ON"]
 hostmakedepends = ["cmake", "ninja", "python", "pkgconf"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/google/shaderc"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "7f66435c59797cdc6370dc97aa5cab21651385ac6c5159975566d51cc3e6650f"
+sha256 = "671c5750638ff5e42e0e0e5325b758a1ab85e6fd0fe934d369a8631c4292f12f"
 tool_flags = {
     "CXXFLAGS": [f"-I{self.profile().sysroot / 'usr/include/glslang'}"]
 }
