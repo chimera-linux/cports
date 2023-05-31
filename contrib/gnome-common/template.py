eefb55ac2f738accff3ac7665616ddec9e5eb8f7
yopito <pierre.bourgin@free.fr>
2023-05-31 23:15:32 +0200

    contrib/gnome-common: new package (3.18.0)

---
+++ contrib/gnome-common/template.py
@@ -0,0 +1,14 @@
+pkgname = "gnome-common"
+pkgver = "3.18.0"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--with-autoconf-archive"]
+hostmakedepends = ["automake"]
+depends = ["automake"]
+pkgdesc = "Common scripts and macros to develop with GNOME"
+maintainer = "yopito <pierre.bourgin@free.fr>"
+license = "GPL-2.0-only"
+url = "https://www.gnome.org"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "22569e370ae755e04527b76328befc4c73b62bfd4a572499fde116b8318af8cf"
+hardening = ["vis"]
