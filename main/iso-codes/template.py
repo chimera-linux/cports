c5a5ba0ddf8c79f6fd42f18fc633d3d989641510
Daniel Kolesa <q66@chimera-linux.org>
2023-05-31 18:52:50 +0200

    main/iso-codes: update to 4.15.0

--- main/iso-codes/template.py
+++ main/iso-codes/template.py
@@ -1,13 +1,12 @@
 pkgname = "iso-codes"
-pkgver = "4.13.0"
+pkgver = "4.15.0"
 pkgrel = 0
 build_style = "gnu_configure"
+configure_gen = []
 hostmakedepends = ["gettext-tiny", "python", "pkgconf"]
 pkgdesc = "List of country, language and currency names"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://salsa.debian.org/iso-codes-team/iso-codes"
 source = f"$(DEBIAN_SITE)/main/i/{pkgname}/{pkgname}_{pkgver}.orig.tar.xz"
-sha256 = "2d4d0e5c02327f52cf7c029202da72f2074348472c26904b7104d2be3e0750ef"
-
-configure_gen = []
+sha256 = "3d50750bf1d62d83b6085f5815ceb8392df34266a15f16bcf8d4cf7eb15d245c"
