629bc8d9f77eca2626595c0c11b000b30750691b
Daniel Kolesa <q66@chimera-linux.org>
2023-05-31 18:51:11 +0200

    contrib/gc: update to 8.2.4

--- contrib/gc/template.py
+++ contrib/gc/template.py
@@ -1,5 +1,5 @@
 pkgname = "gc"
-pkgver = "8.2.2"
+pkgver = "8.2.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -9,14 +9,14 @@ configure_args = [
     "--with-libatomic-ops=none",
 ]
 make_cmd = "gmake"
-hostmakedepends = ["pkgconf", "gmake"]
+hostmakedepends = ["pkgconf", "gmake", "automake", "libtool"]
 makedepends = ["linux-headers"]
 pkgdesc = "Boehm garbage collector for C/C++"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.hboehm.info/gc"
 source = f"https://github.com/ivmai/bdwgc/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "f30107bcb062e0920a790ffffa56d9512348546859364c23a14be264b38836a0"
+sha256 = "3d0d3cdbe077403d3106bb40f0cbb563413d6efdbb2a7e1cd6886595dec48fc2"
 
 tool_flags = {
     "CFLAGS": [
@@ -35,6 +35,3 @@ def post_install(self):
 @subpackage("gc-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
