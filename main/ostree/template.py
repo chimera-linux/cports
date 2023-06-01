d70c6dc4a75d67ad54bf4bb4435aa7a382bcc074
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 18:53:01 +0200

    main/ostree: update to 2023.3

--- main/ostree/template.py
+++ main/ostree/template.py
@@ -1,5 +1,5 @@
 pkgname = "ostree"
-pkgver = "2023.2"
+pkgver = "2023.3"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -12,12 +12,15 @@ configure_args = [
 ]
 make_cmd = "gmake"
 hostmakedepends = [
+    "automake",
+    "libtool",
     "pkgconf",
     "bison",
     "xsltproc",
     "gmake",
     "gobject-introspection",
     "docbook-xsl-nons",
+    "gtk-doc-tools",
 ]
 makedepends = [
     "glib-devel",
@@ -37,7 +40,7 @@ maintainer = "eater <=@eater.me>"
 license = "LGPL-2.0-or-later"
 url = "https://ostreedev.github.io/ostree"
 source = f"https://github.com/ostreedev/ostree/releases/download/v{pkgver}/libostree-{pkgver}.tar.xz"
-sha256 = "ceb0788755a0bff5738d44543552fbf883cf71df481baa3ca7288da7a402bb85"
+sha256 = "9c575a094da2c307769f0aee15de95470c3b5550cbff211fbbc4f6fc75f333b1"
 # failing on their test harness, i will find motivation Soon
 options = ["!check", "!cross"]
 
@@ -45,6 +48,3 @@ options = ["!check", "!cross"]
 @subpackage("ostree-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
