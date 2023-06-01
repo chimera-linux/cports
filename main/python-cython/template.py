8048e8840c97ef6c8ec81d7d4f0f172770f3653a
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 16:34:31 +0200

    main/python-cython: update to 0.29.35

--- main/python-cython/template.py
+++ main/python-cython/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-cython"
-pkgver = "0.29.34"
+pkgver = "0.29.35"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -10,6 +10,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://cython.org"
 source = f"$(PYPI_SITE)/C/Cython/Cython-{pkgver}.tar.gz"
-sha256 = "1909688f5d7b521a60c396d20bba9e47a1b2d2784bfb085401e1e1e7d29a29a8"
+sha256 = "6e381fa0bf08b3c26ec2f616b19ae852c06f5750f4290118bf986b6f85c8c527"
 # flaky tests
 options = ["!check"]
