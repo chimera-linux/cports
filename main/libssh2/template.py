4020f953de57725a56ad2e2303a7107a228a59cf
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 19:46:50 +0200

    main/libssh2: update to 1.11.0

--- main/libssh2/template.py
+++ main/libssh2/template.py
@@ -1,16 +1,21 @@
 pkgname = "libssh2"
-pkgver = "1.10.0"
+pkgver = "1.11.0"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_args = [f"--with-libssl-prefix={self.profile().sysroot / 'usr'}"]
+configure_args = [
+    f"--with-libssl-prefix={self.profile().sysroot / 'usr'}",
+    "--disable-docker-tests",
+]
+configure_gen = []
 hostmakedepends = ["pkgconf"]
 makedepends = ["zlib-devel", "openssl-devel"]
+checkdepends = ["bash"]
 pkgdesc = "Library implementing the SSH2 protocol"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://www.libssh2.org"
 source = f"https://www.libssh2.org/download/{pkgname}-{pkgver}.tar.gz"
-sha256 = "2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51"
+sha256 = "3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461"
 
 
 def post_install(self):
@@ -20,6 +25,3 @@ def post_install(self):
 @subpackage("libssh2-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
