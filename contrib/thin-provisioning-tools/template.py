87b07023486397211834402d6ee9007a71129bae
Daniel Kolesa <q66@chimera-linux.org>
2023-06-02 00:11:54 +0200

    contrib/thin-provisioning-tools: update to 1.0.4

--- contrib/thin-provisioning-tools/template.py
+++ contrib/thin-provisioning-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "thin-provisioning-tools"
-pkgver = "1.0.3"
+pkgver = "1.0.4"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo", "gmake", "gawk"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/jthornber/thin-provisioning-tools"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "8c912be0f7a1cdc40f37fd4226a1794a91fa31667231d4e88338f32cf84b88b6"
+sha256 = "a973786fb9cb49d30be6fb8178d6739bc23609d4114ab601f0983ecdbf349abf"
 # too long
 options = ["!check"]
 
