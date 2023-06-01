a82fafebb7b1c2fc8bb886e74f7f0f647b27685c
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 16:49:56 +0200

    main/python-sphinx_rtd_theme: update to 1.2.1

--- main/python-sphinx_rtd_theme/template.py
+++ main/python-sphinx_rtd_theme/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-sphinx_rtd_theme"
-pkgver = "1.2.0"
+pkgver = "1.2.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,6 +14,6 @@ maintainer = "eater <=@eater.me>"
 license = "MIT"
 url = "https://github.com/readthedocs/sphinx_rtd_theme"
 source = f"https://github.com/readthedocs/sphinx_rtd_theme/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "6f79aaccdf2a6019f6fb7486b22c2b3a37bd8571845ae3105b39486d8b77024e"
+sha256 = "b0b66baaee635120a67e66888b018d9d4ebb439ca6dfcf1e045d1fc30ba171a3"
 # No tests available
 options = ["!check"]
