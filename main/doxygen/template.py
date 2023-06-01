b1722a19ff688674d46a002ece4691adf2eaa1f4
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 23:05:46 +0200

    main/doxygen: update to 1.9.7

--- main/doxygen/template.py
+++ main/doxygen/template.py
@@ -1,5 +1,5 @@
 pkgname = "doxygen"
-pkgver = "1.9.6"
+pkgver = "1.9.7"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "perl", "python", "flex", "bison"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://doxygen.nl"
 source = f"{url}/files/{pkgname}-{pkgver}.src.tar.gz"
-sha256 = "297f8ba484265ed3ebd3ff3fe7734eb349a77e4f95c8be52ed9977f51dea49df"
+sha256 = "87007641c38e2c392c8596f36711eb97633b984c8430f389e7bcf6323a098d94"
 hardening = ["vis", "cfi"]
 
 
