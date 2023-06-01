a9ae8ea1f8ab2867ec6d441431bc4478d16e33ad
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 16:31:03 +0200

    main/python-iniconfig: update to 2.0.0

--- main/python-iniconfig/template.py
+++ main/python-iniconfig/template.py
@@ -1,8 +1,8 @@
 pkgname = "python-iniconfig"
-pkgver = "1.1.1"
+pkgver = "2.0.0"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools_scm"]
+build_style = "python_pep517"
+hostmakedepends = ["python-build", "python-installer", "python-hatch_vcs"]
 checkdepends = ["python-pytest", "python-py"]
 depends = ["python"]
 pkgdesc = "Brain-dead simple config-ini parsing"
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/RonnyPfannschmidt/iniconfig"
 source = f"$(PYPI_SITE)/i/iniconfig/iniconfig-{pkgver}.tar.gz"
-sha256 = "bc3af051d7d14b2ee5ef9969666def0cd1a000e121eaea580d4a313df4b37f32"
+sha256 = "2d91e135bf72d31a410b17c16da610a82cb55f6b0477d1a902134b24a455b8b3"
 # dependency of pytest
 options = ["!check"]
 
