67c98189314a17b52a2dd8b42636297d36b661f6
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 19:33:13 +0200

    main/libxi: update to 1.8.1

--- main/libxi/template.py
+++ main/libxi/template.py
@@ -1,16 +1,22 @@
 pkgname = "libxi"
-pkgver = "1.8"
+pkgver = "1.8.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-malloc0returnsnull"]
-hostmakedepends = ["pkgconf", "xmlto"]
+hostmakedepends = [
+    "pkgconf",
+    "xmlto",
+    "automake",
+    "libtool",
+    "xorg-util-macros",
+]
 makedepends = ["xorgproto", "libxfixes-devel", "libxext-devel"]
 pkgdesc = "X Input extension library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libXi-{pkgver}.tar.bz2"
-sha256 = "2ed181446a61c7337576467870bc5336fc9e222a281122d96c4d39a3298bba00"
+source = f"$(XORG_SITE)/lib/libXi-{pkgver}.tar.gz"
+sha256 = "3b5f47c223e4b63d7f7fe758886b8bf665b20a7edb6962c423892fd150e326ea"
 
 
 def post_install(self):
@@ -20,6 +26,3 @@ def post_install(self):
 @subpackage("libxi-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
