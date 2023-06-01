520697e71ccceac8ffae33b776cac9bb7dd72e2d
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 19:10:18 +0200

    main/opus: update to 1.4

--- main/opus/template.py
+++ main/opus/template.py
@@ -1,16 +1,16 @@
 pkgname = "opus"
-pkgver = "1.3.1"
+pkgver = "1.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-float-approx"]
 make_cmd = "gmake"
-hostmakedepends = ["pkgconf", "gmake"]
+hostmakedepends = ["pkgconf", "gmake", "automake", "libtool"]
 pkgdesc = "Totally open, royalty-free, highly versatile audio codec"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://www.opus-codec.org"
-source = f"https://archive.mozilla.org/pub/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "65b58e1e25b2a114157014736a3d9dfeaad8d41be1c8179866f144a2fb44ff9d"
+source = f"https://downloads.xiph.org/releases/{pkgname}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "c9b32b4253be5ae63d1ff16eea06b94b5f0f2951b7a02aceef58e3a3ce49c51f"
 # FIXME int
 hardening = ["vis", "cfi", "!int"]
 
@@ -22,6 +22,3 @@ def post_install(self):
 @subpackage("opus-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
