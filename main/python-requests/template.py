e3a788e6c8698e75ee9c46a4fdc75f61a00223e9
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 16:46:55 +0200

    main/python-requests: update to 2.31.0

--- main/python-requests/template.py
+++ main/python-requests/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-requests"
-pkgver = "2.28.2"
+pkgver = "2.31.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -15,6 +15,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://python-requests.org"
 source = f"$(PYPI_SITE)/r/requests/requests-{pkgver}.tar.gz"
-sha256 = "98b1b2782e3c6c4904938b84c0eb932721069dfdb9134313beff7c83c2df24bf"
+sha256 = "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
