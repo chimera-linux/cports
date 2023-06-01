5c1e24abeec8d3f301687915c115b3f212f8323b
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 16:25:14 +0200

    main/python-hatchling: new package (1.17.0)

---
+++ main/python-hatchling/template.py
@@ -0,0 +1,29 @@
+pkgname = "python-hatchling"
+pkgver = "1.17.0"
+pkgrel = 0
+build_style = "python_pep517"
+_deps = [
+    "python-packaging",
+    "python-editables",
+    "python-pathspec",
+    "python-pluggy",
+    "python-trove-classifiers",
+]
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+] + _deps
+depends = list(_deps)
+checkdepends = ["python-pytest"] + _deps
+pkgdesc = "Python build backend used by Hatch"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://hatch.pypa.io/latest"
+source = f"$(PYPI_SITE)/h/hatchling/hatchling-{pkgver}.tar.gz"
+sha256 = "b1244db3f45b4ef5a00106a46612da107cdfaf85f1580b8e1c059fefc98b0930"
+# no tests?
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
