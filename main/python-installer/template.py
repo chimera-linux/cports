956185e450aee9fdf4e3571ebda7578aa97edc85
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 15:20:22 +0200

    main/python-installer: new package (0.7.0)

---
+++ main/python-installer/template.py
@@ -0,0 +1,38 @@
+pkgname = "python-installer"
+pkgver = "0.7.0"
+pkgrel = 0
+hostmakedepends = ["python", "python-flit_core"]
+checkdepends = ["python-pytest-xdist"]
+depends = ["python"]
+pkgdesc = "Low-level library for installing from a Python wheel"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://installer.readthedocs.io"
+source = f"$(PYPI_SITE)/i/installer/installer-{pkgver}.tar.gz"
+sha256 = "a26d3e3116289bb08216e0d0f7d925fcef0b0194eedfa0c944bcaaa106c4b631"
+# missing checkdepends
+options = ["!check"]
+
+
+def do_build(self):
+    self.do("python", "-m", "flit_core.wheel")
+
+
+def do_check(self):
+    self.do("python", "-m", "pytest", env={"PYTHONPATH": "src"})
+
+
+def do_install(self):
+    from cbuild.util import python
+
+    self.do(
+        "python",
+        "-m",
+        "installer",
+        "--destdir",
+        self.chroot_destdir,
+        f"dist/installer-{pkgver}-py3-none-any.whl",
+        env={"PYTHONPATH": "src"},
+    )
+    python.precompile(self, "usr/lib")
+    self.install_license("LICENSE")
