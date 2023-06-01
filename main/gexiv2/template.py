4ea6d30d67dc91bb81cb2d54ff8a2159be4f1def
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 22:26:08 +0200

    main/gexiv2: update to 0.14.1

--- main/gexiv2/template.py
+++ main/gexiv2/template.py
@@ -1,5 +1,5 @@
 pkgname = "gexiv2"
-pkgver = "0.14.0"
+pkgver = "0.14.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=true", "-Dvapi=true"]
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Projects/gexiv2"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "e58279a6ff20b6f64fa499615da5e9b57cf65ba7850b72fafdf17221a9d6d69e"
+sha256 = "ec3ee3ec3860b9c78958a55da89cf76ae2305848e12f41945b7b52124d8f6cf9"
 
 
 @subpackage("gexiv2-devel")
