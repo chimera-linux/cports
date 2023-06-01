f2f92dc5d22527fb9590ad08ebfde99c6078a127
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 22:52:22 +0200

    main/ethtool: update to 6.3

--- main/ethtool/template.py
+++ main/ethtool/template.py
@@ -1,16 +1,14 @@
 pkgname = "ethtool"
-pkgver = "6.2"
+pkgver = "6.3"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["pkgconf", "automake", "libtool"]
 makedepends = ["libmnl-devel", "linux-headers"]
 pkgdesc = "Utility for controlling network drivers and hardware"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "http://www.kernel.org/pub/software/network/ethtool"
 source = f"{url}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "86df0114064d4d73f6bf72bf03e85c33964a519ee0c1d1ba65005ad2d0e570e1"
+sha256 = "d9425f0a3df138734001fccc4175fe178c025f938460ac25c4ebc39960168822"
 # FIXME int
 hardening = ["vis", "cfi", "!int"]
-
-configure_gen = []
