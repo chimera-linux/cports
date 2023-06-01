dd5b554efa1aec0ec2f000669a5500265223d027
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 23:18:53 +0200

    contrib/double-conversion: update to 3.3.0

--- contrib/double-conversion/template.py
+++ contrib/double-conversion/template.py
@@ -1,5 +1,5 @@
 pkgname = "double-conversion"
-pkgver = "3.2.1"
+pkgver = "3.3.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_TESTING=ON", "-DBUILD_SHARED_LIBS=ON"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/google/double-conversion"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "e40d236343cad807e83d192265f139481c51fc83a1c49e406ac6ce0a0ba7cd35"
+sha256 = "04ec44461850abbf33824da84978043b22554896b552c5fd11a9c5ae4b4d296e"
 
 
 def post_install(self):
