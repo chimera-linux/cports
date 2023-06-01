1e5030abfac3b5ae54a6b3aec189c00304d7116f
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 19:17:15 +0200

    main/nghttp2: update to 1.53.0

--- main/nghttp2/template.py
+++ main/nghttp2/template.py
@@ -1,8 +1,8 @@
 pkgname = "nghttp2"
-pkgver = "1.52.0"
+pkgver = "1.53.0"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_args = ["--disable-python-bindings"]
+configure_gen = []
 hostmakedepends = ["pkgconf"]
 makedepends = [
     "zlib-devel",
@@ -19,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://nghttp2.org"
 source = f"https://github.com/tatsuhiro-t/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "3ea9f0439e60469ad4d39cb349938684ffb929dd7e8e06a7bffe9f9d21f8ba7d"
+sha256 = "b867184254e5a29b0ba68413aa14f8b0ce1142a371761374598dec092dabb809"
 # FIXME cfi; reproduces in e.g. libsoup
 hardening = ["vis", "!cfi"]
 
@@ -31,6 +31,3 @@ def post_install(self):
 @subpackage("nghttp2-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
