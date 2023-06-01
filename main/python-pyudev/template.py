086aba1e8df1137c1ff649faad470a784de8eeaf
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 16:41:53 +0200

    main/python-pyudev: update to 0.24.1

--- main/python-pyudev/template.py
+++ main/python-pyudev/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pyudev"
-pkgver = "0.24.0"
+pkgver = "0.24.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -10,6 +10,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/pyudev/pyudev"
 source = f"$(PYPI_SITE)/p/pyudev/pyudev-{pkgver}.tar.gz"
-sha256 = "b2a3afe1c99ea751f8296652557eac559874da2a1b1ec0625178706ec5a345f3"
+sha256 = "75e54d37218f5ac45b0da1f0fd9cc5e526a3cac3ef1cfad410cf7ab338b01471"
 # needs itself installed
 options = ["!check"]
