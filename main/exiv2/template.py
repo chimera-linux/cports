9374002138b4a2aef8811a3f6fd83d72987fe1ea
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 22:29:39 +0200

    main/exiv2: update to 0.28.0

--- main/exiv2/template.py
+++ main/exiv2/template.py
@@ -1,5 +1,5 @@
 pkgname = "exiv2"
-pkgver = "0.27.6"
+pkgver = "0.28.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -8,14 +8,14 @@ configure_args = [
     "-DEXIV2_BUILD_UNIT_TESTS=OFF",
 ]
 make_check_target = "unit_test"
-hostmakedepends = ["cmake", "ninja", "pkgconf"]
-makedepends = ["libexpat-devel", "zlib-devel"]
+hostmakedepends = ["cmake", "ninja", "pkgconf", "python"]
+makedepends = ["libexpat-devel", "zlib-devel", "brotli-devel", "inih-devel"]
 pkgdesc = "Image metadata manipulation"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://www.exiv2.org"
 source = f"https://github.com/Exiv2/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}-Source.tar.gz"
-sha256 = "4c192483a1125dc59a3d70b30d30d32edace9e14adf52802d2f853abf72db8a6"
+sha256 = "89af3b5ef7277753ef7a7b5374ae017c6b9e304db3b688f1948e73e103491f3d"
 # needs gtest
 options = ["!check"]
 
