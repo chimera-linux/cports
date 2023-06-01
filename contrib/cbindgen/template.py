02a25c16a56a694ae72a4ec750db2def6774f231
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 23:36:40 +0200

    contrib/cbindgen: update to 0.24.6

--- contrib/cbindgen/template.py
+++ contrib/cbindgen/template.py
@@ -1,5 +1,5 @@
 pkgname = "cbindgen"
-pkgver = "0.24.3"
+pkgver = "0.24.6"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo"]
@@ -9,6 +9,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MPL-2.0"
 url = "https://github.com/eqrion/cbindgen"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "5d693ab54acc085b9f2dbafbcf0a1f089737f7e0cb1686fa338c2aaa05dc7705"
+sha256 = "af0591e687128f7fb4300b0fe24c6091f24593d3a8acadf4fe860bd82c20c4c5"
 # only expected to work with rust nightly
 options = ["!check"]
