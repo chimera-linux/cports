fe5f5fc1769046b9927bbadaa79b8f19235d666d
Daniel Kolesa <q66@chimera-linux.org>
2023-06-01 18:15:54 +0200

    main/rpcsvc-proto: update to 1.4.4

--- main/rpcsvc-proto/template.py
+++ main/rpcsvc-proto/template.py
@@ -1,7 +1,8 @@
 pkgname = "rpcsvc-proto"
-pkgver = "1.4.3"
+pkgver = "1.4.4"
 pkgrel = 0
 build_style = "gnu_configure"
+configure_gen = []
 make_cmd = "gmake"
 hostmakedepends = ["gmake"]
 pkgdesc = "Rpcsvc protocol definitions from glibc"
@@ -9,12 +10,9 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/thkukuk/rpcsvc-proto"
 source = f"{url}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "69315e94430f4e79c74d43422f4a36e6259e97e67e2677b2c7d7060436bd99b1"
+sha256 = "81c3aa27edb5d8a18ef027081ebb984234d5b5860c65bd99d4ac8f03145a558b"
 options = ["!cross"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
