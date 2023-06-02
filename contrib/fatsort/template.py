d87b560a2c642063d8a2e350062bafc8caf9133c
q66 <q66@chimera-linux.org>
2023-06-02 14:36:56 +0200

    contrib/fatsort: new package (1.6.5.640)

---
+++ contrib/fatsort/template.py
@@ -0,0 +1,22 @@
+pkgname = "fatsort"
+pkgver = "1.6.5.640"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_check_target = "tests"
+hostmakedepends = ["gmake"]
+checkdepends = ["bash", "xz"]
+pkgdesc = "FAT filesystem sorting utility"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://fatsort.sourceforge.io"
+source = f"$(SOURCEFORGE_SITE)/project/{pkgname}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "630ece56d9eb3a55524af0aec3aade7854360eba949172a6cfb4768cb8fbe42e"
+# a bunch of seemingly locale-related tests fail
+options = ["!check"]
+
+
+def do_install(self):
+    # makefile uses gnu install syntax
+    self.install_bin("src/fatsort")
+    self.install_man("man/fatsort.1")
