0a0d05838d0b7e1b78ce614fd29a5757eb186530
Daniel Kolesa <q66@chimera-linux.org>
2023-06-03 21:01:33 +0200

    main/xtrans: update to 1.5.0

--- main/xtrans/template.py
+++ main/xtrans/template.py
@@ -1,5 +1,5 @@
 pkgname = "xtrans"
-pkgver = "1.4.0"
+pkgver = "1.5.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "xorg-util-macros"]
@@ -7,8 +7,8 @@ pkgdesc = "Network API translation layer to insulate X"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "377c4491593c417946efcd2c7600d1e62639f7a8bbca391887e2c4679807d773"
+source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.gz"
+sha256 = "a806f8a92f879dcd0146f3f1153fdffe845f2fc0df9b1a26c19312b7b0a29c86"
 hardening = ["vis", "cfi"]
 
 
