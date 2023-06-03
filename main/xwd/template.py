3e4e9a0247c5f9b3d0becfb7b3c0c1f4ad085271
Daniel Kolesa <q66@chimera-linux.org>
2023-06-03 21:00:48 +0200

    main/xwd: update to 1.0.9

--- main/xwd/template.py
+++ main/xwd/template.py
@@ -1,20 +1,17 @@
 pkgname = "xwd"
-pkgver = "1.0.8"
+pkgver = "1.0.9"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["pkgconf", "automake", "libtool", "xorg-util-macros"]
 makedepends = ["libx11-devel", "libxkbfile-devel"]
 pkgdesc = "Dump an image of an X window"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "fbaa2b34163714fe7be22b60920ea4683f63b355babb1781aec2e452a033031b"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
+sha256 = "5fa984e1f7799a7e23ea4e795b3b61483e28df6d0284bae11062f6256c30a9c1"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
