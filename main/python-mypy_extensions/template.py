9f44c7e4ffe499277d281943409734d6f9eaead0
q66 <q66@chimera-linux.org>
2023-06-03 20:31:35 +0200

    main/python-mypy_extensions: new package (1.0.0)

---
+++ main/python-mypy_extensions/template.py
@@ -0,0 +1,18 @@
+pkgname = "python-mypy_extensions"
+pkgver = "1.0.0"
+pkgrel = 0
+build_style = "python_module"
+make_check_target = "tests/testextensions.py"
+hostmakedepends = ["python-setuptools"]
+depends = ["python"]
+checkdepends = ["python-pytest"]
+pkgdesc = "Powerful and lightweight Python tree data structure"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://github.com/python/mypy_extensions"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "c1f1fc0cc5f5be7d3a70b6dd4b85f9e2b02d788d66f3a168652a65df6571df07"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
