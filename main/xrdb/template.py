0f25e9b3014bdb28df5dbc80d2d94680c33b4f47
Daniel Kolesa <q66@chimera-linux.org>
2023-06-03 21:02:29 +0200

    main/xrdb: update to 1.2.2

--- main/xrdb/template.py
+++ main/xrdb/template.py
@@ -1,21 +1,18 @@
 pkgname = "xrdb"
-pkgver = "1.2.1"
+pkgver = "1.2.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-cpp=/usr/bin/clang-cpp,/usr/bin/cpp"]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["pkgconf", "automake", "libtool", "xorg-util-macros"]
 makedepends = ["libx11-devel", "libxmu-devel"]
 pkgdesc = "X server resource database utility"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "4f5d031c214ffb88a42ae7528492abde1178f5146351ceb3c05f3b8d5abee8b4"
+source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
+sha256 = "db2d774a35ae2f7e7ac61cc2de0dcae27fc2aa14399c35721f8300e63ea73549"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
