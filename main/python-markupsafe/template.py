45b982feb9f8e1c600a0cdb966d5465f9bde4304
Daniel Kolesa <q66@chimera-linux.org>
2023-06-03 21:03:27 +0200

    main/python-markupsafe: update to 2.1.3

--- main/python-markupsafe/template.py
+++ main/python-markupsafe/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-markupsafe"
-pkgver = "2.1.2"
+pkgver = "2.1.3"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://palletsprojects.com/p/markupsafe"
 source = f"$(PYPI_SITE)/M/MarkupSafe/MarkupSafe-{pkgver}.tar.gz"
-sha256 = "abcabc8c2b26036d62d4c746381a6f7cf60aafcc653198ad678306986b09450d"
+sha256 = "af598ed32d6ae86f1b747b82783958b1a4ab8f617b06fe68795c7f026abbdcad"
 # dependency of pytest; also needs itsself to be installed
 options = ["!check"]
 
