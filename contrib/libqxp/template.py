6978aa180f7141843713208505740ce591de16ce
Daniel Kolesa <q66@chimera-linux.org>
2023-06-05 04:27:44 +0200

    contrib/libqxp: new package (0.0.2)

---
+++ contrib/libqxp/template.py
@@ -0,0 +1,26 @@
+pkgname = "libqxp"
+pkgver = "0.0.2"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = ["pkgconf", "gmake", "automake", "libtool"]
+makedepends = ["librevenge-devel", "boost-devel", "icu-devel"]
+pkgdesc = "Library for QuarkXPress format"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MPL-2.0"
+url = "https://wiki.documentfoundation.org/DLP/Libraries/libqxp"
+source = (
+    f"https://dev-www.libreoffice.org/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
+)
+sha256 = "e137b6b110120a52c98edd02ebdc4095ee08d0d5295a94316a981750095a945c"
+
+
+@subpackage("libqxp-progs")
+def _progs(self):
+    return self.default_progs()
+
+
+@subpackage("libqxp-devel")
+def _devel(self):
+    return self.default_devel()
