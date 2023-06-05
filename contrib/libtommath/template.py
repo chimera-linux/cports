54aa6521875267bc82a27c89d0ba0598e7bb0963
Daniel Kolesa <q66@chimera-linux.org>
2023-06-05 15:13:34 +0200

    contrib/libtommath: new package (1.2.0)

---
+++ contrib/libtommath/template.py
@@ -0,0 +1,25 @@
+pkgname = "libtommath"
+pkgver = "1.2.0"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_build_args = ["-f", "makefile.shared"]
+make_install_args = ["-f", "makefile.shared"]
+make_use_env = True
+hostmakedepends = ["pkgconf", "gmake", "libtool"]
+pkgdesc = "Portable number theoretic multiple-precision integer library"
+license = "custom:none"
+url = "https://www.libtom.net/LibTomMath"
+source = f"https://github.com/libtom/{pkgname}/releases/download/v{pkgver}/ltm-{pkgver}.tar.xz"
+sha256 = "b7c75eecf680219484055fcedd686064409254ae44bc31a96c5032843c0e18b1"
+options = ["!cross"]
+
+
+def do_check(self):
+    self.do("gmake", "test_standalone")
+    self.do("./test")
+
+
+@subpackage("libtommath-devel")
+def _devel(self):
+    return self.default_devel()
