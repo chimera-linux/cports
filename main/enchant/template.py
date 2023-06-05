f5c0ed9bed46c10662fdf995ec56c465455b14ea
Daniel Kolesa <q66@chimera-linux.org>
2023-06-05 03:01:36 +0200

    main/enchant: enable hunspell support

--- main/enchant/template.py
+++ main/enchant/template.py
@@ -1,10 +1,16 @@
 pkgname = "enchant"
 pkgver = "2.5.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = ["--enable-relocatable", "--disable-static"]
 hostmakedepends = ["pkgconf", "automake", "libtool"]
-makedepends = ["glib-devel", "nuspell-devel", "icu-devel", "libltdl-devel"]
+makedepends = [
+    "glib-devel",
+    "nuspell-devel",
+    "hunspell-devel",
+    "icu-devel",
+    "libltdl-devel",
+]
 checkdepends = ["unittest-cpp"]
 pkgdesc = "Generic spell checking library"
 maintainer = "q66 <q66@chimera-linux.org>"
