58734e1dfe80bca45e7d3f17a4833a7862176b95
Daniel Kolesa <q66@chimera-linux.org>
2023-06-05 03:33:50 +0200

    contirb/librevenge: new package (0.0.5)

---
+++ contrib/librevenge/template.py
@@ -0,0 +1,22 @@
+pkgname = "librevenge"
+pkgver = "0.0.5"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--disable-werror", "--disable-static"]
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = ["gmake", "pkgconf", "automake", "libtool"]
+makedepends = ["boost-devel", "zlib-devel", "cppunit-devel"]
+pkgdesc = "Library for reverse engineered format filters"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MPL-2.0"
+url = "https://sourceforge.net/p/libwpd/librevenge"
+source = f"$(SOURCEFORGE_SITE)/libwpd/{pkgname}-{pkgver}.tar.xz"
+sha256 = "106d0c44bb6408b1348b9e0465666fa83b816177665a22cd017e886c1aaeeb34"
+
+
+@subpackage("librevenge-devel")
+def _devel(self):
+    self.depends += makedepends
+
+    return self.default_devel()
