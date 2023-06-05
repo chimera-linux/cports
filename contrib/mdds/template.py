33413e0c2f81e3cf528cfde547c717faefdf41d8
Daniel Kolesa <q66@chimera-linux.org>
2023-06-05 17:11:24 +0200

    contrib/mdds: new package (2.1.1)

---
+++ contrib/mdds/template.py
@@ -0,0 +1,17 @@
+pkgname = "mdds"
+pkgver = "2.1.1"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "pkgconf", "automake", "libtool"]
+checkdepends = ["boost-devel"]
+pkgdesc = "Collection of multi-dimensional data structures"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://gitlab.com/mdds/mdds"
+source = f"http://kohei.us/files/{pkgname}/src/{pkgname}-{pkgver}.tar.bz2"
+sha256 = "8a3767f0a60c53261b5ebbaa717381446813aef8cf28fe9d0ea1371123bbe3f1"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
