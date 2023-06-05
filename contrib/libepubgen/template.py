72e15f0726084899264adb296289fb171b8f03c6
Daniel Kolesa <q66@chimera-linux.org>
2023-06-05 16:50:48 +0200

    contrib/libepubgen: new package (0.1.1)

---
+++ contrib/libepubgen/template.py
@@ -0,0 +1,20 @@
+pkgname = "libepubgen"
+pkgver = "0.1.1"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = ["pkgconf", "gmake", "automake", "libtool"]
+makedepends = ["librevenge-devel", "boost-devel", "libxml2-devel"]
+checkdepends = ["cppunit-devel"]
+pkgdesc = "EPUB generator for librevenge"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MPL-2.0"
+url = "https://sourceforge.net/projects/libepubgen"
+source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "03e084b994cbeffc8c3dd13303b2cb805f44d8f2c3b79f7690d7e3fc7f6215ad"
+
+
+@subpackage("libepubgen-devel")
+def _devel(self):
+    return self.default_devel()
