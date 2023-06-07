60ed30855050bf1fb3ce4347ec82c2ae994fc290
Daniel Kolesa <q66@chimera-linux.org>
2023-06-07 04:55:21 +0200

    main/gpgme: rebuild with regenerated autotools
    Libtool is cringe and does the -nostdlib linking crap for c++
    libs, so we need to regen stuff to use our patched ver. Since
    it does not link builtins with stock libtool, it will be missing
    outline atomics on aarch64.

--- main/gpgme/template.py
+++ main/gpgme/template.py
@@ -1,8 +1,8 @@
 pkgname = "gpgme"
 pkgver = "1.20.0"
-pkgrel = 1
+pkgrel = 2
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf", "gnupg"]
+hostmakedepends = ["pkgconf", "gnupg", "automake", "libtool"]
 makedepends = ["libgpg-error-devel", "libassuan-devel", "glib-devel"]
 depends = ["gnupg"]
 pkgdesc = "GnuPG Made Easy"
@@ -16,6 +16,3 @@ sha256 = "25a5785a5da356689001440926b94e967d02e13c49eb7743e35ef0cf22e42750"
 @subpackage("gpgme-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
