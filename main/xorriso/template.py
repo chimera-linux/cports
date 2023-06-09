84aab4b3707c30ceb2c171c26329498fa10344c5
Daniel Kolesa <q66@chimera-linux.org>
2023-06-10 00:14:59 +0200

    mani/xorriso: update to 1.5.6

--- main/xorriso/template.py
+++ main/xorriso/template.py
@@ -1,6 +1,5 @@
 pkgname = "xorriso"
-pkgver = "1.5.4.2"
-_xver = "1.5.4.pl02"
+pkgver = "1.5.6"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -8,6 +7,8 @@ configure_args = [
     "--disable-libreadline",
     "MKDIR_P=mkdir -p",  # install-sh is buggy
 ]
+# won't configure
+configure_gen = []
 makedepends = [
     "zlib-devel",
     "libbz2-devel",
@@ -19,9 +20,7 @@ pkgdesc = "ISO 9660 Rock Ridge Filesystem Manipulator"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/xorriso"
-source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{_xver}.tar.gz"
-sha256 = "3ec7393d4a9dcbf5f74309c28a415f55227ec62770b95ae993ac8d7a3b152972"
+source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "d4b6b66bd04c49c6b358ee66475d806d6f6d7486e801106a47d331df1f2f8feb"
 # FIXME fails to generate live
 hardening = ["vis", "!cfi"]
-
-configure_gen = []
