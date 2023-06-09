ac0d7172230b500f0a215ae02e6a502b923fb7ec
Daniel Kolesa <q66@chimera-linux.org>
2023-06-09 23:42:28 +0200

    main/libgusb: update to 0.4.6

--- main/libgusb/template.py
+++ main/libgusb/template.py
@@ -1,5 +1,5 @@
 pkgname = "libgusb"
-pkgver = "0.4.5"
+pkgver = "0.4.6"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocs=false", "-Dintrospection=true"]
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/hughsie/libgusb"
 source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "bc8c6328289f057c8f73b07c1ba6251de96029787309f2672ca252ca90ace1b2"
+sha256 = "1b0422bdcd72183272ac42eec9398c5a0bc48a02f618fa3242c468cbbd003049"
 # no access to usb in container
 options = ["!check", "!cross"]
 
