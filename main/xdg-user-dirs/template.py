4a9a5fa7edf0cfe8100f5d387ffda8a4941e35c7
Daniel Kolesa <q66@chimera-linux.org>
2023-06-10 19:45:20 +0200

    main/xdg-user-dirs: new package (0.18)

---
+++ main/xdg-user-dirs/template.py
@@ -0,0 +1,18 @@
+pkgname = "xdg-user-dirs"
+pkgver = "0.18"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = [
+    "pkgconf",
+    "automake",
+    "libtool",
+    "xsltproc",
+    "docbook-xsl-nons",
+    "gettext-tiny-devel",
+]
+pkgdesc = "Tool to help manage user directories"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "http://www.freedesktop.org/wiki/Software/xdg-user-dirs"
+source = f"https://user-dirs.freedesktop.org/releases/{pkgname}-{pkgver}.tar.gz"
+sha256 = "ec6f06d7495cdba37a732039f9b5e1578bcb296576fde0da40edb2f52220df3c"
