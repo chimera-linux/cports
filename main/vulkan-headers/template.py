c5cf4627b8745f59a15e09fd8b12161d4a9fd4c0
Daniel Kolesa <q66@chimera-linux.org>
2023-06-10 21:42:14 +0200

    main/vulkan-headers: update to 1.3.253

--- main/vulkan-headers/template.py
+++ main/vulkan-headers/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-headers"
-pkgver = "1.3.252"
+pkgver = "1.3.253"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja"]
@@ -10,6 +10,6 @@ url = "https://www.khronos.org/vulkan"
 source = (
     f"https://github.com/KhronosGroup/Vulkan-Headers/archive/v{pkgver}.tar.gz"
 )
-sha256 = "2262a7ba3e12583a5f5f7ebac0c01003cf106baf0ea75809e07958fa9334ea49"
+sha256 = "e45a8c4289b84317252451055b0ac76b13169e10a9f619961fb6479bf6179054"
 # no test suite
 options = ["!check"]
