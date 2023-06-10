62995c47184a9e11cdf0020c6663b303b308d205
Daniel Kolesa <q66@chimera-linux.org>
2023-06-11 00:12:39 +0200

    main/linux-stable-zfs-bin: new package (2.1.12.6.3.7)

---
+++ main/linux-stable-zfs-bin/template.py
@@ -0,0 +1,44 @@
+pkgname = "linux-stable-zfs-bin"
+_kernver = "6.3.7"
+_zfsver = "2.1.12"
+pkgver = f"{_zfsver}.{_kernver}"
+pkgrel = 0
+hostmakedepends = ["gmake", "perl", "python", "ckms"]
+makedepends = ["linux-stable-devel", "zfs-ckms"]
+# provides the same thing as the ckms variant
+depends = [f"linux-stable~{_kernver}", f"zfs~{_zfsver}"]
+pkgdesc = f"OpenZFS modules for kernel {_kernver}"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "CDDL-1.0"
+url = "https://openzfs.github.io/openzfs-docs"
+options = ["!cross"]
+
+
+def init_configure(self):
+    from cbuild.util import linux
+
+    self._linux_version = linux.get_version(self, _kernver)
+    linux.generate_scriptlets_ckms(self, "zfs", self._linux_version)
+
+
+def do_configure(self):
+    from cbuild.util import linux
+
+    linux.ckms_configure(self, "zfs", _zfsver, self._linux_version)
+
+
+def do_build(self):
+    from cbuild.util import linux
+
+    linux.ckms_build(self, "zfs", _zfsver, self._linux_version)
+
+
+def do_install(self):
+    from cbuild.util import linux
+
+    linux.ckms_install(self, "zfs", _zfsver, self._linux_version)
+
+    srcp = linux.get_modsrc(self, "zfs", _zfsver)
+    self.install_license(srcp / "COPYRIGHT")
+    self.install_license(srcp / "LICENSE")
+    self.install_license(srcp / "NOTICE")
