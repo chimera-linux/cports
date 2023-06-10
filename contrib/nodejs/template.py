61f81ae172518f586dffffbb0bd7d48291e46067
Daniel Kolesa <q66@chimera-linux.org>
2023-06-10 22:34:06 +0200

    contrib/nodejs: update to 20.3.0

--- contrib/nodejs/template.py
+++ contrib/nodejs/template.py
@@ -1,5 +1,5 @@
 pkgname = "nodejs"
-pkgver = "20.2.0"
+pkgver = "20.3.0"
 pkgrel = 0
 build_style = "configure"
 configure_args = [
@@ -39,7 +39,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://nodejs.org"
 source = f"{url}/dist/v{pkgver}/node-v{pkgver}.tar.gz"
-sha256 = "cd1b0eb67fffe83112034fbdc64b363287e017f835251702f041982633e4b6aa"
+sha256 = "2f5f80be36315a2dd4a0da123597c3cbbba2b0ec19ba7832bf93414b1a645ca4"
 debug_level = 1  # allow LTO build to not run out of mem
 hardening = ["!cfi"]  # TODO
 options = ["!cross"]
