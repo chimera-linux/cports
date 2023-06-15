158e6a7658e197b92b407cdf9e31ef1a6a196415
Flukey <flukey@vapourmail.eu>
2023-06-15 09:52:38 +0200

    contrib/swaylock: new package (1.7.2)

---
+++ contrib/swaylock/template.py
@@ -0,0 +1,24 @@
+pkgname = "swaylock"
+pkgver = "1.7.2"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+    "scdoc",
+]
+makedepends = [
+    "cairo-devel",
+    "gdk-pixbuf-devel",
+    "libxkbcommon-devel",
+    "linux-pam-devel",
+    "wayland-devel",
+    "wayland-protocols",
+]
+pkgdesc = "Screen locker for Wayland"
+maintainer = "flukey <flukey@vapourmail.eu>"
+license = "MIT"
+url = "https://github.com/swaywm/swaylock"
+source = f"https://github.com/swaywm/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "bf965d47fb6fc1402f854d4679d21a9459713fc0f330bc607c9585db097b4304"
+hardening = ["vis", "cfi"]
