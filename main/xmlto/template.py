f8c4e1091e349eb0ae4692306c8155c13f37fc1c
Daniel Kolesa <q66@chimera-linux.org>
2023-06-16 20:52:58 +0200

    main/xmlto: fix build

--- main/xmlto/template.py
+++ main/xmlto/template.py
@@ -4,7 +4,15 @@ pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["ac_cv_path_BASH=/usr/bin/bash"]
 configure_env = {"GETOPT": "ugetopt"}
-hostmakedepends = ["bash", "xsltproc", "docbook-xsl-nons", "ugetopt"]
+hostmakedepends = [
+    "bash",
+    "xsltproc",
+    "docbook-xsl-nons",
+    "ugetopt",
+    "automake",
+    "libtool",
+    "flex",
+]
 depends = ["bash", "ugetopt", "xsltproc", "docbook-xsl-nons"]
 pkgdesc = "Tool to help transform XML documents into other formats"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -13,5 +21,3 @@ url = "http://cyberelk.net/tim/software/xmlto"
 source = f"http://anduin.linuxfromscratch.org/BLFS/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "1130df3a7957eb9f6f0d29e4aa1c75732a7dfb6d639be013859b5c7ec5421276"
 hardening = ["vis", "cfi"]
-
-configure_gen = []
