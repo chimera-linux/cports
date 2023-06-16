af744f676d9a6093af4f0a843ecc76ec1ba82af5
Daniel Kolesa <q66@chimera-linux.org>
2023-06-16 19:28:36 +0200

    main/gperf: fix build

--- main/gperf/template.py
+++ main/gperf/template.py
@@ -2,6 +2,7 @@ pkgname = "gperf"
 pkgver = "3.1"
 pkgrel = 0
 build_style = "gnu_configure"
+configure_gen = []
 make_cmd = "gmake"
 hostmakedepends = ["gmake"]
 pkgdesc = "Perfect hash function generator"
@@ -12,5 +13,3 @@ source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "588546b945bba4b70b6a3a616e80b4ab466e3f33024a352fc2198112cdbb3ae2"
 # FIXME cfi int
 hardening = ["vis", "!cfi", "!int"]
-
-configure_gen = []
