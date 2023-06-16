de07cd542330197bb63782ce4f34dbbdea9e2e75
Daniel Kolesa <q66@chimera-linux.org>
2023-06-16 20:59:26 +0200

    main/bdfresize: fix build

--- main/bdfresize/template.py
+++ main/bdfresize/template.py
@@ -2,6 +2,7 @@ pkgname = "bdfresize"
 pkgver = "1.5"
 pkgrel = 0
 build_style = "gnu_configure"
+hostmakedepends = ["automake"]
 pkgdesc = "Tool for resizing BDF format font"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
@@ -9,5 +10,3 @@ url = "http://openlab.ring.gr.jp/efont/dist/tools/bdfresize"
 source = f"{url}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "440cfc0620a0237e46352c14a0774caa3f3059759b0a20defefc94e8490897a6"
 hardening = ["vis", "cfi"]
-
-configure_gen = []
