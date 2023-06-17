7157406f7e9e691a3d45579a59f5b1aee9644843
Daniel Kolesa <q66@chimera-linux.org>
2023-06-17 15:13:00 +0200

    main/libcdio: fix clang 16 build

--- main/libcdio/template.py
+++ main/libcdio/template.py
@@ -1,18 +1,18 @@
 pkgname = "libcdio"
 pkgver = "2.1.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
+configure_gen = []
 make_cmd = "gmake"
 hostmakedepends = ["gmake", "pkgconf"]
 makedepends = ["ncurses-devel", "libcddb-devel", "linux-headers"]
+checkdepends = ["bash"]
 pkgdesc = "CD-ROM access library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/libcdio"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "8550e9589dbd594bfac93b81ecf129b1dc9d0d51e90f9696f1b2f9b2af32712b"
-# tests a cd-rom drive, plus fails realpath test
-options = ["!check"]
 
 
 @subpackage("libcdio-devel")
@@ -23,6 +23,3 @@ def _devel(self):
 @subpackage("libcdio-progs")
 def _progs(self):
     return self.default_progs()
-
-
-configure_gen = []
