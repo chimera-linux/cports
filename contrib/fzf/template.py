04c8ce62060816e12d37a99dba764e55b664e974
Wesley Moore <wes@wezm.net>
2023-06-17 13:06:19 +1000

    contrib/fzf: new package (0.42.0)

---
+++ contrib/fzf/template.py
@@ -0,0 +1,25 @@
+pkgname = "fzf"
+pkgver = "0.42.0"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go"]
+makedepends = ["ncurses-devel"]
+pkgdesc = "Command-line fuzzy finder"
+maintainer = "Wesley Moore <wes@wezm.net>"
+license = "MIT"
+url = "https://github.com/junegunn/fzf"
+source = f"{url}/archive/{pkgver}.tar.gz"
+sha256 = "743c1bfc7851b0796ab73c6da7db09d915c2b54c0dd3e8611308985af8ed3df2"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+    self.install_man("man/man1/fzf.1")
+    self.install_file("plugin/fzf.vim", "usr/share/vim/vimfiles/plugin")
+    self.install_file("plugin/fzf.vim", "usr/share/nvim/runtime/plugin")
+
+    with self.pushd("shell"):
+        self.install_completion("completion.bash", "bash")
+        self.install_completion("completion.zsh", "zsh")
+        for ext in ["bash", "fish", "zsh"]:
+            self.install_file(f"key-bindings.{ext}", "usr/share/fzf")
