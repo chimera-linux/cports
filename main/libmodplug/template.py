1ff8b138deaa7300a9d0e5548b53029afbd2c830
Daniel Kolesa <q66@chimera-linux.org>
2023-06-17 13:59:59 +0200

    main/libmodplug: fix on clang 16

--- main/libmodplug/template.py
+++ main/libmodplug/template.py
@@ -1,9 +1,9 @@
 pkgname = "libmodplug"
 pkgver = "0.8.9.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = ["--enable-static"]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["pkgconf", "automake", "libtool"]
 pkgdesc = "MOD playing library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:none"
@@ -15,6 +15,3 @@ sha256 = "457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de"
 @subpackage("libmodplug-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
