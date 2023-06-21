d77c266fb2bcfad3f85b8c515cf3f55bf4093dd6
q66 <q66@chimera-linux.org>
2023-06-21 20:48:31 +0200

    main/gnome-video-effects: update to 0.6.0

--- main/gnome-video-effects/template.py
+++ main/gnome-video-effects/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-video-effects"
-pkgver = "0.5.0"
+pkgver = "0.6.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "gettext-tiny"]
@@ -9,4 +9,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-video-effects"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "4dc68e9b38fdfc1e8e0414e2d7ee83ace78efdee76f30506cc9dcd07394ad0c8"
+sha256 = "d7aeaeb46b3f5a832fb2e0d90b42bf8c6160202ca52ac9add17afce192e3c8a8"
