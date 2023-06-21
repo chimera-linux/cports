70086d03afffe4b81e6e355bc86eabdf6a9f775b
q66 <q66@chimera-linux.org>
2023-06-21 20:40:46 +0200

    main/python-fonttools: update to 4.40.0

--- main/python-fonttools/template.py
+++ main/python-fonttools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-fonttools"
-pkgver = "4.39.4"
+pkgver = "4.40.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT AND OFL-1.1 AND BSD-3-Clause AND Apache-2.0"
 url = "https://github.com/fonttools/fonttools"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "9fab7f15ad7a0b5cd040331090b726c81d8b0d7b043bb348c7dc242d901b4ee5"
+sha256 = "f03f5a5414587fed31d11e007e680ea522a918ea27a793a5931641ef5915bcd2"
 # unpackaged deps
 options = ["!check"]
 
