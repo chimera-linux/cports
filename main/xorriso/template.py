51983b879ccb4f33ccf1d1f6b74ea56ad7d4f36c
q66 <q66@chimera-linux.org>
2023-06-21 20:24:21 +0200

    main/xorriso: update to 1.5.6.2

--- main/xorriso/template.py
+++ main/xorriso/template.py
@@ -1,5 +1,5 @@
 pkgname = "xorriso"
-pkgver = "1.5.6"
+pkgver = "1.5.6.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -20,7 +20,7 @@ pkgdesc = "ISO 9660 Rock Ridge Filesystem Manipulator"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/xorriso"
-source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "d4b6b66bd04c49c6b358ee66475d806d6f6d7486e801106a47d331df1f2f8feb"
+source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver[:-2]}.pl0{pkgver[-1:]}.tar.gz"
+sha256 = "786f9f5df9865cc5b0c1fecee3d2c0f5e04cab8c9a859bd1c9c7ccd4964fdae1"
 # FIXME fails to generate live
 hardening = ["vis", "!cfi"]
