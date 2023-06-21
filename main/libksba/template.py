15d46b47e7e20970929375fa9cff73a44e56dfd7
q66 <q66@chimera-linux.org>
2023-06-21 21:03:51 +0200

    main/libksba: update to 1.6.4

--- main/libksba/template.py
+++ main/libksba/template.py
@@ -1,7 +1,8 @@
 pkgname = "libksba"
-pkgver = "1.6.3"
+pkgver = "1.6.4"
 pkgrel = 0
 build_style = "gnu_configure"
+configure_gen = []
 hostmakedepends = ["pkgconf"]
 makedepends = ["libgpg-error-devel"]
 pkgdesc = "CMS and X.509 access library"
@@ -9,12 +10,9 @@ maintainer = "eater <=@eater.me>"
 license = "GPL-2.0-or-later OR LGPL-3.0-or-later"
 url = "https://gnupg.org/software/libksba/index.html"
 source = f"https://gnupg.org/ftp/gcrypt/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "3f72c68db30971ebbf14367527719423f0a4d5f8103fc9f4a1c01a9fa440de5c"
+sha256 = "bbb43f032b9164d86c781ffe42213a83bf4f2fee91455edfa4654521b8b03b6b"
 
 
 @subpackage("libksba-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
