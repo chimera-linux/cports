962a5ef1ffbcf2605fb28079daa7d8a3cd7fc3b6
q66 <q66@chimera-linux.org>
2023-06-21 20:39:35 +0200

    main/python-platformdirs: update to 3.6.0

--- main/python-platformdirs/template.py
+++ main/python-platformdirs/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-platformdirs"
-pkgver = "3.5.3"
+pkgver = "3.6.0"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_env = {"PYTHONPATH": "src"}
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://platformdirs.readthedocs.io"
 source = f"$(PYPI_SITE)/p/platformdirs/platformdirs-{pkgver}.tar.gz"
-sha256 = "e48fabd87db8f3a7df7150a4a5ea22c546ee8bc39bc2473244730d4b56d2cc4e"
+sha256 = "57e28820ca8094678b807ff529196506d7a21e17156cb1cddb3e74cebce54640"
 # unpackaged checkdepends
 options = ["!check"]
 
