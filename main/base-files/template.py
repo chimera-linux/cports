c86d39f0adc26869fc33255f768b07bce859a63e
q66 <q66@chimera-linux.org>
2023-06-21 20:12:15 +0200

    main/base-files: update to 0.1.20230615

--- main/base-files/template.py
+++ main/base-files/template.py
@@ -1,7 +1,7 @@
 pkgname = "base-files"
-_iana_ver = "20230607"
+_iana_ver = "20230615"
 pkgver = f"0.1.{_iana_ver}"
-pkgrel = 1
+pkgrel = 0
 pkgdesc = "Chimera Linux base system files"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:meta"
