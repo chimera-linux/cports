ec1606efe6fcfe728157bf21d6c54fc8cfc66dac
q66 <q66@chimera-linux.org>
2023-06-21 21:11:26 +0200

    main/ccache: update to 4.8.2

--- main/ccache/template.py
+++ main/ccache/template.py
@@ -1,5 +1,5 @@
 pkgname = "ccache"
-pkgver = "4.8.1"
+pkgver = "4.8.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DENABLE_TESTING=OFF", "-DREDIS_STORAGE_BACKEND=OFF"]
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://ccache.samba.org"
 source = f"https://github.com/ccache/ccache/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "87959b6819530b3dcaeb39992f585b9fc2c7120302809741378097774919fb6f"
+sha256 = "3d3fb3f888a5b16c4fa7ee5214cca76348afd6130e8443de5f6f2424f2076a49"
 hardening = ["vis", "cfi"]
 # not properly set up
 options = ["!check"]
