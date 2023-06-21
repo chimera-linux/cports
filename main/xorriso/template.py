457b183316b37089023204123ce990cfd3bb672b
q66 <q66@chimera-linux.org>
2023-06-21 21:21:31 +0200

    main/xorriso: fix style

--- main/xorriso/template.py
+++ main/xorriso/template.py
@@ -20,7 +20,9 @@ pkgdesc = "ISO 9660 Rock Ridge Filesystem Manipulator"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/xorriso"
-source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver[:-2]}.pl0{pkgver[-1:]}.tar.gz"
+source = (
+    f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver[:-2]}.pl0{pkgver[-1:]}.tar.gz"
+)
 sha256 = "786f9f5df9865cc5b0c1fecee3d2c0f5e04cab8c9a859bd1c9c7ccd4964fdae1"
 # FIXME fails to generate live
 hardening = ["vis", "!cfi"]
