91c1ab8ada2a8badf0a71b66cecccda889ba4b62
q66 <q66@chimera-linux.org>
2023-06-21 20:39:03 +0200

    main/python-pluggy: update to 1.1.0

--- main/python-pluggy/template.py
+++ main/python-pluggy/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pluggy"
-pkgver = "1.0.0"
+pkgver = "1.1.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools_scm"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pytest-dev/pluggy"
 source = f"$(PYPI_SITE)/p/pluggy/pluggy-{pkgver}.tar.gz"
-sha256 = "4224373bacce55f955a878bf9cfa763c1e360858e330072059e10bad68531159"
+sha256 = "c500b592c5512df35622e4faf2135aa0b7e989c7d31344194b4afb9d5e47b1bf"
 # dependency of pytest
 options = ["!check"]
 
