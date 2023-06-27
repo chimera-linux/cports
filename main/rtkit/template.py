a5e6bf547b814154dc39739705c2989db0a357ac
q66 <q66@chimera-linux.org>
2023-06-27 16:00:25 +0200

    main/rtkit: add stub setscheduler patch + optional polkit rule

--- main/rtkit/template.py
+++ main/rtkit/template.py
@@ -1,6 +1,6 @@
 pkgname = "rtkit"
 pkgver = "0.13"
-pkgrel = 1
+pkgrel = 2
 build_style = "meson"
 configure_args = [
     "-Dlibsystemd=disabled",
@@ -17,16 +17,13 @@ source = f"{url}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "a157144cd95cf6d25200e74b74a8f01e4fe51fd421bb63c1f00d471394b640ab"
 hardening = ["vis", "cfi"]
 
-system_users = [
-    {
-        "name": "rtkit",
-        "id": None,
-        "home": "/proc",
-    }
-]
-
+system_users = ["rtkit"]
 
 def post_install(self):
     self.rm(self.destdir / "usr/lib/systemd", recursive=True)
     self.install_license("LICENSE")
     self.install_service(self.files_path / "rtkit")
+    # optional
+    self.install_file(
+        self.files_path / "50-rtkit.rules", "usr/share/polkit-1/rules.d"
+    )
