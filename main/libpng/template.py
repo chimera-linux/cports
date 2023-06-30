6f9cb7e307cc7f3f9923afca68e19b7871feac4e
q66 <q66@chimera-linux.org>
2023-06-30 15:30:36 +0200

    main/libpng: update to 1.6.40

--- main/libpng/template.py
+++ main/libpng/template.py
@@ -1,7 +1,8 @@
 pkgname = "libpng"
-pkgver = "1.6.39"
+pkgver = "1.6.40"
 pkgrel = 0
 build_style = "gnu_configure"
+configure_gen = []
 hostmakedepends = ["pkgconf"]
 makedepends = ["zlib-devel"]
 pkgdesc = "Library for manipulating PNG images"
@@ -9,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Libpng"
 url = "http://www.libpng.org/pub/png/libpng.html"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "1f4696ce70b4ee5f85f1e1623dc1229b210029fa4b7aee573df3e2ba7b036937"
+sha256 = "535b479b2467ff231a3ec6d92a525906fb8ef27978be4f66dbe05d3f3a01b3a1"
 
 
 def post_install(self):
@@ -24,6 +25,3 @@ def _devel(self):
 @subpackage("libpng-progs")
 def _progs(self):
     return self.default_progs()
-
-
-configure_gen = []
