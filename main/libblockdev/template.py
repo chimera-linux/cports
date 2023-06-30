c26a0055421cf39aa7c70b809239b2aba4c68631
q66 <q66@chimera-linux.org>
2023-06-30 05:09:10 +0200

    main/libblockdev: update to 3.0

--- main/libblockdev/template.py
+++ main/libblockdev/template.py
@@ -1,5 +1,5 @@
 pkgname = "libblockdev"
-pkgver = "2.28"
+pkgver = "3.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -8,17 +8,19 @@ configure_args = [
     "--without-nvdimm",
 ]
 make_cmd = "gmake"
-hostmakedepends = ["gmake", "bash", "pkgconf", "python"]
+hostmakedepends = ["gmake", "bash", "pkgconf", "python", "automake", "libtool"]
 makedepends = [
     "cryptsetup-devel",
     "device-mapper-devel",
-    "dmraid-devel",
     "libbytesize-devel",
     "glib-devel",
+    "keyutils-devel",
     "libkmod-devel",
-    "libyaml-devel",
+    "libnvme-devel",
     "parted-devel",
     "pcre2-devel",
+    "libfdisk-devel",
+    "e2fsprogs-devel",
     "linux-headers",
 ]
 pkgdesc = "Library for manipulating block devices"
@@ -26,12 +28,13 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/storaged-project/libblockdev"
 source = f"https://github.com/storaged-project/{pkgname}/releases/download/{pkgver}-1/{pkgname}-{pkgver}.tar.gz"
-sha256 = "82c9c841e28a74fecadedebfae6a772df623cecdf652e5376650fa26da5b7df4"
+sha256 = "80a07c9649907b7dd04dd7be535581c173a69f1aa4b65d3d352fdaddfee22d34"
+
+tool_flags = {
+    "CFLAGS": ["-Wno-error=typedef-redefinition", "-Wno-error=unused-variable"]
+}
 
 
 @subpackage("libblockdev-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
