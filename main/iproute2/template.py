112a585334550da98b6bb6c334fea59f836caddd
q66 <q66@chimera-linux.org>
2023-06-30 15:26:48 +0200

    main/iproute2: update to 6.4.0

--- main/iproute2/template.py
+++ main/iproute2/template.py
@@ -1,5 +1,5 @@
 pkgname = "iproute2"
-pkgver = "6.3.0"
+pkgver = "6.4.0"
 pkgrel = 0
 build_style = "configure"
 configure_env = {"CC": "clang"}
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://wiki.linuxfoundation.org/networking/iproute2"
 source = f"$(KERNEL_SITE)/utils/net/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "dfb2a98db96e7a653cffc6693335a1a466e29a34b6ac528be48f35e1d2766732"
+sha256 = "4c51b8decbc7e4da159ffb066f590cfb93dbf9af7ff86b1647ce42b7c179a272"
 hardening = ["vis", "cfi"]
 
 
