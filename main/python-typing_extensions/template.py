aa32a0ef7c9ab8ab9097a52a68c84c1d0d92c488
q66 <q66@chimera-linux.org>
2023-07-08 16:03:07 +0200

    main/python-typing_extensions: update to 4.7.1

--- main/python-typing_extensions/template.py
+++ main/python-typing_extensions/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-typing_extensions"
-pkgver = "4.7.0"
+pkgver = "4.7.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-installer", "python-flit_core"]
@@ -9,6 +9,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Python-2.0"
 url = "https://github.com/python/typing"
 source = f"$(PYPI_SITE)/t/typing_extensions/typing_extensions-{pkgver}.tar.gz"
-sha256 = "935ccf31549830cda708b42289d44b6f74084d616a00be651601a4f968e77c82"
+sha256 = "b75ddc264f0ba5615db7ba217daeb99701ad295353c45f9e95963337ceeeffb2"
 # in early path
 options = ["!check"]
