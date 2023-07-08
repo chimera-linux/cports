d456418e3ed445548b1ff82aed2c917e01789afa
q66 <q66@chimera-linux.org>
2023-07-08 16:09:01 +0200

    main/python-click: update to 8.1.4

--- main/python-click/template.py
+++ main/python-click/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-click"
-pkgver = "8.1.3"
+pkgver = "8.1.4"
 pkgrel = 0
 build_style = "python_module"
 make_check_env = {"PYTHONPATH": "src"}
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://palletsprojects.com/p/click"
 source = f"$(PYPI_SITE)/c/click/click-{pkgver}.tar.gz"
-sha256 = "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
+sha256 = "b97d0c74955da062a7d4ef92fadb583806a585b2ea81958a81bd72726cbb8e37"
 
 
 def post_install(self):
