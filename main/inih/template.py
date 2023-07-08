d51d901f3570b264a48e351a4d0284f391de9203
q66 <q66@chimera-linux.org>
2023-07-08 15:45:41 +0200

    main/inih: update to 57

--- main/inih/template.py
+++ main/inih/template.py
@@ -1,5 +1,5 @@
 pkgname = "inih"
-pkgver = "56"
+pkgver = "57"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/benhoyt/inih"
 source = f"{url}/archive/r{pkgver}.tar.gz"
-sha256 = "4f2ba6bd122d30281a8c7a4d5723b7af90b56aa828c0e88256d7fceda03a491a"
+sha256 = "f03f98ca35c3adb56b2358573c8d3eda319ccd5287243d691e724b7eafa970b3"
 hardening = ["vis", "cfi"]
 
 
