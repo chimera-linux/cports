91a2f86998a69a19fc8a4506b30ff8eb5cb310f1
q66 <q66@chimera-linux.org>
2023-07-08 15:49:32 +0200

    main/libjpeg-turbo: update to 3.0.0

--- main/libjpeg-turbo/template.py
+++ main/libjpeg-turbo/template.py
@@ -1,5 +1,5 @@
 pkgname = "libjpeg-turbo"
-pkgver = "2.1.5.1"
+pkgver = "3.0.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DWITH_JPEG8=1", "-DCMAKE_INSTALL_LIBDIR=/usr/lib"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "IJG AND BSD-3-Clause AND Zlib"
 url = "https://libjpeg-turbo.org"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "2fdc3feb6e9deb17adec9bafa3321419aa19f8f4e5dea7bf8486844ca22207bf"
+sha256 = "c77c65fcce3d33417b2e90432e7a0eb05f59a7fff884022a9d931775d583bfaa"
 
 # tests segfault with altivec simd
 match self.profile().arch:
