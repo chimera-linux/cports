05a339af3cede75fa23f9e3517aa1899a005d41f
q66 <q66@chimera-linux.org>
2023-07-08 16:05:23 +0200

    main/python-lxml: update to 4.9.3

--- main/python-lxml/template.py
+++ main/python-lxml/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-lxml"
-pkgver = "4.9.2"
+pkgver = "4.9.3"
 pkgrel = 0
 build_style = "python_module"
 make_build_args = ["--with-cython"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause AND custom:ElementTree"
 url = "https://lxml.de"
 source = f"https://github.com/lxml/lxml/archive/lxml-{pkgver}.tar.gz"
-sha256 = "c057e334efc357fc88eb71b888d2df3a44cf3c7aaac56a8753e92090c5217050"
+sha256 = "42b9ab83cb8739d817c7fff41c20f31aa61625bb6f6ab333873a5f3406b139ac"
 # missing checkdepends
 options = ["!check"]
 
