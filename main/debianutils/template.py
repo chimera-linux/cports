a2eb1101203cfd69a26040e2a3164c0299e181a7
q66 <q66@chimera-linux.org>
2023-07-12 03:10:49 +0200

    main/debianutils: update to 5.8

--- main/debianutils/template.py
+++ main/debianutils/template.py
@@ -1,5 +1,5 @@
 pkgname = "debianutils"
-pkgver = "5.7"
+pkgver = "5.8"
 pkgrel = 0
 build_style = "gnu_configure"
 pkgdesc = "Miscellaneous utilities from Debian"
@@ -7,7 +7,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://tracker.debian.org/pkg/debianutils"
 source = f"$(DEBIAN_SITE)/main/d/debianutils/debianutils_{pkgver}.orig.tar.gz"
-sha256 = "27ec9e0e7e44dc8ab611aa576330471bacb07e4491ffecf0d3aa6909c92f9022"
+sha256 = "5b086d27eb9063de4d746760d0faeb40d9464fb855fc8a8e7fb93b03efcec622"
 hardening = ["vis", "cfi"]
 
 
