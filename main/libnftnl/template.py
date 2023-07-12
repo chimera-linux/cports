66558beb32ab15482ca3e6178e874ce61622f093
q66 <q66@chimera-linux.org>
2023-07-12 03:12:13 +0200

    main/libnftnl: update to 1.2.6

--- main/libnftnl/template.py
+++ main/libnftnl/template.py
@@ -1,17 +1,17 @@
 pkgname = "libnftnl"
-pkgver = "1.2.5"
+pkgver = "1.2.6"
 pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
 make_dir = "."
-hostmakedepends = ["gmake", "pkgconf"]
+hostmakedepends = ["gmake", "pkgconf", "automake", "libtool"]
 makedepends = ["linux-headers", "libmnl-devel"]
 pkgdesc = "Low-level netlink API to nf_tables"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://www.netfilter.org/projects/libnftnl"
 source = f"{url}/files/{pkgname}-{pkgver}.tar.xz"
-sha256 = "966de0a8120c8a53db859889749368bfb2cba0c4f0b4c1a30d264eccc45f1226"
+sha256 = "ceeaea2cd92147da19f13a35a7f1a4bc2767ff897e838e4b479cf54b59c777f4"
 # FIXME cfi: verify function pointers
 hardening = ["vis", "!cfi"]
 
@@ -19,6 +19,3 @@ hardening = ["vis", "!cfi"]
 @subpackage("libnftnl-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
