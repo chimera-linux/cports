b6f161ff10e1b5ba24425aa5b9011ab36eda8271
q66 <q66@chimera-linux.org>
2023-07-18 11:47:03 +0200

    main/python-pyyaml: update to 6.0.1

--- main/python-pyyaml/template.py
+++ main/python-pyyaml/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pyyaml"
-pkgver = "6.0"
+pkgver = "6.0.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools", "python-cython"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://pyyaml.org/wiki/PyYAML"
 source = f"$(PYPI_SITE)/P/PyYAML/PyYAML-{pkgver}.tar.gz"
-sha256 = "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
+sha256 = "bfdf460b1736c775f2ba9f6a92bca30bc2095067b8a9d77876d1fad6cc3b4a43"
 
 
 def post_install(self):
