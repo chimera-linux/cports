9e3c32afb0c0e96dbc8cedd24e7bae6a2b2de05a
q66 <q66@chimera-linux.org>
2023-07-25 15:15:11 +0200

    main/eog: add desktop-file-utils

--- main/eog/template.py
+++ main/eog/template.py
@@ -1,6 +1,6 @@
 pkgname = "eog"
 pkgver = "44.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = ["-Dintrospection=true"]
 hostmakedepends = [
@@ -25,7 +25,7 @@ makedepends = [
     "librsvg-devel",
     "libjpeg-turbo-devel",
 ]
-depends = ["hicolor-icon-theme", "shared-mime-info"]
+depends = ["desktop-file-utils", "shared-mime-info"]
 provides = ["so:libeog.so=0"]
 pkgdesc = "GNOME image viewer"
 maintainer = "q66 <q66@chimera-linux.org>"
