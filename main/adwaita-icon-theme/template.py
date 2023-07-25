1cd2a607216f2c2842b9ebd9c20ef7449a512eeb
q66 <q66@chimera-linux.org>
2023-07-25 15:01:54 +0200

    main/adwaita-icon-theme: depend on hicolor-icon-theme

--- main/adwaita-icon-theme/template.py
+++ main/adwaita-icon-theme/template.py
@@ -1,14 +1,14 @@
 pkgname = "adwaita-icon-theme"
 pkgver = "44.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
+configure_gen = []
 make_cmd = "gmake"
 hostmakedepends = ["gmake", "pkgconf"]
+depends = ["hicolor-icon-theme"]
 pkgdesc = "Icon theme for GTK+"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-or-later OR CC-BY-SA-3.0"
 url = "https://gitlab.gnome.org/GNOME/adwaita-icon-theme"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "4889c5601bbfecd25d80ba342209d0a936dcf691ee56bd6eca4cde361f1a664c"
-
-configure_gen = []
--- main/hicolor-icon-theme/template.py
+++ main/hicolor-icon-theme/template.py
@@ -2,11 +2,10 @@ pkgname = "hicolor-icon-theme"
 pkgver = "0.17"
 pkgrel = 0
 build_style = "gnu_configure"
+configure_gen = []
 pkgdesc = "Default fallback theme for freedesktop.org icon themes"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://www.freedesktop.org/wiki/Software/icon-theme"
 source = f"http://icon-theme.freedesktop.org/releases/{pkgname}-{pkgver}.tar.xz"
 sha256 = "317484352271d18cbbcfac3868eab798d67fff1b8402e740baa6ff41d588a9d8"
-
-configure_gen = []
