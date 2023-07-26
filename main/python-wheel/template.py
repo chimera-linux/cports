8014d994f2a965ddba9a5d0c85912eb2fe40faa8
q66 <q66@chimera-linux.org>
2023-07-27 00:37:19 +0200

    main/python-wheel: update to 0.41.0

--- main/python-wheel/template.py
+++ main/python-wheel/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-wheel"
-pkgver = "0.40.0"
+pkgver = "0.41.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pypa/wheel"
 source = f"$(PYPI_SITE)/w/wheel/wheel-{pkgver}.tar.gz"
-sha256 = "cd1196f3faee2b31968d626e1731c94f99cbdb67cf5a46e4f5656cbee7738873"
+sha256 = "55a0f0a5a84869bce5ba775abfd9c462e3a6b1b7b7ec69d72c0b83d673a5114d"
 # TODO
 options = ["!check"]
 
