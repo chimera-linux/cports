21dd519af3cbeda987a606558c7783a6806811fa
q66 <q66@chimera-linux.org>
2023-07-27 00:33:06 +0200

    main/gtar: update to 1.35

--- main/gtar/template.py
+++ main/gtar/template.py
@@ -1,11 +1,12 @@
 pkgname = "gtar"
-pkgver = "1.34"
+pkgver = "1.35"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--program-prefix=g",
     "gl_cv_struct_dirent_d_ino=yes",
 ]
+configure_gen = []
 hostmakedepends = ["texinfo"]
 makedepends = ["acl-devel"]
 pkgdesc = "GNU tape archiver"
@@ -13,7 +14,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/tar"
 source = f"$(GNU_SITE)/tar/tar-{pkgver}.tar.xz"
-sha256 = "63bebd26879c5e1eea4352f0d03c991f966aeb3ddeb3c7445c902568d5411d28"
+sha256 = "4d62ff37342ec7aed748535323930c7cf94acf71c3591882b26a7ea50f3edc16"
 hardening = ["vis", "cfi"]
-
-configure_gen = []
