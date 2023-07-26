98f8cb8bb965a059b204de9f5039ff7b4e9ff8f6
q66 <q66@chimera-linux.org>
2023-07-27 00:38:25 +0200

    main/python-pip: update to 23.2.1

--- main/python-pip/template.py
+++ main/python-pip/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pip"
-pkgver = "23.2"
+pkgver = "23.2.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pip.pypa.io"
 source = f"$(PYPI_SITE)/p/pip/pip-{pkgver}.tar.gz"
-sha256 = "a160a170f3331d9ca1a0247eb1cd79c758879f1f81158f9cd05bbb5df80bea5c"
+sha256 = "fb0bd5435b3200c602b5bf61d2d43c2f13c02e29c1707567ae7fbc514eb9faf2"
 # unpackaged dependencies
 options = ["!check"]
 
