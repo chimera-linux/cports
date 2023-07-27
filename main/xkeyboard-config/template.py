7c1d703147fbe607b0ba60ffdbcab1db7651e7b1
q66 <q66@chimera-linux.org>
2023-07-27 03:19:38 +0200

    main/xkeyboard-config: update to 2.39

--- main/xkeyboard-config/template.py
+++ main/xkeyboard-config/template.py
@@ -1,5 +1,5 @@
 pkgname = "xkeyboard-config"
-pkgver = "2.38"
+pkgver = "2.39"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dxorg-rules-symlinks=true", "-Dcompat-rules=true"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.freedesktop.org/wiki/Software/XKeyboardConfig"
 source = f"$(XORG_SITE)/data/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "0690a91bab86b18868f3eee6d41e9ec4ce6894f655443d490a2184bfac56c872"
+sha256 = "5ac5f533eff7b0c116805fe254fd79b2c9882700a4f9f2c070f8c4eae5aaa682"
 hardening = ["vis", "cfi"]
 
 
