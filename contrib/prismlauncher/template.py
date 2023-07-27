c4bae063bc7f181348c65ad362cf14ac2510c837
q66 <q66@chimera-linux.org>
2023-07-27 03:06:51 +0200

    contrib/prismlauncher: update to 7.2

--- contrib/prismlauncher/template.py
+++ contrib/prismlauncher/template.py
@@ -1,5 +1,5 @@
 pkgname = "prismlauncher"
-pkgver = "7.1"
+pkgver = "7.2"
 pkgrel = 0
 build_style = "cmake"
 configure_env = {"JAVA_HOME": "/usr/lib/jvm/java-17-openjdk"}
@@ -21,4 +21,4 @@ maintainer = "aurelia <git@elia.garden>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/PrismLauncher/PrismLauncher"
 source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "dc7aeff6e0dc12f4f2065e718418a4110ccdbad3e49fbd58e416a213fde7ebb1"
+sha256 = "5733b55c4532286813a6fb7de2f3a38e6f6db743a251919c8b646d32a84514b4"
