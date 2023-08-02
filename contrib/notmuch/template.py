ded91dbd4bfff5da26e201b0e277a3408a186351
psykose <alice@ayaya.dev>
2023-08-02 20:07:45 +0000

    contrib/notmuch: new package (0.37)

---
+++ contrib/notmuch/template.py
@@ -0,0 +1,31 @@
+pkgname = "notmuch"
+pkgver = "0.37"
+pkgrel = 0
+build_style = "configure"
+configure_args = ["--prefix=/usr"]
+make_cmd = "gmake"
+hostmakedepends = [
+    "gmake",
+    "pkgconf",
+]
+makedepends = [
+    "linux-headers",
+    "gmime-devel",
+    "talloc-devel",
+    "xapian-core-devel",
+    "zlib-devel",
+]
+pkgdesc = "Fast, global-search, tag-based email system"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-3.0-or-later"
+url = "https://notmuchmail.org"
+source = f"{url}/releases/notmuch-{pkgver}.tar.xz"
+sha256 = "0e766df28b78bf4eb8235626ab1f52f04f1e366649325a8ce8d3c908602786f6"
+hardening = ["vis", "cfi"]
+# FIXME: they hang forever, after failing a test for -v not silencing output in harness
+options = ["!check"]
+
+
+@subpackage("notmuch-devel")
+def _devel(self):
+    return self.default_devel()
