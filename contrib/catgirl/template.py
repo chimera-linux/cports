644953f39387fe9c5b3fe9d3084c1691fef43e8d
psykose <alice@ayaya.dev>
2023-08-02 17:39:42 +0000

    contrib/catgirl: new package (2.2)

---
+++ contrib/catgirl/template.py
@@ -0,0 +1,27 @@
+pkgname = "catgirl"
+pkgver = "2.2"
+pkgrel = 0
+build_style = "configure"
+configure_args = ["--prefix=/usr", "--mandir=/usr/share/man"]
+make_build_target = "all"
+hostmakedepends = [
+    "pkgconf",
+]
+makedepends = [
+    "libretls-devel",
+    "ncurses-devel",
+]
+pkgdesc = "Terminal IRC client"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-3.0-or-later"
+url = "https://git.causal.agency/catgirl"
+source = f"{url}/snapshot/{pkgname}-{pkgver}.tar.gz"
+sha256 = "fb6d04a099303af05d278c705c1542e7ee61643c030d6a0b68dec5371080a3c7"
+# FIXME: cfi
+hardening = ["vis"]
+# no tests
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
