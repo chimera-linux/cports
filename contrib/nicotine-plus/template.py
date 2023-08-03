ea468a9859ff8ebb104833db1e1dfa5dd5222368
psykose <alice@ayaya.dev>
2023-08-03 16:23:26 +0000

    contrib/nicotine-plus: new package (3.2.9)

---
+++ contrib/nicotine-plus/template.py
@@ -0,0 +1,29 @@
+pkgname = "nicotine-plus"
+pkgver = "3.2.9"
+pkgrel = 0
+build_style = "python_pep517"
+make_check_target = "test/unit"
+hostmakedepends = [
+    "gettext-tiny",
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+checkdepends = [
+    "python-pytest",
+    "python-semidbm",
+]
+depends = [
+    "gtk+3",
+    "python-gobject",
+    "python-semidbm",
+]
+pkgdesc = "Graphical client for the Soulseek network"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-3.0-or-later"
+url = "https://nicotine-plus.github.io/nicotine-plus"
+source = (
+    f"https://github.com/Nicotine-Plus/nicotine-plus/archive/{pkgver}.tar.gz"
+)
+sha256 = "aeaf45742a915345d1635f36ca334c3f332788c7a27262408be9998985f99e41"
