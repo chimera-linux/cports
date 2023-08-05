23efbe5026b11b22b2578133af67668871d82c1c
q66 <q66@chimera-linux.org>
2023-08-05 14:42:21 +0200

    main/python-pycodestyle: update to 2.11.0
    Also switch url as pypi tarball lacks testing data.

--- main/python-pycodestyle/template.py
+++ main/python-pycodestyle/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pycodestyle"
-pkgver = "2.10.0"
+pkgver = "2.11.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -9,8 +9,8 @@ pkgdesc = "Python style guide checker"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/PyCQA/pycodestyle"
-source = f"$(PYPI_SITE)/p/pycodestyle/pycodestyle-{pkgver}.tar.gz"
-sha256 = "347187bdb476329d98f695c213d7295a846d1152ff4fe9bacb8a9590b8ee7053"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "757a3dba55dce2ae8b01fe7b46c20cd1e4c0fe794fe6119bce66b942f35e2db0"
 
 
 def post_install(self):
