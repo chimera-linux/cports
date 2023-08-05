68eaa62841f5bdc9118a6fac5e2606605ca67d71
q66 <q66@chimera-linux.org>
2023-08-05 14:39:24 +0200

    main/libplacebo: update to 6.292.1

--- main/libplacebo/template.py
+++ main/libplacebo/template.py
@@ -1,6 +1,6 @@
 pkgname = "libplacebo"
-pkgver = "6.292.0"
-pkgrel = 1
+pkgver = "6.292.1"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dshaderc=enabled",
@@ -29,7 +29,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://code.videolan.org/videolan/libplacebo"
 source = f"{url}/-/archive/v{pkgver}/{pkgname}-v{pkgver}.tar.gz"
-sha256 = "22116cfada2a878407c1da8f60d4b7497137f7fcb625f90e8669422cae8749f0"
+sha256 = "3f25b32b37f24b2247f5024e8db252e617cb368c3f25e04b3f50d6355009754f"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
 
