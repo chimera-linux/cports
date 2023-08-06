14f400df95e638ce65dad2015b6998175c82aa19
Wesley Moore <wes@wezm.net>
2023-08-06 16:11:26 +1000

    contrib/cage: new package (0.1.5)

---
+++ contrib/cage/template.py
@@ -0,0 +1,17 @@
+pkgname = "cage"
+pkgver = "0.1.5"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf", "scdoc", "wayland-progs"]
+makedepends = ["wayland-protocols", "wlroots-devel"]
+pkgdesc = "Kiosk compositor for Wayland"
+maintainer = "Wesley Moore <wes@wezm.net>"
+license = "MIT"
+url = "https://www.hjdskes.nl/projects/cage"
+source = f"https://github.com/cage-kiosk/cage/releases/download/v{pkgver}/cage-{pkgver}.tar.gz"
+sha256 = "ece0312e559289df0238289ea6c60e9fed32d27fe3ae8a8f83eeff26ddc239e1"
+hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
