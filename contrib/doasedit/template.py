6e08665e3af5660acfe19a2c4622b4a442b432d4
psykose <alice@ayaya.dev>
2023-08-06 06:39:30 +0000

    contrib/doasedit: new package (1.0.6)

---
+++ contrib/doasedit/template.py
@@ -0,0 +1,15 @@
+pkgname = "doasedit"
+pkgver = "1.0.6"
+pkgrel = 0
+depends = ["opendoas"]
+pkgdesc = "Shell script to edit files with doas"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://codeberg.org/TotallyLeGIT/doasedit"
+source = f"https://codeberg.org/TotallyLeGIT/doasedit/archive/{pkgver}.tar.gz"
+sha256 = "bfe0af402b1f2df447a3cecb3027a9176f8de0bbe8f09be9f59a15fcd58e22bf"
+
+
+def do_install(self):
+    self.install_file("doasedit", "usr/bin", mode=0o755)
+    self.install_license("LICENSE")
