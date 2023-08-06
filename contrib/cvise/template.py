0fb9dbd4736fb73dc2f81db819cc18078c32671e
psykose <alice@ayaya.dev>
2023-08-06 03:05:33 +0000

    contrib/cvise: new package (2.8.0)

---
+++ contrib/cvise/template.py
@@ -0,0 +1,40 @@
+pkgname = "cvise"
+pkgver = "2.8.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "clang-tools-extra",
+    "cmake",
+    "flex",
+    "ninja",
+]
+makedepends = [
+    "clang-devel",
+    "llvm-devel",
+]
+depends = [
+    "clang",
+    "python-chardet",
+    "python-pebble",
+    "python-psutil",
+    "unifdef",
+]
+checkdepends = depends + [
+    "python-pytest",
+]
+pkgdesc = "Python port of C-Reduce, for program testcase minimisation"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "NCSA"
+url = "https://github.com/marxin/cvise"
+source = f"{url}/archive/v{pkgver}.tar.gz"
+sha256 = "cb0bd15885b18b4e79be216c6ca7bed546defc0e9b533d6103868580c690a1a1"
+
+
+def do_check(self):
+    # this one test fails but everything else is fine
+    self.do("pytest", "-k", "not test_simple_reduction", wrksrc=self.make_dir)
+
+
+def post_install(self):
+    self.install_license("COPYING")
+    self.rm(self.destdir / "usr/share/cvise/tests", recursive=True)
