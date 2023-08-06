2f80286575e57b34ec6aa1055a50301e02f4ae18
psykose <alice@ayaya.dev>
2023-08-06 03:28:39 +0000

    contrib/python-execnet: new package (2.0.2)

---
+++ contrib/python-execnet/template.py
@@ -0,0 +1,31 @@
+pkgname = "python-execnet"
+pkgver = "2.0.2"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-hatch_vcs",
+    "python-hatchling",
+]
+checkdepends = [
+    "python-pytest",
+]
+pkgdesc = "Distributed python deployment and communication"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/pytest-dev/execnet"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "7500bdc398c39558c04e12eb52075af7365a40f90dec08593f182e8ed733f91e"
+
+
+def init_build(self):
+    self.env["SETUPTOOLS_SCM_PRETEND_VERSION"] = f"{pkgver}"
+
+
+def init_check(self):
+    self.env["PYTHONPATH"] = "src"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
