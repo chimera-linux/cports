aa80e209f5f70011bd725b76a663b923681fbb6d
psykose <alice@ayaya.dev>
2023-08-12 23:47:13 +0000

    main/less: update to 643

--- main/less/template.py
+++ main/less/template.py
@@ -1,23 +1,25 @@
 pkgname = "less"
-pkgver = "633"
+pkgver = "643"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-regex=posix"]
 make_cmd = "gmake"
 hostmakedepends = ["gmake"]
 makedepends = ["ncurses-devel"]
+checkdepends = ["perl"]
 pkgdesc = "Pager program similar to more(1)"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:less OR GPL-3.0-or-later"
 url = "http://www.greenwoodsoftware.com/less"
 source = f"http://www.greenwoodsoftware.com/less/less-{pkgver}.tar.gz"
-sha256 = "2f201d64b828b88af36dfe6cfdba3e0819ece2e446ebe6224813209aaefed04f"
+sha256 = "2911b5432c836fa084c8a2e68f6cd6312372c026a58faaa98862731c8b6052e8"
 hardening = ["vis", "cfi"]
-# test suite not shipped
+# less -> perl -> less cycle
 options = ["!check"]
 
 
 def post_install(self):
+    self.install_license("LICENSE")
     self.install_file(self.files_path / "lesspipe.sh", "usr/bin", mode=0o755)
     self.install_file(
         self.files_path / "zless.sh", "usr/bin", mode=0o755, name="zless"
