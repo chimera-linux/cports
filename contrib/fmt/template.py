7852395b591287b4af550167a56dfaeb9cb29172
psykose <alice@ayaya.dev>
2023-08-14 04:53:41 +0000

    contrib/fmt: update to 10.1.0

--- contrib/fmt/template.py
+++ contrib/fmt/template.py
@@ -1,5 +1,5 @@
 pkgname = "fmt"
-pkgver = "10.0.0"
+pkgver = "10.1.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -20,7 +20,7 @@ url = "https://fmt.dev/latest/index.html"
 source = (
     f"https://github.com/fmtlib/fmt/releases/download/{pkgver}/fmt-{pkgver}.zip"
 )
-sha256 = "4943cb165f3f587f26da834d3056ee8733c397e024145ca7d2a8a96bb71ac281"
+sha256 = "d725fa83a8b57a3cedf238828fa6b167f963041e8f9f7327649bddc68ae316f4"
 # FIXME: cfi test failures
 hardening = ["vis"]
 
