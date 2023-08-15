4f9c1ae306907fc35aa3d69861d2e343a6767415
psykose <alice@ayaya.dev>
2023-08-15 21:01:41 +0000

    contrib/liborcus: rebuild for boost 1.83.0

--- contrib/liborcus/template.py
+++ contrib/liborcus/template.py
@@ -1,6 +1,6 @@
 pkgname = "liborcus"
 pkgver = "0.18.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 make_cmd = "gmake"
 hostmakedepends = ["pkgconf", "gmake", "automake", "libtool", "python"]
