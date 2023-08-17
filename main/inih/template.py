2eae39848ec7b5268be20ca800280749a2548c05
psykose <alice@ayaya.dev>
2023-08-17 03:25:32 +0000

    main/inih: unset CFI

--- main/inih/template.py
+++ main/inih/template.py
@@ -1,6 +1,6 @@
 pkgname = "inih"
 pkgver = "57"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
     "-Ddistro_install=true",
@@ -12,7 +12,8 @@ license = "BSD-3-Clause"
 url = "https://github.com/benhoyt/inih"
 source = f"{url}/archive/r{pkgver}.tar.gz"
 sha256 = "f03f98ca35c3adb56b2358573c8d3eda319ccd5287243d691e724b7eafa970b3"
-hardening = ["vis", "cfi"]
+# FIXME: cfi breaks xdg-desktop-portal-wlr when it loads an empty config
+hardening = ["vis"]
 
 
 def post_install(self):
