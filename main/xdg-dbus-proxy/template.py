276b4a20b4f8dcb89641eee85ee16ad821480f07
psykose <alice@ayaya.dev>
2023-08-17 08:42:42 +0000

    main/xdg-dbus-proxy: update to 0.1.5

--- main/xdg-dbus-proxy/template.py
+++ main/xdg-dbus-proxy/template.py
@@ -1,9 +1,15 @@
 pkgname = "xdg-dbus-proxy"
-pkgver = "0.1.4"
+pkgver = "0.1.5"
 pkgrel = 0
-build_style = "gnu_configure"
-configure_args = ["--enable-man"]
-hostmakedepends = ["pkgconf", "xsltproc", "docbook-xsl-nons"]
+build_style = "meson"
+configure_args = ["-Dman=enabled"]
+hostmakedepends = [
+    "docbook-xsl-nons",
+    "meson",
+    "ninja",
+    "pkgconf",
+    "xsltproc",
+]
 makedepends = ["glib-devel"]
 checkdepends = ["dbus"]
 pkgdesc = "Filtering proxy for D-Bus connections"
@@ -11,6 +17,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/flatpak/xdg-dbus-proxy"
 source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "1ec0eab53d1e49966d722352bcfd51ac402dce5190baedc749a8541e761670ab"
-
-configure_gen = []
+sha256 = "061dcfaf8a0650e5fd9d5432dfe88bda749ea0d079dc136304bfecfbce0661fb"
