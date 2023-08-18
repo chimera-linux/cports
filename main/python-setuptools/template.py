c1b656ef92143ebca07ffe594a324c44c42dbd89
psykose <alice@ayaya.dev>
2023-08-18 12:16:17 +0000

    main/python-setuptools: update to 68.1.2

--- main/python-setuptools/template.py
+++ main/python-setuptools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-setuptools"
-pkgver = "68.1.0"
+pkgver = "68.1.2"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-devel"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pypa/setuptools"
 source = f"$(PYPI_SITE)/s/setuptools/setuptools-{pkgver}.tar.gz"
-sha256 = "d59c97e7b774979a5ccb96388efc9eb65518004537e85d52e81eaee89ab6dd91"
+sha256 = "3d4dfa6d95f1b101d695a6160a7626e15583af71a5f52176efa5d39a054d475d"
 env = {
     "SETUPTOOLS_INSTALL_WINDOWS_SPECIFIC_FILES": "0",
     "SETUPTOOLS_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT": "1",
