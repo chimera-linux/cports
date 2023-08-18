b956a7d49ea234719351e1d3c8531c334bada8ad
psykose <alice@ayaya.dev>
2023-08-18 09:53:45 +0000

    main/libxmlb: update to 0.3.13

--- main/libxmlb/template.py
+++ main/libxmlb/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxmlb"
-pkgver = "0.3.12"
+pkgver = "0.3.13"
 pkgrel = 0
 build_style = "meson"
 # tests require some file to exist in /tmp? so it fails
@@ -11,7 +11,7 @@ maintainer = "eater <=@eater.me>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/hughsie/libxmlb"
 source = f"https://github.com/hughsie/libxmlb/releases/download/{pkgver}/libxmlb-{pkgver}.tar.xz"
-sha256 = "b1e61f64dadc67e458430bb35a9f1b25170aafcf5674a52012ae38a2212227b3"
+sha256 = "ae45ae4c0606f94d98fea16b7097b3470e48c2f277954ae9bc4d9d029ed72496"
 options = ["!cross"]
 
 
