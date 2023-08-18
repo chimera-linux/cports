3173ef67bd677b627af38fffb95bde49e094af6b
Peter <47680251+feurry@users.noreply.github.com>
2023-08-18 10:14:34 -0500

    contrib/keyd: new package (2.4.3)

---
+++ contrib/keyd/template.py
@@ -0,0 +1,23 @@
+pkgname = "keyd"
+pkgver = "2.4.3"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_use_env = True
+hostmakedepends = ["gmake"]
+makedepends = ["linux-headers"]
+pkgdesc = "Key remapping daemon for linux"
+maintainer = "feurry <=feurry@gmail.com>"
+license = "MIT"
+url = "https://github.com/rvaiya/keyd"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "d4075f673879f4950c1c0d43985797603200e993596a5d7fcec1597c2350c380"
+hardening = ["vis", "cfi"]
+# no tests available
+options = ["!check"]
+system_groups = ["keyd"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+    self.install_service(self.files_path / "keyd")
