ded7506e6e2d39394bd45130ef1dd92f3531d052
q66 <q66@chimera-linux.org>
2023-08-20 23:19:06 +0200

    contrib/libzen: new package (0.4.41)

---
+++ contrib/libzen/template.py
@@ -0,0 +1,17 @@
+pkgname = "libzen"
+pkgver = "0.4.41"
+pkgrel = 0
+build_wrksrc = "Project/CMake"
+build_style = "cmake"
+hostmakedepends = ["pkgconf", "cmake", "ninja"]
+pkgdesc = "Shared library for libmediainfo and mediainfo"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "Zlib"
+url = "https://mediaarea.net/en/MediaInfo"
+source = f"https://mediaarea.net/download/source/libzen/{pkgver}/libzen_{pkgver}.tar.bz2"
+sha256 = "eb237d7d3dca6dc6ba068719420a27de0934a783ccaeb2867562b35af3901e2d"
+
+
+@subpackage("libzen-devel")
+def _devel(self):
+    return self.default_devel()
