218b4987a1614db3d53e72715fea8290312991b8
psykose <alice@ayaya.dev>
2023-08-20 07:35:16 +0000

    main/zlib: update to 1.3

--- main/zlib/template.py
+++ main/zlib/template.py
@@ -1,14 +1,14 @@
 pkgname = "zlib"
-pkgver = "1.2.13"
+pkgver = "1.3"
 pkgrel = 0
 build_style = "configure"
 hostmakedepends = ["pkgconf"]
 pkgdesc = "Compression/decompression Library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Zlib"
-url = "http://www.zlib.net"
-source = f"{url}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
+url = "https://www.zlib.net"
+source = f"{url}/fossils/{pkgname}-{pkgver}.tar.gz"
+sha256 = "ff0ba4c292013dbc27530b3a81e1f9a813cd39de01ca5e0f8bf355702efa593e"
 tool_flags = {"CFLAGS": ["-fPIC"]}
 options = ["bootstrap"]
 
