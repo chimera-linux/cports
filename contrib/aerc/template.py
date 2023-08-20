57a276d76eabf9d2bcc00c15fc8d22e122666b33
psykose <alice@ayaya.dev>
2023-08-20 10:46:51 +0000

    contrib/aerc: actually enable notmuch support

--- contrib/aerc/template.py
+++ contrib/aerc/template.py
@@ -1,6 +1,6 @@
 pkgname = "aerc"
 pkgver = "0.15.2"
-pkgrel = 1
+pkgrel = 2
 build_style = "makefile"
 make_check_target = "tests"
 hostmakedepends = [
@@ -20,7 +20,9 @@ license = "MIT"
 url = "https://sr.ht/~rjarry/aerc"
 source = f"https://git.sr.ht/~rjarry/aerc/archive/{pkgver}.tar.gz"
 sha256 = "722da196e8807c497f5472704b8a1737d7780ad0faa7166ae83348bc67b144f7"
-options = ["!strip"]
+tool_flags = {"GOFLAGS": ["-tags=notmuch", "-buildmode=pie"]}
+# ppc64le objcopy
+options = ["!debug"]
 
 
 def post_prepare(self):
