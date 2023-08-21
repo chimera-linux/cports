6cf463615b6884384c1357260806ffae71adc26f
psykose <alice@ayaya.dev>
2023-08-21 03:51:14 +0000

    contrib/libsbsms: new package (2.3.0)

---
+++ contrib/libsbsms/template.py
@@ -0,0 +1,26 @@
+pkgname = "libsbsms"
+pkgver = "2.3.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+pkgdesc = "Library for high quality time and pitch scale modification"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later"
+url = "https://github.com/claytonotey/libsbsms"
+source = (
+    f"https://github.com/claytonotey/libsbsms/archive/refs/tags/{pkgver}.tar.gz"
+)
+sha256 = "4f88d152bc06fedbda9d5d65517d40254a7310c9050601a93122309d45afd2c9"
+# vis breaks symbols
+hardening = []
+# no tests
+options = ["!check"]
+
+
+@subpackage("libsbsms-devel")
+def _devel(self):
+    return self.default_devel()
