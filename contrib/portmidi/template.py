ee52601ea73dc5e19fe1ef98b61d73e155c22b4d
psykose <alice@ayaya.dev>
2023-08-21 03:04:49 +0000

    contrib/portmidi: new package (2.0.4)

---
+++ contrib/portmidi/template.py
@@ -0,0 +1,33 @@
+pkgname = "portmidi"
+pkgver = "2.0.4"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "alsa-lib-devel",
+]
+pkgdesc = "Cross-platform MIDI input/output library"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/PortMidi/portmidi"
+source = (
+    f"https://github.com/PortMidi/portmidi/archive/refs/tags/v{pkgver}.tar.gz"
+)
+sha256 = "64893e823ae146cabd3ad7f9a9a9c5332746abe7847c557b99b2577afa8a607c"
+# vis breaks symbols
+hardening = []
+# no tests
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("license.txt")
+
+
+@subpackage("portmidi-devel")
+def _devel(self):
+    return self.default_devel()
