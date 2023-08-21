8eba99ed95e18f5c8112d8cf7a8babf7778ac8d8
psykose <alice@ayaya.dev>
2023-08-21 04:53:26 +0000

    main/python-fonttools: update to 4.42.1

--- main/python-fonttools/template.py
+++ main/python-fonttools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-fonttools"
-pkgver = "4.42.0"
+pkgver = "4.42.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT AND OFL-1.1 AND BSD-3-Clause AND Apache-2.0"
 url = "https://github.com/fonttools/fonttools"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "f556a5c7a208c58eb1fc2055a6bc6bdb8bc8a3a02acfc6e79f7db75f085fb3b3"
+sha256 = "1e96ea7e11c76649da322fb059fe5506696efe6929983c1d3cee3d636d05629a"
 # unpackaged deps
 options = ["!check"]
 
