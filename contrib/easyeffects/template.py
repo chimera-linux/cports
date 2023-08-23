9c467da4cc02d3bd8d2b3f9d43480caebd5bbcfa
psykose <alice@ayaya.dev>
2023-08-23 18:47:01 +0000

    contrib/easyeffects: new package (7.0.7)
    Co-authored-by: q66 <q66@chimera-linux.org>
    
    Closes https://github.com/chimera-linux/cports/pull/396

---
+++ contrib/easyeffects/template.py
@@ -0,0 +1,48 @@
+pkgname = "easyeffects"
+pkgver = "7.0.7"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Denable-libcpp-workarounds=true"]
+hostmakedepends = [
+    "desktop-file-utils",
+    "gettext",
+    "glib-devel",
+    "gtk-update-icon-cache",
+    "itstool",
+    "meson",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "appstream-glib-devel",
+    "fftw-devel",
+    "fmt-devel",
+    "glib-devel",
+    "gsl-devel",
+    "gtk4-devel",
+    "libadwaita-devel",
+    "libbs2b-devel",
+    "libebur128-devel",
+    "libsamplerate-devel",
+    "libsigc++-devel",
+    "libsndfile-devel",
+    "lilv-devel",
+    "lv2",
+    "nlohmann-json",
+    "onetbb-devel",
+    "pipewire-devel",
+    "rnnoise-devel",
+    "soundtouch-devel",
+    "speexdsp-devel",
+    "zita-convolver-devel",
+]
+pkgdesc = "PipeWire audio plugins"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/wwmm/easyeffects"
+source = (
+    f"https://github.com/wwmm/easyeffects/archive/refs/tags/v{pkgver}.tar.gz"
+)
+sha256 = "08aa95bd15be16185409dbb0aec13000034fc128ea63c05f62620c0c877acb1c"
+# FIXME: cfi
+hardening = ["vis"]
