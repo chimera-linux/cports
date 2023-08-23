00fe7c04da228cfe8d159351ca1d3799d212f81e
q66 <q66@chimera-linux.org>
2023-08-23 03:09:18 +0200

    main/dinit: update to 0.17.0
    We no longer need to rely on a hand-picked git revision.

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -1,9 +1,9 @@
 pkgname = "dinit"
-pkgver = "0.16.999"
-_commit = "122599a461f3dc90a78f51d9f2c55facc70ee000"
+pkgver = "0.17.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--syscontrolsocket=/run/dinitctl"]
+configure_gen = []
 make_cmd = "gmake"
 make_dir = "."
 make_check_args = ["check-igr"]  # additional target
@@ -12,11 +12,7 @@ pkgdesc = "Service manager and init system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://davmac.org/projects/dinit"
-# source = f"https://github.com/davmac314/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-source = f"https://github.com/davmac314/{pkgname}/archive/{_commit}.tar.gz"
-sha256 = "b60f73b5c217c1e7ae22bfad972e25583ad3e5ff1da8c1dd5815d8b95f52df91"
-hardening = ["vis", "cfi"]
-
+source = f"https://github.com/davmac314/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "f3ce6eaaabd571edc16f54907cd6062f22ed5d4b6f9ae6fc1c6533f39469b1f0"
 tool_flags = {"CXXFLAGS": ["-fno-rtti"]}
-
-configure_gen = []
+hardening = ["vis", "cfi"]
