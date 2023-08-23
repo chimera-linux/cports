ab070376e23fb7d17e28e7d212868718e4ea8a5d
psykose <alice@ayaya.dev>
2023-08-23 03:06:12 +0000

    contrib/python-setuptools-rust: update to 1.7.0

--- contrib/python-setuptools-rust/template.py
+++ contrib/python-setuptools-rust/template.py
@@ -1,19 +1,23 @@
 pkgname = "python-setuptools-rust"
-pkgver = "1.6.0"
+pkgver = "1.7.0"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools_scm"]
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools_scm",
+    "python-wheel",
+]
 depends = [
     "python-semantic_version",
     "python-setuptools",
-    "python-typing_extensions",
 ]
 pkgdesc = "Setuptools plugin for Rust support"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/PyO3/setuptools-rust"
 source = f"$(PYPI_SITE)/s/setuptools-rust/setuptools-rust-{pkgver}.tar.gz"
-sha256 = "c86e734deac330597998bfbc08da45187e6b27837e23bd91eadb320732392262"
+sha256 = "c7100999948235a38ae7e555fe199aa66c253dc384b125f5d85473bf81eae3a3"
 # unpackaged checkdepends
 options = ["!check"]
 
