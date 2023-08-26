ab65c65b0d45b907eb6cb09424575b67423ba89c
Jami Kettunen <jami.kettunen@protonmail.com>
2023-08-26 19:38:31 +0300

    contrib/u-boot-sifive_unmatched: update to 2023.07.02

--- contrib/u-boot-sifive_unmatched/template.py
+++ contrib/u-boot-sifive_unmatched/template.py
@@ -1,5 +1,5 @@
 pkgname = "u-boot-sifive_unmatched"
-pkgver = "2023.04"
+pkgver = "2023.07.02"
 pkgrel = 0
 archs = ["riscv64"]
 build_style = "u_boot"
@@ -21,7 +21,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND BSD-3-Clause"
 url = "https://www.denx.de/wiki/U-Boot"
 source = f"https://ftp.denx.de/pub/u-boot/u-boot-{pkgver}.tar.bz2"
-sha256 = "e31cac91545ff41b71cec5d8c22afd695645cd6e2a442ccdacacd60534069341"
+sha256 = "6b6a48581c14abb0f95bd87c1af4d740922406d7b801002a9f94727fdde021d5"
 env = {
     "U_BOOT_TRIPLET": "riscv64-unknown-elf",
     "U_BOOT_TARGETS": "spl/u-boot-spl.bin u-boot.itb",
