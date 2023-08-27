b9320237472d2a20e9b3ad91ab6586ef667ed7b6
q66 <q66@chimera-linux.org>
2023-08-27 03:10:46 +0200

    contrib/libdisplay-info: new package (0.1.1)

---
+++ contrib/libdisplay-info/template.py
@@ -0,0 +1,29 @@
+pkgname = "libdisplay-info"
+pkgver = "0.1.1"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "hwdata-devel",
+]
+pkgdesc = "EDID and DisplayID library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://gitlab.freedesktop.org/emersion/libdisplay-info"
+source = f"{url}/-/archive/{pkgver}/libdisplay-info-{pkgver}.tar.gz"
+sha256 = "a5aeef57817916286526292ec816a5338c4d3c0094ce91e584fc82b57070a44f"
+# edid-decode
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("libdisplay-info-devel")
+def _dev(self):
+    return self.default_devel()
