df309b84add2f94437956b9e539ce72d8cbf0cac
Wesley Moore <wes@wezm.net>
2023-08-28 09:03:29 +1000

    contrib/wl-clipboard: update to 2.2.1

--- contrib/wl-clipboard/template.py
+++ contrib/wl-clipboard/template.py
@@ -1,5 +1,5 @@
 pkgname = "wl-clipboard"
-pkgver = "2.2.0"
+pkgver = "2.2.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -14,5 +14,5 @@ maintainer = "Wesley Moore <wes@wezm.net>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/bugaevc/wl-clipboard"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "414005cfa229d0e54f89a0d9a8473938e4c29adc21a9e556847a4d44ad508874"
+sha256 = "6eb8081207fb5581d1d82c4bcd9587205a31a3d47bea3ebeb7f41aa1143783eb"
 hardening = ["vis", "!cfi"]
