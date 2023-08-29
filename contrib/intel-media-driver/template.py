b6b4da3532ba749d187b1b2922dfa05531d2dd88
psykose <alice@ayaya.dev>
2023-08-29 02:00:37 +0000

    contrib/intel-media-driver: update to 23.3.2

--- contrib/intel-media-driver/template.py
+++ contrib/intel-media-driver/template.py
@@ -1,5 +1,5 @@
 pkgname = "intel-media-driver"
-pkgver = "23.3.1"
+pkgver = "23.3.2"
 pkgrel = 0
 # doesn't build elsewhere
 archs = ["x86_64"]
@@ -25,7 +25,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-3-Clause"
 url = "https://github.com/intel/media-driver"
 source = f"https://github.com/intel/media-driver/archive/refs/tags/intel-media-{pkgver}.tar.gz"
-sha256 = "ce929da08ea917e4bb133ae67b5de42dce9c459b06a2670358a6ada00c48db58"
+sha256 = "6a0fc8513fb60e2e2fc2178a8fb0b25cb5dbacf52a645af9a9297fc5f43822ab"
 # FIXME: cfi
 hardening = ["vis"]
 # no tests
