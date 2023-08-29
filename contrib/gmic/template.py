66340c0433d54c01d87e05d2ebaf7458f03f7380
psykose <alice@ayaya.dev>
2023-08-29 10:49:44 +0000

    contrib/gmic: new package (3.2.6)

---
+++ contrib/gmic/template.py
@@ -0,0 +1,67 @@
+pkgname = "gmic"
+pkgver = "3.2.6"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_build_target = "lib"
+make_build_args = [
+    "cli_shared",
+    "gmic_qt_shared",
+    "QMAKE=qmake6",
+]
+make_use_env = True
+hostmakedepends = [
+    "bash",
+    "gmake",
+    "pkgconf",
+    "qt6-qtbase",
+]
+makedepends = [
+    "fftw-devel",
+    "libcurl-devel",
+    "libomp-devel",
+    "libpng-devel",
+    "libtiff-devel",
+    "libx11-devel",
+    "openexr-devel",
+    "qt6-qtbase-devel",
+]
+pkgdesc = "Full-featured Open-Source Framework for Image Processing"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "CECILL-2.1"
+url = "https://gmic.eu"
+source = f"https://gmic.eu/files/source/gmic_{pkgver}.tar.gz"
+sha256 = "55993e55a30fe2da32f9533b9db2a3250affa2b32003b0c49c36eec2b2c6e007"
+# vis broken
+hardening = []
+# no tests
+# breaks in parallel if lib isn't built first by itself
+options = ["!check", "!parallel"]
+
+
+def init_build(self):
+    # disarm silly flags
+    self.make_build_args += ["OPT_CFLAGS="]
+
+
+def post_install(self):
+    self.install_dir("usr/share")
+    self.mv(
+        self.destdir / "plug-ins",
+        self.destdir / "usr/share/gmic",
+    )
+
+
+@subpackage("gmic-devel")
+def _devel(self):
+    return self.default_devel()
+
+
+@subpackage("gmic-qt")
+def _qt(self):
+    self.pkgdesc = "Qt GUI for GMIC"
+    return [
+        "usr/bin/gmic_qt",
+        "usr/share/applications",
+        "usr/share/icons",
+    ]
