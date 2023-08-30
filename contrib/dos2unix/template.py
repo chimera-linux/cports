0f7046588851a2ee2c585ee47fe1970cce432900
psykose <alice@ayaya.dev>
2023-08-30 13:56:50 +0000

    contrib/dos2unix: update to 7.5.1

--- contrib/dos2unix/template.py
+++ contrib/dos2unix/template.py
@@ -1,6 +1,6 @@
 pkgname = "dos2unix"
-pkgver = "7.5.0"
-pkgrel = 1
+pkgver = "7.5.1"
+pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
 hostmakedepends = ["pkgconf", "gmake", "gettext"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://waterlan.home.xs4all.nl/dos2unix.html"
 source = f"https://waterlan.home.xs4all.nl/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "7a3b01d01e214d62c2b3e04c3a92e0ddc728a385566e4c0356efa66fd6eb95af"
+sha256 = "da07788bb2e029b0d63f6471d166f68528acd8da2cf14823a188e8a9d5c1fc15"
 
 
 def post_install(self):
