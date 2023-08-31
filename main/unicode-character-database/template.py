92a63906c8895462f8fe164f973572cd587b0837
psykose <alice@ayaya.dev>
2023-08-31 02:48:21 +0000

    main/unicode-character-database: update to 15.1.0

--- main/unicode-character-database/template.py
+++ main/unicode-character-database/template.py
@@ -1,5 +1,5 @@
 pkgname = "unicode-character-database"
-pkgver = "15.0.0"
+pkgver = "15.1.0"
 pkgrel = 0
 pkgdesc = "Unicode Character Database"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -10,8 +10,8 @@ source = [
     f"https://www.unicode.org/Public/zipped/{pkgver}/Unihan.zip",
 ]
 sha256 = [
-    "5fbde400f3e687d25cc9b0a8d30d7619e76cb2f4c3e85ba9df8ec1312cb6718c",
-    "24b154691fc97cb44267b925d62064297086b3f896b57a8181c7b6d42702a026",
+    "cb1c663d053926500cd501229736045752713a066bd75802098598b7a7056177",
+    "a0226610e324bcf784ac380e11f4cbf533ee1e6b3d028b0991bf8c0dc3f85853",
 ]
 
 
