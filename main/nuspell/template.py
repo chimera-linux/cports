7b602da9871cda6519945d08f749c7c49e6ff9fb
psykose <alice@ayaya.dev>
2023-08-31 21:14:58 +0000

    main/nuspell: update to 5.1.3

--- main/nuspell/template.py
+++ main/nuspell/template.py
@@ -1,8 +1,8 @@
 pkgname = "nuspell"
-pkgver = "5.1.2"
-pkgrel = 1
+pkgver = "5.1.3"
+pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DBUILD_SHARED_LIBS=ON"]
+configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DBUILD_TESTING=OFF"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = ["icu-devel"]
 checkdepends = ["catch2"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-or-later"
 url = "https://nuspell.github.io"
 source = f"https://github.com/{pkgname}/{pkgname}/archive/v{pkgver}.tar.gz"
-sha256 = "82f5f2faa82df30099555dca94b3a908d38fdba408e31e74535c523f7e6ea18d"
+sha256 = "2b1c96bdc5a32a17fc8fc24a0c205fa52b0a2920dd6139b0a7d7744cdef48c22"
 hardening = ["!cfi"]  # TODO
 # missing checkdepends
 options = ["!check"]
