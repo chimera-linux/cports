4819b6ca7196bfd8226f58aef9f36c059225f9f2
psykose <alice@ayaya.dev>
2023-09-01 01:03:50 +0000

    contrib/wget2: update to 2.1.0

--- contrib/wget2/template.py
+++ contrib/wget2/template.py
@@ -1,12 +1,19 @@
 pkgname = "wget2"
-pkgver = "2.0.1"
+pkgver = "2.1.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--with-lzma",
     "--with-bzip2",
 ]
-hostmakedepends = ["pkgconf", "texinfo"]
+hostmakedepends = [
+    "autoconf",
+    "automake",
+    "gettext-devel",
+    "libtool",
+    "pkgconf",
+    "texinfo",
+]
 makedepends = [
     "gnutls-devel",
     "libpsl-devel",
@@ -25,7 +32,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.com/gnuwget/wget2"
 source = f"$(GNU_SITE)/wget/{pkgname}-{pkgver}.tar.gz"
-sha256 = "0bb7fa03697bb5b8d05e1b5e15b863440826eb845874c4ffb5e32330f9845db1"
+sha256 = "a05dc5191c6bad9313fd6db2777a78f5527ba4774f665d5d69f5a7461b49e2e7"
 
 
 def post_install(self):
@@ -44,6 +51,3 @@ def _lib(self):
 @subpackage("libwget-devel")
 def _dev(self):
     return self.default_devel()
-
-
-configure_gen = []
