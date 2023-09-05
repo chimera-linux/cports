7ec4c55f76db1394a859c2edcd62f7b78cab0c4d
psykose <alice@ayaya.dev>
2023-09-05 02:15:55 +0000

    main/nghttp2: update to 1.56.0

--- main/nghttp2/template.py
+++ main/nghttp2/template.py
@@ -1,5 +1,5 @@
 pkgname = "nghttp2"
-pkgver = "1.55.1"
+pkgver = "1.56.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -19,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://nghttp2.org"
 source = f"https://github.com/tatsuhiro-t/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "19490b7c8c2ded1cf7c3e3a54ef4304e3a7876ae2d950d60a81d0dc6053be419"
+sha256 = "65eee8021e9d3620589a4a4e91ce9983d802b5229f78f3313770e13f4d2720e9"
 # FIXME cfi; reproduces in e.g. libsoup
 hardening = ["vis", "!cfi"]
 
