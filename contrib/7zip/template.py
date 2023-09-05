9b7a739f800c1776c6b48f64106c23d67550c0dd
psykose <alice@ayaya.dev>
2023-09-05 06:27:35 +0000

    contrib/7zip: new package (23.01)

---
+++ contrib/7zip/template.py
@@ -0,0 +1,63 @@
+pkgname = "7zip"
+pkgver = "23.01"
+pkgrel = 0
+# Alone2: CLI with all format support
+build_wrksrc = "CPP/7zip/Bundles/Alone2"
+build_style = "makefile"
+make_cmd = "gmake"
+make_build_args = [
+    # nonfree rar sdk; uncompressed rar technically still supported
+    "DISABLE_RAR_COMPRESS=1",
+    # silence garbage Werror+Weverything
+    "CFLAGS_WARN_WALL=",
+    "CFLAGS_WARN=",
+]
+make_use_env = True
+hostmakedepends = ["gmake"]
+pkgdesc = "File archiver with a high compression ratio"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.0-only AND BSD-3-Clause"
+url = "https://7-zip.org"
+source = f"https://7-zip.org/a/7z{pkgver.replace('.', '')}-src.tar.xz"
+sha256 = "356071007360e5a1824d9904993e8b2480b51b570e8c9faf7c0f58ebe4bf9f74"
+# FIXME cfi
+hardening = ["vis"]
+
+
+def init_build(self):
+    self.make_build_args += [
+        "-f",
+        # cwd is build type (alone2), -f .mak is toolchain type for some flags
+        "../../cmpl_clang.mak",
+        f"CC={self.get_tool('CC')}",
+        f"CXX={self.get_tool('CXX')}",
+    ]
+
+
+def do_check(self):
+    # no actual tests, so just compress+decompress something as useful sanity
+
+    self.do(
+        # b/c is toolchain clang; b/g is gcc
+        self.chroot_cwd / "b/c/7zz",
+        "a",
+        "-y",
+        "xxx.7z",
+        # XXX: this is self.sources_path but chroot
+        self.chroot_builddir / self.wrksrc / "Asm",
+        self.chroot_builddir / self.wrksrc / "C",
+    )
+
+    self.do(
+        self.chroot_cwd / "b/c/7zz",
+        "x",
+        "-y",
+        "-o/tmp",
+        "xxx.7z",
+    )
+
+
+def do_install(self):
+    self.install_license("../../../../DOC/License.txt")
+    self.install_bin("b/c/7zz")
+    self.install_link("7zz", "usr/bin/7z")
