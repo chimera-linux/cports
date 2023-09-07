6a0451e9b5aaf6d32bd11bf7fbfcfbecb44581a2
q66 <q66@chimera-linux.org>
2023-09-07 17:17:30 +0200

    main/alsa-lib: fix signed overflow in pcm_softvol, enable tests

--- main/alsa-lib/template.py
+++ main/alsa-lib/template.py
@@ -1,6 +1,6 @@
 pkgname = "alsa-lib"
 pkgver = "1.2.10"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = [
     "--disable-alisp",
@@ -17,8 +17,6 @@ license = "LGPL-2.1-or-later"
 url = "https://www.alsa-project.org"
 source = f"{url}/files/pub/lib/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "c86a45a846331b1b0aa6e6be100be2a7aef92efd405cf6bac7eef8174baa920e"
-# tests require stuff we disable
-options = ["!check"]
 
 
 @subpackage("alsa-lib-devel")
