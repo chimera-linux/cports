aa4bff48e420d7bbe35c673b236758bf14f59456
q66 <q66@chimera-linux.org>
2023-09-07 02:23:59 +0200

    main/debianutils: update to 5.12

--- main/debianutils/template.py
+++ main/debianutils/template.py
@@ -1,30 +1,34 @@
 pkgname = "debianutils"
-pkgver = "5.8"
+pkgver = "5.12"
 pkgrel = 0
-build_style = "gnu_configure"
 pkgdesc = "Miscellaneous utilities from Debian"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://tracker.debian.org/pkg/debianutils"
-source = f"$(DEBIAN_SITE)/main/d/debianutils/debianutils_{pkgver}.orig.tar.gz"
-sha256 = "5b086d27eb9063de4d746760d0faeb40d9464fb855fc8a8e7fb93b03efcec622"
+source = f"$(DEBIAN_SITE)/main/d/debianutils/debianutils_{pkgver}.tar.xz"
+sha256 = "d386dad5e5b957d35f75a23a7543d45459718e06ab185896dc0eb5054e6e2e3b"
 hardening = ["vis", "cfi"]
 
 
-def post_install(self):
-    # (add|remove)-shell conflicts with our system
-    # installkernel is not something we want either
-    # which is provided by chimerautils
-    self.rm(self.destdir / "usr/bin/add-shell")
-    self.rm(self.destdir / "usr/bin/remove-shell")
-    self.rm(self.destdir / "usr/bin/update-shells")
-    self.rm(self.destdir / "usr/bin/installkernel")
-    self.rm(self.destdir / "usr/bin/which")
-    self.rm(self.destdir / "usr/bin/tempfile")
-    for f in (self.destdir / "usr/share/man").rglob("*.[18]"):
-        match f.name:
-            case "add-shell.8" | "remove-shell.8" | "update-shells.8" | "installkernel.8" | "which.1" | "tempfile.1":
-                f.unlink()
+def do_build(self):
+    from cbuild.util import compiler
+
+    cfl = [
+        "-DHAVE_GETOPT_H",
+        f'-DPACKAGE_VERSION="{pkgver}"',
+        "-Wall",
+        "-Wextra",
+    ]
+
+    cc = compiler.C(self)
+    cc.invoke(["ischroot.c"], "ischroot", flags=cfl)
+    cc.invoke(["run-parts.c"], "run-parts", flags=cfl)
 
 
-configure_gen = []
+def do_install(self):
+    self.install_bin("ischroot")
+    self.install_man("ischroot.1")
+    self.install_bin("run-parts")
+    self.install_man("run-parts.8")
+    self.install_bin("savelog")
+    self.install_man("savelog.8")
