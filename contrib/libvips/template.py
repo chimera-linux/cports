ada6352a2600f715d62ac9bcc1fa0f7cfc72d6e3
psykose <alice@ayaya.dev>
2023-09-07 15:48:35 +0000

    contrib/libvips: new package (8.14.4)

---
+++ contrib/libvips/template.py
@@ -0,0 +1,49 @@
+pkgname = "libvips"
+pkgver = "8.14.4"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "glib-devel",
+    "gobject-introspection",
+    "meson",
+    "pkgconf",
+]
+makedepends = [
+    "fftw-devel",
+    "giflib-devel",
+    "glib-devel",
+    "gobject-introspection",
+    "libexif-devel",
+    "libexpat-devel",
+    "libheif-devel",
+    "libjpeg-turbo-devel",
+    "libjxl-devel",
+    "libpng-devel",
+    "librsvg-devel",
+    "libtiff-devel",
+    "libwebp-devel",
+    "openexr-devel",
+    "orc-devel",
+    "pango-devel",
+    "zlib-devel",
+]
+pkgdesc = "Fast image processing library"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-or-later"
+url = "https://github.com/libvips/libvips"
+source = f"https://github.com/libvips/libvips/releases/download/v{pkgver}/vips-{pkgver}.tar.xz"
+sha256 = "f6b7d86f6f25528859bd191c7e1159a4d6d0bdc0f46197ae8109f2f9d3cb90c0"
+# FIXME: cfi
+hardening = ["vis"]
+# broken
+options = ["!cross"]
+
+
+@subpackage("libvips-devel")
+def _devel(self):
+    return self.default_devel()
+
+
+@subpackage("libvips-progs")
+def _progs(self):
+    return self.default_progs()
