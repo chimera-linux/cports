257d246f8492c08d1f44d8a892daf563db9382b1
q66 <q66@chimera-linux.org>
2023-09-07 02:06:05 +0200

    main/gnome-shell: update to 44.4

--- main/gnome-shell/template.py
+++ main/gnome-shell/template.py
@@ -1,6 +1,6 @@
 pkgname = "gnome-shell"
-pkgver = "44.3"
-pkgrel = 1
+pkgver = "44.4"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dsystemd=false",
@@ -55,6 +55,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Projects/GnomeShell"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "55694b71c2ee4eaef60d93428003f2eaa4cf00186848f5e36b45cfe506fc31bf"
+sha256 = "1dd51e6ee8d92fbcb95ce6ddf11b9fece88d22622c01015ffa9360169cd41866"
 # tests need libmutter-test
 options = ["!check"]
