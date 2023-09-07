852f7ea8482134fed1d7e8252d927ef55daa66d2
q66 <q66@chimera-linux.org>
2023-09-07 02:01:51 +0200

    main/alsa-ucm-conf: update to 1.2.10

--- main/alsa-ucm-conf/template.py
+++ main/alsa-ucm-conf/template.py
@@ -1,13 +1,14 @@
 pkgname = "alsa-ucm-conf"
-pkgver = "1.2.9"
+pkgver = "1.2.10"
 pkgrel = 0
 pkgdesc = "ALSA Use Case Manager topology files"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/alsa-project/alsa-ucm-conf"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1c40618161bc2738c55aab4dee1d0c82514e257116f296815a799adeaab5e48b"
+sha256 = "00e67c31b60494665f45ce57ca9d2c42421c40fbe0140b5a5648291238e63508"
 
 
 def do_install(self):
+    self.install_license("LICENSE")
     self.install_files("ucm2", "usr/share/alsa")
