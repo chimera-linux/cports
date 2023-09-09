a498b8936f6ec6bca13d78f3c8866060e15607a9
q66 <q66@chimera-linux.org>
2023-09-09 22:39:04 +0200

    main/perl-test-deep: disable check

--- main/perl-test-deep/template.py
+++ main/perl-test-deep/template.py
@@ -11,3 +11,5 @@ license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/release/Test-Deep"
 source = f"$(CPAN_SITE)/Test/Test-Deep-{pkgver}.tar.gz"
 sha256 = "b6591f6ccdd853c7efc9ff3c5756370403211cffe46047f082b1cd1611a84e5f"
+# FIXME isa.t
+options = ["!check"]
