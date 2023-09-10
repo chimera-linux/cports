c10b833f538bb71dcd0633da7c808c8663c1b79a
psykose <alice@ayaya.dev>
2023-09-10 02:50:54 +0000

    contrib/wcm: new package (0.7.5)

---
+++ contrib/wcm/template.py
@@ -0,0 +1,27 @@
+pkgname = "wcm"
+pkgver = "0.7.5"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+]
+makedepends = [
+    "gtk+3-devel",
+    "libxml2-devel",
+    "wayfire-devel",
+    "wayland-protocols",
+    "wf-config-devel",
+]
+pkgdesc = "Wayfire Config Manager"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://wayfire.org"
+source = f"https://github.com/WayfireWM/wcm/releases/download/v{pkgver}/wcm-{pkgver}.tar.xz"
+sha256 = "4b7c847f48e80bad3448961b9900a1f4eed8df101daa033592b60dc892f253c9"
+# vis breaks symbols
+hardening = ["!vis"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
