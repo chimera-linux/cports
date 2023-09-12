24553b262543ca6e6879d2c96e3217f142cd2d54
psykose <alice@ayaya.dev>
2023-09-12 18:00:35 +0000

    main/vulkan-headers: upgrade to 1.3.264

--- main/vulkan-headers/template.py
+++ main/vulkan-headers/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-headers"
-pkgver = "1.3.263"
+pkgver = "1.3.264"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja"]
@@ -10,6 +10,6 @@ url = "https://www.khronos.org/vulkan"
 source = (
     f"https://github.com/KhronosGroup/Vulkan-Headers/archive/v{pkgver}.tar.gz"
 )
-sha256 = "6a240d61965fc02b5861065dbfcd1d25418106ddb9747b99c3014faa794c2e9a"
+sha256 = "bdce6a99f0e5869a341946ee0a92eef270ef9b4b106e82f238aa68aeb2cd4995"
 # no test suite
 options = ["!check"]
