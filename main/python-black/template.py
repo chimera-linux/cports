92c03c1073ba24730c5bb0742ccbcaff6bfb1023
psykose <alice@ayaya.dev>
2023-09-12 17:56:14 +0000

    main/python-black: update to 23.9.1

--- main/python-black/template.py
+++ main/python-black/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-black"
-pkgver = "23.9.0"
+pkgver = "23.9.1"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_env = {"PYTHONPATH": "src"}
@@ -23,7 +23,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://black.readthedocs.io"
 source = f"$(PYPI_SITE)/b/black/black-{pkgver}.tar.gz"
-sha256 = "3511c8a7e22ce653f89ae90dfddaf94f3bb7e2587a245246572d3b9c92adf066"
+sha256 = "24b6b3ff5c6d9ea08a8888f6977eae858e1f340d7260cf56d70a49823236b62d"
 # unpackaged checkdepends
 options = ["!check"]
 
