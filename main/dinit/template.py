c4c45e45725dbf82b5486266dae2a4ec031fcb30
q66 <q66@chimera-linux.org>
2023-09-13 21:17:29 +0200

    main/dinit: switch again to a git rev
    This is only a few revisions ahead of 0.17, but it brings sane
    workdir for commands and env-files.

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -1,7 +1,9 @@
 pkgname = "dinit"
 pkgver = "0.17.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
+# temporary
+_commit = "2b8df1e1d1c1371af1d58086635796a3b0781db3"
 configure_args = ["--syscontrolsocket=/run/dinitctl"]
 configure_gen = []
 make_cmd = "gmake"
@@ -12,7 +14,8 @@ pkgdesc = "Service manager and init system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://davmac.org/projects/dinit"
-source = f"https://github.com/davmac314/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "f3ce6eaaabd571edc16f54907cd6062f22ed5d4b6f9ae6fc1c6533f39469b1f0"
+#source = f"https://github.com/davmac314/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
+source = f"https://github.com/davmac314/{pkgname}/archive/{_commit}.tar.gz"
+sha256 = "39c6b91fd7a1de419a18d7ed7c61b208660ea47e8cb5bb46f7080bf42425f580"
 tool_flags = {"CXXFLAGS": ["-fno-rtti"]}
 hardening = ["vis", "cfi"]
