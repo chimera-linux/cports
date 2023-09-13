d901aa075646b5e134e7e460b9c7330a09f24a40
psykose <alice@ayaya.dev>
2023-09-13 12:30:25 +0000

    contrib/python-magic: new package (0.4.27)

---
+++ contrib/python-magic/template.py
@@ -0,0 +1,29 @@
+pkgname = "python-magic"
+pkgver = "0.4.27"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = ["libmagic"]
+checkdepends = depends + ["python-pytest"]
+pkgdesc = "Python wrapper for libmagic"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/ahupp/python-magic"
+source = (
+    f"https://github.com/ahupp/python-magic/archive/refs/tags/{pkgver}.tar.gz"
+)
+sha256 = "3978a25d43d9a7b8a89ae9d726bd4962fc90dc4f69ae852e399f3c56d4b0bd63"
+
+
+def init_check(self):
+    # required for testsuite
+    self.env["LC_ALL"] = "en_US.UTF-8"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
