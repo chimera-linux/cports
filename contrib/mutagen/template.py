f403ad11df54484fdca15d4928ecd1ff2293026c
psykose <alice@ayaya.dev>
2023-09-13 12:36:00 +0000

    contrib/mutagen: new package (1.47.0)

---
+++ contrib/mutagen/template.py
@@ -0,0 +1,19 @@
+pkgname = "mutagen"
+pkgver = "1.47.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "Audio tagger implemented in Python"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later"
+url = "https://github.com/quodlibet/mutagen"
+source = f"https://github.com/quodlibet/mutagen/archive/refs/tags/release-{pkgver}.tar.gz"
+sha256 = "f7489e19d375c31ba1962ab19e11eca8b9f86f05bfd99cef467f8dd875d8941e"
+# missing python-hypothesis
+options = ["!check"]
