2fa4f03df9a4adce9bc83d8c47b6f1b1202f162b
psykose <alice@ayaya.dev>
2023-09-17 15:18:22 +0000

    contrib/python-argcomplete: update to 3.1.2

--- contrib/python-argcomplete/template.py
+++ contrib/python-argcomplete/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-argcomplete"
-pkgver = "3.1.1"
+pkgver = "3.1.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -17,7 +17,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/kislyuk/argcomplete"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "8da1c3e591234df5ea4e741efae090fd34f1dd8cc220215ab31abd2b22b6aaa3"
+sha256 = "af17e62c6d65076de879b985010155a80ab04e0ae2b60a9f87fa39b012e67e3e"
 # missing pexpect
 options = ["!check"]
 
