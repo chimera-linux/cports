a3d1f729a66cf9ec800e5a4598a2d98709778f72
psykose <alice@ayaya.dev>
2023-09-18 16:15:39 +0000

    main/json-glib: update to 1.8.0

--- main/json-glib/template.py
+++ main/json-glib/template.py
@@ -1,5 +1,5 @@
 pkgname = "json-glib"
-pkgver = "1.6.6"
+pkgver = "1.8.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=enabled"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/action/show/Projects/JsonGlib"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "96ec98be7a91f6dde33636720e3da2ff6ecbb90e76ccaa49497f31a6855a490e"
+sha256 = "97ef5eb92ca811039ad50a65f06633f1aae64792789307be7170795d8b319454"
 
 
 @subpackage("json-glib-devel")
