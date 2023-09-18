8d781c56fdd4e8f9c9e822b0d1b7e24a00920fd3
psykose <alice@ayaya.dev>
2023-09-18 21:06:00 +0000

    contrib/dconf-editor: update to 45.0.1

--- contrib/dconf-editor/template.py
+++ contrib/dconf-editor/template.py
@@ -1,9 +1,10 @@
 pkgname = "dconf-editor"
-pkgver = "43.0"
+pkgver = "45.0.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "dconf-devel",
+    "desktop-file-utils",
     "gettext",
     "glib-devel",
     "libhandy-devel",
@@ -22,7 +23,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/dconf-editor"
 source = f"$(GNOME_SITE)/dconf-editor/{pkgver[:-pkgver.rfind('.')]}/dconf-editor-{pkgver}.tar.xz"
-sha256 = "935a3c2dd76cc2a93cd5aee9a54d3947fb111eb396f4b63dc5f0ba8f8d099136"
+sha256 = "1180297678eedae6217cc514a2638c187d2f1d1ef2720cb9079b740c429941dd"
 
 # vala broken
 tool_flags = {"CFLAGS": ["-Wno-incompatible-function-pointer-types"]}
