f60bbd1c9ca170d26d034ea8d587ca499325f092
psykose <alice@ayaya.dev>
2023-09-19 00:52:31 +0000

    contrib/virglrenderer: update to 1.0.0

--- contrib/virglrenderer/template.py
+++ contrib/virglrenderer/template.py
@@ -1,5 +1,5 @@
 pkgname = "virglrenderer"
-pkgver = "0.10.4"
+pkgver = "1.0.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "eater <=@eater.me>"
 license = "MIT"
 url = "https://docs.mesa3d.org/drivers/virgl"
 source = f"https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/{pkgver}/virglrenderer-{pkgver}.tar.gz"
-sha256 = "da3fdc59821f645632d986482594410614a95c0c7c703b25bfb3473bd6674214"
+sha256 = "130c417bfcc8d35063245322609fd310e981a33e2a6c73beac7fb98c4beed8e6"
 
 
 def post_install(self):
