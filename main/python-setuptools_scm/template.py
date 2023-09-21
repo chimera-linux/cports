97aea901d8964e254279ae9bab86e3ca25635a1b
psykose <alice@ayaya.dev>
2023-09-21 03:02:46 +0000

    main/python-setuptools_scm: update to 8.0.1

--- main/python-setuptools_scm/template.py
+++ main/python-setuptools_scm/template.py
@@ -1,21 +1,25 @@
 pkgname = "python-setuptools_scm"
-pkgver = "7.1.0"
+pkgver = "8.0.1"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools", "python-tomli", "python-packaging"]
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-packaging",
+    "python-setuptools",
+    "python-wheel",
+]
 depends = [
     "python",
-    "python-setuptools",
-    "python-tomli",
     "python-packaging",
-    "python-typing_extensions",
+    "python-setuptools",
 ]
 pkgdesc = "Manage Python package versions with SCM tags"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pypa/setuptools_scm"
-source = f"$(PYPI_SITE)/s/setuptools_scm/setuptools_scm-{pkgver}.tar.gz"
-sha256 = "6c508345a771aad7d56ebff0e70628bf2b0ec7573762be9960214730de278f27"
+source = f"$(PYPI_SITE)/s/setuptools-scm/setuptools-scm-{pkgver}.tar.gz"
+sha256 = "e69bf0b8265fdc8f4e070c98235b1b0816ffa8b7f91153400404bf68496012e3"
 # tests fail when the package is not installed
 options = ["!check"]
 
