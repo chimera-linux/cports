99431cc4e1abfa5f6caa6041b9aa96c9ed1a7bf4
psykose <alice@ayaya.dev>
2023-09-27 21:20:05 +0000

    main/fluidsynth: update to 2.3.4

--- main/fluidsynth/template.py
+++ main/fluidsynth/template.py
@@ -1,12 +1,11 @@
 pkgname = "fluidsynth"
-pkgver = "2.3.3"
+pkgver = "2.3.4"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DLIB_SUFFIX=",
     "-DDEFAULT_SOUNDFONT=/usr/share/soundfonts/default.sf2",
 ]
-make_check_target = "check"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = [
     "glib-devel",
@@ -23,8 +22,14 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.fluidsynth.org"
 source = f"https://github.com/FluidSynth/{pkgname}/archive/v{pkgver}.tar.gz"
-sha256 = "321f7d3f72206b2522f30a1cb8ad1936fd4533ffc4d29dd335b1953c9fb371e6"
+sha256 = "1529ef5bc3b9ef3adc2a7964505912f7305103e269e50cc0316f500b22053ac9"
 hardening = ["vis", "cfi"]
+
+
+def do_check(self):
+    # the tests only get built+ran on the check target and don't get built ahead
+    # of time otherwise for ctest
+    self.make.check()
 
 
 @subpackage("libfluidsynth")
