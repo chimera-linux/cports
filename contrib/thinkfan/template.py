5fa1a296a84ae8d1534619b26bb0af85619a1ed4
Val Packett <val@packett.cool>
2023-09-27 02:36:31 -0300

    contrib/thinkfan: new package (1.3.1)

---
+++ contrib/thinkfan/template.py
@@ -0,0 +1,20 @@
+pkgname = "thinkfan"
+pkgver = "1.3.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = ["yaml-cpp-devel"]
+pkgdesc = "Simple fan control program"
+maintainer = "Val Packett <val@packett.cool>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/vmatare/thinkfan"
+source = f"https://github.com/vmatare/thinkfan/archive/{pkgver}.tar.gz"
+sha256 = "9466c8c82b7c4333b280fa66445ab26185ffbb4aada6bcb4a164eed742f8d78c"
+hardening = ["vis"]
+# no tests
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("COPYING")
+    self.install_service(self.files_path / "thinkfan")
