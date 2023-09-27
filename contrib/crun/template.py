edf43b49db7b05a4547646b8257b1dd64be7df24
psykose <alice@ayaya.dev>
2023-09-27 07:06:50 +0000

    contrib/crun: update to 1.9.2

--- contrib/crun/template.py
+++ contrib/crun/template.py
@@ -1,5 +1,5 @@
 pkgname = "crun"
-pkgver = "1.9"
+pkgver = "1.9.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-systemd"]
@@ -27,7 +27,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://github.com/containers/crun"
 source = f"{url}/releases/download/{pkgver}/crun-{pkgver}.tar.xz"
-sha256 = "7e35280cfa1a646b4ebdc94cc434035ab51f36a763c6268acc6aa7bbfd6ed21b"
+sha256 = "0df37442265fc39b6aabd29a00240ff0ebd3baee872d2b2f62d8e72b1e646de0"
 
 
 def post_install(self):
