309e0ebdf21592a3864ba554a2c47454cdec2371
psykose <alice@ayaya.dev>
2023-09-27 21:27:55 +0000

    contrib/yt-dlp: update to 2023.09.24

--- contrib/yt-dlp/template.py
+++ contrib/yt-dlp/template.py
@@ -1,5 +1,5 @@
 pkgname = "yt-dlp"
-pkgver = "2023.07.06"
+pkgver = "2023.09.24"
 pkgrel = 0
 build_style = "python_module"
 make_check_args = ["-k", "not download"]
@@ -11,6 +11,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Unlicense"
 url = "https://github.com/yt-dlp/yt-dlp"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "dcb9dd73745b647756d3b23b705514affe456253e482f0f0a1b5be84e9e29208"
+sha256 = "15e88073dc257527375aea5f0622cb0b73718a035d0731f086a680a8e2d566e9"
 # missing checkdepends
 options = ["!check"]
