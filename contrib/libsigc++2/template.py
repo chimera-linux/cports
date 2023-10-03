0432ce9fb48a393186eadef0349bdd563777e945
q66 <q66@chimera-linux.org>
2023-10-03 18:47:52 +0200

    contrib/libsigc++2: update to 2.12.1

--- contrib/libsigc++2/template.py
+++ contrib/libsigc++2/template.py
@@ -1,5 +1,5 @@
 pkgname = "libsigc++2"
-pkgver = "2.12.0"
+pkgver = "2.12.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dbuild-examples=false", "-Dwarnings=max"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://libsigcplusplus.github.io/libsigcplusplus"
 source = f"$(GNOME_SITE)/libsigc++/{pkgver[:-2]}/libsigc++-{pkgver}.tar.xz"
-sha256 = "1c466d2e64b34f9b118976eb21b138c37ed124d0f61497df2a90ce6c3d9fa3b5"
+sha256 = "a9dbee323351d109b7aee074a9cb89ca3e7bcf8ad8edef1851f4cf359bd50843"
 
 
 @subpackage("libsigc++2-devel")
