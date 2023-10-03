63cdddaddcc7057e6ab9c40fed05e0b325a44608
q66 <q66@chimera-linux.org>
2023-10-03 18:36:58 +0200

    main/sushi: update to 45.0

--- main/sushi/template.py
+++ main/sushi/template.py
@@ -1,5 +1,5 @@
 pkgname = "sushi"
-pkgver = "44.2"
+pkgver = "45.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -29,4 +29,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/sushi"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "6c002fe0aea19027ba448b5aec94d5cd753c9752f996ee033152428738ea43e9"
+sha256 = "6a5f14b05a3471fe43853cc2b0e1ae5484d7f9f86f7ea376179829b9bf4ac1dd"
