5b8781416137d318609b4a677685e0a1f13231af
q66 <q66@chimera-linux.org>
2023-10-03 17:52:12 +0200

    main/gtksourceview: update to 5.10.0

--- main/gtksourceview/template.py
+++ main/gtksourceview/template.py
@@ -1,5 +1,5 @@
 pkgname = "gtksourceview"
-pkgver = "5.8.0"
+pkgver = "5.10.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=enabled", "-Dvapi=true", "-Dgtk_doc=false"]
@@ -25,7 +25,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Projects/GtkSourceView"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "110dd4c20def21886fbf777298fe0ef8cc2ad6023b8f36c7424411a414818933"
+sha256 = "b38a3010c34f59e13b05175e9d20ca02a3110443fec2b1e5747413801bc9c23f"
 # needs graphical environment
 # lto results in broken mouse scrolling in gnome-text-editor
 options = ["!check", "!cross", "!lto"]
