2f97e5ab210cf8fb24df5d5a76d8f99ad5d0c748
q66 <q66@chimera-linux.org>
2023-10-03 18:12:37 +0200

    main/tecla: new package (45.0)

---
+++ main/tecla/template.py
@@ -0,0 +1,21 @@
+pkgname = "tecla"
+pkgver = "45.0"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+    "gettext",
+]
+makedepends = [
+    "gtk4-devel",
+    "libadwaita-devel",
+    "libxkbcommon-devel",
+]
+depends = ["desktop-file-utils"]
+pkgdesc = "GNOME keyboard layout viewer"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://gitlab.gnome.org/GNOME/tecla"
+source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "5c02bb4019b1cffb5663da6107503eff853836a8783dd4705dd04a49f7adc25b"
