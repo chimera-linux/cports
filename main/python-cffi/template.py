231ad2b38595e11d5241e1e9b96a9d89ba682d11
psykose <alice@ayaya.dev>
2023-10-04 09:42:21 +0000

    main/python-cffi: update to 1.16.0

--- main/python-cffi/template.py
+++ main/python-cffi/template.py
@@ -1,8 +1,14 @@
 pkgname = "python-cffi"
-pkgver = "1.15.1"
+pkgver = "1.16.0"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools", "libffi-devel"]
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+    "libffi-devel",
+]
 makedepends = ["python-devel", "libffi-devel"]
 depends = ["python-pycparser"]
 checkdepends = ["python-pycparser", "python-pytest"]
@@ -11,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://cffi.readthedocs.io"
 source = f"$(PYPI_SITE)/c/cffi/cffi-{pkgver}.tar.gz"
-sha256 = "d400bfb9a37b1351253cb402671cea7e89bdecc294e8016a707f6d1d8ac934f9"
+sha256 = "bcb3ef43e58665bbda2fb198698fcae6776483e0c4a631aa5647806c25e02cc0"
 # do_check needs fixing up more
 options = ["!check"]
 
