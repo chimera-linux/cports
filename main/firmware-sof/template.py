c7988a51ee581c4671dac1f9f726be07853e91bd
psykose <alice@ayaya.dev>
2023-10-04 09:54:11 +0000

    main/firmware-sof: update to 2023.09

--- main/firmware-sof/template.py
+++ main/firmware-sof/template.py
@@ -1,5 +1,5 @@
 pkgname = "firmware-sof"
-pkgver = "2.2.6"
+pkgver = "2023.09"
 pkgrel = 0
 archs = ["x86_64"]
 install_if = [f"base-firmware-sof={pkgver}-r{pkgrel}"]
@@ -7,16 +7,19 @@ pkgdesc = "Sound Open Firmware"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://thesofproject.github.io/latest/index.html"
-source = f"https://github.com/thesofproject/sof-bin/releases/download/v{pkgver}/sof-bin-v{pkgver}.tar.gz"
-sha256 = "9322c2a7636d02845c3b26984d58ab8f78d63ff4c766d084c3196a585e000905"
+source = f"https://github.com/thesofproject/sof-bin/releases/download/v{pkgver}/sof-bin-{pkgver}.tar.gz"
+sha256 = "4bcc75c6642348e1a516db7ff9d050b6ea1b8672983542c6952562fa8c1c7b63"
 options = ["!strip", "foreignelf"]
 
 
 def do_install(self):
-    self.install_files(f"sof-v{pkgver}", "usr/lib/firmware/intel")
-    self.install_files(f"sof-tplg-v{pkgver}", "usr/lib/firmware/intel")
-    self.install_link(f"sof-v{pkgver}", "usr/lib/firmware/intel/sof")
-    self.install_link(f"sof-tplg-v{pkgver}", "usr/lib/firmware/intel/sof-tplg")
+    for folder in ["sof", "sof-ace-tplg", "sof-ipc4", "sof-tplg"]:
+        self.install_files(
+            folder, "usr/lib/firmware/intel", name=f"{folder}-v{pkgver}"
+        )
+        self.install_link(
+            f"{folder}-v{pkgver}", f"usr/lib/firmware/intel/{folder}"
+        )
     self.install_license("LICENCE.NXP")
     self.install_license("LICENCE.Intel")
 
