8b18ba096b17b21d30ad9028cff3e47e53bc928f
psykose <alice@ayaya.dev>
2023-10-04 09:41:04 +0000

    main/python-babel: update to 2.13.0

--- main/python-babel/template.py
+++ main/python-babel/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-babel"
-pkgver = "2.12.1"
+pkgver = "2.13.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://babel.pocoo.org"
 source = f"$(PYPI_SITE)/B/Babel/Babel-{pkgver}.tar.gz"
-sha256 = "cc2d99999cd01d44420ae725a21c9e3711b3aadc7976d6147f622d8581963455"
+sha256 = "04c3e2d28d2b7681644508f836be388ae49e0cfe91465095340395b60d00f210"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
