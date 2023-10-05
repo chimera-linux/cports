f0bfddfc2026b1d900dd9b94c2f8eb9765f163e8
psykose <alice@ayaya.dev>
2023-10-05 07:56:19 +0000

    main/libxrandr: update to 1.5.4

--- main/libxrandr/template.py
+++ main/libxrandr/template.py
@@ -1,16 +1,16 @@
 pkgname = "libxrandr"
-pkgver = "1.5.3"
+pkgver = "1.5.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-malloc0returnsnull"]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf", "xorg-util-macros"]
 makedepends = ["xorgproto", "libxext-devel", "libxrender-devel"]
 pkgdesc = "X RandR Library from X.org"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXrandr-{pkgver}.tar.gz"
-sha256 = "3ad316c1781fe2fe22574b819e81f0eff087a8560377f521ba932238b41b251f"
+sha256 = "c72c94dc3373512ceb67f578952c5d10915b38cc9ebb0fd176a49857b8048e22"
 
 
 def post_install(self):
@@ -20,6 +20,3 @@ def post_install(self):
 @subpackage("libxrandr-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
