9f28304922a9e0e75c4e5bc7454f98f4c13cc178
psykose <alice@ayaya.dev>
2023-10-07 14:18:41 +0000

    contrib/wayfire: update to 0.8.0

--- contrib/wayfire/template.py
+++ contrib/wayfire/template.py
@@ -1,6 +1,6 @@
 pkgname = "wayfire"
-pkgver = "0.7.5"
-pkgrel = 1
+pkgver = "0.8.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Duse_system_wfconfig=enabled",
@@ -16,6 +16,7 @@ makedepends = [
     "cairo-devel",
     "glm",
     "libxml2-devel",
+    "nlohmann-json",
     "pango-devel",
     "wayland-protocols",
     "wf-config-devel",
@@ -26,7 +27,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://wayfire.org"
 source = f"https://github.com/WayfireWM/wayfire/releases/download/v{pkgver}/wayfire-{pkgver}.tar.xz"
-sha256 = "f2e3184e72fe7999488fbba10bd38c29350b447489f02961aab5fa8438698b5c"
+sha256 = "6e6af885c08822e3a0b1fd748e1ee75e29bc000e376f6613b26c564f8cbc2baf"
 # vis breaks symbols
 hardening = ["!vis"]
 
