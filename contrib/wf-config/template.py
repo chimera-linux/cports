e23ffcd60f156eacf71967ed97fd61749f63d9cf
psykose <alice@ayaya.dev>
2023-10-07 14:18:45 +0000

    contrib/wf-config: update to 0.8.0

--- contrib/wf-config/template.py
+++ contrib/wf-config/template.py
@@ -1,5 +1,5 @@
 pkgname = "wf-config"
-pkgver = "0.7.1"
+pkgver = "0.8.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -17,7 +17,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://wayfire.org"
 source = f"https://github.com/WayfireWM/wf-config/releases/download/v{pkgver}/wf-config-{pkgver}.tar.xz"
-sha256 = "7d2285adeaca0ff6e36e6ef450564ab02c8e26ba5da3b169257c5bf061f7e5f0"
+sha256 = "16988f63fd054b446d4feac024aafd4cc962193d6d6dfde90e6d6169c23443af"
 # vis breaks syumbols
 hardening = ["!vis"]
 # missing doctest
