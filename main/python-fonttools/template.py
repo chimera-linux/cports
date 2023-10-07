c6232dd4784ddbea6f41a04f4ff93c561c36c5fd
psykose <alice@ayaya.dev>
2023-10-07 14:37:14 +0000

    main/python-fonttools: update to 4.43.1

--- main/python-fonttools/template.py
+++ main/python-fonttools/template.py
@@ -1,15 +1,20 @@
 pkgname = "python-fonttools"
-pkgver = "4.43.0"
+pkgver = "4.43.1"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools"]
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
 depends = ["python"]
 pkgdesc = "Library to manipulate font files from Python"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT AND OFL-1.1 AND BSD-3-Clause AND Apache-2.0"
 url = "https://github.com/fonttools/fonttools"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "234e99158510835e9baefbb100e37fb6402f41543db9bd898b164482bbcb75d3"
+sha256 = "20a7236c186c80b9cdf111aba9b233b204fecce1d7339bebe7868e877a6ba938"
 # unpackaged deps
 options = ["!check"]
 
