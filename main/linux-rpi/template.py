b634a92a09e8fc21b87e7b23edc2549ae131873c
q66 <q66@chimera-linux.org>
2023-10-09 01:49:10 +0200

    main/linux-rpi: update to 6.1.55

--- main/linux-rpi/template.py
+++ main/linux-rpi/template.py
@@ -1,12 +1,12 @@
 # update linux-rpi-zfs-bin when bumping
 pkgname = "linux-rpi"
-pkgver = "6.1.53"
-pkgrel = 1
+pkgver = "6.1.55"
+pkgrel = 0
 archs = ["aarch64"]
 make_dir = "build"
 # necessary for efistub
 make_env = {"CBUILD_BYPASS_STRIP_WRAPPER": "1"}
-_commit = "e079555a4c68356e58249cfc041b28f6eb455bd5"
+_commit = "450d617786926b27c25e930241efbd2e37d66bb8"
 hostmakedepends = ["base-kernel-devel"]
 depends = ["base-kernel"]
 provides = ["linux"]
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://github.com/raspberrypi/linux"
 source = f"{url}/archive/{_commit}.tar.gz"
-sha256 = "32a2fb023a5e81f9fd5ed89db587007d733394fc1969271287a6610c7bb28823"
+sha256 = "a34c46acb2b6ccd09ad8138be3962fe3fe07249ad4c2120d060352a52f4bdb4a"
 # no meaningful checking to be done
 options = [
     "!check",
