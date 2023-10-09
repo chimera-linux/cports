7b3fca64a7f9b169f6ada3d2d7f0a10b568f941b
psykose <alice@ayaya.dev>
2023-10-09 06:45:10 +0000

    main/c-ares: update to 1.20.1

--- main/c-ares/template.py
+++ main/c-ares/template.py
@@ -1,5 +1,5 @@
 pkgname = "c-ares"
-pkgver = "1.19.1"
+pkgver = "1.20.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "libtool"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://c-ares.haxx.se"
 source = f"https://c-ares.haxx.se/download/{pkgname}-{pkgver}.tar.gz"
-sha256 = "321700399b72ed0e037d0074c629e7741f6b2ec2dda92956abe3e9671d3e268e"
+sha256 = "de24a314844cb157909730828560628704f4f896d167dd7da0fa2fb93ea18b10"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
 # does not like the sandbox
