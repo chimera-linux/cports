fdfcb9c4f0f7fc77e74f57f0fce6ff793c3466f8
psykose <alice@ayaya.dev>
2023-10-11 14:32:38 +0000

    contrib/neovim: update to 0.9.4

--- contrib/neovim/template.py
+++ contrib/neovim/template.py
@@ -1,6 +1,6 @@
 # nb: neovim requires either lua5.1 or luaJIT (a mess)
 pkgname = "neovim"
-pkgver = "0.9.2"
+pkgver = "0.9.4"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -35,7 +35,7 @@ maintainer = "yopito <pierre.bourgin@free.fr>"
 license = "Apache-2.0 AND custom:Vim"
 url = "https://neovim.io"
 source = f"https://github.com/neovim/neovim/archive/v{pkgver}.tar.gz"
-sha256 = "06b8518bad4237a28a67a4fbc16ec32581f35f216b27f4c98347acee7f5fb369"
+sha256 = "148356027ee8d586adebb6513a94d76accc79da9597109ace5c445b09d383093"
 # hardening: visibility is needed for "nvim --api-info"
 # testing unchecked yet (via "make test", see test/README.md)
 options = ["!check"]
