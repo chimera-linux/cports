158f4813abeea9b56145f5acfe1f529d79f88d11
psykose <alice@ayaya.dev>
2023-10-12 09:18:44 +0000

    main/shared-mime-info: update to 2.3

--- main/shared-mime-info/template.py
+++ main/shared-mime-info/template.py
@@ -1,5 +1,5 @@
 pkgname = "shared-mime-info"
-pkgver = "2.2"
+pkgver = "2.3"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dupdate-mimedb=false"]
@@ -11,4 +11,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://freedesktop.org/wiki/Software/shared-mime-info"
 source = f"https://gitlab.freedesktop.org/xdg/{pkgname}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "bcf5d552318136cf7b3ae259975f414fbcdc9ebce000c87cf1f0901ff14e619f"
+sha256 = "78eb7d0d6874e2116649067100d72e0d363eb6a51227797140dad3bd5643e6a9"
