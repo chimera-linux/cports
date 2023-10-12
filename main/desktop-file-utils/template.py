d3f13667d06d3415122c9c90839704e2ad36cfe8
psykose <alice@ayaya.dev>
2023-10-12 09:11:53 +0000

    main/desktop-file-utils: update to 0.27

--- main/desktop-file-utils/template.py
+++ main/desktop-file-utils/template.py
@@ -1,5 +1,5 @@
 pkgname = "desktop-file-utils"
-pkgver = "0.26"
+pkgver = "0.27"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -10,5 +10,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://freedesktop.org/wiki/Software/desktop-file-utils"
 source = f"$(FREEDESKTOP_SITE)/{pkgname}/releases/{pkgname}-{pkgver}.tar.xz"
-sha256 = "b26dbde79ea72c8c84fb7f9d870ffd857381d049a86d25e0038c4cef4c747309"
+sha256 = "a0817df39ce385b6621880407c56f1f298168c040c2032cedf88d5b76affe836"
 hardening = ["vis", "cfi"]
