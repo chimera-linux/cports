f47cef74524a0c1c201ce70aca58ca91d3d1c869
psykose <alice@ayaya.dev>
2023-10-12 09:15:11 +0000

    main/libnotify: update to 0.8.3

--- main/libnotify/template.py
+++ main/libnotify/template.py
@@ -1,5 +1,5 @@
 pkgname = "libnotify"
-pkgver = "0.8.2"
+pkgver = "0.8.3"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -26,7 +26,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libnotify"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "c5f4ed3d1f86e5b118c76415aacb861873ed3e6f0c6b3181b828cf584fc5c616"
+sha256 = "ee8f3ef946156ad3406fdf45feedbdcd932dbd211ab4f16f75eba4f36fb2f6c0"
 
 
 @subpackage("libnotify-devel")
