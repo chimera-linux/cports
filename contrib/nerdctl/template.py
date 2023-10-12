0f750d6723bc9a18d688768c540ae36a758809a8
psykose <alice@ayaya.dev>
2023-10-12 03:18:04 +0000

    contrib/nerdctl: update to 1.6.1

--- contrib/nerdctl/template.py
+++ contrib/nerdctl/template.py
@@ -1,6 +1,6 @@
 pkgname = "nerdctl"
-pkgver = "1.6.0"
-pkgrel = 1
+pkgver = "1.6.1"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/nerdctl"]
 hostmakedepends = ["go"]
@@ -10,7 +10,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/containerd/nerdctl"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "a369b1c517d9c3d53d00b29633a6176a05811214a44dd25d339c32cc6a901579"
+sha256 = "853ef640dcf9c742f9aba505f83fd8c6dc02e77cc39f992e75ff0531c1ed7075"
 # objcopy fails to split on ppc
 # can't run tests inside namespaces
 options = ["!debug", "!check"]
