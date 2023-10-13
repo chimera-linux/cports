1f95269000a955bfb0daa113f31cc85fd0d0b929
psykose <alice@ayaya.dev>
2023-10-13 14:40:26 +0000

    contrib/adw-gtk3: update to 5.0

--- contrib/adw-gtk3/template.py
+++ contrib/adw-gtk3/template.py
@@ -1,5 +1,5 @@
 pkgname = "adw-gtk3"
-pkgver = "4.9"
+pkgver = "5.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -11,4 +11,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lassekongo83/adw-gtk3"
 source = f"https://github.com/lassekongo83/adw-gtk3/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "4ca2082cb5c7fe6ece1a060b9d1952fbae338529ccbcb9fe40bc554bd0a04048"
+sha256 = "1c737e21eaa9fd43369ca16ee5f9352a357a5e87a4b60aa1802a6a7f2bc648b9"
