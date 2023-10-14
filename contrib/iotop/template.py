92cd5f93d8ce90150a1970155f03051c87e08210
psykose <alice@ayaya.dev>
2023-10-14 19:50:33 +0000

    contrib/iotop: update to 1.25

--- contrib/iotop/template.py
+++ contrib/iotop/template.py
@@ -1,5 +1,5 @@
 pkgname = "iotop"
-pkgver = "1.24"
+pkgver = "1.25"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -16,7 +16,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/Tomas-M/iotop"
 source = f"https://github.com/Tomas-M/iotop/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "96921047354ea988349c730f88accd06bb10ced04038509952095f5654d2415a"
+sha256 = "51f72021c06044013a9502127160c01f2499ec06dadc4e8a2538da8162676386"
 # FIXME cfi
 hardening = ["vis"]
 # no tests
