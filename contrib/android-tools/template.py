f5216bb0c1e366421030cf76c040afaf231df8eb
q66 <q66@chimera-linux.org>
2023-10-15 00:07:34 +0200

    contrib/android-tools: fix build

--- contrib/android-tools/template.py
+++ contrib/android-tools/template.py
@@ -1,9 +1,9 @@
 pkgname = "android-tools"
 pkgver = "34.0.4"
 pkgrel = 0
-build_style = "cmake"
 # only supports specific little-endian archs, particularly in boringssl
 archs = ["x86_64", "aarch64", "ppc64le", "riscv64"]
+build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "perl", "go", "pkgconf", "protobuf"]
 makedepends = [
     "brotli-devel",
