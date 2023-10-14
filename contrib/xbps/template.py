b18e0a306a39dbdd5dfdf800a100511e0672b313
q66 <q66@chimera-linux.org>
2023-10-14 16:14:05 +0200

    contrib/xbps: update to 0.59.2

--- contrib/xbps/template.py
+++ contrib/xbps/template.py
@@ -1,6 +1,6 @@
 pkgname = "xbps"
-pkgver = "0.59.1"
-pkgrel = 1
+pkgver = "0.59.2"
+pkgrel = 0
 build_style = "configure"
 configure_args = [
     "--prefix=/usr",
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause AND BSD-3-Clause AND MIT"
 url = "https://github.com/void-linux/xbps"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "0cbd8d5f23a62047c75974bca21da9f004a94efffd7f37c68562a8dbc869fb2a"
+sha256 = "a6607e83fcd654a0ae846d729e43fefd8da9a61323e91430f884caf895b4f59b"
 # unpackaged checkdepends
 options = ["!check"]
 
