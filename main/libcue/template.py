e5ac5007ec3d8dc7461f320776401beef7125b3b
psykose <alice@ayaya.dev>
2023-10-14 05:21:33 +0000

    main/libcue: update to 2.3.0

--- main/libcue/template.py
+++ main/libcue/template.py
@@ -1,5 +1,5 @@
 pkgname = "libcue"
-pkgver = "2.2.1"
+pkgver = "2.3.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND BSD-2-Clause"
 url = "https://github.com/lipnitsk/libcue"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "f27bc3ebb2e892cd9d32a7bee6d84576a60f955f29f748b9b487b173712f1200"
+sha256 = "cc1b3a65c60bd88b77a1ddd1574042d83cf7cc32b85fe9481c99613359eb7cfe"
 hardening = ["vis", "cfi"]
 
 
