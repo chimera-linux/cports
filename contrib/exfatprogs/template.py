bdc354600053e4a7ed56195424eee497c6f2f4cf
Val Packett <val@packett.cool>
2023-10-14 14:03:54 -0300

    contrib/exfatprogs: correct license
    Closes https://github.com/chimera-linux/cports/pull/766

--- contrib/exfatprogs/template.py
+++ contrib/exfatprogs/template.py
@@ -1,6 +1,6 @@
 pkgname = "exfatprogs"
 pkgver = "1.2.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 make_cmd = "gmake"
 make_dir = "."
@@ -12,11 +12,7 @@ hostmakedepends = [
 makedepends = ["linux-headers"]
 pkgdesc = "ExFAT filesystem utilities"
 maintainer = "Val Packett <val@packett.cool>"
-license = "MIT"
+license = "GPL-2.0-only"
 url = "https://github.com/exfatprogs/exfatprogs"
 source = f"{url}/releases/download/{pkgver}/exfatprogs-{pkgver}.tar.xz"
 sha256 = "a6f3b1fb4bd37835c8f8cb421aac4eb75b880a51342b29850c4063973162227b"
-
-
-def post_install(self):
-    self.install_license("COPYING")
