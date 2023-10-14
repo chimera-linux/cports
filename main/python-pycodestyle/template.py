73547137ee5ebdbf208c3c49948d4d2e22798f44
psykose <alice@ayaya.dev>
2023-10-14 05:15:28 +0000

    main/python-pycodestyle: update to 2.11.1

--- main/python-pycodestyle/template.py
+++ main/python-pycodestyle/template.py
@@ -1,8 +1,13 @@
 pkgname = "python-pycodestyle"
-pkgver = "2.11.0"
+pkgver = "2.11.1"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools"]
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
 depends = ["python"]
 checkdepends = ["python-pytest"]
 pkgdesc = "Python style guide checker"
@@ -10,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/PyCQA/pycodestyle"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "757a3dba55dce2ae8b01fe7b46c20cd1e4c0fe794fe6119bce66b942f35e2db0"
+sha256 = "a01fdd890c6472eebc32e8baf21e29173c35776e765c64cc83ccd09b99dc5399"
 
 
 def post_install(self):
