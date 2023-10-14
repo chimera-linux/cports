bc8762629549dc559a183a4c7ebfb8efd3ae4038
q66 <q66@chimera-linux.org>
2023-10-15 00:06:20 +0200

    contrib/android-tools: fix ppc64le build and restrict archs

--- contrib/android-tools/template.py
+++ contrib/android-tools/template.py
@@ -2,6 +2,8 @@ pkgname = "android-tools"
 pkgver = "34.0.4"
 pkgrel = 0
 build_style = "cmake"
+# only supports specific little-endian archs, particularly in boringssl
+archs = ["x86_64", "aarch64", "ppc64le", "riscv64"]
 hostmakedepends = ["cmake", "ninja", "perl", "go", "pkgconf", "protobuf"]
 makedepends = [
     "brotli-devel",
