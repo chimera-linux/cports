63c1ce18a55c6133bf4c75226ab1a054c4759ff9
psykose <alice@ayaya.dev>
2023-10-15 17:56:29 +0000

    main/libmbim: update to 1.30.0

--- main/libmbim/template.py
+++ main/libmbim/template.py
@@ -1,28 +1,26 @@
 pkgname = "libmbim"
-pkgver = "1.26.4"
+pkgver = "1.30.0"
 pkgrel = 0
-build_style = "gnu_configure"
-configure_args = ["--disable-static", "--enable-introspection"]
-make_cmd = "gmake"
+build_style = "meson"
+configure_args = ["-Dintrospection=true"]
 hostmakedepends = [
-    "gmake",
-    "pkgconf",
+    "bash-completion",
     "glib-devel",
-    "libgudev-devel",
     "gobject-introspection",
+    "help2man",
+    "libgudev-devel",
+    "meson",
+    "pkgconf",
 ]
 makedepends = ["glib-devel", "libgudev-devel", "linux-headers"]
 pkgdesc = "MBIM modem protocol helper library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://www.freedesktop.org/wiki/Software/libmbim"
-source = f"$(FREEDESKTOP_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "f688cec4c4586a17575f5e327448ce62f2000ef6a07c9e4589873d4a68568ad9"
+source = f"https://gitlab.freedesktop.org/mobile-broadband/libmbim/-/archive/{pkgver}/libmbim-{pkgver}.tar.gz"
+sha256 = "cfc729d23b9bf699b23a7ef2f5d732d6eff96234e31fed36b778771a6e3d3ee5"
 
 
 @subpackage("libmbim-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
