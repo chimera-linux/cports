9391670ed5e53c3f5f2444d59d3b6d979ca84162
psykose <alice@ayaya.dev>
2023-10-15 17:56:11 +0000

    main/libqmi: update to 1.34.0

--- main/libqmi/template.py
+++ main/libqmi/template.py
@@ -1,35 +1,31 @@
 pkgname = "libqmi"
-pkgver = "1.30.8"
+pkgver = "1.34.0"
 pkgrel = 0
-build_style = "gnu_configure"
+build_style = "meson"
 configure_args = [
-    "--disable-static",
-    "--enable-mbim-qmux",
-    "--enable-qrtr",
-    "--enable-introspection",
+    "-Dqrtr=true",
+    "-Dintrospection=true",
 ]
-make_cmd = "gmake"
 hostmakedepends = [
-    "gmake",
-    "pkgconf",
+    "bash-completion",
     "glib-devel",
+    "gobject-introspection",
+    "help2man",
     "libgudev-devel",
     "libmbim-devel",
     "libqrtr-glib-devel",
-    "gobject-introspection",
+    "meson",
+    "pkgconf",
 ]
 makedepends = ["glib-devel", "libgudev-devel", "linux-headers"]
 pkgdesc = "QMI modem protocol helper library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://www.freedesktop.org/wiki/Software/libqmi"
-source = f"$(FREEDESKTOP_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "862482ce9e3ad0bd65d264334ee311cdb94b9df2863b5b7136309b41b8ac1990"
+source = f"https://gitlab.freedesktop.org/mobile-broadband/libqmi/-/archive/{pkgver}/libqmi-{pkgver}.tar.gz"
+sha256 = "8690d25b4d110b6df28b31da0a8bf16c7e966d31abcfeeb854f2753451e7a400"
 
 
 @subpackage("libqmi-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
