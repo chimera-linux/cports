05efe25d65f6fc22f161f5dea1fc8027b03e9aa7
psykose <alice@ayaya.dev>
2023-10-15 13:52:04 +0000

    contrib/oniguruma: update to 6.9.9

--- contrib/oniguruma/template.py
+++ contrib/oniguruma/template.py
@@ -1,15 +1,15 @@
 pkgname = "oniguruma"
-pkgver = "6.9.8"
+pkgver = "6.9.9"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-posix-api=yes"]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 pkgdesc = "Multi-charset regular expression library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/kkos/oniguruma"
 source = f"{url}/releases/download/v{pkgver}/onig-{pkgver}.tar.gz"
-sha256 = "28cd62c1464623c7910565fb1ccaaa0104b2fe8b12bcd646e81f73b47535213e"
+sha256 = "60162bd3b9fc6f4886d4c7a07925ffd374167732f55dce8c491bfd9cd818a6cf"
 
 
 def post_install(self):
@@ -19,6 +19,3 @@ def post_install(self):
 @subpackage("oniguruma-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
