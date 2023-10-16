4c38fd73c1f9c219b78584afc39199d205f9e964
psykose <alice@ayaya.dev>
2023-10-16 02:10:26 +0000

    main/libgtop: update to 2.41.2

--- main/libgtop/template.py
+++ main/libgtop/template.py
@@ -1,17 +1,26 @@
 pkgname = "libgtop"
-pkgver = "2.41.1"
+pkgver = "2.41.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-libgtop-smp", "--enable-introspection"]
 make_cmd = "gmake"
-hostmakedepends = ["gmake", "pkgconf", "perl", "gobject-introspection"]
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "gobject-introspection",
+    "gtk-doc-tools",
+    "libtool",
+    "perl",
+    "pkgconf",
+]
 makedepends = ["glib-devel", "libxau-devel", "linux-headers"]
 pkgdesc = "GNOME library to retrieve system information"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/libgtop"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "43ea9ad13f7caf98303e64172b191be9b96bab340b019deeec72251ee140fe3b"
+sha256 = "d9026cd8a48d27cdffd332f8d60a92764b56424e522c420cd13a01f40daf92c3"
 suid_files = [
     "usr/libexec/libgtop_server2",
 ]
@@ -20,6 +29,3 @@ suid_files = [
 @subpackage("libgtop-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
