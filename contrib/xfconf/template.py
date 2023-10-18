8524e8ae046a8c5526ab04493a4624e00c2ce32e
psykose <alice@ayaya.dev>
2023-10-18 18:50:31 +0000

    contrib/xfconf: update to 4.18.2

--- contrib/xfconf/template.py
+++ contrib/xfconf/template.py
@@ -1,17 +1,19 @@
 pkgname = "xfconf"
-pkgver = "4.18.1"
+pkgver = "4.18.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-gsettings-backend"]
+# broken presently
+configure_gen = []
 make_cmd = "gmake"
 make_check_wrapper = ["dbus-run-session", "xvfb-run"]
 hostmakedepends = [
-    "pkgconf",
+    "glib-devel",
     "gmake",
     "intltool",
-    "vala-devel",
-    "glib-devel",
+    "pkgconf",
     "python",
+    "vala-devel",
 ]
 makedepends = ["glib-devel", "vala-devel", "libxfce4util-devel"]
 checkdepends = ["xserver-xorg-xvfb", "dbus"]
@@ -20,12 +22,9 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND LGPL-2.0-or-later"
 url = "https://xfce.org"
 source = f"https://archive.xfce.org/src/xfce/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "d9714751bbcfdc5a59340da6ef8ddfc0807221587b962d907f97dc0a8a002257"
+sha256 = "dce24fb0555e9718d139c10e714759e03ab4e40a7ffcf3c990f046f7a17213cc"
 
 
 @subpackage("xfconf-devel")
 def _dev(self):
     return self.default_devel()
-
-
-configure_gen = []
