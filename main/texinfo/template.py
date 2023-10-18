2858ab0f17d999a0c73cb9ddaead6e7d1ac5c114
psykose <alice@ayaya.dev>
2023-10-18 17:05:45 +0000

    main/texinfo: update to 7.1

--- main/texinfo/template.py
+++ main/texinfo/template.py
@@ -1,8 +1,10 @@
 pkgname = "texinfo"
-pkgver = "7.0.3"
-pkgrel = 1
+pkgver = "7.1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-static", "--disable-perl-xs"]
+# cycle of autoconf needing this
+configure_gen = []
 hostmakedepends = ["perl", "ncurses-devel"]
 makedepends = ["ncurses-devel"]
 depends = ["perl"]
@@ -12,8 +14,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/texinfo"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "74b420d09d7f528e84f97aa330f0dd69a98a6053e7a4e01767eed115038807bf"
+sha256 = "deeec9f19f159e046fdf8ad22231981806dac332cc372f1c763504ad82b30953"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
-
-configure_gen = []
