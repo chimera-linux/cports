a54b934e1f3203cbc4dc3b4e371a970371b91757
psykose <alice@ayaya.dev>
2023-10-18 18:38:07 +0000

    main/python-urllib3: update to 2.0.7

--- main/python-urllib3/template.py
+++ main/python-urllib3/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-urllib3"
-pkgver = "2.0.6"
+pkgver = "2.0.7"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-installer", "python-hatchling"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://urllib3.readthedocs.io"
 source = f"$(PYPI_SITE)/u/urllib3/urllib3-{pkgver}.tar.gz"
-sha256 = "b19e1a85d206b56d7df1d5e683df4a7725252a964e3993648dd0fb5a1c157564"
+sha256 = "c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84"
 # unpackaged dependency
 options = ["!check", "brokenlinks"]
 
