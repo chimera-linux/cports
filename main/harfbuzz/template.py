ff6eca7f29dcb45bb82062f9539097eb5ac4f35b
psykose <alice@ayaya.dev>
2023-10-18 18:39:04 +0000

    main/harfbuzz: update to 8.2.2

--- main/harfbuzz/template.py
+++ main/harfbuzz/template.py
@@ -1,8 +1,9 @@
 pkgname = "harfbuzz"
-pkgver = "8.2.1"
+pkgver = "8.2.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
+    "-Db_ndebug=true",
     "-Dglib=enabled",
     "-Dfreetype=enabled",
     "-Dcairo=enabled",
@@ -31,7 +32,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://www.freedesktop.org/wiki/Software/HarfBuzz"
 source = f"https://github.com/{pkgname}/{pkgname}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "0fec78f98c9c8faf228957a201c8846f809452c20f8445eb092a1ba6f22dbea5"
+sha256 = "e433ad85fbdf57f680be29479b3f964577379aaf319f557eb76569f0ecbc90f3"
 # test failures since icu 71
 options = ["!cross", "!check"]
 
