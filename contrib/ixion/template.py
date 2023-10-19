d312e8ee47f621bf9b108a5538931e2b5c62fcd6
q66 <q66@chimera-linux.org>
2023-10-19 17:51:52 +0200

    contrib/ixion: update to 0.19.0

--- contrib/ixion/template.py
+++ contrib/ixion/template.py
@@ -1,6 +1,6 @@
 pkgname = "ixion"
-pkgver = "0.18.1"
-pkgrel = 1
+pkgver = "0.19.0"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "gmake", "automake", "libtool", "python"]
 makedepends = ["boost-devel", "python-devel", "mdds"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MPL-2.0"
 url = "https://gitlab.com/ixion/ixion"
 source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "6969e5bd475da5397b5e007d55749e5d0f0a502a72ac6f9a0bb40bbcabebc6e8"
+sha256 = "b5b67ea7fc631a0fda4fff3123f0cc2e3831849bdd8fbae8443be0766a77f243"
 
 
 @subpackage("ixion-libs")
