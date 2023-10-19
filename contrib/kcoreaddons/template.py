b5efc57e8e160ac29d1205c22fd9fae536f114ca
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-19 20:56:54 +0300

    contrib/kcoreaddons: new package (6.2.0)

---
+++ contrib/kcoreaddons/template.py
@@ -0,0 +1,32 @@
+pkgname = "kcoreaddons"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+]
+makedepends = [
+    "qt6-qtdeclarative-devel",
+    "qt6-qttools-devel",
+]
+pkgdesc = "Qt6 addon library with a collection of non-GUI utilities"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND (LGPL-2.1-only OR LGPL-3.0-only)"
+url = "https://api.kde.org/frameworks/kcoreaddons/html"
+source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/kcoreaddons-{pkgver}.tar.xz"
+sha256 = "3f9836a3ce5e72883bf032587b745ce55435aa7627174579d2e018da717338ed"
+# FIXME: cfi breaks at least kpluginfactorytest
+hardening = ["vis", "!cfi"]
+
+
+@subpackage("kcoreaddons-devel")
+def _devel(self):
+    return self.default_devel()
+
+
+@subpackage("kcoreaddons-doc")
+def _doc(self):
+    return self.default_doc(extra=["usr/share/kf6/licenses"])
