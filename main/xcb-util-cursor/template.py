b15f0db3d43cab0d5f1a78fbc4d0821d4d2073cb
psykose <alice@ayaya.dev>
2023-10-20 07:00:55 +0000

    main/xcb-util-cursor: update to 0.1.5

--- main/xcb-util-cursor/template.py
+++ main/xcb-util-cursor/template.py
@@ -1,16 +1,22 @@
 pkgname = "xcb-util-cursor"
-pkgver = "0.1.4"
+pkgver = "0.1.5"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_env = {"M4": "/usr/bin/gm4"}
-hostmakedepends = ["pkgconf", "gm4"]
+hostmakedepends = [
+    "automake",
+    "gm4",
+    "libtool",
+    "pkgconf",
+    "xorg-util-macros",
+]
 makedepends = ["xcb-util-renderutil-devel", "xcb-util-image-devel"]
 pkgdesc = "XCB utilities library - port of libxcursor"
 maintainer = "Wesley Moore <wes@wezm.net>"
 license = "MIT"
 url = "https://xcb.freedesktop.org"
-source = f"{url}/dist/{pkgname}-{pkgver}.tar.gz"
-sha256 = "cc8608ebb695742b6cf84712be29b2b66aa5f6768039528794fca0fa283022bf"
+source = f"https://xorg.freedesktop.org/archive/individual/lib/xcb-util-cursor-{pkgver}.tar.xz"
+sha256 = "0caf99b0d60970f81ce41c7ba694e5eaaf833227bb2cbcdb2f6dc9666a663c57"
 
 
 def post_install(self):
@@ -20,6 +26,3 @@ def post_install(self):
 @subpackage("xcb-util-cursor-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
