8378900ce4c782e01e848a8f748b5da4b8b05a94
psykose <alice@ayaya.dev>
2023-10-20 07:03:02 +0000

    contrib/buildkit: update to 0.12.3

--- contrib/buildkit/template.py
+++ contrib/buildkit/template.py
@@ -1,6 +1,6 @@
 pkgname = "buildkit"
-pkgver = "0.12.2"
-pkgrel = 1
+pkgver = "0.12.3"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/..."]
 hostmakedepends = ["go"]
@@ -10,9 +10,14 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/moby/buildkit"
 source = f"https://github.com/moby/buildkit/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "26c682a08b915b2e739387603f8e1fea1d5bcf67291db0f03733d03b8a1213da"
+sha256 = "9ba8e19cc27d22944ac455e5be5f6aae2f7451d614aa0ac511d16afb217079d1"
 # objcopy ppc64
 options = ["!debug"]
+
+
+def post_extract(self):
+    # delete stray incomplete vendor dir
+    self.rm("vendor/", recursive=True)
 
 
 def post_install(self):
