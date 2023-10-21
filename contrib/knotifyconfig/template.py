db6e7d8a450c252c613d5b59ce7d29c6c9ffaa89
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-22 02:09:09 +0300

    contrib/knotifyconfig: new package (6.2.0)

---
+++ contrib/knotifyconfig/template.py
@@ -0,0 +1,33 @@
+pkgname = "knotifyconfig"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "kcompletion-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "knotifications-devel",
+    "kxmlgui-devel",
+    "libcanberra-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE Configuration dialog for desktop notifications"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-only"
+url = "https://api.kde.org/frameworks/knotifyconfig/html"
+source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/knotifyconfig-{pkgver}.tar.xz"
+sha256 = "b5bfec4582bc22f0c87c70fdd25a05e145fdfe5e4a60a963f512571e03d37ccc"
+hardening = ["vis", "cfi"]
+
+
+@subpackage("knotifyconfig-devel")
+def _devel(self):
+    return self.default_devel()
