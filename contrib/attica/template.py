028b4c3dc062a8927e2d0d1620f69cce01a43cc7
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-22 01:46:50 +0300

    contrib/attica: new package (6.2.0)

---
+++ contrib/attica/template.py
@@ -0,0 +1,27 @@
+pkgname = "attica"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+# requires network access and passes in cbuild chroot
+make_check_args = ["-E", "providertest"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "qt6-qtbase-devel",
+]
+pkgdesc = "Freedesktop Open Collaboration Services (OCS) binding for Qt"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later"
+url = "https://api.kde.org/frameworks/attica/html"
+source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/attica-{pkgver}.tar.xz"
+sha256 = "275fb00b3f483704d800484883a3789797a3198b0ce680d85313d9977ffde2f0"
+hardening = ["vis", "cfi"]
+
+
+@subpackage("attica-devel")
+def _devel(self):
+    return self.default_devel()
