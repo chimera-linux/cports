2ce8ab5e4b7ae9bce633e8fc49fa6aa4cb41cf67
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-21 18:07:12 +0300

    contrib/kbookmarks: new package (6.2.0)

---
+++ contrib/kbookmarks/template.py
@@ -0,0 +1,32 @@
+pkgname = "kbookmarks"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+]
+makedepends = [
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kwidgetsaddons-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qttools-devel",
+]
+pkgdesc = "KDE Bookmarks management library"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.1-or-later"
+url = "https://api.kde.org/frameworks/kbookmarks/html"
+source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/kbookmarks-{pkgver}.tar.xz"
+sha256 = "1af0d6be7c43cb17bc9a8bda464c372b9f27f99d3cdf3be95ec5f24e34368efb"
+hardening = ["vis", "cfi"]
+
+
+@subpackage("kbookmarks-devel")
+def _devel(self):
+    self.depends += ["kwidgetsaddons-devel"]
+
+    return self.default_devel()
