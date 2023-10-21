c20a8199a6bb46bf214d94200110b68013c664e4
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-21 12:44:50 +0300

    contrib/plasma-activities: new package (6.0.5)

---
+++ contrib/plasma-activities/template.py
@@ -0,0 +1,29 @@
+pkgname = "plasma-activities"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "boost-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kwindowsystem-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "Core components for KDE's Activity Manager"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later AND LGPL-2.1-or-later AND (LGPL-2.1-only OR LGPL-3.0-only)"
+url = "https://invent.kde.org/plasma/plasma-activities"
+source = f"$(KDE_SITE)/plasma/{pkgver}/plasma-activities-{pkgver}.tar.xz"
+sha256 = "8811876bc690278ecd733b233ac45ea4fbb06bc1a62d9e17b9268aa4d05ea658"
+hardening = ["vis", "cfi"]
+
+
+@subpackage("plasma-activities-devel")
+def _devel(self):
+    return self.default_devel()
