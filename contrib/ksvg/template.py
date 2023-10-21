70f9fe9587b19534d0e3d51feb243f602018d9a9
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-21 21:28:10 +0300

    contrib/ksvg: new package (6.2.0)

---
+++ contrib/ksvg/template.py
@@ -0,0 +1,35 @@
+pkgname = "ksvg"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+# TODO: configure_args = ["-DBUILD_TOOLS=ON"]?
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+]
+makedepends = [
+    "karchive-devel",
+    "kcolorscheme-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kguiaddons-devel",
+    "kirigami-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtsvg-devel",
+]
+pkgdesc = "KDE Components for handling SVGs"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-2.0-or-later"
+url = "https://invent.kde.org/frameworks/ksvg"
+source = (
+    f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/ksvg-{pkgver}.tar.xz"
+)
+sha256 = "5689bf9dc3efab29fddaea0af00aaec8ca8323e6504f490c3bb3b47a82eb76f3"
+hardening = ["vis", "cfi"]
+
+
+@subpackage("ksvg-devel")
+def _devel(self):
+    return self.default_devel()
