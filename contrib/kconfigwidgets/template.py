30fcdc8d9543c2b41c65a06889547e8e10540fc3
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-21 15:25:52 +0300

    contrib/kconfigwidgets: new package (6.2.0)

---
+++ contrib/kconfigwidgets/template.py
@@ -0,0 +1,45 @@
+pkgname = "kconfigwidgets"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+make_check_wrapper = ["dbus-run-session"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "kcodecs-devel",
+    "kcolorscheme-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kguiaddons-devel",
+    "ki18n-devel",
+    "kwidgetsaddons-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qttools-devel",
+]
+checkdepends = [
+    "dbus",
+]
+pkgdesc = "KDE Widgets for configuration dialogs"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.1-only AND LGPL-2.1-or-later"
+url = "https://develop.kde.org/docs/features/kconfigwidgets"
+source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/kconfigwidgets-{pkgver}.tar.xz"
+sha256 = "3b5e72c6e55e7b9d0b3ebee0fe8b39c098dd07a56de39da1556891925eb7d963"
+hardening = ["vis", "cfi"]
+
+
+@subpackage("kconfigwidgets-devel")
+def _devel(self):
+    self.depends += [
+        "kcodecs-devel",
+        "kconfig-devel",
+        "kwidgetsaddons-devel",
+        "kcolorscheme-devel",
+    ]
+
+    return self.default_devel(extra=["usr/lib/qt6/plugins/designer"])
