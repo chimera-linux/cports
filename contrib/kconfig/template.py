c75d81be780e1fc6d3bd0bed869a81e34d758166
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-21 12:25:21 +0300

    contrib/kconfig: new package (6.2.0)

---
+++ contrib/kconfig/template.py
@@ -0,0 +1,36 @@
+pkgname = "kconfig"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+# FIXME: kconfigtest can work with right env
+make_check_args = ["-E", "kconfigcore-(kconfigtest|test_kconf_update)"]
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+make_check_wrapper = ["dbus-run-session"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+]
+makedepends = [
+    "qt6-qtdeclarative-devel",
+    "qt6-qttools-devel",
+]
+checkdepends = [
+    "bash",
+    "dbus",
+]
+pkgdesc = "KDE Persistent platform-independent application settings"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND LGPL-2.0-only AND LGPL-2.1-or-later"
+url = "https://api.kde.org/frameworks/kconfig/html"
+source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/kconfig-{pkgver}.tar.xz"
+sha256 = "2512bc036fc40cdc23345cbf8037853c7afdc93b7f81528547a451e0f8c429e9"
+# FIXME: cfi breaks at least 5 tests
+hardening = ["vis", "!cfi"]
+
+
+@subpackage("kconfig-devel")
+def _devel(self):
+    self.depends += ["qt6-qtdeclarative-devel"]
+
+    return self.default_devel()
