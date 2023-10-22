e79e7063af8390c1062260e29214b13d6ebff251
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-22 05:06:26 +0300

    contrib/kwayland: new package (6.0.5)

---
+++ contrib/kwayland/template.py
@@ -0,0 +1,28 @@
+pkgname = "kwayland"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "plasma-wayland-protocols",
+    "qt6-qtbase-devel",
+    "qt6-qtwayland-devel",
+    "wayland-protocols",
+]
+pkgdesc = "Qt-style Client and Server library wrapper for the Wayland libraries"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.1-only OR LGPL-3.0-only"
+url = "https://invent.kde.org/frameworks/kwayland"
+source = f"$(KDE_SITE)/plasma/{pkgver}/kwayland-{pkgver}.tar.xz"
+sha256 = "a8b88411d2ad9497e27d6dbd95d647de5ebe6314561e7a8d935fd79b2fe7ae84"
+hardening = ["vis", "cfi"]
+
+
+@subpackage("kwayland-devel")
+def _devel(self):
+    return self.default_devel()
