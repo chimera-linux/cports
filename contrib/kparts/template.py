8fda164b167012ca5e5ffbcc5d6de6a70fe3ea2f
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-22 17:06:06 +0300

    contrib/kparts: new package (6.2.0)

---
+++ contrib/kparts/template.py
@@ -0,0 +1,35 @@
+pkgname = "kparts"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kxmlgui-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE Plugin framework for UI components"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = (
+    "LGPL-2.1-only AND LGPL-2.1-or-later AND (LGPL-2.1-only OR LGPL-3.0-only)"
+)
+url = "https://api.kde.org/frameworks/kparts/html"
+source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/kparts-{pkgver}.tar.xz"
+sha256 = "04f70829abc7033327f42e5e5ee1365f4dba9968b184f34212c5486e36a9beb1"
+hardening = ["vis", "cfi"]
+
+
+@subpackage("kparts-devel")
+def _devel(self):
+    self.depends += ["kxmlgui-devel"]
+
+    return self.default_devel()
