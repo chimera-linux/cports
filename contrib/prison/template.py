0ab476b62a57c8b64079c5587d85c4685aa257e6
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-23 00:20:37 +0300

    contrib/prison: new package (6.2.0)

---
+++ contrib/prison/template.py
@@ -0,0 +1,32 @@
+pkgname = "prison"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+]
+makedepends = [
+    "qrencode-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtmultimedia-devel",
+    "zxing-cpp-devel",
+    # TODO: https://github.com/dmtx/libdmtx (DataMatrix barcodes)
+]
+pkgdesc = "KDE library to produce QR codes and DataMatrix barcodes"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "MIT"
+url = "https://api.kde.org/frameworks/prison/html"
+source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/prison-{pkgver}.tar.xz"
+sha256 = "87719675c1652f0773149b8de384f406949a6d65abe25a6d3611e110d12d6ca8"
+hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    self.install_license("LICENSES/MIT.txt")
+
+
+@subpackage("prison-devel")
+def _devel(self):
+    return self.default_devel()
