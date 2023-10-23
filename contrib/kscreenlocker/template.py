011c2d80f43d173e411f5b2c8e06e9e0ced8813a
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-23 19:56:32 +0300

    contrib/kscreenlocker: new package (6.0.5)

---
+++ contrib/kscreenlocker/template.py
@@ -0,0 +1,50 @@
+pkgname = "kscreenlocker"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+# circular plasma-workspace dep (QML org.kde.plasma.private.sessions) needed by kscreenlocker_greet,
+# ksmserver-ksldTest even needs it installed under /usr/lib/libexec
+make_check_args = ["-E", "(kscreenlocker-kill|ksmserver-ksld)Test"]
+# ksmserver-x11LockerTest only passes under Xvfb
+make_check_wrapper = ["dbus-run-session", "xvfb-run"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcmutils-devel",
+    "kcrash-devel",
+    "kglobalaccel-devel",
+    "ki18n-devel",
+    "kidletime-devel",
+    "kio-devel",
+    "knotifications-devel",
+    "ksvg-devel",
+    "kxmlgui-devel",
+    "layer-shell-qt-devel",
+    "libkscreen-devel",
+    "libplasma-devel",
+    "linux-pam-devel",
+    "qt6-qtdeclarative-devel",
+]
+checkdepends = [
+    "dbus",
+    "xserver-xorg-xvfb",
+]
+depends = ["kdeclarative"]
+pkgdesc = "KDE Library and components for secure lock screen architecture"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later AND (GPL-2.0-only OR GPL-3.0-only)"
+url = "https://invent.kde.org/plasma/kscreenlocker"
+source = f"$(KDE_SITE)/plasma/{pkgver}/kscreenlocker-{pkgver}.tar.xz"
+sha256 = "5bb94a2a0d69cceb7ed4efcbf786b53388567d510a410fd9d01be913d2b163d0"
+hardening = ["vis", "cfi"]
+# TODO: https://community.kde.org/Plasma/Plasma_6.0_Release_notes#New_required_PAM_configuration
+
+
+@subpackage("kscreenlocker-devel")
+def _devel(self):
+    return self.default_devel()
