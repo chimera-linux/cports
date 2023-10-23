3f361586ea1b3c97d1327565e905dde6b38f81a6
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-23 19:23:04 +0300

    contrib/libkscreen: new package (6.0.5)

---
+++ contrib/libkscreen/template.py
@@ -0,0 +1,41 @@
+pkgname = "libkscreen"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+# testbackendloader testEnv(xrandr 1.1) 'preferred.fileName().startsWith(backend)' returned FALSE, flaky tests when parallel
+make_check_args = ["-E", "testbackendloader", "-j1"]
+# kscreen-testqscreenbackend needs X11
+make_check_wrapper = ["xwfb-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "plasma-wayland-protocols",
+    "qt6-qtbase-devel",
+    "qt6-qttools-devel",
+    "qt6-qtwayland-devel",
+]
+checkdepends = [
+    "dbus-x11",
+    "hwdata",
+    "xwayland-run",
+]
+# depends = ["jq"] for zsh completions to work at their full capacity
+pkgdesc = "KDE screen management library"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = (
+    "LGPL-2.1-or-later AND GPL-2.0-or-later AND (GPL-2.0-only OR GPL-3.0-only)"
+)
+url = "https://invent.kde.org/plasma/libkscreen"
+source = f"$(KDE_SITE)/plasma/{pkgver}/libkscreen-{pkgver}.tar.xz"
+sha256 = "01a77569f372d354c172a23f2d4e0661701c024e44d972971b146735e16ae1d9"
+# FIXME: cfi breaks almost all tests
+hardening = ["vis", "!cfi"]
+
+
+@subpackage("libkscreen-devel")
+def _devel(self):
+    return self.default_devel()
