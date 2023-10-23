0c89730bd6b45de432a5f0335fc0259a836cb246
psykose <alice@ayaya.dev>
2023-10-23 19:54:09 +0000

    contrib/rust-analyzer: update to 2023.10.23

--- contrib/rust-analyzer/template.py
+++ contrib/rust-analyzer/template.py
@@ -1,5 +1,5 @@
 pkgname = "rust-analyzer"
-pkgver = "2023.10.16"
+pkgver = "2023.10.23"
 pkgrel = 0
 build_style = "cargo"
 make_env = {"CARGO_PROFILE_RELEASE_PANIC": "unwind"}
@@ -10,7 +10,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/rust-lang/rust-analyzer"
 source = f"{url}/archive/refs/tags/{pkgver.replace('.', '-')}.tar.gz"
-sha256 = "745199162df8a8bef84c30ed638bed01cefd6b23b568fe0051d3b5b14913557c"
+sha256 = "184b56ec10dda9ff29592ec6b2df1f68a96831730b157e3caf7b97e00fccc65c"
 # invokes rustfmt via rustup arg, also take longer to build than the actual
 # build..
 options = ["!check"]
