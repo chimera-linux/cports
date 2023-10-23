5de5333ff97b8d7837c731d5e4957deb65bcae05
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-23 03:21:47 +0300

    contrib/libksysguard: new package (6.0.5)

---
+++ contrib/libksysguard/template.py
@@ -0,0 +1,54 @@
+pkgname = "libksysguard"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "libcap-progs",
+    "ninja",
+]
+makedepends = [
+    "kauth-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kglobalaccel-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "kjobwidgets-devel",
+    "knewstuff-devel",
+    "kpackage-devel",
+    "kservice-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "libcap-devel",
+    "libnl-devel",
+    "libpcap-devel",
+    "libsensors-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE system monitor library"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.1-or-later AND (GPL-2.0-only OR GPL-3.0-only)"
+url = "https://api.kde.org/plasma/libksysguard/html"
+source = f"$(KDE_SITE)/plasma/{pkgver}/libksysguard-{pkgver}.tar.xz"
+sha256 = "ca954f5bb57700bd14bae3233a82094ea69d32271cc3edd6d32bcf38db23e177"
+file_modes = {
+    "usr/lib/libexec/ksysguard/ksgrd_network_helper": ("root", "root", 0o755),
+}
+file_xattrs = {
+    "usr/lib/libexec/ksysguard/ksgrd_network_helper": {
+        "security.capability": "cap_net_raw+ep",
+    },
+}
+# FIXME: cfi crashes plasma-systemmonitor on launch in libKSysGuardSensors.so
+hardening = ["vis", "!cfi"]
+
+
+@subpackage("libksysguard-devel")
+def _devel(self):
+    return self.default_devel()
