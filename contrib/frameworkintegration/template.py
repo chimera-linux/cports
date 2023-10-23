01f89b0460f7c98461b8658f44add5ba1cd8761b
Jami Kettunen <jami.kettunen@protonmail.com>
2023-10-23 16:35:22 +0300

    contrib/frameworkintegration: new package (6.2.0)

---
+++ contrib/frameworkintegration/template.py
@@ -0,0 +1,35 @@
+pkgname = "frameworkintegration"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "kcolorscheme-devel",
+    "kconfig-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "knewstuff-devel",
+    "knotifications-devel",
+    "kpackage-devel",
+    "kwidgetsaddons-devel",
+    "qt6-qtdeclarative-devel",
+    # TODO: packagekitqt6 + AppStreamQt 1.0? (KPackage install handler binaries)
+]
+pkgdesc = "Integration of Qt application with KDE workspaces"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.1-or-later"
+url = "https://api.kde.org/frameworks/frameworkintegration/html"
+source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/frameworkintegration-{pkgver}.tar.xz"
+sha256 = "e41f64b178ad9dadcad51b145bd580868aba061fb80ff15614268d09ec4e0d7c"
+hardening = ["vis", "cfi"]
+
+
+@subpackage("frameworkintegration-devel")
+def _devel(self):
+    return self.default_devel()
