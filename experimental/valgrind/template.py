de823a65e2038b71e32c8764075d43ef8ebff12d
q66 <q66@chimera-linux.org>
2023-10-24 06:01:56 +0200

    experimental/valgrind: remove more old template cruft

--- experimental/valgrind/template.py
+++ experimental/valgrind/template.py
@@ -22,14 +22,16 @@ license = "GPL-2.0-or-later"
 url = "http://valgrind.org"
 source = f"https://sourceware.org/pub/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "10ce1618bb3e33fad16eb79552b0a3e1211762448a0d7fce11c8a6243b9ac971"
+debug_level = 1
 tool_flags = {"CFLAGS": ["-U_FORTIFY_SOURCE", "-fPIC"], "LDFLAGS": ["-fPIC"]}
-nostrip_files = ["usr/libexec/valgrind/*"]
+nostrip_files = ["usr/libexec/valgrind/*", "usr/lib/valgrind/*"]
 hardening = ["!ssp", "!scp", "!pie", "!int"]
-# uses binutils; makes glibc assumptions in tests
-options = ["!cross", "!lto", "!scanshlibs", "!scanrundeps"]
+options = ["!cross", "!lto"]
 exec_wrappers = [("/usr/bin/gsed", "sed")]
 
 
 @subpackage("valgrind-devel")
 def _devel(self):
+    self.depends = [f"{pkgname}={pkgver}-r{pkgrel}"]
+
     return self.default_devel(extra=["usr/share/doc"])
