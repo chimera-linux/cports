987241fd853dfe30482cae98763d412bc03458e3
psykose <alice@ayaya.dev>
2023-10-29 22:06:03 +0000

    main/sord: update to 0.16.16

--- main/sord/template.py
+++ main/sord/template.py
@@ -1,15 +1,15 @@
 pkgname = "sord"
-pkgver = "0.16.14"
+pkgver = "0.16.16"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
-makedepends = ["serd-devel", "pcre2-devel"]
+makedepends = ["serd-devel", "pcre2-devel", "zix-devel"]
 pkgdesc = "C library for storing RDF data in memory"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "ISC"
 url = "https://drobilla.net/software/sord.html"
 source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.xz"
-sha256 = "220fd97d5fcb216e7b85db66f685bfdaad7dc58a50d1f96dfb2558dbc6c4731b"
+sha256 = "257f876d756143da02ee84c9260af93559d6249dd87f317e70ab5fffcc975fd0"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
 
