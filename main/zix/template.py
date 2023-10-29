6a6640502182775200d8f0470a466ca7233bd67b
psykose <alice@ayaya.dev>
2023-10-29 22:05:35 +0000

    main/zix: new package (0.4.2)

---
+++ main/zix/template.py
@@ -0,0 +1,25 @@
+pkgname = "zix"
+pkgver = "0.4.2"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+]
+pkgdesc = "C library of portability wrappers and data structures"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "ISC"
+url = "https://drobilla.net/category/zix"
+source = f"https://download.drobilla.net/zix-{pkgver}.tar.xz"
+sha256 = "0c071cc11ab030bdc668bea3b46781b6dafd47ddd03b6d0c2bc1ebe7177e488d"
+# FIXME cfi
+hardening = ["vis", "!cfi"]
+
+
+def post_install(self):
+    self.install_license("COPYING")
+
+
+@subpackage("zix-devel")
+def _devel(self):
+    return self.default_devel()
