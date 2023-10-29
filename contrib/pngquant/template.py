6ddcea50695848d585615d238ef1dac789742995
psykose <alice@ayaya.dev>
2023-10-29 22:01:05 +0000

    contrib/pngquant: update to 3.0.3

--- contrib/pngquant/template.py
+++ contrib/pngquant/template.py
@@ -1,5 +1,5 @@
 pkgname = "pngquant"
-pkgver = "3.0.1"
+pkgver = "3.0.3"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo", "pkgconf"]
@@ -12,15 +12,15 @@ pkgdesc = "Lossy PNG compressor"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/kornelski/pngquant"
-_libimagequant = "9f15d1c53938a441a6bbfb2de866492797f69e22"
+_libimagequant = "6e9805761851f1a8320380b9f563961f892ec6ba"
 source = [
     f"{url}/archive/refs/tags/{pkgver}.tar.gz",
     f"https://github.com/ImageOptim/libimagequant/archive/{_libimagequant}.tar.gz",
 ]
 source_paths = [".", "lib"]
 sha256 = [
-    "488e4587c27c7515427b231530cc070a6b123727137b32c501658cff3f8a3451",
-    "06738e69b3d807ac99b2e40e00fce74293a60c6c2ac8c4fb3362cdcfc2bd0fb2",
+    "ddd8889a9c269ba454d0c5e4f7167948d55d77c4570b23f671809fd3a68b6822",
+    "d0167e69f9303029bd1df3ea728fe9e7a9c4b64050c266140b119443bd2da4b7",
 ]
 
 
