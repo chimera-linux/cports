552c5687e8c87da2184bf5108aba17bdbefed26a
psykose <alice@ayaya.dev>
2023-10-30 00:18:48 +0000

    main/python-cairo: update to 1.25.1

--- main/python-cairo/template.py
+++ main/python-cairo/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-cairo"
-pkgver = "1.25.0"
+pkgver = "1.25.1"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["pkgconf", "python-setuptools"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later OR MPL-1.1"
 url = "https://pycairo.readthedocs.io"
 source = f"https://github.com/pygobject/pycairo/releases/download/v{pkgver}/pycairo-{pkgver}.tar.gz"
-sha256 = "37842b9bfa6339c45a5025f752e1d78d5840b1a0f82303bdd5610846ad8b5c4f"
+sha256 = "7e2be4fbc3b4536f16db7a11982cbf713e75069a4d73d44fe5a49b68423f5c0c"
 
 
 def do_check(self):
