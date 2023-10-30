badd7e0a2b8bcbc198fda23c1635dde2656549ad
psykose <alice@ayaya.dev>
2023-10-30 00:04:31 +0000

    contrib/python-openssl: update to 23.3.0

--- contrib/python-openssl/template.py
+++ contrib/python-openssl/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-openssl"
-pkgver = "23.2.0"
+pkgver = "23.3.0"
 pkgrel = 0
 build_style = "python_module"
 hostmakedepends = ["python-setuptools"]
@@ -15,6 +15,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://pyopenssl.org"
 source = f"$(PYPI_SITE)/p/pyOpenSSL/pyOpenSSL-{pkgver}.tar.gz"
-sha256 = "276f931f55a452e7dea69c7173e984eb2a4407ce413c918aa34b55f82f9b8bac"
+sha256 = "6b2cba5cc46e822750ec3e5a81ee12819850b11303630d575e98108a079c2b12"
 # unpackaged checkdepends
 options = ["!check"]
