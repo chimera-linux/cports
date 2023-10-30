8ad9650a91d288a4d7711ea98cd06149f117e890
psykose <alice@ayaya.dev>
2023-10-30 00:11:33 +0000

    main/python-babel: update to 2.13.1

--- main/python-babel/template.py
+++ main/python-babel/template.py
@@ -1,8 +1,13 @@
 pkgname = "python-babel"
-pkgver = "2.13.0"
+pkgver = "2.13.1"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools"]
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
 checkdepends = ["python-pytz"]
 depends = ["python", "python-setuptools", "python-pytz"]
 pkgdesc = "Tools for internationalizing Python applications"
@@ -10,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://babel.pocoo.org"
 source = f"$(PYPI_SITE)/B/Babel/Babel-{pkgver}.tar.gz"
-sha256 = "04c3e2d28d2b7681644508f836be388ae49e0cfe91465095340395b60d00f210"
+sha256 = "33e0952d7dd6374af8dbf6768cc4ddf3ccfefc244f9986d4074704f2fbd18900"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
