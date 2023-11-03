95ea58e3420a21bcbf2dcf5ef5473abea64a038a
psykose <alice@ayaya.dev>
2023-11-03 04:40:48 +0000

    contrib/inxi: update to 3.3.31.3

--- contrib/inxi/template.py
+++ contrib/inxi/template.py
@@ -1,5 +1,5 @@
 pkgname = "inxi"
-_pver = "3.3.30-1"
+_pver = "3.3.31-3"
 pkgver = f"{_pver.replace('-', '.')}"
 pkgrel = 0
 depends = ["perl"]
@@ -8,7 +8,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later"
 url = "https://codeberg.org/smxi/inxi"
 source = f"https://codeberg.org/smxi/inxi/archive/{_pver}.tar.gz"
-sha256 = "650712b94bc6b71b51f579ae293b920252f73086c242ddf86270c84bc8128d53"
+sha256 = "24f0a807ed4771590de851f98a64ed9b3f8b5a331f99a5edecbcc7ad59fbe7db"
 
 
 def do_install(self):
