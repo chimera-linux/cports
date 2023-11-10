5b9a69048c3787c57ced0a61fa858a3df04a9d70
q66 <q66@chimera-linux.org>
2023-11-10 23:05:46 +0100

    main/gnome: separate apps to gnome-apps via reverse recommends
    The gnome package now depends only on gnome-core. All the apps
    are pulled in through gnome-apps instead, which installs itself
    with gnome. That means one can constrain away gnome-apps if not
    needed while keeping base-desktop.

--- main/gnome/template.py
+++ main/gnome/template.py
@@ -1,41 +1,50 @@
 pkgname = "gnome"
 pkgver = "44.4"
-pkgrel = 0
+pkgrel = 1
 build_style = "meta"
 depends = [
     f"gnome-core~{pkgver}",
-    "baobab",
-    "eog",
-    "epiphany",
-    "evince",
-    "gnome-browser-connector",
-    "gnome-calculator",
-    "gnome-calendar",
-    "gnome-characters",
-    "gnome-clocks",
-    "gnome-console",
-    # "gnome-contacts",
-    # "gnome-connections",
-    "gnome-disk-utility",
-    "gnome-font-viewer",
-    # "gnome-maps",
-    # "gnome-music",
-    # "gnome-remote-desktop",
-    "gnome-screenshot",
-    "gnome-software",
-    "gnome-shell-extensions",
-    # "gnome-system-monitor",
-    "gnome-text-editor",
-    "gnome-user-docs",
-    # "gnome-user-share",
-    # "gnome-weather",
-    # "orca",
-    # "phodav",
-    "simple-scan",
-    "sushi",
-    "totem",
 ]
 pkgdesc = "GNOME desktop environment (session and apps)"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:meta"
 url = "https://www.gnome.org"
+
+
+@subpackage("gnome-apps")
+def _apps(self):
+    self.pkgdesc = "GNOME desktop environment (apps)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.depends = [
+        "baobab",
+        "eog",
+        "epiphany",
+        "evince",
+        "gnome-browser-connector",
+        "gnome-calculator",
+        "gnome-calendar",
+        "gnome-characters",
+        "gnome-clocks",
+        "gnome-console",
+        # "gnome-contacts",
+        # "gnome-connections",
+        "gnome-disk-utility",
+        "gnome-font-viewer",
+        # "gnome-maps",
+        # "gnome-music",
+        # "gnome-remote-desktop",
+        "gnome-screenshot",
+        "gnome-software",
+        "gnome-shell-extensions",
+        # "gnome-system-monitor",
+        "gnome-text-editor",
+        "gnome-user-docs",
+        # "gnome-user-share",
+        # "gnome-weather",
+        # "orca",
+        # "phodav",
+        "simple-scan",
+        "sushi",
+        "totem",
+    ]
+    return []
