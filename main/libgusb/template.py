ac47b1816c18355b29a77c239346db5363db3e95
q66 <q66@chimera-linux.org>
2023-11-11 18:34:38 +0100

    main/libgusb: update to 0.4.8

--- main/libgusb/template.py
+++ main/libgusb/template.py
@@ -1,5 +1,5 @@
 pkgname = "libgusb"
-pkgver = "0.4.7"
+pkgver = "0.4.8"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocs=false", "-Dintrospection=true"]
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/hughsie/libgusb"
 source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "8acdd015c6f572555f8ec54c045e2d340baeb08468f6be519369c713794234f7"
+sha256 = "77aec51de289afe3bf5f8a1a36ae989d1a59748391097242949daccda9569ede"
 # no access to usb in container
 options = ["!check", "!cross"]
 
