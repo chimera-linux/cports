1d700a2389538386447385607cb19582adfb5257
psykose <alice@ayaya.dev>
2023-11-11 23:51:27 +0000

    contrib/obs-studio: update to 30.0.0

--- contrib/obs-studio/template.py
+++ contrib/obs-studio/template.py
@@ -1,15 +1,20 @@
 pkgname = "obs-studio"
-pkgver = "29.1.3"
-pkgrel = 1
+pkgver = "30.0.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
+    "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
     f"-DOBS_VERSION_OVERRIDE={pkgver}",
     "-DENABLE_ALSA=OFF",
     "-DENABLE_AJA=OFF",
+    # TODO: libdatachannel
+    "-DENABLE_WEBRTC=OFF",
     "-DENABLE_JACK=ON",
     # XXX: python+lua scripting is checked at once, latter needs luajit
     "-DENABLE_SCRIPTING=OFF",
     "-DENABLE_VLC=OFF",
+    # TODO: onevpl
+    "-DENABLE_QSV11=OFF",
 ]
 hostmakedepends = [
     "cmake",
@@ -39,6 +44,7 @@ makedepends = [
     "python-devel",
     "qt6-qtbase-devel",
     "qt6-qtsvg-devel",
+    "rnnoise-devel",
     "speexdsp-devel",
     "srt-devel",
     "udev-devel",
@@ -53,7 +59,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://obsproject.com"
 source = f"https://github.com/obsproject/obs-studio/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "9d9cfbdbdd255f48a23feeefb60089769a65f52bbca24fa31d74125f3bbb0e90"
+sha256 = "c23dd463862b1a8f40365d84fd52105d7eafc3614fb3d470b695ba28a6e4da06"
 # FIXME: cfi
 hardening = ["vis"]
 # don't build with lto
