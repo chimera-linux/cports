6e2d35ceb35997cd65cef845ad0d66960b1e011f
q66 <q66@chimera-linux.org>
2023-11-11 18:31:40 +0100

    main/nautilus: update to 45.1

--- main/nautilus/template.py
+++ main/nautilus/template.py
@@ -1,5 +1,5 @@
 pkgname = "nautilus"
-pkgver = "45.0"
+pkgver = "45.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dtests=headless"]
@@ -32,7 +32,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Apps/Files"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "b311c465ce2ca7bc648f61b865187b4451388dec9358a169f3277e741718ab5d"
+sha256 = "23bdaa9a85466c5937a89daddd080ed03bcc2b49e8b64af607206353bde82fe3"
 options = ["!cross"]
 
 
