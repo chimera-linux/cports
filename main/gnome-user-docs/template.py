ea449c76588d1131105fbcd85fa3a07204fcacb2
q66 <q66@chimera-linux.org>
2023-11-11 18:31:18 +0100

    main/gnome-user-docs: update to 45.1

--- main/gnome-user-docs/template.py
+++ main/gnome-user-docs/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-user-docs"
-pkgver = "45.0"
+pkgver = "45.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -18,5 +18,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "CC-BY-3.0"
 url = "https://gitlab.gnome.org/GNOME/gnome-user-docs"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "ff1cbbdc0e109e99b860a8fbe335b73948e0be9cdb7ed9c384ab92c8243d3ce1"
+sha256 = "2f90c6827b5f1605df2edfbea2b342870300a9a981bc392dc96214d967f3adf1"
 options = ["!splitdoc"]
