e7f7420f1a3fb524e457ab6e30dcf5d5b9b19656
psykose <alice@ayaya.dev>
2023-11-11 23:56:09 +0000

    contrib/libvips: update to 8.15.0

--- contrib/libvips/template.py
+++ contrib/libvips/template.py
@@ -1,5 +1,5 @@
 pkgname = "libvips"
-pkgver = "8.14.5"
+pkgver = "8.15.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Db_ndebug=true"]
@@ -14,6 +14,7 @@ makedepends = [
     "giflib-devel",
     "glib-devel",
     "gobject-introspection",
+    "highway-devel",
     "libexif-devel",
     "libexpat-devel",
     "libheif-devel",
@@ -33,7 +34,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/libvips/libvips"
 source = f"https://github.com/libvips/libvips/releases/download/v{pkgver}/vips-{pkgver}.tar.xz"
-sha256 = "90374e9f6fbd5657b5faf306cacda20658d6144d385316b59b865bc1a487b68d"
+sha256 = "d33f81c6ab4bd1faeedc36dc32f880b19e9d5ff69b502e59d175332dfb8f63f1"
 # FIXME: cfi
 hardening = ["vis"]
 # broken
