4fd64a281e9a38d7b6d66008ff8935c5a7b9903a
q66 <q66@chimera-linux.org>
2023-11-11 18:29:13 +0100

    main/gnome-shell-extensions: update to 45.1

--- main/gnome-shell-extensions/template.py
+++ main/gnome-shell-extensions/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-shell-extensions"
-pkgver = "45.0"
+pkgver = "45.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "gettext", "glib-devel"]
@@ -9,4 +9,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Projects/GnomeShell/Extensions"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "239bf89ff52794f8d5e86acd85fbfe5c24be8ce167c57690db2efd587c339fd6"
+sha256 = "242e15a0c06e820c3fd8dd6aeac1a8ef865ce58882e5975af1d65934bb4d4261"
