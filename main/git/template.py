175fed6f043972f7eb549cbeec6bd76875fd9daf
q66 <q66@chimera-linux.org>
2023-11-14 17:03:03 +0100

    main/git: update to 2.42.1

--- main/git/template.py
+++ main/git/template.py
@@ -1,6 +1,6 @@
 pkgname = "git"
-pkgver = "2.42.0"
-pkgrel = 1
+pkgver = "2.42.1"
+pkgrel = 0
 hostmakedepends = [
     "gmake",
     "asciidoc",
@@ -28,7 +28,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://git-scm.com"
 source = f"https://www.kernel.org/pub/software/scm/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "3278210e9fd2994b8484dd7e3ddd9ea8b940ef52170cdb606daa94d887c93b0d"
+sha256 = "8e46fa96bf35a65625d85fde50391e39bc0620d1bb39afb70b96c4a237a1a4f7"
 hardening = ["!cfi"]  # TODO
 # missing checkdepends
 options = ["!check"]
