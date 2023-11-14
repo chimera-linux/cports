27169f3f53fbfbeac250c883f81083f17d6069f0
q66 <q66@chimera-linux.org>
2023-11-14 16:00:33 +0100

    main/python-urllib3: update to 2.1.0

--- main/python-urllib3/template.py
+++ main/python-urllib3/template.py
@@ -1,15 +1,15 @@
 pkgname = "python-urllib3"
-pkgver = "2.0.7"
+pkgver = "2.1.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-installer", "python-hatchling"]
-depends = ["python", "python-six"]
+depends = ["python"]
 pkgdesc = "HTTP library with thread-safe connection pooling"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://urllib3.readthedocs.io"
 source = f"$(PYPI_SITE)/u/urllib3/urllib3-{pkgver}.tar.gz"
-sha256 = "c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84"
+sha256 = "df7aa8afb0148fa78488e7899b2c59b5f4ffcfa82e6c54ccb9dd37c1d7b52d54"
 # unpackaged dependency
 options = ["!check", "brokenlinks"]
 
