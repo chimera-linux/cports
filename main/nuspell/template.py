b7ee801c0ce9896fde1d770eaf273c2d60e81751
q66 <q66@chimera-linux.org>
2023-11-14 16:57:04 +0100

    main/nuspell: update to 5.1.4

--- main/nuspell/template.py
+++ main/nuspell/template.py
@@ -1,5 +1,5 @@
 pkgname = "nuspell"
-pkgver = "5.1.3"
+pkgver = "5.1.4"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DBUILD_TESTING=OFF"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-or-later"
 url = "https://nuspell.github.io"
 source = f"https://github.com/{pkgname}/{pkgname}/archive/v{pkgver}.tar.gz"
-sha256 = "2b1c96bdc5a32a17fc8fc24a0c205fa52b0a2920dd6139b0a7d7744cdef48c22"
+sha256 = "bdf344c5adbcc6797940f8f8cb75cb59f5a3794eb21b9547751a11782a792ef7"
 hardening = ["!cfi"]  # TODO
 # missing checkdepends
 options = ["!check"]
