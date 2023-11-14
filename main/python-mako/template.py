c9f04f77feb13753620b05cc248482b08f79f912
q66 <q66@chimera-linux.org>
2023-11-14 16:03:05 +0100

    main/python-mako: update to 1.3.0

--- main/python-mako/template.py
+++ main/python-mako/template.py
@@ -1,8 +1,12 @@
 pkgname = "python-mako"
-pkgver = "1.2.4"
+pkgver = "1.3.0"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools"]
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-wheel",
+]
 checkdepends = ["python-pytest", "python-setuptools", "python-markupsafe"]
 depends = ["python-setuptools", "python-markupsafe"]
 pkgdesc = "Fast and lightweight templating engine for Python"
@@ -10,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.makotemplates.org"
 source = f"$(PYPI_SITE)/M/Mako/Mako-{pkgver}.tar.gz"
-sha256 = "d60a3903dc3bb01a18ad6a89cdbe2e4eadc69c0bc8ef1e3773ba53d44c3f7a34"
+sha256 = "e3a9d388fd00e87043edbe8792f45880ac0114e9c4adc69f6e9bfb2c55e3b11b"
 # tests failing with 3.10 for now, should be harmless
 options = ["!check"]
 
