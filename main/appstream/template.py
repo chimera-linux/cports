42b3d1f9f21b2a734df5044f01924db287263b62
q66 <q66@chimera-linux.org>
2023-11-14 17:35:13 +0100

    main/appstream: update to 0.16.4

--- main/appstream/template.py
+++ main/appstream/template.py
@@ -1,5 +1,5 @@
 pkgname = "appstream"
-pkgver = "0.16.3"
+pkgver = "0.16.4"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dsystemd=false", "-Dstemming=false", "-Dapidocs=false"]
@@ -27,7 +27,7 @@ url = "http://www.freedesktop.org/wiki/Distributions/AppStream"
 source = (
     f"https://github.com/ximion/appstream/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "4470a27474dc3cc4938552fbf0394b6a65d8a2055d4f4418df086d65d8f2ba29"
+sha256 = "95d5cf451d1945182a9bc4d597c13e713451a3dba1a5759f45b6b3279ff3774c"
 options = ["!cross"]
 
 
