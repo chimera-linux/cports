d0e0f04bfdd8396b253228a9bc931c72357983c8
q66 <q66@chimera-linux.org>
2023-11-14 23:13:00 +0100

    contrib/u-boot-qemu-riscv64: update to 2023.10

--- contrib/u-boot-qemu-riscv64/template.py
+++ contrib/u-boot-qemu-riscv64/template.py
@@ -1,5 +1,5 @@
 pkgname = "u-boot-qemu-riscv64"
-pkgver = "2023.07"
+pkgver = "2023.10"
 pkgrel = 0
 build_style = "u_boot"
 hostmakedepends = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND BSD-3-Clause"
 url = "https://www.denx.de/wiki/U-Boot"
 source = f"https://ftp.denx.de/pub/u-boot/u-boot-{pkgver}.tar.bz2"
-sha256 = "12e921b466ae731cdbc355e6832b7f22bc90b01aeceef9886f98aaba7b394300"
+sha256 = "e00e6c6f014e046101739d08d06f328811cebcf5ae101348f409cbbd55ce6900"
 env = {
     "U_BOOT_TRIPLET": "riscv64-unknown-elf",
     "U_BOOT_TARGETS": "u-boot",
