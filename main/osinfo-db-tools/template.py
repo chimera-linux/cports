a50281c87a373e8bd483b62cfcd79a34d8e67cbd
q66 <q66@chimera-linux.org>
2023-11-14 16:24:17 +0100

    main/osinfo-db-tools: update to 1.11.0

--- main/osinfo-db-tools/template.py
+++ main/osinfo-db-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "osinfo-db-tools"
-pkgver = "1.10.0"
+pkgver = "1.11.0"
 pkgrel = 0
 build_style = "meson"
 make_check_env = {"MAKE": "gmake"}
@@ -18,6 +18,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://libosinfo.org"
 source = f"https://gitlab.com/libosinfo/{pkgname}/-/archive/v{pkgver}/{pkgname}-v{pkgver}.tar.gz"
-sha256 = "3677ee201cfebcb673b543b9f6fe43d67bc6fb1b55a2540c1af8ce13358c7e6f"
+sha256 = "b03429cad1dadf5e20bd2d24c4b130df35882ed22ebfbadb5f7acd50c6218ee2"
 hardening = ["vis", "cfi"]
 options = ["!cross"]
