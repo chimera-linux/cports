1647f3205efd5bc62c83eaf8f0554233d9bb1e4a
q66 <q66@chimera-linux.org>
2023-11-14 15:36:09 +0100

    main/glslang: update to 13.1.1

--- main/glslang/template.py
+++ main/glslang/template.py
@@ -1,17 +1,16 @@
-# note: some libs are unversioned (rebuild shaderc on updates)
 pkgname = "glslang"
-pkgver = "13.0.0"
+pkgver = "13.1.1"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DBUILD_SHARED_LIBS=ON"]
-hostmakedepends = ["cmake", "ninja", "python", "bison"]
+configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DALLOW_EXTERNAL_SPIRV_TOOLS=ON"]
+hostmakedepends = ["cmake", "ninja", "python", "bison", "spirv-tools-devel"]
 checkdepends = ["bash"]
 pkgdesc = "Khronos reference front-end for GLSL/ESSL + sample SPIR-V generator"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/KhronosGroup/glslang"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "bcda732434f829aa74414ea0e06d329ec8ac28637c38a0de45e17c8fd25a4715"
+sha256 = "1c4d0a5a38c8aaf89a2d7e6093be734320599f5a6775b2726beeb05b0c054e66"
 # missing checkdepends
 options = ["!check"]
 
@@ -25,22 +24,6 @@ def _progs(self):
     return self.default_progs()
 
 
-@subpackage("glslang-devel-static")
-def _devstatic(self):
-    self.pkgdesc = f"{pkgdesc} (static development libraries)"
-    self.depends = []
-    self.install_if = []
-
-    return ["usr/lib/*.a"]
-
-
 @subpackage("glslang-devel")
 def _devel(self):
-    self.options = ["!splitstatic"]
-    self.depends += [f"glslang-devel-static={pkgver}-r{pkgrel}"]
-
-    return [
-        "usr/include",
-        "usr/lib/libglslang.so",
-        "usr/lib/cmake",
-    ]
+    return self.default_devel()
