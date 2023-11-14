ad6df18d038fc039bb82e4d119f7dc2c5cd3e1cf
q66 <q66@chimera-linux.org>
2023-11-14 03:11:50 +0100

    main/dinit: update to 0.17.1

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -1,9 +1,7 @@
 pkgname = "dinit"
-pkgver = "0.17.0"
-pkgrel = 1
+pkgver = "0.17.1"
+pkgrel = 0
 build_style = "gnu_configure"
-# temporary
-_commit = "2b8df1e1d1c1371af1d58086635796a3b0781db3"
 configure_args = ["--syscontrolsocket=/run/dinitctl"]
 configure_gen = []
 make_cmd = "gmake"
@@ -14,8 +12,7 @@ pkgdesc = "Service manager and init system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://davmac.org/projects/dinit"
-# source = f"https://github.com/davmac314/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-source = f"https://github.com/davmac314/{pkgname}/archive/{_commit}.tar.gz"
-sha256 = "39c6b91fd7a1de419a18d7ed7c61b208660ea47e8cb5bb46f7080bf42425f580"
+source = f"https://github.com/davmac314/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "0617956ed2e8cddae5e21f6034546a2f7338364111b99dcc6cd5f3c37080301c"
 tool_flags = {"CXXFLAGS": ["-fno-rtti"]}
 hardening = ["vis", "cfi"]
