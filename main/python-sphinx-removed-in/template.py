e54a0660eb0d22b6d60a9642397e77ccb7da9d29
q66 <q66@chimera-linux.org>
2023-11-14 16:07:15 +0100

    main/python-sphinx-removed-in: update to 0.2.2

--- main/python-sphinx-removed-in/template.py
+++ main/python-sphinx-removed-in/template.py
@@ -1,8 +1,8 @@
 pkgname = "python-sphinx-removed-in"
-pkgver = "0.2.1"
+pkgver = "0.2.2"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools"]
+build_style = "python_pep517"
+hostmakedepends = ["python-build", "python-installer", "python-wheel"]
 checkdepends = ["python-sphinx"]
 depends = ["python-sphinx"]
 pkgdesc = "Sphinx extension for versionremoved and removed-in directives"
@@ -10,6 +10,10 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/MrSenko/sphinx-removed-in"
 source = f"$(PYPI_SITE)/s/sphinx-removed-in/sphinx-removed-in-{pkgver}.tar.gz"
-sha256 = "0588239cb534cd97b1d3900d0444311c119e45296a9f73f1ea81ea81a2cd3db1"
+sha256 = "851ca7ea449d0597da548068c0fc7ca076d84483b2e873a26aa64f24cfd8cd0d"
 # dependency of pytest
 options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
