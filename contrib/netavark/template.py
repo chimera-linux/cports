d6c1f56b4450e50fed39c4a7a38d8f61c332adce
q66 <q66@chimera-linux.org>
2023-11-16 20:56:04 +0100

    contrib/netavark: new package (1.8.0)

---
+++ contrib/netavark/template.py
@@ -0,0 +1,12 @@
+pkgname = "netavark"
+pkgver = "1.8.0"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo", "go-md2man", "protoc"]
+makedepends = ["linux-headers", "rust-std"]
+pkgdesc = "Container network stack"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "Apache-2.0"
+url = "https://github.com/containers/netavark"
+source = f"{url}/archive/v{pkgver}.tar.gz"
+sha256 = "b1422ef6927458e9f80f7d322b751e29ab5d04d8ed6cb065baa82fa4291af10f"
