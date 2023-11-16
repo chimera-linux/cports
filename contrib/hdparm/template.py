569ca6a22ca082109253261a7745615d0a1d9327
Subhaditya Nath <sn03.general@gmail.com>
2023-11-16 13:19:50 +0530

    contrib/hdparm: new package (9.65)

---
+++ contrib/hdparm/template.py
@@ -0,0 +1,21 @@
+pkgname = "hdparm"
+pkgver = "9.65"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_install_args = ["-j1", "binprefix=/usr", "sbindir=/usr/bin"]
+hostmakedepends = ["gmake"]
+makedepends = ["linux-headers"]
+pkgdesc = "Get/Set ATA/SATA drive parameters under Linux"
+maintainer = "Subhaditya Nath <sn03.general@gmail.com>"
+license = "BSD-1-Clause AND GPL-2.0-only"
+url = "https://sourceforge.net/projects/hdparm"
+source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "d14929f910d060932e717e9382425d47c2e7144235a53713d55a94f7de535a4b"
+hardening = ["vis", "cfi"]
+# no test suite
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE.TXT")
