13950990db4f3a168263b4000c8ebe954188b58d
psykose <alice@ayaya.dev>
2023-11-20 07:24:28 +0000

    contrib/lagrange: update to 1.17.4

--- contrib/lagrange/template.py
+++ contrib/lagrange/template.py
@@ -1,5 +1,5 @@
 pkgname = "lagrange"
-pkgver = "1.17.3"
+pkgver = "1.17.4"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -29,7 +29,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-2-Clause"
 url = "https://gmi.skyjake.fi/lagrange"
 source = f"https://github.com/skyjake/lagrange/releases/download/v{pkgver}/lagrange-{pkgver}.tar.gz"
-sha256 = "cd0c4797e399082ccbf4d0ede939ef286b96863c31860f62387fd94275dda3ba"
+sha256 = "8e29bf4cbacdc67bd128bdc41a46e3b0b9d54ae0af82882090f0ae632c678dda"
 # FIXME cfi
 hardening = ["vis"]
 # no tests
