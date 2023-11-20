73538f171752de68bf52a3cca21bf5f972f94817
q66 <q66@chimera-linux.org>
2023-11-20 20:05:24 +0100

    contrib/scons: new package (4.6.0)

---
+++ contrib/scons/template.py
@@ -0,0 +1,22 @@
+pkgname = "scons"
+pkgver = "4.6.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-wheel",
+]
+depends = ["python"]
+pkgdesc = "Build system nobody likes"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "MIT"
+url = "https://scons.org"
+source = f"https://github.com/SCons/scons/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "085fc9df961224b91ed715c5c44a11796a3e614d146139989ab14e8a347425ff"
+# uhhh, nah
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
