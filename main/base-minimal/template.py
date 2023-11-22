6a3b4c78b3917e66aae8d3d61f32d10c418c35c2
q66 <q66@chimera-linux.org>
2023-11-22 17:50:05 +0100

    main/base-minimal: add dependency on procps

--- main/base-minimal/template.py
+++ main/base-minimal/template.py
@@ -1,6 +1,6 @@
 pkgname = "base-minimal"
 pkgver = "0.1"
-pkgrel = 1
+pkgrel = 2
 build_style = "meta"
 depends = [
     "base-bootstrap",
@@ -8,6 +8,7 @@ depends = [
     "turnstile",
     "bsdtar",
     "nyagetty",
+    "procps",
     "chimera-install-scripts",
 ]
 pkgdesc = "Minimal set of packages for a bootable Chimera system"
