941f699dd8c644e85bd4408c9979f8e44f8d719d
psykose <alice@ayaya.dev>
2023-11-24 14:29:14 +0000

    contrib/blueprint-compiler: new package (0.10.0)

---
+++ contrib/blueprint-compiler/template.py
@@ -0,0 +1,21 @@
+pkgname = "blueprint-compiler"
+pkgver = "0.10.0"
+pkgrel = 0
+build_style = "meson"
+make_check_wrapper = ["weston-headless-run"]
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+]
+depends = [
+    "gtk4",
+    "libadwaita",
+    "python-gobject",
+]
+checkdepends = ["weston"] + depends
+pkgdesc = "Markup language compiler for GTK user interfaces"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-3.0-or-later"
+url = "https://jwestman.pages.gitlab.gnome.org/blueprint-compiler"
+source = f"https://gitlab.gnome.org/jwestman/blueprint-compiler/-/archive/v{pkgver}/blueprint-compiler-v{pkgver}.tar.gz"
+sha256 = "2bc729b36897d0959a9890fb0997c9847aa9d2fc9356520bd8a46ed0b51ff4c0"
