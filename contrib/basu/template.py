3f3993bd5396658143c6273f895d9200a2974065
Umar Getagazov <umar@handlerug.me>
2023-11-26 22:58:54 +0300

    contrib/basu: new package (0.2.1)

---
+++ contrib/basu/template.py
@@ -0,0 +1,24 @@
+pkgname = "basu"
+pkgver = "0.2.1"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "gperf",
+    "meson",
+    "pkgconf",
+]
+makedepends = [
+    "libcap",
+]
+pkgdesc = "Standalone sd-bus library and busctl extracted from systemd"
+maintainer = "Umar Getagazov <umar@handlerug.me>"
+license = "LGPL-2.1-or-later"
+url = "https://sr.ht/~emersion/basu"
+source = f"https://git.sr.ht/~emersion/basu/archive/v{pkgver}.tar.gz"
+sha256 = "43b327073d1ac7bc6cbc0d3dfff729348fc970dfff0551ad40e366332e990204"
+hardening = ["vis", "cfi"]
+
+
+@subpackage("basu-devel")
+def _devel(self):
+    return self.default_devel()
