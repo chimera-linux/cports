d4374a7b03f534887ef9fe97f518420a9366669c
Jami Kettunen <jami.kettunen@protonmail.com>
2023-11-26 02:41:40 +0200

    contrib/android-udev-rules: update to 20231124

--- contrib/android-udev-rules/template.py
+++ contrib/android-udev-rules/template.py
@@ -1,5 +1,5 @@
 pkgname = "android-udev-rules"
-pkgver = "20231104"
+pkgver = "20231124"
 pkgrel = 0
 pkgdesc = (
     "Android udev rules list aimed to be the most comprehensive on the net"
@@ -8,7 +8,7 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/M0Rf30/android-udev-rules"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "2f2e82c8d352d45a93806bc776df518da59c19ee9f0bf6aa8606d5d3ae05eccf"
+sha256 = "0de9e2fa5ee2c65d3854c3c796701de1cbf9d47f70fa261bdadba4578632733c"
 options = ["!splitudev"]
 system_groups = ["adbusers"]
 
