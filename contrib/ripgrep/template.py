8d53137cda88be51ae0b53845f7479ad44465b4b
Wesley Moore <wes@wezm.net>
2023-11-27 22:34:34 +1000

    contrib/ripgrep: update to 14.0.2

--- contrib/ripgrep/template.py
+++ contrib/ripgrep/template.py
@@ -1,8 +1,8 @@
 pkgname = "ripgrep"
-pkgver = "13.0.0"
-pkgrel = 1
+pkgver = "14.0.2"
+pkgrel = 2
 build_style = "cargo"
-hostmakedepends = ["cargo", "asciidoc"]
+hostmakedepends = ["cargo"]
 makedepends = ["rust-std"]
 pkgdesc = (
     "Tool that recursively searches the current directory for a regex pattern"
@@ -10,8 +10,15 @@ pkgdesc = (
 maintainer = "Wesley Moore <wes@wezm.net>"
 license = "MIT OR Unlicense"
 url = "https://github.com/BurntSushi/ripgrep"
-source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "0fb17aaf285b3eee8ddab17b833af1e190d73de317ff9648751ab0660d763ed2"
+source = [
+    f"{url}/archive/{pkgver}.tar.gz",
+    f"{url}/releases/download/{pkgver}/ripgrep-{pkgver}-x86_64-unknown-linux-musl.tar.gz",
+]
+source_paths = [".", "docs-prebuilt"]
+sha256 = [
+    "2b9bd8a582d1fea70eb932e389e0895922b9a0147f65f9ad4b601b3f3a82a195",
+    "fe514e7761a393c0844604343a5938061d06dc7fbae9d3c5d669fcbf5a6821bc",
+]
 
 
 def do_prepare(self):
@@ -28,7 +35,7 @@ def post_patch(self):
 
 def post_install(self):
     self.install_license("LICENSE-MIT")
-    self.install_man(next(self.find("target/", "rg.1")))
-    self.install_completion(next(self.find("target/", "rg.bash")), "bash", "rg")
-    self.install_completion(next(self.find("target/", "rg.fish")), "fish", "rg")
-    self.install_completion("complete/_rg", "zsh", "rg")
+    self.install_man("docs-prebuilt/doc/rg.1")
+    self.install_completion("docs-prebuilt/complete/rg.bash", "bash", "rg")
+    self.install_completion("docs-prebuilt/complete/rg.fish", "fish", "rg")
+    self.install_completion("docs-prebuilt/complete/_rg", "zsh", "rg")
