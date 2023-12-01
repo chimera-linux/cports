66b5aee4b86574922a49c4c69f5801b1cd80a7ba
q66 <q66@chimera-linux.org>
2023-12-01 12:37:36 +0100

    main/linux-lts: update to 6.1.64

--- main/linux-lts-zfs-bin/template.py
+++ main/linux-lts-zfs-bin/template.py
@@ -1,6 +1,6 @@
 pkgname = "linux-lts-zfs-bin"
-_kernver = "6.1.60"
-_zfsver = "2.2.1"
+_kernver = "6.1.64"
+_zfsver = "2.2.2"
 pkgver = f"{_zfsver}.{_kernver}"
 pkgrel = 0
 hostmakedepends = ["gmake", "perl", "python", "ckms"]
