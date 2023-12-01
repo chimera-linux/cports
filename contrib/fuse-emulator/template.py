2c153aa695f6a7b2304bc1246462a69412bf2451
q66 <q66@chimera-linux.org>
2023-12-01 21:15:17 +0100

    contrib/fuse-emulator: new package (1.5.7)

---
+++ contrib/fuse-emulator/template.py
@@ -0,0 +1,31 @@
+pkgname = "fuse-emulator"
+pkgver = "1.5.7"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-desktop-integration",
+    "--with-gtk",
+    "--verbose",
+]
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = ["pkgconf", "gmake", "automake", "libtool", "perl"]
+makedepends = [
+    "alsa-lib-devel",
+    "audiofile-devel",
+    "glib-devel",
+    "gtk+3-devel",
+    "libgcrypt-devel",
+    "libpng-devel",
+    "libspectrum-devel",
+    "libxml2-devel",
+    "linux-headers",
+    "sdl-devel",
+    "zlib-devel",
+]
+pkgdesc = "Free Unix Spectrum Emulator"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://fuse-emulator.sourceforge.net"
+source = f"$(SOURCEFORGE_SITE)/fuse-emulator/fuse-{pkgver}.tar.gz"
+sha256 = "f0e2583f2642cdc3b2a737910d24e289d46e4f7e151805e3b0827024b2b45e4d"
