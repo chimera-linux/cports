d0ab16af759d3fff22e5a127a55e1631ab0778e4
q66 <q66@chimera-linux.org>
2023-12-02 12:28:33 +0100

    contrib/base-steamdeck: new package (0.1)

---
+++ contrib/base-steamdeck/template.py
@@ -0,0 +1,17 @@
+pkgname = "base-steamdeck"
+pkgver = "0.1"
+pkgrel = 0
+archs = ["x86_64"]
+build_style = "meta"
+depends = [
+    "firmware-linux-steamdeck",
+    "valve-hardware-audio-processing",
+]
+pkgdesc = "Chimera base package for Steam Deck"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:none"
+url = "https://chimera-linux.org"
+
+
+def do_install(self):
+    pass
