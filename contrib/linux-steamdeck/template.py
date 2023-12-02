b7850cfc4388741754a6525951d022b7a9f9ac78
q66 <q66@chimera-linux.org>
2023-12-02 12:27:34 +0100

    contrib/linux-steamdeck: new package (6.1.52)

---
+++ contrib/linux-steamdeck/template.py
@@ -0,0 +1,80 @@
+# update linux-steamdeck-zfs-bin when bumping
+# also bump update.py
+pkgname = "linux-steamdeck"
+pkgver = "6.1.52"
+pkgrel = 0
+_vver = 9
+archs = ["x86_64"]
+make_dir = "build"
+hostmakedepends = ["base-kernel-devel"]
+depends = ["base-kernel"]
+provides = ["linux"]
+pkgdesc = f"Linux kernel for Steam Deck {pkgver[0:pkgver.rfind('.')]}.x"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-only"
+url = "https://gitlab.com/evlaV/linux-integration"
+source = f"{url}/-/archive/{pkgver}-valve{_vver}/linux-integration-{pkgver}-valve{_vver}.tar.gz"
+sha256 = "7dd894f029612626dcbafe1f4dd5060cdece665bce3c85ca098c75300f947ee3"
+# no meaningful checking to be done
+options = [
+    "!check",
+    "!debug",
+    "!strip",
+    "!scanrundeps",
+    "!scanshlibs",
+    "!linkparallel",
+    "!lto",
+    "textrels",
+    "execstack",
+    "foreignelf",  # vdso32
+]
+
+_flavor = "valve"
+
+if self.profile().cross:
+    broken = "linux-devel does not come out right"
+
+
+def init_configure(self):
+    from cbuild.util import linux
+
+    linux.generate_scriptlets(self, _flavor)
+
+
+def do_configure(self):
+    from cbuild.util import linux
+
+    linux.configure(self, _flavor)
+
+
+def do_build(self):
+    from cbuild.util import linux
+
+    linux.build(self, _flavor)
+
+
+def do_install(self):
+    from cbuild.util import linux
+
+    linux.install(self, _flavor)
+
+
+@subpackage("linux-steamdeck-devel")
+def _devel(self):
+    self.depends += ["clang"]
+    self.options = ["foreignelf", "execstack", "!scanshlibs"]
+    return ["usr/src", "usr/lib/modules/*/build"]
+
+
+@subpackage("linux-steamdeck-dbg")
+def _dbg(self):
+    self.pkgdesc += " (debug files)"
+    self.options = [
+        "!scanrundeps",
+        "!strip",
+        "!scanshlibs",
+        "foreignelf",
+        "execstack",
+        "textrels",
+    ]
+    return ["usr/lib/debug", "boot/System.map-*"]
---
+++ contrib/linux-steamdeck/update.py
@@ -0,0 +1,9 @@
+_vver = 9
+
+pattern = r"/tags/([\d.]+-valve\d+)"
+pkgver = f"{self.pkgver}.{_vver}"
+ignore = ["6.1.61.*"]
+
+
+def fetch_versions(self, src):
+    return map(lambda v: v.replace("-valve", "."), self.fetch_versions(src))
