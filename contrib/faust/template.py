84bcb4312a1cfbc42f636705ed13b383c9ecfc2d
q66 <q66@chimera-linux.org>
2023-12-04 00:36:07 +0100

    contrib/faust: drop some junk libs messing with shlib scan

--- contrib/faust/template.py
+++ contrib/faust/template.py
@@ -1,6 +1,6 @@
 pkgname = "faust"
 pkgver = "2.69.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "makefile"
 make_cmd = "gmake"
 hostmakedepends = ["cmake", "gmake", "pkgconf"]
@@ -14,7 +14,7 @@ sha256 = "169a2f1e8a95e159c78c734c1c5dd818bf5c95b3b002a7efd9f6bb8589357062"
 # skip execinfo
 tool_flags = {"CXXFLAGS": ["-DALPINE"]}
 # no check target
-options = ["!cross", "!check", "foreignelf"]
+options = ["!cross", "!check"]
 
 
 def post_install(self):
@@ -23,3 +23,7 @@ def post_install(self):
     self.rm(self.destdir / "usr/lib/libOSCFaust.a")
     self.rm(self.destdir / "usr/lib/ios-libsndfile.a")
     self.rm(self.destdir / "usr/lib/faust/max-msp/sndfile", recursive=True)
+    self.rm(
+        self.destdir / "usr/lib/faust/android/app/lib/libsndfile",
+        recursive=True,
+    )
