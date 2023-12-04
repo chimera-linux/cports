0c1dc24556f3b38206629c1b6f282c8e8abb13ae
Mohammed Anas <triallax@tutanota.com>
2023-12-04 00:25:44 +0000

    contrib/chezmoi: new package (2.42.3)

---
+++ contrib/chezmoi/template.py
@@ -0,0 +1,25 @@
+pkgname = "chezmoi"
+pkgver = "2.42.3"
+pkgrel = 0
+build_style = "go"
+make_build_args = [
+    f"-ldflags=-X main.version={pkgver} -X main.commit=v{pkgver}",
+]
+hostmakedepends = ["go"]
+go_build_tags = ["noembeddocs", "noupgrade"]
+pkgdesc = "Manage your dotfiles across multiple machines, securely"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "MIT"
+url = "https://chezmoi.io"
+source = f"https://github.com/twpayne/{pkgname}/archive/v{pkgver}.tar.gz"
+sha256 = "095a0e4818f5512fc15f4ead31e8c123423eed588487b465ec9d52b9a1e29484"
+options = ["!debug"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+    with self.pushd("completions"):
+        self.install_completion("chezmoi-completion.bash", "bash")
+        self.install_completion("chezmoi.fish", "fish")
+        self.install_completion("chezmoi.zsh", "zsh")
