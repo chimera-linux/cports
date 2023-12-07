74f6312b43c165c55b99ab3068f704ea0ec019ee
psykose <alice@ayaya.dev>
2023-12-07 16:23:50 +0000

    contrib/python-pebble: update to 5.0.5

--- contrib/python-pebble/template.py
+++ contrib/python-pebble/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pebble"
-pkgver = "5.0.4"
+pkgver = "5.0.5"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -16,4 +16,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-3.0-or-later"
 url = "https://github.com/noxdafox/pebble"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "287dbeec66f3340a7592cba94f03743386f00fa31063d82b6585c54f2885087a"
+sha256 = "a9e81246a6f7c61c3f9f25cdb68fa01b08df67c19c10a93b1eb993bab100e8db"
