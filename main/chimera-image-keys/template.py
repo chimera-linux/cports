34a526a119a013519a489b6391396ff0d7fd3652
q66 <q66@chimera-linux.org>
2023-12-08 02:31:44 +0100

    main/chimera-image-keys: new package (0.1)

---
+++ main/chimera-image-keys/template.py
@@ -0,0 +1,14 @@
+pkgname = "chimera-image-keys"
+pkgver = "0.1"
+pkgrel = 0
+build_style = "meta"
+depends = ["minisign"]
+pkgdesc = "Chimera public keys for image verification"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:meta"
+url = "https://chimera-linux.org"
+
+
+def do_install(self):
+    for f in self.files_path.glob("*.pub"):
+        self.install_file(f, "usr/share/chimera-image-keys")
