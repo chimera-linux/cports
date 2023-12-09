6e9a2b1cffbbc16efa2d2c3cfde5305b23035aeb
psykose <alice@ayaya.dev>
2023-12-09 17:50:18 +0000

    contrib/fish-shell: update to 3.6.4

--- contrib/fish-shell/template.py
+++ contrib/fish-shell/template.py
@@ -1,7 +1,8 @@
 pkgname = "fish-shell"
-pkgver = "3.6.1"
+pkgver = "3.6.4"
 pkgrel = 0
 build_style = "cmake"
+make_build_args = ["--target", "all", "fish_tests"]
 hostmakedepends = ["cmake", "ninja", "pkgconf", "gettext"]
 makedepends = ["ncurses-devel", "pcre2-devel"]
 checkdepends = ["python", "procps"]
@@ -10,9 +11,15 @@ maintainer = "lunamothxyz <mail@lunamoth.xyz>"
 license = "GPL-2.0-only"
 url = "https://fishshell.com"
 source = f"https://github.com/fish-shell/{pkgname}/releases/download/{pkgver}/fish-{pkgver}.tar.xz"
-sha256 = "55402bb47ca6739d8aba25e41780905b5ce1bce0a5e0dd17dca908b5bc0b49b2"
+sha256 = "0f3f610e580de092fbe882c8aa76623ecf91bb16fdf0543241e6e90d5d4bc393"
 # FIXME int: test fail
 hardening = ["vis", "cfi", "!int"]
+
+
+def do_check(self):
+    # fails to find test script via ctest in out-of-tree builddir
+    with self.pushd(self.make_dir):
+        self.do("./fish_tests")
 
 
 def post_install(self):
