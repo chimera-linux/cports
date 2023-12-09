b38188efa3d1b05e9e4cda84c71b345565edc62d
psykose <alice@ayaya.dev>
2023-12-09 18:27:43 +0000

    contrib/mold: update to 2.4.0

--- contrib/mold/template.py
+++ contrib/mold/template.py
@@ -1,5 +1,5 @@
 pkgname = "mold"
-pkgver = "2.3.3"
+pkgver = "2.4.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -19,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/rui314/mold"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "dcba90d5ee852ececb3a1a70110bb2d385079dcd1aa103e970296683c642f856"
+sha256 = "be65f3d785d32ece7b3204ecaa57810847fdd25c232cf704cbfff2dafb1ac107"
 # TODO: a portion of the tests fail, for various reasons, such as assuming
 # presence of gcc, gnu grep, and various toolchain specifics; around 70%
 # of the tests pass right now, fix the rest later
