258ba1a0488caab507a6f403b55c49b157621ae6
psykose <alice@ayaya.dev>
2023-12-09 18:09:48 +0000

    main/c-ares: update to 1.23.0

--- main/c-ares/template.py
+++ main/c-ares/template.py
@@ -1,5 +1,5 @@
 pkgname = "c-ares"
-pkgver = "1.22.1"
+pkgver = "1.23.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "libtool"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://c-ares.haxx.se"
 source = f"https://c-ares.haxx.se/download/{pkgname}-{pkgver}.tar.gz"
-sha256 = "f67c180deb799c670d9dda995a18ce06f6c7320b6c6363ff8fa85b77d0da9db8"
+sha256 = "cb614ecf78b477d35963ebffcf486fc9d55cc3d3216f00700e71b7d4868f79f5"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
 # does not like the sandbox
@@ -18,7 +18,20 @@ options = ["!check"]
 def post_install(self):
     self.install_license("LICENSE.md")
 
+    # only cmake has a toggle to install these..
+    with self.pushd(f"{self.make_dir}/src/tools/.libs"):
+        self.install_bin("adig")
+        self.install_bin("ahost")
+
+    self.install_man("docs/adig.1")
+    self.install_man("docs/ahost.1")
+
 
 @subpackage("c-ares-devel")
 def _devel(self):
     return self.default_devel()
+
+
+@subpackage("c-ares-progs")
+def _progs(self):
+    return self.default_progs()
