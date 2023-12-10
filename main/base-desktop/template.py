db1bb8d1c0db0e5820e6e5e9b82cf8f549d506ff
q66 <q66@chimera-linux.org>
2023-12-10 19:48:56 +0100

    main/base-desktop: make gnome optional

--- main/base-desktop/template.py
+++ main/base-desktop/template.py
@@ -1,19 +1,29 @@
 pkgname = "base-desktop"
 pkgver = "0.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "meta"
 depends = [
     "base-full",
-    "gnome",
-    "pipewire",
-    "gst-plugins-good",
-    "gst-plugins-bad",
-    "gst-libav",
-    "mesa-dri",
-    "xdg-utils",
     "fonts-dejavu",
+    "mesa-dri",
+    "pipewire",
+    "xdg-utils",
 ]
 pkgdesc = "Chimera default desktop session"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:meta"
 url = "https://chimera-linux.org"
+
+
+@subpackage("base-desktop-gnome")
+def _gnome(self):
+    self.pkgdesc = f"{pkgdesc} (GNOME)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "gnome",
+        "gst-plugins-bad",
+        "gst-plugins-good",
+        "gst-libav",
+    ]
+    return []
