003d99b83e44cdee3745bb9268918e8011559b9f
q66 <q66@chimera-linux.org>
2023-12-10 19:42:57 +0100

    main/base-full: ditto

--- main/base-full/template.py
+++ main/base-full/template.py
@@ -1,39 +1,70 @@
 pkgname = "base-full"
 pkgver = "0.2"
-pkgrel = 2
+pkgrel = 3
 build_style = "meta"
-depends = [
-    "base-core",
-    "base-locale",
-    "nano",
-    "initramfs-tools",
-    "openssh",
-    "syslog-ng",
-    "rfkill",
-    "zramctl",
-    "opendoas",
-    "ethtool",
-    "dhcpcd",
-    "usbutils",
-    "f2fs-tools",
-    "fdisk",
-    "elogind-meta",
-    "dbus",
-    "iwd",
-    "chrony",
-    "chimera-artwork",
-    # firmware for all
-    "base-firmware-linux",
-]
+depends = ["base-core"]
 pkgdesc = "Chimera base package for bare metal and virtual machines"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:meta"
 url = "https://chimera-linux.org"
 
-match self.profile().arch:
-    case "x86_64":
-        # intel audio firmware
-        depends += ["base-firmware-sof"]
-    case "ppc64" | "ppc":
-        # ppc mac disk tools
-        depends += ["hfsutils", "mac-fdisk"]
+
+@subpackage("base-full-firmware")
+def _fw(self):
+    self.pkgdesc = f"{pkgdesc} (firmware)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "base-firmware-linux",
+        "firmware-ipw2100",
+        "firmware-ipw2200",
+        "firmware-zd1211",
+    ]
+    if self.rparent.profile().arch == "x86_64":
+        self.depends += ["base-firmware-sof"]
+    return []
+
+
+@subpackage("base-full-misc")
+def _net(self):
+    self.pkgdesc = f"{pkgdesc} (miscellaneous)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "base-locale",
+        "chimera-artwork",
+        "chrony",
+        "nano",
+        "syslog-ng",
+        "zramctl",
+        "opendoas",
+        "usbutils",
+    ]
+    return []
+
+
+@subpackage("base-full-session")
+def _session(self):
+    self.pkgdesc = f"{pkgdesc} (session management)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "elogind-meta",
+        "dbus",
+    ]
+    return []
+
+
+@subpackage("base-full-net")
+def _net(self):
+    self.pkgdesc = f"{pkgdesc} (network)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "ethtool",
+        "dhcpcd",
+        "iwd",
+        "openssh",
+        "rfkill",
+    ]
+    return []
