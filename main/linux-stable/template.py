9d6d1739d73c754b5f06335636e0f18e267b5f0f
q66 <q66@chimera-linux.org>
2023-12-11 19:13:09 +0100

    main/linux-stable: update to 6.6.6

--- main/linux-stable/template.py
+++ main/linux-stable/template.py
@@ -1,6 +1,6 @@
 # update linux-stable-zfs-bin when bumping
 pkgname = "linux-stable"
-pkgver = "6.6.4"
+pkgver = "6.6.6"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]
 make_dir = "build"
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
-sha256 = "49e49660c93d8d6d58f118360d3ca8131695ec34669263ca8f041c876da93e45"
+sha256 = "ebf70a917934b13169e1be5b95c3b6c2fea5bc14e6dc144f1efb8a0016b224c8"
 # no meaningful checking to be done
 options = [
     "!check",
