6f379eecdd9da47344bf142da3a83b224886e560
q66 <q66@chimera-linux.org>
2023-12-13 01:43:04 +0100

    main/dinit: bump to latest git
    We need this for closehandle fixes, so that dbus activation can
    be implemented correctly.

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -1,6 +1,7 @@
 pkgname = "dinit"
 pkgver = "0.17.1"
-pkgrel = 1
+pkgrel = 2
+_commit = "f28ab8c656afa9dcb5a61d5cd4b91f1b43760c3a"
 build_style = "gnu_configure"
 configure_args = ["--syscontrolsocket=/run/dinitctl"]
 configure_gen = []
@@ -12,8 +13,8 @@ pkgdesc = "Service manager and init system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://davmac.org/projects/dinit"
-source = f"https://github.com/davmac314/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "0617956ed2e8cddae5e21f6034546a2f7338364111b99dcc6cd5f3c37080301c"
+source = f"https://github.com/davmac314/{pkgname}/archive/{_commit}.tar.gz"
+sha256 = "0da1bf4b4305d2132af73ea24336d4894b9eb544d6c5f6ae8893c78564fdbf7a"
 tool_flags = {"CXXFLAGS": ["-fno-rtti"]}
 hardening = ["vis", "cfi"]
 
