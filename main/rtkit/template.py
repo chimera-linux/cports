e951b7bfbe0d3b44973f413e5136376b196f1d59
q66 <q66@chimera-linux.org>
2023-12-15 00:50:54 +0100

    main/rtkit: use sysusers

--- main/rtkit/template.py
+++ main/rtkit/template.py
@@ -1,6 +1,6 @@
 pkgname = "rtkit"
 pkgver = "0.13"
-pkgrel = 3
+pkgrel = 4
 build_style = "meson"
 configure_args = [
     "-Dlibsystemd=disabled",
@@ -17,19 +17,16 @@ source = f"{url}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "a157144cd95cf6d25200e74b74a8f01e4fe51fd421bb63c1f00d471394b640ab"
 hardening = ["vis", "cfi"]
 
-system_users = [
-    {
-        "name": "rtkit",
-        "id": None,
-        "home": "/proc",
-    }
-]
-
 
 def post_install(self):
     self.rm(self.destdir / "usr/lib/systemd", recursive=True)
     self.install_license("LICENSE")
     self.install_service(self.files_path / "rtkit")
+    self.install_file(
+        self.files_path / "sysusers.conf",
+        "usr/lib/sysusers.d",
+        name="rtkit.conf",
+    )
     # optional
     self.install_file(
         self.files_path / "50-rtkit.rules", "usr/share/polkit-1/rules.d"
