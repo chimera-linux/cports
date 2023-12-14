912d233af3a558d1a38bfacb7c4e00a3a6e72a97
psykose <alice@ayaya.dev>
2023-12-14 23:49:41 +0000

    contrib/kwindowsystem: update to 5.113.0

--- contrib/kwindowsystem/template.py
+++ contrib/kwindowsystem/template.py
@@ -1,5 +1,5 @@
 pkgname = "kwindowsystem"
-pkgver = "5.112.0"
+pkgver = "5.113.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -31,7 +31,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT AND (LGPL-2.1-only OR LGPL-3.0-only)"
 url = "https://invent.kde.org/frameworks/kwindowsystem"
 source = f"https://download.kde.org/stable/frameworks/{pkgver[:pkgver.rfind('.')]}/kwindowsystem-{pkgver}.tar.xz"
-sha256 = "418e13e52f66a4030757d8479c787adab1112f40f694c0cc26309d84bf793022"
+sha256 = "a1e848e1bd5eebd382ede1b963235b796ebda1fae4a08513d59904aab9daeac3"
 hardening = ["vis", "cfi"]
 
 
