e82387100d962b9bd1641355ce446cbb7713e88b
psykose <alice@ayaya.dev>
2023-12-15 03:50:46 +0000

    main/zenity: update to 4.0.0

--- main/zenity/template.py
+++ main/zenity/template.py
@@ -1,19 +1,26 @@
 pkgname = "zenity"
-pkgver = "3.44.2"
-pkgrel = 1
+pkgver = "4.0.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dwebkitgtk=true"]
-hostmakedepends = ["meson", "pkgconf", "gettext", "itstool"]
+hostmakedepends = [
+    "gettext",
+    "help2man",
+    "itstool",
+    "meson",
+    "pkgconf",
+]
 makedepends = [
-    "gtk+3-devel",
+    "gtk4-devel",
     "glib-devel",
+    "libadwaita-devel",
     "libnotify-devel",
     "libx11-devel",
-    "webkitgtk-devel",
+    "webkitgtk4-devel",
 ]
 pkgdesc = "Display Gtk+ dialogs from the command line"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Projects/Zenity"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "3fb5b8b1044d3d129262d3c54cf220eb7f76bc21bd5ac6d96ec115cd3518300e"
+sha256 = "0b8c8def18dab05cc49bd464b90ca7f945ae52ff5e09242da702a15d94d3e8dd"
