527fb53051ccfa8d428921469256eb1fdaffd8ab
q66 <q66@chimera-linux.org>
2023-12-15 05:08:32 +0100

    main/trousers: use sysusers and tmpfiles

--- main/trousers/template.py
+++ main/trousers/template.py
@@ -1,6 +1,6 @@
 pkgname = "trousers"
 pkgver = "0.3.15"
-pkgrel = 1
+pkgrel = 2
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "libtool", "pkgconf"]
 makedepends = ["openssl-devel", "linux-headers"]
@@ -10,23 +10,24 @@ license = "BSD-3-Clause"
 url = "http://trousers.sourceforge.net"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "1e5be93e518372acf1d92d2f567d01a46fdb0b730487e544e6fb896c59cac77f"
-file_modes = {
-    "etc/tcsd.conf": ("tss", "tss", 0o600),
-    "var/lib/tpm": ("tss", "tss", 0o700),
-}
-system_users = [
-    {
-        "name": "tss",
-        "id": None,
-        "home": "/var/lib/tpm",
-    }
-]
 
 
 def post_install(self):
     self.install_license("LICENSE")
     self.install_service(self.files_path / "tcsd")
-    self.install_dir("var/lib/tpm", mode=0o700, empty=True)
+    self.install_dir("usr/share/trousers")
+    # tmpfiles will copy from it
+    self.mv(self.destdir / "etc/tcsd.conf", self.destdir / "usr/share/trousers")
+    self.install_file(
+        self.files_path / "sysusers.conf",
+        "usr/lib/sysusers.d",
+        name="tss.conf",
+    )
+    self.install_file(
+        self.files_path / "tmpfiles.conf",
+        "usr/lib/tmpfiles.d",
+        name="tss.conf",
+    )
 
 
 @subpackage("libtspi")
