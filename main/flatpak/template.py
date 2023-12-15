501d4a01399895277968f5121413c8a9848f6f97
q66 <q66@chimera-linux.org>
2023-12-15 01:53:42 +0100

    main/flatpak: use tmpfiles, sysusers, trigger

--- main/flatpak/template.py
+++ main/flatpak/template.py
@@ -1,6 +1,6 @@
 pkgname = "flatpak"
 pkgver = "1.14.5"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = [
     "--with-system-bubblewrap",
@@ -60,33 +60,26 @@ depends = [
     "xdg-dbus-proxy",
     "kmod",
 ]
+# invoke the trigger on self
+triggers = ["/usr/share/flatpak"]
 pkgdesc = "Linux application sandboxing and distribution framework"
 maintainer = "eater <=@eater.me>"
 license = "LGPL-2.1-or-later"
 url = "https://flatpak.org"
 source = f"https://github.com/flatpak/flatpak/releases/download/{pkgver}/flatpak-{pkgver}.tar.xz"
 sha256 = "5b70c64ce7ac134e1ea08011256e423ae5c54f277297441583f77d013f27ffac"
-file_modes = {
-    "var/lib/flatpak": ("_flatpak", "_flatpak", 0o755),
-}
 # test runner expects a different env (possible FIXME?)
 options = ["!check", "!cross"]
 
-system_users = [
-    {
-        "name": "_flatpak",
-        "id": None,
-        "home": "/var/lib/flatpak",
-    }
-]
-
 
 def post_install(self):
-    self.rm(self.destdir / "usr/lib/systemd", recursive=True),
-    self.rm(self.destdir / "usr/lib/sysusers.d", recursive=True)
-
-    self.install_dir("var/lib/flatpak", empty=True)
-
+    self.rm(self.destdir / "usr/lib/systemd", recursive=True)
+    self.rm(self.destdir / "usr/lib/tmpfiles.d/flatpak.conf")
+    self.install_file(
+        self.files_path / "tmpfiles.conf",
+        "usr/lib/tmpfiles.d",
+        name="flatpak.conf",
+    )
     self.install_file(
         self.files_path / "modules-load.conf",
         "usr/lib/modules-load.d",
