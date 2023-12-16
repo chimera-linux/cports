4a4fdae0d970ac44603b8f8edd4dff635b978971
q66 <q66@chimera-linux.org>
2023-12-16 17:45:25 +0100

    contrib/radeontop: new package (1.4)

---
+++ contrib/radeontop/template.py
@@ -0,0 +1,16 @@
+pkgname = "radeontop"
+pkgver = "1.4"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_use_env = True
+hostmakedepends = ["gettext", "gmake", "pkgconf"]
+makedepends = ["linux-headers", "libdrm-devel", "libxcb-devel", "ncurses-devel"]
+pkgdesc = "Radeon GPU utilization tool"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-3.0-only"
+url = "https://github.com/clbr/radeontop"
+source = f"{url}/archive/v{pkgver}.tar.gz"
+sha256 = "2c1e2aace1a749d8e4530047ce245004e0f7d1d32a99037917e03d83e60f7ad1"
+# no tests
+options = ["!check"]
