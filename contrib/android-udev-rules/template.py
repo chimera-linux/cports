8754075cb78ffaaf83df2211ecb24723eee16a4f
q66 <q66@chimera-linux.org>
2023-12-17 04:07:35 +0100

    contrib/android-udev-rules: use sysusers.d

--- contrib/android-udev-rules/template.py
+++ contrib/android-udev-rules/template.py
@@ -1,6 +1,6 @@
 pkgname = "android-udev-rules"
 pkgver = "20231207"
-pkgrel = 0
+pkgrel = 1
 pkgdesc = (
     "Android udev rules list aimed to be the most comprehensive on the net"
 )
@@ -10,8 +10,8 @@ url = "https://github.com/M0Rf30/android-udev-rules"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "f8f5513e08cd0e9299ba707c9acc5dec53b9cbdc255d4bfd83abfd378928b6e6"
 options = ["!splitudev"]
-system_groups = ["adbusers"]
 
 
 def do_install(self):
+    self.install_file(self.files_path / "adbusers.conf", "usr/lib/sysusers.d")
     self.install_file("51-android.rules", "usr/lib/udev/rules.d")
