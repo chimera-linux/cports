326238fc4df03e00f3d8d8c2d01d16b7f2b133a0
q66 <q66@chimera-linux.org>
2023-12-17 04:09:21 +0100

    contrib/keyd: use sysusers.d

--- contrib/keyd/template.py
+++ contrib/keyd/template.py
@@ -1,6 +1,6 @@
 pkgname = "keyd"
 pkgver = "2.4.3"
-pkgrel = 1
+pkgrel = 2
 build_style = "makefile"
 make_cmd = "gmake"
 make_use_env = True
@@ -15,9 +15,9 @@ sha256 = "d4075f673879f4950c1c0d43985797603200e993596a5d7fcec1597c2350c380"
 hardening = ["vis", "cfi"]
 # no tests available
 options = ["!check"]
-system_groups = ["keyd"]
 
 
 def post_install(self):
     self.install_license("LICENSE")
     self.install_service(self.files_path / "keyd")
+    self.install_file(self.files_path / "keyd.conf", "usr/lib/sysusers.d")
