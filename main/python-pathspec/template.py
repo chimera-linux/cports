871a682255c0098ef2a7018c98715388c3bffc24
psykose <alice@ayaya.dev>
2023-12-18 02:20:26 +0000

    main/python-pathspec: update to 0.12.1

--- main/python-pathspec/template.py
+++ main/python-pathspec/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pathspec"
-pkgver = "0.11.2"
+pkgver = "0.12.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,6 +14,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MPL-2.0"
 url = "https://github.com/cpburnz/python-pathspec"
 source = f"$(PYPI_SITE)/p/pathspec/pathspec-{pkgver}.tar.gz"
-sha256 = "e0d8d0ac2f12da61956eb2306b69f9469b42f4deb0f3cb6ed47b9cce9996ced3"
+sha256 = "a482d51503a1ab33b1c67a6c3813a26953dbdc71c31dacaef9a838c4e29f5712"
 # cycle
 options = ["!check"]
