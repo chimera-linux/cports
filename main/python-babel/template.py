2ebc0539b3490be352042599c56ae80067227c40
psykose <alice@ayaya.dev>
2023-12-18 02:25:32 +0000

    main/python-babel: update to 2.14.0

--- main/python-babel/template.py
+++ main/python-babel/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-babel"
-pkgver = "2.13.1"
+pkgver = "2.14.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://babel.pocoo.org"
 source = f"$(PYPI_SITE)/B/Babel/Babel-{pkgver}.tar.gz"
-sha256 = "33e0952d7dd6374af8dbf6768cc4ddf3ccfefc244f9986d4074704f2fbd18900"
+sha256 = "6919867db036398ba21eb5c7a0f6b28ab8cbc3ae7a73a44ebe34ae74a4e7d363"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
