902f2b19dd21650a7dff7f29bd7df97a885940b9
psykose <alice@ayaya.dev>
2023-12-18 09:58:54 +0000

    contrib/python-versioneer: new package (0.29)

---
+++ contrib/python-versioneer/template.py
@@ -0,0 +1,23 @@
+pkgname = "python-versioneer"
+pkgver = "0.29"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = ["python"]
+checkdepends = ["python-pytest"]
+pkgdesc = "Version-string manager for python-setuptools projects"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Unlicense"
+url = "https://github.com/python-versioneer/python-versioneer"
+source = f"$(PYPI_SITE)/v/versioneer/versioneer-{pkgver}.tar.gz"
+sha256 = "5ab283b9857211d61b53318b7c792cf68e798e765ee17c27ade9f6c924235731"
+
+
+def do_check(self):
+    self.do("python", "setup.py", "make_versioneer")
+    self.do("python", "-m", "unittest", "discover", "test")
