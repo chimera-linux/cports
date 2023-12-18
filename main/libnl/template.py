da4ffe30162ab50174fed202773232cb69e45bf0
psykose <alice@ayaya.dev>
2023-12-18 05:52:22 +0000

    main/libnl: update to 3.9.0

--- main/libnl/template.py
+++ main/libnl/template.py
@@ -1,17 +1,17 @@
 pkgname = "libnl"
-pkgver = "3.8.0"
+pkgver = "3.9.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-static"]
 make_cmd = "gmake"
-hostmakedepends = ["gmake", "pkgconf", "flex", "bison"]
+hostmakedepends = ["automake", "gmake", "libtool", "pkgconf", "flex", "bison"]
 makedepends = ["linux-headers"]
 pkgdesc = "Netlink Protocol Library Suite"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-only"
 url = "https://github.com/thom311/libnl"
 source = f"{url}/releases/download/libnl{pkgver.replace('.', '_')}/libnl-{pkgver}.tar.gz"
-sha256 = "bb726c6d7a08b121978d73ff98425bf313fa26a27a331d465e4f1d7ec5b838c6"
+sha256 = "aed507004d728a5cf11eab48ca4bf9e6e1874444e33939b9d3dfed25018ee9bb"
 
 
 @subpackage("libnl-devel")
@@ -27,6 +27,3 @@ def _progs(self):
             "usr/lib/libnl",
         ],
     )
-
-
-configure_gen = []
