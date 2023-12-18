f77db598d3905675ff1cc650a49c1d33e20ece23
psykose <alice@ayaya.dev>
2023-12-18 02:16:25 +0000

    main/python-fonttools: update to 4.46.0

--- main/python-fonttools/template.py
+++ main/python-fonttools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-fonttools"
-pkgver = "4.45.0"
+pkgver = "4.46.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT AND OFL-1.1 AND BSD-3-Clause AND Apache-2.0"
 url = "https://github.com/fonttools/fonttools"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "4e52b0a765dc8294e6caf15c68cce59aa2b8ad4b33d08f611002fdbbd6b2fe9e"
+sha256 = "b4f416f1c4160076912f9cc1b0d3b0085215836e4ffe0ef7c704d3a22c53e1e0"
 # unpackaged deps
 options = ["!check"]
 
