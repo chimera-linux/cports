7e52093aebd01d56c18260f10f3c58d171ed7333
psykose <alice@ayaya.dev>
2023-12-18 05:37:47 +0000

    main/opencl-headers: update to 2023.12.14

--- main/opencl-headers/template.py
+++ main/opencl-headers/template.py
@@ -1,12 +1,12 @@
 pkgname = "opencl-headers"
-pkgver = "2023.04.17"
+pkgver = "2023.12.14"
 pkgrel = 0
 pkgdesc = "OpenCL header files"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/KhronosGroup/OpenCL-Headers"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "0ce992f4167f958f68a37918dec6325be18f848dee29a4521c633aae3304915d"
+sha256 = "407d5e109a70ec1b6cd3380ce357c21e3d3651a91caae6d0d8e1719c69a1791d"
 # no test suite
 options = ["!check"]
 
