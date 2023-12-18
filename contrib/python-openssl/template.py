a3b24d94826af2db979ed412d17435dfc817dda9
psykose <alice@ayaya.dev>
2023-12-18 10:30:30 +0000

    contrib/python-openssl: run tests
    and convert to pep517

--- contrib/python-openssl/template.py
+++ contrib/python-openssl/template.py
@@ -1,20 +1,22 @@
 pkgname = "python-openssl"
 pkgver = "23.3.0"
 pkgrel = 0
-build_style = "python_module"
-hostmakedepends = ["python-setuptools"]
+build_style = "python_pep517"
+make_check_env = {"PYTHONPATH": "build/lib"}
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-wheel",
+]
 depends = ["python-cryptography"]
 checkdepends = [
-    "python-pytest",
     "python-flaky",
     "python-pretend",
-    "python-cryptography",
-]
+    "python-pytest",
+] + depends
 pkgdesc = "Python interface to OpenSSL"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://pyopenssl.org"
 source = f"$(PYPI_SITE)/p/pyOpenSSL/pyOpenSSL-{pkgver}.tar.gz"
 sha256 = "6b2cba5cc46e822750ec3e5a81ee12819850b11303630d575e98108a079c2b12"
-# unpackaged checkdepends
-options = ["!check"]
