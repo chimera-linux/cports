128222162de64901a1770916ab13c6a361e00366
psykose <alice@ayaya.dev>
2023-12-18 10:48:51 +0000

    main/clang-rt-cross: update to 17.0.6

--- main/clang-rt-cross/template.py
+++ main/clang-rt-cross/template.py
@@ -1,5 +1,5 @@
 pkgname = "clang-rt-cross"
-pkgver = "17.0.5"
+pkgver = "17.0.6"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -53,7 +53,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://llvm.org"
 source = f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{pkgver}/llvm-project-{pkgver}.src.tar.xz"
-sha256 = "95d7eff82945cf05c16a1851d7b391fc2da726b87c1138125e3b6e4d300ab834"
+sha256 = "58a8818c60e6627064f312dbf46c02d9949956558340938b71cf731ad8bc0813"
 # crosstoolchain
 options = ["!cross", "!check", "!lto"]
 
