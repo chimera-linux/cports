ecc6a67e331cc0ef111c0ebd058401430a4b009f
psykose <alice@ayaya.dev>
2023-12-18 05:29:39 +0000

    main/traceroute: update to 2.1.5

--- main/traceroute/template.py
+++ main/traceroute/template.py
@@ -1,5 +1,5 @@
 pkgname = "traceroute"
-pkgver = "2.1.4"
+pkgver = "2.1.5"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "http://traceroute.sourceforge.net"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "b2e39a1d04ea45a6a60c976a2637d1a16224edd2eaa19a5870af83edb2ffe3fa"
+sha256 = "9c6c260d96eaab51e3ce461b0a84fe87123ebc6dd6c9a59fab803f95b35a859e"
 tool_flags = {"CFLAGS": ["-D_GNU_SOURCE"]}
 hardening = ["vis", "cfi"]
 # no tests
