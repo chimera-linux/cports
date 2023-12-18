b51945ea3e330bd70f36b1489979eec307916e52
psykose <alice@ayaya.dev>
2023-12-18 05:38:45 +0000

    main/libwacom: update to 2.9.0

--- main/libwacom/template.py
+++ main/libwacom/template.py
@@ -1,17 +1,22 @@
 pkgname = "libwacom"
-pkgver = "2.8.0"
+pkgver = "2.9.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocumentation=disabled", "-Dtests=enabled"]
 hostmakedepends = ["meson", "pkgconf"]
-makedepends = ["libgudev-devel", "glib-devel", "libxml2-devel"]
+makedepends = [
+    "glib-devel",
+    "libevdev-devel",
+    "libgudev-devel",
+    "libxml2-devel",
+]
 checkdepends = ["bash"]
 pkgdesc = "Library to handle Wacom tablets"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/linuxwacom/libwacom"
 source = f"{url}/releases/download/{pkgname}-{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "bb04b12c8688d0ff6a108d47a38d2057d572c4d7227d78138abd5fd0ba59f215"
+sha256 = "6f678156797becc4f1537a19aadcc48ed7a54e1ff3cbf591d1233f8a2d82e242"
 
 
 def post_install(self):
