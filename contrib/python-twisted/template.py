ad98cbdf5a8d02d15320a08e0b90a98ff5a01836
psykose <alice@ayaya.dev>
2023-12-18 10:28:13 +0000

    contrib/python-twisted: update to 23.10.0

--- contrib/python-twisted/template.py
+++ contrib/python-twisted/template.py
@@ -1,22 +1,28 @@
 pkgname = "python-twisted"
-pkgver = "22.10.0"
+pkgver = "23.10.0"
 pkgrel = 0
-build_style = "python_module"
+build_style = "python_pep517"
 make_check_target = "tests"
-hostmakedepends = ["python-setuptools", "python-incremental"]
+hostmakedepends = [
+    "python-build",
+    "python-hatchling",
+    "python-incremental",
+    "python-installer",
+]
 makedepends = ["python-devel"]
 depends = [
     "python-attrs",
     "python-automat",
-    "python-cryptography",
     "python-constantly",
+    "python-cryptography",
     "python-h2",
     "python-hyperlink",
     "python-idna",
     "python-incremental",
-    "python-service-identity",
+    "python-openssl",
     "python-priority",
     "python-pyserial",
+    "python-service-identity",
     "python-typing_extensions",
     "python-zope.interface",
 ]
@@ -31,8 +37,8 @@ pkgdesc = "Asynchronous framework for Python"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://twistedmatrix.com"
-source = f"$(PYPI_SITE)/T/Twisted/Twisted-{pkgver}.tar.gz"
-sha256 = "32acbd40a94f5f46e7b42c109bfae2b302250945561783a8b7a059048f2d4d31"
+source = f"$(PYPI_SITE)/t/twisted/twisted-{pkgver}.tar.gz"
+sha256 = "987847a0790a2c597197613686e2784fd54167df3a55d0fb17c8412305d76ce5"
 # unpackaged checkdepends etc.
 options = ["!check"]
 
