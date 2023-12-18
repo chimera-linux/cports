e51dbfe44364cd76a745885b1ba5b7e9bbfa5dbc
psykose <alice@ayaya.dev>
2023-12-18 10:55:16 +0000

    main/libclc: update to 17.0.6

--- main/libclc/template.py
+++ main/libclc/template.py
@@ -1,5 +1,5 @@
 pkgname = "libclc"
-pkgver = "17.0.5"
+pkgver = "17.0.6"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DCMAKE_BUILD_TYPE=Release"]
@@ -21,7 +21,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://libclc.llvm.org"
 source = f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{pkgver}/{pkgname}-{pkgver}.src.tar.xz"
-sha256 = "08de715d000d551c2587d6c30cafb0a11ecd89b765429847570d14e6f1f93249"
+sha256 = "122f641d94d5dfbb3c37534f2b76612fa59d15c36c2a4917369a85eaaca32148"
 # should not matter much but FIXME
 hardening = ["vis", "!cfi"]
 # external-calls-clspv broken
