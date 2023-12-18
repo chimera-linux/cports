4313a7602993a913d01e281c3d6d7b39c82a7a69
psykose <alice@ayaya.dev>
2023-12-18 02:22:54 +0000

    main/python-hatchling: update to 1.20.0

--- main/python-hatchling/template.py
+++ main/python-hatchling/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-hatchling"
-pkgver = "1.18.0"
+pkgver = "1.20.0"
 pkgrel = 0
 build_style = "python_pep517"
 _deps = [
@@ -21,7 +21,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://hatch.pypa.io/latest"
 source = f"$(PYPI_SITE)/h/hatchling/hatchling-{pkgver}.tar.gz"
-sha256 = "50e99c3110ce0afc3f7bdbadff1c71c17758e476731c27607940cfa6686489ca"
+sha256 = "0e0893cbe3d5f9275fc0e5b629087fc23b17abd7065e4db0a310e0a0237bc945"
 # no tests?
 options = ["!check"]
 
