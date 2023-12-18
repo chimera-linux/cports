e2baad7c9593e07f617046cd85a62a2b5a193ea8
psykose <alice@ayaya.dev>
2023-12-18 19:10:42 +0000

    main/gst-plugins-good: update to 1.22.8

--- main/gst-plugins-good/template.py
+++ main/gst-plugins-good/template.py
@@ -1,5 +1,5 @@
 pkgname = "gst-plugins-good"
-pkgver = "1.22.7"
+pkgver = "1.22.8"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -68,7 +68,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "b6db0e18e398b52665b7cdce301c34a8750483d5f4fbac1ede9f80b03743cd15"
+sha256 = "e305b9f07f52743ca481da0a4e0c76c35efd60adaf1b0694eb3bb021e2137e39"
 # FIXME int (extra tests fail, look for SIGILL)
 hardening = ["!int"]
 # 4 out of 105 tests currently fail (qtmux, splitmux, pipelines_tagschecking)
