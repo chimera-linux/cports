862527ed073bfb24c21885a41acc823c9368c282
Mohammed Anas <triallax@tutanota.com>
2023-12-19 09:00:10 +0000

    contrib/chayang: new package (0.1.0)

---
+++ contrib/chayang/template.py
@@ -0,0 +1,24 @@
+pkgname = "chayang"
+pkgver = "0.1.0"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+    "wayland-devel",
+]
+makedepends = [
+    "wayland-devel",
+    "wayland-protocols",
+]
+pkgdesc = "Gradually dim the screen"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "MIT"
+url = "https://git.sr.ht"
+source = f"https://git.sr.ht/~emersion/{pkgname}/archive/v{pkgver}.tar.gz"
+sha256 = "61aa0e2955e8b5cf321ef14a5fd72f0e953da51a390d456e929b74fc5efcb74a"
+hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
