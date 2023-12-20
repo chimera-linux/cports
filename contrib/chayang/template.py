8d1001452515b50bd859307e7978935d28f76171
psykose <alice@ayaya.dev>
2023-12-20 00:26:52 +0000

    contrib/chayang: fix url

--- contrib/chayang/template.py
+++ contrib/chayang/template.py
@@ -1,6 +1,6 @@
 pkgname = "chayang"
 pkgver = "0.1.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 hostmakedepends = [
     "meson",
@@ -14,7 +14,7 @@ makedepends = [
 pkgdesc = "Gradually dim the screen"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "MIT"
-url = "https://git.sr.ht"
+url = "https://git.sr.ht/~emersion/chayang"
 source = f"https://git.sr.ht/~emersion/{pkgname}/archive/v{pkgver}.tar.gz"
 sha256 = "61aa0e2955e8b5cf321ef14a5fd72f0e953da51a390d456e929b74fc5efcb74a"
 hardening = ["vis", "cfi"]
