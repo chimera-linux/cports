33fe16ae1caa910b8bdf4166ddefae2d280a7ba3
psykose <alice@ayaya.dev>
2023-12-20 01:23:32 +0000

    contrib/basu: disable cfi for now
    crashes in object_find_and_run after getting any notification in mako

--- contrib/basu/template.py
+++ contrib/basu/template.py
@@ -1,6 +1,6 @@
 pkgname = "basu"
 pkgver = "0.2.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 hostmakedepends = [
     "gperf",
@@ -16,7 +16,8 @@ license = "LGPL-2.1-or-later"
 url = "https://sr.ht/~emersion/basu"
 source = f"https://git.sr.ht/~emersion/basu/archive/v{pkgver}.tar.gz"
 sha256 = "43b327073d1ac7bc6cbc0d3dfff729348fc970dfff0551ad40e366332e990204"
-hardening = ["vis", "cfi"]
+# FIXME: cfi
+hardening = ["vis", "!cfi"]
 
 
 @subpackage("basu-devel")
