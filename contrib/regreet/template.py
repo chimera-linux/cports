d1d1e8b32a1271ab36a449824e965779a549b057
natthias <natthias@proton.me>
2023-12-20 15:10:34 +0100

    contrib/regreet: new package (1.1.1)

---
+++ contrib/regreet/template.py
@@ -0,0 +1,27 @@
+pkgname = "regreet"
+pkgver = "0.1.1"
+pkgrel = 0
+build_style = "cargo"
+make_build_args = [
+    "--features=gtk4_8",
+]
+hostmakedepends = [
+    "cargo",
+    "pkgconf",
+]
+makedepends = [
+    "gtk4-devel",
+    "rust-std",
+]
+depends = ["greetd"]
+pkgdesc = "Clean and customizable greeter for greetd"
+maintainer = "natthias <natthias@proton.me>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/rharish101/ReGreet"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "a658c91cdf242dfea814f0bfd0c4d877bd39e3af498d36e5024061e3d07ea76b"
+
+
+def post_install(self):
+    self.install_file(self.files_path / "regreet.toml", "etc/greetd")
+    self.install_file(self.files_path / "regreet.conf", "usr/lib/tmpfiles.d")
