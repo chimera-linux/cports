fd1b4e5e9de3b3e79374ab95990ceb91065c728e
psykose <alice@ayaya.dev>
2023-12-21 10:04:22 +0000

    main/libcap-ng: update to 0.8.4

--- main/libcap-ng/template.py
+++ main/libcap-ng/template.py
@@ -1,16 +1,16 @@
 pkgname = "libcap-ng"
-pkgver = "0.8.3"
+pkgver = "0.8.4"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_args = ["--without-python", "--without-python3"]
-hostmakedepends = ["pkgconf"]
+configure_args = ["--without-python3"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 makedepends = ["linux-headers"]
 pkgdesc = "Alternate POSIX capabilities library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "http://people.redhat.com/sgrubb/libcap-ng"
 source = f"http://people.redhat.com/sgrubb/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "bed6f6848e22bb2f83b5f764b2aef0ed393054e803a8e3a8711cb2a39e6b492d"
+sha256 = "68581d3b38e7553cb6f6ddf7813b1fc99e52856f21421f7b477ce5abd2605a8a"
 
 
 @subpackage("libcap-ng-devel")
@@ -22,6 +22,3 @@ def _devel(self):
 @subpackage("libcap-ng-progs")
 def _progs(self):
     return self.default_progs(man="178")
-
-
-configure_gen = []
