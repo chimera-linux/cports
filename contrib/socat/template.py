05c1b0bc30f0f74d850b4615433f08f701570de8
psykose <alice@ayaya.dev>
2023-12-22 21:15:30 +0000

    contrib/socat: correct the licence
    oop

--- contrib/socat/template.py
+++ contrib/socat/template.py
@@ -13,7 +13,7 @@ makedepends = [
 ]
 pkgdesc = "Multipurpose relay for binary protocols"
 maintainer = "psykose <alice@ayaya.dev>"
-license = "GPL-2.0-or-later"
+license = "GPL-2.0-only"
 url = "http://www.dest-unreach.org/socat"
 source = f"{url}/download/socat-{pkgver}.tar.gz"
 sha256 = "6010f4f311e5ebe0e63c77f78613d264253680006ac8979f52b0711a9a231e82"
