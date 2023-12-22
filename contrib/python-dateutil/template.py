6cd1bfce46a2dd883ddd3e70803dbdc26f562a97
psykose <alice@ayaya.dev>
2023-12-22 18:47:48 +0000

    contrib/python-dateutil: new package (2.8.2)

---
+++ contrib/python-dateutil/template.py
@@ -0,0 +1,20 @@
+pkgname = "python-dateutil"
+pkgver = "2.8.2"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools_scm",
+    "python-wheel",
+]
+depends = ["python-six"]
+checkdepends = ["python-pytest"] + depends
+pkgdesc = "Extensions for python datetime"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Apache-2.0"
+url = "https://github.com/dateutil/dateutil"
+source = f"$(PYPI_SITE)/p/python-dateutil/python-dateutil-{pkgver}.tar.gz"
+sha256 = "0123cacc1627ae19ddf3c27a5de5bd67ee4586fbdd6440d9748f8abb483d3e86"
+# pytest-cov
+options = ["!check"]
