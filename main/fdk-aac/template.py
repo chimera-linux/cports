59f93d0833e8170a82d2a74c2884e0b6d6f657cc
psykose <alice@ayaya.dev>
2023-12-22 01:53:33 +0000

    main/fdk-aac: update to 2.0.3

--- main/fdk-aac/template.py
+++ main/fdk-aac/template.py
@@ -1,14 +1,14 @@
 pkgname = "fdk-aac"
-pkgver = "2.0.2"
+pkgver = "2.0.3"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 pkgdesc = "Fraunhofer FDK AAC library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:FDK-AAC"
 url = "http://www.iis.fraunhofer.de/en/ff/amm/impl/fdkaaccodec.html"
 source = f"$(SOURCEFORGE_SITE)/opencore-amr/{pkgname}-{pkgver}.tar.gz"
-sha256 = "c9e8630cf9d433f3cead74906a1520d2223f89bcd3fa9254861017440b8eb22f"
+sha256 = "829b6b89eef382409cda6857fd82af84fabb63417b08ede9ea7a553f811cb79e"
 
 
 def post_install(self):
@@ -18,6 +18,3 @@ def post_install(self):
 @subpackage("fdk-aac-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
