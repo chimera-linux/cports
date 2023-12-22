a2926d40c43034a11762f0f5678643e836dda259
psykose <alice@ayaya.dev>
2023-12-22 02:01:05 +0000

    main/iw: update to 6.7

--- main/iw/template.py
+++ main/iw/template.py
@@ -1,5 +1,5 @@
 pkgname = "iw"
-pkgver = "5.19"
+pkgver = "6.7"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "ISC"
 url = "http://wireless.kernel.org/en/users/Documentation/iw"
 source = f"http://www.kernel.org/pub/software/network/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "f167bbe947dd53bb9ebc0c1dcef5db6ad73ac1d6084f2c6f9376c5c360cc4d4e"
+sha256 = "aacf49c266b29d500d73086798a1c652e760c19126a8599fd811850430789a35"
 hardening = ["vis", "cfi"]
 
 
