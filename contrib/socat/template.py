89f3c1332ee3105f23fa1f59dd44c3120d09d1f8
Subhaditya Nath <sn03.general@gmail.com>
2023-12-24 01:56:55 +0530

    contrib/socat: fix BUILD_DATE, mv *.sh -> examples

--- contrib/socat/template.py
+++ contrib/socat/template.py
@@ -1,6 +1,6 @@
 pkgname = "socat"
 pkgver = "1.8.0.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 # broken
 configure_gen = []
@@ -11,6 +11,7 @@ makedepends = [
     "openssl-devel",
     "readline-devel",
 ]
+checkdepends = ["bash", "iproute2", "procps"]
 pkgdesc = "Multipurpose relay for binary protocols"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-only"
@@ -18,5 +19,14 @@ url = "http://www.dest-unreach.org/socat"
 source = f"{url}/download/socat-{pkgver}.tar.gz"
 sha256 = "6010f4f311e5ebe0e63c77f78613d264253680006ac8979f52b0711a9a231e82"
 hardening = ["vis", "cfi"]
-# needs a smorgasbord of random network utilities
+# tests have heisenbugs
 options = ["!check"]
+
+
+def post_install(self):
+    self.install_dir("usr/share/examples/socat")
+    self.mv(
+        self.destdir / "usr/bin/socat-*.sh",
+        self.destdir / "usr/share/examples/socat",
+        glob=True,
+    )
