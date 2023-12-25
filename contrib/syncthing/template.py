e93c52d3de26f0789abc85348b27ca7fd311fc88
Mohammed Anas <triallax@tutanota.com>
2023-12-25 19:06:57 +0000

    contrib/syncthing: new package (1.27.1)

---
+++ contrib/syncthing/template.py
@@ -0,0 +1,70 @@
+pkgname = "syncthing"
+pkgver = "1.27.1"
+pkgrel = 0
+build_style = "go"
+make_build_args = [
+    f"-ldflags=-X github.com/syncthing/syncthing/lib/build.Version=v{pkgver}",
+    "./cmd/stdiscosrv",
+    "./cmd/strelaysrv",
+    "./cmd/syncthing",
+]
+make_check_args = [
+    "./lib/...",
+    "./cmd/stdiscosrv",
+    "./cmd/strelaysrv",
+    "./cmd/syncthing",
+]
+hostmakedepends = ["go"]
+go_build_tags = ["noupgrade"]
+pkgdesc = "Continuous file synchronization program"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "MPL-2.0"
+url = "https://syncthing.net"
+source = f"https://github.com/syncthing/{pkgname}/archive/v{pkgver}.tar.gz"
+sha256 = "e91671a8291a6d83264f1d1ed731fb6041b25d7259400e5f71cb2d241a48e6a4"
+
+
+def pre_build(self):
+    self.do("go", "generate", "github.com/syncthing/syncthing/lib/api/auto")
+
+
+def post_install(self):
+    self.install_license("cmd/strelaysrv/LICENSE", pkgname="syncthing-relaysrv")
+    self.install_file("etc/firewall-ufw/syncthing", "etc/ufw/applications.d")
+    self.install_file(
+        "etc/linux-desktop/*.desktop", "usr/share/applications", glob=True
+    )
+    self.install_file("etc/linux-sysctl/30-syncthing.conf", "usr/lib/sysctl.d")
+
+    self.install_man("man/*.[157]", glob=True)
+
+    for f in [32, 64, 128, 256, 512]:
+        self.install_file(
+            f"assets/logo-{f}.png",
+            f"usr/share/icons/hicolor/{f}x{f}/apps",
+            name="syncthing.png",
+        )
+    self.install_file(
+        "assets/logo-only.svg",
+        "usr/share/icons/hicolor/scalable/apps",
+        name="syncthing.svg",
+    )
+
+
+@subpackage("syncthing-relaysrv")
+def _relaysrv(self):
+    self.pkgdesc = f"{pkgdesc} (relay server)"
+    self.license = "MIT"
+
+    return [
+        "usr/bin/strelaysrv",
+        "usr/share/licenses/syncthing-relaysrv",
+        "usr/share/man/man1/strelaysrv.1",
+    ]
+
+
+@subpackage("syncthing-discosrv")
+def _discosrv(self):
+    self.pkgdesc = f"{pkgdesc} (discovery server)"
+
+    return ["usr/bin/stdiscosrv", "usr/share/man/man1/stdiscosrv.1"]
