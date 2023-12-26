34ee9eb32bf4fe7ac542c977e4b648eaf5b6b010
psykose <alice@ayaya.dev>
2023-12-26 22:19:53 +0000

    contrib/docker-compose: new package (2.23.3)

---
+++ contrib/docker-compose/template.py
@@ -0,0 +1,27 @@
+pkgname = "docker-compose"
+pkgver = "2.23.3"
+pkgrel = 0
+build_style = "go"
+make_build_args = [
+    "-ldflags",
+    f"-X github.com/docker/compose/v2/internal.Version=v{pkgver}",
+    "./cmd",
+]
+hostmakedepends = ["go"]
+depends = ["docker-cli"]
+pkgdesc = "Docker CLI plugin for compose files"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Apache-2.0"
+url = "https://docs.docker.com/compose"
+source = f"https://github.com/docker/compose/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "29ba96c8d398fbc6f7c791c65e70b97e7df116223f2996062441093258d914fe"
+# need a running docker daemon
+options = ["!debug", "!check"]
+
+
+def do_install(self):
+    self.install_file(
+        f"{self.make_dir}/cmd",
+        "usr/libexec/docker/cli-plugins",
+        name="docker-compose",
+    )
