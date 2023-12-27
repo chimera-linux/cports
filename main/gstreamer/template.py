2b21fb8800a42b056c8f1a3c8219665ce2822fd8
psykose <alice@ayaya.dev>
2023-12-27 22:26:56 +0000

    main/gstreamer: fix ptp-helper caps
    the previous version setcap on the file in destdir but the final package
    never ends up having this, fix this by passing the caps to apk
    
    also |sort :^)

--- main/gstreamer/template.py
+++ main/gstreamer/template.py
@@ -1,31 +1,31 @@
 pkgname = "gstreamer"
 pkgver = "1.22.8"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
-    "-Dptp-helper-permissions=capabilities",
+    "-Dptp-helper-permissions=none",  # manual
     "-Dpackage-origin=https://chimera-linux.org",
     "-Ddbghelp=disabled",
     "-Dintrospection=enabled",
     "-Ddefault_library=shared",
 ]
 hostmakedepends = [
+    "bison",
+    "docbook-xsl-nons",
+    "flex",
+    "gettext",
+    "glib-devel",
+    "gobject-introspection",
+    "libcap-progs",
     "meson",
     "pkgconf",
-    "gettext",
-    "flex",
     "python",
-    "docbook-xsl-nons",
-    "bison",
-    "glib-devel",
-    "libcap-progs",
-    "gobject-introspection",
 ]
 makedepends = [
-    "libxml2-devel",
+    "bash-completion",
     "glib-devel",
     "libcap-devel",
-    "bash-completion",
+    "libxml2-devel",
 ]
 pkgdesc = "Core GStreamer libraries and elements"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -33,6 +33,11 @@ license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "ad4e3db1771139b1db17b1afa7c05db083ae0100bd4da244b71f162dcce41bfc"
+file_xattrs = {
+    "usr/libexec/gstreamer-1.0/gst-ptp-helper": {
+        "security.capability": "cap_net_bind_service,cap_net_admin+ep",
+    },
+}
 options = ["!cross"]
 
 
