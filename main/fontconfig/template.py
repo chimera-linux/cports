96b18e1245f0e7d4029992653ed71156f6b2c523
psykose <alice@ayaya.dev>
2023-12-27 19:12:07 +0000

    main/fontconfig: replace use of 70-no-bitmaps.conf with older scalable version
    in 2.15.0 https://gitlab.freedesktop.org/fontconfig/fontconfig/-/commit/7aa647eb9ee46d98b6172c16e9727aa2bd222627
    no-bitmaps was changed from rejecting non-'scalable' fonts to rejecting
    non-'outline' fonts. our intention is to block bitmap fonts by default
    -- because they are not scalable -- but not to block anything else. an
    "outline" font is one that is geometrically defined, and "color emoji"
    fonts like noto-emoji are considered "scalable" but not "outline". this
    means that the new no-bitmaps rejection of non-outline fonts rejects
    color fonts too. replace it with the old version instead.

--- main/fontconfig/template.py
+++ main/fontconfig/template.py
@@ -1,6 +1,6 @@
 pkgname = "fontconfig"
 pkgver = "2.15.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = [
     "--enable-static",
@@ -29,10 +29,14 @@ sha256 = "f5f359d6332861bd497570848fcb42520964a9e83d5e3abe397b6b6db9bcaaf4"
 
 def post_install(self):
     self.install_license("COPYING")
+    self.install_file(
+        self.files_path / "70-no-nonscalable.conf",
+        "usr/share/fontconfig/conf.avail",
+    )
     # reject bitmap fonts by default, preventing them from being preferred
     self.install_link(
-        "/usr/share/fontconfig/conf.avail/70-no-bitmaps.conf",
-        "etc/fonts/conf.d/70-no-bitmaps.conf",
+        "/usr/share/fontconfig/conf.avail/70-no-nonscalable.conf",
+        "etc/fonts/conf.d/70-no-nonscalable.conf",
     )
 
 
