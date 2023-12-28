cfa24686ff263fdc9f7b6cda7a1fe501d44b6201
psykose <alice@ayaya.dev>
2023-12-28 11:48:53 +0000

    contrib/aardvark-dns: update to 1.9.0

--- contrib/aardvark-dns/template.py
+++ contrib/aardvark-dns/template.py
@@ -1,5 +1,5 @@
 pkgname = "aardvark-dns"
-pkgver = "1.8.0"
+pkgver = "1.9.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo"]
@@ -9,4 +9,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/containers/aardvark-dns"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "c9b818110e3d5d45f8bdb3c9ccc48c994aedb0b19fefcc7577fc1ef7ed294343"
+sha256 = "d6b51743d334c42ec98ff229be044b5b2a5fedf8da45a005447809c4c1e9beea"
