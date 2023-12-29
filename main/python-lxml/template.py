dbae115d00a4b3b2983117a9d3cd1773715ad78b
psykose <alice@ayaya.dev>
2023-12-29 18:21:24 +0000

    main/python-lxml: update to 4.9.4

--- main/python-lxml/template.py
+++ main/python-lxml/template.py
@@ -1,16 +1,26 @@
 pkgname = "python-lxml"
-pkgver = "4.9.3"
+pkgver = "4.9.4"
 pkgrel = 0
-build_style = "python_module"
-make_build_args = ["--with-cython"]
-hostmakedepends = ["python-cython", "python-setuptools"]
+build_style = "python_pep517"
+make_build_env = {"WITH_CYTHON": "true"}
+hostmakedepends = [
+    "python-build",
+    "python-cython",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
 makedepends = ["libxslt-devel", "libxml2-devel", "python-devel"]
 pkgdesc = "Python bindings for the libxml2 and libxslt libraries"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause AND custom:ElementTree"
 url = "https://lxml.de"
 source = f"https://github.com/lxml/lxml/archive/lxml-{pkgver}.tar.gz"
-sha256 = "42b9ab83cb8739d817c7fff41c20f31aa61625bb6f6ab333873a5f3406b139ac"
+sha256 = "0aff5e6d17cf92262d49ca8d5ac51fc686810cea1771842d7c9f1800a32b61a1"
+# https://bugs.gentoo.org/917562
+tool_flags = {
+    "CFLAGS": ["-Wno-incompatible-function-pointer-types"],
+}
 # missing checkdepends
 options = ["!check"]
 
