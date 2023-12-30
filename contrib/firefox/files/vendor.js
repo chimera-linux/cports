3249da8986249cfc046579567a41d5c8b82acdb3
q66 <q66@chimera-linux.org>
2023-12-30 17:40:43 +0100

    contrib/firefox: disable new tab page ads

--- contrib/firefox/files/vendor.js
+++ contrib/firefox/files/vendor.js
@@ -7,3 +7,6 @@ pref("browser.shell.checkDefaultBrowser", false);
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
+
+// Disable some advertising tile garbage on the new tab page
+pref("browser.topsites.contile.enabled", false);
