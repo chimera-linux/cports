d7f5e91945af9e279aeb2d85f7bc3e614268c2fe
Jami Kettunen <jami.kettunen@protonmail.com>
2024-01-02 00:49:16 +0200

    main/hwdata: update to 0.377-2, mark empty

--- main/hwdata/template.py
+++ main/hwdata/template.py
@@ -1,5 +1,6 @@
 pkgname = "hwdata"
-pkgver = "0.376"
+pkgver = "0.377.2"
+_pkgver = "0.377-2"
 pkgrel = 0
 build_style = "configure"
 configure_args = [
@@ -22,10 +23,10 @@ pkgdesc = "Hardware identification databases"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/vcrhonek/hwdata"
-source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "48d85dbf05650b2c382ffaadeb601cac1650f5a34ee5c452df8021af988ea090"
+source = f"{url}/archive/refs/tags/v{_pkgver}.tar.gz"
+sha256 = "e1e79c12b975298ade94a9cf3f76d24a07ef2d25aa0bf4eebaeac9328ce7a2a7"
 # cycle with pciutils, etc.
-options = ["!check"]
+options = ["!check", "empty"]
 
 
 @subpackage("hwdata-devel")
