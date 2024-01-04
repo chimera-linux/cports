d8099e13e5bfb1f7130a410f3d785a8e862f2845
psykose <alice@ayaya.dev>
2024-01-04 19:09:29 +0000

    main/shaderc: update to 2023.8

--- main/shaderc/template.py
+++ main/shaderc/template.py
@@ -1,8 +1,12 @@
 pkgname = "shaderc"
-pkgver = "2023.7"
-pkgrel = 2
+pkgver = "2023.8"
+pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DSHADERC_SKIP_TESTS=ON", "-DSHADERC_SKIP_EXAMPLES=ON"]
+configure_args = [
+    "-DSHADERC_SKIP_TESTS=ON",
+    "-DSHADERC_SKIP_EXAMPLES=ON",
+    "-DPYTHON_EXECUTABLE=python",
+]
 hostmakedepends = ["cmake", "ninja", "python", "pkgconf"]
 makedepends = ["spirv-tools-devel", "spirv-headers", "glslang-devel"]
 pkgdesc = "Collection of tools and libraries for shader compilation"
@@ -10,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/google/shaderc"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "681e1340726a0bf46bea7e31f10cbfe78e01e4446a35d90fedc2b78d400fcdeb"
+sha256 = "dfec5045f30d8f6d3d3914ab5b3cc2695947f266d41261b1459177cd789308d1"
 tool_flags = {
     "CXXFLAGS": [f"-I{self.profile().sysroot / 'usr/include/glslang'}"]
 }
