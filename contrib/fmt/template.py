36793c80aceede28c366dcbc580891fae250adc8
psykose <alice@ayaya.dev>
2024-01-04 21:50:44 +0000

    contrib/fmt: update to 10.2.1

--- contrib/fmt/template.py
+++ contrib/fmt/template.py
@@ -1,12 +1,12 @@
 pkgname = "fmt"
-pkgver = "10.2.0"
+pkgver = "10.2.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DBUILD_SHARED_LIBS=ON",
 ]
 # FIXME: random musl issues
-make_check_args = ["-E", "(chrono|format|unicode|xchar)"]
+make_check_args = ["-E", "(chrono|unicode|xchar)"]
 hostmakedepends = [
     "cmake",
     "ninja",
@@ -20,7 +20,7 @@ url = "https://fmt.dev/latest/index.html"
 source = (
     f"https://github.com/fmtlib/fmt/releases/download/{pkgver}/fmt-{pkgver}.zip"
 )
-sha256 = "8a942861a94f8461a280f823041cde8f620a6d8b0e0aacc98c15bb5a9dd92399"
+sha256 = "312151a2d13c8327f5c9c586ac6cf7cddc1658e8f53edae0ec56509c8fa516c9"
 # FIXME: cfi test failures
 hardening = ["vis"]
 
