4c4ad42b5295878c0d1d845cdfcfdcf68549ee19
psykose <alice@ayaya.dev>
2024-01-04 19:04:51 +0000

    main/perl-xml-parser: update to 2.47

--- main/perl-xml-parser/template.py
+++ main/perl-xml-parser/template.py
@@ -1,6 +1,6 @@
 pkgname = "perl-xml-parser"
-pkgver = "2.46"
-pkgrel = 1
+pkgver = "2.47"
+pkgrel = 0
 build_style = "perl_module"
 make_build_args = ["MAKE=gmake"]
 make_install_args = ["MAKE=gmake"]
@@ -13,4 +13,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/release/XML-Parser"
 source = f"$(CPAN_SITE)/XML/XML-Parser-{pkgver}.tar.gz"
-sha256 = "d331332491c51cccfb4cb94ffc44f9cd73378e618498d4a37df9e043661c515d"
+sha256 = "ad4aae643ec784f489b956abe952432871a622d4e2b5c619e8855accbfc4d1d8"
