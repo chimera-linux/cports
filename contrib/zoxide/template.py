e91b223da5ac5c6c5215f8916c0b5cdcda0ab5d2
Paul A. Patience <paul@apatience.com>
2024-01-05 20:59:49 -0500

    contrib/zoxide: use install_completion

--- contrib/zoxide/template.py
+++ contrib/zoxide/template.py
@@ -1,6 +1,6 @@
 pkgname = "zoxide"
 pkgver = "0.9.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "cargo"
 hostmakedepends = ["cargo"]
 makedepends = ["rust-std"]
@@ -15,16 +15,6 @@ sha256 = "a6c2d993a02211c3d23b242c2c6faab9a2648be7a45ad1ff0586651ac827e914"
 def post_install(self):
     self.install_license("LICENSE")
     self.install_man("man/man1/*.1", glob=True)
-    self.install_file(
-        "contrib/completions/zoxide.bash",
-        "usr/share/bash-completion/completions",
-        name="zoxide",
-    )
-    self.install_file(
-        "contrib/completions/_zoxide",
-        "usr/share/zsh/site-functions",
-    )
-    self.install_file(
-        "contrib/completions/zoxide.fish",
-        "usr/share/fish/completions",
-    )
+    self.install_completion("contrib/completions/zoxide.bash", "bash")
+    self.install_completion("contrib/completions/zoxide.fish", "fish")
+    self.install_completion("contrib/completions/_zoxide", "zsh")
