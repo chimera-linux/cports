247117dc4f9959dfd5ea8d6199d31ff97c6fc429
psykose <alice@ayaya.dev>
2024-01-08 11:43:47 +0000

    contrib/lib_users: new package (0.15)

---
+++ contrib/lib_users/template.py
@@ -0,0 +1,17 @@
+pkgname = "lib_users"
+pkgver = "0.15"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "Checks for deleted system libraries still in use"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-only"
+url = "https://github.com/klausman/lib_users"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "d17f6d2ab633cf5826921c2757e33806495c1db076e13c16c4bc6c6a73d6e2fe"
