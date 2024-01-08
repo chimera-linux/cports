90c554367b7810bcb783855b7d98143581e0b3aa
Erica Z <zerica@callcc.eu>
2024-01-08 16:48:30 +0100

    contrib/btrbk: new package (0.32.6)

---
+++ contrib/btrbk/template.py
@@ -0,0 +1,21 @@
+pkgname = "btrbk"
+pkgver = "0.32.6"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "asciidoctor"]
+depends = ["perl", "btrfs-progs", "openssh", "mbuffer"]
+pkgdesc = "Backup tool for btrfs subvolumes"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "GPL-3.0-or-later"
+url = "https://digint.ch/btrbk"
+source = (
+    f"https://digint.ch/download/{pkgname}/releases/{pkgname}-{pkgver}.tar.xz"
+)
+sha256 = "02e2ac647c918463202cbe607bb95557a4f7fd237069124333c54da5b2bbb76b"
+# no tests
+options = ["!check"]
+
+
+def post_install(self):
+    self.rm(self.destdir / "usr/lib/systemd", recursive=True)
