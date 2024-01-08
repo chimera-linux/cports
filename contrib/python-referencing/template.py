85c1fc8a248dba89732769654acae369a33a7bf4
miko <mikoxyzzz@gmail.com>
2024-01-08 18:55:33 +0100

    contrib/python-referencing: new package (0.32.1)

---
+++ contrib/python-referencing/template.py
@@ -0,0 +1,37 @@
+pkgname = "python-referencing"
+pkgver = "0.32.1"
+pkgrel = 0
+build_style = "python_pep517"
+# the tests in suite/ depend on jsonschema
+make_check_args = ["referencing"]
+hostmakedepends = [
+    "python",
+    "python-build",
+    "python-hatch_vcs",
+    "python-hatchling",
+    "python-installer",
+]
+checkdepends = [
+    "python-attrs",
+    "python-iniconfig",
+    "python-packaging",
+    "python-pluggy",
+    "python-pytest",
+    "python-rpds-py",
+]
+depends = [
+    "python-attrs",
+    "python-rpds-py",
+]
+pkgdesc = "Implementation-agnostic implementation of JSON reference resolution"
+maintainer = "miko <mikoxyzzz@gmail.com>"
+license = "MIT"
+url = "https://github.com/python-jsonschema/referencing"
+source = f"$(PYPI_SITE)/r/referencing/referencing-{pkgver}.tar.gz"
+sha256 = "3c57da0513e9563eb7e203ebe9bb3a1b509b042016433bd1e45a2853466c3dd3"
+# TODO: package python-pytest-subtests
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("COPYING")
