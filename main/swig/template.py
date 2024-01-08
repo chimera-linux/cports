d42a99ac32b3485fd0519192d4e01279bfbacc6c
psykose <alice@ayaya.dev>
2024-01-08 12:32:17 +0000

    main/swig: update to 4.2.0

--- main/swig/template.py
+++ main/swig/template.py
@@ -1,7 +1,9 @@
 pkgname = "swig"
-pkgver = "4.1.1"
+pkgver = "4.2.0"
 pkgrel = 0
 build_style = "gnu_configure"
+# broken
+configure_gen = []
 hostmakedepends = ["byacc"]
 makedepends = ["zlib-devel", "pcre2-devel"]
 pkgdesc = "Simplified Wrapper and Interface Generator"
@@ -9,9 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.swig.org"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "2af08aced8fcd65cdb5cc62426768914bedc735b1c250325203716f78e39ac9b"
+sha256 = "261ca2d7589e260762817b912c075831572b72ff2717942f75b3e51244829c97"
 hardening = ["!cfi"]  # TODO
 # broken check target?
 options = ["!check"]
-
-configure_gen = []
