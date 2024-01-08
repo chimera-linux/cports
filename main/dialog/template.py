d9ebe64416ec986e92a1ce1aa20097b110516683
psykose <alice@ayaya.dev>
2024-01-08 01:22:53 +0000

    main/dialog: update to 1.3.20240101

--- main/dialog/template.py
+++ main/dialog/template.py
@@ -1,20 +1,19 @@
 pkgname = "dialog"
-pkgver = "1.3.20231002"
+pkgver = "1.3.20240101"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-ncursesw", "--disable-nls"]
+# broken to reconf
+configure_gen = []
 makedepends = ["ncurses-devel"]
 pkgdesc = "Tool to display dialog boxes from shell scripts"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-only"
 url = "https://invisible-island.net/dialog"
-source = f"https://invisible-mirror.net/archives/{pkgname}/{pkgname}-{pkgver.replace('.2023', '-2023')}.tgz"
-sha256 = "315640ab0719225d5cbcab130585c05f0791fcf073072a5fe9479969aa2b833b"
+source = f"https://invisible-mirror.net/archives/{pkgname}/{pkgname}-{pkgver.replace('.2024', '-2024')}.tgz"
+sha256 = "9419eb52b95837312a76ccb26002c5d624fab53abde0859f1c7364179dc0ebad"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.rm(self.destdir / "usr/lib", force=True, recursive=True)
-
-
-configure_gen = []
