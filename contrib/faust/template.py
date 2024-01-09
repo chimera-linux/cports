fca1b4a27f017700b24706d584c496e6cf0dc142
psykose <alice@ayaya.dev>
2024-01-09 12:41:49 +0000

    contrib/faust: update to 2.70.3

--- contrib/faust/template.py
+++ contrib/faust/template.py
@@ -1,16 +1,17 @@
 pkgname = "faust"
-pkgver = "2.69.3"
-pkgrel = 1
+pkgver = "2.70.3"
+pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
-hostmakedepends = ["cmake", "gmake", "pkgconf"]
+make_build_args = ["GENERATOR=Ninja"]
+hostmakedepends = ["cmake", "gmake", "ninja", "pkgconf"]
 depends = ["bash"]
 pkgdesc = "Functional programming language for real-time signal processing"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://faust.grame.fr"
 source = f"https://github.com/grame-cncm/faust/releases/download/{pkgver}/faust-{pkgver}.tar.gz"
-sha256 = "169a2f1e8a95e159c78c734c1c5dd818bf5c95b3b002a7efd9f6bb8589357062"
+sha256 = "3ac3aab87d60257b3fff03ffeb42e190480fb9828266fa1c35574b6cbf6a13bb"
 # skip execinfo
 tool_flags = {"CXXFLAGS": ["-DALPINE"]}
 # no check target
