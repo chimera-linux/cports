958a87d69b642e1f359b75e478d2877057015732
psykose <alice@ayaya.dev>
2024-01-09 12:31:28 +0000

    main/python-alabaster: update to 0.7.15

--- main/python-alabaster/template.py
+++ main/python-alabaster/template.py
@@ -1,11 +1,11 @@
 pkgname = "python-alabaster"
-pkgver = "0.7.13"
-pkgrel = 1
+pkgver = "0.7.15"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
+    "python-flit_core",
     "python-installer",
-    "python-setuptools",
     "python-wheel",
 ]
 depends = ["python"]
@@ -14,10 +14,10 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://alabaster.readthedocs.io"
 source = f"$(PYPI_SITE)/a/alabaster/alabaster-{pkgver}.tar.gz"
-sha256 = "a27a4a084d5e690e16e01e03ad2b2e552c61a65469419b907243193de1a84ae2"
+sha256 = "0127f4b1db0afc914883f930e3d40763131aebac295522fc4a04d9e77c703705"
 # no tests
 options = ["!check"]
 
 
 def post_install(self):
-    self.install_license("LICENSE")
+    self.install_license("LICENSE.rst")
