a604416ab5f723b9ee3e775842ad20e64aa1ef18
q66 <q66@chimera-linux.org>
2024-01-09 06:07:26 +0100

    contrib/base-unmatched: remove hook

--- contrib/base-unmatched/template.py
+++ contrib/base-unmatched/template.py
@@ -1,6 +1,6 @@
 pkgname = "base-unmatched"
 pkgver = "0.1"
-pkgrel = 0
+pkgrel = 1
 archs = ["riscv64"]
 depends = ["u-boot-sifive_unmatched", "u-boot-menu"]
 pkgdesc = "Chimera base package for HiFive Unmatched"
@@ -10,6 +10,7 @@ url = "https://chimera-linux.org"
 
 
 def do_install(self):
+    self.install_file(self.files_path / "agetty", "etc/default")
     # u-boot-menu
     self.install_file(self.files_path / "u-boot-device", "etc/default")
     self.install_file(self.files_path / "u-boot-cmdline", "etc/default")
