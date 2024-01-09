06aa548ae2ed0c51dccada53ca2fa126e6051503
psykose <alice@ayaya.dev>
2024-01-09 12:30:17 +0000

    main/perl-net-ssleay: update to 1.94

--- main/perl-net-ssleay/template.py
+++ main/perl-net-ssleay/template.py
@@ -1,6 +1,6 @@
 pkgname = "perl-net-ssleay"
-pkgver = "1.92"
-pkgrel = 1
+pkgver = "1.94"
+pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["gmake", "perl", "openssl"]
 makedepends = ["perl", "zlib-devel", "openssl-devel"]
@@ -10,6 +10,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-2.0"
 url = "https://metacpan.org/release/Net-SSLeay"
 source = f"$(CPAN_SITE)/Net/Net-SSLeay-{pkgver}.tar.gz"
-sha256 = "47c2f2b300f2e7162d71d699f633dd6a35b0625a00cbda8c50ac01144a9396a9"
+sha256 = "9d7be8a56d1bedda05c425306cc504ba134307e0c09bda4a788c98744ebcd95d"
 # missing checkdepends
 options = ["!check"]
