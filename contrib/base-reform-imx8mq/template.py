98fbd320ed73ac495490cb342eb06d821a0b2d53
q66 <q66@chimera-linux.org>
2024-01-09 06:05:10 +0100

    contrib/base-reform-imx8mq: remove hook

--- contrib/base-reform-imx8mq/template.py
+++ contrib/base-reform-imx8mq/template.py
@@ -1,6 +1,6 @@
 pkgname = "base-reform-imx8mq"
 pkgver = "0.1"
-pkgrel = 0
+pkgrel = 1
 archs = ["aarch64"]
 depends = ["u-boot-imx8mq_reform2", "u-boot-menu"]
 pkgdesc = "Chimera base package for MNT Reform 2 with i.MX8MQ"
@@ -10,6 +10,7 @@ url = "https://chimera-linux.org"
 
 
 def do_install(self):
+    self.install_file(self.files_path / "agetty", "etc/default")
     # u-boot-menu
     self.install_file(self.files_path / "u-boot-device", "etc/default")
     self.install_file(self.files_path / "u-boot-cmdline", "etc/default")
