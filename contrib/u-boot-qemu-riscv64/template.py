197ac4347ea39e3e9b2a0a1ecad8f15df4126381
Jami Kettunen <jami.kettunen@protonmail.com>
2024-01-10 01:02:27 +0200

    contrib/u-boot-qemu-riscv64: update to 2024.01

--- contrib/u-boot-qemu-riscv64/template.py
+++ contrib/u-boot-qemu-riscv64/template.py
@@ -1,5 +1,5 @@
 pkgname = "u-boot-qemu-riscv64"
-pkgver = "2023.10"
+pkgver = "2024.01"
 pkgrel = 0
 build_style = "u_boot"
 hostmakedepends = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND BSD-3-Clause"
 url = "https://www.denx.de/wiki/U-Boot"
 source = f"https://ftp.denx.de/pub/u-boot/u-boot-{pkgver}.tar.bz2"
-sha256 = "e00e6c6f014e046101739d08d06f328811cebcf5ae101348f409cbbd55ce6900"
+sha256 = "b99611f1ed237bf3541bdc8434b68c96a6e05967061f992443cb30aabebef5b3"
 env = {
     "U_BOOT_TRIPLET": "riscv64-unknown-elf",
     "U_BOOT_TARGETS": "u-boot",
