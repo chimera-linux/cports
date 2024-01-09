539841b0542aa96cefe646faf5722371cb4cd8fa
q66 <q66@chimera-linux.org>
2024-01-09 06:06:19 +0100

    contrib/base-rockpro64: remove hook

--- contrib/base-rockpro64/template.py
+++ contrib/base-rockpro64/template.py
@@ -1,6 +1,6 @@
 pkgname = "base-rockpro64"
 pkgver = "0.1"
-pkgrel = 0
+pkgrel = 1
 archs = ["aarch64"]
 depends = [
     "firmware-ap6256",
@@ -19,4 +19,5 @@ def do_install(self):
     self.install_file(self.files_path / "u-boot-device", "etc/default")
     self.install_file(self.files_path / "u-boot-cmdline", "etc/default")
     # agetty service customization
+    self.install_file(self.files_path / "agetty", "etc/default")
     self.install_file(self.files_path / "agetty-ttyS2", "etc/default")
