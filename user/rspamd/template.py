8087b4cb15efeb43e2b8fa2de5506f33698decfe
Duncan Bellamy <dunk@denkimushi.com>
2024-01-10 08:25:54 +0000

    user/rspamd: new package (3.7.5)

---
+++ user/rspamd/template.py
@@ -0,0 +1,63 @@
+pkgname = "rspamd"
+pkgver = "3.7.5"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    "-DCONFDIR=/etc/rspamd",
+    "-DENABLE_FASTTEXT=ON",
+    "-DENABLE_HYPERSCAN=ON",
+    "-DENABLE_LUAJIT=OFF",
+    "-DENABLE_URI_INCLUDE=ON",
+    "-DRSPAMD_GROUP=_rspamd",
+    "-DRSPAMD_USER=_rspamd",
+    "-DSYSTEM_FMT=ON",
+    "-DSYSTEM_XXHASH=ON",
+    "-DSYSTEM_ZSTD=ON",
+    "-D_CAN_RUN=0",
+    "-DHAVE_ATOMIC_BUILTINS_EXITCODE=0",
+]
+make_build_args = ["--target", "all", "check"]
+hostmakedepends = ["cmake", "ninja", "perl", "pkgconf", "ragel"]
+makedepends = [
+    "elfutils-devel",
+    "fasttext-devel",
+    "fmt-devel",
+    "glib-devel",
+    "icu-devel",
+    "libsodium-devel",
+    "libunwind-devel",
+    "lua5.4-devel",
+    "openssl-devel",
+    "pcre2-devel",
+    "snowball-devel",
+    "sqlite-devel",
+    "vectorscan-devel",
+    "xxhash-devel",
+    "zstd-devel",
+]
+pkgdesc = "Spam filtering system"
+maintainer = "Duncan Bellamy <dunk@denkimushi.com>"
+license = "Apache-2.0 AND BSD-2-Clause AND BSD-3-Clause AND BSL-1.0 AND CC0-1.0 AND LGPL-3.0-only AND MIT AND Zlib"
+url = "https://rspamd.com/index.html"
+source = f"https://github.com/rspamd/rspamd/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "3fba8b0086fe68ac16146cfc25cfcff352bff52fa5cef7d277007ed008170330"
+
+
+def do_check(self):
+    # full tests require luajit
+    self.do("build/test/rspamd-test-cxx")
+
+
+def post_install(self):
+    self.install_file(
+        self.files_path / "sysusers.conf",
+        "usr/lib/sysusers.d",
+        name="rspamd.conf",
+    )
+    self.install_file(
+        self.files_path / "tmpfiles.conf",
+        "usr/lib/tmpfiles.d",
+        name="rspamd.conf",
+    )
+    self.install_service(self.files_path / "rspamd")
+    self.install_license("LICENSE.md")
