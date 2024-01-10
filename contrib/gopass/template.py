69ec46171a3c6ce652b05d70b593359c0daa09e2
Callum Andrew <contact@candrew.net>
2024-01-10 12:57:17 +1100

    contrib/gopass: new package (1.15.11)

---
+++ contrib/gopass/template.py
@@ -0,0 +1,22 @@
+pkgname = "gopass"
+pkgver = "1.15.11"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go"]
+pkgdesc = "Pass-compatible password manager with more features"
+maintainer = "Callum Andrew <contact@candrew.net>"
+license = "MIT"
+url = "https://www.gopass.pw"
+source = (
+    f"https://github.com/gopasspw/gopass/archive/refs/tags/v{pkgver}.tar.gz"
+)
+sha256 = "f85610a4f114125bd21e1100d6a2970c7ab76f09a7e094aa6be378018979eb56"
+options = ["!debug"]
+
+
+def post_install(self):
+    self.install_man("gopass.1")
+    self.install_license("LICENSE")
+    self.install_completion("bash.completion", "bash")
+    self.install_completion("zsh.completion", "zsh")
+    self.install_completion("fish.completion", "fish")
