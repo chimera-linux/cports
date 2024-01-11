9e9e91637988337712186b42e677536d7fea6a75
psykose <alice@ayaya.dev>
2024-01-11 03:53:01 +0000

    main/python-markdown: update to 3.5.2

--- main/python-markdown/template.py
+++ main/python-markdown/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-markdown"
-pkgver = "3.5.1"
-pkgrel = 1
+pkgver = "3.5.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/Python-Markdown/markdown"
 source = f"$(PYPI_SITE)/M/Markdown/Markdown-{pkgver}.tar.gz"
-sha256 = "b65d7beb248dc22f2e8a31fb706d93798093c308dc1aba295aedeb9d41a813bd"
+sha256 = "e1ac7b3dc550ee80e602e71c1d168002f062e49f1b11e26a36264dafd4df2ef8"
 # checkdepends missing
 options = ["!check"]
 
