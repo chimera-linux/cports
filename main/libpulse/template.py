3f94ea8ce3aca6d9ec6d62fdd1adc715e9605d27
psykose <alice@ayaya.dev>
2024-01-13 00:13:34 +0000

    main/libpulse: update to 17.0

--- main/libpulse/template.py
+++ main/libpulse/template.py
@@ -1,5 +1,5 @@
 pkgname = "libpulse"
-pkgver = "16.1"
+pkgver = "17.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -11,21 +11,21 @@ configure_args = [
     "-Dbashcompletiondir=/usr/share/bash-completion/completions",
 ]
 hostmakedepends = [
-    "meson",
-    "pkgconf",
     "cmake",
+    "meson",
     "perl",
     "perl-xml-parser",
+    "pkgconf",
 ]
 makedepends = [
     "dbus-devel",
-    "udev-devel",
     "glib-devel",
-    "libsndfile-devel",
-    "libsamplerate-devel",
-    "orc-devel",
     "libcap-devel",
+    "libsamplerate-devel",
+    "libsndfile-devel",
     "linux-headers",
+    "orc-devel",
+    "udev-devel",
 ]
 # not in a standard path and therefore not picked up by shlib scanner
 provides = [f"so:libpulsecommon-{pkgver}.so=0"]
@@ -34,7 +34,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.freedesktop.org/wiki/Software/PulseAudio"
 source = f"$(FREEDESKTOP_SITE)/pulseaudio/releases/pulseaudio-{pkgver}.tar.xz"
-sha256 = "8eef32ce91d47979f95fd9a935e738cd7eb7463430dabc72863251751e504ae4"
+sha256 = "053794d6671a3e397d849e478a80b82a63cb9d8ca296bd35b73317bb5ceb87b5"
 options = ["linkundefver"]
 
 
