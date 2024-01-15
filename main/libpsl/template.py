0b4fe229427068ce68de9bce6356eb8c0305d3a7
psykose <alice@ayaya.dev>
2024-01-15 13:56:49 +0000

    main/libpsl: update to 0.21.5

--- main/libpsl/template.py
+++ main/libpsl/template.py
@@ -1,15 +1,15 @@
 pkgname = "libpsl"
-pkgver = "0.21.2"
+pkgver = "0.21.5"
 pkgrel = 0
-build_style = "gnu_configure"
-hostmakedepends = ["pkgconf", "python"]
+build_style = "meson"
+hostmakedepends = ["meson", "ninja", "pkgconf"]
 makedepends = ["icu-devel", "libidn2-devel", "libunistring-devel"]
 pkgdesc = "Public Suffix List library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://rockdaboot.github.io/libpsl"
 source = f"https://github.com/rockdaboot/{pkgname}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "e35991b6e17001afa2c0ca3b10c357650602b92596209b7492802f3768a6285f"
+sha256 = "1dcc9ceae8b128f3c0b3f654decd0e1e891afc6ff81098f227ef260449dae208"
 hardening = ["vis", "cfi"]
 
 
@@ -20,6 +20,3 @@ def post_install(self):
 @subpackage("libpsl-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
