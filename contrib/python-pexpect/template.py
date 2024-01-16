d73f7296b696626e3e1e88431ff8a22aadf3914d
psykose <alice@ayaya.dev>
2024-01-16 17:16:22 +0000

    contrib/python-pexpect: new package (4.9.0)

---
+++ contrib/python-pexpect/template.py
@@ -0,0 +1,27 @@
+pkgname = "python-pexpect"
+pkgver = "4.9.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = ["python-ptyprocess"]
+checkdepends = [
+    "bash",
+    "mandoc",
+    "python-pytest",
+    "zsh",
+] + depends
+pkgdesc = "Python port of expect(1) for child process handling"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "ISC"
+url = "https://pexpect.readthedocs.io/en/stable"
+source = f"$(PYPI_SITE)/p/pexpect/pexpect-{pkgver}.tar.gz"
+sha256 = "ee7d41123f3c9911050ea2c2dac107568dc43b2d3b0c7557a33212c398ead30f"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
