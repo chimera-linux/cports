2fa0a3a46b0aab5da31460e2431cb53f4b5149bb
Erica Z <zerica@callcc.eu>
2024-01-17 08:04:22 +0100

    contrib/openimageio: new package (2.5.10.1)

---
+++ contrib/openimageio/template.py
@@ -0,0 +1,57 @@
+pkgname = "openimageio"
+pkgver = "2.5.10.1"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    # disabled below
+    "-DBUILD_TESTING=OFF",
+    "-DINSTALL_FONTS=OFF",
+    "-DSTOP_ON_WARNING=OFF",
+    "-DUSE_EXTERNAL_PUGIXML=ON",
+]
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = [
+    "boost-devel",
+    "ffmpeg-devel",
+    "fmt-devel",
+    "freetype-devel",
+    "giflib-devel",
+    "libheif-devel",
+    "libpng-devel",
+    "libraw-devel",
+    "libtiff-devel",
+    "libwebp-devel",
+    "mesa-devel",
+    "opencolorio-devel",
+    "openexr-devel",
+    "openjpeg-devel",
+    "onetbb-devel",
+    "pugixml-devel",
+    "python-pybind11-devel",
+    "qt6-qtbase-devel",
+    "robin-map",
+]
+pkgdesc = "Toolset for manipulating VFX-related image file formats"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "Apache-2.0"
+url = "https://github.com/AcademySoftwareFoundation/OpenImageIO"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "8f6a547f6a5d510737ba436f867043db537def65f0fdb14ec30e5a185b619f93"
+# FIXME: tests are broken
+options = ["!check"]
+
+
+@subpackage("openimageio-progs")
+def _progs(self):
+    return self.default_progs()
+
+
+@subpackage("openimageio-devel")
+def _devel(self):
+    return self.default_devel()
+
+
+@subpackage("python-openimageio")
+def _python(self):
+    self.pkgdesc = "Python bindings for openimageio"
+    return ["usr/lib/python*"]
