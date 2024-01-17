8b8c2c33f3a12db84a28952bd4a4ce8fc5463630
Iztok Fister Jr. <iztok@iztok.space>
2024-01-17 17:19:42 +0100

    main/python-jinja2: enable tests

--- main/python-jinja2/template.py
+++ main/python-jinja2/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-jinja2"
 pkgver = "3.1.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -16,8 +16,6 @@ license = "BSD-3-Clause"
 url = "http://jinja.pocoo.org"
 source = f"$(PYPI_SITE)/J/Jinja2/Jinja2-{pkgver}.tar.gz"
 sha256 = "ac8bd6544d4bb2c9792bf3a159e80bba8fda7f07e81bc3aed565432d5925ba90"
-# dependency of pytest
-options = ["!check"]
 
 
 def post_install(self):
