1ef4fd28f72d60c67c3661a14c131e9329815fd3
psykose <alice@ayaya.dev>
2024-01-17 11:38:54 +0000

    contrib/libtraceevent: fixup licences
    the included licence files use '+' as later and non-'+' as 'only', and
    all the declarations in headers are without a '+'.

--- contrib/libtraceevent/template.py
+++ contrib/libtraceevent/template.py
@@ -1,6 +1,6 @@
 pkgname = "libtraceevent"
 pkgver = "1.8.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
     # builds both libtracevent.so and loadable plugins,
@@ -17,7 +17,7 @@ hostmakedepends = [
 makedepends = ["linux-headers"]
 pkgdesc = "Linux kernel trace event library"
 maintainer = "psykose <alice@ayaya.dev>"
-license = "LGPL-2.1-or-later AND GPL-2.0-or-later"
+license = "LGPL-2.1-only AND GPL-2.0-only"
 url = "https://git.kernel.org/pub/scm/libs/libtrace/libtraceevent.git"
 source = f"{url}/snapshot/libtraceevent-{pkgver}.tar.gz"
 sha256 = "919f0c024c7b5059eace52d854d4df00ae7e361a4033e1b4d6fe01d97064a1b9"
