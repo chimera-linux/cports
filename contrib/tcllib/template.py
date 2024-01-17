c4345c580bd00024b7013914149d5378ca296f9c
Erica Z <zerica@callcc.eu>
2024-01-17 13:12:57 +0100

    contrib/tcllib: new package (1.21)

---
+++ contrib/tcllib/template.py
@@ -0,0 +1,12 @@
+pkgname = "tcllib"
+pkgver = "1.21"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["automake", "tcl"]
+makedepends = ["tcl-devel"]
+pkgdesc = "Tcl standard library"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "TCL"
+url = "https://core.tcl-lang.org/tcllib"
+source = f"{url}/uv/tcllib-{pkgver}.tar.xz"
+sha256 = "10c7749e30fdd6092251930e8a1aa289b193a3b7f1abf17fee1d4fa89814762f"
---
+++ contrib/tcllib/update.py
@@ -0,0 +1,2 @@
+url = "https://core.tcl-lang.org/tcllib/timeline?t=release"
+pattern = r"tcllib-([0-9.]+)"
