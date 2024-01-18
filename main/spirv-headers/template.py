da9923973bf5f0ae455349848ac727673f787fae
psykose <alice@ayaya.dev>
2024-01-18 06:16:16 +0000

    main/spirv-headers: update to 1.3.275.0

--- main/spirv-headers/template.py
+++ main/spirv-headers/template.py
@@ -1,5 +1,5 @@
 pkgname = "spirv-headers"
-pkgver = "1.3.268.0"
+pkgver = "1.3.275.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:spirv-headers"
 url = "https://github.com/KhronosGroup/SPIRV-Headers"
 source = f"{url}/archive/refs/tags/vulkan-sdk-{pkgver}.tar.gz"
-sha256 = "1022379e5b920ae21ccfb5cb41e07b1c59352a18c3d3fdcbf38d6ae7733384d4"
+sha256 = "d46b261f1fbc5e85022cb2fada9a6facb5b0c9932b45007a77fe05639a605bd1"
 # no test suite
 options = ["!check"]
 
