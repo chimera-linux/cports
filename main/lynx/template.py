b20d8ebc789ed7d703c0c17a61c3b33d3764f071
psykose <alice@ayaya.dev>
2024-01-18 11:06:31 +0000

    main/lynx: update to 2.9.0

--- main/lynx/template.py
+++ main/lynx/template.py
@@ -1,23 +1,29 @@
 pkgname = "lynx"
-pkgver = "2.9.0_pre12"
+pkgver = "2.9.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--enable-widec",
     "--enable-ipv6",
-    "--with-zlib",
+    "--with-brotli",
     "--with-bzlib",
     "--with-ssl",
+    "--with-zlib",
 ]
-hostmakedepends = ["pkgconf"]
-makedepends = ["zlib-devel", "bzip2-devel", "ncurses-devel", "openssl-devel"]
+hostmakedepends = ["automake", "pkgconf"]
+makedepends = [
+    "brotli-devel",
+    "bzip2-devel",
+    "libidn2-devel",
+    "ncurses-devel",
+    "openssl-devel",
+    "zlib-devel",
+]
 pkgdesc = "Text web browser"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
-url = "http://lynx.invisible-island.net"
-source = f"http://invisible-mirror.net/archives/{pkgname}/tarballs/{pkgname}{pkgver.replace('_pre', 'dev.')}.tar.bz2"
-sha256 = "a6455b159d00776d8ec1051285c972dc1f0c552d0571a0cff02a23ec146ee8e5"
+url = "https://lynx.invisible-island.net"
+source = f"https://invisible-mirror.net/archives/{pkgname}/tarballs/{pkgname}{pkgver.replace('_pre', 'dev.')}.tar.bz2"
+sha256 = "5bcae5e2e6043ca7b220963a97763c49c13218d849ffda6be7739bfd5a2d36ff"
 hardening = ["vis", "cfi"]
 options = ["!cross"]
-
-configure_gen = []
