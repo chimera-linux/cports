e1d2a4426df6da1e5020d51368a0100aaaa76375
psykose <alice@ayaya.dev>
2024-01-18 06:16:18 +0000

    main/spirv-tools: update to 1.3.275.0

--- main/spirv-tools/template.py
+++ main/spirv-tools/template.py
@@ -1,7 +1,7 @@
 # update and rebuild shaderc when updating
 # update vulkan-validationlayers on next release
 pkgname = "spirv-tools"
-pkgver = "1.3.268.0"
+pkgver = "1.3.275.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/KhronosGroup/SPIRV-Tools"
 source = f"{url}/archive/vulkan-sdk-{pkgver}.tar.gz"
-sha256 = "4c19fdcffb5fe8ef8dc93d7a65ae78b64edc7a5688893ee381c57f70be77deaf"
+sha256 = "f6fe32edc00b73400e9d5474d87d474478bf8bc0fb73d2767fecd847c05a4b1d"
 hardening = ["!cfi"]  # TODO
 # needs gtest
 options = ["!check"]
