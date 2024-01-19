a39ec07278f771cf45f91f952eaab197d4f0a375
q66 <q66@chimera-linux.org>
2024-01-19 21:59:36 +0100

    contrib/libbluray-bdj: new package (1.3.4)

---
+++ contrib/libbluray-bdj/template.py
@@ -0,0 +1,26 @@
+pkgname = "libbluray-bdj"
+pkgver = "1.3.4"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--disable-optimizations"]
+configure_gen = []
+hostmakedepends = [
+    "apache-ant",
+    "openjdk17-default",
+    "openjdk17-jdk",
+    "pkgconf",
+]
+makedepends = ["libxml2-devel", "fontconfig-devel"]
+pkgdesc = "Library for Blu-Ray disk playback (BD-J support)"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.1-or-later"
+url = "https://www.videolan.org/developers/libbluray.html"
+source = f"https://download.videolan.org/pub/videolan/libbluray/{pkgver}/libbluray-{pkgver}.tar.bz2"
+sha256 = "478ffd68a0f5dde8ef6ca989b7f035b5a0a22c599142e5cd3ff7b03bbebe5f2b"
+
+
+def post_install(self):
+    # make it not conflict
+    self.rm(self.destdir / "usr/bin", recursive=True)
+    self.rm(self.destdir / "usr/include", recursive=True)
+    self.rm(self.destdir / "usr/lib", recursive=True)
