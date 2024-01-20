249809f5993982278151bde2557e62526a8d1d9a
psykose <alice@ayaya.dev>
2024-01-20 11:31:37 +0000

    main/libtracefs: new package (1.8.2)

---
+++ main/libtracefs/template.py
@@ -0,0 +1,37 @@
+pkgname = "libtracefs"
+pkgver = "1.8.0"
+pkgrel = 0
+build_style = "meson"
+configure_args = [
+    # same as libtraceevent, useless static plugins
+    "-Ddefault_library=shared",
+    "-Dasciidoctor=false",
+]
+hostmakedepends = [
+    "asciidoc",
+    "bison",
+    "flex",
+    "gsed",
+    "meson",
+    "pkgconf",
+    "xmlto",
+]
+makedepends = [
+    "libtraceevent-devel",
+    "linux-headers",
+]
+pkgdesc = "Linux kernel trace file system library"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-or-later"
+url = "https://git.kernel.org/pub/scm/libs/libtrace/libtracefs.git/about"
+source = f"https://git.kernel.org/pub/scm/libs/libtrace/libtracefs.git/snapshot/libtracefs-{pkgver}.tar.gz"
+sha256 = "f92475d5c4cb509983697fb359ee615bef4f08ed8bdc9c690f6118ba68886de0"
+# vis breaks symbols
+hardening = ["!vis"]
+# TODO: needs cunit
+options = ["!check"]
+
+
+@subpackage("libtracefs-devel")
+def _devel(self):
+    return self.default_devel()
---
+++ main/libtracefs/update.py
@@ -0,0 +1,1 @@
+url = "https://git.kernel.org/pub/scm/libs/libtrace/libtracefs.git/refs"
