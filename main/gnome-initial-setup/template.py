aec0a1217cd410a654ebe2496d1cc14f388809b7
psykose <alice@ayaya.dev>
2024-01-22 13:16:42 +0000

    main/gnome-initial-setup: update to 45.3

--- main/gnome-initial-setup/template.py
+++ main/gnome-initial-setup/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-initial-setup"
-pkgver = "45.0"
+pkgver = "45.3"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -8,30 +8,30 @@ configure_args = [
 ]
 hostmakedepends = ["meson", "pkgconf", "glib-devel", "gettext", "dconf"]
 makedepends = [
-    "ibus-devel",
-    "networkmanager-devel",
     "accountsservice-devel",
-    "polkit-devel",
-    "gnome-desktop-devel",
-    "gsettings-desktop-schemas-devel",
     "fontconfig-devel",
-    "libgweather-devel",
+    "gdm-devel",
+    "geoclue-devel",
+    "geocode-glib-devel",
+    "glib-devel",
+    "gnome-desktop-devel",
     "gnome-online-accounts-devel",
+    "gsettings-desktop-schemas-devel",
     "gtk+3-devel",
     "gtk4-devel",
-    "glib-devel",
-    "webkitgtk4-devel",
-    "geocode-glib-devel",
-    "geoclue-devel",
-    "pango-devel",
-    "rest-devel",
-    "json-glib-devel",
     "heimdal-devel",
-    "libsecret-devel",
-    "gdm-devel",
-    "libpwquality-devel",
-    "libnma-devel",
+    "ibus-devel",
+    "json-glib-devel",
     "libadwaita-devel",
+    "libgweather-devel",
+    "libnma-devel",
+    "libpwquality-devel",
+    "libsecret-devel",
+    "networkmanager-devel",
+    "pango-devel",
+    "polkit-devel",
+    "rest-devel",
+    "webkitgtk4-devel",
 ]
 depends = ["dconf"]
 pkgdesc = "GNOME initial setup"
@@ -39,6 +39,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Design/OS/InitialSetup"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "b1afe764798f8948bedb95c7ab31bd78529ac5cb481c41f7a7777f264de54bd8"
+sha256 = "206338c174fb776753320f4ddaac89478192629d143e22b0cc459a859b402a22"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
