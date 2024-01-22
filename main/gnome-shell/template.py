151183fe26c11137eeae6a39b764319f9bbd1c3c
psykose <alice@ayaya.dev>
2024-01-22 13:16:43 +0000

    main/gnome-shell: update to 45.3

--- main/gnome-shell/template.py
+++ main/gnome-shell/template.py
@@ -1,52 +1,52 @@
 pkgname = "gnome-shell"
-pkgver = "45.2"
+pkgver = "45.3"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
+    "-Ddefault_library=shared",
     "-Dsystemd=false",
     "-Dtests=false",
-    "-Ddefault_library=shared",
 ]
 make_check_wrapper = ["weston-headless-run"]
 hostmakedepends = [
-    "meson",
-    "pkgconf",
-    "gettext",
-    "gobject-introspection",
-    "xsltproc",
     "asciidoc",
-    "sassc",
+    "gettext",
     "gjs-devel",
     "glib-devel",
+    "gobject-introspection",
+    "meson",
     "perl",
+    "pkgconf",
+    "sassc",
+    "xsltproc",
 ]
 makedepends = [
-    "gnome-control-center-devel",
-    "evolution-data-server-devel",
-    "gsettings-desktop-schemas-devel",
-    "startup-notification-devel",
-    "mutter-devel",
     "at-spi2-core-devel",
-    "mutter-devel",
-    "gjs-devel",
+    "evolution-data-server-devel",
     "gcr-devel",
-    "gtk4-devel",
-    "libxml2-devel",
-    "ibus-devel",
-    "gnome-bluetooth-devel",
-    "gstreamer-devel",
-    "pipewire-devel",
-    "libpulse-devel",
-    "gnome-desktop-devel",
-    "polkit-devel",
-    "networkmanager-devel",
+    "gjs-devel",
     "gnome-autoar-devel",
+    "gnome-bluetooth-devel",
+    "gnome-control-center-devel",
+    "gnome-desktop-devel",
+    "gsettings-desktop-schemas-devel",
+    "gstreamer-devel",
+    "gtk4-devel",
+    "ibus-devel",
+    "libpulse-devel",
+    "libxml2-devel",
+    "mutter-devel",
+    "mutter-devel",
+    "networkmanager-devel",
+    "pipewire-devel",
+    "polkit-devel",
+    "startup-notification-devel",
 ]
 depends = [
+    "cmd:unzip!unzip",
     "gnome-control-center",
     "gsettings-desktop-schemas",
     "upower",
-    "cmd:unzip!unzip",
 ]
 checkdepends = ["weston"]
 pkgdesc = "Core user interface for GNOME"
@@ -54,6 +54,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Projects/GnomeShell"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "8a0cfbf872b1a762696c86e13defe9f3675e92d79514eba9d000b5b611c26a23"
+sha256 = "3a19724720d8274dc6bced68e0dd5fc7668a04cd79978cbbb8223474ccdda9ab"
 # tests need libmutter-test
 options = ["!check"]
