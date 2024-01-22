a7f1d3ac96312c087c8d12611ea4037c8337a352
psykose <alice@ayaya.dev>
2024-01-22 13:07:44 +0000

    main/ostree: update to 2024.1

--- main/ostree/template.py
+++ main/ostree/template.py
@@ -1,5 +1,5 @@
 pkgname = "ostree"
-pkgver = "2023.8"
+pkgver = "2024.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -41,7 +41,7 @@ maintainer = "eater <=@eater.me>"
 license = "LGPL-2.0-or-later"
 url = "https://ostreedev.github.io/ostree"
 source = f"https://github.com/ostreedev/ostree/releases/download/v{pkgver}/libostree-{pkgver}.tar.xz"
-sha256 = "b6fffc267188e40d60755e6d7f2be65831795baa53b0fd9dd6c6809c7e54796d"
+sha256 = "152327fe804512d4f896fb9b9e9cdc84ad9b4c0b205b8625472147d5a28164af"
 # failing on their test harness, i will find motivation Soon
 options = ["!check"]
 
