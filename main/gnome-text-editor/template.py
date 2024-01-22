6272876a70a5a8d3ab93469a5ed44f2bcd9ea4f8
psykose <alice@ayaya.dev>
2024-01-22 13:12:43 +0000

    main/gnome-text-editor: update to 45.2

--- main/gnome-text-editor/template.py
+++ main/gnome-text-editor/template.py
@@ -1,30 +1,30 @@
 pkgname = "gnome-text-editor"
-pkgver = "45.1"
-pkgrel = 1
+pkgver = "45.2"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
-    "meson",
-    "pkgconf",
+    "desktop-file-utils",
+    "gettext",
     "glib-devel",
     "gtk-update-icon-cache",
-    "gettext",
-    "vala",
     "itstool",
-    "desktop-file-utils",
+    "meson",
+    "pkgconf",
+    "vala",
 ]
 makedepends = [
-    "gtk4-devel",
+    "editorconfig-devel",
+    "enchant-devel",
     "glib-devel",
+    "gtk4-devel",
+    "gtksourceview-devel",
+    "icu-devel",
     "libadwaita-devel",
     "pcre2-devel",
-    "enchant-devel",
-    "gtksourceview-devel",
-    "editorconfig-devel",
-    "icu-devel",
 ]
 pkgdesc = "GNOME text editor"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-text-editor"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "6a86ec9920f466b6ed92695524d3b507b1e84272dafa5341d06a157de868af71"
+sha256 = "0e2552fdb3025446cdad9d59b13d75df9a653fc3ea1dc43b919e0f6fa8a846ac"
