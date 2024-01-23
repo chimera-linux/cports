121ef13917bfb1c40ea39be626ae4dfe6a97fc37
miko <mikoxyzzz@gmail.com>
2024-01-23 18:42:59 +0100

    contrib/libaacs: enable cfi

--- contrib/libaacs/template.py
+++ contrib/libaacs/template.py
@@ -1,6 +1,6 @@
 pkgname = "libaacs"
 pkgver = "0.11.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = ["--disable-optimizations"]
 hostmakedepends = [
@@ -17,8 +17,7 @@ license = "LGPL-2.1-or-later"
 url = "https://www.videolan.org/developers/libaacs.html"
 source = f"https://download.videolan.org/pub/videolan/libaacs/{pkgver}/libaacs-{pkgver}.tar.bz2"
 sha256 = "a88aa0ebe4c98a77f7aeffd92ab3ef64ac548c6b822e8248a8b926725bea0a39"
-# TODO: cfi
-hardening = ["vis"]
+hardening = ["cfi", "vis"]
 
 
 @subpackage("libaacs-devel")
