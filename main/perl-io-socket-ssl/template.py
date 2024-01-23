c3501084f027892017938e26aee58f05a867777b
psykose <alice@ayaya.dev>
2024-01-23 15:07:03 +0000

    main/perl-io-socket-ssl: update to 2.085

--- main/perl-io-socket-ssl/template.py
+++ main/perl-io-socket-ssl/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-io-socket-ssl"
-pkgver = "2.084"
+pkgver = "2.085"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["gmake", "perl"]
@@ -10,6 +10,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/release/IO-Socket-SSL"
 source = f"$(CPAN_SITE)/IO/IO-Socket-SSL-{pkgver}.tar.gz"
-sha256 = "a60d1e04e192363155329560498abd3412c3044295dae092d27fb6e445c71ce1"
+sha256 = "95b2f7c0628a7e246a159665fbf0620d0d7835e3a940f22d3fdd47c3aa799c2e"
 # missing checkdepends
 options = ["!check"]
