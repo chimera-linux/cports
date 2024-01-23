273237d1331ad350326135ce3aa5f6232a2e47f9
yanchan09 <yan@omg.lol>
2024-01-23 14:15:53 +0200

    contrib/fish-shell: use correct check target
    fish has its own test target that should be used instead of just using
    ctest alone

--- contrib/fish-shell/template.py
+++ contrib/fish-shell/template.py
@@ -1,8 +1,8 @@
 pkgname = "fish-shell"
 pkgver = "3.7.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
-make_build_args = ["--target", "all", "fish_tests"]
+make_check_target = "fish_run_tests"
 hostmakedepends = ["cmake", "ninja", "pkgconf", "gettext"]
 makedepends = ["ncurses-devel", "pcre2-devel"]
 checkdepends = ["python", "procps"]
@@ -12,14 +12,7 @@ license = "GPL-2.0-only"
 url = "https://fishshell.com"
 source = f"https://github.com/fish-shell/fish-shell/releases/download/{pkgver}/fish-{pkgver}.tar.xz"
 sha256 = "df1b7378b714f0690b285ed9e4e58afe270ac98dbc9ca5839589c1afcca33ab1"
-# FIXME int: test fail
-hardening = ["vis", "cfi", "!int"]
-
-
-def do_check(self):
-    # fails to find test script via ctest in out-of-tree builddir
-    with self.pushd(self.make_dir):
-        self.do("./fish_tests")
+hardening = ["vis", "cfi"]
 
 
 def post_install(self):
