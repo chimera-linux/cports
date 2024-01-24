069caa750d3044df9e1236e3510277b8ed32daa8
psykose <alice@ayaya.dev>
2024-01-24 18:21:01 +0000

    contrib/parallel: update to 20240122

--- contrib/parallel/template.py
+++ contrib/parallel/template.py
@@ -1,5 +1,5 @@
 pkgname = "parallel"
-pkgver = "20231222"
+pkgver = "20240122"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -11,4 +11,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/parallel"
 source = f"https://ftp.gnu.org/gnu/parallel/parallel-{pkgver}.tar.bz2"
-sha256 = "19466ddc6fa9bbd7be1886f5404129af12448f12ecd3b9562e985ada84da9baa"
+sha256 = "859688cbb5641cd7b6b16b2b960be24aa4e37e655cc8ffcd8af971cd7d5b449f"
