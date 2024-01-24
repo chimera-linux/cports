9a460ca870e50f8cc659cbd0872a13f398ddbaaa
psykose <alice@ayaya.dev>
2024-01-24 06:18:26 +0000

    contrib/wayfire: disable lto for now

--- contrib/wayfire/template.py
+++ contrib/wayfire/template.py
@@ -1,6 +1,6 @@
 pkgname = "wayfire"
 pkgver = "0.8.0"
-pkgrel = 3
+pkgrel = 4
 build_style = "meson"
 configure_args = [
     "-Duse_system_wfconfig=enabled",
@@ -30,6 +30,9 @@ source = f"https://github.com/WayfireWM/wayfire/releases/download/v{pkgver}/wayf
 sha256 = "6e6af885c08822e3a0b1fd748e1ee75e29bc000e376f6613b26c564f8cbc2baf"
 # vis breaks symbols
 hardening = ["!vis"]
+# FIXME: crashes in signal-provider.hpp::provider_t::emit from libblur
+# probably since clang17
+options = ["!lto"]
 
 
 def post_install(self):
