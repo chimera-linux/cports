165d53533e74041b7a9bbb567a86102b8e90d77c
psykose <alice@ayaya.dev>
2024-01-24 04:56:28 +0000

    main/lilv: update to 0.24.24

--- main/lilv/template.py
+++ main/lilv/template.py
@@ -1,6 +1,6 @@
 pkgname = "lilv"
-pkgver = "0.24.22"
-pkgrel = 1
+pkgver = "0.24.24"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
 makedepends = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "ISC"
 url = "https://drobilla.net/software/lilv.html"
 source = f"https://download.drobilla.net/{pkgname}-{pkgver}.tar.xz"
-sha256 = "76f949d0e59fc83363409b5ec5e15c1046fb7dd6589d3c1b920cec1fd29f9ff3"
+sha256 = "6bb6be9f88504176d0642f12de809b2b9e2dc55621a68adb8c7edb99aefabb4f"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
 
