45de070683ecc8dcf4b8aec66846f5dfef2b767b
psykose <alice@ayaya.dev>
2024-01-25 05:19:50 +0000

    contrib/netavark: update to 1.10.0

--- contrib/netavark/template.py
+++ contrib/netavark/template.py
@@ -1,5 +1,5 @@
 pkgname = "netavark"
-pkgver = "1.9.0"
+pkgver = "1.10.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo", "go-md2man", "protoc"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/containers/netavark"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "9ec50b715ded0a0699134c001656fdd1411e3fb5325d347695c6cb8cc5fcf572"
+sha256 = "35020dc6e5d45b0179e8590fe5b9c5f1a8cefc8e5ab94b6cd5447b86a85d1627"
 
 
 def do_install(self):
