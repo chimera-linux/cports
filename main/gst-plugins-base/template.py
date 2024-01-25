888ed01f5350feb4ae6c310caf2ffeebe8daa45f
psykose <alice@ayaya.dev>
2024-01-25 00:50:04 +0000

    main/gst-plugins-base: update to 1.22.9

--- main/gst-plugins-base/template.py
+++ main/gst-plugins-base/template.py
@@ -1,5 +1,5 @@
 pkgname = "gst-plugins-base"
-pkgver = "1.22.8"
+pkgver = "1.22.9"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -50,7 +50,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "eb6792e5c73c6defb9159c36ea6e4b78a2f8af6512678b4bd3b02c8d2d492acf"
+sha256 = "fac3e0dd2d8e9370388b34bf8c21b89d5f63bc3cfc12cd7fdc8fc6c1cba03334"
 # FIXME int
 hardening = ["!int"]
 # FIXME libs_allocators fail
