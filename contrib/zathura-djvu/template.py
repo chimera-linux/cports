50bb88baf4d987c02d92a0e0c4072102b3c64353
ttyyls <contact@behri.org>
2024-01-25 23:55:48 +0100

    contrib/zathura-djvu: new package (0.2.9)

---
+++ contrib/zathura-djvu/template.py
@@ -0,0 +1,23 @@
+pkgname = "zathura-djvu"
+pkgver = "0.2.9"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+]
+makedepends = [
+    "djvulibre-devel",
+    "zathura-devel",
+]
+depends = ["zathura"]
+pkgdesc = "Djvu support for zathura"
+maintainer = "ttyyls <contact@behri.org>"
+license = "Zlib"
+url = "https://pwmt.org/projects/zathura-djvu"
+source = f"{url}/download/{pkgname}-{pkgver}.tar.xz"
+sha256 = "96e6f8a6ee53231073b2f7003264872f84501e63c3da7bf0598d046286b0c200"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
