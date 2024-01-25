f1f5f1ff5ae65d42e4eda0f09cf2f59083e6f160
psykose <alice@ayaya.dev>
2024-01-25 16:21:34 +0000

    main/libpng: update to 1.6.41

--- main/libpng/template.py
+++ main/libpng/template.py
@@ -1,5 +1,5 @@
 pkgname = "libpng"
-pkgver = "1.6.40"
+pkgver = "1.6.41"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Libpng"
 url = "http://www.libpng.org/pub/png/libpng.html"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "535b479b2467ff231a3ec6d92a525906fb8ef27978be4f66dbe05d3f3a01b3a1"
+sha256 = "d6a49a7a4abca7e44f72542030e53319c081fea508daccf4ecc7c6d9958d190f"
 
 
 def post_install(self):
