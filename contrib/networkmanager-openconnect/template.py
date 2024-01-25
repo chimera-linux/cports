d7812f6e33dc92980efc27f1d84f2bb3fa7cfcdc
Erica Z <zerica@callcc.eu>
2024-01-26 00:38:39 +0100

    contrib/networkmanager-openconnect: new package (1.2.10)

---
+++ contrib/networkmanager-openconnect/template.py
@@ -0,0 +1,43 @@
+pkgname = "networkmanager-openconnect"
+pkgver = "1.2.10"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--with-gtk4=yes", "--enable-static=no"]
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "glib-devel",
+    "libtool",
+    "pkgconf",
+    "file",
+    "intltool",
+    "gettext-devel",
+    "gcr-progs",
+    "gmake",
+]
+makedepends = [
+    "gcr3-devel",
+    "gtk+3-devel",
+    "gtk4-devel",
+    "libnma-devel",
+    "libsecret-devel",
+    "libxml2-devel",
+    "networkmanager-devel",
+    "openconnect-devel",
+    "webkitgtk-devel",
+]
+pkgdesc = "OpenConnect support for NetworkManager"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "GPL-2.0-or-later"
+url = "https://gitlab.gnome.org/GNOME/NetworkManager-openconnect"
+source = f"{url}/-/archive/{pkgver}/NetworkManager-openconnect-{pkgver}.tar.bz2"
+sha256 = "df21a8730438b1614de390ecf1f73d379536d388a8e464c9a802dab14dd23c8f"
+
+
+def post_install(self):
+    self.install_file(
+        self.files_path / "sysusers.conf",
+        "usr/lib/sysusers.d",
+        name="nm-openconnect.conf",
+    )
