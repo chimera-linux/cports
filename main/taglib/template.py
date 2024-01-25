e47fda0de61d773e944799a1ff7b3f4fb4926ebc
psykose <alice@ayaya.dev>
2024-01-25 04:41:30 +0000

    main/taglib: update to 2.0

--- main/taglib/template.py
+++ main/taglib/template.py
@@ -1,19 +1,20 @@
 pkgname = "taglib"
-pkgver = "1.13.1"
+pkgver = "2.0"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DWITH_MP4=ON", "-DWITH_ASF=ON", "-DBUILD_SHARED_LIBS=ON"]
+configure_args = [
+    "-DBUILD_SHARED_LIBS=ON",
+    "-DBUILD_TESTING=ON",
+]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
-makedepends = ["zlib-devel"]
+makedepends = ["cppunit-devel", "utfcpp", "zlib-devel"]
 pkgdesc = "Library for accessing ID tags in various media files"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later OR MPL-1.1"
 url = "https://taglib.github.io"
 source = f"https://github.com/taglib/taglib/archive/v{pkgver}.tar.gz"
-sha256 = "c8da2b10f1bfec2cd7dbfcd33f4a2338db0765d851a50583d410bacf055cfd0b"
+sha256 = "e36ea877a6370810b97d84cf8f72b1e4ed205149ab3ac8232d44c850f38a2859"
 hardening = ["!cfi"]  # TODO
-# test target does not work with shared libs
-options = ["!check"]
 
 
 @subpackage("taglib-devel")
