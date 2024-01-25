dc983281352cecac9bc3f82624fc0d4595210f03
psykose <alice@ayaya.dev>
2024-01-25 03:28:12 +0000

    main/python-pluggy: update to 1.4.0

--- main/python-pluggy/template.py
+++ main/python-pluggy/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pluggy"
-pkgver = "1.3.0"
-pkgrel = 1
+pkgver = "1.4.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pytest-dev/pluggy"
 source = f"$(PYPI_SITE)/p/pluggy/pluggy-{pkgver}.tar.gz"
-sha256 = "cf61ae8f126ac6f7c451172cf30e3e43d3ca77615509771b3a984a0730651e12"
+sha256 = "8c85c2876142a764e5b7548e7d9a0e0ddb46f5185161049a79b7e974454223be"
 # dependency of pytest
 options = ["!check"]
 
