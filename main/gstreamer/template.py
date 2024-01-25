714e55f3979d67c49b3b5aba46409a83eba6d619
psykose <alice@ayaya.dev>
2024-01-25 00:49:59 +0000

    main/gstreamer: update to 1.22.9

--- main/gstreamer/template.py
+++ main/gstreamer/template.py
@@ -1,6 +1,6 @@
 pkgname = "gstreamer"
-pkgver = "1.22.8"
-pkgrel = 1
+pkgver = "1.22.9"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dptp-helper-permissions=none",  # manual
@@ -32,7 +32,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "ad4e3db1771139b1db17b1afa7c05db083ae0100bd4da244b71f162dcce41bfc"
+sha256 = "1e7124d347e8cdc80f08ec1d370c201be513002af1102bb20e83c5279cb48ebd"
 file_xattrs = {
     "usr/libexec/gstreamer-1.0/gst-ptp-helper": {
         "security.capability": "cap_net_bind_service,cap_net_admin+ep",
