f35c20dcf41059d2173699eb77d49659aaf74b3d
psykose <alice@ayaya.dev>
2024-01-27 06:41:20 +0000

    contrib/aardvark-dns: fix binary location
    closes https://github.com/chimera-linux/cports/issues/1330

--- contrib/aardvark-dns/template.py
+++ contrib/aardvark-dns/template.py
@@ -1,6 +1,6 @@
 pkgname = "aardvark-dns"
 pkgver = "1.10.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cargo"
 hostmakedepends = ["cargo"]
 makedepends = ["rust-std"]
@@ -10,3 +10,11 @@ license = "Apache-2.0"
 url = "https://github.com/containers/aardvark-dns"
 source = f"{url}/archive/v{pkgver}.tar.gz"
 sha256 = "b3e77b3ff4eb40f010c78ca00762761e8c639c47e1cb67686d1eb7f522fbc81e"
+
+
+def do_install(self):
+    self.install_file(
+        f"target/{self.profile().triplet}/release/aardvark-dns",
+        "usr/libexec/podman",
+        0o755,
+    )
