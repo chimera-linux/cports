f8964dc351763d65b2fd0efa5f6db0789b652edc
psykose <alice@ayaya.dev>
2024-01-27 00:16:14 +0000

    main/perl-mime-tools: update to 5.513

--- main/perl-mime-tools/template.py
+++ main/perl-mime-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-mime-tools"
-pkgver = "5.512"
+pkgver = "5.513"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["gmake", "perl"]
@@ -16,4 +16,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/release/MIME-tools"
 source = f"$(CPAN_SITE)/MIME/MIME-tools-{pkgver}.tar.gz"
-sha256 = "05a76bb173ba3971d1186981978033ca4631ebce17d44e564db8c8dba34ae99a"
+sha256 = "48d81841bcd45d5565c17f761ab0f730b4dc71e91a17959446503ce6b7e2da9c"
