5e1f2e497f1a46f3575e5e6c3a8fc5116f37f6a6
psykose <alice@ayaya.dev>
2024-01-27 13:59:30 +0000

    contrib/pangomm1.4: update to 2.46.4

--- contrib/pangomm1.4/template.py
+++ contrib/pangomm1.4/template.py
@@ -1,5 +1,5 @@
 pkgname = "pangomm1.4"
-pkgver = "2.46.3"
+pkgver = "2.46.4"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gtkmm.org"
 source = f"$(GNOME_SITE)/pangomm/{pkgver[:-2]}/pangomm-{pkgver}.tar.xz"
-sha256 = "410fe04d471a608f3f0273d3a17d840241d911ed0ff2c758a9859c66c6f24379"
+sha256 = "b92016661526424de4b9377f1512f59781f41fb16c9c0267d6133ba1cd68db22"
 
 
 @subpackage("pangomm1.4-devel")
