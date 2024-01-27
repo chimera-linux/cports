a3cf586d1e21092a6ba746ca184923913cc69e4f
psykose <alice@ayaya.dev>
2024-01-27 16:37:17 +0000

    user/vim: new package (9.1.0059)

---
+++ user/vim/template.py
@@ -0,0 +1,55 @@
+pkgname = "vim"
+pkgver = "9.1.0059"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-acl",
+    "--enable-gui=no",
+    # makes them dynamically loaded so we don't install every scripting language
+    # by default
+    "--enable-luainterp=dynamic",
+    "--enable-rubyinterp=dynamic",
+    "--enable-python3interp=dynamic",
+    "--with-compiledby=Chimera Linux",
+    "--without-x",
+]
+# completely broken reconf for some reason
+configure_gen = []
+make_cmd = "gmake"
+make_dir = "."
+make_check_target = "test"
+hostmakedepends = ["gmake"]
+makedepends = [
+    "acl-devel",
+    "libsodium-devel",
+    "lua5.4-devel",
+    "ncurses-devel",
+    "ruby-devel",
+    "python-devel",
+]
+depends = [f"xxd={pkgver}-r{pkgrel}"]
+pkgdesc = "Vi-style text editor"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Vim"
+url = "https://vim.org"
+source = f"https://github.com/vim/vim/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "7463ef80dc13d6a26cef1fa65a8f33c618ed4e324b1e7d133a2458aba1c0848a"
+# FIXME cfi int
+hardening = ["vis", "!cfi", "!int"]
+# TODO
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_file(self.files_path / "vimrc", "etc")
+    self.install_license("LICENSE")
+    # chimerautils-extra ex/view conflict with these symlinks
+    # TODO: just rename and update the code in main.c:parse_command_name
+    self.rm(self.destdir / "usr/bin/ex")
+    self.rm(self.destdir / "usr/bin/view")
+
+
+@subpackage("xxd")
+def _xxd(self):
+    self.pkgdesc = "Tool for viewing/editing hex dumps"
+    return ["usr/bin/xxd"]
