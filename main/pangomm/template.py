3ef170bbdf2c5b9e84e911a595299974fcc33334
psykose <alice@ayaya.dev>
2024-01-27 13:59:29 +0000

    main/pangomm: update to 2.50.2

--- main/pangomm/template.py
+++ main/pangomm/template.py
@@ -1,5 +1,5 @@
 pkgname = "pangomm"
-pkgver = "2.50.1"
+pkgver = "2.50.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gtkmm.org"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "ccc9923413e408c2bff637df663248327d72822f11e394b423e1c5652b7d9214"
+sha256 = "1bc5ab4ea3280442580d68318226dab36ceedfc3288f9d83711cf7cfab50a9fb"
 
 
 @subpackage("pangomm-devel")
