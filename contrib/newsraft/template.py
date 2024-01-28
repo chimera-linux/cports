e374f77b2fa22e9ca92a3941cf5e1efbd0ed1e19
ttyyls <contact@behri.org>
2024-01-28 20:37:09 +0100

    contrib/newsraft: update to (0.23)

--- contrib/newsraft/template.py
+++ contrib/newsraft/template.py
@@ -1,5 +1,5 @@
 pkgname = "newsraft"
-pkgver = "0.22"
+pkgver = "0.23"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = [
@@ -19,7 +19,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "ISC"
 url = "https://codeberg.org/newsraft/newsraft"
 source = f"{url}/archive/{pkgname}-{pkgver}.tar.gz"
-sha256 = "16dbda602a37a2c8052421e90dd00ca065bab4130c6c2c16d5ca2a8a184990b9"
+sha256 = "22c835f56ff84a4aadc86d9e56a5d8e531cc966ff6a1404f0b3f8f1a1a0655dc"
 hardening = ["vis", "cfi"]
 # checks for rfc3339 compliance fail
 # also the tests don't build with vis
