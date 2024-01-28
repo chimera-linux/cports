75d528d79f854a6baa20218822210f1d0de1a7bc
psykose <alice@ayaya.dev>
2024-01-28 17:31:38 +0000

    main/pixman: update to 0.43.2

--- main/pixman/template.py
+++ main/pixman/template.py
@@ -1,5 +1,5 @@
 pkgname = "pixman"
-pkgver = "0.43.0"
+pkgver = "0.43.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -18,7 +18,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pixman.org"
 source = f"https://www.cairographics.org/releases/{pkgname}-{pkgver}.tar.gz"
-sha256 = "a65c28209858fb16bee50d809c80f90a8e415c0e4fd8321078a1822785a5560a"
+sha256 = "ea79297e5418fb528d0466e8b5b91d1be88857fa3706f49777b2925a72ae9924"
 # needs a lot larger stack than musl default
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=2097152"]}
 # FIXME int (test fails)
