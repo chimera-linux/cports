16e37bd966a4e0bc06bc57530e445ae5710c12aa
Mohammed Anas <triallax@tutanota.com>
2024-01-29 21:24:58 +0000

    main/perl-uri: update to 5.25

--- main/perl-uri/template.py
+++ main/perl-uri/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-uri"
-pkgver = "5.24"
+pkgver = "5.25"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["gmake", "perl"]
@@ -10,6 +10,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/release/URI"
 source = f"$(CPAN_SITE)/URI/URI-{pkgver}.tar.gz"
-sha256 = "466dd5a40b2c8e99d53692dc49b971561d5d7c2a30814e5ff0b63a4a25e2aaef"
+sha256 = "ef61941da09fff2503cea3692c4f2fc48c0e4442486e2a28345cb32d44d0d271"
 # missing checkdepends
 options = ["!check"]
