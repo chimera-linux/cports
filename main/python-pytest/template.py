1586788da162441bfe405b00630685f721a9891a
psykose <alice@ayaya.dev>
2024-01-29 22:51:17 +0000

    main/python-pytest: update to 8.0.0

--- main/python-pytest/template.py
+++ main/python-pytest/template.py
@@ -1,34 +1,28 @@
 pkgname = "python-pytest"
-pkgver = "7.4.4"
+pkgver = "8.0.0"
 pkgrel = 0
 build_style = "python_pep517"
-hostmakedepends = [
-    "gmake",
-    "python-attrs",
-    "python-build",
+_deps = [
     "python-iniconfig",
-    "python-installer",
+    "python-packaging",
     "python-pluggy",
     "python-py",
+]
+hostmakedepends = [
+    "gmake",
+    "python-build",
+    "python-installer",
     "python-setuptools_scm",
     "python-sphinx",
     "python-wheel",
-]
-depends = [
-    "python-packaging",
-    "python-tomli",
-    "python-attrs",
-    "python-iniconfig",
-    "python-py",
-    "python-pluggy",
-    "python",
-]
+] + _deps
+depends = list(_deps)
 pkgdesc = "Python unit testing framework"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://docs.pytest.org/en/latest"
 source = f"$(PYPI_SITE)/p/pytest/pytest-{pkgver}.tar.gz"
-sha256 = "2cf0005922c6ace4a3e2ec8b4080eb0d9753fdc93107415332f50ce9e7994280"
+sha256 = "249b1b0864530ba251b7438274c4d251c58d868edaaec8762893ad4a0d71c36c"
 # missing checkdepends
 options = ["!check"]
 
