95dd6c8c30892cdb1cc5545f506fd572e6f10cfd
psykose <alice@ayaya.dev>
2024-01-31 03:16:47 +0000

    main/xdg-utils: update to 1.2.0

--- main/xdg-utils/template.py
+++ main/xdg-utils/template.py
@@ -1,7 +1,6 @@
 pkgname = "xdg-utils"
-pkgver = "1.1.3"
-pkgrel = 1
-_commit = "8ae02631a9806da11b34cd6b274af02d28aee5da"
+pkgver = "1.2.0"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
 make_cmd = "gmake"
@@ -12,9 +11,9 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.freedesktop.org/wiki/Software/xdg-utils"
 source = (
-    f"https://gitlab.freedesktop.org/xdg/{pkgname}/-/archive/{_commit}.tar.gz"
+    f"https://gitlab.freedesktop.org/xdg/xdg-utils/-/archive/v{pkgver}.tar.gz"
 )
-sha256 = "c761bbb0c1f6d21a39e52690909ba57c9d120ec20919d3c4aa0353c834470ed6"
+sha256 = "082b2f13537b7e7e8ced1f3a98eea4f9bfd35967468fc4a0be42cb614301fb27"
 hardening = ["vis", "cfi"]
 # no check target
 options = ["!check"]
