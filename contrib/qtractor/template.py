62d6b2224b5f8c8d934853a2311b8241502621d7
Erica Z <zerica@callcc.eu>
2024-02-01 08:21:34 +0100

    contrib/qtractor: new package (0.9.39)

---
+++ contrib/qtractor/template.py
@@ -0,0 +1,25 @@
+pkgname = "qtractor"
+pkgver = "0.9.39"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DCONFIG_WAYLAND=1"]
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = [
+    "alsa-lib-devel",
+    "ladspa-sdk",
+    "liblo-devel",
+    "libsamplerate-devel",
+    "libsndfile-devel",
+    "lilv-devel",
+    "pipewire-jack-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtsvg-devel",
+    "qt6-qttools-devel",
+    "rubberband-devel",
+]
+pkgdesc = "Audio/MIDI multi-track sequencer"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "GPL-2.0-or-later"
+url = "https://www.qtractor.org"
+source = f"$(SOURCEFORGE_SITE)/qtractor/qtractor-{pkgver}.tar.gz"
+sha256 = "e60c8f371b61ac16d21ef96f25b434aefc5512aebcb90120faa9f11d06fe3555"
