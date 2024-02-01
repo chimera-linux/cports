eb19f5e16ccbb44094805e51b22249ad116f02ef
psykose <alice@ayaya.dev>
2024-02-01 23:30:58 +0000

    main/libusb: update to 1.0.27

--- main/libusb/template.py
+++ main/libusb/template.py
@@ -1,21 +1,18 @@
 pkgname = "libusb"
-pkgver = "1.0.26"
+pkgver = "1.0.27"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 makedepends = ["udev-devel", "linux-headers"]
 pkgdesc = "Library for userspace USB device access"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://libusb.info"
 source = f"https://github.com/libusb/libusb/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "12ce7a61fc9854d1d2a1ffe095f7b5fac19ddba095c259e6067a46500381b5a5"
+sha256 = "ffaa41d741a8a3bee244ac8e54a72ea05bf2879663c098c82fc5757853441575"
 
 
 @subpackage("libusb-devel")
 def _devel(self):
     self.depends = ["virtual:pc:libudev!udev-devel"]
     return self.default_devel()
-
-
-configure_gen = []
