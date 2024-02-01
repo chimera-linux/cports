995c2b7686c03a45ec6724c6a154ebe4f43fc5e8
psykose <alice@ayaya.dev>
2024-02-01 22:36:10 +0000

    main/vulkan-tools: update to 1.3.277

--- main/vulkan-tools/template.py
+++ main/vulkan-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-tools"
-pkgver = "1.3.276"
+pkgver = "1.3.277"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -26,7 +26,7 @@ url = "https://www.khronos.org/vulkan"
 source = (
     f"https://github.com/KhronosGroup/Vulkan-Tools/archive/v{pkgver}.tar.gz"
 )
-sha256 = "24598dd9031cd7cce9b7d9446b466a18bae9fd009579da865330e2dc9d0a2bf9"
+sha256 = "487dd46f82a07ab445dc5338aa978bdacebf20f816ec0cede3d3a7e036d1a925"
 # FIXME: vkcube etc fail
 hardening = ["vis", "!cfi"]
 # no test suite
