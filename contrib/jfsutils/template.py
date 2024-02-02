18ba8a6d262210f7368375727d20270fa26629f0
miko <mikoxyzzz@gmail.com>
2024-02-02 19:18:28 +0100

    contrib/jfsutils: new package (1.1.15)

---
+++ contrib/jfsutils/template.py
@@ -0,0 +1,19 @@
+pkgname = "jfsutils"
+pkgver = "1.1.15"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = [
+    "automake",
+    "gmake",
+]
+makedepends = ["libuuid-devel"]
+pkgdesc = "JFS filesystem utilities"
+maintainer = "miko <mikoxyzzz@gmail.com>"
+license = "GPL-3.0-only"
+url = "https://jfs.sourceforge.net"
+source = f"{url}/project/pub/jfsutils-{pkgver}.tar.gz"
+sha256 = "244a15f64015ce3ea17e49bdf6e1a0fb4f9af92b82fa9e05aa64cb30b5f07a4d"
+hardening = ["cfi", "vis"]
+# no tests
+options = ["!check"]
