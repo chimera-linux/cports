1a0076422b45aa76f9484641c15b39a3dfc9846c
psykose <alice@ayaya.dev>
2024-02-02 02:46:10 +0000

    contrib/eza: update to 0.18.0

--- contrib/eza/template.py
+++ contrib/eza/template.py
@@ -1,5 +1,5 @@
 pkgname = "eza"
-pkgver = "0.17.3"
+pkgver = "0.18.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo", "pkgconf"]
@@ -15,7 +15,7 @@ url = "https://eza.rocks"
 source = (
     f"https://github.com/eza-community/eza/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "8facf4f39fed32b3562b70b2771a8ef3309efd68fd04685446a6df631f133cc9"
+sha256 = "50f6187fa10eb7d2405477ed2b4dfbda7e51d3746b3660664f39b50d74c856a1"
 
 
 def post_install(self):
