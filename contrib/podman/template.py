ab4a8cf020728acee51cadd4a1e9f2b86017059f
psykose <alice@ayaya.dev>
2024-02-03 04:07:32 +0000

    contrib/podman: update to 4.9.2

--- contrib/podman/template.py
+++ contrib/podman/template.py
@@ -1,5 +1,5 @@
 pkgname = "podman"
-pkgver = "4.9.1"
+pkgver = "4.9.2"
 pkgrel = 0
 build_style = "go"
 make_build_args = ["-mod", "vendor", "./cmd/podman", "./cmd/rootlessport"]
@@ -45,7 +45,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://podman.io"
 source = f"https://github.com/containers/podman/archive/v{pkgver}.tar.gz"
-sha256 = "aa60ff61d838eacadeaede1afbe80ef46c1f17f5c0bfbedc08e99c5aa2e106b3"
+sha256 = "5696e2ec07020a5a147444abd0cd6f8563412190212e1f12e6c1e110da0cc6d2"
 # objcopy fails to split on ppc
 options = ["!debug", "!check"]
 
