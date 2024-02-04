6f373d68c733095217a9d1a1d65d20f065240ce3
psykose <alice@ayaya.dev>
2024-02-04 05:07:45 +0000

    contrib/xprop: update to 1.2.7

--- contrib/xprop/template.py
+++ contrib/xprop/template.py
@@ -1,20 +1,17 @@
 pkgname = "xprop"
-pkgver = "1.2.6"
-pkgrel = 1
+pkgver = "1.2.7"
+pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf", "xorg-util-macros"]
 makedepends = ["libx11-devel", "libxmu-devel"]
 pkgdesc = "X property displayer"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
-sha256 = "58ee5ee0c47fa454d3b16312e778c3f549605a8ad0fd0daafc70d2d6174b116d"
+sha256 = "11c06a876b0aa0bfac6cbfe4b3ebe1f5062f8b39b9b1b6c136a8629265f134b6"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
