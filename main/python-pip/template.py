2de69011311521277fbab5afa15a04bf8201eb6b
psykose <alice@ayaya.dev>
2024-02-04 10:12:40 +0000

    main/python-pip: update to 24.0

--- main/python-pip/template.py
+++ main/python-pip/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pip"
-pkgver = "23.3.2"
-pkgrel = 1
+pkgver = "24.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pip.pypa.io"
 source = f"$(PYPI_SITE)/p/pip/pip-{pkgver}.tar.gz"
-sha256 = "7fd9972f96db22c8077a1ee2691b172c8089b17a5652a44494a9ecb0d78f9149"
+sha256 = "ea9bd1a847e8c5774a5777bb398c19e80bcd4e2aa16a4b301b718fe6f593aba2"
 # unpackaged dependencies
 options = ["!check"]
 
