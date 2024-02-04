01fc4b9422b20e2cbbe0d8dbac2d603e49d5ac40
psykose <alice@ayaya.dev>
2024-02-04 05:06:32 +0000

    main/xkbcomp: update to 1.4.7

--- main/xkbcomp/template.py
+++ main/xkbcomp/template.py
@@ -1,20 +1,17 @@
 pkgname = "xkbcomp"
-pkgver = "1.4.6"
+pkgver = "1.4.7"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf", "bison"]
+hostmakedepends = ["automake", "pkgconf", "bison", "xorg-util-macros"]
 makedepends = ["libx11-devel", "libxkbfile-devel"]
 pkgdesc = "XKBD keymap compiler"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
-sha256 = "b216a2c8c0eab83f3dc4a3d5ee2bdf7827b30e49c8907035d0f222138eca0987"
+sha256 = "00cecc490fcbe2f789cf13c408c459673c2c33ab758d802677321cffcda35373"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
