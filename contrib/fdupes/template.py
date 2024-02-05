91b48909b19cbc0f2fb007e0041b3b1409410e03
psykose <alice@ayaya.dev>
2024-02-05 07:03:30 +0000

    contrib/fdupes: update to 2.3.0

--- contrib/fdupes/template.py
+++ contrib/fdupes/template.py
@@ -1,15 +1,15 @@
 pkgname = "fdupes"
-pkgver = "2.2.1"
+pkgver = "2.3.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
-makedepends = ["ncurses-devel", "pcre2-devel"]
+makedepends = ["ncurses-devel", "pcre2-devel", "sqlite-devel"]
 pkgdesc = "Tool for identifying and/or deleting duplicate files"
 maintainer = "autumnontape <autumn@cyfox.net>"
 license = "MIT"
 url = "https://github.com/adrianlopezroche/fdupes"
 source = f"{url}/releases/download/v{pkgver}/fdupes-{pkgver}.tar.gz"
-sha256 = "846bb79ca3f0157856aa93ed50b49217feb68e1b35226193b6bc578be0c5698d"
+sha256 = "6170d64a7e565ee314cca4dd25a123e60aa1e3febb11e57078bebb9c1da7e019"
 hardening = ["vis", "cfi"]
 
 
