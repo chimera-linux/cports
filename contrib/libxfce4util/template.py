c0f27866f5a0a3edc5c11fef6f505cf08f8a301b
psykose <alice@ayaya.dev>
2024-02-05 21:24:21 +0000

    contrib/libxfce4util: update to 4.18.2

--- contrib/libxfce4util/template.py
+++ contrib/libxfce4util/template.py
@@ -1,7 +1,9 @@
 pkgname = "libxfce4util"
-pkgver = "4.18.1"
+pkgver = "4.18.2"
 pkgrel = 0
 build_style = "gnu_configure"
+# generates invalid configure
+configure_gen = []
 make_cmd = "gmake"
 hostmakedepends = ["pkgconf", "gmake", "intltool", "vala"]
 makedepends = ["glib-devel"]
@@ -10,12 +12,9 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
 url = "https://xfce.org"
 source = f"https://archive.xfce.org/src/xfce/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "8a52063a5adc66252238cad9ee6997909b59983ed21c77eb83c5e67829d1b01f"
+sha256 = "d9a329182b78f7e2520cd4aafcbb276bbbf162f6a89191676539ad2e3889c353"
 
 
 @subpackage("libxfce4util-devel")
 def _dev(self):
     return self.default_devel()
-
-
-configure_gen = []
