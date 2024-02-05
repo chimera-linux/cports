03448b28b504115fbef86e6ff5e70d36ee552292
psykose <alice@ayaya.dev>
2024-02-05 06:58:59 +0000

    main/libxkbfile: update to 1.1.3

--- main/libxkbfile/template.py
+++ main/libxkbfile/template.py
@@ -1,15 +1,15 @@
 pkgname = "libxkbfile"
-pkgver = "1.1.2"
+pkgver = "1.1.3"
 pkgrel = 0
-build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
 makedepends = ["xorgproto", "libx11-devel"]
 pkgdesc = "Xkbfile library from X.org"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.gz"
-sha256 = "d1a7e659bc7ae1aa1fc1ecced261c734df5ad5d86af1ef7a946be0e2d841e51d"
+source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.xz"
+sha256 = "a9b63eea997abb9ee6a8b4fbb515831c841f471af845a09de443b28003874bec"
 
 
 def post_install(self):
@@ -19,6 +19,3 @@ def post_install(self):
 @subpackage("libxkbfile-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
