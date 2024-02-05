c1c4b8edf5ae1e24b5754188ff7dee03ca4d595f
psykose <alice@ayaya.dev>
2024-02-05 21:23:10 +0000

    main/ccache: update to 4.9.1

--- main/ccache/template.py
+++ main/ccache/template.py
@@ -1,5 +1,5 @@
 pkgname = "ccache"
-pkgver = "4.9"
+pkgver = "4.9.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DENABLE_TESTING=OFF", "-DREDIS_STORAGE_BACKEND=OFF"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://ccache.samba.org"
 source = f"https://github.com/ccache/ccache/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "1ebc72324e3ab52af0b562bf54189d108e85eef6478d6304a345a3c2dc4018e0"
+sha256 = "4c03bc840699127d16c3f0e6112e3f40ce6a230d5873daa78c60a59c7ef59d25"
 hardening = ["vis", "cfi"]
 # not properly set up
 options = ["!check"]
