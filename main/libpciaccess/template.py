63079d8647a3e2b9b56e693bbdaf84bd231c4287
psykose <alice@ayaya.dev>
2024-02-05 06:56:42 +0000

    main/libpciaccess: update to 0.18

--- main/libpciaccess/template.py
+++ main/libpciaccess/template.py
@@ -1,14 +1,14 @@
 pkgname = "libpciaccess"
-pkgver = "0.17"
+pkgver = "0.18"
 pkgrel = 0
-build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
 pkgdesc = "X11 PCI access library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.gz"
-sha256 = "bf6985a77d2ecb00e2c79da3edfb26b909178ffca3f2e9d14ed0620259ab733b"
+source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.xz"
+sha256 = "5461b0257d495254346f52a9c329b44b346262663675d3fecdb204a7e7c262a9"
 
 
 def post_install(self):
@@ -18,6 +18,3 @@ def post_install(self):
 @subpackage("libpciaccess-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
