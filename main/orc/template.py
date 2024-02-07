a76c2f0003c9a9a4e4eb26a5b4709ed442d439d1
psykose <alice@ayaya.dev>
2024-02-07 20:40:18 +0000

    main/orc: update to 0.4.37
    the crashes were most likely fixed with
    https://gitlab.freedesktop.org/gstreamer/orc/-/commit/960b16b92e2981cf7791cf92f22fe6780a4f936e

--- main/orc/template.py
+++ main/orc/template.py
@@ -1,11 +1,6 @@
 pkgname = "orc"
-# FIXME: rebuilding gst-plugins-base with 0.4.35 then running gst-libav tests
-# crashes with SIGILL in libgstvideotestsrc.so`gst_video_test_src_smpte
-# at videotestsrc.c:400
-# for some reason the function pointer call leads to garbage
-# only on powerpc/aarch64
-pkgver = "0.4.34"
-pkgrel = 1
+pkgver = "0.4.37"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dexamples=disabled",
@@ -21,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "8f47abb3f097171e44eb807adcdabd860fba2effd37d8d3c4fbd5f341cadd41f"
+sha256 = "85638c0d447d989cd0d7e03406adbfbc380e67db2a622a4727a0ce3d440b2974"
 
 
 def post_install(self):
