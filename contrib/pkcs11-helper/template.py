679dbc74e4b0ad9856afc545e9d6c87fdacb66ac
Dmitriy Vakhrushev <dvakhrushev@netgate.com>
2024-02-07 23:48:44 +0700

    contrib/pkcs11-helper: new package (1.30.0)

---
+++ contrib/pkcs11-helper/template.py
@@ -0,0 +1,29 @@
+pkgname = "pkcs11-helper"
+pkgver = "1.30.0"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-slotevent",
+    "--enable-threading",
+]
+make_cmd = "gmake"
+hostmakedepends = ["automake", "pkgconf", "libtool", "gmake"]
+makedepends = ["openssl-devel"]
+pkgdesc = "Helper library for multiple PKCS#11 providers"
+maintainer = "Dmitriy Vakhrushev <dvakhrushev@netgate.com>"
+license = "GPL-2.0-only AND BSD-3-Clause"
+url = "https://github.com/OpenSC/pkcs11-helper"
+source = f"{url}/archive/refs/tags/pkcs11-helper-{pkgver}.tar.gz"
+sha256 = "076c9f664812a45f2da25efc157338b0b8bb1949117f0144050fec176b6fdf78"
+# vis breaks symbols
+hardening = ["!vis"]
+
+
+def post_install(self):
+    self.install_license("COPYING")
+    self.install_license("COPYING.BSD")
+
+
+@subpackage("pkcs11-helper-devel")
+def _devel(self):
+    return self.default_devel()
