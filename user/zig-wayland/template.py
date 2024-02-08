dbdfb5be9daf5cdfe1cf0351de1cf4d81ae10205
Isaac Freund <mail@isaacfreund.com>
2024-02-08 22:35:51 +0100

    cbuild: add zig_package build style

--- user/zig-wayland/template.py
+++ user/zig-wayland/template.py
@@ -1,20 +1,14 @@
 pkgname = "zig-wayland"
 pkgver = "0.1.0"
 pkgrel = 0
+build_style = "zig_package"
+hostmakedepends = ["zig"]
 pkgdesc = "Zig bindings for libwayland"
 maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "MIT"
 url = "https://codeberg.org/ifreund/zig-wayland"
 source = f"{url}/archive/e47e885f5361f7b4d25cc4a7ae856f276b314b34.tar.gz"
 sha256 = "46ce978744d2b4e5ba563b220a9a7a320fc1ea7b15b82800825efd685fa7e0af"
-
-
-def do_install(self):
-    self.install_files(
-        ".",
-        "usr/src/zig/packages/",
-        name="122062beeb6fd2bb21c91e81acb3ea6cbba69d3c00a31b62732254e190b5fc7a934e",
-    )
 
 
 def post_install(self):
