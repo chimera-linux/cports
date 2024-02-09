8be6f69e5361ae2747b690b095e85defacc18540
psykose <alice@ayaya.dev>
2024-02-09 17:29:51 +0000

    main/poppler: update to 24.03.0
    also remove the other -devel packages since there's no real need to have
    granularity there

--- main/poppler/template.py
+++ main/poppler/template.py
@@ -1,5 +1,5 @@
 pkgname = "poppler"
-pkgver = "24.01.0"
+pkgver = "24.03.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -15,28 +15,28 @@ configure_args = [
 ]
 hostmakedepends = [
     "cmake",
+    "glib-devel",
+    "gobject-introspection",
     "ninja",
     "pkgconf",
-    "gobject-introspection",
-    "glib-devel",
 ]
 makedepends = [
-    "glib-devel",
+    "boost-devel",
     "cairo-devel",
+    "glib-devel",
     "lcms2-devel",
     "libcurl-devel",
-    "boost-devel",
     "libpng-devel",
     "libtiff-devel",
-    "openjpeg-devel",
     "nss-devel",
+    "openjpeg-devel",
 ]
 pkgdesc = "PDF rendering library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only OR GPL-3.0-only"
 url = "https://poppler.freedesktop.org"
 source = f"{url}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "c7def693a7a492830f49d497a80cc6b9c85cb57b15e9be2d2d615153b79cae08"
+sha256 = "bafbf0db5713dec25b5d16eb2cd87e4a62351cdc40f050c3937cd8dd6882d446"
 # needs unshipped sample files
 options = ["!check"]
 
@@ -53,14 +53,7 @@ def _lib(self):
 def _devel(self):
     self.pkgdesc = f"{pkgdesc} (development files)"
 
-    return [
-        "usr/include/poppler/*.h",
-        "usr/include/poppler/splash",
-        "usr/include/poppler/fofi",
-        "usr/include/poppler/goo",
-        "usr/lib/libpoppler.so",
-        "usr/lib/pkgconfig/poppler.pc",
-    ]
+    return self.default_devel()
 
 
 @subpackage("libpoppler-cpp")
@@ -70,33 +63,8 @@ def _cpp_lib(self):
     return ["usr/lib/libpoppler-cpp.so.*"]
 
 
-@subpackage("libpoppler-cpp-devel")
-def _cpp_devel(self):
-    self.pkgdesc = f"{pkgdesc} (C++ development files)"
-    self.depends += [f"libpoppler-devel={pkgver}-r{pkgrel}"]
-
-    return [
-        "usr/include/poppler/cpp",
-        "usr/lib/libpoppler-cpp.so",
-        "usr/lib/pkgconfig/poppler-cpp.pc",
-    ]
-
-
 @subpackage("libpoppler-glib")
 def _glib(self):
     self.pkgdesc = f"{pkgdesc} (GLib binding)"
 
     return ["usr/lib/libpoppler-glib.so.*", "usr/lib/girepository-1.0"]
-
-
-@subpackage("libpoppler-glib-devel")
-def _glib_devel(self):
-    self.pkgdesc = f"{pkgdesc} (GLib development files)"
-    self.depends += [f"libpoppler-devel={pkgver}-r{pkgrel}"]
-
-    return [
-        "usr/include/poppler/glib",
-        "usr/lib/libpoppler-glib.so",
-        "usr/lib/pkgconfig/poppler-glib.pc",
-        "usr/share/gir-1.0",
-    ]
