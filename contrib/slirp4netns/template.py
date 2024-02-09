348584c134ee33edbb81d89101106918be786d73
q66 <q66@chimera-linux.org>
2024-02-09 13:40:33 +0100

    contrib/slirp4netns: disable cfi for now
    Fixes https://github.com/chimera-linux/cports/issues/1400

--- contrib/slirp4netns/template.py
+++ contrib/slirp4netns/template.py
@@ -1,6 +1,6 @@
 pkgname = "slirp4netns"
 pkgver = "1.2.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 hostmakedepends = [
     "automake",
@@ -18,7 +18,8 @@ license = "GPL-2.0-or-later"
 url = "https://github.com/rootless-containers/slirp4netns"
 source = f"https://github.com/rootless-containers/slirp4netns/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "acce648fab8fe5f113c41a8fd6d20177708519b4ddaa60f845e1998a17b22ca5"
-hardening = ["vis", "cfi"]
+# cfi failure likely due to libslirp non-cfi timer shenanigans
+hardening = ["vis", "!cfi"]
 # needs ncat from nmap
 options = ["!check"]
 
