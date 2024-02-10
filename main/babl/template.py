6bc70f1174d48c6651ebfdb2337521f69222ea5c
psykose <alice@ayaya.dev>
2024-02-10 16:11:45 +0000

    main/babl: update to 0.1.108

--- main/babl/template.py
+++ main/babl/template.py
@@ -1,5 +1,5 @@
 pkgname = "babl"
-pkgver = "0.1.106"
+pkgver = "0.1.108"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Denable-gir=true", "-Dwith-docs=false"]
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-only"
 url = "https://gegl.org/babl"
 source = f"https://download.gimp.org/pub/{pkgname}/{pkgver[:-4]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "d325135d3304f088c134cc620013acf035de2e5d125a50a2d91054e7377c415f"
+sha256 = "26defe9deaab7ac4d0e076cab49c2a0d6ebd0df0c31fd209925a5f07edee1475"
 # FIXME all tests fail
 hardening = ["!int"]
 
