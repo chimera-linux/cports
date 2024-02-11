472cb34affd4fec27a6738ecc5fe8855c85dfe6c
Isaac Freund <mail@isaacfreund.com>
2024-02-11 23:20:09 +0100

    contrib/pyonji: new package (0.1.0)

---
+++ contrib/pyonji/template.py
@@ -0,0 +1,11 @@
+pkgname = "pyonji"
+pkgver = "0.1.0"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go"]
+pkgdesc = "Easy-to-use tool to send e-mail patches"
+maintainer = "Isaac Freund <mail@isaacfreund.com>"
+license = "GPL-3.0-only"
+url = "https://sr.ht/~emersion/pyonji"
+source = f"https://git.sr.ht/~emersion/pyonji/refs/download/v{pkgver}/pyonji-{pkgver}.tar.gz"
+sha256 = "88608121b9dd88ed89f598b39befac94eaeff061b65971625f8ed9607bce7996"
