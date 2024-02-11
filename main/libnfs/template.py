8beb330a0a10147bdfa22630521404c4ad5d938c
psykose <alice@ayaya.dev>
2024-02-11 11:32:36 +0000

    main/libnfs: update to 5.0.3

--- main/libnfs/template.py
+++ main/libnfs/template.py
@@ -1,5 +1,5 @@
 pkgname = "libnfs"
-pkgver = "5.0.2"
+pkgver = "5.0.3"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "libtool"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/sahlberg/libnfs"
 source = f"{url}/archive/{pkgname}-{pkgver}.tar.gz"
-sha256 = "637e56643b19da9fba98f06847788c4dad308b723156a64748041035dcdf9bd3"
+sha256 = "d945cb4f4c8f82ee1f3640893a168810f794a28e1010bb007ec5add345e9df3e"
 
 
 @subpackage("libnfs-devel")
