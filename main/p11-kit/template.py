f32d89c33629809877fe2f25efa9913d142ce49d
Mohammed Anas <triallax@tutanota.com>
2024-02-13 13:41:37 +0000

    main/p11-kit: update to 0.25.3

--- main/p11-kit/template.py
+++ main/p11-kit/template.py
@@ -1,5 +1,5 @@
 pkgname = "p11-kit"
-pkgver = "0.24.1"
+pkgver = "0.25.3"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -27,7 +27,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/p11-glue/p11-kit"
 source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "d8be783efd5cd4ae534cee4132338e3f40f182c3205d23b200094ec85faaaef8"
+sha256 = "d8ddce1bb7e898986f9d250ccae7c09ce14d82f1009046d202a0eb1b428b2adc"
 
 
 def post_install(self):
