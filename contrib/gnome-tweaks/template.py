e98a4fc8738d5f233b3e534de7d63025e606aed7
Mohammed Anas <triallax@tutanota.com>
2024-02-13 19:55:34 +0000

    contrib/gnome-tweaks: update to 45.1

--- contrib/gnome-tweaks/template.py
+++ contrib/gnome-tweaks/template.py
@@ -1,6 +1,6 @@
 pkgname = "gnome-tweaks"
-pkgver = "45.0"
-pkgrel = 1
+pkgver = "45.1"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "gettext"]
 makedepends = ["libhandy-devel"]
@@ -18,4 +18,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later AND CC0-1.0"
 url = "https://wiki.gnome.org/Apps/Tweaks"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "253994658ae861794d0c6e0e0f475dfe1caf277e3674b8792790238f34cfd6ee"
+sha256 = "95ffa7f38d9b1dfd5e4cebefb752419fea21cd407021395adc9f112854416d4f"
