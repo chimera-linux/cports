34714f2d7b5d8980967bd258a5d03d5eee0c6d65
Mohammed Anas <triallax@tutanota.com>
2024-02-13 09:30:42 +0000

    contrib/numbat: update to 1.10.1

--- contrib/numbat/template.py
+++ contrib/numbat/template.py
@@ -1,5 +1,5 @@
 pkgname = "numbat"
-pkgver = "1.9.0"
+pkgver = "1.10.1"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo"]
@@ -9,10 +9,21 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "MIT OR Apache-2.0"
 url = "https://github.com/sharkdp/numbat"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5a0435bf938d6166c5089106cfb39049b0b207dec69a96552ccc3f114d515fd9"
+sha256 = "e593983a42fe138bf84a2172537c5c1763a8743c65a952fbfd8df67a17f04526"
 
 
 def do_install(self):
     self.cargo.install(wrksrc="numbat-cli")
     self.install_license("LICENSE-MIT")
     self.install_files("numbat/modules", "usr/share/numbat")
+
+    self.install_file("assets/numbat.desktop", "usr/share/applications")
+    self.install_file(
+        "assets/numbat.svg", "usr/share/icons/hicolor/scalable/apps"
+    )
+    for f in [16, 22, 24, 32, 48, 64, 128, 256, 512]:
+        self.install_file(
+            f"assets/numbat-{f}x{f}.png",
+            f"usr/share/icons/hicolor/{f}x{f}/apps",
+            name="numbat.png",
+        )
