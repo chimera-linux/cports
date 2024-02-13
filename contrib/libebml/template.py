27ba439586e79a480f73edda2a6a1f244dbd1baf
psykose <alice@ayaya.dev>
2024-02-13 17:13:02 +0000

    contrib/libebml: new package (1.4.5)

---
+++ contrib/libebml/template.py
@@ -0,0 +1,21 @@
+pkgname = "libebml"
+pkgver = "1.4.5"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_SHARED_LIBS=ON"]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+pkgdesc = "C++ library to parse Extensible Binary Meta-Language files"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-or-later"
+url = "https://matroska-org.github.io/libebml"
+source = f"https://dl.matroska.org/downloads/libebml/libebml-{pkgver}.tar.xz"
+sha256 = "4971640b0592da29c2d426f303e137a9b0b3d07e1b81d069c1e56a2f49ab221b"
+
+
+@subpackage("libebml-devel")
+def _devel(self):
+    return self.default_devel()
