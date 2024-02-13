cbd6356cd02a5915b6fcdf224ed7371fd21c573d
psykose <alice@ayaya.dev>
2024-02-13 11:41:28 +0000

    contrib/mutagen: run tests

--- contrib/mutagen/template.py
+++ contrib/mutagen/template.py
@@ -8,12 +8,10 @@ hostmakedepends = [
     "python-setuptools",
     "python-wheel",
 ]
-checkdepends = ["python-pytest"]
+checkdepends = ["python-hypothesis", "python-pytest"]
 pkgdesc = "Audio tagger implemented in Python"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/quodlibet/mutagen"
 source = f"https://github.com/quodlibet/mutagen/archive/refs/tags/release-{pkgver}.tar.gz"
 sha256 = "f7489e19d375c31ba1962ab19e11eca8b9f86f05bfd99cef467f8dd875d8941e"
-# missing python-hypothesis
-options = ["!check"]
