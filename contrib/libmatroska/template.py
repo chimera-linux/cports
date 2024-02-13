49d1957d6647d16c2873eefd9c004167ea42df5a
psykose <alice@ayaya.dev>
2024-02-13 17:13:17 +0000

    contrib/libmatroska: new package (1.7.1)

---
+++ contrib/libmatroska/template.py
@@ -0,0 +1,24 @@
+pkgname = "libmatroska"
+pkgver = "1.7.1"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_SHARED_LIBS=ON"]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = ["libebml-devel"]
+pkgdesc = "C++ libary to parse Matroska files"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-or-later"
+url = "https://github.com/Matroska-Org/libmatroska"
+source = (
+    f"https://dl.matroska.org/downloads/libmatroska/libmatroska-{pkgver}.tar.xz"
+)
+sha256 = "572a3033b8d93d48a6a858e514abce4b2f7a946fe1f02cbfeca39bfd703018b3"
+
+
+@subpackage("libmatroska-devel")
+def _devel(self):
+    return self.default_devel()
