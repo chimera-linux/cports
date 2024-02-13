0b42a430332630cefc2197b94ad9f8e1bcd0fa42
psykose <alice@ayaya.dev>
2024-02-13 12:00:03 +0000

    contrib/tailscale: update to 1.58.2

--- contrib/tailscale/template.py
+++ contrib/tailscale/template.py
@@ -1,9 +1,13 @@
 pkgname = "tailscale"
-pkgver = "1.54.0"
-pkgrel = 1
+pkgver = "1.58.2"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
-    f"-ldflags=-X tailscale.com/version.longStamp={pkgver} -X tailscale.com/version.shortStamp={pkgver}"
+    "-ldflags="
+    f" -X tailscale.com/version.longStamp={pkgver}"
+    f" -X tailscale.com/version.shortStamp={pkgver}",
+    "./cmd/tailscale",
+    "./cmd/tailscaled",
 ]
 hostmakedepends = ["go"]
 depends = ["iptables", "ca-certificates"]
@@ -12,13 +16,8 @@ maintainer = "Val Packett <val@packett.cool>"
 license = "BSD-3-Clause"
 url = "https://github.com/tailscale/tailscale"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "c895a0f489706535ed400b0599d7d932d9eebc5f1bad2c236408a1e4b86620e7"
+sha256 = "452f355408e4e2179872387a863387e06346fc8a6f9887821f9b8a072c6a5b0a"
 options = ["!debug"]
-
-
-def do_build(self):
-    self.golang.build(wrksrc="cmd/tailscaled")
-    self.golang.build(wrksrc="cmd/tailscale")
 
 
 def post_install(self):
