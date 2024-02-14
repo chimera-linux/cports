f8cf58632452c9fdc52187cc57d0daabe889689c
psykose <alice@ayaya.dev>
2024-02-14 11:43:58 +0000

    main/tmux: update to 3.4

--- main/tmux/template.py
+++ main/tmux/template.py
@@ -1,21 +1,17 @@
 pkgname = "tmux"
-pkgver = "3.3a"
+pkgver = "3.4"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_args = ["--prefix=/usr"]
-hostmakedepends = ["byacc", "pkgconf"]
+hostmakedepends = ["automake", "byacc", "pkgconf"]
 makedepends = ["libevent-devel", "ncurses-devel"]
 pkgdesc = "Terminal multiplexer"
 maintainer = "Justin Berthault <justin.berthault@zaclys.net>"
 license = "ISC"
 url = "https://tmux.github.io"
 source = f"https://github.com/tmux/tmux/releases/download/{pkgver}/tmux-{pkgver}.tar.gz"
-sha256 = "e4fd347843bd0772c4f48d6dde625b0b109b7a380ff15db21e97c11a4dcdf93f"
+sha256 = "551ab8dea0bf505c0ad6b7bb35ef567cdde0ccb84357df142c254f35a23e19aa"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
