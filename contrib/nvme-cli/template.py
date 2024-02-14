e9fbb34d31f1207c07c74685e7c55d99b3718182
psykose <alice@ayaya.dev>
2024-02-14 12:40:19 +0000

    contrib/nvme-cli: update to 2.8

--- contrib/nvme-cli/template.py
+++ contrib/nvme-cli/template.py
@@ -1,5 +1,5 @@
 pkgname = "nvme-cli"
-pkgver = "2.7.1"
+pkgver = "2.8"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -18,10 +18,8 @@ pkgdesc = "NVMe management command line interface"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/linux-nvme/nvme-cli"
-source = (
-    f"https://github.com/linux-nvme/nvme-cli/archive/refs/tags/v{pkgver}.tar.gz"
-)
-sha256 = "4c69d0f3c8b553110d0f63f5876d56627abd2d9f5d3904e2480cedd03cb15654"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "0743d9188792a87d39187ae5e5cb31e8f46cca8c6f100547c50ec0dd659d2531"
 hardening = ["vis", "cfi"]
 # require /dev nvme device
 options = ["!check"]
