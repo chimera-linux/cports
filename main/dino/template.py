3258b9c029fb8d293917534a52f48b46552ec186
miko <mikoxyzzz@gmail.com>
2024-02-16 20:08:26 +0100

    main/dino: enable tests, vis

--- main/dino/template.py
+++ main/dino/template.py
@@ -1,13 +1,12 @@
 pkgname = "dino"
 pkgver = "0.4.3"
-pkgrel = 1
+pkgrel = 2
 build_style = "cmake"
 # TODO: openpgp maybe
 configure_args = [
+    "-DBUILD_TESTS=ON",
     "-DDINO_PLUGIN_ENABLED_notification-sound=ON",
     "-DUSE_SOUP3=ON",
-    # TODO
-    "-DBUILD_TESTS=OFF",
 ]
 hostmakedepends = [
     "cmake",
@@ -42,8 +41,12 @@ license = "GPL-3.0-or-later"
 url = "https://github.com/dino/dino"
 source = f"{url}/archive/v{pkgver}.tar.gz"
 sha256 = "432d7c3b5170c595b1b31a8d64d73ded26e32af9f03a2d1a01828c22a8ade3fa"
-# TODO
-options = ["!check"]
-
 # generates errors with llvm 16
 tool_flags = {"CFLAGS": ["-Wno-incompatible-function-pointer-types"]}
+# FIXME: cfi breaks tests
+hardening = ["!cfi", "vis"]
+
+
+def do_check(self):
+    for test in ["libdino", "signal-protocol-vala", "xmpp-vala"]:
+        self.do(f"./build/{test}-test")
