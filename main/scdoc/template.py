7316d7ec952e4ced42aea02bedc73f2f2069589a
psykose <alice@ayaya.dev>
2024-02-17 10:37:16 +0000

    main/scdoc: update to 1.11.3

--- main/scdoc/template.py
+++ main/scdoc/template.py
@@ -1,5 +1,5 @@
 pkgname = "scdoc"
-pkgver = "1.11.2"
+pkgver = "1.11.3"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://git.sr.ht/~sircmpwn/scdoc"
 source = f"https://git.sr.ht/~sircmpwn/scdoc/archive/{pkgver}.tar.gz"
-sha256 = "e9ff9981b5854301789a6778ee64ef1f6d1e5f4829a9dd3e58a9a63eacc2e6f0"
+sha256 = "4c5c6136540384e5455b250f768e7ca11b03fdba1a8efc2341ee0f1111e57612"
 tool_flags = {"CFLAGS": [f'-DVERSION="{pkgver}"']}
 hardening = ["vis", "cfi"]
 
