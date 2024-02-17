f944a1dc6516c5d16144bee0a9be9286214d45a0
psykose <alice@ayaya.dev>
2024-02-17 10:51:44 +0000

    user/pcsx2: update to 1.7.5560

--- user/pcsx2/template.py
+++ user/pcsx2/template.py
@@ -1,5 +1,5 @@
 pkgname = "pcsx2"
-pkgver = "1.7.5550"
+pkgver = "1.7.5560"
 pkgrel = 0
 # pcsx2 doesn't support anything else
 archs = ["x86_64"]
@@ -52,7 +52,7 @@ pkgdesc = "Playstation 2 emulator"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later AND LGPL-3.0-or-later"
 url = "https://pcsx2.net"
-_patches = "33fae19199a8dd267990cdbbe724f043b9006600"
+_patches = "612721e040a6c4b70d18ae285cc670bf4f64a63e"
 _glslang = "11.12.0"
 _gtest = "v1.14.0"
 _rcheevos = "3cadf84c30bbc050c0fec79d26e1c8ff504bda42"
@@ -74,8 +74,8 @@ source_paths = [
     "3rdparty/fast_float",
 ]
 sha256 = [
-    "478b0440a048f1553bcbbf201787c6e2ffb0cd3518865ffbdf1d95d69390fc3f",
-    "9be02f63fc6f689e8a03d0a9097e8ab84407549fb0948f234636eb11728e3178",
+    "37818a9a721fe0b897ff2bc5848a2fefd1b4825b53ca6812541c90d426ece6e0",
+    "71f6ce80c5109460e22d3c754ae1d493b2eb4798e6e6a165dc8b69ac09a50919",
     "7795a97450fecd9779f3d821858fbc2d1a3bf1dd602617d95b685ccbcabc302f",
     "8ad598c73ad796e0d8280b082cebd82a630d73e73cd3c70057938a6501bba5d7",
     "f2e623936167b7300c4b65abf6ea096218d2492310438eb3932a76b3cc804ecb",
