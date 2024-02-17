823ec82051e77af633b0be7190080c6b1f7ffca1
psykose <alice@ayaya.dev>
2024-02-17 10:38:13 +0000

    contrib/crun: update to 1.14.3

--- contrib/crun/template.py
+++ contrib/crun/template.py
@@ -1,5 +1,5 @@
 pkgname = "crun"
-pkgver = "1.14.1"
+pkgver = "1.14.3"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-systemd"]
@@ -27,7 +27,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://github.com/containers/crun"
 source = f"{url}/releases/download/{pkgver}/crun-{pkgver}.tar.xz"
-sha256 = "c416560f158c4a4a27d239f21a3809e5fd52fbf2106b0c5319343973508d1cec"
+sha256 = "bd1703c346087fdfecba4f9e6f15ad7b55ab1d9456c3c344b79b8672978df9b4"
 
 
 def post_install(self):
