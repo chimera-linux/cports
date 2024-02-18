a842ba25bb312fd9c248c6ed314e3246e5d655dd
psykose <alice@ayaya.dev>
2024-02-18 16:05:00 +0000

    main/highway: update to 1.1.0

--- main/highway/template.py
+++ main/highway/template.py
@@ -1,6 +1,6 @@
 pkgname = "highway"
-pkgver = "1.0.7"
-pkgrel = 1
+pkgver = "1.1.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DBUILD_SHARED_LIBS=ON",
@@ -12,13 +12,13 @@ hostmakedepends = [
     "ninja",
     "pkgconf",
 ]
-makedepends = ["gtest-devel"]
+makedepends = ["gtest-devel", "linux-headers"]
 pkgdesc = "Google's SIMD library with runtime dispatch"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0 OR BSD-3-Clause"
 url = "https://github.com/google/highway"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "5434488108186c170a5e2fca5e3c9b6ef59a1caa4d520b008a9b8be6b8abe6c5"
+sha256 = "354a8b4539b588e70b98ec70844273e3f2741302c4c377bcc4e81b3d1866f7c9"
 # FIXME: cfi breaks a few tests
 hardening = ["vis"]
 
