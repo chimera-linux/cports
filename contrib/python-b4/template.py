633b2d5d501b6ac1d5919c0182187f26bef28c11
miko <mikoxyzzz@gmail.com>
2024-02-18 11:03:01 +0100

    contrib/python-b4: update to 0.13.0

--- contrib/python-b4/template.py
+++ contrib/python-b4/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-b4"
-pkgver = "0.12.4"
+pkgver = "0.13.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -22,4 +22,4 @@ license = "GPL-2.0-or-later"
 url = "https://github.com/mricon/b4"
 # pypi tarball doesn't contain tests
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "a983ff0681cedd2d7d49f7371fd7b8ca5d142bbb4e3756cc995cd31817df13a5"
+sha256 = "816aa2a26539e2ac92790b04726fbe74d52bfe49bd24570e6cfea8281464b9e2"
