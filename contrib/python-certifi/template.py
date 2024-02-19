7b8a5038ef129d8f4eef0fa609df63b412fab0b5
miko <mikoxyzzz@gmail.com>
2024-02-19 10:41:46 +0100

    contrib/python-certifi: new package (2024.2.2)

---
+++ contrib/python-certifi/template.py
@@ -0,0 +1,19 @@
+pkgname = "python-certifi"
+pkgver = "2024.2.2"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = ["ca-certificates", "python"]
+pkgdesc = "Python package for providing Mozilla's CA bundle"
+maintainer = "miko <mikoxyzzz@gmail.com>"
+license = "MPL-2.0"
+url = "https://github.com/certifi/python-certifi"
+source = f"$(PYPI_SITE)/c/certifi/certifi-{pkgver}.tar.gz"
+sha256 = "0569859f95fc761b18b45ef421b1290a0f65f147e92a1e5eb3e635f9a5e4e66f"
+# no tests
+options = ["!check"]
