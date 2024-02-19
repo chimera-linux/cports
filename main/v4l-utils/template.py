005c7a749c6c8e954c0001eed44d74e271f22404
Mohammed Anas <triallax@tutanota.com>
2024-02-19 11:52:10 +0000

    main/v4l-utils: update to 1.26.1

--- main/v4l-utils/template.py
+++ main/v4l-utils/template.py
@@ -1,19 +1,11 @@
 pkgname = "v4l-utils"
-pkgver = "1.24.1"
-pkgrel = 1
-build_style = "gnu_configure"
-configure_args = ["--disable-qv4l2", "--with-udevdir=/usr/lib/udev"]
-make_cmd = "gmake"
-hostmakedepends = [
-    "gmake",
-    "automake",
-    "libtool",
-    "pkgconf",
-    "gettext-devel",
-]
+pkgver = "1.26.1"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dqv4l2=disabled", "-Dudevdir=/usr/lib/udev"]
+hostmakedepends = ["bash", "gettext-devel", "meson", "perl", "pkgconf"]
 makedepends = [
     "libjpeg-turbo-devel",
-    "sysfsutils-devel",
     "udev-devel",
     "libx11-devel",
     "mesa-devel",
@@ -24,12 +16,9 @@ pkgdesc = "Userspace tools and libraries for V4L"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://linuxtv.org/wiki/index.php/V4l-utils"
-source = f"http://linuxtv.org/downloads/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "cbb7fe8a6307f5ce533a05cded70bb93c3ba06395ab9b6d007eb53b75d805f5b"
-tool_flags = {
-    "CFLAGS": ["-D__off_t=off_t", "-D__off64_t=off_t"],
-    "LDFLAGS": ["-largp"],
-}
+source = f"https://linuxtv.org/downloads/{pkgname}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "4a71608c0ef7df2931176989e6d32b445c0bdc1030a2376d929c8ca6e550ec4e"
+tool_flags = {"LDFLAGS": ["-largp"]}
 
 
 @subpackage("v4l-utils-devel")
