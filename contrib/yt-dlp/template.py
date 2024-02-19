1b746dc20e00fab3e13a8d5cd70c30c426883691
miko <mikoxyzzz@gmail.com>
2024-02-19 10:47:59 +0100

    contrib/yt-dlp: enable tests

--- contrib/yt-dlp/template.py
+++ contrib/yt-dlp/template.py
@@ -9,13 +9,16 @@ hostmakedepends = [
     "python-installer",
     "python-wheel",
 ]
-checkdepends = ["python-pytest", "python-nose", "flake8"]
-depends = ["python", "python-certifi"]
+depends = ["python-certifi"]
+checkdepends = [
+    "python-brotli",
+    "python-pytest",
+    "python-requests",
+    "python-websockets",
+] + depends
 pkgdesc = "CLI program to download videos from YouTube and other sites"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Unlicense"
 url = "https://github.com/yt-dlp/yt-dlp"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "079dbf54586e8120c7d76e0dd1b7a60e6c13f4d23f9681fc919577f2fe17d8cd"
-# missing checkdepends
-options = ["!check"]
