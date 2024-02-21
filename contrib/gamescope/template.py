e233afa7f79a6b31b4dac7fe93d1d1ff766ef6dc
psykose <alice@ayaya.dev>
2024-02-21 21:19:15 +0000

    contrib/gamescope: update to 3.14.2

--- contrib/gamescope/template.py
+++ contrib/gamescope/template.py
@@ -1,6 +1,6 @@
 pkgname = "gamescope"
-pkgver = "3.13.8"
-pkgrel = 1
+pkgver = "3.14.2"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "cmake",
@@ -33,7 +33,7 @@ makedepends = [
     "vulkan-headers",
     "vulkan-loader-devel",
     "wayland-protocols",
-    "wlroots-devel",
+    "wlroots0.17-devel",
     "xwayland",
 ]
 pkgdesc = "SteamOS compositor"
@@ -42,7 +42,7 @@ license = "BSD-2-Clause"
 url = "https://github.com/ValveSoftware/gamescope"
 source = [
     f"{url}/archive/refs/tags/{pkgver}.tar.gz",
-    "https://github.com/ValveSoftware/openvr/archive/1a0ea26642e517824b66871e6a12280a426cfec3.tar.gz",
+    "https://github.com/ValveSoftware/openvr/archive/15f0838a0487feb7da60acd39aab8099b994234c.tar.gz",
     "https://github.com/Joshua-Ashton/vkroots/archive/d5ef31abc7cb5c69aee4bcb67b10dd543c1ff7ac.tar.gz",
     "https://github.com/Joshua-Ashton/reshade/archive/9fdbea6892f9959fdc18095d035976c574b268b7.tar.gz",
 ]
@@ -53,8 +53,8 @@ source_paths = [
     "src/reshade",
 ]
 sha256 = [
-    "660120f41aa5fb62d4d5f467751470add3897d305dae480cf27d6d49ae2d4817",
-    "6285504e64a37df47856ffa4a12709d0703da37ee1b0c9fe9e8e52a55127dd7d",
+    "91ed63557d3601723f1b1d554ea65a3850656a78bb7e32610330035160704836",
+    "cf63f641985a70e6a4cb6aad081832f3a33bf26f8973f71c69af24debc8ceadf",
     "b4eca5edca75355ea1443ad96fd59b0a407f6a2ce17ef5a8f9849c05fc10155f",
     "165726ad21fbfc221c0363e40b597834068a416a11a1204ae2ac6d13ec161035",
 ]
