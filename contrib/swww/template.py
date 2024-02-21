741151604e9ebefb72cca7297b586783d180ea3d
Nova <Froggo8311@proton.me>
2024-02-21 14:30:07 -0600

    contrib/swww: update to 0.8.2

--- contrib/swww/template.py
+++ contrib/swww/template.py
@@ -1,15 +1,15 @@
 pkgname = "swww"
-pkgver = "0.8.1"
-pkgrel = 1
+pkgver = "0.8.2"
+pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo"]
 makedepends = ["rust-std"]
 pkgdesc = "Solution to your Wayland Wallpaper Woes"
-maintainer = "Froggo <froggo8311@proton.me>"
+maintainer = "Nova <froggo8311@proton.me>"
 license = "GPL-3.0-only"
-url = "https://github.com/Horus645/swww"
+url = "https://github.com/LGFae/swww"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7612ae780d0aa86b772d1e224346137d490eba48e158033185d52649ff01b757"
+sha256 = "6733cda771a0e635dbd00f7aef78ed60f1ccdf640647ecfe02d0cdfdef996b68"
 
 
 def post_install(self):
