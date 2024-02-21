52b439ad65302277c33ed557b84c56e3fff2fa66
psykose <alice@ayaya.dev>
2024-02-21 18:04:32 +0000

    contrib/gimp: update to 2.99.18

--- contrib/gimp/template.py
+++ contrib/gimp/template.py
@@ -1,5 +1,5 @@
 pkgname = "gimp"
-pkgver = "2.99.16"
+pkgver = "2.99.18"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -11,66 +11,65 @@ configure_args = [
     "-Dlua=disabled",
 ]
 hostmakedepends = [
-    "meson",
-    "pkgconf",
-    "perl",
     "gegl",
     "gettext-devel",
+    "gjs",
     "glib-devel",
     "gobject-introspection",
-    "xsltproc",
-    "bash",
     "iso-codes",
-    "shared-mime-info",
+    "meson",
+    "perl",
+    "pkgconf",
     "python-gobject",
-    "gjs",
+    "shared-mime-info",
+    "xsltproc",
 ]
 makedepends = [
+    "alsa-lib-devel",
+    "appstream-glib-devel",
+    "at-spi2-core-devel",
     "babl-devel",
-    "gegl-devel",
-    "gtk+3-devel",
-    "glib-devel",
-    "librsvg-devel",
-    "gdk-pixbuf-devel",
-    "libtiff-devel",
-    "libpng-devel",
-    "libwebp-devel",
-    "libjpeg-turbo-devel",
-    "openjpeg-devel",
-    "libheif-devel",
-    "libxpm-devel",
-    "gexiv2-devel",
-    "zlib-devel",
-    "xz-devel",
-    "libarchive-devel",
-    "libpoppler-glib-devel",
-    "poppler-data",
-    "libgs-devel",
     "cairo-devel",
-    "freetype-devel",
-    "harfbuzz-devel",
-    "pango-devel",
     "fontconfig-devel",
+    "freetype-devel",
+    "gdk-pixbuf-devel",
+    "gegl-devel",
+    "gexiv2-devel",
+    "glib-devel",
+    "gtk+3-devel",
+    "harfbuzz-devel",
+    "json-glib-devel",
     "lcms2-devel",
-    "webkitgtk-devel",
+    "libarchive-devel",
+    "libgs-devel",
+    "libgudev-devel",
+    "libheif-devel",
+    "libjpeg-turbo-devel",
+    "libmypaint-devel",
+    "libpng-devel",
+    "libpoppler-glib-devel",
+    "librsvg-devel",
+    "libtiff-devel",
+    "libunwind-devel",
+    "libwebp-devel",
     "libxcursor-devel",
     "libxmu-devel",
-    "alsa-lib-devel",
-    "libmypaint-devel",
-    "mypaint-brushes",
-    "at-spi2-core-devel",
-    "appstream-glib-devel",
-    "json-glib-devel",
-    "libgudev-devel",
-    "libunwind-devel",
+    "libxpm-devel",
     "linux-headers",
+    "mypaint-brushes",
+    "openjpeg-devel",
+    "pango-devel",
+    "poppler-data",
+    "webkitgtk-devel",
+    "xz-devel",
+    "zlib-devel",
 ]
 depends = [
-    "mypaint-brushes",
     "iso-codes",
+    "gjs",
+    "mypaint-brushes",
     # prevent scripts from crashing (which prints annoying messages)
     "python-gobject",
-    "gjs",
 ]
 checkdepends = ["dbus"]
 pkgdesc = "GNU Image Manipulation Program"
@@ -78,7 +77,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-only"
 url = "https://www.gimp.org"
 source = f"https://download.gimp.org/pub/{pkgname}/v{pkgver[:-3]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "6b4496edee447339f923276755247eadb64ec40d8aec241d06b62d1a6eb6508d"
+sha256 = "8c1bb7a94ac0d4d0cde4d701d8b356387c2ecd87abbd35bbf7d222d40f6ddb6e"
 # FIXME: it worksish but crashes often/early
 hardening = ["!int"]
 options = ["!cross"]
