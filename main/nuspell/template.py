ca80279b8e65b5f0279f9dcd5e6d495cc5d00677
miko <mikoxyzzz@gmail.com>
2024-02-21 21:44:44 +0100

    main/nuspell: enable tests

--- main/nuspell/template.py
+++ main/nuspell/template.py
@@ -2,10 +2,9 @@ pkgname = "nuspell"
 pkgver = "5.1.4"
 pkgrel = 1
 build_style = "cmake"
-configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DBUILD_TESTING=OFF"]
+configure_args = ["-DBUILD_SHARED_LIBS=ON"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
-makedepends = ["icu-devel"]
-checkdepends = ["catch2"]
+makedepends = ["catch2-devel", "icu-devel"]
 pkgdesc = "Fast and safe spell checking software"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-or-later"
@@ -13,8 +12,6 @@ url = "https://nuspell.github.io"
 source = f"https://github.com/nuspell/nuspell/archive/v{pkgver}.tar.gz"
 sha256 = "bdf344c5adbcc6797940f8f8cb75cb59f5a3794eb21b9547751a11782a792ef7"
 hardening = ["!cfi"]  # TODO
-# missing checkdepends
-options = ["!check"]
 
 
 @subpackage("nuspell-devel")
