15e1a85a2fb172188b07ec3ac2f3b7d3a59b4095
Mohammed Anas <triallax@tutanota.com>
2024-02-21 21:47:04 +0000

    main/python-cython: update to 3.0.8

--- main/python-cython/template.py
+++ main/python-cython/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-cython"
-pkgver = "0.29.36"
-pkgrel = 1
+pkgver = "3.0.8"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -15,6 +15,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://cython.org"
 source = f"$(PYPI_SITE)/C/Cython/Cython-{pkgver}.tar.gz"
-sha256 = "41c0cfd2d754e383c9eeb95effc9aa4ab847d0c9747077ddd7c0dcb68c3bc01f"
+sha256 = "8333423d8fd5765e7cceea3a9985dd1e0a5dfeb2734629e1a2ed2d6233d39de6"
 # flaky tests
 options = ["!check"]
