0dd1ca185060da4fa3d9606edebc0813afed6f7b
psykose <alice@ayaya.dev>
2024-02-24 05:35:20 +0000

    contrib/thin-provisioning-tools: update to 1.0.12

--- contrib/thin-provisioning-tools/template.py
+++ contrib/thin-provisioning-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "thin-provisioning-tools"
-pkgver = "1.0.11"
+pkgver = "1.0.12"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo", "gmake", "gawk", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/jthornber/thin-provisioning-tools"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "a4262cc3559ff0f08b0485e3d6eaeb92aac1260080336ba6e08eae9cd162bae2"
+sha256 = "55f3423c22d3a5716be88d0ae9155c95d8145cb3ee65f89ff96d2d6a9ede6ad5"
 # too long
 options = ["!check"]
 
