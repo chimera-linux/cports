c15c187ddb0c889492e8f16013ee20aaf7840729
psykose <alice@ayaya.dev>
2024-02-24 05:22:00 +0000

    main/libksba: update to 1.6.6

--- main/libksba/template.py
+++ main/libksba/template.py
@@ -1,5 +1,5 @@
 pkgname = "libksba"
-pkgver = "1.6.5"
+pkgver = "1.6.6"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -10,7 +10,7 @@ maintainer = "eater <=@eater.me>"
 license = "GPL-2.0-or-later OR LGPL-3.0-or-later"
 url = "https://gnupg.org/software/libksba/index.html"
 source = f"https://gnupg.org/ftp/gcrypt/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "a564628c574c99287998753f98d750babd91a4e9db451f46ad140466ef2a6d16"
+sha256 = "5dec033d211559338838c0c4957c73dfdc3ee86f73977d6279640c9cd08ce6a4"
 
 
 @subpackage("libksba-devel")
