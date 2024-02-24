d846a9ccfa978f36fe2fba928aa2b050a105e277
psykose <alice@ayaya.dev>
2024-02-24 05:47:57 +0000

    main/python-trove-classifiers: update to 2024.2.23

--- main/python-trove-classifiers/template.py
+++ main/python-trove-classifiers/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-trove-classifiers"
-pkgver = "2024.2.22"
+pkgver = "2024.2.23"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -16,6 +16,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/pypa/trove-classifiers"
 source = f"$(PYPI_SITE)/t/trove-classifiers/trove-classifiers-{pkgver}.tar.gz"
-sha256 = "458650355240a16a98bb430ddac088412af48a86093d400206f09030eb595e8a"
+sha256 = "8385160a12aac69c93fff058fb613472ed773a24a27eb3cd4b144cfbdd79f38c"
 # cycle
 options = ["!check"]
