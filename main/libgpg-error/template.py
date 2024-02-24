c288d8952f0194d7fb70e9915cef40fb2bf78ec1
psykose <alice@ayaya.dev>
2024-02-24 05:45:55 +0000

    main/libgpg-error: update to 1.48

--- main/libgpg-error/template.py
+++ main/libgpg-error/template.py
@@ -1,14 +1,15 @@
 pkgname = "libgpg-error"
-pkgver = "1.47"
+pkgver = "1.48"
 pkgrel = 0
 build_style = "gnu_configure"
+configure_gen = []
 hostmakedepends = ["pkgconf"]
 pkgdesc = "Library for error values used by GnuPG components"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gnupg.org"
 source = f"{url}/ftp/gcrypt/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "9e3c670966b96ecc746c28c2c419541e3bcb787d1a73930f5e5f5e1bcbbb9bdb"
+sha256 = "89ce1ae893e122924b858de84dc4f67aae29ffa610ebf668d5aa539045663d6f"
 # needs qemu and patching
 options = ["!cross"]
 
@@ -25,6 +26,3 @@ def _devel(self):
 @subpackage("libgpg-error-progs")
 def _progs(self):
     return self.default_progs()
-
-
-configure_gen = []
