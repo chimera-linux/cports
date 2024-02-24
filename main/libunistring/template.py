f8cf2c49804ce18bf90702643c50c5844f8fc7d0
psykose <alice@ayaya.dev>
2024-02-24 17:25:49 +0000

    main/libunistring: update to 1.2

--- main/libunistring/template.py
+++ main/libunistring/template.py
@@ -1,18 +1,16 @@
 pkgname = "libunistring"
-pkgver = "1.1"
+pkgver = "1.2"
 pkgrel = 0
 build_style = "gnu_configure"
+hostmakedepends = ["automake", "libtool"]
 pkgdesc = "Library for manipulating Unicode strings"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-or-later"
 url = "http://www.gnu.org/software/libunistring"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "a2252beeec830ac444b9f68d6b38ad883db19919db35b52222cf827c385bdb6a"
+sha256 = "fd6d5662fa706487c48349a758b57bc149ce94ec6c30624ec9fdc473ceabbc8e"
 
 
 @subpackage("libunistring-devel")
 def _devel(self):
     return self.default_devel(extra=["usr/share"])
-
-
-configure_gen = []
