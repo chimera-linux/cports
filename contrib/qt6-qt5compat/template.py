e9d936667d918f650c8a34ff24e858310f64ba4c
miko <mikoxyzzz@gmail.com>
2024-02-25 21:15:42 +0100

    contrib/qt6-qt5compat: enable tests
    they seem to work just fine

--- contrib/qt6-qt5compat/template.py
+++ contrib/qt6-qt5compat/template.py
@@ -4,7 +4,7 @@ pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf", "qt6-qtbase"]
 makedepends = ["qt6-qtdeclarative-devel"]
-pkgdesc = "Module containing unsuppored Qt5 APIs"
+pkgdesc = "Module containing unsupported Qt5 APIs"
 maintainer = "aurelia <git@elia.garden>"
 license = (
     "LGPL-2.1-only AND LGPL-3.0-only AND GPL-3.0-only WITH Qt-GPL-exception-1.0"
@@ -12,8 +12,6 @@ license = (
 url = "https://www.qt.io"
 source = f"https://download.qt.io/official_releases/qt/{pkgver[:-2]}/{pkgver}/submodules/qt5compat-everywhere-src-{pkgver}.tar.xz"
 sha256 = "e07b08ab7658e4856f07e3262ab342df4ed7e7a69f2720e56bb2128729191967"
-# TODO
-options = ["!check"]
 
 
 def post_install(self):
