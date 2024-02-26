ac955b57654b2bae778959ab53bf0e4ff9b8c994
miko <mikoxyzzz@gmail.com>
2024-02-26 16:45:12 +0100

    main/glslang: FIXME for !check

--- main/glslang/template.py
+++ main/glslang/template.py
@@ -4,6 +4,7 @@ pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DALLOW_EXTERNAL_SPIRV_TOOLS=ON"]
 hostmakedepends = ["cmake", "ninja", "python", "bison", "spirv-tools-devel"]
+makedepends = ["gtest-devel"]
 checkdepends = ["bash"]
 pkgdesc = "Khronos reference front-end for GLSL/ESSL + sample SPIR-V generator"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -11,7 +12,7 @@ license = "BSD-3-Clause"
 url = "https://github.com/KhronosGroup/glslang"
 source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "80bbb916a23e94ea9cbfb1acb5d1a44a7e0c9613bcf5b5947c03f2273bdc92b0"
-# missing checkdepends
+# FIXME: cmake doesn't pick up the system-supplied gtest
 options = ["!check"]
 
 
