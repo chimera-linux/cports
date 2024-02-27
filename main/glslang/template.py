a55c327715426ac12754b38b67174ad6ab9ea74e
psykose <alice@ayaya.dev>
2024-02-27 03:41:53 +0000

    main/glslang: run tests and set !int
    closes https://github.com/chimera-linux/cports/pull/1502

--- main/glslang/template.py
+++ main/glslang/template.py
@@ -1,9 +1,14 @@
 pkgname = "glslang"
 pkgver = "14.0.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
-configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DALLOW_EXTERNAL_SPIRV_TOOLS=ON"]
+configure_args = [
+    "-DALLOW_EXTERNAL_SPIRV_TOOLS=ON",
+    "-DBUILD_SHARED_LIBS=ON",
+    "-DGLSLANG_TESTS=ON",
+]
 hostmakedepends = ["cmake", "ninja", "python", "bison", "spirv-tools-devel"]
+makedepends = ["gtest-devel"]
 checkdepends = ["bash"]
 pkgdesc = "Khronos reference front-end for GLSL/ESSL + sample SPIR-V generator"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -11,8 +16,8 @@ license = "BSD-3-Clause"
 url = "https://github.com/KhronosGroup/glslang"
 source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "80bbb916a23e94ea9cbfb1acb5d1a44a7e0c9613bcf5b5947c03f2273bdc92b0"
-# missing checkdepends
-options = ["!check"]
+# FIXME: tests reveal a suboverflow in TIntermConstantUnion::fold that should be fixed
+hardening = ["!int"]
 
 
 def post_install(self):
