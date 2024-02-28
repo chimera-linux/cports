2be5968b19eaf20dd3ac442474e2d6a3cb539072
psykose <alice@ayaya.dev>
2024-02-28 04:09:15 +0000

    contrib/swaync: new package (0.10.1)

---
+++ contrib/swaync/template.py
@@ -0,0 +1,31 @@
+pkgname = "swaync"
+pkgver = "0.10.1"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "gobject-introspection",
+    "meson",
+    "pkgconf",
+    "sassc",
+    "scdoc",
+    "vala",
+]
+makedepends = [
+    "granite-devel",
+    "gtk+3-devel",
+    "gtk-layer-shell-devel",
+    "json-glib-devel",
+    "libhandy-devel",
+    "libpulse-devel",
+]
+pkgdesc = "Notification daemon for sway"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/ErikReider/SwayNotificationCenter"
+source = f"https://github.com/ErikReider/SwayNotificationCenter/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "5586d8a679dde5e530cb8b6f0c86abdd0d5e41362fc1c4e56e2211edea0f7a13"
+
+
+def post_install(self):
+    self.rm(self.destdir / "usr/lib/systemd", recursive=True)
+    self.install_service(self.files_path / "swaync.user")
