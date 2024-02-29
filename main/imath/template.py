4f779e1f66ff589bd5b9e3cc5c2511a60adb4407
psykose <alice@ayaya.dev>
2024-02-29 23:13:33 +0000

    main/imath: update to 3.1.11

--- main/imath/template.py
+++ main/imath/template.py
@@ -1,5 +1,5 @@
 pkgname = "imath"
-pkgver = "3.1.10"
+pkgver = "3.1.11"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -19,7 +19,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-3-Clause"
 url = "https://imath.readthedocs.io/en/latest"
 source = f"https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f2943e86bfb694e216c60b9a169e5356f8a90f18fbd34d7b6e3450be14f60b10"
+sha256 = "9057849585e49b8b85abe7cc1e76e22963b01bfdc3b6d83eac90c499cd760063"
 hardening = ["vis", "cfi"]
 
 
