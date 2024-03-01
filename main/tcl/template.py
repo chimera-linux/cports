6006eede3aaac3d5be9f99d15802e05c9ff470c7
psykose <alice@ayaya.dev>
2024-03-01 02:08:21 +0000

    main/tcl: update to 8.6.14

--- main/tcl/template.py
+++ main/tcl/template.py
@@ -1,5 +1,5 @@
 pkgname = "tcl"
-pkgver = "8.6.13"
+pkgver = "8.6.14"
 pkgrel = 0
 build_wrksrc = "unix"
 build_style = "gnu_configure"
@@ -12,7 +12,7 @@ configure_args = [
     "tcl_cv_strstr_unbroken=ok",
     "tcl_cv_strtoul_unbroken=ok",
 ]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf"]
 makedepends = ["zlib-devel", "sqlite-devel"]
 provides = ["so:libtcl8.6.so=0"]
 pkgdesc = "TCL scripting language"
@@ -20,7 +20,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "TCL"
 url = "http://www.tcl.tk"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}{pkgver}-src.tar.gz"
-sha256 = "43a1fae7412f61ff11de2cfd05d28cfc3a73762f354a417c62370a54e2caf066"
+sha256 = "5880225babf7954c58d4fb0f5cf6279104ce1cd6aa9b71e9a6322540e1c4de66"
 # no check target
 options = ["!check", "!lto", "!splitstatic"]
 
@@ -48,6 +48,3 @@ def _devel(self):
         "usr/share/man/man3",
         "usr/lib/*.a",
     ]
-
-
-configure_gen = []
