f2eeef487a03e8dfbb0ca3761b45396f3bf4286e
miko <mikoxyzzz@gmail.com>
2024-03-01 21:47:24 +0100

    main/gtest: enable tests

--- main/gtest/template.py
+++ main/gtest/template.py
@@ -3,7 +3,12 @@ pkgname = "gtest"
 pkgver = "1.14.0"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DCMAKE_SKIP_RPATH=ON"]
+configure_args = [
+    "-DBUILD_SHARED_LIBS=ON",
+    "-DCMAKE_SKIP_INSTALL_RPATH=ON",
+    "-Dgmock_build_tests=ON",
+    "-Dgtest_build_tests=ON",
+]
 hostmakedepends = ["cmake", "ninja", "python", "pkgconf"]
 pkgdesc = "Google's framework for writing C++ tests"
 maintainer = "yopito <pierre.bourgin@free.fr>"
@@ -13,8 +18,6 @@ source = (
     f"https://github.com/google/googletest/archive/refs/tags/v{pkgver}.tar.gz"
 )
 sha256 = "8ad598c73ad796e0d8280b082cebd82a630d73e73cd3c70057938a6501bba5d7"
-# no tests provided by upstream
-options = ["!check"]
 
 
 def post_install(self):
