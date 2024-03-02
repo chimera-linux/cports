78aed9ab732b56d8b065d2e6b04fa06181c3aac7
psykose <alice@ayaya.dev>
2024-03-02 09:00:28 +0000

    contrib/xlsfonts: update to 1.0.8
    also make it not instantly crash

--- contrib/xlsfonts/template.py
+++ contrib/xlsfonts/template.py
@@ -1,20 +1,18 @@
 pkgname = "xlsfonts"
-pkgver = "1.0.7"
-pkgrel = 1
+pkgver = "1.0.8"
+pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf", "xorg-util-macros"]
 makedepends = ["libx11-devel"]
 pkgdesc = "Server font list displayer for X"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
-sha256 = "b92d4954eaf525674ff83f7e85240ef166c240a774277f71c30674f9f7794171"
-hardening = ["vis", "cfi"]
+sha256 = "448ba05919bafc6b2ca3d21982646b060e2af3a27a5cc05fcedbe8b2608388cb"
+# FIXME: int
+hardening = ["vis", "cfi", "!int"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
