bc5b32af0c22fb7689da04e6c7a4687e5c670061
psykose <alice@ayaya.dev>
2024-03-02 19:58:15 +0000

    main/libfontenc: update to 1.1.8

--- main/libfontenc/template.py
+++ main/libfontenc/template.py
@@ -1,16 +1,18 @@
 pkgname = "libfontenc"
-pkgver = "1.1.7"
+pkgver = "1.1.8"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-encodingsdir=/usr/share/fonts/encodings"]
+# cycle with mkfontscale
+configure_gen = []
 hostmakedepends = ["pkgconf"]
 makedepends = ["xorgproto", "zlib-devel"]
 pkgdesc = "Fontenc Library from X.org"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.gz"
-sha256 = "5e5f210329823f08f97bfe9fd5b4105070c789bc5aef88ce01d86d8203d4aa9f"
+source = f"$(XORG_SITE)/lib/{pkgname}-{pkgver}.tar.xz"
+sha256 = "7b02c3d405236e0d86806b1de9d6868fe60c313628b38350b032914aa4fd14c6"
 
 
 def post_install(self):
@@ -20,6 +22,3 @@ def post_install(self):
 @subpackage("libfontenc-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
