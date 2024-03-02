c11af78dd4d08e8c0c3dc598f63640dbf0219b98
ttyyls <contact@behri.org>
2024-03-02 22:16:43 +0100

    contrib/libgrapheme: new package (2.0.2)

---
+++ contrib/libgrapheme/template.py
@@ -0,0 +1,22 @@
+pkgname = "libgrapheme"
+pkgver = "2.0.2"
+pkgrel = 0
+build_style = "makefile"
+make_check_target = "test"
+hostmakedepends = ["pkgconf"]
+pkgdesc = "Unicode string library"
+maintainer = "ttyyls <contact@behri.org>"
+license = "ISC AND Unicode-3.0"
+url = "https://libs.suckless.org/libgrapheme"
+source = f"https://dl.suckless.org/libgrapheme/libgrapheme-{pkgver}.tar.gz"
+sha256 = "a68bbddde76bd55ba5d64116ce5e42a13df045c81c0852de9ab60896aa143125"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+    self.install_license("data/LICENSE", name="LICENSE-UNICODE")
+
+
+@subpackage("libgrapheme-devel")
+def _devel(self):
+    return self.default_devel()
