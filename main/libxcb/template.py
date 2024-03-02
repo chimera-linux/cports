bb36006dc34835624b15ab1752956fcc50984c6d
psykose <alice@ayaya.dev>
2024-03-02 19:59:16 +0000

    main/libxcb: update to 1.16.1

--- main/libxcb/template.py
+++ main/libxcb/template.py
@@ -1,8 +1,10 @@
 pkgname = "libxcb"
-pkgver = "1.16"
+pkgver = "1.16.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-build-docs", "--enable-xinput", "--enable-xkb"]
+# broken reconf
+configure_gen = []
 hostmakedepends = ["pkgconf", "libtool", "xorg-util-macros", "xcbproto"]
 makedepends = ["xcbproto", "libxdmcp-devel", "libxau-devel"]
 pkgdesc = "X protocol C language binding"
@@ -10,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xcb.freedesktop.org"
 source = f"https://xorg.freedesktop.org/archive/individual/lib/libxcb-{pkgver}.tar.xz"
-sha256 = "4348566aa0fbf196db5e0a576321c65966189210cb51328ea2bb2be39c711d71"
+sha256 = "f24d187154c8e027b358fc7cb6588e35e33e6a92f11c668fe77396a7ae66e311"
 
 
 def post_install(self):
@@ -21,6 +23,3 @@ def post_install(self):
 def _devel(self):
     self.depends += ["xcbproto"]
     return self.default_devel(extra=["usr/share/doc"])
-
-
-configure_gen = []
