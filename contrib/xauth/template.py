93a8f0665cd57af249fae5bd50d8cbd91384e3f2
psykose <alice@ayaya.dev>
2024-03-03 20:22:35 +0000

    contrib/xauth: update to 1.1.3

--- contrib/xauth/template.py
+++ contrib/xauth/template.py
@@ -1,15 +1,15 @@
 pkgname = "xauth"
-pkgver = "1.1.2"
-pkgrel = 1
+pkgver = "1.1.3"
+pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf", "xorg-util-macros"]
 makedepends = ["xtrans", "libxau-devel", "libxext-devel", "libxmu-devel"]
 pkgdesc = "X authentication utility"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
-sha256 = "84d27a1023d8da524c134f424b312e53cb96e08871f96868aa20316bfcbbc054"
+sha256 = "88c288e0a30bf071631118644f5232cae3a79713a7c82dd31a236e8e2c6fca15"
 hardening = ["vis", "cfi"]
 # needs cmdtest
 options = ["!check"]
@@ -17,6 +17,3 @@ options = ["!check"]
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
