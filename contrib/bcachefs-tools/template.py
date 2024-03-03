9def486b25ba99c530a53d29f7db898fe42dbc31
psykose <alice@ayaya.dev>
2024-03-03 20:02:57 +0000

    contrib/bcachefs-tools: update to 1.6.4

--- contrib/bcachefs-tools/template.py
+++ contrib/bcachefs-tools/template.py
@@ -1,9 +1,12 @@
 pkgname = "bcachefs-tools"
-pkgver = "1.4.0"
+pkgver = "1.6.4"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
-make_install_args = ["ROOT_SBINDIR=/usr/bin"]
+make_install_args = [
+    "ROOT_SBINDIR=/usr/bin",
+    "TRIPLET=" + self.profile().triplet,
+]
 make_use_env = True
 hostmakedepends = ["cargo", "gmake", "pkgconf"]
 makedepends = [
@@ -25,7 +28,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://github.com/koverstreet/bcachefs-tools"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "3fea7aba076d1400eba29317c8b1de0b8ed83012ab004afaa948fc0750807f58"
+sha256 = "9d33b234b282ec3d3e4be43a4af85766b2cc3b6bc08f7687d36dde96a254728e"
 # no tests
 options = ["!check"]
 
@@ -33,8 +36,8 @@ options = ["!check"]
 def do_prepare(self):
     from cbuild.util import cargo
 
-    cargo.Cargo(self).vendor(wrksrc="rust-src")
-    cargo.setup_vendor(self, wrksrc="rust-src")
+    cargo.Cargo(self).vendor()
+    cargo.setup_vendor(self)
 
 
 def init_build(self):
