996c91bd318af35d8b90816c841fa9d8229f79d6
psykose <alice@ayaya.dev>
2024-03-03 06:06:01 +0000

    main/libxcursor: update to 1.2.2

--- main/libxcursor/template.py
+++ main/libxcursor/template.py
@@ -1,15 +1,15 @@
 pkgname = "libxcursor"
-pkgver = "1.2.1"
+pkgver = "1.2.2"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf", "xorg-util-macros"]
 makedepends = ["xorgproto", "libxfixes-devel", "libxrender-devel"]
 pkgdesc = "Client-side cursor loading library for X"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXcursor-{pkgver}.tar.gz"
-sha256 = "77f96b9ad0a3c422cfa826afabaf1e02b9bfbfc8908c5fa1a45094faad074b98"
+sha256 = "98c3a30a3f85274c167d1ac5419d681ce41f14e27bfa5fe3003c8172cd8af104"
 
 
 def post_install(self):
@@ -19,6 +19,3 @@ def post_install(self):
 @subpackage("libxcursor-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
