854d1f717cc158fe1ed90b2e345054e767d4b0bd
psykose <alice@ayaya.dev>
2024-03-03 23:34:03 +0000

    contrib/xrefresh: update to 1.1.0

--- contrib/xrefresh/template.py
+++ contrib/xrefresh/template.py
@@ -1,20 +1,17 @@
 pkgname = "xrefresh"
-pkgver = "1.0.7"
-pkgrel = 1
+pkgver = "1.1.0"
+pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf", "xorg-util-macros"]
 makedepends = ["libx11-devel"]
 pkgdesc = "Refresh all or a part of an X screen"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/{pkgname}-{pkgver}.tar.gz"
-sha256 = "f2817920f119bd9146ed3cde223b8a4ab17cb72da4ece7bddde35e18b31aa337"
+sha256 = "cbf0d3ed80f03188841a96ceb20e615b40a006e3928be2e179d9d5a0ded639b2"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
