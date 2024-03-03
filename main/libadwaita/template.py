40982fb9acf9a7ab734cf2edb0d8c90f3f865eb3
psykose <alice@ayaya.dev>
2024-03-03 06:09:13 +0000

    main/libadwaita: update to 1.4.4

--- main/libadwaita/template.py
+++ main/libadwaita/template.py
@@ -1,5 +1,5 @@
 pkgname = "libadwaita"
-pkgver = "1.4.3"
+pkgver = "1.4.4"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -31,7 +31,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libadwaita"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "ae9622222b0eb18e23675655ad2ba01741db4d8655a796f4cf077b093e2f5841"
+sha256 = "f802b7d8d5ae33be4650ef571a580f144a806202a26f527dacd57d1560938828"
 options = ["!cross"]
 
 
