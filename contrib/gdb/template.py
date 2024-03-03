50cd6e33de27bc085b184c32d777b2cdf08f6985
psykose <alice@ayaya.dev>
2024-03-03 07:41:55 +0000

    contrib/gdb: update to 14.2

--- contrib/gdb/template.py
+++ contrib/gdb/template.py
@@ -1,6 +1,6 @@
 pkgname = "gdb"
-pkgver = "14.1"
-pkgrel = 3
+pkgver = "14.2"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--disable-werror",
@@ -33,7 +33,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/gdb"
 source = f"$(GNU_SITE)/gdb/gdb-{pkgver}.tar.xz"
-sha256 = "d66df51276143451fcbff464cc8723d68f1e9df45a6a2d5635a54e71643edb80"
+sha256 = "2d4dd8061d8ded12b6c63f55e45344881e8226105f4d2a9b234040efa5ce7772"
 # massive
 options = ["!check", "!cross"]
 
