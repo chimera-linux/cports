442de45bf995abdac6d604ed9805889b466970f9
Jami Kettunen <jami.kettunen@protonmail.com>
2024-03-03 13:40:07 +0200

    contrib/plasma-wayland-protocols: adopt

--- contrib/plasma-wayland-protocols/template.py
+++ contrib/plasma-wayland-protocols/template.py
@@ -1,6 +1,6 @@
 pkgname = "plasma-wayland-protocols"
 pkgver = "1.13.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -8,7 +8,7 @@ hostmakedepends = [
     "ninja",
 ]
 pkgdesc = "Plasma-specific wayland protocols"
-maintainer = "psykose <alice@ayaya.dev>"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "MIT AND (LGPL-2.1-only OR LGPL-3.0-only)"
 url = "https://invent.kde.org/libraries/plasma-wayland-protocols"
 source = f"$(KDE_SITE)/plasma-wayland-protocols/plasma-wayland-protocols-{pkgver}.tar.xz"
