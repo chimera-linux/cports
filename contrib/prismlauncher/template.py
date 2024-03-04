3fe4230880b9b7eb251c6354766366969c518f45
psykose <alice@ayaya.dev>
2024-03-04 04:10:55 +0000

    contrib/prismlauncher: update to 8.2

--- contrib/prismlauncher/template.py
+++ contrib/prismlauncher/template.py
@@ -1,18 +1,19 @@
 pkgname = "prismlauncher"
-pkgver = "8.0"
+pkgver = "8.2"
 pkgrel = 0
 build_style = "cmake"
 configure_env = {"JAVA_HOME": "/usr/lib/jvm/java-17-openjdk"}
 hostmakedepends = [
     "cmake",
-    "ninja",
     "extra-cmake-modules",
-    "qt6-qtbase",
+    "ninja",
     "openjdk17-jdk",
+    "pkgconf",
+    "qt6-qtbase",
 ]
 makedepends = [
-    "qt6-qtbase-devel",
     "qt6-qt5compat-devel",
+    "qt6-qtbase-devel",
     "qt6-qtsvg-devel",
     "quazip-devel",
     "zlib-devel",
@@ -22,4 +23,4 @@ maintainer = "aurelia <git@elia.garden>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/PrismLauncher/PrismLauncher"
 source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "462f35eeda6e107b5f23a97500accf43e4227a0fb40145b29d0895bcfe3372b0"
+sha256 = "80bc2f7657d9432c11039799fe85a31c284affa84a41cafb758ac32a96bd10fe"
