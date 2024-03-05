b452b9cb7ee04ab390483ea530079e2ab2d3c5e6
psykose <alice@ayaya.dev>
2024-03-05 21:23:17 +0000

    contrib/croc: update to 9.6.13

--- contrib/croc/template.py
+++ contrib/croc/template.py
@@ -1,5 +1,5 @@
 pkgname = "croc"
-pkgver = "9.6.6"
+pkgver = "9.6.13"
 pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
@@ -8,8 +8,10 @@ maintainer = "Paul A. Patience <paul@apatience.com>"
 license = "MIT"
 url = "https://github.com/schollz/croc"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "9dd954e0068df2be416c71161665bfc283f150d30ba0bf96cee723701e93616f"
-options = ["!debug"]
+sha256 = "5362ae8433ebd4fda9efcd853b4b8959992cf5f531ef0958ea6ed969f2eafa7b"
+# debug: fails to split on powerpc
+# check: needs network access
+options = ["!debug", "!check"]
 
 
 def post_install(self):
