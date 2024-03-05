a7170d2c9731ad3e76d1179cb50c00b397d1f5b9
psykose <alice@ayaya.dev>
2024-03-05 02:00:37 +0000

    main/gst-libav: update to 1.24.1

--- main/gst-libav/template.py
+++ main/gst-libav/template.py
@@ -1,5 +1,5 @@
 pkgname = "gst-libav"
-pkgver = "1.22.10"
+pkgver = "1.24.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddefault_library=shared"]
@@ -16,6 +16,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "d6dda7aa38a44173278de675ccd92acff0abf473f7bc02e7d1cdd4ce0f3b7642"
+sha256 = "549b7821fb5a4c866d6197888496015106cc6ec96ffb12a64e6efa91aa6562fd"
 # FIXME int
 hardening = ["!int"]
