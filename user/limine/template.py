3ee507a4354c265ee4051a5b37a588d09d0e53ce
psykose <alice@ayaya.dev>
2024-03-05 11:01:29 +0000

    user/limine: new package (7.0.5)

---
+++ user/limine/template.py
@@ -0,0 +1,22 @@
+pkgname = "limine"
+pkgver = "7.0.5"
+pkgrel = 0
+# these targets implemented
+archs = ["aarch64", "x86_64", "riscv64"]
+build_style = "gnu_configure"
+configure_args = ["--enable-all", "TOOLCHAIN_FOR_TARGET=llvm"]
+make_cmd = "gmake"
+hostmakedepends = ["automake", "gmake", "mtools", "nasm"]
+pkgdesc = "Multiprotocol EFI bootloader"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-2-Clause AND 0BSD"
+url = "https://limine-bootloader.org"
+source = f"https://github.com/limine-bootloader/limine/releases/download/v{pkgver}/limine-{pkgver}.tar.xz"
+sha256 = "b5f44cc02e5e46a310a647f737f8cf16ee75f31df51398d8122e8ed2cdd956c8"
+# no test suite
+options = ["!check"]
+
+
+def post_install(self):
+    self.rm(self.destdir / f"usr/share/doc/{pkgname}/COPYING")
+    self.install_license("COPYING")
