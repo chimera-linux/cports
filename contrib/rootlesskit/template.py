2ddae7533fb61e76cd076ca843af303cccc07711
psykose <alice@ayaya.dev>
2024-03-05 23:00:55 +0000

    contrib/rootlesskit: update to 2.0.2

--- contrib/rootlesskit/template.py
+++ contrib/rootlesskit/template.py
@@ -1,5 +1,5 @@
 pkgname = "rootlesskit"
-pkgver = "2.0.1"
+pkgver = "2.0.2"
 pkgrel = 0
 build_style = "go"
 make_build_args = [
@@ -13,6 +13,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/rootless-containers/rootlesskit"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "4e984e7f581d226c12b5627415a7bfc3284a0b25534e5dd0c44964a8bc784906"
+sha256 = "8324faa8ae2bab7b79d9b3b5acb9568ab28a3e3df4900001dc4a8b01adaff161"
 # no tests
 options = ["!debug", "!check"]
