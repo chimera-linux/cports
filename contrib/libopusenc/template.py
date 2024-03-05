89c50edb3951049d9b48fa8411e0cf40f6a5b000
psykose <alice@ayaya.dev>
2024-03-05 03:52:59 +0000

    contrib/libopusenc: new package (0.2.1)

---
+++ contrib/libopusenc/template.py
@@ -0,0 +1,25 @@
+pkgname = "libopusenc"
+pkgver = "0.2.1"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = [
+    "automake",
+    "libtool",
+    "pkgconf",
+]
+makedepends = ["opus-devel"]
+pkgdesc = "Library for encoding Opus files"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-3-Clause"
+url = "https://opus-codec.org"
+source = f"https://archive.mozilla.org/pub/opus/libopusenc-{pkgver}.tar.gz"
+sha256 = "8298db61a8d3d63e41c1a80705baa8ce9ff3f50452ea7ec1c19a564fe106cbb9"
+
+
+def post_install(self):
+    self.install_license("COPYING")
+
+
+@subpackage("libopusenc-devel")
+def _devel(self):
+    return self.default_devel()
