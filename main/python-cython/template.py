b4b7015c1884c51a8bbd0182fe53545844f50b80
psykose <alice@ayaya.dev>
2024-03-06 08:18:29 +0000

    main/python-cython: update to 3.0.9

--- main/python-cython/template.py
+++ main/python-cython/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-cython"
-pkgver = "3.0.8"
+pkgver = "3.0.9"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,6 +15,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://cython.org"
 source = f"$(PYPI_SITE)/C/Cython/Cython-{pkgver}.tar.gz"
-sha256 = "8333423d8fd5765e7cceea3a9985dd1e0a5dfeb2734629e1a2ed2d6233d39de6"
+sha256 = "a2d354f059d1f055d34cfaa62c5b68bc78ac2ceab6407148d47fb508cf3ba4f3"
 # flaky tests
 options = ["!check"]
