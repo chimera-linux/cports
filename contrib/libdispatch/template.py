6208a4b1f84851a00b5ff04e63e20ce90e6cae7d
psykose <alice@ayaya.dev>
2024-03-06 08:33:32 +0000

    contrib/libdispatch: update to 5.10

--- contrib/libdispatch/template.py
+++ contrib/libdispatch/template.py
@@ -1,5 +1,5 @@
 pkgname = "libdispatch"
-pkgver = "5.9.2"
+pkgver = "5.10"
 pkgrel = 0
 build_style = "cmake"
 # these always fail on linux for some reason on musl
@@ -17,7 +17,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://apple.github.io/swift-corelibs-libdispatch"
 source = f"https://github.com/apple/swift-corelibs-libdispatch/archive/refs/tags/swift-{pkgver}-RELEASE.tar.gz"
-sha256 = "b1f3e46ed248df6a3456d20bc23b2d8a12b740a40185d81b668b1d31735cadf2"
+sha256 = "16e088cf12654d22658879710b9694a6fad1c94d5e5d0c597741b71fbcb3e034"
 # FIXME: cfi
 hardening = ["vis"]
 
