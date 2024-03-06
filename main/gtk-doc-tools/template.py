0060a8193dd78cee964ddf05038be83e703dfacc
psykose <alice@ayaya.dev>
2024-03-06 08:20:41 +0000

    main/gtk-doc-tools: update to 1.34.0

--- main/gtk-doc-tools/template.py
+++ main/gtk-doc-tools/template.py
@@ -1,32 +1,32 @@
 pkgname = "gtk-doc-tools"
-pkgver = "1.33.2"
-pkgrel = 2
+pkgver = "1.34.0"
+pkgrel = 0
 build_style = "meson"
 # glib cyclic dep
 configure_args = ["-Dtests=false"]
 hostmakedepends = [
-    "meson",
     "docbook-xml",
     "docbook-xsl-nons",
+    "gettext",
     "itstool",
-    "xsltproc",
+    "meson",
     "pkgconf",
     "python-pygments",
-    "gettext",
+    "xsltproc",
 ]
 depends = [
     "docbook-xml",
     "docbook-xsl-nons",
-    "xsltproc",
-    "python-pygments",
     "python-lxml",
+    "python-pygments",
+    "xsltproc",
 ]
 pkgdesc = "Documentation tool for public library API"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND GFDL-1.1-or-later"
 url = "http://www.gtk.org/gtk-doc"
 source = f"$(GNOME_SITE)/gtk-doc/{pkgver[:-2]}/gtk-doc-{pkgver}.tar.xz"
-sha256 = "cc1b709a20eb030a278a1f9842a362e00402b7f834ae1df4c1998a723152bf43"
+sha256 = "b20b72b32a80bc18c7f975c9d4c16460c2276566a0b50f87d6852dff3aa7861c"
 options = ["!splitdoc"]
 
 
