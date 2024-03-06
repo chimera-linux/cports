7aa7df9276edf4b866a06ca6b1c46c0d42841bbd
psykose <alice@ayaya.dev>
2024-03-06 08:18:12 +0000

    main/pango: update to 1.52.1

--- main/pango/template.py
+++ main/pango/template.py
@@ -1,33 +1,33 @@
 pkgname = "pango"
-pkgver = "1.52.0"
+pkgver = "1.52.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=enabled"]
 hostmakedepends = [
+    "glib-devel",
+    "gobject-introspection",
+    "help2man",
     "meson",
     "pkgconf",
-    "glib-devel",
-    "help2man",
-    "gobject-introspection",
 ]
 makedepends = [
+    "cairo-devel",
     "fribidi-devel",
     "harfbuzz-devel",
-    "libxft-devel",
     "libthai-devel",
-    "cairo-devel",
+    "libxft-devel",
 ]
 checkdepends = [
+    "fonts-cantarell-otf",
     "fonts-dejavu-ttf",
     "fonts-liberation-ttf",
-    "fonts-cantarell-otf",
 ]
 pkgdesc = "Text rendering and layout library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.pango.org"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:pkgver.rfind('.')]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "1ec8518879c3f43224499f08e8ecbbdf4a5d302ed6cd3853b4fa949f82b89a9b"
+sha256 = "58728a0a2d86f60761208df9493033d18ecb2497abac80ee1a274ad0c6e55f0f"
 
 
 @subpackage("pango-xft")
