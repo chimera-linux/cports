2803b0845e15a834b9ef6000de5644126594ef90
ttyyls <contact@behri.org>
2024-03-06 22:20:06 +0100

    contrib/stress-ng: new package (0.17.07)

---
+++ contrib/stress-ng/template.py
@@ -0,0 +1,30 @@
+pkgname = "stress-ng"
+pkgver = "0.17.07"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "pkgconf"]
+makedepends = [
+    "acl-devel",
+    "libjpeg-turbo-devel",
+    "linux-headers",
+    "xxhash-devel",
+    "zlib-devel",
+]
+pkgdesc = "Stress test a computer in various selectable ways"
+maintainer = "ttyyls <contact@behri.org>"
+license = "GPL-2.0-or-later"
+url = "https://github.com/ColinIanKing/stress-ng"
+source = f"https://github.com/ColinIanKing/stress-ng/archive/refs/tags/V{pkgver}.tar.gz"
+sha256 = "b0bc1495adce6c7a1f82d53f363682b243d6d7e93a06be7f94c9559c0a311a6f"
+env = {
+    "MAN_COMPRESS": "0",
+    "PRESERVE_CFLAGS": "1",
+}
+hardening = ["!int"]
+# no portable tests defined
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("COPYING")
