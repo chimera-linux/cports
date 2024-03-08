b86a466059604c9d3b60cb48ffe0c7193a5c39e4
psykose <alice@ayaya.dev>
2024-03-08 22:37:38 +0000

    main/shaderc: update to 2024.0

--- main/shaderc/template.py
+++ main/shaderc/template.py
@@ -1,6 +1,6 @@
 pkgname = "shaderc"
-pkgver = "2023.8"
-pkgrel = 1
+pkgver = "2024.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DSHADERC_SKIP_TESTS=ON",
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/google/shaderc"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "dfec5045f30d8f6d3d3914ab5b3cc2695947f266d41261b1459177cd789308d1"
+sha256 = "c761044e4e204be8e0b9a2d7494f08671ca35b92c4c791c7049594ca7514197f"
 tool_flags = {
     "CXXFLAGS": [f"-I{self.profile().sysroot / 'usr/include/glslang'}"]
 }
