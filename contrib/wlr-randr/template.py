a8d57e2b40c4d57e0be1680fc8b9726d315f7f33
Isaac Freund <mail@isaacfreund.com>
2024-03-08 17:14:08 +0100

    contrib/wlr-randr: update to 0.4.1

--- contrib/wlr-randr/template.py
+++ contrib/wlr-randr/template.py
@@ -1,5 +1,5 @@
 pkgname = "wlr-randr"
-pkgver = "0.4.0"
+pkgver = "0.4.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -15,7 +15,7 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "MIT"
 url = "https://sr.ht/~emersion/wlr-randr"
 source = f"https://git.sr.ht/~emersion/wlr-randr/refs/download/v{pkgver}/wlr-randr-{pkgver}.tar.gz"
-sha256 = "da6814105e97843d2aedc6d0639b210a571ae85b36aca69ee1ecf173e9652a7a"
+sha256 = "fd5783002c13d37267898e571f03b618363d13952a264cf3d61a35383869d631"
 
 
 def post_install(self):
