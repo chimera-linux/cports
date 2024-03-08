f701273bd208f703514377f99c0be0af56310e6b
psykose <alice@ayaya.dev>
2024-03-08 10:23:43 +0000

    main/bluez: update to 5.73

--- main/bluez/template.py
+++ main/bluez/template.py
@@ -1,59 +1,54 @@
 pkgname = "bluez"
-pkgver = "5.72"
-pkgrel = 1
+pkgver = "5.73"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
-    "--disable-systemd",
-    "--disable-experimental",
     "--disable-deprecated",
+    "--disable-experimental",
     "--disable-mesh",
     "--disable-nfc",
+    "--disable-systemd",
     "--enable-cups",
+    "--enable-library",
     "--enable-sixaxis",
     "--enable-threads",
-    "--enable-library",
     "--with-udevdir=/usr/lib/udev",
 ]
 make_cmd = "gmake"
 hostmakedepends = [
-    "gmake",
-    "pkgconf",
-    "flex",
-    "python-docutils",
     "automake",
+    "flex",
+    "gmake",
     "libtool",
+    "pkgconf",
+    "python-docutils",
 ]
 # TODO: look into porting to libedit later
 # same story as iwd, really crappy usage of readline API
 makedepends = [
-    "udev-devel",
-    "dbus-devel",
     "cups-devel",
+    "dbus-devel",
     "glib-devel",
     "libical-devel",
-    "readline-devel",
     "linux-headers",
     "musl-bsd-headers",
+    "readline-devel",
+    "udev-devel",
 ]
 pkgdesc = "Linux Bluetooth stack"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "http://www.bluez.org"
 source = f"$(KERNEL_SITE)/bluetooth/{pkgname}-{pkgver}.tar.xz"
-sha256 = "499d7fa345a996c1bb650f5c6749e1d929111fa6ece0be0e98687fee6124536e"
-tool_flags = {"CFLAGS": ["-Wno-deprecated-declarations"]}
-options = ["linkundefver"]
-
-
-def post_patch(self):
-    self.mv(
-        "obexd/src/org.bluez.obex.service",
-        "obexd/src/org.bluez.obex.service.in",
-    )
+sha256 = "257e9075ce05c70d48c5defd254e78c418416f7584b45f9dddc884ff88e3fc53"
+tool_flags = {
+    "CFLAGS": ["-Wno-deprecated-declarations"],
+    # workaround for --gc-sections breaking in test files
+    "LDFLAGS": ["-Wl,-z,nostart-stop-gc"],
+}
 
 
 def post_install(self):
-    self.install_file("src/main.conf", "etc/bluetooth")
     self.install_service(self.files_path / "bluetoothd")
 
 
