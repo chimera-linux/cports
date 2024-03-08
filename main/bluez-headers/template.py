09f95ca6bb27d953ae3477a9e5edd23377392442
psykose <alice@ayaya.dev>
2024-03-08 10:55:06 +0000

    main/bluez-headers: update to 5.73

--- main/bluez-headers/template.py
+++ main/bluez-headers/template.py
@@ -1,5 +1,5 @@
 pkgname = "bluez-headers"
-pkgver = "5.72"
+pkgver = "5.73"
 pkgrel = 0
 depends = ["!bluez-devel"]
 pkgdesc = "Linux Bluetooth stack (header files)"
@@ -7,7 +7,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "http://www.bluez.org"
 source = f"$(KERNEL_SITE)/bluetooth/bluez-{pkgver}.tar.xz"
-sha256 = "499d7fa345a996c1bb650f5c6749e1d929111fa6ece0be0e98687fee6124536e"
+sha256 = "257e9075ce05c70d48c5defd254e78c418416f7584b45f9dddc884ff88e3fc53"
 
 
 def do_install(self):
