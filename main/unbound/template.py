b2017b996558f49f92b34594ee18a4217bd116d1
psykose <alice@ayaya.dev>
2024-03-08 11:00:18 +0000

    main/unbound: update to 1.19.2

--- main/unbound/template.py
+++ main/unbound/template.py
@@ -1,5 +1,5 @@
 pkgname = "unbound"
-pkgver = "1.19.1"
+pkgver = "1.19.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -28,7 +28,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://nlnetlabs.nl/projects/unbound/about"
 source = f"https://nlnetlabs.nl/downloads/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "bc1d576f3dd846a0739adc41ffaa702404c6767d2b6082deb9f2f97cbb24a3a9"
+sha256 = "cc560d345734226c1b39e71a769797e7fdde2265cbb77ebce542704bba489e55"
 
 
 def post_install(self):
