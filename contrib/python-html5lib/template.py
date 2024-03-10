d5435feb3cb313c6d1c6902f6aa94f365bbd3890
psykose <alice@ayaya.dev>
2024-03-10 13:19:03 +0000

    contrib/python-html5lib: new package (1.1)

---
+++ contrib/python-html5lib/template.py
@@ -0,0 +1,31 @@
+pkgname = "python-html5lib"
+pkgver = "1.1"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = [
+    "python-six",
+    "python-webencodings",
+]
+checkdepends = [
+    "python-mock",
+    "python-pytest",
+    "python-pytest-expect",
+] + depends
+pkgdesc = "Python html parser"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/html5lib/html5lib-python"
+source = f"$(PYPI_SITE)/h/html5lib/html5lib-{pkgver}.tar.gz"
+sha256 = "b2e5b40261e20f354d198eae92afc10d750afb487ed5e50f9c4eaf07c184146f"
+# broken with new pytest
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
