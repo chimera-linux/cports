77c3702f49b22dfab2bb281f2063e0efd1d49fec
Erica Z <zerica@callcc.eu>
2024-03-10 18:47:13 +0100

    contrib/tectonic: new package (0.15.0)

---
+++ contrib/tectonic/template.py
@@ -0,0 +1,30 @@
+pkgname = "tectonic"
+pkgver = "0.15.0"
+pkgrel = 0
+build_style = "cargo"
+make_build_args = [
+    "--no-default-features",
+    "--features",
+    "geturl-curl,serialization,external-harfbuzz",
+]
+make_install_args = make_build_args
+make_check_args = ["--features", "external-harfbuzz"]
+hostmakedepends = ["cargo", "pkgconf"]
+makedepends = [
+    "freetype-devel",
+    "graphite2-devel",
+    "harfbuzz-devel",
+    "icu-devel",
+    "libcurl-devel",
+    "openssl-devel",
+]
+pkgdesc = "Modernized LaTeX engine"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "MIT"
+url = "https://tectonic-typesetting.github.io/en-US"
+source = f"https://github.com/tectonic-typesetting/tectonic/archive/refs/tags/tectonic@{pkgver}.tar.gz"
+sha256 = "3c13de312c4fe39ff905ad17e64a15a3a59d33ab65dacb0a8b9482c57e6bc6aa"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
---
+++ contrib/tectonic/update.py
@@ -0,0 +1,2 @@
+url = "https://api.github.com/repos/tectonic-typesetting/tectonic/git/refs/tags"
+pattern = r"refs/tags/tectonic@([0-9.]+)"
