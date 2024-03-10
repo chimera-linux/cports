f4cd093bcd0f5f8fcccf9deb441014fda3f35deb
psykose <alice@ayaya.dev>
2024-03-10 13:18:55 +0000

    contrib/python-pytest-expect: new package (1.1.0)

---
+++ contrib/python-pytest-expect/template.py
@@ -0,0 +1,26 @@
+pkgname = "python-pytest-expect"
+pkgver = "1.1.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = [
+    "python-pytest",
+    "python-u-msgpack",
+]
+pkgdesc = "Pytest plugin to store test expectations"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/gsnedders/pytest-expect"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "8392093123dfc9807c28fee251e1d710aaed0d46ee77c7531528fb0a265eb798"
+# no tests
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
