a976150f57fba9c823c9f9d9e8d7873124506675
psykose <alice@ayaya.dev>
2024-03-10 08:12:12 +0000

    main/debianutils: update to 5.17

--- main/debianutils/template.py
+++ main/debianutils/template.py
@@ -1,12 +1,12 @@
 pkgname = "debianutils"
-pkgver = "5.16"
+pkgver = "5.17"
 pkgrel = 0
 pkgdesc = "Miscellaneous utilities from Debian"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://tracker.debian.org/pkg/debianutils"
 source = f"$(DEBIAN_SITE)/main/d/debianutils/debianutils_{pkgver}.tar.xz"
-sha256 = "eeb069c2639906f4181214dd1c4e448bc825229b0250ebf66f69c7344e8aa1e0"
+sha256 = "367654878388f532cd8a897fe64766e2d57ae4c60da1d4d8f20dcdf2fb0cbde8"
 hardening = ["vis", "cfi"]
 
 
