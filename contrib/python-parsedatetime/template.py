65dd7bf2aee264a0837b4d80a40ced9d1d83ba18
Duncan Bellamy <dunk@denkimushi.com>
2024-03-10 21:28:38 +0000

    contrib/python-parsedatetime: new package (2.6)

---
+++ contrib/python-parsedatetime/template.py
@@ -0,0 +1,17 @@
+pkgname = "python-parsedatetime"
+pkgver = "2.6"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "Parse human-readable date/time strings"
+maintainer = "Duncan Bellamy <dunk@denkimushi.com>"
+license = "Apache-2.0"
+url = "https://github.com/bear/parsedatetime"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "0cbf3fe4dee18c88df343bc568d35fdc67774846cb4aec2b2626d1bee7a0c6c5"
