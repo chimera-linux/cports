09b09b06e02b459c42b4893ffffe8c6b2979d850
psykose <alice@ayaya.dev>
2024-03-11 11:23:18 +0000

    main/python-packaging: update to 24.0

--- main/python-packaging/template.py
+++ main/python-packaging/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-packaging"
-pkgver = "23.2"
-pkgrel = 1
+pkgver = "24.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-installer", "python-flit_core"]
 checkdepends = ["python-pytest"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0 OR BSD-2-Clause"
 url = "https://github.com/pypa/packaging"
 source = f"$(PYPI_SITE)/p/packaging/packaging-{pkgver}.tar.gz"
-sha256 = "048fb0e9405036518eaaf48a55953c750c11e1a1b68e0dd1a9d62ed0c092cfc5"
+sha256 = "eb82c5e3e56209074766e6885bb04b8c38a0c015d0a30036ebe7ece34c9989e9"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
