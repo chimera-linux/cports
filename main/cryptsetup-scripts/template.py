63cb5d9e2db69697e7515f94d0b2f00fff179662
Mohammed Anas <triallax@tutanota.com>
2024-03-11 13:48:19 +0000

    main/cryptsetup-scripts: update to 2.7.1.1

--- main/cryptsetup-scripts/template.py
+++ main/cryptsetup-scripts/template.py
@@ -1,6 +1,6 @@
 pkgname = "cryptsetup-scripts"
 _debver = "1"
-_cver = "2.7.0"
+_cver = "2.7.1"
 pkgver = f"{_cver}.{_debver}"
 pkgrel = 0
 build_style = "makefile"
@@ -15,6 +15,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://salsa.debian.org/cryptsetup-team/cryptsetup"
 source = f"{url}/-/archive/debian/2%25{_cver}-{_debver}/cryptsetup-debian-2%25{_cver}-{_debver}.tar.gz"
-sha256 = "9cd6cf4bf5a1a85ed66852df0e53ba540ec832f6d3d97dcd90fa6dc28c172bda"
+sha256 = "6340a815e2e2d5e55ec99722e90228b6ff9ab2e4428245bbf3fd4212d161bb29"
 # no test suite
 options = ["!check"]
