755d70719ec102b15453a1ae8487a193fbfbb838
psykose <alice@ayaya.dev>
2024-03-11 11:19:53 +0000

    contrib/yt-dlp: update to 2024.03.10

--- contrib/yt-dlp/template.py
+++ contrib/yt-dlp/template.py
@@ -1,21 +1,21 @@
 pkgname = "yt-dlp"
-pkgver = "2023.12.30"
-pkgrel = 1
+pkgver = "2024.03.10"
+pkgrel = 0
 build_style = "python_pep517"
 make_check_args = ["-k", "not download"]
 hostmakedepends = [
     "python-build",
+    "python-hatchling",
     "python-installer",
-    "python-setuptools",
     "python-wheel",
 ]
 checkdepends = ["python-pytest", "python-nose", "flake8"]
-depends = ["python"]
+depends = ["python", "python-certifi"]
 pkgdesc = "CLI program to download videos from YouTube and other sites"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Unlicense"
 url = "https://github.com/yt-dlp/yt-dlp"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "2878501990f3ab6aa8eabb7346c16d4b4cbc01984372a1f0b3a6be8d260c7aff"
+sha256 = "079dbf54586e8120c7d76e0dd1b7a60e6c13f4d23f9681fc919577f2fe17d8cd"
 # missing checkdepends
 options = ["!check"]
