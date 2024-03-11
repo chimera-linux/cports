a5d4d7f4615fc378bbe106fa5163d5e9da75810b
Augustine <avgwst@tutanota.de>
2024-03-11 18:06:19 +0100

    contrib/dbus-test-runner: new package (19.04.0)

---
+++ contrib/dbus-test-runner/template.py
@@ -0,0 +1,30 @@
+pkgname = "dbus-test-runner"
+pkgver = "19.04.0"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--disable-static"]
+make_cmd = "gmake"
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+]
+makedepends = [
+    "glib-devel",
+]
+depends = ["dbus"]
+checkdepends = ["bash", "dbus", "python-dbusmock"]
+pkgdesc = "Test executables under a new DBus session"
+maintainer = "avgwst <avgwst@tutanota.de>"
+license = "GPL-3.0-only"
+url = "https://launchpad.net/dbus-test-runner"
+source = f"{url}/{pkgver[:pkgver.rfind('.')]}/{pkgver}/+download/{pkgname}-{pkgver}.tar.gz"
+sha256 = "645a32fbd909baf2c01438f0cbda29dc9cd01a7aba5504c45610d88e8a57cb76"
+
+
+@subpackage("dbus-test-runner-devel")
+def _devel(self):
+    return self.default_devel()
