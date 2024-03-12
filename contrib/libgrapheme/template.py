21ee0e1bdc600697b24239d51fb4a84de83aaa16
q66 <q66@chimera-linux.org>
2024-03-12 12:34:47 +0100

    contrib/libgrapheme: use CFLAGS in linking + disable LTO

--- contrib/libgrapheme/template.py
+++ contrib/libgrapheme/template.py
@@ -1,6 +1,6 @@
 pkgname = "libgrapheme"
 pkgver = "2.0.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "makefile"
 make_check_target = "test"
 hostmakedepends = ["pkgconf"]
@@ -10,6 +10,8 @@ license = "ISC AND Unicode-3.0"
 url = "https://libs.suckless.org/libgrapheme"
 source = f"https://dl.suckless.org/libgrapheme/libgrapheme-{pkgver}.tar.gz"
 sha256 = "a68bbddde76bd55ba5d64116ce5e42a13df045c81c0852de9ab60896aa143125"
+# link errors on ppc*
+options = ["!lto"]
 
 
 def post_install(self):
