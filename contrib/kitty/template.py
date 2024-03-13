36bc23d62217c06f977f3b8bcd8e228c62a56d96
psykose <alice@ayaya.dev>
2024-03-13 10:30:39 +0000

    contrib/kitty: update to 0.33.0

--- contrib/kitty/template.py
+++ contrib/kitty/template.py
@@ -1,6 +1,6 @@
 pkgname = "kitty"
-pkgver = "0.32.2"
-pkgrel = 2
+pkgver = "0.33.0"
+pkgrel = 0
 hostmakedepends = [
     "go",
     "pkgconf",
@@ -25,6 +25,7 @@ makedepends = [
     "libxrandr-devel",
     "openssl-devel",
     "python-devel",
+    "simde",
     "xxhash-devel",
 ]
 depends = [
@@ -36,7 +37,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-only"
 url = "https://sw.kovidgoyal.net/kitty"
 source = f"https://github.com/kovidgoyal/kitty/releases/download/v{pkgver}/kitty-{pkgver}.tar.xz"
-sha256 = "d4bb54f7bfc16e274d60326323555b63733915c3e32d2ef711fd9860404bd6f2"
+sha256 = "5b11b4edddba41269824df9049d60e22ffc35551446161018dfaf5cd22f77297"
 # nah
 options = ["!cross"]
 
