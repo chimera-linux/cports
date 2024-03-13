9c3ce17532c7461a835b20a0bf43bd4770e69497
miko <mikoxyzzz@gmail.com>
2024-03-13 15:08:00 +0100

    main/enchant: enable tests

--- main/enchant/template.py
+++ main/enchant/template.py
@@ -6,20 +6,18 @@ configure_args = ["--enable-relocatable", "--disable-static"]
 hostmakedepends = ["pkgconf", "automake", "libtool"]
 makedepends = [
     "glib-devel",
-    "nuspell-devel",
     "hunspell-devel",
     "icu-devel",
     "libltdl-devel",
+    "nuspell-devel",
+    "unittest-cpp-devel",
 ]
-checkdepends = ["unittest-cpp"]
 pkgdesc = "Generic spell checking library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://abiword.github.io/enchant"
 source = f"https://github.com/AbiWord/enchant/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "a1c2e5b59acca000bbfb24810af4a1165733d407f2154786588e076c8cd57bfc"
-# missing checkdepends
-options = ["!check"]
 
 
 @subpackage("enchant-devel")
