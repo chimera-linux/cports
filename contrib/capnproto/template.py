ed7aab4f6c93302b2fd2c39c592d5040533f5ed1
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-03-14 21:28:51 +0100

    contrib/capnproto: new package (1.0.2)

---
+++ contrib/capnproto/template.py
@@ -0,0 +1,32 @@
+pkgname = "capnproto"
+pkgver = "1.0.2"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_SHARED_LIBS=ON"]
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = [
+    "libucontext-devel",
+    "linux-headers",
+    "openssl-devel",
+    "zlib-devel",
+]
+pkgdesc = "Cap'n Proto serialization/RPC system"
+maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
+license = "MIT"
+url = "https://capnproto.org"
+source = f"{url}/capnproto-c++-{pkgver}.tar.gz"
+sha256 = "9057dbc0223366b74bbeca33a05de164a229b0377927f1b7ef3828cdd8cb1d7e"
+
+
+if self.profile().cross:
+    hostmakedepends += ["capnproto-devel"]
+    configure_args += ["-DEXTERNAL_CAPNP=ON", "-DBUILD_TESTING=OFF"]
+
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
+
+
+@subpackage("capnproto-devel")
+def _devel(self):
+    return self.default_devel(extra=["usr/bin"])
