0103e370c763df6b26dbb5a5e4ea74d11b25e703
psykose <alice@ayaya.dev>
2024-03-14 10:23:46 +0000

    main/python-wheel: update to 0.43.0

--- main/python-wheel/template.py
+++ main/python-wheel/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-wheel"
-pkgver = "0.42.0"
-pkgrel = 1
+pkgver = "0.43.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -18,7 +18,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pypa/wheel"
 source = f"$(PYPI_SITE)/w/wheel/wheel-{pkgver}.tar.gz"
-sha256 = "c45be39f7882c9d34243236f2d63cbd58039e360f85d0913425fbd7ceea617a8"
+sha256 = "465ef92c69fa5c5da2d1cf8ac40559a8c940886afcef87dcf14b9470862f1d85"
 # cylic
 options = ["!check"]
 
