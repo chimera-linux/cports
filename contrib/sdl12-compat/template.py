7f689ab89929c8752b5ecf5296b2187f1d2e05bd
Erica Z <zerica@callcc.eu>
2024-03-14 06:39:52 +0100

    contrib/sdl12-compat: new package (1.2.68)

---
+++ contrib/sdl12-compat/template.py
@@ -0,0 +1,19 @@
+pkgname = "sdl12-compat"
+pkgver = "1.2.68"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = ["sdl-devel"]
+# is dlopen'ed
+depends = ["so:libSDL2-2.0.so.0!sdl"]
+pkgdesc = "Compatibility layer for SDL 1.2"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "Zlib"
+url = "https://github.com/libsdl-org/sdl12-compat"
+source = f"{url}/archive/refs/tags/release-{pkgver}.tar.gz"
+sha256 = "63c6e4dcc1154299e6f363c872900be7f3dcb3e42b9f8f57e05442ec3d89d02d"
+
+
+@subpackage("sdl12-compat-devel")
+def _devel(self):
+    return self.default_devel()
