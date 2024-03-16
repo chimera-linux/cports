8cc0ee5c5256e3b488af4ab2ee0496d000a5f3c2
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-03-16 17:28:59 +0100

    cbuild: build auditable rust binaries if cargo-auditable is available

--- src/cbuild/util/cargo.py
+++ src/cbuild/util/cargo.py
@@ -136,10 +136,15 @@ class Cargo:
         if legacy.is_file():
             self.template.error("cargo: found legacy .cargo/config")
 
+        auditable = (
+            self.template.bldroot_path / "usr/bin/cargo-auditable"
+        ).exists()
+
+        cargo = ["cargo", "auditable"] if auditable else ["cargo"]
         return self.template.do(
             *wrapper,
             *ewrapper,
-            "cargo",
+            *cargo,
             command,
             *bargs,
             *args,
