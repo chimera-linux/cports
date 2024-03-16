ef2fc5a9fcf0dadf3a0ec1022d225b1a88c5e011
psykose <alice@ayaya.dev>
2024-03-16 14:43:00 +0000

    contrib/spek: new package (0.8.5)

---
+++ contrib/spek/template.py
@@ -0,0 +1,23 @@
+pkgname = "spek"
+pkgver = "0.8.5"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "libtool",
+    "pkgconf",
+]
+makedepends = [
+    "ffmpeg-devel",
+    "wxwidgets-devel",
+]
+pkgdesc = "Acoustic spectrum analyser"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-3.0-only"
+url = "https://github.com/alexkay/spek"
+source = f"{url}/releases/download/v{pkgver}/spek-{pkgver}.tar.xz"
+sha256 = "1bccf85a14a01af8f2f30476cbad004e8bf6031f500e562bbe5bbd1e5eb16c59"
+hardening = ["vis", "cfi"]
