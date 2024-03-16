e48432548b0038130796fd1de4686fcd84633ebe
psykose <alice@ayaya.dev>
2024-03-16 15:07:30 +0000

    contrib/git-cliff: generate completions/manpage

--- contrib/git-cliff/template.py
+++ contrib/git-cliff/template.py
@@ -1,6 +1,6 @@
 pkgname = "git-cliff"
 pkgver = "2.1.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "cargo"
 make_check_args = [
     "--",
@@ -16,8 +16,25 @@ license = "Apache-2.0 OR MIT"
 url = "https://github.com/orhun/git-cliff"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "1c321b180b071ccfa8986931576fc02ca4dbc75dff035e4c663c2cfb8ecd4683"
+# generates manpages/completions with host bins
+options = ["!cross"]
+
+
+def post_build(self):
+    self.do(
+        f"target/{self.profile().triplet}/release/git-cliff-mangen",
+        env={"OUT_DIR": "."},
+    )
+    self.do(
+        f"target/{self.profile().triplet}/release/git-cliff-completions",
+        env={"OUT_DIR": "."},
+    )
 
 
 def do_install(self):
-    self.cargo.install(wrksrc="git-cliff")
+    self.install_bin(f"target/{self.profile().triplet}/release/git-cliff")
+    self.install_man("git-cliff.1")
+    self.install_completion("git-cliff.bash", "bash")
+    self.install_completion("git-cliff.fish", "fish")
+    self.install_completion("_git-cliff", "zsh")
     self.install_license("LICENSE-MIT")
