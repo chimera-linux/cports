c06b3317407224fb2959d120a27169247dabf625
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-03-16 17:33:39 +0100

    main/cargo-auditable: new package (0.6.4)

---
+++ main/cargo-auditable/template.py
@@ -0,0 +1,27 @@
+pkgname = "cargo-auditable"
+pkgver = "0.6.4"
+pkgrel = 0
+build_style = "cargo"
+make_build_args = ["-p", "cargo-auditable"]
+make_check_args = make_build_args + [
+    "--",
+    "--skip",
+    # probably fails because we have slightly older cargo
+    "test_wasm",
+]
+hostmakedepends = ["cargo"]
+makedepends = ["rust-std"]
+depends = ["cargo"]
+pkgdesc = "Tool for embedding dependency information in rust binaries"
+maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
+license = "Apache-2.0 OR MIT"
+url = "https://github.com/rust-secure-code/cargo-auditable"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "3e3f4134d81b47277d34c44bc1169c9b0356612977651f8e98e2ba1a470b69a2"
+
+
+def do_install(self):
+    self.install_bin(
+        f"./target/{self.profile().triplet}/release/cargo-auditable"
+    )
+    self.install_license("LICENSE-MIT")
