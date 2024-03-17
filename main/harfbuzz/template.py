52944814900813f4316d76b8c9b36fea774a6e48
psykose <alice@ayaya.dev>
2024-03-17 17:01:21 +0000

    main/harfbuzz: update to 8.3.1

--- main/harfbuzz/template.py
+++ main/harfbuzz/template.py
@@ -1,29 +1,29 @@
 pkgname = "harfbuzz"
-pkgver = "8.3.0"
-pkgrel = 1
+pkgver = "8.3.1"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Db_ndebug=true",
-    "-Dglib=enabled",
-    "-Dfreetype=enabled",
     "-Dcairo=enabled",
-    "-Dicu=enabled",
-    "-Dgraphite2=enabled",
-    "-Dintrospection=enabled",
     "-Ddocs=enabled",
+    "-Dfreetype=enabled",
+    "-Dglib=enabled",
+    "-Dgraphite2=enabled",
+    "-Dicu=enabled",
+    "-Dintrospection=enabled",
 ]
 hostmakedepends = [
+    "glib-devel",
+    "gobject-introspection",
+    "gtk-doc-tools",
     "meson",
     "pkgconf",
-    "glib-devel",
-    "gtk-doc-tools",
-    "gobject-introspection",
     # prevent installing self through freetype
     "freetype-bootstrap",
 ]
 makedepends = [
-    "freetype-bootstrap",
     "cairo-devel",
+    "freetype-bootstrap",
     "graphite2-devel",
     "icu-devel",
 ]
@@ -32,9 +32,8 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://www.freedesktop.org/wiki/Software/HarfBuzz"
 source = f"https://github.com/harfbuzz/harfbuzz/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "109501eaeb8bde3eadb25fab4164e993fbace29c3d775bcaa1c1e58e2f15f847"
-# test failures since icu 71
-options = ["!cross", "!check"]
+sha256 = "f73e1eacd7e2ffae687bc3f056bb0c705b7a05aee86337686e09da8fc1c2030c"
+options = ["!cross"]
 
 
 def post_install(self):
