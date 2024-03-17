1991c476a7f4a2bb229187051c2b61cd4d13ac93
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-03-17 22:33:18 +0100

    contrib/oath-toolkit: new package (2.6.11)

---
+++ contrib/oath-toolkit/template.py
@@ -0,0 +1,28 @@
+pkgname = "oath-toolkit"
+pkgver = "2.6.11"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gmake",
+    "gtk-doc-tools",
+    "libtool",
+    "libxml2-progs",
+    "pkgconf",
+]
+makedepends = ["linux-pam-devel", "libxml2-devel"]
+pkgdesc = "OATH one-time password toolkit"
+maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
+license = "GPL-3.0-or-later"
+url = "http://www.nongnu.org/oath-toolkit"
+source = f"http://download.savannah.gnu.org/releases/{pkgname}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "fc512a4a5b46f4c43ab0586c3189fece4d54f7e649397d6fa1e23428431e2cb4"
+# TODO: failing checks are yet to be investigated
+options = ["!check"]
+
+
+@subpackage("oath-toolkit-devel")
+def _devel(self):
+    return self.default_devel()
