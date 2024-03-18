536db8fce41de769f9aeb6ed12e7427eab3ea18b
Mohammed Anas <triallax@tutanota.com>
2024-03-18 15:16:54 +0000

    main/gtkmm: update to 4.14.0

--- main/gtkmm/template.py
+++ main/gtkmm/template.py
@@ -1,5 +1,5 @@
 pkgname = "gtkmm"
-pkgver = "4.12.0"
+pkgver = "4.14.0"
 pkgrel = 0
 build_style = "meson"
 make_check_wrapper = ["weston-headless-run"]
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gtkmm.org"
 source = f"$(GNOME_SITE)/gtkmm/{pkgver[:-2]}/gtkmm-{pkgver}.tar.xz"
-sha256 = "fbc3e7618123345c0148ef71abb6548d421f52bb224fbda34875b677dc032c92"
+sha256 = "9350a0444b744ca3dc69586ebd1b6707520922b6d9f4f232103ce603a271ecda"
 
 
 @subpackage("gtkmm-devel")
