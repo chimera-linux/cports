9498cda61475a87ca1e1e3cfe8e1d66db7e25cad
Mohammed Anas <triallax@tutanota.com>
2024-03-18 14:31:50 +0000

    main/power-profiles-daemon: update to 0.20

--- main/power-profiles-daemon/template.py
+++ main/power-profiles-daemon/template.py
@@ -1,8 +1,8 @@
 pkgname = "power-profiles-daemon"
-pkgver = "0.13"
-pkgrel = 2
+pkgver = "0.20"
+pkgrel = 0
 build_style = "meson"
-configure_args = ["-Dsystemdsystemunitdir=/tmp"]
+configure_args = ["-Dsystemdsystemunitdir=", "-Dtests=false"]
 hostmakedepends = [
     "glib-devel",
     "meson",
@@ -18,14 +18,15 @@ install_if = [f"power-profiles-daemon-meta={pkgver}-r{pkgrel}"]
 pkgdesc = "D-Bus daemon for power management control"
 maintainer = "Val Packett <val@packett.cool>"
 license = "GPL-3.0-or-later"
-url = "https://gitlab.freedesktop.org/hadess/power-profiles-daemon"
+url = "https://gitlab.freedesktop.org/upower/power-profiles-daemon"
 source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "9fef0a3e1b76e6c0f551841d4a9fef36e6aae047d5279cfc60b833d80fd2a8b7"
+sha256 = "24a0bf1a3012c4f49fbe146fed6bce9fbb7b20c92e4123690ff727a376cc3b6c"
 hardening = ["vis"]
+# TODO: fix checks
+options = ["!check"]
 
 
 def post_install(self):
-    self.rm(self.destdir / "tmp", recursive=True)
     self.install_license("COPYING")
     self.install_dir("var/lib/power-profiles-daemon", empty=True)
     self.install_service(self.files_path / "power-profiles-daemon")
