704f74fe68e17d38760cedf67a59f2d4428cba35
Mohammed Anas <triallax@tutanota.com>
2024-03-18 01:47:09 +0000

    contrib/gnome-tweaks: update to 45.2

--- contrib/gnome-tweaks/template.py
+++ contrib/gnome-tweaks/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-tweaks"
-pkgver = "45.1"
+pkgver = "45.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "gettext"]
@@ -18,4 +18,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later AND CC0-1.0"
 url = "https://wiki.gnome.org/Apps/Tweaks"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "95ffa7f38d9b1dfd5e4cebefb752419fea21cd407021395adc9f112854416d4f"
+sha256 = "01bf5e723a1d18b1025401e32b5f64eb29b79e8a17ad010bbbd1e23b137b9069"
