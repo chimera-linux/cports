c015b66489d2c1a90bb910413dadcbafa376f3e4
psykose <alice@ayaya.dev>
2024-03-18 17:07:06 +0000

    contrib/go-md2man: update to 2.0.4

--- contrib/go-md2man/template.py
+++ contrib/go-md2man/template.py
@@ -1,6 +1,6 @@
 pkgname = "go-md2man"
-pkgver = "2.0.3"
-pkgrel = 1
+pkgver = "2.0.4"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Markdown to manpage converter"
@@ -8,14 +8,9 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://github.com/cpuguy83/go-md2man"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "7ca3a04bb4ab83387538235decc42a535097a05d2fb9f2266d0c47b33119501f"
+sha256 = "b0a4c7c077ede56967deef6ab7e7696c0f46124b0b3360fd05564ec5a536f11f"
 # objcopy fails on ppc
 options = ["!debug"]
-
-
-def post_extract(self):
-    # delete stray incomplete vendor dir
-    self.rm("vendor/", recursive=True)
 
 
 def post_build(self):
