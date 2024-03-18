2776a3ebe234a09c60b4f3c1ba680e7e284a6590
Mohammed Anas <triallax@tutanota.com>
2024-03-18 14:54:39 +0000

    main/npth: update to 1.7

--- main/npth/template.py
+++ main/npth/template.py
@@ -1,19 +1,16 @@
 pkgname = "npth"
-pkgver = "1.6"
+pkgver = "1.7"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 pkgdesc = "New portable threads library"
 maintainer = "eater <=@eater.me>"
 license = "LGPL-2.1-or-later"
 url = "https://gnupg.org/software/npth/index.html"
 source = f"https://gnupg.org/ftp/gcrypt/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "1393abd9adcf0762d34798dc34fdcf4d0d22a8410721e76f1e3afcd1daa4e2d1"
+sha256 = "8589f56937b75ce33b28d312fccbf302b3b71ec3f3945fde6aaa74027914ad05"
 
 
 @subpackage("npth-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
