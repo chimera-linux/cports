4e11c2d1d037a8787fcfde19df6d54a57363e235
psykose <alice@ayaya.dev>
2024-03-18 18:17:49 +0000

    main/python-glad: update to 2.0.6

--- main/python-glad/template.py
+++ main/python-glad/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-glad"
-pkgver = "2.0.5"
+pkgver = "2.0.6"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://glad.dav1d.de"
 source = f"https://github.com/Dav1dde/glad/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "850351f1960f3fed775f0b696d7f17615f306beb56be38a20423284627626df1"
+sha256 = "61a70234dc7da467cceb07fcdd6dec1213d6143a1b6b19ccc5d7b64cc247ea47"
 # unpackaged checkdepends
 options = ["!check"]
 
