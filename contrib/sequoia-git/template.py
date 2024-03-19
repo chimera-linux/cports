9fffc9050531a0a7a6e3bd567198453679d2095b
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-03-19 23:10:55 +0100

    contrib/sequoia-git: new package (0.1.0)

---
+++ contrib/sequoia-git/template.py
@@ -0,0 +1,31 @@
+pkgname = "sequoia-git"
+pkgver = "0.1.0"
+pkgrel = 0
+build_style = "cargo"
+make_check_env = {"TARGET": self.profile().triplet}
+hostmakedepends = ["cargo", "pkgconf"]
+makedepends = [
+    "bzip2-devel",
+    "libgit2-devel",
+    "nettle-devel",
+    "openssl-devel",
+    "rust-std",
+    "sqlite-devel",
+]
+checkdepends = ["git", "gnupg"]
+pkgdesc = "Tool to verify git commit signatures based on a policy"
+maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
+license = "LGPL-2.0-or-later"
+url = "https://gitlab.com/sequoia-pgp/sequoia-git"
+source = f"{url}/-/archive/v{pkgver}/sequoia-git-v{pkgver}.tar.gz"
+sha256 = "c1f7d2647538f3335dab8862a9c4b78bac8c41bb22a3917ec45989849fd035dd"
+options = ["!cross"]
+
+
+def do_install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/sq-git")
+    self.install_man("man/*.1", glob=True)
+
+    self.install_completion("completions/sq-git.bash", "bash", "sq-git")
+    self.install_completion("completions/sq-git.fish", "fish", "sq-git")
+    self.install_completion("completions/_sq-git", "zsh", "sq-git")
---
+++ contrib/sequoia-git/update.py
@@ -0,0 +1,2 @@
+url = "https://gitlab.com/sequoia-pgp/sequoia-git/-/tags"
+pattern = r">v([0-9.]+)</a>"
