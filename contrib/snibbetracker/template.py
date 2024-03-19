5745048dcf0566093429a18309cdac39d6b085df
Erica Z <zerica@callcc.eu>
2024-03-19 06:51:39 +0100

    contrib/snibbetracker: new package (1.1.1)

---
+++ contrib/snibbetracker/template.py
@@ -0,0 +1,19 @@
+pkgname = "snibbetracker"
+pkgver = "1.1.1"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_dir = "res/linux"
+makedepends = ["sdl-devel", "gmake"]
+pkgdesc = "Fakebit music tracker"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "MIT"
+url = "https://github.com/lundstroem/snibbetracker"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "e8d587e70547fbe94de89a95582efcd0ea00cda2632a27082577e520e16dbb78"
+# no tests
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
