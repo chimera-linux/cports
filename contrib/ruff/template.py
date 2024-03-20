195b88a5f67ea6e0319a47a189b6121f1be0216e
psykose <alice@ayaya.dev>
2024-03-20 23:26:19 +0000

    contrib/ruff: new package (0.3.4)

---
+++ contrib/ruff/template.py
@@ -0,0 +1,49 @@
+pkgname = "ruff"
+pkgver = "0.3.4"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "cargo",
+    "python-build",
+    "python-installer",
+    "python-maturin",
+]
+makedepends = ["rust-std"]
+pkgdesc = "Python formatter and linter"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://docs.astral.sh/ruff"
+source = f"https://github.com/astral-sh/ruff/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "2354027cf56ab289bf3d498ea2f80707ef28b47765a50ea951270c325875a58e"
+# generates completions with host bin
+options = ["!cross"]
+
+
+def post_patch(self):
+    from cbuild.util import cargo
+
+    cargo.Cargo(self).vendor()
+    cargo.setup_vendor(self)
+
+
+def post_build(self):
+    for shell in ["bash", "fish", "zsh"]:
+        with open(self.cwd / f"ruff.{shell}", "w") as f:
+            self.do(
+                "./target/release/ruff",
+                "--generate-shell-completion",
+                shell,
+                stdout=f,
+            )
+
+
+def do_check(self):
+    from cbuild.util import cargo
+
+    cargo.Cargo(self).check()
+
+
+def post_install(self):
+    for shell in ["bash", "fish", "zsh"]:
+        self.install_completion(f"ruff.{shell}", shell)
+    self.install_license("LICENSE")
