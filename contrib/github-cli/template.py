281a1dde303046eec1f11f4122f52fd43788bc5f
Mohammed Anas <triallax@tutanota.com>
2024-03-21 15:45:00 +0000

    contrib/github-cli: update to 2.46.0

--- contrib/github-cli/template.py
+++ contrib/github-cli/template.py
@@ -1,6 +1,6 @@
 pkgname = "github-cli"
-pkgver = "2.45.0"
-pkgrel = 1
+pkgver = "2.46.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X github.com/cli/cli/v2/internal/build.Version=v{pkgver}",
@@ -15,7 +15,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "MIT"
 url = "https://cli.github.com"
 source = f"https://github.com/cli/cli/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "61363c109487a17fad44dff3a55f0c7dd36c8d6e4d7b630755705bd3aa34a323"
+sha256 = "663871687310c671ecc183a258fa573622e1e972c681982ac79a25c967fd40b2"
 # cross: uses native binary to generate completions
 # debug: fails to split on powerpc
 # check: needs network access
