db1cc94154d937d5a5fe6e6f3ca2d1204efbb3ca
q66 <q66@chimera-linux.org>
2024-03-21 17:14:39 +0100

    contrib/pinentry-bemenu: add default subpackage

--- contrib/pinentry-bemenu/template.py
+++ contrib/pinentry-bemenu/template.py
@@ -1,6 +1,6 @@
 pkgname = "pinentry-bemenu"
 pkgver = "0.13.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
 makedepends = [
@@ -15,3 +15,14 @@ license = "GPL-3.0-or-later"
 url = "https://github.com/t-8ch/pinentry-bemenu"
 source = f"{url}/archive/v{pkgver}.tar.gz"
 sha256 = "db513f0cb2ee81f064da2dca976cb9376857da4bef316d493b347f692521bb40"
+
+
+@subpackage("pinentry-bemenu-default")
+def _def(self):
+    self.depends = [f"{pkgname}={pkgver}-r{pkgrel}"]
+
+    def inst():
+        self.mkdir(self.destdir / "usr/bin", parents=True)
+        self.ln_s("pinentry-bemenu", self.destdir / "usr/bin/pinentry")
+
+    return inst
