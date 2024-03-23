66ae9c1862550977e2773aa627f086a32e29541e
psykose <alice@ayaya.dev>
2024-03-23 07:08:31 +0000

    contrib/python-pytest-mock: update to 3.14.0

--- contrib/python-pytest-mock/template.py
+++ contrib/python-pytest-mock/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pytest-mock"
-pkgver = "3.12.0"
+pkgver = "3.14.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,7 +15,7 @@ maintainer = "Duncan Bellamy <dunk@denkimushi.com>"
 license = "MIT"
 url = "https://pytest-mock.readthedocs.io/en/latest/index.html"
 source = f"$(PYPI_SITE)/p/pytest-mock/pytest-mock-{pkgver}.tar.gz"
-sha256 = "31a40f038c22cad32287bb43932054451ff5583ff094bca6f675df2f8bc1a6e9"
+sha256 = "2719255a1efeceadbc056d6bf3df3d1c5015530fb40cf347c0f9afac88410bd0"
 
 
 def post_install(self):
