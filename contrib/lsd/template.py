89b12cb957519b1b3bff83ed9ba7f7420b1412c0
psykose <alice@ayaya.dev>
2024-03-23 07:05:42 +0000

    contrib/lsd: update to 1.1.0

--- contrib/lsd/template.py
+++ contrib/lsd/template.py
@@ -1,16 +1,16 @@
 pkgname = "lsd"
-pkgver = "1.0.0"
+pkgver = "1.1.0"
 pkgrel = 0
 build_style = "cargo"
-hostmakedepends = ["cargo"]
-makedepends = ["rust-std"]
+hostmakedepends = ["cargo", "pkgconf"]
+makedepends = ["libgit2-devel", "rust-std"]
 checkdepends = ["git"]
 pkgdesc = "Next gen ls command"
 maintainer = "aurelia <git@elia.garden>"
 license = "Apache-2.0"
 url = "https://github.com/lsd-rs/lsd"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "ab34e9c85bc77cfa42b43bfb54414200433a37419f3b1947d0e8cfbb4b7a6325"
+sha256 = "4bbd180deeef2674e55724bb4297ee0442bea956e36f9c4cd2fcca4e82bb4026"
 
 
 def post_install(self):
