044a55a0dc17bb0476cfa376617d4719a8a44a3c
q66 <q66@chimera-linux.org>
2024-03-23 19:03:02 +0100

    contrib/neovim: build with luajit

--- contrib/neovim/template.py
+++ contrib/neovim/template.py
@@ -1,13 +1,11 @@
 # nb: neovim requires either lua5.1 or luaJIT (a mess)
 pkgname = "neovim"
 pkgver = "0.9.5"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 configure_args = [
     "-DCMAKE_BUILD_TYPE=RelWithDebInfo",
-    "-DPREFER_LUA=on",
-    f"-DLUA_INCLUDE_DIR={self.profile().sysroot / 'usr/include/lua5.1'}",
-    f"-DLUA_LIBRARIES={self.profile().sysroot / 'usr/lib/lua/5.1'}",
+    "-DPREFER_LUA=OFF",
 ]
 hostmakedepends = [
     "cmake",
@@ -24,7 +22,6 @@ makedepends = [
     "unibilium-devel",
     "libtermkey-devel",
     "libvterm-devel",
-    "lua5.1-devel",
     "libuv-devel",
     "lua5.1-libluv-devel",
     "msgpack-c-devel",
@@ -39,6 +36,17 @@ sha256 = "fe74369fc30a32ec7a086b1013acd0eacd674e7570eb1acc520a66180c9e9719"
 # hardening: visibility is needed for "nvim --api-info"
 # testing unchecked yet (via "make test", see test/README.md)
 options = ["!check"]
+
+
+match self.profile().arch:
+    case "aarch64" | "ppc64le" | "ppc64" | "x86_64":
+        configure_args += ["-DPREFER_LUA=OFF"]
+        hostmakedepends += ["luajit"]
+        makedepends += ["luajit-devel"]
+    case _:
+        configure_args += ["-DPREFER_LUA=ON"]
+        hostmakedepends += ["lua5.1"]
+        makedepends += ["lua5.1-devel"]
 
 
 def post_install(self):
