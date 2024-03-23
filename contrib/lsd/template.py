825228521d78784630133d44115e1d7007e56241
psykose <alice@ayaya.dev>
2024-03-23 23:20:37 +0000

    contrib/lsd: update to 1.1.1

--- contrib/lsd/template.py
+++ contrib/lsd/template.py
@@ -1,5 +1,5 @@
 pkgname = "lsd"
-pkgver = "1.1.0"
+pkgver = "1.1.1"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo", "pkgconf"]
@@ -10,7 +10,7 @@ maintainer = "aurelia <git@elia.garden>"
 license = "Apache-2.0"
 url = "https://github.com/lsd-rs/lsd"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "4bbd180deeef2674e55724bb4297ee0442bea956e36f9c4cd2fcca4e82bb4026"
+sha256 = "7933e196bf7b164ea8879078f8a8e87381e0c49f71867e0036c82916199aba61"
 
 
 def post_install(self):
