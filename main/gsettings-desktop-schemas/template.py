f99611cdc0f571c954a33544c88477d0b5b9e90e
q66 <q66@chimera-linux.org>
2024-03-24 14:30:14 +0100

    main/gsettings-desktop-schemas: update to 46.0

--- main/gsettings-desktop-schemas/template.py
+++ main/gsettings-desktop-schemas/template.py
@@ -1,5 +1,5 @@
 pkgname = "gsettings-desktop-schemas"
-pkgver = "45.0"
+pkgver = "46.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=true"]
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "365c8d04daf79b38c8b3dc9626349a024f9e4befdd31fede74b42f7a9fbe0ae2"
+sha256 = "493a46a1161b6388d57aa72f632a79ce96c42d5ffbd1d0b00f496ec5876f8575"
 options = ["!cross"]
 
 
