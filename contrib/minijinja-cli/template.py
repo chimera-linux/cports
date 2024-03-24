9d998d6ea9d7985f7fa7ab15c9214fe1f6812da6
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-03-24 15:20:47 +0100

    contrib/minijinja-cli: new package (1.0.15)

---
+++ contrib/minijinja-cli/template.py
@@ -0,0 +1,25 @@
+pkgname = "minijinja-cli"
+pkgver = "1.0.15"
+pkgrel = 0
+build_wrksrc = "minijinja-cli"
+build_style = "cargo"
+# cargo install rebuilds without these args in install to not ship feature at runtime
+make_build_args = ["--features=completions"]
+make_build_env = {
+    "ASSET_OUT_DIR": "assets",
+}
+hostmakedepends = ["cargo", "pkgconf"]
+makedepends = ["rust-std", "zstd-devel"]
+pkgdesc = "Minimal jinja implementation"
+maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
+license = "Apache-2.0"
+url = "https://github.com/mitsuhiko/minijinja"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "df5cca17313a25ef4dfadb5337a7cd24c245327a652fb87e0a6950fc7a7e86e7"
+
+
+def post_install(self):
+    self.install_man("assets/man/minijinja-cli.1")
+    self.install_completion("assets/completions/minijinja-cli.bash", "bash")
+    self.install_completion("assets/completions/minijinja-cli.fish", "fish")
+    self.install_completion("assets/completions/_minijinja-cli", "zsh")
