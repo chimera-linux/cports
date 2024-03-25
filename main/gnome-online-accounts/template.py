3dd6edee8d1bcdf7914877d8498f1847cd108e5f
q66 <q66@chimera-linux.org>
2024-03-25 02:44:45 +0100

    main/gnome-online-accounts: update to 3.50.0

--- main/gnome-online-accounts/template.py
+++ main/gnome-online-accounts/template.py
@@ -1,12 +1,11 @@
 pkgname = "gnome-online-accounts"
-pkgver = "3.48.1"
+pkgver = "3.50.0"
 pkgrel = 0
 build_style = "meson"
 # TODO: figure out if we can make it work with heimdal
 configure_args = [
     "-Dintrospection=true",
     "-Dvapi=true",
-    "-Dmedia_server=true",
     "-Dman=true",
     "-Dkerberos=false",
 ]
@@ -23,9 +22,10 @@ hostmakedepends = [
 makedepends = [
     "glib-devel",
     "dbus-devel",
-    "gtk+3-devel",
+    "gtk4-devel",
     "webkitgtk-devel",
     "json-glib-devel",
+    "libadwaita-devel",
     "libsecret-devel",
     "libsoup-devel",
     "libxml2-devel",
@@ -36,7 +36,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-online-accounts"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "3ea0c71084bf595cce5caa37ceff2e853d0ec9644bb01fd4643300adb9517f8a"
+sha256 = "b0f64f0dcfbb8885a3a759e1c9de7fb204adb3ce6bd32c30bcf52d5533c06d4e"
 options = ["!cross"]
 
 
