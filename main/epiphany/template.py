8f59c518e7b013778b34be53d9ca552534fe5651
q66 <q66@chimera-linux.org>
2024-03-25 04:03:44 +0100

    main/epiphany: update to 46.0

--- main/epiphany/template.py
+++ main/epiphany/template.py
@@ -1,5 +1,5 @@
 pkgname = "epiphany"
-pkgver = "45.3"
+pkgver = "46.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dunit_tests=disabled"]
@@ -38,4 +38,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://wiki.gnome.org/Apps/Web"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "6c302e9bdd66290cb0e26f62843cd4c435a4955abdbb4f151f07dc80495d6730"
+sha256 = "f4348f2cf51c07c0c106d130172d4d23f2cd4068771e1de007b758ca2ade5660"
