6fc07b63fcb4990e7f829a388b6d9843ce6ca556
Mohammed Anas <triallax@tutanota.com>
2024-03-25 19:47:38 +0000

    main/glibmm: update to 2.80.0

--- main/glibmm/template.py
+++ main/glibmm/template.py
@@ -1,5 +1,5 @@
 pkgname = "glibmm"
-pkgver = "2.78.1"
+pkgver = "2.80.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "glib-devel", "perl", "pkgconf"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gtkmm.org"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "f473f2975d26c3409e112ed11ed36406fb3843fa975df575c22d4cb843085f61"
+sha256 = "539b0a29e15a96676c4f0594541250566c5ca44da5d4d87a3732fa2d07909e4a"
 
 
 @subpackage("glibmm-devel")
