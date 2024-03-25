c72e9a55d77aaea0501e19bb2107d3d53612dfa8
q66 <q66@chimera-linux.org>
2024-03-25 04:01:17 +0100

    main/gnome-user-docs: update to 46.0

--- main/gnome-user-docs/template.py
+++ main/gnome-user-docs/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-user-docs"
-pkgver = "45.5"
+pkgver = "46.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -18,5 +18,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "CC-BY-3.0"
 url = "https://gitlab.gnome.org/GNOME/gnome-user-docs"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "74167fcf42a04d9e5d3cc130d27bc203d87b685ce606a84674df24e1ff719e54"
+sha256 = "8c18ae0c4a0c142e193111b713c959813441bb5d9df25977b13818f6e4ae6291"
 options = ["!splitdoc"]
