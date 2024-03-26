7ae9d1d311bb308808dcb0d03e2d2951ec2776eb
psykose <alice@ayaya.dev>
2024-03-26 02:48:35 +0000

    contrib/icewm: update to 3.4.7

--- contrib/icewm/template.py
+++ contrib/icewm/template.py
@@ -1,5 +1,5 @@
 pkgname = "icewm"
-pkgver = "3.4.6"
+pkgver = "3.4.7"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -35,4 +35,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://ice-wm.org"
 source = f"https://github.com/ice-wm/icewm/archive/{pkgver}.tar.gz"
-sha256 = "cbcc5c271c724310e46806a42e4515eaca7eaff8aa307d209120e66f7b464aaa"
+sha256 = "bca8c417365e37a3e069d5c95365f0280eaddfd3011647ad3a958c0e95f57db2"
