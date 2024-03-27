415dce54c8c149a1b6a64d40af769e6bc0bb6132
psykose <alice@ayaya.dev>
2024-03-27 11:24:29 +0000

    main/python-pyasn1_modules: update to 0.4.0

--- main/python-pyasn1_modules/template.py
+++ main/python-pyasn1_modules/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pyasn1_modules"
-pkgver = "0.3.0"
-pkgrel = 1
+pkgver = "0.4.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://pyasn1.readthedocs.io/en/latest/contents.html"
 source = f"$(PYPI_SITE)/p/pyasn1_modules/pyasn1_modules-{pkgver}.tar.gz"
-sha256 = "5bd01446b736eb9d31512a30d46c1ac3395d676c6f3cafa4c03eb54b9925631c"
+sha256 = "831dbcea1b177b28c9baddf4c6d1013c24c3accd14a1873fffaa6a2e905f17b6"
 
 
 def post_install(self):
