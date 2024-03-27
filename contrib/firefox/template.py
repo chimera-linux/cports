fbefb4d514973c4c1f5dfd7ac92d2f551677fffc
psykose <alice@ayaya.dev>
2024-03-27 08:41:33 +0000

    contrib/firefox: update to 124.0.1

--- contrib/firefox/template.py
+++ contrib/firefox/template.py
@@ -1,5 +1,5 @@
 pkgname = "firefox"
-pkgver = "123.0.1"
+pkgver = "124.0.1"
 pkgrel = 0
 make_cmd = "gmake"
 hostmakedepends = [
@@ -63,7 +63,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-only AND LGPL-2.1-only AND LGPL-3.0-only AND MPL-2.0"
 url = "https://www.mozilla.org/firefox"
 source = f"$(MOZILLA_SITE)/firefox/releases/{pkgver}/source/firefox-{pkgver}.source.tar.xz"
-sha256 = "d5dcb955b65e0f164a90cac0760724486e36e896221b98f244801dfd045d741c"
+sha256 = "ddac16aea855e057ff6be3c143f7155cc20f452e1f45eb6288ff27e9346ab843"
 debug_level = 1  # defatten, especially with LTO
 tool_flags = {
     "LDFLAGS": ["-Wl,-rpath=/usr/lib/firefox", "-Wl,-z,stack-size=2097152"]
