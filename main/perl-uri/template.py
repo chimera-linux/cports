0bc818fe0774fe76f77f89baaee9ae15d01daad7
psykose <alice@ayaya.dev>
2024-03-27 11:22:16 +0000

    main/perl-uri: update to 5.28

--- main/perl-uri/template.py
+++ main/perl-uri/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-uri"
-pkgver = "5.27"
+pkgver = "5.28"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["gmake", "perl"]
@@ -10,6 +10,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/release/URI"
 source = f"$(CPAN_SITE)/URI/URI-{pkgver}.tar.gz"
-sha256 = "11962d8a8a8496906e5d34774affc235a1c95c112d390c0b4171f3e91e9e2a97"
+sha256 = "e7985da359b15efd00917fa720292b711c396f2f9f9a7349e4e7dec74aa79765"
 # missing checkdepends
 options = ["!check"]
