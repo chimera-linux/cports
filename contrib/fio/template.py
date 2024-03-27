012a4316ec8441230bb929cc52855f52220a1826
psykose <alice@ayaya.dev>
2024-03-27 04:54:56 +0000

    contrib/fio: update to 3.37

--- contrib/fio/template.py
+++ contrib/fio/template.py
@@ -1,6 +1,6 @@
 pkgname = "fio"
-pkgver = "3.36"
-pkgrel = 1
+pkgver = "3.37"
+pkgrel = 0
 build_style = "configure"
 configure_args = [
     "--disable-native",
@@ -24,5 +24,5 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-only"
 url = "https://github.com/axboe/fio"
 source = f"https://github.com/axboe/fio/archive/refs/tags/fio-{pkgver}.tar.gz"
-sha256 = "b34b8f3c5cd074c09ea487ffe3f444e95565c214b34a73042f35b00cbaab0e17"
+sha256 = "b59099d42d5c62a8171974e54466a688c8da6720bf74a7f16bf24fb0e51ff92d"
 hardening = ["cfi", "vis"]
