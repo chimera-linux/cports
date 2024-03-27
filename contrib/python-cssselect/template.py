6edc65fc8de1cfd1ece9a4427ac46dff2f0e5d20
psykose <alice@ayaya.dev>
2024-03-27 15:51:11 +0000

    contrib/python-cssselect: new package (1.2.0)

---
+++ contrib/python-cssselect/template.py
@@ -0,0 +1,24 @@
+pkgname = "python-cssselect"
+pkgver = "1.2.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+checkdepends = [
+    "python-lxml",
+    "python-pytest",
+]
+pkgdesc = "Python module for CSS selectors"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-3-Clause"
+url = "https://cssselect.readthedocs.io/en/latest"
+source = f"$(PYPI_SITE)/c/cssselect/cssselect-{pkgver}.tar.gz"
+sha256 = "666b19839cfaddb9ce9d36bfe4c969132c647b92fc9088c4e23f786b30f1b3dc"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
