02040ff98dd5d1b4e147b6e0edb5f2a8c9debeda
psykose <alice@ayaya.dev>
2024-03-29 17:59:45 +0000

    contrib/easyeffects: update to 7.1.6

--- contrib/easyeffects/template.py
+++ contrib/easyeffects/template.py
@@ -1,5 +1,5 @@
 pkgname = "easyeffects"
-pkgver = "7.1.5"
+pkgver = "7.1.6"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Denable-libcpp-workarounds=true"]
@@ -42,6 +42,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/wwmm/easyeffects"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f365c70a5ff750b73c1996e5355109c4f9ea48c68e4420eab189a5ede9af90cc"
-# FIXME: cfi
-hardening = ["vis"]
+sha256 = "7a206fe02472b87a79e02e48dc7c96ce4a1f6dd700c14f1e1502357882d81aa7"
