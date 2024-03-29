02b7f1d442713d18ce2d3c965e65f7bcdbf37413
Evangelos Paterakis <evan@geopjr.dev>
2024-03-29 21:53:28 +0200

    main/libspelling: update repo url

--- main/libspelling/template.py
+++ main/libspelling/template.py
@@ -20,7 +20,7 @@ makedepends = [
 pkgdesc = "Spellcheck library for GTK 4"
 maintainer = "GeopJr <evan@geopjr.dev>"
 license = "LGPL-2.1-or-later"
-url = "https://gitlab.gnome.org/chergert/libspelling"
+url = "https://gitlab.gnome.org/GNOME/libspelling"
 source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "413b22a358e77f2302d15a8fbd3ed4ad8fdecea38dfd5c687af4c567c6b3e15a"
 
--- main/libspelling/update.py
+++ main/libspelling/update.py
@@ -1,3 +1,3 @@
-url = "https://gitlab.gnome.org/chergert/libspelling/-/tags"
+url = "https://gitlab.gnome.org/GNOME/libspelling/-/tags"
 pattern = r"/tags/([\d.]+)\">"
 ignore = ["*.99*"]
