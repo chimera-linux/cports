5bc2601a0a2620912f8e107941be040ae783a86e
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-04-01 11:47:35 +0200

    contrib/cargo-crev: new package (0.25.6)

---
+++ contrib/cargo-crev/template.py
@@ -0,0 +1,17 @@
+pkgname = "cargo-crev"
+pkgver = "0.25.6"
+pkgrel = 0
+build_wrksrc = "cargo-crev"
+build_style = "cargo"
+hostmakedepends = ["cargo", "pkgconf"]
+makedepends = ["openssl-devel", "libgit2-devel"]
+pkgdesc = "Cryptographically verifiable code review system for cargo"
+maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
+license = "MPL-2.0 OR MIT OR Apache-2.0"
+url = "https://github.com/crev-dev/cargo-crev"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "8a8b737aff1361677e3733133944728871ccf7ac00ea15b32f9d0ef6d5814f62"
+
+
+def post_install(self):
+    self.install_license("LICENSE-MIT")
