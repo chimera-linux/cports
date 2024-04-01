d58164172e0e87d732232d4e4f3857228bb22698
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-04-01 14:08:34 +0200

    contrib/tokei: new package (12.1.2)

---
+++ contrib/tokei/template.py
@@ -0,0 +1,28 @@
+pkgname = "tokei"
+pkgver = "12.1.2"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo"]
+makedepends = ["rust-std"]
+pkgdesc = "CLI for counting lines of code with stats per language"
+maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
+license = "Apache-2.0 OR MIT"
+url = "https://github.com/XAMPPRocky/tokei"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "81ef14ab8eaa70a68249a299f26f26eba22f342fb8e22fca463b08080f436e50"
+
+
+def do_prepare(self):
+    # we patch the lockfile so vendor after patch
+    pass
+
+
+def post_patch(self):
+    from cbuild.util import cargo
+
+    self.cargo.vendor()
+    cargo.setup_vendor(self)
+
+
+def post_install(self):
+    self.install_license("LICENCE-MIT")
