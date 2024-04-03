0e8caa2218543ee3c4ace1f8811f3cf26207200b
Mohammed Anas <triallax@tutanota.com>
2024-04-03 13:35:55 +0100

    main/libical: update to 3.0.18

--- main/libical/template.py
+++ main/libical/template.py
@@ -1,29 +1,30 @@
 pkgname = "libical"
-pkgver = "3.0.17"
-pkgrel = 1
+pkgver = "3.0.18"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DICAL_BUILD_DOCS=false",
     "-DGOBJECT_INTROSPECTION=true",
     "-DICAL_GLIB_VAPI=true",
+    "-DLIBICAL_BUILD_EXAMPLES=false",
 ]
 make_check_args = ["-E", "(icalrecurtest|icalrecurtest_r)"]
 hostmakedepends = [
     "cmake",
-    "ninja",
-    "pkgconf",
     "gettext",
     "glib-devel",
-    "libxml2-devel",
-    "perl",
-    "vala",
     "gobject-introspection",
+    "libxml2-devel",
+    "ninja",
+    "perl",
+    "pkgconf",
+    "vala",
 ]
 makedepends = [
     "glib-devel",
+    "icu-devel",
     "libxml2-devel",
     "vala-devel",
-    "icu-devel",
 ]
 checkdepends = ["python-gobject"]
 pkgdesc = "Open source implementation of iCalendar protocols and formats"
@@ -31,7 +32,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MPL-2.0 OR LGPL-2.1-only"
 url = "https://libical.github.io/libical"
 source = f"https://github.com/libical/libical/archive/v{pkgver}.tar.gz"
-sha256 = "bcda9a6db6870240328752854d1ea475af9bbc6356e6771018200e475e5f781b"
+sha256 = "72b7dc1a5937533aee5a2baefc990983b66b141dd80d43b51f80aced4aae219c"
 options = ["!cross"]
 
 
