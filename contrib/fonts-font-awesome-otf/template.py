05cbd6ec9fd62f7460902a0f47343745179777bd
Mohammed Anas <triallax@tutanota.com>
2024-04-03 13:14:07 +0100

    contrib/fonts-font-awesome-otf: update to 6.5.2

--- contrib/fonts-font-awesome-otf/template.py
+++ contrib/fonts-font-awesome-otf/template.py
@@ -1,12 +1,12 @@
 pkgname = "fonts-font-awesome-otf"
-pkgver = "6.5.1"
+pkgver = "6.5.2"
 pkgrel = 0
 pkgdesc = "Iconic font set"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "OFL-1.1"
 url = "https://fontawesome.com"
 source = f"https://github.com/FortAwesome/Font-Awesome/releases/download/{pkgver}/fontawesome-free-{pkgver}-desktop.zip"
-sha256 = "88d13abdade8b24b5fbdf6fe7d3ee55507d2827be91990a066ed96b8a2a58003"
+sha256 = "6392bc956eb3d391c9d7a14e891ce8010226ffc0c75f1338db126f13cb9cb8f4"
 
 
 def do_install(self):
