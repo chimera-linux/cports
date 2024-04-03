351ddd21e2e7c6cf37590c6e431b8fdabfa7101a
psykose <alice@ayaya.dev>
2024-04-03 22:22:51 +0000

    contrib/python-pyqt-builder: update to 1.16.0

--- contrib/python-pyqt-builder/template.py
+++ contrib/python-pyqt-builder/template.py
@@ -1,11 +1,11 @@
 pkgname = "python-pyqt-builder"
-pkgver = "1.15.4"
+pkgver = "1.16.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
-    "python-setuptools",
+    "python-setuptools_scm",
     "python-wheel",
 ]
 pkgdesc = "PEP517 backend for PyQt projects"
@@ -13,10 +13,10 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "custom:sip"
 url = "https://www.riverbankcomputing.com/software/pyqt-builder"
 source = f"$(PYPI_SITE)/P/PyQt-builder/PyQt-builder-{pkgver}.tar.gz"
-sha256 = "39f8c75db17d9ce17cb6bbf3df1650b5cebc1ea4e5bd73843d21cc96612b2ae1"
+sha256 = "47bbd2cfa5430020108f9f40301e166cbea98b6ef3e53953350bdd4c6b31ab18"
 # no tests
 options = ["!check"]
 
 
 def post_install(self):
-    self.install_license("LICENSE")
+    self.install_license("pyqtbuild/bundle/qt_wheel_distinfo/LICENSE")
