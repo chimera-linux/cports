b8c50d8188823cacf173b9eb6fa96c5abad6ee47
Erica Z <zerica@callcc.eu>
2024-04-04 15:10:48 +0200

    main/libgcrypt: fix cross

--- main/libgcrypt/template.py
+++ main/libgcrypt/template.py
@@ -7,7 +7,7 @@ configure_args = [
     "--without-capabilities",
     "ac_cv_sys_symbol_underscore=no",
 ]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["pkgconf", "libgpg-error-devel"]
 makedepends = ["libgpg-error-devel"]
 pkgdesc = "GNU cryptographic library"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -16,6 +16,11 @@ url = "https://www.gnupg.org"
 source = f"{url}/ftp/gcrypt/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
 sha256 = "8b0870897ac5ac67ded568dcfadf45969cfa8a6beb0fd60af2a9eadc2a3272aa"
 options = ["linkundefver"]
+
+
+def init_configure(self):
+    with self.profile("host"):
+        self.make_build_args += ["CC_FOR_BUILD=" + self.get_tool("CC")]
 
 
 @subpackage("libgcrypt-devel")
