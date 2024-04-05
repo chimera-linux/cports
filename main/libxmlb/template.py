d54ff9b2e3601531dc0eacb255ad0e3642f96e06
psykose <alice@ayaya.dev>
2024-04-05 04:00:44 +0000

    main/libxmlb: update to 0.3.16

--- main/libxmlb/template.py
+++ main/libxmlb/template.py
@@ -1,10 +1,10 @@
 pkgname = "libxmlb"
-pkgver = "0.3.15"
+pkgver = "0.3.16"
 pkgrel = 0
 build_style = "meson"
 # tests require some file to exist in /tmp? so it fails
-configure_args = ["-Dtests=false", "-Dgtkdoc=false"]
-hostmakedepends = ["pkgconf", "meson", "cmake", "gobject-introspection"]
+configure_args = ["-Dgtkdoc=false"]
+hostmakedepends = ["pkgconf", "meson", "gobject-introspection"]
 makedepends = ["xz-devel", "zstd-devel"]
 checkdepends = ["shared-mime-info"]
 depends = ["shared-mime-info"]
@@ -13,7 +13,7 @@ maintainer = "eater <=@eater.me>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/hughsie/libxmlb"
 source = f"https://github.com/hughsie/libxmlb/releases/download/{pkgver}/libxmlb-{pkgver}.tar.xz"
-sha256 = "4cf605965d0e669db737da6443314664ea471807f2719a84550f2490670beea9"
+sha256 = "8007d39eefaa047d23e323523ae3c4da5fca77543b0fc4e188f7a3cd28872ff4"
 options = ["!cross"]
 
 
