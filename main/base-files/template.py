a89ab81d8d5b7bef7b0e5d79f30cedf32505f595
Matthias Nyman <natthias@proton.me>
2024-04-05 11:24:57 +0200

    main/base-files: add network group

--- main/base-files/template.py
+++ main/base-files/template.py
@@ -1,7 +1,7 @@
 pkgname = "base-files"
 _iana_ver = "20240305"
 pkgver = f"0.1.{_iana_ver}"
-pkgrel = 0
+pkgrel = 1
 # highest priority dir owner
 replaces_priority = 65535
 pkgdesc = "Chimera Linux base system files"
