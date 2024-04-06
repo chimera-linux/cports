9eedd451a64032fbb6c1ee443dd2318b060c0d4b
psykose <alice@ayaya.dev>
2024-04-06 15:19:05 +0000

    contrib/swayimg: update to 2.2

--- contrib/swayimg/template.py
+++ contrib/swayimg/template.py
@@ -1,14 +1,18 @@
 pkgname = "swayimg"
-pkgver = "2.1"
-pkgrel = 1
+pkgver = "2.2"
+pkgrel = 0
 build_style = "meson"
+configure_args = [
+    f"-Dversion={pkgver}",
+    "-Dzsh=enabled",
+]
 hostmakedepends = [
-    "bash-completion",
     "meson",
     "pkgconf",
     "wayland-protocols",
 ]
 makedepends = [
+    "bash-completion",
     "fontconfig-devel",
     "json-c-devel",
     "libavif-devel",
@@ -28,7 +32,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://github.com/artemsen/swayimg"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "d82fb8e75cdabf470677797444ec19b00c83e0e06d80be774727194404624e7e"
+sha256 = "fc9cf24def4459570d2cd7fcd142e7e2228fc8ca77892c773b2061de778ac5c3"
 
 
 def post_install(self):
