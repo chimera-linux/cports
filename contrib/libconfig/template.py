57d9123f2e25302c9f219750d05c652570f9b36e
q66 <q66@chimera-linux.org>
2024-04-06 16:09:11 +0200

    contrib/libconfig: new package (1.7.3)

---
+++ contrib/libconfig/template.py
@@ -0,0 +1,25 @@
+pkgname = "libconfig"
+pkgver = "1.7.3"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--disable-examples"]
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "byacc",
+    "flex",
+    "libtool",
+    "pkgconf",
+    "texinfo",
+]
+pkgdesc = "Configuration file library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.1-or-later"
+url = "https://hyperrealm.com/libconfig/libconfig.html"
+source = f"https://github.com/hyperrealm/libconfig/archive/v{pkgver}.tar.gz"
+sha256 = "68757e37c567fd026330c8a8449aa5f9cac08a642f213f2687186b903bd7e94e"
+
+
+@subpackage("libconfig-devel")
+def _dev(self):
+    return self.default_devel()
