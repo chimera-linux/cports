cbce7f51157ede34cd5a9ed2b9a0c63bce3c0652
q66 <q66@chimera-linux.org>
2024-04-06 06:22:47 +0200

    user/kde1-kdebase: new package (1.1.2)

---
+++ user/kde1-kdebase/template.py
@@ -0,0 +1,47 @@
+pkgname = "kde1-kdebase"
+pkgver = "1.1.2"
+pkgrel = 0
+_gitrev = "4987e047002f9b8364c16fa0e6650717c24bcc7e"
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "elogind-devel",
+    "kde1-kdelibs-devel",
+    "libjpeg-turbo-devel",
+    "libpulse-devel",
+    "libxau-devel",
+    "libxdmcp-devel",
+    "libxpm-devel",
+    "libxscrnsaver-devel",
+    "libxt-devel",
+    "linux-pam-devel",
+    "mesa-devel",
+    "ncurses-devel",
+    "openssl-devel",
+    "qt1-devel",
+]
+pkgdesc = "KDE1 base applications"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-only"
+url = "https://github.com/KDE/kde1-kdebase"
+source = f"{url}/archive/{_gitrev}.tar.gz"
+sha256 = "480b847c73cf00bed6c8f6095e73bf61e41e6ae38d9fb0e1d97d3b149521107d"
+hardening = ["!int", "!format"]
+options = ["!lto"]
+
+tool_flags = {
+    "CFLAGS": [
+        "-Wno-deprecated-non-prototype",
+    ],
+    "CXXFLAGS": [
+        "-std=gnu++98",
+        "-Wno-format-security",
+        "-Wno-c++11-extensions",
+        "-Wno-c++11-compat-deprecated-writable-strings",
+    ],
+}
