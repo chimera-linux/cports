7c4ae0877b29e8333d2661a6d1ec66b2e136515d
ttyyls <contact@behri.org>
2024-04-08 01:32:01 +0000

    contrib/oksh: update to 7.5

--- contrib/oksh/template.py
+++ contrib/oksh/template.py
@@ -1,24 +1,19 @@
 pkgname = "oksh"
-pkgver = "7.4"
+pkgver = "7.5"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
+make_dir = "."
 makedepends = ["ncurses-devel"]
 pkgdesc = "Portable OpenBSD ksh, based on the Public Domain Korn Shell (pdksh)"
 maintainer = "ttyyls <contact@behri.org>"
 license = "custom:none"
 url = "https://github.com/ibara/oksh"
 source = f"{url}/releases/download/{pkgname}-{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "be9a8d457bf373bd04618074c41b46f4edec2ba1c57a58be881d60eaa6628596"
+sha256 = "40b895c3f8e9311bfe2b230e9b3786712550ef488ced33bfd7cd3f89fceeed5d"
 hardening = ["vis", "cfi"]
 # There are no tests
 options = ["!check"]
-
-
-def do_install(self):
-    self.install_bin("build/oksh")
-    self.install_man("ksh.1")
-    self.install_man("oksh.1")
 
 
 def post_install(self):
