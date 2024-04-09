94563efca248780de006de37c10b8f5fef86746d
Sid Pranjale <mail@sidonthe.net>
2024-04-09 21:02:54 +0530

    contrib/intel-undervolt: new package (1.7)

---
+++ contrib/intel-undervolt/template.py
@@ -0,0 +1,26 @@
+pkgname = "intel-undervolt"
+pkgver = "1.7"
+pkgrel = 0
+archs = ["x86_64"]
+build_style = "makefile"
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "pkgconf"]
+makedepends = ["elogind-devel"]
+pkgdesc = "Intel CPU undervolting tool"
+maintainer = "Sid Pranjale <mail@sidonthe.net>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/kitsunyan/intel-undervolt"
+source = f"{url}/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "29a7ebaee4830d65d0b5cefa6d497887d4f23f34659876dfe944f3a020cf33ff"
+hardening = ["vis", "cfi"]
+# no tests
+options = ["!check"]
+
+
+def do_configure(self):
+    self.do("./configure", "--enable-elogind")
+
+
+def post_install(self):
+    self.install_service(self.files_path / "intel-undervolt")
+    self.install_service(self.files_path / "intel-undervolt-loop")
