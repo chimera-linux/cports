e1ce41bab075354590828de96ffc6612e1e7831c
psykose <alice@ayaya.dev>
2024-04-09 23:35:29 +0000

    main/gst-libav: update to 1.24.2

--- main/gst-libav/template.py
+++ main/gst-libav/template.py
@@ -1,14 +1,14 @@
 pkgname = "gst-libav"
-pkgver = "1.24.1"
+pkgver = "1.24.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddefault_library=shared"]
 hostmakedepends = ["meson", "pkgconf"]
 makedepends = [
-    "gstreamer-devel",
-    "gst-plugins-base-devel",
-    "orc-devel",
     "ffmpeg-devel",
+    "gst-plugins-base-devel",
+    "gstreamer-devel",
+    "orc-devel",
 ]
 depends = ["orc", f"gst-plugins-base~{pkgver}"]
 pkgdesc = "GStreamer FFmpeg plugin"
@@ -16,6 +16,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "549b7821fb5a4c866d6197888496015106cc6ec96ffb12a64e6efa91aa6562fd"
+sha256 = "962838648e14ce8a78322ab16f84c7d84d86a6d7bebb6574ac05dea84a7c7c9b"
 # FIXME int
 hardening = ["!int"]
