9e8134a4d2b204f0cb6b8b336e89e0072bcea2bc
psykose <alice@ayaya.dev>
2024-04-10 10:55:26 +0000

    main/libdvdread: update to 6.1.3

--- main/libdvdread/template.py
+++ main/libdvdread/template.py
@@ -1,21 +1,18 @@
 pkgname = "libdvdread"
-pkgver = "6.1.2"
+pkgver = "6.1.3"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-libdvdcss"]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 makedepends = ["libdvdcss-devel"]
 pkgdesc = "DVD access library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://www.videolan.org/developers/libdvdread.html"
 source = f"https://download.videolan.org/pub/videolan/{pkgname}/{pkgver}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "cc190f553758ced7571859e301f802cb4821f164d02bfacfd320c14a4e0da763"
+sha256 = "ce35454997a208cbe50e91232f0e73fb1ac3471965813a13b8730a8f18a15369"
 
 
 @subpackage("libdvdread-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
