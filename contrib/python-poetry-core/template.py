33c33267f9e601c567496bac725b5458130ec686
psykose <alice@ayaya.dev>
2024-04-10 03:47:51 +0000

    contrib/python-poetry-core: new package (1.9.0)

---
+++ contrib/python-poetry-core/template.py
@@ -0,0 +1,34 @@
+pkgname = "python-poetry-core"
+pkgver = "1.9.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = [
+    "python-fastjsonschema",
+    "python-lark-parser",
+    "python-packaging",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "PEP517 build backend for Poetry"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/python-poetry/poetry-core"
+source = f"$(PYPI_SITE)/p/poetry_core/poetry_core-{pkgver}.tar.gz"
+sha256 = "fa7a4001eae8aa572ee84f35feb510b321bd652e5cf9293249d62853e1f935a2"
+# FIXME
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+    # prune vendored modules
+    self.rm(
+        self.destdir / "usr/lib/python*/site-packages/poetry/core/_vendor",
+        glob=True,
+        recursive=True,
+    )
