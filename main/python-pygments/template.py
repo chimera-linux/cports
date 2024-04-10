4254976c06aad0fc721c826bbe83914480a5b503
Mohammed Anas <triallax@tutanota.com>
2024-04-10 13:03:42 +0100

    main/python-pygments: update to 2.17.2

--- main/python-pygments/template.py
+++ main/python-pygments/template.py
@@ -1,20 +1,19 @@
 pkgname = "python-pygments"
-pkgver = "2.16.1"
-pkgrel = 1
+pkgver = "2.17.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
+    "python-hatchling",
     "python-installer",
-    "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python"]
 pkgdesc = "Generic syntax highlighter written in Python"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://pygments.org"
-source = f"$(PYPI_SITE)/P/Pygments/Pygments-{pkgver}.tar.gz"
-sha256 = "1daff0494820c69bc8941e407aa20f577374ee88364ee10a98fdbe0aece96e29"
+source = f"$(PYPI_SITE)/p/pygments/pygments-{pkgver}.tar.gz"
+sha256 = "da46cec9fd2de5be3a8a784f434e4c4ab670b4ff54d605c4c2717e9d49c4c367"
 # dependency of pytest
 options = ["!check"]
 
