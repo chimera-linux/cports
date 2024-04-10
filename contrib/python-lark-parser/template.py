2217856f9066756fd67c6ac784708242a4fd6675
psykose <alice@ayaya.dev>
2024-04-10 03:46:38 +0000

    contrib/python-lark-parser: new package (1.1.9)

---
+++ contrib/python-lark-parser/template.py
@@ -0,0 +1,21 @@
+pkgname = "python-lark-parser"
+pkgver = "1.1.9"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "Parsing toolkit for Python"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/lark-parser/lark"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "32f5f3e1418df3a9032d1a8f9bea62427319e28733ac463f5f37d311b1d99c01"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
