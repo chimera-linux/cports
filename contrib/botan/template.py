ab33641ba41a25460e1aa919be8f436b906e9f43
psykose <alice@ayaya.dev>
2024-04-10 14:35:00 +0000

    contrib/botan: new package (3.4.0)

---
+++ contrib/botan/template.py
@@ -0,0 +1,45 @@
+pkgname = "botan"
+pkgver = "3.4.0"
+pkgrel = 0
+build_style = "configure"
+configure_script = "./configure.py"
+configure_args = [
+    "--prefix=/usr",
+    "--with-boost",
+    "--with-bzip2",
+    "--with-lzma",
+    "--with-os-feature=getrandom",
+    "--with-sqlite3",
+    "--with-zlib",
+]
+configure_gen = []
+make_cmd = "gmake"
+hostmakedepends = [
+    "gmake",
+    "pkgconf",
+    "python",
+]
+makedepends = [
+    "boost-devel",
+    "bzip2-devel",
+    "sqlite-devel",
+    "xz-devel",
+    "zlib-devel",
+]
+pkgdesc = "Crypto and TLS for C++"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-2-Clause"
+url = "https://botan.randombit.net"
+source = f"https://botan.randombit.net/releases/Botan-{pkgver}.tar.xz"
+sha256 = "71843afcc0a2c585f8f33fa304f0b58ae4b9c5d8306f894667b3746044277557"
+# FIXME: cfi
+hardening = ["vis"]
+
+
+def post_install(self):
+    self.install_license("license.txt")
+
+
+@subpackage("botan-devel")
+def _devel(self):
+    return self.default_devel()
