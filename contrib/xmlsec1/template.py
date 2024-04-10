950e249943b3bd8bbc6917b3aae9cbe5dd2bf7b0
psykose <alice@ayaya.dev>
2024-04-10 03:06:04 +0000

    contrib/xmlsec1: update to 1.3.4

--- contrib/xmlsec1/template.py
+++ contrib/xmlsec1/template.py
@@ -1,24 +1,24 @@
 pkgname = "xmlsec1"
-pkgver = "1.3.3"
+pkgver = "1.3.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-docs"]
 hostmakedepends = ["pkgconf", "automake", "libtool", "gnutls-devel"]
 makedepends = [
-    "libxslt-devel",
     "gnutls-devel",
     "libgcrypt-devel",
-    "nspr-devel",
-    "nss-devel",
     "libgcrypt-devel",
     "libltdl-devel",
+    "libxslt-devel",
+    "nspr-devel",
+    "nss-devel",
 ]
 pkgdesc = "XML Security Library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.aleksey.com/xmlsec"
 source = f"{url}/download/{pkgname}-{pkgver}.tar.gz"
-sha256 = "ab5b9a9ffd6960f46f7466d9d91f174ec37e8c31989237ba6b9eacdd816464f2"
+sha256 = "45ad9078d41ae76844ad2f8651600ffeec0fdd128ead988a8d69e907c57aee75"
 # broken tests build + tests reach internet
 options = ["!cross", "!check"]
 
