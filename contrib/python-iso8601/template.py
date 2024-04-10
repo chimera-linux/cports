c7c3ba6d4155ef79da1ab87ed67be86f605f1e13
psykose <alice@ayaya.dev>
2024-04-10 03:48:35 +0000

    contrib/python-iso8601: new package (2.1.0)

---
+++ contrib/python-iso8601/template.py
@@ -0,0 +1,24 @@
+pkgname = "python-iso8601"
+pkgver = "2.1.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-poetry-core",
+]
+checkdepends = [
+    "python-hypothesis",
+    "python-pytest",
+    "python-pytz",
+]
+pkgdesc = "ISO 8601 parser for Python"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/micktwomey/pyiso8601"
+source = f"$(PYPI_SITE)/i/iso8601/iso8601-{pkgver}.tar.gz"
+sha256 = "6b1d3829ee8921c4301998c909f7829fa9ed3cbdac0d3b16af2d743aed1ba8df"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
