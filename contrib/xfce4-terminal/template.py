490b52ba1429f6a142752891d5beed51f547bba1
Mohammed Anas <triallax@tutanota.com>
2024-04-10 12:54:42 +0100

    contrib/xfce4-terminal: update to 1.1.3

--- contrib/xfce4-terminal/template.py
+++ contrib/xfce4-terminal/template.py
@@ -1,18 +1,16 @@
 pkgname = "xfce4-terminal"
-pkgver = "1.0.4"
+pkgver = "1.1.3"
 pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
 hostmakedepends = ["pkgconf", "gmake", "intltool"]
 makedepends = ["exo-devel", "libxfce4ui-devel", "vte-gtk3-devel"]
-depends = [
-    "gsettings-desktop-schemas",
-]
+depends = ["gsettings-desktop-schemas"]
 pkgdesc = "Xfce terminal emulator"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://xfce.org"
 source = f"https://archive.xfce.org/src/apps/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "78e55957af7c6fc1f283e90be33988661593a4da98383da1b0b54fdf6554baf4"
+sha256 = "214dd588d441b69f816009682a3bb4652cc19bed7ea64b612a12f097417b3d45"
 
 configure_gen = []
---
+++ contrib/xfce4-terminal/update.py
@@ -0,0 +1,2 @@
+pkgname = "xfce4-terminal-xfce4-terminal"
+url = "https://gitlab.xfce.org/apps/xfce4-terminal/-/tags"
