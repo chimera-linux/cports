9d8a8f2d4b8b19b5381a7a4b43936f4d53d195d7
Mohammed Anas <triallax@tutanota.com>
2024-04-10 12:35:42 +0100

    contrib/gnome-terminal: update to 3.52.0

--- contrib/gnome-terminal/template.py
+++ contrib/gnome-terminal/template.py
@@ -1,10 +1,7 @@
 pkgname = "gnome-terminal"
-pkgver = "3.44.1"
+pkgver = "3.52.0"
 pkgrel = 0
 build_style = "meson"
-configure_args = [
-    "-Dnautilus_extension=false",
-]
 hostmakedepends = [
     "meson",
     "pkgconf",
@@ -15,30 +12,37 @@ hostmakedepends = [
     "itstool",
 ]
 makedepends = [
-    "gtk+3-devel",
-    "vte-gtk3-devel",
-    "dconf-devel",
     "glib-devel",
-    "pcre2-devel",
     "gsettings-desktop-schemas-devel",
+    "gtk+3-devel",
+    "libhandy-devel",
     "libuuid-devel",
-    "gnome-shell",  # "nautilus-devel",
+    "nautilus-devel",
+    "pcre2-devel",
+    "vte-gtk3-devel",
 ]
 pkgdesc = "GNOME terminal emulator"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/Terminal"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "fb6f20ee1ff231a9aaedab13d5dc6e5a64c955711224848b790086e88959d37b"
-# FIXME cfi
-hardening = ["vis", "!cfi"]
-options = ["!cross"]
+source = f"https://gitlab.gnome.org/GNOME/{pkgname}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "7a90e9d21846e2181200708d418d5d2e767dac9d1b6b01aca2b0f496146062bf"
+# Upstream claims "LTO very much NOT supported"
+# https://gitlab.gnome.org/GNOME/gnome-terminal/-/blob/09c8b31168460c325ac00820759d6eefdf3957ab/meson.build#L226
+options = ["!cross", "!lto"]
 
 
-@subpackage("nautilus-gnome-terminal-extension", False)
-def _devel(self):
+def post_install(self):
+    self.rm(self.destdir / "usr/lib/systemd", recursive=True)
+
+
+@subpackage("nautilus-gnome-terminal-extension")
+def _nautilus_extension(self):
     self.pkgdesc = "GNOME terminal extension for Nautilus"
     self.depends += [f"{pkgname}={pkgver}-r{pkgrel}"]
     self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}", "nautilus"]
 
-    return ["usr/lib/nautilus"]
+    return [
+        "usr/lib/nautilus",
+        "usr/share/metainfo/org.gnome.Terminal.Nautilus.metainfo.xml",
+    ]
