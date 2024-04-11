ffc52fa2ee50fae630f59d7802bd2a4890cc6e0c
psykose <alice@ayaya.dev>
2024-04-11 05:08:42 +0000

    main/cpupower: include x86_64-specific utilities too
    debian also places these in 'cpupower'

--- main/cpupower/template.py
+++ main/cpupower/template.py
@@ -1,6 +1,6 @@
 pkgname = "cpupower"
 pkgver = "6.8.4"
-pkgrel = 0
+pkgrel = 1
 build_style = "makefile"
 make_cmd = "gmake"
 make_build_target = "cpupower"
@@ -23,8 +23,14 @@ make_build_args = [
 ]
 make_install_target = "cpupower_install"
 make_install_args = list(make_build_args)
+make_use_env = True
 hostmakedepends = ["gmake"]
-makedepends = ["linux-headers", "pciutils-devel"]
+makedepends = [
+    "libcap-devel",
+    "libnl-devel",
+    "linux-headers",
+    "pciutils-devel",
+]
 pkgdesc = "Linux CPU power management tools"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
@@ -33,6 +39,18 @@ source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[:pkgver.find('.')]}.
 sha256 = "d5dec495fc00605fa9e04114df547fbc92b33d9ea7a4a2b7073c589590e79e63"
 # nope
 options = ["!check"]
+
+if self.profile().arch == "x86_64":
+    make_build_args += [
+        "intel-speed-select",
+        "turbostat",
+        "x86_energy_perf_policy",
+    ]
+    make_install_args += [
+        "intel-speed-select_install",
+        "turbostat_install",
+        "x86_energy_perf_policy_install",
+    ]
 
 
 @subpackage("cpupower-devel")
