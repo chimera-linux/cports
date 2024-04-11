7e90aceecbb77eaf5c099966bf6b285ea5d112da
q66 <q66@chimera-linux.org>
2024-04-11 14:37:45 +0200

    main/autooconf: update to 2.72

--- main/autoconf/template.py
+++ main/autoconf/template.py
@@ -1,8 +1,9 @@
 pkgname = "autoconf"
-pkgver = "2.71"
-pkgrel = 1
+pkgver = "2.72"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_env = {"M4": "/usr/bin/gm4"}
+configure_gen = []
 hostmakedepends = ["perl", "gm4", "texinfo"]
 depends = ["cmd:awk!base-files", "gm4", "perl"]
 pkgdesc = "Generates automatic source code configuration scripts"
@@ -10,6 +11,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/autoconf"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "431075ad0bf529ef13cb41e9042c542381103e80015686222b8a9d4abef42a1c"
-
-configure_gen = []
+sha256 = "afb181a76e1ee72832f6581c0eddf8df032b83e2e0239ef79ebedc4467d92d6e"
