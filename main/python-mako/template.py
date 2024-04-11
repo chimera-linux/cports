3fd5f4e633f1b386ace92caa562f57814097dd54
psykose <alice@ayaya.dev>
2024-04-11 05:29:26 +0000

    main/python-mako: update to 1.3.3

--- main/python-mako/template.py
+++ main/python-mako/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-mako"
-pkgver = "1.3.2"
+pkgver = "1.3.3"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.makotemplates.org"
 source = f"$(PYPI_SITE)/M/Mako/Mako-{pkgver}.tar.gz"
-sha256 = "2a0c8ad7f6274271b3bb7467dd37cf9cc6dab4bc19cb69a4ef10669402de698e"
+sha256 = "e16c01d9ab9c11f7290eef1cfefc093fb5a45ee4a3da09e2fec2e4d1bae54e73"
 # tests failing with 3.10 for now, should be harmless
 options = ["!check"]
 
