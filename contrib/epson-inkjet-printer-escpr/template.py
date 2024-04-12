0e85f3f225910bc82ff8d41274673a6fe69670d9
Mohammed Anas <triallax@tutanota.com>
2024-04-12 23:51:29 +0100

    contrib/epson-inkjet-printer-escpr: update to 1.8.5

--- contrib/epson-inkjet-printer-escpr/template.py
+++ contrib/epson-inkjet-printer-escpr/template.py
@@ -1,5 +1,5 @@
 pkgname = "epson-inkjet-printer-escpr"
-pkgver = "1.8.4"
+pkgver = "1.8.5"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -16,5 +16,5 @@ pkgdesc = "Epson Inkjet Printer Driver (ESC/P-R)"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://download.ebz.epson.net/dsc/search/01/search?OSC=LX&productName=B700"
-source = f"https://download3.ebz.epson.net/dsc/f/03/00/15/57/25/a928e7d08c825ef1cdb892e70318d986720cef8a/epson-inkjet-printer-escpr-{pkgver}-1.tar.gz"
-sha256 = "3cf19fc139886997146c0b31d86fcbc718e11b76d1b9b5aaf391f13405c9450e"
+source = f"https://download3.ebz.epson.net/dsc/f/03/00/15/68/89/fbff579f15226ffcc4a16895bd6bce6842277802/epson-inkjet-printer-escpr-{pkgver}-1.tar.gz"
+sha256 = "27ca5426abece5ff1da859caa5ac4bc993b0a00e8ea74af63ffd9960c6a36a24"
