fcef07afb49f254cd8038684b54c8453e21f5179
Subhaditya Nath <sn03.general@gmail.com>
2024-04-13 00:09:49 +0530

    contrib/git-absorb: update to 0.6.13

--- contrib/git-absorb/template.py
+++ contrib/git-absorb/template.py
@@ -1,5 +1,5 @@
 pkgname = "git-absorb"
-pkgver = "0.6.12"
+pkgver = "0.6.13"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo", "pkgconf", "asciidoc"]
@@ -9,19 +9,7 @@ maintainer = "Subhaditya Nath <sn03.general@gmail.com>"
 license = "BSD-3-Clause"
 url = "https://github.com/tummychow/git-absorb"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "88a64712bcb4885a65984359c783e7f16b76fe4ca4ccd339d0c2d83139d0428b"
-
-
-def do_prepare(self):
-    # we patch the lockfile so vendor after patch
-    pass
-
-
-def post_patch(self):
-    from cbuild.util import cargo
-
-    self.cargo.vendor()
-    cargo.setup_vendor(self)
+sha256 = "5646857dd764d0a486405e777b3ec4e919511abc96bd0e02e71ec9e94d151115"
 
 
 def post_build(self):
