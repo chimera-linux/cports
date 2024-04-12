43463f1bc768fa0c4fb0e37e31778e5389f02af8
Erica Z <zerica@callcc.eu>
2024-04-12 21:44:00 +0200

    contrib/qtractor: update to 0.9.90

--- contrib/qtractor/template.py
+++ contrib/qtractor/template.py
@@ -1,6 +1,6 @@
 pkgname = "qtractor"
-pkgver = "0.9.39"
-pkgrel = 2
+pkgver = "0.9.90"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DCONFIG_WAYLAND=1"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -23,4 +23,4 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-2.0-or-later"
 url = "https://www.qtractor.org"
 source = f"$(SOURCEFORGE_SITE)/qtractor/qtractor-{pkgver}.tar.gz"
-sha256 = "e60c8f371b61ac16d21ef96f25b434aefc5512aebcb90120faa9f11d06fe3555"
+sha256 = "f231c0c06da54c22ab0cdeb8b87ee9499eda683c798983e1cd159898699f7476"
