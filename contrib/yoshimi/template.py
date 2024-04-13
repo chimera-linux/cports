08c8487cc1b3a6a9c2e86ad95d70fecf0e7401ee
psykose <alice@ayaya.dev>
2024-04-13 06:40:16 +0000

    contrib/yoshimi: new package (2.3.2)

---
+++ contrib/yoshimi/template.py
@@ -0,0 +1,29 @@
+pkgname = "yoshimi"
+pkgver = "2.3.2"
+pkgrel = 0
+build_style = "cmake"
+cmake_dir = "src"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "alsa-lib-devel",
+    "argp-standalone",
+    "cairo-devel",
+    "fftw-devel",
+    "fltk-devel",
+    "fontconfig-devel",
+    "libedit-readline-devel",
+    "lv2",
+    "mesa-devel",
+    "mxml3-devel",
+    "pipewire-jack-devel",
+]
+pkgdesc = "Software synthesizer"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later"
+url = "https://yoshimi.github.io"
+source = f"https://github.com/Yoshimi/yoshimi/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "76fcf0aaf959665f4d1a54c8e76dc30108bcf1434f76a0761e13217f973f27f6"
