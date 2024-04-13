ab04acbcbd9ca7bca745a4b0b3f1f79274995360
Mohammed Anas <triallax@tutanota.com>
2024-04-13 16:41:05 +0100

    main/ijs: add !distlicense

--- main/ijs/template.py
+++ main/ijs/template.py
@@ -12,6 +12,7 @@ license = "MIT"
 url = "https://www.ghostscript.com"
 source = f"https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs{_gsver.replace('.', '')}/ghostscript-{_gsver}.tar.xz"
 sha256 = "6ee3057773646d6a2c6d117eb53a17d6752feadc513828e4322f68b7b7789ff6"
+options = ["!distlicense"]
 
 
 def post_install(self):
