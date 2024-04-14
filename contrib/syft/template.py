edb8e4f7036a8681edd1767d6ec97d39fe1b1afa
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-04-14 15:01:29 +0200

    contrib/syft: update to 1.2.0

--- contrib/syft/template.py
+++ contrib/syft/template.py
@@ -1,5 +1,5 @@
 pkgname = "syft"
-pkgver = "1.1.0"
+pkgver = "1.2.0"
 pkgrel = 0
 build_style = "go"
 make_build_args = [
@@ -12,7 +12,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0"
 url = "https://github.com/anchore/syft"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5fbc5774fce472e8331ae0619e7f1cf12fdb3b358dcc46c52f3f3a0aaeeb1d07"
+sha256 = "c563805e4100562b7c4733f68fd45f75bca694a272853bd294076aea5977e58f"
 # Test suite depends on docker
 # generates manpages/completions with host bins
 options = ["!check", "!cross"]
