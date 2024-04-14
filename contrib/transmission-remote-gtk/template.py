bd983a0a909147ab5cef68c6995779ad38029fd7
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-04-14 14:53:10 +0200

    contrib/transmission-remote-gtk: new packge (1.6.0)

---
+++ contrib/transmission-remote-gtk/template.py
@@ -0,0 +1,18 @@
+pkgname = "transmission-remote-gtk"
+pkgver = "1.6.0"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "appstream-glib",
+    "desktop-file-utils",
+    "gettext",
+    "meson",
+    "pkgconf",
+]
+makedepends = ["gtk+3-devel", "json-glib-devel", "libsoup-devel"]
+pkgdesc = "GTK client for remote management of transmission (torrent client)"
+maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
+license = "GPL-2.0-or-later"
+url = "https://github.com/transmission-remote-gtk/transmission-remote-gtk"
+source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
+sha256 = "b090844f6a482e6f3588070ff3fdd54b79e8f85df02b39853cfb01fccee10cac"
