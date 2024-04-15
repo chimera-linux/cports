229a7d1059bf8903d0a4c82a55b527d7d5d5236d
Mohammed Anas <triallax@tutanota.com>
2024-04-15 02:43:37 +0100

    main/bash-completion: update to 2.13.0

--- main/bash-completion/template.py
+++ main/bash-completion/template.py
@@ -1,10 +1,13 @@
 pkgname = "bash-completion"
-pkgver = "2.11"
+pkgver = "2.13.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
 make_cmd = "gmake"
-make_install_args = ["profiledir=/etc/bash/bashrc.d"]
+make_install_args = [
+    "profiledir=/etc/bash/bashrc.d",
+    "compatdir=/usr/share/bash-completion/completions",
+]
 hostmakedepends = ["gmake", "pkgconf"]
 checkdepends = ["bash", "python-pytest", "python-pexpect"]
 depends = ["bash"]
@@ -13,6 +16,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/scop/bash-completion"
 source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "73a8894bad94dee83ab468fa09f628daffd567e8bef1a24277f1e9a0daf911ac"
+sha256 = "c5f99a39e40f0d154c03ff15438e87ece1f5ac666336a4459899e2ff4bedf3d1"
 # what's the point (needs a truckload of checkdepends too)
 options = ["!check"]
