924e1ab65a45c410f6c6bcb6929a3034befa36c6
psykose <alice@ayaya.dev>
2024-04-15 15:26:52 +0000

    contrib/fuzzel: update to 1.10.2

--- contrib/fuzzel/template.py
+++ contrib/fuzzel/template.py
@@ -1,5 +1,5 @@
 pkgname = "fuzzel"
-pkgver = "1.10.1"
+pkgver = "1.10.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -22,7 +22,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://codeberg.org/dnkl/fuzzel"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "e89bf8395d92aa0975e6487049c47e44e40ebf0997dc493a8bb03ec465f1910a"
+sha256 = "5362175f301af3f27c23138ac339294ce33dff61720ffc7eb13e78ec56f60a8b"
 hardening = ["vis", "cfi"]
 # no tests
 options = ["!check"]
