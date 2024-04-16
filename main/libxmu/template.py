a4a2b72f24aacd12ac36a8324a0c9111a454a04f
psykose <alice@ayaya.dev>
2024-04-16 20:08:00 +0000

    main/libxmu: update to 1.2.1

--- main/libxmu/template.py
+++ main/libxmu/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxmu"
-pkgver = "1.2.0"
+pkgver = "1.2.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "libtool", "pkgconf", "xorg-util-macros"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXmu-{pkgver}.tar.gz"
-sha256 = "b4686c4b4570044bcfc35bfaa3edbe68185ddf8e3250387f74a140c8e45afb2f"
+sha256 = "bf0902583dd1123856c11e0a5085bd3c6e9886fbbd44954464975fd7d52eb599"
 
 
 def post_install(self):
