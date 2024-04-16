345bc00add4998e5f7f344297776216c0687d4e6
q66 <q66@chimera-linux.org>
2024-04-16 11:42:04 +0200

    main/llvm, main/clang-rt-crt-cross: rebuild for aarch64 getauxval

--- main/clang-rt-crt-cross/template.py
+++ main/clang-rt-crt-cross/template.py
@@ -1,7 +1,7 @@
 pkgname = "clang-rt-crt-cross"
 _musl_ver = "1.2.5"
 pkgver = "18.1.3"
-pkgrel = 0
+pkgrel = 1
 build_wrksrc = f"llvm-project-{pkgver}.src"
 build_style = "cmake"
 configure_args = [
