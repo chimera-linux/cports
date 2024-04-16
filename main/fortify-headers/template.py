ea874470aa8ce29a9e31672d6a0b6c4daf26f8a5
q66 <q66@chimera-linux.org>
2024-04-16 18:52:14 +0200

    main/fortify-headers: update to 2.2

--- main/fortify-headers/template.py
+++ main/fortify-headers/template.py
@@ -1,6 +1,6 @@
 pkgname = "fortify-headers"
-pkgver = "2.1"
-pkgrel = 5
+pkgver = "2.2"
+pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
 make_check_target = "clang"
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "0BSD"
 url = "https://git.2f30.org/fortify-headers"
 source = f"https://github.com/jvoisin/fortify-headers/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "12e43fd91ee0327c5f0611b72b6f2e2d4b93fae289a80e059104ef2c4801c622"
+sha256 = "127fb933c21ac6e6f426c6405a11ab83d02e97908bc95fba6ac16875ef4772b5"
 tool_flags = {"CFLAGS": ["-Wno-macro-redefined"]}
 options = ["bootstrap"]
 
