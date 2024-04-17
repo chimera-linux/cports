d2557ddbba389f059a6966ac6d9b4404bb2d6daa
Mohammed Anas <triallax@tutanota.com>
2024-04-17 03:18:02 +0100

    contrib/gopls: update to 0.15.3

--- contrib/gopls/template.py
+++ contrib/gopls/template.py
@@ -1,5 +1,5 @@
 pkgname = "gopls"
-pkgver = "0.15.2"
+pkgver = "0.15.3"
 pkgrel = 0
 build_wrksrc = "gopls"
 build_style = "go"
@@ -12,7 +12,7 @@ url = "https://github.com/golang/tools/tree/master/gopls"
 source = (
     f"https://github.com/golang/tools/archive/refs/tags/gopls/v{pkgver}.tar.gz"
 )
-sha256 = "cf5b8246b36967eb8fbbed518ea941110cc6bbcc7f42a44bc5a4fe0d7ee61652"
+sha256 = "7fb0a43ff5e562089ca0ec3e8e7819dd205977758c02ea9bded05f56d5080a54"
 # debug: fails to split on powerpc
 # check: regtest/marker fails with go1.22
 options = ["!debug", "!check"]
