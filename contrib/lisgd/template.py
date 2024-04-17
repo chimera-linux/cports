038368c43acb08865d86e44cf3b8cb51f4233bb1
Nova <Froggo8311@proton.me>
2024-04-16 21:22:46 -0500

    contrib/lisgd: update to 0.4.0

--- contrib/lisgd/template.py
+++ contrib/lisgd/template.py
@@ -1,16 +1,16 @@
 pkgname = "lisgd"
-pkgver = "0.3.7"
+pkgver = "0.4.0"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
 hostmakedepends = ["gmake"]
 makedepends = ["libinput-devel", "libx11-devel", "wayland-devel"]
 pkgdesc = "Bind gestures on touchscreens and other devices via libinput"
-maintainer = "Froggo <froggo8311@proton.me>"
+maintainer = "Nova <froggo8311@proton.me>"
 license = "MIT"
 url = "https://git.sr.ht/~mil/lisgd"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "e4c5751984ef1f8d21f6f6548c184bca21a626b345fa0489e94f276ca49e950e"
+sha256 = "1eef0a3c4c297714b52dd061d40611c955ea8479ef3e60cfb0f7ab9cb22e65e7"
 # no check target
 options = ["!check"]
 
