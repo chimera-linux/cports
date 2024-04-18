98ab82c712234a92cb97c5e3114656f04c6c1cb4
Isaac Freund <mail@isaacfreund.com>
2024-04-18 17:34:51 +0200

    contrib/janet: update to 1.34.0

--- contrib/janet/template.py
+++ contrib/janet/template.py
@@ -1,5 +1,5 @@
 pkgname = "janet"
-pkgver = "1.33.0"
+pkgver = "1.34.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Depoll=true"]
@@ -11,7 +11,7 @@ url = "https://janet-lang.org"
 source = (
     f"https://github.com/janet-lang/janet/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "c9018fbd69b825cfc706d8c40e9464be37e924ce07089933e92f4f931ccf0d8d"
+sha256 = "d49670c564dcff6f9f7945067fa2acbd3431d923c25fc4ce6e400de28eeb0b1b"
 
 
 def post_install(self):
