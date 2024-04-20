4d3e486697ddb5c32765198c049bf5dcde01ec3f
Erica Z <zerica@callcc.eu>
2024-04-20 06:43:13 +0200

    contrib/gnome-shell-extension-appindicator: update to 58

--- contrib/gnome-shell-extension-appindicator/template.py
+++ contrib/gnome-shell-extension-appindicator/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-shell-extension-appindicator"
-pkgver = "57"
+pkgver = "58"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -12,4 +12,4 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/ubuntu/gnome-shell-extension-appindicator"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "788fa3ff2f8525efbd794bffd0fcec3d77c9c1cbfcff0fadeb93b4925751ca41"
+sha256 = "7a6fda28ddd214fbb832486b85c73447ff0c5fc7a36c6d1510c4ff6801cff9d8"
