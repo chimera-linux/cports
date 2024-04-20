ab8ff4415c55c192485438f6bbd246f9fff11ba2
ttyyls <contact@behri.org>
2024-04-20 19:12:47 +0100

    contrib/foot: update to 1.17.2

--- contrib/foot/template.py
+++ contrib/foot/template.py
@@ -1,5 +1,5 @@
 pkgname = "foot"
-pkgver = "1.17.1"
+pkgver = "1.17.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dterminfo-base-name=foot-extra"]
@@ -26,7 +26,7 @@ maintainer = "flukey <flukey@vapourmail.eu>"
 license = "MIT"
 url = "https://codeberg.org/dnkl/foot"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "da49d302fb98d61e674dace27d44ab6e6e6446a1ee156a09a430efb738d74b39"
+sha256 = "6b6c52f3e6d9ad357b9704996ce3039523c87df7b8e00a9250f8858d16495bf6"
 hardening = ["vis", "cfi"]
 
 
