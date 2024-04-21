122bca9bc60d44bedbe68e4977ddb16bf4be9143
Erica Z <zerica@callcc.eu>
2024-04-21 17:01:48 +0200

    contrib/ispc: new package (1.24.0)

---
+++ contrib/ispc/template.py
@@ -0,0 +1,40 @@
+pkgname = "ispc"
+pkgver = "1.24.0"
+pkgrel = 0
+archs = ["x86_64", "aarch64", "armv7"]
+build_style = "cmake"
+configure_args = ["-DCMAKE_BUILD_TYPE=Release"]
+hostmakedepends = [
+    "bison",
+    "cmake",
+    "flex",
+    "llvm-tools",
+    "ninja",
+    "python",
+]
+makedepends = [
+    "clang-devel",
+    "llvm-devel",
+    "ncurses-devel",
+    "onetbb-devel",
+]
+pkgdesc = "Implicit SPMD program compiler"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "BSD-3-Clause"
+url = "https://ispc.github.io"
+source = f"https://github.com/ispc/ispc/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "a45ec5402d8a3b23d752125a083fa031becf093b8304ccec55b1c2f37b5479c3"
+
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
+
+
+@subpackage("ispc-libs")
+def _libs(self):
+    return self.default_libs()
+
+
+@subpackage("ispc-devel")
+def _devel(self):
+    return self.default_devel()
