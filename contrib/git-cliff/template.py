15258eef43c011bf4b0106180a593ede5e74f630
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-04-21 13:30:54 +0200

    contrib/git-cliff: update to 2.2.1

--- contrib/git-cliff/template.py
+++ contrib/git-cliff/template.py
@@ -1,5 +1,5 @@
 pkgname = "git-cliff"
-pkgver = "2.2.0"
+pkgver = "2.2.1"
 pkgrel = 0
 build_style = "cargo"
 make_check_args = [
@@ -15,7 +15,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/orhun/git-cliff"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "d89af1c654e63ce2f6c09cd63d691bf531f0ccd76b670316cc71529f1d4eaccb"
+sha256 = "8573c4dc28fd6d6c1e9be7156193c13d177af093a060ae9e3bd4cd60ff3e05c4"
 # generates manpages/completions with host bins
 options = ["!cross"]
 
