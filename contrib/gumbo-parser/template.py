596da7223268f57cc77efa63f6f5329f8c86abc2
Grigory Kirillov <txgk@bk.ru>
2024-04-21 04:25:46 +0300

    contrib/gumbo-parser: update url

--- contrib/gumbo-parser/template.py
+++ contrib/gumbo-parser/template.py
@@ -1,6 +1,6 @@
 pkgname = "gumbo-parser"
 pkgver = "0.12.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_gen = ["./autogen.sh"]
 make_cmd = "gmake"
@@ -15,7 +15,7 @@ makedepends = ["gtest-devel"]
 pkgdesc = "HTML5 parsing library in pure C99"
 maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
-url = "https://codeberg.org/grisha/gumbo-parser"
+url = "https://codeberg.org/gumbo-parser/gumbo-parser"
 source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "c0bb5354e46539680724d638dbea07296b797229a7e965b13305c930ddc10d82"
 
