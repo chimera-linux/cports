d6ca9c4ec712f67a5db74b79db39d0c9784690b3
q66 <q66@chimera-linux.org>
2024-04-23 00:20:13 +0200

    contrib/argon2: move from main, enable lto
    We only disabled lto to allow static cryptsetup link, no longer
    as cryptsetup does not depend on it anymore.

--- contrib/argon2/template.py
+++ contrib/argon2/template.py
@@ -1,6 +1,6 @@
 pkgname = "argon2"
 pkgver = "20190702"
-pkgrel = 0
+pkgrel = 1
 build_style = "makefile"
 make_cmd = "gmake"
 make_build_args = ["OPTTARGET=none", "ARGON2_VERSION=" + pkgver]
@@ -14,8 +14,6 @@ license = "Apache-2.0 OR CC0-1.0"
 url = "https://github.com/P-H-C/phc-winner-argon2"
 source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "daf972a89577f8772602bf2eb38b6a3dd3d922bf5724d45e7f9589b5e830442c"
-# otherwise generates broken static libs so we can't link cryptsetup-static
-options = ["!lto"]
 
 
 @subpackage("argon2-progs")
