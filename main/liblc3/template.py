db9aa77be6bdfaf6bca4cee3291063a83abb89ce
psykose <alice@ayaya.dev>
2024-04-22 16:58:54 +0000

    main/liblc3: update to 1.1.1

--- main/liblc3/template.py
+++ main/liblc3/template.py
@@ -1,5 +1,5 @@
 pkgname = "liblc3"
-pkgver = "1.1.0"
+pkgver = "1.1.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dtools=true"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/google/liblc3"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "958725e277685f9506d30ea341c38a03b245c3b33852cd074da6c8857525e808"
+sha256 = "b65e38943708529efd04a87dd1a9f16a9856ed6199d082b18e7d42fb5c59486e"
 
 
 @subpackage("liblc3-devel")
