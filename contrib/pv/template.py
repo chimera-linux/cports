3ab0fc11f8a5c26a03f5663a7f020025795c270a
Erica Z <zerica@callcc.eu>
2024-04-22 22:13:36 +0200

    contrib/pv: update to 1.8.9

--- contrib/pv/template.py
+++ contrib/pv/template.py
@@ -1,5 +1,5 @@
 pkgname = "pv"
-pkgver = "1.8.5"
+pkgver = "1.8.9"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "gettext-devel"]
@@ -8,4 +8,4 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-3.0-or-later"
 url = "https://www.ivarch.com/programs/pv.shtml"
 source = f"https://www.ivarch.com/programs/sources/{pkgname}-{pkgver}.tar.gz"
-sha256 = "d22948d06be06a5be37336318de540a2215be10ab0163f8cd23a20149647b780"
+sha256 = "a0789d8f8c5a08faf370b5a07d1d936aeff9504a4f49da76d4164797ac4606e6"
