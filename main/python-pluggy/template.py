38b3f64efa75a19f8d428605dc4c0d45b9c94a9e
psykose <alice@ayaya.dev>
2024-04-22 17:14:04 +0000

    main/python-pluggy: update to 1.5.0

--- main/python-pluggy/template.py
+++ main/python-pluggy/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pluggy"
-pkgver = "1.4.0"
+pkgver = "1.5.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pytest-dev/pluggy"
 source = f"$(PYPI_SITE)/p/pluggy/pluggy-{pkgver}.tar.gz"
-sha256 = "8c85c2876142a764e5b7548e7d9a0e0ddb46f5185161049a79b7e974454223be"
+sha256 = "2cffa88e94fdc978c4c574f15f9e59b7f4201d439195c3715ca9e2486f1d0cf1"
 # dependency of pytest
 options = ["!check"]
 
