7a4a386f2463fc0ae84780053794cc90b41bf2b1
triallax <triallax@tutanota.com>
2024-04-22 22:09:35 +0100

    main/python-pycups: update to 2.0.4

--- main/python-pycups/template.py
+++ main/python-pycups/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pycups"
-pkgver = "2.0.1"
+pkgver = "2.0.4"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,6 +15,6 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/OpenPrinting/pycups"
 source = f"$(PYPI_SITE)/p/pycups/pycups-{pkgver}.tar.gz"
-sha256 = "57434ce5f62548eb12949ca8217f066f4eeb21a5d6ab8b13471dce350e380c90"
+sha256 = "843e385c1dbf694996ca84ef02a7f30c28376035588f5fbeacd6bae005cf7c8d"
 # No test suite
 options = ["!check"]
