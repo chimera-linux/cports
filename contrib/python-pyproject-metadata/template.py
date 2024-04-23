5e64c703ee1969913606b24e99b7edb58973d2e8
psykose <alice@ayaya.dev>
2024-04-23 08:39:15 +0000

    contrib/python-pyproject-metadata: update to 0.8.0

--- contrib/python-pyproject-metadata/template.py
+++ contrib/python-pyproject-metadata/template.py
@@ -1,21 +1,20 @@
 pkgname = "python-pyproject-metadata"
-pkgver = "0.7.1"
-pkgrel = 1
+pkgver = "0.8.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
+    "python-flit_core",
     "python-installer",
-    "python-setuptools",
-    "python-wheel",
 ]
 checkdepends = ["python-pytest"]
 depends = ["python-packaging"]
 pkgdesc = "PEP 621 metadata parsing"
 maintainer = "Duncan Bellamy <dunk@denkimushi.com>"
 license = "MIT"
-url = "https://https://pep621.readthedocs.io/index.html"
-source = f"https://github.com/FFY00/python-pyproject-metadata/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "ad7467871eea29206d7cca024ee31bf9c1b13f7b9bfdefe94b481c7b671a8954"
+url = "https://github.com/pypa/pyproject-metadata"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "bb5015c9f3869c6fa59d8fdf29840a58cda2a570cb557b8aecae0f9e97a4cf88"
 
 
 def post_install(self):
