8ea4ca5cf7a3d2af132792e71c6bf9733ed26ff5
Erica Z <zerica@callcc.eu>
2024-04-23 08:54:53 +0200

    contrib/python-outcome: new package (1.3.0)

---
+++ contrib/python-outcome/template.py
@@ -0,0 +1,22 @@
+pkgname = "python-outcome"
+pkgver = "1.3.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = ["python-attrs"]
+checkdepends = ["python-pytest"] + depends
+pkgdesc = "Capture the outcome of Python calls"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "Apache-2.0 OR MIT"
+url = "https://github.com/python-trio/outcome"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "5427e7d3c48bcb9d383e08ba057359b570c6118d0e640da5baeb0e298502a4b2"
+
+
+def post_install(self):
+    self.install_license("LICENSE.MIT")
