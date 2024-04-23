46be2a6aed72c1ccaa9827042631b23c240df395
psykose <alice@ayaya.dev>
2024-04-23 08:37:40 +0000

    contrib/python-meson: update to 0.16.0

--- contrib/python-meson/template.py
+++ contrib/python-meson/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-meson"
-pkgver = "0.15.0"
+pkgver = "0.16.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -23,7 +23,7 @@ maintainer = "Duncan Bellamy <dunk@denkimushi.com>"
 license = "MIT"
 url = "https://meson-python.readthedocs.io/en/latest/index.html"
 source = f"https://github.com/mesonbuild/meson-python/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "7c29eb0d17bb7813f5e5c89f5df5f56eda446e263b4d4a9f5f286548a0eb385f"
+sha256 = "1f464e7a1e15971b70c8208d537fd0193b33c345aac5887af2806e55426ca571"
 
 
 def post_install(self):
