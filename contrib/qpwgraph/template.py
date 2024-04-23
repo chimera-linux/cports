ece39e338cb0f27925135dff841a3fcc0bd8ba15
psykose <alice@ayaya.dev>
2024-04-23 08:23:25 +0000

    contrib/qpwgraph: update to 0.7.0

--- contrib/qpwgraph/template.py
+++ contrib/qpwgraph/template.py
@@ -1,5 +1,5 @@
 pkgname = "qpwgraph"
-pkgver = "0.6.3"
+pkgver = "0.7.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -17,7 +17,7 @@ pkgdesc = "Pipewire graph manager"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.freedesktop.org/rncbc/qpwgraph"
-source = f"https://gitlab.freedesktop.org/rncbc/qpwgraph/-/archive/v{pkgver}/qpwgraph-v{pkgver}.tar.bz2"
-sha256 = "d69052c77303f56ae326c2f19924efc3fde6e06ef997e937be42b29bc404398f"
+source = f"{url}/-/archive/v{pkgver}/qpwgraph-v{pkgver}.tar.bz2"
+sha256 = "1988716b31398ab87b11298b2d17268b8e88af00abf4c30ba7ae56844987efbb"
 # FIXME: cfi
 hardening = ["vis"]
