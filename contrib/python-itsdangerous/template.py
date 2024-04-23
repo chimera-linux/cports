7ab1938779930359ec6ca1db223cf45e17b567ab
psykose <alice@ayaya.dev>
2024-04-23 08:49:54 +0000

    contrib/python-itsdangerous: update to 2.2.0

--- contrib/python-itsdangerous/template.py
+++ contrib/python-itsdangerous/template.py
@@ -1,22 +1,23 @@
 pkgname = "python-itsdangerous"
-pkgver = "2.1.2"
+pkgver = "2.2.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
+    "python-flit_core",
     "python-installer",
-    "python-setuptools",
-    "python-wheel",
+]
+checkdepends = [
+    "python-freezegun",
+    "python-pytest",
 ]
 pkgdesc = "Python3 helper to pass trusted data to untrusted environments"
 maintainer = "firefly-cpp <iztok@iztok.space>"
 license = "BSD-3-Clause"
 url = "https://github.com/pallets/itsdangerous"
 source = f"$(PYPI_SITE)/i/itsdangerous/itsdangerous-{pkgver}.tar.gz"
-sha256 = "5dbbc68b317e5e42f327f9021763545dc3fc3bfe22e6deb96aaf1fc38874156a"
-# no tests
-options = ["!check"]
+sha256 = "e0050c0b7da1eea53ffaf149c0cfbb5c6e2e2b69c4bef22c81fa6eb73e5f6173"
 
 
 def post_install(self):
-    self.install_license("LICENSE.rst")
+    self.install_license("LICENSE.txt")
