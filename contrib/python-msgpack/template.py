9bf9acb014410c9d8d86a918d41761a55a237933
Erica Z <zerica@callcc.eu>
2024-04-23 08:28:30 +0200

    contrib/python-msgpack: new package (1.0.8)

---
+++ contrib/python-msgpack/template.py
@@ -0,0 +1,19 @@
+pkgname = "python-msgpack"
+pkgver = "1.0.8"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-cython",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+makedepends = ["python-devel"]
+checkdepends = ["python-pytest"]
+pkgdesc = "MessagePack serializer for Python"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "Apache-2.0"
+url = "https://msgpack.org"
+source = f"https://github.com/msgpack/msgpack-python/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "481996e14606bc215a8aed396c773bd4c3ae8b5afeac6622a3e02a4b33981b02"
