473a057db7fd141d3e8a5ba739f48424c80501b2
triallax <triallax@tutanota.com>
2024-04-24 21:37:40 +0100

    contrib/libxfce4ui: generate `configure`, ignore unstable versions

--- contrib/libxfce4ui/template.py
+++ contrib/libxfce4ui/template.py
@@ -3,33 +3,39 @@ pkgver = "4.18.6"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-tests", "--disable-static"]
-configure_gen = []
 make_cmd = "gmake"
+# TODO: gobject-introspection, fails to build with it for some reason
 hostmakedepends = [
-    "pkgconf",
-    "gmake",
-    "intltool",
-    "vala-devel",
-    "glib-devel",
+    "automake",
     "gettext-devel",
+    "glib-devel",
+    "gmake",
+    "gtk-doc-tools",
+    "intltool",
+    "libtool",
+    "pkgconf",
     "python",
+    "vala-devel",
+    "xfce4-dev-tools",
 ]
 makedepends = [
     "glib-devel",
-    "vala-devel",
     "gtk+3-devel",
+    "libgtop-devel",
+    "libsm-devel",
     "libxfce4util-devel",
-    "xfconf-devel",
     "libxml2-devel",
     "startup-notification-devel",
-    "libsm-devel",
-    "libgtop-devel",
+    "vala-devel",
+    "xfconf-devel",
 ]
 pkgdesc = "Xfce UI library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
-url = "https://xfce.org"
-source = f"https://archive.xfce.org/src/xfce/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.bz2"
+url = "https://docs.xfce.org/xfce/libxfce4ui/start"
+source = (
+    f"$(XFCE_SITE)/xfce/libxfce4ui/{pkgver[:-2]}/libxfce4ui-{pkgver}.tar.bz2"
+)
 sha256 = "77dd99206cc8c6c7f69c269c83c7ee6a037bca9d4a89b1a6d9765e5a09ce30cd"
 
 
