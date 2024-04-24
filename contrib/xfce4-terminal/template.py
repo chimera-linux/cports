3047435d69c795ac439b2cab896463e1e29df741
triallax <triallax@tutanota.com>
2024-04-24 21:38:12 +0100

    contrib/xfce4-terminal: remove unnecessary runtime dep, generate `configure`

--- contrib/xfce4-terminal/template.py
+++ contrib/xfce4-terminal/template.py
@@ -1,16 +1,22 @@
 pkgname = "xfce4-terminal"
 pkgver = "1.1.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 make_cmd = "gmake"
-hostmakedepends = ["pkgconf", "gmake", "intltool"]
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "gtk-doc-tools",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
 makedepends = ["exo-devel", "libxfce4ui-devel", "vte-gtk3-devel"]
-depends = ["gsettings-desktop-schemas"]
 pkgdesc = "Xfce terminal emulator"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
-url = "https://xfce.org"
-source = f"https://archive.xfce.org/src/apps/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.bz2"
+url = "https://docs.xfce.org/apps/xfce4-terminal/start"
+source = f"$(XFCE_SITE)/apps/xfce4-terminal/{pkgver[:-2]}/xfce4-terminal-{pkgver}.tar.bz2"
 sha256 = "214dd588d441b69f816009682a3bb4652cc19bed7ea64b612a12f097417b3d45"
-
-configure_gen = []
