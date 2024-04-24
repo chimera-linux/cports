c78644da9686e5bebb6a506a2f8db98c3db16cd4
triallax <triallax@tutanota.com>
2024-04-24 21:38:02 +0100

    contrib/libxfce4util: generate `configure`, ignore unstable versions

--- contrib/libxfce4util/template.py
+++ contrib/libxfce4util/template.py
@@ -2,16 +2,24 @@ pkgname = "libxfce4util"
 pkgver = "4.18.2"
 pkgrel = 0
 build_style = "gnu_configure"
-# generates invalid configure
-configure_gen = []
 make_cmd = "gmake"
-hostmakedepends = ["pkgconf", "gmake", "intltool", "vala"]
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "gtk-doc-tools",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "vala",
+    "xfce4-dev-tools",
+]
 makedepends = ["glib-devel"]
 pkgdesc = "Utility library for Xfce"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
-url = "https://xfce.org"
-source = f"https://archive.xfce.org/src/xfce/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.bz2"
+url = "https://docs.xfce.org/xfce/libxfce4util/start"
+source = f"$(XFCE_SITE)/xfce/libxfce4util/{pkgver[:-2]}/libxfce4util-{pkgver}.tar.bz2"
 sha256 = "d9a329182b78f7e2520cd4aafcbb276bbbf162f6a89191676539ad2e3889c353"
 
 
