d103583190a22a40a9b3e8fccd4cc3776f77628d
psykose <alice@ayaya.dev>
2024-04-24 10:31:38 +0000

    main/libaom: fix aarch64 build, disarm asserts

--- main/libaom/template.py
+++ main/libaom/template.py
@@ -1,6 +1,6 @@
 pkgname = "libaom"
 pkgver = "3.9.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 configure_args = [
     "-DBUILD_SHARED_LIBS=ON",
@@ -29,8 +29,8 @@ sha256 = ["a662e22299752547488c8e1412c0b41981efa8dbb1a25c696ded7ba9c472e919"]
 options = ["!check"]
 
 tool_flags = {
-    "CFLAGS": ["-D_GNU_SOURCE"],
-    "CXXFLAGS": ["-D_GNU_SOURCE"],
+    "CFLAGS": ["-D_GNU_SOURCE", "-DNDEBUG"],
+    "CXXFLAGS": ["-D_GNU_SOURCE", "-DNDEBUG"],
     "LDFLAGS": ["-Wl,-z,stack-size=2097152"],
 }
 
