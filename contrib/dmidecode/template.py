7729e3b59ce887a7c84e2432e88ede3619fc9e9d
psykose <alice@ayaya.dev>
2024-04-24 12:53:01 +0000

    contrib/dmidecode: update to 3.6

--- contrib/dmidecode/template.py
+++ contrib/dmidecode/template.py
@@ -1,16 +1,19 @@
 pkgname = "dmidecode"
-pkgver = "3.5"
+pkgver = "3.6"
 pkgrel = 0
 # smbios/dmi support
 archs = ["aarch64", "riscv64", "x86_64"]
 build_style = "makefile"
+make_cmd = "gmake"
 make_install_args = ["prefix=/usr", "sbindir=/usr/bin"]
+hostmakedepends = ["gmake", "pkgconf"]
+makedepends = ["bash-completion"]
 pkgdesc = "Utility for reporting system hardware"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://nongnu.org/dmidecode"
 source = f"https://de.freedif.org/savannah/dmidecode/dmidecode-{pkgver}.tar.xz"
-sha256 = "79d76735ee8e25196e2a722964cf9683f5a09581503537884b256b01389cc073"
+sha256 = "e40c65f3ec3dafe31ad8349a4ef1a97122d38f65004ed66575e1a8d575dd8bae"
 hardening = ["vis", "cfi"]
 # none present
 options = ["!check"]
