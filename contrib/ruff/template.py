4201a1920c044c6010028f552a1bb98b706ae822
psykose <alice@ayaya.dev>
2024-04-25 18:09:02 +0000

    contrib/ruff: update to 0.4.2

--- contrib/ruff/template.py
+++ contrib/ruff/template.py
@@ -1,5 +1,5 @@
 pkgname = "ruff"
-pkgver = "0.4.1"
+pkgver = "0.4.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,7 +14,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://docs.astral.sh/ruff"
 source = f"https://github.com/astral-sh/ruff/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7213ad13b15e766fb4f5d2fa24ffe1a1a55b47822a159e3c8f64b4a0e6ebca49"
+sha256 = "7f08b9b79afdf75eb5528986f0ac8a7fe0183d5b1917cba7d7d595b09cb58d6a"
 # generates completions with host bin
 options = ["!cross"]
 
