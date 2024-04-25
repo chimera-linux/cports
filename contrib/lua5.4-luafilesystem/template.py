021e7c6e40da85c6c5d4acc87180fe19f945030a
ttyyls <contact@behri.org>
2024-04-25 08:07:17 +0100

    contrib/lua5.4-luafilesystem: new package (1.8.0)
    Co-authored-by: q66 <q66@chimera-linux.org>

---
+++ contrib/lua5.4-luafilesystem/template.py
@@ -0,0 +1,21 @@
+pkgname = "lua5.4-luafilesystem"
+pkgver = "1.8.0"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_build_args = ["LUA_VERSION=5.4"]
+make_install_args = ["LUA_VERSION=5.4"]
+hostmakedepends = ["gmake"]
+makedepends = ["lua5.4-devel"]
+pkgdesc = "File system library for lua"
+maintainer = "ttyyls <contact@behri.org>"
+license = "MIT"
+url = "https://lunarmodules.github.io/luafilesystem"
+source = f"https://github.com/lunarmodules/luafilesystem/archive/refs/tags/v{pkgver.replace('.', '_')}.tar.gz"
+sha256 = "16d17c788b8093f2047325343f5e9b74cccb1ea96001e45914a58bbae8932495"
+# no tests
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
