9da22d32d713ec28b702d4fe9450272a1160e0f2
psykose <alice@ayaya.dev>
2024-04-25 10:32:03 +0000

    contrib/python-referencing: update to 0.35.0

--- contrib/python-referencing/template.py
+++ contrib/python-referencing/template.py
@@ -1,11 +1,10 @@
 pkgname = "python-referencing"
-pkgver = "0.34.0"
-pkgrel = 1
+pkgver = "0.35.0"
+pkgrel = 0
 build_style = "python_pep517"
 # the tests in suite/ depend on jsonschema
 make_check_args = ["referencing"]
 hostmakedepends = [
-    "python",
     "python-build",
     "python-hatch_vcs",
     "python-hatchling",
@@ -29,7 +28,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/python-jsonschema/referencing"
 source = f"$(PYPI_SITE)/r/referencing/referencing-{pkgver}.tar.gz"
-sha256 = "5773bd84ef41799a5a8ca72dc34590c041eb01bf9aa02632b4a973fb0181a844"
+sha256 = "191e936b0c696d0af17ad7430a3dc68e88bc11be6514f4757dc890f04ab05889"
 
 
 def post_install(self):
