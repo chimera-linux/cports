4a291ce46ba7a092b50d269474e220e606e54554
psykose <alice@ayaya.dev>
2024-04-25 10:59:50 +0000

    contrib/openjdk17: update to 17.0.10_p9

--- contrib/openjdk17/template.py
+++ contrib/openjdk17/template.py
@@ -1,7 +1,7 @@
 pkgname = "openjdk17"
 _majver = "17"
 _fver = f"{_majver}.0.11"
-_bver = "0"
+_bver = "9"
 pkgver = f"{_fver}_p{_bver}"
 pkgrel = 0
 # we don't attempt zero, it's a waste of time
@@ -66,7 +66,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only WITH Classpath-exception-2.0"
 url = "https://openjdk.org"
 source = f"https://github.com/openjdk/jdk{_majver}u/archive/jdk-{_fver}+{_bver}.tar.gz"
-sha256 = "a27bf73c29969541dd00de572794a381bca7fd0ea91f3982cbde53e144a84dad"
+sha256 = "84e2e143dcab2160dcde3fe6d6b12d7d030533a5bce1aec0ac20d09c023deae0"
 # FIXME: SIGILL in jvm
 hardening = ["!int"]
 # TODO later
