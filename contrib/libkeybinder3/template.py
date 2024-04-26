1f231b149dca2ee062146544eceed1cd26789f6a
triallax <triallax@tutanota.com>
2024-04-26 21:31:22 +0100

    contrib/libkeybinder3: drop `vis`
    Breaks `xfce4-volumed-pulse` build.

--- contrib/libkeybinder3/template.py
+++ contrib/libkeybinder3/template.py
@@ -1,29 +1,26 @@
 pkgname = "libkeybinder3"
 pkgver = "0.3.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_env = {"NOCONFIGURE": "1"}
 configure_gen = ["./autogen.sh"]
 make_cmd = "gmake"
 hostmakedepends = [
-    "gnome-common",
-    "gtk-doc-tools",
-    "pkgconf",
-    "libtool",
-    "gobject-introspection",
-    "gmake",
     "gettext",
+    "gmake",
+    "gnome-common",
+    "gobject-introspection",
+    "gtk-doc-tools",
+    "libtool",
+    "pkgconf",
 ]
 makedepends = ["gtk+3-devel"]
 pkgdesc = "Library for registering global keyboard shortcuts (GTK+3)"
 maintainer = "yopito <pierre.bourgin@free.fr>"
 license = "MIT"
 url = "https://github.com/engla/keybinder"
-source = (
-    f"https://github.com/engla/keybinder/archive/keybinder-3.0-v{pkgver}.tar.gz"
-)
+source = f"{url}/archive/keybinder-3.0-v{pkgver}.tar.gz"
 sha256 = "2eec50be6dfafa672d719e719f639df10403f270b2473e60deb2fb8455d13c51"
-hardening = ["vis"]
 # crossbuild fails: does not find gobject-introspection nor gtk-doc
 options = ["!cross"]
 
