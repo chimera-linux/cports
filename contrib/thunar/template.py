4dfdd9b4ad7d7f9ce6dad6c964c39c70af8151d2
triallax <triallax@tutanota.com>
2024-04-28 02:05:08 +0100

    contrib/thunar: new package (4.18.10)

---
+++ contrib/thunar/template.py
@@ -0,0 +1,39 @@
+pkgname = "thunar"
+pkgver = "4.18.10"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "gtk-doc-tools",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "exo-devel",
+    "gdk-pixbuf-devel",
+    "glib-devel",
+    "gtk+3-devel",
+    "libgudev-devel",
+    "libnotify-devel",
+    "libxfce4ui-devel",
+    "libxfce4util-devel",
+    "pango-devel",
+    "xfce4-panel-devel",
+    "xfconf-devel",
+]
+pkgdesc = "Xfce file manager"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later AND LGPL-2.0-or-later"
+url = "https://docs.xfce.org/xfce/thunar/start"
+source = f"$(XFCE_SITE)/xfce/thunar/{'.'.join(pkgver.split('.')[:-1])}/thunar-{pkgver}.tar.bz2"
+sha256 = "e8308a1f139602d8c125f594bfcebb063b7dac1fbb6e14293bab4cdb3ecf1d08"
+
+
+@subpackage("thunar-devel")
+def _dev(self):
+    return self.default_devel()
