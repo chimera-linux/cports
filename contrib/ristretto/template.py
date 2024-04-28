f5698ebe3e2bf955842692b3be0092641ed9dfb6
triallax <triallax@tutanota.com>
2024-04-28 02:06:55 +0100

    contrib/ristretto: new package (0.13.2)

---
+++ contrib/ristretto/template.py
@@ -0,0 +1,32 @@
+pkgname = "ristretto"
+pkgver = "0.13.2"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "cairo-devel",
+    "file-devel",
+    "glib-devel",
+    "gtk+3-devel",
+    "libexif-devel",
+    "libx11-devel",
+    "libxfce4ui-devel",
+    "libxfce4util-devel",
+    "xfconf-devel",
+]
+pkgdesc = "Xfce image viewer"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/apps/ristretto/start"
+source = f"$(XFCE_SITE)/apps/ristretto/{pkgver[:-2]}/ristretto-{pkgver}.tar.bz2"
+sha256 = "779f5ede3016019eec01d64a025583078d3936e35d4288ec2e8433494d757dd9"
+# TODO
+options = ["!check"]
