b0904fbe2d3b4fbc3164633c2c103c5ca80799bb
triallax <triallax@tutanota.com>
2024-04-28 02:07:29 +0100

    contrib/xfce4-screensaver: new package (4.18.3)

---
+++ contrib/xfce4-screensaver/template.py
@@ -0,0 +1,44 @@
+pkgname = "xfce4-screensaver"
+pkgver = "4.18.3"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--with-xf86gamma-ext"]
+make_cmd = "gmake"
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "dbus-glib-devel",
+    "elogind-devel",
+    "garcon-devel",
+    "glib-devel",
+    "gtk+3-devel",
+    "libwnck-devel",
+    "libx11-devel",
+    "libxext-devel",
+    "libxfce4ui-devel",
+    "libxfce4util-devel",
+    "libxklavier-devel",
+    "libxrandr",
+    "libxscrnsaver-devel",
+    "libxxf86vm-devel",
+    "linux-pam-devel",
+    "mesa-devel",
+    "shadow-devel",
+    "xfconf-devel",
+]
+pkgdesc = "Xfce screensaver"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://xfce.org"
+url = "https://docs.xfce.org/apps/xfce4-screensaver/start"
+source = f"$(XFCE_SITE)/apps/xfce4-screensaver/{pkgver[:-2]}/xfce4-screensaver-{pkgver}.tar.bz2"
+sha256 = "d171316136a1189dfe69ef3da7f7a7f842014129ece184cc21ffb13bc0e13a39"
+# TODO
+options = ["!check"]
