7ebb9f137f649c45428f4490c9b2465433d58ae5
triallax <triallax@tutanota.com>
2024-04-29 18:17:19 +0100

    contrib/gigolo: new package (0.5.3)

---
+++ contrib/gigolo/template.py
@@ -0,0 +1,24 @@
+pkgname = "gigolo"
+pkgver = "0.5.3"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "glib-devel",
+    "gtk+3-devel",
+]
+pkgdesc = "Xfce GIO/GVFS frontend"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-only"
+url = "https://docs.xfce.org/apps/gigolo/start"
+source = f"$(XFCE_SITE)/apps/gigolo/{pkgver[:-2]}/gigolo-{pkgver}.tar.bz2"
+sha256 = "d25984f65744665e2433335249f9547a38cead45440027af0c397ebf254d2fd0"
