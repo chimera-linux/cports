50864951c44966c566a8965222ab8d9ca6576abb
triallax <triallax@tutanota.com>
2024-04-29 18:18:12 +0100

    contrib/mousepad: new package (0.6.2)

---
+++ contrib/mousepad/template.py
@@ -0,0 +1,27 @@
+pkgname = "mousepad"
+pkgver = "0.6.2"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "glib-devel",
+    "gspell-devel",
+    "gtk+3-devel",
+    "gtksourceview4-devel",
+    "libxfce4ui-devel",
+    "polkit-devel",
+]
+pkgdesc = "Xfce text editor"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/apps/mousepad/start"
+source = f"$(XFCE_SITE)/apps/mousepad/{pkgver[:-2]}/mousepad-{pkgver}.tar.bz2"
+sha256 = "e7cacb3b8cb1cd689e6341484691069e73032810ca51fc747536fc36eb18d19d"
