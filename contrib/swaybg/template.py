2bed7bcf879474907d840c1e6ed7c3a0b6638dcb
psykose <alice@ayaya.dev>
2024-04-29 06:13:49 +0000

    contrib/swaybg: update to 1.2.1

--- contrib/swaybg/template.py
+++ contrib/swaybg/template.py
@@ -1,11 +1,12 @@
 pkgname = "swaybg"
-pkgver = "1.2.0"
+pkgver = "1.2.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "meson",
     "pkgconf",
     "scdoc",
+    "wayland-progs",
 ]
 makedepends = [
     "cairo-devel",
@@ -18,7 +19,7 @@ maintainer = "flukey <flukey@vapourmail.eu>"
 license = "MIT"
 url = "https://github.com/swaywm/swaybg"
 source = f"{url}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "c0205b34f1fad94553b6cb2c2b983cc33186018026058cad0b841a00bc3087e3"
+sha256 = "6af1fdf0e57b1cc5345febed786b761fea0e170943a82639f94cfaed7df84f8f"
 hardening = ["vis", "cfi"]
 
 
