e122835a7614cda9ea313b913baf6162cbe77c68
triallax <triallax@tutanota.com>
2024-04-29 18:18:01 +0100

    contrib/xfce4-taskmanager: new package (1.5.7)

---
+++ contrib/xfce4-taskmanager/template.py
@@ -0,0 +1,30 @@
+pkgname = "xfce4-taskmanager"
+pkgver = "1.5.7"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "cairo-devel",
+    "glib-devel",
+    "gtk+3-devel",
+    "libwnck-devel",
+    "libx11-devel",
+    "libxfce4ui-devel",
+    "libxmu-devel",
+    "xfconf-devel",
+]
+pkgdesc = "Xfce task manager"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/apps/xfce4-taskmanager/start"
+source = f"$(XFCE_SITE)/apps/xfce4-taskmanager/{pkgver[:-2]}/xfce4-taskmanager-{pkgver}.tar.bz2"
+sha256 = "6736832f5a64533e536f4994280bd907da19666cda8d2f465c8a53bb581f68bb"
