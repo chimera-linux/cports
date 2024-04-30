3a1638df9ab76fc19d72a7d1262cd48027cddc69
triallax <triallax@tutanota.com>
2024-04-30 20:23:02 +0100

    contrib/libburn: new package (1.5.6)

---
+++ contrib/libburn/template.py
@@ -0,0 +1,17 @@
+pkgname = "libburn"
+pkgver = "1.5.6"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["automake", "libtool", "pkgconf"]
+makedepends = ["linux-headers"]
+pkgdesc = "Library for reading, mastering and writing optical discs"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://dev.lovelyhq.com/libburnia/libburn"
+source = f"http://files.libburnia-project.org/releases/libburn-{pkgver}.tar.gz"
+sha256 = "7295491b4be5eeac5e7a3fb2067e236e2955ffdc6bbd45f546466edee321644b"
+
+
+@subpackage("libburn-devel")
+def _devel(self):
+    return self.default_devel()
