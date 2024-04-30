dd1379a4f3e741768f9f683d4fe553fb3d9685f3
q66 <q66@chimera-linux.org>
2024-04-30 18:17:24 +0200

    main/gnome: merge -core into the template

--- main/gnome/template.py
+++ main/gnome/template.py
@@ -1,14 +1,51 @@
 pkgname = "gnome"
-pkgver = "46.0"
-pkgrel = 1
+pkgver = "46.1"
+pkgrel = 0
 build_style = "meta"
-depends = [
-    f"gnome-core~{pkgver}",
-]
+depends = [f"gnome-core~{pkgver}"]
 pkgdesc = "GNOME desktop environment (session and apps)"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:meta"
 url = "https://www.gnome.org"
+
+
+@subpackage("gnome-core")
+def _core(self):
+    self.pkgdesc = "GNOME desktop environment (minimal session)"
+    # a bunch of stuff is indirect dependencies we don't need to list
+    # some of them are here anyway, for clarity but no other purpose
+    self.depends = [
+        "adwaita-icon-theme",
+        "dconf",
+        "desktop-file-utils",
+        "fonts-cantarell-otf",
+        "fonts-source-code-pro-otf",
+        "gdm",
+        "gjs",
+        "glib-networking",
+        "gnome-backgrounds-gnome",
+        "gnome-bluetooth",
+        "gnome-color-manager",
+        "gnome-control-center",
+        "gnome-desktop",
+        "gnome-initial-setup",
+        "gnome-keyring",
+        "gnome-online-accounts",
+        "gnome-session",
+        "gnome-settings-daemon",
+        "gnome-shell",
+        "gnome-video-effects",
+        "gsettings-desktop-schemas",
+        "gvfs",
+        "mutter",
+        "nautilus",
+        "tracker",
+        "tracker-miners",
+        "yelp",
+        "xdg-desktop-portal-gnome",
+        "xdg-user-dirs-gtk",
+    ]
+    return []
 
 
 @subpackage("gnome-apps")
