c4c6b9fb0da93f8af42074e44ad06bb441eab54b
ttyyls <contact@behri.org>
2024-04-30 13:11:01 +0100

    user/git-extras: update to 7.2.0

--- user/git-extras/template.py
+++ user/git-extras/template.py
@@ -1,19 +1,28 @@
 pkgname = "git-extras"
-pkgver = "7.1.0"
+pkgver = "7.2.0"
 pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_install_args = [
+    "COMPL_DIR=$(DESTDIR)/usr/share/bash-completion/completions"
+]
+hostmakedepends = ["git", "gmake", "bash"]
 depends = ["git", "bash"]
 pkgdesc = "Extra Git utilities"
 maintainer = "ttyyls <contact@behri.org>"
 license = "MIT"
 url = "https://github.com/tj/git-extras"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "e5c855361d2f1ec1be6ee601247153d9c8c04a221949b6ec3903b32fa736f542"
+sha256 = "f570f19b9e3407e909cb98d0536c6e0b54987404a0a053903a54b81680c347f1"
+# no tests defined
+options = ["!check"]
 
 
-def do_install(self):
-    self.install_files("bin", "usr")
-    self.install_man("man/*.1", glob=True)
-    self.install_completion("etc/bash_completion.sh", "bash")
+def do_build(self):
+    pass
+
+
+def post_install(self):
     self.install_completion("etc/git-extras-completion.zsh", "zsh")
     self.install_completion("etc/git-extras.fish", "fish")
     self.install_license("LICENSE")
