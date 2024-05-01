37078ea0ae2ef7b9873a9845a262b06a4110f8a0
psykose <alice@ayaya.dev>
2024-05-01 16:36:05 +0000

    main/nano: update to 8.0

--- main/nano/template.py
+++ main/nano/template.py
@@ -1,20 +1,19 @@
 pkgname = "nano"
-pkgver = "7.2"
+pkgver = "8.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-utf8"]
+# fails to regen
+configure_gen = []
 makedepends = ["ncurses-devel", "file-devel", "linux-headers"]
 pkgdesc = "GNU nano text editor"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.nano-editor.org"
 source = f"https://www.nano-editor.org/dist/v{pkgver[0]}/nano-{pkgver}.tar.xz"
-sha256 = "86f3442768bd2873cec693f83cdf80b4b444ad3cc14760b74361474fc87a4526"
+sha256 = "c17f43fc0e37336b33ee50a209c701d5beb808adc2d9f089ca831b40539c9ac4"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_file("syntax/nanorc.nanorc", "usr/share/examples/nano")
-
-
-configure_gen = []
