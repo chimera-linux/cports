8c67b5b31e7990de70f1181016132a057285270b
psykose <alice@ayaya.dev>
2024-05-02 15:30:25 +0000

    contrib/nodejs: update to 22.1.0

--- contrib/nodejs/template.py
+++ contrib/nodejs/template.py
@@ -1,5 +1,5 @@
 pkgname = "nodejs"
-pkgver = "22.0.0"
+pkgver = "22.1.0"
 pkgrel = 0
 build_style = "configure"
 configure_args = [
@@ -39,7 +39,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://nodejs.org"
 source = f"{url}/dist/v{pkgver}/node-v{pkgver}.tar.gz"
-sha256 = "47cd57486564a8cb4728ec01f1d98b510c6547e19e96a7f7583cf882d803d0c6"
+sha256 = "1179664c69612f3f67df6fac32008df31f92e28c61d57c1e3297b84ea8879b5b"
 debug_level = 1  # allow LTO build to not run out of mem
 hardening = ["!cfi"]  # TODO
 options = ["!cross"]
