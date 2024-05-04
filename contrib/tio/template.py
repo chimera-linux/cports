57845d584f8f66996c107ad8d0df32ac39f4e0e4
psykose <alice@ayaya.dev>
2024-05-04 11:38:05 +0000

    contrib/tio: update to 3.1

--- contrib/tio/template.py
+++ contrib/tio/template.py
@@ -1,5 +1,5 @@
 pkgname = "tio"
-pkgver = "3.0"
+pkgver = "3.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -9,7 +9,6 @@ hostmakedepends = [
 makedepends = [
     "bash-completion",
     "glib-devel",
-    "inih-devel",
     "linux-headers",
     "lua5.4-devel",
 ]
@@ -18,4 +17,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/tio/tio"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "82a0948bbbd9eff87e86e795c60994cdb1263a1334f1db68f061a0704e600b6d"
+sha256 = "ebb9f66b0d7c98ff534b604f7e02361a79cf33b095364efca9d4958c5e29e484"
