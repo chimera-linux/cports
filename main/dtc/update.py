9dd36d6353d7e35eae8e53484aac08b47b104d30
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-05 20:35:33 +0300

    main/*: fix git.kernel.org update-check

---
+++ main/dtc/update.py
@@ -0,0 +1,2 @@
+url = "https://git.kernel.org/pub/scm/utils/dtc/dtc.git/refs/tags"
+pattern = r"\?h=v(\d.+)(?='>v\d)"
---
+++ main/libtraceevent/update.py
@@ -0,0 +1,2 @@
+url = "https://git.kernel.org/pub/scm/libs/libtrace/libtraceevent.git/refs/tags"
+pattern = r"\?h=libtraceevent-(\d.+)(?='>libtraceevent-\d)"
