11eca7ded895a98df104bb33b2193ceb0e0c4478
triallax <triallax@tutanota.com>
2024-05-06 12:15:18 +0100

    contrib/libayatana-indicator: new package (0.9.4)

---
+++ contrib/libayatana-indicator/template.py
@@ -0,0 +1,25 @@
+pkgname = "libayatana-indicator"
+pkgver = "0.9.4"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DENABLE_TESTS=ON"]
+# Tests run xvfb-run on their own but that fails for some reason
+make_check_wrapper = ["xvfb-run"]
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = [
+    "ayatana-ido-devel",
+    "glib-devel",
+    "gtk+3-devel",
+]
+checkdepends = ["bash", "dbus-test-runner", "xserver-xorg-xvfb"]
+pkgdesc = "Ayatana Indicators Shared Library"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-3.0-only"
+url = "https://github.com/AyatanaIndicators/libayatana-indicator"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "a18d3c682e29afd77db24366f8475b26bda22b0e16ff569a2ec71cd6eb4eac95"
+
+
+@subpackage("libayatana-indicator-devel")
+def _devel(self):
+    return self.default_devel()
