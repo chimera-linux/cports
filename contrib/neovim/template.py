569dcd83adf7f3418aa80001f25886f502d4616f
psykose <alice@ayaya.dev>
2024-05-06 10:19:28 +0000

    contrib/neovim: rebuild against tree-sitter

--- contrib/neovim/template.py
+++ contrib/neovim/template.py
@@ -1,7 +1,7 @@
 # nb: neovim requires either lua5.1 or luaJIT (a mess)
 pkgname = "neovim"
 pkgver = "0.9.5"
-pkgrel = 2
+pkgrel = 3
 build_style = "cmake"
 configure_args = [
     "-DCMAKE_BUILD_TYPE=RelWithDebInfo",
