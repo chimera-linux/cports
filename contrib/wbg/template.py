d8600cae87028949a8e122166ac50076682250fc
Ogromny <ogromnycoding@gmail.com>
2024-05-07 14:08:44 +0200

    contrib/wbg: new package (1.2.0)

---
+++ contrib/wbg/template.py
@@ -0,0 +1,28 @@
+pkgname = "wbg"
+pkgver = "1.2.0"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+    "wayland-progs",
+]
+makedepends = [
+    "libjpeg-turbo-devel",
+    "libpng-devel",
+    "libwebp-devel",
+    "pixman-devel",
+    "tllist",
+    "wayland-devel",
+    "wayland-protocols",
+]
+pkgdesc = "Wallpaper application for Wayland compositors"
+maintainer = "ogromny <ogromnycoding@gmail.com>"
+license = "MIT"
+url = "https://codeberg.org/dnkl/wbg"
+source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "a176576f3ca8d0494a5ce60a06970c093dcb46020ca678e5a327034ad8477a5e"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
