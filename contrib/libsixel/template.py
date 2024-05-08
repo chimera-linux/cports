6b1dc0b4fe9e8328bac47e4dc37ca1badbe19903
Matthias Nyman <natthias@proton.me>
2024-05-08 21:57:32 +0200

    contrib/libsixel: new package (1.10.3)

---
+++ contrib/libsixel/template.py
@@ -0,0 +1,33 @@
+pkgname = "libsixel"
+pkgver = "1.10.3"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+]
+makedepends = [
+    "gdk-pixbuf-devel",
+    "libjpeg-turbo-devel",
+    "libpng-devel",
+]
+pkgdesc = "SIXEL encoder/decoder"
+maintainer = "natthias <natthias@proton.me>"
+license = "MIT"
+url = "https://github.com/libsixel/libsixel"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "028552eb8f2a37c6effda88ee5e8f6d87b5d9601182ddec784a9728865f821e0"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("libsixel-devel")
+def _devel(self):
+    return self.default_devel()
+
+
+@subpackage("libsixel-progs")
+def _progs(self):
+    return self.default_progs()
