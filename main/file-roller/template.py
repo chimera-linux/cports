4203e20683512a1490375fd5a93941d0e71d539d
psykose <alice@ayaya.dev>
2024-05-08 16:20:21 +0000

    main/file-roller: update to 44.2

--- main/file-roller/template.py
+++ main/file-roller/template.py
@@ -1,29 +1,29 @@
 pkgname = "file-roller"
-pkgver = "44.1"
+pkgver = "44.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
-    "meson",
-    "pkgconf",
+    "desktop-file-utils",
     "gettext",
     "glib-devel",
-    "desktop-file-utils",
-    "itstool",
     "gtk-update-icon-cache",
+    "itstool",
+    "meson",
+    "pkgconf",
 ]
 makedepends = [
-    "gtk4-devel",
     "glib-devel",
+    "gtk4-devel",
+    "json-glib-devel",
+    "libadwaita-devel",
     "libarchive-devel",
     "libnotify-devel",
-    "nautilus-devel",
-    "libadwaita-devel",
     "libportal-devel",
-    "json-glib-devel",
+    "nautilus-devel",
 ]
 pkgdesc = "GNOME archiver frontend"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/FileRoller"
 source = f"https://gitlab.gnome.org/GNOME/file-roller/-/archive/{pkgver}/file-roller-{pkgver}.tar.gz"
-sha256 = "70f53c6994d961b981f077377a2076461c0973eaee1c51d194d937f1ded85443"
+sha256 = "68b8fdef6ab1d999d21ccc8d29f08c6bf5fde8a5d56164dec9fc8a0649936023"
