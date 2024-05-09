33f2d24abc3ca40ea5d82872a5810460140cdfb6
psykose <alice@ayaya.dev>
2024-05-09 23:51:19 +0000

    contrib/chromium: update to 124.0.6367.201

--- contrib/chromium/template.py
+++ contrib/chromium/template.py
@@ -1,6 +1,6 @@
 pkgname = "chromium"
 # https://chromiumdash.appspot.com/releases?platform=Linux
-pkgver = "124.0.6367.118"
+pkgver = "124.0.6367.201"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "x86_64"]
 configure_args = [
@@ -139,7 +139,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://www.chromium.org"
 source = f"https://commondatastorage.googleapis.com/chromium-browser-official/chromium-{pkgver}.tar.xz"
-sha256 = "8aa5a14aad1234b48b568da9ef23d6e0b1b72d7f4ca5c4039462e54e6ad45d96"
+sha256 = "11773c4cfce4b39ae1bbe711b2907cb86e9aa299b7c6a71656edb876f0c85992"
 debug_level = 0
 tool_flags = {
     "CFLAGS": [
