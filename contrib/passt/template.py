23c8d5f161a64947e395dbb52f48a717a0a72d37
psykose <alice@ayaya.dev>
2024-05-10 15:44:43 +0000

    contrib/passt: update to 2024_05_10.7288448

--- contrib/passt/template.py
+++ contrib/passt/template.py
@@ -1,5 +1,5 @@
 pkgname = "passt"
-_pkgver = "2024_04_26.d03c4e2"
+_pkgver = "2024_05_10.7288448"
 # yeardate only
 pkgver = _pkgver.split(".")[0].replace("_", ".")
 pkgrel = 0
@@ -15,7 +15,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-3-Clause AND GPL-2.0-or-later"
 url = "https://passt.top/passt/about"
 source = f"https://passt.top/passt/snapshot/passt-{_pkgver}.tar.zst"
-sha256 = "7da888be4a17c416faaa20a22e98149b5286fa1b57eda1cce7c466976154687d"
+sha256 = "9a325f22019b9c70074165c546dbab4cce49f339261b981a37dff4d90f43f7ed"
 # tries to pass this via __attribute(optimise) for some stuff but that is ignored
 tool_flags = {"CFLAGS": ["-fno-strict-aliasing"]}
 hardening = ["vis", "cfi"]
