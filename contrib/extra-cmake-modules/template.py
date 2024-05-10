35f664ec1bd2ab3a6bab3d4ec8a915ff7ea0a4e0
psykose <alice@ayaya.dev>
2024-05-10 11:59:10 +0000

    contrib/extra-cmake-modules: update to 6.2.0

--- contrib/extra-cmake-modules/template.py
+++ contrib/extra-cmake-modules/template.py
@@ -1,5 +1,5 @@
 pkgname = "extra-cmake-modules"
-pkgver = "6.1.0"
+pkgver = "6.2.0"
 pkgrel = 0
 build_style = "cmake"
 # who knows why these fail
@@ -23,7 +23,7 @@ maintainer = "aurelia <git@elia.garden>"
 license = "BSD-3-Clause"
 url = "https://invent.kde.org/frameworks/extra-cmake-modules"
 source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/extra-cmake-modules-{pkgver}.tar.xz"
-sha256 = "76c9edf00807e6cf8d4ae35f5195b4bc3fe94648d976fef532bf7f97d86388bd"
+sha256 = "6374bfa0dded8be265c702acd5de11eecd2851c625b93e1c87d8d0f5f1a8ebe1"
 
 
 def post_install(self):
