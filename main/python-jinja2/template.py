3290f6114a60aa416acf3d60a00471fd970ae3c0
psykose <alice@ayaya.dev>
2024-05-10 23:52:00 +0000

    main/python-jinja2: update to 3.1.4

--- main/python-jinja2/template.py
+++ main/python-jinja2/template.py
@@ -1,24 +1,23 @@
 pkgname = "python-jinja2"
-pkgver = "3.1.3"
+pkgver = "3.1.4"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
+    "python-flit_core",
     "python-installer",
-    "python-setuptools",
-    "python-wheel",
 ]
 checkdepends = ["python-pytest", "python-markupsafe"]
 depends = ["python", "python-markupsafe"]
 pkgdesc = "Python template engine"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
-url = "http://jinja.pocoo.org"
-source = f"$(PYPI_SITE)/J/Jinja2/Jinja2-{pkgver}.tar.gz"
-sha256 = "ac8bd6544d4bb2c9792bf3a159e80bba8fda7f07e81bc3aed565432d5925ba90"
+url = "https://jinja.palletsprojects.com"
+source = f"$(PYPI_SITE)/J/Jinja2/jinja2-{pkgver}.tar.gz"
+sha256 = "4a3aee7acbbe7303aede8e9648d13b8bf88a429282aa6122a993f0ac800cb369"
 # dependency of pytest
 options = ["!check"]
 
 
 def post_install(self):
-    self.install_license("LICENSE.rst")
+    self.install_license("LICENSE.txt")
