c6e12cf4605af3503bd8bfba8b12044ed526dfef
psykose <alice@ayaya.dev>
2024-05-11 01:10:36 +0000

    contrib/python-btrfs: new package (14.1)

---
+++ contrib/python-btrfs/template.py
@@ -0,0 +1,18 @@
+pkgname = "python-btrfs"
+pkgver = "14.1"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+pkgdesc = "Python module and utilities for interacting with btrfs filesystems"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-3.0-or-later"
+url = "https://github.com/knorrie/python-btrfs"
+source = f"$(PYPI_SITE)/b/btrfs/btrfs-{pkgver}.tar.gz"
+sha256 = "04f28fc13df78bc7d060991465b9c9f2740d6e62b0d03aba7a46fd9abedac046"
+# no tests
+options = ["!check"]
