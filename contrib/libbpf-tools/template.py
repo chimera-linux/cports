dea16524e64a63c94148d1b4a0b11cd389448ea4
psykose <alice@ayaya.dev>
2024-05-12 19:18:49 +0000

    contrib/libbpf-tools: new package (0.30.0)

---
+++ contrib/libbpf-tools/template.py
@@ -0,0 +1,33 @@
+pkgname = "libbpf-tools"
+pkgver = "0.30.0"
+pkgrel = 0
+build_wrksrc = "libbpf-tools"
+build_style = "makefile"
+make_cmd = "gmake"
+make_build_args = ["prefix=/usr", "V=1", "APP_PREFIX=bpf-"]
+make_install_args = list(make_build_args)
+hostmakedepends = [
+    "bpftool",
+    "gmake",
+    "pkgconf",
+]
+makedepends = [
+    "argp-standalone",
+    "libbpf-devel",
+    "linux-headers",
+]
+pkgdesc = "Standalone eBPF programs from BCC"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-only OR BSD-2-Clause"
+url = "https://github.com/iovisor/bcc/tree/master/libbpf-tools"
+source = f"https://github.com/iovisor/bcc/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "d113f842965fd84f8bf2f3e9dda73a2cae59a4d27bec3fa87d0b57ee99b58273"
+tool_flags = {
+    # see libbpf comment about bpf headers
+    "CFLAGS": ["-I/usr/include/bpf/uapi"],
+    "CXXFLAGS": ["-I/usr/include/bpf/uapi"],
+    "LDFLAGS": ["-largp"],
+}
+# check: no tests
+# distlicense: no actual bsd license in the folders, just headers
+options = ["!check", "!distlicense"]
