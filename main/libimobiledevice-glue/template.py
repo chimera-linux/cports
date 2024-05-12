ac4fe9e90419710afda7fb73d570e590aa646cd3
psykose <alice@ayaya.dev>
2024-05-12 14:40:53 +0000

    main/libimobiledevice-glue: new package (1.2.0)

---
+++ main/libimobiledevice-glue/template.py
@@ -0,0 +1,21 @@
+pkgname = "libimobiledevice-glue"
+pkgver = "1.2.0"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = [
+    "automake",
+    "libtool",
+    "pkgconf",
+]
+makedepends = ["libplist-devel"]
+pkgdesc = "Common code library for the libimobiledevice project"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-or-later"
+url = "https://libimobiledevice.org"
+source = f"https://github.com/libimobiledevice/libimobiledevice-glue/releases/download/{pkgver}/libimobiledevice-glue-{pkgver}.tar.bz2"
+sha256 = "ff9cbc240c9780edfa43914a057b86362054053721b65fb04f54a25023b92b62"
+
+
+@subpackage("libimobiledevice-glue-devel")
+def _devel(self):
+    return self.default_devel()
