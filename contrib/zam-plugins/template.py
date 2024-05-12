a23bc4d75410cd56f99a970f5b0a5c057f192032
psykose <alice@ayaya.dev>
2024-05-12 14:12:33 +0000

    contrib/zam-plugins: update to 4.3

--- contrib/zam-plugins/template.py
+++ contrib/zam-plugins/template.py
@@ -1,5 +1,5 @@
 pkgname = "zam-plugins"
-pkgver = "4.2"
+pkgver = "4.3"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -23,10 +23,10 @@ pkgdesc = "Collection of audio plugins for high-quality audio processing"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/zamaudio/zam-plugins"
-_dpf = "63dfb7610bc37dee69f4a303f3e3362529d95f24"
-_pugl = "2e98e220b5b860c1c8cd5809fad61baf27380a37"
+_dpf = "077fcf5758ed6038bfe6e7aee1e407aa02e807b2"
+_pugl = "e33b2f6b0cea6d6263990aa9abe6a69fdfba5973"
 source = [
-    f"https://github.com/zamaudio/zam-plugins/archive/refs/tags/{pkgver}.tar.gz",
+    f"{url}/archive/refs/tags/{pkgver}.tar.gz",
     f"https://github.com/DISTRHO/DPF/archive/{_dpf}.tar.gz",
     f"https://github.com/DISTRHO/pugl/archive/{_pugl}.tar.gz",
 ]
@@ -36,9 +36,9 @@ source_paths = [
     "dpf/dgl/src/pugl-upstream",
 ]
 sha256 = [
-    "d39fa1d6c279acdae075eb789d53803707e0d7116fcc7b12a0638e2f4866dbc1",
-    "499a2d27f935931cdef7e5b14e13b42adcd783cdc4e9eb13598be59570461877",
-    "7bd49fec34c39d1381c22eb8a1eed20332c104ded414b8d7b4779c5cdf112fbc",
+    "5c681e2830f1f5400364a4a5c7df72c95a3a5e81aa82c87a4cbf387511752857",
+    "c4e8ca5ef8637dc9c6fdaa7ac88eee8227c46d91cf30f781b79e1b471fced50c",
+    "7e813d35d619a0ba3e790be5e102cfd2dc7c1f7b99333c9aa0a8661ca8419e02",
 ]
 # FIXME: cfi
 hardening = ["vis"]
