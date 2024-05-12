24352378cab33ee6dc6ac31a5f701df6de18a65d
ttyyls <contact@behri.org>
2024-05-12 22:47:08 +0100

    contrib/kakoune: update to 2024.05.09

--- contrib/kakoune/template.py
+++ contrib/kakoune/template.py
@@ -1,5 +1,5 @@
 pkgname = "kakoune"
-pkgver = "2023.08.05"
+pkgver = "2024.05.09"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -11,5 +11,5 @@ maintainer = "superwhiskers <whiskerdev@protonmail.com>"
 license = "Unlicense"
 url = "https://kakoune.org"
 source = f"https://github.com/mawww/kakoune/releases/download/v{pkgver}/kakoune-{pkgver}.tar.bz2"
-sha256 = "3e45151e0addd3500de2d6a29b5aacf2267c42bb256d44a782e73defb29cda5c"
+sha256 = "2190bddfd3af590c0593c38537088976547506f47bd6eb6c0e22350dbd16a229"
 hardening = ["vis", "cfi"]
