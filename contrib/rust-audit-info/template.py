774bf90c3d44d01757f45006df3ebd9baae677bc
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-05-13 23:27:25 +0200

    contrib/rust-audit-info: update to 0.5.4

--- contrib/rust-audit-info/template.py
+++ contrib/rust-audit-info/template.py
@@ -1,5 +1,5 @@
 pkgname = "rust-audit-info"
-pkgver = "0.5.2"
+pkgver = "0.5.4"
 pkgrel = 0
 build_wrksrc = "rust-audit-info"
 build_style = "cargo"
@@ -10,7 +10,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "MIT OR Apache-2.0"
 url = "https://github.com/rust-secure-code/cargo-auditable/tree/master/rust-audit-info"
 source = f"https://github.com/rust-secure-code/cargo-auditable/archive/refs/tags/rust-audit-info/v{pkgver}.tar.gz"
-sha256 = "d9bf684d1954db44e2f254e2fff5d204cb12e652ae95941abed9e10812786046"
+sha256 = "d9d53352b5f2dc34a93b1ae6408cdd50085f40e24d832af6938e0c3b7c906ff5"
 
 
 def post_install(self):
