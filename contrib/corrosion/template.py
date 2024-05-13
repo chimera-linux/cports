fcc7960095e1bb190c6e4295027b8a77f569cb10
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-05-13 23:14:36 +0200

    contrib/corrosion: update to 0.5.0

--- contrib/corrosion/template.py
+++ contrib/corrosion/template.py
@@ -1,14 +1,14 @@
 pkgname = "corrosion"
-pkgver = "0.4.9"
+pkgver = "0.5.0"
 pkgrel = 0
 build_style = "cmake"
-hostmakedepends = ["cmake", "ninja", "cargo"]
+hostmakedepends = ["cmake", "ninja", "cargo-auditable"]
 pkgdesc = "Tool for integrating Rust into an existing CMake project"
 maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "MIT"
 url = "https://github.com/corrosion-rs/corrosion"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "3346b21c4986c077988e10a19b8737a7b56f6f84ef8e800058b58d1f138e8fa9"
+sha256 = "bbe0d4a31cef91b890134af82789fb6e8ecc33270472beea9cecb8f2b7b7ed65"
 # Checks require rustup, because they support specifying specific toolchains
 options = ["!check"]
 
