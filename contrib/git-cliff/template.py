819e2deb2005d4e210a3dccae8a22a7b26e89543
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-05-13 23:39:00 +0200

    contrib/git-cliff: update to 2.2.2

--- contrib/git-cliff/template.py
+++ contrib/git-cliff/template.py
@@ -1,21 +1,23 @@
 pkgname = "git-cliff"
-pkgver = "2.2.1"
+pkgver = "2.2.2"
 pkgrel = 0
 build_style = "cargo"
 make_check_args = [
     "--",
+    "--skip=repo::test::get_latest_commit",
+    "--skip=repo::test::get_latest_tag",
     "--skip=repo::test::git_log",
     "--skip=repo::test::git_tags",
     "--skip=repo::test::git_upstream_remote",
 ]
 hostmakedepends = ["cargo-auditable", "pkgconf"]
-makedepends = ["libgit2-devel"]
+makedepends = ["libgit2-devel", "zstd-devel"]
 pkgdesc = "Changelog generator for conventional commits"
 maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/orhun/git-cliff"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "8573c4dc28fd6d6c1e9be7156193c13d177af093a060ae9e3bd4cd60ff3e05c4"
+sha256 = "c47b517907cfede556c50d790fddc07039c7ab477a2a059dde57090c97adfbac"
 # generates manpages/completions with host bins
 options = ["!cross"]
 
