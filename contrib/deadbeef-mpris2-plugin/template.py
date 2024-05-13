2a4df39a4d75504e13f036e1f4c771b178ddf725
psykose <alice@ayaya.dev>
2024-05-13 19:42:55 +0000

    contrib/deadbeef-mpris2-plugin: new package (1.16)

---
+++ contrib/deadbeef-mpris2-plugin/template.py
@@ -0,0 +1,22 @@
+pkgname = "deadbeef-mpris2-plugin"
+pkgver = "1.16"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--disable-static"]
+hostmakedepends = [
+    "automake",
+    "libtool",
+    "pkgconf",
+]
+makedepends = [
+    "deadbeef-devel",
+    "glib-devel",
+]
+pkgdesc = "Mpris plugin for deadbeef"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-only"
+url = "https://github.com/DeaDBeeF-Player/deadbeef-mpris2-plugin"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "3218e2adeea56267552f561316d1e243965b3a03481ab4fdbc535b54a97f4865"
+# breaks symbols
+hardening = ["!vis"]
