ed234790c441a4b65e93a39f91a90549042db652
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-05-14 12:50:31 +0200

    contrib/fonts-nerd-fonts: new package (3.2.1)

---
+++ contrib/fonts-nerd-fonts/template.py
@@ -0,0 +1,205 @@
+pkgname = "fonts-nerd-fonts"
+pkgver = "3.2.1"
+pkgrel = 0
+pkgdesc = "Nerd Fonts"
+maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
+license = "custom:meta"
+url = "https://www.nerdfonts.com"
+# Filled later in the template
+source = []
+source_paths = []
+sha256 = [
+    "cf165059645c02212fbdb5a27858056772d12a762468a0971c5bed3de88d0427",
+    "6168e416a4331e8d59d5436d9a3c8170f98ee044e2fe662cbf1d7bea612f927d",
+    "c307d029f638a411834972569317a04b9d75e968a53b12e41c787db064a6bef7",
+    "1e4b7f7c8528c9d43760eacdb5d423ad272f028cad5958e594a30486dce409d3",
+    "20f3bc015e46e4fe9f57cfe495eaa167b796e047df4f98676fe16649aba4ffb8",
+    "d71be5a39c772a58d2592e490784298753573ee47836633ade381f1eeee39c72",
+    "432c4a9080038554a94a46f435cb1aca3d58e23deb445e5c39d2c740a9edac84",
+    "5328418c755f076363e46960f311c734c0939167ab02eb718a8fb7bcc1bbd2e8",
+    "97e2774a01a151fc2f711f018831d24777472daf0f2822230ce20a7ac227a041",
+    "5af79aaebec3aa474c801e48aa2cb901cf1e9c03cbb3b6c189fed439e867e6e9",
+    "9672540e8707d221fe48fa5da49aec5a605b2d38a85c9fed959eb76990b33367",
+    "45d29f5be8add78ae271550d542387f4c98bbbe62e395a3646bc67f66f9d3c5c",
+    "dc0835a985f19491526fb5773ad7d9646bbdb962cda5076615b686427fb565dc",
+    "173f5a8fe5bb0a4e7db61ac9a77e97c7b5071b9b2a9f8dbaf8432311518a5f4a",
+    "b8f14c715bdcce4afafd9c4e57acba5b9d5c1c3357f054a505716187ae1e98b1",
+    "c01665970a43c13b27ea1fb9c85055d9c2369b3f9f2d1b37c08ffeaad7830cb0",
+    "6e7dd3ebdb82ee4a5b358801c082553692431e246c996dcbb3a737a28c78e4db",
+    "b94bde4d2e9ceb1f2c19b2846c1a9892797e4e15e9594303eb7088534244a18b",
+    "d3910065466618646baf9a621d385f8122c310e29d449462e5bd25bd300e0dd7",
+    "d1ca4f8bae74e089775a9ba6b2015ee79e03ec0970e76499f56131499524efa4",
+    "2d6db874195ac7ec4d9b396a9612e1e342b060d8aef0304b0b99c49c3b9a351d",
+    "def4b29f7aa0620a7fa12edb28197a2697680b5e21636fa3dcc602e08ae13bc4",
+    "c0224b12e3da90550bb1c42a9149346326ebbc9f41cf83a5e6c8944e57772bc5",
+    "0f247f19afece755de1a1b7b2eae38d48023ce4436323ab51f571d38314c6c73",
+    "44a4ce529f51dc5f067a10b851acd06a1dc7ea7d6af5ba8a420e057232c8d948",
+    "7ab613fac9d5931bd32d8e5a438ae0cb17536317e171f28ee5ddb8a171941041",
+    "89b9023f3f6d5e4b8040022ff29b36faee440cf49015fda163215c922569b5f3",
+    "4fb0ff16a5e587e65f59e55d08283729de700f0c0bf682ec1bef18ab51264ada",
+    "23552caf1b6075858756de2e8f3a3fbb9799b478882f3f7afeefa8ca7f4601ae",
+    "85b0b39b30a9ddd89462b0a7a732b461de04f667dc01a5e987bf0c2accbb576f",
+    "2c9f5a1117e1b5a640287081680837e7b870c94868627766483011ff0b80c3f7",
+    "69e0a42a6e8072b2a084f09a5056e857e9b620afd57d7d07c8d1da2cf4809b41",
+    "7a96f8cc30e57a426ffce3b844deb10d6325181b28df9b76735308f2d787dc2d",
+    "a016b61e49c395c5b1e5bd2c919ae668724a33f2ef14da3becf932da401a080d",
+    "1be123fa355a9acee79d19891c02499792f6a4e12ffa8973c6b1cb738465f337",
+    "b5d5e7b26c76f1cfe97a697716fdd682b18faf9f73883915c2b0c2d9fcc48cf5",
+    "3132ff085ce404a3c065054638f3dc68de27d07a5cc5379b90b2a248877971eb",
+    "6cf8822bc1ca18e34b06578c7499f380c019e6ffc883eed26df5f498dfcc4006",
+    "2282cb16651a377d5b084486a268189fdafd9067775f1f24ffaf5a36a2459501",
+    "9d0ad55cbd83967ca93e940f4951d41b23a44c892d9a4788af460d71f8bb81a4",
+    "a2720d1fbf7ebb51b6a0ab191de41d96c7594d61f02c582db2249ea30c1ce263",
+    "62484ea8f66ae3b75610b89c5d3258f8610844fec6d8402239c27a5042dccc7d",
+    "1abdaf5542b75a51d75de6373148a8e401b28eda88e6c90f243fb6114fd659ea",
+    "e5d2e8180d5df1fe473b598edac5f67ce38b1d906248936eb386dd332a338346",
+    "c7a5ff556ff4aad5381e8a57e0d17d79c591e1267f0a9c271b09306ac4581aba",
+    "eb3697dd13c5e50951023c0768538a2f70e1e4f22d94800a45779a6d1b49f7a3",
+    "5c60700788b093e0bfb30b2f59b52397f3fdc2cdf2b6fb9307a58b61e7ddfe74",
+    "4be621219ff5508c4176487b4fdc8cd0f6d8d8fceeb935f83df11a49dca3096f",
+    "96d43a8c53173b992537a1d34ae984f65a8999b7c8991a02c17d4b77497581fa",
+    "fcb796892b105c98ad05a29f184f09a02038970ff6a95f8cda082cd1b1b08cde",
+    "fe3bd035d43b943de17e6e0da1dfb0bd1dcc1b319e10c5e5925ab70436d48385",
+    "1a58c57b63f380441ea07d44780680844afe12dd299651b87db59f9f311702d1",
+    "e5f7db6999bf6dd6501d8933575caab49827486ce774c6c35a6e411da8c6cfba",
+    "3ccfd4d8d625c708e593e46ea0404536984f69f11c7a08b5a59eb9bbc9dfde3c",
+    "dbcfa99f4d41f05e4b59eb2197873515c035eff1a68800bf0f43db471ef7ba57",
+    "56a43a2729035f7fec17b68823a7db9ea06fcf9ec8d0b5e70d73ab79488a1b21",
+    "b6b9d7c604cc55d2c6859ad098371dfbfe540eb2d5aab4faad21f81149659338",
+    "b7a3eee24d2b0910f3a07705f1e053221d2535f07c5a13614c4dead2620e9978",
+    "658cc9506acf89b5f5dfc6b55eb9899b782dcf8d89f3d11c9abb2c4d52617f12",
+    "21b43c2314614733f5e9b4b4f3117edc3986d2b0f2a33409b111cc38360e8391",
+    "aa1b09dca861a306cd24c2c47fded88b63ae833ff0e815dad3642465583e300f",
+    "2df9c9e54e5a2bc19c2ffd1df7ee283e02ba2e9a5fda1067b4884375ae4b0b7a",
+]
+options = ["empty"]
+
+# curl https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/bin/scripts/lib/fonts.json | jq '[.fonts[] | {"package": .caskName, "name": .unpatchedName, "folder": .folderName, "spdx": (.licenseId | sub("OFL-1.1-no-RFN"; "OFL-1.1") | sub("OFL-1.1-RFN"; "OFL-1.1") | sub(" or "; " OR "))} | select(.spdx | test("LicenseRef") | not) | "(\'\(.package)\', \'\(.name)\', \'\(.folder)\', \'\(.spdx)\')"]' | sed -e 's/  "/  /g' -e 's/)"/)/g'
+_fonts = [
+    ("0xproto", "0xProto", "0xProto", "OFL-1.1"),
+    ("3270", "IBM 3270", "3270", "BSD-3-Clause"),
+    ("agave", "Agave", "Agave", "MIT"),
+    ("anonymice", "Anonymous Pro", "AnonymousPro", "OFL-1.1"),
+    ("arimo", "Arimo", "Arimo", "Apache-2.0"),
+    ("aurulent-sans-mono", "Aurulent Sans Mono", "AurulentSansMono", "OFL-1.1"),
+    ("bigblue-terminal", "BigBlue Terminal", "BigBlueTerminal", "CC-BY-SA-4.0"),
+    (
+        "bitstream-vera-sans-mono",
+        "Bitstream Vera Sans Mono",
+        "BitstreamVeraSansMono",
+        "Bitstream-Vera",
+    ),
+    ("blex-mono", "IBM Plex Mono", "IBMPlexMono", "OFL-1.1"),
+    ("caskaydia-cove", "Cascadia Code", "CascadiaCode", "OFL-1.1"),
+    ("caskaydia-mono", "Cascadia Mono", "CascadiaMono", "OFL-1.1"),
+    ("code-new-roman", "Code New Roman", "CodeNewRoman", "OFL-1.1"),
+    ("comic-shanns-mono", "Comic Shanns Mono", "ComicShannsMono", "MIT"),
+    ("commit-mono", "Commit Mono", "CommitMono", "OFL-1.1"),
+    ("cousine", "Cousine", "Cousine", "Apache-2.0"),
+    ("d2coding", "D2Coding", "D2Coding", "OFL-1.1"),
+    ("daddy-time-mono", "DaddyTimeMono", "DaddyTimeMono", "OFL-1.1"),
+    (
+        "dejavu-sans-mono",
+        "DejaVu Sans Mono",
+        "DejaVuSansMono",
+        "Bitstream-Vera",
+    ),
+    ("droid-sans-mono", "Droid Sans Mono", "DroidSansMono", "Apache-2.0"),
+    ("envy-code-r", "Envy Code R", "EnvyCodeR", "OFL-1.1"),
+    (
+        "fantasque-sans-mono",
+        "Fantasque Sans Mono",
+        "FantasqueSansMono",
+        "OFL-1.1",
+    ),
+    ("fira-code", "Fira Code", "FiraCode", "OFL-1.1"),
+    ("fira-mono", "Fira", "FiraMono", "OFL-1.1"),
+    ("geist-mono", "Geist Mono", "GeistMono", "OFL-1.1"),
+    ("go-mono", "Go Mono", "Go-Mono", "BSD-3-Clause-Clear"),
+    ("gohufont", "Gohu", "Gohu", "WTFPL"),
+    ("hack", "Hack", "Hack", "Bitstream-Vera AND MIT"),
+    ("hasklug", "Hasklig", "Hasklig", "OFL-1.1"),
+    ("hurmit", "Hermit", "Hermit", "OFL-1.1"),
+    ("im-writing", "iA Writer", "iA-Writer", "OFL-1.1"),
+    ("inconsolata", "Inconsolata", "Inconsolata", "OFL-1.1"),
+    ("inconsolata-go", "InconsolataGo", "InconsolataGo", "OFL-1.1"),
+    ("inconsolata-lgc", "Inconsolata LGC", "InconsolataLGC", "OFL-1.1"),
+    ("intone-mono", "Intel One Mono", "IntelOneMono", "OFL-1.1"),
+    ("iosevka", "Iosevka", "Iosevka", "OFL-1.1"),
+    ("iosevka-term", "Iosevka Term", "IosevkaTerm", "OFL-1.1"),
+    ("iosevka-term-slab", "Iosevka Term Slab", "IosevkaTermSlab", "OFL-1.1"),
+    ("jetbrains-mono", "JetBrains Mono", "JetBrainsMono", "OFL-1.1"),
+    ("lekton", "Lekton", "Lekton", "OFL-1.1"),
+    ("liberation", "Liberation Mono", "LiberationMono", "OFL-1.1"),
+    ("lilex", "Lilex", "Lilex", "OFL-1.1"),
+    ("martian-mono", "MartianMono", "MartianMono", "OFL-1.1"),
+    ("meslo-lg", "Meslo LG", "Meslo", "Apache-2.0"),
+    ("monaspace", "Monaspace", "Monaspace", "OFL-1.1"),
+    ("monoid", "Monoid", "Monoid", "MIT OR OFL-1.1"),
+    ("mononoki", "Mononoki", "Mononoki", "OFL-1.1"),
+    ("mplus", "MPlus", "MPlus", "OFL-1.1"),
+    ("noto", "Noto", "Noto", "OFL-1.1"),
+    ("open-dyslexic", "OpenDyslexic", "OpenDyslexic", "Bitstream-Vera"),
+    ("overpass", "Overpass", "Overpass", "OFL-1.1 OR LGPL-2.1-only"),
+    ("profont", "ProFont", "ProFont", "MIT"),
+    ("proggy-clean-tt", "ProggyCleanTT", "ProggyClean", "MIT"),
+    ("recursive-mono", "Recursive Mono", "Recursive", "OFL-1.1"),
+    ("roboto-mono", "Roboto Mono", "RobotoMono", "Apache-2.0"),
+    ("shure-tech-mono", "Share Tech Mono", "ShareTechMono", "OFL-1.1"),
+    ("sauce-code-pro", "Source Code Pro", "SourceCodePro", "OFL-1.1"),
+    ("space-mono", "Space Mono", "SpaceMono", "OFL-1.1"),
+    ("symbols-only", "Symbols Only", "NerdFontsSymbolsOnly", "MIT"),
+    ("terminess-ttf", "Terminus", "Terminus", "OFL-1.1"),
+    ("tinos", "Tinos", "Tinos", "Apache-2.0"),
+    ("victor-mono", "Victor Mono", "VictorMono", "OFL-1.1"),
+    ("zed-mono", "Zed Mono", "ZedMono", "OFL-1.1"),
+]
+
+
+def do_install(self):
+    for package, name, folder, spdx in _fonts:
+        self.install_file(
+            f"{folder}/*.*tf",
+            f"usr/share/fonts/nerd-{package}",
+            glob=True,
+        )
+        for file in [
+            "Apache License.txt",
+            "Bitstream Vera License.txt",
+            "COPYING-LICENSE",
+            "LICENCE.md",
+            "LICENSE",
+            "LICENSE.TXT",
+            "LICENSE.md",
+            "LICENSE.txt",
+            "LICENSE_OFL.txt",
+            "Licence.txt",
+            "OFL.txt",
+            "SIL Open Font License.txt",
+            "license.txt",
+        ]:
+            if (self.cwd / folder / file).exists():
+                self.install_license(
+                    f"{folder}/{file}", pkgname=f"fonts-nerd-{package}"
+                )
+
+
+def _font_subpackage(package, name, folder, spdx):
+    @subpackage(f"fonts-nerd-{package}")
+    def _spkg(self):
+        self.pkgdesc = f"{name} with Nerd Font patches"
+        self.license = spdx
+        self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+        return [
+            f"usr/share/fonts/nerd-{package}",
+            f"usr/share/licenses/fonts-nerd-{package}",
+        ]
+
+
+for _package, _name, _folder, _spdx in _fonts:
+    _download_name = _folder.replace(" ", "")
+    source.append(
+        f"https://github.com/ryanoasis/nerd-fonts/releases/download/v{pkgver}/{_download_name}.tar.xz"
+    )
+    source_paths.append(_folder)
+    _font_subpackage(_package, _name, _folder, _spdx)
