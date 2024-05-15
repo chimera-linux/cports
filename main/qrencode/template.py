de77237869a50ae1e3e619d0f0820dd69ec763cc
triallax <triallax@tutanota.com>
2024-05-15 23:53:23 +0100

    main/qrencode: enable int and generate configure

--- main/qrencode/template.py
+++ main/qrencode/template.py
@@ -1,18 +1,16 @@
 pkgname = "qrencode"
 pkgver = "4.1.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = ["--with-tests"]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 makedepends = ["libpng-devel"]
 pkgdesc = "Library for encoding QR codes"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://fukuchi.org/works/qrencode/index.html.en"
-source = f"https://fukuchi.org/works/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
+source = f"https://fukuchi.org/works/qrencode/qrencode-{pkgver}.tar.bz2"
 sha256 = "e455d9732f8041cf5b9c388e345a641fd15707860f928e94507b1961256a6923"
-# FIXME int
-hardening = ["!int"]
 
 
 @subpackage("qrencode-devel")
@@ -23,6 +21,3 @@ def _devel(self):
 @subpackage("qrencode-progs")
 def _progs(self):
     return self.default_progs()
-
-
-configure_gen = []
