21f989a8a1ac09fd7dd8214490420f9c38c927d4
psykose <alice@ayaya.dev>
2024-05-15 00:29:09 +0000

    contrib/cava: update to 0.10.2

--- contrib/cava/template.py
+++ contrib/cava/template.py
@@ -1,5 +1,5 @@
 pkgname = "cava"
-pkgver = "0.10.1"
+pkgver = "0.10.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -30,8 +30,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://github.com/karlstav/cava"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "a3a60814326fa34b54e93ce0b1e66460d55f1007e576c5152fd47024d9ceaff9"
-hardening = ["vis", "cfi"]
+sha256 = "853ee78729ed3501d0cdf9c1947967ad3bfe6526d66a029b4ddf9adaa6334d4f"
 
 
 def post_install(self):
