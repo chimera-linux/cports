a857fc91a870e38d3a8e5c0bd8911b584e005fd6
triallax <triallax@tutanota.com>
2024-05-15 23:00:35 +0100

    main/strace: enable int, generate configure, run tests

--- main/strace/template.py
+++ main/strace/template.py
@@ -1,6 +1,6 @@
 pkgname = "strace"
 pkgver = "6.9"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = [
     "--with-libdw",
@@ -9,7 +9,6 @@ configure_args = [
     "--disable-gcc-Werror",
 ]
 configure_env = {"CPPFLAGS": f"-I{self.profile().sysroot / 'usr/include'}"}
-configure_gen = []
 make_cmd = "gmake"
 # there's over a thousand tests and a ~50 of them
 # fail due to various reasons, mostly harmless ones
@@ -17,7 +16,7 @@ make_check_args = [
     "TESTS=bpf.gen epoll_pwait.gen getcpu.gen open.gen read-write.gen "
     "readlink.gen seccomp-filter.gen mmap ioctl caps readv"
 ]
-hostmakedepends = ["gmake"]
+hostmakedepends = ["automake", "gmake", "libtool"]
 makedepends = ["elfutils-devel", "linux-headers"]
 pkgdesc = "System call tracer"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -25,7 +24,4 @@ license = "LGPL-2.1-or-later"
 url = "https://strace.io"
 source = f"https://github.com/strace/strace/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "da189e990a82e3ca3a5a4631012f7ecfd489dab459854d82d8caf6a865c1356a"
-# FIXME int (breaks)
-hardening = ["vis", "cfi", "!int"]
-# something's weird and it gets stuck
-options = ["!check"]
+hardening = ["vis", "cfi"]
