23af5ef6da60edd4e8d7080d697491838ea94fe4
Erica Z <zerica@callcc.eu>
2024-05-15 13:36:09 +0200

    contrib/python-zstandard: new package (0.22.0)

---
+++ contrib/python-zstandard/template.py
@@ -0,0 +1,25 @@
+pkgname = "python-zstandard"
+pkgver = "0.22.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-cffi",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+makedepends = ["python-devel"]
+checkdepends = ["python-pytest"]
+pkgdesc = "Python bindings to the Zstandard compression library"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "BSD-3-Clause"
+url = "https://github.com/indygreg/python-zstandard"
+source = f"{url}/releases/download/{pkgver}/zstandard-{pkgver}.tar.gz"
+sha256 = "8226a33c542bcb54cd6bd0a366067b610b41713b64c9abec1bc4533d69f51e70"
+# tests fail to find internal modules
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
