83c92bedcf02c0d4a9d439a2cd4ec8e81760de2b
triallax <triallax@tutanota.com>
2024-05-16 11:47:49 +0100

    main/opus: enable int

--- main/opus/template.py
+++ main/opus/template.py
@@ -1,6 +1,6 @@
 pkgname = "opus"
 pkgver = "1.5.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
     "-Dcustom-modes=true",
@@ -13,12 +13,9 @@ pkgdesc = "Totally open, royalty-free, highly versatile audio codec"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://www.opus-codec.org"
-source = (
-    f"https://downloads.xiph.org/releases/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-)
+source = f"https://downloads.xiph.org/releases/opus/opus-{pkgver}.tar.gz"
 sha256 = "65c1d2f78b9f2fb20082c38cbe47c951ad5839345876e46941612ee87f9a7ce1"
-# FIXME int
-hardening = ["vis", "cfi", "!int"]
+hardening = ["vis", "cfi"]
 
 
 def post_install(self):
