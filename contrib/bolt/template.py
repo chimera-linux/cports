5e9ce5390086263d40e4f424dcc628d3b8f75892
psykose <alice@ayaya.dev>
2024-05-16 12:46:27 +0000

    contrib/bolt: update to 0.9.8

--- contrib/bolt/template.py
+++ contrib/bolt/template.py
@@ -1,5 +1,5 @@
 pkgname = "bolt"
-pkgver = "0.9.7"
+pkgver = "0.9.8"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dman=true", "-Dsystemd=false"]
@@ -19,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.freedesktop.org/bolt/bolt"
 source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "593c7e7d0ecebd7b9e2c3efabe33f8fec5501ff02ffd6d8563cfc14a91c31c1c"
+sha256 = "5a4306aa21ee398e1e9f2a5072748c9469c9360bf5edc7dcec2f12fc17be122e"
 
 
 def post_install(self):
