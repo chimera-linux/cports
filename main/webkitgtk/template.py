37f3b03119ead671b35f84a7a9910439fbcdf6ed
psykose <alice@ayaya.dev>
2024-05-16 12:47:47 +0000

    main/webkitgtk: update to 2.44.2

--- main/webkitgtk/template.py
+++ main/webkitgtk/template.py
@@ -1,6 +1,6 @@
 pkgname = "webkitgtk"
-pkgver = "2.44.1"
-pkgrel = 1
+pkgver = "2.44.2"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DPORT=GTK",
@@ -91,7 +91,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later AND BSD-2-Clause"
 url = "https://webkitgtk.org"
 source = f"{url}/releases/{pkgname}-{pkgver}.tar.xz"
-sha256 = "425b1459b0f04d0600c78d1abb5e7edfa3c060a420f8b231e9a6a2d5d29c5561"
+sha256 = "523f42c8ff24832add17631f6eaafe8f9303afe316ef1a7e1844b952a7f7521b"
 debug_level = 1  # otherwise LTO link runs out of memory + fat debuginfo
 tool_flags = {
     "CFLAGS": ["-DNDEBUG"],
