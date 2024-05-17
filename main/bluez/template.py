658a2c2d192068de463dfca2532a19c13b3d6ac7
psykose <alice@ayaya.dev>
2024-05-17 00:51:54 +0000

    main/bluez: update to 5.76

--- main/bluez/template.py
+++ main/bluez/template.py
@@ -1,6 +1,6 @@
 pkgname = "bluez"
-pkgver = "5.75"
-pkgrel = 1
+pkgver = "5.76"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--disable-deprecated",
@@ -40,7 +40,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "http://www.bluez.org"
 source = f"$(KERNEL_SITE)/bluetooth/{pkgname}-{pkgver}.tar.xz"
-sha256 = "988cb3c4551f6e3a667708a578f5ca9f93fc896508f98f08709be4f8ab033c2f"
+sha256 = "55e2c645909ad82d833c42ce85ec20434e0ef0070941b1eab73facdd240bbd63"
 tool_flags = {
     "CFLAGS": ["-Wno-deprecated-declarations"],
     # workaround for --gc-sections breaking in test files
