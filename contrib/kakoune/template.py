3d89bb0bc00fb6b002982cd7d3a4c0aee5618f9e
ttyyls <contact@behri.org>
2024-05-18 14:53:37 +0100

    user/kakoune: update to 2024.05.18

--- contrib/kakoune/template.py
+++ contrib/kakoune/template.py
@@ -1,15 +1,16 @@
 pkgname = "kakoune"
-pkgver = "2024.05.09"
+pkgver = "2024.05.18"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
 make_install_args = ["gzip_man=no"]
 make_use_env = True
 hostmakedepends = ["gmake"]
+checkdepends = ["git"]
 pkgdesc = "Modal code editor inspired by vim"
 maintainer = "superwhiskers <whiskerdev@protonmail.com>"
 license = "Unlicense"
 url = "https://kakoune.org"
 source = f"https://github.com/mawww/kakoune/releases/download/v{pkgver}/kakoune-{pkgver}.tar.bz2"
-sha256 = "2190bddfd3af590c0593c38537088976547506f47bd6eb6c0e22350dbd16a229"
+sha256 = "dae8ac2e61d21d9bcd10145aa70b421234309a7b0bc57fad91bc34dbae0cb9fa"
 hardening = ["vis", "cfi"]
