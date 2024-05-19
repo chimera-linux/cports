92fee0031f901122b28e8b243a7c9e3a21fcd013
psykose <alice@ayaya.dev>
2024-05-19 09:41:50 +0000

    main/nghttp2: update to 1.62.1

--- main/nghttp2/template.py
+++ main/nghttp2/template.py
@@ -1,17 +1,17 @@
 pkgname = "nghttp2"
-pkgver = "1.62.0"
+pkgver = "1.62.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
 hostmakedepends = ["pkgconf"]
 makedepends = [
-    "zlib-devel",
-    "openssl-devel",
-    "libevent-devel",
-    "libev-devel",
     "c-ares-devel",
     "jansson-devel",
+    "libev-devel",
+    "libevent-devel",
     "libxml2-devel",
+    "openssl-devel",
+    "zlib-devel",
 ]
 checkdepends = ["cppunit-devel"]
 pkgdesc = "HTTP/2 C Library"
@@ -19,7 +19,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://nghttp2.org"
 source = f"https://github.com/tatsuhiro-t/nghttp2/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "26798308fa0a12dabdb7ba8c77f74383019d3a0f1f36d25958b836af22474958"
+sha256 = "2345d4dc136fda28ce243e0bb21f2e7e8ef6293d62c799abbf6f633a6887af72"
 # FIXME cfi; reproduces in e.g. libsoup
 hardening = ["vis", "!cfi"]
 
