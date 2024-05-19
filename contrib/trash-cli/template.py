7ce99bb385d745056c27346d5f99434c8ded46bf
psykose <alice@ayaya.dev>
2024-05-19 02:02:25 +0000

    contrib/trash-cli: update to 0.24.4.17

--- contrib/trash-cli/template.py
+++ contrib/trash-cli/template.py
@@ -1,5 +1,5 @@
 pkgname = "trash-cli"
-pkgver = "0.23.11.10"
+pkgver = "0.24.4.17"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,7 +14,7 @@ maintainer = "Hugo Machet <mail@hmachet.com>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/andreafrancia/trash-cli"
 source = f"https://github.com/andreafrancia/trash-cli/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "166a5b725c323a801b884b7040877788eefa592866149cf5647ef1fd3a14ceb9"
+sha256 = "8476c926b0f4df5c09fe94a8573b04aeefff43d2e9ff0044c2caaf4c883e0e50"
 # unpackaged checkdepends
 options = ["!check"]
 
