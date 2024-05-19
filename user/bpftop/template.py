d06a4b3f2531b0b7dc117a6933658ebb09e4adb7
psykose <alice@ayaya.dev>
2024-05-19 22:03:07 +0000

    user/bpftop: update to 0.5.2

--- user/bpftop/template.py
+++ user/bpftop/template.py
@@ -1,6 +1,6 @@
 pkgname = "bpftop"
-pkgver = "0.5.1"
-pkgrel = 1
+pkgver = "0.5.2"
+pkgrel = 0
 build_style = "cargo"
 hostmakedepends = [
     "cargo-auditable",
@@ -15,7 +15,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/Netflix/bpftop"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "8457caf5ededba38aad01ed6317bd737a8079bbb26ca9a79cfdca5848a8c80f6"
+sha256 = "4033c77e99afd9297466c619f9049fddd9fcf26fda25fc9cc80c4a0c0ae7d40a"
 
 
 def do_install(self):
