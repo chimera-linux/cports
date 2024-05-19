85970faa2db18e468f090df052fb55df0671db64
psykose <alice@ayaya.dev>
2024-05-19 19:08:30 +0000

    main/file-roller: update to 44.3

--- main/file-roller/template.py
+++ main/file-roller/template.py
@@ -1,5 +1,5 @@
 pkgname = "file-roller"
-pkgver = "44.2"
+pkgver = "44.3"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -26,4 +26,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/FileRoller"
 source = f"https://gitlab.gnome.org/GNOME/file-roller/-/archive/{pkgver}/file-roller-{pkgver}.tar.gz"
-sha256 = "68b8fdef6ab1d999d21ccc8d29f08c6bf5fde8a5d56164dec9fc8a0649936023"
+sha256 = "c82f64b2a8a700d95b6d306a69c8a0b7dc5bc04995f620b7b7f8f4d1ca5ed829"
