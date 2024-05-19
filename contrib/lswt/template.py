59eb383e655fdf68a91ec99949a59bed3e0aba6f
Hugo Machet <mail@hmachet.com>
2024-05-19 10:37:57 +0200

    contrib/lswt: new package (2.0.0)

---
+++ contrib/lswt/template.py
@@ -0,0 +1,22 @@
+pkgname = "lswt"
+pkgver = "2.0.0"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+hostmakedepends = [
+    "gmake",
+    "wayland-progs",
+]
+makedepends = [
+    "linux-headers",
+    "wayland-devel",
+]
+pkgdesc = "List Wayland toplevels"
+maintainer = "Hugo Machet <mail@hmachet.com>"
+license = "GPL-3.0-only"
+url = "https://git.sr.ht/~leon_plickat/lswt"
+source = f"{url}/archive/v{pkgver}.tar.gz"
+sha256 = "8e23cc5c00bb715b0a1610938111cb76eb9efe1eea87408123620a8a7155e6ab"
+hardening = ["vis", "cfi"]
+# No tests exist
+options = ["!check"]
