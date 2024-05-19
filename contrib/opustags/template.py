b458fd46ac043e3095a62d0a1b0b08516e6bb6a7
psykose <alice@ayaya.dev>
2024-05-19 05:36:11 +0000

    contrib/opustags: update to 1.10.1

--- contrib/opustags/template.py
+++ contrib/opustags/template.py
@@ -1,5 +1,5 @@
 pkgname = "opustags"
-pkgver = "1.10.0"
+pkgver = "1.10.1"
 pkgrel = 0
 build_style = "cmake"
 make_check_target = "check"
@@ -19,7 +19,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-3-Clause"
 url = "https://github.com/fmang/opustags"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "761d3ac036205b9193bc6a610c3610c401d89fde2c955acfa7e26b1328e190b7"
+sha256 = "703096e9c41481e30ab90eefdd8fafc4c3a138998b3f8281aa4f023e7058bc86"
 hardening = ["vis", "cfi"]
 # TODO: unpackaged perl modules
 options = ["!check"]
