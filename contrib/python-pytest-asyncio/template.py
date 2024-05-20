e26400acc645da535206dbe2b489b6bc969f87ba
psykose <alice@ayaya.dev>
2024-05-20 17:13:00 +0000

    contrib/python-pytest-asyncio: update to 0.23.7

--- contrib/python-pytest-asyncio/template.py
+++ contrib/python-pytest-asyncio/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pytest-asyncio"
-pkgver = "0.23.6"
+pkgver = "0.23.7"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ pkgdesc = "Asyncio support for pytest"
 maintainer = "firefly-cpp <iztok@iztok.space>"
 license = "Apache-2.0"
 url = "https://github.com/pytest-dev/pytest-asyncio"
-source = f"$(PYPI_SITE)/p/pytest-asyncio/pytest-asyncio-{pkgver}.tar.gz"
-sha256 = "ffe523a89c1c222598c76856e76852b787504ddb72dd5d9b6617ffa8aa2cde5f"
+source = f"$(PYPI_SITE)/p/pytest-asyncio/pytest_asyncio-{pkgver}.tar.gz"
+sha256 = "5f5c72948f4c49e7db4f29f2521d4031f1c27f86e57b046126654083d4770268"
 # missing dependencies
 options = ["!check"]
