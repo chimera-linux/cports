091e2dd4811ce2d54d1c44c7b4710ff183d43f1c
triallax <triallax@tutanota.com>
2024-05-20 12:40:30 +0100

    main/libshumate: fix url, remove unneeded configure option

--- main/libshumate/template.py
+++ main/libshumate/template.py
@@ -1,8 +1,8 @@
 pkgname = "libshumate"
 pkgver = "1.2.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
-configure_args = ["-Dsysprof=disabled", "-Dgtk_doc=false"]
+configure_args = ["-Dgtk_doc=false"]
 make_check_env = {"GTK_A11Y": "none"}
 make_check_wrapper = ["weston-headless-run"]
 hostmakedepends = [
@@ -24,8 +24,8 @@ checkdepends = ["weston"]
 pkgdesc = "GTK library to display maps"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "LGPL-2.1-or-later"
-url = "https://gitlab.gnome.org/GNOME/Maps"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+url = "https://gitlab.gnome.org/GNOME/libshumate"
+source = f"$(GNOME_SITE)/libshumate/{pkgver[:-2]}/libshumate-{pkgver}.tar.xz"
 sha256 = "1105ee077e2147f2a039cddfa616fa5cb9438883dd940427e11699dcd6549c11"
 options = ["!cross"]
 
