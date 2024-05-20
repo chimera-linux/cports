c95a167f94be1267a21d8c58d60cda0bb7673e7e
psykose <alice@ayaya.dev>
2024-05-20 17:10:32 +0000

    main/python-requests: update to 2.32.0

--- main/python-requests/template.py
+++ main/python-requests/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-requests"
-pkgver = "2.31.0"
-pkgrel = 2
+pkgver = "2.32.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -20,6 +20,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://requests.readthedocs.io"
 source = f"$(PYPI_SITE)/r/requests/requests-{pkgver}.tar.gz"
-sha256 = "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+sha256 = "fa5490319474c82ef1d2c9bc459d3652e3ae4ef4c4ebdd18a21145a47ca4b6b8"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
