8bf5cfcc8587cf8587707dfb45f80c370b6ca2c9
psykose <alice@ayaya.dev>
2024-05-20 18:09:17 +0000

    contrib/tree-sitter: update pkgdesc

--- contrib/tree-sitter/template.py
+++ contrib/tree-sitter/template.py
@@ -1,13 +1,14 @@
 pkgname = "tree-sitter"
+# match to tree-sitter-cli
 pkgver = "0.22.6"
-pkgrel = 0
+pkgrel = 1
 build_style = "makefile"
 make_cmd = "gmake"
 hostmakedepends = ["gmake", "pkgconf"]
-pkgdesc = "Parser generator tool and incremental parsing library"
+pkgdesc = "Incremental parsing library for language grammars"
 maintainer = "yopito <pierre.bourgin@free.fr>"
 license = "MIT"
-url = "https://tree-sitter.github.io"
+url = "https://tree-sitter.github.io/tree-sitter"
 source = f"https://github.com/tree-sitter/tree-sitter/archive/v{pkgver}.tar.gz"
 sha256 = "e2b687f74358ab6404730b7fb1a1ced7ddb3780202d37595ecd7b20a8f41861f"
 # check requires cargo/fixture stuff (from remote repositories)
