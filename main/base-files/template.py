69f38bfc0925e0e0d59655735c87a091958450d0
triallax <triallax@tutanota.com>
2024-05-20 23:07:03 +0100

    main/base-files: remove base-locale subpackage
    pulled out into separate template in next commit

--- main/base-files/template.py
+++ main/base-files/template.py
@@ -1,7 +1,7 @@
 pkgname = "base-files"
 _iana_ver = "20240318"
 pkgver = f"0.1.{_iana_ver}"
-pkgrel = 0
+pkgrel = 1
 # highest priority dir owner
 replaces_priority = 65535
 pkgdesc = "Chimera Linux base system files"
@@ -133,15 +133,6 @@ def _basedevs(self):
     self.pkgdesc = "Base package for static development packages"
     self.depends = []
     self.install_if = []
-    self.options = ["empty"]
-
-    return []
-
-
-@subpackage("base-locale")
-def _baseloc(self):
-    self.pkgdesc = "Base package for locale data"
-    self.depends = []
     self.options = ["empty"]
 
     return []
