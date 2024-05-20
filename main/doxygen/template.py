5e445228b38f4767f7421a91b980b483caae70e1
psykose <alice@ayaya.dev>
2024-05-20 17:08:59 +0000

    main/doxygen: update to 1.11.0

--- main/doxygen/template.py
+++ main/doxygen/template.py
@@ -1,5 +1,5 @@
 pkgname = "doxygen"
-pkgver = "1.10.0"
+pkgver = "1.11.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "perl", "python", "flex", "bison"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://doxygen.nl"
 source = f"{url}/files/{pkgname}-{pkgver}.src.tar.gz"
-sha256 = "dd7c556b4d96ca5e682534bc1f1a78a5cfabce0c425b14c1b8549802686a4442"
+sha256 = "c9edfdf8c5f3e8bee0c4c967850caead27099883ee7ff8b11044e6d63faf3607"
 hardening = ["vis", "cfi"]
 
 
