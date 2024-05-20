bd8a55c43ef792f083e7b70d53221a810e6ba7b1
psykose <alice@ayaya.dev>
2024-05-20 17:42:05 +0000

    contrib/qt6ct: rebuild for qt6.7.1

--- contrib/qt6ct/template.py
+++ contrib/qt6ct/template.py
@@ -1,6 +1,6 @@
 pkgname = "qt6ct"
 pkgver = "0.9"
-pkgrel = 2
+pkgrel = 3
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "qt6-qttools", "qt6-qtbase"]
 makedepends = ["qt6-qtbase-devel", "qt6-qttools-devel"]
