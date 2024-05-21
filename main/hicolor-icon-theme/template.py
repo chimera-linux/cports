89a341b782ccf997072ec2dea146e6a82b7fdd9e
psykose <alice@ayaya.dev>
2024-05-21 00:33:06 +0000

    main/hicolor-icon-theme:

--- main/hicolor-icon-theme/template.py
+++ main/hicolor-icon-theme/template.py
@@ -9,3 +9,9 @@ license = "GPL-2.0-or-later"
 url = "https://www.freedesktop.org/wiki/Software/icon-theme"
 source = f"http://icon-theme.freedesktop.org/releases/{pkgname}-{pkgver}.tar.xz"
 sha256 = "db0e50a80aa3bf64bb45cbca5cf9f75efd9348cf2ac690b907435238c3cf81d7"
+
+
+@subpackage("hicolor-icon-theme-devel")
+def _devel(self):
+    self.depends += [f"{pkgname}={pkgver}-r{pkgrel}"]
+    return self.default_devel()
