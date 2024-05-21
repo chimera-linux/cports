efcb36ca39b296308c8d961708e1c572026a59e5
psykose <alice@ayaya.dev>
2024-05-21 00:31:41 +0000

    main/hicolor-icon-theme: update to 0.18

--- main/hicolor-icon-theme/template.py
+++ main/hicolor-icon-theme/template.py
@@ -1,11 +1,11 @@
 pkgname = "hicolor-icon-theme"
-pkgver = "0.17"
+pkgver = "0.18"
 pkgrel = 0
-build_style = "gnu_configure"
-configure_gen = []
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
 pkgdesc = "Default fallback theme for freedesktop.org icon themes"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://www.freedesktop.org/wiki/Software/icon-theme"
 source = f"http://icon-theme.freedesktop.org/releases/{pkgname}-{pkgver}.tar.xz"
-sha256 = "317484352271d18cbbcfac3868eab798d67fff1b8402e740baa6ff41d588a9d8"
+sha256 = "db0e50a80aa3bf64bb45cbca5cf9f75efd9348cf2ac690b907435238c3cf81d7"
