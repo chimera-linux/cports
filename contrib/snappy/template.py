35c7e1ca7175b6cb696b0e0c651f6e007afc2943
psykose <alice@ayaya.dev>
2024-05-22 16:30:47 +0000

    contrib/snappy: update to 1.2.1

--- contrib/snappy/template.py
+++ contrib/snappy/template.py
@@ -1,5 +1,5 @@
 pkgname = "snappy"
-pkgver = "1.2.0"
+pkgver = "1.2.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -13,7 +13,7 @@ maintainer = "eater <=@eater.me>"
 license = "BSD-3-Clause"
 url = "https://google.github.io/snappy"
 source = f"https://github.com/google/snappy/archive/{pkgver}.tar.gz"
-sha256 = "9b8f10fbb5e3bc112f2e5e64f813cb73faea42ec9c533a5023b5ae08aedef42e"
+sha256 = "736aeb64d86566d2236ddffa2865ee5d7a82d26c9016b36218fcc27ea4f09f86"
 # tests depend on in-tree gtest
 options = ["!check"]
 
