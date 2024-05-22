d97ddab3ab333aa3912d4bbf10fcac5296aa1c07
psykose <alice@ayaya.dev>
2024-05-22 15:00:02 +0000

    contrib/tree-sitter-lua: new package (0.1.0)

---
+++ contrib/tree-sitter-lua/template.py
@@ -0,0 +1,31 @@
+pkgname = "tree-sitter-lua"
+pkgver = "0.1.0"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_check_target = "test"
+hostmakedepends = [
+    "gmake",
+    "tree-sitter-cli",
+    "pkgconf",
+]
+pkgdesc = "Lua grammar for tree-sitter"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/tree-sitter-grammars/tree-sitter-lua"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "230cfcbfa74ed1f7b8149e9a1f34c2efc4c589a71fe0f5dc8560622f8020d722"
+
+
+def post_install(self):
+    self.install_license("LICENSE.md")
+    self.install_dir("usr/lib/tree-sitter")
+    self.install_link(
+        "usr/lib/tree-sitter/lua.so", "../libtree-sitter-lua.so.0"
+    )
+    self.install_files("queries", "usr/share/tree-sitter/queries", name="lua")
+
+
+@subpackage("tree-sitter-lua-devel")
+def _devel(self):
+    return self.default_devel()
