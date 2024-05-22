361b3d85a99536a32ddc1a662ccb86a09b8af5e8
ttyyls <contact@behri.org>
2024-05-22 10:18:42 +0100

    contrib/python-requests-file: update to 2.1.0

--- contrib/python-requests-file/template.py
+++ contrib/python-requests-file/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-requests-file"
-pkgver = "2.0.0"
+pkgver = "2.1.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -17,5 +17,5 @@ pkgdesc = "File transport adapter for python-requests"
 maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/dashea/requests-file"
-source = f"$(PYPI_SITE)/r/requests-file/requests-file-{pkgver}.tar.gz"
-sha256 = "20c5931629c558fda566cacc10cfe2cd502433e628f568c34c80d96a0cc95972"
+source = f"$(PYPI_SITE)/r/requests_file/requests_file-{pkgver}.tar.gz"
+sha256 = "0f549a3f3b0699415ac04d167e9cb39bccfb730cb832b4d20be3d9867356e658"
