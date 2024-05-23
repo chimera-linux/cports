9c4ee3b8488c95b97293ba1c41224e5fb635da51
triallax <triallax@tutanota.com>
2024-05-23 16:18:46 +0100

    main/gnome-screenshot: correct description, remove cfi TODO and vis

--- main/gnome-screenshot/template.py
+++ main/gnome-screenshot/template.py
@@ -1,21 +1,19 @@
 pkgname = "gnome-screenshot"
 pkgver = "41.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
-hostmakedepends = ["meson", "pkgconf", "gettext", "glib-devel"]
+hostmakedepends = ["gettext", "glib-devel", "meson", "pkgconf"]
 makedepends = [
-    "libcanberra-devel",
     "glib-devel",
-    "libhandy-devel",
     "gtk+3-devel",
-    "libxext-devel",
+    "libcanberra-devel",
+    "libhandy-devel",
     "libx11-devel",
+    "libxext-devel",
 ]
-pkgdesc = "Optional extensions for GNOME shell"
+pkgdesc = "GNOME screenshot utility"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-screenshot"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "4adb7dec926428f74263d5796673cf142e4720b6e768f5468a8d0933f98c9597"
-# FIXME cfi
-hardening = ["vis", "!cfi"]
