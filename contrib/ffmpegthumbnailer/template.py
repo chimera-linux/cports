b78f52c588b03d06026a3da83110b2f786e5865e
triallax <triallax@tutanota.com>
2024-05-23 18:20:02 +0100

    contrib/ffmpegthumbnailer: new package (2.2.2)

---
+++ contrib/ffmpegthumbnailer/template.py
@@ -0,0 +1,18 @@
+pkgname = "ffmpegthumbnailer"
+pkgver = "2.2.2"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DENABLE_THUMBNAILER=ON"]
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = ["ffmpeg-devel", "libjpeg-turbo-devel", "libpng-devel"]
+pkgdesc = "Video thumbnailer utilizing ffpmeg"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://github.com/dirkvdb/ffmpegthumbnailer"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "8c4c42ab68144a9e2349710d42c0248407a87e7dc0ba4366891905322b331f92"
+
+
+@subpackage("ffmpegthumbnailer-devel")
+def _devel(self):
+    return self.default_devel()
