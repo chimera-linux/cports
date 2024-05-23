31baa9d4a14cfcb6f9dfbde2f4739277e80ee340
ttyyls <contact@behri.org>
2024-05-23 20:57:30 +0100

    contrib/python-botocore: new package (1.34.113)

---
+++ contrib/python-botocore/template.py
@@ -0,0 +1,24 @@
+pkgname = "python-botocore"
+pkgver = "1.34.114"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = [
+    "python-dateutil",
+    "python-jmespath",
+    "python-urllib3",
+]
+checkdepends = depends + ["python-pytest"]
+pkgdesc = "Core module supporting boto3 and aws cli"
+maintainer = "ttyyls <contact@behri.org>"
+license = "Apache-2.0"
+url = "https://github.com/boto/botocore"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "dbf3090ffd705c90975f63eb437de4fdf94819b338bb1ac6e009391cbefede6e"
+# takes forever
+options = ["!check"]
