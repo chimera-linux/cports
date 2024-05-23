53a524d89d7fa7413a0b78b91a68fb30a9bb60b5
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-05-23 18:21:28 +0200

    contrib/cargo-crev: update to 0.25.9

--- contrib/cargo-crev/template.py
+++ contrib/cargo-crev/template.py
@@ -1,16 +1,16 @@
 pkgname = "cargo-crev"
-pkgver = "0.25.6"
+pkgver = "0.25.9"
 pkgrel = 0
 build_wrksrc = "cargo-crev"
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
-makedepends = ["openssl-devel", "libgit2-devel"]
+makedepends = ["openssl-devel", "libgit2-devel", "sqlite-devel"]
 pkgdesc = "Cryptographically verifiable code review system for cargo"
 maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "MPL-2.0 OR MIT OR Apache-2.0"
 url = "https://github.com/crev-dev/cargo-crev"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "8a8b737aff1361677e3733133944728871ccf7ac00ea15b32f9d0ef6d5814f62"
+sha256 = "7ebc63b272f09730d44c469d39413e3208538e885cf977bf4a61d768948700a2"
 
 
 def post_install(self):
