cae84a27f31f5b22844409b56a931e98d7e01bd5
ttyyls <contact@behri.org>
2024-05-23 20:57:03 +0100

    contrib/python-jmespath: new package (1.0.1)

---
+++ contrib/python-jmespath/template.py
@@ -0,0 +1,30 @@
+pkgname = "python-jmespath"
+pkgver = "1.0.1"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+checkdepends = [
+    "python-hypothesis",
+    "python-pytest-xdist",
+]
+pkgdesc = "JSON matching expressions"
+maintainer = "ttyyls <contact@behri.org>"
+license = "MIT"
+url = "https://jmespath.org"
+source = (
+    f"https://github.com/jmespath/jmespath.py/archive/refs/tags/{pkgver}.tar.gz"
+)
+sha256 = "6a02470b1716ec7a32abe89a873a4795c41c938468225f8a53d860980ec9e3c6"
+
+
+def init_check(self):
+    self.make_check_args += [f"--numprocesses={self.make_jobs}"]
+
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
