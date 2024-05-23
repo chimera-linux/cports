68c7f51ee659760492215aa7ecc37e76d222f7d0
triallax <triallax@tutanota.com>
2024-05-23 16:14:05 +0100

    main/gnome-menus: generate `configure`, sort deps

--- main/gnome-menus/template.py
+++ main/gnome-menus/template.py
@@ -5,11 +5,13 @@ build_style = "gnu_configure"
 configure_args = ["--disable-static"]
 make_cmd = "gmake"
 hostmakedepends = [
-    "gmake",
-    "pkgconf",
-    "gobject-introspection",
+    "automake",
+    "gettext-devel",
     "glib-devel",
-    "gettext",
+    "gmake",
+    "gobject-introspection",
+    "libtool",
+    "pkgconf",
 ]
 makedepends = ["glib-devel"]
 pkgdesc = "GNOME menu definitions"
@@ -23,6 +25,3 @@ sha256 = "d9348f38bde956fc32753b28c1cde19c175bfdbf1f4d5b06003b3aa09153bb1f"
 @subpackage("gnome-menus-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
