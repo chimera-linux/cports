82514c70996a3baa9843461a226c92b505aa7ed6
psykose <alice@ayaya.dev>
2024-05-24 13:00:43 +0000

    contrib/labwc: new package (0.7.2)

---
+++ contrib/labwc/template.py
@@ -0,0 +1,34 @@
+pkgname = "labwc"
+pkgver = "0.7.2"
+pkgrel = 0
+build_style = "meson"
+configure_args = [
+    "-Dman-pages=enabled",
+    "-Dxwayland=enabled",
+]
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+    "scdoc",
+    "wayland-progs",
+]
+makedepends = [
+    "cairo-devel",
+    "glib-devel",
+    "libdrm-devel",
+    "librsvg-devel",
+    "libxcb-devel",
+    "libxml2-devel",
+    "pango-devel",
+    "wayland-devel",
+    "wayland-protocols",
+    "wlroots0.17-devel",
+    "xwayland",
+]
+depends = ["xwayland"]
+pkgdesc = "Stacking wayland compositor"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-only"
+url = "https://github.com/labwc/labwc"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "b00119451a91a75cc063cfa753f956623acdde4b93bbf78b2b6d5fe7f94c597e"
