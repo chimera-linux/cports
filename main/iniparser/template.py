439809e229c0d6cd168d40ec4575af503b3d7dcf
psykose <alice@ayaya.dev>
2024-05-24 20:26:03 +0000

    main/iniparser: update to 4.2.2

--- main/iniparser/template.py
+++ main/iniparser/template.py
@@ -1,29 +1,26 @@
 pkgname = "iniparser"
-pkgver = "4.2.1"
+pkgver = "4.2.2"
 pkgrel = 0
-build_style = "makefile"
-make_cmd = "gmake"
-make_dir = "."
-# unset extra defaults
-make_build_args = ["ADDITIONAL_CFLAGS="]
-hostmakedepends = ["gmake"]
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "doxygen",
+    "ninja",
+    "pkgconf",
+]
 checkdepends = ["bash"]
 pkgdesc = "C library for INI file parsing"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
-url = "https://github.com/ndevilla/iniparser"
-source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "9120fd13260be1dbec74b8aaf47777c434976626f3b3288c0d17b70e21cce2d2"
+url = "https://gitlab.com/iniparser/iniparser"
+source = f"{url}/-/archive/v{pkgver}.tar.gz"
+sha256 = "454bc88a4a508a43340a5c1b7e4d57f9b41a51d6ca5d72798fc7f32c435290bd"
 # vis breaks symbols
 hardening = []
 
 
-def do_install(self):
+def post_install(self):
     self.install_license("LICENSE")
-    # bruh moment
-    self.install_file("libiniparser.*", "usr/lib", glob=True)
-    self.install_link("usr/lib/libiniparser.so", "libiniparser.so.1")
-    self.install_file("src/*.h", "usr/include", glob=True)
 
 
 @subpackage("iniparser-devel")
