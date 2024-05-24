c5d8dccfb881cfc5b0bbb40011d6f20592b75ff3
psykose <alice@ayaya.dev>
2024-05-24 07:00:34 +0000

    main/python-typing_extensions: update to 4.12.0

--- main/python-typing_extensions/template.py
+++ main/python-typing_extensions/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-typing_extensions"
-pkgver = "4.11.0"
+pkgver = "4.12.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-installer", "python-flit_core"]
@@ -7,8 +7,8 @@ depends = ["python"]
 pkgdesc = "Static typing extensions for Python"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Python-2.0"
-url = "https://github.com/python/typing"
+url = "https://github.com/python/typing_extensions"
 source = f"$(PYPI_SITE)/t/typing_extensions/typing_extensions-{pkgver}.tar.gz"
-sha256 = "83f085bd5ca59c80295fc2a82ab5dac679cbe02b9f33f7d83af68e241bea51b0"
+sha256 = "8cbcdc8606ebcb0d95453ad7dc5065e6237b6aa230a31e81d0f440c30fed5fd8"
 # in early path
 options = ["!check"]
