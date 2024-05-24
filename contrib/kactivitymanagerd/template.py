beaf75f309dca623c7ea44ebdb7314bed273b106
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-24 22:46:28 +0300

    contrib/kactivitymanagerd: new package (6.0.5)

---
+++ contrib/kactivitymanagerd/template.py
@@ -0,0 +1,28 @@
+pkgname = "kactivitymanagerd"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "boost-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kglobalaccel-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kxmlgui-devel",
+    "qt6-qtdeclarative-devel",
+]
+# depends = ["qt6-qtbase-sql"]
+pkgdesc = "KDE Manage user's activities and track usage patterns"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-only OR GPL-3.0-only"
+url = "https://invent.kde.org/plasma/kactivitymanagerd"
+source = f"$(KDE_SITE)/plasma/{pkgver}/kactivitymanagerd-{pkgver}.tar.xz"
+sha256 = "59ef968fa0990ed7aec6682bdbc8c51df1224a297934084c463060bc916fe09e"
+hardening = ["vis", "cfi"]
