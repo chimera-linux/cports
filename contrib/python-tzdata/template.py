2b53a89973a180856850c5f276f667cdec8af8c9
ttyyls <contact@behri.org>
2024-05-25 13:19:05 +0100

    contrib/python-tzdata: new package (2024.1)

---
+++ contrib/python-tzdata/template.py
@@ -0,0 +1,18 @@
+pkgname = "python-tzdata"
+pkgver = "2024.1"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = ["python-attrs", "tzdata"]
+checkdepends = depends + ["python-pytest", "python-pytest-subtests"]
+pkgdesc = "Python package wrapping the IANA time zone database"
+maintainer = "ttyyls <contact@behri.org>"
+license = "Apache-2.0"
+url = "https://github.com/python/tzdata"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "ee55197750b6002030c370f72c93f5f8fa863b98c7a3312850781cdc1e9f1037"
