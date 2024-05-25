5949004c1a207eabcd3b74c43ed161f5860eb2a9
psykose <alice@ayaya.dev>
2024-05-25 02:21:19 +0000

    contrib/iperf: new package (3.17.1)

---
+++ contrib/iperf/template.py
@@ -0,0 +1,25 @@
+pkgname = "iperf"
+pkgver = "3.17.1"
+pkgrel = 0
+build_style = "gnu_configure"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "libtool",
+]
+makedepends = ["openssl-devel"]
+pkgdesc = "IP bandwidth measurement tool"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-3-Clause-LBNL"
+url = "https://github.com/esnet/iperf"
+source = f"https://github.com/esnet/iperf/releases/download/{pkgver}/iperf-{pkgver}.tar.gz"
+sha256 = "84404ca8431b595e86c473d8f23d8bb102810001f15feaf610effd3b318788aa"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("iperf-devel")
+def _devel(self):
+    return self.default_devel()
