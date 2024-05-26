0b2533f8e88cf81220e4e69a576ab6b60567edbc
psykose <alice@ayaya.dev>
2024-05-26 15:10:24 +0000

    main/enchant: update to 2.8.0

--- main/enchant/template.py
+++ main/enchant/template.py
@@ -1,6 +1,6 @@
 pkgname = "enchant"
-pkgver = "2.7.3"
-pkgrel = 1
+pkgver = "2.8.0"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-relocatable", "--disable-static"]
 make_cmd = "gmake"
@@ -25,7 +25,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://abiword.github.io/enchant"
 source = f"https://github.com/AbiWord/enchant/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "fe6ad4cbe8c71b9384ffdef962be52d4d2bd5ebfb6351435bb390543d4f78b1e"
+sha256 = "c57add422237b8a7eed116a9a88d8be4f7b9281778fa36f03e1f2c051ecb0372"
 # missing checkdepends
 options = ["!check"]
 
