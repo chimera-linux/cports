8dfb08b1006d3bd9eac2a0b981cbd3dba4fd4be5
psykose <alice@ayaya.dev>
2024-05-26 05:25:04 +0000

    user/racket: enable on ppc64le/riscv64

--- user/racket/template.py
+++ user/racket/template.py
@@ -1,9 +1,10 @@
 pkgname = "racket"
 pkgver = "8.11.1"
-pkgrel = 1
-archs = ["aarch64", "x86_64"]
+pkgrel = 2
+archs = ["aarch64", "ppc64le", "riscv64", "x86_64"]
 build_wrksrc = "src"
 build_style = "gnu_configure"
+configure_args = ["--disable-strip"]
 configure_gen = []
 hostmakedepends = ["automake", "sqlite"]
 makedepends = [
@@ -20,6 +21,21 @@ source = f"https://download.racket-lang.org/installers/{pkgver}/racket-{pkgver}-
 sha256 = "47f744eb989e6486c6a6772bc7680a1f07948afdb157ea21b96d1c8b0f7ec447"
 # no tests, cross requires external chezscheme
 options = ["!check", "!cross"]
+
+match self.profile().arch:
+    case "aarch64" | "x86_64":
+        configure_args += ["--enable-csonly", "--enable-csdefault"]
+    case "ppc64le" | "riscv64":
+        configure_args += ["--enable-bconly", "--enable-bcdefault"]
+        makedepends += ["libucontext-devel"]
+        tool_flags = {
+            "CFLAGS": ["-D_GNU_SOURCE"],
+            "LDFLAGS": ["-lucontext"],
+        }
+
+
+def post_extract(self):
+    self.rm("src/bc/foreign/libffi", recursive=True)
 
 
 def post_install(self):
