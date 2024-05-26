75e5a96805542df037b817718caf90db648ad136
psykose <alice@ayaya.dev>
2024-05-26 02:59:22 +0000

    main/libsodium: update to 1.0.20

--- main/libsodium/template.py
+++ main/libsodium/template.py
@@ -1,15 +1,15 @@
 pkgname = "libsodium"
-pkgver = "1.0.19"
+pkgver = "1.0.20"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["lt_cv_prog_compiler_static_works=yes"]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 pkgdesc = "Modern and easy-to-use crypto library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "ISC"
 url = "https://libsodium.org"
 source = f"https://download.libsodium.org/{pkgname}/releases/{pkgname}-{pkgver}.tar.gz"
-sha256 = "018d79fe0a045cca07331d37bd0cb57b2e838c51bc48fd837a1472e50068bbea"
+sha256 = "ebb65ef6ca439333c2bb41a0c1990587288da07f6c7fd07cb3a18cc18d30ce19"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
 
@@ -21,6 +21,3 @@ def post_install(self):
 @subpackage("libsodium-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
