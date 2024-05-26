c829cbbcd5898dcf80c0fccb9f09e064879f015b
psykose <alice@ayaya.dev>
2024-05-26 02:56:45 +0000

    main/libuninameslist: update to 20240524

--- main/libuninameslist/template.py
+++ main/libuninameslist/template.py
@@ -1,5 +1,5 @@
 pkgname = "libuninameslist"
-pkgver = "20230916"
+pkgver = "20240524"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "libtool"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/fontforge/libuninameslist"
 source = f"{url}/releases/download/{pkgver}/{pkgname}-dist-{pkgver}.tar.gz"
-sha256 = "3ce49721de808a389f90997e9217adac449ab23e2fbf2115b22a8664e0e0a686"
+sha256 = "cb69d6b0b1bf896c98cd00497d3078be2d22b896b0dc7cba2bb3d6bc3172dac5"
 hardening = ["vis", "cfi"]
 
 
