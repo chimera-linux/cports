464001e010be3ee813a90997162942f12fcdfe3c
psykose <alice@ayaya.dev>
2024-05-26 03:05:08 +0000

    contrib/valgrind: update to 3.23.0

--- contrib/valgrind/template.py
+++ contrib/valgrind/template.py
@@ -1,5 +1,5 @@
 pkgname = "valgrind"
-pkgver = "3.22.0"
+pkgver = "3.23.0"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "ppc64", "ppc", "x86_64"]
 build_style = "gnu_configure"
@@ -22,7 +22,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "http://valgrind.org"
 source = f"https://sourceware.org/pub/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "c811db5add2c5f729944caf47c4e7a65dcaabb9461e472b578765dd7bf6d2d4c"
+sha256 = "c5c34a3380457b9b75606df890102e7df2c702b9420c2ebef9540f8b5d56264d"
 debug_level = 1
 tool_flags = {"CFLAGS": ["-U_FORTIFY_SOURCE", "-fPIC"], "LDFLAGS": ["-fPIC"]}
 nostrip_files = ["usr/libexec/valgrind/*", "usr/lib/valgrind/*"]
