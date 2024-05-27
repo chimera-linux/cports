8c7dd1a76b297f5fc92fde38dff6be5be44e0509
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 07:40:58 +0300

    contrib/milou: new package (6.0.5)

---
+++ contrib/milou/template.py
@@ -0,0 +1,27 @@
+pkgname = "milou"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "kconfig-devel",
+    "ki18n-devel",
+    "krunner-devel",
+    "ksvg-devel",
+    "kwindowsystem-devel",
+    "libplasma-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE Dedicated search application"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
+url = "https://api.kde.org/plasma/milou/html"
+source = f"$(KDE_SITE)/plasma/{pkgver}/milou-{pkgver}.tar.xz"
+sha256 = "708e18b5b424985a167507e9b5934de93c65acaf2f524466312d09515eb842cf"
+# FIXME: cfi kills krunner (plasma-workspace) on launch (Alt+Space) in libmilouqmlplugin.so
+hardening = ["vis", "!cfi"]
