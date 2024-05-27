8c5acf7fba2722c3435047d095c46c155d33f25c
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 07:39:55 +0300

    contrib/ocean-sound-theme: new package (6.0.5)

---
+++ contrib/ocean-sound-theme/template.py
@@ -0,0 +1,19 @@
+# TODO: rename to sound-theme-ocean?
+pkgname = "ocean-sound-theme"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+]
+makedepends = [
+    "qt6-qtbase-devel",
+]
+pkgdesc = "Ocean Sound Theme for KDE Plasma"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "CC-BY-SA-4.0"
+url = "https://api.kde.org/plasma/ocean-sound-theme/html"
+source = f"$(KDE_SITE)/plasma/{pkgver}/ocean-sound-theme-{pkgver}.tar.xz"
+sha256 = "6015f3931a74134fdd7eef5c7c7592ef8b740fcc084d7726f8df993dc5289f6b"
