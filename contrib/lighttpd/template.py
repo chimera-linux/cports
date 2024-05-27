28cc6eba4e3da29e6b52655e023351a586664001
psykose <alice@ayaya.dev>
2024-05-27 02:59:18 +0000

    contrib/lighttpd: fix some stuff
    - missing openssl support
    - add more compressors
    - add lua
    - add webdav props
    - devendor xxhash

--- contrib/lighttpd/template.py
+++ contrib/lighttpd/template.py
@@ -1,15 +1,36 @@
 pkgname = "lighttpd"
 pkgver = "1.4.76"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
+configure_args = [
+    "-Dwith_brotli=enabled",
+    "-Dwith_libdeflate=enabled",
+    "-Dwith_lua=true",
+    "-Dwith_openssl=true",
+    "-Dwith_webdav_props=enabled",
+    "-Dwith_xxhash=enabled",
+    "-Dwith_zstd=enabled",
+]
 hostmakedepends = ["meson", "pkgconf"]
-makedepends = ["musl-bsd-headers", "openssl-devel", "pcre2-devel"]
+makedepends = [
+    "brotli-devel",
+    "libdeflate-devel",
+    "libxml2-devel",
+    "lua5.4-devel",
+    "musl-bsd-headers",
+    "openssl-devel",
+    "pcre2-devel",
+    "sqlite-devel",
+    "xxhash-devel",
+    "zlib-devel",
+    "zstd-devel",
+]
 checkdepends = ["perl"]
 pkgdesc = "Lightweight web server"
 maintainer = "yanchan09 <yan@omg.lol>"
 license = "BSD-3-Clause"
 url = "https://lighttpd.net"
-source = f"https://download.lighttpd.net/{pkgname}/releases-{pkgver[:pkgver.rfind('.')]}.x/{pkgname}-{pkgver}.tar.xz"
+source = f"https://download.lighttpd.net/lighttpd/releases-{pkgver[:pkgver.rfind('.')]}.x/lighttpd-{pkgver}.tar.xz"
 sha256 = "8cbf4296e373cfd0cedfe9d978760b5b05c58fdc4048b4e2bcaf0a61ac8f5011"
 
 
