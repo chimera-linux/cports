01cbb27fac09b9e732a236778047f7dee4903dcf
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 08:33:22 +0300

    contrib/konsole: new package (24.05.0)

---
+++ contrib/konsole/template.py
@@ -0,0 +1,42 @@
+pkgname = "konsole"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kbookmarks-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kglobalaccel-devel",
+    "kguiaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "knewstuff-devel",
+    "knotifications-devel",
+    "knotifyconfig-devel",
+    "kparts-devel",
+    "kpty-devel",
+    "ktextwidgets-devel",
+    "qt6-qt5compat-devel",
+    "qt6-qtmultimedia-devel",
+]
+pkgdesc = "KDE's Terminal Emulator"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/konsole"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/konsole-{pkgver}.tar.xz"
+sha256 = "53efd25c1d574448db57f446f423328c7f2f08889f67defc1ed5c783c9ef47f4"
+# FIXME: cfi breaks at least TerminalInterfaceTest like https://paste.c-net.org/yrqifaktnyhy
+hardening = ["vis", "!cfi"]
