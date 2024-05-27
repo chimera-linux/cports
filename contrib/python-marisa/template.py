1bf5a983ff4cd690996b116bceaf2c009684db70
psykose <alice@ayaya.dev>
2024-05-27 06:11:53 +0000

    contrib/python-marisa: new package (0.2.6)

---
+++ contrib/python-marisa/template.py
@@ -0,0 +1,33 @@
+# python-marisa-trie is a different pypi module
+pkgname = "python-marisa"
+# match to marisa-trie
+pkgver = "0.2.6"
+pkgrel = 0
+build_wrksrc = "bindings/python"
+build_style = "python_pep517"
+hostmakedepends = [
+    "gmake",
+    "python-build",
+    "python-devel",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+    "swig",
+]
+makedepends = ["marisa-trie-devel"]
+pkgdesc = "Python bindings for libmarisa"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-2-Clause OR LGPL-2.1-or-later"
+url = "https://github.com/s-yata/marisa-trie"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "1063a27c789e75afa2ee6f1716cc6a5486631dcfcb7f4d56d6485d2462e566de"
+# no tests
+options = ["!check"]
+
+
+def pre_build(self):
+    self.do("gmake", "-C", "..", "swig-python")
+
+
+def post_install(self):
+    self.install_license("../../COPYING.md")
