30b30bb6e60a271ed1b6074153f164deaa970c84
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 08:31:02 +0300

    contrib/plasma-pa: new package (6.0.5)

---
+++ contrib/plasma-pa/template.py
@@ -0,0 +1,37 @@
+pkgname = "plasma-pa"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+# FIXME: only test, needs selenium-webdriver-at-spi-run
+make_check_args = ["-E", "applettest"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcmutils-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kdeclarative-devel",
+    "kdoctools-devel",
+    "kglobalaccel-devel",
+    "ki18n-devel",
+    "kstatusnotifieritem-devel",
+    "ksvg-devel",
+    "kwindowsystem-devel",
+    "libcanberra-devel",
+    "libplasma-devel",
+    "libpulse-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE Plasma PulseAudio integration"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later AND LGPL-2.0-or-later"
+url = "https://invent.kde.org/plasma/plasma-pa"
+source = f"$(KDE_SITE)/plasma/{pkgver}/plasma-pa-{pkgver}.tar.xz"
+sha256 = "ecbf3bb40720809481cab6857e7c1b34368a139d1162b00cd46b2cc63cd9aac4"
+# FIXME: cfi kills systemsettings (when leaving "Sound" page) in libplasma-volume-declarative.so
+hardening = ["vis", "!cfi"]
