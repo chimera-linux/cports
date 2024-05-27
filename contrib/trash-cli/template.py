0f188e86a1d4f344b09550cb3f8168500a0984a8
psykose <alice@ayaya.dev>
2024-05-27 03:06:57 +0000

    contrib/trash-cli: update to 0.24.5.26

--- contrib/trash-cli/template.py
+++ contrib/trash-cli/template.py
@@ -1,5 +1,5 @@
 pkgname = "trash-cli"
-pkgver = "0.24.4.17"
+pkgver = "0.24.5.26"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,8 +13,8 @@ pkgdesc = "Command line interface to the freedesktop.org trashcan"
 maintainer = "Hugo Machet <mail@hmachet.com>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/andreafrancia/trash-cli"
-source = f"https://github.com/andreafrancia/trash-cli/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "8476c926b0f4df5c09fe94a8573b04aeefff43d2e9ff0044c2caaf4c883e0e50"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "1d7dec1ad8f0264ceb1b0211d25fffee99c9409cd2e1d36dcc82ac5540f39ce5"
 # unpackaged checkdepends
 options = ["!check"]
 
