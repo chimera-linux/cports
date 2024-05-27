e26770dae2b859f35af115a60eb03bfb1ae9e5cc
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 08:12:21 +0300

    contrib/xwaylandvideobridge: new package (0.4.0)

---
+++ contrib/xwaylandvideobridge/template.py
@@ -0,0 +1,29 @@
+pkgname = "xwaylandvideobridge"
+pkgver = "0.4.0"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DQT_MAJOR_VERSION=6"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "knotifications-devel",
+    "kpipewire-devel",
+    "kstatusnotifieritem-devel",
+    "kwindowsystem-devel",
+    "qt6-qtdeclarative-devel",
+    "xcb-util-devel",
+]
+pkgdesc = "Utility to stream Wayland screens/windows to X11 applications"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://invent.kde.org/system/xwaylandvideobridge"
+source = f"$(KDE_SITE)/xwaylandvideobridge/xwaylandvideobridge-{pkgver}.tar.xz"
+sha256 = "ea72ac7b2a67578e9994dcb0619602ead3097a46fb9336661da200e63927ebe6"
+# TODO: check CFI stability again, previously crashed on screenshare
+hardening = ["vis", "cfi"]
