f759031e7a7b60cb311b737a35dd239904d68ff7
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 07:36:39 +0300

    contrib/fonts-hack-ttf: new package (3.003)

---
+++ contrib/fonts-hack-ttf/template.py
@@ -0,0 +1,19 @@
+pkgname = "fonts-hack-ttf"
+pkgver = "3.003"
+pkgrel = 0
+pkgdesc = "Typeface designed for source code"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "MIT AND custom:Bitstream-Vera"
+url = "https://sourcefoundry.org/hack"
+source = (
+    f"https://github.com/source-foundry/Hack/archive/refs/tags/v{pkgver}.tar.gz"
+)
+sha256 = "3c6f1a20e86744077e83c9bacf879a5b13f659f1c07e9c5c57d6efc3cbe66c07"
+
+
+def do_install(self):
+    self.install_file(
+        "config/fontconfig/45-Hack.conf", "usr/share/fontconfig/conf.avail"
+    )
+    self.install_file("build/ttf/*.ttf", "usr/share/fonts/hack", glob=True)
+    self.install_license("LICENSE.md")
