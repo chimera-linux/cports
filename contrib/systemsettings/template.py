30c23134168c9eb6acf6d5de7689cb0b7f4deaae
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 08:32:02 +0300

    contrib/systemsettings: new package (6.0.5)

---
+++ contrib/systemsettings/template.py
@@ -0,0 +1,38 @@
+pkgname = "systemsettings"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "kauth-devel",
+    "kcmutils-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kguiaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "kirigami-devel",
+    "kitemmodels-devel",
+    "kitemviews-devel",
+    "kpackage-devel",
+    "krunner-devel",
+    "kxmlgui-devel",
+    "plasma-activities-devel",
+    "plasma-workspace-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE System settings"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://userbase.kde.org/System_Settings"
+source = f"$(KDE_SITE)/plasma/{pkgver}/systemsettings-{pkgver}.tar.xz"
+sha256 = "b722ba21fabd8cd27e2371a83e33a9a4e1403257d454b3f01c6fa46c2874cf93"
+# FIXME: cfi crash on launch
+hardening = ["vis", "!cfi"]
