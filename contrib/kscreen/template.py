359daf248d19e3b3e92b84f2f50a589f43a22879
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 08:05:44 +0300

    contrib/kscreen: new package (6.0.5)

---
+++ contrib/kscreen/template.py
@@ -0,0 +1,34 @@
+pkgname = "kscreen"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "kcmutils-devel",
+    "kconfig-devel",
+    "kdbusaddons-devel",
+    "kglobalaccel-devel",
+    "ki18n-devel",
+    "ksvg-devel",
+    "kwindowsystem-devel",
+    "kxmlgui-devel",
+    "layer-shell-qt-devel",
+    "libkscreen-devel",
+    "libplasma-devel",
+    "qt6-qtsensors-devel",
+    "xcb-util-devel",
+]
+pkgdesc = "KDE screen management"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
+url = "https://invent.kde.org/plasma/kscreen"
+source = f"$(KDE_SITE)/plasma/{pkgver}/kscreen-{pkgver}.tar.xz"
+sha256 = "eded8b558167d89a29a830a7bd3c7d692b365da529c7b6e59ebdadeb916a93f0"
+# FIXME: cfi breaks both tests like https://paste.c-net.org/zfdjtfn6ssy6
+hardening = ["vis", "!cfi"]
