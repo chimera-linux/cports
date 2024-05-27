cf82d31f45f491dcf49dea1569a4e0bf7aeb8e6f
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 08:52:24 +0300

    contrib/plasma-systemmonitor: new package (6.0.5)

---
+++ contrib/plasma-systemmonitor/template.py
@@ -0,0 +1,37 @@
+pkgname = "plasma-systemmonitor"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "kconfig-devel",
+    "kdbusaddons-devel",
+    "kglobalaccel-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kirigami-addons-devel",
+    "kirigami-devel",
+    "kitemmodels-devel",
+    "knewstuff-devel",
+    "kpackage-devel",
+    "kservice-devel",
+    "libksysguard-devel",
+    "qt6-qtdeclarative-devel",
+]
+depends = [
+    "kquickcharts",
+    "ksystemstats",
+]
+pkgdesc = "KDE System Resource Usage Monitor"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
+url = "https://apps.kde.org/plasma-systemmonitor"
+source = f"$(KDE_SITE)/plasma/{pkgver}/plasma-systemmonitor-{pkgver}.tar.xz"
+sha256 = "662518e6dc763d19cbd878c145a3761a6a2e2765fd237278ffdd0a2a5253080b"
+# FIXME: cfi crash on launch
+hardening = ["vis", "!cfi"]
