565e8243e952c72e75feeca572f83adc3a430947
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 08:23:53 +0300

    contrib/bluedevil: new package (6.0.5.1)

---
+++ contrib/bluedevil/template.py
@@ -0,0 +1,32 @@
+pkgname = "bluedevil"
+pkgver = "6.0.5.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "bluez-qt-devel",
+    "kcmutils-devel",
+    "kcoreaddons-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "knotifications-devel",
+    "ksvg-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "libplasma-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE Plasma Bluetooth integration"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later AND LGPL-2.0-or-later"
+url = "https://invent.kde.org/plasma/bluedevil"
+source = f"$(KDE_SITE)/plasma/{'.'.join(pkgver.split('.')[0:3])}/bluedevil-{pkgver}.tar.xz"
+sha256 = "66fce2d49f6ac716b551e3030f8ed3f71beec0428da1f99af7e959bc90f53ce2"
+hardening = ["vis", "cfi"]
