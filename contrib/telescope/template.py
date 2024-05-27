31374460740a7f03506e99dcabeecb904f590c1d
ttyyls <contact@behri.org>
2024-05-27 09:31:26 +0100

    contrib/telescope: update to 0.9.1

--- contrib/telescope/template.py
+++ contrib/telescope/template.py
@@ -1,6 +1,6 @@
 pkgname = "telescope"
-pkgver = "0.9"
-pkgrel = 1
+pkgver = "0.9.1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
 hostmakedepends = ["pkgconf"]
@@ -15,7 +15,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "ISC AND Unicode-3.0 AND BSD-3-Clause AND MIT"
 url = "https://telescope.omarpolo.com"
 source = f"https://ftp.omarpolo.com/telescope-{pkgver}.tar.gz"
-sha256 = "c963c82afde87ed5397ddd6a399ff7dfdbd32144ce742cb5a11c2ad6476d7cf5"
+sha256 = "fed1ef4db27e713c8719f1e32cc30c911532e90d1bd8aa507acf3db74973c7ac"
 hardening = ["vis", "cfi"]
 
 
