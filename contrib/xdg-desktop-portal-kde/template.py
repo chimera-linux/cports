654cc3b71a037ddbe8d6e38145f79183a9b6bad1
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 07:13:56 +0300

    contrib/xdg-desktop-portal-kde: new package (6.0.5)

---
+++ contrib/xdg-desktop-portal-kde/template.py
@@ -0,0 +1,46 @@
+pkgname = "xdg-desktop-portal-kde"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+make_check_wrapper = ["dbus-run-session"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kglobalaccel-devel",
+    "kguiaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "kirigami-devel",
+    "knotifications-devel",
+    "kstatusnotifieritem-devel",
+    "kwayland-devel",
+    "plasma-wayland-protocols",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtwayland-devel",
+    "wayland-protocols",
+    # TODO: KIOFuse
+]
+checkdepends = [
+    "dbus",
+    "python-gobject",
+]
+# TODO: kiconthemes plasma-workspace kiofuse?
+depends = [
+    "xdg-desktop-portal",
+]
+pkgdesc = "Backend implementation for xdg-desktop-portal using Qt/KF6"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later"
+url = "https://invent.kde.org/plasma/xdg-desktop-portal-kde"
+source = f"$(KDE_SITE)/plasma/{pkgver}/xdg-desktop-portal-kde-{pkgver}.tar.xz"
+sha256 = "00bdf442d37b3080abfd2958425dd724a3a5019d50dfd7cb319e5160b27a6b05"
+hardening = ["vis", "cfi"]
