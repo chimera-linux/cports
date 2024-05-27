cd36d0d164d6c7b783f4dcb1df6232ab6be56b91
Adam J. Clayborn <claybie@claybie.org>
2024-05-27 13:35:40 +0000

    Fix libvips template source to prevent github.com error 404.

--- contrib/libvips/template.py
+++ contrib/libvips/template.py
@@ -33,7 +33,7 @@ pkgdesc = "Fast image processing library"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/libvips/libvips"
-source = f"https://github.com/libvips/libvips/releases/download/v{pkgver}a/vips-{pkgver}.tar.xz"
+source = f"https://github.com/libvips/libvips/releases/download/v{pkgver}/vips-{pkgver}.tar.xz"
 sha256 = "a2ab15946776ca7721d11cae3215f20f1f097b370ff580cd44fc0f19387aee84"
 # broken
 options = ["!cross"]
