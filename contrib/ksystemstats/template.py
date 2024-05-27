90ddd44bc94eb75297d71459943023fe32a77d58
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 08:51:31 +0300

    contrib/ksystemstats: new package (6.0.5)

---
+++ contrib/ksystemstats/template.py
@@ -0,0 +1,39 @@
+pkgname = "ksystemstats"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["dbus-run-session"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "elogind-devel",
+    "kcoreaddons-devel",
+    "kdbusaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "libksysguard-devel",
+    "libnl-devel",
+    "libsensors-devel",
+    "networkmanager-qt-devel",
+    "qt6-qtdeclarative-devel",
+    "solid-devel",
+]
+checkdepends = [
+    "dbus",
+]
+pkgdesc = "KDE Plugin based system monitoring daemon"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
+url = "https://invent.kde.org/plasma/ksystemstats"
+source = f"$(KDE_SITE)/plasma/{pkgver}/ksystemstats-{pkgver}.tar.xz"
+sha256 = "bdc6fa95c0b4dad9210c7a7734e1c233bc408f09fcaf4961ba709affa1fd4284"
+# silence some ~600 lines of spam...
+tool_flags = {"CXXFLAGS": ["-Wno-deprecated-declarations"]}
+# FIXME: cfi breaks at least ksystemstatstest in dbusApi() like https://paste.c-net.org/tnqlkafoixrz
+hardening = ["vis", "!cfi"]
