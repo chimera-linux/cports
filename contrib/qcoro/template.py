81d0f302d5d176e9b4b8456363f73732efb977c1
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 08:26:57 +0300

    contrib/qcoro: new package (0.10.0)

---
+++ contrib/qcoro/template.py
@@ -0,0 +1,45 @@
+pkgname = "qcoro"
+pkgver = "0.10.0"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DQCORO_BUILD_EXAMPLES=OFF"]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+]
+makedepends = [
+    "qt6-qtdeclarative-devel",
+    "qt6-qtwebsockets-devel",
+]
+checkdepends = [
+    "dbus-x11",
+]
+pkgdesc = "Qt C++ Coroutine Library"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "MIT"
+url = "https://qcoro.dvratil.cz"
+source = (
+    f"https://github.com/danvratil/qcoro/archive/refs/tags/v{pkgver}.tar.gz"
+)
+sha256 = "b7c8f00273ad27d85814bf4ec93eb6922c75656800a61d11854d36355a4a1aec"
+# FIXME: vis breaks symbols for test-qcorothread build,
+#        cfi breaks at least test-qcoro{task,websocket}
+hardening = ["!vis", "!cfi"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("qcoro-devel")
+def _devel(self):
+    self.depends += [
+        "qt6-qtdeclarative-devel",
+        "qt6-qtwebsockets-devel",
+    ]
+
+    return self.default_devel(
+        extra=[
+            "usr/lib/qt6/mkspecs",
+        ]
+    )
