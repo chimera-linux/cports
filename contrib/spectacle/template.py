d5bd10ed0081fb5dbae5060895dbee0614629174
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-27 08:47:27 +0300

    contrib/spectacle: new package (24.05.0)

---
+++ contrib/spectacle/template.py
@@ -0,0 +1,42 @@
+pkgname = "spectacle"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kglobalaccel-devel",
+    "kguiaddons-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kirigami-devel",
+    "knotifications-devel",
+    "kpipewire-devel",
+    "kwidgetsaddons-devel",
+    "kxmlgui-devel",
+    "layer-shell-qt-devel",
+    "opencv-devel",
+    "plasma-wayland-protocols",
+    "qt6-qtmultimedia-devel",
+    "qt6-qtwayland-devel",
+    "xcb-util-devel",
+    "zxing-cpp-devel",
+    # "purpose-devel",  # TODO: package for export to websites functionality?
+]
+pkgdesc = "KDE Screenshot capture utility"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/spectacle"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/spectacle-{pkgver}.tar.xz"
+sha256 = "def2851e8db3cc00eab0810d988014b15b8f23b474b932b1cdf3c6144326d5aa"
+# FIXME: cfi kills app on launch
+hardening = ["vis", "!cfi"]
---
+++ contrib/spectacle/update.py
@@ -0,0 +1,2 @@
+# needs opencv packaged, deal with it a tiny bit later
+ignore = ["24.05.0"]
