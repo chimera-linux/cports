bcc0f16924cea798cd4de8698473c8291942b3f5
psykose <alice@ayaya.dev>
2024-05-29 13:59:02 +0000

    contrib/kcmutils: add checkdepend

--- contrib/kcmutils/template.py
+++ contrib/kcmutils/template.py
@@ -19,6 +19,7 @@ makedepends = [
     "plasma-activities-devel",
     "qt6-qtdeclarative-devel",
 ]
+checkdepends = ["kirigami"]
 pkgdesc = "Utilities for KDE System Settings modules"
 maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "LGPL-2.1-only AND LGPL-2.1-or-later"
