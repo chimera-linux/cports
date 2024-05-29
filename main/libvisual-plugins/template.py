f3fc88bb13f459aab1f3c4fadbe4352495811378
triallax <triallax@tutanota.com>
2024-05-30 00:33:38 +0100

    main/libvisual: update to 0.4.2

---
+++ main/libvisual-plugins/template.py
@@ -0,0 +1,42 @@
+pkgname = "libvisual-plugins"
+pkgver = "0.4.2"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--disable-alsa",
+    "--disable-portaudio",
+    "--enable-gdkpixbuf-plugin",
+    "--enable-gstreamer-plugin",
+    "--enable-jack",
+    "--enable-opengl",
+    "--enable-pulseaudio",
+]
+hostmakedepends = [
+    "automake",
+    "bison",
+    "gettext-devel",
+    "libtool",
+    "pkgconf",
+]
+makedepends = [
+    "glu-devel",
+    "gstreamer-devel",
+    "gtk+3-devel",
+    "libpulse-devel",
+    "libvisual-devel",
+    "pipewire-jack-devel",
+]
+install_if = [f"libvisual-plugins-meta={pkgver}-r{pkgrel}"]
+pkgdesc = "Abstraction library for audio visualization plugins (plugins)"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later"
+url = "http://libvisual.org"
+source = f"https://github.com/Libvisual/libvisual/releases/download/libvisual-plugins-{pkgver}/libvisual-plugins-{pkgver}.tar.bz2"
+sha256 = "a1dd04eb3d311d68b4f43a5c707df7aba7a98a9cab820c58395a7f5d7d6d0157"
+
+
+@subpackage("libvisual-plugins-meta")
+def _meta(self):
+    self.pkgdesc = f"{pkgdesc} (recommends package)"
+    self.options = ["empty"]
+    return []
