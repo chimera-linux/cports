c1140b56c9abafebce2c126735907d8e454e5ee3
psykose <alice@ayaya.dev>
2024-05-29 16:30:51 +0000

    contrib/rust-analyzer: make update-check nicer

--- contrib/rust-analyzer/update.py
+++ contrib/rust-analyzer/update.py
@@ -1,3 +1,7 @@
 url = "https://api.github.com/repos/rust-lang/rust-analyzer/git/refs/tags"
 pattern = r"refs/tags/(\d+\-\d+\-\d+)"
 ignore = ["nightly"]
+
+
+def fetch_versions(self, src):
+    return map(lambda v: v.replace("-", "."), self.fetch_versions(src))
