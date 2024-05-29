bf28c966b0e4714c72911270a7daf572e253f773
psykose <alice@ayaya.dev>
2024-05-29 13:06:01 +0000

    contrib/libdotconf: new package (1.4.1)

---
+++ contrib/libdotconf/template.py
@@ -0,0 +1,20 @@
+pkgname = "libdotconf"
+pkgver = "1.4.1"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = [
+    "automake",
+    "libtool",
+    "pkgconf",
+]
+pkgdesc = "Dot.conf configuration file parser library"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-only AND Apache-1.1"
+url = "https://github.com/williamh/dotconf"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "5922c46cacf99b2ecc4853d28a2bda4a489292e73276e604bd9cba29dfca892d"
+
+
+@subpackage("libdotconf-devel")
+def _devel(self):
+    return self.default_devel()
