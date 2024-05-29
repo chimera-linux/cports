97d679a1bcf01556ed39f693864bbdb6cdc9c54f
triallax <triallax@tutanota.com>
2024-05-30 00:33:38 +0100

    main/libvisual: update to 0.4.2

--- main/libvisual/template.py
+++ main/libvisual/template.py
@@ -1,23 +1,32 @@
 pkgname = "libvisual"
-pkgver = "0.4.1"
+pkgver = "0.4.2"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_args = ["--disable-examples"]
+configure_args = ["--disable-examples", "--disable-lv-tool"]
 make_cmd = "gmake"
 # must be used to overwrite generated junk that messes up build
 make_dir = "."
-hostmakedepends = ["gmake", "pkgconf"]
+hostmakedepends = [
+    "autoconf-archive",
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "libtool",
+    "pkgconf",
+]
 pkgdesc = "Abstraction library for audio visualization plugins"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
-url = "http://sourceforge.net/projects/libvisual"
-source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "aa12877417f76d3642d9f4c723302380d833175639d63a55641d01928a5ddb7d"
+url = "http://libvisual.org"
+source = f"https://github.com/Libvisual/libvisual/releases/download/libvisual-{pkgver}/libvisual-{pkgver}.tar.gz"
+sha256 = "63085fd9835c42c9399ea6bb13a7ebd4b1547ace75c4595ce8e9759512bd998a"
+
+
+def post_install(self):
+    # We disable the tool but the manpage gets installed anyway
+    self.rm(self.destdir / "usr/share/man/man1/lv-tool-0.4.1")
 
 
 @subpackage("libvisual-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
