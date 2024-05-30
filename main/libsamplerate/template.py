95acd4626f09f262ed915f09d5e18cf320dd62fc
triallax <triallax@tutanota.com>
2024-05-30 01:24:29 +0100

    main/libsamplerate: update to 0.2.2

--- main/libsamplerate/template.py
+++ main/libsamplerate/template.py
@@ -1,16 +1,16 @@
 pkgname = "libsamplerate"
-pkgver = "0.1.9"
+pkgver = "0.2.2"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
-makedepends = ["libsndfile-devel", "fftw-devel", "linux-headers"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
+makedepends = ["linux-headers"]
+checkdepends = ["fftw-devel", "libsndfile-devel"]
 pkgdesc = "Sample Rate Converter for audio"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
-url = "http://www.mega-nerd.com/SRC"
-source = f"{url}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "0a7eb168e2f21353fb6d84da152e4512126f7dc48ccb0be80578c565413444c1"
-tool_flags = {"CFLAGS": ["-fPIC"]}
+url = "https://libsndfile.github.io/libsamplerate"
+source = f"https://github.com/libsndfile/libsamplerate/releases/download/{pkgver}/libsamplerate-{pkgver}.tar.xz"
+sha256 = "3258da280511d24b49d6b08615bbe824d0cacc9842b0e4caf11c52cf2b043893"
 
 
 def post_install(self):
@@ -20,6 +20,3 @@ def post_install(self):
 @subpackage("libsamplerate-devel")
 def _devel(self):
     return self.default_devel(extra=["usr/share/doc"])
-
-
-configure_gen = []
