30860d4647d73a5743b9200ce94eee084ece7cf0
triallax <triallax@tutanota.com>
2024-05-30 21:24:39 +0100

    main/python-sphinxcore-applehelp: update to 1.0.8, fix update-check

--- main/python-sphinxcontrib-applehelp/template.py
+++ main/python-sphinxcontrib-applehelp/template.py
@@ -1,16 +1,16 @@
 pkgname = "python-sphinxcontrib-applehelp"
-pkgver = "1.0.4"
-pkgrel = 1
+pkgver = "1.0.8"
+pkgrel = 0
 build_style = "python_pep517"
-hostmakedepends = ["python-build", "python-installer", "python-flit_core"]
+hostmakedepends = ["python-build", "python-flit_core", "python-installer"]
 checkdepends = ["python-sphinx"]
 depends = ["python"]
 pkgdesc = "Sphinx extension which outputs Apple help book"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
-url = "http://sphinx-doc.org"
-source = f"$(PYPI_SITE)/s/sphinxcontrib-applehelp/sphinxcontrib-applehelp-{pkgver}.tar.gz"
-sha256 = "828f867945bbe39817c210a1abfd1bc4895c8b73fcaade56d45357a348a07d7e"
+url = "https://www.sphinx-doc.org"
+source = f"$(PYPI_SITE)/s/sphinxcontrib-applehelp/sphinxcontrib_applehelp-{pkgver}.tar.gz"
+sha256 = "c40a4f96f3776c4393d933412053962fac2b84f4c99a7982ba42e09576a70619"
 # circular checkdepends
 options = ["!check"]
 
