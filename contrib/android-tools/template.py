e80ee004cc53e2da327e399cfee924322136d603
Jami Kettunen <jami.kettunen@protonmail.com>
2024-05-30 15:25:54 +0300

    contrib/android-tools: update to 35.0.1

--- contrib/android-tools/template.py
+++ contrib/android-tools/template.py
@@ -1,31 +1,36 @@
 pkgname = "android-tools"
-pkgver = "34.0.5"
-pkgrel = 3
+pkgver = "35.0.1"
+pkgrel = 0
 # only supports specific little-endian archs, particularly in boringssl
 archs = ["x86_64", "aarch64", "ppc64le", "riscv64"]
 build_style = "cmake"
-hostmakedepends = ["cmake", "ninja", "perl", "go", "pkgconf", "protobuf"]
+hostmakedepends = [
+    "cmake",
+    "go",
+    "ninja",
+    "perl",
+    "pkgconf",
+    "protobuf",
+]
 makedepends = [
     "brotli-devel",
+    "gtest-devel",
+    "libusb-devel",
+    "linux-headers",
     "lz4-devel",
     "pcre2-devel",
-    "libusb-devel",
-    "zstd-devel",
     "protobuf-devel",
-    "linux-headers",
-    "gtest-devel",
+    "zstd-devel",
 ]
-depends = ["python", "android-udev-rules"]
+depends = ["android-udev-rules", "python"]
 pkgdesc = "Android platform tools (e.g. adb and fastboot)"
 maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "Apache-2.0 AND ISC AND GPL-2.0-only AND MIT"
 url = "https://github.com/nmeum/android-tools"
 source = f"{url}/releases/download/{pkgver}/android-tools-{pkgver}.tar.xz"
-sha256 = "fb09cff12cfb82acf42a8ebebbc0342671bfcd02117716368bdc73fdda60304a"
+sha256 = "654030c7f96d25d7224cd6861fac14a043cf1d3980f40288cdfbe219f94ffaf9"
 tool_flags = {"CXXFLAGS": ["-D_LARGEFILE64_SOURCE"]}
 hardening = ["vis", "cfi"]
-# no tests
-options = ["!check"]
 
 
 def post_install(self):
