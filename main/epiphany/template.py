1047c0ab080b5e051ca1941b2adbfa5458ff00fa
psykose <alice@ayaya.dev>
2024-05-30 20:06:59 +0000

    main/epiphany: update to 46.1

--- main/epiphany/template.py
+++ main/epiphany/template.py
@@ -1,36 +1,36 @@
 pkgname = "epiphany"
-pkgver = "46.0"
+pkgver = "46.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dunit_tests=disabled"]
 hostmakedepends = [
+    "desktop-file-utils",
+    "gettext",
+    "glib-devel",
+    "itstool",
     "meson",
     "pkgconf",
-    "glib-devel",
-    "gettext",
-    "itstool",
-    "desktop-file-utils",
 ]
 makedepends = [
-    "webkitgtk4-devel",
     "cairo-devel",
     "gcr-devel",
     "gdk-pixbuf-devel",
     "glib-devel",
-    "gsettings-desktop-schemas-devel",
-    "gtk4-devel",
-    "gstreamer-devel",
-    "nettle-devel",
-    "json-glib-devel",
-    "libarchive-devel",
-    "libadwaita-devel",
-    "libsecret-devel",
-    "libxml2-devel",
-    "libportal-devel",
-    "libsoup-devel",
-    "sqlite-devel",
     "gmp-devel",
+    "gsettings-desktop-schemas-devel",
+    "gstreamer-devel",
+    "gtk4-devel",
     "iso-codes",
+    "json-glib-devel",
+    "libadwaita-devel",
+    "libarchive-devel",
+    "libportal-devel",
+    "libsecret-devel",
+    "libsoup-devel",
+    "libxml2-devel",
+    "nettle-devel",
+    "sqlite-devel",
+    "webkitgtk4-devel",
 ]
 depends = ["iso-codes"]
 pkgdesc = "GNOME web browser"
@@ -38,4 +38,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://wiki.gnome.org/Apps/Web"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "f4348f2cf51c07c0c106d130172d4d23f2cd4068771e1de007b758ca2ade5660"
+sha256 = "260f81469ed64746c2b1143f2f38bedcd96847786567b097d9f72fd3bd9db142"
