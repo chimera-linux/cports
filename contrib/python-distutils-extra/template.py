968683aee7d36701711a992268decdcc9419453c
triallax <triallax@tutanota.com>
2024-05-30 23:26:49 +0100

    contrib/python-distutils-extra: update to 3.0

--- contrib/python-distutils-extra/template.py
+++ contrib/python-distutils-extra/template.py
@@ -1,19 +1,22 @@
 pkgname = "python-distutils-extra"
-pkgver = "2.47"
+pkgver = "3.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
-    "python-wheel",
     "python-setuptools",
+    "python-wheel",
 ]
-depends = ["python"]
+depends = ["intltool", "python"]
+checkdepends = ["intltool", "python-gobject", "python-httplib2"]
 pkgdesc = "Enhanced distutils package for Python"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://launchpad.net/python-distutils-extra"
 source = f"https://salsa.debian.org/python-team/modules/python-distutils-extra/-/archive/{pkgver}/python-distutils-extra-{pkgver}.tar.bz2"
-sha256 = "bc8979d320d5b79016e65b5e598aa100f96ef04aeba0e389d4dfec7c4d9f6c15"
-# TODO
-options = ["!check"]
+sha256 = "ee1a6a3f97fd87db6d94007324239da58d9b4860f11f05dd66614565de2e6123"
+
+
+def do_check(self):
+    self.do("python", "setup.py", "test")
