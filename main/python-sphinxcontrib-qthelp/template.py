f22f1c6c235d9f4b7810a88e12ee83fb8c0d01d3
triallax <triallax@tutanota.com>
2024-05-30 21:22:01 +0100

    main/python-sphinxcontrib-qthelp: update to 1.0.7, fix update-check

--- main/python-sphinxcontrib-qthelp/template.py
+++ main/python-sphinxcontrib-qthelp/template.py
@@ -1,23 +1,18 @@
 pkgname = "python-sphinxcontrib-qthelp"
-pkgver = "1.0.3"
-pkgrel = 1
+pkgver = "1.0.7"
+pkgrel = 0
 build_style = "python_pep517"
-hostmakedepends = [
-    "python-build",
-    "python-installer",
-    "python-setuptools",
-    "python-wheel",
-]
+hostmakedepends = ["python-build", "python-flit_core", "python-installer"]
 checkdepends = ["python-sphinx"]
 depends = ["python"]
 pkgdesc = "Sphinx extension which outputs QtHelp document"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
-url = "http://sphinx-doc.org"
+url = "https://www.sphinx-doc.org"
 source = (
-    f"$(PYPI_SITE)/s/sphinxcontrib-qthelp/sphinxcontrib-qthelp-{pkgver}.tar.gz"
+    f"$(PYPI_SITE)/s/sphinxcontrib-qthelp/sphinxcontrib_qthelp-{pkgver}.tar.gz"
 )
-sha256 = "4c33767ee058b70dba89a6fc5c1892c0d57a54be67ddd3e7875a18d14cba5a72"
+sha256 = "053dedc38823a80a7209a80860b16b722e9e0209e32fea98c90e4e6624588ed6"
 # circular checkdepends
 options = ["!check"]
 
