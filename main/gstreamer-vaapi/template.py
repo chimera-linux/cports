9be1e212319801005c68708c1c2a110d788ad3c2
psykose <alice@ayaya.dev>
2024-05-30 13:27:02 +0000

    main/gstreamer-vaapi: new package (1.24.4)

---
+++ main/gstreamer-vaapi/template.py
@@ -0,0 +1,32 @@
+pkgname = "gstreamer-vaapi"
+pkgver = "1.24.4"
+pkgrel = 0
+build_style = "meson"
+configure_args = [
+    "--auto-features=enabled",
+    "-Ddefault_library=shared",
+    # misc
+    "-Ddoc=disabled",
+]
+make_check_args = ["--timeout-multiplier=5"]
+hostmakedepends = [
+    "gettext",
+    "meson",
+    "pkgconf",
+    "wayland-progs",
+]
+makedepends = [
+    "gstreamer-devel",
+    "gst-plugins-bad-devel",
+    "gst-plugins-base-devel",
+    "gtk+3-devel",
+    "libva-devel",
+    "libxrandr-devel",
+    "wayland-protocols",
+]
+pkgdesc = "GStreamer VA-API plugins"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-or-later"
+url = "https://gstreamer.freedesktop.org"
+source = f"{url}/src/gstreamer-vaapi/gstreamer-vaapi-{pkgver}.tar.xz"
+sha256 = "2dfb322e37370b6671b62ff60dc1da81f87dac5a17c1c7ef15a783d2355900b8"
