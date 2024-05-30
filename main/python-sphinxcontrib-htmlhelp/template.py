d2d2e4a528d67f97d44483b195aaa2457ee9e67d
triallax <triallax@tutanota.com>
2024-05-30 21:23:06 +0100

    main/python-sphinxcontrib-htmlhelp: update to 2.0.5, fix update-check

--- main/python-sphinxcontrib-htmlhelp/template.py
+++ main/python-sphinxcontrib-htmlhelp/template.py
@@ -1,16 +1,16 @@
 pkgname = "python-sphinxcontrib-htmlhelp"
-pkgver = "2.0.1"
-pkgrel = 1
+pkgver = "2.0.5"
+pkgrel = 0
 build_style = "python_pep517"
-hostmakedepends = ["python-build", "python-installer", "python-flit_core"]
+hostmakedepends = ["python-build", "python-flit_core", "python-installer"]
 checkdepends = ["python-sphinx"]
 depends = ["python"]
 pkgdesc = "Sphinx extension which outputs HTML document"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
-url = "http://sphinx-doc.org"
-source = f"$(PYPI_SITE)/s/sphinxcontrib-htmlhelp/sphinxcontrib-htmlhelp-{pkgver}.tar.gz"
-sha256 = "0cbdd302815330058422b98a113195c9249825d681e18f11e8b1f78a2f11efff"
+url = "https://www.sphinx-doc.org"
+source = f"$(PYPI_SITE)/s/sphinxcontrib-htmlhelp/sphinxcontrib_htmlhelp-{pkgver}.tar.gz"
+sha256 = "0dc87637d5de53dd5eec3a6a01753b1ccf99494bd756aafecd74b4fa9e729015"
 # circular checkdepends
 options = ["!check"]
 
