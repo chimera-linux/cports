b14916d459cf835c4df2a0b9132382e0083a7d46
Erica Z <zerica@callcc.eu>
2024-05-30 14:06:02 +0200

    contrib/schismtracker: update to 20240529

--- contrib/schismtracker/template.py
+++ contrib/schismtracker/template.py
@@ -1,5 +1,5 @@
 pkgname = "schismtracker"
-pkgver = "20240523"
+pkgver = "20240529"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf"]
@@ -9,5 +9,5 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-2.0-or-later"
 url = "https://schismtracker.org"
 source = f"https://github.com/schismtracker/schismtracker/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "956e844c0da6ff57a97ee27a173cb07c9c9c550f24bce23a3525f37936ee4fb2"
+sha256 = "73c475b3344c460cbc543878f8c728af3c62fba11211604834d880c0a41a506e"
 tool_flags = {"CFLAGS": [f'-DVERSION2="{pkgver}"']}
