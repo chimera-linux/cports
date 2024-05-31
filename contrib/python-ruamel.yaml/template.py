f4c2b1ab329edd5a6cee244cf40244e69d95451e
psykose <alice@ayaya.dev>
2024-05-31 17:38:41 +0000

    contrib/python-ruamel.yaml: new package (0.18.6)

---
+++ contrib/python-ruamel.yaml/template.py
@@ -0,0 +1,25 @@
+pkgname = "python-ruamel.yaml"
+pkgver = "0.18.6"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-devel",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+depends = ["python-ruamel.yaml.clib"]
+checkdepends = ["python-pytest"]
+pkgdesc = "YAML parser/emitter for Python"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://sourceforge.net/projects/ruamel-yaml"
+source = f"$(PYPI_SITE)/r/ruamel.yaml/ruamel.yaml-{pkgver}.tar.gz"
+sha256 = "8b27e6a217e786c6fbe5634d8f3f11bc63e0f80f6a5890f28863d9c45aac311b"
+# no tests on pypi
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
