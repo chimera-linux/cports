ba7776d520d68cabfaa9777f94e9f6d0a576314b
psykose <alice@ayaya.dev>
2024-05-31 12:34:33 +0000

    contrib/libaccounts-glib: new package (1.27)

---
+++ contrib/libaccounts-glib/template.py
@@ -0,0 +1,33 @@
+pkgname = "libaccounts-glib"
+pkgver = "1.27"
+pkgrel = 0
+build_style = "meson"
+make_check_wrapper = ["dbus-run-session", "--"]
+hostmakedepends = [
+    "gobject-introspection",
+    "meson",
+    "pkgconf",
+    "vala",
+]
+makedepends = [
+    "check-devel",
+    "glib-devel",
+    "libxml2-devel",
+    "sqlite-devel",
+]
+checkdepends = ["dbus"]
+pkgdesc = "GLib-based library for managing the accounts database"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-only"
+url = "https://gitlab.com/accounts-sso/libaccounts-glib"
+source = (
+    f"{url}/-/archive/VERSION_{pkgver}/libaccounts-glib-VERSION_{pkgver}.tar.gz"
+)
+sha256 = "a8407a5897a2e425ea1aa955ecf88485dd2fd417919de275b27c781a5d0637a5"
+# gobject-introspection
+options = ["!cross"]
+
+
+@subpackage("libaccounts-glib-devel")
+def _devel(self):
+    return self.default_devel()
