a41e10fadee3259badcee3e3f6e9fb94ab6a0f08
psykose <alice@ayaya.dev>
2024-05-31 23:17:54 +0000

    contrib/ebook-tools: new package (0.2.2)

---
+++ contrib/ebook-tools/template.py
@@ -0,0 +1,27 @@
+pkgname = "ebook-tools"
+pkgver = "0.2.2"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+]
+makedepends = [
+    "libxml2-devel",
+    "libzip-devel",
+]
+pkgdesc = "Tools for accessing and converting ebook formats"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://sourceforge.net/projects/ebook-tools"
+source = f"$(SOURCEFORGE_SITE)/ebook-tools/ebook-tools-{pkgver}.tar.gz"
+sha256 = "cbc35996e911144fa62925366ad6a6212d6af2588f1e39075954973bbee627ae"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("ebook-tools-devel")
+def _devel(self):
+    return self.default_devel()
