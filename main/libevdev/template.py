005585f892bca0a3944c0f87a3ed9b5aab6d0549
psykose <alice@ayaya.dev>
2024-05-31 10:15:23 +0000

    main/libevdev: update to 1.13.2

--- main/libevdev/template.py
+++ main/libevdev/template.py
@@ -1,5 +1,5 @@
 pkgname = "libevdev"
-pkgver = "1.13.1"
+pkgver = "1.13.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-gcov"]
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.freedesktop.org/wiki/Software/libevdev"
 source = f"$(FREEDESKTOP_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "06a77bf2ac5c993305882bc1641017f5bec1592d6d1b64787bad492ab34f2f36"
+sha256 = "3eca86a6ce55b81d5bce910637fc451c8bbe373b1f9698f375c7f1ad0de3ac48"
 # FIXME int
 hardening = ["!int"]
 
