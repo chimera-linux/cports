3c87a47ab0e2ba5a745ad0756b051ff6c0e447a3
psykose <alice@ayaya.dev>
2024-05-31 19:58:57 +0000

    contrib/qqc2-breeze-style: new package (6.0.5)

---
+++ contrib/qqc2-breeze-style/template.py
@@ -0,0 +1,37 @@
+pkgname = "qqc2-breeze-style"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+# testAnimationSpeedModifier_kconfig() write not going through? 'longDurationSpy.wait()' returned FALSE
+# make_check_args = ["-E", "animationspeedmodifiertest"]
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcolorscheme-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kguiaddons-devel",
+    "kiconthemes-devel",
+    "kirigami-devel",
+    "kquickcharts-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qttools-devel",
+]
+pkgdesc = "Breeze inspired QQC2 style"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later"
+url = "https://invent.kde.org/plasma/qqc2-breeze-style"
+source = f"$(KDE_SITE)/plasma/{pkgver}/qqc2-breeze-style-{pkgver}.tar.xz"
+sha256 = "d4fcc59646a066fd392a9ed8657189eec677e72249c8a4e7247ed89abc474496"
+# CFI: check
+hardening = ["vis", "!cfi"]
+
+
+@subpackage("qqc2-breeze-style-devel")
+def _devel(self):
+    return self.default_devel()
