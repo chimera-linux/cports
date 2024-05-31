f1e481539ef57791c9dbe90d11aa2ceb388627cd
psykose <alice@ayaya.dev>
2024-05-31 16:28:35 +0000

    contrib/gwenview: new package (24.05.0)

---
+++ contrib/gwenview/template.py
@@ -0,0 +1,65 @@
+pkgname = "gwenview"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_args = [
+    "-E",
+    "("
+    # who knows
+    + "placetreemodeltest|"
+    + "urlutilstest|"
+    + "contextmanagertest"
+    + ")",
+]
+make_check_wrapper = [
+    "wlheadless-run",
+    "--",
+]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+    "wayland-progs",
+]
+makedepends = [
+    # "baloo-devel",
+    # "cfitsio-devel",
+    "exiv2-devel",
+    "kdoctools-devel",
+    "kguiaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kimageannotator-devel",
+    "kio-devel",
+    "kitemmodels-devel",
+    "knotifications-devel",
+    "kparts-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "lcms2-devel",
+    "libkdcraw-devel",
+    "libtiff-devel",
+    "phonon-devel",
+    "plasma-activities-devel",
+    "purpose-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtsvg-devel",
+    "qt6-qtwayland-devel",
+    "wayland-devel",
+    "wayland-protocols",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE image viewer"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/gwenview"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/gwenview-{pkgver}.tar.xz"
+sha256 = "b1ed8270ca4f7967c7636967a18a8b9bf1728970f6cf8a7e11b423d0bceae4b0"
+# avoid crash in raw thumbnailer
+tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=0x200000"]}
+# CFI: crashes on start
+hardening = ["vis", "!cfi"]
+# TODO
+options = ["!cross"]
