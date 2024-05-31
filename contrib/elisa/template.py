867c92e2fa2936906c984e1b5c64ec1358049a80
psykose <alice@ayaya.dev>
2024-05-31 09:47:52 +0000

    contrib/elisa: new package (24.05.0)

---
+++ contrib/elisa/template.py
@@ -0,0 +1,48 @@
+pkgname = "elisa"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+# some tests try open the same test.ogg
+make_check_args = ["-j1"]
+make_check_wrapper = [
+    "wlheadless-run",
+    "--",
+]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kfilemetadata-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "kirigami-addons-devel",
+    "kirigami-devel",
+    "kxmlgui-devel",
+    "qqc2-desktop-style-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtmultimedia-devel",
+    "qt6-qtsvg-devel",
+    # "vlc-devel",
+]
+depends = ["kirigami-addons"]
+checkdepends = ["xwayland-run"] + depends
+pkgdesc = "KDE music player"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-3.0-or-later"
+url = "https://apps.kde.org/elisa"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/elisa-{pkgver}.tar.xz"
+sha256 = "2d377125cb3432d7a220b0e907133348e67d251c8cda60c25e04bba0d24e8df7"
+# CFI: crashes on start
+hardening = ["vis", "!cfi"]
+# TODO
+options = ["!cross"]
