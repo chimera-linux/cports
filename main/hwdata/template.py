332ae9ca91be3cb577c9dbf761e5c2d235297d1c
psykose <alice@ayaya.dev>
2024-05-31 10:12:59 +0000

    main/hwdata: update to 0.383

--- main/hwdata/template.py
+++ main/hwdata/template.py
@@ -1,5 +1,5 @@
 pkgname = "hwdata"
-pkgver = "0.382"
+pkgver = "0.383"
 pkgrel = 0
 build_style = "configure"
 configure_args = [
@@ -23,7 +23,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/vcrhonek/hwdata"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5e25457b562a5227eb77eac21d5e4344bd1183c7d62b41e7d3e780ae33e053c1"
+sha256 = "97f328f85e51ea78ad14620705dba269cbbb47911e56bdd8b35360e6cdccc00c"
 # cycle with pciutils, etc.
 options = ["!check", "empty"]
 
