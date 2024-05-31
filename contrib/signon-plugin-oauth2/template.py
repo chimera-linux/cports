5cd50c4beb59d6d9a394aaa0d07e96637fec3799
psykose <alice@ayaya.dev>
2024-05-31 15:07:46 +0000

    contrib/signon-plugin-oauth2: new package (0.25_git20210102)

---
+++ contrib/signon-plugin-oauth2/template.py
@@ -0,0 +1,58 @@
+pkgname = "signon-plugin-oauth2"
+# fewer deprecations
+pkgver = "0.25_git20210102"
+pkgrel = 0
+_gitrev = "d759439066f0a34e5ad352ebab0b3bb2790d429e"
+build_style = "makefile"
+make_cmd = "gmake"
+make_use_env = True
+hostmakedepends = [
+    "gmake",
+    "pkgconf",
+    "qt6-qtbase",
+]
+makedepends = [
+    "qt6-qtbase-devel",
+    "signond-devel",
+]
+pkgdesc = "Oauth 1.0 and 2.0 plugin for signond"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-only"
+url = "https://gitlab.com/accounts-sso/signon-plugin-oauth2"
+source = f"{url}/-/archive/{_gitrev}.tar.gz"
+sha256 = "e27678964563cbb64d9bd4088c4c7876bb2202c8e7af8ea77d8f27c578a8d6e4"
+
+
+if self.profile().arch == "riscv64":
+    broken = "qmake busted under emulation (https://bugreports.qt.io/browse/QTBUG-98951)"
+
+
+def do_configure(self):
+    # TODO: build style these
+    self.do(
+        "qmake6",
+        "LIBDIR=/usr/lib",
+        "PREFIX=/usr",
+        f"QMAKE_CFLAGS={self.get_cflags(shell=True)}",
+        f"QMAKE_CXXFLAGS={self.get_cxxflags(shell=True)}",
+        f"QMAKE_LDFLAGS={self.get_ldflags(shell=True)}",
+    )
+
+
+def init_install(self):
+    self.make_install_args += [f"INSTALL_ROOT={self.chroot_destdir}"]
+
+
+def post_install(self):
+    # mistakenly installed
+    self.rm(self.destdir / "usr/bin/signon-oauth2plugin-tests")
+    self.rm(
+        self.destdir / "usr/share/signon-oauth2plugin-tests", recursive=True
+    )
+
+
+@subpackage("signon-plugin-oauth2-devel")
+def _devel(self):
+    self.pkgdesc = f"{pkgdesc} (development files)"
+    self.depends += [f"{pkgname}={pkgver}-r{pkgrel}"]
+    return ["usr/include", "usr/lib/pkgconfig"]
