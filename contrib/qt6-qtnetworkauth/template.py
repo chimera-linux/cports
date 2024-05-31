423268ac62e66931682e9f804d7ec2c317d78ce5
psykose <alice@ayaya.dev>
2024-05-31 16:48:17 +0000

    contrib/qt6-qtnetworkauth: new package (6.7.1)

---
+++ contrib/qt6-qtnetworkauth/template.py
@@ -0,0 +1,48 @@
+pkgname = "qt6-qtnetworkauth"
+pkgver = "6.7.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = ["qt6-qtbase-devel"]
+pkgdesc = "Qt6 Networkauth component"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = (
+    "LGPL-2.1-only AND LGPL-3.0-only AND GPL-3.0-only WITH Qt-GPL-exception-1.0"
+)
+url = "https://www.qt.io"
+source = f"https://download.qt.io/official_releases/qt/{pkgver[:-2]}/{pkgver}/submodules/qtnetworkauth-everywhere-src-{pkgver}.tar.xz"
+sha256 = "012b3a05c34f2cdd197bd209696493c9e8085daeaa36589fd791d449d841575d"
+# cross: TODO
+options = ["!cross"]
+
+
+def init_check(self):
+    self.make_check_env = {
+        "QT_QPA_PLATFORM": "offscreen",
+        "QML2_IMPORT_PATH": str(
+            self.chroot_cwd / f"{self.make_dir}/lib/qt6/qml"
+        ),
+    }
+
+
+def post_install(self):
+    self.rm(self.destdir / "usr/tests", recursive=True)
+
+
+@subpackage("qt6-qtnetworkauth-devel")
+def _devel(self):
+    self.depends += [
+        f"qt6-qtbase-devel~{pkgver[:-2]}",
+    ]
+    return self.default_devel(
+        extra=[
+            "usr/lib/qt6/metatypes",
+            "usr/lib/qt6/mkspecs",
+            "usr/lib/qt6/modules",
+            "usr/lib/*.prl",
+        ]
+    )
