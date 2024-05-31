85515f4ed1dd7463ce106bad06936bc9cb61aa76
triallax <triallax@tutanota.com>
2024-05-31 23:55:10 +0100

    main/gnome-desktop: re-add Arabic to LINGUAS
    seems to work fine now

--- main/gnome-desktop/template.py
+++ main/gnome-desktop/template.py
@@ -1,31 +1,31 @@
 pkgname = "gnome-desktop"
 pkgver = "44.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
     "-Dudev=enabled",
     "-Dsystemd=disabled",
 ]
 hostmakedepends = [
+    "docbook-xml",
+    "gettext",
+    "glib-devel",
+    "gobject-introspection",
+    "itstool",
     "meson",
     "pkgconf",
-    "gobject-introspection",
-    "glib-devel",
-    "gettext",
-    "docbook-xml",
-    "itstool",
 ]
 makedepends = [
-    "udev-devel",
+    "fontconfig-devel",
+    "gdk-pixbuf-devel",
+    "glib-devel",
     "gsettings-desktop-schemas-devel",
     "gtk+3-devel",
     "gtk4-devel",
-    "gdk-pixbuf-devel",
-    "glib-devel",
-    "fontconfig-devel",
+    "iso-codes",
     "libseccomp-devel",
     "libxkbcommon-devel",
-    "iso-codes",
+    "udev-devel",
     "xkeyboard-config",
 ]
 depends = [
@@ -41,7 +41,7 @@ url = "https://gitlab.gnome.org/GNOME/gnome-desktop"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
 sha256 = "42c773745d84ba14bc1cf1c4c6f4606148803a5cd337941c63964795f3c59d42"
 # needs graphical environment
-options = ["!check"]
+options = ["!check", "!cross"]
 
 
 @subpackage("gnome-desktop-devel")
