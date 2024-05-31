269f2d577d1f15861b06aba827987eb782c66f59
psykose <alice@ayaya.dev>
2024-05-31 23:44:53 +0000

    contrib/okular: new package (24.05.0)

---
+++ contrib/okular/template.py
@@ -0,0 +1,66 @@
+pkgname = "okular"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+# FIXME segfaults/weird failures
+make_check_args = [
+    "-E",
+    "(parttest|visibilitytest|signunsignedfieldtest|documenttest|mainshelltest|annotationtoolbartest)",
+]
+make_check_wrapper = [
+    "dbus-run-session",
+    "--",
+    "wlheadless-run",
+    "--",
+]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "discount-devel",
+    "djvulibre-devel",
+    "ebook-tools-devel",
+    "karchive-devel",
+    "kbookmarks-devel",
+    "kcompletion-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdegraphics-mobipocket-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "kparts-devel",
+    "kpty-devel",
+    "ktextwidgets-devel",
+    "kwallet-devel",
+    "kwindowsystem-devel",
+    "kxmlgui-devel",
+    "libkexiv2-devel",
+    "libspectre-devel",
+    "libzip-devel",
+    "phonon-devel",
+    "plasma-activities-devel",
+    "poppler-qt-devel",
+    "purpose-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtspeech-devel",
+    "qt6-qtsvg-devel",
+    "threadweaver-devel",
+]
+checkdepends = ["dbus", "xwayland-run"]
+pkgdesc = "KDE document viewer"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/okular"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/okular-{pkgver}.tar.xz"
+sha256 = "6f322f78de2aa0a4079351fe4651c080bd98d27eeb7f9dcc37765271b4a3ac80"
+tool_flags = {"CFLAGS": ["-D_GNU_SOURCE"]}
+# CFI: crashes tests
+hardening = ["vis", "!cfi"]
+# TODO
+options = ["!cross"]
