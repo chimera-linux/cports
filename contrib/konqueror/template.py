03795e915d3c0e858273066b374d809c1c161570
psykose <alice@ayaya.dev>
2024-06-01 05:06:46 +0000

    contrib/konqueror: new package (24.05.0)

---
+++ contrib/konqueror/template.py
@@ -0,0 +1,59 @@
+pkgname = "konqueror"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_args = [
+    "-E",
+    # FIXME: crashes / hangs
+    "(konqviewtest"
+    + "|konqhtmltest"
+    + "|sidebar-modulemanagertest"
+    + "|konqpopupmenutest"
+    + "|webengine_partapi_test"
+    + "|konqviewmgrtest)",
+]
+make_check_wrapper = ["dbus-run-session", "--", "wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "hunspell",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "hunspell-devel",
+    "karchive-devel",
+    "kcmutils-devel",
+    "kcodecs-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdesu-devel",
+    "kdoctools-devel",
+    "kguiaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "knotifications-devel",
+    "kparts-devel",
+    "ktextwidgets-devel",
+    "kwallet-devel",
+    "kwindowsystem-devel",
+    "plasma-activities-devel",
+    "qt6-qtwebengine-devel",
+    "sonnet-devel",
+]
+checkdepends = ["dbus", "xwayland-run"]
+pkgdesc = "KDE web browser and file previewer"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-3.0-only AND GPL-2.0-or-later"
+url = "https://apps.kde.org/konqueror"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/konqueror-{pkgver}.tar.xz"
+sha256 = "508942c79fb65a023157cdc26bbfcc7451c506fa5f8cd06efeb1da9d8262242d"
+# CFI: crashes tests
+hardening = ["vis", "!cfi"]
+
+
+@subpackage("konqueror-devel")
+def _devel(self):
+    return self.default_devel()
