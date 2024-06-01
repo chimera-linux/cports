f087c15b07f335f35f3ce2622c1c4c1eb831ec49
psykose <alice@ayaya.dev>
2024-06-01 03:12:09 +0000

    contrib/sysprof: !int

--- contrib/sysprof/template.py
+++ contrib/sysprof/template.py
@@ -1,6 +1,6 @@
 pkgname = "sysprof"
 pkgver = "46.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
     # creates static separately itself
@@ -33,6 +33,8 @@ license = "GPL-2.0-or-later"
 url = "https://www.sysprof.com"
 source = f"$(GNOME_SITE)/sysprof/{'.'.join(pkgver.rsplit('.')[:-1])}/sysprof-{pkgver}.tar.xz"
 sha256 = "73aa7e75ebab3e4e0946a05a723df7e6ee4249e3b9e884dba35500aba2a1d176"
+# sysprof`sysprof_disk_usage_record_fiber muloverflow when busy i/o
+hardening = ["!int"]
 
 
 def post_install(self):
