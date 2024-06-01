8624208888519124128cdd3723c9cb1df059197e
psykose <alice@ayaya.dev>
2024-06-01 01:55:38 +0000

    contrib/oxygen-sounds: new package (6.0.5)

---
+++ contrib/oxygen-sounds/template.py
@@ -0,0 +1,19 @@
+pkgname = "oxygen-sounds"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "Ocean Sound Theme for KDE Plasma"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "CC-BY-SA-4.0"
+url = "https://invent.kde.org/plasma/oxygen-sounds"
+source = f"$(KDE_SITE)/plasma/{pkgver}/oxygen-sounds-{pkgver}.tar.xz"
+sha256 = "6b34747abd6839017d0eacc1d52c85b526dbb678201fc4cc3719278671253952"
