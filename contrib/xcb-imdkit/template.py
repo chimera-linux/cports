e0bab42208f756c8cc9407c510978c34c737f39f
psykose <alice@ayaya.dev>
2024-06-01 03:05:13 +0000

    contrib/xcb-imdkit: update to 1.0.9

--- contrib/xcb-imdkit/template.py
+++ contrib/xcb-imdkit/template.py
@@ -1,5 +1,5 @@
 pkgname = "xcb-imdkit"
-pkgver = "1.0.8"
+pkgver = "1.0.9"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -18,7 +18,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-only"
 url = "https://github.com/fcitx/xcb-imdkit"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "bea897d27df2b66bbff144ba85a1e183dbc1a6a5bd6179abed11aef21ab2e38a"
+sha256 = "c2f0bbad8a335a64cdc7c19ac7b6ea1f0887dd6300ca9a4fa2e2fec6b9d3f695"
 # FIXME: cfi
 hardening = ["vis"]
 
