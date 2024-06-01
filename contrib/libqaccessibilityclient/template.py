96d9979249142199c238833612df1b2b8cab7c10
psykose <alice@ayaya.dev>
2024-06-01 06:08:40 +0000

    contrib/libqaccessibilityclient: new package (0.6.0)

---
+++ contrib/libqaccessibilityclient/template.py
@@ -0,0 +1,31 @@
+pkgname = "libqaccessibilityclient"
+pkgver = "0.6.0"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_WITH_QT6=ON"]
+# FIXME: fails for unknown reasons
+make_check_args = ["-E", "libkdeaccessibilityclient-tst_accessibilityclient"]
+make_check_wrapper = ["dbus-run-session", "wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+]
+makedepends = [
+    "qt6-qtbase-devel",
+]
+checkdepends = ["dbus", "xwayland-run"]
+pkgdesc = "Accessibility helper library"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-only OR LGPL-3.0-only"
+url = "https://invent.kde.org/libraries/libqaccessibilityclient"
+source = f"$(KDE_SITE)/libqaccessibilityclient/libqaccessibilityclient-{pkgver}.tar.xz"
+sha256 = "4c50c448622dc9c5041ed10da7d87b3e4e71ccb49d4831a849211d423c5f5d33"
+# CFI: check
+hardening = ["vis", "!cfi"]
+
+
+@subpackage("libqaccessibilityclient-devel")
+def _devel(self):
+    self.depends += ["qt6-qtbase-devel"]
+    return self.default_devel()
