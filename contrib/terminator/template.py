0eccae1bff586dcbab2a6b436e7c4ee02905edf8
psykose <alice@ayaya.dev>
2024-06-01 02:58:26 +0000

    contrib/terminator: update to 2.1.4

--- contrib/terminator/template.py
+++ contrib/terminator/template.py
@@ -1,6 +1,6 @@
 pkgname = "terminator"
-pkgver = "2.1.3"
-pkgrel = 1
+pkgver = "2.1.4"
+pkgrel = 0
 build_style = "python_pep517"
 make_check_target = "tests"
 make_check_wrapper = ["xvfb-run"]
@@ -37,7 +37,7 @@ maintainer = "yopito <pierre.bourgin@free.fr>"
 license = "GPL-2.0-only"
 url = "https://gnome-terminator.org"
 source = f"https://github.com/gnome-terminator/terminator/releases/download/v{pkgver}/terminator-{pkgver}.tar.gz"
-sha256 = "0ae9943f3d6b72230c14bcb355de84dd81274c033e76ca4698e80d7c93cd6ae5"
+sha256 = "af27b0ece862e61dde71d0827afa4a29a414e44599effe3edeebc52cbdf0c5e8"
 hardening = ["vis"]
 # testsuite fails within container and xvfb
 options = ["!check"]
