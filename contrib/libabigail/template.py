0f0a2561db31c89fc58ef0628979cd42c230e4d2
psykose <alice@ayaya.dev>
2024-06-01 19:37:02 +0000

    contrib/libabigail: new package (2.5)

---
+++ contrib/libabigail/template.py
@@ -0,0 +1,73 @@
+pkgname = "libabigail"
+pkgver = "2.5"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--disable-apidoc",
+    "--disable-ctf",
+    "--disable-deb",
+    "--disable-fedabipkgdiff",
+    "--disable-rpm",
+    "--disable-rpm415",
+    "--enable-abidb",
+    "--enable-bash-completion",
+    "--enable-btf",
+    "--enable-manual",
+    "--enable-python3",  # test stuff
+    "--enable-tar",
+]
+make_cmd = "gmake"
+make_build_args = ["man"]
+hostmakedepends = [
+    "automake",
+    "gmake",
+    "libtool",
+    "pkgconf",
+    "python",
+    "python-sphinx",
+]
+makedepends = [
+    "bash-completion",
+    "elfutils-devel",
+    "libbpf-devel",
+    "libxml2-devel",
+    "musl-fts-devel",
+]
+pkgdesc = "Library and tooling for ABI-related tasks"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Apache-2.0 WITH LLVM-exception"
+url = "https://sourceware.org/libabigail"
+source = f"https://mirrors.kernel.org/sourceware/libabigail/libabigail-{pkgver}.tar.xz"
+sha256 = "7cfc4e9b00ae38d87fb0c63beabb32b9cbf9ce410e52ceeb5ad5b3c5beb111f3"
+tool_flags = {
+    # see libbpf comment about bpf headers
+    "CFLAGS": ["-I/usr/include/bpf/uapi"],
+    "CXXFLAGS": ["-I/usr/include/bpf/uapi"],
+}
+# CFI: fails most tests
+hardening = ["vis", "!cfi"]
+# FIXME: fail due to abi diff in musl headers, some bashisms, ..
+options = ["!check"]
+
+
+def post_install(self):
+    self.do(
+        "gmake",
+        "-C",
+        f"{self.make_dir}/doc/manuals",
+        f"DESTDIR={self.chroot_destdir}",
+        "install-man-and-info-doc",
+    )
+    for comp in ["abicompat", "abidiff", "abidw", "abilint", "abipkgdiff"]:
+        self.install_completion(f"bash-completion/{comp}", "bash", name=comp)
+    self.install_license("LICENSE.txt")
+
+
+@subpackage("libabigail-devel")
+def _devel(self):
+    return self.default_devel()
+
+
+@subpackage("libabigail-progs")
+def _progs(self):
+    return self.default_progs()
