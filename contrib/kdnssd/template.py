4755d5d35f85df66ca1d2e876b974ee98df8e342
psykose <alice@ayaya.dev>
2024-06-01 02:06:24 +0000

    contrib/kdnssd: new package (6.2.0)

---
+++ contrib/kdnssd/template.py
@@ -0,0 +1,29 @@
+pkgname = "kdnssd"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "avahi-devel",
+    "qt6-qtbase-devel",
+    "qt6-qttools-devel",
+]
+pkgdesc = "KDE zeroconf integration"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://api.kde.org/frameworks/kdnssd/html/index.html"
+source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/kdnssd-{pkgver}.tar.xz"
+sha256 = "509011367c11c3b17034d743ea6934951a6fc525b9527d18c38af81f60b08f45"
+# CFI: check
+hardening = ["vis", "!cfi"]
+
+
+@subpackage("kdnssd-devel")
+def _devel(self):
+    return self.default_devel()
