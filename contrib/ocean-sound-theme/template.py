363f0f890c58435ce88d49ad7e1ed17a8e34e42d
psykose <alice@ayaya.dev>
2024-06-01 01:52:25 +0000

    contrib/ocean-sound-theme: fix url

--- contrib/ocean-sound-theme/template.py
+++ contrib/ocean-sound-theme/template.py
@@ -1,7 +1,7 @@
 # TODO: rename to sound-theme-ocean?
 pkgname = "ocean-sound-theme"
 pkgver = "6.0.5"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -14,6 +14,6 @@ makedepends = [
 pkgdesc = "Ocean Sound Theme for KDE Plasma"
 maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "CC-BY-SA-4.0"
-url = "https://api.kde.org/plasma/ocean-sound-theme/html"
+url = "https://invent.kde.org/plasma/ocean-sound-theme"
 source = f"$(KDE_SITE)/plasma/{pkgver}/ocean-sound-theme-{pkgver}.tar.xz"
 sha256 = "6015f3931a74134fdd7eef5c7c7592ef8b740fcc084d7726f8df993dc5289f6b"
