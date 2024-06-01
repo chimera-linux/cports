0ca39b4f11ddb8f5bc0f81f78629efa815d22f2f
psykose <alice@ayaya.dev>
2024-06-01 04:54:18 +0000

    contrib/polkit-kde-agent-1: new package (6.0.5)

---
+++ contrib/polkit-kde-agent-1/template.py
@@ -0,0 +1,33 @@
+pkgname = "polkit-kde-agent-1"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "polkit-qt-1-devel",
+    "ki18n-devel",
+    "kcrash-devel",
+    "kwindowsystem-devel",
+    "kdbusaddons-devel",
+    "kcoreaddons-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE polkit authentication daemon"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://invent.kde.org/plasma/polkit-kde-agent-1"
+source = f"$(KDE_SITE)/plasma/{pkgver}/polkit-kde-agent-1-{pkgver}.tar.xz"
+sha256 = "72cfa367fdd46b2c9176e47cd1307f2ea71c0207f2e3dccf06d292dce4d448a9"
+# CFI: check
+hardening = ["vis", "!cfi"]
+
+
+def post_install(self):
+    # TODO: dinit user service with graphical
+    self.rm(self.destdir / "usr/lib/systemd/user", recursive=True)
