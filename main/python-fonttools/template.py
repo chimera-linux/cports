5f4031232e2f748932922ddd74950e03ed7c8aa5
psykose <alice@ayaya.dev>
2024-06-01 03:02:25 +0000

    main/python-fonttools: update to 4.53.0

--- main/python-fonttools/template.py
+++ main/python-fonttools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-fonttools"
-pkgver = "4.52.4"
+pkgver = "4.53.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT AND OFL-1.1 AND BSD-3-Clause AND Apache-2.0"
 url = "https://github.com/fonttools/fonttools"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "06b50877534910977f091637ae660baa2f8d929d87dacf002b0d964abe78a0e0"
+sha256 = "f48a1e557e4c64b3a61c7a599bae3a33b44c0bbd190d8feb130d23065b0dfe71"
 
 
 def post_install(self):
