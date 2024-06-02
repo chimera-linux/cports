b20fb179a57952630b08f6a5755af3e8106056bc
psykose <alice@ayaya.dev>
2024-06-02 19:57:52 +0000

    contrib/plasma-disks: new package (6.0.5)
    not sure if this even works in kinfocenter yet

---
+++ contrib/plasma-disks/template.py
@@ -0,0 +1,35 @@
+pkgname = "plasma-disks"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "libcap-progs",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcoreaddons-devel",
+    "kdbusaddons-devel",
+    "knotifications-devel",
+    "ki18n-devel",
+    "solid-devel",
+    "kservice-devel",
+    "kio-devel",
+    "kauth-devel",
+    "kcmutils-devel",
+    "qt6-qtdeclarative-devel",
+]
+depends = ["smartmontools"]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE disk failure monitor"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-only OR GPL-3.0-only"
+url = "https://invent.kde.org/plasma/plasma-disks"
+source = f"$(KDE_SITE)/plasma/{pkgver}/plasma-disks-{pkgver}.tar.xz"
+sha256 = "321ec2f317704223997278ef4bf3b9cf012e2ffaec5a8d57e57feac6814e3ad8"
+# CFI: check
+hardening = ["vis", "!cfi"]
