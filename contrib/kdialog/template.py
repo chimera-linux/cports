e7a2f4fcf299e6533922a36d72d52481e975ff73
psykose <alice@ayaya.dev>
2024-06-02 19:12:32 +0000

    contrib/kdialog: new package (24.05.0)

---
+++ contrib/kdialog/template.py
@@ -0,0 +1,30 @@
+pkgname = "kdialog"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kdbusaddons-devel",
+    "kguiaddons-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "knotifications-devel",
+    "ktextwidgets-devel",
+    "kwindowsystem-devel",
+]
+pkgdesc = "KDE dialog displayer"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later"
+url = "https://develop.kde.org/docs/administration/kdialog"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kdialog-{pkgver}.tar.xz"
+sha256 = "8b7d922affbb415af0cfd43de8612e1dad8eb3e5b8d3f43284f1bd50dee0b240"
+# CFI: check
+hardening = ["vis", "!cfi"]
+# TODO
+options = ["!cross"]
