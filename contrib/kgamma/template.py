95b8d93142fb6d3afffbc472917fe77b354bfb72
psykose <alice@ayaya.dev>
2024-06-03 09:20:36 +0000

    contrib/kgamma: new package (6.0.5)

---
+++ contrib/kgamma/template.py
@@ -0,0 +1,27 @@
+pkgname = "kgamma"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcmutils-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE tool for adjusting monitor gamma"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://invent.kde.org/plasma/kgamma"
+source = f"$(KDE_SITE)/plasma/{pkgver}/kgamma-{pkgver}.tar.xz"
+sha256 = "659deb646fda17038d4548698c7ac4b08c12660623b78d75437a606d6a7bad36"
+# CFI: check
+hardening = ["vis", "!cfi"]
