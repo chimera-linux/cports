a110bac99c016bb99690b8174144c3d0bdbd891e
Ogromny <ogromnycoding@gmail.com>
2024-06-03 20:44:59 +0200

    contrib/toxic: new package (0.15.1)

---
+++ contrib/toxic/template.py
@@ -0,0 +1,40 @@
+pkgname = "toxic"
+pkgver = "0.15.1"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_build_args = [
+    "DISABLE_GAMES=1",
+    "PREFIX=/usr",
+]
+make_install_args = [
+    "PREFIX=/usr",
+]
+make_use_env = True
+hostmakedepends = [
+    "asciidoc",
+    "gmake",
+    "pkgconf",
+]
+makedepends = [
+    "c-toxcore-devel",
+    "freealut-devel",
+    "libconfig-devel",
+    "libcurl-devel",
+    "libnotify-devel",
+    "libpng-devel",
+    "libx11-devel",
+    "linux-headers",
+    "ncurses-devel",
+    "openal-soft-devel",
+    "python-devel",
+    "qrencode-devel",
+]
+pkgdesc = "Tox-based instant messaging and video chat client"
+maintainer = "ogromny <ogromnycoding@gmail.com>"
+license = "GPL-3.0-only"
+url = "https://github.com/JFreegman/toxic"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "56cedc37b22a1411c68fd8b395f40f515d6a4779be02540c5cd495665caa127c"
+# toxic has no tests
+options = ["!check"]
