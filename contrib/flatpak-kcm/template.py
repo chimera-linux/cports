6ad51f6f1b3786c9c45f0343a8e47e51677b8d7f
psykose <alice@ayaya.dev>
2024-06-03 15:52:54 +0000

    contrib/flatpak-kcm: new package (6.0.5)

---
+++ contrib/flatpak-kcm/template.py
@@ -0,0 +1,32 @@
+pkgname = "flatpak-kcm"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+# segfaults in libflatpak probably due to checking system-repo related stuff
+# in chroot
+make_check_args = ["-E", "flatpakpermissiontest"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "flatpak-devel",
+    "kcmutils-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kitemmodels-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtsvg-devel",
+]
+pkgdesc = "KDE Flatpak permissions KCM"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://invent.kde.org/plasma/flatpak-kcm"
+source = f"$(KDE_SITE)/plasma/{pkgver}/flatpak-kcm-{pkgver}.tar.xz"
+sha256 = "741b946c07d0d4e509f8cf2969b231e5f371020d72fa276a6f7e6163cd270ce3"
+# CFI: check
+hardening = ["vis", "!cfi"]
