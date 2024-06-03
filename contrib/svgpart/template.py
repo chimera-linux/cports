496688118ca56e7bc7c71f52761913e1e0559a3b
psykose <alice@ayaya.dev>
2024-06-03 10:44:14 +0000

    contrib/svgpart: new package (24.05.0)

---
+++ contrib/svgpart/template.py
@@ -0,0 +1,28 @@
+pkgname = "svgpart"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kparts-devel",
+    "kxmlgui-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtsvg-devel",
+]
+pkgdesc = "KParts plugin for SVG"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/svgpart"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/svgpart-{pkgver}.tar.xz"
+sha256 = "7d02438ac3b0d88e59a653f2302d1dbc7a2535ce627d49eb50a3a33852db42d4"
+# CFI: check
+hardening = ["vis", "!cfi"]
