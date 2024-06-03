f8215a3c803912b74e5bc35ad4966d024cc9bf46
psykose <alice@ayaya.dev>
2024-06-03 10:34:53 +0000

    contrib/dolphin-plugins: new package (24.05.0)

---
+++ contrib/dolphin-plugins/template.py
@@ -0,0 +1,33 @@
+pkgname = "dolphin-plugins"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "dolphin-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "ktexteditor-devel",
+    "ktextwidgets-devel",
+    "kxmlgui-devel",
+    "qt6-qtdeclarative-devel",
+    "solid-devel",
+]
+pkgdesc = "Plugins for the KDE Dolphin file manager"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/dolphin_plugins"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/dolphin-plugins-{pkgver}.tar.xz"
+)
+sha256 = "715329f75eb22d6f59baabc5defa1aa37c73ea16d62141c0eae695325c01ac2e"
+# CFI: check
+hardening = ["vis", "!cfi"]
