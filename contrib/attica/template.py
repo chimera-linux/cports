f69a7fba7aa54279258425bbc02179ad57d54fe7
triallax <triallax@tutanota.com>
2024-06-03 21:41:47 +0100

    contrib/attica: !cfi

--- contrib/attica/template.py
+++ contrib/attica/template.py
@@ -1,6 +1,6 @@
 pkgname = "attica"
 pkgver = "6.2.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 # requires network access and passes in cbuild chroot
 make_check_args = ["-E", "providertest"]
@@ -19,7 +19,9 @@ license = "LGPL-2.0-or-later"
 url = "https://api.kde.org/frameworks/attica/html"
 source = f"$(KDE_SITE)/frameworks/{pkgver[:pkgver.rfind('.')]}/attica-{pkgver}.tar.xz"
 sha256 = "275fb00b3f483704d800484883a3789797a3198b0ce680d85313d9977ffde2f0"
-hardening = ["vis", "cfi"]
+# FIXME: cfi causes crash when pressing "Get New Plugins..." button in the
+# "Wallpaper" section of Plasma's system settings app
+hardening = ["vis", "!cfi"]
 
 
 @subpackage("attica-devel")
