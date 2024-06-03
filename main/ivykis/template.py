78400087173a4401c6ebfab1554c443ddcd37be0
psykose <alice@ayaya.dev>
2024-06-03 08:21:04 +0000

    main/ivykis: update to 0.43.1

--- main/ivykis/template.py
+++ main/ivykis/template.py
@@ -1,19 +1,20 @@
 pkgname = "ivykis"
-pkgver = "0.43"
+pkgver = "0.43.1"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = [
+    "automake",
+    "libtool",
+    "pkgconf",
+]
 pkgdesc = "Library for asynchronous I/O readiness notification"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
 url = "http://libivykis.sourceforge.net"
 source = f"$(SOURCEFORGE_SITE)/libivykis/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "ff1c09569a806f2e926c8f9545fbf70d10f6fe14b66e2077e7dab1ca8e52a269"
+sha256 = "c7d2b18bd9342e7edfd13e0e38a68dbfeaa6d31e92e3e6772baa39429dfebb9f"
 
 
 @subpackage("ivykis-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
