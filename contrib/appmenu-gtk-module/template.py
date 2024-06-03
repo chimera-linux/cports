32a2221564cb953d6e2754a7f4e7169a0025fb91
psykose <alice@ayaya.dev>
2024-06-03 09:37:45 +0000

    contrib/appmenu-gtk-module: new package (24.05)

---
+++ contrib/appmenu-gtk-module/template.py
@@ -0,0 +1,24 @@
+pkgname = "appmenu-gtk-module"
+pkgver = "24.05"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dgtk=3"]
+meson_dir = "subprojects/appmenu-gtk-module"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+]
+makedepends = ["gtk+3-devel"]
+pkgdesc = "GTK module for appmenu"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-3.0-only"
+url = "https://gitlab.com/vala-panel-project/vala-panel-appmenu"
+source = f"{url}/-/archive/{pkgver}/vala-panel-appmenu-{pkgver}.tar.gz"
+sha256 = "989f1e44cd38aad137640c1d39803e81bb37c971e18b6282abe4f3e0ba622703"
+# CFI: check
+hardening = ["vis", "!cfi"]
+
+
+@subpackage("appmenu-gtk-module-devel")
+def _devel(self):
+    return self.default_devel()
---
+++ contrib/appmenu-gtk-module/update.py
@@ -0,0 +1,2 @@
+url = "https://gitlab.com/vala-panel-project/vala-panel-appmenu/-/tags"
+pattern = r"/vala-panel-appmenu-([\d\.]+).tar"
