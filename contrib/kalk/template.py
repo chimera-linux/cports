cffe3781885d2ac305c418c1ea8e481601af6e00
psykose <alice@ayaya.dev>
2024-06-03 10:48:31 +0000

    contrib/kalk: new package (24.05.0)

---
+++ contrib/kalk/template.py
@@ -0,0 +1,28 @@
+pkgname = "kalk"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kirigami-devel",
+    "kunitconversion-devel",
+    "libqalculate-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE Calculator"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-3.0-or-later AND CC0-1.0"
+url = "https://apps.kde.org/kalk"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kalk-{pkgver}.tar.xz"
+sha256 = "119958b50d6e103b219c11381a9f666a4c7f509000c35ff33aa8aa5ac6430091"
+# CFI: crashes tests
+hardening = ["vis", "!cfi"]
