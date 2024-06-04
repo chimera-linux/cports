d356ae8ed63b9c67bf4a3f9cec09c6777095adfe
Justin Berthault <justin.berthault@zaclys.net>
2024-06-04 23:43:30 +0200

    contrib/python-unidecode: new package (1.3.8)

---
+++ contrib/python-unidecode/template.py
@@ -0,0 +1,21 @@
+pkgname = "python-unidecode"
+pkgver = "1.3.8"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "ASCII transliterations of Unicode text for Python"
+maintainer = "Justin Berthault <justin.berthault@zaclys.net>"
+license = "GPL-2.0-or-later"
+url = "https://pypi.org/project/Unidecode"
+source = f"$(PYPI_SITE)/U/Unidecode/Unidecode-{pkgver}.tar.gz"
+sha256 = "cfdb349d46ed3873ece4586b96aa75258726e2fa8ec21d6f00a591d98806c2f4"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
