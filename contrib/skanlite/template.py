09bc568a2c4a81a19da73e2a176a1f65c52177e1
psykose <alice@ayaya.dev>
2024-06-04 11:36:41 +0000

    contrib/skanlite: new package (24.05.0)

---
+++ contrib/skanlite/template.py
@@ -0,0 +1,27 @@
+pkgname = "skanlite"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DQT_MAJOR_VERSION=6"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcoreaddons-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kxmlgui-devel",
+    "libksane-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE scanning application"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-only OR GPL-3.0-only"
+url = "https://apps.kde.org/skanlite"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/skanlite-{pkgver}.tar.xz"
+sha256 = "a6ef7b7a8eb4a60edfa5fcb392b827f490389ea0364ed69360f69c723d6b3fcc"
