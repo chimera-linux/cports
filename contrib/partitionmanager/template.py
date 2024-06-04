91efabb1be377c19798ad4ecd008c489690e2873
psykose <alice@ayaya.dev>
2024-06-04 09:46:03 +0000

    contrib/partitionmanager: new package (24.05.0)

---
+++ contrib/partitionmanager/template.py
@@ -0,0 +1,36 @@
+pkgname = "partitionmanager"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kjobwidgets-devel",
+    "kpmcore-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "kxmlgui-devel",
+    "polkit-qt-1-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE drive management utility"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-3.0-or-later"
+url = "https://apps.kde.org/partitionmanager"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/partitionmanager-{pkgver}.tar.xz"
+)
+sha256 = "682f0302213d95b628064226cfcced6aa299bc81b7ed46a6e705e811031fba66"
