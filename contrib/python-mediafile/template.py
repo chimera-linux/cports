fd50169778cc646a7c01636e5ac6761674f79296
Justin Berthault <justin.berthault@zaclys.net>
2024-06-04 23:37:19 +0200

    contrib/python-mediafile: new package (0.12.0)

---
+++ contrib/python-mediafile/template.py
@@ -0,0 +1,17 @@
+pkgname = "python-mediafile"
+pkgver = "0.12.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = ["python-build", "python-flit_core", "python-installer"]
+depends = ["mutagen", "python-six"]
+checkdepends = ["python-pytest"] + depends
+pkgdesc = "Handles low-level interfacing for file tags in Python"
+maintainer = "Justin Berthault <justin.berthault@zaclys.net>"
+license = "MIT"
+url = "https://github.com/beetbox/mediafile"
+source = f"$(PYPI_SITE)/m/mediafile/mediafile-{pkgver}.tar.gz"
+sha256 = "d75d805a06ed56150dbcea76505e700f9809abd9e98f98117ae46f5df2ccf1d7"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
