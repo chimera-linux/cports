f33d384bd43fe7ac2bf72971cbbba94e4346f009
psykose <alice@ayaya.dev>
2024-06-04 10:53:20 +0000

    contrib/qtkeychain: new package (0.14.3)

---
+++ contrib/qtkeychain/template.py
@@ -0,0 +1,31 @@
+pkgname = "qtkeychain"
+pkgver = "0.14.3"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_WITH_QT6=ON"]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "libsecret-devel",
+    "qt6-qttools-devel",
+]
+pkgdesc = "Qt library for storing data in the system keychain"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-3-Clause"
+url = "https://github.com/frankosterfeld/qtkeychain"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "a22c708f351431d8736a0ac5c562414f2b7bb919a6292cbca1ff7ac0849cb0a7"
+hardening = ["vis", "!cfi"]
+
+
+def post_install(self):
+    self.install_license("COPYING")
+
+
+@subpackage("qtkeychain-devel")
+def _devel(self):
+    self.depends += ["qt6-qtbase-devel"]
+    return self.default_devel()
