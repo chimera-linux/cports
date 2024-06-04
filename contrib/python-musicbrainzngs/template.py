5545d460f3baa7852d5d3ec37b62fd20a4b5a562
Justin Berthault <justin.berthault@zaclys.net>
2024-06-04 23:42:36 +0200

    contrib/python-musicbrainzngs: new package (0.7.1)

---
+++ contrib/python-musicbrainzngs/template.py
@@ -0,0 +1,21 @@
+pkgname = "python-musicbrainzngs"
+pkgver = "0.7.1"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+    "python-wheel",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "Musicbrainz NGS webservice interface python bindings"
+maintainer = "Justin Berthault <justin.berthault@zaclys.net>"
+license = "BSD-2-Clause"
+url = "https://github.com/alastair/python-musicbrainzngs"
+source = f"$(PYPI_SITE)/m/musicbrainzngs/musicbrainzngs-{pkgver}.tar.gz"
+sha256 = "ab1c0100fd0b305852e65f2ed4113c6de12e68afd55186987b8ed97e0f98e627"
+
+
+def post_install(self):
+    self.install_license("COPYING")
