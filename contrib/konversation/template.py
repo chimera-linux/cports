c1020fe77f95c45071b37857e270b98e979d27ce
psykose <alice@ayaya.dev>
2024-06-04 11:48:30 +0000

    contrib/konversation: new package (24.05.0)

---
+++ contrib/konversation/template.py
@@ -0,0 +1,48 @@
+pkgname = "konversation"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "karchive-devel",
+    "kbookmarks-devel",
+    "kcodecs-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kglobalaccel-devel",
+    "ki18n-devel",
+    "kidletime-devel",
+    "kio-devel",
+    "kitemviews-devel",
+    "knewstuff-devel",
+    "knotifications-devel",
+    "knotifyconfig-devel",
+    "kparts-devel",
+    "kstatusnotifieritem-devel",
+    "ktextwidgets-devel",
+    "kwallet-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "qca-devel",
+    "qt6-qt5compat-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtmultimedia-devel",
+]
+pkgdesc = "KDE IRC client"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-3.0-only"
+url = "https://konversation.kde.org"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/konversation-{pkgver}.tar.xz"
+)
+sha256 = "f38e56bb5b99d3d15870ecf39419031862e6781cd16266dc33625434124b1f9b"
