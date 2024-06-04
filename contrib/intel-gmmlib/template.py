be62581bfe7253f2dcda6660e0639bd9b8facfd0
psykose <alice@ayaya.dev>
2024-06-04 20:20:41 +0000

    contrib/intel-gmmlib: update to 22.3.20

--- contrib/intel-gmmlib/template.py
+++ contrib/intel-gmmlib/template.py
@@ -1,5 +1,5 @@
 pkgname = "intel-gmmlib"
-pkgver = "22.3.19"
+pkgver = "22.3.20"
 pkgrel = 0
 # aarch64 segfaults in tests
 # only needed for intel-media-driver anyway
@@ -15,7 +15,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://github.com/intel/gmmlib"
 source = f"https://github.com/intel/gmmlib/archive/refs/tags/intel-gmmlib-{pkgver}.tar.gz"
-sha256 = "ea9c418b0fd84a982850f230cb2d783dfe2e1f9923065f54b2fcaad1e9b33417"
+sha256 = "227c1614d4817154b4e6fbfb667ba2b231ea7aa53796d91f1800af9aeb703f59"
 # FIXME: cfi testsuite sigill
 hardening = ["vis"]
 # check cross: testsuite runs as part of install(), disabling that also doesn't build it..
