733096cacb4eafe56b49d1b83af639d4824246a2
psykose <alice@ayaya.dev>
2024-06-04 10:41:02 +0000

    contrib/khelpcenter: new package (24.05.0)

---
+++ contrib/khelpcenter/template.py
@@ -0,0 +1,38 @@
+pkgname = "khelpcenter"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "karchive-devel",
+    "kbookmarks-devel",
+    "kcompletion-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kservice-devel",
+    "ktexttemplate-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "kxmlgui-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtwebengine-devel",
+    "xapian-core-devel",
+]
+checkdepends = ["perl"]
+pkgdesc = "KDE application documentation viewer"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/khelpcenter"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/khelpcenter-{pkgver}.tar.xz"
+sha256 = "c4c500bd461c5a326415b137a8da32c097426d2ab707ca9cd50c277a11a77b12"
