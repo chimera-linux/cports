f3f1944699e561a0a976720cfda51b77c88022a6
psykose <alice@ayaya.dev>
2024-06-04 10:09:03 +0000

    contrib/yakuake: new package (24.05.0)

---
+++ contrib/yakuake/template.py
@@ -0,0 +1,39 @@
+pkgname = "yakuake"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "karchive-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kglobalaccel-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "knewstuff-devel",
+    "knotifications-devel",
+    "knotifyconfig-devel",
+    "kparts-devel",
+    "kstatusnotifieritem-devel",
+    "kwayland-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtsvg-devel",
+]
+depends = ["konsole"]
+pkgdesc = "KDE drop-down terminal"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-only OR GPL-3.0-only"
+url = "https://apps.kde.org/yakuake"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/yakuake-{pkgver}.tar.xz"
+sha256 = "113c2d0ee354a12ab6ca3469fe68c086217688dc2ee680335fffe4c79d9afba2"
