704e8fca20d04df4f38c1a79d89a597e82ae9ca7
Justin Berthault <justin.berthault@zaclys.net>
2024-06-04 23:33:33 +0200

    contrib/python-jellyfish: new package (1.0.4)

---
+++ contrib/python-jellyfish/template.py
@@ -0,0 +1,37 @@
+pkgname = "python-jellyfish"
+pkgver = "1.0.4"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "cargo",
+    "python-build",
+    "python-installer",
+    "python-maturin",
+    "python-wheel",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "Python library for approximate and phonetic string matching"
+maintainer = "Justin Berthault <justin.berthault@zaclys.net>"
+license = "BSD-2-Clause"
+url = "https://jamesturk.github.io/jellyfish"
+source = f"$(PYPI_SITE)/j/jellyfish/jellyfish-{pkgver}.tar.gz"
+sha256 = "72aabb3bedd513cdd20712242fd51173b59972c0b146b7a0b9c6f32f1656293f"
+
+
+def init_build(self):
+    from cbuild.util import cargo
+
+    renv = cargo.get_environment(self)
+    self.make_env.update(renv)
+
+
+def do_prepare(self):
+    from cbuild.util import cargo
+
+    self.cargo = cargo.Cargo(self)
+    self.cargo.vendor()
+    cargo.setup_vendor(self)
+
+
+def post_install(self):
+    self.install_license("LICENSE")
