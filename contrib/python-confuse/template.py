880d5b98bef612a726a1a8156ef2cc2d67aa7c76
Justin Berthault <justin.berthault@zaclys.net>
2024-06-04 23:32:24 +0200

    contrib/python-confuse: new package (2.0 1)

---
+++ contrib/python-confuse/template.py
@@ -0,0 +1,17 @@
+pkgname = "python-confuse"
+pkgver = "2.0.1"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = ["python-build", "python-flit_core", "python-installer"]
+depends = ["python-pyyaml"]
+checkdepends = ["python-pytest"] + depends
+pkgdesc = "YAML config files for Python"
+maintainer = "Justin Berthault <justin.berthault@zaclys.net>"
+license = "MIT"
+url = "https://github.com/beetbox/confuse"
+source = f"$(PYPI_SITE)/c/confuse/confuse-{pkgver}.tar.gz"
+sha256 = "7379a2ad49aaa862b79600cc070260c1b7974d349f4fa5e01f9afa6c4dd0611f"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
