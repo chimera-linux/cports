1b053ef212771508a8dfa933eced0df0f41f04b8
psykose <alice@ayaya.dev>
2024-06-05 15:46:39 +0000

    contrib/discover: new package (6.0.5)

---
+++ contrib/discover/template.py
@@ -0,0 +1,63 @@
+pkgname = "discover"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+# fails in chroot env
+make_check_args = ["-E", "flatpaktest"]
+make_check_wrapper = [
+    "dbus-run-session",
+    "--",
+    "wlheadless-run",
+    "--",
+]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "appstream-qt-devel",
+    "attica-devel",
+    "flatpak-devel",
+    "fwupd-devel",
+    "karchive-devel",
+    "kauth-devel",
+    "kcmutils-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kidletime-devel",
+    "kio-devel",
+    "kirigami-addons-devel",
+    "kirigami-devel",
+    "knewstuff-devel",
+    "knotifications-devel",
+    "kstatusnotifieritem-devel",
+    "kuserfeedback-devel",
+    "kxmlgui-devel",
+    "purpose-devel",
+    "qt6-qtdeclarative-devel",
+]
+depends = [
+    "kirigami-addons",
+    "purpose",
+]
+checkdepends = [
+    "dbus",
+    "xwayland-run",
+] + depends
+pkgdesc = "KDE application manager"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "(GPL-2.0-only OR GPL-3.0-only) AND (LGPL-2.1-only OR LGPL-3.0-only)"
+url = "https://userbase.kde.org/Discover"
+source = f"$(KDE_SITE)/plasma/{pkgver}/discover-{pkgver}.tar.xz"
+sha256 = "944ca03fe131e0e92f795b32e4e7fc714c9a59fc18a4bbda9f50408d0c6af395"
+
+
+if self.profile().arch in ["aarch64", "ppc64le", "x86_64"]:
+    makedepends += ["qt6-qtwebview-devel"]
