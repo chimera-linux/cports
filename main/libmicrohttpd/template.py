17a226a81949c283cbc7c9b5358b55330a3c9732
q66 <q66@chimera-linux.org>
2024-06-05 15:50:32 +0200

    main/libmicrohttpd: new package (1.0.1)

---
+++ main/libmicrohttpd/template.py
@@ -0,0 +1,18 @@
+pkgname = "libmicrohttpd"
+pkgver = "1.0.1"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["automake", "libtool", "pkgconf"]
+makedepends = ["gnutls-devel", "linux-headers"]
+checkdepends = ["libcurl-devel"]
+pkgdesc = "HTTP server library"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "LGPL-2.1-or-later"
+url = "https://www.gnu.org/software/libmicrohttpd"
+source = f"$(GNU_SITE)/libmicrohttpd/libmicrohttpd-{pkgver}.tar.gz"
+sha256 = "a89e09fc9b4de34dde19f4fcb4faaa1ce10299b9908db1132bbfa1de47882b94"
+
+
+@subpackage("libmicrohttpd-devel")
+def _devel(self):
+    return self.default_devel()
