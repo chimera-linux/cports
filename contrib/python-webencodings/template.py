dae22e6f698d6c64d6be06bfa11c313534599cf5
psykose <alice@ayaya.dev>
2024-06-05 00:20:51 +0000

    contrib/python-webencodings: depend on python
    is leaf

--- contrib/python-webencodings/template.py
+++ contrib/python-webencodings/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-webencodings"
 pkgver = "0.5.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -9,6 +9,7 @@ hostmakedepends = [
     "python-wheel",
 ]
 checkdepends = ["python-pytest"]
+depends = ["python"]
 pkgdesc = "Python WHATWG encoding implementation"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
