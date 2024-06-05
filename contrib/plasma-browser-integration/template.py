56244ae75f934da2f0c69b50c8a8d40b55e58c86
psykose <alice@ayaya.dev>
2024-06-05 12:10:37 +0000

    contrib/plasma-browser-integration: new package (6.0.5)

---
+++ contrib/plasma-browser-integration/template.py
@@ -0,0 +1,39 @@
+pkgname = "plasma-browser-integration"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kio-devel",
+    "ki18n-devel",
+    "kcoreaddons-devel",
+    "kconfig-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "knotifications-devel",
+    "plasma-activities-devel",
+    "krunner-devel",
+    "purpose-devel",
+    "kfilemetadata-devel",
+    "plasma-workspace-devel",
+    "kjobwidgets-devel",
+    "kservice-devel",
+    "kstatusnotifieritem-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE integration with the system browser"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-3.0-or-later AND MIT"
+url = "https://community.kde.org/Plasma/Browser_Integration"
+source = (
+    f"$(KDE_SITE)/plasma/{pkgver}/plasma-browser-integration-{pkgver}.tar.xz"
+)
+sha256 = "30dce955e27aa7656c82758240adee98f27736096bc0f832a1d1034ccf3ee418"
+# the MIT one has no attribution in it..
+options = ["!distlicense"]
