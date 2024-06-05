b13397ba4f8dd5fa5490988f09ec1ace5176043d
ttyyls <contact@behri.org>
2024-06-05 02:39:11 +0100

    user/gmid: new package

---
+++ user/gmid/template.py
@@ -0,0 +1,38 @@
+pkgname = "gmid"
+pkgver = "2.1.1"
+pkgrel = 0
+build_style = "configure"
+configure_args = ["PREFIX=/usr", "MANDIR=/usr/share/man"]
+make_check_target = "regress"
+hostmakedepends = ["flex", "pkgconf"]
+makedepends = [
+    "libevent-devel",
+    "libretls-devel",
+    "openssl-devel",
+]
+checkdepends = [
+    "ca-certificates",
+    "openssh",
+    "procps",
+]
+pkgdesc = "Gemini server"
+maintainer = "ttyyls <contact@behri.org>"
+license = "ISC"
+url = "https://gmid.omarpolo.com"
+source = f"https://ftp.omarpolo.com/gmid-{pkgver}.tar.gz"
+sha256 = "9eb0fe4272616e71ef28adb1a10808adb58db01626acc39fddebf58e0a0ac4bf"
+tool_flags = {"CFLAGS": ["-Wno-deprecated-declarations"]}
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+    self.install_sysusers(self.files_path / "sysusers.conf")
+    self.install_tmpfiles(self.files_path / "tmpfiles.conf")
+    self.install_service(self.files_path / "gmid")
+
+
+@subpackage("gmid-utils")
+def _(self):
+    self.subdesc = "utils"
+
+    return ["cmd:gemexp", "cmd:gg", "cmd:titan"]
