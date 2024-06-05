eed7316dcc831c90cdf1b2c6dd236a86308e7017
psykose <alice@ayaya.dev>
2024-06-05 13:09:37 +0000

    contrib/libfakekey: new package (0.3)

---
+++ contrib/libfakekey/template.py
@@ -0,0 +1,21 @@
+pkgname = "libfakekey"
+pkgver = "0.3"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = [
+    "automake",
+    "libtool",
+    "pkgconf",
+]
+makedepends = ["libxtst-devel"]
+pkgdesc = "Library for simulating X11 keypresses"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later"
+url = "https://git.yoctoproject.org/libfakekey"
+source = f"http://git.yoctoproject.org/cgit/cgit.cgi/libfakekey/snapshot/libfakekey-{pkgver}.tar.gz"
+sha256 = "d282fa6481a5b85f71e36e8bad4cfa938cc8eaac4c42ffa27f9203ac634813f4"
+
+
+@subpackage("libfakekey-devel")
+def _devel(self):
+    return self.default_devel()
