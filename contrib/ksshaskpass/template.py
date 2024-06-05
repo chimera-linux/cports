a351b04a31b06d348ea5b1d525b4de1c13f60cfc
psykose <alice@ayaya.dev>
2024-06-05 13:38:46 +0000

    contrib/ksshaskpass: new package (6.0.5)

---
+++ contrib/ksshaskpass/template.py
@@ -0,0 +1,25 @@
+pkgname = "ksshaskpass"
+pkgver = "6.0.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcoreaddons-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "kwallet-devel",
+    "kwidgetsaddons-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE askpass helper"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://invent.kde.org/plasma/ksshaskpass"
+source = f"$(KDE_SITE)/plasma/{pkgver}/ksshaskpass-{pkgver}.tar.xz"
+sha256 = "6df0eeef43a041eb1a6d71ba524feac57fc1f48aeb44540366f12404cc4986bc"
