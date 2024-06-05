27ef3e13bdb62ae3eebc43da61b78cd6234397dc
psykose <alice@ayaya.dev>
2024-06-05 13:50:27 +0000

    contrib/qt6-qtquicktimeline: new package (6.7.1)

---
+++ contrib/qt6-qtquicktimeline/template.py
@@ -0,0 +1,53 @@
+pkgname = "qt6-qtquicktimeline"
+pkgver = "6.7.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "qt6-qtdeclarative-devel",
+    "vulkan-headers",
+]
+pkgdesc = "Qt6 keyframe-timeline component"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = (
+    "LGPL-2.1-only AND LGPL-3.0-only AND GPL-3.0-only WITH Qt-GPL-exception-1.0"
+)
+url = "https://www.qt.io"
+source = f"https://download.qt.io/official_releases/qt/{pkgver[:-2]}/{pkgver}/submodules/qtquicktimeline-everywhere-src-{pkgver}.tar.xz"
+sha256 = "2d79204d87b7a13cb0154f1ab705afcd1f8c668c0a03e0694205141f54725744"
+# cross: TODO
+# check: fails to find simpletest.qml
+options = ["!cross", "!check"]
+
+
+def init_check(self):
+    self.make_check_env = {
+        "QT_QPA_PLATFORM": "offscreen",
+        "QML2_IMPORT_PATH": str(
+            self.chroot_cwd / f"{self.make_dir}/lib/qt6/qml"
+        ),
+    }
+
+
+def post_install(self):
+    self.rm(self.destdir / "usr/tests", recursive=True)
+
+
+@subpackage("qt6-qtquicktimeline-devel")
+def _devel(self):
+    self.depends += [
+        f"qt6-qtbase-devel~{pkgver[:-2]}",
+        f"qt6-qtdeclarative-devel~{pkgver[:-2]}",
+    ]
+    return self.default_devel(
+        extra=[
+            "usr/lib/qt6/metatypes",
+            "usr/lib/qt6/mkspecs",
+            "usr/lib/qt6/modules",
+            "usr/lib/*.prl",
+        ]
+    )
