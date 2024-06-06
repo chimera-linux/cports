5f1b4f607ee9dd02bb3f55a9cd37ee1be20ba4cc
psykose <alice@ayaya.dev>
2024-06-06 12:38:45 +0000

    contrib/kpimtextedit: new package (24.05.0)

---
+++ contrib/kpimtextedit/template.py
@@ -0,0 +1,41 @@
+pkgname = "kpimtextedit"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+# hangs
+make_check_args = ["-E", "richtextcomposertest"]
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcodecs-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "ktextaddons-devel",
+    "kwidgetsaddons-devel",
+    "kxmlgui-devel",
+    "qt6-qtdeclarative-devel",
+    "sonnet-devel",
+    "syntax-highlighting-devel",
+]
+pkgdesc = "KDE PIM textedit class"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.1-or-later"
+url = "https://api.kde.org/kdepim/kpimtextedit/html"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/kpimtextedit-{pkgver}.tar.xz"
+)
+sha256 = "9091ad74332ddbb98837253c7d5be6319ae25eb0a99a282682fdbe991882449b"
+
+
+@subpackage("kpimtextedit-devel")
+def _devel(self):
+    return self.default_devel()
