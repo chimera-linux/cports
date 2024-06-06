bdc5bc61ad48f1e91a3e351ec1b27c4441fe2ef7
psykose <alice@ayaya.dev>
2024-06-06 17:59:39 +0000

    contrib/qt6-qtwebengine: split designer plugin

--- contrib/qt6-qtwebengine/template.py
+++ contrib/qt6-qtwebengine/template.py
@@ -111,6 +111,7 @@ def _devel(self):
             "usr/lib/qt6/metatypes",
             "usr/lib/qt6/mkspecs",
             "usr/lib/qt6/modules",
+            "usr/lib/qt6/plugins/designer",
             "usr/lib/*.prl",
         ]
     )
