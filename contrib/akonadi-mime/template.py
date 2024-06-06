f0eec9c8868ad5123b3245f98b4ec1d27248e622
psykose <alice@ayaya.dev>
2024-06-06 18:27:12 +0000

    contrib/akonadi-mime: new package (24.05.0)

---
+++ contrib/akonadi-mime/template.py
@@ -0,0 +1,41 @@
+pkgname = "akonadi-mime"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+# broken for some reason
+make_check_args = ["-E", "mailserializerplugintest"]
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+    "xsltproc",
+]
+makedepends = [
+    "akonadi-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kitemmodels-devel",
+    "kmime-devel",
+    "kxmlgui-devel",
+    "libxslt-devel",
+    "qt6-qtdeclarative-devel",
+    "shared-mime-info",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE Akonadi mime libraries"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-3.0-only"
+url = "https://api.kde.org/kdepim/akonadi-mime/html"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/akonadi-mime-{pkgver}.tar.xz"
+)
+sha256 = "a65820d7d275079b479cb20f0fc516f59f846c29b5dcd0a89d3350c6972800fc"
+
+
+@subpackage("akonadi-mime-devel")
+def _devel(self):
+    self.depends += ["akonadi-devel"]
+    return self.default_devel()
