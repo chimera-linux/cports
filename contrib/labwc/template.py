99f21818c8de1664b5e0c6910ddb3c974c9d2f45
psykose <alice@ayaya.dev>
2024-06-06 17:34:55 +0000

    contrib/labwc: add portal conf

--- contrib/labwc/template.py
+++ contrib/labwc/template.py
@@ -1,6 +1,6 @@
 pkgname = "labwc"
 pkgver = "0.7.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
     "-Dman-pages=enabled",
@@ -32,3 +32,9 @@ license = "GPL-2.0-only"
 url = "https://github.com/labwc/labwc"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "b00119451a91a75cc063cfa753f956623acdde4b93bbf78b2b6d5fe7f94c597e"
+
+
+def post_install(self):
+    self.install_file(
+        self.files_path / "labwc-portals.conf", "usr/share/xdg-desktop-portal"
+    )
