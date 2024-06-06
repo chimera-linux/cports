6fe7fd2d3c7a1b7b345cb076d32d8d02016ebb82
ttyyls <contact@behri.org>
2024-06-06 09:32:15 +0100

    contrib/lua5.4-say: new package (1.4.1)

---
+++ contrib/lua5.4-say/template.py
@@ -0,0 +1,14 @@
+pkgname = "lua5.4-say"
+pkgver = "1.4.1"
+pkgrel = 0
+pkgdesc = "Lua string hashing library, useful for internationalization"
+maintainer = "ttyyls <contact@behri.org>"
+license = "MIT"
+url = "https://github.com/lunarmodules/say"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "ce07547ca49ef42cc799e2a30b3c65ce77039978e32e7961799a252d61a56486"
+
+
+def do_install(self):
+    self.install_files("src/say", "usr/share/lua/5.4")
+    self.install_license("LICENSE")
