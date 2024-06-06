3ad9d1e2061eefda8637d91a74569d3cb650da40
psykose <alice@ayaya.dev>
2024-06-06 17:48:08 +0000

    contrib/akonadi-contacts: new package (24.05.0)

---
+++ contrib/akonadi-contacts/template.py
@@ -0,0 +1,52 @@
+pkgname = "akonadi-contacts"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "akonadi-devel",
+    "grantleetheme-devel",
+    "kcodecs-devel",
+    "kcompletion-devel",
+    "kconfig-devel",
+    "kcontacts-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "kmime-devel",
+    "kservice-devel",
+    "ktextaddons-devel",
+    "ktexttemplate-devel",
+    "ktextwidgets-devel",
+    "kxmlgui-devel",
+    "prison-devel",
+    "qt6-qtdeclarative-devel",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE Akonadi contacts libraries"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-2.0-or-later"
+url = "https://api.kde.org/kdepim/akonadi-contacts/html/index.html"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/akonadi-contacts-{pkgver}.tar.xz"
+)
+sha256 = "f003217344952477b7dfdf57a0bda23ca173a52ecef748d31723c59309fed942"
+
+
+@subpackage("akonadi-contacts-devel")
+def _devel(self):
+    self.depends += [
+        "akonadi-devel",
+        "grantleetheme-devel",
+        "kcontacts-devel",
+        "qt6-qtbase-devel",
+    ]
+    return self.default_devel()
