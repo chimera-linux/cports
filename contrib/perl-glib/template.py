c3711cdcfad2b25497d521fbf033ffe47d757a85
psykose <alice@ayaya.dev>
2024-06-06 08:28:25 +0000

    contrib/perl-glib: new package (1.3294)

---
+++ contrib/perl-glib/template.py
@@ -0,0 +1,18 @@
+pkgname = "perl-glib"
+pkgver = "1.3294"
+pkgrel = 0
+build_style = "perl_module"
+hostmakedepends = [
+    "gmake",
+    "perl",
+    "perl-extutils-depends",
+    "perl-extutils-pkgconfig",
+]
+makedepends = ["glib-devel", "perl"]
+depends = ["perl"]
+pkgdesc = "Perl module for GLib"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-or-later"
+url = "https://metacpan.org/dist/Glib"
+source = f"$(CPAN_SITE)/Glib/Glib-{pkgver}.tar.gz"
+sha256 = "d715f5a86bcc187075de85e7ae5bc07b0714d6edc196a92da43986efa44e5cbb"
