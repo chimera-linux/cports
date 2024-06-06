952fecadfdae8561c1fba1bd2544b065b0a8587b
psykose <alice@ayaya.dev>
2024-06-06 12:34:50 +0000

    contrib/ktextaddons: new package (1.5.4)

---
+++ contrib/ktextaddons/template.py
@@ -0,0 +1,51 @@
+pkgname = "ktextaddons"
+pkgver = "1.5.4"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_WITH_QT6=ON"]
+make_check_args = [
+    "-E",
+    "(texttospeechwidgettest"  # hangs
+    # fail in headless
+    + "|texttospeechactionstest"
+    + "|grammalecteresultwidgettest"
+    + "|grammalecteconfigwidgettest"
+    + "|languagetoolconfigwidgettest"
+    + "|texttospeechactionstest"
+    # need translator plugins
+    + "|translatorwidgettest"
+    + "|translatorengineloadertest"
+    + ")",
+]
+make_check_env = {"QT_QPA_PLATFORM": "offscreen"}
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "karchive-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtspeech-devel",
+    "qt6-qttools-devel",
+    "qtkeychain-devel",
+    "sonnet-devel",
+    "syntax-highlighting-devel",
+]
+pkgdesc = "KDE text handling addons library"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-or-later"
+url = "https://invent.kde.org/libraries/ktextaddons"
+source = f"$(KDE_SITE)/ktextaddons/ktextaddons-{pkgver}.tar.xz"
+sha256 = "64b80602e84b25e9164620af3f6341fa865b85e826ab8f5e02061ae24a277b20"
+
+
+@subpackage("ktextaddons-devel")
+def _devel(self):
+    self.depends += ["kconfigwidgets-devel"]
+    return self.default_devel(extra=["usr/lib/qt6/plugins/designer"])
