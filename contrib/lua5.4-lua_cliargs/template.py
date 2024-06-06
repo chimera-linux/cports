74c79f35c4889ab8b319f16f4e223dae88d1a779
ttyyls <contact@behri.org>
2024-06-06 09:31:45 +0100

    contrib/lua5.4-lua_cliargs: new package (3.0.2)

---
+++ contrib/lua5.4-lua_cliargs/template.py
@@ -0,0 +1,16 @@
+pkgname = "lua5.4-lua_cliargs"
+pkgver = "3.0.2"
+pkgrel = 0
+pkgdesc = "CLI argument parsing module for Lua"
+maintainer = "ttyyls <contact@behri.org>"
+license = "MIT"
+url = "https://github.com/lunarmodules/lua_cliargs"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "a7a57ab9c73f6c44040a78305b6dc7780ca1565cc4c9057d74a6608cb0443af4"
+
+
+def do_install(self):
+    self.install_license("LICENSE")
+    self.install_dir("usr/share/lua/5.4")
+    self.install_file("src/cliargs.lua", "usr/share/lua/5.4")
+    self.install_files("src/cliargs", "usr/share/lua/5.4")
