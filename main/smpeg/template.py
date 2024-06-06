076b09168140561abe3278a65e029644d9300bf9
psykose <alice@ayaya.dev>
2024-06-06 08:23:41 +0000

    main/smpeg: fix smpeg-config

--- main/smpeg/template.py
+++ main/smpeg/template.py
@@ -1,10 +1,15 @@
 pkgname = "smpeg"
 pkgver = "2.0.0"
-pkgrel = 1
+pkgrel = 2
 build_style = "gnu_configure"
-configure_gen = []
+configure_gen = ["./autogen.sh"]
 make_cmd = "gmake"
-hostmakedepends = ["gmake", "pkgconf"]
+hostmakedepends = [
+    "automake",
+    "gmake",
+    "libtool",
+    "pkgconf",
+]
 makedepends = ["sdl-devel"]
 pkgdesc = "MPEG decoding library"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -12,10 +17,14 @@ license = "LGPL-2.1-or-later"
 url = "https://icculus.org/smpeg"
 source = f"https://www.libsdl.org/projects/smpeg/release/smpeg2-{pkgver}.tar.gz"
 sha256 = "979a65b211744a44fa641a9b6e4d64e64a12ff703ae776bafe3c4c4cd85494b3"
+tool_flags = {"CFLAGS": ["-Wno-register"]}
 # no check target
 options = ["!check"]
 
-tool_flags = {"CFLAGS": ["-Wno-register"]}
+
+def post_extract(self):
+    # breaks smpeg-config because it bad
+    self.rm("acinclude/sdl2.m4")
 
 
 @subpackage("smpeg-devel")
