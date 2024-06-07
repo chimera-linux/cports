a43dcd802f29f976c0a6f08536d150dffb6a0d42
psykose <alice@ayaya.dev>
2024-06-07 18:02:27 +0000

    contrib/libksieve: new package (24.05.0)

---
+++ contrib/libksieve/template.py
@@ -0,0 +1,51 @@
+pkgname = "libksieve"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+# sieveeditorhelphtmlwidgettest: qtwebengine doesnt work in chroot
+# findbar-findbarbasetest: failed focus tracking
+make_check_args = [
+    "-E",
+    "(sieveeditorhelphtmlwidgettest|findbar-findbarbasetest)",
+]
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "karchive-devel",
+    "kcoreaddons-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kidentitymanagement-devel",
+    "kio-devel",
+    "kmailtransport-devel",
+    "kmime-devel",
+    "knewstuff-devel",
+    "ktextaddons-devel",
+    "libkdepim-devel",
+    "libsasl-devel",
+    "pimcommon-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtwebengine-devel",
+    "sonnet-devel",
+    "syntax-highlighting-devel",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE Sieve scripting library"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-2.0-or-later"
+url = "https://api.kde.org/kdepim/libksieve/html"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/libksieve-{pkgver}.tar.xz"
+sha256 = "552e05a6a132b71a78bcb5a81ccebd829b9d59cea4e09e43f4805d97df92cb43"
+
+
+@subpackage("libksieve-devel")
+def _devel(self):
+    self.depends += ["syntax-highlighting-devel"]
+    return self.default_devel()
