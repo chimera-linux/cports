2445ee26bc749455a8757b3bcf590b41295d00e0
psykose <alice@ayaya.dev>
2024-06-07 16:47:30 +0000

    contrib/akonadi-import-wizard: new package (24.05.0)

---
+++ contrib/akonadi-import-wizard/template.py
@@ -0,0 +1,42 @@
+pkgname = "akonadi-import-wizard"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "akonadi-devel",
+    "karchive-devel",
+    "kauth-devel",
+    "kconfig-devel",
+    "kcontacts-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kidentitymanagement-devel",
+    "kio-devel",
+    "kmailtransport-devel",
+    "mailcommon-devel",
+    "mailimporter-devel",
+    "messagelib-devel",
+    "pimcommon-devel",
+    "qt6-qtdeclarative-devel",
+    "qtkeychain-devel",
+]
+pkgdesc = "KDE Akonadi assistant for importing data into KDEPIM"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-2.0-or-later"
+url = "https://invent.kde.org/pim/akonadi-import-wizard"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/akonadi-import-wizard-{pkgver}.tar.xz"
+sha256 = "2f570795e1a457f751422ee43f5dad24479b09ae3699fa15af660fdbbd21658d"
+
+
+@subpackage("akonadi-import-wizard-devel")
+def _devel(self):
+    self.depends += ["kmailtransport-devel"]
+    return self.default_devel()
