dde063816e6dd80ee03fb0cda4d4942f29a1a5a3
psykose <alice@ayaya.dev>
2024-06-07 20:23:38 +0000

    contrib/croc: update to 10.0.8

--- contrib/croc/template.py
+++ contrib/croc/template.py
@@ -1,6 +1,6 @@
 pkgname = "croc"
-pkgver = "10.0.7"
-pkgrel = 1
+pkgver = "10.0.8"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "File transfer tool"
@@ -8,7 +8,7 @@ maintainer = "Paul A. Patience <paul@apatience.com>"
 license = "MIT"
 url = "https://github.com/schollz/croc"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "595c96bfef8949bedbcd7b91d91dae4a3024a542c2c28d4a7d91bb4a2a28faf8"
+sha256 = "9dde7d5114b4466a7351f9117e5ffc0b2866e5dae5d094bd1bc65c83787528c1"
 # debug: fails to split on powerpc
 # check: needs network access
 options = ["!debug", "!check"]
