c47c2022973ab015ea7241c03bc9caad00519663
psykose <alice@ayaya.dev>
2024-06-07 20:25:04 +0000

    contrib/perl-image-exiftool: update to 12.86

--- contrib/perl-image-exiftool/template.py
+++ contrib/perl-image-exiftool/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-image-exiftool"
-pkgver = "12.76"
+pkgver = "12.86"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = [
@@ -13,7 +13,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://exiftool.org"
 source = f"https://exiftool.org/Image-ExifTool-{pkgver}.tar.gz"
-sha256 = "5d3430ec57aa031f7ca43170f7ed6338a66bda99ab95b9e071f1ee27555f515f"
+sha256 = "7f6a8090f1831783d4b3045847d9c13a229393dd6366e0eab46c44a400b86914"
 
 
 @subpackage("exiftool")
