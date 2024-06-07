50b51c2a1c5905796da6002a98ae4b27dbc33efa
ttyyls <contact@behri.org>
2024-06-07 09:03:03 +0100

    contrib/python-botocore: update to 1.34.121

--- contrib/python-botocore/template.py
+++ contrib/python-botocore/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-botocore"
-pkgver = "1.34.119"
+pkgver = "1.34.121"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -19,6 +19,6 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/boto/botocore"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "016871815bc7d3c41e16d993eda1c884402b8d171032803644a11208b1d217b7"
+sha256 = "20e0c5ebef9f909daf5687b589acee74de816a346582a3872912ab9884fda807"
 # takes forever
 options = ["!check"]
