38bc19d40dc5d3aa2f804cba965f6ba2196884e5
psykose <alice@ayaya.dev>
2024-06-07 13:43:15 +0000

    contrib/ksmtp: new package (24.05.0)

---
+++ contrib/ksmtp/template.py
@@ -0,0 +1,32 @@
+pkgname = "ksmtp"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+# needs networking
+make_check_args = ["-E", "smtptest"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "libsasl-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "KDE SMTP library"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.1-or-later"
+url = "https://api.kde.org/kdepim/ksmtp/html"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/ksmtp-{pkgver}.tar.xz"
+sha256 = "06acfb397b2ad5b3b7b3175abf65b32c49d56d5c14c90ec290ec9b01a559d52f"
+
+
+@subpackage("ksmtp-devel")
+def _devel(self):
+    self.depends += ["kcoreaddons-devel"]
+    return self.default_devel()
