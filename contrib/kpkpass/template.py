a9b85ff15cffb5f280f535314603f58f1c4b3810
psykose <alice@ayaya.dev>
2024-06-07 17:43:41 +0000

    contrib/kpkpass: new package (24.05.0)

---
+++ contrib/kpkpass/template.py
@@ -0,0 +1,28 @@
+pkgname = "kpkpass"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "karchive-devel",
+    "qt6-qtbase-devel",
+    "shared-mime-info",
+]
+pkgdesc = "KDE PIM library for Apple Wallet pass files"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later"
+url = "https://api.kde.org/kdepim/kpkpass/html"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kpkpass-{pkgver}.tar.xz"
+sha256 = "b370dc112507dbf87033d5a04914beeae9e653820374fd1b48b476f467d8ad28"
+
+
+@subpackage("kpkpass-devel")
+def _devel(self):
+    self.depends += ["karchive-devel"]
+    return self.default_devel()
