398837675e945ffdad9cfd8ec0d5cff75f0a5bea
psykose <alice@ayaya.dev>
2024-06-07 20:38:48 +0000

    main/volk: update to 1.3.283.0

--- main/volk/template.py
+++ main/volk/template.py
@@ -1,6 +1,6 @@
 pkgname = "volk"
-pkgver = "1.3.270"
-pkgrel = 1
+pkgver = "1.3.283.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DVOLK_HEADERS_ONLY=ON",
@@ -15,8 +15,10 @@ pkgdesc = "Vulkan meta loader"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://github.com/zeux/volk"
-source = f"https://github.com/zeux/volk/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "95530bc7850b0358e4bad899eb653f882ee8a08088257d90c5042cec02208f52"
+source = (
+    f"https://github.com/zeux/volk/archive/refs/tags/vulkan-sdk-{pkgver}.tar.gz"
+)
+sha256 = "872035f1f26c53b218632a3a8dbccbd276710aaabafb9bb1bc1a6c0633ee6aab"
 
 
 def post_install(self):
