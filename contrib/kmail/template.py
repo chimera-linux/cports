0610b7c4a6230319542a5a7927f4c555e0c19235
psykose <alice@ayaya.dev>
2024-06-07 19:15:00 +0000

    contrib/kmail: new package (24.05.0)

---
+++ contrib/kmail/template.py
@@ -0,0 +1,76 @@
+pkgname = "kmail"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_args = ["-E", "akonadi-sqlite-.*"]
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "akonadi-contacts-devel",
+    "akonadi-devel",
+    "akonadi-mime-devel",
+    "akonadi-search-devel",
+    "gpgme-devel",
+    "kbookmarks-devel",
+    "kcalendarcore-devel",
+    "kcalutils-devel",
+    "kcmutils-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcontacts-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kguiaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kidentitymanagement-devel",
+    "kio-devel",
+    "kitemviews-devel",
+    "kjobwidgets-devel",
+    "kldap-devel",
+    "kmailtransport-devel",
+    "kmime-devel",
+    "knotifications-devel",
+    "knotifyconfig-devel",
+    "kontactinterface-devel",
+    "kparts-devel",
+    "kpimtextedit-devel",
+    "kservice-devel",
+    "kstatusnotifieritem-devel",
+    "ktextaddons-devel",
+    "ktextwidgets-devel",
+    "ktnef-devel",
+    "kuserfeedback-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "kxmlgui-devel",
+    "libgravatar-devel",
+    "libkdepim-devel",
+    "libkleo-devel",
+    "libksieve-devel",
+    "mailcommon-devel",
+    "messagelib-devel",
+    "pimcommon-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtwebengine-devel",
+    "qtkeychain-devel",
+    "sonnet-devel",
+]
+depends = [
+    "kdepim-runtime",
+    "kmail-account-wizard",
+]
+checkdepends = ["xwayland-run"] + depends
+pkgdesc = "KDE Mail Client"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-2.0-only"
+url = "https://apps.kde.org/kmail2"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kmail-{pkgver}.tar.xz"
+sha256 = "a98a04afbd0feadbeeb5eda5960c902b84c93ee8d2615dad90e04d87ab062bf4"
