eec5f60fc8663227221e5d459036b6c9e8e70f4f
psykose <alice@ayaya.dev>
2024-06-07 14:01:11 +0000

    contrib/kmbox: new package (24.05.0)

---
+++ contrib/kmbox/template.py
@@ -0,0 +1,29 @@
+pkgname = "kmbox"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kmime-devel",
+    "qt6-qtdeclarative-devel",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE PIM mbox access library"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later"
+url = "https://api.kde.org/kdepim/kmbox/html"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kmbox-{pkgver}.tar.xz"
+sha256 = "d9de14bf135ab5fbc7f33915a2bab22533169688c6d0fd113e7b51e6d7440e9a"
+
+
+@subpackage("kmbox-devel")
+def _devel(self):
+    self.depends += ["kmime-devel"]
+    return self.default_devel()
