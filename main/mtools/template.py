c145b8956e515eab7645a0eb8c1636278e4d05a6
psykose <alice@ayaya.dev>
2024-06-07 20:31:18 +0000

    main/mtools: update to 4.0.44

--- main/mtools/template.py
+++ main/mtools/template.py
@@ -1,21 +1,18 @@
 pkgname = "mtools"
-pkgver = "4.0.43"
+pkgver = "4.0.44"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["texinfo"]
+hostmakedepends = ["automake", "texinfo"]
 makedepends = ["acl-devel", "linux-headers"]
 pkgdesc = "Utilities to access MS-DOS disks"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/mtools"
-source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "541e179665dc4e272b9602f2074243591a157da89cc47064da8c5829dbd2b339"
+source = f"$(GNU_SITE)/mtools/mtools-{pkgver}.tar.bz2"
+sha256 = "37dc4df022533c3d4b2ec1c78973c27c7e8b585374c2d46ab64c6a3db31eddb8"
 # FIXME cfi
 hardening = ["vis", "!cfi"]
 
 
 def post_install(self):
     self.install_file("mtools.conf", "etc")
-
-
-configure_gen = []
