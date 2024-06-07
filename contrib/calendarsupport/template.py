b98595b98168125ffacdb8d5a609ba965b74ec1e
psykose <alice@ayaya.dev>
2024-06-07 14:32:43 +0000

    contrib/calendarsupport: new package (24.05.0)

---
+++ contrib/calendarsupport/template.py
@@ -0,0 +1,49 @@
+pkgname = "calendarsupport"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "akonadi-calendar-devel",
+    "akonadi-devel",
+    "akonadi-notes-devel",
+    "kcalendarcore-devel",
+    "kcalutils-devel",
+    "kcalutils-devel",
+    "kcodecs-devel",
+    "kguiaddons-devel",
+    "kholidays-devel",
+    "ki18n-devel",
+    "kidentitymanagement-devel",
+    "kio-devel",
+    "kmime-devel",
+    "ktextaddons-devel",
+    "qt6-qtdeclarative-devel",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE library for calendar support"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-3.0-only"
+url = "https://api.kde.org/kdepim/calendarsupport/html"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/calendarsupport-{pkgver}.tar.xz"
+)
+sha256 = "50c85b665ad9ef3b7a514216add5265fbf242362379267549a3ed723848cbcc7"
+
+
+@subpackage("calendarsupport-devel")
+def _devel(self):
+    self.depends += [
+        "akonadi-calendar-devel",
+        "kidentitymanagement-devel",
+        "kmime-devel",
+        "qt6-qtbase-devel",
+    ]
+    return self.default_devel()
