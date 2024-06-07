31d4cf4209b8be3a0bd23d8e086484916a001ba4
psykose <alice@ayaya.dev>
2024-06-07 12:30:43 +0000

    contrib/kldap: new package (24.05.0)

---
+++ contrib/kldap/template.py
@@ -0,0 +1,32 @@
+pkgname = "kldap"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kdoctools-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kwidgetsaddons-devel",
+    "libsasl-devel",
+    # "openldap-devel",  # TODO: needed for actual LDAP support
+    "qt6-qtdeclarative-devel",
+    "qtkeychain-devel",
+]
+pkgdesc = "KDE LDAP access API"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later"
+url = "https://api.kde.org/kdepim/kldap/html"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kldap-{pkgver}.tar.xz"
+sha256 = "8a29b797ac9200ece5ec753b1dbfec54d4da5a8e0f82d5823c6f3ec08dea1eae"
+
+
+@subpackage("kldap-devel")
+def _devel(self):
+    return self.default_devel()
