b24c5950d8d1d867c331728a032c8d64191f1d66
psykose <alice@ayaya.dev>
2024-06-07 14:42:19 +0000

    contrib/mailimporter: new package (24.05.0)

---
+++ contrib/mailimporter/template.py
@@ -0,0 +1,39 @@
+pkgname = "mailimporter"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "akonadi-devel",
+    "akonadi-mime-devel",
+    "karchive-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kmime-devel",
+    "pimcommon-devel",
+    "qt6-qtdeclarative-devel",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE PIM library for importing mail"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-2.0-only"
+url = "https://api.kde.org/kdepim/mailimporter/html"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/mailimporter-{pkgver}.tar.xz"
+)
+sha256 = "29d6701f683a247866b59490eb2605544abe85d79953dccf3ff28386356d3f4c"
+
+
+@subpackage("mailimporter-devel")
+def _devel(self):
+    self.depends += ["karchive-devel"]
+    return self.default_devel()
