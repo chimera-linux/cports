6305d6c7afbb8e214b3b6f77d2a6e0278d04379f
psykose <alice@ayaya.dev>
2024-06-07 17:29:03 +0000

    contrib/eventviews: new package (24.05.0)

---
+++ contrib/eventviews/template.py
@@ -0,0 +1,46 @@
+pkgname = "eventviews"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "akonadi-calendar-devel",
+    "akonadi-devel",
+    "calendarsupport-devel",
+    "kcalendarcore-devel",
+    "kcalutils-devel",
+    "kcodecs-devel",
+    "kcompletion-devel",
+    "kconfigwidgets-devel",
+    "kcontacts-devel",
+    "kdiagram-devel",
+    "kguiaddons-devel",
+    "kholidays-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kitemmodels-devel",
+    "kmime-devel",
+    "kservice-devel",
+    "libkdepim-devel",
+    "qt6-qtdeclarative-devel",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE event views library"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-2.0-or-later"
+url = "https://api.kde.org/kdepim/eventviews/html"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/eventviews-{pkgver}.tar.xz"
+sha256 = "803351e7b855fa1790ebfc1e98eab345fb6b70558d4af97b37aa34404025791f"
+
+
+@subpackage("eventviews-devel")
+def _devel(self):
+    self.depends += []
+    return self.default_devel()
