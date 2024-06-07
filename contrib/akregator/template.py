c748486e5214c9ee22eba241ca927c3b819de938
psykose <alice@ayaya.dev>
2024-06-07 16:21:52 +0000

    contrib/akregator: new package (24.05.0)

---
+++ contrib/akregator/template.py
@@ -0,0 +1,43 @@
+pkgname = "akregator"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "akonadi-devel",
+    "grantleetheme-devel",
+    "kcmutils-devel",
+    "kcodecs-devel",
+    "kcrash-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "knotifications-devel",
+    "knotifyconfig-devel",
+    "kontactinterface-devel",
+    "kparts-devel",
+    "kstatusnotifieritem-devel",
+    "ktextaddons-devel",
+    "ktextwidgets-devel",
+    "kuserfeedback-devel",
+    "kxmlgui-devel",
+    "libkdepim-devel",
+    "messagelib-devel",
+    "pimcommon-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtwebengine-devel",
+    "syndication-devel",
+]
+pkgdesc = "KDE RSS feed reader"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/akregator"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/akregator-{pkgver}.tar.xz"
+sha256 = "a95fa7f5e1ead24e56c7b500c5fdfe3111995484c181deb813b3002212099bb3"
+# INT: probably a shift overflow in remap.cpp:CalcHash
+hardening = ["!int"]
