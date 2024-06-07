a9c14a523839f0c7bc8c8bf103089649ead73fe0
psykose <alice@ayaya.dev>
2024-06-07 20:24:01 +0000

    contrib/ccid: update to 1.6.0

--- contrib/ccid/template.py
+++ contrib/ccid/template.py
@@ -1,17 +1,29 @@
 pkgname = "ccid"
-pkgver = "1.5.5"
+pkgver = "1.6.0"
 pkgrel = 0
-build_style = "gnu_configure"
-configure_args = ["--enable-twinserial"]
-configure_gen = []
-hostmakedepends = ["automake", "libtool", "pkgconf", "flex", "perl"]
-makedepends = ["zlib-devel", "pcsc-lite-devel", "udev-devel", "libusb-devel"]
+build_style = "meson"
+configure_args = [
+    "-Ddefault_library=shared",
+    "-Dserial=true",
+]
+hostmakedepends = [
+    "flex",
+    "meson",
+    "perl",
+    "pkgconf",
+]
+makedepends = [
+    "libusb-devel",
+    "pcsc-lite-devel",
+    "udev-devel",
+    "zlib-devel",
+]
 pkgdesc = "PC/SC driver to support CCID compliant readers"
 maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "LGPL-2.1-or-later"
 url = "https://ccid.apdu.fr"
-source = f"{url}/files/ccid-{pkgver}.tar.bz2"
-sha256 = "194708f75fe369d45dd7c15e8b3e8a7db8b49cfc5557574ca2a2e76ef12ca0ca"
+source = f"{url}/files/ccid-{pkgver}.tar.xz"
+sha256 = "cdca7c22c45169cfc300d65d5362b7644ee195289f4fb8bf475a6cd321752c2c"
 
 
 def post_install(self):
