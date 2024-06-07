83a8762bb8110ea5b80b35f5bdd4c9a32962c1f2
psykose <alice@ayaya.dev>
2024-06-07 15:26:54 +0000

    contrib/qt6-qtlocation: new package (6.7.1)

---
+++ contrib/qt6-qtlocation/template.py
@@ -0,0 +1,55 @@
+pkgname = "qt6-qtlocation"
+pkgver = "6.7.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "qt6-qtdeclarative-devel",
+    "qt6-qtpositioning-devel",
+]
+pkgdesc = "Qt6 Location component"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = (
+    "LGPL-2.1-only AND LGPL-3.0-only AND GPL-3.0-only WITH Qt-GPL-exception-1.0"
+)
+url = "https://www.qt.io"
+source = f"https://download.qt.io/official_releases/qt/{pkgver[:-2]}/{pkgver}/submodules/qtlocation-everywhere-src-{pkgver}.tar.xz"
+sha256 = "3870178c2ec9fda35f823ad24f3b883b70ec082a6a496eeb2d1fbd57b6268608"
+# FIXME?
+# hardening = ["!int"]
+# cross: TODO
+options = ["!cross"]
+
+
+def init_check(self):
+    self.make_check_env = {
+        "QT_QPA_PLATFORM": "offscreen",
+        "QML2_IMPORT_PATH": str(
+            self.chroot_cwd / f"{self.make_dir}/lib/qt6/qml"
+        ),
+    }
+
+
+def post_install(self):
+    self.rm(self.destdir / "usr/tests", recursive=True)
+
+
+@subpackage("qt6-qtlocation-devel")
+def _devel(self):
+    self.depends += [
+        f"qt6-qtbase-devel~{pkgver[:-2]}",
+        f"qt6-qtdeclarative-devel~{pkgver[:-2]}",
+        f"qt6-qtpositioning-devel~{pkgver[:-2]}",
+    ]
+    return self.default_devel(
+        extra=[
+            "usr/lib/qt6/metatypes",
+            "usr/lib/qt6/mkspecs",
+            "usr/lib/qt6/modules",
+            "usr/lib/*.prl",
+        ]
+    )
