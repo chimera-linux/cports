b602d2802f4e0b5552c9d153c89c186393b62957
psykose <alice@ayaya.dev>
2024-06-07 12:36:40 +0000

    contrib/libkdepim: new package (24.05.0)

---
+++ contrib/libkdepim/template.py
@@ -0,0 +1,32 @@
+pkgname = "libkdepim"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kldap-devel",
+    "kcompletion-devel",
+    "ki18n-devel",
+    "kwidgetsaddons-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qttools-devel",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE PIM common library"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-2.0-or-later"
+url = "https://api.kde.org/kdepim/libkdepim/html"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/libkdepim-{pkgver}.tar.xz"
+sha256 = "62fedf50c99296fb076c9f4ea0d907228a3f912c8f927c459156cb5c8950ba8a"
+
+
+@subpackage("libkdepim-devel")
+def _devel(self):
+    return self.default_devel(extra=["usr/lib/qt6/plugins/designer"])
