93cf38890a7c0a8f596a2b4d7e0931abcac655fd
psykose <alice@ayaya.dev>
2024-06-07 18:58:56 +0000

    contrib/kontact: new package (24.05.0)

---
+++ contrib/kontact/template.py
@@ -0,0 +1,32 @@
+pkgname = "kontact"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "grantleetheme-devel",
+    "kcmutils-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kguiaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kontactinterface-devel",
+    "libkdepim-devel",
+    "pimcommon-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtwebengine-devel",
+]
+pkgdesc = "KDE PIM unified UI"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-only AND GPL-2.0-or-later"
+url = "https://kontact.kde.org"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kontact-{pkgver}.tar.xz"
+sha256 = "184a37465583d67878254dc9e19fbfd8cbf49b451bd8613a1a11824f9ebf10a6"
