5fa3c92bced43a05af2b5d78f7c5607c7fe7dbbb
psykose <alice@ayaya.dev>
2024-06-07 18:08:51 +0000

    contrib/kaddressbook: new package (24.05.0)

---
+++ contrib/kaddressbook/template.py
@@ -0,0 +1,41 @@
+pkgname = "kaddressbook"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "akonadi-contacts-devel",
+    "akonadi-devel",
+    "akonadi-search-devel",
+    "grantleetheme-devel",
+    "kcmutils-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kontactinterface-devel",
+    "kuserfeedback-devel",
+    "libkdepim-devel",
+    "pimcommon-devel",
+    "qt6-qtbase-devel",
+]
+depends = ["kdepim-runtime"]
+pkgdesc = "KDE address book"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-2.0-or-later"
+url = "https://apps.kde.org/kaddressbook"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/kaddressbook-{pkgver}.tar.xz"
+)
+sha256 = "b1d27df875f5ac17b996bdc668af340582dc235852b358a475f002f185241351"
+
+
+@subpackage("kaddressbook-devel")
+def _devel(self):
+    return self.default_devel()
