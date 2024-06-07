c3c43324bcdf788a0c209ec9f82e35315b73038a
psykose <alice@ayaya.dev>
2024-06-07 18:45:50 +0000

    contrib/kalarm: new package (24.05.0)

---
+++ contrib/kalarm/template.py
@@ -0,0 +1,56 @@
+pkgname = "kalarm"
+pkgver = "24.05.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "akonadi-contacts-devel",
+    "akonadi-devel",
+    "akonadi-mime-devel",
+    "kauth-devel",
+    "kcalendarcore-devel",
+    "kcalutils-devel",
+    "kcodecs-devel",
+    "kcompletion-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcontacts-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kglobalaccel-devel",
+    "kguiaddons-devel",
+    "kholidays-devel",
+    "ki18n-devel",
+    "kidentitymanagement-devel",
+    "kio-devel",
+    "kitemmodels-devel",
+    "kjobwidgets-devel",
+    "kmailtransport-devel",
+    "kmime-devel",
+    "knotifications-devel",
+    "knotifyconfig-devel",
+    "kservice-devel",
+    "kstatusnotifieritem-devel",
+    "ktextaddons-devel",
+    "ktextwidgets-devel",
+    "kwidgetsaddons-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "kxmlgui-devel",
+    "libcanberra-devel",
+    "qt6-qtbase-devel",
+]
+depends = ["kdepim-runtime"]
+pkgdesc = "KDE personal alarm scheduler"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later AND GPL-2.0-or-later"
+url = "https://apps.kde.org/kalarm"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kalarm-{pkgver}.tar.xz"
+sha256 = "b8b4acfd4511deb1d04bfcf3c17fc3995a4ad37348e3fae947e44999c4a10d66"
