b752907abf1f3147327e738548d7d84f298faaa4
psykose <alice@ayaya.dev>
2024-06-08 15:13:47 +0000

    contrib/perl-role-tiny: new package (2.002004)

---
+++ contrib/perl-role-tiny/template.py
@@ -0,0 +1,19 @@
+pkgname = "perl-role-tiny"
+pkgver = "2.002004"
+pkgrel = 0
+build_style = "perl_module"
+make_cmd = "gmake"
+hostmakedepends = [
+    "gmake",
+    "perl",
+]
+makedepends = ["perl"]
+depends = ["perl"]
+pkgdesc = "Perl module for role composition"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
+url = "https://metacpan.org/pod/Role::Tiny"
+source = (
+    f"https://cpan.metacpan.org/authors/id/H/HA/HAARG/Role-Tiny-{pkgver}.tar.gz"
+)
+sha256 = "d7bdee9e138a4f83aa52d0a981625644bda87ff16642dfa845dcb44d9a242b45"
