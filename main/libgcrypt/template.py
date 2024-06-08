36b64a0299f780fb9a1875bb0faea731a9236302
psykose <alice@ayaya.dev>
2024-06-08 14:11:32 +0000

    main/libgcrypt: fix cross

--- main/libgcrypt/template.py
+++ main/libgcrypt/template.py
@@ -7,7 +7,13 @@ configure_args = [
     "--without-capabilities",
     "ac_cv_sys_symbol_underscore=no",
 ]
-hostmakedepends = ["pkgconf"]
+configure_gen = ["./autogen.sh"]
+hostmakedepends = [
+    "automake",
+    "libgpg-error-devel",  # for gpg-error.m4
+    "libtool",
+    "pkgconf",
+]
 makedepends = ["libgpg-error-devel"]
 pkgdesc = "GNU cryptographic library"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -18,6 +24,11 @@ sha256 = "8b0870897ac5ac67ded568dcfadf45969cfa8a6beb0fd60af2a9eadc2a3272aa"
 options = ["linkundefver"]
 
 
+def post_extract(self):
+    # ancient dogshit
+    self.rm("m4/gpg-error.m4")
+
+
 @subpackage("libgcrypt-devel")
 def _devel(self):
     return self.default_devel(extra=["usr/share/info"])
@@ -26,6 +37,3 @@ def _devel(self):
 @subpackage("libgcrypt-progs")
 def _progs(self):
     return self.default_progs()
-
-
-configure_gen = []
