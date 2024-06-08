87c7dca1338ca4b82fe4ad05a14844158112b0c0
psykose <alice@ayaya.dev>
2024-06-08 14:30:54 +0000

    contrib/perl-path-tiny: new package (0.146)

---
+++ contrib/perl-path-tiny/template.py
@@ -0,0 +1,13 @@
+pkgname = "perl-path-tiny"
+pkgver = "0.146"
+pkgrel = 0
+build_style = "perl_module"
+hostmakedepends = ["gmake", "perl"]
+makedepends = ["perl"]
+depends = ["perl"]
+pkgdesc = "Perl module for using extensions during the build"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Apache-2.0"
+url = "https://metacpan.org/pod/Path::Tiny"
+source = f"https://cpan.metacpan.org/authors/id/D/DA/DAGOLDEN/Path-Tiny-{pkgver}.tar.gz"
+sha256 = "861ef09bca68254e9ab24337bb6ec9d58593a792e9d68a27ee6bec2150f06741"
