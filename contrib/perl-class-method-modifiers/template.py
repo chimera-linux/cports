3fb13aeda57fdeb37e17dcfd3ade2e4653db92d0
psykose <alice@ayaya.dev>
2024-06-08 15:16:00 +0000

    contrib/perl-class-method-modifiers: new package (2.15)

---
+++ contrib/perl-class-method-modifiers/template.py
@@ -0,0 +1,17 @@
+pkgname = "perl-class-method-modifiers"
+pkgver = "2.15"
+pkgrel = 0
+build_style = "perl_module"
+make_cmd = "gmake"
+hostmakedepends = [
+    "gmake",
+    "perl",
+]
+makedepends = ["perl"]
+depends = ["perl"]
+pkgdesc = "Perl module for Moose-like method modifiers"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
+url = "https://metacpan.org/pod/Class::Method::Modifiers"
+source = f"$(CPAN_SITE)/Class/Class-Method-Modifiers-{pkgver}.tar.gz"
+sha256 = "65cd85bfe475d066e9186f7a8cc636070985b30b0ebb1cde8681cf062c2e15fc"
