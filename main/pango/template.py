d6e372b0d1fc19267ddb50ae7ef9d2862d805cb6
psykose <alice@ayaya.dev>
2024-06-09 19:02:08 +0000

    main/pango: update to 1.54.0

--- main/pango/template.py
+++ main/pango/template.py
@@ -1,5 +1,5 @@
 pkgname = "pango"
-pkgver = "1.52.2"
+pkgver = "1.54.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=enabled"]
@@ -27,7 +27,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.pango.org"
 source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:pkgver.rfind('.')]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "d0076afe01082814b853deec99f9349ece5f2ce83908b8e58ff736b41f78a96b"
+sha256 = "8a9eed75021ee734d7fc0fdf3a65c3bba51dfefe4ae51a9b414a60c70b2d1ed8"
 
 
 @subpackage("pango-xft")
