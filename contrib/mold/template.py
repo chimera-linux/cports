d36b48837ad7dcdda56e9c735368c4b78b3e48ab
psykose <alice@ayaya.dev>
2024-06-09 12:14:23 +0000

    contrib/mold: update to 2.32.0

--- contrib/mold/template.py
+++ contrib/mold/template.py
@@ -1,6 +1,6 @@
 pkgname = "mold"
-pkgver = "2.31.0"
-pkgrel = 1
+pkgver = "2.32.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DMOLD_USE_MIMALLOC=OFF",
@@ -20,7 +20,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/rui314/mold"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "3dc3af83a5d22a4b29971bfad17261851d426961c665480e2ca294e5c74aa1e5"
+sha256 = "4b7e4146ea0f52be9adae8b417399f3676a041e65b55e3f25f088120d30a320b"
 # TODO: a portion of the tests fail, for various reasons, such as assuming
 # presence of gcc, gnu grep, and various toolchain specifics; around 70%
 # of the tests pass right now, fix the rest later
