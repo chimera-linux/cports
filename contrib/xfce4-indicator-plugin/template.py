2c84839e8282199d86b207997569a4bc642b406b
triallax <triallax@tutanota.com>
2024-06-10 16:40:07 +0100

    contrib/xfce4-indicator-plugin: new package (2.4.2)

---
+++ contrib/xfce4-indicator-plugin/template.py
@@ -0,0 +1,31 @@
+pkgname = "xfce4-indicator-plugin"
+pkgver = "2.4.2"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "ayatana-ido-devel",
+    "gtk+3-devel",
+    "libayatana-indicator-devel",
+    "libx11-devel",
+    "libxfce4ui-devel",
+    "libxfce4util-devel",
+    "xfce4-panel-devel",
+    "xfconf-devel",
+]
+pkgdesc = "Xfce messaging menu panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/panel-plugins/xfce4-indicator-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-indicator-plugin/{pkgver[:-2]}/xfce4-indicator-plugin-{pkgver}.tar.bz2"
+sha256 = "4f50364e4db2993a036244dc18540b061637515ad127769b67d8f7301e2eaa1a"
