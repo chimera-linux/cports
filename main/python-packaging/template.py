d6db4d5e0b633538cd01b82a0a800f852bd19615
psykose <alice@ayaya.dev>
2024-06-10 10:35:45 +0000

    main/python-packaging: update to 24.1

--- main/python-packaging/template.py
+++ main/python-packaging/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-packaging"
-pkgver = "24.0"
+pkgver = "24.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-installer", "python-flit_core"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0 OR BSD-2-Clause"
 url = "https://github.com/pypa/packaging"
 source = f"$(PYPI_SITE)/p/packaging/packaging-{pkgver}.tar.gz"
-sha256 = "eb82c5e3e56209074766e6885bb04b8c38a0c015d0a30036ebe7ece34c9989e9"
+sha256 = "026ed72c8ed3fcce5bf8950572258698927fd1dbda10a5e981cdf0ac37f4f002"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
