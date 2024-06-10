1f47bdb4fa2c32ccb7e008a573b8f12e337e3719
triallax <triallax@tutanota.com>
2024-06-10 16:40:09 +0100

    contrib/xfce4-whiskermenu-plugin: new package (2.8.3)

---
+++ contrib/xfce4-whiskermenu-plugin/template.py
@@ -0,0 +1,27 @@
+pkgname = "xfce4-whiskermenu-plugin"
+pkgver = "2.8.3"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "gettext",
+    "pkgconf",
+]
+makedepends = [
+    "gtk+3-devel",
+    "exo-devel",
+    "garcon-devel",
+    "xfce4-panel-devel",
+    "libxfce4ui-devel",
+    "libxfce4util-devel",
+    "xfconf-devel",
+    "accountsservice-devel",
+    "gtk-layer-shell-devel",
+]
+pkgdesc = "Xfce application launcher panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/panel-plugins/xfce4-whiskermenu-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-whiskermenu-plugin/{pkgver[:-2]}/xfce4-whiskermenu-plugin-{pkgver}.tar.bz2"
+sha256 = "e776c287658f98d0739447279522fe78766088438242cf2365a49c8973fc9cd0"
