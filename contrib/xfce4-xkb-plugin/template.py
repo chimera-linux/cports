eca0a846b16acc3325eee681c8a13b4a72982b2e
triallax <triallax@tutanota.com>
2024-06-10 16:40:10 +0100

    contrib/xfce4-xkb-plugin: new package (0.8.3)

---
+++ contrib/xfce4-xkb-plugin/template.py
@@ -0,0 +1,34 @@
+pkgname = "xfce4-xkb-plugin"
+pkgver = "0.8.3"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "garcon-devel",
+    "gtk+3-devel",
+    "libnotify-devel",
+    "librsvg-devel",
+    "libwnck-devel",
+    "libx11-devel",
+    "libxfce4ui-devel",
+    "libxfce4util-devel",
+    "libxklavier-devel",
+    "xfce4-panel-devel",
+    "xfconf-devel",
+]
+pkgdesc = "Xfce keyboard layout panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/panel-plugins/xfce4-xkb-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-xkb-plugin/{pkgver[:-2]}/xfce4-xkb-plugin-{pkgver}.tar.bz2"
+sha256 = "f0bfe97875ef1ca0a3b6a6fac312663c9cada151cf1ac96071393d320cd04987"
