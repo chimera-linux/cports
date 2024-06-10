495a39c9d634646b1ec78b838974289fec1793f7
triallax <triallax@tutanota.com>
2024-06-10 16:41:13 +0100

    contrib/xfce4-generic-slider: new package (1.0.0)

---
+++ contrib/xfce4-generic-slider/template.py
@@ -0,0 +1,26 @@
+pkgname = "xfce4-generic-slider"
+pkgver = "1.0.0"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "gtk+3-devel",
+    "libxfce4ui-devel",
+    "xfce4-panel-devel",
+]
+pkgdesc = "Xfce generic slider panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-3.0-only"
+url = "https://docs.xfce.org/panel-plugins/xfce4-generic-slider/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-generic-slider/{pkgver[:-2]}/xfce4-generic-slider-{pkgver}.tar.bz2"
+sha256 = "06074d1123c1767bd0e25c6e7b34ade0a1e9edf51c996b8a643772e5881024bb"
