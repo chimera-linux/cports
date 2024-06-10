8e85e8e8bbc121a876ceaa969d935d5854c40b2b
Erica Z <zerica@callcc.eu>
2024-06-10 20:58:45 +0200

    main/libmikmod: disable cfi

--- main/libmikmod/template.py
+++ main/libmikmod/template.py
@@ -1,20 +1,18 @@
 pkgname = "libmikmod"
 pkgver = "3.3.11.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 pkgdesc = "Mikmod module player and library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "http://mikmod.shlomifish.org"
 source = f"$(SOURCEFORGE_SITE)/mikmod/{pkgname}-{pkgver}.tar.gz"
 sha256 = "ad9d64dfc8f83684876419ea7cd4ff4a41d8bcd8c23ef37ecb3a200a16b46d19"
-hardening = ["vis", "cfi"]
+# CFI: crashes in sc2 ucm
+hardening = ["vis", "!cfi"]
 
 
 @subpackage("libmikmod-devel")
 def _devel(self):
     return self.default_devel(extra=["usr/share/info"])
-
-
-configure_gen = []
