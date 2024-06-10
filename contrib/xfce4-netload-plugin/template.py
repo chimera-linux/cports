f0f895e29a4478d9eb7a1b0f9a5a3c5948c69082
triallax <triallax@tutanota.com>
2024-06-10 16:40:08 +0100

    contrib/xfce4-netload-plugin: new package (1.4.1)

---
+++ contrib/xfce4-netload-plugin/template.py
@@ -0,0 +1,26 @@
+pkgname = "xfce4-netload-plugin"
+pkgver = "1.4.1"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "gtk+3-devel",
+    "libxfce4ui-devel",
+    "xfce4-panel-devel",
+]
+pkgdesc = "Xfce network load panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/panel-plugins/xfce4-netload-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-netload-plugin/{pkgver[:-2]}/xfce4-netload-plugin-{pkgver}.tar.bz2"
+sha256 = "9fac3a3ad52e18584bfb127cd1721d56de1004b9fdd140915fded89704ccb44e"
