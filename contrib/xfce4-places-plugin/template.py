f85afc061f2e2e073f6ece3a5c4f686e443cb159
triallax <triallax@tutanota.com>
2024-06-10 16:40:08 +0100

    contrib/xfce4-places-plugin: new package (1.8.3)

---
+++ contrib/xfce4-places-plugin/template.py
@@ -0,0 +1,32 @@
+pkgname = "xfce4-places-plugin"
+pkgver = "1.8.3"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "exo-devel",
+    "gtk+3-devel",
+    "libnotify-devel",
+    "libwnck-devel",
+    "libx11-devel",
+    "libxfce4ui-devel",
+    "libxfce4util-devel",
+    "xfce4-panel-devel",
+    "xfconf-devel",
+]
+pkgdesc = "Xfce folders and media quick access panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/panel-plugins/xfce4-places-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-places-plugin/{pkgver[:-2]}/xfce4-places-plugin-{pkgver}.tar.bz2"
+sha256 = "f11d0e6d03f22ab02c2e6b507d365b5a918532e8819e50647ee1860eca60c743"
