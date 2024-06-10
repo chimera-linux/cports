b90424a3d1bb4f7ce784fbcda5565d2e2ced4ebc
psykose <alice@ayaya.dev>
2024-06-10 10:34:33 +0000

    user/limine: update to 7.7.2

--- user/limine/template.py
+++ user/limine/template.py
@@ -1,5 +1,5 @@
 pkgname = "limine"
-pkgver = "7.7.1"
+pkgver = "7.7.2"
 pkgrel = 0
 # these targets implemented
 archs = ["aarch64", "x86_64", "riscv64"]
@@ -12,7 +12,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-2-Clause AND 0BSD"
 url = "https://limine-bootloader.org"
 source = f"https://github.com/limine-bootloader/limine/releases/download/v{pkgver}/limine-{pkgver}.tar.zst"
-sha256 = "3e35aa964baf703ebd57716735da135bd4f44685445e7cdf2caafb822f6a3bc7"
+sha256 = "fe06930628571ac4d89d08598680c8df5522eb39a325d6aaeb20840ba3293abf"
 # no test suite
 options = ["!check"]
 
