9db8f665032a3a745e09cfefad73280e44a86a33
triallax <triallax@tutanota.com>
2024-06-10 16:40:08 +0100

    contrib/xfce4-smartbookmark-plugin: new package (0.5.2)

---
+++ contrib/xfce4-smartbookmark-plugin/template.py
@@ -0,0 +1,25 @@
+pkgname = "xfce4-smartbookmark-plugin"
+pkgver = "0.5.2"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "libxfce4ui-devel",
+    "xfce4-panel-devel",
+]
+pkgdesc = "Xfce browser search panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/panel-plugins/xfce4-smartbookmark-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-smartbookmark-plugin/{pkgver[:-2]}/xfce4-smartbookmark-plugin-{pkgver}.tar.bz2"
+sha256 = "2a279e2f5a54518684e62ad1f3cd8ef950826505b39725f1b5d8f0c43031cdd3"
