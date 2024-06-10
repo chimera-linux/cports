2fb3d389beb7e643daf6a4ed69e554137cc24bcc
triallax <triallax@tutanota.com>
2024-06-10 16:40:07 +0100

    contrib/xfce4-genmon-plugin: new package (4.2.0)

---
+++ contrib/xfce4-genmon-plugin/template.py
@@ -0,0 +1,28 @@
+pkgname = "xfce4-genmon-plugin"
+pkgver = "4.2.0"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "gtk+3-devel",
+    "libxfce4ui-devel",
+    "libxfce4util-devel",
+    "xfce4-panel-devel",
+    "xfconf-devel",
+]
+pkgdesc = "Xfce generic program monitor panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "LGPL-2.1-or-later"
+url = "https://docs.xfce.org/panel-plugins/xfce4-genmon-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-genmon-plugin/{pkgver[:-2]}/xfce4-genmon-plugin-{pkgver}.tar.bz2"
+sha256 = "948d08ee5f2140847f109b531bc1d4cc6268496913ea7600d3c5ad89025a0362"
