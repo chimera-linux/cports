7971cbb039855c0c291a364e427125bdcc25ffd2
triallax <triallax@tutanota.com>
2024-06-10 16:40:07 +0100

    contrib/xfce4-cpugraph-plugin: new package (1.2.10)

---
+++ contrib/xfce4-cpugraph-plugin/template.py
@@ -0,0 +1,28 @@
+pkgname = "xfce4-cpugraph-plugin"
+pkgver = "1.2.10"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "gtk+3-devel",
+    "libxfce4ui-devel",
+    "libxfce4util-devel",
+    "xfce4-panel-devel",
+    "xfconf-devel",
+]
+pkgdesc = "Xfce clipboard manager app and panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/panel-plugins/xfce4-cpugraph-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-cpugraph-plugin/{pkgver[:pkgver.rfind('.')]}/xfce4-cpugraph-plugin-{pkgver}.tar.bz2"
+sha256 = "37792dd052691712195658169b95fb6583f924485ce7a467b33d01e08775d915"
