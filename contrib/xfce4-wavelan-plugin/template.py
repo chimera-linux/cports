7ff55f9235e5a951b7f5148d5fdbc7b0128bd67e
triallax <triallax@tutanota.com>
2024-06-10 16:40:09 +0100

    contrib/xfce4-wavelan-plugin: new package (0.6.3)

---
+++ contrib/xfce4-wavelan-plugin/template.py
@@ -0,0 +1,30 @@
+pkgname = "xfce4-wavelan-plugin"
+pkgver = "0.6.3"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "libxfce4ui-devel",
+    "xfce4-panel-devel",
+]
+depends = ["network-manager-applet"]
+pkgdesc = "Xfce WLAN stats panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "BSD-2-Clause"
+url = "https://docs.xfce.org/panel-plugins/xfce4-wavelan-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-wavelan-plugin/{pkgver[:-2]}/xfce4-wavelan-plugin-{pkgver}.tar.bz2"
+sha256 = "61c0c2f56cb70872d403b770dd76349df9ff24c0dbe905ee1b4f913c34d8f72b"
+
+
+def post_install(self):
+    self.install_license("COPYING")
