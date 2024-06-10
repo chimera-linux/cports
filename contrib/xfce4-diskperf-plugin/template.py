1821ef497529e482949350fe4d4cc3ec46f92c1b
triallax <triallax@tutanota.com>
2024-06-10 16:40:07 +0100

    contrib/xfce4-diskperf-plugin: new package (2.7.0)

---
+++ contrib/xfce4-diskperf-plugin/template.py
@@ -0,0 +1,31 @@
+pkgname = "xfce4-diskperf-plugin"
+pkgver = "2.7.0"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "gtk+3-devel",
+    "libxfce4ui-devel",
+    "libxfce4util-devel",
+    "xfce4-panel-devel",
+]
+pkgdesc = "Xfce disk performance panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "BSD-2-Clause"
+url = "https://docs.xfce.org/panel-plugins/xfce4-diskperf-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-diskperf-plugin/{pkgver[:-2]}/xfce4-diskperf-plugin-{pkgver}.tar.bz2"
+sha256 = "5909a65341a6af4d7ff3c7bb87aeac91c763f69b43ae9dc4a10668ac226fecc9"
+
+
+def post_install(self):
+    self.install_license("COPYING")
