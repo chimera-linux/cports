b7d79c9bb36f9866b3e5e4a85f3e70539ddb0074
psykose <alice@ayaya.dev>
2024-06-11 20:28:36 +0000

    main/alsa-lib: update to 1.2.12

--- main/alsa-lib/template.py
+++ main/alsa-lib/template.py
@@ -1,5 +1,5 @@
 pkgname = "alsa-lib"
-pkgver = "1.2.11"
+pkgver = "1.2.12"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.alsa-project.org"
 source = f"{url}/files/pub/lib/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "9f3f2f69b995f9ad37359072fbc69a3a88bfba081fc83e9be30e14662795bb4d"
+sha256 = "4868cd908627279da5a634f468701625be8cc251d84262c7e5b6a218391ad0d2"
 
 
 @subpackage("alsa-lib-devel")
