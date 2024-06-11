037b0c546095c8e7229de6f0b435e3a8b1243fc6
triallax <triallax@tutanota.com>
2024-06-11 02:28:48 +0100

    contrib/chezmoi: update to 2.49.0

--- contrib/chezmoi/template.py
+++ contrib/chezmoi/template.py
@@ -1,6 +1,6 @@
 pkgname = "chezmoi"
-pkgver = "2.48.2"
-pkgrel = 1
+pkgver = "2.49.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X main.version={pkgver} -X main.commit=v{pkgver}",
@@ -12,7 +12,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "MIT"
 url = "https://chezmoi.io"
 source = f"https://github.com/twpayne/chezmoi/archive/v{pkgver}.tar.gz"
-sha256 = "49f6ab88240fea76b12c50e1f8a63a3e652a4ed48e283a0f89ac47fbd21a62fe"
+sha256 = "10353cdc817d020b4ac2175aa0e45ac72cba2d11e16829e630ace7f5fe600ba3"
 # debug: fails to split on powerpc
 # check: needs network access
 options = ["!debug", "!check"]
