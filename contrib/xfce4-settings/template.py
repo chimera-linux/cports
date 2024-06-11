5711ca817f6b7824cc215b6622ebad2c7fde1469
triallax <triallax@tutanota.com>
2024-06-12 00:54:27 +0100

    contrib/xfce4-settings: update to 4.18.6

--- contrib/xfce4-settings/template.py
+++ contrib/xfce4-settings/template.py
@@ -1,5 +1,5 @@
 pkgname = "xfce4-settings"
-pkgver = "4.18.5"
+pkgver = "4.18.6"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -43,7 +43,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-only"
 url = "https://docs.xfce.org/xfce/xfce4-settings/start"
 source = f"$(XFCE_SITE)/xfce/xfce4-settings/{pkgver[:-2]}/xfce4-settings-{pkgver}.tar.bz2"
-sha256 = "8d875b4b079114762e1d917dfdee4c333287424d21e75058d1c6ef2a449e7d1a"
+sha256 = "d9a9051b6026edd6766c64bb403b51e9167e4d31e7f1c7f843d3aed19f667bfe"
 
 
 @subpackage("xfce4-settings-default-themes")
