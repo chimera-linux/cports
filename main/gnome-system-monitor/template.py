943f0a15aee1eb197b9f0e54bc6dc694b69949df
triallax <triallax@tutanota.com>
2024-06-11 02:21:11 +0100

    main/gnome-system-monitor: clean up deps

--- main/gnome-system-monitor/template.py
+++ main/gnome-system-monitor/template.py
@@ -8,27 +8,24 @@ configure_args = [
 hostmakedepends = [
     "gettext",
     "glib-devel",
+    "gtk-update-icon-cache",
     "itstool",
-    "libxml2-progs",
     "meson",
     "pkgconf",
 ]
 makedepends = [
-    "glib-devel",
     "glibmm-devel",
     "gtkmm-devel",
     "libadwaita-devel",
     "libgtop-devel",
     "librsvg-devel",
     "libxml2-devel",
-    "libxslt-devel",
-    "polkit-devel",
 ]
-checkdepends = ["appstream-glib-devel", "desktop-file-utils"]
+checkdepends = ["appstream", "desktop-file-utils"]
 pkgdesc = "GNOME system monitor"
 maintainer = "GeopJr <evan@geopjr.dev>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/SystemMonitor"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[0:pkgver.find('.')]}/{pkgname}-{pkgver}.tar.xz"
+source = f"$(GNOME_SITE)/gnome-system-monitor/{pkgver[0:pkgver.find('.')]}/gnome-system-monitor-{pkgver}.tar.xz"
 sha256 = "5376248158c686c308255472e8c2a9e4c17255642c149fc7198c6d034e547599"
 tool_flags = {"CFLAGS": ["-D_BSD_SOURCE"]}
