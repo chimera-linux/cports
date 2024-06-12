b86cc1474a1f9626c03741ae50a66c35ba1b1a46
ttyyls <contact@behri.org>
2024-06-12 17:53:39 +0100

    contrib/nbfc-linux: update to 0.2.7

--- contrib/nbfc-linux/template.py
+++ contrib/nbfc-linux/template.py
@@ -1,6 +1,6 @@
 pkgname = "nbfc-linux"
-pkgver = "0.1.15"
-pkgrel = 1
+pkgver = "0.2.7"
+pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
 make_build_args = ["confdir=/etc"]
@@ -11,11 +11,10 @@ maintainer = "Sid Pranjale <mail@sidonthe.net>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/nbfc-linux/nbfc-linux"
 source = f"{url}/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "d1b37800886a66a6ef5a98ba45a88812fa451579028f5e4032375c62f46df535"
+sha256 = "5915af1fd3c23bdb8289cc9428fad825daf49dc187b50bbe759c317c9785bd35"
 # no tests
 options = ["!check"]
 
 
 def post_install(self):
     self.install_service(self.files_path / "nbfc")
-    self.rm(self.destdir / "usr/lib/systemd/system/nbfc_service.service")
