5d5b92ee1397ff872b307c72d5e9c3a4eb829495
triallax <triallax@tutanota.com>
2024-06-12 01:32:52 +0100

    contrib/glycin-loaders: build with cargo-auditable, run checks
    turns out the original comment regarding tests needing system-installed
    loaders was not true at all; for tests, the buildsystem installs the
    loaders into a separate location in the build directory and then the
    tests use these loaders

--- contrib/glycin-loaders/template.py
+++ contrib/glycin-loaders/template.py
@@ -1,10 +1,9 @@
 pkgname = "glycin-loaders"
 pkgver = "1.0.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
-configure_args = ["-Dtests=false"]
 hostmakedepends = [
-    "cargo",
+    "cargo-auditable",
     "gettext",
     "meson",
     "pkgconf",
@@ -19,14 +18,17 @@ makedepends = [
     "rust-std",
 ]
 depends = ["bubblewrap"]
+checkdepends = ["bubblewrap", "gtk4-devel"]
 pkgdesc = "Sandboxed and extendable image decoding"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "MPL-2.0 OR LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/sophie-h/glycin"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+source = (
+    f"$(GNOME_SITE)/glycin-loaders/{pkgver[:-2]}/glycin-loaders-{pkgver}.tar.xz"
+)
 sha256 = "d0f022462ff555856e85ea940474470bb36b37c9ffcbcba63a03fe5e954370cf"
-# Needs loaders to be system-installed
-options = ["!check"]
+# deleting CARGO_BUILD_TARGET from env breaks cross
+options = ["!cross"]
 
 
 def init_build(self):
@@ -34,12 +36,5 @@ def init_build(self):
 
     renv = cargo.get_environment(self)
     self.make_env.update(renv)
-
-
-def post_install(self):
-    for loader in (self.cwd / "loaders").glob("glycin-*"):
-        self.install_file(
-            f"./build/cargo_target/loaders/{self.profile().triplet}/release/{loader.name}",
-            "usr/libexec/glycin-loaders/1+",
-            mode=0o755,
-        )
+    # so target/release is not triple-prefixed for buildsystem integration
+    del self.make_env["CARGO_BUILD_TARGET"]
