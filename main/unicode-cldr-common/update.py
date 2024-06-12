68046c0195b7670abeb12076ac4e99a81371fc5c
psykose <alice@ayaya.dev>
2024-06-12 20:56:03 +0000

    main/unicode-cldr-common: fix update-check

--- main/unicode-cldr-common/update.py
+++ main/unicode-cldr-common/update.py
@@ -1,1 +1,6 @@
-pattern = r">release-([\d-]+)<"
+url = "https://api.github.com/repos/unicode-org/cldr/git/refs/tags"
+pattern = r"refs/tags/release-([\d-]+)\""
+
+
+def fetch_versions(self, src):
+    return map(lambda v: v.replace("-", "."), self.fetch_versions(src))
