51ac1d19dd8dc6a410088fe6d0a1414d507ff322
psykose <alice@ayaya.dev>
2024-06-12 21:04:13 +0000

    contrib/pgn-extract: update to 24.10

--- contrib/pgn-extract/template.py
+++ contrib/pgn-extract/template.py
@@ -1,5 +1,5 @@
 pkgname = "pgn-extract"
-pkgver = "22.11"
+pkgver = "24.10"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -13,7 +13,7 @@ maintainer = "shtayerc <david.murko@mailbox.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.cs.kent.ac.uk/people/staff/djb/pgn-extract"
 source = f"{url}/pgn-extract-{pkgver.replace('.', '-')}.tgz"
-sha256 = "331e84d55299987dc27f159292ea3b59b94c47edc972f32e96f4e6c0c8621c0b"
+sha256 = "f15ab628218111e048b6d9313ea519f03125edd6edf2c860811246d5da921279"
 
 
 def init_build(self):
