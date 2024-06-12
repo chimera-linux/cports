daec0fac09d3bf79ca36db093b08ddcb2067c228
Erica Z <zerica@callcc.eu>
2024-06-12 17:16:01 +0200

    contrib/mgba: split devel

--- contrib/mgba/template.py
+++ contrib/mgba/template.py
@@ -1,6 +1,6 @@
 pkgname = "mgba"
 pkgver = "0.10.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -29,3 +29,8 @@ url = "https://mgba.io"
 source = f"https://github.com/mgba-emu/mgba/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "be2cda7de3da8819fdab0c659c5cd4c4b8ca89d9ecddeeeef522db6d31a64143"
 hardening = ["!int"]
+
+
+@subpackage("mgba-devel")
+def _devel(self):
+    return self.default_devel()
