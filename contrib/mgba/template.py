2a4b907126ca1c0c392062a819ea862acbeef2ba
Erica Z <zerica@callcc.eu>
2024-06-12 16:13:20 +0200

    contrib/mgba: new package (0.10.3)

---
+++ contrib/mgba/template.py
@@ -0,0 +1,31 @@
+pkgname = "mgba"
+pkgver = "0.10.3"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "libzip-progs",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "elfutils-devel",
+    "ffmpeg-devel",
+    "libedit-devel",
+    "libepoxy-devel",
+    "libpng-devel",
+    "libzip-devel",
+    "lua5.4-devel",
+    "minizip-devel",
+    # next release: qt6
+    "sdl-devel",
+    "sqlite-devel",
+    "zlib-devel",
+]
+pkgdesc = "GBA emulator"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "MPL-2.0"
+url = "https://mgba.io"
+source = f"https://github.com/mgba-emu/mgba/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "be2cda7de3da8819fdab0c659c5cd4c4b8ca89d9ecddeeeef522db6d31a64143"
+hardening = ["!int"]
