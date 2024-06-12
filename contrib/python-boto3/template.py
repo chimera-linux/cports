ef4a13f568185d84375a14b54e1456a91ae4aa6e
ttyyls <contact@behri.org>
2024-06-12 17:20:15 +0100

    contrib/python-boto3: update to 1.34.124

--- contrib/python-boto3/template.py
+++ contrib/python-boto3/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-boto3"
-pkgver = "1.34.123"
+pkgver = "1.34.124"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
@@ -26,7 +26,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/boto/boto3"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "714b7e7a494c822cd8a0f737a035867f91eec9488dae52540f36953b02c2f502"
+sha256 = "bb409462ec9683c229bc88f5c1f3868e1198b4cbf1cfdf8a9581a7db49a6e8f0"
 
 
 def init_check(self):
