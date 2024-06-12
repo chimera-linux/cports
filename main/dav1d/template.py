3db1578e8dbafee681b42e09ae5750553a8e23bf
psykose <alice@ayaya.dev>
2024-06-12 20:41:43 +0000

    main/dav1d: update to 1.4.3

--- main/dav1d/template.py
+++ main/dav1d/template.py
@@ -1,5 +1,5 @@
 pkgname = "dav1d"
-pkgver = "1.4.2"
+pkgver = "1.4.3"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -13,8 +13,8 @@ pkgdesc = "Small and fast AV1 decoder"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://code.videolan.org/videolan/dav1d"
-source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "fdcf77c191eae67812f5e0f09a146bd8533f10f3f8018a861dca68ea735e73ae"
+source = f"{url}/-/archive/{pkgver}/dav1d-{pkgver}.tar.gz"
+sha256 = "88a023e58d955e0886faf49c72940e0e90914a948a8e60c1326ce3e09e7a6099"
 # FIXME cfi, int
 hardening = ["vis", "!cfi", "!int"]
 
