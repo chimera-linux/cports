70dc94c3195a6c4a9d8004d0cf476321b891a913
psykose <alice@ayaya.dev>
2024-06-12 12:41:19 +0000

    main/cpupower: update to 6.9.4

--- main/cpupower/template.py
+++ main/cpupower/template.py
@@ -1,5 +1,5 @@
 pkgname = "cpupower"
-pkgver = "6.9.3"
+pkgver = "6.9.4"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -36,7 +36,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[:pkgver.find('.')]}.x/linux-{pkgver}.tar.xz"
-sha256 = "c321c46401368774fc236f57095b205a5da57415f9a6008018902f9fd5eddfae"
+sha256 = "272800e0d1a7d01a78bce95a3aaf5c80816f50eb15c517d7003e58355760ecc2"
 # nope
 options = ["!check"]
 
