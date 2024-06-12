bd5201345cbe0ddf572bb1ba83b2fcb8910013eb
psykose <alice@ayaya.dev>
2024-06-12 14:07:08 +0000

    main/freeglut: update to 3.6.0

--- main/freeglut/template.py
+++ main/freeglut/template.py
@@ -1,5 +1,5 @@
 pkgname = "freeglut"
-pkgver = "3.4.0"
+pkgver = "3.6.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://freeglut.sourceforge.net"
 source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "3c0bcb915d9b180a97edaebd011b7a1de54583a838644dcd42bb0ea0c6f3eaec"
+sha256 = "9c3d4d6516fbfa0280edc93c77698fb7303e443c1aaaf37d269e3288a6c3ea52"
 # no tests
 options = ["!check"]
 
