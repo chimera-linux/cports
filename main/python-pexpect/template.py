e1fec7b16f00e534b468eacac57bc9c3a2c56a8d
miko <mikoxyzzz@gmail.com>
2024-06-13 07:35:13 +0200

    main/python-pexpect: move from contrib/

--- main/python-pexpect/template.py
+++ main/python-pexpect/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pexpect"
 pkgver = "4.9.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "python_pep517"
 # FIXME: some test in this can hang on ppc64le
 make_check_args = ["--deselect", "tests/test_socket.py"]
