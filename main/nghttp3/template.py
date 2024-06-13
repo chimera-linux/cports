8cb72b387062b80412f5aab5c6714dc242401a0a
psykose <alice@ayaya.dev>
2024-06-13 13:23:27 +0000

    main/nghttp3: update to 1.4.0

--- main/nghttp3/template.py
+++ main/nghttp3/template.py
@@ -1,5 +1,5 @@
 pkgname = "nghttp3"
-pkgver = "1.3.0"
+pkgver = "1.4.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -12,7 +12,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://nghttp2.org/nghttp3"
 source = f"https://github.com/ngtcp2/nghttp3/releases/download/v{pkgver}/nghttp3-{pkgver}.tar.xz"
-sha256 = "450525981d302f23832b18edd1a62cf58019392ca6402408d0eb1a7f3fd92ecf"
+sha256 = "c87b27ef33982a3b831dd349f4a75c55bd4c22a8ec0890095b84b54009df9d6a"
 
 
 def post_install(self):
