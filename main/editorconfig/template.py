378f97415600ec7fbd429919b1d7f037e121417e
psykose <alice@ayaya.dev>
2024-06-13 18:43:09 +0000

    main/editorconfig: update to 0.12.8

--- main/editorconfig/template.py
+++ main/editorconfig/template.py
@@ -1,5 +1,5 @@
 pkgname = "editorconfig"
-pkgver = "0.12.7"
+pkgver = "0.12.8"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://editorconfig.org"
 source = f"https://github.com/editorconfig/editorconfig-core-c/archive/v{pkgver}.tar.gz"
-sha256 = "f89d2e144fd67bdf0d7acfb2ac7618c6f087e1b3f2c3a707656b4180df422195"
+sha256 = "508f7633416a2ce3c05104ea7daac61c4953803c9935cca6e059086cfa67ee63"
 # test files not available
 options = ["!check"]
 
