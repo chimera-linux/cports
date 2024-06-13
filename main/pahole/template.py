db719fbc8a280af44c604bc35f312bcd286f33b7
q66 <q66@chimera-linux.org>
2024-06-13 03:54:26 +0200

    main/pahole: pretend we are a newer version because memes

--- main/pahole/template.py
+++ main/pahole/template.py
@@ -1,6 +1,6 @@
 pkgname = "pahole"
-pkgver = "1.26"
-pkgrel = 3
+pkgver = "1.27"
+pkgrel = 1
 build_style = "cmake"
 configure_args = [
     "-D__LIB=lib",
@@ -23,7 +23,7 @@ pkgdesc = "Debug information utilities"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://git.kernel.org/pub/scm/devel/pahole/pahole.git"
-source = f"https://fedorapeople.org/~acme/dwarves/dwarves-{pkgver}.tar.xz"
+source = f"https://fedorapeople.org/~acme/dwarves/dwarves-1.26.tar.xz"
 sha256 = "ad4c08339850e404609e2808012580b7e98366d2b91054bb93fe6dca94651fb4"
 tool_flags = {
     # see libbpf comment about bpf headers
