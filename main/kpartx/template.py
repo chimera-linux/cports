26b29ab20733cb64d807fa85b2bccec4fb09f1a2
psykose <alice@ayaya.dev>
2024-06-13 18:43:42 +0000

    main/kpartx: update to 0.9.9

--- main/kpartx/template.py
+++ main/kpartx/template.py
@@ -1,5 +1,5 @@
 pkgname = "kpartx"
-pkgver = "0.9.8"
+pkgver = "0.9.9"
 pkgrel = 0
 build_wrksrc = "kpartx"
 build_style = "makefile"
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "http://christophe.varoqui.free.fr"
 source = f"https://github.com/opensvc/multipath-tools/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "0f8cfe691d54192c7593cd932e3080bcd35d25454ae2ebb4043213b38eec34ab"
+sha256 = "5c79219a7cce4eba5a196363ac121b9c0f3765e70e032889a5bf8e0c95de7bcb"
 hardening = ["vis", "cfi"]
 # no test suite
 options = ["!check"]
