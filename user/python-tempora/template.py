b976ad3635a943201970f950283bc0fb858c2dfa
ttyyls <contact@behri.org>
2024-06-15 22:13:07 +0100

    user/python-tempora: new package

---
+++ user/python-tempora/template.py
@@ -0,0 +1,31 @@
+pkgname = "python-tempora"
+pkgver = "5.8.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools_scm",
+    "python-wheel",
+]
+depends = [
+    "python-dateutil",
+    "python-jaraco.functools",
+    "python-pytz",
+]
+checkdepends = [
+    "python-pytest",
+    "python-freezegun",
+]
+pkgdesc = "Objects and routines pertaining to date and time"
+maintainer = "ttyyls <contact@behri.org>"
+license = "MIT"
+url = "https://github.com/jaraco/tempora"
+source = f"$(PYPI_SITE)/t/tempora/tempora-{pkgver}.tar.gz"
+sha256 = "1e9606e65a3f2063460961d68515dee07bdaca0859305a8d3e6604168175fef1"
+# fixtures aren't applied
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
