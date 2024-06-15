7dd991bcb02aa9c5ab77a03bd754ae4585eba0b4
psykose <alice@ayaya.dev>
2024-06-15 14:03:46 +0000

    contrib/python-fastjsonschema: update to 2.20.0

--- contrib/python-fastjsonschema/template.py
+++ contrib/python-fastjsonschema/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-fastjsonschema"
-pkgver = "2.19.1"
-pkgrel = 1
+pkgver = "2.20.0"
+pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
-    # pytest-benchmark
-    "--deselect=tests/benchmarks/test_benchmark.py",
     # hangs
     "--deselect=tests/test_examples.py",
+    "--benchmark-skip",
 ]
 hostmakedepends = [
     "python-build",
@@ -15,13 +14,13 @@ hostmakedepends = [
     "python-wheel",
 ]
 depends = ["python"]
-checkdepends = ["python-pytest"]
+checkdepends = ["python-pytest-benchmark"]
 pkgdesc = "Python json schema validator"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-3-Clause"
 url = "https://horejsek.github.io/python-fastjsonschema"
 source = f"$(PYPI_SITE)/f/fastjsonschema/fastjsonschema-{pkgver}.tar.gz"
-sha256 = "e3126a94bdc4623d3de4485f8d468a12f02a67921315ddc87836d6e456dc789d"
+sha256 = "3d48fc5300ee96f5d116f10fe6f28d938e6008f59a6a025c2649475b87f76a23"
 
 
 def post_install(self):
