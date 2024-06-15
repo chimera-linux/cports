dfd2557ef91aeed8451e2946c8f32a41b4717046
psykose <alice@ayaya.dev>
2024-06-15 17:34:43 +0000

    contrib/openvdb: new package (11.0.0)

---
+++ contrib/openvdb/template.py
@@ -0,0 +1,50 @@
+pkgname = "openvdb"
+pkgver = "11.0.0"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    "-DOPENVDB_CORE_STATIC=OFF",  # 1.4gb lol
+    "-DOPENVDB_BUILD_NANOVDB=ON",
+    "-DOPENVDB_BUILD_PYTHON_MODULE=ON",
+    "-DOPENVDB_BUILD_UNITTESTS=ON",
+]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "boost-devel",
+    "c-blosc-devel",
+    "gtest-devel",
+    "onetbb-devel",
+    "python-devel",
+    "python-pybind11-devel",
+]
+pkgdesc = "Sparse volume data tools"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MPL-2.0"
+url = "https://www.openvdb.org"
+source = f"https://github.com/AcademySoftwareFoundation/openvdb/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "6314ff1db057ea90050763e7b7d7ed86d8224fcd42a82cdbb9c515e001b96c74"
+# 4 unittests fail outside of x86_64 with no easy way to skip them
+options = []
+
+if self.profile().arch != "x86_64":
+    options += ["!check"]
+
+
+@subpackage("openvdb-devel")
+def _devel(self):
+    return self.default_devel()
+
+
+@subpackage("openvdb-progs")
+def _progs(self):
+    return self.default_progs()
+
+
+@subpackage("python-pyopenvdb")
+def _python(self):
+    self.pkgdesc = f"{pkgdesc} (python bindings)"
+    return ["usr/lib/python*"]
