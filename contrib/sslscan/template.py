05c89150b80099e4251febe7db36fa5f1bc178c4
psykose <alice@ayaya.dev>
2024-06-16 22:57:25 +0000

    contrib/sslscan: update to 2.1.4

--- contrib/sslscan/template.py
+++ contrib/sslscan/template.py
@@ -1,5 +1,5 @@
 pkgname = "sslscan"
-pkgver = "2.1.3"
+pkgver = "2.1.4"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -11,7 +11,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later WITH custom:OpenSSL-exception"
 url = "https://github.com/rbsec/sslscan"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "6beec9345635b41fa2c1bbc5f0854f10014e4b2b4179e9e9a3bda6bdb9e1aa41"
+sha256 = "3e2a5b1f53d1f132b4d999ff450d2cc40e9efb648cea89b74f5944b768a10e63"
 hardening = ["vis", "cfi"]
 # no tests
 options = ["!check"]
