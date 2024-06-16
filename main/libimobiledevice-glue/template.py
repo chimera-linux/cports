071c388f52c3cab7c92455e58f87db8fbc2ab895
psykose <alice@ayaya.dev>
2024-06-16 12:44:40 +0000

    main/libimobiledevice-glue: update to 1.3.0

--- main/libimobiledevice-glue/template.py
+++ main/libimobiledevice-glue/template.py
@@ -1,6 +1,6 @@
 pkgname = "libimobiledevice-glue"
-pkgver = "1.2.0"
-pkgrel = 1
+pkgver = "1.3.0"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
     "automake",
@@ -13,7 +13,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-or-later"
 url = "https://libimobiledevice.org"
 source = f"https://github.com/libimobiledevice/libimobiledevice-glue/releases/download/{pkgver}/libimobiledevice-glue-{pkgver}.tar.bz2"
-sha256 = "ff9cbc240c9780edfa43914a057b86362054053721b65fb04f54a25023b92b62"
+sha256 = "96ec4eb2b1e217392149eafb2b5c3cd3e7110200f0e2bb5003c37d3ead7244ef"
 
 
 @subpackage("libimobiledevice-glue-devel")
