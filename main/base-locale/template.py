63275b44557bf9cab4a6ef4e2be5addc0e826cea
q66 <q66@chimera-linux.org>
2024-06-17 20:17:09 +0200

    main/base-locale: new package
    Closes https://github.com/chimera-linux/cports/pull/2027

---
+++ main/base-locale/template.py
@@ -0,0 +1,10 @@
+pkgname = "base-locale"
+pkgver = "0.2"
+pkgrel = 0
+build_style = "meta"
+depends = ["musl-locales"]
+pkgdesc = "Base package for locale data"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:meta"
+url = "https://chimera-linux.org"
+options = ["empty"]
