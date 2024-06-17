7fba43cd64158a0af59fe95bc13ac40798701a09
Subhaditya Nath <sn03.general@gmail.com>
2024-06-17 16:54:31 +0530

    contrib/tree-sitter-query: fix typo in pkgdesc

--- contrib/tree-sitter-query/template.py
+++ contrib/tree-sitter-query/template.py
@@ -9,7 +9,7 @@ hostmakedepends = [
     "tree-sitter-cli",
     "pkgconf",
 ]
-pkgdesc = "Tree-sitter query grammar for tree-sitte"
+pkgdesc = "Tree-sitter query grammar for tree-sitter"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/tree-sitter-grammars/tree-sitter-query"
