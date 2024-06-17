f7ecc78d0b9c9f364092f3e4197834d67b68575b
psykose <alice@ayaya.dev>
2024-06-17 18:31:44 +0000

    main/python-urllib3: update to 2.2.2

--- main/python-urllib3/template.py
+++ main/python-urllib3/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-urllib3"
-pkgver = "2.2.1"
+pkgver = "2.2.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-installer", "python-hatchling"]
@@ -9,16 +9,10 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://urllib3.readthedocs.io"
 source = f"$(PYPI_SITE)/u/urllib3/urllib3-{pkgver}.tar.gz"
-sha256 = "d0570876c61ab9e520d776c38acbbb5b05a776d3f9ff98a5c8fd5162a444cf19"
+sha256 = "dd505485549a7a552833da5e6063639d0d177c04f23bc3864e41e5dc5f612168"
 # unpackaged dependency
 options = ["!check", "brokenlinks"]
 
 
 def post_install(self):
-    for f in (self.destdir / "usr/lib").glob(
-        "python*/site-packages/urllib3/packages/six.py"
-    ):
-        f.unlink()
-        f.symlink_to("../../six.py")
-
     self.install_license("LICENSE.txt")
