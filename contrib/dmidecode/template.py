f9c495e08ff87f5abf13609eb11d0198ea7c11a1
psykose <alice@ayaya.dev>
2024-06-17 23:37:21 +0000

    contrib/dmidecode: back to cdn
    other one is poof

--- contrib/dmidecode/template.py
+++ contrib/dmidecode/template.py
@@ -12,7 +12,7 @@ pkgdesc = "Utility for reporting system hardware"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://nongnu.org/dmidecode"
-source = f"https://de.freedif.org/savannah/dmidecode/dmidecode-{pkgver}.tar.xz"
+source = f"https://download.savannah.gnu.org/releases/dmidecode/dmidecode-{pkgver}.tar.xz"
 sha256 = "e40c65f3ec3dafe31ad8349a4ef1a97122d38f65004ed66575e1a8d575dd8bae"
 hardening = ["vis", "cfi"]
 # none present
