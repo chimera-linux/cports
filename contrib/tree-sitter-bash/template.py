af956479f5057c9054a43c1cfedb653ccc04e3d5
psykose <alice@ayaya.dev>
2024-06-17 20:13:41 +0000

    contrib/tree-sitter-bash: new package (0.21.0)

---
+++ contrib/tree-sitter-bash/template.py
@@ -0,0 +1,30 @@
+pkgname = "tree-sitter-bash"
+pkgver = "0.21.0"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_check_target = "test"
+hostmakedepends = [
+    "gmake",
+    "pkgconf",
+    "tree-sitter-cli",
+]
+pkgdesc = "Bash grammar for tree-sitter"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/tree-sitter/tree-sitter-bash"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "f0515efda839cfede851adb24ac154227fbc0dfb60c6c11595ecfa9087d43ceb"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+    self.install_dir("usr/lib/tree-sitter")
+    self.install_link(
+        "usr/lib/tree-sitter/bash.so", "../libtree-sitter-bash.so.0"
+    )
+
+
+@subpackage("tree-sitter-bash-devel")
+def _devel(self):
+    return self.default_devel()
