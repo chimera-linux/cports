15cff244e5be59574a0b24f7585db0f20626acdd
q66 <q66@chimera-linux.org>
2024-06-18 23:38:33 +0200

    main/linux-stable: update to 6.9.5

--- main/linux-stable/template.py
+++ main/linux-stable/template.py
@@ -1,6 +1,6 @@
 # update linux-stable-zfs-bin when bumping
 pkgname = "linux-stable"
-pkgver = "6.8.12"
+pkgver = "6.9.5"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]
 make_dir = "build"
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
-sha256 = "19b31956d229b5b9ca5671fa1c74320179682a3d8d00fc86794114b21da86039"
+sha256 = "a51fb4ab5003a6149bd9bf4c18c9b1f0f4945c272549095ab154b9d1052f95b1"
 # no meaningful checking to be done
 options = [
     "!check",
@@ -20,7 +20,6 @@ options = [
     "!strip",
     "!scanrundeps",
     "!scanshlibs",
-    "!linkparallel",
     "!lto",
     "textrels",
     "execstack",
