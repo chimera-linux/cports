61f79e1f596a67e93f8478398648e9c4bb611534
triallax <triallax@tutanota.com>
2024-06-18 12:09:45 +0100

    contrib/xfce4-eyes-plugin: new package (4.6.0)

---
+++ contrib/xfce4-eyes-plugin/template.py
@@ -0,0 +1,26 @@
+pkgname = "xfce4-eyes-plugin"
+pkgver = "4.6.0"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "intltool",
+    "libtool",
+    "pkgconf",
+    "xfce4-dev-tools",
+]
+makedepends = [
+    "gtk+3-devel",
+    "libxfce4ui-devel",
+    "xfce4-panel-devel",
+]
+pkgdesc = "Xfce eyes panel plugin"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-2.0-or-later"
+url = "https://docs.xfce.org/panel-plugins/xfce4-eyes-plugin/start"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-eyes-plugin/{pkgver[:-2]}/xfce4-eyes-plugin-{pkgver}.tar.bz2"
+sha256 = "ad0ff05d88ba393b7c8922f8233edd33fc0a4e8b000b61de1f8f3a10c5ae5324"
