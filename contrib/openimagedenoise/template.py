54ebf7c91d551af24663b70335dcd655a4b9d43d
psykose <alice@ayaya.dev>
2024-06-19 01:36:53 +0200

    contrib/openimagedenoise: update source

--- contrib/openimagedenoise/template.py
+++ contrib/openimagedenoise/template.py
@@ -18,7 +18,7 @@ pkgdesc = "Ray-tracing denoising library"
 maintainer = "Erica Z <zerica@callcc.eu>"
 license = "Apache-2.0"
 url = "https://www.openimagedenoise.org"
-source = f"https://github.com/OpenImageDenoise/oidn/releases/download/v{pkgver}/oidn-{pkgver}.src.tar.gz"
+source = f"https://github.com/RenderKit/oidn/releases/download/v{pkgver}/oidn-{pkgver}.src.tar.gz"
 sha256 = "cce3010962ec84e0ba1acd8c9055a3d8de402fedb1b463517cfeb920a276e427"
 # set in release
 tool_flags = {"CFLAGS": ["-DNDEBUG"], "CXXFLAGS": ["-DNDEBUG"]}
