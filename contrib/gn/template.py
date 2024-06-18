817cb706aeb10964800b07e58fd17cebc9a776da
q66 <q66@chimera-linux.org>
2024-06-18 14:07:57 +0200

    contrib/gn: new package (0_git20240402)
    I need this personally and we could also use it for chromium
    n stuff i guess instead of bootstrapping it within the template.

---
+++ contrib/gn/template.py
@@ -0,0 +1,38 @@
+pkgname = "gn"
+pkgver = "0_git20240402"
+pkgrel = 0
+_gitrev = "415b3b19e094cd4b6982147693485df65037f942"
+hostmakedepends = ["ninja", "python"]
+depends = ["ninja"]
+pkgdesc = "Build system that generates ninja"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-3-Clause"
+url = "https://gn.googlesource.com/gn"
+# shamelessly leech off alpine
+source = f"https://ab-sn.lnl.gay/gn-{_gitrev}.tar.zst"
+sha256 = "47419d585f533f7b903d7dc85b66b88a72bc7f8bf788d9e2093a981acc7379a6"
+hardening = ["vis", "cfi"]
+
+
+def do_configure(self):
+    self.do(
+        "python",
+        "./build/gen.py",
+        "--no-last-commit-position",
+        "--no-static-libstdc++",
+        "--no-strip",
+        "--allow-warnings",
+    )
+
+
+def do_build(self):
+    self.do("ninja", f"-j{self.make_jobs}", "-C", "out")
+
+
+def do_check(self):
+    self.do("./out/gn_unittests")
+
+
+def do_install(self):
+    self.install_license("LICENSE")
+    self.install_bin("out/gn")
---
+++ contrib/gn/update.py
@@ -0,0 +1,3 @@
+# lol
+url = "https://gitlab.alpinelinux.org/alpine/aports/-/raw/master/community/gn/APKBUILD"
+pattern = r"pkgver=(.*)"
