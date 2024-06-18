56ae29620a3428e9b7323b44d8f55a6687693bfc
psykose <alice@ayaya.dev>
2024-06-18 23:47:52 +0200

    contrib/inxi: update to 3.3.35.1

--- contrib/inxi/template.py
+++ contrib/inxi/template.py
@@ -1,5 +1,5 @@
 pkgname = "inxi"
-_pver = "3.3.34-1"
+_pver = "3.3.35-1"
 pkgver = f"{_pver.replace('-', '.')}"
 pkgrel = 0
 depends = ["perl"]
@@ -7,8 +7,8 @@ pkgdesc = "Fully featured CLI system information tool"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later"
 url = "https://codeberg.org/smxi/inxi"
-source = f"https://codeberg.org/smxi/inxi/archive/{_pver}.tar.gz"
-sha256 = "7cfc5c0abe10cb59f281733ce1d526583312344007756e7713fd5c51200b80fb"
+source = f"{url}/archive/{_pver}.tar.gz"
+sha256 = "08e43312bc60435d770607c3611f2fa35478ea0f48c60d5d5fd60ab2ee421f2e"
 
 
 def do_install(self):
--- contrib/inxi/update.py
+++ contrib/inxi/update.py
@@ -1,2 +1,6 @@
 url = "https://codeberg.org/smxi/inxi/tags"
 pattern = r">(\d+\.\d+\.\d+\-\d+)<"
+
+
+def fetch_versions(self, src):
+    return map(lambda v: v.replace("-", "."), self.fetch_versions(src))
