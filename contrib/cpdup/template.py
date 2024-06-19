7a7e91b6e1bcafdf4da4702fb02fcb29ef33180c
Renato Botelho do Couto <renato@netgate.com>
2024-06-19 09:55:43 -0500

    contrib/cpdup: new package (1.22)

---
+++ contrib/cpdup/template.py
@@ -0,0 +1,19 @@
+pkgname = "cpdup"
+pkgver = "1.22"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+hostmakedepends = ["pkgconf"]
+makedepends = ["gmake", "openssl-devel"]
+pkgdesc = "Comprehensive filesystem mirroring and backup program"
+maintainer = "Renato Botelho do Couto <renato@netgate.com>"
+license = "BSD-2-Clause"
+url = "https://github.com/DragonFlyBSD/cpdup"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "2dbfa16a080f8dce1be64a511e785b8491e59be8a0f3d1cef035d08147cc4793"
+# Not available
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
