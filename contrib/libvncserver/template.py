00cdbd01a883944439ef46bc20326124a38fbbeb
psykose <alice@ayaya.dev>
2024-06-20 01:58:21 +0200

    contrib/libvncserver: new package (0.9.14)

---
+++ contrib/libvncserver/template.py
@@ -0,0 +1,34 @@
+pkgname = "libvncserver"
+pkgver = "0.9.14"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    "-DWITH_EXAMPLES=OFF",
+    "-DWITH_GCRYPT=OFF",
+    "-DWITH_GNUTLS=OFF",
+    "-DWITH_SYSTEMD=OFF",
+]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "ffmpeg-devel",
+    "libjpeg-turbo-devel",
+    "libpng-devel",
+    "openssl-devel",
+    "lzo-devel",
+    "zlib-devel",
+]
+pkgdesc = "VNC client/server libraries"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later"
+url = "https://github.com/LibVNC/libvncserver"
+source = f"{url}/archive/refs/tags/LibVNCServer-{pkgver}.tar.gz"
+sha256 = "83104e4f7e28b02f8bf6b010d69b626fae591f887e949816305daebae527c9a5"
+
+
+@subpackage("libvncserver-devel")
+def _devel(self):
+    return self.default_devel()
