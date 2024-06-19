9e2f842452c1621679ebd05d393958cf27821e24
miko <mikoxyzzz@gmail.com>
2024-06-19 13:23:15 +0200

    contrib/exfatprogs: update to 1.2.4

--- contrib/exfatprogs/template.py
+++ contrib/exfatprogs/template.py
@@ -1,5 +1,5 @@
 pkgname = "exfatprogs"
-pkgver = "1.2.3"
+pkgver = "1.2.4"
 pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
@@ -15,4 +15,4 @@ maintainer = "Val Packett <val@packett.cool>"
 license = "GPL-2.0-only"
 url = "https://github.com/exfatprogs/exfatprogs"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "a8ecebeb31cc23652d82af0853214ce85dba6021a9e8affb5aa38f47ae0f7975"
+sha256 = "62b48bddd19c49ca0ebef44a836856be1917531717d6c1d600e026fe1af95a78"
