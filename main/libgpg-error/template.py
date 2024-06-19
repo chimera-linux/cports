ff92384ecaa574b97f6aaa5bd6c03f4fa3a8fef9
psykose <alice@ayaya.dev>
2024-06-19 16:58:29 +0200

    main/libgpg-error: update to 1.50

--- main/libgpg-error/template.py
+++ main/libgpg-error/template.py
@@ -1,5 +1,5 @@
 pkgname = "libgpg-error"
-pkgver = "1.49"
+pkgver = "1.50"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gnupg.org"
 source = f"{url}/ftp/gcrypt/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "8b79d54639dbf4abc08b5406fb2f37e669a2dec091dd024fb87dd367131c63a9"
+sha256 = "69405349e0a633e444a28c5b35ce8f14484684518a508dc48a089992fe93e20a"
 
 
 def post_install(self):
