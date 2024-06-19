19c7a2c3336ef2d3aded723c2e4b78c82ec7a066
psykose <alice@ayaya.dev>
2024-06-19 17:16:30 +0200

    main/libxslt: update to 1.1.41

--- main/libxslt/template.py
+++ main/libxslt/template.py
@@ -1,6 +1,6 @@
 pkgname = "libxslt"
-pkgver = "1.1.40"
-pkgrel = 1
+pkgver = "1.1.41"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-python=no"]
 hostmakedepends = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://xmlsoft.org/XSLT"
 source = f"$(GNOME_SITE)/libxslt/{pkgver[:pkgver.rfind('.')]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "194715db023035f65fb566402f2ad2b5eab4c29d541f511305c40b29b1f48d13"
+sha256 = "3ad392af91115b7740f7b50d228cc1c5fc13afc1da7f16cb0213917a37f71bda"
 # test code seemingly incompatible with current libxml2
 options = ["!cross", "!check"]
 
