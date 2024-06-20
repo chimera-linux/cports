12587436bea757728479b6232190021080c57129
psykose <alice@ayaya.dev>
2024-06-21 01:09:55 +0200

    main/python-pip: update to 24.1

--- main/python-pip/template.py
+++ main/python-pip/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pip"
-pkgver = "24.0"
+pkgver = "24.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,11 +14,13 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pip.pypa.io"
 source = f"$(PYPI_SITE)/p/pip/pip-{pkgver}.tar.gz"
-sha256 = "ea9bd1a847e8c5774a5777bb398c19e80bcd4e2aa16a4b301b718fe6f593aba2"
+sha256 = "bdae551038c0ce6a83030b4aedef27fc95f0daa683593fea22fa05e55ed8e317"
 # unpackaged dependencies
 options = ["!check"]
 
 
 def post_install(self):
     self.install_license("LICENSE.txt")
+    # replace shim with symlink
+    self.rm(self.destdir / "usr/bin/pip")
     self.install_link("usr/bin/pip", "pip3")
