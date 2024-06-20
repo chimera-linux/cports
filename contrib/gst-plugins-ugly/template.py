ccefc19bcea6990bd7598b0fd4fc81deaab012b3
psykose <alice@ayaya.dev>
2024-06-20 15:49:37 +0200

    contrib/gst-plugins-ugly: update to 1.24.5

--- contrib/gst-plugins-ugly/template.py
+++ contrib/gst-plugins-ugly/template.py
@@ -1,5 +1,5 @@
 pkgname = "gst-plugins-ugly"
-pkgver = "1.24.4"
+pkgver = "1.24.5"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -33,4 +33,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/gst-plugins-ugly/gst-plugins-ugly-{pkgver}.tar.xz"
-sha256 = "4604f8709c0bc4d6960ef6ae6fd91e0b20af011bfe22e103f5b85377cf3f1ef4"
+sha256 = "333267b6e00770440a4a00402910dd59ed8fd619eaebf402815fbe111da7776d"
