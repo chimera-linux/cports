1853b3787f684afd156b5123bc03d0fb11fd5cdd
Erica Z <zerica@callcc.eu>
2024-06-20 11:26:02 +0200

    contrib/furnace: update to 0.6.4

--- contrib/furnace/template.py
+++ contrib/furnace/template.py
@@ -1,5 +1,5 @@
 pkgname = "furnace"
-pkgver = "0.6.3"
+pkgver = "0.6.4"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -29,14 +29,14 @@ depends = ["zenity"]
 pkgdesc = "Multi-system chiptune tracker"
 maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-2.0-or-later"
-url = "https://github.com/tildearrow/furnace"
+url = "https://tildearrow.org/furnace"
 _adpcm_commit = "ef7a217154badc3b99978ac481b268c8aab67bd8"
 source = [
-    f"{url}/archive/refs/tags/v{pkgver}.tar.gz",
+    f"https://github.com/tildearrow/furnace/archive/refs/tags/v{pkgver}.tar.gz",
     f"https://github.com/superctr/adpcm/archive/{_adpcm_commit}.tar.gz",
 ]
 source_paths = [".", "extern/adpcm"]
 sha256 = [
-    "bc28647c2175fc4e0c831d2d3f814c37828bb525af42897e5337f0e6c4168ffb",
+    "76f4a890196f31b030bd445dcaaa5de81d4ea2fc34538a23323985ff6b3e6c32",
     "46da29342d2968ff222ba00e07c646e038b76af2e6c86de037c653059a056251",
 ]
