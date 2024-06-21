ff612023ac61820e30973778a575f84e275b2cc8
psykose <alice@ayaya.dev>
2024-06-21 14:44:51 +0200

    contrib/tree-sitter-vimdoc: update to 3.0.0

--- contrib/tree-sitter-vimdoc/template.py
+++ contrib/tree-sitter-vimdoc/template.py
@@ -1,6 +1,6 @@
 pkgname = "tree-sitter-vimdoc"
-pkgver = "2.5.1"
-pkgrel = 1
+pkgver = "3.0.0"
+pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
 make_check_target = "test"
@@ -14,13 +14,13 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/neovim/tree-sitter-vimdoc"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "063645096504b21603585507c41c6d8718ff3c11b2150c5bfc31e8f3ee9afea3"
+sha256 = "a639bf92bf57bfa1cdc90ca16af27bfaf26a9779064776dd4be34c1ef1453f6c"
 
 
 def post_install(self):
     self.install_dir("usr/lib/tree-sitter")
     self.install_link(
-        "usr/lib/tree-sitter/vimdoc.so", "../libtree-sitter-vimdoc.so.2"
+        "usr/lib/tree-sitter/vimdoc.so", "../libtree-sitter-vimdoc.so.3"
     )
 
 
