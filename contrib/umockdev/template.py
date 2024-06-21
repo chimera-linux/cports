ff8259d6b31609aa89b84a37f8691909483557eb
triallax <triallax@tutanota.com>
2024-06-21 13:42:16 +0100

    contrib/umockdev: new package (0.18.3)

---
+++ contrib/umockdev/template.py
@@ -0,0 +1,25 @@
+pkgname = "umockdev"
+pkgver = "0.18.3"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "gobject-introspection",
+    "meson",
+    "pkgconf",
+    "python",
+    "vala",
+]
+makedepends = ["glib-devel", "libpcap-devel", "linux-headers", "udev-devel"]
+pkgdesc = "Mock hardware devices"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "LGPL-2.1-or-later"
+url = "https://github.com/martinpitt/umockdev"
+source = f"https://github.com/martinpitt/umockdev/releases/download/{pkgver}/umockdev-{pkgver}.tar.xz"
+sha256 = "aba95c323037c842f1617931260231b8557d119aa2891cbca8b811fcc559294a"
+# TODO
+options = ["!check", "!cross"]
+
+
+@subpackage("umockdev-devel")
+def _devel(self):
+    return self.default_devel()
