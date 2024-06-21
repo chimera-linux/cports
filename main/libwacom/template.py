2541a396987ef7a7ad6cfbc0070f5efd361a51d1
psykose <alice@ayaya.dev>
2024-06-21 15:05:56 +0200

    main/libwacom: update to 2.12.2

--- main/libwacom/template.py
+++ main/libwacom/template.py
@@ -1,5 +1,5 @@
 pkgname = "libwacom"
-pkgver = "2.12.1"
+pkgver = "2.12.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocumentation=disabled", "-Dtests=enabled"]
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/linuxwacom/libwacom"
 source = f"{url}/releases/download/{pkgname}-{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "5b047a28d20b01dd744e194c56c1ba5db54b5d6ee3a8b2d820a1ad6e33d85572"
+sha256 = "c8319c40c70edd05d1839c0d3f449c23bdc90cd1f0d819bd0c1ec7c00b117700"
 
 
 def post_install(self):
