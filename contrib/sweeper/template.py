e1b26b1d6813a2b11c249d388c4dc6438e5dcf4b
psykose <alice@ayaya.dev>
2024-06-22 23:46:09 +0200

    contrib/sweeper: new package (24.05.1)

---
+++ contrib/sweeper/template.py
@@ -0,0 +1,30 @@
+pkgname = "sweeper"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kbookmarks-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "ktextwidgets-devel",
+    "kxmlgui-devel",
+    "plasma-activities-stats-devel",
+    "qt6-qtbase-devel",
+]
+pkgdesc = "KDE cache cleaner"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.1-or-later"
+url = "https://apps.kde.org/sweeper"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/sweeper-{pkgver}.tar.xz"
+sha256 = "5f43feb8a996c2c00d95e5a62f2288aed4931b0c1d4a5f708a3bcc3c6cd70435"
