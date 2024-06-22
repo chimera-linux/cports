8bef6f7b59a30d9b143ef3933a447bf841bb4138
psykose <alice@ayaya.dev>
2024-06-22 02:51:10 +0200

    contrib/python-sip: update to 6.8.4

--- contrib/python-sip/template.py
+++ contrib/python-sip/template.py
@@ -1,23 +1,20 @@
 pkgname = "python-sip"
-pkgver = "6.8.3"
+pkgver = "6.8.4"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
-    "python-setuptools",
+    "python-setuptools_scm",
     "python-wheel",
 ]
-depends = [
-    "python-packaging",
-    "python-setuptools",
-]
+depends = ["python-packaging"]
 pkgdesc = "Tool for making python bindings for C/C++ libraries"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "custom:sip"
-url = "https://www.riverbankcomputing.com/software/sip"
+url = "https://github.com/Python-SIP/sip"
 source = f"$(PYPI_SITE)/s/sip/sip-{pkgver}.tar.gz"
-sha256 = "888547b018bb24c36aded519e93d3e513d4c6aa0ba55b7cc1affbd45cf10762c"
+sha256 = "c8f4032f656de3fedbf81243cdbc9e9fd4064945b8c6961eaa81f03cd88554cb"
 # no tests
 options = ["!check"]
 
