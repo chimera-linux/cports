9d17fa0e30ff8b581b12f306c03a8d7f5e68a750
psykose <alice@ayaya.dev>
2024-06-22 22:28:14 +0200

    contrib/ktrip: new package (24.05.1)

---
+++ contrib/ktrip/template.py
@@ -0,0 +1,27 @@
+pkgname = "ktrip"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kirigami-addons-devel",
+    "kpublictransport-devel",
+    "qqc2-desktop-style-devel",
+    "qt6-qtdeclarative-devel",
+]
+depends = ["kirigami-addons"]
+pkgdesc = "KDE trip planner"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/ktrip"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/ktrip-{pkgver}.tar.xz"
+sha256 = "4fe89426f5a261b2fc33de30adbc0a5405321b13e5e1d644a034aa772ac49b37"
