6cc15ec018d99011d0d4fa83847d10a7949e190c
psykose <alice@ayaya.dev>
2024-06-22 05:12:09 +0200

    contrib/kddockwidgets: new package (2.1.0)

---
+++ contrib/kddockwidgets/template.py
@@ -0,0 +1,29 @@
+pkgname = "kddockwidgets"
+pkgver = "2.1.0"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    "-DKDDockWidgets_QT6=ON",
+    "-DKDDockWidgets_EXAMPLES=OFF",
+]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "nlohmann-json",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "Dock Widget Framework for Qt"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-only OR GPL-3.0-only"
+url = "https://www.kdab.com/development-resources/qt-tools/kddockwidgets"
+source = f"https://github.com/KDAB/KDDockWidgets/releases/download/v{pkgver}/kddockwidgets-{pkgver}.tar.gz"
+sha256 = "cf3242b8fde8988b2661366b6a9597bcb67164074c4f31d03ec2999b475a25d7"
+
+
+@subpackage("kddockwidgets-devel")
+def _devel(self):
+    return self.default_devel()
