e6cf307af7d1b029469604ec02d27a2d18bca18c
psykose <alice@ayaya.dev>
2024-06-23 01:32:00 +0200

    contrib/accessibility-inspector: new package (24.05.1)

---
+++ contrib/accessibility-inspector/template.py
@@ -0,0 +1,29 @@
+pkgname = "accessibility-inspector"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "ki18n-devel",
+    "kxmlgui-devel",
+    "libqaccessibilityclient-devel",
+    "qt6-qtbase-devel",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE application accessibility tree inspector"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later"
+url = "https://apps.kde.org/accessibilityinspector"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/accessibility-inspector-{pkgver}.tar.xz"
+sha256 = "099ab34616d4ceff0fbf83671a39068e4e7f53ec1992519e0fd3c3e7b61a9176"
