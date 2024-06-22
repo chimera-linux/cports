9bcb66fb2af0dedc0fc5579fb20a03a552818443
psykose <alice@ayaya.dev>
2024-06-22 02:54:13 +0200

    main/python-cairo: update to 1.26.1

--- main/python-cairo/template.py
+++ main/python-cairo/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-cairo"
-pkgver = "1.26.0"
+pkgver = "1.26.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later OR MPL-1.1"
 url = "https://pycairo.readthedocs.io"
 source = f"https://github.com/pygobject/pycairo/releases/download/v{pkgver}/pycairo-{pkgver}.tar.gz"
-sha256 = "2dddd0a874fbddb21e14acd9b955881ee1dc6e63b9c549a192d613a907f9cbeb"
+sha256 = "a11b999ce55b798dbf13516ab038e0ce8b6ec299b208d7c4e767a6f7e68e8430"
 
 
 @subpackage("python-cairo-devel")
