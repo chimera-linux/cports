a17f48ab6ef349335230fae63c15d1ece29afac4
psykose <alice@ayaya.dev>
2024-06-22 23:31:31 +0200

    contrib/ktorrent: new package (24.05.1)

---
+++ contrib/ktorrent/template.py
@@ -0,0 +1,55 @@
+pkgname = "ktorrent"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "boost-devel",
+    "kcmutils-devel",
+    "kcompletion-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdnssd-devel",
+    "kdoctools-devel",
+    "kglobalaccel-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "kitemviews-devel",
+    "knotifications-devel",
+    "knotifyconfig-devel",
+    "kparts-devel",
+    "kplotting-devel",
+    "kstatusnotifieritem-devel",
+    "ktextwidgets-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "kxmlgui-devel",
+    "libktorrent-devel",
+    "phonon-devel",
+    "qt6-qt5compat-devel",
+    "qt6-qtbase-devel",
+    "solid-devel",
+    "syndication-devel",
+    "taglib-devel",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE torrent client"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/ktorrent"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/ktorrent-{pkgver}.tar.xz"
+sha256 = "c0d2c8bc185d4f335320575d4de4c07597600db8d8d9e4fd38f83522b79217b9"
+
+if self.profile().arch in ["aarch64", "ppc64le", "x86_64"]:
+    makedepends += ["qt6-qtwebengine-devel"]
