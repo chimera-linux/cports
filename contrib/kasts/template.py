a7c6354d475506b9aa986f3977ec97170f2e07c9
psykose <alice@ayaya.dev>
2024-06-23 01:15:41 +0200

    contrib/kasts: new package (24.05.1)

---
+++ contrib/kasts/template.py
@@ -0,0 +1,32 @@
+pkgname = "kasts"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "breeze-icons-devel",
+    "kcolorscheme-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kirigami-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtmultimedia-devel",
+    "qt6-qtsvg-devel",
+    "qtkeychain-devel",
+    "syndication-devel",
+    "taglib-devel",
+    "threadweaver-devel",
+]
+pkgdesc = "KDE convergent podcast player"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/kasts"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kasts-{pkgver}.tar.xz"
+sha256 = "6029595e573300e83f82acbf782166de989687db1a879a894f7d5b705955aa18"
