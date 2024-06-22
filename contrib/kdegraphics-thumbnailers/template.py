b5792273fe18b0e9f5fded4c14715c4154848853
psykose <alice@ayaya.dev>
2024-06-22 22:45:38 +0200

    contrib/kdegraphics-thumbnailers: new package (24.05.1)

---
+++ contrib/kdegraphics-thumbnailers/template.py
@@ -0,0 +1,26 @@
+pkgname = "kdegraphics-thumbnailers"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DQT_MAJOR_VERSION=6"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "karchive-devel",
+    "kdegraphics-mobipocket-devel",
+    "kio-devel",
+    "libkdcraw-devel",
+    "libkexiv2-devel",
+    "qt6-qtbase-devel",
+]
+pkgdesc = "KDE thumbnailers for PostScript/RAW/MobiPocket/Blender"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/kdegraphics_thumbnailers"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kdegraphics-thumbnailers-{pkgver}.tar.xz"
+sha256 = "7cd69cfc2fab59b5c61421c6648b3689aef1059c9df1dc546011d62f7c5ccbc0"
