386b362c6504fb248d013bc378a2415e6fe2f401
psykose <alice@ayaya.dev>
2024-06-22 22:54:18 +0200

    contrib/kdenetwork-filesharing: new package (24.05.1)

---
+++ contrib/kdenetwork-filesharing/template.py
@@ -0,0 +1,29 @@
+pkgname = "kdenetwork-filesharing"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kauth-devel",
+    "kcompletion-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kwidgetsaddons-devel",
+    "qcoro-devel",
+    "qt6-qtdeclarative-devel",
+]
+# net, testparm
+depends = ["samba-common"]
+pkgdesc = "KDE samba filesharing plugin"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/kdenetwork_filesharing"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kdenetwork-filesharing-{pkgver}.tar.xz"
+sha256 = "6eefa96c1467ac990051acd158f297b84dc902cfe195ddd8f60afbf504fc0035"
