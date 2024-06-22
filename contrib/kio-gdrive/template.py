901c59982f8a57fc1027fd9d2da96b36be677e87
psykose <alice@ayaya.dev>
2024-06-22 21:56:30 +0200

    contrib/kio-gdrive: new package (24.05.1)

---
+++ contrib/kio-gdrive/template.py
@@ -0,0 +1,29 @@
+pkgname = "kio-gdrive"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DQT_MAJOR_VERSION=6"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kaccounts-integration-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "knotifications-devel",
+    "libkgapi-devel",
+    "purpose-devel",
+    "qt6-qtbase-devel",
+    "qtkeychain-devel",
+]
+pkgdesc = "KDE KIO plugin for Google Drive"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/kio_gdrive"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kio-gdrive-{pkgver}.tar.xz"
+sha256 = "aacab6e19e84a8a60c7bd1d917fe9687b891e35f130373db31b58203ec6f8b25"
