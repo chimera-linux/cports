39750b1c945fe0b20e0cc7292552f6d014132e34
psykose <alice@ayaya.dev>
2024-06-22 22:18:18 +0200

    contrib/kpublictransport: new package (24.05.1)

---
+++ contrib/kpublictransport/template.py
@@ -0,0 +1,32 @@
+pkgname = "kpublictransport"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "ki18n-devel",
+    "networkmanager-qt-devel",
+    "protobuf-devel",
+    "qt6-qtdeclarative-devel",
+    "zlib-devel",
+]
+pkgdesc = "KDE library for accessing public transport information"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later"
+url = "https://api.kde.org/kdepim/kpublictransport/html"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/kpublictransport-{pkgver}.tar.xz"
+)
+sha256 = "4707090df6a5a7bf342b484dbc07bcf627e60f8f67a0d8009cb2df695a91a1b6"
+
+
+@subpackage("kpublictransport-devel")
+def _devel(self):
+    self.depends += ["qt6-qtbase-devel", "zlib-devel"]
+    return self.default_devel()
