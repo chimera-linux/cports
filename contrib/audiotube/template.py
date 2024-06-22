712e7103338e2e7f29e86e8843e165fc219a15b7
psykose <alice@ayaya.dev>
2024-06-23 00:16:43 +0200

    contrib/audiotube: new package (24.05.1)

---
+++ contrib/audiotube/template.py
@@ -0,0 +1,40 @@
+pkgname = "audiotube"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+_deps = [
+    "kirigami-addons",
+    "python-ytmusicapi",
+    "yt-dlp",
+]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+] + _deps
+makedepends = [
+    "futuresql-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "ki18n-devel",
+    "kirigami-addons-devel",
+    "kirigami-devel",
+    "kwindowsystem-devel",
+    "python-devel",
+    "python-pybind11-devel",
+    "qcoro-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtmultimedia-devel",
+    "qt6-qtsvg-devel",
+]
+depends = list(_deps)
+pkgdesc = "KDE Youtube Music player"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/audiotube"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/audiotube-{pkgver}.tar.xz"
+sha256 = "6ea7b8a5bf52ce99b1b3da983ab92cfa7222d2d1cc9f1dd343ce0d03c7eccf6a"
+# only test needs net
+options = ["!check"]
