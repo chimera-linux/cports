6d258d82472aea2a1bb580a2f3c2a38eee24d105
psykose <alice@ayaya.dev>
2024-06-23 00:01:50 +0200

    contrib/audiocd-kio: new package (24.05.1)

---
+++ contrib/audiocd-kio/template.py
@@ -0,0 +1,42 @@
+pkgname = "audiocd-kio"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "cdparanoia-devel",
+    "flac-devel",
+    "kconfig-devel",
+    "ki18n-devel",
+    "kdoctools-devel",
+    "kio-devel",
+    "kcmutils-devel",
+    "libkcddb-devel",
+    "libkcompactdisc-devel",
+    "libvorbis-devel",
+    "qt6-qtbase-devel",
+]
+# mp3 encoding
+depends = ["lame"]
+pkgdesc = "KDE bookmarks editor"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/kio_audiocd"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/audiocd-kio-{pkgver}.tar.xz"
+sha256 = "c094b7b7e8840c10ac31005188bcf6f31e44b932bd30765ae4d429676c2e9ae8"
+
+
+@subpackage("audiocd-kio-devel")
+def _devel(self):
+    self.depends += [
+        "kio-devel",
+        "libkcddb-devel",
+        "qt6-qtbase-devel",
+    ]
+    return self.default_devel()
