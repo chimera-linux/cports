173913f9f216ee59a126d551286082dda5d97fce
psykose <alice@ayaya.dev>
2024-06-23 01:26:06 +0200

    contrib/juk: new package (24.05.1)

---
+++ contrib/juk/template.py
@@ -0,0 +1,42 @@
+pkgname = "juk"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcompletion-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kglobalaccel-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "kjobwidgets-devel",
+    "knotifications-devel",
+    "kstatusnotifieritem-devel",
+    "ktextwidgets-devel",
+    "kwallet-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "kxmlgui-devel",
+    "phonon-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtsvg-devel",
+    "taglib-devel",
+]
+# TODO: can't play anything without a phonon backend (vlc)
+pkgdesc = "KDE music player and manager"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-only"
+url = "https://juk.kde.org"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/juk-{pkgver}.tar.xz"
+sha256 = "9da89d500fdce7e6ed377d0efa2983af5eaf87a8cbdfce29ac81833e61621bb8"
