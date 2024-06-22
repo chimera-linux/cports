46a155dc8ae10ba32ac928b58cc59f9244fcadd4
psykose <alice@ayaya.dev>
2024-06-22 23:10:06 +0200

    contrib/kget: new package (24.05.1)

---
+++ contrib/kget/template.py
@@ -0,0 +1,46 @@
+pkgname = "kget"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "boost-devel",
+    "gpgme-qt-devel",
+    "kcmutils-devel",
+    "kcompletion-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "kitemviews-devel",
+    "knotifications-devel",
+    "knotifyconfig-devel",
+    "kstatusnotifieritem-devel",
+    "kwallet-devel",
+    "kwidgetsaddons-devel",
+    "kwindowsystem-devel",
+    "kxmlgui-devel",
+    "libktorrent-devel",
+    "libmms-devel",
+    "qt6-qtbase-devel",
+    "sqlite-devel",
+]
+checkdepends = ["xwayland-run"]
+pkgdesc = "KDE download manager"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://apps.kde.org/kget"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/kget-{pkgver}.tar.xz"
+sha256 = "bf55cae5502c5551ddbac1a0389f568e9888310df15ffc4c8604c66d2cc7bf80"
