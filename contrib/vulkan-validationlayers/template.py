21c00d578595ae0af289b8f76bc833f9cb0c13bb
psykose <alice@ayaya.dev>
2024-06-23 20:44:18 +0200

    contrib/vulkan-validationlayers: new package (1.3.288)
    this was removed in 863c0b8298e18225aafba10f5870cced8bd03bb6
    but just builds these days and back then the vulkan* things
    weren't versioned with matching versions

---
+++ contrib/vulkan-validationlayers/template.py
@@ -0,0 +1,30 @@
+pkgname = "vulkan-validationlayers"
+pkgver = "1.3.288"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    "-DCMAKE_BUILD_TYPE=Release",
+    "-DBUILD_WERROR=OFF",
+    "-DUSE_ROBIN_HOOD_HASHING=OFF",
+]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "libx11-devel",
+    "libxcb-devel",
+    "libxrandr-devel",
+    "spirv-headers",
+    "spirv-tools-devel",
+    "vulkan-headers",
+    "vulkan-utility-libraries",
+    "wayland-devel",
+]
+pkgdesc = "Validation layers to catch Vulkan issues"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Apache-2.0"
+url = "https://vulkan.lunarg.com/doc/sdk/latest/linux/khronos_validation_layer.html"
+source = f"https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "7703075c39fec5d8e78de20db8a4fc578846c96a9017a60770dab06eececfff2"
