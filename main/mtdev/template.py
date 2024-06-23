5ded578c443dfc4bc43636d433c69f5c4f38aabd
psykose <alice@ayaya.dev>
2024-06-23 07:04:24 +0200

    main/mtdev: update to 1.1.7

--- main/mtdev/template.py
+++ main/mtdev/template.py
@@ -1,15 +1,15 @@
 pkgname = "mtdev"
-pkgver = "1.1.6"
+pkgver = "1.1.7"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 makedepends = ["linux-headers"]
 pkgdesc = "Multitouch Protocol Translation Library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
-url = "http://bitmath.org/code/mtdev"
-source = f"{url}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "15d7b28da8ac71d8bc8c9287c2045fd174267bc740bec10cfda332dc1204e0e0"
+url = "https://bitmath.org/code/mtdev"
+source = f"{url}/mtdev-{pkgver}.tar.bz2"
+sha256 = "a107adad2101fecac54ac7f9f0e0a0dd155d954193da55c2340c97f2ff1d814e"
 
 
 def post_install(self):
@@ -20,6 +20,3 @@ def post_install(self):
 def _devel(self):
     self.depends += ["linux-headers"]
     return self.default_devel()
-
-
-configure_gen = []
