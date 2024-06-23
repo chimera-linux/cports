315c509ec75474f2200c4f843ce0b89d42ef4539
ttyyls <contact@behri.org>
2024-06-23 02:13:43 +0100

    contrib/copyq: update to 9.0.0

--- contrib/copyq/template.py
+++ contrib/copyq/template.py
@@ -1,5 +1,5 @@
 pkgname = "copyq"
-pkgver = "8.0.0"
+pkgver = "9.0.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -27,7 +27,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "GPL-3.0-or-later"
 url = "https://hluk.github.io/CopyQ"
 source = f"https://github.com/hluk/CopyQ/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "4883538182df81d1c88497d3f2b7b0d9d1f59a0381654869c45dccfc78daf9aa"
+sha256 = "6f8cce371cda8edb87e2a8d2f9de27022720b7b8fae51f67ee7d69b73526f432"
 # FIXME: loading plugins fail with cfi enabled
 hardening = ["vis", "!cfi"]
 # requires building project a second time in debug mode
