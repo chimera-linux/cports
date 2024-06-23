e0466da9476d8cd8e4e84f8dd14e35c376da9f28
psykose <alice@ayaya.dev>
2024-06-23 08:01:15 +0200

    contrib/cereal: new package (1.3.2)

---
+++ contrib/cereal/template.py
@@ -0,0 +1,26 @@
+pkgname = "cereal"
+pkgver = "1.3.2"
+pkgrel = 0
+build_style = "cmake"
+# -m32 unsupported + broken in fortify-headers
+configure_args = [
+    "-DSKIP_PORTABILITY_TEST=ON",
+    "-DBUILD_DOC=OFF",
+    "-DBUILD_SANDBOX=OFF",
+    "-DSKIP_PERFORMANCE_COMPARISON=ON",
+    "-DWITH_WERROR=OFF",
+]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+]
+pkgdesc = "C++ library for serialization"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-3-Clause"
+url = "https://github.com/USCiLab/cereal"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "16a7ad9b31ba5880dac55d62b5d6f243c3ebc8d46a3514149e56b5e7ea81f85f"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
