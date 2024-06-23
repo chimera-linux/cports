f7591aa7674d2e64957bafd50fac25900f4ccb2c
psykose <alice@ayaya.dev>
2024-06-23 03:33:33 +0200

    contrib/itinerary: new package (24.05.1)

---
+++ contrib/itinerary/template.py
@@ -0,0 +1,59 @@
+pkgname = "itinerary"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+# fails with no output
+make_check_args = ["-E", "itinerary-self-test"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcalendarcore-devel",
+    "kcontacts-devel",
+    "kcoreaddons-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kfilemetadata-devel",
+    "khealthcertificate-devel",
+    "kholidays-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "kirigami-addons-devel",
+    "kitemmodels-devel",
+    "kitinerary-devel",
+    "knotifications-devel",
+    "kopeninghours-devel",
+    "kosmindoormap-devel",
+    "kpkpass-devel",
+    "kpublictransport-devel",
+    "kunitconversion-devel",
+    "kwindowsystem-devel",
+    "libical-devel",
+    "libquotient-devel",
+    "networkmanager-qt-devel",
+    "prison-devel",
+    "qqc2-desktop-style-devel",
+    "qt6-qtdeclarative-devel",
+    "qt6-qtlocation-devel",
+    "qt6-qtmultimedia-devel",
+    "qt6-qtpositioning-devel",
+    "solid-devel",
+    "zlib-devel",
+]
+depends = [
+    "kirigami-addons",
+    "kitemmodels",
+    "kopeninghours",
+    "prison",
+    "qt6-qtlocation",
+]
+pkgdesc = "KDE digital travel assistant"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later"
+url = "https://apps.kde.org/itinerary"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/itinerary-{pkgver}.tar.xz"
+sha256 = "24043b36fc4babe2482b1c77b134c47220bb5f8e46ab247a80b84a311b7aad11"
