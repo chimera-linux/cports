b3ef9358aac2863bef28d97951d59f10ea185feb
psykose <alice@ayaya.dev>
2024-06-23 22:35:16 +0200

    contrib/doctest: new package (2.4.11)

---
+++ contrib/doctest/template.py
@@ -0,0 +1,19 @@
+pkgname = "doctest"
+pkgver = "2.4.11"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DDOCTEST_USE_STD_HEADERS=ON"]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+]
+pkgdesc = "Single-header testing framework"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/doctest/doctest"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "632ed2c05a7f53fa961381497bf8069093f0d6628c5f26286161fbd32a560186"
+
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
