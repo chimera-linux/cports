b169d2209c03cdc9915a4cb2885b93b9216aaf3f
psykose <alice@ayaya.dev>
2024-06-23 03:36:28 +0200

    main/python-hatchling: update to 1.25.0

--- main/python-hatchling/template.py
+++ main/python-hatchling/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-hatchling"
-pkgver = "1.24.2"
+pkgver = "1.25.0"
 pkgrel = 0
 build_style = "python_pep517"
 _deps = [
@@ -21,7 +21,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://hatch.pypa.io/latest"
 source = f"$(PYPI_SITE)/h/hatchling/hatchling-{pkgver}.tar.gz"
-sha256 = "41ddc27cdb25db9ef7b68bef075f829c84cb349aa1bff8240797d012510547b0"
+sha256 = "7064631a512610b52250a4d3ff1bd81551d6d1431c4eb7b72e734df6c74f4262"
 # no tests?
 options = ["!check"]
 
