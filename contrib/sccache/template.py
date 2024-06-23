8284a8a2e1624ee1a45db74b00af9de5545be447
Erica Z <zerica@callcc.eu>
2024-06-23 22:28:12 +0200

    contrib/sccache: enable distributed server

--- contrib/sccache/template.py
+++ contrib/sccache/template.py
@@ -1,6 +1,6 @@
 pkgname = "sccache"
 pkgver = "0.8.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "cargo"
 hostmakedepends = [
     "cargo-auditable",
@@ -18,3 +18,18 @@ source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "30b951b49246d5ca7d614e5712215cb5f39509d6f899641f511fb19036b5c4e5"
 # fails due to comparing ldd output to a glibc bin
 options = ["!check"]
+
+
+# sccache-dist is only supported on x86_64 Linux machines and on FreeBSD
+if self.profile().arch == "x86_64":
+    make_build_args = [
+        "--no-default-features",
+        "--features=all,dist-server",
+    ]
+    make_install_args = make_build_args
+
+
+@subpackage("sccache-dist", self.profile().arch == "x86_64")
+def _dist(self):
+    self.pkgdesc = f"{pkgdesc} (distributed server)"
+    return ["usr/bin/sccache-dist"]
