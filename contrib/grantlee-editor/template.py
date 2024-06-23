fb36b71f1494af3f3ac27d755b271b94937838e2
psykose <alice@ayaya.dev>
2024-06-23 02:26:50 +0200

    contrib/grantlee-editor: new package (24.05.1)

---
+++ contrib/grantlee-editor/template.py
@@ -0,0 +1,37 @@
+pkgname = "grantlee-editor"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "akonadi-contacts-devel",
+    "grantleetheme-devel",
+    "karchive-devel",
+    "kcrash-devel",
+    "kdbusaddons-devel",
+    "kdoctools-devel",
+    "kimap-devel",
+    "kio-devel",
+    "kmime-devel",
+    "ktextaddons-devel",
+    "kxmlgui-devel",
+    "messagelib-devel",
+    "pimcommon-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtwebengine-devel",
+    "syntax-highlighting-devel",
+]
+pkgdesc = "KDE editor for Grantlee themes"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://invent.kde.org/pim/grantlee-editor"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/grantlee-editor-{pkgver}.tar.xz"
+)
+sha256 = "38626e0550e0815aaf27002e0dd3edd1bec4cadd138cc17a24ebed42a4c1a22d"
