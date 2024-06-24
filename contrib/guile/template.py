f20e7e3c7aad2de80db858eec3e924c410969b3b
psykose <alice@ayaya.dev>
2024-06-24 04:53:17 +0200

    contrib/guile: new package (3.0.10)

---
+++ contrib/guile/template.py
@@ -0,0 +1,39 @@
+pkgname = "guile"
+pkgver = "3.0.10"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--disable-error-on-warning",
+    "--disable-static",
+]
+make_cmd = "gmake"
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gmake",
+    "gperf",
+    "libtool",
+    "pkgconf",
+]
+makedepends = [
+    "gc-devel",
+    "gmp-devel",
+    "libffi-devel",
+    "libunistring-devel",
+    "ncurses-devel",
+    "readline-devel",
+]
+pkgdesc = "GNU Scheme implementation"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-3.0-or-later AND GPL-3.0-or-later"
+url = "https://www.gnu.org/software/guile"
+source = f"$(GNU_SITE)/guile/guile-{pkgver}.tar.gz"
+sha256 = "2dbdbc97598b2faf31013564efb48e4fed44131d28e996c26abe8a5b23b56c2a"
+# broken af
+options = ["!lto"]
+
+
+@subpackage("guile-devel")
+def _devel(self):
+    self.depends += ["gc-devel", "gmp-devel"]
+    return self.default_devel()
