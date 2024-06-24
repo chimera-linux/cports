9b5e1ea5408e3135e1f7b63f537807407f6a5bc4
psykose <alice@ayaya.dev>
2024-06-24 09:45:57 +0200

    main/otf2bdf: new package (3.1_p1)

---
+++ main/otf2bdf/template.py
@@ -0,0 +1,20 @@
+pkgname = "otf2bdf"
+pkgver = "3.1_p1"
+pkgrel = 0
+build_style = "gnu_configure"
+make_dir = "."
+hostmakedepends = [
+    "automake",
+    "pkgconf",
+]
+makedepends = ["freetype-devel"]
+pkgdesc = "OpenType to BDF font converter"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+# original site is gone
+url = "https://github.com/jirutka/otf2bdf"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "deb1590c249edf11dda1c7136759b59207ea0ac1c737e1c2d68dedf87c51716e"
+# no separate licence file
+# no tests
+options = ["!distlicense", "!check"]
---
+++ main/otf2bdf/update.py
@@ -0,0 +1,1 @@
+pattern = r"refs/tags/v([\d._p]+).tar.gz"
