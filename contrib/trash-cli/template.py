242328673379780ec2af66df52ab570b10a6cdb8
miko <mikoxyzzz@gmail.com>
2024-06-24 23:29:58 +0200

    contrib/trash-cli: enable tests

--- contrib/trash-cli/template.py
+++ contrib/trash-cli/template.py
@@ -2,6 +2,12 @@ pkgname = "trash-cli"
 pkgver = "0.24.5.26"
 pkgrel = 0
 build_style = "python_pep517"
+# FIXME: fails with {'command output': "trash-put: 'link-to-dir/' trashed in ~/tmpsjbhqli4_trash_cli_test/trash-dir"} != {'command output': "trash-put: 'link-to-dir/' trashed in /trash-dir"}
+make_check_args = [
+    "-k",
+    "not test_link_to_dir_without_slashes "
+    "and not test_link_to_dir_with_slashes",
+]
 hostmakedepends = [
     "python-build",
     "python-installer",
@@ -9,14 +15,18 @@ hostmakedepends = [
     "python-wheel",
 ]
 depends = ["python-psutil", "python-six"]
+checkdepends = [
+    "python-flexmock",
+    "python-mock",
+    "python-parameterized",
+    "python-pytest",
+] + depends
 pkgdesc = "Command line interface to the freedesktop.org trashcan"
 maintainer = "Hugo Machet <mail@hmachet.com>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/andreafrancia/trash-cli"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "1d7dec1ad8f0264ceb1b0211d25fffee99c9409cd2e1d36dcc82ac5540f39ce5"
-# unpackaged checkdepends
-options = ["!check"]
 
 
 def post_install(self):
