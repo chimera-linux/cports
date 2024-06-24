80b566dc5f1c8e26f719f3fb93553df05be8ba29
Erica Z <zerica@callcc.eu>
2024-06-24 13:34:28 +0200

    contrib/fennel: update to 1.5.0

--- contrib/fennel/template.py
+++ contrib/fennel/template.py
@@ -1,5 +1,5 @@
 pkgname = "fennel"
-pkgver = "1.4.2"
+pkgver = "1.5.0"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -11,7 +11,7 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "MIT"
 url = "https://fennel-lang.org"
 source = f"https://git.sr.ht/~technomancy/fennel/archive/{pkgver}.tar.gz"
-sha256 = "d507e6a99d856c73ddef117ec8dddcb614d42a424b0dbb8f84a4487b63ecac4c"
+sha256 = "96c8cb1ed78597dd3c58b969dda7273fda30bb707b337394fe806d285883b3c3"
 
 
 def post_install(self):
