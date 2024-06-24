4c6d6c3dcadcb62fea1202f9ae7c6b9d94824eec
psykose <alice@ayaya.dev>
2024-06-24 09:58:53 +0200

    main/console-setup: update to 1.228

--- main/console-setup/template.py
+++ main/console-setup/template.py
@@ -1,16 +1,19 @@
 pkgname = "console-setup"
-pkgver = "1.226"
+pkgver = "1.228"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
 make_build_target = "build-linux"
 make_install_target = "install-linux"
 hostmakedepends = [
-    "gmake",
-    "perl",
     "bdfresize",
-    "perl-xml-parser",
     "font-unifont-bdf",
+    "fonts-dejavu-otf",
+    "gmake",
+    "mandoc",
+    "otf2bdf",
+    "perl",
+    "perl-xml-parser",
 ]
 depends = ["kbd"]
 pkgdesc = "Console font and keymap setup program"
@@ -18,7 +21,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND custom:console-setup"
 url = "https://salsa.debian.org/installer-team/console-setup"
 source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "35022ada1f810a9729e08539bb361f13dbde449b83878776e67237c239ea6022"
+sha256 = "98f5511fe0882a55fcc07378e872c949ee25a4717c82310c1691f46470765bb0"
 # no tests
 options = ["bootstrap", "!check"]
 
