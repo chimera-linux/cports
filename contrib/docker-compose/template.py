60e9dd96fc27355c6af276b3c14eb6170c2da9f7
psykose <alice@ayaya.dev>
2024-06-24 16:12:43 +0200

    contrib/docker-compose: update to 2.28.1

--- contrib/docker-compose/template.py
+++ contrib/docker-compose/template.py
@@ -1,5 +1,5 @@
 pkgname = "docker-compose"
-pkgver = "2.28.0"
+pkgver = "2.28.1"
 pkgrel = 0
 build_style = "go"
 make_build_args = [
@@ -14,7 +14,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://docs.docker.com/compose"
 source = f"https://github.com/docker/compose/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "752a6c95d077126d6c459150f1db8111d2b7d01fa7495667dced07d8e3e8ef31"
+sha256 = "856f1b509ef7190fedadec369e290bfb08c2fafb4f858b80a27caf350554fb50"
 # need a running docker daemon
 options = ["!debug", "!check"]
 
