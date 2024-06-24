429d78fa18f826f090591862e0fba30f40c05c83
psykose <alice@ayaya.dev>
2024-06-24 21:33:09 +0200

    main/sqlite: add stat4 and raise the var/recurse limit
    stat4 should make it more optimised in general, and huge databases with
    a some software run into the latter, and other distributors all raise
    the vars now too

--- main/sqlite/template.py
+++ main/sqlite/template.py
@@ -1,12 +1,14 @@
 pkgname = "sqlite"
 pkgver = "3.46.0"
 _amalg = "3460000"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = [
-    "--enable-threadsafe",
     "--enable-dynamic-extensions",
+    "--enable-fts3",
+    "--enable-fts4",
     "--enable-fts5",
+    "--enable-threadsafe",
 ]
 configure_gen = []
 hostmakedepends = ["pkgconf"]
@@ -20,17 +22,18 @@ sha256 = "6f8e6a7b335273748816f9b3b62bbdc372a889de8782d7f048c653a447417a7d"
 options = ["!parallel"]
 
 _cflags = [
-    "-DSQLITE_ENABLE_DBSTAT_VTAB",
-    "-DSQLITE_ENABLE_COLUMN_METADATA",
-    "-DSQLITE_ENABLE_UNLOCK_NOTIFY",
-    "-DSQLITE_SECURE_DELETE",
-    "-DSQLITE_ENABLE_FTS3",
-    "-DSQLITE_ENABLE_FTS3_PARENTHESIS",
-    "-DSQLITE_ENABLE_FTS4",
-    "-DSQLITE_ENABLE_FTS3_TOKENIZER=1",
-    "-DSQLITE_ENABLE_BATCH_ATOMIC_WRITE=1",
-    "-DSQLITE_ENABLE_DESERIALIZE",
     "-DHAVE_FDATASYNC",
+    "-DSQLITE_ENABLE_BATCH_ATOMIC_WRITE=1",
+    "-DSQLITE_ENABLE_COLUMN_METADATA",
+    "-DSQLITE_ENABLE_DBSTAT_VTAB",
+    "-DSQLITE_ENABLE_DESERIALIZE",
+    "-DSQLITE_ENABLE_FTS3_PARENTHESIS",
+    "-DSQLITE_ENABLE_FTS3_TOKENIZER=1",
+    "-DSQLITE_ENABLE_STAT4=1",
+    "-DSQLITE_ENABLE_UNLOCK_NOTIFY",
+    "-DSQLITE_MAX_EXPR_DEPTH=5000",
+    "-DSQLITE_MAX_VARIABLE_NUMBER=250000",
+    "-DSQLITE_SECURE_DELETE",
 ]
 
 if self.profile().endian == "big":
