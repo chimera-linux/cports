8920c07fc49dfac83ba691c18fc3cc65eeced68a
psykose <alice@ayaya.dev>
2024-06-24 11:47:50 +0200

    main/findutils: update to 4.10.0

--- main/findutils/template.py
+++ main/findutils/template.py
@@ -1,19 +1,19 @@
 pkgname = "findutils"
-pkgver = "4.9.0"
+pkgver = "4.10.0"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_args = [
-    "--program-prefix=g",
-    "ac_cv_lib_error_at_line=no",
-    "ac_cv_header_sys_cdefs_h=no",
+configure_args = ["--program-prefix=g"]
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "texinfo",
 ]
-hostmakedepends = ["texinfo"]
 pkgdesc = "GNU find utilities"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/findutils"
 source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "a2bfb8c09d436770edc59f50fa483e785b161a3b7b9d547573cb08065fd462fe"
+sha256 = "1387e0b67ff247d2abde998f90dfbf70c1491391a59ddfecb8ae698789f0a4f5"
 hardening = ["vis", "cfi"]
 
 
@@ -25,6 +25,3 @@ def post_install(self):
     self.rm(self.destdir / "usr/share/man/man1/glocate.1")
     self.rm(self.destdir / "usr/share/man/man1/gupdatedb.1")
     self.rm(self.destdir / "usr/share/man/man5", recursive=True)
-
-
-configure_gen = []
