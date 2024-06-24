eb76ab2ee98584d25535880d997ffb968183d660
triallax <triallax@tutanota.com>
2024-06-24 10:50:50 +0100

    contrib/seahorse: build with openldap and manpage
    also remove unneeded dep

--- contrib/seahorse/template.py
+++ contrib/seahorse/template.py
@@ -1,14 +1,14 @@
 pkgname = "seahorse"
 pkgver = "43.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
-configure_args = ["-Dldap-support=false"]
+configure_args = ["-Dmanpage=true"]
 hostmakedepends = [
     "desktop-file-utils",
+    "docbook-xsl-nons",
     "gcr3-devel",
     "gettext",
     "gnupg",
-    "gobject-introspection",
     "itstool",
     "libhandy-devel",
     "libsecret-devel",
@@ -16,6 +16,7 @@ hostmakedepends = [
     "openssh",
     "pkgconf",
     "vala",
+    "xsltproc",
 ]
 makedepends = [
     "avahi-devel",
@@ -28,10 +29,11 @@ makedepends = [
     "libpwquality-devel",
     "libsecret-devel",
     "libsoup-devel",
+    "openldap-devel",
 ]
 pkgdesc = "Password and encryption key manager for GNOME"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/seahorse"
-source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
+source = f"{url}/-/archive/{pkgver}/seahorse-{pkgver}.tar.gz"
 sha256 = "0a2512e9e8fd3e271177df7f1e01f8c6e7bce8867b4d258148360105721af108"
