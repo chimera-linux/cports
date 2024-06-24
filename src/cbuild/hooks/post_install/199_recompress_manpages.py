cc738fe54016c0adee20d5ebaf88ee50a4f2d3d8
q66 <q66@chimera-linux.org>
2024-06-25 00:15:18 +0200

    cbuild: compress manpages
    This is a pass right before the autosplit logic but after
    everything else, which compresses manpages (which are guaranteed
    not to be compressed as they are extracted in an earlier hook)
    and retargets any potential symlinks.
    
    Fixes https://github.com/chimera-linux/cports/issues/304

---
+++ src/cbuild/hooks/post_install/199_recompress_manpages.py
@@ -0,0 +1,41 @@
+from cbuild.core import chroot
+
+
+def invoke(pkg):
+    # don't run for early stages (stage0 can't at all because no
+    # chroot, stage1 would just waste time on this pointlessly
+    if pkg.stage < 2:
+        return
+    # otherwise go over everything; do 2 passes in order to make sure
+    # all symlinks to files remain valid while we work on them, do them
+    # first to safely retarget, then process files
+    syms = []
+    files = []
+    for f in (pkg.destdir / "usr/share/man").rglob("*.*"):
+        # skip non-files and symlinks to non-files
+        # dead links are okay, the .exists() handles it
+        if f.exists() and not f.is_file():
+            continue
+        # capture symlinks
+        if f.is_symlink():
+            syms.append(f)
+        else:
+            files.append(f)
+    # now process links
+    for f in syms:
+        linktgt = f.readlink()
+        f.unlink()
+        f.with_suffix(f"{f.suffix}.gz").symlink_to(f"{linktgt}.gz")
+    # and then files
+    for f in files:
+        cf = pkg.chroot_destdir / f.relative_to(pkg.destdir)
+        chroot.enter(
+            "gzip",
+            "-9n",
+            cf,
+            check=True,
+            ro_root=True,
+            ro_build=True,
+            ro_dest=False,
+            unshare_all=True,
+        )
