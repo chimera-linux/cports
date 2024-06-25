c5729c4e5235c8d8292df1b3025021804712620b
triallax <triallax@tutanota.com>
2024-06-25 21:43:52 +0100

    main/gnome-color-manager: run tests, get rid of cfi fixme and vis

--- main/gnome-color-manager/template.py
+++ main/gnome-color-manager/template.py
@@ -1,23 +1,21 @@
 pkgname = "gnome-color-manager"
 pkgver = "3.36.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
-hostmakedepends = ["meson", "pkgconf", "glib-devel", "gettext", "itstool"]
+make_check_wrapper = ["wlheadless-run", "--"]
+hostmakedepends = ["gettext", "glib-devel", "itstool", "meson", "pkgconf"]
 makedepends = [
-    "glib-devel",
     "colord-devel",
-    "lcms2-devel",
+    "glib-devel",
     "gtk+3-devel",
+    "lcms2-devel",
     "libcanberra-devel",
 ]
 depends = ["colord", "shared-color-targets"]
+checkdepends = ["xwayland-run"]
 pkgdesc = "Color profile manager for the GNOME desktop"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-color-manager"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+source = f"$(GNOME_SITE)/gnome-color-manager/{pkgver[:-2]}/gnome-color-manager-{pkgver}.tar.xz"
 sha256 = "9ddb9e6b6472e119801381f90905332ec1d4258981721bba831ca246ceb3ad3b"
-# FIXME cfi
-hardening = ["vis", "!cfi"]
-# needs a graphical environment
-options = ["!check"]
