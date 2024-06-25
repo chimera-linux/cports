b17931c5c720376d828a1b45983b414d6e78041f
psykose <alice@ayaya.dev>
2024-06-25 10:30:23 +0200

    main/libassuan: update to 3.0.1

--- main/libassuan/template.py
+++ main/libassuan/template.py
@@ -1,5 +1,5 @@
 pkgname = "libassuan"
-pkgver = "3.0.0"
+pkgver = "3.0.1"
 pkgrel = 0
 build_style = "gnu_configure"
 # their autoconf is dumb
@@ -10,8 +10,8 @@ pkgdesc = "IPC library used by some GnuPG related software"
 maintainer = "eater <=@eater.me>"
 license = "GPL-3.0-or-later"
 url = "https://www.gnupg.org/related_software/libassuan"
-source = f"https://gnupg.org/ftp/gcrypt/{pkgname}/{pkgname}-{pkgver}.tar.bz2"
-sha256 = "0b160cbb898b852c6c04314b9a63e90ca87501305ad72a58a010f808665bbaf6"
+source = f"https://gnupg.org/ftp/gcrypt/libassuan/libassuan-{pkgver}.tar.bz2"
+sha256 = "c8f0f42e6103dea4b1a6a483cb556654e97302c7465308f58363778f95f194b1"
 
 
 @subpackage("libassuan-devel")
