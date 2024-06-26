2367c93c822ae2c2332fe91f194d7a107177703b
triallax <triallax@tutanota.com>
2024-06-26 18:55:04 +0100

    main/zstd: run tests

--- main/zstd/template.py
+++ main/zstd/template.py
@@ -5,33 +5,31 @@ build_style = "meson"
 configure_args = [
     "-Db_ndebug=true",
     "-Dbin_contrib=true",
+    "-Dbin_tests=true",
     "-Dlz4=enabled",
     "-Dlzma=enabled",
     "-Dzlib=enabled",
 ]
 make_dir = "mbuild"
 meson_dir = "build/meson"
-hostmakedepends = ["pkgconf", "meson"]
-makedepends = ["zlib-devel", "xz-devel", "lz4-devel"]
-checkdepends = ["gtest-devel"]
+hostmakedepends = ["meson", "pkgconf"]
+makedepends = ["lz4-devel", "xz-devel", "zlib-devel"]
 provides = [f"libzstd={pkgver}-r{pkgrel}"]
 pkgdesc = "Zstd compression utilities"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://www.zstd.net"
-source = f"https://github.com/facebook/{pkgname}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.gz"
+source = f"https://github.com/facebook/zstd/releases/download/v{pkgver}/zstd-{pkgver}.tar.gz"
 sha256 = "8c29e06cf42aacc1eafc4077ae2ec6c6fcb96a626157e0593d5e82a34fd403c1"
 compression = "deflate"
 hardening = ["!cfi"]  # TODO
-# checkdepends not available yet
-options = ["!check"]
 
 
 def post_install(self):
     self.install_license("LICENSE")
     for tool in ["zstdgrep", "zstdless"]:
         self.rm(self.destdir / "usr/bin" / tool)
-        self.rm(self.destdir / "usr/share/man/man1" / (tool + ".1"))
+        self.rm(self.destdir / f"usr/share/man/man1/{tool}.1")
 
 
 @subpackage("zstd-progs")
