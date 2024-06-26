d9cc64d158d2eaab24f97d771884a120bace978d
triallax <triallax@tutanota.com>
2024-06-26 01:44:02 +0100

    main/gtksourceview: run tests

--- main/gtksourceview/template.py
+++ main/gtksourceview/template.py
@@ -3,32 +3,35 @@ pkgver = "5.12.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=enabled", "-Dvapi=true"]
+make_check_wrapper = ["dbus-run-session", "wlheadless-run", "--"]
 hostmakedepends = [
-    "meson",
-    "pkgconf",
-    "gobject-introspection",
-    "vala",
-    "glib-devel",
-    "pcre2-devel",
     "gettext",
+    "glib-devel",
+    "gobject-introspection",
+    "meson",
+    "pcre2-devel",
+    "pkgconf",
+    "vala",
     "xsltproc",
 ]
 makedepends = [
-    "gtk4-devel",
     "glib-devel",
+    "gtk4-devel",
     "libxml2-devel",
-    "vala",
     "pcre2-devel",
+    "vala",
 ]
+checkdepends = ["dbus", "xwayland-run"]
 pkgdesc = "Advanced Gtk4 text editor widget"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Projects/GtkSourceView"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+source = (
+    f"$(GNOME_SITE)/gtksourceview/{pkgver[:-2]}/gtksourceview-{pkgver}.tar.xz"
+)
 sha256 = "84c82aad985c5aadae7cea7804904a76341ec82b268d46594c1a478f39b42c1f"
-# needs graphical environment
 # lto results in broken mouse scrolling in gnome-text-editor
-options = ["!check", "!cross", "!lto"]
+options = ["!cross", "!lto"]
 
 
 @subpackage("gtksourceview-devel")
