05a57f9670f767a1adb23226a0251165ed08a063
Erica Z <zerica@callcc.eu>
2024-06-26 09:59:14 +0200

    */*: rebuild with numpy 2.0.0

--- contrib/opencv/template.py
+++ contrib/opencv/template.py
@@ -1,6 +1,6 @@
 pkgname = "opencv"
 pkgver = "4.10.0"
-pkgrel = 2
+pkgrel = 3
 build_style = "cmake"
 configure_args = [
     # rm NDEBUG
--- contrib/python-fasttext/template.py
+++ contrib/python-fasttext/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-fasttext"
-pkgver = "0.9.2"
+pkgver = "0.9.3"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -17,8 +17,8 @@ pkgdesc = "Python bindings for fasttext"
 maintainer = "Duncan Bellamy <dunk@denkimushi.com>"
 license = "MIT"
 url = "https://fasttext.cc/index.html"
-source = f"https://github.com/facebookresearch/fasttext/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7ea4edcdb64bfc6faaaec193ef181bdc108ee62bb6a04e48b2e80b639a99e27e"
+source = f"$(PYPI_SITE)/f/fasttext/fasttext-{pkgver}.tar.gz"
+sha256 = "eb03f2ef6340c6ac9e4398a30026f05471da99381b307aafe2f56e4cd26baaef"
 # Tests require 300+MB test data downloaded with a script
 options = ["!check"]
 
