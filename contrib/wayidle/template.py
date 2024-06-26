d7214f821d9ffc20d2861e425ad240e0cf3195c8
psykose <alice@ayaya.dev>
2024-06-26 16:14:34 +0200

    contrib/wayidle: update to 0.2.0

--- contrib/wayidle/template.py
+++ contrib/wayidle/template.py
@@ -1,5 +1,5 @@
 pkgname = "wayidle"
-pkgver = "0.1.1"
+pkgver = "0.2.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -9,7 +9,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "ISC"
 url = "https://git.sr.ht/~whynothugo/wayidle"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "f71d7c9f62d39b254b11a716e7ba7da9ae81224bacb8b273ab5d9595b9fa6b7d"
+sha256 = "36f191372658a7ac25a4343bcd227ce06523358ca1801daa807fd07b1cd34d27"
 # no tests
 options = ["!check"]
 
