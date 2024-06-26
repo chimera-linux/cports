6103b0d8e998b6b5d4fc73f4ac276fa55940ee98
psykose <alice@ayaya.dev>
2024-06-26 06:56:04 +0200

    main/svt-av1: update to 2.1.1
    note the decoder is now removed entirely

--- main/svt-av1/template.py
+++ main/svt-av1/template.py
@@ -1,6 +1,6 @@
 pkgname = "svt-av1"
-pkgver = "2.1.0"
-pkgrel = 1
+pkgver = "2.1.1"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DBUILD_SHARED_LIBS=ON",
@@ -19,7 +19,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-3-Clause-Clear"
 url = "https://gitlab.com/AOMediaCodec/SVT-AV1"
 source = f"{url}/-/archive/v{pkgver}/SVT-AV1-v{pkgver}.tar.bz2"
-sha256 = "2bfd098770bba185cd1ced8e1ff389837e3dca0d8b5cfb0d97c925a61dbbf955"
+sha256 = "e490d8e8ef8cd1f8f814fd207590f36dc1c1eb228efec959cfea113c57797ced"
 # FIXME int: muloverflow in svt_av1_find_best_sub_pixel_tree for certain encodes
 hardening = ["vis", "cfi", "!int"]
 # needs patching+clones of a bunch of stuff
@@ -42,11 +42,6 @@ def _devel(self):
     return self.default_devel()
 
 
-@subpackage("libsvtav1dec")
-def _dec(self):
-    return ["usr/lib/libSvtAv1Dec.so.*"]
-
-
-@subpackage("libsvtav1enc")
-def _enc(self):
-    return ["usr/lib/libSvtAv1Enc.so.*"]
+@subpackage("svt-av1-libs")
+def _libs(self):
+    return self.default_libs()
