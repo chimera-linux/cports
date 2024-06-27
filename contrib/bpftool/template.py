46157dbed05a337936a381261ff5d716c29893a3
psykose <alice@ayaya.dev>
2024-06-27 18:11:14 +0200

    contrib/bpftool: update to 6.9.7

--- contrib/bpftool/template.py
+++ contrib/bpftool/template.py
@@ -1,5 +1,5 @@
 pkgname = "bpftool"
-pkgver = "6.9.6"
+pkgver = "6.9.7"
 pkgrel = 0
 build_wrksrc = "tools/bpf/bpftool"
 build_style = "makefile"
@@ -29,6 +29,6 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-only"
 url = "https://perf.wiki.kernel.org/index.php/Main_Page"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[:pkgver.find('.')]}.x/linux-{pkgver}.tar.xz"
-sha256 = "5d4366e2b89998f274abe03557ef3bc78b58e47fc62c102d51e6f49e5ed96b4b"
+sha256 = "e4f588cd91eef9d461e5e14fdf9415feff8a72fbcc274089a0f768a58de001f8"
 # nope
 options = ["!check"]
