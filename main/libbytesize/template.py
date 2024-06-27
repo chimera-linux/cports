6f6f88a7a57d670bb7af88435e9b1594cecf900b
triallax <triallax@tutanota.com>
2024-06-28 00:27:27 +0100

    main/libbytesize: run tests

--- main/libbytesize/template.py
+++ main/libbytesize/template.py
@@ -4,22 +4,20 @@ pkgrel = 1
 build_style = "gnu_configure"
 make_cmd = "gmake"
 hostmakedepends = [
+    "automake",
+    "gettext",
     "gmake",
+    "libtool",
     "pkgconf",
     "python",
-    "gettext",
-    "automake",
-    "libtool",
 ]
 makedepends = ["gmp-devel", "mpfr-devel", "pcre2-devel"]
 pkgdesc = "Library for operations with sizes in bytes"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/storaged-project/libbytesize"
-source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.gz"
+source = f"{url}/releases/download/{pkgver}/libbytesize-{pkgver}.tar.gz"
 sha256 = "1d1ce3be8ac59fd59511d0794c7327d5cf33f1e83496837b17e19ac49400cad1"
-# cba
-options = ["!check"]
 
 
 @subpackage("libbytesize-devel")
