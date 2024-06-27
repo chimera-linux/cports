c511e0f303385ef6837984f0022a8a060710b5a8
psykose <alice@ayaya.dev>
2024-06-28 00:38:18 +0200

    contrib/stylua: new package (0.20.0)

---
+++ contrib/stylua/template.py
@@ -0,0 +1,26 @@
+pkgname = "stylua"
+pkgver = "0.20.0"
+pkgrel = 0
+build_style = "cargo"
+make_build_args = [
+    "--features",
+    "lua54,luau,fromstr,serialize",
+]
+make_install_args = list(make_build_args)
+make_check_args = list(make_build_args)
+hostmakedepends = [
+    "cargo",
+]
+makedepends = [
+    "rust-std",
+]
+pkgdesc = "Lua formatter"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MPL-2.0"
+url = "https://github.com/JohnnyMorganz/StyLua"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "f4a27b12669953d2edf55b89cc80381f97a2dfa735f53f95c6ae6015c8c35ffb"
+
+
+def do_install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/stylua")
