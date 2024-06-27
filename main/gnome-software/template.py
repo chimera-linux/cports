9ea5673889891e531fb07adb7631c354a0817750
triallax <triallax@tutanota.com>
2024-06-27 23:18:31 +0100

    main/gnome-software: remove !check TODO

--- main/gnome-software/template.py
+++ main/gnome-software/template.py
@@ -3,6 +3,7 @@ pkgver = "46.2"
 pkgrel = 1
 build_style = "meson"
 configure_args = [
+    "-Dtests=false",
     "-Dpackagekit=false",
     "-Dmalcontent=false",
     "-Dwebapps=false",
@@ -11,6 +12,7 @@ configure_args = [
     "-Dhardcoded_proprietary_webapps=false",
     "-Ddefault_library=shared",
 ]
+make_check_wrapper = ["dbus-run-session", "wlheadless-run", "--"]
 hostmakedepends = [
     "appstream",
     "docbook-xsl-nons",
@@ -35,14 +37,16 @@ makedepends = [
     "linux-headers",
     "polkit-devel",
 ]
-checkdepends = ["dbus"]
+checkdepends = ["dbus", "xwayland-run"]
 pkgdesc = "GNOME software center"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-software"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
+source = (
+    f"$(GNOME_SITE)/gnome-software/{pkgver[:-2]}/gnome-software-{pkgver}.tar.xz"
+)
 sha256 = "e700cba287764f5e2255514e312e160550fdbf7a5d3fe16358bb6f7b6732b974"
-# TODO
+# Most tests need system dbus
 options = ["!check"]
 
 
