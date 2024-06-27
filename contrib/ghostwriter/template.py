72240cab6469d1b5d976af70afa31aa9b972717d
psykose <alice@ayaya.dev>
2024-06-27 20:02:43 +0200

    contrib/ghostwriter: new package (24.05.1)

---
+++ contrib/ghostwriter/template.py
@@ -0,0 +1,32 @@
+pkgname = "ghostwriter"
+pkgver = "24.05.1"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_WITH_QT6=ON"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "hunspell-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "kdoctools-devel",
+    "kwidgetsaddons-devel",
+    "kxmlgui-devel",
+    "qt6-qt5compat-devel",
+    "qt6-qtsvg-devel",
+    "qt6-qttools-devel",
+    "qt6-qtwebengine-devel",
+    "sonnet-devel",
+]
+depends = ["cmark"]
+pkgdesc = "KDE markdown editor"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-3.0-or-later"
+url = "https://apps.kde.org/ghostwriter"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/ghostwriter-{pkgver}.tar.xz"
+sha256 = "769b2116071ffdd22157a654a01108040b3a0c40d44cd14f7b71d5164983926e"
