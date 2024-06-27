5aeda7f7d5b79cba3f6101a81fabe2d8187d501b
psykose <alice@ayaya.dev>
2024-06-27 18:56:54 +0200

    main/linux-stable: update to 6.9.7

--- main/linux-stable/template.py
+++ main/linux-stable/template.py
@@ -1,7 +1,7 @@
 # update linux-stable-zfs-bin when bumping
 pkgname = "linux-stable"
-pkgver = "6.9.6"
-pkgrel = 9
+pkgver = "6.9.7"
+pkgrel = 0
 archs = ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]
 make_dir = "build"
 hostmakedepends = ["base-kernel-devel"]
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
-sha256 = "5d4366e2b89998f274abe03557ef3bc78b58e47fc62c102d51e6f49e5ed96b4b"
+sha256 = "e4f588cd91eef9d461e5e14fdf9415feff8a72fbcc274089a0f768a58de001f8"
 # no meaningful checking to be done
 options = [
     "!check",
