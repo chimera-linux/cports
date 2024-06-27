173e1131da13dd187153e7b0bd77098c19589126
triallax <triallax@tutanota.com>
2024-06-28 00:03:26 +0100

    main/libavif: run tests, enable libsharpyuv support

--- main/libavif/template.py
+++ main/libavif/template.py
@@ -1,35 +1,34 @@
 pkgname = "libavif"
 pkgver = "1.0.4"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 configure_args = [
     "-DAVIF_BUILD_APPS=ON",
     "-DAVIF_BUILD_GDK_PIXBUF=ON",
     "-DAVIF_CODEC_DAV1D=ON",
     "-DAVIF_CODEC_AOM=ON",
-    "-DAVIF_BUILD_TESTS=OFF",
-    "-DAVIF_ENABLE_WERROR=OFF",
-    "-DAVIF_ENABLE_GTEST=OFF",
+    "-DAVIF_BUILD_TESTS=ON",
+    "-DAVIF_ENABLE_GTEST=ON",
 ]
-make_check_target = "avif_test_all"
-hostmakedepends = ["cmake", "ninja", "pkgconf", "gdk-pixbuf-devel"]
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = [
-    "gdk-pixbuf-devel",
     "dav1d-devel",
+    "gdk-pixbuf-devel",
+    "gtest-devel",
     "libaom-devel",
     "libjpeg-turbo-devel",
     "libpng-devel",
+    "libwebp-devel",
     "zlib-ng-compat-devel",
 ]
+checkdepends = ["bash"]
 pkgdesc = "Library for encoding and decoding .avif files"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/AOMediaCodec/libavif"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "dc56708c83a4b934a8af2b78f67f866ba2fb568605c7cf94312acf51ee57d146"
-hardening = ["!cfi"]  # TODO when we have tests
-# doesn't pass with current dependencies, needs gtest
-options = ["!check"]
+hardening = ["!cfi"]  # TODO
 
 
 def post_install(self):
