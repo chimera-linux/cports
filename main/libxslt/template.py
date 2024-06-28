14e11a7f50a2ef5f0db094e503b74fe30dd12dc6
triallax <triallax@tutanota.com>
2024-06-28 16:18:39 +0100

    main/libxslt: move xsltConf.sh to -devel, run tests, remove !cross

--- main/libxslt/template.py
+++ main/libxslt/template.py
@@ -1,6 +1,6 @@
 pkgname = "libxslt"
 pkgver = "1.1.41"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = ["--with-python=no"]
 hostmakedepends = [
@@ -10,15 +10,13 @@ hostmakedepends = [
     "libtool",
     "pkgconf",
 ]
-makedepends = ["libxml2-devel", "libgcrypt-devel"]
+makedepends = ["libgcrypt-devel", "libxml2-devel"]
 pkgdesc = "GNOME XSLT parser library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://xmlsoft.org/XSLT"
-source = f"$(GNOME_SITE)/libxslt/{pkgver[:pkgver.rfind('.')]}/{pkgname}-{pkgver}.tar.xz"
+source = f"$(GNOME_SITE)/libxslt/{pkgver[:pkgver.rfind('.')]}/libxslt-{pkgver}.tar.xz"
 sha256 = "3ad392af91115b7740f7b50d228cc1c5fc13afc1da7f16cb0213917a37f71bda"
-# test code seemingly incompatible with current libxml2
-options = ["!cross", "!check"]
 
 
 def post_install(self):
@@ -27,7 +25,7 @@ def post_install(self):
 
 @subpackage("libxslt-devel")
 def _devel(self):
-    return self.default_devel()
+    return self.default_devel(extra=["usr/lib/xsltConf.sh"])
 
 
 @subpackage("xsltproc")
