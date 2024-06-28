663d13adb479f5baf01cbdc721f432523599b67e
ttyyls <contact@behri.org>
2024-06-28 22:39:12 +0100

    contrib/lua5.4-luaexpat: update to 1.5.2

--- contrib/lua5.4-luaexpat/template.py
+++ contrib/lua5.4-luaexpat/template.py
@@ -1,5 +1,5 @@
 pkgname = "lua5.4-luaexpat"
-pkgver = "1.5.1"
+pkgver = "1.5.2"
 pkgrel = 0
 build_style = "makefile"
 make_dir = "."
@@ -9,7 +9,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "MIT"
 url = "https://lunarmodules.github.io/luaexpat"
 source = f"https://github.com/lunarmodules/luaexpat/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "7d455f154de59eb0b073c3620bc8b873f7f697b3f21a112e6ff8dc9fca6d0826"
+sha256 = "89d83f2141edec31be576425637216928221918fe95dc3854d1b7fd4c627213f"
 # no tests defined
 options = ["!check"]
 
