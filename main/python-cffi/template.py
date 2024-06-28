454e5a30d3a2357e62ee58a63a56cced4ded4042
triallax <triallax@tutanota.com>
2024-06-28 17:36:59 +0100

    main/python-cffi: run tests

--- main/python-cffi/template.py
+++ main/python-cffi/template.py
@@ -1,15 +1,15 @@
 pkgname = "python-cffi"
 pkgver = "1.16.0"
-pkgrel = 1
+pkgrel = 2
 build_style = "python_pep517"
 hostmakedepends = [
+    "libffi-devel",
     "python-build",
     "python-installer",
     "python-setuptools",
     "python-wheel",
-    "libffi-devel",
 ]
-makedepends = ["python-devel", "libffi-devel"]
+makedepends = ["libffi-devel", "python-devel"]
 depends = ["python-pycparser"]
 checkdepends = ["python-pycparser", "python-pytest"]
 pkgdesc = "C FFI for Python"
@@ -18,8 +18,6 @@ license = "MIT"
 url = "https://cffi.readthedocs.io"
 source = f"$(PYPI_SITE)/c/cffi/cffi-{pkgver}.tar.gz"
 sha256 = "bcb3ef43e58665bbda2fb198698fcae6776483e0c4a631aa5647806c25e02cc0"
-# do_check needs fixing up more
-options = ["!check"]
 
 
 def post_install(self):
