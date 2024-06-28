140ea8cdcfaa5a72ac1a30ac0ea1971e72f4b71e
triallax <triallax@tutanota.com>
2024-06-29 00:12:45 +0100

    main/python-installer: clean up

--- main/python-installer/template.py
+++ main/python-installer/template.py
@@ -1,16 +1,16 @@
 pkgname = "python-installer"
 pkgver = "0.7.0"
-pkgrel = 1
-hostmakedepends = ["python", "python-flit_core"]
-checkdepends = ["python-pytest-xdist"]
+pkgrel = 2
+hostmakedepends = ["python-flit_core"]
+checkdepends = ["python-pytest"]
 depends = ["python"]
 pkgdesc = "Low-level library for installing from a Python wheel"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
-url = "https://installer.readthedocs.io"
+url = "https://installer.pypa.io/en/stable"
 source = f"$(PYPI_SITE)/i/installer/installer-{pkgver}.tar.gz"
 sha256 = "a26d3e3116289bb08216e0d0f7d925fcef0b0194eedfa0c944bcaaa106c4b631"
-# missing checkdepends
+# cycle
 options = ["!check"]
 
 
