7055b6c563af7af33635b36d0b0499ca9d4ccc2e
Renato Botelho do Couto <renato@netgate.com>
2024-06-28 13:43:12 -0300

    contrib/iftop: new package (1.0_p4)

---
+++ contrib/iftop/template.py
@@ -0,0 +1,13 @@
+pkgname = "iftop"
+pkgver = "1.0_p4"
+pkgrel = 0
+build_style = "gnu_configure"
+make_dir = "."
+hostmakedepends = ["automake"]
+makedepends = ["libpcap-devel", "linux-headers", "ncurses-devel"]
+pkgdesc = "Display bandwidth usage on an interface by host"
+maintainer = "Renato Botelho do Couto <renato@netgate.com>"
+license = "GPL-2.0-or-later"
+url = "https://pdw.ex-parrot.com/iftop"
+source = f"{url}/download/iftop-1.0pre4.tar.gz"
+sha256 = "f733eeea371a7577f8fe353d86dd88d16f5b2a2e702bd96f5ffb2c197d9b4f97"
