2ffb7c21a9ab84f5fe4a1b52457312771a56234c
psykose <alice@ayaya.dev>
2024-06-28 22:54:33 +0200

    contrib/fuse-overlayfs: update to 1.14

--- contrib/fuse-overlayfs/template.py
+++ contrib/fuse-overlayfs/template.py
@@ -1,5 +1,5 @@
 pkgname = "fuse-overlayfs"
-pkgver = "1.13"
+pkgver = "1.14"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -14,6 +14,6 @@ pkgdesc = "FUSE implementation for overlayfs"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/containers/fuse-overlayfs"
-source = f"https://github.com/containers/fuse-overlayfs/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "96d10344921d5796bcba7a38580ae14a53c4e60399bb90b238ac5a10b3bb65b2"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "0779d1ee8fbb6adb48df40e54efa9c608e1d7bbd844800a4c32c110d5fcbe9f2"
 hardening = ["vis", "cfi"]
