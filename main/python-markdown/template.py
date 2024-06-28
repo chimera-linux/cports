232187c76ee0ac3b8bf6b29427c9836278f01a01
triallax <triallax@tutanota.com>
2024-06-29 00:23:33 +0100

    main/python-markdown: run tests

--- main/python-markdown/template.py
+++ main/python-markdown/template.py
@@ -8,7 +8,7 @@ hostmakedepends = [
     "python-setuptools",
     "python-wheel",
 ]
-checkdepends = ["python-pyyaml", "python-pytest"]
+checkdepends = ["python-pyyaml"]
 depends = ["python"]
 pkgdesc = "Python implementation of Markdown"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -16,8 +16,10 @@ license = "BSD-3-Clause"
 url = "https://github.com/Python-Markdown/markdown"
 source = f"$(PYPI_SITE)/M/Markdown/Markdown-{pkgver}.tar.gz"
 sha256 = "ed4f41f6daecbeeb96e576ce414c41d2d876daa9a16cb35fa8ed8c2ddfad0224"
-# checkdepends missing
-options = ["!check"]
+
+
+def do_check(self):
+    self.do("python", "-m", "unittest", "discover")
 
 
 def post_install(self):
