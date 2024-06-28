8bc3ab1667b54dde28f20dc0a2630ff875fbc67b
triallax <triallax@tutanota.com>
2024-06-28 16:29:25 +0100

    main/mobile-broadband-provider-info: clean up template

--- main/mobile-broadband-provider-info/template.py
+++ main/mobile-broadband-provider-info/template.py
@@ -2,16 +2,16 @@ pkgname = "mobile-broadband-provider-info"
 pkgver = "20240407"
 pkgrel = 0
 build_style = "meson"
-hostmakedepends = ["meson", "xsltproc", "pkgconf"]
+hostmakedepends = ["meson", "pkgconf", "xsltproc"]
 checkdepends = ["libxml2-progs"]
 pkgdesc = "Database of mobile broadband service providers"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:none"
 url = "https://gitlab.gnome.org/GNOME/mobile-broadband-provider-info"
-source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
+source = (
+    f"{url}/-/archive/{pkgver}/mobile-broadband-provider-info-{pkgver}.tar.gz"
+)
 sha256 = "0a3985bd38d23d8225a85c96a6b8e4afec022ece7bf0cc7efc43f296012f9699"
-# doesn't like our shell
-# options = ["!check"]
 
 
 def post_install(self):
