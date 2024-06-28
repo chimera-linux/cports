1186ddd262590e752d007697971e1b3a9e29ae63
Jami Kettunen <jami.kettunen@protonmail.com>
2024-06-28 14:35:46 +0300

    contrib/fastfetch: update to 2.17.0

--- contrib/fastfetch/template.py
+++ contrib/fastfetch/template.py
@@ -1,5 +1,5 @@
 pkgname = "fastfetch"
-pkgver = "2.16.0"
+pkgver = "2.17.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_TESTS=ON"]
@@ -26,7 +26,7 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "MIT"
 url = "https://github.com/fastfetch-cli/fastfetch"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "8c5d137e0439d70189fc267176840feaba5f3e06177d57c4be9a866595651803"
+sha256 = "b108ecff62f4e834f639da8f24d332af7901376e3ace97ebfc6ec81114315556"
 tool_flags = {"CFLAGS": ["-DNDEBUG"]}
 # TODO: CFI dies immediately (ffPlatformPathAddHome at FFlist.c:31:12)
 hardening = ["vis", "!cfi"]
