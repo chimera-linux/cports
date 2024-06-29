3e1203f4b50d2431f7c47985bb23e6ff892cbe4b
psykose <alice@ayaya.dev>
2024-06-29 19:45:42 +0200

    contrib/valve-hardware-audio-processing: update to 0.49

--- contrib/valve-hardware-audio-processing/template.py
+++ contrib/valve-hardware-audio-processing/template.py
@@ -1,10 +1,11 @@
 pkgname = "valve-hardware-audio-processing"
-pkgver = "0.39"
-pkgrel = 2
+pkgver = "0.49"
+pkgrel = 0
 archs = ["x86_64"]
 build_style = "makefile"
 make_cmd = "gmake"
 make_build_args = ["FAUST_COMPILER=clang++"]
+make_install_args = ["SHELL=/usr/bin/bash"]
 make_use_env = True
 hostmakedepends = ["gmake", "bash", "faust"]
 makedepends = ["boost-devel", "lv2"]
@@ -14,10 +15,15 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.com/evlaV/valve-hardware-audio-processing"
 source = f"{url}/-/archive/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "1cf9d639dc88651087e8ce1e536ed1e260ce52211ba8794cba2a7d29db4b4efb"
+sha256 = "1c4c5ab7259f0e0bb57cfb42f55ffd742023ad7b0f3b6f1e30432b68f18ea1af"
 # no tests
 options = ["!cross", "!check"]
 
 
 def init_configure(self):
     self.make_install_env = {"DEST_DIR": str(self.chroot_destdir)}
+
+
+def post_install(self):
+    # glibc bin from sof-bin
+    self.rm(self.destdir / "usr/lib/firmware/amd/sof/sof-logger")
