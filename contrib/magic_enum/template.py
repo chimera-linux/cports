c7dff2932a019c444189cf451b14c3f1fbfe474a
psykose <alice@ayaya.dev>
2024-06-30 01:30:25 +0200

    contrib/magic_enum: update to 0.9.6

--- contrib/magic_enum/template.py
+++ contrib/magic_enum/template.py
@@ -1,8 +1,11 @@
 pkgname = "magic_enum"
-pkgver = "0.9.5"
+pkgver = "0.9.6"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DMAGIC_ENUM_OPT_BUILD_EXAMPLES=OFF"]
+configure_args = [
+    "-DMAGIC_ENUM_OPT_BUILD_EXAMPLES=OFF",
+    "-DMAGIC_ENUM_OPT_INSTALL_PACKAGE_XML=OFF",
+]
 hostmakedepends = [
     "cmake",
     "ninja",
@@ -13,7 +16,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://github.com/Neargye/magic_enum"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "44ad80db5a72f5047e01d90e18315751d9ac90c0ab42cbea7a6f9ec66a4cd679"
+sha256 = "814791ff32218dc869845af7eb89f898ebbcfa18e8d81aa4d682d18961e13731"
 
 
 def post_install(self):
