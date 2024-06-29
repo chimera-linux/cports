adbe520ff205606087b5a5f1766181d96d24d79f
miko <mikoxyzzz@gmail.com>
2024-06-29 20:31:47 +0200

    contrib/python-automat: enable tests

--- contrib/python-automat/template.py
+++ contrib/python-automat/template.py
@@ -9,15 +9,13 @@ hostmakedepends = [
     "python-wheel",
 ]
 depends = ["python-setuptools", "python-attrs", "python-six"]
-checkdepends = ["python-pytest", "python-graphviz"] + depends
+checkdepends = ["python-pytest-benchmark"] + depends
 pkgdesc = "Finite state machines for Python"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/glyph/Automat"
 source = f"$(PYPI_SITE)/A/Automat/Automat-{pkgver}.tar.gz"
 sha256 = "e56beb84edad19dcc11d30e8d9b895f75deeb5ef5e96b84a467066b3b84bb04e"
-# unpackaged checkdepends
-options = ["!check"]
 
 
 def post_install(self):
