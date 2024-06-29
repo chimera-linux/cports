3e78e908931d5d9292bb5d66b786772d45cd8d93
Jami Kettunen <jami.kettunen@protonmail.com>
2024-06-29 21:11:21 +0300

    contrib/sddm-kcm: new package (6.1.1)

---
+++ contrib/sddm-kcm/template.py
@@ -0,0 +1,28 @@
+pkgname = "sddm-kcm"
+pkgver = "6.1.2"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+]
+makedepends = [
+    "karchive-devel",
+    "kauth-devel",
+    "kcmutils-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kio-devel",
+    "knewstuff-devel",
+    "kservice",
+    "qt6-qtdeclarative-devel",
+]
+depends = ["sddm"]
+pkgdesc = "KDE Login Screen (SDDM) KCM"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://invent.kde.org/plasma/sddm-kcm"
+source = f"$(KDE_SITE)/plasma/{pkgver}/sddm-kcm-{pkgver}.tar.xz"
+sha256 = "6c78b202420e3d2d597a22b98f6e24023539d0f806f5ce09f7dcc83efe7480d7"
