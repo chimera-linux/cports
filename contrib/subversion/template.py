059522bd5e221fb3e0dee70e4add33d529e70dea
psykose <alice@ayaya.dev>
2024-06-30 18:27:15 +0200

    contrib/subversion: fix L

--- contrib/subversion/template.py
+++ contrib/subversion/template.py
@@ -52,7 +52,7 @@ source = (
     f"https://archive.apache.org/dist/subversion/subversion-{pkgver}.tar.gz"
 )
 sha256 = "cf70775e5ed075ebc6a63fe8619dc6b530da254a3f61ba53a502dd83c8f14afc"
-# not even once also nobody cares l l l l
+# not even once also nobody cares L
 options = ["!cross", "!check"]
 
 
