c5468dc93b37ede82141b62b6dbf11193c7ce1eb
psykose <alice@ayaya.dev>
2024-06-30 21:58:42 +0200

    main/ccache: update to 4.10.1

--- main/ccache/template.py
+++ main/ccache/template.py
@@ -1,5 +1,5 @@
 pkgname = "ccache"
-pkgver = "4.10"
+pkgver = "4.10.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DENABLE_TESTING=OFF", "-DREDIS_STORAGE_BACKEND=OFF"]
@@ -9,6 +9,7 @@ hostmakedepends = [
     "perl",
 ]
 makedepends = [
+    "blake3-devel",
     "fmt-devel",
     "xxhash-devel",
     "zstd-devel",
@@ -20,11 +21,9 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://ccache.dev"
 source = f"https://github.com/ccache/ccache/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "83630b5e922b998ab2538823e0cad962c0f956fad1fcf443dd5288269a069660"
+sha256 = "3a43442ce3916ea48bb6ccf6f850891cbff01d1feddff7cd4bbd49c5cf1188f6"
 # cfi crashes in fmt template expansion
 hardening = ["vis", "!cfi"]
-# not properly set up
-options = ["!check"]
 
 
 def post_install(self):
