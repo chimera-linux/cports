49fca55fa856c848f951b03da2ae0b2dd0259a1c
Renato Botelho do Couto <renato@netgate.com>
2024-06-30 14:24:15 -0300

    contrib/ipmitool: new package (1.8.19)

---
+++ contrib/ipmitool/template.py
@@ -0,0 +1,30 @@
+pkgname = "ipmitool"
+pkgver = "1.8.19"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-intf-free",
+    "--enable-intf-imb",
+    "--enable-intf-lanplus",
+    "--enable-intf-open",
+    "--enable-intf-usb",
+    "--enable-ipmishell",
+]
+make_cmd = "gmake"
+make_dir = "."
+hostmakedepends = ["automake", "gmake", "libtool"]
+makedepends = [
+    "linux-headers",
+    "openssl-devel",
+    "readline-devel",
+]
+pkgdesc = "CLI to manage IPMI systems"
+maintainer = "Renato Botelho do Couto <renato@netgate.com>"
+license = "BSD-3-Clause"
+url = "https://codeberg.org/IPMITool/ipmitool"
+source = f"{url}/archive/IPMITOOL_{pkgver.replace('.', '_')}.tar.gz"
+sha256 = "ce13c710fea3c728ba03a2a65f2dd45b7b13382b6f57e25594739f2e4f20d010"
+
+
+def post_install(self):
+    self.install_license("COPYING")
