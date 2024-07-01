9b0b3b914a122679d5a76aaf67528ec7ce005e2d
psykose <alice@ayaya.dev>
2024-07-01 15:27:51 +0200

    main/python-pillow: update to 10.4.0

--- main/python-pillow/template.py
+++ main/python-pillow/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pillow"
-pkgver = "10.3.0"
+pkgver = "10.4.0"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_target = "Tests"
@@ -22,10 +22,10 @@ depends = ["python"]
 checkdepends = ["python-pytest"]
 pkgdesc = "Python Imaging Library"
 maintainer = "q66 <q66@chimera-linux.org>"
-license = "custom:PIL"
+license = "MIT-CMU"
 url = "https://python-pillow.org"
 source = f"$(PYPI_SITE)/p/pillow/pillow-{pkgver}.tar.gz"
-sha256 = "9d2455fbf44c914840c793e89aa82d0e1763a14253a000743719ae5946814b2d"
+sha256 = "166c1cd4d24309b30d61f79f4a9114b7b2313d7450912277855ff5dfd7cd4a06"
 
 
 def post_install(self):
