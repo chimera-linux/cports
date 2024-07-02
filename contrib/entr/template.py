7e133aee5a692688b56cf5294a22e361445ed639
psykose <alice@ayaya.dev>
2024-07-02 16:41:43 +0200

    contrib/entr: update to 5.6

--- contrib/entr/template.py
+++ contrib/entr/template.py
@@ -1,5 +1,5 @@
 pkgname = "entr"
-pkgver = "5.5"
+pkgver = "5.6"
 pkgrel = 0
 build_style = "configure"
 make_install_args = ["PREFIX=/usr"]
@@ -15,7 +15,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "ISC"
 url = "https://eradman.com/entrproject"
 source = f"https://eradman.com/entrproject/code/entr-{pkgver}.tar.gz"
-sha256 = "128c0ce2efea5ae6bd3fd33c3cd31e161eb0c02609d8717ad37e95b41656e526"
+sha256 = "0222b8df928d3b5a3b5194d63e7de098533e04190d9d9a154b926c6c1f9dd14e"
 hardening = ["vis", "cfi"]
 # need `vim'
 options = ["!check"]
