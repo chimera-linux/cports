b65300138c8f412ea06d4a01bce1d760139cb5a2
psykose <alice@ayaya.dev>
2024-07-02 17:12:05 +0200

    contrib/hut: update to 0.6.0

--- contrib/hut/template.py
+++ contrib/hut/template.py
@@ -1,6 +1,6 @@
 pkgname = "hut"
-pkgver = "0.5.0"
-pkgrel = 1
+pkgver = "0.6.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go", "scdoc"]
 pkgdesc = "CLI tool for sr.ht"
@@ -8,7 +8,7 @@ maintainer = "Hugo Machet <mail@hmachet.com>"
 license = "AGPL-3.0-only"
 url = "https://git.sr.ht/~xenrox/hut"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "0f78917a2da718b0317cd73307549f429340c7f5cac84c6356341e4fae800cc1"
+sha256 = "f6abe54b433c30557c49aa41d351ec97ef24b4bee3f9dbc91e7db8f366592f99"
 # !cross: completions are generated with built artifact
 options = ["!debug", "!cross"]
 
