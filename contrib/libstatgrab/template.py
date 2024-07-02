cd2e22e6f662040b076968dc2d4caf4f4998ad94
psykose <alice@ayaya.dev>
2024-07-02 18:16:24 +0200

    contrib/libstatgrab: new package (0.92.1)

---
+++ contrib/libstatgrab/template.py
@@ -0,0 +1,25 @@
+pkgname = "libstatgrab"
+pkgver = "0.92.1"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = [
+    "automake",
+    "libtool",
+    "pkgconf",
+]
+makedepends = [
+    "linux-headers",
+    "ncurses-devel",
+]
+checkdepends = ["perl"]
+pkgdesc = "Cross-platform library for system statistics"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
+url = "https://libstatgrab.org"
+source = f"https://github.com/libstatgrab/libstatgrab/releases/download/LIBSTATGRAB_{pkgver.replace('.', '_')}/libstatgrab-{pkgver}.tar.gz"
+sha256 = "5688aa4a685547d7174a8a373ea9d8ee927e766e3cc302bdee34523c2c5d6c11"
+
+
+@subpackage("libstatgrab-devel")
+def _devel(self):
+    return self.default_devel()
---
+++ contrib/libstatgrab/update.py
@@ -0,0 +1,2 @@
+url = "https://github.com/libstatgrab/libstatgrab/tags"
+pattern = r"refs/tags/LIBSTATGRAB_([\d_]+).tar.gz"
