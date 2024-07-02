0a4d184edf9666e53e8455b3d8c31ed82f34c9b6
Isaac Freund <mail@isaacfreund.com>
2024-07-02 11:16:03 +0200

    contrib/libqtxdg: new package (4.0.0)

---
+++ contrib/libqtxdg/template.py
@@ -0,0 +1,26 @@
+pkgname = "libqtxdg"
+pkgver = "4.0.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "lxqt-build-tools",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "qt6-qtbase-devel",
+    "qt6-qtsvg-devel",
+    "qt6-qttools-devel",
+]
+pkgdesc = "Qt implementation of freedesktop.org xdg specs"
+maintainer = "Isaac Freund <mail@isaacfreund.com>"
+license = "LGPL-2.1-or-later"
+url = "https://github.com/lxqt/libqtxdg"
+source = f"{url}/releases/download/{pkgver}/libqtxdg-{pkgver}.tar.xz"
+sha256 = "8c1b250de914b2e1fb451c213ee6f249b7b5729c85ac3283fd75615a47a66b62"
+
+
+@subpackage("libqtxdg-devel")
+def _devel(self):
+    return self.default_devel()
