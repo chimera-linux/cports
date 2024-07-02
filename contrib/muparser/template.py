4ae78e11f327780546f49d600fbfacbfb1338141
Isaac Freund <mail@isaacfreund.com>
2024-07-02 12:08:11 +0200

    contrib/muparser: new package (2.3.4)

---
+++ contrib/muparser/template.py
@@ -0,0 +1,27 @@
+pkgname = "muparser"
+pkgver = "2.3.4"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "libomp-devel",
+]
+pkgdesc = "Qt implementation of freedesktop.org xdg specs"
+maintainer = "Isaac Freund <mail@isaacfreund.com>"
+license = "BSD-2-Clause"
+url = "https://beltoforion.de/en/muparser"
+source = f"https://github.com/beltoforion/muparser/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "0c3fa54a3ebf36dda0ed3e7cd5451c964afbb15102bdbcba08aafb359a290121"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("muparser-devel")
+def _devel(self):
+    return self.default_devel()
