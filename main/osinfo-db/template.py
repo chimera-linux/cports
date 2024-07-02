bfd0aeb51f8c839b82ce98cff4d8da0ca66a6cc9
psykose <alice@ayaya.dev>
2024-07-02 16:54:54 +0200

    main/osinfo-db: update to 20240701

--- main/osinfo-db/template.py
+++ main/osinfo-db/template.py
@@ -1,5 +1,5 @@
 pkgname = "osinfo-db"
-pkgver = "20240523"
+pkgver = "20240701"
 pkgrel = 0
 hostmakedepends = ["osinfo-db-tools"]
 pkgdesc = "Osinfo database of information about operating systems"
@@ -7,7 +7,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://libosinfo.org"
 source = f"!https://releases.pagure.org/libosinfo/{pkgname}-{pkgver}.tar.xz"
-sha256 = "9deff2dfd294b24cec9f0d62042f0443ad8fdc6606f8bea951e3e53170a906c5"
+sha256 = "1d7381a72f0c45f473befa4a92fa010a37fc4f7b2bb5d1f68e06da440ef6905d"
 options = ["!cross"]
 
 
