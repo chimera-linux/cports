fd002e660b9cb8ff4baf1ebb8c012fceeed84bbb
triallax <triallax@tutanota.com>
2024-07-03 01:21:00 +0100

    main/msgraph: new package (0.2.3)

---
+++ main/msgraph/template.py
@@ -0,0 +1,25 @@
+pkgname = "msgraph"
+pkgver = "0.2.3"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dgtk_doc=false"]
+hostmakedepends = ["gobject-introspection", "meson", "pkgconf"]
+makedepends = [
+    "glib-devel",
+    "gnome-online-accounts-devel",
+    "json-glib-devel",
+    "libsoup-devel",
+]
+checkdepends = ["libxml2-devel", "uhttpmock-devel"]
+pkgdesc = "GLib-based library for MS Graph protocol"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "LGPL-3.0-or-later"
+url = "https://gitlab.gnome.org/GNOME/msgraph"
+source = f"$(GNOME_SITE)/msgraph/{pkgver[:-2]}/msgraph-{pkgver}.tar.xz"
+sha256 = "ed42e800cb7f0a07477cd9e3e744cdc1a240a6ad7ab96b8a875806267a9fddb0"
+options = ["!cross"]
+
+
+@subpackage("msgraph-devel")
+def _devel(self):
+    return self.default_devel()
