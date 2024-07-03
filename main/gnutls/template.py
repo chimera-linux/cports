af6477db5bb024306452d47ccb52081adb96898a
psykose <alice@ayaya.dev>
2024-07-03 16:10:07 +0200

    main/gnutls: update to 3.8.6
    and enable all the features

--- main/gnutls/template.py
+++ main/gnutls/template.py
@@ -1,29 +1,42 @@
 pkgname = "gnutls"
-pkgver = "3.8.5"
-pkgrel = 2
+pkgver = "3.8.6"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--disable-rpath",
     "--disable-static",
     "--disable-valgrind-tests",
+    "--enable-afalg",
     "--enable-ktls",
+    "--with-brotli",
     "--with-default-trust-store-file=/etc/ssl/certs/ca-certificates.crt",
+    "--with-tpm2",
     "--with-zlib",
+    "--with-zstd",
 ]
-configure_gen = []
-hostmakedepends = ["pkgconf", "gettext", "trousers-devel"]
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "gtk-doc-tools",
+    "libtool",
+    "pkgconf",
+    "trousers-devel",
+]
 makedepends = [
-    "libgpg-error-devel",
+    "brotli-devel",
+    "gmp-devel",
     "libidn2-devel",
+    "libkcapi-devel",
     "libtasn1-devel",
     "libunistring-devel",
     "linux-headers",
-    "lzo-devel",
     "nettle-devel",
     "p11-kit-devel",
+    "tpm2-tss-devel",
     "trousers-devel",
     "unbound-devel",
     "zlib-ng-compat-devel",
+    "zstd-devel",
 ]
 # dlopened
 depends = ["libtspi"]
@@ -32,7 +45,11 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gnutls.org"
 source = f"https://www.gnupg.org/ftp/gcrypt/{pkgname}/v{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "66269a2cfe0e1c2dabec87bdbbd8ab656f396edd9a40dd006978e003cfa52bfc"
+sha256 = "2e1588aae53cb32d43937f1f4eca28febd9c0c7aa1734fc5dd61a7e81e0ebcdd"
+
+
+def post_install(self):
+    self.install_file(self.files_path / "config", "etc/gnutls")
 
 
 @subpackage("gnutls-devel")
