f792a7aed19d2eda2f815461a1360ea66e334cc5
triallax <triallax@tutanota.com>
2024-07-03 11:14:34 +0100

    main/libmysofa: run tests, disable int

--- main/libmysofa/template.py
+++ main/libmysofa/template.py
@@ -1,17 +1,17 @@
 pkgname = "libmysofa"
 pkgver = "1.3.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
-# TODO cunit
-configure_args = ["-DBUILD_TESTS=OFF"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
-makedepends = ["zlib-ng-compat-devel"]
+makedepends = ["cunit-devel", "zlib-ng-compat-devel"]
 pkgdesc = "Reader for AES SOFA files to get better HRTFs"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/hoene/libmysofa"
 source = f"{url}/archive/v{pkgver}.tar.gz"
 sha256 = "6c5224562895977e87698a64cb7031361803d136057bba35ed4979b69ab4ba76"
+# FIXME: breaks fail-issue-167a test
+hardening = ["!int"]
 
 
 def post_install(self):
