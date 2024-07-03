7acaec55e12c0b1cb3c8318ef68297e71b5b7418
psykose <alice@ayaya.dev>
2024-07-03 13:21:45 +0200

    contrib/go: remove pointless testdata from package

--- contrib/go/template.py
+++ contrib/go/template.py
@@ -1,6 +1,6 @@
 pkgname = "go"
 pkgver = "1.22.5"
-pkgrel = 0
+pkgrel = 1
 hostmakedepends = ["bash"]
 checkdepends = [
     "libatomic-chimera-devel-static",
@@ -76,10 +76,16 @@ def _get_binpath(self):
 
 def _clear_pkg(self, arch, ppath):
     if arch:
-        self.rm(ppath / f"tool/linux_{arch}", recursive=True)
-        self.rm(ppath / f"linux_{arch}", recursive=True)
-    for f in (ppath / "tool").iterdir():
+        self.rm(ppath / f"pkg/tool/linux_{arch}", recursive=True)
+        self.rm(ppath / f"pkg/linux_{arch}", recursive=True)
+    for f in (ppath / "pkg/tool").iterdir():
         self.rm(f / "api", force=True)
+
+    # cleanup useless testdata
+    for f in (ppath / "src").rglob("testdata"):
+        self.rm(f, recursive=True)
+    for f in (ppath / "src").rglob("*_test.go"):
+        self.rm(f)
 
 
 @custom_target("bootstrap", "build")
@@ -124,4 +130,4 @@ def do_install(self):
 
     self.install_license("LICENSE")
 
-    _clear_pkg(self, _hostarch, self.destdir / "usr/lib/go/pkg")
+    _clear_pkg(self, _hostarch, self.destdir / "usr/lib/go")
