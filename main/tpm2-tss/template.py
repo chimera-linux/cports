9e1192db9ab5f7a8ec5d249f5d55a3e7db526ca8
psykose <alice@ayaya.dev>
2024-07-04 12:06:56 +0200

    main/tpm2-tss: update to 4.1.3

--- main/tpm2-tss/template.py
+++ main/tpm2-tss/template.py
@@ -1,6 +1,6 @@
 pkgname = "tpm2-tss"
-pkgver = "4.0.1"
-pkgrel = 3
+pkgver = "4.1.3"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--enable-unit",
@@ -9,14 +9,21 @@ configure_args = [
     "--with-tctidefaultmodule=device",
     "--with-tctidefaultconfig=/dev/tpmrm0",
 ]
-configure_gen = []
 make_cmd = "gmake"
-make_check_args = ["-j1"]
-hostmakedepends = ["cmocka", "gmake", "perl", "pkgconf"]
+hostmakedepends = [
+    "automake",
+    "cmocka",
+    "gmake",
+    "libltdl-devel",  # ltdl.m4
+    "libtool",
+    "perl",
+    "pkgconf",
+]
 makedepends = [
     "cmocka-devel",
     "json-c-devel",
     "libcurl-devel",
+    "libftdi1-devel",
     "libuuid-devel",
     "linux-headers",
     "openssl-devel",
@@ -26,9 +33,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/tpm2-software/tpm2-tss"
 source = f"{url}/releases/download/{pkgver}/tpm2-tss-{pkgver}.tar.gz"
-sha256 = "532a70133910b6bd842289915b3f9423c0205c0ea009d65294ca18a74087c950"
-# a few fail seemingly due to namespaces
-options = ["!check"]
+sha256 = "37f1580200ab78305d1fc872d89241aaee0c93cbe85bc559bf332737a60d3be8"
 
 
 def post_install(self):
