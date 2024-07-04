003030bf9a98825fa604a9b152104501ab3d2aac
triallax <triallax@tutanota.com>
2024-07-04 15:15:44 +0100

    contrib/yaml-cpp: run tests

--- contrib/yaml-cpp/template.py
+++ contrib/yaml-cpp/template.py
@@ -2,8 +2,9 @@ pkgname = "yaml-cpp"
 pkgver = "0.8.0"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DYAML_BUILD_SHARED_LIBS=ON"]
+configure_args = ["-DYAML_CPP_BUILD_TESTS=ON", "-DYAML_BUILD_SHARED_LIBS=ON"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = ["gtest-devel"]
 pkgdesc = "C++ YAML parser and emitter"
 maintainer = "Val Packett <val@packett.cool>"
 license = "MIT"
@@ -11,8 +12,6 @@ url = "https://github.com/jbeder/yaml-cpp"
 source = f"https://github.com/jbeder/yaml-cpp/archive/{pkgver}.tar.gz"
 sha256 = "fbe74bbdcee21d656715688706da3c8becfd946d92cd44705cc6098bb23b3a16"
 hardening = ["vis"]
-# tries to install them
-options = ["!check"]
 
 
 def post_install(self):
