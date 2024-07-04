e24ad04c3b9813e347822373730b7ee6e516376a
psykose <alice@ayaya.dev>
2024-07-04 20:04:44 +0200

    main/nuspell: update to 5.1.6

--- main/nuspell/template.py
+++ main/nuspell/template.py
@@ -1,8 +1,12 @@
 pkgname = "nuspell"
-pkgver = "5.1.5"
+pkgver = "5.1.6"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DBUILD_SHARED_LIBS=ON"]
+configure_args = [
+    "-DBUILD_SHARED_LIBS=ON",
+    # pandoc...
+    "-DBUILD_DOCS=OFF",
+]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = ["catch2-devel", "icu-devel"]
 pkgdesc = "Fast and safe spell checking software"
@@ -10,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-or-later"
 url = "https://nuspell.github.io"
 source = f"https://github.com/nuspell/nuspell/archive/v{pkgver}.tar.gz"
-sha256 = "39fa6d9da6797cd711d7981b1f55359c55dfa4a302560e645cb59af6f764401a"
+sha256 = "5d4baa1daf833a18dc06ae0af0571d9574cc849d47daff6b9ce11dac0a5ded6a"
 hardening = ["!cfi"]  # TODO
 
 
