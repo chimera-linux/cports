381d730071df167d1e5f3a2013452918a96df7c8
ttyyls <contact@behri.org>
2024-07-06 13:46:37 +0100

    contrib/broot: update to 1.39.1

--- contrib/broot/template.py
+++ contrib/broot/template.py
@@ -1,5 +1,5 @@
 pkgname = "broot"
-pkgver = "1.39.0"
+pkgver = "1.39.1"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -14,7 +14,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "MIT"
 url = "https://dystroy.org/broot"
 source = f"https://github.com/Canop/broot/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "d1d2ccc11543ff4ea645d57a5e78639542a6f510b585a78c31ddb3a24399bf61"
+sha256 = "b186eaa995a50ef36f0d166104350e4f5c1a0b74cbe7ef101eb16b68fb028cdf"
 
 
 def post_install(self):
