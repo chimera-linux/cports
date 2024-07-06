6a77e0acb020085627e0bb0b5ddfd42481a4b3be
psykose <alice@ayaya.dev>
2024-07-06 12:00:16 +0200

    main/openrc-settingsd: update to 1.5.0

--- main/openrc-settingsd/template.py
+++ main/openrc-settingsd/template.py
@@ -1,5 +1,5 @@
 pkgname = "openrc-settingsd"
-pkgver = "1.4.0"
+pkgver = "1.5.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dopenrc=disabled", "-Denv-update="]
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.com/postmarketOS/openrc-settingsd"
 source = f"{url}/-/archive/v{pkgver}/openrc-settingsd-v{pkgver}.tar.gz"
-sha256 = "9d09e02de0faf15ee9a6a742586cf9b1a314591ec50f84e2513636ecfe4f2ff3"
+sha256 = "f4a74262e0012783047ec1d12dfdf4ef3a983e9abaeb436e17703d6881f72292"
 
 
 def post_install(self):
