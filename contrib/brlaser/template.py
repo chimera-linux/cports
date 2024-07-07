3ad930d8f361531b84bc275f78085d39de10ef8d
miko <mikoxyzzz@gmail.com>
2024-07-07 19:49:42 +0200

    contrib/brlaser: new package (6.2.6)

---
+++ contrib/brlaser/template.py
@@ -0,0 +1,13 @@
+pkgname = "brlaser"
+pkgver = "6.2.6"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = ["cmake", "ninja"]
+makedepends = ["cups-devel"]
+pkgdesc = "CUPS driver for monochrome Brother laser printers"
+maintainer = "miko <mikoxyzzz@gmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://github.com/Owl-Maintain/brlaser"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "551f85a234c75c52c52bb76f379eb458ebc7bd7164d55763d97e197a0ad47f6d"
+hardening = ["cfi", "vis"]
