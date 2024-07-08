9eae39f8f1c6ac5e2737518a2940e49a0f4ae5ba
q66 <q66@chimera-linux.org>
2024-07-08 22:06:36 +0200

    main/base-full: merge with base-core, base-minimal

--- main/base-full/template.py
+++ main/base-full/template.py
@@ -1,18 +1,54 @@
 pkgname = "base-full"
-pkgver = "0.2"
-pkgrel = 4
+pkgver = "0.3"
+pkgrel = 0
 build_style = "meta"
-depends = ["base-core"]
+depends = [
+    "base-bootstrap",
+    "dinit-chimera",
+    "turnstile",
+]
+provides = [f"base-core={pkgver}-r{pkgrel}"]
 pkgdesc = "Chimera base package for bare metal and virtual machines"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:meta"
 url = "https://chimera-linux.org"
 
 
+@subpackage("base-full-console")
+def _console(self):
+    self.pkgdesc = f"{pkgdesc} (console tools)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
+    # transitional
+    self.provides = [f"base-core-console={pkgver}-r{pkgrel}"]
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "console-setup",
+        "dmesg",
+        "kbd",
+        "nyagetty",
+    ]
+    return []
+
+
+@subpackage("base-full-core")
+def _core(self):
+    self.pkgdesc = f"{pkgdesc} (core tools)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
+    self.depends = [
+        "bsdtar",
+        "chimera-install-scripts",
+        "procps",
+    ]
+    return []
+
+
 @subpackage("base-full-firmware")
 def _fw(self):
     self.pkgdesc = f"{pkgdesc} (firmware)"
     self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
     self.depends = [
         f"{pkgname}={pkgver}-r{pkgrel}",
         "base-firmware-linux",
@@ -25,13 +61,69 @@ def _fw(self):
     return []
 
 
+@subpackage("base-full-fs")
+def _fs(self):
+    self.pkgdesc = f"{pkgdesc} (filesystem tools)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
+    # transitional
+    self.provides = [f"base-core-fs={pkgver}-r{pkgrel}"]
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "fdisk",
+        "fstrim",
+        "mkfs",
+        "e2fsprogs",
+        "f2fs-tools",
+        "xfsprogs",
+        "btrfs-progs",
+        "dosfstools",
+    ]
+    match self.rparent.profile().arch:
+        case "ppc64" | "ppc":
+            # ppc mac disk tools
+            self.depends += ["hfsutils", "mac-fdisk"]
+    return []
+
+
+@subpackage("base-full-kernel")
+def _kern(self):
+    self.pkgdesc = f"{pkgdesc} (kernel tooling)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
+    # transitional
+    self.provides = [f"base-core-kernel={pkgver}-r{pkgrel}"]
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "base-kernel",
+        "initramfs-tools",
+    ]
+    return []
+
+
 @subpackage("base-full-locale")
 def _locale(self):
     self.pkgdesc = f"{pkgdesc} (locale)"
     self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
     self.depends = [
         f"{pkgname}={pkgver}-r{pkgrel}",
         "base-locale",
+    ]
+    return []
+
+
+@subpackage("base-full-man")
+def _man(self):
+    self.pkgdesc = f"{pkgdesc} (manpages)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
+    # transitional
+    self.provides = [f"base-core-man={pkgver}-r{pkgrel}"]
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "base-man",
+        "man-pages",
     ]
     return []
 
@@ -40,11 +132,20 @@ def _locale(self):
 def _misc(self):
     self.pkgdesc = f"{pkgdesc} (miscellaneous)"
     self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
+    # transitional
+    self.provides = [f"base-core-misc={pkgver}-r{pkgrel}"]
     self.depends = [
         f"{pkgname}={pkgver}-r{pkgrel}",
+        "bc-gh",
         "chimera-artwork",
+        "chimerautils-extra",
         "chrony",
+        "file",
+        "less",
+        "lscpu",
         "nano",
+        "pciutils",
         "syslog-ng",
         "zramctl",
         "opendoas",
@@ -53,14 +154,21 @@ def _misc(self):
     return []
 
 
-@subpackage("base-full-session")
-def _session(self):
-    self.pkgdesc = f"{pkgdesc} (session management)"
+@subpackage("base-full-net-tools")
+def _net(self):
+    self.pkgdesc = f"{pkgdesc} (network tools)"
     self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
+    # transitional
+    self.provides = [f"base-core-net={pkgver}-r{pkgrel}"]
     self.depends = [
         f"{pkgname}={pkgver}-r{pkgrel}",
-        "elogind-meta",
-        "dbus",
+        "ethtool",
+        "iputils",
+        "iproute2",
+        "traceroute",
+        "iw",
+        "rfkill",
     ]
     return []
 
@@ -69,12 +177,51 @@ def _session(self):
 def _net(self):
     self.pkgdesc = f"{pkgdesc} (network)"
     self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
     self.depends = [
         f"{pkgname}={pkgver}-r{pkgrel}",
-        "ethtool",
         "dhcpcd",
         "iwd",
         "openssh",
-        "rfkill",
     ]
+    return []
+
+
+@subpackage("base-full-session")
+def _session(self):
+    self.pkgdesc = f"{pkgdesc} (session management)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "elogind-meta",
+        "dbus",
+    ]
+    return []
+
+
+@subpackage("base-full-sound")
+def _session(self):
+    self.pkgdesc = f"{pkgdesc} (sound)"
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.provider_priority = 100
+    self.depends = [
+        f"{pkgname}={pkgver}-r{pkgrel}",
+        "pipewire",
+    ]
+    return []
+
+
+@subpackage("base-minimal")
+def _minimal(self):
+    self.pkgdesc = f"{pkgdesc} (metapackage for small installations)"
+    self.provides = [
+        f"base-full-firmware={pkgver}-r{pkgrel}",
+        f"base-full-kernel={pkgver}-r{pkgrel}",
+        f"base-full-misc={pkgver}-r{pkgrel}",
+        f"base-full-net={pkgver}-r{pkgrel}",
+        f"base-full-session={pkgver}-r{pkgrel}",
+        f"base-full-sound={pkgver}-r{pkgrel}",
+    ]
+    self.provider_priority = 0
     return []
