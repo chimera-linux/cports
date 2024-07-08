43d99e7f2c6e4414d89d2d3e620e6738a94b0606
Erica Z <zerica@callcc.eu>
2024-07-08 06:10:08 +0200

    contrib/mbuffer: update to 20240707

--- contrib/mbuffer/template.py
+++ contrib/mbuffer/template.py
@@ -1,5 +1,5 @@
 pkgname = "mbuffer"
-pkgver = "20240107"
+pkgver = "20240707"
 pkgrel = 0
 build_style = "cmake"
 make_dir = "."
@@ -9,7 +9,7 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-3.0-only"
 url = "https://www.maier-komor.de/mbuffer.html"
 source = f"https://www.maier-komor.de/software/{pkgname}/{pkgname}-{pkgver}.tgz"
-sha256 = "d78606e17dd9026a53236d5ecc07a70202d2642d17e3d907471cdb58a0458814"
+sha256 = "7f3926e92faa81b96a32420f80288b55f1850480f2a032c16ed61b53267cae09"
 
 
 def pre_configure(self):
