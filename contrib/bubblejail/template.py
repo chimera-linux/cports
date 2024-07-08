a54d6412479cff247d03d336c27e484f074571a9
ttyyls <contact@behri.org>
2024-07-08 02:38:01 +0100

    contrib/bubblejail: update to 0.9.0

--- contrib/bubblejail/template.py
+++ contrib/bubblejail/template.py
@@ -1,11 +1,7 @@
 pkgname = "bubblejail"
-pkgver = "0.8.3"
-pkgrel = 1
+pkgver = "0.9.0"
+pkgrel = 0
 build_style = "meson"
-configure_args = [
-    "-Dbytecode-optimization=0",
-    "-Duse_python_site_packages_dir=true",
-]
 hostmakedepends = [
     "meson",
     "pkgconf",
@@ -19,6 +15,7 @@ depends = [
     "desktop-file-utils",
     "libnotify",
     "libseccomp",
+    "python-lxns",
     "python-pyqt6",
     "python-pyxdg",
     "python-tomli-w",
@@ -29,5 +26,5 @@ pkgdesc = "Bubblewrap based sandboxing for desktop applications"
 maintainer = "ttyyls <contact@behri.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/igo95862/bubblejail"
-source = f"{url}/releases/download/{pkgver}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "6baf82d87a65d9544cf1a64d9be813cc2c21f4af97d6c3124196d6c0fa652983"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "58e77add41f16b4275e4e8ede9eff647cb90f2a3f8eadd177cf6e7ee81483b39"
