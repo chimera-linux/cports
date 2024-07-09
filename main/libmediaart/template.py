13725f5b0cbfdcab3e2a65b99255fb55d91902c9
triallax <triallax@tutanota.com>
2024-07-09 19:28:48 +0100

    main/libmediaart: new package (1.9.6)

---
+++ main/libmediaart/template.py
@@ -0,0 +1,24 @@
+pkgname = "libmediaart"
+pkgver = "1.9.6"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dimage_library=gdk-pixbuf"]
+hostmakedepends = [
+    "gobject-introspection",
+    "meson",
+    "pkgconf",
+    "vala",
+]
+makedepends = ["gdk-pixbuf-devel", "glib-devel"]
+pkgdesc = "Library for handling media art caches"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "LGPL-2.1-or-later"
+url = "https://gitlab.gnome.org/GNOME/libmediaart"
+source = f"$(GNOME_SITE)/libmediaart/{pkgver[:-2]}/libmediaart-{pkgver}.tar.xz"
+sha256 = "c3bc5025d7db380587f9c8eb800c611f6b5a16d6b4b78fcff93f62876a677f17"
+options = ["!cross"]
+
+
+@subpackage("libmediaart-devel")
+def _devel(self):
+    return self.default_devel()
