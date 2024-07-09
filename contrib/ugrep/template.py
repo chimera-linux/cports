cbdd1c9f61b44fd7940ccaa2e82c87f2e635fc83
psykose <alice@ayaya.dev>
2024-07-09 22:55:28 +0200

    contrib/ugrep: new package (6.2.0)

---
+++ contrib/ugrep/template.py
@@ -0,0 +1,35 @@
+pkgname = "ugrep"
+pkgver = "6.2.0"
+pkgrel = 0
+build_style = "gnu_configure"
+make_cmd = "gmake"
+make_dir = "."
+make_check_target = "test"
+hostmakedepends = [
+    "automake",
+    "gmake",
+    "pkgconf",
+]
+makedepends = [
+    "brotli-devel",
+    "bzip2-devel",
+    "linux-headers",
+    "lz4-devel",
+    "pcre2-devel",
+    "xz-devel",
+    "zlib-ng-compat-devel",
+    "zstd-devel",
+]
+checkdepends = ["bash"]
+pkgdesc = "Grep-compatible file searcher"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-3-Clause"
+url = "https://ugrep.com"
+source = f"https://github.com/Genivia/ugrep/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "e7b54e8e7d2d9058167269673fd783651071ba1ace547cf6c926b833607d2e1b"
+# relies on diff interpreting a regex
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
