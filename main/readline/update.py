8f8e25395abb7f17c3a9588b37a41ace643e880b
psykose <alice@ayaya.dev>
2024-07-09 05:54:22 +0200

    main/readline: inline

--- main/readline/update.py
+++ main/readline/update.py
@@ -2,14 +2,13 @@ url = "https://git.savannah.gnu.org/cgit/readline.git/log"
 pattern = r">Readline-([\d\.]+\spatch\s\d+)"
 
 
-def remapmesenpai(v):
-    # truncate garbage
-    ver = v.replace(" patch ", ".")
-    # pad .7 -> .007
-    bigver, patch = ver.rsplit(".", 1)
-    zeropatch = patch.zfill(3)
-    return f"{bigver}.{zeropatch}"
-
-
 def fetch_versions(self, src):
+    def remapmesenpai(v):
+        # truncate garbage
+        ver = v.replace(" patch ", ".")
+        # pad .7 -> .007
+        bigver, patch = ver.rsplit(".", 1)
+        zeropatch = patch.zfill(3)
+        return f"{bigver}.{zeropatch}"
+
     return map(remapmesenpai, self.fetch_versions(src))
