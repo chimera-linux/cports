bbd1d48268138342f80e62144dfd669a80c10fbf
psykose <alice@ayaya.dev>
2024-07-10 00:00:39 +0200

    contrib/python-docstring-to-markdown: new package (0.15)

---
+++ contrib/python-docstring-to-markdown/template.py
@@ -0,0 +1,16 @@
+pkgname = "python-docstring-to-markdown"
+pkgver = "0.15"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "Python module for converting docstrings to markdown"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "LGPL-2.1-or-later"
+url = "https://github.com/python-lsp/docstring-to-markdown"
+source = f"$(PYPI_SITE)/d/docstring-to-markdown/docstring-to-markdown-{pkgver}.tar.gz"
+sha256 = "e146114d9c50c181b1d25505054a8d0f7a476837f0da2c19f07e06eaed52b73d"
