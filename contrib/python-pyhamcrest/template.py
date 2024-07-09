c059c787bbff478150d4a7d10c9dd02e66b0c29f
psykose <alice@ayaya.dev>
2024-07-09 23:36:43 +0200

    contrib/python-pyhamcrest: new package (2.1.0)

---
+++ contrib/python-pyhamcrest/template.py
@@ -0,0 +1,25 @@
+pkgname = "python-pyhamcrest"
+pkgver = "2.1.0"
+pkgrel = 0
+build_style = "python_pep517"
+make_build_env = {"SETUPTOOLS_SCM_PRETEND_VERSION": pkgver}
+hostmakedepends = [
+    "python-build",
+    "python-hatch_vcs",
+    "python-hatchling",
+    "python-installer",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "Python framework for writing matcher objects"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-3-Clause"
+url = "https://hamcrest.org"
+# no tests on pypi
+source = (
+    f"https://github.com/hamcrest/PyHamcrest/archive/refs/tags/V{pkgver}.tar.gz"
+)
+sha256 = "3ce821e78ca4dd0a1d5993186b2cb98ef473ef7910c92116294f8c5f26b6b5ad"
+
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
