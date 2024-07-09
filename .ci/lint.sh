c7be64626ae650ac806654e74976ee81a8bc3fad
psykose <alice@ayaya.dev>
2024-07-09 20:31:36 +0200

    ci: use correct redir for silent command

--- .ci/lint.sh
+++ .ci/lint.sh
@@ -2,7 +2,7 @@
 
 RET=0
 
-if command -v ruff 2>/dev/null; then
+if command -v ruff >/dev/null; then
     ruff check
 else
     flake8 main contrib user src
@@ -12,7 +12,7 @@ if [ $? -ne 0 ]; then
     RET=1
 fi
 
-if command -v ruff 2>/dev/null; then
+if command -v ruff >/dev/null; then
     ruff format --diff
 else
     find main contrib user src -name '*.py' -exec black --fast --check {} +
