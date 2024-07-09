bfd8caeca88dc2b499b1aaaee56b9ccf57ca0840
psykose <alice@ayaya.dev>
2024-07-09 05:34:42 +0200

    contrib/python-jsonschema: update to 4.23.0

--- contrib/python-jsonschema/template.py
+++ contrib/python-jsonschema/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-jsonschema"
-pkgver = "4.22.0"
+pkgver = "4.23.0"
 pkgrel = 0
 build_style = "python_pep517"
 # needs pip
@@ -10,23 +10,28 @@ hostmakedepends = [
     "python-hatchling",
     "python-installer",
 ]
-checkdepends = [
-    "python-pytest",
-    "python-jsonschema-specifications",
-    "python-referencing",
-]
 depends = [
     "python-attrs",
     "python-jsonschema-specifications",
     "python-referencing",
     "python-rpds-py",
 ]
+checkdepends = [
+    "python-pytest-xdist",
+] + depends
 pkgdesc = "Implementation of the JSON Schema specification for Python"
 maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/python-jsonschema/jsonschema"
 source = f"$(PYPI_SITE)/j/jsonschema/jsonschema-{pkgver}.tar.gz"
-sha256 = "5b22d434a45935119af990552c862e5d6d564e8f6601206b305a61fdf661a2b7"
+sha256 = "d71497fef26351a33265337fa77ffeb82423f3ea21283cd9467bb03999266bc4"
+
+
+def init_check(self):
+    self.make_check_args += [
+        f"--numprocesses={self.make_jobs}",
+        "--dist=worksteal",
+    ]
 
 
 def post_install(self):
