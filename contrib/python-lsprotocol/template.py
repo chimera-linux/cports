e70419ac1020fb6a6ee1a24cf8202e2b222e6827
psykose <alice@ayaya.dev>
2024-07-09 23:37:02 +0200

    contrib/python-lsprotocol: new package (2023.0.1)

---
+++ contrib/python-lsprotocol/template.py
@@ -0,0 +1,31 @@
+pkgname = "python-lsprotocol"
+pkgver = "2023.0.1"
+pkgrel = 0
+build_wrksrc = "packages/python"
+build_style = "python_pep517"
+make_check_args = ["../../tests/python"]
+hostmakedepends = [
+    "python-build",
+    "python-flit_core",
+    "python-installer",
+]
+depends = [
+    "python-attrs",
+    "python-cattrs",
+    "python-jsonschema",
+]
+checkdepends = [
+    "python-pyhamcrest",
+    "python-pytest",
+] + depends
+pkgdesc = "Language server protocol types for Python"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/microsoft/lsprotocol"
+# no tests in pypi
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "0697ef66ca1b547cadb4c27080d6095c2e403b9aeb05401de383126a70720a56"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
