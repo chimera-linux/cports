6e0952be56e9fab2c9eab294af0133e65afaef54
psykose <alice@ayaya.dev>
2024-07-09 04:27:30 +0200

    contrib/nautilus-open-any-terminal: update to 0.6.0

--- contrib/nautilus-open-any-terminal/template.py
+++ contrib/nautilus-open-any-terminal/template.py
@@ -1,12 +1,12 @@
 pkgname = "nautilus-open-any-terminal"
-pkgver = "0.5.1"
+pkgver = "0.6.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "gettext",
     "python-build",
     "python-installer",
-    "python-setuptools",
+    "python-setuptools_scm",
     "python-wheel",
 ]
 depends = [
@@ -18,6 +18,10 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-only"
 url = "https://github.com/Stunkymonkey/nautilus-open-any-terminal"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "1f66f7588c22486100e72e1efff96d7b2977ae05a05b14674417a143666c6a62"
+sha256 = "96a1cf9ebd24b3a0b33c70c37f6ab39b11e6d11dc374c9199e54a0b1f19e163a"
 # no tests
 options = ["!check"]
+
+
+def init_build(self):
+    self.env["SETUPTOOLS_SCM_PRETEND_VERSION"] = f"{pkgver}"
