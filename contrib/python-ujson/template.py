3825755bae7686c9329c15ec155142ae04c44ca3
psykose <alice@ayaya.dev>
2024-07-09 23:41:52 +0200

    contrib/python-ujson: new package (5.10.0)

---
+++ contrib/python-ujson/template.py
@@ -0,0 +1,21 @@
+pkgname = "python-ujson"
+pkgver = "5.10.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-devel",
+    "python-installer",
+    "python-setuptools_scm",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "Native json encoder for Python"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "BSD-3-Clause AND TCL"
+url = "https://github.com/ultrajson/ultrajson"
+source = f"$(PYPI_SITE)/u/ujson/ujson-{pkgver}.tar.gz"
+sha256 = "b3cd8f3c5d8c7738257f1018880444f7b7d9b66232c64649f562d7ba86ad4bc1"
+
+
+def post_install(self):
+    self.install_license("LICENSE.txt")
