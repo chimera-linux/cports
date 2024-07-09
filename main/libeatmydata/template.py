88348cf6e85e1f0cb64636eb785a1dbf45b550cc
psykose <alice@ayaya.dev>
2024-07-09 03:54:48 +0200

    main/libeatmydata: new package (131)

---
+++ main/libeatmydata/template.py
@@ -0,0 +1,19 @@
+pkgname = "libeatmydata"
+pkgver = "131"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = ["--disable-static"]
+hostmakedepends = [
+    "automake",
+    "libtool",
+]
+checkdepends = [
+    "bash",
+    "strace",
+]
+pkgdesc = "Preloadable library to stub out fsync"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-3.0-or-later"
+url = "https://www.flamingspork.com/projects/libeatmydata"
+source = f"https://github.com/stewartsmith/libeatmydata/releases/download/v{pkgver}/libeatmydata-{pkgver}.tar.gz"
+sha256 = "cf18a8c52138a38541be3478af446c06048108729d7e18476492d62d54baabc4"
