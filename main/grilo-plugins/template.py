7f74429686ff5e72df14e17ac0abfb94968fd7b5
triallax <triallax@tutanota.com>
2024-07-09 19:28:31 +0100

    main/grilo-plugins: enable some more stuff
    also add some implicit deps

--- main/grilo-plugins/template.py
+++ main/grilo-plugins/template.py
@@ -1,18 +1,11 @@
 pkgname = "grilo-plugins"
 pkgver = "0.3.16"
-pkgrel = 1
+pkgrel = 2
 build_style = "meson"
 configure_args = [
-    # TODO: gom
-    "-Denable-bookmarks=no",
-    "-Denable-thetvdb=no",
-    # TODO: libdmapsharing
-    "-Denable-dmap=no",
     # TODO: libgdata if/when it moves off libsoup2
     # (see https://gitlab.gnome.org/GNOME/libgdata/-/merge_requests/49)
     "-Denable-youtube=no",
-    # TODO: libmediaart
-    "-Denable-local-metadata=no",
     # enable if/when this moves off libsoup2
     "-Denable-opensubtitles=no",
 ]
@@ -26,15 +19,22 @@ hostmakedepends = [
 makedepends = [
     "avahi-glib-devel",
     "gnome-online-accounts-devel",
+    "gom-devel",
     "grilo-devel",
     "gstreamer-devel",
     "json-glib-devel",
+    "libarchive-devel",
+    "libdmapsharing-devel",
+    "libmediaart-devel",
     "liboauth-devel",
     "libsoup-devel",
+    "libxml2-devel",
     "lua5.4-devel",
+    "sqlite-devel",
     "totem-pl-parser-devel",
     "tracker-devel",
 ]
+checkdepends = ["gst-plugins-good"]
 pkgdesc = "Collection of plugins for Grilo"
 maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
@@ -43,6 +43,8 @@ source = (
     f"$(GNOME_SITE)/grilo-plugins/{pkgver[:-3]}/grilo-plugins-{pkgver}.tar.xz"
 )
 sha256 = "fe6f4dbe586c6b8ba2406394e202f22d009d642a96eb3a54f32f6a21d084cdcb"
+# FIXME: Two tests fail
+options = ["!check"]
 
 
 @subpackage("grilo-plugins-devel")
