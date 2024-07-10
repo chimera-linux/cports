f306d3146d4071399bf58373f0cb8a0f03c70074
Gnarwhal <git.aspect893@passmail.net>
2024-07-10 03:39:15 +0000

    contrib/ddclient: new package (3.11.2)

---
+++ contrib/ddclient/template.py
@@ -0,0 +1,24 @@
+pkgname = "ddclient"
+pkgver = "3.11.2"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = [
+    "automake",
+    "curl",
+]
+depends = [
+    "curl",
+    "perl",
+]
+pkgdesc = "Client used to update dynamic DNS entries"
+maintainer = "Gnarwhal <git.aspect893@passmail.net>"
+license = "GPL-2.0-or-later"
+url = "https://ddclient.net"
+source = (
+    f"https://github.com/ddclient/ddclient/archive/refs/tags/v{pkgver}.tar.gz"
+)
+sha256 = "243cd832abd3cdd2b49903e1b5ed7f450e2d9c4c0eaf8ce4fe692c244d3afd77"
+
+
+def post_install(self):
+    self.install_service(self.files_path / "ddclient")
