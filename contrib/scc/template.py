31fa07dcad660c70e607354a2482528afd113d77
Wesley Moore <wes@wezm.net>
2024-07-11 08:34:30 +1000

    contrib/scc: update to 3.3.5

--- contrib/scc/template.py
+++ contrib/scc/template.py
@@ -1,6 +1,6 @@
 pkgname = "scc"
-pkgver = "3.3.4"
-pkgrel = 2
+pkgver = "3.3.5"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Fast and accurate code counter"
@@ -8,7 +8,7 @@ maintainer = "Wesley Moore <wes@wezm.net>"
 license = "MIT OR Unlicense"
 url = "https://github.com/boyter/scc"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "3097e23532d9a254fe217c81557136c7ac5aa4d48a200b61b366330e5eaf7ce4"
+sha256 = "028869a7d053879a8e3f2872fdd792f165db13696918d08863475c638f08ef06"
 # objcopy fails on ppc
 options = ["!debug"]
 
