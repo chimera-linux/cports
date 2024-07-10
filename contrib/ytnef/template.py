bbab5d937420cd6f73bde7e37635613e80357b5b
triallax <triallax@tutanota.com>
2024-07-10 19:05:00 +0100

    contrib/ytnef: split progs, add ytnefprocess rdeps

--- contrib/ytnef/template.py
+++ contrib/ytnef/template.py
@@ -1,6 +1,6 @@
 pkgname = "ytnef"
 pkgver = "2.1.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "libtool", "pkgconf"]
 pkgdesc = "Yerase's TNEF Stream Reader"
@@ -14,3 +14,10 @@ sha256 = "340f03f495884611209e9c0bc943fad06ce920e8c79655aa228d5ca7418dc360"
 @subpackage("ytnef-devel")
 def _devel(self):
     return self.default_devel()
+
+
+@subpackage("ytnef-progs")
+def _progs(self):
+    self.depends += ["perl-mailtools", "perl-mime-tools"]
+
+    return self.default_progs()
