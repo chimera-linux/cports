1628def6471e584a8fe3fa4db578700a5c0205a3
psykose <alice@ayaya.dev>
2024-07-10 03:48:06 +0200

    ci: be a bit more verbose

--- .ci/lint.sh
+++ .ci/lint.sh
@@ -3,8 +3,10 @@
 RET=0
 
 if command -v ruff >/dev/null; then
+    echo "=> Running ruff check"
     ruff check
 else
+    echo "=> Running flake8"
     flake8 main contrib user src
 fi
 
@@ -13,8 +15,10 @@ if [ $? -ne 0 ]; then
 fi
 
 if command -v ruff >/dev/null; then
+    echo "=> Running ruff format --diff"
     ruff format --diff
 else
+    echo "=> Running black --check"
     find main contrib user src -name '*.py' -exec black --fast --check {} +
 fi
 
