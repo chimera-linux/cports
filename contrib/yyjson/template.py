c7dd8e81ac294ca110db36dd16036ee294fb5e27
Jami Kettunen <jami.kettunen@protonmail.com>
2024-07-11 10:34:08 +0300

    contrib/yyjson: new package (0.10.0)

---
+++ contrib/yyjson/template.py
@@ -0,0 +1,21 @@
+pkgname = "yyjson"
+pkgver = "0.10.0"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DYYJSON_BUILD_TESTS=ON"]
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+pkgdesc = "High performance C JSON library"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "MIT"
+url = "https://ibireme.github.io/yyjson"
+source = f"https://github.com/ibireme/yyjson/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "0d901cb2c45c5586e3f3a4245e58c2252d6b24bf4b402723f6179523d389b165"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("yyjson-devel")
+def _devel(self):
+    return self.default_devel()
