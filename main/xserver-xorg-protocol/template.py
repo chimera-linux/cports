0f3da2ba1753843d7a94bdfd7a17794aeb533104
psykose <alice@ayaya.dev>
2024-07-11 06:28:33 +0200

    main/xserver-xorg-protocol: update to 20240503

--- main/xserver-xorg-protocol/template.py
+++ main/xserver-xorg-protocol/template.py
@@ -1,14 +1,15 @@
 # update when xorg/xwayland update calls for it
 pkgname = "xserver-xorg-protocol"
-_commit = "85ff1cdbd3286838e512fe7c70af149a8743b6fe"
-pkgver = "20180227"
+_commit = "53a0442b8700ddcb33caa5d1d17ca9a290000283"
+pkgver = "20240503"
 pkgrel = 0
 pkgdesc = "X server protocol name registry"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"!https://gitlab.freedesktop.org/xorg/xserver/-/raw/{_commit}/dix/protocol.txt"
-sha256 = "b8f6921aaf1d88c74b0cd88295bdfe28b84b432200b8d2fb5b40b0d6eef6016d"
+sha256 = "88b141646fc68627338443a820eeb949149ba73d4ddc925d4717d56a92e7e138"
+options = ["!distlicense"]
 
 
 def do_install(self):
