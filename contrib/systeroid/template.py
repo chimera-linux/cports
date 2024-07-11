aa2e53cf7be49b67c26036746b296c1943330d4e
psykose <alice@ayaya.dev>
2024-07-11 02:03:44 +0200

    contrib/systeroid: new package (0.4.4)

---
+++ contrib/systeroid/template.py
@@ -0,0 +1,24 @@
+pkgname = "systeroid"
+pkgver = "0.4.4"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable"]
+makedepends = ["rust-std"]
+pkgdesc = "TUI and helper tool for sysctl values"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Apache-2.0 OR MIT"
+url = "https://systeroid.cli.rs"
+source = (
+    f"https://github.com/orhun/systeroid/archive/refs/tags/v{pkgver}.tar.gz"
+)
+sha256 = "b638d1167227a30cf793847a08fee42a5810bfb6a493ea0289e29c8260ef916c"
+# needs kernel docs to exist
+options = ["!check"]
+
+
+def do_install(self):
+    self.install_license("LICENSE-MIT")
+    self.install_bin(f"target/{self.profile().triplet}/release/systeroid")
+    self.install_bin(f"target/{self.profile().triplet}/release/systeroid-tui")
+    self.install_man("man8/systeroid.8")
+    self.install_man("man8/systeroid-tui.8")
