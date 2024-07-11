b2a8af6d24ff5ebc9cf061f44f2bd3fd9d2ab7b4
triallax <triallax@tutanota.com>
2024-07-12 00:44:11 +0100

    contrib/xbps: run tests

--- contrib/xbps/template.py
+++ contrib/xbps/template.py
@@ -5,7 +5,7 @@ build_style = "configure"
 configure_args = [
     "--prefix=/usr",
     "--sysconfdir=/etc",
-    "--disable-tests",
+    "--enable-tests",
 ]
 make_cmd = "gmake"
 hostmakedepends = ["pkgconf", "gmake"]
@@ -18,8 +18,10 @@ license = "BSD-2-Clause AND BSD-3-Clause AND MIT"
 url = "https://github.com/void-linux/xbps"
 source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "a6607e83fcd654a0ae846d729e43fefd8da9a61323e91430f884caf895b4f59b"
-# unpackaged checkdepends
-options = ["!check"]
+
+
+def init_check(self):
+    self.make_check_env = {"NPROCS": str(self.make_jobs)}
 
 
 def post_install(self):
