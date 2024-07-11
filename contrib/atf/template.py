632798b6e2f9f2f4ee50911dfea2d8ea13d7fd30
triallax <triallax@tutanota.com>
2024-07-12 00:39:19 +0100

    contrib/atf: new package (0.21)

---
+++ contrib/atf/template.py
@@ -0,0 +1,22 @@
+pkgname = "atf"
+pkgver = "0.21"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["automake", "libtool", "pkgconf"]
+pkgdesc = "Testing library"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "BSD-2-Clause"
+url = "https://github.com/freebsd/atf"
+source = f"{url}/releases/download/atf-{pkgver}/atf-{pkgver}.tar.gz"
+sha256 = "92bc64180135eea8fe84c91c9f894e678767764f6dbc8482021d4dde09857505"
+tool_flags = {"CXXFLAGS": ["-std=gnu++11"]}
+
+
+def post_install(self):
+    self.install_license("COPYING")
+    self.uninstall("usr/tests")
+
+
+@subpackage("atf-devel")
+def _devel(self):
+    return self.default_devel()
