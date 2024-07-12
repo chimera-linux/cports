6aa99c4062525744c53c28ca4c6cf108c47dccc9
psykose <alice@ayaya.dev>
2024-07-12 04:14:57 +0200

    contrib/ruff-lsp: new package (0.0.54)

---
+++ contrib/ruff-lsp/template.py
@@ -0,0 +1,29 @@
+pkgname = "ruff-lsp"
+pkgver = "0.0.54"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-hatchling",
+    "python-installer",
+]
+depends = [
+    "python-lsprotocol",
+    "python-packaging",
+    "python-pygls",
+    "ruff",
+]
+checkdepends = [
+    "python-lsp-jsonrpc",
+    "python-pytest-asyncio",
+] + depends
+pkgdesc = "LSP server for ruff"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/astral-sh/ruff-lsp"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "fa5809a234e96c81da275100556929c64ae38abb18bddb12add8a0c0a24694b4"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
