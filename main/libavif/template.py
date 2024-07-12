9f7296b27cf766dea9f00cf6fb56151085db47be
psykose <alice@ayaya.dev>
2024-07-12 03:29:37 +0200

    main/libavif: update to 1.1.0

--- main/libavif/template.py
+++ main/libavif/template.py
@@ -1,13 +1,15 @@
 pkgname = "libavif"
-pkgver = "1.0.4"
-pkgrel = 1
+pkgver = "1.1.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DAVIF_BUILD_APPS=ON",
     "-DAVIF_BUILD_GDK_PIXBUF=ON",
-    "-DAVIF_CODEC_DAV1D=ON",
-    "-DAVIF_CODEC_AOM=ON",
+    "-DAVIF_CODEC_DAV1D=SYSTEM",
+    "-DAVIF_CODEC_AOM=SYSTEM",
+    "-DAVIF_LIBYUV=OFF",
     "-DAVIF_BUILD_TESTS=ON",
+    "-DAVIF_GTEST=SYSTEM",
     "-DAVIF_ENABLE_GTEST=ON",
 ]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -27,7 +29,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/AOMediaCodec/libavif"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "dc56708c83a4b934a8af2b78f67f866ba2fb568605c7cf94312acf51ee57d146"
+sha256 = "edb31951005d7a143be1724f24825809599a4832073add50eaf987733defb5c8"
 hardening = ["!vis", "!cfi"]
 
 
