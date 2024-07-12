7c22c4ff2decc39a333d204e207a3ff816e372b6
psykose <alice@ayaya.dev>
2024-07-12 13:36:06 +0200

    main/automake: update to 1.17

--- main/automake/template.py
+++ main/automake/template.py
@@ -1,7 +1,9 @@
 pkgname = "automake"
-pkgver = "1.16.5"
-pkgrel = 1
+pkgver = "1.17"
+pkgrel = 0
 build_style = "gnu_configure"
+# circle with itself
+configure_gen = []
 hostmakedepends = ["perl", "autoconf"]
 checkdepends = ["flex", "gettext-devel", "pkgconf"]
 depends = ["perl", "autoconf"]
@@ -9,19 +11,19 @@ pkgdesc = "GNU Standards-compliant Makefile generator"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://www.gnu.org/software/automake"
-source = f"$(GNU_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
-sha256 = "07bd24ad08a64bc17250ce09ec56e921d6343903943e99ccf63bbf0705e34605"
+source = f"$(GNU_SITE)/automake/automake-{pkgver}.tar.xz"
+sha256 = "8920c1fc411e13b90bf704ef9db6f29d540e76d232cb3b2c9f4dc4cc599bd990"
 # flakey, a different set of tests fails every time
 options = ["!check"]
 
 
 def post_install(self):
+    # .patch suffix after second dot is omitted from the script version
+    mver = ".".join(pkgver.split(".")[0:2])
+    # remove hardlinks
     fp = self.destdir / "usr/bin/aclocal"
     fp.unlink()
-    fp.symlink_to(f"aclocal-{pkgver[:-2]}")
+    fp.symlink_to(f"aclocal-{mver}")
     fp = self.destdir / "usr/bin/automake"
     fp.unlink()
-    fp.symlink_to(f"automake-{pkgver[:-2]}")
-
-
-configure_gen = []
+    fp.symlink_to(f"automake-{mver}")
