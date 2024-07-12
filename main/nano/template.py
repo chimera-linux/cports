0272a3a2a4d360aba2fbcdf3f4c492b8284bf920
psykose <alice@ayaya.dev>
2024-07-12 16:13:10 +0200

    main/nano: update to 8.1

--- main/nano/template.py
+++ main/nano/template.py
@@ -1,6 +1,6 @@
 pkgname = "nano"
-pkgver = "8.0"
-pkgrel = 1
+pkgver = "8.1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-utf8"]
 # fails to regen
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.nano-editor.org"
 source = f"https://www.nano-editor.org/dist/v{pkgver[0]}/nano-{pkgver}.tar.xz"
-sha256 = "c17f43fc0e37336b33ee50a209c701d5beb808adc2d9f089ca831b40539c9ac4"
+sha256 = "93b3e3e9155ae389fe9ccf9cb7ab380eac29602835ba3077b22f64d0f0cbe8cb"
 hardening = ["vis", "cfi"]
 
 
