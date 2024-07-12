c849de28afa397d7112b5f9a546288e132863095
psykose <alice@ayaya.dev>
2024-07-12 03:11:08 +0200

    contrib/buildkit: update to 0.15.0

--- contrib/buildkit/template.py
+++ contrib/buildkit/template.py
@@ -1,6 +1,6 @@
 pkgname = "buildkit"
-pkgver = "0.14.1"
-pkgrel = 3
+pkgver = "0.15.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/..."]
 hostmakedepends = ["go"]
@@ -10,7 +10,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/moby/buildkit"
 source = f"https://github.com/moby/buildkit/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5a587973a76173a5e42af0a3d25999596b5374e4b2f373c51cd07e64aa779da0"
+sha256 = "ebf4b82f7379818d8fcfe5c54034e7f93c062c95f663bfd3c2a622583f62a8a4"
 # debug: objcopy ppc64
 # check: cannot work in bwrap
 options = ["!debug", "!check"]
