4a065aa02070b320d589c60cb997b669b2472121
q66 <q66@chimera-linux.org>
2024-07-12 23:06:22 +0200

    contrib/xfce4-screensaver: drop dbus-glib and fix pam path
    For some reason the pam file wasn't installed previously?

--- contrib/xfce4-screensaver/template.py
+++ contrib/xfce4-screensaver/template.py
@@ -1,6 +1,8 @@
 pkgname = "xfce4-screensaver"
 pkgver = "4.18.3"
 pkgrel = 0
+# workaround for lack of gdbus port
+_dbus_gmain_rev = "93e8fced640e29bd6fbcc066a6c854a8dd74f8ab"
 build_style = "gnu_configure"
 configure_args = ["--with-xf86gamma-ext"]
 make_cmd = "gmake"
@@ -16,7 +18,7 @@ hostmakedepends = [
     "xfce4-dev-tools",
 ]
 makedepends = [
-    "dbus-glib-devel",
+    "dbus-devel",
     "elogind-devel",
     "garcon-devel",
     "glib-devel",
@@ -39,5 +41,22 @@ pkgdesc = "Xfce screensaver"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/apps/xfce4-screensaver/start"
-source = f"$(XFCE_SITE)/apps/xfce4-screensaver/{pkgver[:-2]}/xfce4-screensaver-{pkgver}.tar.bz2"
-sha256 = "d171316136a1189dfe69ef3da7f7a7f842014129ece184cc21ffb13bc0e13a39"
+source = [
+    f"$(XFCE_SITE)/apps/xfce4-screensaver/{pkgver[:-2]}/xfce4-screensaver-{pkgver}.tar.bz2",
+    f"https://gitlab.freedesktop.org/dbus/dbus-glib/-/archive/{_dbus_gmain_rev}/dbus-glib-{_dbus_gmain_rev}.tar.gz",
+]
+source_paths = [".", "dbus-gmain"]
+sha256 = [
+    "d171316136a1189dfe69ef3da7f7a7f842014129ece184cc21ffb13bc0e13a39",
+    "b05a1cab9564d5490df3f92d564cec8582e82cc264130a071b0e0a8d9350ec18",
+]
+
+
+def post_extract(self):
+    self.mkdir("src/dbus-gmain")
+    self.cp("dbus-gmain/dbus-gmain.c", "src")
+    self.cp("dbus-gmain/dbus-gmain/dbus-gmain.h", "src/dbus-gmain")
+
+
+def post_install(self):
+    self.rename("etc/pam.d", "usr/lib/pam.d", relative=False)
