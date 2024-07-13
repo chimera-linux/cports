1271a9d1f5573d0a4a718def703d6aa4df27f73d
ttyyls <contact@behri.org>
2024-07-13 02:18:59 +0100

    user/got: update to 0.101

--- user/got/template.py
+++ user/got/template.py
@@ -1,5 +1,5 @@
 pkgname = "got"
-pkgver = "0.100"
+pkgver = "0.101"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -30,7 +30,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "ISC"
 url = "https://gameoftrees.org"
 source = f"{url}/releases/portable/got-portable-{pkgver}.tar.gz"
-sha256 = "fc3a8a2067ff699d3d68bfeb07bb5ef806ae1e627e98e4eb5446e4a93f565012"
+sha256 = "25064182c731a0cbf80e48bbeecf2d628e2be41046f84aec0d89d8e7f6a6dcc0"
 env = {"GOT_RELEASE": "Yes"}
 hardening = ["vis", "cfi"]
 # tests require pre-installing got/tog in pre_check and running ssh locally
