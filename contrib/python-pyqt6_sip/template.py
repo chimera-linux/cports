4513a4e9bb2b91ef614e706ab549fec190125bda
psykose <alice@ayaya.dev>
2024-07-13 08:39:10 +0200

    contrib/python-pyqt6_sip: update to 13.8.0

--- contrib/python-pyqt6_sip/template.py
+++ contrib/python-pyqt6_sip/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pyqt6_sip"
-pkgver = "13.6.0"
-pkgrel = 1
+pkgver = "13.8.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -16,7 +16,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "custom:sip"
 url = "https://www.riverbankcomputing.com/software/sip"
 source = f"$(PYPI_SITE)/P/PyQt6_sip/PyQt6_sip-{pkgver}.tar.gz"
-sha256 = "2486e1588071943d4f6657ba09096dc9fffd2322ad2c30041e78ea3f037b5778"
+sha256 = "2f74cf3d6d9cab5152bd9f49d570b2dfb87553ebb5c4919abfde27f5b9fd69d4"
 # no tests
 options = ["!check"]
 
