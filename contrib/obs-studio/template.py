b5d010e0d7f6d23780a6bc4e5f042a7b4526332a
psykose <alice@ayaya.dev>
2024-07-13 08:47:16 +0200

    contrib/obs-studio: update to 30.2.0

--- contrib/obs-studio/template.py
+++ contrib/obs-studio/template.py
@@ -1,20 +1,22 @@
 pkgname = "obs-studio"
-pkgver = "30.1.2"
-pkgrel = 2
+pkgver = "30.2.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
     f"-DOBS_VERSION_OVERRIDE={pkgver}",
     "-DENABLE_ALSA=OFF",
     "-DENABLE_AJA=OFF",
-    "-DENABLE_WEBRTC=ON",
     "-DENABLE_JACK=ON",
+    # won't work without proprietary nvidia drivers
+    "-DENABLE_NATIVE_NVENC=OFF",
+    # TODO: onevpl
+    "-DENABLE_QSV11=OFF",
     "-DENABLE_SCRIPTING=ON",
     "-DENABLE_SCRIPTING_LUA=OFF",
     "-DENABLE_SCRIPTING_PYTHON=ON",
     "-DENABLE_VLC=OFF",
-    # TODO: onevpl
-    "-DENABLE_QSV11=OFF",
+    "-DENABLE_WEBRTC=ON",
     "-DOPENGL_opengl_LIBRARY=/usr/lib/libGL.so",
 ]
 hostmakedepends = [
@@ -40,6 +42,7 @@ makedepends = [
     "libxkbcommon-devel",
     "mbedtls-devel",
     "mesa-devel",
+    "nlohmann-json",
     "pciutils-devel",
     "pipewire-devel",
     "pipewire-jack-devel",
@@ -50,6 +53,7 @@ makedepends = [
     "speexdsp-devel",
     "srt-devel",
     "udev-devel",
+    "uthash",
     "v4l-utils-devel",
     "wayland-devel",
     "x264-devel",
@@ -59,7 +63,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://obsproject.com"
 source = f"https://github.com/obsproject/obs-studio/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "490bae1c392b3b344b0270afd8cb887da4bc50bd92c0c426e96713c1ccb9701a"
+sha256 = "a94e585aa9e1c300a9294df80d9370c1d83eb64d9c331002968c6422019821dc"
 hardening = ["vis", "!cfi"]
 # don't build with lto
 options = ["!check"]
