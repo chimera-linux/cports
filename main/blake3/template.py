e5c36d6f6acdb3242c8008d379c914cc02c1e2ea
psykose <alice@ayaya.dev>
2024-07-13 08:35:47 +0200

    main/blake3: update to 1.5.2

--- main/blake3/template.py
+++ main/blake3/template.py
@@ -1,6 +1,6 @@
 pkgname = "blake3"
-pkgver = "1.5.1"
-pkgrel = 1
+pkgver = "1.5.2"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
 cmake_dir = "c"
@@ -13,10 +13,8 @@ pkgdesc = "BLAKE3 cryptographic hash implementation"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "CC0-1.0 OR Apache-2.0"
 url = "https://github.com/BLAKE3-team/BLAKE3"
-source = (
-    f"https://github.com/BLAKE3-team/BLAKE3/archive/refs/tags/{pkgver}.tar.gz"
-)
-sha256 = "822cd37f70152e5985433d2c50c8f6b2ec83aaf11aa31be9fe71486a91744f37"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "1bc38cd3cc501458054c3bb473e5e00338d6175121424040079025ea305ddef3"
 hardening = ["vis", "cfi"]
 
 
