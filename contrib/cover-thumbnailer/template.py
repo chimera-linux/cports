3b18c5853812f4aa63f4e875b5b375d29c21418e
triallax <triallax@tutanota.com>
2024-07-13 01:13:17 +0100

    contrib/cover-thumbnailer: new package (0.10.1)

---
+++ contrib/cover-thumbnailer/template.py
@@ -0,0 +1,15 @@
+pkgname = "cover-thumbnailer"
+pkgver = "0.10.1"
+pkgrel = 0
+hostmakedepends = ["bash", "gettext"]
+depends = ["gtk+3", "python-gobject", "python-pillow"]
+pkgdesc = "Thumbnailer for music album and picture folder covers"
+maintainer = "triallax <triallax@tutanota.com>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/flozz/cover-thumbnailer"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "3b5fd373554376a74ea88bb42ad21f0d109b456ed660fe9703468523e1eda279"
+
+
+def do_install(self):
+    self.do("./install.sh", "--package", self.chroot_destdir)
