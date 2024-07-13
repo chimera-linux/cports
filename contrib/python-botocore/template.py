fbf664a03d75fd89a477b6874df4aee091e1c521
ttyyls <contact@behri.org>
2024-07-13 02:21:07 +0100

    contrib/python-botocore: update to 1.34.144

--- contrib/python-botocore/template.py
+++ contrib/python-botocore/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-botocore"
-pkgver = "1.34.143"
+pkgver = "1.34.144"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -19,6 +19,6 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/boto/botocore"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "5e02b6facf4a418f8b156552227bebe4bbe519605e7783a3d55d9600f44a9536"
+sha256 = "01f8beca28c5b76c1f34adcc120c1a5d3f6eb38dda8fd10bf19035d2f42fb8cf"
 # takes forever
 options = ["!check"]
