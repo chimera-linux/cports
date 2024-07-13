b165036f739afb3c3a3fba80161e737a7504f4d0
Erica Z <zerica@callcc.eu>
2024-07-13 18:04:34 +0200

    contrib/sdl1.2_mixer: new package (1.2.12)

---
+++ contrib/sdl1.2_mixer/template.py
@@ -0,0 +1,32 @@
+pkgname = "sdl1.2_mixer"
+pkgver = "1.2.12"
+pkgrel = 0
+build_style = "gnu_configure"
+# make sure they're pulled as runtime deps
+configure_args = [
+    "--disable-music-mod-shared",
+    "--disable-music-ogg-shared",
+    "--disable-music-flac-shared",
+]
+configure_gen = ["./autogen.sh"]
+make_cmd = "gmake"
+hostmakedepends = ["automake", "gmake", "pkgconf"]
+makedepends = [
+    "flac-devel",
+    "libmikmod-devel",
+    "libvorbis-devel",
+    "sdl12-compat-devel",
+]
+pkgdesc = "SDL 1.2 audio mixer library"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "Zlib"
+url = "https://www.libsdl.org/projects/old/SDL_mixer/release-1.2.html"
+source = f"https://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-{pkgver}.tar.gz"
+sha256 = "1644308279a975799049e4826af2cfc787cad2abb11aa14562e402521f86992a"
+# no tests
+options = ["!check"]
+
+
+@subpackage("sdl1.2_mixer-devel")
+def _devel(self):
+    return self.default_devel()
