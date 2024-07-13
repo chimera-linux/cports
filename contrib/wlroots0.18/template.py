99eff34df882d69b55a475e4ff410fcdce112a59
psykose <alice@ayaya.dev>
2024-07-13 06:46:02 +0200

    contrib/wlroots0.18: new package (0.18.0)

---
+++ contrib/wlroots0.18/template.py
@@ -0,0 +1,55 @@
+pkgname = "wlroots0.18"
+pkgver = "0.18.0"
+pkgrel = 0
+build_style = "meson"
+configure_args = [
+    # all auto features are needed,
+    # don't accidentally end up with them disabled
+    "--auto-features=enabled",
+    "-Dexamples=false",
+]
+hostmakedepends = [
+    "glslang-progs",
+    "meson",
+    "pkgconf",
+    "xwayland-devel",
+]
+makedepends = [
+    "hwdata-devel",
+    "lcms2-devel",
+    "libdisplay-info-devel",
+    "libdrm-devel",
+    "libgbm-devel",
+    "libinput-devel",
+    "libliftoff-devel",
+    "libseat-devel",
+    "libxcb-devel",
+    "libxkbcommon-devel",
+    "mesa-devel",
+    "pixman-devel",
+    "udev-devel",
+    "vulkan-headers",
+    "vulkan-loader-devel",
+    "wayland-devel",
+    "wayland-protocols",
+    "xcb-util-errors-devel",
+    "xcb-util-renderutil-devel",
+    "xcb-util-wm-devel",
+]
+provides = [f"wlroots={pkgver}-r{pkgrel}"]
+pkgdesc = "Modular Wayland compositor library 0.18.x"
+maintainer = "flukey <flukey@vapourmail.eu>"
+license = "MIT"
+url = "https://gitlab.freedesktop.org/wlroots/wlroots"
+source = f"https://gitlab.freedesktop.org/wlroots/wlroots/-/releases/{pkgver}/downloads/wlroots-{pkgver}.tar.gz"
+sha256 = "89e13735d83b02f0fa519268b1fe893f7843499a5ea34d2bbda054a011722e53"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("wlroots0.18-devel")
+def _devel(self):
+    self.provides = [f"wlroots-devel={pkgver}-r{pkgrel}"]
+    return self.default_devel()
