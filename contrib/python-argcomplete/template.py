6bbf345206b8e1bc944b419f9f8fb68ff3d47b75
triallax <triallax@tutanota.com>
2024-07-14 02:51:38 +0100

    contrib/python-argcomplete: run tests

--- contrib/python-argcomplete/template.py
+++ contrib/python-argcomplete/template.py
@@ -10,18 +10,18 @@ hostmakedepends = [
     "python-wheel",
 ]
 depends = ["python"]
-checkdepends = [
-    "python-pytest",
-]
+checkdepends = ["bash", "python-pexpect", "python-pip", "zsh"]
 pkgdesc = "Python tab completion plugin"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/kislyuk/argcomplete"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "a66504125897239587a5422e31535698704baa67d54604557b17176d56bc69bd"
-# missing pexpect
-options = ["!check"]
 
 
 def init_build(self):
-    self.env["SETUPTOOLS_SCM_PRETEND_VERSION"] = f"{pkgver}"
+    self.env["SETUPTOOLS_SCM_PRETEND_VERSION"] = pkgver
+
+
+def do_check(self):
+    self.do("python", "-m", "unittest", "-b", "test/test.py")
