a2bd1c3070515c9fcc78238a362af6a4dcfcdf77
psykose <alice@ayaya.dev>
2024-07-14 04:03:53 +0200

    contrib/python-ytmusicapi: update to 1.7.5

--- contrib/python-ytmusicapi/template.py
+++ contrib/python-ytmusicapi/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-ytmusicapi"
-pkgver = "1.7.4"
+pkgver = "1.7.5"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,7 +14,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://github.com/sigma67/ytmusicapi"
 source = f"$(PYPI_SITE)/y/ytmusicapi/ytmusicapi-{pkgver}.tar.gz"
-sha256 = "a0abb52d5d652224a45eb172e4767e06eb49bec119bd7417b6b002c042c65454"
+sha256 = "b306c330bdf81888f01d9f62fea96bf19bdd521999536988e98dbf6be41009fe"
 # like all tests need net
 options = ["!check"]
 
