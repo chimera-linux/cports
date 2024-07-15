3bd8044fb0ba8f72d837a0fa1cd1e7decd5cd779
psykose <alice@ayaya.dev>
2024-07-15 10:07:23 +0200

    main/linux-headers-cross: update to 6.10

--- main/linux-headers-cross/template.py
+++ main/linux-headers-cross/template.py
@@ -1,5 +1,5 @@
 pkgname = "linux-headers-cross"
-pkgver = "6.9.9"
+pkgver = "6.10"
 pkgrel = 0
 hostmakedepends = ["gmake", "perl"]
 depends = []
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://www.kernel.org"
 source = f"$(KERNEL_SITE)/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
-sha256 = "2be05b487eb239a3bf687d628a8f104177d09c310f00bcc2a5e50f1733421eb9"
+sha256 = "774698422ee54c5f1e704456f37c65c06b51b4e9a8b0866f34580d86fef8e226"
 # nothing to test
 options = ["!cross", "!check", "empty"]
 
