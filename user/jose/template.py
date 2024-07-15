cf089af413f59af3f5af4d6e8cb094636f08d86e
Matthias Nyman <natthias@proton.me>
2024-07-15 17:58:53 +0200

    user/jose: new package (14)

---
+++ user/jose/template.py
@@ -0,0 +1,29 @@
+pkgname = "jose"
+pkgver = "14"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "asciidoc",
+    "libxml2-progs",
+    "meson",
+    "pkgconf",
+]
+makedepends = [
+    "jansson-devel",
+    "openssl-devel",
+    "zlib-ng-compat-devel",
+]
+checkdepends = ["jq"]
+pkgdesc = "JSON Object Signing and Encryption standards in C"
+maintainer = "natthias <natthias@proton.me>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/latchset/jose"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "3ffb8ea9a0fa5194051499d96bfde957621fcd490ef7ed95effc699029d7ad3b"
+# vis breaks symbols
+hardening = ["!vis", "!cfi"]
+
+
+@subpackage("jose-devel")
+def _(self):
+    return self.default_devel()
