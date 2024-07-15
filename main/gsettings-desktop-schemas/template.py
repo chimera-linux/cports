fe468a2aeac82038973643155f03129c274365df
psykose <alice@ayaya.dev>
2024-07-15 15:25:31 +0200

    main/gsettings-desktop-schemas: update to 46.1

--- main/gsettings-desktop-schemas/template.py
+++ main/gsettings-desktop-schemas/template.py
@@ -1,26 +1,32 @@
 pkgname = "gsettings-desktop-schemas"
-pkgver = "46.0"
+pkgver = "46.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=true"]
-hostmakedepends = ["meson", "pkgconf", "glib-devel", "gobject-introspection"]
+hostmakedepends = [
+    "gettext",
+    "meson",
+    "pkgconf",
+    "glib-devel",
+    "gobject-introspection",
+]
 makedepends = ["glib-devel"]
 depends = [
-    "fonts-cantarell-otf",
-    "fonts-source-code-pro-otf",
     "adwaita-icon-theme",
     "chimera-artwork",
+    "fonts-cantarell-otf",
+    "fonts-source-code-pro-otf",
 ]
 pkgdesc = "Collection of GSettings schemas"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas"
-source = f"$(GNOME_SITE)/{pkgname}/{pkgver[:-2]}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "493a46a1161b6388d57aa72f632a79ce96c42d5ffbd1d0b00f496ec5876f8575"
+source = f"$(GNOME_SITE)/gsettings-desktop-schemas/{pkgver[:-2]}/gsettings-desktop-schemas-{pkgver}.tar.xz"
+sha256 = "9b88101437a6958ebe6bbd812e49bbf1d09cc667011e415559d847e870468a61"
 options = ["!cross"]
 
 
 @subpackage("gsettings-desktop-schemas-devel")
 def _devel(self):
-    self.depends += [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.depends += [self.parent]
     return self.default_devel()
