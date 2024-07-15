1bdfe653b7729bba17e4561a68b83002436af7df
triallax <triallax@tutanota.com>
2024-07-15 19:02:49 +0100

    user/harec: update to 0.24.2

--- user/harec/template.py
+++ user/harec/template.py
@@ -1,5 +1,5 @@
 pkgname = "harec"
-pkgver = "0.24.0"
+pkgver = "0.24.2"
 pkgrel = 0
 archs = ["aarch64", "riscv64", "x86_64"]
 build_style = "makefile"
@@ -13,7 +13,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-3.0-only"
 url = "https://git.sr.ht/~sircmpwn/harec"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "add6a7c4cbfd130c5e9fcecd2d43bec39640ed9f9cfbe9166e4b7e945a46b7de"
+sha256 = "88b3961b236fbfe3a0dfb46bb954741fa5c031bbda6d07fbc238c98f0abb41a2"
 tool_flags = {
     # Taken from configs/linux.mk
     "CFLAGS": ["-std=c11", "-D_XOPEN_SOURCE=700", "-Iinclude"],
