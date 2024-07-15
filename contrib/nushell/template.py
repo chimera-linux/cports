1063e4033abf5676b7403ec963e44142cf835007
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-07-15 09:33:44 +0200

    contrib/nushell: new package (0.95.0)

---
+++ contrib/nushell/template.py
@@ -0,0 +1,22 @@
+pkgname = "nushell"
+pkgver = "0.95.0"
+pkgrel = 0
+build_style = "cargo"
+make_env = {
+    "PREFIX": "/usr",
+}
+hostmakedepends = ["cargo-auditable", "pkgconf"]
+makedepends = ["libgit2-devel", "openssl-devel", "sqlite-devel", "zstd-devel"]
+pkgdesc = "Shell with a focus on structured data"
+maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
+license = "MIT"
+url = "https://www.nushell.sh"
+source = f"https://github.com/nushell/nushell/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "f41a0f41af3996581f9bd485cfe5d55f26dd486dc3812b386bd43439c72a6d16"
+# Checks fail with libgit2 < 1.8.1
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_shell("/usr/bin/nu")
+    self.install_license("LICENSE")
