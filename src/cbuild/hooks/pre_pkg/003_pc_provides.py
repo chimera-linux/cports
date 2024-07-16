062b34a79163be918989d83d1f6a6ded6689e51b
q66 <q66@chimera-linux.org>
2024-07-16 12:19:55 +0200

    cbuild: handle pkg-config providers correctly

--- src/cbuild/hooks/pre_pkg/003_pc_provides.py
+++ src/cbuild/hooks/pre_pkg/003_pc_provides.py
@@ -36,7 +36,7 @@ def invoke(pkg):
         cdv = pkg.chroot_destdir / rlp
         pcc = chroot.enter(
             "pkg-config",
-            "--modversion",
+            "--print-provides",
             sn,
             capture_output=True,
             bootstrapping=False,
@@ -50,20 +50,29 @@ def invoke(pkg):
         )
         if pcc.returncode != 0:
             pkg.error("failed scanning .pc files (missing pkgconf?)")
-        # sanitize version for apk
-        mver = pcc.stdout.decode().strip()
-        mver = re.sub("-(alpha|beta|rc|pre)", "_\\1", mver)
-        # fallback
-        if len(mver) == 0 or pkg.alternative:
-            mver = "0"
-        elif not cli.check_version(mver):
-            # test with apk
-            pkg.error(f"invalid pkgconf version {mver}")
-        if sn in pcset:
-            logger.get().out_plain(f"   pc: {sn}={mver} from {rlp} (skipped)")
-        else:
-            pcs[sn] = f"{sn}={mver}"
-            logger.get().out_plain(f"   pc: {sn}={mver} from {rlp}")
+        # parse the output
+        for ln in pcc.stdout.strip().splitlines():
+            plist = ln.decode().split(" = ")
+            if len(plist) != 2:
+                pkg.error(
+                    f"failed scanning .pc files (invalid provider '{ln}' in '{sn}'"
+                )
+            pname, mver = plist
+            # sanitize version for apk
+            mver = re.sub("-(alpha|beta|rc|pre)", "_\\1", mver)
+            # fallback
+            if len(mver) == 0 or pkg.alternative:
+                mver = "0"
+            elif not cli.check_version(mver):
+                # test with apk
+                pkg.error(f"invalid pkgconf version {mver}")
+            if pname in pcset:
+                logger.get().out_plain(
+                    f"   pc: {pname}={mver} from {rlp} (skipped)"
+                )
+            else:
+                pcs[pname] = f"{pname}={mver}"
+                logger.get().out_plain(f"   pc: {pname}={mver} from {rlp}")
 
     for f in pkg.destdir.glob("usr/lib/pkgconfig/*.pc"):
         scan_pc(f)
