70ee8d6c339c010c3ca11b6f50bdbab936d5bd7b
psykose <alice@ayaya.dev>
2024-07-17 03:12:40 +0200

    main/python-nftables: update to 1.1.0

--- main/python-nftables/template.py
+++ main/python-nftables/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-nftables"
-pkgver = "1.0.9"
-pkgrel = 1
+pkgver = "1.1.0"
+pkgrel = 0
 build_wrksrc = "py"
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND GPL-2.0-or-later"
 url = "https://netfilter.org/projects/nftables"
 source = f"{url}/files/nftables-{pkgver}.tar.xz"
-sha256 = "a3c304cd9ba061239ee0474f9afb938a9bb99d89b960246f66f0c3a0a85e14cd"
+sha256 = "ef3373294886c5b607ee7be82c56a25bc04e75f802f8e8adcd55aac91eb0aa24"
 # no tests
 options = ["!check"]
 
