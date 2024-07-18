899e70c9a9d728a2dca335c8e03f5916497af748
psykose <alice@ayaya.dev>
2024-07-18 10:18:46 +0200

    contrib/igsc: update to 0.8.19

--- contrib/igsc/template.py
+++ contrib/igsc/template.py
@@ -1,5 +1,5 @@
 pkgname = "igsc"
-pkgver = "0.8.18"
+pkgver = "0.8.19"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DSYSLOG=OFF", "-DENABLE_WERROR=OFF"]
@@ -17,7 +17,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/intel/igsc"
 source = f"{url}/archive/refs/tags/V{pkgver}.tar.gz"
-sha256 = "8c6460ac3dedf7f12b4a278bead9043f56fa8a7309425a4eb97a6a2245d1b74d"
+sha256 = "e3bf75b940a72f2e84b696185ddb265a41852820d548314a41c7b183aa940eb1"
 
 
 @subpackage("igsc-devel")
