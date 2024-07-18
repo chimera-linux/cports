b32cf2fcc8f330ba7070b6220706721de3fd2258
psykose <alice@ayaya.dev>
2024-07-18 16:13:30 +0200

    contrib/tuba: update to 0.8.2

--- contrib/tuba/template.py
+++ contrib/tuba/template.py
@@ -1,6 +1,6 @@
 pkgname = "tuba"
-pkgver = "0.8.1"
-pkgrel = 1
+pkgver = "0.8.2"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dclapper=true",
@@ -30,4 +30,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later"
 url = "https://tuba.geopjr.dev"
 source = f"https://github.com/GeopJr/Tuba/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "2c52448061bc82e2a7690faa2bd16e1d3cb506bd0b1a2b1ed32623051710ba06"
+sha256 = "b5791df008bc8ae7e1947cbd509b3bb905c4957c6dd65747676f85a4a2d3c3fe"
