48d847108b03785177a08be1da16f74315769bb3
q66 <q66@chimera-linux.org>
2024-07-19 11:06:02 +0200

    contrib/kaccounts-providers: make webengine optional

--- contrib/kaccounts-providers/template.py
+++ contrib/kaccounts-providers/template.py
@@ -15,7 +15,6 @@ makedepends = [
     "kio-devel",
     "kpackage-devel",
     "libaccounts-qt-devel",
-    "qt6-qtwebengine-devel",
     "signond-devel",
 ]
 pkgdesc = "KDE providers for online accounts"
@@ -25,3 +24,7 @@ url = "https://invent.kde.org/network/kaccounts-providers"
 source = f"$(KDE_SITE)/release-service/{pkgver}/src/kaccounts-providers-{pkgver}.tar.xz"
 sha256 = "60ee944bec87a60da3b860584f5c61d06983455169d89b5356034ae4216dd1d5"
 hardening = ["vis"]
+
+
+if self.profile().arch in ["aarch64", "ppc64le", "x86_64"]:
+    makedepends += ["qt6-qtwebenigne-devel"]
