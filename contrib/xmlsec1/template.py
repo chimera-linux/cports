7e6346eb8f4049d6a273955ed7cfc071a5351794
psykose <alice@ayaya.dev>
2024-07-19 03:58:53 +0200

    contrib/xmlsec1: update to 1.3.5

--- contrib/xmlsec1/template.py
+++ contrib/xmlsec1/template.py
@@ -1,8 +1,8 @@
 pkgname = "xmlsec1"
-pkgver = "1.3.4"
+pkgver = "1.3.5"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_args = ["--disable-docs"]
+configure_args = ["--disable-docs", "--enable-http"]
 hostmakedepends = ["pkgconf", "automake", "libtool", "gnutls-devel"]
 makedepends = [
     "gnutls-devel",
@@ -18,7 +18,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.aleksey.com/xmlsec"
 source = f"{url}/download/{pkgname}-{pkgver}.tar.gz"
-sha256 = "45ad9078d41ae76844ad2f8651600ffeec0fdd128ead988a8d69e907c57aee75"
+sha256 = "2ffd4ad1f860ec93e47a680310ab2bc94968bd07566e71976bd96133d9504917"
 # broken tests build + tests reach internet
 options = ["!cross", "!check"]
 
