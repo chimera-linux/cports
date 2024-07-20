b74f3ebac6a1e86cf4019666d52979f3c5d57f0f
q66 <q66@chimera-linux.org>
2024-07-20 14:49:18 +0200

    contrib/plasma-firewall: drop pointless ufw subpkg

--- contrib/plasma-firewall/template.py
+++ contrib/plasma-firewall/template.py
@@ -1,6 +1,6 @@
 pkgname = "plasma-firewall"
 pkgver = "6.1.3"
-pkgrel = 1
+pkgrel = 2
 build_style = "cmake"
 make_check_wrapper = ["wlheadless-run", "--"]
 hostmakedepends = [
@@ -25,13 +25,3 @@ license = "GPL-3.0-only"
 url = "https://invent.kde.org/plasma/plasma-firewall"
 source = f"$(KDE_SITE)/plasma/{pkgver}/plasma-firewall-{pkgver}.tar.xz"
 sha256 = "f2a0a74e01969630cd21a0f03697a924ea33d6da2f92b08d900d28b97843cf93"
-
-
-# TODO: it also supports firewalld but i did not test that
-@subpackage("plasma-firewall-ufw")
-def _ufw(self):
-    self.subdesc = "ufw support"
-    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}", "ufw"]
-    self.depends += ["ufw"]
-    self.options = ["empty"]
-    return []
