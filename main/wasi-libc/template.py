a21cb060ec4d80ca95cfe391cccf20146c5a11d6
psykose <alice@ayaya.dev>
2024-07-20 15:51:43 +0200

    main/wasi-libc: update to 0.20240708

--- main/wasi-libc/template.py
+++ main/wasi-libc/template.py
@@ -1,7 +1,7 @@
 pkgname = "wasi-libc"
-pkgver = "0.20240412"
-pkgrel = 2
-_gitrev = "9e8c542319242a5e536e14e6046de5968d298038"
+pkgver = "0.20240708"
+pkgrel = 0
+_gitrev = "3f43ea9abb24ed8d24d760989e1d87ea385f8eaa"
 make_cmd = "gmake"
 hostmakedepends = ["gmake"]
 pkgdesc = "WebAssembly libc implementation"
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0 WITH LLVM-exception AND Apache-2.0 AND MIT AND CC0-1.0 AND BSD-2-Clause"
 url = "https://github.com/WebAssembly/wasi-libc"
 source = f"{url}/archive/{_gitrev}.tar.gz"
-sha256 = "d86d115be80cc796da0e1b7d63f192da91e17927cf24273be7d89b2a9284ad4c"
+sha256 = "8c02329d63e23a8aa7187e83f70daa2eed69cdc84ebcc753201c2caf4fb1b207"
 # no tests
 options = ["!check", "!lto"]
 
