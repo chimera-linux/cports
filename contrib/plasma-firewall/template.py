21186959da9628c02bb2ea086dc6a5308bb896f3
q66 <q66@chimera-linux.org>
2024-07-20 12:25:59 +0200

    contrib/plasma-firewall: only install ufw package when have ufw

--- contrib/plasma-firewall/template.py
+++ contrib/plasma-firewall/template.py
@@ -1,6 +1,6 @@
 pkgname = "plasma-firewall"
 pkgver = "6.1.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 make_check_wrapper = ["wlheadless-run", "--"]
 hostmakedepends = [
@@ -31,7 +31,7 @@ sha256 = "f2a0a74e01969630cd21a0f03697a924ea33d6da2f92b08d900d28b97843cf93"
 @subpackage("plasma-firewall-ufw")
 def _ufw(self):
     self.subdesc = "ufw support"
-    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    self.install_if = [f"{pkgname}={pkgver}-r{pkgrel}", "ufw"]
     self.depends += ["ufw"]
     self.options = ["empty"]
     return []
