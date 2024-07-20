ff763f013862ec4a3d6c75234e6dd9b096358ff2
psykose <alice@ayaya.dev>
2024-07-20 13:59:11 +0200

    main/lensfun: remove sse disable
    seems to work

--- main/lensfun/template.py
+++ main/lensfun/template.py
@@ -1,6 +1,6 @@
 pkgname = "lensfun"
 pkgver = "0.3.4"
-pkgrel = 1
+pkgrel = 2
 build_style = "cmake"
 configure_args = ["-DINSTALL_HELPER_SCRIPTS=OFF", "-DBUILD_TESTS=ON"]
 hostmakedepends = ["cmake", "ninja", "pkgconf", "python-setuptools"]
@@ -13,11 +13,6 @@ source = (
     f"https://github.com/lensfun/lensfun/archive/refs/tags/v{pkgver}.tar.gz"
 )
 sha256 = "dafb39c08ef24a0e2abd00d05d7341b1bf1f0c38bfcd5a4c69cf5f0ecb6db112"
-
-# tests segfault with altivec simd
-match self.profile().arch:
-    case "x86_64":
-        configure_args += ["-DBUILD_FOR_SSE=OFF", "-DBUILD_FOR_SSE2=OFF"]
 
 
 @subpackage("lensfun-devel")
