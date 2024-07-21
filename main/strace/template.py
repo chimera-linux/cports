2c3d64b2a46fb5636825d1aef6eeafb67320fa5c
psykose <alice@ayaya.dev>
2024-07-21 22:41:01 +0200

    main/strace: update to 6.10

--- main/strace/template.py
+++ main/strace/template.py
@@ -1,6 +1,6 @@
 pkgname = "strace"
-pkgver = "6.9"
-pkgrel = 1
+pkgver = "6.10"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--with-libdw",
@@ -23,5 +23,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://strace.io"
 source = f"https://github.com/strace/strace/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.xz"
-sha256 = "da189e990a82e3ca3a5a4631012f7ecfd489dab459854d82d8caf6a865c1356a"
+sha256 = "765ec71aa1de2fe37363c1e40c7b7669fc1d40c44bb5d38ba8e8cd82c4edcf07"
 hardening = ["vis", "cfi"]
