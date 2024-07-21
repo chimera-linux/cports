f4da0bbd4585d52f8fd0d01ef7736c7959c9b4a6
psykose <alice@ayaya.dev>
2024-07-21 06:14:43 +0200

    main/python-sphinxcontrib-htmlhelp: update to 2.0.6

--- main/python-sphinxcontrib-htmlhelp/template.py
+++ main/python-sphinxcontrib-htmlhelp/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-sphinxcontrib-htmlhelp"
-pkgver = "2.0.5"
+pkgver = "2.0.6"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-flit_core", "python-installer"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://www.sphinx-doc.org"
 source = f"$(PYPI_SITE)/s/sphinxcontrib-htmlhelp/sphinxcontrib_htmlhelp-{pkgver}.tar.gz"
-sha256 = "0dc87637d5de53dd5eec3a6a01753b1ccf99494bd756aafecd74b4fa9e729015"
+sha256 = "c6597da06185f0e3b4dc952777a04200611ef563882e0c244d27a15ee22afa73"
 # circular checkdepends
 options = ["!check"]
 
