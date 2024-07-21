8b55dadf4bdc4004638e4ad81ae49b830a3d7a1f
psykose <alice@ayaya.dev>
2024-07-21 06:14:19 +0200

    main/libcdio-paranoia:

--- main/libcdio-paranoia/template.py
+++ main/libcdio-paranoia/template.py
@@ -1,6 +1,6 @@
 pkgname = "libcdio-paranoia"
 _v1 = "10.2"
-_v2 = "2.0.1"
+_v2 = "2.0.2"
 pkgver = f"{_v1}.{_v2}"
 pkgrel = 0
 build_style = "gnu_configure"
@@ -8,14 +8,14 @@ configure_args = ["--disable-example-progs", "--enable-cpp-progs"]
 make_cmd = "gmake"
 # out of tree build is broken
 make_dir = "."
-hostmakedepends = ["gmake", "pkgconf"]
+hostmakedepends = ["automake", "libtool", "gmake", "pkgconf"]
 makedepends = ["libcdio-devel", "linux-headers"]
 pkgdesc = "CD paranoia utility/libraries from libcdio"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/libcdio"
-source = f"$(GNU_SITE)/libcdio/{pkgname}-{_v1}+{_v2}.tar.bz2"
-sha256 = "33b1cf305ccfbfd03b43936975615000ce538b119989c4bec469577570b60e8a"
+source = f"$(GNU_SITE)/libcdio/libcdio-paranoia-{_v1}+{_v2}.tar.bz2"
+sha256 = "186892539dedd661276014d71318c8c8f97ecb1250a86625256abd4defbf0d0c"
 
 
 @subpackage("libcdio-paranoia-devel")
@@ -26,6 +26,3 @@ def _devel(self):
 @subpackage("libcdio-paranoia-progs")
 def _progs(self):
     return self.default_progs()
-
-
-configure_gen = []
