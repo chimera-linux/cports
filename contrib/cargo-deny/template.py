95a42b66d078854192c1b25c8657548056662371
triallax <triallax@tutanota.com>
2024-07-22 18:27:13 +0100

    contrib/cargo-deny: use system certs

--- contrib/cargo-deny/template.py
+++ contrib/cargo-deny/template.py
@@ -1,9 +1,13 @@
 pkgname = "cargo-deny"
 pkgver = "0.14.24"
-pkgrel = 0
+pkgrel = 1
 build_style = "cargo"
+make_build_args = ["--no-default-features", "--features=native-certs"]
+make_install_args = [*make_build_args]
+make_check_args = [*make_build_args]
 hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = ["rust-std", "zstd-devel"]
+depends = ["ca-certificates"]
 pkgdesc = "Cargo plugin for linting dependencies"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "MIT OR Apache-2.0"
