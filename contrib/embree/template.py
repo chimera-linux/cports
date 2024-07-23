27bf44f2127ded63c8f4dd960153b95ee9c783dc
Erica Z <zerica@callcc.eu>
2024-07-23 11:14:44 +0200

    contrib/embree: update to 4.3.3

--- contrib/embree/template.py
+++ contrib/embree/template.py
@@ -1,5 +1,5 @@
 pkgname = "embree"
-pkgver = "4.3.2"
+pkgver = "4.3.3"
 pkgrel = 0
 # embree really doesn't want to build outside of these without ragepatching
 archs = ["aarch64", "x86_64"]
@@ -15,7 +15,7 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "Apache-2.0"
 url = "https://www.embree.org"
 source = f"https://github.com/embree/embree/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "dc7bb6bac095b2e7bc64321435acd07c6137d6d60e4b79ec07bb0b215ddf81cb"
+sha256 = "8a3bc3c3e21aa209d9861a28f8ba93b2f82ed0dc93341dddac09f1f03c36ef2d"
 # guilty until proven otherwise
 hardening = ["!int"]
 
