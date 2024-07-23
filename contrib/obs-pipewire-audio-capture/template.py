45dd7ff03e71a7c81029e6ca29081530a690e204
Gnarwhal <git.aspect893@passmail.net>
2024-07-23 19:23:05 +0000

    contrib/obs-pipewire-audio-capture: new package (1.1.5)

---
+++ contrib/obs-pipewire-audio-capture/template.py
@@ -0,0 +1,20 @@
+pkgname = "obs-pipewire-audio-capture"
+pkgver = "1.1.5"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "obs-studio-devel",
+    "pipewire-devel",
+]
+pkgdesc = "OBS plugin for capturing device and application audio using pipewire"
+maintainer = "Gnarwhal <git.aspect893@passmail.net>"
+license = "GPL-2.0-or-later"
+url = "https://obsproject.com/forum/resources/pipewire-audio-capture.1458"
+source = f"https://github.com/dimtpap/obs-pipewire-audio-capture/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "9746c0564cc1e41aa3acf5bc516f4d3646450628c34bfaca49485e45a32cde23"
+hardening = ["vis", "!cfi"]
