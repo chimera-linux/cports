aee61a73df417cab4e73e17fbc48d9c2627b4660
Erica Z <zerica@callcc.eu>
2024-07-23 11:47:52 +0200

    user/plan9port: add update check

--- user/plan9port/update.py
+++ user/plan9port/update.py
@@ -1,2 +1,6 @@
-# no proper releases
-ignore = True
+url = "https://github.com/9fans/plan9port/commits/master.atom"
+pattern = r"<updated>(\d\d\d\d-\d\d-\d\d)T"  # YYYY-mm-dd
+
+
+def fetch_versions(self, src):
+    return map(lambda v: "0_git" + v.replace("-", ""), self.fetch_versions(src))
