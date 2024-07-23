e586071bdff0ba9f9874ed778b1d245d37792895
triallax <triallax@tutanota.com>
2024-07-23 02:46:39 +0100

    contrib/python-git-filter-repo: update to 2.45.0

--- contrib/python-git-filter-repo/template.py
+++ contrib/python-git-filter-repo/template.py
@@ -1,14 +1,12 @@
 pkgname = "python-git-filter-repo"
-pkgver = "2.38.0"
-pkgrel = 1
-build_wrksrc = "release"
+pkgver = "2.45.0"
+pkgrel = 0
 build_style = "python_pep517"
 make_build_env = {"SETUPTOOLS_SCM_PRETEND_VERSION": pkgver}
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools_scm",
-    "python-wheel",
 ]
 depends = ["git", "python"]
 checkdepends = ["bash", "rsync", *depends]
@@ -16,9 +14,9 @@ pkgdesc = "Tool for rewriting git history"
 maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/newren/git-filter-repo"
-# pypi tarball doesn't contain tests and man pages
+# pypi tarball doesn't contain man page
 source = f"{url}/releases/download/v{pkgver}/git-filter-repo-{pkgver}.tar.xz"
-sha256 = "db954f4cae9e47c6be3bd3161bc80540d44f5379cb9cf9df498f4e019f0a41a9"
+sha256 = "430a2c4a5d6f010ebeafac6e724e3d8d44c83517f61ea2b2d0d07ed8a6fc555a"
 # TODO: figure out why some of the tests fail
 options = ["!check"]
 
@@ -28,5 +26,5 @@ def do_check(self):
 
 
 def post_install(self):
-    self.install_license("../COPYING.mit")
-    self.install_man("../Documentation/man1/git-filter-repo.1")
+    self.install_license("COPYING.mit")
+    self.install_man("Documentation/man1/git-filter-repo.1")
