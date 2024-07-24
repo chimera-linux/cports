9ffd7d13844e805fd1f832b4c71b7432c73890ab
q66 <q66@chimera-linux.org>
2024-07-24 18:07:25 +0200

    contrib/u-boot-*: update to 2024.07

--- contrib/u-boot-qemu-riscv64_smode/template.py
+++ contrib/u-boot-qemu-riscv64_smode/template.py
@@ -1,5 +1,5 @@
 pkgname = "u-boot-qemu-riscv64_smode"
-pkgver = "2024.01"
+pkgver = "2024.07"
 pkgrel = 0
 build_style = "u_boot"
 hostmakedepends = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND BSD-3-Clause"
 url = "https://www.denx.de/wiki/U-Boot"
 source = f"https://ftp.denx.de/pub/u-boot/u-boot-{pkgver}.tar.bz2"
-sha256 = "b99611f1ed237bf3541bdc8434b68c96a6e05967061f992443cb30aabebef5b3"
+sha256 = "f591da9ab90ef3d6b3d173766d0ddff90c4ed7330680897486117df390d83c8f"
 env = {
     "U_BOOT_TRIPLET": "riscv64-unknown-elf",
     "U_BOOT_TARGETS": "u-boot",
--- contrib/u-boot-sifive_unmatched/template.py
+++ contrib/u-boot-sifive_unmatched/template.py
@@ -1,5 +1,5 @@
 pkgname = "u-boot-sifive_unmatched"
-pkgver = "2024.01"
+pkgver = "2024.07"
 pkgrel = 0
 archs = ["riscv64"]
 build_style = "u_boot"
@@ -21,7 +21,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND BSD-3-Clause"
 url = "https://www.denx.de/wiki/U-Boot"
 source = f"https://ftp.denx.de/pub/u-boot/u-boot-{pkgver}.tar.bz2"
-sha256 = "b99611f1ed237bf3541bdc8434b68c96a6e05967061f992443cb30aabebef5b3"
+sha256 = "f591da9ab90ef3d6b3d173766d0ddff90c4ed7330680897486117df390d83c8f"
 env = {
     "U_BOOT_TRIPLET": "riscv64-unknown-elf",
     "U_BOOT_TARGETS": "spl/u-boot-spl.bin u-boot.itb",
