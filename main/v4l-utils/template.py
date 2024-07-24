e9d33f09292b221558eeaf0656f76e49fbba96c3
psykose <alice@ayaya.dev>
2024-07-24 21:24:25 +0200

    main/v4l-utils: update to 1.28.1

--- main/v4l-utils/template.py
+++ main/v4l-utils/template.py
@@ -1,23 +1,29 @@
 pkgname = "v4l-utils"
-pkgver = "1.26.1"
+pkgver = "1.28.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dqv4l2=disabled", "-Dudevdir=/usr/lib/udev"]
-hostmakedepends = ["bash", "gettext-devel", "meson", "perl", "pkgconf"]
+hostmakedepends = [
+    "bash",
+    "gettext-devel",
+    "meson",
+    "perl",
+    "pkgconf",
+]
 makedepends = [
+    "argp-standalone",
+    "glu-devel",
     "libjpeg-turbo-devel",
-    "udev-devel",
     "libx11-devel",
     "mesa-devel",
-    "glu-devel",
-    "argp-standalone",
+    "udev-devel",
 ]
 pkgdesc = "Userspace tools and libraries for V4L"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://linuxtv.org/wiki/index.php/V4l-utils"
 source = f"https://linuxtv.org/downloads/v4l-utils/v4l-utils-{pkgver}.tar.xz"
-sha256 = "4a71608c0ef7df2931176989e6d32b445c0bdc1030a2376d929c8ca6e550ec4e"
+sha256 = "0fa075ce59b6618847af6ea191b6155565ccaa44de0504581ddfed795a328a82"
 tool_flags = {"LDFLAGS": ["-largp"]}
 
 
