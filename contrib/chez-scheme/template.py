e9ac6928db33526d6c2328db27031be36c1cb74c
psykose <alice@ayaya.dev>
2024-07-24 23:51:16 +0200

    contrib/chez-scheme: try fix

--- contrib/chez-scheme/template.py
+++ contrib/chez-scheme/template.py
@@ -29,6 +29,10 @@ match self.profile().arch:
         _machine = "tarm32le"
     case "ppc":
         _machine = "tppc32le"
+    case "ppc64le":
+        _machine = "tppc64le"
+    case "ppc64":
+        _machine = "tppc64be"
     case "riscv64":
         _machine = "trv64le"
     case "x86_64":
