fe107e6f8c8f055e53541d493553afe522583bd3
Erica Z <zerica@callcc.eu>
2024-07-24 22:06:06 +0200

    main/libpwquality: fix cross

--- main/libpwquality/template.py
+++ main/libpwquality/template.py
@@ -15,7 +15,6 @@ hostmakedepends = [
     "automake",
     "libtool",
     "gettext-devel",
-    "python-devel",
     "python-setuptools",
 ]
 makedepends = ["cracklib-devel", "linux-pam-devel", "python-devel"]
@@ -26,7 +25,15 @@ license = "BSD-3-Clause OR GPL-2.0-or-later"
 url = "https://github.com/libpwquality/libpwquality"
 source = f"{url}/releases/download/libpwquality-{pkgver}/libpwquality-{pkgver}.tar.bz2"
 sha256 = "6fcf18b75d305d99d04d2e42982ed5b787a081af2842220ed63287a2d6a10988"
-options = ["!cross"]
+
+
+def init_build(self):
+    # see build_style/python_pep517
+    self.make_build_env = {
+        "PYTHON_CROSS_LIBDIR": self.profile().sysroot / "usr/lib",
+        "PYTHON_CROSS_INCDIR": self.profile().sysroot
+        / f"usr/include/python{self.python_version}",
+    }
 
 
 def post_install(self):
