a4e7a3e5d7c94907f6dd4789d711cb4b3a4215d2
psykose <alice@ayaya.dev>
2024-07-24 21:21:04 +0200

    contrib/perf: update to 6.10.1

--- contrib/perf/template.py
+++ contrib/perf/template.py
@@ -1,5 +1,5 @@
 pkgname = "perf"
-pkgver = "6.10"
+pkgver = "6.10.1"
 pkgrel = 0
 build_wrksrc = "tools/perf"
 build_style = "makefile"
@@ -53,7 +53,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-only"
 url = "https://perf.wiki.kernel.org/index.php/Main_Page"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[:pkgver.find('.')]}.x/linux-{pkgver}.tar.xz"
-sha256 = "774698422ee54c5f1e704456f37c65c06b51b4e9a8b0866f34580d86fef8e226"
+sha256 = "70109dfd1cd1c5f8a58eb1cb37122b9bf93f9c6a6280bf91019263c7339cf76b"
 # nope
 # docs are a single tips file that gets displayed in the TUI
 options = ["!check", "!splitdoc"]
