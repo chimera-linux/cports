52d583c760f2725343527987d336699508239a8d
q66 <q66@chimera-linux.org>
2024-07-25 23:25:40 +0200

    contirb/clang-rt-builtins-mingw-w64: configure with profile
    This is to eliminate the "target" cflags and so on when building
    on other archs.

--- contrib/clang-rt-builtins-mingw-w64/template.py
+++ contrib/clang-rt-builtins-mingw-w64/template.py
@@ -55,22 +55,23 @@ def do_configure(self):
 
     for an in _targets:
         at = an + "-w64-mingw32"
-        with self.stamp(f"{an}_configure") as s:
-            s.check()
-            cmake.configure(
-                self,
-                f"build-{an}",
-                self.cmake_dir,
-                [
-                    *self.configure_args,
-                    f"-DCMAKE_SYSROOT=/usr/{at}",
-                    f"-DCMAKE_ASM_COMPILER_TARGET={at}",
-                    f"-DCMAKE_C_COMPILER_TARGET={at}",
-                    f"-DCMAKE_CXX_COMPILER_TARGET={at}",
-                ],
-                cross_build=False,
-                generator="Unix Makefiles",
-            )
+        with self.profile(an if an != "i686" else "x86_64"):
+            with self.stamp(f"{an}_configure") as s:
+                s.check()
+                cmake.configure(
+                    self,
+                    f"build-{an}",
+                    self.cmake_dir,
+                    [
+                        *self.configure_args,
+                        f"-DCMAKE_SYSROOT=/usr/{at}",
+                        f"-DCMAKE_ASM_COMPILER_TARGET={at}",
+                        f"-DCMAKE_C_COMPILER_TARGET={at}",
+                        f"-DCMAKE_CXX_COMPILER_TARGET={at}",
+                    ],
+                    cross_build=False,
+                    generator="Unix Makefiles",
+                )
 
 
 def do_build(self):
