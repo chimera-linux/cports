3e5553f2a10f7ce9230dd7e6b17a1ca62c5e73fb
psykose <alice@ayaya.dev>
2024-07-25 03:17:43 +0200

    contrib/python-b4: update to 0.14.1

--- contrib/python-b4/template.py
+++ contrib/python-b4/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-b4"
-pkgver = "0.14.0"
+pkgver = "0.14.1"
 pkgrel = 0
 build_style = "python_pep517"
 # it tries to run the tests from the build dir if we don't do this...
@@ -24,4 +24,4 @@ license = "GPL-2.0-or-later"
 url = "https://github.com/mricon/b4"
 # pypi tarball doesn't contain tests
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "a2470164ffeffd3b8217de658d2a2ee9053698f272268ac7563c752d605881d3"
+sha256 = "c2c679ca223f5814196714dc0c7a2aeb7f4740e49a7878f4c00a3d8963c7f14b"
