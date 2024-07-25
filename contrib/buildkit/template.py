5949ed2cb54637bb7efe00f306edbd09f627c4ad
psykose <alice@ayaya.dev>
2024-07-25 11:54:45 +0200

    contrib/buildkit: update to 0.15.1

--- contrib/buildkit/template.py
+++ contrib/buildkit/template.py
@@ -1,6 +1,6 @@
 pkgname = "buildkit"
-pkgver = "0.15.0"
-pkgrel = 1
+pkgver = "0.15.1"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/..."]
 hostmakedepends = ["go"]
@@ -9,8 +9,8 @@ pkgdesc = "Concurrent, cache-efficient, and Dockerfile-agnostic builder toolkit"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/moby/buildkit"
-source = f"https://github.com/moby/buildkit/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "ebf4b82f7379818d8fcfe5c54034e7f93c062c95f663bfd3c2a622583f62a8a4"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "5ee1ccf7d376c5e399c50ae400cfc24ec3c682df8139e8ba46f41ea261f10b4f"
 # cannot work in bwrap
 options = ["!check"]
 
