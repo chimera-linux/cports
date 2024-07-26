89cf48d66f077bbcb8ef15851428280fcdc3a4da
q66 <q66@chimera-linux.org>
2024-07-26 02:55:47 +0200

    main/turnstile: update to 0.1.9

--- main/turnstile/template.py
+++ main/turnstile/template.py
@@ -1,8 +1,8 @@
 pkgname = "turnstile"
-pkgver = "0.1.8"
-pkgrel = 4
+pkgver = "0.1.9"
+pkgrel = 0
 build_style = "meson"
-configure_args = ["-Dmanage_rundir=true"]
+configure_args = ["-Dmanage_rundir=true", "-Dpamdir=/usr/lib/pam.d"]
 hostmakedepends = ["meson", "pkgconf", "scdoc"]
 makedepends = ["linux-pam-devel"]
 depends = ["dinit-chimera"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/turnstile"
 source = f"https://github.com/chimera-linux/turnstile/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7eaab8c80c76ae9a9a711d7dc57ec346b9af09be99b526a5a3129a7fc9bd7a76"
+sha256 = "1a23f8a6d4fdcfd195cee042cead0ff4d9e9e10ad97721435b86775a8a4e660d"
 hardening = ["vis", "cfi"]
 options = ["brokenlinks", "!splitdinit"]
 
@@ -25,5 +25,3 @@ def post_install(self):
     # also default systemwide link
     self.install_dir("usr/lib/dinit.d/boot.d")
     self.install_link("usr/lib/dinit.d/boot.d/turnstiled", "../turnstiled")
-    # move pam.d stuff, FIXME in turnstile later
-    self.mv("etc/pam.d", "usr/lib/pam.d", relative=False)
