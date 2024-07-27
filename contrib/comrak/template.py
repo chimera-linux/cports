9b9535cef6369c6264cfc40f1b21809f3b8f8038
triallax <triallax@tutanota.com>
2024-07-27 21:08:38 +0100

    contrib/comrak: update to 0.26.0

--- contrib/comrak/template.py
+++ contrib/comrak/template.py
@@ -1,9 +1,7 @@
 pkgname = "comrak"
-pkgver = "0.24.1"
+pkgver = "0.26.0"
 pkgrel = 0
 build_style = "cargo"
-# we patch Cargo.toml and Cargo.lock
-prepare_after_patch = True
 hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = ["oniguruma-devel"]
 pkgdesc = "CommonMark compatible GitHub Flavored Markdown parser and formatter"
@@ -11,7 +9,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "BSD-2-Clause"
 url = "https://github.com/kivikakk/comrak"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "045aadd282968c15499bf308974fb9ea4293b9b8ada29baaafa818546943297d"
+sha256 = "d2024f260caf4e403043b66acfacb4923bcb4cb66017fa10a8603e8e8749d145"
 
 
 def post_install(self):
