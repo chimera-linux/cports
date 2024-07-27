e86ff93afacbada1136b855d95c8630a07692539
triallax <triallax@tutanota.com>
2024-07-27 15:13:17 +0100

    contrib/pam_wrapper: update to 1.1.7

--- contrib/pam_wrapper/template.py
+++ contrib/pam_wrapper/template.py
@@ -1,5 +1,5 @@
 pkgname = "pam_wrapper"
-pkgver = "1.1.5"
+pkgver = "1.1.7"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DUNIT_TESTING=ON"]
@@ -11,7 +11,15 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-3.0-or-later"
 url = "https://cwrap.org/pam_wrapper.html"
 source = f"https://ftp.samba.org/pub/cwrap/pam_wrapper-{pkgver}.tar.gz"
-sha256 = "bbc47d9990eef7b33db55d63a9e2cde5d16e8989a17c6456b8ca2a582d72f889"
+sha256 = "d1c93e2b687d08834df0e09026dd63d2ce4f577701d406a013e9a8afe469bde1"
+
+
+@subpackage("pam_wrapper-python")
+def _python(self):
+    self.subdesc = "Python bindings"
+    self.depends += ["python"]
+
+    return ["usr/lib/python*"]
 
 
 @subpackage("pam_wrapper-devel")
