7a3a422f3936fc47bda05a4ad6bb6c42b21af9ee
psykose <alice@ayaya.dev>
2024-07-27 10:10:01 +0200

    contrib/podman-compose: new package (1.2.0)

---
+++ contrib/podman-compose/template.py
@@ -0,0 +1,27 @@
+pkgname = "podman-compose"
+pkgver = "1.2.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+]
+depends = [
+    "podman",
+    "python-dotenv",
+    "python-pyyaml",
+]
+checkdepends = ["python-pytest", *depends]
+pkgdesc = "Podman utility for docker-compose.yml support"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-only"
+url = "https://github.com/containers/podman-compose"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "211b235cf6ffe38f5f65fa188471e4b2706895c1ef4e800bb2a59a01f272674e"
+# tests need to run podman which isn't gonna work in bwrap without networking and so on
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_completion("./completion/bash/podman-compose", "bash")
