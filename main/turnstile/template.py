861b2d51f588730170b52a3a0bf3f3b252321d69
q66 <q66@chimera-linux.org>
2024-07-28 17:35:59 +0200

    main/turnstile: update to 0.1.10

--- main/turnstile/template.py
+++ main/turnstile/template.py
@@ -1,5 +1,5 @@
 pkgname = "turnstile"
-pkgver = "0.1.9"
+pkgver = "0.1.10"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dmanage_rundir=true", "-Dpamdir=/usr/lib/pam.d"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/turnstile"
 source = f"https://github.com/chimera-linux/turnstile/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1a23f8a6d4fdcfd195cee042cead0ff4d9e9e10ad97721435b86775a8a4e660d"
+sha256 = "186adf1402f3c63eecdbed241145cb029b7b1268c701a87381522d5b64583f2d"
 hardening = ["vis", "cfi"]
 options = ["brokenlinks", "!splitdinit"]
 
