2e2bb98cb2a4f5db9fe159588627a98a7592ea87
q66 <q66@chimera-linux.org>
2024-07-28 15:09:19 +0200

    contrib/apr: fix up path in /usr

--- contrib/apr/template.py
+++ contrib/apr/template.py
@@ -1,7 +1,8 @@
 pkgname = "apr"
 pkgver = "1.7.4"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
+configure_args = ["--with-installbuilddir=/usr/lib/apr/build-1"]
 make_cmd = "gmake"
 hostmakedepends = ["automake", "gmake", "libtool", "pkgconf"]
 makedepends = ["libexpat-devel", "libuuid-devel", "linux-headers"]
@@ -13,7 +14,6 @@ source = f"https://archive.apache.org/dist/apr/apr-{pkgver}.tar.gz"
 sha256 = "a4137dd82a185076fa50ba54232d920a17c6469c30b0876569e1c2a05ff311d9"
 # not even once
 options = ["!cross"]
-
 
 @subpackage("apr-devel")
 def _devel(self):
