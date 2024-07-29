ed326aa046800583f0064803fafbd564f55179ec
psykose <alice@ayaya.dev>
2024-07-29 15:25:19 +0200

    main/python-sphinxcontrib-htmlhelp: update to 2.1.0

--- main/python-sphinxcontrib-htmlhelp/template.py
+++ main/python-sphinxcontrib-htmlhelp/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-sphinxcontrib-htmlhelp"
-pkgver = "2.0.6"
+pkgver = "2.1.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-flit_core", "python-installer"]
@@ -10,10 +10,10 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://www.sphinx-doc.org"
 source = f"$(PYPI_SITE)/s/sphinxcontrib-htmlhelp/sphinxcontrib_htmlhelp-{pkgver}.tar.gz"
-sha256 = "c6597da06185f0e3b4dc952777a04200611ef563882e0c244d27a15ee22afa73"
+sha256 = "c9e2916ace8aad64cc13a0d233ee22317f2b9025b9cf3295249fa985cc7082e9"
 # circular checkdepends
 options = ["!check"]
 
 
 def post_install(self):
-    self.install_license("LICENSE")
+    self.install_license("LICENCE.rst")
