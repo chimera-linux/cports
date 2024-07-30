aa4fd77f617438fce6b78434821707fd7ad16b0f
Your Name <you@example.com>
2024-07-30 12:37:40 +0100

    contrib/tree-sitter-c: upate to 0.21.4

--- contrib/tree-sitter-c/template.py
+++ contrib/tree-sitter-c/template.py
@@ -1,5 +1,5 @@
 pkgname = "tree-sitter-c"
-pkgver = "0.21.3"
+pkgver = "0.21.4"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
@@ -14,7 +14,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://github.com/tree-sitter/tree-sitter-c"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "75a3780df6114cd37496761c4a7c9fd900c78bee3a2707f590d78c0ca3a24368"
+sha256 = "19194c47a6faf81509aea338b96dd9b59ffd8a7f26bce6487cf4275065433870"
 
 
 def post_install(self):
