05e148aee2ebb2f16c4d281905f28c89cd3fdea6
triallax <triallax@tutanota.com>
2024-07-30 16:34:42 +0100

    contrib/xfce4-screenshooter: update to 1.11.1

--- contrib/xfce4-screenshooter/template.py
+++ contrib/xfce4-screenshooter/template.py
@@ -1,5 +1,5 @@
 pkgname = "xfce4-screenshooter"
-pkgver = "1.11.0"
+pkgver = "1.11.1"
 pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
@@ -25,7 +25,6 @@ makedepends = [
     "libxfce4util-devel",
     "libxfixes-devel",
     "libxi-devel",
-    "libxml2-devel",
     "pango-devel",
     "xfce4-panel-devel",
     "xfconf-devel",
@@ -36,7 +35,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/apps/xfce4-screenshooter/start"
 source = f"$(XFCE_SITE)/apps/xfce4-screenshooter/{'.'.join(pkgver.split('.')[:-1])}/xfce4-screenshooter-{pkgver}.tar.bz2"
-sha256 = "8b55bc2c63951a9b5d8304348f2622b90bf13bb84d505703c37c4f7391b7f5a2"
+sha256 = "d94c4a37ac9b26f6d73214bdc254624a4ede4e111bee8d34e689f8f04c37d34d"
 # Tries to run built executable to generate manpage
 options = ["!cross"]
 
