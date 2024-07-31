57fa916423f2b28969cf7d2b224aa666c40ea65b
psykose <alice@ayaya.dev>
2024-07-31 03:33:59 +0200

    main/gst-plugins-good: update to 1.24.6

--- main/gst-plugins-good/template.py
+++ main/gst-plugins-good/template.py
@@ -1,6 +1,6 @@
 pkgname = "gst-plugins-good"
-pkgver = "1.24.5"
-pkgrel = 3
+pkgver = "1.24.6"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "--auto-features=enabled",
@@ -70,7 +70,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/gst-plugins-good/gst-plugins-good-{pkgver}.tar.xz"
-sha256 = "badcfc5292b035bde99a77327d468b2f0b116b40420bc9f25fb8e3970824ba75"
+sha256 = "996b9c8d1d246ed43be304718b6086e5a17d4ae8114d1920aed9ea75b920ba2d"
 # sys/v4l2/gstv4l2object.c v4l2object->ioctl = v4l2_ioctl;
 tool_flags = {"CFLAGS": ["-Wno-incompatible-function-pointer-types"]}
 # FIXME int (extra tests fail, look for SIGILL)
