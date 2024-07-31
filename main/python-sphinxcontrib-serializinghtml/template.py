2df6661dd4d62a13fcd2244f10b96b1fa27ee83f
psykose <alice@ayaya.dev>
2024-07-31 04:08:25 +0200

    main/python-sphinxcontrib-serializinghtml: update to 2.0.0

--- main/python-sphinxcontrib-serializinghtml/template.py
+++ main/python-sphinxcontrib-serializinghtml/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-sphinxcontrib-serializinghtml"
-pkgver = "1.1.10"
+pkgver = "2.0.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-flit_core", "python-installer"]
@@ -10,10 +10,10 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://www.sphinx-doc.org"
 source = f"$(PYPI_SITE)/s/sphinxcontrib-serializinghtml/sphinxcontrib_serializinghtml-{pkgver}.tar.gz"
-sha256 = "93f3f5dc458b91b192fe10c397e324f262cf163d79f3282c158e8436a2c4511f"
+sha256 = "e9d912827f872c029017a53f0ef2180b327c3f7fd23c87229f7a8e8b70031d4d"
 # circular checkdepends
 options = ["!check"]
 
 
 def post_install(self):
-    self.install_license("LICENSE")
+    self.install_license("LICENCE.rst")
