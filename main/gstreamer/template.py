64a0792a3104b8b7ec19102a067172a97a2d8bc5
psykose <alice@ayaya.dev>
2024-07-31 03:33:28 +0200

    main/gstreamer: update to 1.24.6

--- main/gstreamer/template.py
+++ main/gstreamer/template.py
@@ -1,6 +1,6 @@
 pkgname = "gstreamer"
-pkgver = "1.24.5"
-pkgrel = 1
+pkgver = "1.24.6"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dptp-helper-permissions=none",  # manual
@@ -35,7 +35,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/gstreamer/gstreamer-{pkgver}.tar.xz"
-sha256 = "2bdef209252bf146351843134b797db6b6e7adb4c00d82e83bd5abe608253a7b"
+sha256 = "64342060d7c6f9e36a35e3be38a4f5ac3b41ed93b0853619be45141ef3cc1b9d"
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=0x200000"]}
 file_modes = {
     "usr/libexec/gstreamer-1.0/gst-ptp-helper": ("root", "root", 0o755),
