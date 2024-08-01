47913fb4f5a1aa6dea25a436a6f2f3c64279d7d5
triallax <triallax@tutanota.com>
2024-08-01 23:45:57 +0100

    user/qbe: enable int

--- user/qbe/template.py
+++ user/qbe/template.py
@@ -1,6 +1,6 @@
 pkgname = "qbe"
 pkgver = "1.2"
-pkgrel = 1
+pkgrel = 2
 archs = ["aarch64", "riscv64", "x86_64"]
 build_style = "makefile"
 pkgdesc = "Compiler backend"
@@ -9,7 +9,6 @@ license = "MIT"
 url = "https://c9x.me/compile"
 source = f"https://c9x.me/compile/release/qbe-{pkgver}.tar.xz"
 sha256 = "a6d50eb952525a234bf76ba151861f73b7a382ac952d985f2b9af1df5368225d"
-hardening = ["!int"]
 
 
 def post_install(self):
