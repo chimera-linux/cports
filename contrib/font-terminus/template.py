5f0f75289519fd6cc87903af76d0e55cc6f0933c
psykose <alice@ayaya.dev>
2024-08-01 17:19:03 +0200

    contrib/font-terminus: new package (4.49.1)

---
+++ contrib/font-terminus/template.py
@@ -0,0 +1,26 @@
+pkgname = "font-terminus"
+pkgver = "4.49.1"
+pkgrel = 0
+build_style = "configure"
+configure_args = [
+    "--prefix=/usr",
+    "--x11dir=/usr/share/fonts/misc",
+    "--otbdir=/usr/share/fonts/misc",
+]
+make_cmd = "gmake"
+make_build_args = ["all", "otb"]
+make_install_args = ["install-otb"]
+hostmakedepends = [
+    "bdftopcf",
+    "gmake",
+    "python",
+]
+makedepends = ["freetype-devel"]
+pkgdesc = "Monospace bitmap font"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "OFL-1.1"
+url = "https://terminus-font.sourceforge.net"
+source = f"$(SOURCEFORGE_SITE)/terminus-font/terminus-font-{pkgver}.tar.gz"
+sha256 = "d961c1b781627bf417f9b340693d64fc219e0113ad3a3af1a3424c7aa373ef79"
+# no tests
+options = ["!check"]
