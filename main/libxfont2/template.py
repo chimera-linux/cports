de43c88836f71c0be8a0fbf30b7de62737d2a309
psykose <alice@ayaya.dev>
2024-08-02 02:01:36 +0200

    main/libxfont2: update to 2.0.7

--- main/libxfont2/template.py
+++ main/libxfont2/template.py
@@ -1,15 +1,25 @@
 pkgname = "libxfont2"
-pkgver = "2.0.6"
+pkgver = "2.0.7"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
-makedepends = ["xorgproto", "xtrans", "freetype-devel", "libfontenc-devel"]
+hostmakedepends = [
+    "automake",
+    "libtool",
+    "pkgconf",
+    "xorg-util-macros",
+]
+makedepends = [
+    "freetype-devel",
+    "libfontenc-devel",
+    "xorgproto",
+    "xtrans",
+]
 pkgdesc = "X font 2 library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/lib/libXfont2-{pkgver}.tar.gz"
-sha256 = "a944df7b6837c8fa2067f6a5fc25d89b0acc4011cd0bc085106a03557fb502fc"
+source = f"$(XORG_SITE)/lib/libXfont2-{pkgver}.tar.xz"
+sha256 = "8b7b82fdeba48769b69433e8e3fbb984a5f6bf368b0d5f47abeec49de3e58efb"
 # FIXME int (e.g. xorg fails check)
 hardening = ["!int"]
 
@@ -21,6 +31,3 @@ def post_install(self):
 @subpackage("libxfont2-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
