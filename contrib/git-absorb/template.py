ed81d7df51b8f20affff061d80cf968768d253d0
triallax <triallax@tutanota.com>
2024-08-02 02:54:53 +0100

    contrib/git-absorb: install completions

--- contrib/git-absorb/template.py
+++ contrib/git-absorb/template.py
@@ -1,21 +1,36 @@
 pkgname = "git-absorb"
 pkgver = "0.6.13"
-pkgrel = 0
+pkgrel = 1
 build_style = "cargo"
-hostmakedepends = ["cargo-auditable", "pkgconf", "asciidoc"]
+prepare_after_patch = True
+hostmakedepends = ["asciidoc", "cargo-auditable", "pkgconf"]
 makedepends = ["libgit2-devel"]
-pkgdesc = "Automatic git commit --fixup; like hg absorb"
+pkgdesc = "Automatic git commit --fixup"
 maintainer = "Subhaditya Nath <sn03.general@gmail.com>"
 license = "BSD-3-Clause"
 url = "https://github.com/tummychow/git-absorb"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "5646857dd764d0a486405e777b3ec4e919511abc96bd0e02e71ec9e94d151115"
+# generates completions with host bin
+options = ["!cross"]
 
 
 def post_build(self):
     self.do("make", "-C", "Documentation")
 
+    for shell in ["bash", "fish", "nushell", "zsh"]:
+        with open(self.cwd / f"git-absorb.{shell}", "w") as cf:
+            self.do(
+                f"./target/{self.profile().triplet}/release/git-absorb",
+                "--gen-completions",
+                shell,
+                stdout=cf,
+            )
+
 
 def post_install(self):
     self.install_license("LICENSE.md")
     self.install_man("Documentation/git-absorb.1")
+
+    for shell in ["bash", "fish", "nushell", "zsh"]:
+        self.install_completion(f"git-absorb.{shell}", shell)
