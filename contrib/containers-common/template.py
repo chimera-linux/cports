f187bfaf8e12d6619294c8e1fdf5bf75f40636d2
psykose <alice@ayaya.dev>
2024-08-03 05:05:51 +0200

    contrib/containers-common: update to 0.60.0

--- contrib/containers-common/template.py
+++ contrib/containers-common/template.py
@@ -1,5 +1,5 @@
 pkgname = "containers-common"
-pkgver = "0.59.2"
+pkgver = "0.60.0"
 pkgrel = 0
 make_cmd = "gmake"
 make_build_args = ["-C", "docs"]
@@ -11,9 +11,9 @@ license = "Apache-2.0"
 url = "https://github.com/containers/common"
 _base_url = url.removesuffix("/common")
 _common_ver = pkgver
-_storage_ver = "1.54.0"
-_image_ver = "5.31.1"
-_skopeo_ver = "1.15.1"
+_storage_ver = "1.55.0"
+_image_ver = "5.32.0"
+_skopeo_ver = "1.16.0"
 _shortnames_ver = "2023.02.20"
 source = [
     f"{_base_url}/common/archive/v{_common_ver}.tar.gz",
@@ -23,10 +23,10 @@ source = [
     f"{_base_url}/shortnames/archive/v{_shortnames_ver}.tar.gz",
 ]
 sha256 = [
-    "69f6ce1752ab55b439229bbd6b9d31f0f74ab67517768d277a8e4891e2c85de2",
-    "eaca427de11b7144673d643c36f1d9d277bef520ce4b7fc070c15c8b93fc1485",
-    "123f0f28a7be9072bd137dc2d2adb8510d590a10adc6b1649290a9f5dfdf7de7",
-    "4db9798afd45e3282f35ddad3e494db99231e84470f30d6b105a3ed687e67ee0",
+    "c9f9a2ba78c69234b78d4b168bb2f70e3459e66d7931e68fde449af9b52e0062",
+    "d6c2d3af9e0f674c248477d521d0f8fc5eac050c65e2fd3f823cc42502b22847",
+    "f7582d2b4fed4967921ff154752cc33e8f319a232b561710a3e2872ba754945d",
+    "fed91fd067605460ef33431163227471b1e85c8768203fc393345d6ffd645448",
     "336ba679d4e510d2eb59cb11321bf16a36ef2dba58024e79dd76b89ffee539e6",
 ]
 # no tests
