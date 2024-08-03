1b997f2935b03610139092e44b4d0f2075865564
q66 <q66@chimera-linux.org>
2024-08-03 15:19:35 +0200

    contrib/python-pyasynchat: new package (1.0.4)

---
+++ contrib/python-pyasynchat/template.py
@@ -0,0 +1,19 @@
+pkgname = "python-pyasynchat"
+pkgver = "1.0.4"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-pyasyncore",
+    "python-setuptools",
+]
+depends = ["python-pyasyncore"]
+pkgdesc = "Python asynchat for 3.12"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "Python-2.0"
+url = "https://github.com/simonrob/pyasynchat"
+source = f"$(PYPI_SITE)/p/pyasynchat/pyasynchat-{pkgver}.tar.gz"
+sha256 = "3f5333df649e46c56d48c57e6a4b7163fd07f626bfd884e22ef373ab3c3a4670"
+# ???
+options = ["!check"]
