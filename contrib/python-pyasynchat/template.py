339192a3d5f0c446d97b4805d0e91cdeb62fcd7a
psykose <alice@ayaya.dev>
2024-08-03 15:46:44 +0200

    contrib/python-pyasynchat: run tests

--- contrib/python-pyasynchat/template.py
+++ contrib/python-pyasynchat/template.py
@@ -5,15 +5,17 @@ build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
-    "python-pyasyncore",
     "python-setuptools",
 ]
 depends = ["python-pyasyncore"]
+checkdepends = [
+    "python-pytest",
+    "python-tests",
+    *depends,
+]
 pkgdesc = "Python asynchat for 3.12"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Python-2.0"
 url = "https://github.com/simonrob/pyasynchat"
 source = f"$(PYPI_SITE)/p/pyasynchat/pyasynchat-{pkgver}.tar.gz"
 sha256 = "3f5333df649e46c56d48c57e6a4b7163fd07f626bfd884e22ef373ab3c3a4670"
-# ???
-options = ["!check"]
