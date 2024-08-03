41640baaa869cf88eb6528c0ba25acd285bb145f
q66 <q66@chimera-linux.org>
2024-08-03 15:32:49 +0200

    contrib/fail2ban: new package (1.1.0)

---
+++ contrib/fail2ban/template.py
@@ -0,0 +1,31 @@
+pkgname = "fail2ban"
+pkgver = "1.1.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "pkgconf",
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+]
+depends = ["python-pyasynchat"]
+pkgdesc = "Daemon to ban hosts that cause multiple auth errors"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-only"
+url = "https://fail2ban.org"
+source = f"https://github.com/fail2ban/fail2ban/archive/{pkgver}.tar.gz"
+sha256 = "474fcc25afdaf929c74329d1e4d24420caabeea1ef2e041a267ce19269570bae"
+# doesn't work with pytest
+options = ["!check"]
+
+
+def post_install(self):
+    self.uninstall("usr/lib/python*/site-packages/fail2ban/tests", glob=True)
+    # asyncore+asynchat
+    self.uninstall("usr/lib/python*/site-packages/fail2ban/compat", glob=True)
+    # pep517 can't install to normal paths
+    with self.pushd(self.destdir / "usr/lib/python*/site-packages", glob=True):
+        self.mv("usr/share", self.destdir / "usr")
+        self.mv("etc", self.destdir)
+    # service last
+    self.install_service(self.files_path / "fail2ban")
