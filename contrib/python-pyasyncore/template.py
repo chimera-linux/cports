3ddcdee6898e3384be0b73a456ca85fd8ab107eb
psykose <alice@ayaya.dev>
2024-08-03 15:49:20 +0200

    contrib/python-pyasyncore: update comment

--- contrib/python-pyasyncore/template.py
+++ contrib/python-pyasyncore/template.py
@@ -8,11 +8,15 @@ hostmakedepends = [
     "python-setuptools",
 ]
 depends = ["python"]
+checkdepends = [
+    "python-pytest",
+    "python-tests",
+]
 pkgdesc = "Python asyncore for 3.12"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Python-2.0"
 url = "https://github.com/simonrob/pyasyncore"
 source = f"$(PYPI_SITE)/p/pyasyncore/pyasyncore-{pkgver}.tar.gz"
 sha256 = "2c7a8b9b750ba6260f1e5a061456d61320a80579c6a43d42183417da89c7d5d6"
-# ???
+# relies on <3.12 unittest imports
 options = ["!check"]
