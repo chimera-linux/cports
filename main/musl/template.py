7d19eb4ef6f8960ffe0c2a8ef83ce58bb21e1bcf
q66 <q66@chimera-linux.org>
2024-08-04 15:13:45 +0200

    main/musl: use external allocator

--- main/musl/template.py
+++ main/musl/template.py
@@ -1,13 +1,14 @@
 pkgname = "musl"
 pkgver = "1.2.5_git20240705"
-pkgrel = 0
+pkgrel = 1
 _commit = "dd1e63c3638d5f9afb857fccf6ce1415ca5f1b8b"
-_scudo_ver = "18.1.8"
 build_style = "gnu_configure"
 configure_args = ["--prefix=/usr", "--disable-gcc-wrapper"]
 configure_gen = []
 make_cmd = "gmake"
+make_dir = "."
 hostmakedepends = ["gmake"]
+makedepends = []
 depends = [self.with_pkgver("musl-progs")]
 provides = ["so:libc.so=0"]
 provider_priority = 999
@@ -16,15 +17,8 @@ pkgdesc = "Musl C library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://www.musl-libc.org"
-source = [
-    f"https://git.musl-libc.org/cgit/musl/snapshot/musl-{_commit}.tar.gz",
-    f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{_scudo_ver}/compiler-rt-{_scudo_ver}.src.tar.xz",
-]
-source_paths = [".", "compiler-rt"]
-sha256 = [
-    "a6886a65387d2547aae10c1ba31a35529a5c4bbe4205b2a9255c774d5da77329",
-    "e054e99a9c9240720616e927cb52363abbc8b4f1ef0286bad3df79ec8fdf892f",
-]
+source = f"https://git.musl-libc.org/cgit/musl/snapshot/musl-{_commit}.tar.gz"
+sha256 = "a6886a65387d2547aae10c1ba31a35529a5c4bbe4205b2a9255c774d5da77329"
 compression = "deflate"
 # scp makes it segfault
 hardening = ["!scp"]
@@ -36,9 +30,9 @@ _use_mng = self.profile().wordsize == 32
 
 if _use_mng:
     configure_args += ["--with-malloc=mallocng"]
-elif self.profile().arch == "aarch64":
-    # disable aarch64 memory tagging in scudo, as it fucks up qemu-user
-    tool_flags = {"CXXFLAGS": ["-DSCUDO_DISABLE_TBI"]}
+else:
+    configure_args += ["--with-malloc=external"]
+    makedepends += ["musl-allocator-scudo"]
 
 if self.stage > 0:
     # have base-files extract first in normal installations
@@ -59,26 +53,16 @@ def post_extract(self):
     # reported in libc.so --version
     with open(self.cwd / "VERSION", "w") as f:
         f.write(pkgver)
-    # prepare scudo subdir
-    self.mkdir("src/malloc/scudo/scudo", parents=True)
-    # move compiler-rt stuff in there
-    scpath = self.cwd / "compiler-rt/lib/scudo/standalone"
-    for f in scpath.glob("*.cpp"):
-        self.cp(f, "src/malloc/scudo")
-    for f in scpath.glob("*.h"):
-        self.cp(f, "src/malloc/scudo")
-    for f in scpath.glob("*.inc"):
-        self.cp(f, "src/malloc/scudo")
-    self.cp(scpath / "include/scudo/interface.h", "src/malloc/scudo/scudo")
-    # remove wrappers
-    for f in (self.cwd / "src/malloc/scudo").glob("wrappers_*"):
-        f.unlink()
-    # copy in our own wrappers
-    self.cp(self.files_path / "wrappers.cpp", "src/malloc/scudo")
-    # now we're ready to get patched
     # but also remove musl's x86_64 asm memcpy as it's actually
     # noticeably slower than the c implementation
     self.rm("src/string/x86_64/memcpy.s")
+
+
+def post_patch(self):
+    if _use_mng:
+        return
+    with self.pushd("src/malloc/external"):
+        self.do("ar", "x", "/usr/lib/musl-scudo.a")
 
 
 def init_configure(self):
