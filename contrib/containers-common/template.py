a861406d7b9f3572eee84286133fbd8758d61b00
psykose <alice@ayaya.dev>
2024-08-04 14:47:00 +0200

    contrib/containers-common: light refactor

--- contrib/containers-common/template.py
+++ contrib/containers-common/template.py
@@ -20,6 +20,12 @@ source = [
     f"{_base_url}/image/archive/v{_image_ver}.tar.gz",
     f"{_base_url}/shortnames/archive/v{_shortnames_ver}.tar.gz",
 ]
+source_paths = [
+    "common",
+    "storage",
+    "image",
+    "shortnames",
+]
 sha256 = [
     "c9f9a2ba78c69234b78d4b168bb2f70e3459e66d7931e68fde449af9b52e0062",
     "d6c2d3af9e0f674c248477d521d0f8fc5eac050c65e2fd3f823cc42502b22847",
@@ -31,9 +37,9 @@ options = ["!check"]
 
 
 def do_build(self):
-    self.do("gmake", wrksrc=f"common-{_common_ver}/docs")
-    self.do("gmake", wrksrc=f"storage-{_storage_ver}/docs")
-    self.do("gmake", "docs", wrksrc=f"image-{_image_ver}")
+    self.do("gmake", wrksrc="common/docs")
+    self.do("gmake", wrksrc="storage/docs")
+    self.do("gmake", "docs", wrksrc="image")
 
 
 def do_install(self):
@@ -41,7 +47,7 @@ def do_install(self):
     self.install_dir("etc/containers/oci/hooks.d")
     self.install_dir("var/lib/containers/sigstore", empty=True)
 
-    with self.pushd(f"common-{_common_ver}"):
+    with self.pushd("common"):
         self.install_file("pkg/config/containers.conf", "etc/containers")
         self.install_file("pkg/config/containers.conf", "usr/share/containers")
         self.install_file("pkg/seccomp/seccomp.json", "etc/containers")
@@ -54,18 +60,18 @@ def do_install(self):
             wrksrc="docs",
         )
 
-    with self.pushd(f"storage-{_storage_ver}"):
+    with self.pushd("storage"):
         self.install_file("storage.conf", "etc/containers")
         self.install_file("storage.conf", "usr/share/containers")
         self.do(
             "gmake", "install", f"DESTDIR={self.chroot_destdir}", wrksrc="docs"
         )
 
-    with self.pushd(f"image-{_image_ver}"):
+    with self.pushd("image"):
         self.install_file("registries.conf", "etc/containers")
         self.do("gmake", "install", f"DESTDIR={self.chroot_destdir}")
 
-    with self.pushd(f"shortnames-{_shortnames_ver}"):
+    with self.pushd("shortnames"):
         self.install_file(
             "shortnames.conf",
             "etc/containers/registries.conf.d",
