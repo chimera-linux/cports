e87969ac24f770ac52bf7dca71502ed88e20b8bc
psykose <alice@ayaya.dev>
2024-08-04 11:10:08 +0200

    main/gnome-user-docs: update to 46.4

--- main/gnome-user-docs/template.py
+++ main/gnome-user-docs/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-user-docs"
-pkgver = "46.1"
+pkgver = "46.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -18,5 +18,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "CC-BY-3.0"
 url = "https://gitlab.gnome.org/GNOME/gnome-user-docs"
 source = f"$(GNOME_SITE)/gnome-user-docs/{pkgver[:-2]}/gnome-user-docs-{pkgver}.tar.xz"
-sha256 = "a97293cbeeb797eb4f4d169d713bb6583bd12c3791e140283e4356d2fe180ada"
+sha256 = "2776072f15f92bd05028345f3ac8a093d44260de52a620d62693c0e5eea20534"
 options = ["!splitdoc"]
