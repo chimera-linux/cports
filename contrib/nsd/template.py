87b1cde31fb49eacf6eddcbaad7fe9d8555d985c
psykose <alice@ayaya.dev>
2024-08-04 10:59:54 +0200

    contrib/nsd: update to 4.10.1

--- contrib/nsd/template.py
+++ contrib/nsd/template.py
@@ -1,6 +1,6 @@
 pkgname = "nsd"
-pkgver = "4.10.0"
-pkgrel = 1
+pkgver = "4.10.1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-user=_nsd"]
 # simdzone fails to load its own includes if we don't do this
@@ -16,7 +16,7 @@ maintainer = "yanchan09 <yan@omg.lol>"
 license = "BSD-3-Clause"
 url = "https://nlnetlabs.nl/projects/nsd/about"
 source = f"https://nlnetlabs.nl/downloads/nsd/nsd-{pkgver}.tar.gz"
-sha256 = "6317d7f5e3f01c33912f313d66a33dd1ace1cdf7f19d5c590b2e430d8ca4605f"
+sha256 = "c0190f923f0095995f2e6331dacd92c6e1f4d578b880d61690602b43a5acfd84"
 hardening = ["cfi", "vis"]
 
 
