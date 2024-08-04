8959088b31499a82cf1c43d43364c8c0a10a88a0
ttyyls <contact@behri.org>
2024-08-04 11:45:47 +0100

    contrib/zathura-djvu: update to 0.2.10

--- contrib/zathura-djvu/template.py
+++ contrib/zathura-djvu/template.py
@@ -1,6 +1,6 @@
 pkgname = "zathura-djvu"
-pkgver = "0.2.9"
-pkgrel = 1
+pkgver = "0.2.10"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "meson",
@@ -16,7 +16,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Zlib"
 url = "https://pwmt.org/projects/zathura-djvu"
 source = f"{url}/download/zathura-djvu-{pkgver}.tar.xz"
-sha256 = "96e6f8a6ee53231073b2f7003264872f84501e63c3da7bf0598d046286b0c200"
+sha256 = "32e9d89929a76cd7d3fcbaf79f441868bdabedf17317d1d1843faa1f19338d95"
 
 
 def post_install(self):
