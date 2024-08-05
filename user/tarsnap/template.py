25e4988a534c2d899758d6b41952870c0413e060
Nasado <hi@nasado.name>
2024-08-04 20:59:46 -0600

    user/tarsnap: new package

---
+++ user/tarsnap/template.py
@@ -0,0 +1,20 @@
+pkgname = "tarsnap"
+pkgver = "1.0.40"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_gen = []
+makedepends = [
+    "e2fsprogs-devel",
+    "openssl-devel",
+    "zlib-ng-compat-devel",
+]
+pkgdesc = "Online backups for the truly paranoid"
+maintainer = "Nasado <hi@nasado.name>"
+license = "custom:tarsnap"
+url = "https://www.tarsnap.com"
+source = f"{url}/download/tarsnap-autoconf-{pkgver}.tgz"
+sha256 = "bccae5380c1c1d6be25dccfb7c2eaa8364ba3401aafaee61e3c5574203c27fd5"
+
+
+def post_install(self):
+    self.install_license("COPYING")
