53c77adac3dfd3e07145736364205457508e6c6e
psykose <alice@ayaya.dev>
2024-08-05 16:17:04 +0200

    main/gspell: reconf, ignore prerelease

--- main/gspell/template.py
+++ main/gspell/template.py
@@ -1,20 +1,26 @@
 pkgname = "gspell"
 pkgver = "1.12.2"
-pkgrel = 2
+pkgrel = 3
 build_style = "gnu_configure"
-configure_args = ["--disable-static", "--enable-introspection", "--enable-vala"]
+configure_args = [
+    "--disable-static",
+    "--enable-introspection",
+    "--enable-vala",
+]
 make_cmd = "gmake"
 hostmakedepends = [
+    "automake",
+    "gettext-devel",
     "gmake",
-    "pkgconf",
-    "gettext",
     "gobject-introspection",
+    "gtk-doc-tools",
+    "libtool",
+    "pkgconf",
     "vala",
-    "glib-devel",
 ]
 makedepends = [
-    "glib-devel",
     "enchant-devel",
+    "glib-devel",
     "gtk+3-devel",
     "icu-devel",
     "vala",
@@ -25,13 +31,11 @@ license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gspell"
 source = f"$(GNOME_SITE)/gspell/{pkgver[:-2]}/gspell-{pkgver}.tar.xz"
 sha256 = "b4e993bd827e4ceb6a770b1b5e8950fce3be9c8b2b0cbeb22fdf992808dd2139"
-# needs graphical env
-options = ["!check"]
+# check: needs seatful headless
+# cross: gobject-introspection
+options = ["!check", "!cross"]
 
 
 @subpackage("gspell-devel")
 def _devel(self):
     return self.default_devel()
-
-
-configure_gen = []
