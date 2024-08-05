bd30ec988de67ab488946f6d728af4c81310e183
psykose <alice@ayaya.dev>
2024-08-05 12:03:37 +0200

    main/python-flake8: update to 7.1.1

--- main/python-flake8/template.py
+++ main/python-flake8/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-flake8"
-pkgver = "7.1.0"
+pkgver = "7.1.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://flake8.pycqa.org"
 source = f"https://github.com/PyCQA/flake8/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "af2223f8d6476097ac0bf58a08910fa19b4c045d37f6c8e26532d3f4076b78ba"
+sha256 = "e389d1806e1f911b092fd41b7989c751f05eda510bab4ac255b144da96477e2b"
 # some failures in new testsuite
 options = ["!check"]
 
