db1ebce0fa8fcaabf5785f93e9b5f07f7c20cb7c
psykose <alice@ayaya.dev>
2024-08-05 15:43:51 +0200

    contrib/nvme-cli: update to 2.10.2

--- contrib/nvme-cli/template.py
+++ contrib/nvme-cli/template.py
@@ -1,5 +1,5 @@
 pkgname = "nvme-cli"
-pkgver = "2.10"
+pkgver = "2.10.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -19,7 +19,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/linux-nvme/nvme-cli"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "ffbd53a7c8bc00d51c9264b4e12c223f087d1dc6199655ff50b691e0fafeda67"
+sha256 = "b3c256959ff34124788aa96c8602c9cef00705d01cc3cb9322bf3269e00ae904"
 hardening = ["vis", "cfi"]
 # require /dev nvme device
 options = ["!check"]
