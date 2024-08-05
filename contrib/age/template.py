3d36b67341dc2612cad612d0d520653e9efe4515
triallax <triallax@tutanota.com>
2024-08-05 18:59:24 +0100

    contrib/age: run tests

--- contrib/age/template.py
+++ contrib/age/template.py
@@ -10,8 +10,6 @@ license = "BSD-3-Clause"
 url = "https://github.com/FiloSottile/age"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "cefe9e956401939ad86a9c9d7dcf843a43b6bcdf4ee7d8e4508864f227a3f6f0"
-# tests invoke network downloads for vectors
-options = ["!check"]
 
 
 def post_install(self):
