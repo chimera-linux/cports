a12a9a0fc801d53e537159a50a07191bd9a7420e
q66 <q66@chimera-linux.org>
2024-08-06 13:02:20 +0200

    contrib/crane: move test rm to post_extract
    This prevents errors when manually re-running tests

--- contrib/crane/template.py
+++ contrib/crane/template.py
@@ -12,18 +12,17 @@ source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "4bae53f34011e35ef874a60123b8ae70a5e992d804decb030479dbb888afe6d1"
 
 
+def post_extract(self):
+    # marked broken on arm64/darwin; apparently also on musl in general
+    self.do("rm", "pkg/v1/google/auth_test.go")
+
+
 # docs are present but they do not render properly as manpages
 # and so they are skipped here
 def post_build(self):
     for shell in ["bash", "fish", "zsh"]:
         with open(self.cwd / f"crane.{shell}comp", "w") as cf:
             self.do(f"{self.make_dir}/crane", "completion", shell, stdout=cf)
-
-
-def pre_check(self):
-    # This particular test is marked broken on arm64 and darwin.
-    # It seems it is also broken on linux-musl.
-    self.do("rm", "pkg/v1/google/auth_test.go")
 
 
 def post_install(self):
