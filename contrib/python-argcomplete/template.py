7a3d78c1b9c74c677a801b53f3649ad98b60278b
psykose <alice@ayaya.dev>
2024-08-06 21:16:34 +0200

    contrib/python-argcomplete: update to 3.5.0

--- contrib/python-argcomplete/template.py
+++ contrib/python-argcomplete/template.py
@@ -1,27 +1,49 @@
 pkgname = "python-argcomplete"
-pkgver = "3.4.0"
+pkgver = "3.5.0"
 pkgrel = 0
 build_style = "python_pep517"
+make_build_env = {"SETUPTOOLS_SCM_PRETEND_VERSION": pkgver}
 hostmakedepends = [
     "python-build",
     "python-installer",
-    "python-setuptools",
     "python-setuptools_scm",
-    "python-wheel",
 ]
 depends = ["python"]
-checkdepends = ["bash", "python-pexpect", "python-pip", "zsh"]
+checkdepends = [
+    "bash",
+    "python-pexpect",
+    "python-pip",
+    "zsh",
+]
 pkgdesc = "Python tab completion plugin"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/kislyuk/argcomplete"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "a66504125897239587a5422e31535698704baa67d54604557b17176d56bc69bd"
+sha256 = "8f240c6252d7a72bfc0a1273fdca658c28195081fd83b56562928917703ef60c"
 
 
-def init_build(self):
-    self.env["SETUPTOOLS_SCM_PRETEND_VERSION"] = pkgver
-
-
+# this is identical to the default do_check, but without pytest
 def do_check(self):
-    self.do("python", "-m", "unittest", "-b", "test/test.py")
+    whl = list(
+        map(
+            lambda p: str(p.relative_to(self.cwd)),
+            self.cwd.glob("dist/*.whl"),
+        )
+    )
+
+    self.rm(".cbuild-checkenv", recursive=True, force=True)
+    self.do(
+        "python3",
+        "-m",
+        "venv",
+        "--without-pip",
+        "--system-site-packages",
+        "--clear",
+        ".cbuild-checkenv",
+    )
+
+    envpy = self.chroot_cwd / ".cbuild-checkenv/bin/python3"
+
+    self.do(envpy, "-m", "installer", *whl)
+    self.do(envpy, "test/test.py", path=[envpy.parent])
