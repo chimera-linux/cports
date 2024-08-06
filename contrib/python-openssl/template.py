499eab3c0ea97bf717a8c7c9b828c289aadc7bd7
psykose <alice@ayaya.dev>
2024-08-06 21:20:11 +0200

    contrib/python-openssl: update to 24.2.1

--- contrib/python-openssl/template.py
+++ contrib/python-openssl/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-openssl"
-pkgver = "24.1.0"
+pkgver = "24.2.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python-cryptography"]
 checkdepends = [
@@ -19,5 +18,5 @@ pkgdesc = "Python interface to OpenSSL"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://pyopenssl.org"
-source = f"$(PYPI_SITE)/p/pyOpenSSL/pyOpenSSL-{pkgver}.tar.gz"
-sha256 = "cabed4bfaa5df9f1a16c0ef64a0cb65318b5cd077a7eda7d6970131ca2f41a6f"
+source = f"$(PYPI_SITE)/p/pyopenssl/pyopenssl-{pkgver}.tar.gz"
+sha256 = "4247f0dbe3748d560dcbb2ff3ea01af0f9a1a001ef5f7c4c647956ed8cbf0e95"
