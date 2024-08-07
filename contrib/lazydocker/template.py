85647e63aae9d19f93d8942c5ddcb4b151ece6d0
ttyyls <contact@behri.org>
2024-08-07 11:01:46 +0100

    contrib/lazydocker: new package (0.23.3)

---
+++ contrib/lazydocker/template.py
@@ -0,0 +1,15 @@
+pkgname = "lazydocker"
+pkgver = "0.23.3"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go"]
+pkgdesc = "TUI for docker resource visualization and management"
+maintainer = "ttyyls <contact@behri.org>"
+license = "MIT"
+url = "https://github.com/jesseduffield/lazydocker"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "6ea52b69c3fb5cb371a01e55d1deeaf053b17fb240be069af577246c5169f4f1"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
