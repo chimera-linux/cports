3a6813227ed14f1ddf7ce2310a191657e6bbb5b8
Renato Botelho do Couto <renato@netgate.com>
2024-08-07 10:12:54 -0300

    contrib/igmpproxy: new package (0.4)

---
+++ contrib/igmpproxy/template.py
@@ -0,0 +1,19 @@
+pkgname = "igmpproxy"
+pkgver = "0.4"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["automake"]
+makedepends = ["linux-headers"]
+pkgdesc = "Multicast forwarding IGMP proxy"
+maintainer = "Renato Botelho do Couto <renato@netgate.com>"
+license = "GPL-2.0-or-later"
+url = "https://github.com/pali/igmpproxy"
+source = f"{url}/releases/download/{pkgver}/igmpproxy-{pkgver}.tar.gz"
+sha256 = "afa4b75a823b82f71ce99f33eae4e8136b906ae8a5ede5caaad93bac38cdae24"
+# Not available
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("COPYING")
+    self.install_service(self.files_path / "igmpproxy")
