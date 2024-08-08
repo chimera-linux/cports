3638047d008d24057ea0c20734924516fd9aa521
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-08-08 23:28:18 +0200

    contrib/openpgp-card-tools: update to 0.11.3

--- contrib/openpgp-card-tools/template.py
+++ contrib/openpgp-card-tools/template.py
@@ -1,16 +1,16 @@
 pkgname = "openpgp-card-tools"
-pkgver = "0.10.1"
+pkgver = "0.11.3"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
-makedepends = ["rust-std", "pcsc-lite-devel", "nettle-devel", "bzip2-devel"]
+makedepends = ["rust-std", "pcsc-lite-devel"]
 depends = ["ccid"]
 pkgdesc = "CLI tool for inspecting, configuring and using OpenPGP cards"
 maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0 OR MIT"
 url = "https://codeberg.org/openpgp-card/openpgp-card-tools"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "fb8cd1c95c558f1e16a749746dadc6040deca54f4e3aa7735ffa5f082cdfcc01"
+sha256 = "083fe2e127baaee5dc799274d7208db5a3b7debde433f762263b8e4912af1d3c"
 
 
 def post_install(self):
