ccdf23751fc4d64495438ee8d39c76358f1daffa
Wesley Moore <wes@wezm.net>
2024-08-08 11:14:56 +0000

    contrib/jaq: update to 1.6.0

--- contrib/jaq/template.py
+++ contrib/jaq/template.py
@@ -1,5 +1,5 @@
 pkgname = "jaq"
-pkgver = "1.5.1"
+pkgver = "1.6.0"
 pkgrel = 0
 build_style = "cargo"
 # disable the default mimalloc feature and just use the system allocator
@@ -13,7 +13,7 @@ maintainer = "Wesley Moore <wes@wezm.net>"
 license = "MIT"
 url = "https://github.com/01mf02/jaq"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "a4052d93f761274036e40fdb27f186ffe9555a93d88fee8e2364b6a677ae6426"
+sha256 = "64b3431970cd4c27f3c4e665913218f44a0f44be7e22401eea34d52d8f3745a9"
 
 
 def do_install(self):
