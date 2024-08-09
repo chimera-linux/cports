906a5cfa9d770a05a8bc696e417f604e2d338434
Wesley Moore <wes@wezm.net>
2024-08-09 13:22:13 +0000

    user/lego: new package

---
+++ user/lego/template.py
@@ -0,0 +1,18 @@
+pkgname = "lego"
+pkgver = "4.17.4"
+pkgrel = 0
+build_style = "go"
+make_build_args = ["-ldflags", f"-X main.version={pkgver}", "./cmd/lego"]
+hostmakedepends = ["go"]
+pkgdesc = "Let's Encrypt/ACME client"
+maintainer = "Wesley Moore <wes@wezm.net>"
+license = "MIT"
+url = "https://github.com/go-acme/lego"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "27f873708c904ce6c6347f47cae1eba3a00a7948e2b915982f8a209f069c1277"
+# check: tests need network access: https://github.com/go-acme/lego/issues/560
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
