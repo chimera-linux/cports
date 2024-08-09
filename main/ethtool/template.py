d966a741c455330f1c058881d3d6f816e0307b01
psykose <alice@ayaya.dev>
2024-08-09 20:14:54 +0200

    main/ethtool: update to 6.10

--- main/ethtool/template.py
+++ main/ethtool/template.py
@@ -1,5 +1,5 @@
 pkgname = "ethtool"
-pkgver = "6.9"
+pkgver = "6.10"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "libtool"]
@@ -9,6 +9,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "http://www.kernel.org/pub/software/network/ethtool"
 source = f"{url}/ethtool-{pkgver}.tar.xz"
-sha256 = "a71b0354010661c5cf178bc606ed50fcb91805cf1897ad0eb2818387a5fd0cd9"
+sha256 = "cc613fe8a2bcddee17a1e6e0d763c0f3ea33c7e930658d2d7f601aa65e426a1f"
 # FIXME int
 hardening = ["vis", "cfi", "!int"]
