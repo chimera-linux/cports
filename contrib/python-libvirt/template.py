57b8e61a80f476916d06b5056683ee6e0cc9debd
psykose <alice@ayaya.dev>
2024-08-09 21:47:22 +0200

    contrib/python-libvirt: update to 10.6.0

--- contrib/python-libvirt/template.py
+++ contrib/python-libvirt/template.py
@@ -1,12 +1,12 @@
 pkgname = "python-libvirt"
-pkgver = "10.5.0"
+pkgver = "10.6.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "pkgconf",
     "python-build",
+    "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 makedepends = [
     "libvirt-devel",
@@ -14,7 +14,6 @@ makedepends = [
 ]
 depends = ["python"]
 checkdepends = [
-    "python-installer",
     "python-lxml",
     "python-pytest",
 ]
@@ -23,4 +22,4 @@ maintainer = "cesorious <cesorious@gmail.com>"
 license = "LGPL-2.1-or-later"
 url = "https://pypi.org/project/libvirt-python"
 source = f"https://libvirt.org/sources/python/libvirt-python-{pkgver}.tar.gz"
-sha256 = "785023500f58d3e8e829af98647d43eee97e517aacc9d9e7ded43594ea52d032"
+sha256 = "e4259cbd724f784fca9bf22b13e8af1195dead6beee4c6ea08481a66dfdc79e4"
