77eeb9701194f96f6ffac52938af1a4051956f80
psykose <alice@ayaya.dev>
2024-08-09 21:49:30 +0200

    contrib/libucontext: update to 1.3.1

--- contrib/libucontext/template.py
+++ contrib/libucontext/template.py
@@ -1,16 +1,15 @@
 pkgname = "libucontext"
-pkgver = "1.2"
-pkgrel = 1
+pkgver = "1.3.1"
+pkgrel = 0
 build_style = "meson"
-hostmakedepends = ["meson", "ninja", "pkgconf", "scdoc"]
+hostmakedepends = ["meson", "pkgconf", "scdoc"]
 pkgdesc = "Implementation of ucontext featuring glibc-compatible ABI"
 maintainer = "eater <=@eater.me>"
 license = "ISC"
 url = "https://github.com/kaniini/libucontext"
-source = (
-    f"https://github.com/kaniini/libucontext/archive/refs/tags/v{pkgver}.tar.gz"
-)
-sha256 = "2657e087c493263e7bbbde152a5bc08ce22dc5a7970887ac4fd251b90b58401f"
+source = f"{url}/archive/refs/tags/libucontext-{pkgver}.tar.gz"
+sha256 = "1243ee9f03ad38e624f6844427b7bc1f0a05aa5de70f15f3b03805a364b971d6"
+# see common-trampoline.c (1.3.1) libucontext_trampoline comment
 options = ["!framepointer"]
 
 
