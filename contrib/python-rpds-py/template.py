5a5d81cbcf2b9032f5445b57226585df2cf84caa
psykose <alice@ayaya.dev>
2024-08-09 21:41:12 +0200

    contrib/python-rpds-py: update to 0.20.0

--- contrib/python-rpds-py/template.py
+++ contrib/python-rpds-py/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-rpds-py"
-pkgver = "0.19.1"
+pkgver = "0.20.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -21,7 +21,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/crate-py/rpds"
 source = f"$(PYPI_SITE)/r/rpds-py/rpds_py-{pkgver}.tar.gz"
-sha256 = "31dd5794837f00b46f4096aa8ccaa5972f73a938982e32ed817bb520c465e520"
+sha256 = "d72a210824facfdaf8768cf2d7ca25a042c30320b3020de2fa04640920d4e121"
 
 
 def post_patch(self):
