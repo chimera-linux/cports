e0e30679a1c911d473a3cd24e48439fa8f6fc379
Jami Kettunen <jami.kettunen@protonmail.com>
2024-08-10 20:19:50 +0300

    contrib/fastfetch: update to 2.21.3

--- contrib/fastfetch/template.py
+++ contrib/fastfetch/template.py
@@ -1,5 +1,5 @@
 pkgname = "fastfetch"
-pkgver = "2.21.0"
+pkgver = "2.21.3"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_TESTS=ON", "-DENABLE_SYSTEM_YYJSON=ON"]
@@ -9,6 +9,7 @@ makedepends = [
     "dbus-devel",
     "dconf-devel",
     "ddcutil-devel",
+    "elfutils-devel",
     "libmagick-devel",
     "libpulse-devel",
     "libxrandr-devel",
@@ -26,7 +27,7 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "MIT"
 url = "https://github.com/fastfetch-cli/fastfetch"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "21d085a612b6bd9ab0f4e7bffe2632e313e6f67d432251cfd5a7f877b7194733"
+sha256 = "cec1f126ade7a5ef971901b1cdbe79f5864523d7a0a92732991619485d13e2e7"
 tool_flags = {"CFLAGS": ["-DNDEBUG"]}
 # CFI: dies immediately (ffPlatformPathAddHome at FFlist.c:31:12)
 hardening = ["vis", "!cfi"]
