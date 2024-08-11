e74edf463f5b401949549419c320ea8f31c29ba1
psykose <alice@ayaya.dev>
2024-08-11 18:09:53 +0200

    contrib/pcsc-tools: update to 1.7.2

--- contrib/pcsc-tools/template.py
+++ contrib/pcsc-tools/template.py
@@ -1,6 +1,6 @@
 pkgname = "pcsc-tools"
-pkgver = "1.7.1"
-pkgrel = 1
+pkgver = "1.7.2"
+pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
 hostmakedepends = [
@@ -21,4 +21,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://pcsc-tools.apdu.fr"
 source = f"{url}/pcsc-tools-{pkgver}.tar.bz2"
-sha256 = "0d024b589e15d79eac8506cd67df7b53cf91e9e6a493c8319f33cd29b5f36426"
+sha256 = "fd6fcc25e8140f613b3bf44b02eda4a8a429ee4939fb777f474e2c829f7b472c"
