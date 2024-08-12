d2d5557f4eef055ce100367d3ffee5bf37004964
psykose <alice@ayaya.dev>
2024-08-12 14:03:11 +0200

    contrib/capstone: update to 5.0.2

--- contrib/capstone/template.py
+++ contrib/capstone/template.py
@@ -1,5 +1,5 @@
 pkgname = "capstone"
-pkgver = "5.0.1"
+pkgver = "5.0.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
@@ -14,7 +14,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-3-Clause-Clear"
 url = "https://www.capstone-engine.org"
 source = f"https://github.com/capstone-engine/capstone/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "2b9c66915923fdc42e0e32e2a9d7d83d3534a45bb235e163a70047951890c01a"
+sha256 = "9d0be727cc942075a1696f576b88918eb0daf9db7a02f563f0c4e51a439a611d"
 hardening = ["vis", "!cfi"]
 
 
