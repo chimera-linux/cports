8537d644fec2a9b6149bdaf4204ada1ca80a99fc
psykose <alice@ayaya.dev>
2024-08-12 14:17:23 +0200

    contrib/crispy-doom: update to 7.0

--- contrib/crispy-doom/template.py
+++ contrib/crispy-doom/template.py
@@ -1,5 +1,5 @@
 pkgname = "crispy-doom"
-pkgver = "6.0"
+pkgver = "7.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf"]
@@ -15,5 +15,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/fabiangreffrath/crispy-doom"
 source = f"{url}/archive/crispy-doom-{pkgver}.tar.gz"
-sha256 = "2b85649c615efeac7573883370e9434255af301222b323120692cb9649b7f420"
+sha256 = "25eea88fdbe1320ad0d1a3e0ed66ae8d985c39b79e442beab5fc36d9d5ddfc42"
 hardening = ["vis", "!cfi", "!int"]
