0fe635d5f558907535f69e75c02aebf70ab84524
psykose <alice@ayaya.dev>
2024-08-12 14:04:33 +0200

    contrib/bluetuith: update to 0.2.3

--- contrib/bluetuith/template.py
+++ contrib/bluetuith/template.py
@@ -1,6 +1,6 @@
 pkgname = "bluetuith"
-pkgver = "0.2.2"
-pkgrel = 5
+pkgver = "0.2.3"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 depends = ["bluez"]
@@ -9,7 +9,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://github.com/darkhz/bluetuith"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "2a02f51c53668fa3171e642e25f268fc50fbb2438f764956fb7cd46fb786083d"
+sha256 = "ff4ca6e5fda87b33c472b703c81b4000d1df4efb2bdc0af1762e4a3e3c507228"
 
 
 def post_install(self):
