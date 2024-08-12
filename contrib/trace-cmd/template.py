433a7c789309734652c8fbdfc2f7eae6551524e2
psykose <alice@ayaya.dev>
2024-08-12 15:03:31 +0200

    contrib/trace-cmd: new package

---
+++ contrib/trace-cmd/template.py
@@ -0,0 +1,26 @@
+pkgname = "trace-cmd"
+pkgver = "3.3"
+pkgrel = 0
+build_style = "meson"
+configure_args = [f"-Dversion-tag={pkgver}"]
+make_build_args = ["all", "docs"]
+hostmakedepends = [
+    "asciidoc",
+    "bash",
+    "meson",
+    "pkgconf",
+    "xmlto",
+]
+makedepends = [
+    "libtraceevent-devel",
+    "libtracefs-devel",
+    "linux-headers",
+    "zlib-ng-compat-devel",
+    "zstd-devel",
+]
+pkgdesc = "Linux ftrace CLI tool"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-only"
+url = "https://www.trace-cmd.org"
+source = f"https://git.kernel.org/pub/scm/utils/trace-cmd/trace-cmd.git/snapshot/trace-cmd-v{pkgver}.tar.gz"
+sha256 = "0671048dd2b8abdb8b2d0eb9cc91335c997620a7da72f858381ff4206f32065f"
