c3bfdabd4071951d9931ecaf3e54812cdfab4fec
Mara <177581589+catgirlconspiracy@users.noreply.github.com>
2024-08-13 00:34:52 +0200

    contrib/ansible-core: new package

---
+++ contrib/ansible-core/template.py
@@ -0,0 +1,38 @@
+pkgname = "ansible-core"
+pkgver = "2.17.3"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+]
+depends = [
+    "python-cryptography",
+    "python-jinja2",
+    "python-packaging",
+    "python-pyyaml",
+    "python-resolvelib",
+]
+checkdepends = [
+    "git",
+    "openssh",
+    "python-pytest",
+    "python-pytest-mock",
+    "python-pytest-xdist",
+    *depends,
+]
+pkgdesc = "Configuration management and multinode orchestration framework"
+subdesc = "core components"
+maintainer = "Mara <177581589+catgirlconspiracy@users.noreply.github.com>"
+license = "GPL-3.0-or-later"
+url = "https://ansible.com"
+source = f"$(PYPI_SITE)/a/ansible-core/ansible_core-{pkgver}.tar.gz"
+sha256 = "917557065339fe36e7078e9bea47eefab6d6877f3bd435fa5f0d766d04c58485"
+
+
+def do_check(self):
+    self.do(
+        "./bin/ansible-test",
+        "units",
+    )
