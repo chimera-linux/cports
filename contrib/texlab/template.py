fc8b8ba907986d63c5ddabbf558de958a358422d
ttyyls <contact@behri.org>
2024-08-12 11:35:54 +0100

    contrib/texlab: new package (5.19.0)

---
+++ contrib/texlab/template.py
@@ -0,0 +1,16 @@
+pkgname = "texlab"
+pkgver = "5.19.0"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable"]
+makedepends = ["rust-std"]
+pkgdesc = "LaTeX LSP server"
+maintainer = "ttyyls <contact@behri.org>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/latex-lsp/texlab"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "ad72171dd267fd73ecc6a05f9ff3cc068e77a3b82f986305ab455aeade841294"
+
+
+def do_install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/texlab")
