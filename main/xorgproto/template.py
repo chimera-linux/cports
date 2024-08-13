27e791d5a89b4a4a6d53d9c4287c37498c4bd0f5
triallax <triallax@tutanota.com>
2024-08-13 03:35:32 +0100

    main/xorgproto: clean up

--- main/xorgproto/template.py
+++ main/xorgproto/template.py
@@ -3,7 +3,7 @@ pkgver = "2024.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-legacy"]
-hostmakedepends = ["pkgconf", "xorg-util-macros", "automake", "libtool"]
+hostmakedepends = ["automake", "libtool", "pkgconf", "xorg-util-macros"]
 pkgdesc = "Combined X.Org X11 protocol headers"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
@@ -18,16 +18,10 @@ def post_install(self):
     for f in self.cwd.glob("COPYING-*"):
         self.install_license(f)
 
-    for f in (self.destdir / "usr/include/X11/extensions").glob("apple*"):
-        f.unlink()
-    for f in (self.destdir / "usr/include/X11/extensions").glob("windows*"):
-        f.unlink()
-
-    self.uninstall(f"usr/share/licenses/{pkgname}/COPYING-applewmproto")
-    self.uninstall(f"usr/share/licenses/{pkgname}/COPYING-windowswmproto")
-
-    self.uninstall("usr/share/pkgconfig/applewmproto.pc")
-    self.uninstall("usr/share/pkgconfig/windowswmproto.pc")
+    for os in ["apple", "windows"]:
+        self.uninstall(f"usr/include/X11/extensions/{os}*", glob=True)
+        self.uninstall(f"usr/share/licenses/{pkgname}/COPYING-{os}wmproto")
+        self.uninstall(f"usr/share/pkgconfig/{os}wmproto.pc")
 
     # provided by libx11-devel
     self.uninstall("usr/include/X11/extensions/XKBgeom.h")
