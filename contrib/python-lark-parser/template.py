647a1222c1843c6f7e28aedba3ab897f9e1c86dc
psykose <alice@ayaya.dev>
2024-08-13 15:16:33 +0200

    contrib/python-lark-parser: update to 1.2.1

--- contrib/python-lark-parser/template.py
+++ contrib/python-lark-parser/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-lark-parser"
-pkgver = "1.1.9"
-pkgrel = 1
+pkgver = "1.2.1"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python"]
 checkdepends = ["python-pytest"]
@@ -15,7 +14,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://github.com/lark-parser/lark"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "32f5f3e1418df3a9032d1a8f9bea62427319e28733ac463f5f37d311b1d99c01"
+sha256 = "6e74e07b35829809e846e5d2b1e5b806394b35a8ad2569e1df45dd8d49b71681"
 
 
 def post_install(self):
