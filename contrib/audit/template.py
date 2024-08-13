4c7903f127648f5c4c35407feed66acf606dadc3
triallax <triallax@tutanota.com>
2024-08-13 01:09:23 +0100

    contrib/audit: https url

--- contrib/audit/template.py
+++ contrib/audit/template.py
@@ -1,6 +1,6 @@
 pkgname = "audit"
 pkgver = "4.0.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = [
     "--disable-zos-remote",
@@ -18,7 +18,7 @@ makedepends = [
 pkgdesc = "User space tools for kernel auditing"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-or-later"
-url = "http://people.redhat.com/sgrubb/audit"
+url = "https://people.redhat.com/sgrubb/audit"
 source = f"{url}/audit-{pkgver}.tar.gz"
 sha256 = "d5d1b5d50ee4a2d0d17875bc6ae6bd6a7d5b34d9557ea847a39faec531faaa0a"
 
