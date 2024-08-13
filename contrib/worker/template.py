fa79ee89fade4c1f92d5d3d966e299983a67a36f
triallax <triallax@tutanota.com>
2024-08-13 03:43:09 +0100

    contrib/worker: update to 5.1.0

--- contrib/worker/template.py
+++ contrib/worker/template.py
@@ -1,9 +1,8 @@
 pkgname = "worker"
-pkgver = "5.0.2"
+pkgver = "5.1.0"
 pkgrel = 0
-build_style = "configure"
-configure_args = ["--prefix=/usr"]
-hostmakedepends = ["pkgconf"]
+build_style = "gnu_configure"
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 makedepends = [
     "dbus-devel",
     "file-devel",
@@ -19,7 +18,7 @@ url = "http://www.boomerangsworld.de/cms/worker/index.html"
 source = (
     f"http://www.boomerangsworld.de/cms/worker/downloads/worker-{pkgver}.tar.gz"
 )
-sha256 = "9381e0217bb2d2aef6a4784653e02e23009434c1ff89c6ac11758815cff0e4ca"
+sha256 = "4df7ffd48f51658a36a171ac9eb19e4acebd5bd9cfbbb3850f0175060e91d35b"
 hardening = ["cfi", "vis"]
 # no tests
 options = ["!check"]
