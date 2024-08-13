8fdfcdd577d7bec92ccc68b889f8f8a7c24ec082
psykose <alice@ayaya.dev>
2024-08-13 15:15:54 +0200

    contrib/docker-compose: update to 2.29.2

--- contrib/docker-compose/template.py
+++ contrib/docker-compose/template.py
@@ -1,6 +1,6 @@
 pkgname = "docker-compose"
-pkgver = "2.29.1"
-pkgrel = 1
+pkgver = "2.29.2"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     "-ldflags",
@@ -14,7 +14,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://docs.docker.com/compose"
 source = f"https://github.com/docker/compose/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "9749d621b1b8bc1f5881741e0cbad20b5133a0aeedb9339af06b0187be8141b1"
+sha256 = "02bb02c6f018c1332b462a6140e0c1c1301cc86158d76c9cab84d7fa29254bc1"
 # need a running docker daemon
 options = ["!check"]
 
