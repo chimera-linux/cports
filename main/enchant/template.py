681d329f85b26619a17b041bfe5c8a5a6911b04a
psykose <alice@ayaya.dev>
2024-08-14 17:59:57 +0200

    main/enchant: update to 2.8.2

--- main/enchant/template.py
+++ main/enchant/template.py
@@ -1,5 +1,5 @@
 pkgname = "enchant"
-pkgver = "2.8.1"
+pkgver = "2.8.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-relocatable", "--disable-static"]
@@ -25,7 +25,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://abiword.github.io/enchant"
 source = f"https://github.com/AbiWord/enchant/releases/download/v{pkgver}/enchant-{pkgver}.tar.gz"
-sha256 = "ff79de470b8eb16f53849dc49f2bce8ca4eb7decabfc1349716fe12616e52f4e"
+sha256 = "8f19535adb5577b83b00e02f330fe9b9eb40dd21f19e2899636fc4d3a7696375"
 
 
 @subpackage("enchant-devel")
