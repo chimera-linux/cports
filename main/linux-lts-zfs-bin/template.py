e85cf29e402dac5e78c389ea6112235c8e81e84d
psykose <alice@ayaya.dev>
2024-08-14 17:55:23 +0200

    main/linux-lts: update to 6.6.46

--- main/linux-lts-zfs-bin/template.py
+++ main/linux-lts-zfs-bin/template.py
@@ -1,5 +1,5 @@
 pkgname = "linux-lts-zfs-bin"
-_kernver = "6.6.45"
+_kernver = "6.6.46"
 _zfsver = "2.2.4"
 pkgver = f"{_zfsver}.{_kernver}"
 pkgrel = 0
--- main/linux-lts/template.py
+++ main/linux-lts/template.py
@@ -1,6 +1,6 @@
 # update linux-lts-zfs-bin when bumping
 pkgname = "linux-lts"
-pkgver = "6.6.45"
+pkgver = "6.6.46"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]
 make_dir = "build"
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
-sha256 = "121bed240767e4a0959c1609e78eeaaf3e0620d9d1a5ed1f6e36bdf609c4f179"
+sha256 = "052f932396d9c7d84ceeda91226a8ef797c12188bde41e6c419602d990dd45f2"
 # no meaningful checking to be done
 options = [
     "!check",
