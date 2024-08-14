c7819f53c4ef683565ce2b02a27ddd7bcfab142a
q66 <q66@chimera-linux.org>
2024-08-14 11:28:28 +0200

    contrib/cliphlist: disable tests

--- contrib/cliphist/template.py
+++ contrib/cliphist/template.py
@@ -10,3 +10,5 @@ license = "GPL-3.0-only"
 url = "https://github.com/sentriz/cliphist"
 source = f"{url}/archive/v{pkgver}.tar.gz"
 sha256 = "02285cf3358a1851e34f95c0c369b27284d8d0996759d759fa2bbcb5b30fb13d"
+# cannot use nopTestdeps{} blabla ...
+options = ["!check"]
