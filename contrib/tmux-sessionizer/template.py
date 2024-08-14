a74269fdccd139f6defe7ea3d4ef748142d561ac
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-08-14 19:14:28 -0300

    contrib/tmux-sessionizer: new package
    update maintainer name
    
    contrib/tmux-sessionizer: fix dependencies and url
    
    contrib/tmux-sessionizer: rename source so it doesn't conflict
    
    contrib/tmux-sessionizer: install license in do_install
    
    contrib/tmux-sessionizer: reword pkg description
    
    change cargo for cargo auditable and remove useless do_install
    
    fix postinstall wrong function name
    
    change source to use archive one
    
    remove useless format string
    
    bump version

---
+++ contrib/tmux-sessionizer/template.py
@@ -0,0 +1,16 @@
+pkgname = "tmux-sessionizer"
+pkgver = "0.4.3"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable", "pkgconf"]
+makedepends = ["rust-std", "openssl-devel", "libgit2-devel"]
+pkgdesc = "Tool for opening git repositories as tmux sessions"
+maintainer = "jabuxas <lucasbarbieri.c@gmail.com>"
+license = "MIT"
+url = "https://github.com/jrmoulton/tmux-sessionizer"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "e7baf324409af475a96d2b034a2ecdb46452feb7d0ef3ddc41e834475063a1f7"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
