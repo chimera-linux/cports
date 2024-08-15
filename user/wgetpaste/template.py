c0e35741f7c045424818e6a0d02a55a35af5e179
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-08-15 02:18:30 -0300

    user/wgetpaste: new package

---
+++ user/wgetpaste/template.py
@@ -0,0 +1,16 @@
+pkgname = "wgetpaste"
+pkgver = "2.34"
+pkgrel = 0
+depends = ["bash", "wget2"]
+pkgdesc = "Command-line interface to various paste-bins"
+maintainer = "jabuxas <lucasbarbieri.c@gmail.com>"
+license = "MIT"
+url = "https://github.com/zlin/wgetpaste"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "097b77440460365976f8f66e99b0150c8a9527307f6ecade1db6b60a0bfad781"
+
+
+def do_install(self):
+    self.install_bin("wgetpaste")
+    self.install_completion("_wgetpaste", "zsh")
+    self.install_license("LICENSE")
