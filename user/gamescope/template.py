75fba9fd1ef6bdfb3b5fc719460cbe431cfa7eac
triallax <triallax@tutanota.com>
2024-08-15 20:33:39 +0100

    user/gamescope: update to 3.14.29

--- user/gamescope/template.py
+++ user/gamescope/template.py
@@ -1,5 +1,5 @@
 pkgname = "gamescope"
-pkgver = "3.14.24"
+pkgver = "3.14.29"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -58,7 +58,7 @@ source = [
     f"{url}/archive/refs/tags/{pkgver}.tar.gz",
     "https://github.com/ValveSoftware/openvr/archive/ebd425331229365dc3ec42d1bb8b2cc3c2332f81.tar.gz",
     "https://github.com/Joshua-Ashton/vkroots/archive/5106d8a0df95de66cc58dc1ea37e69c99afc9540.tar.gz",
-    "https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/a5c9826e6d7d8b504b07d1c02425e6f62b020791.tar.gz",
+    "https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/4666996b2a34b53899aedafcd3586fb1c7a16af6.tar.gz",
     "https://gitlab.freedesktop.org/emersion/libliftoff/-/archive/8b08dc1c14fd019cc90ddabe34ad16596b0691f4.tar.gz",
     "https://github.com/Joshua-Ashton/reshade/archive/696b14cd6006ae9ca174e6164450619ace043283.tar.gz",
 ]
@@ -71,10 +71,10 @@ source_paths = [
     "src/reshade",
 ]
 sha256 = [
-    "429ef04aab9b3ca5cac76752f59e2219d8326010c0cd9533ad580f65f5bfd3a3",
+    "e4d50ab4419760d2f52203a06184b31366b672a1d34d72f3e51d9ff3cdf56507",
     "c1913dbd1f3218b113ca7921d8cf9a1c1ddd28b432452c236cdd027dfbd7a95b",
     "37b77586e91f7ebee70380dcddd73bf01ae4acef1053e6be41d0485ede022422",
-    "f3f91b679114e565d94e87cd0c4c61444e48d7ef8a77cd101ef3081fd87f4726",
+    "83d95c49caa8bf92019428ae0b5e59804d5d662c2589b0cc8c39fe4029267bde",
     "8de28aee6f90f47b7fc7037dcd2360166197c0b5d2033f3afdbd34f2ea1bf216",
     "3aa6feda7773cc8ffa8fb012fe95e6207c776101e29198d0e0d34a0c5e339f6a",
 ]
@@ -94,5 +94,3 @@ tool_flags = {"CXXFLAGS": ["-DRTLD_DEEPBIND=0"]}
 
 def post_install(self):
     self.install_license("LICENSE")
-    # already installed
-    self.uninstall("usr/share/licenses/gamescope/LICENSE")
