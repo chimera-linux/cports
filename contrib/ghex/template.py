9b5ec1c44a776c864714bbb45508b32ed69ba642
q66 <q66@chimera-linux.org>
2024-08-15 15:46:12 +0200

    contrib/ghex: new package

---
+++ contrib/ghex/template.py
@@ -0,0 +1,37 @@
+pkgname = "ghex"
+pkgver = "46.0"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "appstream-glib",
+    "desktop-file-utils",
+    "gettext",
+    "glib-devel",
+    "gobject-introspection",
+    "itstool",
+    "meson",
+    "pkgconf",
+    "vala",
+]
+makedepends = [
+    "gtk4-devel",
+    "libadwaita-devel",
+    "linux-headers",
+]
+pkgdesc = "Hex editor for GNOME"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "GPL-2.0-or-later"
+url = "https://gitlab.gnome.org/GNOME/ghex"
+source = f"$(GNOME_SITE)/ghex/{pkgver[:-2]}/ghex-{pkgver}.tar.xz"
+sha256 = "a1c46f3020cb358b8323025db3a539c97d994a4c46f701f48edc6357f7fbcbd1"
+options = ["!cross"]
+
+
+@subpackage("ghex-libs")
+def _libs(self):
+    return self.default_libs()
+
+
+@subpackage("ghex-devel")
+def _devel(self):
+    return self.default_devel()
