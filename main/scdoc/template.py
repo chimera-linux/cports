10095863326b2bce4751ef384575e11de846a1d0
triallax <triallax@tutanota.com>
2024-08-16 19:44:21 +0100

    main/scdoc: clean up, fix cross

--- main/scdoc/template.py
+++ main/scdoc/template.py
@@ -3,24 +3,23 @@ pkgver = "1.11.3"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
+make_build_args = []
+make_install_args = []
+make_check_args = []
 hostmakedepends = ["pkgconf", "gmake"]
 pkgdesc = "Tool for generating roff manual pages"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://git.sr.ht/~sircmpwn/scdoc"
-source = f"https://git.sr.ht/~sircmpwn/scdoc/archive/{pkgver}.tar.gz"
+source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "4c5c6136540384e5455b250f768e7ca11b03fdba1a8efc2341ee0f1111e57612"
-tool_flags = {"CFLAGS": [f'-DVERSION="{pkgver}"']}
 hardening = ["vis", "cfi"]
 
 if self.profile().cross:
-    hostmakedepends = ["scdoc"]
-
-
-def pre_build(self):
-    if not self.profile().cross:
-        return
-    self.ln_s("/usr/bin/scdoc", self.cwd / "scdoc")
+    hostmakedepends += ["scdoc"]
+    make_build_args += ["HOST_SCDOC=/usr/bin/scdoc"]
+    make_check_args += ["HOST_SCDOC=/usr/bin/scdoc"]
+    make_install_args += ["HOST_SCDOC=/usr/bin/scdoc"]
 
 
 def post_install(self):
