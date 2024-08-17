94826d1b7e41d5c18520cba44eec86518dffc3b2
Erica Z <zerica@callcc.eu>
2024-08-17 11:37:47 +0200

    contrib/smpeg0: new package

---
+++ contrib/smpeg0/template.py
@@ -0,0 +1,26 @@
+pkgname = "smpeg0"
+pkgver = "0.4.5"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_gen = ["./autogen.sh"]
+hostmakedepends = ["automake", "pkgconf"]
+makedepends = ["sdl12-compat-devel", "glu-devel"]
+pkgdesc = "MPEG decoding library for SDL 1.2"
+maintainer = "Erica Z <zerica@callcc.eu>"
+license = "LGPL-2.1-or-later"
+url = "https://icculus.org/smpeg"
+source = f"https://github.com/icculus/smpeg/archive/refs/tags/release_{pkgver.replace('.', '_')}.tar.gz"
+sha256 = "e2e53bfd2e6401e2c29e5eb3929be0e8698bc9e4c9d731751f67e77b408f1f74"
+tool_flags = {"CFLAGS": ["-Dregister=", "-fPIC"]}
+
+
+@subpackage("smpeg0-devel")
+def _devel(self):
+    return self.default_devel()
+
+
+# so cmd:plaympeg doesn't conflict with smpeg proper
+# below -devel so it doesn't pick up smpeg-config
+@subpackage("smpeg0-progs")
+def _progs(self):
+    return self.default_progs()
---
+++ contrib/smpeg0/update.py
@@ -0,0 +1,2 @@
+# legacy version
+ignore = True
