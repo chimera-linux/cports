920a2d1ab1e08654e1a7ec5d451469d5b9a3ba41
q66 <q66@chimera-linux.org>
2024-08-18 16:58:13 +0200

    main/librtas: update to 2.0.6

--- main/librtas/template.py
+++ main/librtas/template.py
@@ -1,5 +1,5 @@
 pkgname = "librtas"
-pkgver = "2.0.5"
+pkgver = "2.0.6"
 pkgrel = 0
 archs = ["ppc*"]
 build_style = "gnu_configure"
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/ibm-power-utilities/librtas"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "b4928b300350c12618563a051be01189275853fd8eacaac509c9aa1460991ec4"
+sha256 = "b88ca9ac5acafb924cd0aaf56c89a7f149c84ade0fc6840f3ef8356ab96a1254"
 
 
 @subpackage("librtas-devel")
