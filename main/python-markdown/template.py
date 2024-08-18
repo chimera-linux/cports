e47f37b2635dd7e46cb4355739bdd4517e39c5af
triallax <triallax@tutanota.com>
2024-08-18 13:03:46 +0100

    main/python-markdown: update to 3.7

--- main/python-markdown/template.py
+++ main/python-markdown/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-markdown"
-pkgver = "3.6"
+pkgver = "3.7"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,8 +14,8 @@ pkgdesc = "Python implementation of Markdown"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/Python-Markdown/markdown"
-source = f"$(PYPI_SITE)/M/Markdown/Markdown-{pkgver}.tar.gz"
-sha256 = "ed4f41f6daecbeeb96e576ce414c41d2d876daa9a16cb35fa8ed8c2ddfad0224"
+source = f"$(PYPI_SITE)/M/Markdown/markdown-{pkgver}.tar.gz"
+sha256 = "2ae2471477cfd02dbbf038d5d9bc226d40def84b4fe2986e49b59b6b472bbed2"
 
 
 def do_check(self):
