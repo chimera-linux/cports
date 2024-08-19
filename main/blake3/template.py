89431a95cc0a2e1bcd48712bae2554f2b7ea0fd8
psykose <alice@ayaya.dev>
2024-08-19 23:22:53 +0200

    main/blake3: update to 1.5.4

--- main/blake3/template.py
+++ main/blake3/template.py
@@ -1,5 +1,5 @@
 pkgname = "blake3"
-pkgver = "1.5.3"
+pkgver = "1.5.4"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
@@ -14,7 +14,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "CC0-1.0 OR Apache-2.0"
 url = "https://github.com/BLAKE3-team/BLAKE3"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "ec9114480857334858e73b727199c573bfdbed6138a83be573f076d37e671fc1"
+sha256 = "ddd24f26a31d23373e63d9be2e723263ac46c8b6d49902ab08024b573fd2a416"
 hardening = ["vis", "cfi"]
 
 
