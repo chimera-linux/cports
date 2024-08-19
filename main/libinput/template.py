55735f9b5e59cfaf401519605d0e06adb7e1d88f
psykose <alice@ayaya.dev>
2024-08-19 13:41:44 +0200

    main/libinput: update to 1.26.2

--- main/libinput/template.py
+++ main/libinput/template.py
@@ -1,5 +1,5 @@
 pkgname = "libinput"
-pkgver = "1.26.1"
+pkgver = "1.26.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -20,7 +20,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.freedesktop.org/wiki/Software/libinput"
 source = f"https://gitlab.freedesktop.org/libinput/libinput/-/archive/{pkgver}/libinput-{pkgver}.tar.gz"
-sha256 = "84fdd16ba0bd3a9adf6c1ffe4292b7a644b0d70f57f81f8239fd499a801189fb"
+sha256 = "5c1c4150f217fea1db2d1fd88e2607b2f1928cfde65c34da65a9f24dcfd69464"
 
 
 def post_install(self):
