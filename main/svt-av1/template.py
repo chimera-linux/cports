6f6f783090855512dddbe79c2a82911310348c53
psykose <alice@ayaya.dev>
2024-08-19 23:33:17 +0200

    main/svt-av1: update to 2.2.0

--- main/svt-av1/template.py
+++ main/svt-av1/template.py
@@ -1,8 +1,9 @@
 pkgname = "svt-av1"
-pkgver = "2.1.2"
+pkgver = "2.2.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
+    "-DCMAKE_BUILD_STYLE=Release",
     "-DBUILD_SHARED_LIBS=ON",
     "-DBUILD_TESTING=OFF",
     "-DENABLE_AVX512=ON",
@@ -18,19 +19,12 @@ pkgdesc = "AOMedia Scalable Video Technology AV1 Encoder/Decoder"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-3-Clause-Clear"
 url = "https://gitlab.com/AOMediaCodec/SVT-AV1"
-source = f"{url}/-/archive/v{pkgver}/SVT-AV1-v{pkgver}.tar.bz2"
-sha256 = "a1d95875f7539d49f7c8fdec0623fbf984804a168da6289705d53268e3b38412"
+source = f"{url}/-/archive/v{pkgver}/SVT-AV1-v{pkgver}.tar.gz"
+sha256 = "d5b3094b2583eb9c15705efa92a8b413f01d718ca0adce6826ae1f0f1c69b4fd"
 # FIXME int: muloverflow in svt_av1_find_best_sub_pixel_tree for certain encodes
 hardening = ["vis", "cfi", "!int"]
 # needs patching+clones of a bunch of stuff
 options = ["!check"]
-
-tool_flags = {
-    # DNDEBUG to disarm NDEBUG checks (as Release always sets)
-    # O3 for encoder
-    "CFLAGS": ["-DNDEBUG", "-O3"],
-    "CXXFLAGS": ["-DNDEBUG", "-O3"],
-}
 
 
 def post_install(self):
