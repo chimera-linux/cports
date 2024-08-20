2f86c296f4c6483e407a367b49350ff61c4f1ef4
psykose <alice@ayaya.dev>
2024-08-20 14:16:38 +0200

    main/libbytesize: update to 2.11

--- main/libbytesize/template.py
+++ main/libbytesize/template.py
@@ -1,6 +1,6 @@
 pkgname = "libbytesize"
-pkgver = "2.10"
-pkgrel = 2
+pkgver = "2.11"
+pkgrel = 0
 build_style = "gnu_configure"
 make_cmd = "gmake"
 hostmakedepends = [
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/storaged-project/libbytesize"
 source = f"{url}/releases/download/{pkgver}/libbytesize-{pkgver}.tar.gz"
-sha256 = "1d1ce3be8ac59fd59511d0794c7327d5cf33f1e83496837b17e19ac49400cad1"
+sha256 = "1f6ce157750ed1201ce83edd3c23d997a615c7da30f9a0e5bb44635f66768d81"
 
 
 @subpackage("libbytesize-devel")
