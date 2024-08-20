53509de862a41faa7a17bc6bcdfa4b619a821169
cassiofb-dev <cassiofb.souza@gmail.com>
2024-08-20 13:38:09 -0300

    contrib/qdiskinfo: new package

---
+++ contrib/qdiskinfo/template.py
@@ -0,0 +1,19 @@
+pkgname = "qdiskinfo"
+pkgver = "0.3"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+]
+makedepends = ["qt6-qtbase-devel"]
+depends = [
+    "qt6-qtsvg",
+    "smartmontools",
+]
+pkgdesc = "Frontend for smartctl do display SMART data"
+maintainer = "cassiofb-dev <contact@cassiofernando.com>"
+license = "GPL-3.0-only"
+url = "https://github.com/edisionnano/QDiskInfo"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "f9829a488ff08395e14f953d41a85dac9c91714fdd34bc9a76a46fe761511209"
