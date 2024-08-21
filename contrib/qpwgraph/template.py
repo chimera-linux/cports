9d95bee3cd641fa5e5fd5dcd66a76214ce0c4391
psykose <alice@ayaya.dev>
2024-08-21 20:42:24 +0200

    contrib/qpwgraph: update to 0.7.7

--- contrib/qpwgraph/template.py
+++ contrib/qpwgraph/template.py
@@ -1,5 +1,5 @@
 pkgname = "qpwgraph"
-pkgver = "0.7.6"
+pkgver = "0.7.7"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -18,5 +18,5 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.freedesktop.org/rncbc/qpwgraph"
 source = f"{url}/-/archive/v{pkgver}/qpwgraph-v{pkgver}.tar.gz"
-sha256 = "c8e9289f44e7ed998f6c4b288e62da92718bad2e03d76b2b4f772a3a76e3a90b"
+sha256 = "3e89242b9ccbde5d575557e4bf98506037548ee6ebc175a793228a03793d7cc8"
 hardening = ["vis", "!cfi"]
