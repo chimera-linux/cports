f63dc7f078d0bc588ed35499b13fcd020b64ba05
q66 <q66@chimera-linux.org>
2024-08-21 22:11:58 +0200

    main/dns-root-data: new package (2024041802)
    Fixes https://github.com/chimera-linux/cports/issues/2750

---
+++ main/dns-root-data/template.py
@@ -0,0 +1,16 @@
+pkgname = "dns-root-data"
+pkgver = "2024041802"
+pkgrel = 0
+pkgdesc = "DNS root hints and DNSSEC trust anchor"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:none"
+url = "https://data.iana.org/root-anchors"
+# we could use a source package to generate the files the same as debian
+# does, but then we create a depcycle as that needs programs that come
+# with unbound as well as some others
+source = f"$(DEBIAN_SITE)/main/d/dns-root-data/dns-root-data_{pkgver}_all.deb"
+sha256 = "1c60b5794e508681d7d4f61c16eebb959d90f0a6619367947059154be6e1a135"
+
+
+def do_install(self):
+    self.install_files("usr/share/dns", "usr/share")
---
+++ main/dns-root-data/update.py
@@ -0,0 +1,2 @@
+url = "http://ftp.debian.org/debian/pool/main/d/dns-root-data"
+ignore = ["*~*"]
