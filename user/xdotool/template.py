f0a2f3f94a17a446eef4493211e58b1be701ec97
q66 <q66@chimera-linux.org>
2024-08-22 16:18:38 +0200

    user/xdotool: what

--- user/xdotool/template.py
+++ user/xdotool/template.py
@@ -3,7 +3,7 @@ pkgver = "3.20211022.1"
 pkgrel = 0
 build_style = "makefile"
 make_cmd = "gmake"
-make_install_args = ["INSTALLMAN=$(DESTDIR)/usr/share/man"]
+make_install_args = ["INSTALLMAN=/usr/share/man"]
 make_check_target = "test"
 make_check_args = ["-j1"]
 hostmakedepends = ["gmake", "perl", "pkgconf"]
@@ -28,6 +28,10 @@ sha256 = "96f0facfde6d78eacad35b91b0f46fecd0b35e474c03e00e30da3fdd345f9ada"
 # uses a ruby module (minitest) that used to be bundled and is no more
 options = ["!check"]
 exec_wrappers = [("/usr/bin/ugetopt", "getopt")]
+
+
+def post_install(self):
+    self.install_license("COPYRIGHT")
 
 
 @subpackage("xdotool-devel")
