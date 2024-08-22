5598a3a06dc2de2a47b18d8b87fb8d6817284bf4
q66 <q66@chimera-linux.org>
2024-08-23 00:21:20 +0200

    main/gmake: provides make

--- main/gmake/template.py
+++ main/gmake/template.py
@@ -1,9 +1,11 @@
 pkgname = "gmake"
 pkgver = "4.4.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
-configure_args = ["--without-guile", "--program-prefix=g"]
+configure_args = ["--without-guile"]
+configure_gen = []
 checkdepends = ["perl"]
+replaces = ["bmake<=20240808-r0"]
 pkgdesc = "GNU Make build tool"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
@@ -13,5 +15,3 @@ sha256 = "8814ba072182b605d156d7589c19a43b89fc58ea479b9355146160946f8cf6e9"
 hardening = ["!cfi"]  # needs figuring out hidden visibility first
 # perl needs gmake to build, so tests introduce a cycle
 options = ["!check"]
-
-configure_gen = []
