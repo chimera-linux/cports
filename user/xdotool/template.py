40aafe5d555bd442d61a940c4cc4df7436b03bc6
q66 <q66@chimera-linux.org>
2024-08-22 16:02:06 +0200

    user/xdotool: new package

---
+++ user/xdotool/template.py
@@ -0,0 +1,35 @@
+pkgname = "xdotool"
+pkgver = "3.20211022.1"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_install_args = [f"INSTALLMAN=$(DESTDIR)/usr/share/man"]
+make_check_target = "test"
+make_check_args = ["-j1"]
+hostmakedepends = ["gmake", "perl", "pkgconf"]
+makedepends = ["libxinerama-devel", "libxkbcommon-devel", "libxtst-devel"]
+checkdepends = [
+    "bash",
+    "gnome-session",
+    "lsof",
+    "procps",
+    "ruby",
+    "setxkbmap",
+    "ugetopt",
+    "xprop",
+    "xserver-xorg-xvfb",
+]
+pkgdesc = "Command-line X11 automation tool"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-3-Clause"
+url = "https://github.com/jordansissel/xdotool"
+source = f"{url}/releases/download/v{pkgver}/{pkgname}-{pkgver}.tar.gz"
+sha256 = "96f0facfde6d78eacad35b91b0f46fecd0b35e474c03e00e30da3fdd345f9ada"
+# uses a ruby module (minitest) that used to be bundled and is no more
+options = ["!check"]
+exec_wrappers = [("/usr/bin/ugetopt", "getopt")]
+
+
+@subpackage("xdotool-devel")
+def _(self):
+    return self.default_devel()
