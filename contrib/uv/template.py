1b17d7ada8ee888e353420fe501e5f1e4a3739ef
psykose <alice@ayaya.dev>
2024-08-22 16:13:18 +0200

    contrib/uv: update to 0.3.1

--- contrib/uv/template.py
+++ contrib/uv/template.py
@@ -1,5 +1,5 @@
 pkgname = "uv"
-pkgver = "0.3.0"
+pkgver = "0.3.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -19,7 +19,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/astral-sh/uv"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "d585904958c0fb12bdb65a975e27912f6fccbea0030ea9a196c74bee6b1227d2"
+sha256 = "6436b8ba15e74be0ca27ebf9a75b1d6b0ea60b9405bc2575c1bca40795c1abeb"
 # too many of them need net
 options = ["!check"]
 
@@ -32,27 +32,6 @@ def post_patch(self):
     from cbuild.util import cargo
 
     cargo.Cargo(self).vendor()
-
-    # TODO: our cargo vendor does not take into account custom source entries
-    # (this is spat out by `cargo vendor`)
-
-    with open(f"{self.cwd}/.cargo/config.toml", "a") as f:
-        f.write(
-            """
-[source."git+https://github.com/astral-sh/pubgrub?rev=aaef464c1b0d8eea4ff9ffaee4f3458c236d10da"]
-git = "https://github.com/astral-sh/pubgrub"
-rev = "aaef464c1b0d8eea4ff9ffaee4f3458c236d10da"
-replace-with = "vendored-sources"
-[source."git+https://github.com/astral-sh/reqwest-middleware?rev=5e3eaf254b5bd481c75d2710eed055f95b756913"]
-git = "https://github.com/astral-sh/reqwest-middleware"
-rev = "5e3eaf254b5bd481c75d2710eed055f95b756913"
-replace-with = "vendored-sources"
-[source."git+https://github.com/charliermarsh/rs-async-zip?rev=011b24604fa7bc223daaad7712c0694bac8f0a87"]
-git = "https://github.com/charliermarsh/rs-async-zip"
-rev = "011b24604fa7bc223daaad7712c0694bac8f0a87"
-replace-with = "vendored-sources"
-"""
-        )
 
 
 def do_check(self):
