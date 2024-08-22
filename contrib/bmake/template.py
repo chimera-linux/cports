439eb924655a46b9a66e013715de13cac1f4a73e
q66 <q66@chimera-linux.org>
2024-08-23 00:52:45 +0200

    contrib/bmake: move from main

--- contrib/bmake/template.py
+++ contrib/bmake/template.py
@@ -1,6 +1,6 @@
 pkgname = "bmake"
 pkgver = "20240808"
-pkgrel = 0
+pkgrel = 1
 pkgdesc = "Portable version of NetBSD make"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
@@ -9,6 +9,10 @@ source = f"https://www.crufty.net/ftp/pub/sjg/bmake-{pkgver}.tar.gz"
 sha256 = "b59189251b483decd4492f1f74387b2a584c03d5aa4637cd48b38ec62b9c0848"
 hardening = ["vis", "cfi"]
 options = ["bootstrap"]
+
+
+if self.profile().cross:
+    hostmakedepends = ["bmake"]
 
 
 def do_build(self):
@@ -28,7 +32,7 @@ def do_build(self):
 def do_install(self):
     eargs = []
     if self.profile().cross:
-        eargs = ["BMAKE=make"]
+        eargs = ["BMAKE=bmake"]
     self.do(
         self.chroot_cwd / "boot-strap",
         "--prefix=/usr",
@@ -39,9 +43,7 @@ def do_install(self):
     )
     self.uninstall("usr/share/man")
     self.install_man("bmake.1")
-    self.install_man("make.1")
     self.install_license("LICENSE")
-    self.install_link("usr/bin/make", "bmake")
 
 
 def do_check(self):
