b03191a6fd290c69047e99b1083f982ffd1025dc
psykose <alice@ayaya.dev>
2024-08-22 16:17:58 +0200

    contrib/parallel: update to 20240822

--- contrib/parallel/template.py
+++ contrib/parallel/template.py
@@ -1,14 +1,12 @@
 pkgname = "parallel"
-pkgver = "20240722"
+pkgver = "20240822"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = [
-    "automake",
-]
+hostmakedepends = ["automake"]
 depends = ["perl"]
 pkgdesc = "Shell tool for executing jobs in parallel"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/parallel"
 source = f"https://ftp.gnu.org/gnu/parallel/parallel-{pkgver}.tar.bz2"
-sha256 = "c7335471f776af28bea9464ad85a50f2ed120f78fbf75ead6647aeea8e0e53f0"
+sha256 = "d7bbd95b7631980b172be04cbd2138d5f7d8c063d6da5ad8f9f70dfd88c8309d"
