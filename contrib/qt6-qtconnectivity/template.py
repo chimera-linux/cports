df8b2e5ebab79af9e47b154bbd7000fc889f7757
psykose <alice@ayaya.dev>
2024-08-22 18:11:10 +0200

    contrib/qt6-qtconnectivity: new package

---
+++ contrib/qt6-qtconnectivity/template.py
@@ -0,0 +1,50 @@
+pkgname = "qt6-qtconnectivity"
+pkgver = "6.7.2"
+pkgrel = 0
+build_style = "cmake"
+# cmake import
+make_check_args = ["-E", "module_includes"]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "bluez-devel",
+    "linux-headers",
+    "qt6-qtbase-devel",
+    "qt6-qtdeclarative-devel",
+]
+pkgdesc = "Qt6 Connectivity component"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = (
+    "LGPL-2.1-only AND LGPL-3.0-only AND GPL-3.0-only WITH Qt-GPL-exception-1.0"
+)
+url = "https://www.qt.io"
+source = f"https://download.qt.io/official_releases/qt/{pkgver[:-2]}/{pkgver}/submodules/qtconnectivity-everywhere-src-{pkgver}.tar.xz"
+sha256 = "8ed321b242f0e956473a295fa31670271f9b3acb797508644cb740f89f6c08e8"
+
+
+def init_check(self):
+    self.make_check_env = {
+        "QT_QPA_PLATFORM": "offscreen",
+        "QML2_IMPORT_PATH": str(
+            self.chroot_cwd / f"{self.make_dir}/lib/qt6/qml"
+        ),
+    }
+
+
+@subpackage("qt6-qtconnectivity-devel")
+def _(self):
+    self.depends += [
+        f"qt6-qtbase-devel~{pkgver[:-2]}",
+        f"qt6-qtdeclarative-devel~{pkgver[:-2]}",
+    ]
+    return self.default_devel(
+        extra=[
+            "usr/lib/qt6/metatypes",
+            "usr/lib/qt6/mkspecs",
+            "usr/lib/qt6/modules",
+            "usr/lib/*.prl",
+        ]
+    )
