d8fc6e9e992a00eca0874189f16e07be2e29b112
psykose <alice@ayaya.dev>
2024-08-24 01:30:22 +0200

    contrib/vulkan-utility-libraries: update to 1.3.294

--- contrib/vulkan-utility-libraries/template.py
+++ contrib/vulkan-utility-libraries/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-utility-libraries"
-pkgver = "1.3.293"
+pkgver = "1.3.294"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_TESTS=ON"]
@@ -18,6 +18,6 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/KhronosGroup/Vulkan-Utility-Libraries"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "c983280edddd728ff57649bc8b6c912120d26019d95b032d7300233d981bbe21"
+sha256 = "9b008530f9ddf9ce05e0fb7adaf8ebb7517a5816c969ace592b3756fdaa8ec97"
 # static-only library, so just keep it as one package
 options = ["!lintstatic"]
