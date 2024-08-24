d026482bc9791e9b776c02f45b7f4bb40ead7ec0
triallax <triallax@tutanota.com>
2024-08-24 19:32:43 +0100

    main/umockdev: run tests

--- main/umockdev/template.py
+++ main/umockdev/template.py
@@ -1,6 +1,6 @@
 pkgname = "umockdev"
 pkgver = "0.18.4"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 hostmakedepends = [
     "gobject-introspection",
@@ -10,14 +10,14 @@ hostmakedepends = [
     "vala",
 ]
 makedepends = ["glib-devel", "libpcap-devel", "linux-headers", "udev-devel"]
+checkdepends = ["libgudev-devel", "udev"]
 pkgdesc = "Mock hardware devices"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/martinpitt/umockdev"
 source = f"https://github.com/martinpitt/umockdev/releases/download/{pkgver}/umockdev-{pkgver}.tar.xz"
 sha256 = "115306f17be78f8c99e20652e2deccdd48df38736bf00eb4f43fefc3809a319c"
-# TODO
-options = ["!check", "!cross"]
+options = ["!cross"]
 
 
 @subpackage("umockdev-devel")
