d3a9caa2ee8abe8639e36fa4ad559b2cadea7f6e
psykose <alice@ayaya.dev>
2024-08-25 21:15:21 +0200

    user/limine: update to 8.0.6

--- user/limine/template.py
+++ user/limine/template.py
@@ -1,5 +1,5 @@
 pkgname = "limine"
-pkgver = "8.0.5"
+pkgver = "8.0.6"
 pkgrel = 0
 # these targets implemented
 archs = ["aarch64", "x86_64", "riscv64"]
@@ -11,7 +11,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-2-Clause AND 0BSD"
 url = "https://limine-bootloader.org"
 source = f"https://github.com/limine-bootloader/limine/releases/download/v{pkgver}/limine-{pkgver}.tar.zst"
-sha256 = "b9906c4f32793220f9acfb66f9f601540ba9edd131a9753f33d2dd6fb96ad2ee"
+sha256 = "938fe8aa4ca47c05e9fe49d64134c97f43a787e3ff400456c22cec05843e3e1a"
 # no test suite
 options = ["!check"]
 
