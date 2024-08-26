b6104f9e90a6af3ce580eea98f05ffeeae32871d
Denis Strizhkin <strdenis02@gmail.com>
2024-08-26 22:29:07 +0300

    user/v2ray: new package

---
+++ user/v2ray/template.py
@@ -0,0 +1,27 @@
+pkgname = "v2ray"
+pkgver = "5.17.1"
+pkgrel = 0
+build_style = "go"
+make_build_args = ["./main"]
+hostmakedepends = ["go"]
+pkgdesc = "Platform for building proxies to bypass network restrictions"
+maintainer = "Denis Strizhkin <strdenis02@gmail.com>"
+license = "MIT"
+url = "https://v2fly.org"
+source = (
+    f"https://github.com/v2fly/v2ray-core/archive/refs/tags/v{pkgver}.tar.gz"
+)
+sha256 = "e6798d1a29f6a52a3c0cc7176803b73e292427bc7858d534d0529a278936b8b0"
+# check: needs network access
+options = ["!check"]
+
+
+def do_install(self):
+    self.install_bin("build/main", name="v2ray")
+    self.install_license("LICENSE")
+    self.install_service(self.files_path / "v2ray")
+    self.install_file(
+        "release/config/config.json",
+        dest="usr/share/examples/v2ray",
+        name="config.json",
+    )
