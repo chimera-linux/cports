7d9b0214f1f58121fac022f693401e344aad5780
triallax <triallax@tutanota.com>
2024-08-26 17:41:22 +0100

    main/snapshot: move from contrib, correct deps

--- main/snapshot/template.py
+++ main/snapshot/template.py
@@ -1,21 +1,23 @@
 pkgname = "snapshot"
 pkgver = "46.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 hostmakedepends = [
     "appstream",
     "cargo",
     "desktop-file-utils",
     "gettext",
+    "glib-devel",
     "gtk-update-icon-cache",
     "meson",
     "pkgconf",
 ]
 makedepends = [
-    "gstreamer-devel",
     "gst-plugins-bad-devel",
     "gst-plugins-base-devel",
+    "gstreamer-devel",
     "libadwaita-devel",
+    "rust-std",
 ]
 depends = [
     "gstreamer-libcamera",
@@ -25,7 +27,9 @@ pkgdesc = "GNOME camera app"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-3.0-or-later"
 url = "https://apps.gnome.org/Snapshot"
-source = f"$(GNOME_SITE)/snapshot/{pkgver[:-2]}/snapshot-{pkgver}.tar.xz"
+source = (
+    f"$(GNOME_SITE)/snapshot/{pkgver.split('.')[0]}/snapshot-{pkgver}.tar.xz"
+)
 sha256 = "45957a0415f454d63531491aa04795cee0ced4edddd5d8fa0d000f5ef0735b67"
 
 
