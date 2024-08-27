6baa0bac8e0a751e4b9575144a013f480cdb8082
psykose <alice@ayaya.dev>
2024-08-27 17:51:52 +0200

    contrib/python-pefile: update to 2024.8.26

--- contrib/python-pefile/template.py
+++ contrib/python-pefile/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-pefile"
-pkgver = "2023.2.7"
-pkgrel = 1
+pkgver = "2024.8.26"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python"]
 checkdepends = ["python-pytest"]
@@ -15,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/erocarrera/pefile"
 source = f"$(PYPI_SITE)/p/pefilE/pefile-{pkgver}.tar.gz"
-sha256 = "82e6114004b3d6911c77c3953e3838654b04511b8b66e8583db70c65998017dc"
+sha256 = "3ff6c5d8b43e8c37bb6e6dd5085658d658a7a0bdcd20b6a07b1fcfc1c4e9d632"
 # tests not included
 options = ["!check"]
 
