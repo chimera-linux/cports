7136a8d2c859bab6994e3d191cabe7ab6a5b5cdc
Cormac Stephenson <c7s@kasku.net>
2024-08-27 13:55:22 +0100

    contrib/inotify-tools: new package

---
+++ contrib/inotify-tools/template.py
@@ -0,0 +1,20 @@
+pkgname = "inotify-tools"
+pkgver = "4.23.9.0"
+pkgrel = 0
+build_style = "gnu_configure"
+makedepends = ["automake", "slibtool"]
+pkgdesc = "Command-line interface to inotify"
+maintainer = "c7s <c7s@kasku.net>"
+license = "GPL-2.0-only"
+url = "https://github.com/inotify-tools/inotify-tools"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "1dfa33f80b6797ce2f6c01f454fd486d30be4dca1b0c5c2ea9ba3c30a5c39855"
+
+
+def post_install(self):
+    self.install_license("COPYING")
+
+
+@subpackage("inotify-tools-devel")
+def _(self):
+    return self.default_devel()
