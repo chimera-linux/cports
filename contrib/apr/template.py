0bbd5af6ac06543040204313b1194470f77353eb
psykose <alice@ayaya.dev>
2024-08-28 19:18:57 +0200

    contrib/apr: update to 1.7.5

--- contrib/apr/template.py
+++ contrib/apr/template.py
@@ -1,17 +1,17 @@
 pkgname = "apr"
-pkgver = "1.7.4"
-pkgrel = 1
+pkgver = "1.7.5"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-installbuilddir=/usr/lib/apr/build-1"]
 make_check_args = ["-j1"]
-hostmakedepends = ["automake", "libtool", "pkgconf"]
+hostmakedepends = ["automake", "slibtool", "pkgconf"]
 makedepends = ["libexpat-devel", "libuuid-devel", "linux-headers"]
 pkgdesc = "Apache Portable Runtime"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://apr.apache.org"
 source = f"https://archive.apache.org/dist/apr/apr-{pkgver}.tar.gz"
-sha256 = "a4137dd82a185076fa50ba54232d920a17c6469c30b0876569e1c2a05ff311d9"
+sha256 = "3375fa365d67bcf945e52b52cba07abea57ef530f40b281ffbe977a9251361db"
 # not even once
 options = ["!cross"]
 
