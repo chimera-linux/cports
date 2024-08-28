dbaf2c8f21a4ffa0244c460aaa397290d7946d4c
psykose <alice@ayaya.dev>
2024-08-28 04:46:02 +0200

    contrib/libpipeline: update to 1.5.8

--- contrib/libpipeline/template.py
+++ contrib/libpipeline/template.py
@@ -1,20 +1,17 @@
 pkgname = "libpipeline"
-pkgver = "1.5.7"
+pkgver = "1.5.8"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf", "slibtool"]
 makedepends = ["check-devel"]
 pkgdesc = "Pipeline manipulation library"
 maintainer = "eater <=@eater.me>"
 license = "GPL-3.0-or-later"
 url = "https://libpipeline.gitlab.io/libpipeline"
 source = f"https://download.savannah.nongnu.org/releases/libpipeline/libpipeline-{pkgver}.tar.gz"
-sha256 = "b8b45194989022a79ec1317f64a2a75b1551b2a55bea06f67704cb2a2e4690b0"
+sha256 = "1b1203ca152ccd63983c3f2112f7fe6fa5afd453218ede5153d1b31e11bb8405"
 
 
 @subpackage("libpipeline-devel")
 def _(self):
     return self.default_devel()
-
-
-configure_gen = []
