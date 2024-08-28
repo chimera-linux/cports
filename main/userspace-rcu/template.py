c9d02e85139c2f8c37935aad75ea306f364ab7ba
psykose <alice@ayaya.dev>
2024-08-28 23:10:42 +0200

    main/userspace-rcu: update to 0.14.1

--- main/userspace-rcu/template.py
+++ main/userspace-rcu/template.py
@@ -1,15 +1,15 @@
 pkgname = "userspace-rcu"
-pkgver = "0.14.0"
+pkgver = "0.14.1"
 pkgrel = 0
 build_style = "gnu_configure"
 make_check_args = ["-j1"]
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf", "slibtool"]
 pkgdesc = "Userspace RCU library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://liburcu.org"
 source = f"https://www.lttng.org/files/urcu/userspace-rcu-{pkgver}.tar.bz2"
-sha256 = "ca43bf261d4d392cff20dfae440836603bf009fce24fdc9b2697d837a2239d4f"
+sha256 = "231acb13dc6ec023e836a0f0666f6aab47dc621ecb1d2cd9d9c22f922678abc0"
 tool_flags = {"CFLAGS": ["-DLITTLE_ENDIAN=4321", "-DBIG_ENDIAN=1234"]}
 # XXX: tests pass when run outside the suite...
 options = ["!check"]
@@ -23,6 +23,3 @@ else:
 @subpackage("userspace-rcu-devel")
 def _(self):
     return self.default_devel(extra=["usr/share/doc"])
-
-
-configure_gen = []
