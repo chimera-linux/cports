79b9e796a64cb4ff46d38f31f1a74fdb33587088
Erica Z <zerica@callcc.eu>
2024-08-29 16:21:20 +0200

    contrib/qtractor: update to 1.2.0

--- contrib/qtractor/template.py
+++ contrib/qtractor/template.py
@@ -1,5 +1,5 @@
 pkgname = "qtractor"
-pkgver = "1.1.1"
+pkgver = "1.2.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DCONFIG_WAYLAND=1"]
@@ -23,4 +23,4 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-2.0-or-later"
 url = "https://www.qtractor.org"
 source = f"$(SOURCEFORGE_SITE)/qtractor/qtractor-{pkgver}.tar.gz"
-sha256 = "43fe804bd999c2c1bec05fe1d31b7beecf2aa6e2f070ed428e855a5fdb5af507"
+sha256 = "e292a2e0b7359cf5ad32d7ff58d28f4aaa8e9631c2ec65b94550116c862dd1ee"
