558feadd7f7255f8fe7dfaee8508660707423516
q66 <q66@chimera-linux.org>
2024-08-29 23:30:59 +0200

    cbuild: fix recompression of manpages pointing outside mandir
    This hits some specific rare cases like postgresql. It is also
    somewhat tricky to handle as these links in mandir may not point
    to a manpage in the same package (in postgres it does not, the
    alternatives package points to main package's dir and the symlink
    resolve as a dep). So we need to iterate every possible subpkg,
    testing the paths as we go. Fortunately, this is narrowed down
    to a slow path that is invoked only when needed.
    
    Fixes https://github.com/chimera-linux/cports/issues/2807

--- src/cbuild/hooks/destdir/199_recompress_manpages.py
+++ src/cbuild/hooks/destdir/199_recompress_manpages.py
@@ -10,8 +10,10 @@ def invoke(pkg):
     # all symlinks to files remain valid while we work on them, do them
     # first to safely retarget, then process files
     syms = []
-    files = []
-    for f in (pkg.destdir / "usr/share/man").rglob("*.*"):
+    # make this a set to dedup
+    files = set()
+    manbase = pkg.destdir / "usr/share/man"
+    for f in manbase.rglob("*.*"):
         # skip non-files and symlinks to non-files
         # dead links are okay, the .exists() handles it
         if f.exists() and not f.is_file():
@@ -20,24 +22,40 @@ def invoke(pkg):
         if f.is_symlink():
             syms.append(f)
         else:
-            files.append(f)
+            files.add(str(pkg.chroot_destdir / f.relative_to(pkg.destdir)))
     # now process links
     for f in syms:
         linktgt = f.readlink()
+        # if it points outside mandir, also add it for processing
+        linkfull = (f.parent / linktgt).resolve()
+        if not linkfull.is_relative_to(manbase):
+            # now we need to go over each possible subpackage and check...
+            # this is very slow but we only do it after filtering things
+            # so it will almost never actually happen
+            for sp in pkg.rparent.subpkg_list + [pkg.rparent]:
+                spf = (
+                    sp.destdir
+                    / "usr/share/man"
+                    / f.parent.relative_to(manbase)
+                    / linktgt
+                ).resolve()
+                if spf.is_file():
+                    files.add(
+                        str(sp.chroot_destdir / spf.relative_to(sp.destdir))
+                    )
+        # always remove link afterwards
         f.unlink()
         f.with_suffix(f"{f.suffix}.gz").symlink_to(f"{linktgt}.gz")
     # and then files
     for f in files:
-        rp = f.relative_to(pkg.destdir)
         # keep to avoid tripping the hardlink detector
         chroot.enter(
             "gzip",
-            "-9nfk",
-            pkg.chroot_destdir / rp,
+            "-9nf",
+            f,
             check=True,
             ro_root=True,
             ro_build=True,
             ro_dest=False,
             unshare_all=True,
         )
-        (pkg.destdir / rp).unlink()
