a117ad79cea78567fd60d2eb4581120963849953
triallax <triallax@tutanota.com>
2024-08-30 04:12:39 +0100

    main/gtk4: ignore 4.14.6

--- main/gtk4/update.py
+++ main/gtk4/update.py
@@ -1,3 +1,4 @@
 url = "https://gitlab.gnome.org/GNOME/gtk/-/tags"
 pkgname = "gtk"
-ignore = ["4.*[13579].*"]
+# 4.14.6 is a typo: https://gitlab.gnome.org/GNOME/gtk/-/issues/6966#note_2209226
+ignore = ["4.*[13579].*", "4.14.6"]
