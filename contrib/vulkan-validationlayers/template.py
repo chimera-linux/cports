ba78714ba485af01dacd3d73ecd614e91feb4c40
psykose <alice@ayaya.dev>
2024-08-31 01:14:04 +0200

    contrib/vulkan-validationlayers: update to 1.3.294

--- contrib/vulkan-validationlayers/template.py
+++ contrib/vulkan-validationlayers/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-validationlayers"
-pkgver = "1.3.290"
+pkgver = "1.3.294"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -26,5 +26,12 @@ pkgdesc = "Validation layers to catch Vulkan issues"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://vulkan.lunarg.com/doc/sdk/latest/linux/khronos_validation_layer.html"
-source = f"https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "37fd611f1c5c5879a68c6f63b09e600a3858f519aa147a2535961fa3d8e889ae"
+source = [
+    f"https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/refs/tags/v{pkgver}.tar.gz",
+    "https://github.com/KhronosGroup/SPIRV-Headers/archive/f013f08e4455bcc1f0eed8e3dd5e2009682656d9.tar.gz",
+]
+source_paths = [".", "spirv-headers"]
+sha256 = [
+    "1fc7d56a04dbf1b77ef498ec9a0222fac66e1b652f1cc1800598f27b9ec28c3e",
+    "452418924e8c281872e7a5ac34fdb977f2be5f880997d559e1e9062df13ae2cd",
+]
