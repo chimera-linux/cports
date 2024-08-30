2213750c3bed81ad5618c9212d4fa1e70808370d
psykose <alice@ayaya.dev>
2024-08-30 22:43:58 +0200

    main/gst-plugins-good: remove libv4l2 use
    not used by default as v4l2 is used directly

--- main/gst-plugins-good/template.py
+++ main/gst-plugins-good/template.py
@@ -1,6 +1,6 @@
 pkgname = "gst-plugins-good"
 pkgver = "1.24.7"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
     "--auto-features=enabled",
@@ -11,23 +11,24 @@ configure_args = [
     # there are too many auto features and it's difficult to take care that
     # nothing is accidentally disabled and so on, so implicitly enable all,
     # and then disable what's not relevant to us:
+    "-Daalib=disabled",  # old and obsolete
     "-Damrnb=disabled",
     "-Damrwbdec=disabled",
     "-Ddirectsound=disabled",
+    "-Ddv1394=disabled",  # maybe?
+    "-Ddv=disabled",  # maybe?
+    "-Doss4=disabled",
+    "-Doss=disabled",
     "-Dosxaudio=disabled",
     "-Dosxvideo=disabled",
-    "-Doss=disabled",
-    "-Doss4=disabled",
-    "-Dwaveform=disabled",
-    "-Drpicamsrc=disabled",  # proprietary
-    "-Dspeex=disabled",  # obsolete, replaced by opus
-    "-Dximagesrc=disabled",  # maybe? probably obsolete
-    "-Daalib=disabled",  # old and obsolete
-    "-Ddv=disabled",  # maybe?
-    "-Ddv1394=disabled",  # maybe?
     "-Dqt5=disabled",  # no qt5
     "-Dqt6=disabled",  # in contrib
+    "-Drpicamsrc=disabled",  # proprietary
     "-Dshout2=disabled",  # libshout needs speex which we don't package
+    "-Dspeex=disabled",  # obsolete, replaced by opus
+    "-Dv4l2-libv4l2=disabled",  # uses v4l2 directly, lib not used by default
+    "-Dwaveform=disabled",
+    "-Dximagesrc=disabled",  # maybe? probably obsolete
 ]
 make_check_args = ["--timeout-multiplier=5"]
 make_check_wrapper = ["wlheadless-run", "--"]
@@ -59,7 +60,6 @@ makedepends = [
     "pipewire-jack-devel",
     "taglib-devel",
     "twolame-devel",
-    "v4l-utils-devel",
     "wavpack-devel",
 ]
 depends = ["libsoup"]  # dynamically loaded
@@ -71,8 +71,6 @@ license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/gst-plugins-good/gst-plugins-good-{pkgver}.tar.xz"
 sha256 = "759acb11e6de8373ff8cbb5e7ab8eb9a38631be81cf24220267b001eb55593c1"
-# sys/v4l2/gstv4l2object.c v4l2object->ioctl = v4l2_ioctl;
-tool_flags = {"CFLAGS": ["-Wno-incompatible-function-pointer-types"]}
 # FIXME int (extra tests fail, look for SIGILL)
 # in 1.24.4, pipelines_effectv only
 hardening = ["!int"]
