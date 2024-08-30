1f3bd18ac7d2ecace9e43857e5cccc34cb888ab7
psykose <alice@ayaya.dev>
2024-08-30 19:47:00 +0200

    main/json-glib: update to 1.10.0

--- main/json-glib/template.py
+++ main/json-glib/template.py
@@ -1,16 +1,22 @@
 pkgname = "json-glib"
-pkgver = "1.8.0"
+pkgver = "1.10.0"
 pkgrel = 0
 build_style = "meson"
-configure_args = ["-Dintrospection=enabled"]
-hostmakedepends = ["meson", "pkgconf", "glib-devel", "gobject-introspection"]
+configure_args = ["-Dintrospection=enabled", "-Dinstalled_tests=false"]
+hostmakedepends = [
+    "gettext",
+    "glib-devel",
+    "gobject-introspection",
+    "meson",
+    "pkgconf",
+]
 makedepends = ["glib-devel"]
 pkgdesc = "JSON parser for glib-based projects"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/action/show/Projects/JsonGlib"
 source = f"$(GNOME_SITE)/json-glib/{pkgver[:-2]}/json-glib-{pkgver}.tar.xz"
-sha256 = "97ef5eb92ca811039ad50a65f06633f1aae64792789307be7170795d8b319454"
+sha256 = "1bca8d66d96106ecc147df3133b95a5bb784f1fa6f15d06dd7c1a8fb4a10af7b"
 
 
 @subpackage("json-glib-devel")
