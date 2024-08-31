b0ac8d65147de7427a1153b8afbe776b92795a39
Tulip Blossom <tulilirockz@outlook.com>
2024-08-30 21:10:46 -0300

    contrib/nushell: update to 0.97.1

--- contrib/nushell/template.py
+++ contrib/nushell/template.py
@@ -1,5 +1,5 @@
 pkgname = "nushell"
-pkgver = "0.96.1"
+pkgver = "0.97.1"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "MIT"
 url = "https://www.nushell.sh"
 source = f"https://github.com/nushell/nushell/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "829e2f91d130d7b0063a08b1fadb737bdff616ac744eba43baa5fc42aa8b682b"
+sha256 = "e605d5a7f104b7f2bf99197ca2f34a4a68f68cc12ecab41f606113e6a65b67b1"
 # Checks fail with libgit2 < 1.8.1
 options = ["!check"]
 
