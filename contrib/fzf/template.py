4de0120c5b2f965ad458b136a7b481acc157aa65
Wesley Moore <wes@wezm.net>
2024-08-31 12:55:35 +1000

    contrib/fzf: update to 0.55.0

--- contrib/fzf/template.py
+++ contrib/fzf/template.py
@@ -1,6 +1,6 @@
 pkgname = "fzf"
-pkgver = "0.54.3"
-pkgrel = 1
+pkgver = "0.55.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 makedepends = ["ncurses-devel"]
@@ -9,7 +9,7 @@ maintainer = "Wesley Moore <wes@wezm.net>"
 license = "MIT"
 url = "https://github.com/junegunn/fzf"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "6413f3916f8058b396820f9078b1336d94c72cbae39c593b1d16b83fcc4fdf74"
+sha256 = "805383f71bca7f8fb271ecd716852aea88fd898d5027d58add9e43df6ea766da"
 
 
 def post_install(self):
