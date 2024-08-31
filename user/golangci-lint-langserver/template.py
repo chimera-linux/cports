fd36c1cbe3905d805251011055f48b38909a966b
Denis Strizhkin <strdenis02@gmail.com>
2024-09-01 01:18:38 +0300

    user/golangci-lint-langserver: new package

---
+++ user/golangci-lint-langserver/template.py
@@ -0,0 +1,16 @@
+pkgname = "golangci-lint-langserver"
+pkgver = "0.0.9"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go"]
+depends = ["golangci-lint"]
+pkgdesc = "Language server for golangci-lint"
+maintainer = "Denis Strizhkin <strdenis02@gmail.com>"
+license = "MIT"
+url = "https://github.com/nametake/golangci-lint-langserver"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "ad7241d271b9b46b6fc784ab1d035c322bd6ae44938514d69f5ad516a1a9fbfd"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
