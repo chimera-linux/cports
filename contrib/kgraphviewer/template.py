05d348dc3ba6aad5c8b47d5a7c940c85fa7b7135
psykose <alice@ayaya.dev>
2024-08-31 03:19:29 +0200

    contrib/kgraphviewer: update to 24.08.0

--- contrib/kgraphviewer/template.py
+++ contrib/kgraphviewer/template.py
@@ -1,6 +1,6 @@
 pkgname = "kgraphviewer"
-pkgver = "2.5.0"
-pkgrel = 1
+pkgver = "24.08.0"
+pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -27,8 +27,10 @@ pkgdesc = "Graphviz DOT file viewer"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-only"
 url = "https://invent.kde.org/graphics/kgraphviewer"
-source = f"$(KDE_SITE)/kgraphviewer/{pkgver}/kgraphviewer-{pkgver}.tar.xz"
-sha256 = "872bee63fb4df6f7fb2b4eaf02ff825cba3ca953ac02509a287fe5cd0f1e2b69"
+source = (
+    f"$(KDE_SITE)/release-service/{pkgver}/src/kgraphviewer-{pkgver}.tar.xz"
+)
+sha256 = "f2efe1591be2794df6eb299ed6ab32629bf91208c097a0e2a5fe89c8ce5c8cd1"
 hardening = ["vis"]
 
 
