511e9944bd21376e2b14e410a892e13da98c54c9
Tulip Blossom <tulilirockz@outlook.com>
2024-08-31 13:59:03 -0300

    user/anyrun: use proper source and hash

--- user/anyrun/template.py
+++ user/anyrun/template.py
@@ -15,8 +15,8 @@ pkgdesc = "Wayland native krunner-like runner"
 maintainer = "tulilirockz <tulilirockz@outlook.com>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/anyrun-org/anyrun"
-source = f"{url}/archive/refs/heads/master.tar.gz"
-sha256 = "bbb80457b32621b2faba708a88f2e0c7339c24d34cdd36026c85c3449ad1f7c6"
+source = f"{url}/archive/c6101a31a80b51e32e96f6a77616b609770172e0.tar.gz"
+sha256 = "1bcbba5301280249966fbcdd9e2d2fdec3586a3bade1c8bd8904e1d77fb1492f"
 # check: no meaningful tests at all
 options = ["!check"]
 
