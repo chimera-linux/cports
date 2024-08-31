24ceb6b4b6392015df1a59a6464030b6fea23427
Denis Strizhkin <strdenis02@gmail.com>
2024-09-01 01:17:05 +0300

    user/golangci-lint: new package

---
+++ user/golangci-lint/template.py
@@ -0,0 +1,14 @@
+pkgname = "golangci-lint"
+pkgver = "1.60.3"
+pkgrel = 0
+build_style = "go"
+make_build_args = ["./cmd/golangci-lint"]
+hostmakedepends = ["go"]
+pkgdesc = "Linters runner for Go"
+maintainer = "Denis Strizhkin <strdenis02@gmail.com>"
+license = "GPL-3.0-or-later"
+url = "https://golangci-lint.run"
+source = f"https://github.com/golangci/golangci-lint/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "038510ae3d094ef1553ee9d4a29a5514b46c83fe68b86279a33d4f284c0a71b2"
+# some tests fail because of chroot and some need network
+options = ["!check"]
