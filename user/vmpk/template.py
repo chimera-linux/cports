428c9214bf8e519318c6ddb5c0ee155586c04a6c
psykose <alice@ayaya.dev>
2024-09-01 04:06:48 +0200

    user/vmpk: new package

---
+++ user/vmpk/template.py
@@ -0,0 +1,19 @@
+pkgname = "vmpk"
+pkgver = "0.9.0"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+]
+makedepends = [
+    "drumstick-devel",
+    "qt6-qtbase-devel",
+    "qt6-qttools-devel",
+]
+pkgdesc = "Virtual MIDI piano keyboard"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-3.0-or-later"
+url = "https://sourceforge.net/projects/vmpk"
+source = f"$(SOURCEFORGE_SITE)/vmpk/vmpk-{pkgver}.tar.gz"
+sha256 = "3cfd900843b1e068fda9a4e075c204086447a2e233d4800d785d2124480ae2f5"
