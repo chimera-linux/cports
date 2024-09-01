4d81aa0dc4a8713491724925c119b41b00c3fc8f
psykose <alice@ayaya.dev>
2024-09-01 10:43:21 +0200

    main/python-lit: new package
    closes https://github.com/chimera-linux/cports/issues/2836

---
+++ main/python-lit/template.py
@@ -0,0 +1,22 @@
+pkgname = "python-lit"
+pkgver = "18.1.8"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+]
+depends = [
+    "cmd:FileCheck!llvm-tools",
+    "cmd:not!llvm-tools",
+]
+pkgdesc = "Software testing tool from LLVM"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "Apache-2.0"
+url = "https://pypi.org/project/lit"
+# the pypi source has the correct version instead of 'dev0'
+source = f"$(PYPI_SITE)/l/lit/lit-{pkgver}.tar.gz"
+sha256 = "47c174a186941ae830f04ded76a3444600be67d5e5fb8282c3783fba671c4edb"
+# would need to run with itself (annoying)
+options = ["!check"]
