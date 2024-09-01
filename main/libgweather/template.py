1b53141347f3070c4fff65ab12604ee90076165d
psykose <alice@ayaya.dev>
2024-09-01 22:48:37 +0200

    main/libgweather: update to 4.4.4

--- main/libgweather/template.py
+++ main/libgweather/template.py
@@ -1,5 +1,5 @@
 pkgname = "libgweather"
-pkgver = "4.4.2"
+pkgver = "4.4.4"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -29,7 +29,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Projects/LibGWeather"
 source = f"$(GNOME_SITE)/libgweather/{pkgver[:-2]}/libgweather-{pkgver}.tar.xz"
-sha256 = "a6e427b4770ada48945f3aa906af710fc833cff0d42df91f1828302740d794ec"
+sha256 = "7017677753cdf7d1fdc355e4bfcdb1eba8369793a8df24d241427a939cbf4283"
 # needs network access
 options = ["!check"]
 
