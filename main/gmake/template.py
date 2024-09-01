e07cee899d228dbc6f9d82066d8181272f02a050
psykose <alice@ayaya.dev>
2024-09-01 10:11:19 +0200

    main/gmake: provide a gmake symlink
    partly since it's gmake, but mostly because my muscle memory got really used to
    typing gmake all the time and it owns the name so might as well

--- main/gmake/template.py
+++ main/gmake/template.py
@@ -1,6 +1,6 @@
 pkgname = "gmake"
 pkgver = "4.4.1"
-pkgrel = 1
+pkgrel = 2
 build_style = "gnu_configure"
 configure_args = ["--without-guile"]
 configure_gen = []
@@ -15,3 +15,7 @@ sha256 = "8814ba072182b605d156d7589c19a43b89fc58ea479b9355146160946f8cf6e9"
 hardening = ["!cfi"]  # needs figuring out hidden visibility first
 # perl needs gmake to build, so tests introduce a cycle
 options = ["!check"]
+
+
+def post_install(self):
+    self.install_link("usr/bin/gmake", "make")
