a40efb39315a3f8f84c77c64573d05fbb1599087
ttyyls <contact@behri.org>
2024-09-02 11:45:35 +0100

    contrib/stress-ng: update to 0.18.03

--- contrib/stress-ng/template.py
+++ contrib/stress-ng/template.py
@@ -1,5 +1,5 @@
 pkgname = "stress-ng"
-pkgver = "0.18.02"
+pkgver = "0.18.03"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["pkgconf"]
@@ -15,7 +15,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/ColinIanKing/stress-ng"
 source = f"https://github.com/ColinIanKing/stress-ng/archive/refs/tags/V{pkgver}.tar.gz"
-sha256 = "45eac8d354df5be26c9675ec7fc24910f846e47eb6b151e9955d6eae30cfe060"
+sha256 = "a19a25ba47c9196db23aab97df4ef55d8ec80d8a5a6afe580b3bec2b870422a9"
 env = {
     "MAN_COMPRESS": "0",
     "PRESERVE_CFLAGS": "1",
