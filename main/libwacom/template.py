bde3d7a54ff054b0c609b6884ce98ec078edadf5
psykose <alice@ayaya.dev>
2024-09-02 10:51:57 +0200

    main/libwacom: update to 2.13.0

--- main/libwacom/template.py
+++ main/libwacom/template.py
@@ -1,6 +1,6 @@
 pkgname = "libwacom"
-pkgver = "2.12.2"
-pkgrel = 1
+pkgver = "2.13.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocumentation=disabled", "-Dtests=enabled"]
 make_check_args = ["--timeout-multiplier", "4"]
@@ -18,7 +18,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/linuxwacom/libwacom"
 source = f"{url}/releases/download/libwacom-{pkgver}/libwacom-{pkgver}.tar.xz"
-sha256 = "c8319c40c70edd05d1839c0d3f449c23bdc90cd1f0d819bd0c1ec7c00b117700"
+sha256 = "acd18121441bbc00fc5c881fca08a33319ab814b798eac8d0be6354923f8fb08"
 
 
 def post_install(self):
