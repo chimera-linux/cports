520bab9eaede78d2b941e79966687ee3e381f240
psykose <alice@ayaya.dev>
2024-09-04 12:39:35 +0200

    main/weston: update to 14.0.0

--- main/weston/template.py
+++ main/weston/template.py
@@ -1,5 +1,5 @@
 pkgname = "weston"
-pkgver = "13.0.3"
+pkgver = "14.0.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -16,36 +16,37 @@ configure_args = [
 make_check_env = {"XDG_RUNTIME_DIR": "/tmp"}
 hostmakedepends = ["meson", "pkgconf", "wayland-progs", "wayland-protocols"]
 makedepends = [
-    "libpng-devel",
-    "libwebp-devel",
-    "lcms2-devel",
-    "pixman-devel",
-    "mesa-devel",
-    "pango-devel",
     "cairo-devel",
-    "mtdev-devel",
+    "dbus-devel",
     "glu-devel",
+    "lcms2-devel",
+    "libdisplay-info-devel",
+    "libdrm-devel",
     "libinput-devel",
+    "libpng-devel",
+    "libseat-devel",
+    "libva-devel",
+    "libwebp-devel",
     "libxcb-devel",
     "libxcursor-devel",
     "libxkbcommon-devel",
+    "linux-pam-devel",
+    "mesa-devel",
+    "mtdev-devel",
+    "pango-devel",
+    "pipewire-devel",
+    "pixman-devel",
+    "udev-devel",
     "wayland-devel",
     "wayland-protocols",
-    "libdrm-devel",
-    "linux-pam-devel",
-    "udev-devel",
-    "dbus-devel",
-    "libva-devel",
-    "pipewire-devel",
-    "libseat-devel",
 ]
-checkdepends = ["mesa-dri", "xwayland"]
+checkdepends = ["xwayland"]
 pkgdesc = "Reference implementation of a Wayland compositor"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://wayland.freedesktop.org"
 source = f"https://gitlab.freedesktop.org/wayland/weston/-/releases/{pkgver}/downloads/weston-{pkgver}.tar.xz"
-sha256 = "27f68d96e3b97d98daadef13a202356524924fa381418fa6716b9136ef099093"
+sha256 = "47fd0325b0b948e9b003a38fdf4eb3a8581f3fdc740b8932b35ae8793bf4e4a5"
 
 
 def post_install(self):
