31556b06e78e8f3f780afec6706cc1ab907da03d
psykose <alice@ayaya.dev>
2024-09-04 04:06:50 +0200

    contrib/android-tools: update to 35.0.2

--- contrib/android-tools/template.py
+++ contrib/android-tools/template.py
@@ -1,9 +1,14 @@
 pkgname = "android-tools"
-pkgver = "35.0.1"
-pkgrel = 7
+pkgver = "35.0.2"
+pkgrel = 0
 # only supports specific little-endian archs, particularly in boringssl
 archs = ["x86_64", "aarch64", "ppc64le", "riscv64"]
 build_style = "cmake"
+configure_args = [
+    # until next libusb
+    "-DANDROID_TOOLS_USE_BUNDLED_LIBUSB=ON",
+    "-DANDROID_TOOLS_LIBUSB_ENABLE_UDEV=ON",
+]
 hostmakedepends = [
     "cmake",
     "go",
@@ -14,12 +19,14 @@ hostmakedepends = [
 ]
 makedepends = [
     "brotli-devel",
+    "fmt-devel",
     "gtest-devel",
     "libusb-devel",
     "linux-headers",
     "lz4-devel",
     "pcre2-devel",
     "protobuf-devel",
+    "udev-devel",
     "zstd-devel",
 ]
 depends = ["android-udev-rules", "python"]
@@ -28,7 +35,7 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "Apache-2.0 AND ISC AND GPL-2.0-only AND MIT"
 url = "https://github.com/nmeum/android-tools"
 source = f"{url}/releases/download/{pkgver}/android-tools-{pkgver}.tar.xz"
-sha256 = "654030c7f96d25d7224cd6861fac14a043cf1d3980f40288cdfbe219f94ffaf9"
+sha256 = "d2c3222280315f36d8bfa5c02d7632b47e365bfe2e77e99a3564fb6576f04097"
 tool_flags = {"CXXFLAGS": ["-D_LARGEFILE64_SOURCE"]}
 hardening = ["vis", "cfi"]
 
