4a8d61856f14b90b5489ceb0d5102e958fdb4463
psykose <alice@ayaya.dev>
2024-09-04 02:50:54 +0200

    main/geoclue: update to 2.7.2

--- main/geoclue/template.py
+++ main/geoclue/template.py
@@ -1,6 +1,6 @@
 pkgname = "geoclue"
-pkgver = "2.7.1"
-pkgrel = 1
+pkgver = "2.7.2"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Ddbus-srv-user=_geoclue",
@@ -10,27 +10,27 @@ configure_args = [
     "-Ddemo-agent=false",  # problematic meson.build
 ]
 hostmakedepends = [
-    "meson",
-    "pkgconf",
+    "gettext",
     "glib-devel",
     "gobject-introspection",
+    "meson",
+    "pkgconf",
     "vala",
-    "gettext",
 ]
 makedepends = [
-    "udev-devel",
-    "json-glib-devel",
-    "libsoup-devel",
-    "libnotify-devel",
-    "modemmanager-devel",
     "avahi-glib-devel",
+    "json-glib-devel",
+    "libnotify-devel",
+    "libsoup-devel",
+    "modemmanager-devel",
+    "udev-devel",
 ]
 pkgdesc = "D-Bus geoinformation service"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.freedesktop.org/geoclue/geoclue/wikis/home"
 source = f"https://gitlab.freedesktop.org/geoclue/geoclue/-/archive/{pkgver}/geoclue-{pkgver}.tar.bz2"
-sha256 = "5624cd41148643c46d681d39153c7d26fdb8831e7e7c8601c300732fa8a6db1c"
+sha256 = "01d24a44a036769049411899ae83c9a86f73d272cca3df6af5a03bfac32456d2"
 
 
 def post_install(self):
