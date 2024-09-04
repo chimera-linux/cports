64416247df9ec2b528c09cbe0c1e8380b41b17fb
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-05 02:23:51 +0300

    main/xdg-desktop-portal-gnome: update to 47_rc

--- main/xdg-desktop-portal-gnome/template.py
+++ main/xdg-desktop-portal-gnome/template.py
@@ -1,5 +1,5 @@
 pkgname = "xdg-desktop-portal-gnome"
-pkgver = "46.2"
+pkgver = "47_rc"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dsystemduserunitdir=/tmp/delete_me"]
@@ -15,8 +15,8 @@ pkgdesc = "Backend implementation for xdg-desktop-portal for GNOME"
 maintainer = "eater <=@eater.me>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/xdg-desktop-portal-gnome"
-source = f"https://download.gnome.org/sources/xdg-desktop-portal-gnome/{pkgver.split('.')[0]}/xdg-desktop-portal-gnome-{pkgver}.tar.xz"
-sha256 = "b5c65ea25e8483502d033a613be6dc6b71883ac07f1a3e474ad18049c47d16d6"
+source = f"https://download.gnome.org/sources/xdg-desktop-portal-gnome/{pkgver[:2]}/xdg-desktop-portal-gnome-{pkgver.replace('_', '.')}.tar.xz"
+sha256 = "3f28f1dd76b6520b5c52f0a61066c80255b4d59483a480dd98b72b2d001fd866"
 
 
 def post_install(self):
