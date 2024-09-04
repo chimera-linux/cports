8cece99d44b1261a2d762a8b67b1972574002146
psykose <alice@ayaya.dev>
2024-09-04 04:10:00 +0200

    contrib/python-websockets: update to 13.0.1

--- contrib/python-websockets/template.py
+++ contrib/python-websockets/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-websockets"
-pkgver = "13.0"
+pkgver = "13.0.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 makedepends = ["python-devel"]
 checkdepends = ["python-pytest"]
@@ -17,7 +16,7 @@ license = "BSD-3-Clause"
 url = "https://github.com/python-websockets/websockets"
 # pypi tarball doesn't ship tests
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "b07a0f5272289d6f02045992d5af427a77af4e74167e26e5c20ed799596802ed"
+sha256 = "0cfb2f88ac0b693ed282d408479edc57b948e6a786c0d3e48222e4e8d6ade85d"
 # tests require a network connection :^)
 options = ["!check"]
 
