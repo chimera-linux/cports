839e74f4fd448437429d4e304d2661b6617ba99d
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-05 00:56:28 +0300

    main/gsettings-desktop-schemas: update to 47_rc

--- main/gsettings-desktop-schemas/template.py
+++ main/gsettings-desktop-schemas/template.py
@@ -1,5 +1,5 @@
 pkgname = "gsettings-desktop-schemas"
-pkgver = "46.1"
+pkgver = "47_rc"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=true"]
@@ -21,8 +21,8 @@ pkgdesc = "Collection of GSettings schemas"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas"
-source = f"$(GNOME_SITE)/gsettings-desktop-schemas/{pkgver[:-2]}/gsettings-desktop-schemas-{pkgver}.tar.xz"
-sha256 = "9b88101437a6958ebe6bbd812e49bbf1d09cc667011e415559d847e870468a61"
+source = f"$(GNOME_SITE)/gsettings-desktop-schemas/{pkgver[:2]}/gsettings-desktop-schemas-{pkgver.replace('_', '.')}.tar.xz"
+sha256 = "890d11b57b2464ebbfe6d273a2872659b714fdd1263b10454c6b3ba8099736b8"
 options = ["!cross"]
 
 
