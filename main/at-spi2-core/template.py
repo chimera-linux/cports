03d27ef7474777bed5169ad373bae97df73cadc0
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-05 00:54:05 +0300

    main/at-spi2-core: update to 2.53.90

--- main/at-spi2-core/template.py
+++ main/at-spi2-core/template.py
@@ -1,5 +1,5 @@
 pkgname = "at-spi2-core"
-pkgver = "2.52.0"
+pkgver = "2.53.90"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -22,10 +22,8 @@ pkgdesc = "Assistive Technology Service Provider Interface"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/at-spi2-core"
-source = (
-    f"$(GNOME_SITE)/at-spi2-core/{pkgver[:-2]}/at-spi2-core-{pkgver}.tar.xz"
-)
-sha256 = "0ac3fc8320c8d01fa147c272ba7fa03806389c6b03d3c406d0823e30e35ff5ab"
+source = f"$(GNOME_SITE)/at-spi2-core/{pkgver[:4]}/at-spi2-core-{pkgver}.tar.xz"
+sha256 = "0548e8dfc77b0a190827c767264a0b725311ab3af96588d52cadbab35e05c6aa"
 # non-trivial dbus setup
 options = ["!check", "!cross"]
 
