5876940a36532600e5877338e0981df4c9f65bee
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-05 04:48:20 +0300

    main/gnome-user-docs: update to 47_beta

--- main/gnome-user-docs/template.py
+++ main/gnome-user-docs/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-user-docs"
-pkgver = "46.4"
+pkgver = "47_beta"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -15,6 +15,6 @@ pkgdesc = "User documentation for GNOME"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "CC-BY-3.0"
 url = "https://gitlab.gnome.org/GNOME/gnome-user-docs"
-source = f"$(GNOME_SITE)/gnome-user-docs/{pkgver[:-2]}/gnome-user-docs-{pkgver}.tar.xz"
-sha256 = "2776072f15f92bd05028345f3ac8a093d44260de52a620d62693c0e5eea20534"
+source = f"$(GNOME_SITE)/gnome-user-docs/{pkgver[:2]}/gnome-user-docs-{pkgver.replace('_', '.')}.tar.xz"
+sha256 = "57754f150915d909d019d2bd5af5eadb2532d1d12d05391d71a6c24835dac5a5"
 options = ["!splitdoc"]
