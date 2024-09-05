7e041b6d93d83d1effc37615c6a0776282608cd2
psykose <alice@ayaya.dev>
2024-09-05 18:03:19 +0200

    contrib/python-jeepney: new package

---
+++ contrib/python-jeepney/template.py
@@ -0,0 +1,24 @@
+pkgname = "python-jeepney"
+pkgver = "0.8.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-flit_core",
+    "python-installer",
+]
+depends = ["python"]
+pkgdesc = "Python DBus protocol wrapper"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://gitlab.com/takluyver/jeepney"
+source = f"$(PYPI_SITE)/j/jeepney/jeepney-{pkgver}.tar.gz"
+sha256 = "5efe48d255973902f6badc3ce55e2aa6c5c3b3bc642059ef3a91247bcfcc5806"
+# bunch more deps
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+    self.uninstall("usr/lib/python*/site-packages/jeepney/tests", glob=True)
+    self.uninstall("usr/lib/python*/site-packages/jeepney/io/tests", glob=True)
