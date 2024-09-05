89aea761facf536adafe96fce8447eb43012fecd
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-05 04:50:45 +0300

    main/baobab: update to 47_alpha

--- main/baobab/template.py
+++ main/baobab/template.py
@@ -1,5 +1,5 @@
 pkgname = "baobab"
-pkgver = "46.0"
+pkgver = "47_alpha"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -21,6 +21,6 @@ pkgdesc = "Graphical directory tree analyzer for GNOME"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/action/show/Apps/DiskUsageAnalyzer"
-source = f"$(GNOME_SITE)/baobab/{pkgver[:-2]}/baobab-{pkgver}.tar.xz"
-sha256 = "ce4def5c82d05671a5009f7bebcf85ac98675d9d8160d28ad9181b269a72e37c"
+source = f"$(GNOME_SITE)/baobab/{pkgver[:2]}/baobab-{pkgver.replace('_', '.')}.tar.xz"
+sha256 = "d36715445809ea11a2ad2970cea80b35d9d1a7271f08049654279b474821451c"
 hardening = ["vis", "!cfi"]
