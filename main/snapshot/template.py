a39b47cf42f2fa60bc512eefacae0e9aa49c58fc
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-05 06:17:30 +0300

    [WIP] main/snapshot: update to 47_beta

--- main/snapshot/template.py
+++ main/snapshot/template.py
@@ -1,6 +1,6 @@
 pkgname = "snapshot"
-pkgver = "46.3"
-pkgrel = 2
+pkgver = "47_beta"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "appstream",
@@ -17,20 +17,21 @@ makedepends = [
     "gst-plugins-base-devel",
     "gstreamer-devel",
     "libadwaita-devel",
+    "libseccomp-devel",
     "rust-std",
 ]
 depends = [
     "gstreamer-libcamera",
     "gstreamer-pipewire",
 ]
+# FIXME: drop clippy, required by "Cargo clippy - aperture" test which fails otherwise
+checkdepends = ["rust-clippy"]
 pkgdesc = "GNOME camera app"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-3.0-or-later"
 url = "https://apps.gnome.org/Snapshot"
-source = (
-    f"$(GNOME_SITE)/snapshot/{pkgver.split('.')[0]}/snapshot-{pkgver}.tar.xz"
-)
-sha256 = "45957a0415f454d63531491aa04795cee0ced4edddd5d8fa0d000f5ef0735b67"
+source = f"$(GNOME_SITE)/snapshot/{pkgver[:2]}/snapshot-{pkgver.replace('_', '.')}.tar.xz"
+sha256 = "d06c673457acd61b928b8a4f64a0bb4b9f3b42b4c03ae39e8737768d93d146bc"
 
 
 def init_build(self):
