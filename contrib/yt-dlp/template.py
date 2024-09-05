b367fa95cbbfc7dfe36e2efcd218c00392448588
psykose <alice@ayaya.dev>
2024-09-05 18:10:28 +0200

    contrib/yt-dlp: add a recommend dep set
    closes https://github.com/chimera-linux/cports/pull/2853

--- contrib/yt-dlp/template.py
+++ contrib/yt-dlp/template.py
@@ -1,6 +1,6 @@
 pkgname = "yt-dlp"
 pkgver = "2024.08.06"
-pkgrel = 0
+pkgrel = 1
 build_style = "python_pep517"
 make_check_args = ["-k", "not download"]
 hostmakedepends = [
@@ -23,3 +23,19 @@ license = "Unlicense"
 url = "https://github.com/yt-dlp/yt-dlp"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "bee43ceee58bdccc1bf48a394073371d3ff2b930d8007cc20a4563bdd027a085"
+
+
+@subpackage("yt-dlp-recommends")
+def _(self):
+    self.depends = [
+        "ffmpeg",
+        "mutagen",
+        "python-brotli",
+        "python-pycryptodomex",
+        "python-secretstorage",
+        "python-websockets",
+    ]
+    self.subdesc = "recommended dependencies"
+    self.install_if = [self.parent]
+    self.options = ["empty"]
+    return []
