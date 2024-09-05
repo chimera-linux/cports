653d8cd32a08890d23f5049ac5ca62917885f38a
Tulip Blossom <tulilirockz@outlook.com>
2024-09-04 23:54:29 -0300

    user/gnome-podcasts: new package (0.7.1)

---
+++ user/gnome-podcasts/template.py
@@ -0,0 +1,48 @@
+pkgname = "gnome-podcasts"
+pkgver = "0.7.1"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "cargo-auditable",
+    "desktop-file-utils",
+    "gettext",
+    "meson",
+    "pkgconf",
+]
+makedepends = [
+    "dbus-devel",
+    "gst-plugins-bad-devel",
+    "gstreamer-devel",
+    "libadwaita-devel",
+    "openssl-devel",
+    "rust-std",
+    "sqlite-devel",
+]
+pkgdesc = "GTK-based podcast aggregator"
+maintainer = "tulilirockz <tulilirockz@outlook.com>"
+license = "GPL-3.0-or-later"
+url = "https://gitlab.gnome.org/World/podcasts"
+source = f"{url}/-/archive/{pkgver}/podcasts-{pkgver}.tar.gz"
+sha256 = "933d33dfd0f36343f9c80f055a48e14307a0665b35097da176767ddbfe583399"
+# check: broken tests due to sandboxing
+options = ["!check"]
+
+
+def post_patch(self):
+    from cbuild.util import cargo
+
+    cargo.Cargo(self, wrksrc=".").vendor()
+
+
+def init_build(self):
+    from cbuild.util import cargo
+
+    renv = cargo.get_environment(self)
+    self.make_env.update(renv)
+
+
+def post_install(self):
+    self.install_bin(
+        f"build/podcasts-gtk/src/{self.profile().triplet}/release/podcasts-gtk",
+        name="gnome-podcasts",
+    )
