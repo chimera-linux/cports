6c2677853d32ee2faaca6f779a15cf8aa2e92d94
psykose <alice@ayaya.dev>
2024-09-05 20:16:03 +0200

    main/perl-uri: update to 5.29

--- main/perl-uri/template.py
+++ main/perl-uri/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-uri"
-pkgver = "5.28"
+pkgver = "5.29"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["perl"]
@@ -10,6 +10,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/release/URI"
 source = f"$(CPAN_SITE)/URI/URI-{pkgver}.tar.gz"
-sha256 = "e7985da359b15efd00917fa720292b711c396f2f9f9a7349e4e7dec74aa79765"
+sha256 = "a34b9f626c3ff1e20c0d4a23ec5c8b7ae1de1fb674ecefed7e46791388137372"
 # missing checkdepends
 options = ["!check"]
