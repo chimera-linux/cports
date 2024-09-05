7594084b821f72592fd128cb4eef5bef2d8b6dc5
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-05 06:21:18 +0300

    main/gnome-maps: update to 47_rc

--- main/gnome-maps/template.py
+++ main/gnome-maps/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-maps"
-pkgver = "46.11"
+pkgver = "47_rc"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -44,6 +44,6 @@ pkgdesc = "GNOME maps"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later AND CC0-1.0"
 url = "https://apps.gnome.org/Maps"
-source = f"$(GNOME_SITE)/gnome-maps/{pkgver.split('.')[0]}/gnome-maps-{pkgver}.tar.xz"
-sha256 = "940b41b9740b0813325e3916758716cf8fa0ee4e0c9191f260cec06d92135835"
+source = f"$(GNOME_SITE)/gnome-maps/{pkgver[:2]}/gnome-maps-{pkgver.replace('_', '.')}.tar.xz"
+sha256 = "5a0222b259804c02dba8a978b8efb35f1595ee111fde9e7d527c9da259eb1c26"
 options = ["!cross"]
