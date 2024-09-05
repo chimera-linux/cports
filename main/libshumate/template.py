14ea6772e51351a3b0fffe8446e8dc95b03f3839
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-05 07:50:33 +0300

    main/libshumate: update to 1.3_rc

--- main/libshumate/template.py
+++ main/libshumate/template.py
@@ -1,5 +1,5 @@
 pkgname = "libshumate"
-pkgver = "1.2.3"
+pkgver = "1.3_rc"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dgtk_doc=false"]
@@ -25,8 +25,8 @@ pkgdesc = "GTK library to display maps"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libshumate"
-source = f"$(GNOME_SITE)/libshumate/{pkgver[:-2]}/libshumate-{pkgver}.tar.xz"
-sha256 = "4cc6cd73f6d87155f62766ab63e5aacc473bd9a0ce35906932acfc839e964c0c"
+source = f"$(GNOME_SITE)/libshumate/{pkgver[:3]}/libshumate-{pkgver.replace('_', '.')}.tar.xz"
+sha256 = "9b485d5d54de77b003dd19643f9614183279fa08de2520e2eb8a5eb10031390c"
 options = ["!cross"]
 
 
