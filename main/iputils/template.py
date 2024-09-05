3b34f4fa8b3807c569cc5b6f76f57edc5bb9d9f6
psykose <alice@ayaya.dev>
2024-09-05 14:55:15 +0200

    main/iputils: update to 20240905

--- main/iputils/template.py
+++ main/iputils/template.py
@@ -1,18 +1,18 @@
 pkgname = "iputils"
-pkgver = "20240117"
-pkgrel = 1
+pkgver = "20240905"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-DNO_SETCAP_OR_SUID=true",
     "-DUSE_IDN=false",
 ]
 hostmakedepends = [
+    "docbook-xsl",
+    "iproute2",
+    "libcap-progs",
     "meson",
     "pkgconf",
     "xsltproc",
-    "docbook-xsl",
-    "libcap-progs",
-    "iproute2",
 ]
 makedepends = ["libcap-devel"]
 pkgdesc = "Useful utilities for Linux networking"
@@ -20,7 +20,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause AND GPL-2.0-or-later"
 url = "https://github.com/iputils/iputils"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "a5d66e2997945b2541b8f780a7f5a5ec895d53a517ae1dc4f3ab762573edea9a"
+sha256 = "055b4e6e4f298c97fd5848898099e59b4590db63fac3f7ad4fa796354ad44403"
 file_modes = {
     "usr/bin/clockdiff": ("root", "root", 0o755),
     "usr/bin/ping": ("root", "root", 0o755),
