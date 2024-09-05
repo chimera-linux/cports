a24ebed149e21d1bce4eb28dd8aa2b3e51eb0f5d
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-05 07:44:57 +0300

    main/gtkmm: update to 4.15.1

--- main/gtkmm/template.py
+++ main/gtkmm/template.py
@@ -1,5 +1,5 @@
 pkgname = "gtkmm"
-pkgver = "4.14.0"
+pkgver = "4.15.1"
 pkgrel = 0
 build_style = "meson"
 make_check_wrapper = ["wlheadless-run", "--"]
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gtkmm.org"
 source = f"$(GNOME_SITE)/gtkmm/{pkgver[:-2]}/gtkmm-{pkgver}.tar.xz"
-sha256 = "9350a0444b744ca3dc69586ebd1b6707520922b6d9f4f232103ce603a271ecda"
+sha256 = "1b3576c951c96796fa36efa379ad614e34d86f91c0f8b56f4443011e0b10d157"
 
 
 @subpackage("gtkmm-devel")
