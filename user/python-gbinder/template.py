a0ad3936f35c6b3a365738083ae9e74ad57ad520
Tulip Blossom <tulilirockz@outlook.com>
2024-09-07 17:27:21 -0300

    user/python-gbinder: new package

---
+++ user/python-gbinder/template.py
@@ -0,0 +1,23 @@
+pkgname = "python-gbinder"
+pkgver = "1.1.2"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "pkgconf",
+    "python-build",
+    "python-cython",
+    "python-installer",
+    "python-setuptools",
+]
+makedepends = [
+    "libgbinder-devel",
+    "python-devel",
+]
+pkgdesc = "Python bindings for libgbinder"
+maintainer = "tulilirockz <tulilirockz@outlook.com>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/erfanoabdi/gbinder-python"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "2dc424d5c2594146612e4bd752964f8928a62eec7c5ce6046f4c582079d0b537"
+# check: needs manually running script in .py with manual setup
+options = ["!check"]
