7bd6a4c31e6f0c5162e759dc0c0315d1f6e6a68d
psykose <alice@ayaya.dev>
2024-09-07 08:01:04 +0200

    user/g4music: update to 3.9.1

--- user/g4music/template.py
+++ user/g4music/template.py
@@ -1,5 +1,5 @@
 pkgname = "g4music"
-pkgver = "3.9"
+pkgver = "3.9.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -21,4 +21,4 @@ maintainer = "tulilirockz <tulilirockz@outlook.com>"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.gnome.org/neithern/g4music"
 source = f"{url}/-/archive/v{pkgver}/g4music-v{pkgver}.tar.gz"
-sha256 = "cdc1a79ca579cc211d5da596db4b5791d0899ceafc19dd9028f63b3d25232a36"
+sha256 = "50574f49505ba239da4f7bdfb57e2199e74e40ec8e10feb4953fcdf529401b22"
