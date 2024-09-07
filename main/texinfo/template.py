a33c865368f755136c8c461a613afedb70863b2c
psykose <alice@ayaya.dev>
2024-09-07 22:04:30 +0200

    main/texinfo: update to 7.1.1

--- main/texinfo/template.py
+++ main/texinfo/template.py
@@ -1,6 +1,6 @@
 pkgname = "texinfo"
-pkgver = "7.1"
-pkgrel = 2
+pkgver = "7.1.1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-static", "--disable-perl-xs"]
 # cycle of autoconf needing this
@@ -14,5 +14,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/texinfo"
 source = f"$(GNU_SITE)/texinfo/texinfo-{pkgver}.tar.xz"
-sha256 = "deeec9f19f159e046fdf8ad22231981806dac332cc372f1c763504ad82b30953"
+sha256 = "31ae37e46283529432b61bee1ce01ed0090d599e606fc6a29dca1f77c76a6c82"
 hardening = ["vis", "!cfi"]
