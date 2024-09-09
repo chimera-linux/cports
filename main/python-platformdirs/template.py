8339fd6aafa48aa139d01bcbfc1090a8c22cb2ff
psykose <alice@ayaya.dev>
2024-09-09 10:15:49 +0200

    main/python-platformdirs: update to 4.3.2

--- main/python-platformdirs/template.py
+++ main/python-platformdirs/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-platformdirs"
-pkgver = "4.3.1"
+pkgver = "4.3.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-installer", "python-hatch_vcs"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://platformdirs.readthedocs.io"
 source = f"$(PYPI_SITE)/p/platformdirs/platformdirs-{pkgver}.tar.gz"
-sha256 = "63b79589009fa8159973601dd4563143396b35c5f93a58b36f9049ff046949b1"
+sha256 = "9e5e27a08aa095dd127b9f2e764d74254f482fef22b0970773bfba79d091ab8c"
 # unpackaged checkdepends
 options = ["!check"]
 
