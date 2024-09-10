0e02af18c94b8ce4d763e187f7eb1605969d260a
Tulip Blossom <tulilirockz@outlook.com>
2024-09-10 00:40:40 -0300

    user/gdu: new package (5.29.0)

---
+++ user/gdu/template.py
@@ -0,0 +1,19 @@
+pkgname = "gdu"
+pkgver = "5.29.0"
+pkgrel = 0
+build_style = "go"
+make_build_args = ["./cmd/gdu"]
+# expects writing to /xyzxyz to give eperm instead of erofs (which happens in --ro bwrap)
+make_check_args = ["-skip", "TestOutputFileError", "./..."]
+hostmakedepends = ["go"]
+pkgdesc = "Disk usage analyzer written in Go"
+maintainer = "tulilirockz <tulilirockz@outlook.com>"
+license = "MIT"
+url = "https://github.com/dundee/gdu"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "42e972f46e49995be24b223c91375bfbea547f5e8cf94c0364f7b3eb5b0ed0a3"
+
+
+def post_install(self):
+    self.install_man("gdu.1")
+    self.install_license("LICENSE.md")
