8d85b192ca9b2ee4c00e03657b98c305008afbfb
psykose <alice@ayaya.dev>
2024-09-10 14:32:09 +0200

    contrib/binaryen: run tests

--- contrib/binaryen/template.py
+++ contrib/binaryen/template.py
@@ -1,6 +1,6 @@
 pkgname = "binaryen"
 pkgver = "119"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 configure_args = [
     "-DBUILD_SHARED_LIBS=ON",
@@ -14,19 +14,28 @@ hostmakedepends = [
 makedepends = ["gtest-devel"]
 checkdepends = [
     "nodejs",
-    "filecheck",
+    "lit",
+    "llvm-tools",
 ]
 pkgdesc = "Optimizer and compiler/toolchain library for WebAssembly"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://github.com/WebAssembly/binaryen"
-source = f"{url}/archive/refs/tags/version_{pkgver}.tar.gz"
-sha256 = "9c2614212f628fad451b847ffa0ce2fc59339453f4ea1bacf4417590caa5fc71"
+source = [
+    f"{url}/archive/refs/tags/version_{pkgver}.tar.gz",
+    "https://github.com/WebAssembly/testsuite/archive/e05365077e13a1d86ffe77acfb1a835b7aa78422.tar.gz",
+]
+source_paths = [".", "test/spec/testsuite"]
+sha256 = [
+    "9c2614212f628fad451b847ffa0ce2fc59339453f4ea1bacf4417590caa5fc71",
+    "0c9961b7b308f87bed2f8187892047fe5575af2298d0bfdca526223219dfc899",
+]
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=0x200000"]}
 # FIXME: negoverflow wasm::Literal::abs in tests
 hardening = ["!int"]
-# take forever
-options = ["!check"]
+# 'filecheck' is a shitty python port of the llvm FileCheck, just use the original
+# only for 'lit' below
+exec_wrappers = [("/usr/bin/FileCheck", "filecheck")]
 
 
 def check(self):
@@ -36,6 +45,17 @@ def check(self):
         "--no-torture",
         "--binaryen-bin",
         f"{self.make_dir}/bin",
+        "crash",
+        "dylink",
+        "example",
+        "gtest",
+        "lld",
+        "unit",
+        "validator",
+        "version",
+        "wasm-opt",
+        # fail with llvm18 lit lol
+        # "lit",
     )
 
 
