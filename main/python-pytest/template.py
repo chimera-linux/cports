b816e9fdcc227c3b158b67cd427ce8d806764cc8
psykose <alice@ayaya.dev>
2024-09-10 14:38:23 +0200

    main/python-pytest: update to 8.3.3

--- main/python-pytest/template.py
+++ main/python-pytest/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pytest"
-pkgver = "8.3.2"
+pkgver = "8.3.3"
 pkgrel = 0
 build_style = "python_pep517"
 _deps = [
@@ -20,7 +20,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://docs.pytest.org/en/latest"
 source = f"$(PYPI_SITE)/p/pytest/pytest-{pkgver}.tar.gz"
-sha256 = "c132345d12ce551242c87269de812483f5bcc87cdbb4722e48487ba194f9fdce"
+sha256 = "70b98107bd648308a7952b06e6ca9a50bc660be218d53c257cc1fc94fda10181"
 # missing checkdepends
 options = ["!check"]
 
