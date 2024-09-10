fa05c164dac07475b6b619c82ad4340a01bcbf2b
Erica Z <zerica@callcc.eu>
2024-09-10 10:39:23 +0200

    contrib/schismtracker: update to 20240909

--- contrib/schismtracker/template.py
+++ contrib/schismtracker/template.py
@@ -1,13 +1,17 @@
 pkgname = "schismtracker"
-pkgver = "20240809"
+pkgver = "20240909"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf"]
-makedepends = ["sdl-devel", "flac-devel"]
+makedepends = [
+    "flac-devel",
+    "sdl-devel",
+    "utf8proc-devel",
+]
 pkgdesc = "Reimplementation of Impulse Tracker"
 maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-2.0-or-later"
 url = "https://schismtracker.org"
 source = f"https://github.com/schismtracker/schismtracker/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "4dafacc4db2516629d377097573a3cad9ee41de44b2f3c956b360779440168e0"
+sha256 = "3e29fb646e08ae210f027d1c8aaed4b6a0514b731202cb437a37b9685d40d6cd"
 tool_flags = {"CFLAGS": ["-D_GNU_SOURCE", f'-DVERSION2="{pkgver}"']}
