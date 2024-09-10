b66520a1813bb16032e7f5c254beac1af6f212d4
triallax <triallax@tutanota.com>
2024-09-10 12:00:33 +0100

    contrib/python-filecheck: update to 1.0.1
    technically a different project, but the old one declared the new as its
    successor and the pypi package was handed over too so ¯\_(ツ)_/¯

--- contrib/python-filecheck/template.py
+++ contrib/python-filecheck/template.py
@@ -1,19 +1,18 @@
-# pypi name
 pkgname = "python-filecheck"
-pkgver = "0.0.24"
+pkgver = "1.0.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
-    "python-hatchling",
     "python-installer",
+    "python-poetry-core",
 ]
 checkdepends = ["python-pytest"]
 depends = ["python"]
 pkgdesc = "Reimplementation of LLVM FileCheck in Python"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
-url = "https://github.com/mull-project/FileCheck.py"
+url = "https://github.com/AntonLydike/filecheck"
 # pypi has no tests
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "4a89468778e47a35ee413f83070188f47a1842f367de6dfb511cc390d635cf11"
+sha256 = "229c1b4f34aae1538d6d0ba2cf2d8cd09b77db51297236d893307f95231a296e"
--- contrib/python-filecheck/update.py
+++
@@ -1,1 +0,0 @@
-pkgname = "FileCheck.py"
