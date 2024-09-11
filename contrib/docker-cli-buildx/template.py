9897eb2a7e911f072ab8a8f0c16163b89c795888
ttyyls <contact@behri.org>
2024-09-11 08:01:35 +0100

    contrib/docker-cli-buildx: update to 0.17.0

--- contrib/docker-cli-buildx/template.py
+++ contrib/docker-cli-buildx/template.py
@@ -1,6 +1,6 @@
 pkgname = "docker-cli-buildx"
-pkgver = "0.16.2"
-pkgrel = 3
+pkgver = "0.17.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X github.com/docker/buildx/version.Version=v{pkgver}",
@@ -15,7 +15,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/docker/buildx"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f314635765f3dc5efe089244280cd24a577e83d339fec1970fed16977bf28382"
+sha256 = "f846f36dc93d78e9b1c94b716279a04f1d6f58ef5d445c6615ec8e5e3a7b5efa"
 # some tests rely on network
 options = ["!check"]
 
