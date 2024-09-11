442d167d2ab10cea73f1e6c438bafe3597ebc528
jabuxas <jabuxas@proton.me>
2024-09-11 14:52:49 -0300

    user/lxde-common: new package

---
+++ user/lxde-common/template.py
@@ -0,0 +1,14 @@
+pkgname = "lxde-common"
+pkgver = "0.99.2"
+pkgrel = 0
+build_style = "gnu_configure"
+hostmakedepends = ["automake", "gettext-devel", "intltool", "pkgconf"]
+makedepends = ["glib-devel"]
+pkgdesc = "LXDE Session default configuration files"
+maintainer = "jabuxas <jabuxas@proton.me>"
+license = "GPL-2.0-only"
+url = "https://github.com/lxde/lxde-common"
+source = f"https://downloads.sourceforge.net/lxde/lxde-common-{pkgver}.tar.xz"
+sha256 = "1cd9bc900960c995d48ffbbdc86ecffda7c806168c67aaa50c53113794234856"
+# no tests
+options = ["!check"]
