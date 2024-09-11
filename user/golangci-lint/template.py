47a4234aff8ba5cef7204d3cdf79063cd76a4ca7
Tulip Blossom <tulilirockz@outlook.com>
2024-09-11 11:28:10 -0300

    user/golangci-lint: update to 1.61.0

--- user/golangci-lint/template.py
+++ user/golangci-lint/template.py
@@ -1,6 +1,6 @@
 pkgname = "golangci-lint"
-pkgver = "1.60.3"
-pkgrel = 1
+pkgver = "1.61.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/golangci-lint"]
 hostmakedepends = ["go"]
@@ -9,6 +9,6 @@ maintainer = "Denis Strizhkin <strdenis02@gmail.com>"
 license = "GPL-3.0-or-later"
 url = "https://golangci-lint.run"
 source = f"https://github.com/golangci/golangci-lint/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "038510ae3d094ef1553ee9d4a29a5514b46c83fe68b86279a33d4f284c0a71b2"
+sha256 = "f4efcc09dde3eb81ba7e2fc4230d3e99375a4b176dd28c31cab07371cf5c07db"
 # some tests fail because of chroot and some need network
 options = ["!check"]
