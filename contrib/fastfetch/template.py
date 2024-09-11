8f199df47489f9ee60c45e72ac75743b0e00276e
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-11 10:37:42 +0300

    contrib/fastfetch: update to 2.24.0

--- contrib/fastfetch/template.py
+++ contrib/fastfetch/template.py
@@ -1,5 +1,5 @@
 pkgname = "fastfetch"
-pkgver = "2.23.0"
+pkgver = "2.24.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_TESTS=ON", "-DENABLE_SYSTEM_YYJSON=ON"]
@@ -27,7 +27,7 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "MIT"
 url = "https://github.com/fastfetch-cli/fastfetch"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "3c92dd2cb15faf55d87846eda8d14456af2d0e0938998434144074c695c57529"
+sha256 = "675ac3f9dbe00277416744fa36a28fc9cd1284d17f055a4db339063bfc6a8209"
 tool_flags = {"CFLAGS": ["-DNDEBUG"]}
 # CFI: dies immediately (ffPlatformPathAddHome at FFlist.c:31:12)
 hardening = ["vis", "!cfi"]
