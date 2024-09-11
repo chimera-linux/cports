e42366a2c4e4803c2d80a237487ade9b305e093d
jabuxas <jabuxas@proton.me>
2024-09-11 14:56:40 -0300

    user/lxde: new package

---
+++ user/lxde/template.py
@@ -0,0 +1,26 @@
+pkgname = "lxde"
+pkgver = "0.5.5"
+pkgrel = 0
+build_style = "meta"
+depends = [
+    "gpicview",
+    "lxappearance",
+    "lxappearance-obconf",
+    "lxde-common",
+    "lxde-icon-theme",
+    "lxinput",
+    "lxlauncher",
+    "lxpanel",
+    "lxrandr",
+    "lxsession",
+    "lxtask",
+    "lxterminal",
+    "openbox",
+    "pcmanfm",
+    "xserver-xorg-minimal",
+]
+pkgdesc = "LXDE desktop environment"
+subdesc = "session"
+maintainer = "jabuxas <jabuxas@proton.me>"
+license = "custom:meta"
+url = "https://www.lxde.org"
