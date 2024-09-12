5434273acfa39372a9017eadc0796c295083836f
psykose <alice@ayaya.dev>
2024-09-12 19:56:05 +0200

    contrib/plasma-wayland-protocols: update to 1.14.0

--- contrib/plasma-wayland-protocols/template.py
+++ contrib/plasma-wayland-protocols/template.py
@@ -1,6 +1,6 @@
 pkgname = "plasma-wayland-protocols"
-pkgver = "1.13.0"
-pkgrel = 1
+pkgver = "1.14.0"
+pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -12,7 +12,7 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "MIT AND (LGPL-2.1-only OR LGPL-3.0-only)"
 url = "https://invent.kde.org/libraries/plasma-wayland-protocols"
 source = f"$(KDE_SITE)/plasma-wayland-protocols/plasma-wayland-protocols-{pkgver}.tar.xz"
-sha256 = "dd477e352f5ff6e6ac686286c4b22b19bf5a4921b85ee5a7da02bb7aa115d57e"
+sha256 = "1a4385ecfc79f7589f07381cab11c3ff51f6e2fa4b73b78600d6ad096394bf81"
 
 
 def post_install(self):
