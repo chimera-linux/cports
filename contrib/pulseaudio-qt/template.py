a3230e8bb324d28e914e54802f4e56d859c0447b
psykose <alice@ayaya.dev>
2024-09-12 18:23:45 +0200

    contrib/pulseaudio-qt: update to 1.6.0

--- contrib/pulseaudio-qt/template.py
+++ contrib/pulseaudio-qt/template.py
@@ -1,5 +1,5 @@
 pkgname = "pulseaudio-qt"
-pkgver = "1.5.0"
+pkgver = "1.6.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_WITH_QT6=ON"]
@@ -18,7 +18,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-only OR LGPL-3.0-only"
 url = "https://invent.kde.org/libraries/pulseaudio-qt"
 source = f"$(KDE_SITE)/pulseaudio-qt/pulseaudio-qt-{pkgver}.tar.xz"
-sha256 = "cd8f51c8700073d0fd90d5784083aceb73e72ba9a704e605e0a67909426a8520"
+sha256 = "1becbadacb36a9d6a431a0c93cdb428f8f67f37cf2d23768675983318c0ade84"
 hardening = ["vis"]
 
 
