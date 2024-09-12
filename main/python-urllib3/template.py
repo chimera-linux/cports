ce5db853edb56e4a3025dc7606e1e85235ac4e1a
psykose <alice@ayaya.dev>
2024-09-12 13:05:19 +0200

    main/python-urllib3: update to 2.2.3

--- main/python-urllib3/template.py
+++ main/python-urllib3/template.py
@@ -1,15 +1,20 @@
 pkgname = "python-urllib3"
-pkgver = "2.2.2"
+pkgver = "2.2.3"
 pkgrel = 0
 build_style = "python_pep517"
-hostmakedepends = ["python-build", "python-installer", "python-hatchling"]
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-hatch_vcs",
+    "python-hatchling",
+]
 depends = ["python"]
 pkgdesc = "HTTP library with thread-safe connection pooling"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://urllib3.readthedocs.io"
 source = f"$(PYPI_SITE)/u/urllib3/urllib3-{pkgver}.tar.gz"
-sha256 = "dd505485549a7a552833da5e6063639d0d177c04f23bc3864e41e5dc5f612168"
+sha256 = "e7d814a81dad81e6caf2ec9fdedb284ecc9c73076b62654547cc64ccdcae26e9"
 # unpackaged dependency
 options = ["!check", "brokenlinks"]
 
