9781a0c4f3e5d491f06926af0290f290dfb6d83e
psykose <alice@ayaya.dev>
2024-09-13 14:02:52 +0200

    main/libpng: update to 1.6.44

--- main/libpng/template.py
+++ main/libpng/template.py
@@ -1,6 +1,6 @@
 pkgname = "libpng"
-pkgver = "1.6.43"
-pkgrel = 2
+pkgver = "1.6.44"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf", "libtool"]
 makedepends = ["zlib-ng-compat-devel"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Libpng"
 url = "http://www.libpng.org/pub/png/libpng.html"
 source = f"$(SOURCEFORGE_SITE)/libpng/libpng-{pkgver}.tar.xz"
-sha256 = "6a5ca0652392a2d7c9db2ae5b40210843c0bbc081cbd410825ab00cc59f14a6c"
+sha256 = "60c4da1d5b7f0aa8d158da48e8f8afa9773c1c8baa5d21974df61f1886b8ce8e"
 
 
 def post_install(self):
