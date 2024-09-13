aa24a80797a41bf2dfa4a31f3cd2ed3aa0e86a50
psykose <alice@ayaya.dev>
2024-09-13 18:43:27 +0200

    main/libshumate: update to 1.3.0

--- main/libshumate/template.py
+++ main/libshumate/template.py
@@ -1,5 +1,5 @@
 pkgname = "libshumate"
-pkgver = "1.2.3"
+pkgver = "1.3.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dgtk_doc=false"]
@@ -26,7 +26,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libshumate"
 source = f"$(GNOME_SITE)/libshumate/{pkgver[:-2]}/libshumate-{pkgver}.tar.xz"
-sha256 = "4cc6cd73f6d87155f62766ab63e5aacc473bd9a0ce35906932acfc839e964c0c"
+sha256 = "8227a6e8281cde12232894fef83760d44fa66b39ef033c61ed934a86c6dc75d4"
 options = ["!cross"]
 
 
