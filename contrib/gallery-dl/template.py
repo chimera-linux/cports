75160a6579ce53c49d671cfb4bd7519ceb61cbc3
psykose <alice@ayaya.dev>
2024-09-13 14:25:52 +0200

    contrib/gallery-dl: update to 1.27.4

--- contrib/gallery-dl/template.py
+++ contrib/gallery-dl/template.py
@@ -1,5 +1,5 @@
 pkgname = "gallery-dl"
-pkgver = "1.27.3"
+pkgver = "1.27.4"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
@@ -18,7 +18,7 @@ maintainer = "Nasado <hi@nasado.name>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/mikf/gallery-dl"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "230986ed8957d8e65ea9b453df50b9bbd3fb86d0ee4b2c7ea9487db7c47aa1c7"
+sha256 = "6f2d749d5fa2c05beefa99b0bae53a180e7a9299f9a48c307ea26b3f854e8bae"
 
 
 def pre_build(self):
