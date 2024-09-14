dd21d4d1d197a63515ec13757754ab6d0cf95022
psykose <alice@ayaya.dev>
2024-09-14 18:56:02 +0200

    contrib/bmake: update to 20240901

--- contrib/bmake/template.py
+++ contrib/bmake/template.py
@@ -1,12 +1,12 @@
 pkgname = "bmake"
-pkgver = "20240808"
-pkgrel = 1
+pkgver = "20240901"
+pkgrel = 0
 pkgdesc = "Portable version of NetBSD make"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://www.crufty.net/help/sjg/bmake.html"
 source = f"https://www.crufty.net/ftp/pub/sjg/bmake-{pkgver}.tar.gz"
-sha256 = "b59189251b483decd4492f1f74387b2a584c03d5aa4637cd48b38ec62b9c0848"
+sha256 = "b5d753befc42e8a852a38e5cb6137c4e5a91585a6cb3888cf0645725c7759a66"
 hardening = ["vis", "cfi"]
 
 
