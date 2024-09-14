e9e5b53942413b031b9102174801da8d085c65a4
ttyyls <contact@behri.org>
2024-09-14 08:34:48 +0100

    user/zvm: update to 0.7.5

--- user/zvm/template.py
+++ user/zvm/template.py
@@ -1,6 +1,6 @@
 pkgname = "zvm"
-pkgver = "0.7.4"
-pkgrel = 3
+pkgver = "0.7.5"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Zig version manager"
@@ -8,7 +8,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "MIT"
 url = "https://github.com/tristanisham/zvm"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "ff72b3beda8dac5bd8d7d9fddb3252460ed454285ad9acb20f24ac00b45db102"
+sha256 = "bc3e5406c1de6a20529d98c91738912b118da46ec0df6b3b3224afa579140386"
 
 
 def post_install(self):
