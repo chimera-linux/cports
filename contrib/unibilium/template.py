94e6bccaf60dc2567a90c740de60b96eb1692bbd
psykose <alice@ayaya.dev>
2024-09-14 15:41:42 +0200

    contrib/unibilium: update to 2.1.2

--- contrib/unibilium/template.py
+++ contrib/unibilium/template.py
@@ -1,15 +1,16 @@
 pkgname = "unibilium"
-pkgver = "2.1.1"
-pkgrel = 1
-build_style = "makefile"
+pkgver = "2.1.2"
+pkgrel = 0
+build_style = "gnu_configure"
+make_dir = "."
 make_check_target = "test"
-hostmakedepends = ["libtool", "perl", "pkgconf"]
+hostmakedepends = ["automake", "libtool", "perl", "pkgconf"]
 pkgdesc = "Simple, self-contained terminfo library"
 maintainer = "yopito <pierre.bourgin@free.fr>"
 license = "LGPL-3.0-or-later"
 url = "https://github.com/neovim/unibilium"
 source = f"https://github.com/neovim/unibilium/archive/v{pkgver}.tar.gz"
-sha256 = "6f0ee21c8605340cfbb458cbd195b4d074e6d16dd0c0e12f2627ca773f3cabf1"
+sha256 = "370ecb07fbbc20d91d1b350c55f1c806b06bf86797e164081ccc977fc9b3af7a"
 # crossbuild fails because of libtool
 options = ["!cross"]
 
