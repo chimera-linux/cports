78486732944695bcb9b0b8153f97ba58d24542e1
psykose <alice@ayaya.dev>
2024-09-15 23:33:36 +0200

    main/python-idna: update to 3.10

--- main/python-idna/template.py
+++ main/python-idna/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-idna"
-pkgver = "3.9"
+pkgver = "3.10"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_target = "tests"
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/kjd/idna"
 source = f"$(PYPI_SITE)/i/idna/idna-{pkgver}.tar.gz"
-sha256 = "e5c5dafde284f26e9e0f28f6ea2d6400abd5ca099864a67f576f3981c6476124"
+sha256 = "12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9"
 # dep cycle with pytest
 options = ["!check"]
 
