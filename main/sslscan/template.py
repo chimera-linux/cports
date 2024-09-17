ac8a62f74ccee87de8c97e42cacf03ea41344f38
psykose <alice@ayaya.dev>
2024-09-17 17:15:31 +0200

    main/sslscan: update to 2.1.5

--- main/sslscan/template.py
+++ main/sslscan/template.py
@@ -1,5 +1,5 @@
 pkgname = "sslscan"
-pkgver = "2.1.4"
+pkgver = "2.1.5"
 pkgrel = 0
 build_style = "makefile"
 make_build_args = [f"GIT_VERSION={pkgver}"]
@@ -9,7 +9,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later WITH custom:OpenSSL-exception"
 url = "https://github.com/rbsec/sslscan"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "3e2a5b1f53d1f132b4d999ff450d2cc40e9efb648cea89b74f5944b768a10e63"
+sha256 = "f1fa33deba4033745cdfda5d0674f4f6e9f572548062737acc04b3e972edc43f"
 hardening = ["vis", "cfi"]
 # no tests
 options = ["!check"]
