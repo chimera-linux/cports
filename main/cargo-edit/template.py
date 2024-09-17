0e29e0b2d3f52b3ed0e0bd4bdbda4cacb7e447f9
psykose <alice@ayaya.dev>
2024-09-17 17:05:23 +0200

    main/cargo-edit: update to 0.13.0

--- main/cargo-edit/template.py
+++ main/cargo-edit/template.py
@@ -1,15 +1,14 @@
 pkgname = "cargo-edit"
-pkgver = "0.12.3"
+pkgver = "0.13.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
-makedepends = ["openssl-devel", "libgit2-devel"]
 pkgdesc = "CLI utility for managing cargo dependencies"
 maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/killercup/cargo-edit"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5ee14b3f16caa2f36d1e6d41f768043f134e552bec8dbe9d3afe00621b2d0020"
+sha256 = "c81a73fb1ef4ffef722835baf473beed9868ce2c58ad98a27596f2cbabbfcba3"
 # Checks don't work with our cargo config overrides
 options = ["!check"]
 
