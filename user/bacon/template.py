90ceee62b16eec92d93ce22c1d153aa3c4cb2583
Tulip Blossom <tulilirockz@outlook.com>
2024-09-17 00:19:02 -0300

    user/bacon: new package

---
+++ user/bacon/template.py
@@ -0,0 +1,15 @@
+pkgname = "bacon"
+pkgver = "2.21.0"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable"]
+pkgdesc = "Background rust code checker"
+maintainer = "tulilirockz <tulilirockz@outlook.com>"
+license = "AGPL-3.0-only"
+url = "https://dystroy.org/bacon"
+source = f"https://github.com/Canop/bacon/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "ff43a713b5a301a90ac4e1432cc119f9e52b2563d71dbafa30546cef4a7aeacd"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
