46e43eedb93306f079ecfd185b1c7f1e761038c9
Renato Botelho do Couto <renato@netgate.com>
2024-09-18 10:42:31 -0300

    main/libdbi: disable docs and fix build
    Disable docs building since it requires openjade.  While here, add
    automake and libtool dependencies and remove empty configure_gen.

--- main/libdbi/template.py
+++ main/libdbi/template.py
@@ -1,8 +1,9 @@
 pkgname = "libdbi"
 pkgver = "0.9.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+configure_args = ["--disable-docs"]
+hostmakedepends = ["automake", "libtool", "pkgconf"]
 pkgdesc = "Database-independent abstraction layer for C"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
@@ -16,6 +17,3 @@ sha256 = "dafb6cdca524c628df832b6dd0bf8fabceb103248edb21762c02d3068fca4503"
 @subpackage("libdbi-devel")
 def _(self):
     return self.default_devel()
-
-
-configure_gen = []
