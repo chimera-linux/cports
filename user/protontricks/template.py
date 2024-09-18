4387a2ac5d58a641dc5d0a10488b6eeb4e3f2c2f
Erica Z <zerica@callcc.eu>
2024-09-18 08:41:23 +0200

    user/protontricks: update to 1.12.0

--- user/protontricks/template.py
+++ user/protontricks/template.py
@@ -1,5 +1,5 @@
 pkgname = "protontricks"
-pkgver = "1.11.1"
+pkgver = "1.12.0"
 pkgrel = 0
 build_style = "python_pep517"
 make_build_env = {"SETUPTOOLS_SCM_PRETEND_VERSION": pkgver}
@@ -25,4 +25,4 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-3.0-only"
 url = "https://github.com/Matoking/protontricks"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "9601a6e4531165856e77e19c54bc2cd3c6743cb931739c630e234be1de15975f"
+sha256 = "6714e8fdd6f279659490a0fb85fc05b42bc91eb877a2b5febeb384f60ffb1051"
