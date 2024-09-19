35a4efb0209ef8a5b6624ba3667933fdb463473e
psykose <alice@ayaya.dev>
2024-09-19 12:38:52 +0200

    main/intel-media-driver: update to 24.3.4

--- main/intel-media-driver/template.py
+++ main/intel-media-driver/template.py
@@ -1,5 +1,5 @@
 pkgname = "intel-media-driver"
-pkgver = "24.3.3"
+pkgver = "24.3.4"
 pkgrel = 0
 # doesn't build elsewhere
 archs = ["x86_64"]
@@ -25,7 +25,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-3-Clause"
 url = "https://github.com/intel/media-driver"
 source = f"{url}/archive/refs/tags/intel-media-{pkgver}.tar.gz"
-sha256 = "5eb35a1dd601c75ccc3af3c5b21acc82b245ae79ac8d7264f99215be8d064194"
+sha256 = "58978f9ee4981532e60be2f2768673b1f3825db09971ebb98fbd7e8819cab6eb"
 # INT: crashes during certain vaapi decode (twitch.tv?)
 hardening = ["vis", "!cfi", "!int"]
 
