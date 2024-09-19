e7ab42a9103d850356fd93ca52025b495fbf8eb9
psykose <alice@ayaya.dev>
2024-09-19 02:35:07 +0200

    main/libraw: update to 0.21.3

--- main/libraw/template.py
+++ main/libraw/template.py
@@ -1,20 +1,20 @@
 pkgname = "libraw"
-pkgver = "0.21.2"
-pkgrel = 1
+pkgver = "0.21.3"
+pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf", "automake", "libtool"]
+hostmakedepends = ["pkgconf", "automake", "slibtool"]
 makedepends = [
-    "zlib-ng-compat-devel",
     "jasper-devel",
-    "libjpeg-turbo-devel",
     "lcms2-devel",
+    "libjpeg-turbo-devel",
+    "zlib-ng-compat-devel",
 ]
 pkgdesc = "Raw image decoder library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-only OR CDDL-1.0"
 url = "https://libraw.org"
 source = f"{url}/data/LibRaw-{pkgver}.tar.gz"
-sha256 = "fe7288013206854baf6e4417d0fb63ba4ed7227bf36fff021992671c2dd34b03"
+sha256 = "dba34b7fc1143503942fa32ad9db43e94f714e62a4a856e91617f8f3e1e0aa5c"
 
 
 def post_install(self):
