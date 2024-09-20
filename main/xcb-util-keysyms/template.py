24865cfc622ad242e0f631577fc576804098b777
Renato Botelho do Couto <renato@netgate.com>
2024-09-20 09:48:23 -0300

    main/xcb-util-keysyms: install license file
    while here, add autotools dependencies and remove empty configure_gen

--- main/xcb-util-keysyms/template.py
+++ main/xcb-util-keysyms/template.py
@@ -2,7 +2,7 @@ pkgname = "xcb-util-keysyms"
 pkgver = "0.4.1"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "libtool", "pkgconf", "xorg-util-macros"]
 makedepends = ["xcb-util-devel"]
 pkgdesc = "XCB utilities library - key constants and keycode conversion"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -12,9 +12,10 @@ source = f"{url}/dist/xcb-util-keysyms-{pkgver}.tar.gz"
 sha256 = "1fa21c0cea3060caee7612b6577c1730da470b88cbdf846fa4e3e0ff78948e54"
 
 
+def post_install(self):
+    self.install_license("COPYING")
+
+
 @subpackage("xcb-util-keysyms-devel")
 def _(self):
     return self.default_devel()
-
-
-configure_gen = []
