584b2dcd2e0d98400d2a00f5f83d3bc2da6117d6
Renato Botelho do Couto <renato@netgate.com>
2024-09-20 08:10:19 -0300

    main/spice-protocol: install license file

--- main/spice-protocol/template.py
+++ main/spice-protocol/template.py
@@ -1,6 +1,6 @@
 pkgname = "spice-protocol"
 pkgver = "0.14.4"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
 pkgdesc = "Protocol definition for SPICE project"
@@ -9,3 +9,7 @@ license = "BSD-3-Clause"
 url = "https://gitlab.freedesktop.org/spice/spice-protocol"
 source = f"https://www.spice-space.org/download/releases/spice-protocol-{pkgver}.tar.xz"
 sha256 = "04ffba610d9fd441cfc47dfaa135d70096e60b1046d2119d8db2f8ea0d17d912"
+
+
+def post_install(self):
+    self.install_license("COPYING")
