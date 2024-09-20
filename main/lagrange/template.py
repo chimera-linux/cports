37f3147f2dbf0497eb5671dfcd98e7a10c24057d
psykose <alice@ayaya.dev>
2024-09-20 20:36:12 +0200

    main/lagrange: update to 1.18.0

--- main/lagrange/template.py
+++ main/lagrange/template.py
@@ -1,6 +1,6 @@
 pkgname = "lagrange"
-pkgver = "1.17.6"
-pkgrel = 1
+pkgver = "1.18.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DCMAKE_BUILD_TYPE=Release",
@@ -30,7 +30,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-2-Clause"
 url = "https://gmi.skyjake.fi/lagrange"
 source = f"https://github.com/skyjake/lagrange/releases/download/v{pkgver}/lagrange-{pkgver}.tar.gz"
-sha256 = "b9d0982617fec495565ac9c09fb788a0be207d6fdf2324edc390e5cac8b1523b"
+sha256 = "d23a89bfcdbd654f2b54c2406e359d7687595bc1b5dbbd13882e02397d546345"
 hardening = ["vis", "!cfi"]
 
 
