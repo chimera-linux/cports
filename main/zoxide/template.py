8d39ddfbf52d2f3d2b09732f24b864206691d265
psykose <alice@ayaya.dev>
2024-09-20 23:22:22 +0200

    main/zoxide: update to 0.9.6

--- main/zoxide/template.py
+++ main/zoxide/template.py
@@ -1,5 +1,5 @@
 pkgname = "zoxide"
-pkgver = "0.9.5"
+pkgver = "0.9.6"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -9,7 +9,7 @@ maintainer = "aurelia <git@elia.garden>"
 license = "MIT"
 url = "https://github.com/ajeetdsouza/zoxide"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "1278660e671d96c5421f0910fa7d79b9e0bb0bfacf7611ff63bf383f721d7a4f"
+sha256 = "e1811511a4a9caafa18b7d1505147d4328b39f6ec88b88097fe0dad59919f19c"
 
 
 def post_install(self):
