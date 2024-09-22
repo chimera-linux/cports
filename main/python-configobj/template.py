fecc23a2d51812a1e801650c551570d1af133873
psykose <alice@ayaya.dev>
2024-09-22 03:36:20 +0200

    main/python-configobj: update to 5.0.9

--- main/python-configobj/template.py
+++ main/python-configobj/template.py
@@ -1,22 +1,20 @@
 pkgname = "python-configobj"
-pkgver = "5.0.8"
-pkgrel = 1
+pkgver = "5.0.9"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
-depends = ["python-six"]
-pkgdesc = "Simple but powerful config file reader and writer"
+depends = ["python"]
+checkdepends = ["python-pytest"]
+pkgdesc = "Python config file reader and writer"
 maintainer = "yopito <pierre.bourgin@free.fr>"
 license = "BSD-3-Clause"
 url = "https://github.com/DiffSK/configobj"
 source = f"https://github.com/DiffSK/configobj/archive/v{pkgver}.tar.gz"
-sha256 = "547dc047e31c71d7a8732016336769ed450588f34a7c13077aa7acc7df245eda"
-# testing requires a pip environment
-options = ["!check"]
+sha256 = "2bd70f9ce7912679c4ba9c80da289877906db0ca6bd02c3ab545d660e9b60d4f"
 
 
 def post_install(self):
