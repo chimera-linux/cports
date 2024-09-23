86d6904a9df6a1888f496a3c83aa84739d378c7d
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-09-23 13:26:49 +0200

    main/openpgp-card-tools: update to 0.11.6

--- main/openpgp-card-tools/template.py
+++ main/openpgp-card-tools/template.py
@@ -1,17 +1,17 @@
 pkgname = "openpgp-card-tools"
-pkgver = "0.11.3"
-pkgrel = 1
+pkgver = "0.11.6"
+pkgrel = 0
 build_style = "cargo"
 prepare_after_patch = True
 hostmakedepends = ["cargo-auditable", "pkgconf"]
-makedepends = ["rust-std", "pcsc-lite-devel"]
+makedepends = ["rust-std", "pcsc-lite-devel", "dbus-devel"]
 depends = ["ccid"]
 pkgdesc = "CLI tool for inspecting, configuring and using OpenPGP cards"
 maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0 OR MIT"
 url = "https://codeberg.org/openpgp-card/openpgp-card-tools"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "083fe2e127baaee5dc799274d7208db5a3b7debde433f762263b8e4912af1d3c"
+sha256 = "d38817f28a3c86cd6a4d4712e141d923c8bd156127db9b64c010bb5d9c14914e"
 # generates completions using host binary
 options = ["!cross"]
 
@@ -19,13 +19,17 @@ options = ["!cross"]
 def post_build(self):
     self.do(
         f"target/{self.profile().triplet}/release/oct",
-        "completions",
-        "completions",
+        env={"OCT_MANPAGE_OUTPUT_DIR": "man"},
+    )
+    self.do(
+        f"target/{self.profile().triplet}/release/oct",
+        env={"OCT_COMPLETION_OUTPUT_DIR": "completions"},
     )
 
 
 def post_install(self):
     self.install_license("LICENSES/MIT.txt")
+    self.install_man("man/*.1", glob=True)
     with self.pushd("completions"):
         self.install_completion("oct.bash", "bash", "oct")
         self.install_completion("oct.fish", "fish", "oct")
