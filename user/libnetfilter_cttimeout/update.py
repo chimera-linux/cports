8aab292a7c5ea8abaac76fabafcf823d5513b6ed
Tulip Blossom <tulilirockz@outlook.com>
2024-09-23 01:12:51 -0300

    user/libnetfilter_cttimeout: new package

---
+++ user/libnetfilter_cttimeout/update.py
@@ -0,0 +1,4 @@
+url = "https://git.netfilter.org/libnetfilter_cttimeout/info/refs"
+pattern = r"refs/tags/libnetfilter_cttimeout-([\d.]+)"
+# because netfilter is being annoying with anubis
+agent_name = "git"
