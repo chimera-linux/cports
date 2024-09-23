1da18f700e491d84efae54787812899bd6375d77
psykose <alice@ayaya.dev>
2024-09-23 16:01:57 +0200

    main/yoshimi: update to 2.3.3

--- main/yoshimi/template.py
+++ main/yoshimi/template.py
@@ -1,6 +1,6 @@
 pkgname = "yoshimi"
-pkgver = "2.3.2"
-pkgrel = 1
+pkgver = "2.3.3"
+pkgrel = 0
 build_style = "cmake"
 cmake_dir = "src"
 hostmakedepends = [
@@ -26,4 +26,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://yoshimi.github.io"
 source = f"https://github.com/Yoshimi/yoshimi/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "76fcf0aaf959665f4d1a54c8e76dc30108bcf1434f76a0761e13217f973f27f6"
+sha256 = "f15ff1a909c273ea75febfd15655560a556b6b832afe57f918ea966702daf2f6"
