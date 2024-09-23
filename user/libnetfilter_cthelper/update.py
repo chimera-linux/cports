fab1dc5983ad28f9f421078c679d554bd021ac4b
Tulip Blossom <tulilirockz@outlook.com>
2024-09-23 01:11:37 -0300

    user/libnetfilter_cthelper: new package

---
+++ user/libnetfilter_cthelper/update.py
@@ -0,0 +1,4 @@
+url = "https://git.netfilter.org/libnetfilter_cthelper/info/refs"
+pattern = r"refs/tags/libnetfilter_cthelper-([\d.]+)"
+# because netfilter is being annoying with anubis
+agent_name = "git"
