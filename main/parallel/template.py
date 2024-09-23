95b4014a532bce374109aabd92d61c0293dcf92e
psykose <alice@ayaya.dev>
2024-09-23 16:01:26 +0200

    main/parallel: update to 20240922

--- main/parallel/template.py
+++ main/parallel/template.py
@@ -1,5 +1,5 @@
 pkgname = "parallel"
-pkgver = "20240822"
+pkgver = "20240922"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake"]
@@ -9,4 +9,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/parallel"
 source = f"https://ftp.gnu.org/gnu/parallel/parallel-{pkgver}.tar.bz2"
-sha256 = "d7bbd95b7631980b172be04cbd2138d5f7d8c063d6da5ad8f9f70dfd88c8309d"
+sha256 = "63210715e8b7c5e129e098f333cd7cdd5fc7a2f325e8e0fb9ed6edba9f1acbc4"
