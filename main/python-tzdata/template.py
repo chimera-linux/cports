9f9612baace2a7a42ed5955a9a1f68a1c53f2cb8
ttyyls <contact@behri.org>
2024-09-24 20:22:14 +0100

    main/python-tzdata: update to 2024.2

--- main/python-tzdata/template.py
+++ main/python-tzdata/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-tzdata"
-pkgver = "2024.1"
+pkgver = "2024.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,4 +15,4 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/python/tzdata"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "ee55197750b6002030c370f72c93f5f8fa863b98c7a3312850781cdc1e9f1037"
+sha256 = "1091c44555ab376a136cf32763f746e35137c20038d5013745685b7276e47b6b"
