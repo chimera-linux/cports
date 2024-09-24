8f82f29e46dffc61ba6c35a456435cecd06fbf01
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-09-24 12:22:50 +0200

    main/openpgp-card-tool-git: update to 0.1.4

--- main/openpgp-card-tool-git/template.py
+++ main/openpgp-card-tool-git/template.py
@@ -1,16 +1,22 @@
 pkgname = "openpgp-card-tool-git"
-pkgver = "0.1.3"
+pkgver = "0.1.4"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
-makedepends = ["rust-std", "openssl-devel", "pcsc-lite-devel", "sqlite-devel"]
+makedepends = [
+    "dbus-devel",
+    "openssl-devel",
+    "pcsc-lite-devel",
+    "rust-std",
+    "sqlite-devel",
+]
 depends = ["ccid"]
 pkgdesc = "Drop in replacement for gpg in git for usage with OpenPGP cards"
 maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0 OR MIT"
 url = "https://codeberg.org/openpgp-card/tool-git"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "e0994fb2135343905a45b741b34a2ba76671ba01a04adb8231e7865d2ed7151e"
+sha256 = "1bf653e40f6ceea03e0dad3c6e8b89c2c099ec580b249a5e2936904388e49507"
 
 
 def post_install(self):
