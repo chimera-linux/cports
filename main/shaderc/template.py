69d77078b0798d15108ee90adf0be3f038c8944b
psykose <alice@ayaya.dev>
2024-09-24 04:18:07 +0200

    main/shaderc: update to 2024.3
    no rebuilds cause only 1 line of diff

--- main/shaderc/template.py
+++ main/shaderc/template.py
@@ -1,5 +1,5 @@
 pkgname = "shaderc"
-pkgver = "2024.2"
+pkgver = "2024.3"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -13,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/google/shaderc"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "c25e24d47c911b808266684d9c75ee09a390a5c537c17465eb15ea6905e702c3"
+sha256 = "d5c68b5de5d4c7859d9699054493e0a42a2a5eb21b425d63f7b7dd543db0d708"
 tool_flags = {
     "CXXFLAGS": [f"-I{self.profile().sysroot / 'usr/include/glslang'}"]
 }
