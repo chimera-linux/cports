4acd3bad6a11df6e95d649ed614752a97e5b9e52
psykose <alice@ayaya.dev>
2024-09-25 01:35:13 +0200

    main/harfbuzz: update to 10.0.1

--- main/harfbuzz/template.py
+++ main/harfbuzz/template.py
@@ -1,5 +1,5 @@
 pkgname = "harfbuzz"
-pkgver = "10.0.0"
+pkgver = "10.0.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -33,7 +33,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "http://www.freedesktop.org/wiki/Software/HarfBuzz"
 source = f"https://github.com/harfbuzz/harfbuzz/releases/download/{pkgver}/harfbuzz-{pkgver}.tar.xz"
-sha256 = "c2dfe016ad833a5043ecc6579043f04e8e6d50064e02ad449bb466e6431e3e04"
+sha256 = "b2cb13bd351904cb9038f907dc0dee0ae07127061242fe3556b2795c4e9748fc"
 options = ["!cross"]
 
 
