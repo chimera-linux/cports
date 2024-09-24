f8275ae9cbd7d7138eeaf3eb63cc82939a0057e2
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2024-09-24 11:21:50 +0200

    main/minijinja-cli: update to 2.3.1

--- main/minijinja-cli/template.py
+++ main/minijinja-cli/template.py
@@ -1,5 +1,5 @@
 pkgname = "minijinja-cli"
-pkgver = "2.2.0"
+pkgver = "2.3.1"
 pkgrel = 0
 build_wrksrc = "minijinja-cli"
 build_style = "cargo"
@@ -15,7 +15,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0"
 url = "https://github.com/mitsuhiko/minijinja"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "fcf800a574a864f62e197e11d509bae29ef409877c59214bbd6c42b215012677"
+sha256 = "c933a1d9f53de26bb776a61b9346e0b202c332b4ff1309db5d0178a9b603ca06"
 
 
 def post_install(self):
