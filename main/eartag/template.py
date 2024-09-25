f3788ff3c5bfca9e2413f9101282d1439253eb0a
psykose <alice@ayaya.dev>
2024-09-25 20:20:33 +0200

    main/eartag: update to 0.6.2

--- main/eartag/template.py
+++ main/eartag/template.py
@@ -1,5 +1,5 @@
 pkgname = "eartag"
-pkgver = "0.6.1"
+pkgver = "0.6.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -22,8 +22,8 @@ pkgdesc = "Edit audio file tags"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://apps.gnome.org/EarTag"
-source = f"https://gitlab.gnome.org/World/eartag/-/archive/{pkgver}/eartag-{pkgver}.tar.bz2"
-sha256 = "952d339caf74ed842a13a8039b6d97c0203581d97b1d47dc7da2e094c8f755ae"
+source = f"https://gitlab.gnome.org/World/eartag/-/archive/{pkgver}/eartag-{pkgver}.tar.gz"
+sha256 = "2684a34550c43fda7f42f6ae49feac80ca21e843cd3503012eff58bda17383ed"
 
 
 def post_install(self):
