aa2021b2d17a7e9fcec3af7b8066ff334250d851
psykose <alice@ayaya.dev>
2024-09-25 08:03:51 +0200

    main/libdispatch: update to 6.0.1

--- main/libdispatch/template.py
+++ main/libdispatch/template.py
@@ -1,5 +1,5 @@
 pkgname = "libdispatch"
-pkgver = "6.0"
+pkgver = "6.0.1"
 pkgrel = 0
 build_style = "cmake"
 # these always fail on linux for some reason on musl
@@ -17,7 +17,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://apple.github.io/swift-corelibs-libdispatch"
 source = f"https://github.com/apple/swift-corelibs-libdispatch/archive/refs/tags/swift-{pkgver}-RELEASE.tar.gz"
-sha256 = "fa6fe877cf88391d3faba14147ed5531871b424b28e854534f28e83fc77d2081"
+sha256 = "150066ba341e49b8518a0b879ba82941d6d8734ab37cb76683f2a155369e5030"
 hardening = ["vis", "!cfi"]
 
 
