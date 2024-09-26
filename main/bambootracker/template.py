f4d2e6876bd962947a08017953b8f104a30d4263
Erica Z <zerica@callcc.eu>
2024-09-26 08:43:45 +0200

    main/bambootracker: update to 0.6.4

--- main/bambootracker/template.py
+++ main/bambootracker/template.py
@@ -1,11 +1,10 @@
 pkgname = "bambootracker"
-pkgver = "0.6.3"
+pkgver = "0.6.4"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DSYSTEM_RTAUDIO=ON",
     "-DSYSTEM_RTMIDI=ON",
-    "-DWARNINGS_ARE_ERRORS=OFF",
 ]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = [
@@ -19,6 +18,6 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-2.0-or-later"
 url = "https://bambootracker.github.io/BambooTracker"
 source = f"https://github.com/BambooTracker/BambooTracker/releases/download/v{pkgver}/BambooTracker-src-v{pkgver}.tar.gz"
-sha256 = "75e2e1c0c4c99254de96503aea6da0d7effd84b800f93327fc4d9fc16846ca46"
+sha256 = "fad9f045ff525ad406b25d2f58c23faca64008abf27b1326c33021a8e4436adf"
 # crashes instantly with default stack size
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=0x200000"]}
