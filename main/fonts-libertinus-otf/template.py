9a2475bbdb41cd715f3348d24646d35378bc90a1
psykose <alice@ayaya.dev>
2024-09-27 01:42:28 +0200

    main/fonts-libertinus-otf: update to 7.051

--- main/fonts-libertinus-otf/template.py
+++ main/fonts-libertinus-otf/template.py
@@ -1,12 +1,12 @@
 pkgname = "fonts-libertinus-otf"
-pkgver = "7.050"
+pkgver = "7.051"
 pkgrel = 0
 pkgdesc = "Fonts based on Linux Libertine/Biolinum, with extended math support"
 maintainer = "autumnontape <autumn@cyfox.net>"
 license = "OFL-1.1"
 url = "https://github.com/alerque/libertinus"
 source = f"{url}/releases/download/v{pkgver}/Libertinus-{pkgver}.tar.zst"
-sha256 = "cbb54c4c482376eb17bb6397494489baacff0755d3864f9b5c772e2f3d43d429"
+sha256 = "250677c929d3775a30913643594379af264ac2ef2801035aa1dcbe30b9be23a6"
 
 
 def install(self):
