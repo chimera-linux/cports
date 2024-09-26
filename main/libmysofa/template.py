4dc407d50f6ce5ff07c47447f03672d52a71a95c
triallax <triallax@tutanota.com>
2024-09-26 10:15:26 +0100

    main/libmysofa: run all tests

--- main/libmysofa/template.py
+++ main/libmysofa/template.py
@@ -2,8 +2,11 @@ pkgname = "libmysofa"
 pkgver = "1.3.2"
 pkgrel = 1
 build_style = "cmake"
+# tests fail when run in parallel
+make_check_args = ["-j1"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = ["cunit-devel", "zlib-ng-compat-devel"]
+checkdepends = ["nodejs"]
 pkgdesc = "Reader for AES SOFA files to get better HRTFs"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
