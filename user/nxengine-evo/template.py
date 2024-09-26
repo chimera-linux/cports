7e872cd0c87ce87914faa968b2eb314a54fda445
Tulip Blossom <tulilirockz@outlook.com>
2024-09-26 16:40:36 -0300

    user/nxengine-evo: new package

---
+++ user/nxengine-evo/template.py
@@ -0,0 +1,37 @@
+pkgname = "nxengine-evo"
+# change source on update
+pkgver = "2.6.5.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+]
+makedepends = [
+    "libjpeg-turbo-devel",
+    "libpng-devel",
+    "nlohmann-json",
+    "sdl-devel",
+    "sdl_image-devel",
+    "sdl_mixer-devel",
+    "spdlog-devel",
+    "utfcpp",
+]
+pkgdesc = "Open-source rewrite of the Cave Story engine"
+maintainer = "tulilirockz <tulilirockz@outlook.com>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/nxengine/nxengine-evo"
+source = [
+    f"{url}/archive/refs/tags/v2.6.5-1.tar.gz",
+    f"{url}/releases/download/v2.6.5-1/NXEngine-Evo-v2.6.5-1-Win64.zip",
+]
+source_paths = [".", "win"]
+sha256 = [
+    "db9b78b0c4005959ab8f3a6a05c02d86e764e6593cdd11a2178c581bb03a0699",
+    "b7f8b57e555ac84a75f436100e395719153102b1b8a7238349957b5734dafd0d",
+]
+
+
+def post_install(self):
+    self.uninstall("usr/share/nxengine/data")
+    self.install_files("win/data", "usr/share/nxengine")
