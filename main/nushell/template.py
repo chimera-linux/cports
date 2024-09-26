11ec85070fb5ac72ed647ffedda55a8d229926df
triallax <triallax@tutanota.com>
2024-09-26 17:24:44 +0100

    main/nushell: reenable checks
    libgit2 is now at version 1.8.1

--- main/nushell/template.py
+++ main/nushell/template.py
@@ -3,15 +3,19 @@ pkgver = "0.98.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
-makedepends = ["libgit2-devel", "openssl-devel", "sqlite-devel", "zstd-devel"]
+makedepends = [
+    "libgit2-devel",
+    "openssl-devel",
+    "rust-std",
+    "sqlite-devel",
+    "zstd-devel",
+]
 pkgdesc = "Shell with a focus on structured data"
 maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "MIT"
 url = "https://www.nushell.sh"
 source = f"https://github.com/nushell/nushell/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "c77fd63c4a5f2d35f7dcbb3e9bd76dfaa23acc6bc21fb1de4e7a4a94dc458839"
-# Checks fail with libgit2 < 1.8.1
-options = ["!check"]
 
 
 def post_install(self):
