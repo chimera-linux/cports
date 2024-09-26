95dc9f78dfbfb30ee80715e94de7e28635a22e7b
psykose <alice@ayaya.dev>
2024-09-27 01:48:29 +0200

    main/libspelling: update to 0.4.1

--- main/libspelling/template.py
+++ main/libspelling/template.py
@@ -1,5 +1,5 @@
 pkgname = "libspelling"
-pkgver = "0.4.0"
+pkgver = "0.4.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -29,7 +29,7 @@ maintainer = "GeopJr <evan@geopjr.dev>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libspelling"
 source = f"{url}/-/archive/{pkgver}/libspelling-{pkgver}.tar.gz"
-sha256 = "92ef62dca817c7d6ddbd0b6a0eb4f5ea5725b3211547de9321dcbfe3ef9707d3"
+sha256 = "a6e46bbd8965bc9346bd3fa8edebb3878c2792d60da72bb8a018c97d308da499"
 # introspection
 options = ["!cross"]
 
