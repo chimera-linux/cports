1f5c5b3c12a9b40292ea95d231bf5202aefdf1f2
psykose <alice@ayaya.dev>
2024-09-27 04:04:49 +0200

    main/hwloc: !lintcomp
    how i lost this from rebase, no idea

--- main/hwloc/template.py
+++ main/hwloc/template.py
@@ -20,7 +20,8 @@ url = "https://www.open-mpi.org/projects/hwloc"
 source = f"https://www.open-mpi.org/software/hwloc/v{".".join(pkgver.split(".")[0:2])}/downloads/hwloc-{pkgver}.tar.bz2"
 sha256 = "f7f88fecae067100f1a1a915b658add0f4f71561259482910a69baea22fe8409"
 # can't run them in bwrap with no sysfs
-options = ["!check"]
+# lintcomp: eager preload under different name
+options = ["!check", "!lintcomp"]
 
 
 def post_install(self):
