d229918040a7144d8356f1cba6754fd937716ddf
psykose <alice@ayaya.dev>
2024-09-27 14:49:37 +0200

    main/firmware-sof: update to 2024.09
    drop versions in folders; not required. we had them because old sof had them,
    and then once it didn't apk couldn't replace symlink with dir, but now the
    latter is fixed

--- main/firmware-sof/template.py
+++ main/firmware-sof/template.py
@@ -1,5 +1,5 @@
 pkgname = "firmware-sof"
-pkgver = "2024.06"
+pkgver = "2024.09"
 pkgrel = 0
 archs = ["x86_64"]
 install_if = [self.with_pkgver("base-firmware-sof")]
@@ -8,18 +8,21 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://thesofproject.github.io/latest/index.html"
 source = f"https://github.com/thesofproject/sof-bin/releases/download/v{pkgver}/sof-bin-{pkgver}.tar.gz"
-sha256 = "581ca3285bb56837a8954953f629ebddce644152b673ecd4bbfae1504306d7d6"
+sha256 = "ea47d99f81359008d07618bca103cf78f82d84e940bfe941a28afe07c8cbc620"
 options = ["!strip", "foreignelf"]
 
 
 def install(self):
-    for folder in ["sof", "sof-ace-tplg", "sof-ipc4", "sof-tplg"]:
-        self.install_files(
-            folder, "usr/lib/firmware/intel", name=f"{folder}-v{pkgver}"
-        )
-        self.install_link(
-            f"usr/lib/firmware/intel/{folder}", f"{folder}-v{pkgver}"
-        )
+    for folder in [
+        "sof",
+        "sof-ipc4",
+        "sof-ipc4-lib",
+        "sof-ipc4-tplg",
+        "sof-tplg",
+    ]:
+        self.install_files(folder, "usr/lib/firmware/intel")
+    # compat link, following the default install.sh
+    self.install_link("usr/lib/firmware/intel/sof-ace-tplg", "sof-ipc4-tplg")
     self.install_license("LICENCE.NXP")
     self.install_license("LICENCE.Intel")
 
