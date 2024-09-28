18bf04ece63de96769be21bba42ab4de161e8b7f
Earldridge Jazzed Pineda <earldridgejazzedpineda@gmail.com>
2024-09-28 10:27:53 +0800

    user/adwaita-icon-theme-legacy: new package

---
+++ user/adwaita-icon-theme-legacy/template.py
@@ -0,0 +1,11 @@
+pkgname = "adwaita-icon-theme-legacy"
+pkgver = "46.2"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf", "gtk-update-icon-cache"]
+pkgdesc = "Fullcolor icon theme providing fallback for legacy apps"
+maintainer = "Earldridge Jazzed Pineda <earldridgejazzedpineda@gmail.com>"
+license = "LGPL-3.0-or-later OR CC-BY-SA-3.0"
+url = "https://gitlab.gnome.org/GNOME/adwaita-icon-theme-legacy"
+source = f"$(GNOME_SITE)/adwaita-icon-theme-legacy/{pkgver[:-2]}/adwaita-icon-theme-legacy-{pkgver}.tar.xz"
+sha256 = "548480f58589a54b72d18833b755b15ffbd567e3187249d74e2e1f8f99f22fb4"
