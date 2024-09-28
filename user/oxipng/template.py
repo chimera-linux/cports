e3f87bce47b92f932125c4cebd4e78e01824a60f
Tulip Blossom <tulilirockz@outlook.com>
2024-09-28 19:28:50 -0300

    user/oxipng: new package

---
+++ user/oxipng/template.py
@@ -0,0 +1,16 @@
+pkgname = "oxipng"
+pkgver = "9.1.2"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable"]
+makedepends = ["rust-std"]
+pkgdesc = "Multithreaded PNG optimizer"
+maintainer = "tulilirockz <tulilirockz@outlook.com>"
+license = "MIT"
+url = "https://github.com/shssoichiro/oxipng"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "8eae13e5aa6f500b231b4d15b9fefdeb5f6cc566ddab959b9b7a03a00bb3a520"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
