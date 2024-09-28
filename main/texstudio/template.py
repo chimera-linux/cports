bf038f23976f855a814f3d531c1886c25c1bfdf0
Erica Z <zerica@callcc.eu>
2024-09-28 11:13:49 +0200

    main/texstudio: update to 4.8.4

--- main/texstudio/template.py
+++ main/texstudio/template.py
@@ -1,5 +1,5 @@
 pkgname = "texstudio"
-pkgver = "4.8.3"
+pkgver = "4.8.4"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -17,4 +17,4 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-3.0-or-later"
 url = "https://www.texstudio.org"
 source = f"https://github.com/texstudio-org/texstudio/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "5dc43932272eccda23763b6d28c08df344ce2107ef2fd69734daaeb5a8ad1893"
+sha256 = "aec719cb21b788a41576375eeeba734600a54ff64130dd93f5d0da1efa37414c"
