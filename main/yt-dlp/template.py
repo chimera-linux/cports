443113d13b3b2b23a0a742275e2ba6391f86bc1a
psykose <alice@ayaya.dev>
2024-09-28 09:06:03 +0200

    main/yt-dlp: update to 2024.09.27

--- main/yt-dlp/template.py
+++ main/yt-dlp/template.py
@@ -1,6 +1,6 @@
 pkgname = "yt-dlp"
-pkgver = "2024.08.06"
-pkgrel = 1
+pkgver = "2024.09.27"
+pkgrel = 0
 build_style = "python_pep517"
 make_check_args = ["-k", "not download"]
 hostmakedepends = [
@@ -22,7 +22,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Unlicense"
 url = "https://github.com/yt-dlp/yt-dlp"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "bee43ceee58bdccc1bf48a394073371d3ff2b930d8007cc20a4563bdd027a085"
+sha256 = "2b43e46d96321ad1186f7109c6972a887f55e90dbdcccccc3fdf4a499d8c4c75"
 
 
 @subpackage("yt-dlp-recommends")
