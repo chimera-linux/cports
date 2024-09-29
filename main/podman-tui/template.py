6edae6bdbcfae398f69a87fbcd0cf2e8866e07b8
psykose <alice@ayaya.dev>
2024-09-29 07:06:16 +0200

    main/podman-tui: update to 1.2.2

--- main/podman-tui/template.py
+++ main/podman-tui/template.py
@@ -1,26 +1,29 @@
 pkgname = "podman-tui"
-pkgver = "1.2.1"
-pkgrel = 1
+pkgver = "1.2.2"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 makedepends = [
     "libbtrfs-devel",
     "linux-headers",
+    "sqlite-devel",
 ]
 checkdepends = [
     "gpgme-devel",
     "pkgconf",
 ]
 go_build_tags = [
-    "exclude_graphdriver_devicemapper",
-    "exclude_graphdriver_btrfs",
     "btrfs_noversion",
     "containers_image_openpgp",
+    "exclude_graphdriver_btrfs",
+    "exclude_graphdriver_devicemapper",
+    "libsqlite3",
     "remote",
 ]
+go_check_tags = [*go_build_tags]
 pkgdesc = "Podman terminal UI"
 maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/containers/podman-tui"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "e97fb24ded58d5dccb71fd21221cc2cae25853797ca44e1710baeaf3d5d77b6f"
+sha256 = "97243e025bf8a0ad4b7e87197cc4e608da0af8bb447b1ffd0e01bcb58273b619"
