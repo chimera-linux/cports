71eb5dd583b53317b2d21b706ed30379b6c919c4
psykose <alice@ayaya.dev>
2024-09-30 01:26:54 +0200

    main/scc: update to 3.4.0

--- main/scc/template.py
+++ main/scc/template.py
@@ -1,6 +1,6 @@
 pkgname = "scc"
-pkgver = "3.3.5"
-pkgrel = 4
+pkgver = "3.4.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Fast and accurate code counter"
@@ -8,7 +8,7 @@ maintainer = "Wesley Moore <wes@wezm.net>"
 license = "MIT OR Unlicense"
 url = "https://github.com/boyter/scc"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "028869a7d053879a8e3f2872fdd792f165db13696918d08863475c638f08ef06"
+sha256 = "bdedb6f32d1c3d73ac7e55780021c742bc8ed32f6fb878ee3e419f9acc76bdaa"
 
 
 def post_install(self):
