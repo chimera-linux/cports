cbb0982eeeb72ee4a0f9864809bb777d5a87bf51
psykose <alice@ayaya.dev>
2024-09-30 01:25:33 +0200

    main/wl-kbptr: update to 0.2.2

--- main/wl-kbptr/template.py
+++ main/wl-kbptr/template.py
@@ -1,6 +1,6 @@
 pkgname = "wl-kbptr"
-pkgver = "0.2.1"
-pkgrel = 2
+pkgver = "0.2.2"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
 makedepends = [
@@ -14,7 +14,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/moverest/wl-kbptr"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "c014d8c12613b6267067cacf3f8737acc66514f18c1474de55d3cb3498c96bbe"
+sha256 = "d37524010f777eed0dd5a482b2b30737d8938bfd936140c666861ca212b25ebb"
 hardening = ["vis", "!cfi"]
 
 
