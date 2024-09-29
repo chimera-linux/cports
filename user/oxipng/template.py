98b33bb5d421786db58271895ae56bc47b096690
q66 <q66@chimera-linux.org>
2024-09-29 04:14:33 +0200

    user/oxipng: just rust things

--- user/oxipng/template.py
+++ user/oxipng/template.py
@@ -12,5 +12,10 @@ source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "8eae13e5aa6f500b231b4d15b9fefdeb5f6cc566ddab959b9b7a03a00bb3a520"
 
 
+def post_extract(self):
+    # makes aarch64 run in qemu lol
+    self.rm(".cargo/config.toml")
+
+
 def post_install(self):
     self.install_license("LICENSE")
