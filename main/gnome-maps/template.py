937161d448d5d1db5ab156503e7b7eae12b889bc
q66 <q66@chimera-linux.org>
2024-09-29 03:46:59 +0200

    main/gnome-maps: update to 47.0

--- main/gnome-maps/template.py
+++ main/gnome-maps/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-maps"
-pkgver = "46.11"
+pkgver = "47.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -45,5 +45,5 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later AND CC0-1.0"
 url = "https://apps.gnome.org/Maps"
 source = f"$(GNOME_SITE)/gnome-maps/{pkgver.split('.')[0]}/gnome-maps-{pkgver}.tar.xz"
-sha256 = "940b41b9740b0813325e3916758716cf8fa0ee4e0c9191f260cec06d92135835"
+sha256 = "fb4e27007f75b0015ef6a0761a00768c38b295670b50f7931a466c64c3fb86d5"
 options = ["!cross"]
