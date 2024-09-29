af7a3066c94b235ae559e894a42b3645c7a0d157
q66 <q66@chimera-linux.org>
2024-09-29 03:38:33 +0200

    main/gnome-backgrounds: update to 47.0

--- main/gnome-backgrounds/template.py
+++ main/gnome-backgrounds/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-backgrounds"
-pkgver = "46.0"
+pkgver = "47.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND CC-BY-2.0 AND CC-BY-SA-2.0 AND CC-BY-SA-3.0"
 url = "https://gitlab.gnome.org/GNOME/gnome-backgrounds"
 source = f"$(GNOME_SITE)/gnome-backgrounds/{pkgver[:-2]}/gnome-backgrounds-{pkgver}.tar.xz"
-sha256 = "4ddd3ac439a4a067876805921bb75f4d3c8b85a218d47c276dddde8928443c2e"
+sha256 = "874a4a39c4261736f6a854722833400b612441c4681aa5982d90b15abc9c91fd"
 
 
 @subpackage("gnome-backgrounds-gnome")
