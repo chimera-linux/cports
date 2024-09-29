c1ea121b1ed3bac84348edd336ad837dd2537b68
psykose <alice@ayaya.dev>
2024-09-29 09:24:30 +0200

    main/sysprof: don't install capture lib
    this allows building it with zero deps separately, so it can be used in gtk or
    anywhere without cycles.

--- main/sysprof/template.py
+++ main/sysprof/template.py
@@ -1,12 +1,15 @@
+# sync with main/sysprof-capture
 pkgname = "sysprof"
 pkgver = "47.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
     # creates static separately itself
     "-Ddefault_library=shared",
     "-Dsystemdunitdir=systemd",
     "-Dexamples=false",
+    # in sysprof-capture
+    "-Dinstall-static=false",
 ]
 hostmakedepends = [
     "desktop-file-utils",
@@ -29,7 +32,7 @@ makedepends = [
 ]
 pkgdesc = "System-wide profiler for Linux"
 maintainer = "psykose <alice@ayaya.dev>"
-license = "GPL-2.0-or-later"
+license = "GPL-3.0-or-later AND BSD-2-Clause-Patent"
 url = "https://www.sysprof.com"
 source = f"$(GNOME_SITE)/sysprof/{'.'.join(pkgver.rsplit('.')[:-1])}/sysprof-{pkgver}.tar.xz"
 sha256 = "7424c629434660654288c04248998c357d1ce87ee1559fd44df1980992ef5df5"
@@ -38,16 +41,11 @@ hardening = ["!int"]
 
 
 def post_install(self):
+    self.install_license("src/libsysprof-capture/COPYING")
     self.install_service(self.files_path / "sysprof")
     self.uninstall("usr/systemd")
 
 
-@subpackage("sysprof-devel-static")
-def _(self):
-    return ["usr/lib/*.a"]
-
-
 @subpackage("sysprof-devel")
 def _(self):
-    self.depends += [self.with_pkgver("sysprof-devel-static")]
     return self.default_devel()
