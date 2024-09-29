ad1f65bac48f3a3889204a7fa4b459a152a1f63e
psykose <alice@ayaya.dev>
2024-09-29 04:35:28 +0200

    main/snapshot: more missge

--- main/snapshot/template.py
+++ main/snapshot/template.py
@@ -1,6 +1,6 @@
 pkgname = "snapshot"
 pkgver = "47.0.1"
-pkgrel = 1
+pkgrel = 2
 build_style = "meson"
 hostmakedepends = [
     "appstream",
@@ -21,6 +21,7 @@ makedepends = [
     "rust-std",
 ]
 depends = [
+    "glycin-loaders",
     "gst-plugins-rs-gtk4",
     "gstreamer-libcamera",
     "gstreamer-pipewire",
