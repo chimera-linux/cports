b12c69b31174b55958445e34e00fbaafe4807873
q66 <q66@chimera-linux.org>
2024-09-30 01:49:45 +0200

    main/turnstile: use emptydir + tmpfiles

--- main/turnstile/template.py
+++ main/turnstile/template.py
@@ -1,6 +1,6 @@
 pkgname = "turnstile"
 pkgver = "0.1.10"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = ["-Dmanage_rundir=true", "-Dpamdir=/usr/lib/pam.d"]
 hostmakedepends = ["meson", "pkgconf", "scdoc"]
@@ -12,16 +12,16 @@ license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/turnstile"
 source = f"https://github.com/chimera-linux/turnstile/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "186adf1402f3c63eecdbed241145cb029b7b1268c701a87381522d5b64583f2d"
+file_modes = {
+    "+usr/lib/dinit.d/user/boot.d": ("root", "root", 0o755, True),
+}
 hardening = ["vis", "cfi"]
 options = ["brokenlinks", "!splitdinit"]
 
 
 def post_install(self):
     self.install_license("COPYING.md")
-    # just make sure it exists
-    self.install_dir("usr/lib/dinit.d/user/boot.d", empty=True)
-    # linger
-    self.install_dir("var/lib/turnstiled/linger", empty=True)
+    self.install_tmpfiles(self.files_path / "tmpfiles.conf")
     # also default systemwide link
     self.install_dir("usr/lib/dinit.d/boot.d")
     self.install_link("usr/lib/dinit.d/boot.d/turnstiled", "../turnstiled")
