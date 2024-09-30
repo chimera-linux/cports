de2a44d5d69d8ebd5ee6e2927e1ec1de1f3730b2
psykose <alice@ayaya.dev>
2024-09-30 10:02:35 +0200

    main/python-pyasn1: update to 0.6.1

--- main/python-pyasn1/template.py
+++ main/python-pyasn1/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-pyasn1"
-pkgver = "0.6.0"
+pkgver = "0.6.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python"]
 checkdepends = ["python-pytest"]
@@ -15,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://pyasn1.readthedocs.io/en/latest/contents.html"
 source = f"$(PYPI_SITE)/p/pyasn1/pyasn1-{pkgver}.tar.gz"
-sha256 = "3a35ab2c4b5ef98e17dfdec8ab074046fbda76e281c5a706ccd82328cfc8f64c"
+sha256 = "6f580d2bdd84365380830acf45550f2511469f673cb4a5ae3857a3170128b034"
 
 
 def post_install(self):
