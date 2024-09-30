d2a8d6243b899640b78fa05f822306bd989404d0
psykose <alice@ayaya.dev>
2024-09-30 09:34:10 +0200

    main/mbuffer: tiny cleanup

--- main/mbuffer/template.py
+++ main/mbuffer/template.py
@@ -1,9 +1,10 @@
 pkgname = "mbuffer"
 pkgver = "20240929"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 make_dir = "."
-hostmakedepends = ["cmake", "ninja", "bash", "linux-headers"]
+hostmakedepends = ["cmake", "ninja"]
+makedepends = ["linux-headers"]
 pkgdesc = "Tool for buffering data streams"
 maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-3.0-only"
@@ -13,4 +14,4 @@ sha256 = "efb6c6de3e2459d2398774cdd44ec0a6e6b88c4132ede43d3d5e2f6c18d9a6a7"
 
 
 def pre_configure(self):
-    self.do("bash", "mkversion.sh")
+    self.do("sh", "mkversion.sh")
