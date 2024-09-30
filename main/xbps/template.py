e75d87fa17aaf4354f39c226ede891c019a97659
q66 <q66@chimera-linux.org>
2024-09-30 03:19:45 +0200

    main/xbps: remove void repos/keys and kyua tests

--- main/xbps/template.py
+++ main/xbps/template.py
@@ -1,6 +1,6 @@
 pkgname = "xbps"
 pkgver = "0.59.2"
-pkgrel = 1
+pkgrel = 2
 build_style = "configure"
 configure_args = [
     "--prefix=/usr",
@@ -17,6 +17,8 @@ license = "BSD-2-Clause AND BSD-3-Clause AND MIT"
 url = "https://github.com/void-linux/xbps"
 source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "a6607e83fcd654a0ae846d729e43fefd8da9a61323e91430f884caf895b4f59b"
+# one bashcomp for all
+options = ["!lintcomp"]
 
 
 def init_check(self):
@@ -26,6 +28,11 @@ def init_check(self):
 def post_install(self):
     self.install_license("LICENSE")
     self.install_license("LICENSE.3RDPARTY")
+    # lol kyua
+    self.uninstall("usr/tests")
+    # this xpbs is only for xbps-src and bootstrapping
+    self.uninstall("usr/share/xbps.d")
+    self.uninstall("var/db/xbps")
 
 
 @subpackage("xbps-devel")
