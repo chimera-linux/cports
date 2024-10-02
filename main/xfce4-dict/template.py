5e4135401c24e0752511dee29ae67e9e1a264b39
triallax <triallax@tutanota.com>
2024-10-02 10:26:47 +0100

    main/xfce4-dict: update to 0.8.8

--- main/xfce4-dict/template.py
+++ main/xfce4-dict/template.py
@@ -1,5 +1,5 @@
 pkgname = "xfce4-dict"
-pkgver = "0.8.7"
+pkgver = "0.8.8"
 pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
@@ -26,4 +26,4 @@ url = "https://docs.xfce.org/apps/xfce4-dict/start"
 source = (
     f"$(XFCE_SITE)/apps/xfce4-dict/{pkgver[:-2]}/xfce4-dict-{pkgver}.tar.bz2"
 )
-sha256 = "a44316fcf45c7616e430a68d822601e779e75d52f00a9047c77a0416f73fd43d"
+sha256 = "0d0cb079d276d82c174a52c94ee4df12df490a525befc44650e261f8e5eae916"
