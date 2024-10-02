3d6e2396b98d4ef45798664233c9d07e22ac3d3f
psykose <alice@ayaya.dev>
2024-10-02 05:43:52 +0200

    main/xkeyboard-config: update to 2.43

--- main/xkeyboard-config/template.py
+++ main/xkeyboard-config/template.py
@@ -1,5 +1,5 @@
 pkgname = "xkeyboard-config"
-pkgver = "2.42"
+pkgver = "2.43"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dxorg-rules-symlinks=true", "-Dcompat-rules=true"]
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://www.freedesktop.org/wiki/Software/XKeyboardConfig"
 source = f"$(XORG_SITE)/data/xkeyboard-config/xkeyboard-config-{pkgver}.tar.xz"
-sha256 = "a6b06ebfc1f01fc505f2f05f265f95f67cc8873a54dd247e3c2d754b8f7e0807"
+sha256 = "c810f362c82a834ee89da81e34cd1452c99789339f46f6037f4b9e227dd06c01"
 hardening = ["vis", "cfi"]
 
 
