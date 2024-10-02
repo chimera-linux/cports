bf71d9c5dd564c5fdf92dd78de323493689b638e
triallax <triallax@tutanota.com>
2024-10-03 00:25:52 +0100

    main/magic-wormhole-transit-relay: update to 0.3.1
    Closes #3037

--- main/magic-wormhole-transit-relay/template.py
+++ main/magic-wormhole-transit-relay/template.py
@@ -1,5 +1,5 @@
 pkgname = "magic-wormhole-transit-relay"
-pkgver = "0.2.1"
+pkgver = "0.3.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -7,14 +7,14 @@ hostmakedepends = [
     "python-installer",
     "python-setuptools",
 ]
-depends = ["python-twisted"]
+depends = ["python-autobahn", "python-twisted"]
 checkdepends = ["python-mock", "python-pytest", *depends]
 pkgdesc = "Transit relay server for Magic Wormhole"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "MIT"
 url = "https://github.com/magic-wormhole/magic-wormhole-transit-relay"
 source = f"$(PYPI_SITE)/m/magic-wormhole-transit-relay/magic-wormhole-transit-relay-{pkgver}.tar.gz"
-sha256 = "cb4801b46890eaff97286e0e3fec62d1d52ffe317d140083b6336a1fb4e8fa5e"
+sha256 = "2ef2efbe4d34f0e624870f842259fd733ba77152ed310af4349774a62884900e"
 
 
 def post_install(self):
