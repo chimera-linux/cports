6ea3d094f24c03c736ab9a81e533858c00d06f7b
psykose <alice@ayaya.dev>
2024-10-03 12:51:07 +0200

    main/python-dbusmock: update to 0.32.2

--- main/python-dbusmock/template.py
+++ main/python-dbusmock/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-dbusmock"
-pkgver = "0.32.1"
+pkgver = "0.32.2"
 pkgrel = 0
 build_style = "python_pep517"
 # needs upower
@@ -17,4 +17,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-3.0-or-later"
 url = "https://github.com/martinpitt/python-dbusmock"
 source = f"https://github.com/martinpitt/python-dbusmock/releases/download/{pkgver}/dist.python-dbusmock-{pkgver}.tar.gz"
-sha256 = "f02c34493d420b3cf2d8d341b79a60841593b7b7615adc3ece17ff86f8275924"
+sha256 = "357c269d53a5bb3f87983bec7ff700a0c4d071481f7cf7221ec8376224fc4316"
