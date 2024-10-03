f138b286691832ae8d58e76490f368a9522df56e
psykose <alice@ayaya.dev>
2024-10-03 12:47:19 +0200

    main/eza: update to 0.20.1

--- main/eza/template.py
+++ main/eza/template.py
@@ -1,5 +1,5 @@
 pkgname = "eza"
-pkgver = "0.20.0"
+pkgver = "0.20.1"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -15,7 +15,7 @@ url = "https://eza.rocks"
 source = (
     f"https://github.com/eza-community/eza/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "e6c058b13aecbed9f037c0607f0df19bc0a3532fea14dacd0090878ed4bbfadc"
+sha256 = "e78a84cc5324ebb6481293d32edfdbc7de78511d5190b4808a0896f8ce4d652e"
 
 
 def post_install(self):
