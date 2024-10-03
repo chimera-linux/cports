a10bd7eb188422d5ff5f222dad2d60ad3bc60230
psykose <alice@ayaya.dev>
2024-10-03 03:32:30 +0200

    main/python-setuptools-rust: update to 1.10.2

--- main/python-setuptools-rust/template.py
+++ main/python-setuptools-rust/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-setuptools-rust"
-pkgver = "1.10.1"
+pkgver = "1.10.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools_scm",
-    "python-wheel",
 ]
 depends = [
     "python-semantic_version",
@@ -17,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/PyO3/setuptools-rust"
 source = f"$(PYPI_SITE)/s/setuptools-rust/setuptools_rust-{pkgver}.tar.gz"
-sha256 = "d79035fc54cdf9342e9edf4b009491ecab06c3a652b37c3c137c7ba85547d3e6"
+sha256 = "5d73e7eee5f87a6417285b617c97088a7c20d1a70fcea60e3bdc94ff567c29dc"
 # unpackaged checkdepends
 options = ["!check"]
 
