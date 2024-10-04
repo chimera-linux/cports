0a8db473bba71744119eca81d3cbaaa3f0f6834d
psykose <alice@ayaya.dev>
2024-10-04 09:25:19 +0200

    main/fonts-noto-emoji-ttf: update to 2.047

--- main/fonts-noto-emoji-ttf/template.py
+++ main/fonts-noto-emoji-ttf/template.py
@@ -1,12 +1,12 @@
 pkgname = "fonts-noto-emoji-ttf"
-pkgver = "2.042"
-pkgrel = 1
+pkgver = "2.047"
+pkgrel = 0
 pkgdesc = "Google Noto emoji fonts"
 maintainer = "GeopJr <evan@geopjr.dev>"
 license = "OFL-1.1"
 url = "https://github.com/googlefonts/noto-emoji"
 source = f"{url}/archive/v{pkgver}/font-noto-emoji-{pkgver}.tar.gz"
-sha256 = "b56bd2fa4029d0f057b66b742ac59af243dbc91067fed3eb4929dac762440fc9"
+sha256 = "2cfaf5a427eb26334cdb30d98e4a0c005b660504a339249dc54373e566f09b50"
 # No copyright header in license text
 options = ["!distlicense"]
 
