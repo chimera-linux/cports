9374ec3fdd1cde2d6f4fb3b55b454e44e433d312
psykose <alice@ayaya.dev>
2024-10-04 09:30:31 +0200

    user/lego: update to 4.19.0

--- user/lego/template.py
+++ user/lego/template.py
@@ -1,6 +1,6 @@
 pkgname = "lego"
-pkgver = "4.18.0"
-pkgrel = 1
+pkgver = "4.19.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["-ldflags", f"-X main.version={pkgver}", "./cmd/lego"]
 hostmakedepends = ["go"]
@@ -9,7 +9,7 @@ maintainer = "Wesley Moore <wes@wezm.net>"
 license = "MIT"
 url = "https://github.com/go-acme/lego"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "850bc2db37c3ce33837e5c9f18b2b8f1b2a45c07fc6ef13a4539f8f871a1aad3"
+sha256 = "3a880f4351fbf023196469ee7dc914f953d59617f5abf7d816f0c809dfb71bb0"
 # check: tests need network access: https://github.com/go-acme/lego/issues/560
 options = ["!check"]
 
