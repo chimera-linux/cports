f5fac1c6fd3ad609d56fc5f4f71576919795a96f
ttyyls <contact@behri.org>
2024-10-05 18:57:58 +0100

    main/fzf: install fzf-tmux

--- main/fzf/template.py
+++ main/fzf/template.py
@@ -1,6 +1,6 @@
 pkgname = "fzf"
 pkgver = "0.56.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "go"
 hostmakedepends = ["go"]
 makedepends = ["ncurses-devel"]
@@ -17,9 +17,21 @@ def post_install(self):
     self.install_man("man/man1/fzf.1")
     self.install_file("plugin/fzf.vim", "usr/share/vim/vimfiles/plugin")
     self.install_file("plugin/fzf.vim", "usr/share/nvim/runtime/plugin")
+    self.install_bin("bin/fzf-tmux")
+    self.install_man("man/man1/fzf-tmux.1")
 
     with self.pushd("shell"):
         self.install_completion("completion.bash", "bash")
         self.install_completion("completion.zsh", "zsh")
+
         for ext in ["bash", "fish", "zsh"]:
             self.install_file(f"key-bindings.{ext}", "usr/share/fzf")
+
+
+@subpackage("fzf-tmux")
+def _(self):
+    self.subdesc = "tmux integration script"
+    self.depends = [self.parent, "bash", "tmux"]
+    self.install_if = [self.parent, "bash", "tmux"]
+
+    return ["cmd:fzf-tmux"]
