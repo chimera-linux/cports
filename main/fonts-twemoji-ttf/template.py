360f7e0bfba302cb50ed2f924f2072503894cd08
psykose <alice@ayaya.dev>
2024-10-05 07:52:09 +0200

    main/fonts-twemoji-ttf: update to 15.1.0

--- main/fonts-twemoji-ttf/template.py
+++ main/fonts-twemoji-ttf/template.py
@@ -1,12 +1,12 @@
 pkgname = "fonts-twemoji-ttf"
-pkgver = "15.0.3"
+pkgver = "15.1.0"
 pkgrel = 0
 pkgdesc = "Twitter Color Emoji SVGinOT Font"
 maintainer = "Val Packett <val@packett.cool>"
 license = "CC-BY-4.0"
 url = "https://github.com/13rac1/twemoji-color-font"
 source = f"{url}/releases/download/v{pkgver}/TwitterColorEmoji-SVGinOT-Linux-{pkgver}.tar.gz"
-sha256 = "de6a5cb90dc1684c8f98230eadf58b0385c4f5491f6c082823017d9ca1d3ec7a"
+sha256 = "c8a5302ee4e4c2188ce785edd84c50c616a07f6e99fe1b91aecba4e1db341295"
 
 
 def install(self):
