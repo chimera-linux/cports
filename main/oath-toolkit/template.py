8b25084556601c1536ba7e74010647596b5c2cbe
q66 <q66@chimera-linux.org>
2024-10-05 17:09:01 +0200

    main/oath-toolkit: update to 2.6.12

--- main/oath-toolkit/template.py
+++ main/oath-toolkit/template.py
@@ -1,5 +1,5 @@
 pkgname = "oath-toolkit"
-pkgver = "2.6.11"
+pkgver = "2.6.12"
 pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
@@ -16,7 +16,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "GPL-3.0-or-later"
 url = "http://www.nongnu.org/oath-toolkit"
 source = f"http://download.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-{pkgver}.tar.gz"
-sha256 = "fc512a4a5b46f4c43ab0586c3189fece4d54f7e649397d6fa1e23428431e2cb4"
+sha256 = "cafdf739b1ec4b276441c6aedae6411434bbd870071f66154b909cc6e2d9e8ba"
 # TODO: failing checks are yet to be investigated
 options = ["!check"]
 
