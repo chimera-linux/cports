77992f7ac6c769c1084dba401ee78eac71d6e862
psykose <alice@ayaya.dev>
2024-10-06 20:16:23 +0200

    main/tree-sitter: update to 0.24.2

--- main/tree-sitter/template.py
+++ main/tree-sitter/template.py
@@ -1,6 +1,6 @@
 pkgname = "tree-sitter"
 # match to tree-sitter-cli
-pkgver = "0.24.1"
+pkgver = "0.24.2"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "yopito <pierre.bourgin@free.fr>"
 license = "MIT"
 url = "https://tree-sitter.github.io/tree-sitter"
 source = f"https://github.com/tree-sitter/tree-sitter/archive/v{pkgver}.tar.gz"
-sha256 = "7adb5bb3b3c2c4f4fdc980a9a13df8fbf3526a82b5c37dd9cf2ed29de56a4683"
+sha256 = "199da041ac7ef62bccdda9b7bec28aafa073f7eea2677680aa7992d503c9cc64"
 # check requires cargo/fixture stuff (from remote repositories)
 options = ["!check"]
 
