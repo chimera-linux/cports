f5db3300bf4d5941a196bd0973620002e2092600
psykose <alice@ayaya.dev>
2024-10-08 17:36:27 +0200

    main/gnome-bluetooth: update to 47.1

--- main/gnome-bluetooth/template.py
+++ main/gnome-bluetooth/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-bluetooth"
-pkgver = "47.0"
+pkgver = "47.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -30,7 +30,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Projects/GnomeBluetooth"
 source = f"$(GNOME_SITE)/gnome-bluetooth/{pkgver[:-2]}/gnome-bluetooth-{pkgver}.tar.xz"
-sha256 = "7aa406d3cb71152f525bdbc9788e11816204408197b6411c6ee25554bc4cb673"
+sha256 = "03e3e7403a15108ffc1496210a1da5c2961b2834a5c07eccc7a3f493195daba3"
 options = ["!cross"]
 
 
