c6a4f1b452ce2f889e7bfb601788df8344708d45
Erica Z <zerica@callcc.eu>
2024-10-08 08:59:36 +0200

    main/msmtp: update to 1.8.27

--- main/msmtp/template.py
+++ main/msmtp/template.py
@@ -1,15 +1,24 @@
 pkgname = "msmtp"
-pkgver = "1.8.26"
+pkgver = "1.8.27"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["automake", "gettext-devel", "pkgconf"]
-makedepends = ["gnutls-devel", "libsecret-devel", "libidn2-devel"]
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "pkgconf",
+]
+makedepends = [
+    "gnutls-devel",
+    "libidn2-devel",
+    "libsecret-devel",
+]
+checkdepends = ["bash"]
 pkgdesc = "SMTP client"
 maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-3.0-or-later"
 url = "https://marlam.de/msmtp"
 source = f"{url}/releases/msmtp-{pkgver}.tar.xz"
-sha256 = "6cfc488344cef189267e60aea481f00d4c7e2a59b53c6c659c520a4d121f66d8"
+sha256 = "94030580a63a747faa0a3b9b1b264ae355aad33a4d94b832bfeb5b21633c965e"
 hardening = ["vis", "cfi"]
 
 
