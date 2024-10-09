cd9332e73ebf7199334f7e8fb1891099fac363db
psykose <alice@ayaya.dev>
2024-10-09 13:12:53 +0200

    main/perl-uri: update to 5.30

--- main/perl-uri/template.py
+++ main/perl-uri/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-uri"
-pkgver = "5.29"
+pkgver = "5.30"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["perl"]
@@ -8,8 +8,8 @@ depends = ["perl"]
 pkgdesc = "Perl Uniform Resource Identifiers module"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
-url = "https://metacpan.org/release/URI"
+url = "https://metacpan.org/pod/URI"
 source = f"$(CPAN_SITE)/URI/URI-{pkgver}.tar.gz"
-sha256 = "a34b9f626c3ff1e20c0d4a23ec5c8b7ae1de1fb674ecefed7e46791388137372"
+sha256 = "be366cf5d923e2c0d63e5fe5f707f614a144020961a2a0fe0b2c922f5fb80a95"
 # missing checkdepends
 options = ["!check"]
