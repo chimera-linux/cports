709b3e725d37ea0c118291a6f7e678330e1643fd
psykose <alice@ayaya.dev>
2024-10-09 13:18:46 +0200

    main/ansible-core: update to 2.17.5

--- main/ansible-core/template.py
+++ main/ansible-core/template.py
@@ -1,6 +1,6 @@
 pkgname = "ansible-core"
-pkgver = "2.17.4"
-pkgrel = 1
+pkgver = "2.17.5"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -29,7 +29,7 @@ maintainer = "Mara <177581589+catgirlconspiracy@users.noreply.github.com>"
 license = "GPL-3.0-or-later"
 url = "https://ansible.com"
 source = f"$(PYPI_SITE)/a/ansible-core/ansible_core-{pkgver}.tar.gz"
-sha256 = "44a1f30076796536ba2455cad18d36e62870f04e632e3ca2ebe970d7beacf24d"
+sha256 = "ae7f51fd13dc9d57c9bcd43ef23f9c255ca8f18f4b5c0011a4f9b724d92c5a8e"
 
 
 def check(self):
