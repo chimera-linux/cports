2917940922f3315d8ec33960d286e338ae5b8e37
psykose <alice@ayaya.dev>
2024-10-09 13:06:35 +0200

    main/python-sphinx_rtd_theme: update to 3.0.0

--- main/python-sphinx_rtd_theme/template.py
+++ main/python-sphinx_rtd_theme/template.py
@@ -1,20 +1,19 @@
 pkgname = "python-sphinx_rtd_theme"
-pkgver = "2.0.0"
-pkgrel = 1
+pkgver = "3.0.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
-    "python-setuptools",
     "python-build",
     "python-installer",
-    "python-wheel",
+    "python-setuptools",
 ]
 depends = ["python", "python-sphinxcontrib-jquery"]
 pkgdesc = "Sphinx theme for readthedocs.org"
 maintainer = "eater <=@eater.me>"
 license = "MIT"
 url = "https://github.com/readthedocs/sphinx_rtd_theme"
-source = f"https://github.com/readthedocs/sphinx_rtd_theme/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "40446e6789dd34deb4e9814e379bae0aa74057b6fb43de4b343a48c84fc0f8db"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "6de403ec3f0a07de4728cf940d1a2c83fd92fa1786889b3617716774591032ef"
 # No tests available
 options = ["!check"]
 
