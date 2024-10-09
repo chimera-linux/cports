bd1c6525d6f6e609672a8814deefb5c01b78e9fb
psykose <alice@ayaya.dev>
2024-10-09 13:09:06 +0200

    main/python-s3transfer: update to 0.10.3

--- main/python-s3transfer/template.py
+++ main/python-s3transfer/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-s3transfer"
-pkgver = "0.10.2"
+pkgver = "0.10.3"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
@@ -19,7 +19,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/boto/s3transfer"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "72286cce808b1f7f55b38af9657cc8e6ddc31ad0a99f092601ef539f50344060"
+sha256 = "f333eeee0376ee88a55f6306883b73f671d7a62a8acf0eb3679c4e1fe8c66796"
 
 
 def init_check(self):
