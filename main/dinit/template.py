1f77a9db254773a7f8f37a84b8027b5fbcbde834
q66 <q66@chimera-linux.org>
2024-10-09 12:36:16 +0200

    main/dinit: update to git master
    This is a temporary measure to get our new features in. The most
    visible one is services with arguments (svcname@argvalue) for
    parameterized services. The other stuff is related to being able
    to listen to environment changes so we can implement autotriggered
    graphical user services.
    
    It's only a few commits on top of 0.19 so let's pretend it's still
    0.19.

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -1,6 +1,8 @@
 pkgname = "dinit"
-pkgver = "0.18.0"
-pkgrel = 1
+pkgver = "0.19.0"
+# temporary so we get our features
+_gitrev = "92cb58eedaf930fed60d17b6247c1f2155c78ec8"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--syscontrolsocket=/run/dinitctl"]
 configure_gen = []
@@ -10,7 +12,7 @@ pkgdesc = "Service manager and init system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://davmac.org/projects/dinit"
-source = f"https://github.com/davmac314/dinit/archive/v{pkgver}.tar.gz"
-sha256 = "ec854903e93416b3f65e72009dcde4965869d8793e2314565484d94ede534e48"
+source = f"https://github.com/davmac314/dinit/archive/{_gitrev}.tar.gz"
+sha256 = "dba72f3ca0d8cbd50d5f36f49aad4c1eaad8b30a66fac3abd6f8785cafb00e73"
 tool_flags = {"CXXFLAGS": ["-fno-rtti"]}
 hardening = ["vis", "cfi"]
