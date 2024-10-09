436a479c854c53d7dfd3a02ffdaac33c8cfa12e6
psykose <alice@ayaya.dev>
2024-10-09 13:11:54 +0200

    main/wasm-component-ld: update to 0.5.10

--- main/wasm-component-ld/template.py
+++ main/wasm-component-ld/template.py
@@ -1,5 +1,5 @@
 pkgname = "wasm-component-ld"
-pkgver = "0.5.9"
+pkgver = "0.5.10"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -11,7 +11,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0 WITH LLVM-exception OR Apache-2.0 OR MIT"
 url = "https://github.com/bytecodealliance/wasm-component-ld"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "01da262f61e65f97b07af8231008257b8391c1f29382286b77cdd4240418ea36"
+sha256 = "aa049a93da595e4dacf742865f13e7fb1cec1ab47de9258f0a11d81ad6c7a77b"
 
 
 def post_install(self):
