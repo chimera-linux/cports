dac50610632b1ea5f4f17b87d13835f52d504c27
psykose <alice@ayaya.dev>
2024-10-10 10:44:56 +0200

    main/python-zope.interface: update to 7.1.0

--- main/python-zope.interface/template.py
+++ main/python-zope.interface/template.py
@@ -1,7 +1,9 @@
 pkgname = "python-zope.interface"
-pkgver = "7.0.3"
+pkgver = "7.1.0"
 pkgrel = 0
 build_style = "python_pep517"
+# useless setuptools version check
+make_build_args = ["--skip-dependency-check"]
 hostmakedepends = [
     "python-build",
     "python-installer",
@@ -13,8 +15,8 @@ pkgdesc = "Zope interfaces for Python"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "ZPL-2.1"
 url = "https://github.com/zopefoundation/zope.interface"
-source = f"$(PYPI_SITE)/z/zope.interface/zope.interface-{pkgver}.tar.gz"
-sha256 = "cd2690d4b08ec9eaf47a85914fe513062b20da78d10d6d789a792c0b20307fb1"
+source = f"$(PYPI_SITE)/z/zope.interface/zope_interface-{pkgver}.tar.gz"
+sha256 = "3f005869a1a05e368965adb2075f97f8ee9a26c61898a9e52a9764d93774f237"
 # not functional yet
 options = ["!check"]
 
