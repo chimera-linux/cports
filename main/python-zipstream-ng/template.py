89d25f3b4058330e06c244277d94088c21bd81e8
psykose <alice@ayaya.dev>
2024-10-10 10:45:51 +0200

    main/python-zipstream-ng: update to 1.8.0

--- main/python-zipstream-ng/template.py
+++ main/python-zipstream-ng/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-zipstream-ng"
-pkgver = "1.7.1"
+pkgver = "1.8.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,5 +13,5 @@ pkgdesc = "Streamable zip file generator"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "LGPL-3.0-only"
 url = "https://github.com/pR0Ps/zipstream-ng"
-source = f"$(PYPI_SITE)/z/zipstream-ng/zipstream-ng-{pkgver}.tar.gz"
-sha256 = "f92023b9ca578cd7fdd94ec733c65664ecf7ee32493e38cdf8e365a1316e9ffc"
+source = f"$(PYPI_SITE)/z/zipstream-ng/zipstream_ng-{pkgver}.tar.gz"
+sha256 = "b7129d2c15d26934b3e1cb22256593b6bdbd03c553c26f4199a5bf05110642bc"
