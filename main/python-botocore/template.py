860f6687a1a8630943fa874b14ba41e4b2003b8f
psykose <alice@ayaya.dev>
2024-10-10 10:43:34 +0200

    main/python-botocore: update to 1.35.37

--- main/python-botocore/template.py
+++ main/python-botocore/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-botocore"
-pkgver = "1.35.32"
+pkgver = "1.35.37"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -19,6 +19,6 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/boto/botocore"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "4f53dfc2d968e3fa9e92a101cdbc822bd055e579fea34b1644429c527089d17a"
+sha256 = "765626b3180345f5e86f84a45d4b3231c9bb330b222d7a8448602c5731e7768b"
 # takes forever
 options = ["!check"]
