81f40bcc00d092c4ebdf4f3dce89aebef5b304a2
psykose <alice@ayaya.dev>
2024-10-10 10:45:12 +0200

    main/python-sphinx_rtd_theme: update to 3.0.1

--- main/python-sphinx_rtd_theme/template.py
+++ main/python-sphinx_rtd_theme/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-sphinx_rtd_theme"
-pkgver = "3.0.0"
+pkgver = "3.0.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ maintainer = "eater <=@eater.me>"
 license = "MIT"
 url = "https://github.com/readthedocs/sphinx_rtd_theme"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "6de403ec3f0a07de4728cf940d1a2c83fd92fa1786889b3617716774591032ef"
+sha256 = "588b3d1448be4dece8d85bbdd8f0f7de703e8b49011e052d676c609dcd3374cd"
 # No tests available
 options = ["!check"]
 
