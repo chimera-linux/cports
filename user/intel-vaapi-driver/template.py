8d248306b641cac4e77395a4ea1c57bf915560f3
psykose <alice@ayaya.dev>
2024-10-11 13:47:31 +0200

    user/intel-vaapi-driver: new package
    closes https://github.com/chimera-linux/cports/issues/3068

---
+++ user/intel-vaapi-driver/template.py
@@ -0,0 +1,16 @@
+pkgname = "intel-vaapi-driver"
+pkgver = "2.4.1"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
+makedepends = ["libdrm-devel", "libva-devel", "libx11-devel"]
+pkgdesc = "VA-API driver for Haswell and older Intel GPUs"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://github.com/intel/intel-vaapi-driver"
+source = f"{url}/releases/download/{pkgver}/intel-vaapi-driver-{pkgver}.tar.bz2"
+sha256 = "0081fce08eb3a83f7d99c3b853c8fdfa0af437b8f5b0fb7c66faeb83bcbe0c19"
+
+
+def post_install(self):
+    self.install_license("COPYING")
