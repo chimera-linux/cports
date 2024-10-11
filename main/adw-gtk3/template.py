d1cdfca3be9440fd1839bd62806707145cf85d36
psykose <alice@ayaya.dev>
2024-10-11 14:14:03 +0200

    main/adw-gtk3: update to 5.5

--- main/adw-gtk3/template.py
+++ main/adw-gtk3/template.py
@@ -1,5 +1,5 @@
 pkgname = "adw-gtk3"
-pkgver = "5.4"
+pkgver = "5.5"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -10,5 +10,5 @@ pkgdesc = "Libadwaita's theme for gtk3"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lassekongo83/adw-gtk3"
-source = f"https://github.com/lassekongo83/adw-gtk3/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1e3c7d6517c254ad7cc233274b1ded600db50f52c11f96d7000c098f9e9c0f9e"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "02433c9ef67267776e0a4c822e28b6810063a68c1ecf2f17f0936a9c45a20b9b"
