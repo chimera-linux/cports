636b269f35691d15b8fe0d50d0b55c9c1ce3cb7a
psykose <alice@ayaya.dev>
2024-10-11 21:39:12 +0200

    main/gnome-firmware: new package

---
+++ main/gnome-firmware/template.py
@@ -0,0 +1,26 @@
+pkgname = "gnome-firmware"
+pkgver = "47.0"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Delogind=true", "-Dconsolekit=false", "-Dsystemd=false"]
+hostmakedepends = [
+    "desktop-file-utils",
+    "gettext",
+    "gobject-introspection",
+    "help2man",
+    "meson",
+    "pkgconf",
+]
+makedepends = [
+    "elogind-devel",
+    "libadwaita-devel",
+    "libxmlb-devel",
+    "fwupd-devel",
+]
+pkgdesc = "GNOME firmware updater"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-or-later"
+url = "https://gitlab.gnome.org/World/gnome-firmware"
+source = f"{url}/-/archive/{pkgver}.tar.gz"
+sha256 = "b39534a0aab3c0d8b8c27152af7a02b7f7c8082d87b9d5cf8ebdd9b520f03bb3"
+options = ["!cross"]
