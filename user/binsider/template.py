7a5d0d2d56afb1061057d2e63f729a25f61984f0
psykose <alice@ayaya.dev>
2024-10-11 14:24:31 +0200

    user/binsider: update to 0.2.1

--- user/binsider/template.py
+++ user/binsider/template.py
@@ -1,5 +1,5 @@
 pkgname = "binsider"
-pkgver = "0.2.0"
+pkgver = "0.2.1"
 pkgrel = 0
 # inline asm for ppc64* needs nightly
 # ppc64*/riscv64/aarch64 needs a new nix crate release for ptrace::getregs
@@ -13,7 +13,7 @@ maintainer = "tulilirockz <tulilirockz@outlook.com>"
 license = "Apache-2.0 OR MIT"
 url = "https://binsider.dev"
 source = f"https://github.com/orhun/binsider/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f6792950c77795485414a4e82fce7898debed271a4d6fc6e509dc9bfe7879e72"
+sha256 = "a3bc10379677b19656436924f478798f205b371bc703feab938c1b2512f39500"
 
 
 def post_install(self):
