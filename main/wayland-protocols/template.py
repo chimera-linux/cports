0e868b7824be63e73ac9b609557fcb5eb9fcb30a
psykose <alice@ayaya.dev>
2024-10-12 15:48:47 +0200

    main/wayland-protocols: update to 1.38

--- main/wayland-protocols/template.py
+++ main/wayland-protocols/template.py
@@ -1,5 +1,5 @@
 pkgname = "wayland-protocols"
-pkgver = "1.37"
+pkgver = "1.38"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "wayland-progs"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://wayland.freedesktop.org"
 source = f"https://gitlab.freedesktop.org/wayland/wayland-protocols/-/archive/{pkgver}/wayland-protocols-{pkgver}.tar.gz"
-sha256 = "c3b215084eb4cf318415533554c2c2714e58ed75847d7c3a8e50923215ffbbf3"
+sha256 = "a6069948458a1d86cea2b33a9735e67d7524118c32c388d75efb881a9e9d2cd9"
 
 
 def post_install(self):
