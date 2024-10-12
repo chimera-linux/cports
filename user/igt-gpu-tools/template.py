9d6ac1f4ed2b3705793b40a497be6112ea4416d2
psykose <alice@ayaya.dev>
2024-10-12 15:46:36 +0200

    user/igt-gpu-tools: new package
    closes https://github.com/chimera-linux/cports/issues/3069

---
+++ user/igt-gpu-tools/template.py
@@ -0,0 +1,37 @@
+pkgname = "igt-gpu-tools"
+pkgver = "1.29"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dtests=disabled", "-Drunner=disabled"]
+hostmakedepends = [
+    "bash",
+    "bison",
+    "flex",
+    "meson",
+    "pkgconf",
+]
+makedepends = [
+    "cairo-devel",
+    "elfutils-devel",
+    "libdrm-devel",
+    "libkmod-devel",
+    "pixman-devel",
+    "procps-devel",
+]
+pkgdesc = "Tooling for Intel GPUs"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://gitlab.freedesktop.org/drm/igt-gpu-tools"
+source = f"{url}/-/archive/v{pkgver}.tar.gz"
+sha256 = "e324d9907a6ab55ecb68cccadbb8146d929b6a3de6af65dc560a4cdeb3ca4a80"
+# need fancy setup for integration tests
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("COPYING")
+
+
+@subpackage("igt-gpu-tools-devel")
+def _(self):
+    return self.default_devel()
