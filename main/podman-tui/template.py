0243991d3bc19f35cb871e344de689ff3d087367
psykose <alice@ayaya.dev>
2024-10-12 15:15:43 +0200

    main/podman-tui: update to 1.2.3

--- main/podman-tui/template.py
+++ main/podman-tui/template.py
@@ -1,6 +1,6 @@
 pkgname = "podman-tui"
-pkgver = "1.2.2"
-pkgrel = 1
+pkgver = "1.2.3"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 makedepends = [
@@ -26,4 +26,4 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/containers/podman-tui"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "97243e025bf8a0ad4b7e87197cc4e608da0af8bb447b1ffd0e01bcb58273b619"
+sha256 = "ce13ff135187463820360c89c853bdc98d219e1f856c66eb4c96be9caa0ffefc"
