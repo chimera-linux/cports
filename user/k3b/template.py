cba670fbcd532df94d777e0dca74404590679617
psykose <alice@ayaya.dev>
2024-10-12 19:00:04 +0200

    user/k3b: new package
    lord save me x3
    
    closes https://github.com/chimera-linux/cports/issues/3070

---
+++ user/k3b/template.py
@@ -0,0 +1,60 @@
+pkgname = "k3b"
+pkgver = "24.08.2"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "ffmpeg-devel",
+    "flac-devel",
+    "karchive-devel",
+    "kauth-devel",
+    "kcmutils-devel",
+    "kconfig-devel",
+    "kcoreaddons-devel",
+    "kdoctools-devel",
+    "kfilemetadata-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kio-devel",
+    "kjobwidgets-devel",
+    "knewstuff-devel",
+    "knotifications-devel",
+    "knotifyconfig-devel",
+    "kwidgetsaddons-devel",
+    "kxmlgui-devel",
+    "lame-devel",
+    "libdvdread-devel",
+    "libkcddb-devel",
+    "libmusicbrainz-devel",
+    "libsamplerate-devel",
+    "libsndfile-devel",
+    "libvorbis-devel",
+    "qt6-qt5compat-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtmultimedia-devel",
+    "solid-devel",
+    "taglib-devel",
+]
+depends = [
+    "cdrdao",
+    "dvd+rw-tools",
+    "libburn",
+    "libcdio-paranoia",  # dlopen
+    "libdvdcss",  # dlopen
+    "schilytools-cdrtools",
+]
+pkgdesc = "KDE disc burning and ripping application"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "GPL-2.0-only"
+url = "https://apps.kde.org/k3b"
+source = f"$(KDE_SITE)/release-service/{pkgver}/src/k3b-{pkgver}.tar.xz"
+sha256 = "aea81dd1942cc683c722e96b33ac7de58099fe24e223448272c148f4b8804a6f"
+
+if self.profile().arch in ["aarch64", "ppc64le", "x86_64"]:
+    makedepends += ["qt6-qtwebengine-devel"]
