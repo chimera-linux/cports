1e0b902a9899241538eb436095092050f4313378
psykose <alice@ayaya.dev>
2024-10-12 15:12:57 +0200

    main/trace-cmd: update to 3.3.1

--- main/trace-cmd/template.py
+++ main/trace-cmd/template.py
@@ -1,6 +1,6 @@
 pkgname = "trace-cmd"
-pkgver = "3.3"
-pkgrel = 1
+pkgver = "3.3.1"
+pkgrel = 0
 build_style = "meson"
 configure_args = [f"-Dversion-tag={pkgver}"]
 make_build_args = ["all", "docs"]
@@ -25,4 +25,10 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-only"
 url = "https://www.trace-cmd.org"
 source = f"https://git.kernel.org/pub/scm/utils/trace-cmd/trace-cmd.git/snapshot/trace-cmd-v{pkgver}.tar.gz"
-sha256 = "0671048dd2b8abdb8b2d0eb9cc91335c997620a7da72f858381ff4206f32065f"
+sha256 = "2efe103389367e93c764c4a788880ba51018a65dec21b0411965a5f06a6338c1"
+
+
+def post_install(self):
+    self.rename(
+        "usr/share/bash-completion/completions/trace-cmd.bash", "trace-cmd"
+    )
