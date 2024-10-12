de8c45a1cb35b624196a0641fbb47b9cb7af17ec
psykose <alice@ayaya.dev>
2024-10-12 15:18:16 +0200

    main/libtraceevent: update to 1.8.4

--- main/libtraceevent/template.py
+++ main/libtraceevent/template.py
@@ -1,5 +1,5 @@
 pkgname = "libtraceevent"
-pkgver = "1.8.3"
+pkgver = "1.8.4"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -20,7 +20,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-only AND GPL-2.0-only"
 url = "https://git.kernel.org/pub/scm/libs/libtrace/libtraceevent.git"
 source = f"{url}/snapshot/libtraceevent-{pkgver}.tar.gz"
-sha256 = "233d88adf5bae6b4511980f0f6314f348326b55fdb3dc9c4212c810e1ab06789"
+sha256 = "dc456d4d2bf4b4cd4d0c737d3374a8093f9e5ca18c1d7fc2279a4bf41e613121"
 # vis breaks symbols
 hardening = ["!vis"]
 
