5fddcacebe1e78a2632b6ff98c7230468252eca9
psykose <alice@ayaya.dev>
2024-10-12 20:47:03 +0200

    main/xwud: update to 1.0.7

--- main/xwud/template.py
+++ main/xwud/template.py
@@ -1,20 +1,17 @@
 pkgname = "xwud"
-pkgver = "1.0.6"
-pkgrel = 1
+pkgver = "1.0.7"
+pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf", "xorg-util-macros"]
 makedepends = ["libx11-devel"]
 pkgdesc = "X image displayer"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/xwud-{pkgver}.tar.gz"
-sha256 = "262171b0c434966ddbbe8a54afb9615567ad74d4cc2e823e14e51e099ec3ab0d"
+source = f"$(XORG_SITE)/app/xwud-{pkgver}.tar.xz"
+sha256 = "e55cbedab36d7a5f671abf8e594888afc48caa116d51d429ea53ea317ec0c61e"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
