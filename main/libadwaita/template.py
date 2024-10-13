8a72fb067f1f2bffb3741a4dc5341e2c18592784
psykose <alice@ayaya.dev>
2024-10-13 20:15:17 +0200

    main/libadwaita: update to 1.6.1

--- main/libadwaita/template.py
+++ main/libadwaita/template.py
@@ -1,5 +1,5 @@
 pkgname = "libadwaita"
-pkgver = "1.6.0"
+pkgver = "1.6.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -31,7 +31,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libadwaita"
 source = f"$(GNOME_SITE)/libadwaita/{pkgver[:-2]}/libadwaita-{pkgver}.tar.xz"
-sha256 = "9ed783934cb99c9101639194e1fc072cb09303b8f3c2fc6623390186a9dad8d3"
+sha256 = "d00ac845a4545d92e6805e31095a51c68f9f4e02426900472084b0cddce3f833"
 options = ["!cross"]
 
 
