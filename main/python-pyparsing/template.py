584844061b262b083f61e5b422bae02d49856159
psykose <alice@ayaya.dev>
2024-10-13 20:14:18 +0200

    main/python-pyparsing: update to 3.2.0

--- main/python-pyparsing/template.py
+++ main/python-pyparsing/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pyparsing"
-pkgver = "3.1.4"
+pkgver = "3.2.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/pyparsing/pyparsing"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "852644f146a0f90bb61f41ffb3572d4268d6f51a7242f8e2dac873cbc17963f4"
+sha256 = "91f52e4e06a2ee736531a3038fc2c7d6bb83f321831435fcedc14f95749c86ac"
 # calls urlopen
 options = ["!check"]
 
