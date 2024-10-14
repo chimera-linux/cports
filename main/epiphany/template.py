5b3b9205dd514a4e68f123d85369d94e81cc6592
psykose <alice@ayaya.dev>
2024-10-15 00:13:05 +0200

    main/epiphany: update to 47.1

--- main/epiphany/template.py
+++ main/epiphany/template.py
@@ -1,5 +1,5 @@
 pkgname = "epiphany"
-pkgver = "47.0"
+pkgver = "47.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dunit_tests=disabled"]
@@ -38,4 +38,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://wiki.gnome.org/Apps/Web"
 source = f"$(GNOME_SITE)/epiphany/{pkgver[:-2]}/epiphany-{pkgver}.tar.xz"
-sha256 = "8e38ee7f90fd6054f8aef0e7f505d53c4520742f5acd1f008282a47b2764affa"
+sha256 = "642fd72045f6e9b18f09c3ef16df762d9b7816119abb22edd5d82ef687c6a004"
