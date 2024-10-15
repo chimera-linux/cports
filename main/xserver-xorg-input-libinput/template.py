1b4aba746a511e39783be3bd403ce71cf2203fc2
psykose <alice@ayaya.dev>
2024-10-15 07:09:42 +0200

    main/xserver-xorg-input-libinput: update to 1.5.0

--- main/xserver-xorg-input-libinput/template.py
+++ main/xserver-xorg-input-libinput/template.py
@@ -1,9 +1,13 @@
 pkgname = "xserver-xorg-input-libinput"
-pkgver = "1.4.0"
-pkgrel = 1
+pkgver = "1.5.0"
+pkgrel = 0
 build_style = "gnu_configure"
-configure_gen = []
-hostmakedepends = ["pkgconf"]
+hostmakedepends = [
+    "automake",
+    "pkgconf",
+    "slibtool",
+    "xorg-util-macros",
+]
 makedepends = ["xserver-xorg-devel", "libinput-devel"]
 depends = ["virtual:xserver-abi-input~24!xserver-xorg-core"]
 pkgdesc = "Generic input driver for X.org server based on libinput"
@@ -11,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/driver/xf86-input-libinput-{pkgver}.tar.gz"
-sha256 = "e49131746f6fc639f58ee014b424a90be2e2073b55b9392517505fd3302fbbbe"
+sha256 = "936ce0ad647b49eb332c7d72945e346ffc61d48a45a009e3f0403a90dbf5908e"
 
 
 def post_install(self):
