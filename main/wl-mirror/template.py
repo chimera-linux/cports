6b715304f033865a8c6742ca07008249f36c211b
ttyyls <contact@behri.org>
2024-10-15 09:06:24 +0100

    main/wl-mirror: update to 0.17.0

--- main/wl-mirror/template.py
+++ main/wl-mirror/template.py
@@ -1,5 +1,5 @@
 pkgname = "wl-mirror"
-pkgver = "0.16.5"
+pkgver = "0.17.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -13,4 +13,4 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/Ferdi265/wl-mirror"
 source = f"{url}/releases/download/v{pkgver}/wl-mirror-{pkgver}.tar.gz"
-sha256 = "ea7aab751de2a176c115af75fd3594c3a461efe2ffbfd2ffac085ff3f5b9c65c"
+sha256 = "01a0874b7a5fbda3995f6b4a8a5e58cae2471229d28366896edd57548b2d4eca"
