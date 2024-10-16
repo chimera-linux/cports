80109bcd2cb386d2710479671e942f251ac4ddb6
psykose <alice@ayaya.dev>
2024-10-16 21:13:34 +0200

    main/libssh2: update to 1.11.1

--- main/libssh2/template.py
+++ main/libssh2/template.py
@@ -1,5 +1,5 @@
 pkgname = "libssh2"
-pkgver = "1.11.0"
+pkgver = "1.11.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://www.libssh2.org"
 source = f"https://www.libssh2.org/download/libssh2-{pkgver}.tar.gz"
-sha256 = "3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461"
+sha256 = "d9ec76cbe34db98eec3539fe2c899d26b0c837cb3eb466a56b0f109cabf658f7"
 
 
 def post_install(self):
