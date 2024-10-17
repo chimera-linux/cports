147e4a74db97ff2b0588a3edd82b01e15f2c6db9
Erica Z <zerica@callcc.eu>
2024-10-17 21:45:07 +0200

    main/leptonica: update to 1.85.0

--- main/leptonica/template.py
+++ main/leptonica/template.py
@@ -1,5 +1,5 @@
 pkgname = "leptonica"
-pkgver = "1.84.1"
+pkgver = "1.85.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
@@ -18,7 +18,7 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "BSD-2-Clause"
 url = "http://www.leptonica.org"
 source = f"https://github.com/DanBloomberg/leptonica/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "ecd7a868403b3963c4e33623595d77f2c87667e2cfdd9b370f87729192061bef"
+sha256 = "c01376bce0379d4ea4bc2ec5d5cbddaa49e2f06f88242619ab8c059e21adf233"
 
 
 def post_install(self):
