aee7286ae7d635b35e5f60a77974d540947bd7fc
psykose <alice@ayaya.dev>
2024-10-17 21:43:52 +0200

    main/thunderbird: update to 128.3.2

--- main/thunderbird/template.py
+++ main/thunderbird/template.py
@@ -1,5 +1,5 @@
 pkgname = "thunderbird"
-pkgver = "128.3.1"
+pkgver = "128.3.2"
 pkgrel = 0
 hostmakedepends = [
     "automake",
@@ -58,7 +58,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-only AND LGPL-2.1-only AND LGPL-3.0-only AND MPL-2.0"
 url = "https://www.thunderbird.net"
 source = f"$(MOZILLA_SITE)/thunderbird/releases/{pkgver}esr/source/thunderbird-{pkgver}esr.source.tar.xz"
-sha256 = "0e4a00d92b30b7d82213f710a1f5277796d965717888ae01e0cddf6b7bd889c9"
+sha256 = "7c836d148dafc1d46297e77a4984858c2130f6703cec80d6beb8aadd693dd711"
 debug_level = 1  # defatten, especially with LTO
 tool_flags = {
     "LDFLAGS": ["-Wl,-rpath=/usr/lib/thunderbird", "-Wl,-z,stack-size=2097152"]
