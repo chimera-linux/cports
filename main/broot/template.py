3dd65f9f9aa2d36d70ca95f645d2853ca25ac1f1
ttyyls <contact@behri.org>
2024-10-17 21:42:26 +0100

    main/broot: update to 1.44.1

--- main/broot/template.py
+++ main/broot/template.py
@@ -1,5 +1,5 @@
 pkgname = "broot"
-pkgver = "1.44.0"
+pkgver = "1.44.1"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -14,9 +14,10 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "MIT"
 url = "https://dystroy.org/broot"
 source = f"https://github.com/Canop/broot/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "a235f9e326d39416b484ea5e677642194f726b7683bd2d5dcd1520ba37afb34d"
+sha256 = "f0fe553e89b4173023cd35896ff9f94100b9605a23455a00562ed329962440ae"
 
 
-def post_install(self):
+def install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/broot")
     self.install_license("LICENSE")
     self.install_man("man/page", cat=1, name="broot")
