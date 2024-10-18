48fa51413461223bf1135b3b174596a79f0755db
psykose <alice@ayaya.dev>
2024-10-19 01:20:22 +0200

    main/libdex: update to 0.8.1

--- main/libdex/template.py
+++ main/libdex/template.py
@@ -1,5 +1,5 @@
 pkgname = "libdex"
-pkgver = "0.8.0"
+pkgver = "0.8.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -25,7 +25,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "LGPL-2.1-or-later AND BSD-3-Clause"
 url = "https://gitlab.gnome.org/GNOME/libdex"
 source = f"$(GNOME_SITE)/libdex/{'.'.join(pkgver.rsplit('.')[:-1])}/libdex-{pkgver}.tar.xz"
-sha256 = "659ab5d0eb7f10bd36af0cde08055158987690b9714e7f5b001c788ec1d01a25"
+sha256 = "955475ad3e43aabd6f6f70435264b5ee77bd265bd95545211fee026b08d378a0"
 # for liburing
 tool_flags = {"CFLAGS": ["-D_GNU_SOURCE"]}
 # gobject-introspection
