c379979d0ef3b076ab6a8155513053bdf7f78669
psykose <alice@ayaya.dev>
2024-10-19 01:22:52 +0200

    main/python-cryptography: update to 43.0.3

--- main/python-cryptography/template.py
+++ main/python-cryptography/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-cryptography"
-pkgver = "43.0.1"
+pkgver = "43.0.3"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -26,7 +26,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause OR Apache-2.0"
 url = "https://github.com/pyca/cryptography"
 source = f"$(PYPI_SITE)/c/cryptography/cryptography-{pkgver}.tar.gz"
-sha256 = "203e92a75716d8cfb491dc47c79e17d0d9207ccffcbcb35f598fbe463ae3444d"
+sha256 = "315b9001266a492a6ff443b61238f956b214dbec9910a081ba5b6646a055a805"
 
 
 def prepare(self):
