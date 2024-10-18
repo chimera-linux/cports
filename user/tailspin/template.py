6e8a7f83fab03e541214019fd7f1e1e8d49841cb
psykose <alice@ayaya.dev>
2024-10-19 01:29:12 +0200

    user/tailspin: update to 4.0.0

--- user/tailspin/template.py
+++ user/tailspin/template.py
@@ -1,6 +1,6 @@
 pkgname = "tailspin"
-pkgver = "3.0.2"
-pkgrel = 1
+pkgver = "4.0.0"
+pkgrel = 0
 build_style = "cargo"
 make_check_args = ["--bins"]  # disable integration tests
 hostmakedepends = ["cargo-auditable"]
@@ -10,7 +10,7 @@ maintainer = "sonata-chen <sonatachen39@gmail.com>"
 license = "MIT"
 url = "https://github.com/bensadeh/tailspin"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "6a54127187fc894ca5c58d0f65108769c9c2ac022185f29b87818aaa4150ed0b"
+sha256 = "f13ab53eb3bd59733d3fe53a6f03dd42be3801eef7456155f520139036ffb865"
 
 
 def post_install(self):
