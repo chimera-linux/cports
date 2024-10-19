ce3edbc45ad3478e5acbd78beb5f72a86d54b5aa
psykose <alice@ayaya.dev>
2024-10-19 05:32:55 +0200

    main/typst: update to 0.12.0

--- main/typst/template.py
+++ main/typst/template.py
@@ -1,12 +1,13 @@
 pkgname = "typst"
-pkgver = "0.11.1"
+pkgver = "0.12.0"
 pkgrel = 0
-build_wrksrc = "crates/typst-cli"
 build_style = "cargo"
+make_build_args = ["-p", "typst-cli"]
 make_build_env = {
     "TYPST_VERSION": f"{pkgver}",
     "GEN_ARTIFACTS": "./generated",
 }
+make_check_args = ["-p", "typst-cli"]
 hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = ["rust-std", "openssl-devel"]
 pkgdesc = "Markup-based typesetting system"
@@ -14,12 +15,15 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0"
 url = "https://typst.app"
 source = f"https://github.com/typst/typst/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "b1ba054e821073daafd90675c4822bcd8166f33fe2e3acba87ba1451a0d1fc56"
+sha256 = "5e92463965c0cf6aa003a3bacd1c68591ef2dc0db59dcdccb8f7b084836a1266"
+# takes forever
+options = ["!check"]
 
 
 def install(self):
-    self.install_bin(f"../../target/{self.profile().triplet}/release/typst")
-    self.install_man("generated/typst*.1", glob=True)
-    self.install_completion("generated/typst.bash", "bash")
-    self.install_completion("generated/typst.fish", "fish")
-    self.install_completion("generated/_typst", "zsh")
+    self.install_bin(f"target/{self.profile().triplet}/release/typst")
+    with self.pushd("crates/typst-cli/generated"):
+        self.install_man("typst*.1", glob=True)
+        self.install_completion("typst.bash", "bash")
+        self.install_completion("typst.fish", "fish")
+        self.install_completion("_typst", "zsh")
