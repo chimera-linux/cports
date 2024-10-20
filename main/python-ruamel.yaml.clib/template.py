81eabb1eaabc3d9d7d4913db5dc41023b4b116f0
psykose <alice@ayaya.dev>
2024-10-20 05:20:50 +0200

    main/python-ruamel.yaml.clib: update to 0.2.9

--- main/python-ruamel.yaml.clib/template.py
+++ main/python-ruamel.yaml.clib/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-ruamel.yaml.clib"
-pkgver = "0.2.8"
+pkgver = "0.2.9"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -7,7 +7,6 @@ hostmakedepends = [
     "python-devel",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python"]
 checkdepends = ["python-pytest"]
@@ -16,7 +15,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://sourceforge.net/projects/ruamel-yaml-clib"
 source = f"$(PYPI_SITE)/r/ruamel.yaml.clib/ruamel.yaml.clib-{pkgver}.tar.gz"
-sha256 = "beb2e0404003de9a4cab9753a8805a8fe9320ee6673136ed7f04255fe60bb512"
+sha256 = "e99304a75481da179163d5b9b841fc20dc8b99ff62b13081e474b278e15362f3"
 # abysmal vendored libyaml
 tool_flags = {"CFLAGS": ["-Wno-incompatible-function-pointer-types"]}
 # no tests on pypi
