b87b1892393d980696b39c2e790518bdbe9331fb
psykose <alice@ayaya.dev>
2024-10-20 05:28:32 +0200

    main/libmysofa: update to 1.3.3

--- main/libmysofa/template.py
+++ main/libmysofa/template.py
@@ -1,6 +1,6 @@
 pkgname = "libmysofa"
-pkgver = "1.3.2"
-pkgrel = 1
+pkgver = "1.3.3"
+pkgrel = 0
 build_style = "cmake"
 # tests fail when run in parallel
 make_check_args = ["-j1"]
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/hoene/libmysofa"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "6c5224562895977e87698a64cb7031361803d136057bba35ed4979b69ab4ba76"
+sha256 = "a15f7236a2b492f8d8da69f6c71b5bde1ef1bac0ef428b94dfca1cabcb24c84f"
 # FIXME: breaks fail-issue-167a test
 hardening = ["!int"]
 
