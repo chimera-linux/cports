14101a7a14f70bf3a2f9faa629347d3211acb5c9
psykose <alice@ayaya.dev>
2024-10-20 05:19:54 +0200

    main/tinymist: update to 0.12.0

--- main/tinymist/template.py
+++ main/tinymist/template.py
@@ -1,6 +1,6 @@
 pkgname = "tinymist"
-pkgver = "0.11.32"
-pkgrel = 1
+pkgver = "0.12.0"
+pkgrel = 0
 build_style = "cargo"
 make_build_args = ["--bin", "tinymist"]
 hostmakedepends = ["cargo", "pkgconf"]
@@ -11,7 +11,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://myriad-dreamin.github.io/tinymist"
 source = f"https://github.com/Myriad-Dreamin/tinymist/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "877d0c3722ac863d5f6f6469bd8b7b9a7ee461a2e4f50310ed0d42b795c1f182"
+sha256 = "4b919f2a823b727edb6601d9675a2716baa816ea5e510b09fea4d0d696cc31a0"
 # takes forever
 options = ["!check"]
 
