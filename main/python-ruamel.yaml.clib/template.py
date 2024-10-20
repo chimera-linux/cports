b68c8bc5cacc21209a934eff6b874643af2ec7f6
psykose <alice@ayaya.dev>
2024-10-20 13:02:11 +0200

    main/python-ruamel.yaml.clib: update to 0.2.12

--- main/python-ruamel.yaml.clib/template.py
+++ main/python-ruamel.yaml.clib/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-ruamel.yaml.clib"
-pkgver = "0.2.9"
+pkgver = "0.2.12"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,7 +15,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://sourceforge.net/projects/ruamel-yaml-clib"
 source = f"$(PYPI_SITE)/r/ruamel.yaml.clib/ruamel.yaml.clib-{pkgver}.tar.gz"
-sha256 = "e99304a75481da179163d5b9b841fc20dc8b99ff62b13081e474b278e15362f3"
+sha256 = "6c8fbb13ec503f99a91901ab46e0b07ae7941cd527393187039aec586fdfd36f"
 # abysmal vendored libyaml
 tool_flags = {"CFLAGS": ["-Wno-incompatible-function-pointer-types"]}
 # no tests on pypi
