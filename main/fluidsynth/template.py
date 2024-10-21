4a7e343a60bddf7c5651cb3554e4ba5fa299dfe4
psykose <alice@ayaya.dev>
2024-10-21 07:12:41 +0200

    main/fluidsynth: update to 2.3.7

--- main/fluidsynth/template.py
+++ main/fluidsynth/template.py
@@ -1,6 +1,6 @@
 pkgname = "fluidsynth"
-pkgver = "2.3.6"
-pkgrel = 1
+pkgver = "2.3.7"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DLIB_SUFFIX=",
@@ -23,7 +23,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.fluidsynth.org"
 source = f"https://github.com/FluidSynth/fluidsynth/archive/v{pkgver}.tar.gz"
-sha256 = "3340d73286b28fe6e5150fbe12648d4640e86c64c228878b572773bd08cac531"
+sha256 = "38d1d910783ab67c07a4d859d1aa95525979ff352b927e25b1ae894c774bb4c4"
 # CFI: doesn't work (run drumstick-vpiano)
 hardening = ["vis", "!cfi"]
 
