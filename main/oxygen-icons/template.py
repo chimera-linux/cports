4f2a2b7b6cc23dc2adf74258e9c52f08173d5703
psykose <alice@ayaya.dev>
2024-10-22 09:53:39 +0200

    main/oxygen-icons: update to 6.1.0

--- main/oxygen-icons/template.py
+++ main/oxygen-icons/template.py
@@ -1,5 +1,5 @@
 pkgname = "oxygen-icons"
-pkgver = "6.0.0"
+pkgver = "6.1.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -19,4 +19,4 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "GPL-2.0-or-later"
 url = "https://api.kde.org/frameworks/oxygen-icons/html"
 source = f"$(KDE_SITE)/oxygen-icons/oxygen-icons-{pkgver}.tar.xz"
-sha256 = "28ec182875dcc15d9278f45ced11026aa392476f1f454871b9e2c837008e5774"
+sha256 = "16ca971079c5067c4507cabf1b619dc87dd6b326fd5c2dd9f5d43810f2174d68"
