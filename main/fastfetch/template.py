031e70580a1e1546b6315fd603201bf2793cbd90
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-23 10:21:44 +0300

    main/fastfetch: update to 2.28.0

--- main/fastfetch/template.py
+++ main/fastfetch/template.py
@@ -1,5 +1,5 @@
 pkgname = "fastfetch"
-pkgver = "2.27.1"
+pkgver = "2.28.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_TESTS=ON", "-DENABLE_SYSTEM_YYJSON=ON"]
@@ -27,7 +27,7 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "MIT"
 url = "https://github.com/fastfetch-cli/fastfetch"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "de12f8cdb52bc1f123aa9b37813f009eeb09f15cbf43b033693c2936716e2626"
+sha256 = "115d9947ee0acf6246894888998db31de024f651123396c6251033390c241dc7"
 tool_flags = {"CFLAGS": ["-DNDEBUG"]}
 # CFI: dies immediately (ffPlatformPathAddHome at FFlist.c:31:12)
 hardening = ["vis", "!cfi"]
