33c7d9061e8f699c9fa74e6e09052414ccb903b7
psykose <alice@ayaya.dev>
2024-10-23 09:52:32 +0200

    user/wasmtime: update to 26.0.0

--- user/wasmtime/template.py
+++ user/wasmtime/template.py
@@ -1,5 +1,5 @@
 pkgname = "wasmtime"
-pkgver = "25.0.2"
+pkgver = "26.0.0"
 pkgrel = 0
 # no implementation for other architectures
 archs = ["aarch64", "riscv64", "x86_64"]
@@ -25,7 +25,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0"
 url = "https://wasmtime.dev"
 source = f"https://github.com/bytecodealliance/wasmtime/releases/download/v{pkgver}/wasmtime-v{pkgver}-src.tar.gz"
-sha256 = "1a2df663e463673fefcdc3626a6f6dbe8aa7a3ba71162d8c0e2e86d3381f3048"
+sha256 = "a5755f1e8738ab95f96a906fc1880ba9f1ec8ffc085b27e21eff215552ea171e"
 # wast tests take like an hour
 options = ["!check"]
 
