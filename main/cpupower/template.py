7b2b04deb861cac58652a96b8ba16e1fd163ffd7
psykose <alice@ayaya.dev>
2024-10-23 09:54:40 +0200

    main/cpupower: update to 6.11.5

--- main/cpupower/template.py
+++ main/cpupower/template.py
@@ -1,5 +1,5 @@
 pkgname = "cpupower"
-pkgver = "6.11.4"
+pkgver = "6.11.5"
 pkgrel = 0
 build_style = "makefile"
 make_build_target = "cpupower"
@@ -34,7 +34,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[:pkgver.find('.')]}.x/linux-{pkgver}.tar.xz"
-sha256 = "bd54b0a0a46574919706698b1411ec48cf2a58345c4d8990e414acc4730e8f55"
+sha256 = "471485b3b7f2fb637bd8fe3d00944c4c135c7d8ee02f357f33690baab0752a07"
 # nope
 options = ["!check"]
 
