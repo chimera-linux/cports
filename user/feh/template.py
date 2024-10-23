cf2b4888c797b847e9d9c9e21fb8573bf5764988
Daringcuteseal <daringcuteseal@gmail.com>
2024-10-23 17:19:34 +0700

    user/feh: new package

---
+++ user/feh/template.py
@@ -0,0 +1,33 @@
+pkgname = "feh"
+pkgver = "3.10.3"
+pkgrel = 0
+build_style = "makefile"
+make_build_args = [
+    "exif=1",
+    "help=1",
+    "inotify=1",
+    "magic=1",
+]
+make_check_target = "test"
+make_use_env = True
+makedepends = [
+    "file-devel",
+    "imlib2-devel",
+    "libcurl-devel",
+    "libexif-devel",
+    "libpng-devel",
+    "libx11-devel",
+    "libxinerama-devel",
+    "libxt-devel",
+]
+checkdepends = ["perl", "perl-test-command", "mandoc"]
+pkgdesc = "Fast and light image viewer"
+maintainer = "daringcuteseal <daringcuteseal@gmail.com>"
+license = "MIT"
+url = "https://feh.finalrewind.org"
+source = f"{url}/feh-{pkgver}.tar.bz2"
+sha256 = "5426e2799770217af1e01c2e8c182d9ca8687d84613321d8ab4a66fe4041e9c8"
+
+
+def post_install(self):
+    self.install_license("COPYING")
