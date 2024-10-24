a389746b9ee62b08d103e32ba417b113fd890cfc
psykose <alice@ayaya.dev>
2024-10-24 09:14:46 +0200

    main/uv: update to 0.4.26

--- main/uv/template.py
+++ main/uv/template.py
@@ -1,5 +1,5 @@
 pkgname = "uv"
-pkgver = "0.4.25"
+pkgver = "0.4.26"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -19,7 +19,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/astral-sh/uv"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "b3bd159eb8a40ad29e8c5dc4bec84c03a5bcfef04ad75e0665777c21d886b56c"
+sha256 = "a85767a9a230216774a9b57b8b29f09a255074ad3216e9502090a4a03cd2a494"
 # too many of them need net
 # completions with host bin
 options = ["!check", "!cross"]
