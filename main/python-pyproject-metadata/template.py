152fe4f5a1d774acfcb6d11f7526255154e35d16
psykose <alice@ayaya.dev>
2024-10-24 09:17:35 +0200

    main/python-pyproject-metadata: update to 0.9.0

--- main/python-pyproject-metadata/template.py
+++ main/python-pyproject-metadata/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pyproject-metadata"
-pkgver = "0.8.1"
+pkgver = "0.9.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,7 +14,7 @@ maintainer = "Duncan Bellamy <dunk@denkimushi.com>"
 license = "MIT"
 url = "https://github.com/pypa/pyproject-metadata"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "dec0ca7f8c70633595af5b9904e429ab6d15b8d35b633942843b044139d64c27"
+sha256 = "e23f57ee2c83e364badf6503a21e0d80db3440afe520b211c1d1e9060010f0f8"
 
 
 def post_install(self):
