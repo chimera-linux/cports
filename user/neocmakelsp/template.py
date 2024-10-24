fcfad5577a1ff676b314153df52671ce7823045a
ttyyls <contact@behri.org>
2024-10-24 19:59:57 +0100

    user/neocmakelsp: new package

---
+++ user/neocmakelsp/template.py
@@ -0,0 +1,19 @@
+pkgname = "neocmakelsp"
+pkgver = "0.8.12"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable"]
+makedepends = ["rust-std"]
+pkgdesc = "CMake language server"
+maintainer = "ttyyls <contact@behri.org>"
+license = "MIT"
+url = "https://neocmakelsp.github.io"
+source = f"https://github.com/neocmakelsp/neocmakelsp/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "de00f241038ac64ce3149ff9529791517e7ea4f106e37ee88b0bbac9c7cb42e4"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+    self.install_completion("completions/bash/neocmakelsp", "bash")
+    self.install_completion("completions/fish/neocmakelsp.fish", "fish")
+    self.install_completion("completions/zsh/_neocmakelsp", "zsh")
