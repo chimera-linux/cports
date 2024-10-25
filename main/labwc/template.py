40e0140bf13601a4e013d4a56aa57e9a55024edc
psykose <alice@ayaya.dev>
2024-10-26 01:58:39 +0200

    main/labwc: update to 0.8.1

--- main/labwc/template.py
+++ main/labwc/template.py
@@ -1,5 +1,5 @@
 pkgname = "labwc"
-pkgver = "0.8.0"
+pkgver = "0.8.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -17,6 +17,7 @@ makedepends = [
     "glib-devel",
     "libdrm-devel",
     "librsvg-devel",
+    "libsfdo-devel",
     "libxcb-devel",
     "libxml2-devel",
     "pango-devel",
@@ -31,10 +32,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-only"
 url = "https://github.com/labwc/labwc"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "1bfe5791c6002c1355821046623fa64231afd42e4d2ec1f6a9af44ef3b24f780"
-
-
-def post_install(self):
-    self.install_file(
-        self.files_path / "labwc-portals.conf", "usr/share/xdg-desktop-portal"
-    )
+sha256 = "8e510655cf0c84875c541f4afeb636e707d365210993ad22d64d8bc3108a3433"
