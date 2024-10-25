d2647c9dba31169f9aec7e99b5c30a024786f38d
psykose <alice@ayaya.dev>
2024-10-25 14:39:46 +0200

    main/github-cli: update to 2.60.0

--- main/github-cli/template.py
+++ main/github-cli/template.py
@@ -1,5 +1,5 @@
 pkgname = "github-cli"
-pkgver = "2.59.0"
+pkgver = "2.60.0"
 pkgrel = 0
 build_style = "go"
 make_build_args = [
@@ -15,7 +15,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "MIT"
 url = "https://cli.github.com"
 source = f"https://github.com/cli/cli/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "d24ed01e5aa1e8f42b397333462f9cd5c54e4845a5142044381fc8eb713fa001"
+sha256 = "1936a80a668caef437b2f409eaa10e48613a3502db7da9eea011b163769218a7"
 # cross: uses native binary to generate completions
 # check: needs network access
 options = ["!cross", "!check"]
