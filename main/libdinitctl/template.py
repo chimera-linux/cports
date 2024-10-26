29b245d08f2c9c862b0b0a32af42a29a65a1ac3b
q66 <q66@chimera-linux.org>
2024-10-27 00:20:42 +0200

    main/libdinitctl: add dbus services

--- main/libdinitctl/template.py
+++ main/libdinitctl/template.py
@@ -1,6 +1,6 @@
 pkgname = "libdinitctl"
 pkgver = "0_git20241009"
-pkgrel = 0
+pkgrel = 1
 _gitrev = "c4d85d34ecf4536c87907766928e9e2c6e642604"
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -16,6 +16,8 @@ sha256 = "f56577dda8ba3668be51e6cc4d79e53dc2463b710038f2cb68da4dad9d4a1fe9"
 
 def post_install(self):
     self.install_license("COPYING.md")
+    self.install_service(self.files_path / "dbus")
+    self.install_service(self.files_path / "dbus.user")
     self.install_service(self.files_path / "dinit-dbus")
     self.install_service(self.files_path / "dinit-dbus.user")
 
@@ -23,6 +25,9 @@ def post_install(self):
 @subpackage("dinit-dbus")
 def _(self):
     self.pkgdesc = "DBus interface to dinit"
+    self.depends += ["dbus"]
+    # provides same service files
+    self.replaces = ["dbus-dinit<1.14.10-r14"]
     return [
         "cmd:dinit-dbus",
         "usr/lib/dinit.d",
