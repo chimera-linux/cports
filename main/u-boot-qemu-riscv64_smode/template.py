ad06feb2f3ca04e39696e1dfda6f94a155f658c5
q66 <q66@chimera-linux.org>
2024-10-27 02:51:54 +0100

    main/u-boot-*: update to 2024.10

--- main/u-boot-qemu-riscv64_smode/template.py
+++ main/u-boot-qemu-riscv64_smode/template.py
@@ -1,5 +1,5 @@
 pkgname = "u-boot-qemu-riscv64_smode"
-pkgver = "2024.07"
+pkgver = "2024.10"
 pkgrel = 0
 build_style = "u_boot"
 hostmakedepends = [
@@ -7,6 +7,8 @@ hostmakedepends = [
     "dtc",
     "flex",
     "gcc-riscv64-unknown-elf",
+    "gnutls-devel",
+    "libuuid-devel",
     "openssl-devel",
     "python-setuptools",
 ]
@@ -15,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND BSD-3-Clause"
 url = "https://www.denx.de/wiki/U-Boot"
 source = f"https://ftp.denx.de/pub/u-boot/u-boot-{pkgver}.tar.bz2"
-sha256 = "f591da9ab90ef3d6b3d173766d0ddff90c4ed7330680897486117df390d83c8f"
+sha256 = "b28daf4ac17e43156363078bf510297584137f6df50fced9b12df34f61a92fb0"
 env = {
     "U_BOOT_TRIPLET": "riscv64-unknown-elf",
     "U_BOOT_TARGETS": "u-boot",
