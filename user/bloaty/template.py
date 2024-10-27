ba7ee221d2051803f1ef3879bc09502ea49c59e8
q66 <q66@chimera-linux.org>
2024-10-28 00:43:53 +0100

    user/bloaty: new package

---
+++ user/bloaty/template.py
@@ -0,0 +1,22 @@
+pkgname = "bloaty"
+pkgver = "1.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = ["cmake", "ninja", "pkgconf", "protobuf"]
+makedepends = [
+    "abseil-cpp-devel",
+    "capstone-devel",
+    "protobuf-devel",
+    "re2-devel",
+    "zlib-ng-compat-devel",
+]
+pkgdesc = "Size profiler for binaries"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "Apache-2.0"
+url = "https://github.com/google/bloaty"
+source = f"{url}/releases/download/v{pkgver}/bloaty-{pkgver}.tar.bz2"
+sha256 = "a308d8369d5812aba45982e55e7c3db2ea4780b7496a5455792fb3dcba9abd6f"
+
+
+def install(self):
+    self.install_bin("build/bloaty")
