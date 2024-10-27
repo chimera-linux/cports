d58445f8f7facf88d21da06d1a4e052532737764
psykose <alice@ayaya.dev>
2024-10-27 15:49:26 +0100

    main/python-pip: update to 24.3

--- main/python-pip/template.py
+++ main/python-pip/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pip"
-pkgver = "24.2"
+pkgver = "24.3"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pip.pypa.io"
 source = f"$(PYPI_SITE)/p/pip/pip-{pkgver}.tar.gz"
-sha256 = "5b5e490b5e9cb275c879595064adce9ebd31b854e3e803740b72f9ccf34a45b8"
+sha256 = "cd831345d9ce4f74ff7115203d3a0bd6730a1ba814f9327ede48910b1e30a447"
 # unpackaged dependencies
 options = ["!check"]
 
