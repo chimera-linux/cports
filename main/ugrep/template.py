90918fd3d315691db2b0a728e1e5546c76a4932a
psykose <alice@ayaya.dev>
2024-10-27 11:28:50 +0100

    main/ugrep: update to 7.0.1

--- main/ugrep/template.py
+++ main/ugrep/template.py
@@ -1,5 +1,5 @@
 pkgname = "ugrep"
-pkgver = "7.0.0"
+pkgver = "7.0.1"
 pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
@@ -24,7 +24,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-3-Clause"
 url = "https://ugrep.com"
 source = f"https://github.com/Genivia/ugrep/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5b24228ff4452d861bf1b2b0afa0232d13e8bee0289c1d1e963e7dc102eb745b"
+sha256 = "59241f3ce62bbb00c3eaccfd218d18191c7acf0e91d217db28010af3590259c9"
 
 
 def post_install(self):
