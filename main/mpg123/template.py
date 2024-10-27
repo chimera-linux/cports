40c977dfa535f8f77243a288ddf3db452232c5e6
psykose <alice@ayaya.dev>
2024-10-27 15:51:01 +0100

    main/mpg123: update to 1.32.8

--- main/mpg123/template.py
+++ main/mpg123/template.py
@@ -1,5 +1,5 @@
 pkgname = "mpg123"
-pkgver = "1.32.7"
+pkgver = "1.32.8"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -27,7 +27,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-only"
 url = "https://www.mpg123.org"
 source = f"$(SOURCEFORGE_SITE)/mpg123/mpg123-{pkgver}.tar.bz2"
-sha256 = "3c8919243707951cac0e3c39bbf28653bcaffc43c98ff16801a27350db8f0f21"
+sha256 = "feee1374c79540e0e405df0bc45fde20ad67011425c361a2759e2146894a27a7"
 
 
 def _genlib(libn, descn, iif):
