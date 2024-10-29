f05add8a44f34c7571a912dee924feb23b532a22
psykose <alice@ayaya.dev>
2024-10-29 17:56:40 +0100

    main/python-pytest-benchmark: update to 5.0.0

--- main/python-pytest-benchmark/template.py
+++ main/python-pytest-benchmark/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-pytest-benchmark"
-pkgver = "4.0.0"
+pkgver = "5.0.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = [
     "python-py-cpuinfo",
@@ -18,7 +17,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "BSD-2-Clause"
 url = "https://github.com/ionelmc/pytest-benchmark"
 source = f"$(PYPI_SITE)/p/pytest-benchmark/pytest-benchmark-{pkgver}.tar.gz"
-sha256 = "fb0785b83efe599a6a956361c0691ae1dbb5318018561af10f3e915caa0048d1"
+sha256 = "cd0adf68516eea7ac212b78a7eb6fc3373865507de8562bb3bfff2f2f852cc63"
 # FIXME
 options = ["!check"]
 
