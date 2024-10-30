fc715fc5226625db30589050e2590dddb54fc7d2
psykose <alice@ayaya.dev>
2024-10-30 05:24:48 +0100

    main/ucode-intel: update to 20241029

--- main/ucode-intel/template.py
+++ main/ucode-intel/template.py
@@ -1,5 +1,5 @@
 pkgname = "ucode-intel"
-pkgver = "20240910"
+pkgver = "20241029"
 pkgrel = 0
 archs = ["x86_64"]
 hostmakedepends = ["iucode-tool"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:proprietary"
 url = "https://github.com/intel/Intel-Linux-Processor-Microcode-Data-Files"
 source = f"{url}/archive/microcode-{pkgver}.tar.gz"
-sha256 = "8b7582eac7e9a691356e18b3bdcbc7b2db09494e040ec980a4a5fb6d0da261bf"
+sha256 = "1cae9cde48fb44444ed40ce045c34a1debbc8cfff3fb84e44c62798459f818c3"
 options = ["!strip", "foreignelf"]
 
 
