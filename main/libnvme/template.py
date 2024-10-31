c18006b77bb5022971b1b92097a5dd0598029f6d
psykose <alice@ayaya.dev>
2024-10-31 11:01:09 +0100

    main/libnvme: update to 1.11

--- main/libnvme/template.py
+++ main/libnvme/template.py
@@ -1,5 +1,5 @@
 pkgname = "libnvme"
-pkgver = "1.10"
+pkgver = "1.11"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocs=false"]
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/linux-nvme/libnvme"
 source = f"{url}/archive/v{pkgver}/libnvme-v{pkgver}.tar.gz"
-sha256 = "add9efa968e8341459fcaa2d96f2594a6f871e1fb3f57c02ce1c8ba5424f3960"
+sha256 = "f6da60036b14e0427246718a32abff442228331f9ae3853f89160cf366d83dfe"
 
 
 @subpackage("libnvme-python")
