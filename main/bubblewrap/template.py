775a3d8216d77284281ec69d375c1a9327e5e0cb
psykose <alice@ayaya.dev>
2024-10-31 04:56:19 +0100

    main/bubblewrap: update to 0.11.0

--- main/bubblewrap/template.py
+++ main/bubblewrap/template.py
@@ -1,5 +1,5 @@
 pkgname = "bubblewrap"
-pkgver = "0.10.0"
+pkgver = "0.11.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "xsltproc", "docbook-xsl-nons"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.0-or-later"
 url = "https://github.com/containers/bubblewrap"
 source = f"{url}/releases/download/v{pkgver}/bubblewrap-{pkgver}.tar.xz"
-sha256 = "65d92cf44a63a51e1b7771f70c05013dce5bd6b0b2841c4b4be54b0c45565471"
+sha256 = "988fd6b232dafa04b8b8198723efeaccdb3c6aa9c1c7936219d5791a8b7a8646"
 hardening = ["vis", "cfi"]
 
 # seccomp tests fail on aarch64 with efault instead of econnrefused for various assertions
