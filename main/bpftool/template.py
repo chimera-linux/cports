281e821e8028e94ec0bb83fa3af174d9b2c241e3
psykose <alice@ayaya.dev>
2024-10-31 04:42:40 +0100

    main/bpftool: update to 7.5.0

--- main/bpftool/template.py
+++ main/bpftool/template.py
@@ -1,5 +1,5 @@
 pkgname = "bpftool"
-pkgver = "7.4.0"
+pkgver = "7.5.0"
 pkgrel = 0
 build_wrksrc = "src"
 build_style = "makefile"
@@ -30,12 +30,12 @@ url = "https://github.com/libbpf/bpftool"
 source = [
     f"{url}/archive/refs/tags/v{pkgver}.tar.gz",
     # bpftool uses libbpf internals
-    "https://github.com/libbpf/libbpf/archive/20ea95b4505c477af3b6ff6ce9d19cee868ddc5d.tar.gz",
+    "https://github.com/libbpf/libbpf/archive/09b9e83102eb8ab9e540d36b4559c55f3bcdb95d.tar.gz",
 ]
 source_paths = [".", "libbpf"]
 sha256 = [
-    "131c6284799dd955af7f581c5362b8f0704ea3af7e5c4eac49b3b91a064b6ad4",
-    "f0c10bb89af29533b27146c8ea3dd1fa89905a8933387be782ead09488820db6",
+    "a126f8cb06f887741ce45cd4f823583ae70aebc3f615cc4ed2a5eec8676a9681",
+    "f94a66ab80e79aa11e15409479d8bc2572649f0ef25dbd2daf503ea5b05067ad",
 ]
 # nope
 options = ["!check"]
