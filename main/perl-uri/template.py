36562c3c4a26e6c8f43198e4c4ebbc306b475c0c
psykose <alice@ayaya.dev>
2024-10-31 14:41:24 +0100

    main/perl-uri: downgrade to 5.29
    was reverted upstream

--- main/perl-uri/template.py
+++ main/perl-uri/template.py
@@ -1,6 +1,6 @@
 pkgname = "perl-uri"
-pkgver = "5.30"
-pkgrel = 0
+pkgver = "5.29"
+pkgrel = 1
 build_style = "perl_module"
 hostmakedepends = ["perl"]
 makedepends = ["perl"]
@@ -10,6 +10,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/pod/URI"
 source = f"$(CPAN_SITE)/URI/URI-{pkgver}.tar.gz"
-sha256 = "be366cf5d923e2c0d63e5fe5f707f614a144020961a2a0fe0b2c922f5fb80a95"
+sha256 = "a34b9f626c3ff1e20c0d4a23ec5c8b7ae1de1fb674ecefed7e46791388137372"
 # missing checkdepends
 options = ["!check"]
