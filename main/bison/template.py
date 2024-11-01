5b4af9f43b09966f309e756bfc3a26fd0c4e3287
triallax <triallax@tutanota.com>
2024-11-01 19:50:52 +0000

    main/bison: enable diagnostic colors, generate configure

--- main/bison/template.py
+++ main/bison/template.py
@@ -1,10 +1,11 @@
 pkgname = "bison"
 pkgver = "3.8.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = ["--disable-yacc"]
 make_check_args = ["-j1"]
-hostmakedepends = ["perl", "gm4", "texinfo"]
+hostmakedepends = ["automake", "gettext-devel", "gm4", "perl", "texinfo"]
+makedepends = ["gettext-devel"]
 checkdepends = ["flex", "autoconf"]
 depends = ["gm4"]
 pkgdesc = "GNU yacc(1) replacement"
@@ -15,4 +16,8 @@ source = f"$(GNU_SITE)/bison/bison-{pkgver}.tar.xz"
 sha256 = "9bba0214ccf7f1079c5d59210045227bcf619519840ebfa80cd3849cff5a5bf2"
 hardening = ["vis", "!cfi"]
 
-configure_gen = []
+
+def init_configure(self):
+    self.configure_args += [
+        f"--with-libtextstyle-prefix={self.chroot_destdir}/usr",
+    ]
