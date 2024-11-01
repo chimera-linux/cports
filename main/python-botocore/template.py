c1a2e65fa3c64a04b6a2e9578ab72c9ebcbc2fa1
ttyyls <contact@behri.org>
2024-11-01 21:29:43 +0100

    main/python-botocore: update to 1.35.54

--- main/python-botocore/template.py
+++ main/python-botocore/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-botocore"
-pkgver = "1.35.49"
+pkgver = "1.35.54"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -19,6 +19,6 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/boto/botocore"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "dcee43fca1fbf93746038bc3fb229821a1ae2e4a1aa77e89d5ab1accd0bf7b36"
+sha256 = "420c8d7a630e4a872d641079ed257549fcd7ee1b213af1863013a8dda0a975ad"
 # takes forever
 options = ["!check"]
