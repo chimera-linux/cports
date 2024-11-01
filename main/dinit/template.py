3aeb8a98028647f6e9648cb2acf96e97ffe1a776
triallax <triallax@tutanota.com>
2024-11-01 21:56:21 +0000

    main/dinit: install completions, switch build style to configure
    configure script is handrolled and complains about being passed a bunch
    of arguments it doesn't recognise, e.g:
    
    Warning: Unknown variable: --sysconfdir

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -2,11 +2,9 @@ pkgname = "dinit"
 pkgver = "0.19.1"
 # temporary so we get our features
 _gitrev = "f883bcb2eabe6c2646bc74647a3aeaece09a9875"
-pkgrel = 0
-build_style = "gnu_configure"
-configure_args = ["--syscontrolsocket=/run/dinitctl"]
-configure_gen = []
-make_dir = "."
+pkgrel = 1
+build_style = "configure"
+configure_args = ["--sbindir=/usr/bin", "--syscontrolsocket=/run/dinitctl"]
 make_check_args = ["check-igr"]  # additional target
 pkgdesc = "Service manager and init system"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -16,3 +14,10 @@ source = f"https://github.com/davmac314/dinit/archive/{_gitrev}.tar.gz"
 sha256 = "1dd190e8fb906dfa80316365f0f6fac03113a134763b794fa625183ab8b74878"
 tool_flags = {"CXXFLAGS": ["-fno-rtti"]}
 hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    with self.pushd("contrib/shell-completion"):
+        self.install_completion("bash/dinitctl", "bash", "dinitctl")
+        self.install_completion("fish/dinitctl.fish", "fish", "dinitctl")
+        self.install_completion("zsh/_dinit", "zsh", "dinitctl")
