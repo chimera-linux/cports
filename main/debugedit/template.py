8ed408c84d4a2a1f4613e978c314dc85bc357ce0
triallax <triallax@tutanota.com>
2024-11-01 23:11:46 +0000

    main/debugedit: update to 5.1

--- main/debugedit/template.py
+++ main/debugedit/template.py
@@ -1,9 +1,9 @@
 pkgname = "debugedit"
-pkgver = "5.0"
+pkgver = "5.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "bash", "help2man", "pkgconf"]
-makedepends = ["elfutils-devel", "musl-bsd-headers"]
+makedepends = ["elfutils-devel", "musl-bsd-headers", "xxhash-devel"]
 pkgdesc = "Utilities for creating debuginfo and source file distributions"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-3.0-or-later AND LGPL-2.0-or-later"
@@ -11,7 +11,7 @@ url = "https://sourceware.org/debugedit"
 source = (
     f"https://sourceware.org/pub/debugedit/{pkgver}/debugedit-{pkgver}.tar.xz"
 )
-sha256 = "e9ecd7d350bebae1f178ce6776ca19a648b6fe8fa22f5b3044b38d7899aa553e"
+sha256 = "ee9b688b2ed8fa62551c54cb5dc31aaa05853e7dedbd9e1237c77894ea5e3626"
 # CFI: check
 hardening = ["vis", "!cfi"]
 # check: most of them fail (TODO)
