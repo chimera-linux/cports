80a5eef8a5a22c8939597a85450cea2642f329fc
psykose <alice@ayaya.dev>
2024-11-02 15:03:43 +0100

    user/mu: update to 1.12.7

--- user/mu/template.py
+++ user/mu/template.py
@@ -1,5 +1,5 @@
 pkgname = "mu"
-pkgver = "1.12.6"
+pkgver = "1.12.7"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -17,7 +17,7 @@ maintainer = "tj <tjheeta@gmail.com>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/djcb/mu"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "13d55aa56fbe8746504a5858117b8092660c5cbd59c48c9b1a6038f99b2d12b0"
+sha256 = "9212cf0f9d3b6342d5a0aea40a3d5b5116fe4da198719cccab1f8fa6683bd8b4"
 
 
 @subpackage("mu-mu4e-emacs")
