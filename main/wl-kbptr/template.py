c0836f25d5f460e0ceb3a14321ffdaf7a2b1e3d2
ttyyls <contact@behri.org>
2024-11-02 13:08:06 +0100

    main/wl-kbptr: update to 0.2.3

--- main/wl-kbptr/template.py
+++ main/wl-kbptr/template.py
@@ -1,5 +1,5 @@
 pkgname = "wl-kbptr"
-pkgver = "0.2.2"
+pkgver = "0.2.3"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -14,7 +14,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/moverest/wl-kbptr"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "d37524010f777eed0dd5a482b2b30737d8938bfd936140c666861ca212b25ebb"
+sha256 = "0d03f83d94b6acfdb07f3ee3760c1abc207eef5a8346af38978d6bcb46ac58b8"
 hardening = ["vis", "!cfi"]
 
 
