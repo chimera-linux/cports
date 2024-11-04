79fc938b1ad1fab705f20da2c0eec789815d5624
q66 <q66@chimera-linux.org>
2024-11-04 15:40:36 +0100

    main/nyagetty: autoenable serial gettys, detect params, use args
    Using args means we no longer need to generate a fixed set
    of services for all possible gettys.
    
    The autoenable behavior can be disabled in the config with
    GETTY_DISABLE_CONSOLE_DETECT key. The new code results in
    automatic enablement of gettys for active consoles (provided
    that a service exists). Additionally, instead of hardcoding
    a baud rate and other params, try detecting them first.
    
    All this means we'll be able to remove this code from the live
    initramfs, and it's a bit less work to figure out for everyone.

---
+++ main/nyagetty/files/agetty-service.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+case "$1" in
+    tty[0-9]*|console) exec /usr/lib/agetty-default "$@" ;;
+esac
+
+exec /usr/lib/agetty-serial "$@" ;;
--- main/nyagetty/template.py
+++ main/nyagetty/template.py
@@ -1,6 +1,6 @@
 pkgname = "nyagetty"
 pkgver = "2.38.99"
-pkgrel = 4
+pkgrel = 5
 build_style = "meson"
 hostmakedepends = ["meson"]
 makedepends = ["linux-headers"]
@@ -13,83 +13,22 @@ source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "7033d6840f839a6ad6d788d92f45efd0bb10c835c0560dba5d15ad8a6b9dff90"
 hardening = ["vis", "cfi"]
 
-# sync securetty in base-files with this when updating
-_ttys = [
-    # /dev/console
-    ("console", None),
-    # graphical terminals
-    ("tty1", None),
-    ("tty2", None),
-    ("tty3", None),
-    ("tty4", None),
-    ("tty5", None),
-    ("tty6", None),
-    ("tty7", None),
-    ("tty8", None),
-    # generic serial
-    ("ttyS0", True),
-    ("ttyS1", True),
-    ("ttyS2", True),
-    ("ttyS3", True),
-    ("ttyS4", True),
-    # usb serial
-    ("ttyUSB0", True),
-    ("ttyUSB1", True),
-    # various serial
-    ("ttyAMA0", True),
-    ("ttyAMA1", True),
-    ("ttySIF0", True),
-    ("ttySIF1", True),
-    ("ttymxc0", True),
-    ("ttymxc1", True),
-    ("ttymxc2", True),
-    ("ttymxc3", True),
-    # ibm/xen terminals
-    ("hvc0", 38400),
-    ("hvc1", 38400),
-    ("hvsi0", 19200),
-    ("hvsi1", 19200),
-]
-
 
 def post_install(self):
     # agetty dinit helper
-    self.install_file(
-        self.files_path / "dinit-agetty", "usr/libexec", mode=0o755
-    )
+    self.install_file(self.files_path / "dinit-agetty", "usr/lib", mode=0o755)
     # agetty conf wrapper
+    self.install_file(self.files_path / "agetty-default", "usr/lib", mode=0o755)
+    self.install_file(self.files_path / "agetty-serial", "usr/lib", mode=0o755)
     self.install_file(
-        self.files_path / "agetty-default", "usr/libexec", mode=0o755
+        self.files_path / "agetty-service.sh",
+        "usr/lib",
+        name="agetty-service",
+        mode=0o755,
     )
-    self.install_file(
-        self.files_path / "agetty-serial", "usr/libexec", mode=0o755
-    )
-    # core service
+    # core services
     self.install_service(self.files_path / "agetty", enable=True)
-    # generate services for individual gettys
-    for name, baud in _ttys:
-        svpath = self.destdir / f"usr/lib/dinit.d/agetty-{name}"
-        with open(svpath, "w") as sv:
-            if baud is None:
-                cmd = f"agetty-default {name}"
-            elif baud is True:
-                cmd = f"agetty-serial {name}"
-            else:
-                cmd = f"agetty-serial {name} {baud}"
-            sv.write(
-                f"""# agetty service for {name}
-type            = process
-command         = /usr/libexec/{cmd}
-restart         = true
-depends-on      = login.target
-termsignal      = HUP
-restart         = true
-smooth-recovery = true
-inittab-id      = {name.removeprefix('tty')}
-inittab-line    = {name}
-"""
-            )
-        svpath.chmod(0o644)
+    self.install_service(self.files_path / "agetty-service")
 
 
 @subpackage("nyagetty-dinit")
@@ -99,4 +38,4 @@ def _(self):
     self.depends = [self.parent, "dinit-chimera"]
     self.install_if = [self.parent, "dinit-chimera"]
 
-    return ["usr/lib/dinit.d/agetty*", "usr/libexec/dinit-agetty"]
+    return ["usr/lib/dinit.d/agetty*", "usr/lib/dinit-agetty"]
