b1ef6741bbd390db4cd8172d94ec31f864a099be
psykose <alice@ayaya.dev>
2024-11-07 21:43:16 +0100

    main/libxcursor: update to 1.2.3

--- main/libxcursor/template.py
+++ main/libxcursor/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxcursor"
-pkgver = "1.2.2"
+pkgver = "1.2.3"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "libtool", "pkgconf", "xorg-util-macros"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXcursor-{pkgver}.tar.gz"
-sha256 = "98c3a30a3f85274c167d1ac5419d681ce41f14e27bfa5fe3003c8172cd8af104"
+sha256 = "74e72da27e61cc2cfd2e267c14f500ea47775850048ee0b00362a55c9b60ee9b"
 
 
 def post_install(self):
