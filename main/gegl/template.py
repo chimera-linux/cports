e8f2e11869aec140470cb465cc6e4a7f70ec7871
psykose <alice@ayaya.dev>
2024-11-07 22:17:48 +0100

    main/gegl: update to 0.4.50

--- main/gegl/template.py
+++ main/gegl/template.py
@@ -1,5 +1,5 @@
 pkgname = "gegl"
-pkgver = "0.4.48"
+pkgver = "0.4.50"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -44,7 +44,7 @@ url = "https://gegl.org"
 source = (
     f"https://download.gimp.org/pub/gegl/{pkgver[:-3]}/gegl-{pkgver}.tar.xz"
 )
-sha256 = "418c26d94be8805d7d98f6de0c6825ca26bd74fcacb6c188da47533d9ee28247"
+sha256 = "6084969b06ee86ca71142133773f27e13f02e5a6a22c2cfce452ecaaddb790c1"
 # TODO
 hardening = ["!int"]
 
