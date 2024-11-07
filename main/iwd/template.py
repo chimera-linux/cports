4e3c46f8275c5c6c58134a738da67c92492f1b69
psykose <alice@ayaya.dev>
2024-11-07 23:08:01 +0100

    main/iwd: update to 3.1

--- main/iwd/template.py
+++ main/iwd/template.py
@@ -1,6 +1,6 @@
 pkgname = "iwd"
-pkgver = "3.0"
-pkgrel = 1
+pkgver = "3.1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--libexecdir=/usr/lib",  # XXX drop libexec
@@ -23,7 +23,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://iwd.wiki.kernel.org"
 source = f"$(KERNEL_SITE)/network/wireless/iwd-{pkgver}.tar.xz"
-sha256 = "bd167ab368b6ba302b6c948a4f41f02d233a12e20d5094b1c0393325309f8a60"
+sha256 = "a2b6d21315fdd0e060d5f75f5a0cc55018ea9d112a080f2ccd6dae2e1d923ac5"
 tool_flags = {
     "CFLAGS": ["-Wno-unknown-warning-option", "-Wno-duplicate-decl-specifier"]
 }
