0fccd80d29edbff742f2571a5d631b76fb1183d1
psykose <alice@ayaya.dev>
2024-11-07 22:09:39 +0100

    user/bacon: update to 3.2.0

--- user/bacon/template.py
+++ user/bacon/template.py
@@ -1,5 +1,5 @@
 pkgname = "bacon"
-pkgver = "3.1.1"
+pkgver = "3.2.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -9,7 +9,7 @@ maintainer = "tulilirockz <tulilirockz@outlook.com>"
 license = "AGPL-3.0-only"
 url = "https://dystroy.org/bacon"
 source = f"https://github.com/Canop/bacon/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f078f2326da81cab31a3196d4ac43d0771d5679c7d5b32ce38309ff89f80559a"
+sha256 = "d8b516b0af8564fbd470513bd6420ba077e74a3860d655efaf9ec12fda47e7a1"
 
 
 def post_install(self):
