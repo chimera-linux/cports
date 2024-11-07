b31cd95a09409b36b698cc5a51275785d8feaa95
psykose <alice@ayaya.dev>
2024-11-07 21:54:38 +0100

    main/xbacklight: update to 1.2.4

--- main/xbacklight/template.py
+++ main/xbacklight/template.py
@@ -1,20 +1,17 @@
 pkgname = "xbacklight"
-pkgver = "1.2.3"
-pkgrel = 1
+pkgver = "1.2.4"
+pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf", "xorg-util-macros"]
 makedepends = ["libx11-devel", "libxrandr-devel", "xcb-util-devel"]
 pkgdesc = "Adjusts X backlight brightness"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
-source = f"$(XORG_SITE)/app/xbacklight-{pkgver}.tar.bz2"
-sha256 = "3a27f324777ae99fee476cfb2f064576fb8cba4eb77f97cda37adda1c1d39ade"
+source = f"$(XORG_SITE)/app/xbacklight-{pkgver}.tar.xz"
+sha256 = "d4c30b0e6f18c82f387585a737ee3b72d468c927892b08a898c41bc12248e8ee"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
