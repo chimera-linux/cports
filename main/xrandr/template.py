8d082c581a961fa7c07107210196acb9d7e865d0
psykose <alice@ayaya.dev>
2024-11-07 22:06:00 +0100

    main/xrandr: update to 1.5.3

--- main/xrandr/template.py
+++ main/xrandr/template.py
@@ -1,20 +1,17 @@
 pkgname = "xrandr"
-pkgver = "1.5.2"
-pkgrel = 1
+pkgver = "1.5.3"
+pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf", "xorg-util-macros"]
 makedepends = ["libxrandr-devel"]
 pkgdesc = "Command line interface to X RandR extension"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/xrandr-{pkgver}.tar.xz"
-sha256 = "c8bee4790d9058bacc4b6246456c58021db58a87ddda1a9d0139bf5f18f1f240"
+sha256 = "f8dd7566adb74147fab9964680b6bbadee87cf406a7fcff51718a5e6949b841c"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
