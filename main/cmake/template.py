06af79cb5b5f6d1bc3f27a0163100dfe9850fd6a
psykose <alice@ayaya.dev>
2024-11-07 20:51:14 +0100

    main/cmake: update to 3.31.0

--- main/cmake/template.py
+++ main/cmake/template.py
@@ -1,5 +1,5 @@
 pkgname = "cmake"
-pkgver = "3.30.5"
+pkgver = "3.31.0"
 pkgrel = 0
 build_style = "configure"
 configure_args = [
@@ -26,7 +26,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://cmake.org"
 source = f"https://www.cmake.org/files/v{pkgver[:-2]}/cmake-{pkgver}.tar.gz"
-sha256 = "9f55e1a40508f2f29b7e065fa08c29f82c402fa0402da839fffe64a25755a86d"
+sha256 = "300b71db6d69dcc1ab7c5aae61cbc1aa2778a3e00cbd918bc720203e311468c3"
 # prevent cmake self-bootstrap false positive nonsense
 tool_flags = {
     "CXXFLAGS": ["-Wno-unused-command-line-argument"],
