ce9df4d8a9179e561f931accb2d8e47814c3a5b0
psykose <alice@ayaya.dev>
2024-11-07 20:40:55 +0100

    main/tailscale: update to 1.76.6

--- main/tailscale/template.py
+++ main/tailscale/template.py
@@ -1,5 +1,5 @@
 pkgname = "tailscale"
-pkgver = "1.76.3"
+pkgver = "1.76.6"
 pkgrel = 0
 build_style = "go"
 make_build_args = [
@@ -16,7 +16,7 @@ maintainer = "Val Packett <val@packett.cool>"
 license = "BSD-3-Clause"
 url = "https://github.com/tailscale/tailscale"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1cc2ef1b7b6491c48446ec4c20c413c2300e8b7e171b119d843af46d0ce3125f"
+sha256 = "1603c78a6a5e9f83b278d305e1196fbfdeeb841be10ac2ddb7ea433c2701234b"
 # check: needs network access
 # cross: completions with host bin
 options = ["!check", "!cross"]
