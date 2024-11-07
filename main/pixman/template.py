37b9006cd6e8f0b5e5de192166d6b6959efadc3f
psykose <alice@ayaya.dev>
2024-11-07 21:49:45 +0100

    main/pixman: update to 0.44.0

--- main/pixman/template.py
+++ main/pixman/template.py
@@ -1,14 +1,12 @@
 pkgname = "pixman"
-pkgver = "0.43.4"
-pkgrel = 1
+pkgver = "0.44.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dgnu-inline-asm=enabled",
     "-Dlibpng=enabled",
     "-Dgtk=disabled",
     "-Dopenmp=disabled",
-    "-Diwmmxt=disabled",
-    "-Da64-neon=disabled",  # added with 0.42.x, fails to build on clang
 ]
 make_check_args = ["--timeout-multiplier", "3"]
 hostmakedepends = ["meson", "pkgconf", "perl"]
@@ -18,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pixman.org"
 source = f"https://www.cairographics.org/releases/pixman-{pkgver}.tar.gz"
-sha256 = "a0624db90180c7ddb79fc7a9151093dc37c646d8c38d3f232f767cf64b85a226"
+sha256 = "89a4c1e1e45e0b23dffe708202cb2eaffde0fe3727d7692b2e1739fec78a7dac"
 # needs a lot larger stack than musl default
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=2097152"]}
 # FIXME int (test fails)
