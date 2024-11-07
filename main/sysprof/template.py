676a3fb972fb0c7b148862dd493235490ed1cf3f
psykose <alice@ayaya.dev>
2024-11-07 21:48:36 +0100

    main/sysprof: update to 47.1

--- main/sysprof/template.py
+++ main/sysprof/template.py
@@ -1,7 +1,7 @@
 # sync with main/sysprof-capture
 pkgname = "sysprof"
-pkgver = "47.0"
-pkgrel = 2
+pkgver = "47.1"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "--libexecdir=/usr/lib",  # XXX drop libexec
@@ -37,7 +37,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-3.0-or-later AND BSD-2-Clause-Patent"
 url = "https://www.sysprof.com"
 source = f"$(GNOME_SITE)/sysprof/{'.'.join(pkgver.rsplit('.')[:-1])}/sysprof-{pkgver}.tar.xz"
-sha256 = "7424c629434660654288c04248998c357d1ce87ee1559fd44df1980992ef5df5"
+sha256 = "d3baba814be0cdaef28704c8004ff3110e99525422060f2a993a22a47b9334eb"
 # sysprof`sysprof_disk_usage_record_fiber muloverflow when busy i/o
 hardening = ["!int"]
 
