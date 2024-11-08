b7ec8d296d2b1557bf6b0b36183be5115a965f76
psykose <alice@ayaya.dev>
2024-11-08 22:06:45 +0100

    main/qtxdg-tools: update to 4.1.0

--- main/qtxdg-tools/template.py
+++ main/qtxdg-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "qtxdg-tools"
-pkgver = "4.0.0"
+pkgver = "4.1.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -16,4 +16,4 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/qtxdg-tools"
 source = f"{url}/releases/download/{pkgver}/qtxdg-tools-{pkgver}.tar.xz"
-sha256 = "4cd485e2780290d99d7242f605db9f0ffe5ed598cc672ed5e4acef871ce27b3a"
+sha256 = "dbd59b7641091a226fb58222e11b4aeb36e6e65dc235280897d066e59fa966b6"
