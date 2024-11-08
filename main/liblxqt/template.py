e30e851bf775c1819a3c2174b45e54fd35b6d916
psykose <alice@ayaya.dev>
2024-11-08 22:05:00 +0100

    main/liblxqt: update to 2.1.0

--- main/liblxqt/template.py
+++ main/liblxqt/template.py
@@ -1,5 +1,5 @@
 pkgname = "liblxqt"
-pkgver = "2.0.0"
+pkgver = "2.1.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -21,7 +21,7 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/liblxqt"
 source = f"{url}/releases/download/{pkgver}/liblxqt-{pkgver}.tar.xz"
-sha256 = "61092556146b769dc9bab328550f3f157e618a1a09ae1e052db61fc0db07b3e4"
+sha256 = "10820b62f83c5f53439b8690c9d71deaad7aa31e6506f9ec53cb789d47b13ce0"
 
 
 @subpackage("liblxqt-devel")
