59b22fecc10f1bdc3f6b956f430228e560e60fa5
psykose <alice@ayaya.dev>
2024-11-08 02:11:21 +0100

    main/kunifiedpush: new package

---
+++ main/kunifiedpush/template.py
@@ -0,0 +1,33 @@
+pkgname = "kunifiedpush"
+pkgver = "1.0.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_wrapper = ["dbus-run-session", "--"]
+hostmakedepends = [
+    "cmake",
+    "extra-cmake-modules",
+    "gettext",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kservice-devel",
+    "kcmutils-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtwebsockets-devel",
+]
+checkdepends = ["dbus"]
+pkgdesc = "KDE library for push notifications"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "LGPL-2.0-or-later"
+url = "https://api.kde.org/kunifiedpush/html"
+source = f"$(KDE_SITE)/kunifiedpush/kunifiedpush-{pkgver}.tar.xz"
+sha256 = "2ddeba21306d0307114ec50a2c38159ec62359f9fc6cdd58da30a369fbd550cf"
+
+
+@subpackage("kunifiedpush-devel")
+def _(self):
+    self.depends += ["qt6-qtbase-devel"]
+    return self.default_devel()
