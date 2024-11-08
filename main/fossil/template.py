8e980bf122eb93c7c3c6600f467a06b3a6822d35
Erica Z <zerica@callcc.eu>
2024-11-08 09:21:38 +0100

    main/fossil: update to 2.25

--- main/fossil/template.py
+++ main/fossil/template.py
@@ -1,6 +1,6 @@
 pkgname = "fossil"
-pkgver = "2.24"
-pkgrel = 1
+pkgver = "2.25"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--json",
@@ -18,7 +18,7 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "BSD-2-Clause"
 url = "https://fossil-scm.org"
 source = f"https://fossil-scm.org/home/tarball/version-{pkgver}/fossil-src-{pkgver}.tar.gz"
-sha256 = "e6f5a559369bf16baf539e69e6d0cea5a6410efa9a6e7f160c7a4932080413be"
+sha256 = "611cfa50d08899eb993a5f475f988b4512366cded82688c906cf913e5191b525"
 # tests are unmaintained: https://fossil-scm.org/forum/forumpost/77cd773882607d94
 options = ["!cross", "!check"]
 
