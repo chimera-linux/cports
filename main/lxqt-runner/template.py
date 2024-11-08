ad75dfa46c3fff28d62aac74cc0755a4e9e89fb1
psykose <alice@ayaya.dev>
2024-11-08 22:09:03 +0100

    main/lxqt-runner: update to 2.1.0

--- main/lxqt-runner/template.py
+++ main/lxqt-runner/template.py
@@ -1,5 +1,5 @@
 pkgname = "lxqt-runner"
-pkgver = "2.0.0"
+pkgver = "2.1.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -23,4 +23,4 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/lxqt-runner"
 source = f"{url}/releases/download/{pkgver}/lxqt-runner-{pkgver}.tar.xz"
-sha256 = "9f8031d780304d1c3b2c9b56a27744341c5e81d7cf2155bac959eaa6929e949c"
+sha256 = "2b7f233792099d0ee2369576c5f2e0ebb3dce9137593e7cdf0a1ffe10feff6a6"
