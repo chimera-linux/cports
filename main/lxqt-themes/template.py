ed072e51152f17cb4ba32c0696b605a9a120a95c
psykose <alice@ayaya.dev>
2024-11-08 22:08:59 +0100

    main/lxqt-themes: update to 2.1.0

--- main/lxqt-themes/template.py
+++ main/lxqt-themes/template.py
@@ -1,5 +1,5 @@
 pkgname = "lxqt-themes"
-pkgver = "2.0.0"
+pkgver = "2.1.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -14,4 +14,4 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/lxqt-themes"
 source = f"{url}/releases/download/{pkgver}/lxqt-themes-{pkgver}.tar.xz"
-sha256 = "927aa0bd8ecf718c4a91e820277af12a24d329b99e9e7ca4868311c1de76911d"
+sha256 = "cdd0101c5a53a0e49315c7af3ba784e70a8a2410526eab3d63f32a6678bd0fac"
