c75faa403cbca98d16fb2388d9df9a661fff917b
psykose <alice@ayaya.dev>
2024-11-08 22:05:02 +0100

    main/lxqt-globalkeys: update to 2.1.0

--- main/lxqt-globalkeys/template.py
+++ main/lxqt-globalkeys/template.py
@@ -1,5 +1,5 @@
 pkgname = "lxqt-globalkeys"
-pkgver = "2.0.0"
+pkgver = "2.1.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -20,7 +20,7 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/lxqt-globalkeys"
 source = f"{url}/releases/download/{pkgver}/lxqt-globalkeys-{pkgver}.tar.xz"
-sha256 = "13e7a72686890a40b65d8ae13c79ed287eb971fbc0285769c40c8b97e7af43f7"
+sha256 = "70cc56c452626a2c3ceb7ade8745ed61bac10c7d9aa082443a74aba1e3942874"
 
 
 @subpackage("lxqt-globalkeys-devel")
