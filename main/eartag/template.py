e4cb200669d5aec21d10579dbb020b33967a5f0c
psykose <alice@ayaya.dev>
2024-11-09 14:04:12 +0100

    main/eartag: update to 0.6.3

--- main/eartag/template.py
+++ main/eartag/template.py
@@ -1,5 +1,5 @@
 pkgname = "eartag"
-pkgver = "0.6.2"
+pkgver = "0.6.3"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -23,7 +23,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://apps.gnome.org/EarTag"
 source = f"https://gitlab.gnome.org/World/eartag/-/archive/{pkgver}/eartag-{pkgver}.tar.gz"
-sha256 = "2684a34550c43fda7f42f6ae49feac80ca21e843cd3503012eff58bda17383ed"
+sha256 = "fb44da1c75ef9869c816dd512d4d90090cac56dc8047ed83ca77b9679af02cd3"
 
 
 def post_install(self):
