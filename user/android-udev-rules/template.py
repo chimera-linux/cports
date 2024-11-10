e30a6565b6dfa827cb476c8b222f80c8d59d523c
psykose <alice@ayaya.dev>
2024-11-10 01:06:08 +0100

    user/android-udev-rules: update to 20241109

--- user/android-udev-rules/template.py
+++ user/android-udev-rules/template.py
@@ -1,12 +1,12 @@
 pkgname = "android-udev-rules"
-pkgver = "20241019"
+pkgver = "20241109"
 pkgrel = 0
 pkgdesc = "Comprehensive Android udev rules collection"
 maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/M0Rf30/android-udev-rules"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "1fdb0aa5db086c371310f97c59191706968c8bf93d8c4a63b0c7a04734fd84df"
+sha256 = "178251de2683dfe27d8bfd2259bc0bc8e38e68b3bc780baca68b8ae9d3d18aca"
 options = ["!splitudev"]
 
 
