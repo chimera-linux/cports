42370fc960b67dea9bfd53e5bf33ff83f3ebc474
psykose <alice@ayaya.dev>
2024-11-10 19:11:41 +0100

    user/unrar: update to 7.1.1

--- user/unrar/template.py
+++ user/unrar/template.py
@@ -1,5 +1,5 @@
 pkgname = "unrar"
-pkgver = "7.0.9"
+pkgver = "7.1.1"
 pkgrel = 0
 build_style = "makefile"
 make_use_env = True
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:unrar"
 url = "https://www.rarlab.com/rar_add.htm"
 source = f"https://www.rarlab.com/rar/unrarsrc-{pkgver}.tar.gz"
-sha256 = "505c13f9e4c54c01546f2e29b2fcc2d7fabc856a060b81e5cdfe6012a9198326"
+sha256 = "2e9cbc9d1c250b40f4a7a6a363b6ccfa3703e190534979d18c8c4ac5ae35dafc"
 # no tests
 options = ["!check"]
 
