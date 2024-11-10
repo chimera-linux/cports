99549c4d278bbc12b97e49bb9747c7fdd32becf9
psykose <alice@ayaya.dev>
2024-11-10 01:00:15 +0100

    main/c-ares: update to 1.34.3

--- main/c-ares/template.py
+++ main/c-ares/template.py
@@ -1,5 +1,5 @@
 pkgname = "c-ares"
-pkgver = "1.34.2"
+pkgver = "1.34.3"
 pkgrel = 0
 build_style = "gnu_configure"
 # circular gtest
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://c-ares.haxx.se"
 source = f"https://github.com/c-ares/c-ares/releases/download/v{pkgver}/c-ares-{pkgver}.tar.gz"
-sha256 = "35410aa625cc9ee95b66475f54aea7c81c673fb63d75fad5eee267711503b72a"
+sha256 = "26e1f7771da23e42a18fdf1e58912a396629e53a2ac71b130af93bbcfb90adbe"
 hardening = ["vis", "!cfi"]
 # does not like the sandbox
 options = ["!check"]
