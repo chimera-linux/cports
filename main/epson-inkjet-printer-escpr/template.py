c86263d60a91dba9b584664c755ae51bc999d652
triallax <triallax@tutanota.com>
2024-11-11 22:44:05 +0000

    main/epson-inkjet-printer-escpr: update to 1.8.6

--- main/epson-inkjet-printer-escpr/template.py
+++ main/epson-inkjet-printer-escpr/template.py
@@ -1,5 +1,5 @@
 pkgname = "epson-inkjet-printer-escpr"
-pkgver = "1.8.5"
+pkgver = "1.8.6"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -8,7 +8,7 @@ configure_args = [
     "--with-cupsfilterdir=/usr/lib/cups/filter",
 ]
 make_dir = "."
-hostmakedepends = ["automake", "libtool"]
+hostmakedepends = ["automake", "slibtool"]
 makedepends = ["cups-devel"]
 depends = ["cups-filters"]
 pkgdesc = "Epson Inkjet Printer Driver"
@@ -16,5 +16,5 @@ subdesc = "ESC/P-R"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://download.ebz.epson.net/dsc/search/01/search?OSC=LX&productName=B700"
-source = f"https://download3.ebz.epson.net/dsc/f/03/00/15/68/89/fbff579f15226ffcc4a16895bd6bce6842277802/epson-inkjet-printer-escpr-{pkgver}-1.tar.gz"
-sha256 = "27ca5426abece5ff1da859caa5ac4bc993b0a00e8ea74af63ffd9960c6a36a24"
+source = f"https://download3.ebz.epson.net/dsc/f/03/00/16/21/81/74d098a47c3a616713079c9cd5904b468bb33dea/epson-inkjet-printer-escpr-{pkgver}-1.tar.gz"
+sha256 = "8556d7e0e5cf7b8cb7ed2ce3bb7b957579557635f80c548dfc0d849b3dde09c4"
