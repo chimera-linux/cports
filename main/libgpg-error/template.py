0a701c17b1decb34988a4eae3cd8cfdbd8213d15
psykose <alice@ayaya.dev>
2024-11-12 00:59:41 +0100

    main/libgpg-error: update to 1.51

--- main/libgpg-error/template.py
+++ main/libgpg-error/template.py
@@ -1,15 +1,19 @@
 pkgname = "libgpg-error"
-pkgver = "1.50"
+pkgver = "1.51"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_gen = []
-hostmakedepends = ["pkgconf"]
+hostmakedepends = [
+    "automake",
+    "gettext-devel",
+    "pkgconf",
+    "slibtool",
+]
 pkgdesc = "Library for error values used by GnuPG components"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gnupg.org"
 source = f"{url}/ftp/gcrypt/libgpg-error/libgpg-error-{pkgver}.tar.bz2"
-sha256 = "69405349e0a633e444a28c5b35ce8f14484684518a508dc48a089992fe93e20a"
+sha256 = "be0f1b2db6b93eed55369cdf79f19f72750c8c7c39fc20b577e724545427e6b2"
 
 
 def post_install(self):
