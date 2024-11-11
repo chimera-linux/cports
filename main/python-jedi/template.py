38567f044632b169cb3f72cade7a50a0f822a3a5
psykose <alice@ayaya.dev>
2024-11-11 04:47:46 +0100

    main/python-jedi: update to 0.19.2

--- main/python-jedi/template.py
+++ main/python-jedi/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-jedi"
-pkgver = "0.19.1"
+pkgver = "0.19.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -18,7 +18,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://jedi.readthedocs.io/en/latest"
 source = f"$(PYPI_SITE)/j/jedi/jedi-{pkgver}.tar.gz"
-sha256 = "cf0496f3651bc65d7174ac1b7d043eff454892c708a87d1b683e57b569927ffd"
+sha256 = "4770dc3de41bde3966b02eb84fbcf557fb33cce26ad23da12c742fb50ecb11f0"
 
 
 def init_check(self):
