d2b3a306191ac02e836f3f1fd16f75653dfbbcc2
psykose <alice@ayaya.dev>
2024-11-11 07:23:46 +0100

    main/libheif: update to 1.19.2

--- main/libheif/template.py
+++ main/libheif/template.py
@@ -1,11 +1,12 @@
 pkgname = "libheif"
-pkgver = "1.18.2"
-pkgrel = 1
+pkgver = "1.19.2"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DWITH_DAV1D=ON",
     "-DWITH_JPEG_DECODER=ON",
     "-DWITH_JPEG_ENCODER=ON",
+    "-DWITH_UNCOMPRESSED_CODEC=ON",
 ]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = [
@@ -17,13 +18,14 @@ makedepends = [
     "libpng-devel",
     "libwebp-devel",
     "x265-devel",
+    "zlib-ng-compat-devel",
 ]
 pkgdesc = "HEIF and AVIF file format decoder and encoder"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-or-later"
 url = "http://www.libheif.org"
 source = f"https://github.com/strukturag/libheif/archive/v{pkgver}.tar.gz"
-sha256 = "c2bf75ff61250fcdcb831f0d5d9d06808205d9ee3dd0749ef7a7c6f85dd30dc9"
+sha256 = "c5caaed602e68799e2f4cebc243730505912d45a14ee8d650fccc7258507524a"
 hardening = ["!vis", "!cfi"]
 
 
