eee235bd303f0af9c942511b0f02932c9e5c8705
psykose <alice@ayaya.dev>
2024-11-11 07:18:13 +0100

    main/mutter: update to 47.1

--- main/mutter/template.py
+++ main/mutter/template.py
@@ -1,5 +1,5 @@
 pkgname = "mutter"
-pkgver = "47.0"
+pkgver = "47.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -70,7 +70,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Projects/Mutter"
 source = f"$(GNOME_SITE)/mutter/{pkgver.split('.')[0]}/mutter-{pkgver}.tar.xz"
-sha256 = "2d0ea90150ac6cd0219d0078db05d6e1514d6ae21bf9f3f740d4fb039129016b"
+sha256 = "905474a32cd99b34342cd69e74bb01971b387e2fa22361b6d99add109409ddc9"
 # libmutter crashes gnome-shell with some applications? FIXME debug
 hardening = ["!int"]
 # needs graphical environment
