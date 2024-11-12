33d8d2a612e8ee04edc267a3bbc6220d365a1f60
psykose <alice@ayaya.dev>
2024-11-12 22:02:49 +0100

    main/python-tomli: update to 2.1.0

--- main/python-tomli/template.py
+++ main/python-tomli/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-tomli"
-pkgver = "2.0.2"
+pkgver = "2.1.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-flit_core", "python-installer"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/hukkin/tomli"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "a3a652f16bf326ba763ada67169165daf87ff9c465e21ad8264f2657beaf5264"
+sha256 = "28acaee6066d53256a2351ad5dc9003ba25d43578b4177c421de0ece71caa103"
 
 
 def post_install(self):
