b24aed14145c3cecfef2700e1af4e8fab94edac7
psykose <alice@ayaya.dev>
2024-11-12 22:02:29 +0100

    main/alsa-ucm-conf: update to 1.2.13

--- main/alsa-ucm-conf/template.py
+++ main/alsa-ucm-conf/template.py
@@ -1,12 +1,12 @@
 pkgname = "alsa-ucm-conf"
-pkgver = "1.2.12"
+pkgver = "1.2.13"
 pkgrel = 0
 pkgdesc = "ALSA Use Case Manager topology files"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/alsa-project/alsa-ucm-conf"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "ad8dd8d79bda54f9d28b095ce9dfa009de9970daf7b57dda86216a4e4977fe4e"
+sha256 = "0b1c6740c665078a1c2818ef4902417d75bba4f0fb7fd558633707c367a727e7"
 
 
 def install(self):
