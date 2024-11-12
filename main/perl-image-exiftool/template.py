696bf3d196d3276f39daa651d643c56069b2da23
psykose <alice@ayaya.dev>
2024-11-12 21:41:56 +0100

    main/perl-image-exiftool: update to 13.03

--- main/perl-image-exiftool/template.py
+++ main/perl-image-exiftool/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-image-exiftool"
-pkgver = "13.02"
+pkgver = "13.03"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["perl"]
@@ -10,7 +10,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://exiftool.org"
 source = f"https://exiftool.org/Image-ExifTool-{pkgver}.tar.gz"
-sha256 = "b98ce4f610b7ca8b4989b94f12e1f4f40d4aa2b364d9c2915e97f41dc0f5d008"
+sha256 = "0912e1315318889574f355e5832340632c556a14d30711e94d801085ad0a8e4f"
 
 
 @subpackage("exiftool")
