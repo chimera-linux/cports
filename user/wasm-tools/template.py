527dec001ae7f16dc3303adc710eb76c71885fb8
psykose <alice@ayaya.dev>
2024-11-13 05:01:33 +0100

    user/wasm-tools: update to 1.220.0

--- user/wasm-tools/template.py
+++ user/wasm-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "wasm-tools"
-pkgver = "1.219.1"
+pkgver = "1.220.0"
 pkgrel = 0
 # wasmtime
 archs = ["aarch64", "riscv64", "x86_64"]
@@ -14,7 +14,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "Apache-2.0 WITH LLVM-exception OR Apache-2.0 OR MIT"
 url = "https://github.com/bytecodealliance/wasm-tools"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "16dfd1dfb07bf95f12459fcfd846a23b7afaff1d17ba7aa550091f0aa2f7e5ec"
+sha256 = "eba930e6bccf3c14e1e7c8da9e112bbe58439a0746d4e83208294b2993638fc7"
 # needs checked out submodules and whatever
 options = ["!check"]
 
