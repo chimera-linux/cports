353e5561e2894ee6e00f1f94bc177ecdb1e800c2
psykose <alice@ayaya.dev>
2024-11-14 01:47:50 +0100

    main/xprop: update to 1.2.8

--- main/xprop/template.py
+++ main/xprop/template.py
@@ -1,5 +1,5 @@
 pkgname = "xprop"
-pkgver = "1.2.7"
+pkgver = "1.2.8"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf", "xorg-util-macros"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/xprop-{pkgver}.tar.gz"
-sha256 = "11c06a876b0aa0bfac6cbfe4b3ebe1f5062f8b39b9b1b6c136a8629265f134b6"
+sha256 = "a8394338c99775160d733d54d73cdcdc5c9c80a0dc8cffd5be1b7c977254b745"
 hardening = ["vis", "cfi"]
 
 
