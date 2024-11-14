7a60ce06deca984ff0a9049655db094916c21ad1
psykose <alice@ayaya.dev>
2024-11-14 23:12:43 +0100

    main/qpwgraph: update to 0.8.0

--- main/qpwgraph/template.py
+++ main/qpwgraph/template.py
@@ -1,5 +1,5 @@
 pkgname = "qpwgraph"
-pkgver = "0.7.9"
+pkgver = "0.8.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -18,5 +18,4 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.freedesktop.org/rncbc/qpwgraph"
 source = f"{url}/-/archive/v{pkgver}/qpwgraph-v{pkgver}.tar.gz"
-sha256 = "4edf358f0a5126b868f5a9b8c86c4ba0e90630e2218e0e633d47c873620d6a6a"
-hardening = ["vis", "!cfi"]
+sha256 = "1b6dab91515c0c2a04bf549ac647723b70ff1506465876a969a591d37e966d7e"
