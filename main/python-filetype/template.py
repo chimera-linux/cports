b71fc670ce27270ab8c402a20a08322590dacf4c
psykose <alice@ayaya.dev>
2024-11-16 06:04:24 +0100

    main/python-filetype: new package

---
+++ main/python-filetype/template.py
@@ -0,0 +1,22 @@
+pkgname = "python-filetype"
+pkgver = "1.2.0"
+pkgrel = 0
+build_style = "python_pep517"
+make_check_args = ["--benchmark-skip"]
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+]
+checkdepends = ["python-pytest-benchmark"]
+depends = ["python"]
+pkgdesc = "Python library to infer binary file types"
+maintainer = "psykose <alice@ayaya.dev>"
+license = "MIT"
+url = "https://h2non.github.io/filetype.py"
+source = f"$(PYPI_SITE)/f/filetype/filetype-{pkgver}.tar.gz"
+sha256 = "66b56cd6474bf41d8c54660347d37afcc3f7d1970648de365c102ef77548aadb"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
