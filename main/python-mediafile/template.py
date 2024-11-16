fef3e88b17827b18ac02b18c25569b078a86fb3b
psykose <alice@ayaya.dev>
2024-11-16 06:04:24 +0100

    main/python-mediafile: update to 0.13.0

--- main/python-mediafile/template.py
+++ main/python-mediafile/template.py
@@ -1,16 +1,16 @@
 pkgname = "python-mediafile"
-pkgver = "0.12.0"
+pkgver = "0.13.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-flit_core", "python-installer"]
-depends = ["mutagen", "python-six"]
+depends = ["mutagen", "python-filetype"]
 checkdepends = ["python-pytest", *depends]
 pkgdesc = "Handles low-level interfacing for file tags in Python"
 maintainer = "Justin Berthault <justin.berthault@zaclys.net>"
 license = "MIT"
 url = "https://github.com/beetbox/mediafile"
 source = f"$(PYPI_SITE)/m/mediafile/mediafile-{pkgver}.tar.gz"
-sha256 = "d75d805a06ed56150dbcea76505e700f9809abd9e98f98117ae46f5df2ccf1d7"
+sha256 = "de71063e1bffe9733d6ccad526ea7dac8a9ce760105827f81ab0cb034c729a6d"
 
 
 def post_install(self):
