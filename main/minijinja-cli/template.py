df9977b608542c020a74803542edd998fcdcb733
psykose <alice@ayaya.dev>
2024-11-16 06:26:18 +0100

    main/minijinja-cli: update to 2.5.0
    completions is on by default so it's too much effort to disable for
    final bin, whatever

--- main/minijinja-cli/template.py
+++ main/minijinja-cli/template.py
@@ -1,10 +1,8 @@
 pkgname = "minijinja-cli"
-pkgver = "2.3.1"
+pkgver = "2.5.0"
 pkgrel = 0
-build_wrksrc = "minijinja-cli"
 build_style = "cargo"
-# cargo install rebuilds without these args in install to not ship feature at runtime
-make_build_args = ["--features=completions"]
+make_build_args = ["--bin", "minijinja-cli"]
 make_build_env = {
     "ASSET_OUT_DIR": "assets",
 }
@@ -15,12 +13,13 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0"
 url = "https://github.com/mitsuhiko/minijinja"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "c933a1d9f53de26bb776a61b9346e0b202c332b4ff1309db5d0178a9b603ca06"
+sha256 = "63e9f1ece32cc7edea5fc762e3bfe48571f71ec3b112cc8f7b0c1a1619dab81e"
 
 
-def post_install(self):
-    self.install_man("assets/man/minijinja-cli.1")
-    with self.pushd("assets/completions"):
+def install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/minijinja-cli")
+    self.install_man("minijinja-cli/assets/man/minijinja-cli.1")
+    with self.pushd("minijinja-cli/assets/completions"):
         self.install_completion("minijinja-cli.bash", "bash")
         self.install_completion("minijinja-cli.fish", "fish")
         self.install_completion("_minijinja-cli", "zsh")
