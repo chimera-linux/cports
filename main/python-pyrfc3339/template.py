db01b66f7d4becd5c14ce61145e5da85fc86aa29
psykose <alice@ayaya.dev>
2024-11-16 06:14:27 +0100

    main/python-pyrfc3339: update to 2.0.1

--- main/python-pyrfc3339/template.py
+++ main/python-pyrfc3339/template.py
@@ -1,22 +1,23 @@
 pkgname = "python-pyrfc3339"
-pkgver = "1.1"
+pkgver = "2.0.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
-depends = ["python-pytz"]
+depends = ["python"]
+checkdepends = ["python-pytest"]
 pkgdesc = "Generate and parse RFC 3339 timestamps"
 maintainer = "Duncan Bellamy <dunk@denkimushi.com>"
 license = "MIT"
 url = "https://github.com/kurtraschke/pyRFC3339"
-source = f"$(PYPI_SITE)/p/pyRFC3339/pyRFC3339-{pkgver}.tar.gz"
-sha256 = "81b8cbe1519cdb79bed04910dd6fa4e181faf8c88dff1e1b987b5f7ab23a5b1a"
+# pypi has no tests
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "b608e002080bf3cad923d1c1c74078954b383ba509adbf05bee1d2915fd5b43a"
 # tests require nose
-options = ["!check"]
+# options = ["!check"]
 
 
 def post_install(self):
