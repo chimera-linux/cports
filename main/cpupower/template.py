53ae912d45dc859d9d6f69400a770126814b8159
psykose <alice@ayaya.dev>
2024-11-17 19:57:09 +0100

    main/cpupower: update to 6.11.9

--- main/cpupower/template.py
+++ main/cpupower/template.py
@@ -1,5 +1,5 @@
 pkgname = "cpupower"
-pkgver = "6.11.8"
+pkgver = "6.11.9"
 pkgrel = 0
 build_style = "makefile"
 make_build_target = "cpupower"
@@ -34,7 +34,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[:pkgver.find('.')]}.x/linux-{pkgver}.tar.xz"
-sha256 = "aee8a844fe152420bece70ffae8525b9b23089aa4da31fa32f30e1859bf93c3d"
+sha256 = "75658a7aa3bd9598c96ee1e5862c5e1d34fced75c28d825c727a1510a6f384b4"
 # nope
 options = ["!check"]
 
