8202e2b94bf46361468a5bd9f1b49679c6a6fd16
Denis Strizhkin <strdenis02@gmail.com>
2024-11-17 19:35:08 +0300

    main/gnome-remote-desktop: new package

---
+++ main/gnome-remote-desktop/template.py
@@ -0,0 +1,36 @@
+pkgname = "gnome-remote-desktop"
+pkgver = "47.1"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dvnc=true", "-Dsystemd=false", "-Dtests=false"]
+hostmakedepends = ["cmake", "meson", "pkgconf", "asciidoc", "gettext"]
+makedepends = [
+    "cairo-devel",
+    "dbus-devel",
+    "fdk-aac-devel",
+    "ffmpeg-devel",
+    "nv-codec-headers",
+    "freerdp-devel",
+    "fuse-devel",
+    "glib-devel",
+    "libdrm-devel",
+    "libei-devel",
+    "libepoxy-devel",
+    "libgudev-devel",
+    "libnotify-devel",
+    "libsecret-devel",
+    "libvncserver-devel",
+    "libxkbcommon-devel",
+    "opus-devel",
+    "pipewire-devel",
+    "polkit-devel",
+    "tpm2-tss-devel",
+]
+pkgdesc = "GNOME Remote Desktop server"
+maintainer = "Denis Strizhkin <strdenis02@gmail.com>"
+license = "GPL-2.0-or-later"
+url = "https://gitlab.gnome.org/GNOME/gnome-remote-desktop"
+source = f"{url}/-/archive/{pkgver}/gnome-remote-desktop-{pkgver}.tar.gz"
+sha256 = "bfdfc646947aaac02e7c3230386a34872f501d6ddd66484154536949f51bf1f1"
+# needs real /sys /proc for some udev things to work
+options = ["!check"]
