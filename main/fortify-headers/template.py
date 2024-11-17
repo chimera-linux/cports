4bf84a8e052f092e3ad21ce4ef688460c55be171
psykose <alice@ayaya.dev>
2024-11-17 05:33:39 +0100

    main/fortify-headers: update to 2.3.3

--- main/fortify-headers/template.py
+++ main/fortify-headers/template.py
@@ -1,6 +1,6 @@
 pkgname = "fortify-headers"
-pkgver = "2.3.1"
-pkgrel = 1
+pkgver = "2.3.3"
+pkgrel = 0
 build_style = "makefile"
 make_check_target = "clang"
 make_check_args = ["-C", "tests", "run"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "0BSD"
 url = "https://git.2f30.org/fortify-headers"
 source = f"https://github.com/jvoisin/fortify-headers/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "531a029fb5ff3c266f472d0aad74f750a40e15d476cf80c67ffb39c07f34a0a7"
+sha256 = "26d81fc55b8ce3db22c7a697616392aeba928e921d975053a3f00221d1a33c08"
 tool_flags = {"CFLAGS": ["-Wno-macro-redefined"]}
 options = ["bootstrap"]
 
