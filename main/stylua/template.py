85d801ae66cbd340e37cd1cdb8d936df054c3110
psykose <alice@ayaya.dev>
2024-11-17 18:19:22 +0100

    main/stylua: update to 2.0.0

--- main/stylua/template.py
+++ main/stylua/template.py
@@ -1,10 +1,10 @@
 pkgname = "stylua"
-pkgver = "0.20.0"
-pkgrel = 1
+pkgver = "2.0.0"
+pkgrel = 0
 build_style = "cargo"
 make_build_args = [
     "--features",
-    "lua54,luau,fromstr,serialize",
+    "lua54,luajit,luau,fromstr,serialize",
 ]
 make_install_args = [*make_build_args]
 make_check_args = [*make_build_args]
@@ -15,7 +15,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MPL-2.0"
 url = "https://github.com/JohnnyMorganz/StyLua"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f4a27b12669953d2edf55b89cc80381f97a2dfa735f53f95c6ae6015c8c35ffb"
+sha256 = "6222cb07ec22963cce2ff2cb0a64f8a5df59d9aa434191610948ad3607d668e0"
 
 
 def install(self):
