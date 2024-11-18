9686f8523803182a022eb2e80329a0afff22ebce
q66 <q66@chimera-linux.org>
2024-11-18 21:28:33 +0100

    main/dinit: update to latest git
    This brings support for capabilities and a few other features.

--- main/dinit/template.py
+++ main/dinit/template.py
@@ -1,18 +1,21 @@
 pkgname = "dinit"
 pkgver = "0.19.1"
 # temporary so we get our features
-_gitrev = "f883bcb2eabe6c2646bc74647a3aeaece09a9875"
-pkgrel = 1
+_gitrev = "29c189ac8a12aa1c78e4bfd37b6c5984a9f033da"
+pkgrel = 2
 build_style = "configure"
 configure_args = ["--sbindir=/usr/bin", "--syscontrolsocket=/run/dinitctl"]
 make_check_args = ["check-igr"]  # additional target
+makedepends = ["libcap-devel"]
 pkgdesc = "Service manager and init system"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://davmac.org/projects/dinit"
 source = f"https://github.com/davmac314/dinit/archive/{_gitrev}.tar.gz"
-sha256 = "1dd190e8fb906dfa80316365f0f6fac03113a134763b794fa625183ab8b74878"
-tool_flags = {"CXXFLAGS": ["-fno-rtti"]}
+sha256 = "2d9901722643282827625d4a9ef77af025116c8c4ba17621a3ac3a98c9ed0b8a"
+# hand-rolled configure scripts/makefiles lol
+# drop the -lcap later when fixed upstream
+tool_flags = {"CXXFLAGS": ["-fno-rtti"], "LDFLAGS": ["-lcap"]}
 hardening = ["vis", "cfi"]
 
 
