809e6e36d614fa582a3ea472b6072e69c635e57f
psykose <alice@ayaya.dev>
2024-11-18 11:51:22 +0100

    main/python-setproctitle: update to 1.3.4

--- main/python-setproctitle/template.py
+++ main/python-setproctitle/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-setproctitle"
-pkgver = "1.3.3"
-pkgrel = 1
+pkgver = "1.3.4"
+pkgrel = 0
 build_style = "python_pep517"
 make_check_target = "tests"
 hostmakedepends = [
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/dvarrazzo/py-setproctitle"
 source = f"$(PYPI_SITE)/s/setproctitle/setproctitle-{pkgver}.tar.gz"
-sha256 = "c913e151e7ea01567837ff037a23ca8740192880198b7fbb90b16d181607caae"
+sha256 = "3b40d32a3e1f04e94231ed6dfee0da9e43b4f9c6b5450d53e6dd7754c34e0c50"
 # can't find itself
 options = ["!check"]
 
