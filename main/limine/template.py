83c89cfdf215d321f9318a8252c262522e0f245a
q66 <q66@chimera-linux.org>
2024-11-18 16:53:17 +0100

    main/linux-stable: update to 6.12.1

--- main/limine/template.py
+++ main/limine/template.py
@@ -13,7 +13,7 @@ url = "https://limine-bootloader.org"
 source = f"https://github.com/limine-bootloader/limine/releases/download/v{pkgver}/limine-{pkgver}.tar.zst"
 sha256 = "f1fb33ff84286fdd88f3327a28426997b4461c53b141ca275a79916f3e8789e2"
 # no test suite
-options = ["!check"]
+options = ["!check", "!debug"]
 
 
 def post_install(self):
--- main/linux-stable/template.py
+++ main/linux-stable/template.py
@@ -1,6 +1,6 @@
 # update linux-stable-zfs-bin when bumping
 pkgname = "linux-stable"
-pkgver = "6.11.9"
+pkgver = "6.12.1"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]
 make_dir = "build"
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
-sha256 = "75658a7aa3bd9598c96ee1e5862c5e1d34fced75c28d825c727a1510a6f384b4"
+sha256 = "0193b1d86dd372ec891bae799f6da20deef16fc199f30080a4ea9de8cef0c619"
 # no meaningful checking to be done
 options = [
     "!check",
