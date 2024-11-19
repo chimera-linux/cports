dc71c1d378a4c934c9e392026e62d6ccbebcec09
psykose <alice@ayaya.dev>
2024-11-19 20:47:06 +0100

    main/ncdu: update to 1.21

--- main/ncdu/template.py
+++ main/ncdu/template.py
@@ -1,6 +1,6 @@
 pkgname = "ncdu"
-pkgver = "1.20"
-pkgrel = 1
+pkgver = "1.21"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
     "automake",
@@ -12,7 +12,7 @@ maintainer = "psykose <alice@ayaya.dev>"
 license = "MIT"
 url = "https://dev.yorhel.nl/ncdu"
 source = f"https://dev.yorhel.nl/download/ncdu-{pkgver}.tar.gz"
-sha256 = "5fe2bb841abe72374bb242dbb93293c4ae053078432d896a7481b2ff10be9572"
+sha256 = "a894d3a9b46bce578a6039bef48f54533ec402fb589b0769bfbb1d1edf9601a6"
 hardening = ["vis", "cfi"]
 
 
