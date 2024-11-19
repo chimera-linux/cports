7e3c838da8d98bc7070aeeb21ee656b6fbf4578a
Denis Strizhkin <strdenis02@gmail.com>
2024-11-19 04:52:35 +0300

    install sysusers and tmpfiles

--- main/gnome-remote-desktop/template.py
+++ main/gnome-remote-desktop/template.py
@@ -2,7 +2,12 @@ pkgname = "gnome-remote-desktop"
 pkgver = "47.1"
 pkgrel = 0
 build_style = "meson"
-configure_args = ["-Dvnc=true", "-Dsystemd=false", "-Dtests=false"]
+configure_args = [
+    "-Dvnc=true",
+    "-Dsystemd=false",
+    "-Dtests=false",
+    "-Duser=_gnome-remote-desktop",
+]
 hostmakedepends = ["cmake", "meson", "pkgconf", "asciidoc", "gettext"]
 makedepends = [
     "cairo-devel",
@@ -34,3 +39,12 @@ source = f"{url}/-/archive/{pkgver}/gnome-remote-desktop-{pkgver}.tar.gz"
 sha256 = "bfdfc646947aaac02e7c3230386a34872f501d6ddd66484154536949f51bf1f1"
 # needs real /sys /proc for some udev things to work
 options = ["!check"]
+
+
+def post_install(self):
+    self.install_sysusers(
+        self.cwd / "build/data/gnome-remote-desktop-sysusers.conf"
+    )
+    self.install_tmpfiles(
+        self.cwd / "build/data/gnome-remote-desktop-tmpfiles.conf"
+    )
