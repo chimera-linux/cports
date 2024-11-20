2bbd3cb771b9dc1c777e7837b76746cf9f029ffe
psykose <alice@ayaya.dev>
2024-11-20 10:27:10 +0100

    main/python-pytest-rerunfailures: update to 15.0

--- main/python-pytest-rerunfailures/template.py
+++ main/python-pytest-rerunfailures/template.py
@@ -1,15 +1,14 @@
 pkgname = "python-pytest-rerunfailures"
-pkgver = "14.0"
-pkgrel = 1
+pkgver = "15.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
-depends = ["python"]
-checkdepends = ["python-pytest"]
+depends = ["python-pytest"]
+checkdepends = [*depends]
 pkgdesc = "Pytest plugin to rerun failed tests multiple times"
 maintainer = "psykose <alice@ayaya.dev>"
 license = "MPL-2.0"
@@ -17,6 +16,6 @@ url = "https://github.com/pytest-dev/pytest-rerunfailures"
 source = (
     f"$(PYPI_SITE)/p/pytest-rerunfailures/pytest-rerunfailures-{pkgver}.tar.gz"
 )
-sha256 = "4a400bcbcd3c7a4ad151ab8afac123d90eca3abe27f98725dc4d9702887d2e92"
+sha256 = "2d9ac7baf59f4c13ac730b47f6fa80e755d1ba0581da45ce30b72fb3542b4474"
 # fail with pytest 8.2
 options = ["!check"]
