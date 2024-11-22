df6660f5333846d4b102f741fb725e9f39160c0d
ttyyls <contact@behri.org>
2024-11-22 18:17:07 +0100

    user/perl-file-basedir: new package

---
+++ user/perl-file-basedir/template.py
@@ -0,0 +1,16 @@
+pkgname = "perl-file-basedir"
+pkgver = "0.09"
+pkgrel = 0
+build_style = "perl_module"
+hostmakedepends = ["perl"]
+makedepends = ["perl"]
+depends = [
+    "perl-ipc-system-simple",
+    "perl-file-which",
+]
+checkdepends = [*depends]
+pkgdesc = "Parses streams to create MIME entities"
+license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
+url = "https://metacpan.org/dist/File-BaseDir"
+source = f"$(CPAN_SITE)/File/File-BaseDir-{pkgver}.tar.gz"
+sha256 = "6da6f7281562ac8f11ef1a3af6aedb51c41182b60f1f122ced0079efd92967d9"
