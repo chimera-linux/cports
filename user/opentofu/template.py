c6eb625810fbd3d2cf752c20581a105d2231116c
psykose <alice@ayaya.dev>
2024-11-22 12:10:38 +0100

    user/opentofu: update to 1.8.5

--- user/opentofu/template.py
+++ user/opentofu/template.py
@@ -1,6 +1,6 @@
 pkgname = "opentofu"
-pkgver = "1.8.4"
-pkgrel = 1
+pkgver = "1.8.5"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X main.version={pkgver} -X github.com/opentofu/opentofu/version.dev=no",
@@ -12,4 +12,4 @@ maintainer = "Gabriel M. Dutra <dmdutra@proton.me>"
 license = "MPL-2.0"
 url = "https://github.com/opentofu/opentofu"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "89eb05e7ed1b20e3d0c799758b2cfc8d6dc212e8a09b20d9eaf03017be4d74e6"
+sha256 = "07613c3b7d6c0a7c3ede29da6a4f33d764420326c07a1c41e52e215428858ef4"
