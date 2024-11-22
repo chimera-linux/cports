b7da016559432ccaa24c231e1dfd04a92cd94e03
psykose <alice@ayaya.dev>
2024-11-22 11:14:22 +0100

    user/postgresql16: update to 16.6

--- user/postgresql16/template.py
+++ user/postgresql16/template.py
@@ -1,7 +1,7 @@
 pkgname = "postgresql16"
 _major = pkgname.removeprefix("postgresql")
-pkgver = f"{_major}.5"
-pkgrel = 1
+pkgver = f"{_major}.6"
+pkgrel = 0
 # NOTE: version 16 doesn't work with meson + tarball
 # switch to meson for version 17
 build_style = "gnu_configure"
@@ -48,7 +48,7 @@ url = "https://www.postgresql.org"
 source = [
     f"https://ftp.postgresql.org/pub/source/v{pkgver}/postgresql-{pkgver}.tar.bz2"
 ]
-sha256 = ["a6cbbb7037f98cb8afa7d3970b7c48040cf02b115e39253a0c037a8bb8e778f0"]
+sha256 = ["23369cdaccd45270ac5dcc30fa9da205d5be33fa505e1f17a0418d2caeca477b"]
 # checks depend on libpq already being installed
 options = ["!check"]
 
