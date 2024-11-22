71a6e10a972a2780c77a48ac9bccaef7c76e4f0c
psykose <alice@ayaya.dev>
2024-11-22 11:03:41 +0100

    main/localsearch: update to 3.8.1

--- main/localsearch/template.py
+++ main/localsearch/template.py
@@ -1,6 +1,6 @@
 pkgname = "localsearch"
-pkgver = "3.8.0"
-pkgrel = 1
+pkgver = "3.8.1"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     # TODO: user services with dinit?
@@ -68,7 +68,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gnome.pages.gitlab.gnome.org/tinysparql"
 source = f"$(GNOME_SITE)/localsearch/{pkgver[:-2]}/localsearch-{pkgver}.tar.xz"
-sha256 = "e4e83a1daebb5263915b45b719731e3f7041d959cbd637ea29afbe910c92bbf9"
+sha256 = "a7b24a4f07805df7543a4dd023684fcde5ee699ca00eb5b09123a049d3aeccd8"
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=0x200000"]}
 # check relies on stuff unsupported in chroot
 options = ["!check", "!cross"]
