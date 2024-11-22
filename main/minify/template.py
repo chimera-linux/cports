c16f11d895274f9f3deafac60f42d23c11ed7452
psykose <alice@ayaya.dev>
2024-11-22 12:17:27 +0100

    main/minify: update to 2.21.1

--- main/minify/template.py
+++ main/minify/template.py
@@ -1,6 +1,6 @@
 pkgname = "minify"
-pkgver = "2.20.37"
-pkgrel = 5
+pkgver = "2.21.1"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/minify"]
 hostmakedepends = ["go"]
@@ -9,7 +9,7 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "MIT"
 url = "https://github.com/tdewolff/minify"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "33d9bf68d416c9e256412d5045562371bd202f1dabeb111c17bf914b71cabedb"
+sha256 = "076ea8bb067f2a17036fa4b37ccc42648090ba3ffadfc5d57a469d289b19d354"
 
 
 def post_install(self):
