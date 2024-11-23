27e2621e3185958e9701bd0fb163426065838bb1
ttyyls <contact@behri.org>
2024-11-23 09:30:09 +0100

    user/perl-file-which: new package

---
+++ user/perl-file-which/template.py
@@ -0,0 +1,12 @@
+pkgname = "perl-file-which"
+pkgver = "1.27"
+pkgrel = 0
+build_style = "perl_module"
+hostmakedepends = ["perl"]
+makedepends = ["perl"]
+depends = ["perl"]
+pkgdesc = "Perl implementation of the which utility as an API"
+license = "GPL-1.0-or-later OR Artistic-1.0-Perl"
+url = "https://metacpan.org/pod/File-Which"
+source = f"https://cpan.metacpan.org/authors/id/P/PL/PLICEASE/File-Which-{pkgver}.tar.gz"
+sha256 = "3201f1a60e3f16484082e6045c896842261fc345de9fb2e620fd2a2c7af3a93a"
