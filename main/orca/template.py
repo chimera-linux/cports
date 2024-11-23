2ac5ef44c8b07c2e8060c95c331dbfa3f3f8d95f
psykose <alice@ayaya.dev>
2024-11-23 21:26:42 +0100

    main/orca: update to 47.2

--- main/orca/template.py
+++ main/orca/template.py
@@ -1,5 +1,5 @@
 pkgname = "orca"
-pkgver = "47.1"
+pkgver = "47.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -32,4 +32,4 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "LGPL-2.1-or-later"
 url = "https://orca.gnome.org"
 source = f"$(GNOME_SITE)/orca/{pkgver[:-2]}/orca-{pkgver}.tar.xz"
-sha256 = "d07d7acde8565546973a9f29730732f31726ab6bc9dcccead2e1204d7d8044ed"
+sha256 = "5e67af357f7198ea00a44381cab4c4f94e6826d6ed800668f39416ce2aab8e5a"
