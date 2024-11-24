deaba3b5e2d0dcfb9c55518da78f1598e9b87c42
q66 <q66@chimera-linux.org>
2024-11-24 02:18:46 +0100

    main/zfs: disable sha256 accel on pre-power8 ppc

--- main/zfs/template.py
+++ main/zfs/template.py
@@ -2,7 +2,7 @@
 # also update linux-*-zfs-bin
 pkgname = "zfs"
 pkgver = "2.2.6"
-pkgrel = 2
+pkgrel = 3
 build_style = "gnu_configure"
 configure_args = [
     "--libexecdir=/usr/lib",  # XXX drop libexec
