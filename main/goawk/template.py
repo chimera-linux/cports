181f054fce690be91dbd6822115ff6ad84159854
psykose <alice@ayaya.dev>
2024-11-25 09:57:13 +0100

    main/goawk: update to 1.29.1

--- main/goawk/template.py
+++ main/goawk/template.py
@@ -1,6 +1,6 @@
 pkgname = "goawk"
-pkgver = "1.29.0"
-pkgrel = 1
+pkgver = "1.29.1"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 checkdepends = ["gawk"]
@@ -9,7 +9,7 @@ maintainer = "Paul A. Patience <paul@apatience.com>"
 license = "MIT"
 url = "https://github.com/benhoyt/goawk"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "91bf708ad6776bea7e1fbd1a0969042de8ebc3139b89468a2a1f5405f3401f54"
+sha256 = "9c355857faf7168f99e78d090ebe993ff10732a5ff34735cdc3e86256ce8c989"
 
 
 def post_install(self):
