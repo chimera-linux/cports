ebfed1cd5c1c09d2b2a0c711f904f62d63b4bd69
Erica Z <zerica@callcc.eu>
2024-11-26 10:31:56 +0100

    main/milkytracker: update to 1.05.00

--- main/milkytracker/template.py
+++ main/milkytracker/template.py
@@ -1,5 +1,5 @@
 pkgname = "milkytracker"
-pkgver = "1.04.00"
+pkgver = "1.05.00"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -14,4 +14,4 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-3.0-or-later"
 url = "https://milkytracker.github.io"
 source = f"https://github.com/milkytracker/MilkyTracker/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "29b9c9572ad8bf8f4add2de19c3f8fb0382738763a92e76f3d01dea82c40ff72"
+sha256 = "370a33057025a15077cb71d0765f32caa782d760ded94aa495f3019541c32af5"
