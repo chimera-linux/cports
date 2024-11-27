cbf97b18f4f6031fdc7c3a30c65165b9a320ffcf
metalparade <comer@live.cn>
2024-11-27 14:07:32 +0800

    user/ibus-libpinyin: new package

---
+++ user/ibus-libpinyin/template.py
@@ -0,0 +1,31 @@
+pkgname = "ibus-libpinyin"
+pkgver = "1.15.8"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+    "--enable-cloud-input-mode",
+    "--enable-opencc",
+    "--enable-boost",
+]
+hostmakedepends = ["intltool", "libtool", "pkgconf", "automake", "gsed"]
+makedepends = [
+    "gettext-devel",
+    "sqlite-devel",
+    "libuuid-devel",
+    "opencc-devel",
+    "lua5.4-devel",
+    "ibus-devel",
+    "libpinyin-devel",
+    "libsoup-devel",
+    "json-glib-devel",
+    "boost-devel",
+    "python-gobject-devel",
+]
+depends = ["ibus"]
+pkgdesc = "Intelligent Pinyin engine based on libpinyin for IBus"
+maintainer = "metalparade <comer@live.cn>"
+license = "GPL-3.0-or-later"
+url = "https://github.com/libpinyin/ibus-libpinyin"
+source = f"{url}/releases/download/{pkgver}/ibus-libpinyin-{pkgver}.tar.gz"
+sha256 = "1d32eb82a09bc043da0e2b65849bc61c8820b99f30a8cff10d807a1c44848bfa"
+exec_wrappers = [("/usr/bin/gsed", "sed")]
