791a46dc40cb1d69c68acc4edfceed5e7bf64bb8
q66 <q66@chimera-linux.org>
2024-11-30 02:05:46 +0100

    main/*: rebuild the rest against new rust and llvm 19

--- main/fmt/template.py
+++ main/fmt/template.py
@@ -1,6 +1,6 @@
 pkgname = "fmt"
 pkgver = "11.0.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 configure_args = [
     "-DBUILD_SHARED_LIBS=ON",
--- main/rust-bindgen/template.py
+++ main/rust-bindgen/template.py
@@ -1,6 +1,6 @@
 pkgname = "rust-bindgen"
 pkgver = "0.70.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "cargo"
 make_build_args = ["--bins"]
 make_install_args = ["--bins"]
