a7dc72be5cd20ed12ef9f265f5c52844a8f4e202
ttyyls <contact@behri.org>
2024-12-02 14:40:53 +0100

    main/pdfjs: update to 4.9.124

--- main/pdfjs/template.py
+++ main/pdfjs/template.py
@@ -1,12 +1,12 @@
 pkgname = "pdfjs"
-pkgver = "4.8.69"
+pkgver = "4.9.124"
 pkgrel = 0
 pkgdesc = "Platform for parsing and rendering PDFs"
 maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/mozilla/pdf.js"
 source = f"{url}/releases/download/v{pkgver}/pdfjs-{pkgver}-dist.zip"
-sha256 = "887a6e5f0cda2136daf99cc5ae5fe34581c7bfef2ce64f5c27963d965d88d2c6"
+sha256 = "e871dead8a5328f03a682216fdef805673c73fdbb172f984ef96a45d907cf199"
 # no tests defined
 options = ["!check"]
 
