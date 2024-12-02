8219978766a545796fb3ae403de435d42dae2ba1
q66 <q66@chimera-linux.org>
2024-12-02 19:12:50 +0100

    main/webkitgtk*: update to 2.46.4

--- main/webkitgtk/template.py
+++ main/webkitgtk/template.py
@@ -1,5 +1,5 @@
 pkgname = "webkitgtk"
-pkgver = "2.46.3"
+pkgver = "2.46.4"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -94,7 +94,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later AND BSD-2-Clause"
 url = "https://webkitgtk.org"
 source = f"{url}/releases/webkitgtk-{pkgver}.tar.xz"
-sha256 = "85e09fa6ff9fea49678ba9975dbc64ea3242833f8f8a7d6a8937b2f292fcb28d"
+sha256 = "0eff5f0ab0a2872ec87df62bc32e3289c8af625716ac71e94b298d74e0374176"
 debug_level = 1  # otherwise LTO link runs out of memory + fat debuginfo
 tool_flags = {
     "CFLAGS": ["-DNDEBUG"],
