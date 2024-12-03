f5da591c11bb2324d8104d761f42c4961eea632f
q66 <q66@chimera-linux.org>
2024-12-03 22:28:29 +0100

    main/wireplumber: update to 0.5.7

--- main/wireplumber/template.py
+++ main/wireplumber/template.py
@@ -1,6 +1,6 @@
 pkgname = "wireplumber"
-pkgver = "0.5.6"
-pkgrel = 1
+pkgver = "0.5.7"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dsystem-lua=true",
@@ -24,7 +24,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pipewire.pages.freedesktop.org/wireplumber"
 source = f"https://gitlab.freedesktop.org/pipewire/wireplumber/-/archive/{pkgver}/wireplumber-{pkgver}.tar.gz"
-sha256 = "ce7b7217d880bed1438e408ea412716a259cb46b09f597bfd652a577dc60185c"
+sha256 = "8cd43a582f68368ee5a915e97bf9dadc44d2dfe8c7bb4f96bb7b40ea2ed7848f"
 
 
 def post_install(self):
