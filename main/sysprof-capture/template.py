cca9cb11f803118ccaa6c7dd2b48fb5278405410
q66 <q66@chimera-linux.org>
2024-12-03 22:07:40 +0100

    main/sysprof: update to 47.2

--- main/sysprof-capture/template.py
+++ main/sysprof-capture/template.py
@@ -1,6 +1,6 @@
 # sync with main/sysprof
 pkgname = "sysprof-capture"
-pkgver = "47.1"
+pkgver = "47.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -22,7 +22,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "BSD-2-Clause-Patent"
 url = "https://www.sysprof.com"
 source = f"$(GNOME_SITE)/sysprof/{'.'.join(pkgver.rsplit('.')[:-1])}/sysprof-{pkgver}.tar.xz"
-sha256 = "d3baba814be0cdaef28704c8004ff3110e99525422060f2a993a22a47b9334eb"
+sha256 = "e4b5ede9fd978ec3f0d5a0d44d0429a6d201c362bf6cb4527319031ae462c54f"
 # sysprof`sysprof_disk_usage_record_fiber muloverflow when busy i/o
 hardening = ["!int"]
 # static lib only
