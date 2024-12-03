7f07fc7e6eaf3103b3626ede9b377f09b2279ced
q66 <q66@chimera-linux.org>
2024-12-03 21:26:25 +0100

    main/kexec-tools: update to 2.0.30

--- main/kexec-tools/template.py
+++ main/kexec-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "kexec-tools"
-pkgver = "2.0.29"
+pkgver = "2.0.30"
 pkgrel = 0
 archs = ["aarch64", "armhf", "armv7", "ppc64", "ppc64le", "x86_64"]
 build_style = "gnu_configure"
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org/pub/linux/utils/kernel/kexec"
 source = f"$(KERNEL_SITE)/utils/kernel/kexec/kexec-tools-{pkgver}.tar.xz"
-sha256 = "67b1ac503aade45a54db0bc79228a8830a35d5d4f83cee932cff3e7a81a4a9ec"
+sha256 = "748a5fa7af27a8059d1de907bddfc32ac530c7189b5770c83e6f3d898d82124e"
 hardening = ["vis", "cfi"]
 # no tests
 options = ["!check"]
