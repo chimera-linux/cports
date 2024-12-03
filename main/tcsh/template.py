10c982df3e3f7311e1aa439ca84aab98dd073b55
q66 <q66@chimera-linux.org>
2024-12-03 21:41:48 +0100

    main/tcsh: update to 6.24.14

--- main/tcsh/template.py
+++ main/tcsh/template.py
@@ -1,5 +1,5 @@
 pkgname = "tcsh"
-pkgver = "6.24.13"
+pkgver = "6.24.14"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-nls"]
@@ -10,7 +10,7 @@ maintainer = "tulilirockz <tulilirockz@outlook.com>"
 license = "BSD-2-Clause"
 url = "https://astron.com/pub/tcsh"
 source = f"{url}/tcsh-{pkgver}.tar.gz"
-sha256 = "1e927d52e9c85d162bf985f24d13c6ccede9beb880d86fec492ed15480a5c71a"
+sha256 = "36880f258a63fc11fe72a65098b585ebc4ecdee24388b8ebec97e6ae8e485318"
 
 
 def post_install(self):
