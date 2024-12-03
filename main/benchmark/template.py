814974573d79072c39bf369d99a9e2dd51a78ede
q66 <q66@chimera-linux.org>
2024-12-03 21:19:17 +0100

    main/benchmark: update to 1.9.1

--- main/benchmark/template.py
+++ main/benchmark/template.py
@@ -1,5 +1,5 @@
 pkgname = "benchmark"
-pkgver = "1.9.0"
+pkgver = "1.9.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -20,7 +20,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/google/benchmark"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "35a77f46cc782b16fac8d3b107fbfbb37dcd645f7c28eee19f3b8e0758b48994"
+sha256 = "32131c08ee31eeff2c8968d7e874f3cb648034377dfc32a4c377fa8796d84981"
 
 
 @subpackage("benchmark-devel")
