d197960a45285c964f1e040f84ac40140296823e
q66 <q66@chimera-linux.org>
2024-12-03 21:51:09 +0100

    main/blake3: update to 1.5.5

--- main/blake3/template.py
+++ main/blake3/template.py
@@ -1,6 +1,6 @@
 pkgname = "blake3"
-pkgver = "1.5.4"
-pkgrel = 1
+pkgver = "1.5.5"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
 cmake_dir = "c"
@@ -14,7 +14,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "CC0-1.0 OR Apache-2.0"
 url = "https://github.com/BLAKE3-team/BLAKE3"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "ddd24f26a31d23373e63d9be2e723263ac46c8b6d49902ab08024b573fd2a416"
+sha256 = "6feba0750efc1a99a79fb9a495e2628b5cd1603e15f56a06b1d6cb13ac55c618"
 hardening = ["vis", "cfi"]
 
 
