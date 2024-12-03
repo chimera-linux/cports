618c25c5ca5d29bffba1902e7828039e82b8fa07
q66 <q66@chimera-linux.org>
2024-12-03 22:09:50 +0100

    main/gnome-control-center: update to 47.2

--- main/gnome-control-center/template.py
+++ main/gnome-control-center/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-control-center"
-pkgver = "47.1.1"
+pkgver = "47.2"
 pkgrel = 0
 build_style = "meson"
 make_check_wrapper = ["wlheadless-run", "--"]
@@ -68,7 +68,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-control-center"
 source = f"$(GNOME_SITE)/gnome-control-center/{pkgver.split('.')[0]}/gnome-control-center-{pkgver}.tar.xz"
-sha256 = "051fd4057157f4b2337013fbefc96e3d129654e3fc960d4849d354252400be77"
+sha256 = "434a322dc3743858b89d8165d89f89dc05968b6b7be3400944ced4271250d3e9"
 
 
 @subpackage("gnome-control-center-devel")
