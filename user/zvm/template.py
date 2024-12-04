09638048cbb0242b61cf6f2eb544eef6cb4b422d
ttyyls <contact@behri.org>
2024-12-04 12:45:37 +0100

    user/zvm: update to 0.8.2

--- user/zvm/template.py
+++ user/zvm/template.py
@@ -1,5 +1,5 @@
 pkgname = "zvm"
-pkgver = "0.8.1"
+pkgver = "0.8.2"
 pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
@@ -8,7 +8,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "MIT"
 url = "https://github.com/tristanisham/zvm"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "9f822942e125159176d25f742bf11e8af2827ba7840d9f3f8db9491451a9722f"
+sha256 = "cf681dc7cf67994094c7ac4c0c7075d972d5382d94fe4873e4722e195380cb61"
 
 
 def post_install(self):
