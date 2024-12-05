395f9f8c5fc8b13866392f4c56772e680abbe503
Earldridge Jazzed Pineda <earldridgejazzedpineda@gmail.com>
2024-12-05 22:58:58 +0800

    user/jj: update to 0.24.0

--- user/jj/template.py
+++ user/jj/template.py
@@ -1,5 +1,5 @@
 pkgname = "jj"
-pkgver = "0.23.0"
+pkgver = "0.24.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = [
@@ -19,7 +19,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://martinvonz.github.io/jj"
 source = f"https://github.com/martinvonz/jj/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "18e0cc5600c06e940defce0a23ab4b29885bac265f94176603e5f8f926ed000e"
+sha256 = "c0e92ec25b7500deec2379a95ab655c6c92021cf4ccb29511fee2377e37b35d6"
 # generates completions with host binary
 options = ["!cross"]
 
