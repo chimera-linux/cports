e3021abb58a9f9ca8e0048622664f2dcdf245efa
triallax <triallax@tutanota.com>
2024-12-05 23:45:03 +0000

    main/sslscan: update to 2.1.6

--- main/sslscan/template.py
+++ main/sslscan/template.py
@@ -1,5 +1,5 @@
 pkgname = "sslscan"
-pkgver = "2.1.5"
+pkgver = "2.1.6"
 pkgrel = 0
 build_style = "makefile"
 make_build_args = [f"GIT_VERSION={pkgver}"]
@@ -9,7 +9,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-3.0-or-later WITH custom:OpenSSL-exception"
 url = "https://github.com/rbsec/sslscan"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "f1fa33deba4033745cdfda5d0674f4f6e9f572548062737acc04b3e972edc43f"
+sha256 = "5995b32c065715e8da2fd83ad99c07de4938ff55d46c6665bdc71b74814236a8"
 hardening = ["vis", "cfi"]
 # no tests
 options = ["!check"]
