acd4332f6833b1b20066ce756aa707c9e31a15ff
triallax <triallax@tutanota.com>
2024-12-06 21:20:07 +0000

    main/python-zope.interface: update to 7.2

--- main/python-zope.interface/template.py
+++ main/python-zope.interface/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-zope.interface"
-pkgver = "7.1.1"
+pkgver = "7.2"
 pkgrel = 0
 build_style = "python_pep517"
 # useless setuptools version check
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "ZPL-2.1"
 url = "https://github.com/zopefoundation/zope.interface"
 source = f"$(PYPI_SITE)/z/zope.interface/zope.interface-{pkgver}.tar.gz"
-sha256 = "4284d664ef0ff7b709836d4de7b13d80873dc5faeffc073abdb280058bfac5e3"
+sha256 = "8b49f1a3d1ee4cdaf5b32d2e738362c7f5e40ac8b46dd7d1a65e82a4872728fe"
 # not functional yet
 options = ["!check"]
 
