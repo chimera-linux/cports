7efe851de0433a0d55fb1ae916ad1be094c723b4
ttyyls <contact@behri.org>
2024-12-06 21:24:15 +0100

    main/tailscale: update to 1.78.1

--- main/tailscale/template.py
+++ main/tailscale/template.py
@@ -1,6 +1,6 @@
 pkgname = "tailscale"
-pkgver = "1.76.6"
-pkgrel = 2
+pkgver = "1.78.1"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     "-ldflags="
@@ -16,7 +16,7 @@ maintainer = "Val Packett <val@packett.cool>"
 license = "BSD-3-Clause"
 url = "https://github.com/tailscale/tailscale"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1603c78a6a5e9f83b278d305e1196fbfdeeb841be10ac2ddb7ea433c2701234b"
+sha256 = "dbc25cc241bb233f183475f003d5508af7b45add1ca548b35a6a6fea91fb91af"
 # check: needs network access
 # cross: completions with host bin
 options = ["!check", "!cross"]
