3bd475ca52b50c1fa94fdc38bff9204649d3368a
triallax <triallax@tutanota.com>
2024-12-06 21:32:12 +0000

    main/python-six: update to 1.17.0

--- main/python-six/template.py
+++ main/python-six/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-six"
-pkgver = "1.16.0"
-pkgrel = 1
+pkgver = "1.17.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/benjaminp/six"
 source = f"$(PYPI_SITE)/s/six/six-{pkgver}.tar.gz"
-sha256 = "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+sha256 = "ff70335d468e7eb6ec65b95b99d3a2836546063f63acc5171de367e834932a81"
 # dependency of core stuff
 options = ["!check"]
 
