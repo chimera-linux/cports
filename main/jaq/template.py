be633b4af36cba6c3d1981fa4866e2f749dc3477
Wesley Moore <wes@wezm.net>
2024-12-07 20:26:08 +1000

    main/jaq: update to 2.0.0

--- main/jaq/template.py
+++ main/jaq/template.py
@@ -1,11 +1,11 @@
 pkgname = "jaq"
-pkgver = "1.6.0"
+pkgver = "2.0.0"
 pkgrel = 0
 build_style = "cargo"
 # disable the default mimalloc feature and just use the system allocator
-make_build_args = ["--no-default-features"]
-make_install_args = ["--no-default-features"]
-make_check_args = ["--no-default-features"]
+make_build_args = ["--no-default-features", "--bin", "jaq"]
+make_install_args = [*make_build_args]
+make_check_args = [*make_build_args]
 hostmakedepends = ["cargo-auditable"]
 makedepends = ["rust-std"]
 pkgdesc = "JSON data processing tool with jq compatible syntax"
@@ -13,7 +13,7 @@ maintainer = "Wesley Moore <wes@wezm.net>"
 license = "MIT"
 url = "https://github.com/01mf02/jaq"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "64b3431970cd4c27f3c4e665913218f44a0f44be7e22401eea34d52d8f3745a9"
+sha256 = "7535387562c0e5519811bfd1f405eb8cb7683533781e0790981221e8e5b723c2"
 
 
 def install(self):
