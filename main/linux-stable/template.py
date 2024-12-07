75969a5b6245ecbe824fe96f614e1423331f0163
q66 <q66@chimera-linux.org>
2024-12-07 20:44:15 +0100

    main/linux-stable: update to 6.12.3

--- main/linux-stable/template.py
+++ main/linux-stable/template.py
@@ -1,6 +1,6 @@
 # update linux-stable-zfs-bin when bumping
 pkgname = "linux-stable"
-pkgver = "6.12.1"
+pkgver = "6.12.3"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]
 make_dir = "build"
@@ -12,7 +12,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
-sha256 = "0193b1d86dd372ec891bae799f6da20deef16fc199f30080a4ea9de8cef0c619"
+sha256 = "c89809cc777d50f1ea484a118630281a26383707a0e752c96fd834f6e765deae"
 # no meaningful checking to be done
 options = [
     "!check",
