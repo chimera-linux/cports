36f2ce5ecce65fa7f8f76c281016db933875b9ab
q66 <q66@chimera-linux.org>
2024-12-08 01:03:55 +0100

    main/rpi-boot: update to 1.20241205

--- main/rpi-boot/template.py
+++ main/rpi-boot/template.py
@@ -1,15 +1,15 @@
 pkgname = "rpi-boot"
-pkgver = "1.20241023"
+pkgver = "1.20241205"
 pkgrel = 0
 archs = ["aarch64"]
-_gitrev = "c3a480143e6697e121a0e1da81cfb9e1ff5f8070"
+_gitrev = "b543406ce066d3fcaa03078511bc47e62ad0bbec"
 replaces = ["firmware-rpi<=20220905-r0"]
 pkgdesc = "Bootloader for Raspberry Pi"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:raspberry"
 url = "https://github.com/raspberrypi/firmware"
 source = f"{url}/archive/{_gitrev}.tar.gz"
-sha256 = "4d614ff0b28f2395f98a46a7ee862105d2ca9de0425ee93e6e418931965cb615"
+sha256 = "a27ffcdb8b16bdba8edca5fd038bd32c2ebab7e3a2c8b86de67e6dd7aabff784"
 options = ["!strip", "foreignelf", "execstack"]
 
 
