6b0dbe4dae1ac02e23f35f00fd936deb968d093d
q66 <q66@chimera-linux.org>
2024-12-08 18:28:46 +0100

    main/bats: update to 1.11.1

--- main/bats/template.py
+++ main/bats/template.py
@@ -1,5 +1,5 @@
 pkgname = "bats"
-pkgver = "1.11.0"
+pkgver = "1.11.1"
 pkgrel = 0
 hostmakedepends = ["bash"]
 checkdepends = ["bash", "procps"]
@@ -9,7 +9,7 @@ maintainer = "hge <h.gersen@gmail.com>"
 license = "MIT"
 url = "https://github.com/bats-core/bats-core"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "aeff09fdc8b0c88b3087c99de00cf549356d7a2f6a69e3fcec5e0e861d2f9063"
+sha256 = "5c57ed9616b78f7fd8c553b9bae3c7c9870119edd727ec17dbd1185c599f79d9"
 
 
 def check(self):
