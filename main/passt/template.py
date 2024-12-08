fbf17abb977038f0a049d1ec6c4ef5516c57fc2d
q66 <q66@chimera-linux.org>
2024-12-08 14:12:15 +0100

    main/passt: update to 2024.11.27

--- main/passt/template.py
+++ main/passt/template.py
@@ -1,5 +1,5 @@
 pkgname = "passt"
-_pkgver = "2024_11_21.238c69f"
+_pkgver = "2024_11_27.c0fbc7e"
 # yeardate only
 pkgver = _pkgver.split(".")[0].replace("_", ".")
 pkgrel = 0
@@ -13,7 +13,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "BSD-3-Clause AND GPL-2.0-or-later"
 url = "https://passt.top/passt/about"
 source = f"https://passt.top/passt/snapshot/passt-{_pkgver}.tar.zst"
-sha256 = "c2dff5632407af9b4b82c9b4937e11f253ebf9fba65d79e75f7ef3315f1ff641"
+sha256 = "5ae0044c0b134b7cae6d084c352e0e003431ea5a12cf4b54b2d47044c0ae2b08"
 # tries to pass this via __attribute(optimise) for some stuff but that is ignored
 tool_flags = {"CFLAGS": ["-fno-strict-aliasing"]}
 hardening = ["vis", "cfi"]
