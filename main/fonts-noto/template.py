bf5c32bbc4b43bd8272ede72b0f805894895733d
q66 <q66@chimera-linux.org>
2024-12-08 03:26:26 +0100

    main/fonts-noto: update to 2024.12.01

--- main/fonts-noto/template.py
+++ main/fonts-noto/template.py
@@ -1,12 +1,12 @@
 pkgname = "fonts-noto"
-pkgver = "2024.11.01"
+pkgver = "2024.12.01"
 pkgrel = 0
 pkgdesc = "Google Noto fonts"
 maintainer = "GeopJr <evan@geopjr.dev>"
 license = "OFL-1.1"
 url = "https://github.com/notofonts/notofonts.github.io"
 source = f"{url}/archive/refs/tags/noto-monthly-release-{pkgver}.zip"
-sha256 = "6fe04d05db30ec56beb0fbb6a94fb6439477d815aae902ce075001c086325f78"
+sha256 = "04620d14d924dea9bb08c0090578a21d349ab0a76cdaa5126c60c60de66d2542"
 
 
 def install(self):
