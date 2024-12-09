1fbf645ce08a2b501fa80d9aad20cad3cc0ee37c
q66 <q66@chimera-linux.org>
2024-12-09 15:14:38 +0100

    user/bacon: update to 3.5.0

--- user/bacon/template.py
+++ user/bacon/template.py
@@ -1,5 +1,5 @@
 pkgname = "bacon"
-pkgver = "3.3.0"
+pkgver = "3.5.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -9,7 +9,7 @@ maintainer = "tulilirockz <tulilirockz@outlook.com>"
 license = "AGPL-3.0-only"
 url = "https://dystroy.org/bacon"
 source = f"https://github.com/Canop/bacon/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "640a6a76213ef4b7337c7a3495afba3056ee5d58b3b7aefebe35edfc9c179e16"
+sha256 = "a5819aa6b5a56d089dba3e51ce469845af84b7c6feeba02f21f038ea92accad9"
 
 
 def post_install(self):
