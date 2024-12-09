41f588135824efc76eaa5d279c72a3254d0bc85d
q66 <q66@chimera-linux.org>
2024-12-09 15:19:14 +0100

    user/j4-dmenu-desktop: update to 3.2

--- user/j4-dmenu-desktop/template.py
+++ user/j4-dmenu-desktop/template.py
@@ -1,6 +1,6 @@
 pkgname = "j4-dmenu-desktop"
-pkgver = "3.1"
-pkgrel = 1
+pkgver = "3.2"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Denable-tests=true"]
 hostmakedepends = ["meson", "pkgconf"]
@@ -11,4 +11,4 @@ maintainer = "wipet <47226783+whypet@users.noreply.github.com>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/enkore/j4-dmenu-desktop"
 source = f"{url}/archive/r{pkgver}.tar.gz"
-sha256 = "0e6cf25663cc220e3e3e2bc013fe957c3e4a44f900b5ee6a7609cd501021652d"
+sha256 = "99e2f394570f8fb0b1a2cde2926980fd9685ce60e1500b575057f8424c9ad555"
