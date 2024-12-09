3d0afa06b4864ccd044e79e9829d1a2ea58e4b65
q66 <q66@chimera-linux.org>
2024-12-09 12:23:16 +0100

    main/log4cplus: update to 2.1.2

--- main/log4cplus/template.py
+++ main/log4cplus/template.py
@@ -1,5 +1,5 @@
 pkgname = "log4cplus"
-pkgver = "2.1.1"
+pkgver = "2.1.2"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -8,7 +8,7 @@ maintainer = "Renato Botelho do Couto <renato@netgate.com>"
 license = "Apache-2.0 AND BSD-2-Clause"
 url = "https://log4cplus.github.io/log4cplus"
 source = f"https://downloads.sourceforge.net/sourceforge/log4cplus/log4cplus-stable/log4cplus-{pkgver}.tar.xz"
-sha256 = "a1d8e67a207f90a9dd4f82b28a1f3ac6dead5a80c2bed071277a9e865698a82b"
+sha256 = "fbdabb4ef734fe1cc62169b23f0b480cc39127ac7b09b810a9c1229490d67e9e"
 
 
 def post_install(self):
