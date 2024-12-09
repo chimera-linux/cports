f0e9aed8b0af1b3b1b377317c4b1426cbf38d4d1
ttyyls <contact@behri.org>
2024-12-09 10:55:15 +0100

    user/neatvc: new package

---
+++ user/neatvnc/template.py
@@ -0,0 +1,30 @@
+pkgname = "neatvnc"
+pkgver = "0.9.2"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
+makedepends = [
+    "aml-devel",
+    "ffmpeg-devel",
+    "gnutls-devel",
+    "libdrm-devel",
+    "libgbm-devel",
+    "libjpeg-turbo-devel",
+    "pixman-devel",
+    "zlib-ng-compat-devel",
+]
+pkgdesc = "VNC server library"
+maintainer = "ttyyls <contact@behri.org>"
+license = "ISC"
+url = "https://github.com/any1/neatvnc"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "1efa6080b47371d6bc44f627d6692fae4f2ea2faf30faad3462bbbb3c503e17c"
+
+
+def post_install(self):
+    self.install_license("COPYING")
+
+
+@subpackage("neatvnc-devel")
+def _(self):
+    return self.default_devel()
