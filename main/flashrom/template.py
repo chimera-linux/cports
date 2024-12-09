e22e0736433a9c02e7ca08f4741019e3841a5da7
q66 <q66@chimera-linux.org>
2024-12-09 03:02:16 +0100

    main/flashrom: update to 1.5.0

--- main/flashrom/template.py
+++ main/flashrom/template.py
@@ -1,6 +1,6 @@
 pkgname = "flashrom"
-pkgver = "1.4.0"
-pkgrel = 1
+pkgver = "1.5.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Duse_internal_dmi=false"]
 hostmakedepends = ["meson", "pkgconf", "python-sphinx"]
@@ -16,10 +16,16 @@ pkgdesc = "Utility for flashing ROM chips"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://www.flashrom.org"
-source = f"https://download.flashrom.org/releases/flashrom-{pkgver}.tar.xz"
-sha256 = "ad7ee1b49239c6fb4f8f55e36706fcd731435db1a4bd2fab3d80f1f72508ccee"
+source = f"https://download.flashrom.org/releases/flashrom-v{pkgver}.tar.xz"
+sha256 = "3ef431cd0f039c1f7b929e81be145885e79192d16a843827e42977f488d9fec5"
 # needs special configuration?
 options = ["!check", "linkundefver"]
+
+
+def post_install(self):
+    self.rename(
+        "usr/share/bash-completion/completions/flashrom.bash", "flashrom"
+    )
 
 
 @subpackage("flashrom-devel")
