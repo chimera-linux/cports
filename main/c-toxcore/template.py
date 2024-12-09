1f4b6c2e698c602ffdfbbc049532e3f604f10215
q66 <q66@chimera-linux.org>
2024-12-09 15:45:45 +0100

    main/c-toxcore: update to 0.2.20

--- main/c-toxcore/template.py
+++ main/c-toxcore/template.py
@@ -1,6 +1,6 @@
 pkgname = "c-toxcore"
-pkgver = "0.2.19"
-pkgrel = 1
+pkgver = "0.2.20"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DUNITTEST=ON"]
 hostmakedepends = [
@@ -21,7 +21,7 @@ maintainer = "ogromny <ogromnycoding@gmail.com>"
 license = "GPL-3.0-only"
 url = "https://github.com/TokTok/c-toxcore"
 source = f"{url}/releases/download/v{pkgver}/c-toxcore-{pkgver}.tar.gz"
-sha256 = "8b418f6470db085cf59a9915685613556556df2bf427148f1814b7b118628594"
+sha256 = "a9c89a8daea745d53e5d78e7aacb99c7b4792c4400a5a69c71238f45d6164f4c"
 
 
 def post_install(self):
