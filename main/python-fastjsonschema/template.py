6c869ae5c28de1c683e768244948cdc4c8116aaa
q66 <q66@chimera-linux.org>
2024-12-09 12:41:10 +0100

    main/python-fastjsonschema: update to 2.21.1

--- main/python-fastjsonschema/template.py
+++ main/python-fastjsonschema/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-fastjsonschema"
-pkgver = "2.20.0"
+pkgver = "2.21.1"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
@@ -20,7 +20,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://horejsek.github.io/python-fastjsonschema"
 source = f"$(PYPI_SITE)/f/fastjsonschema/fastjsonschema-{pkgver}.tar.gz"
-sha256 = "3d48fc5300ee96f5d116f10fe6f28d938e6008f59a6a025c2649475b87f76a23"
+sha256 = "794d4f0a58f848961ba16af7b9c85a3e88cd360df008c59aac6fc5ae9323b5d4"
 
 
 def post_install(self):
