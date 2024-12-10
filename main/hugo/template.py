6ecabcf7f61cbe74e81e0231ab74add78f610258
q66 <q66@chimera-linux.org>
2024-12-10 03:04:14 +0100

    main/hugo: update to 0.139.4

--- main/hugo/template.py
+++ main/hugo/template.py
@@ -1,5 +1,5 @@
 pkgname = "hugo"
-pkgver = "0.139.3"
+pkgver = "0.139.4"
 pkgrel = 0
 build_style = "go"
 make_build_args = [
@@ -12,7 +12,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://gohugo.io"
 source = f"https://github.com/gohugoio/hugo/archive/v{pkgver}.tar.gz"
-sha256 = "8d83115df9a540b87817bab7615d836ff7ed1b3a80a3e8299ee7c2d3d4cae197"
+sha256 = "8c89c968ace21086528a164cd9a1019809c0064f0ea2ef8a82b035c2d079d19b"
 # tests require network access
 # manpages and completions are generated with the resulting binary so no cross
 options = ["!check", "!cross"]
