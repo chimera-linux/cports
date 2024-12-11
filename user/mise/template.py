4817820098fd6381c3fd8f94333d72959a6af5f6
ttyyls <contact@behri.org>
2024-12-11 20:00:41 +0100

    user/mise: update to 2024.12.6

--- user/mise/template.py
+++ user/mise/template.py
@@ -1,5 +1,5 @@
 pkgname = "mise"
-pkgver = "2024.12.5"
+pkgver = "2024.12.6"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = [
@@ -28,7 +28,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "MIT"
 url = "https://mise.jdx.dev"
 source = f"https://github.com/jdx/mise/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0b7b2bb056bc9429d585c8c23202b1c299793e9b7b72017e3f7b5df390b25ad8"
+sha256 = "c8f41169d6c9c44dd754ea47cbd83f85b0ba01d721fdbe68f71b30abe05d26e3"
 
 
 def install(self):
