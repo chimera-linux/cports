f900e864f41656bd1484bf33fde504f0c1c4176c
Erica Z <zerica@callcc.eu>
2024-12-12 22:55:30 +0100

    main/pv: update to 1.9.15

--- main/pv/template.py
+++ main/pv/template.py
@@ -1,5 +1,5 @@
 pkgname = "pv"
-pkgver = "1.9.7"
+pkgver = "1.9.15"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "gettext-devel"]
@@ -8,4 +8,4 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-3.0-or-later"
 url = "https://www.ivarch.com/programs/pv.shtml"
 source = f"https://www.ivarch.com/programs/sources/pv-{pkgver}.tar.gz"
-sha256 = "6976a789ee1bbcfdc47732015b60589c1d0408892252dd7cbb44f43363a2f33a"
+sha256 = "e2b17564ab9eba1ec2caae285960cbf363b4401dffda191a60a0befe68e17dac"
