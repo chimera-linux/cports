8e08bc11070d5af8dc9dda1f23573f353183a6b6
ttyyls <contact@behri.org>
2024-12-13 11:31:58 +0100

    user/moreutils: new package

---
+++ user/moreutils/template.py
@@ -0,0 +1,22 @@
+pkgname = "moreutils"
+pkgver = "0.70"
+pkgrel = 0
+build_style = "makefile"
+make_build_args = ["DOCBOOKXSL=/usr/share/xsl-nons/docbook"]
+hostmakedepends = [
+    "docbook-xsl-nons",
+    "libxml2-progs",
+    "perl",
+    "xsltproc",
+]
+makedepends = ["linux-headers"]
+depends = ["perl-ipc-run"]
+pkgdesc = "Miscallaenous unix utilities"
+maintainer = "ttyyls <contact@behri.org>"
+license = "GPL-2.0-or-later"
+url = "https://joeyh.name/code/moreutils"
+source = f"https://git.joeyh.name/index.cgi/moreutils.git/snapshot/moreutils-{pkgver}.tar.gz"
+sha256 = "f2bf46d410ba567cc8d01507e94916994e48742722e690dc498fab59f5250132"
+hardening = ["vis", "cfi"]
+# no tests defined
+options = ["!check"]
