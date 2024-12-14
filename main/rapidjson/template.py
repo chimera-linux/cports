4ac8b895cef9fb849668351c743f168ddefdcd25
q66 <q66@chimera-linux.org>
2024-12-14 17:24:26 +0100

    main/rapidjson: clang 19 fixes

--- main/rapidjson/template.py
+++ main/rapidjson/template.py
@@ -1,7 +1,7 @@
 # header files only
 pkgname = "rapidjson"
 pkgver = "1.1.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 configure_args = [
     "-DGTESTSRC_FOUND=ON",
@@ -27,6 +27,20 @@ source = (
     f"https://github.com/Tencent/rapidjson/archive/refs/tags/v{pkgver}.tar.gz"
 )
 sha256 = "bf7ced29704a1e696fbccf2a2b4ea068e7774fa37f6d7dd4039d0787f8bed98e"
+# noise
+tool_flags = {
+    "CXXFLAGS": [
+        "-Wno-exceptions",
+        "-Wno-c++98-compat",
+        "-Wno-c++98-compat-pedantic",
+        "-Wno-deprecated-declarations",
+        "-Wno-extra-semi-stmt",
+        "-Wno-suggest-override",
+        "-Wno-suggest-destructor-override",
+        "-Wno-unsafe-buffer-usage",
+        "-Wno-zero-as-null-pointer-constant",
+    ]
+}
 
 
 def post_install(self):
