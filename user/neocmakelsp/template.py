a800aa819500ccd54e5f466d40e9c14a7393ed63
ttyyls <contact@behri.org>
2024-12-15 10:54:20 +0100

    user/neocmakelsp: update to 0.8.13

--- user/neocmakelsp/template.py
+++ user/neocmakelsp/template.py
@@ -1,5 +1,5 @@
 pkgname = "neocmakelsp"
-pkgver = "0.8.12.2"
+pkgver = "0.8.13"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -9,7 +9,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "MIT"
 url = "https://neocmakelsp.github.io"
 source = f"https://github.com/neocmakelsp/neocmakelsp/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "74f6979fc6e3dd2e5d58cce314eb0e71280b7da2a4874795634a01c01e0fbab2"
+sha256 = "57c68173ddd2362d14a272a57eba25892ce0be02ac864b2d7f572e06f77ab55a"
 
 
 def post_install(self):
