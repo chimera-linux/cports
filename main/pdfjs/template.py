a6fd38bf37a74d24c9db48db3c98f41eaef79f32
ttyyls <contact@behri.org>
2024-12-15 11:07:06 +0100

    main/pdfjs: switch to legacy distribution

--- main/pdfjs/template.py
+++ main/pdfjs/template.py
@@ -1,12 +1,12 @@
 pkgname = "pdfjs"
 pkgver = "4.9.155"
-pkgrel = 0
+pkgrel = 1
 pkgdesc = "Platform for parsing and rendering PDFs"
 maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/mozilla/pdf.js"
-source = f"{url}/releases/download/v{pkgver}/pdfjs-{pkgver}-dist.zip"
-sha256 = "1059ba50947a634a531d0bcbef77532020f43811e7996415f502829ec1cc925f"
+source = f"{url}/releases/download/v{pkgver}/pdfjs-{pkgver}-legacy-dist.zip"
+sha256 = "0f4778090298eb36c569b78ea4e9ca643ef9bfddb090f22464604c08b6256f73"
 # no tests defined
 options = ["!check"]
 
