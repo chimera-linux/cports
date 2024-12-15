de209d67a00d522ed8e7f74673579ff262707169
q66 <q66@chimera-linux.org>
2024-12-15 20:20:37 +0100

    main/perf: update to 6.12.5

--- main/perf/template.py
+++ main/perf/template.py
@@ -1,5 +1,5 @@
 pkgname = "perf"
-pkgver = "6.12.1"
+pkgver = "6.12.5"
 pkgrel = 0
 build_wrksrc = "tools/perf"
 build_style = "makefile"
@@ -58,7 +58,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://perf.wiki.kernel.org/index.php/Main_Page"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[:pkgver.find('.')]}.x/linux-{pkgver}.tar.xz"
-sha256 = "0193b1d86dd372ec891bae799f6da20deef16fc199f30080a4ea9de8cef0c619"
+sha256 = "39207fce1ce42838e085261bae0af5ce4a0843aa777cfc0f5c49bc7729602bcd"
 # nope
 # docs are a single tips file that gets displayed in the TUI
 options = ["!check", "!splitdoc"]
