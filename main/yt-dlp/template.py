22676c9502c6bc0959800629743ae8868545c3fd
triallax <triallax@tutanota.com>
2024-12-16 15:23:56 +0000

    main/yt-dlp: update to 2024.12.13

--- main/yt-dlp/template.py
+++ main/yt-dlp/template.py
@@ -1,5 +1,5 @@
 pkgname = "yt-dlp"
-pkgver = "2024.12.06"
+pkgver = "2024.12.13"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
@@ -26,7 +26,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Unlicense"
 url = "https://github.com/yt-dlp/yt-dlp"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "43b4c86dd39fe5ba616873cddff4c7b4c940d577d1e49f2f9729807028fe33ff"
+sha256 = "40d8c886302f6f17749ddb2fa1fa0e05d6a6a68908411c69e09daa510a3e86d6"
 
 
 @subpackage("yt-dlp-recommends")
