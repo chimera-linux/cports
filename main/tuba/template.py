673de0082f8bad110b35eccb8dd55146cef7a704
triallax <triallax@tutanota.com>
2024-12-16 14:13:18 +0000

    main/tuba: update to 0.9.0

--- main/tuba/template.py
+++ main/tuba/template.py
@@ -1,6 +1,6 @@
 pkgname = "tuba"
-pkgver = "0.8.4"
-pkgrel = 2
+pkgver = "0.9.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dclapper=true",
@@ -15,6 +15,7 @@ hostmakedepends = [
 makedepends = [
     "clapper-devel",
     "glib-devel",
+    "gstreamer-devel",
     "gtksourceview-devel",
     "icu-devel",
     "json-glib-devel",
@@ -24,10 +25,10 @@ makedepends = [
     "libspelling-devel",
     "libxml2-devel",
 ]
-depends = ["webp-pixbuf-loader"]
+depends = ["webp-pixbuf-loader", "gst-plugins-good"]
 pkgdesc = "GTK fediverse client"
 maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://tuba.geopjr.dev"
 source = f"https://github.com/GeopJr/Tuba/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "844fc1e3dc7d13a6967e9ac7c43bf887a00c9817f6666043ae002536d6a24c78"
+sha256 = "efdd74f543025473507b2f9b18e7c272c2376b779ff65f1a2f8e41f8b99fe2ac"
