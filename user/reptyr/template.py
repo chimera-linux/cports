725eeabd6e7f4bc27401ceb6d7d8eda1a07c0261
LeFantome <fantome137@proton.me>
2024-12-16 11:37:41 -0800

    user/reptyr: use patch instead of sed in template.py

--- user/reptyr/template.py
+++ user/reptyr/template.py
@@ -12,21 +12,12 @@ source = (
     f"https://github.com/nelhage/reptyr/archive/refs/tags/{pkgname}-{pkgver}.tar.gz"
 )
 sha256 = "c6ffbc34a511ac00d072219bda30699e51f2f4eb483cbae9e32e981d49e8b380"
+patch_args = ["--ignore-space-change", "--ignore-whitespace"]
 # Tests require additional permissions
 options = ["!check"]
 
-
-def patch(self):
-    self.do("sed", "-i", """""", "-e", """s#python2#python3#""", "Makefile")
-    self.do(
-        "sed",
-        "-i",
-        """""",
-        "-e",
-        """s#etc/bash_completion.d#usr/share/bash-completion/completions#""",
-        "Makefile",
-    )
-
+#def patch(self):
+#    self.do("cat", "Makefile")
 
 def check(self):
     self.do("make", "test")
