f6779f64339d5b46f57a2a686b95c616d36cbf86
triallax <triallax@tutanota.com>
2024-12-16 23:39:43 +0000

    main/xfdesktop: update to 4.20.0

--- main/xfdesktop/template.py
+++ main/xfdesktop/template.py
@@ -1,16 +1,16 @@
 pkgname = "xfdesktop"
-pkgver = "4.18.1"
+pkgver = "4.20.0"
 pkgrel = 0
 build_style = "gnu_configure"
-# check target fails without this
-make_dir = "."
+configure_args = [
+    "--with-default-backdrop-filename=/usr/share/backgrounds/chimera/bg-l.svg"
+]
 hostmakedepends = [
     "automake",
     "gettext",
     "gettext-devel",
-    "intltool",
-    "libtool",
     "pkgconf",
+    "slibtool",
     "xfce4-dev-tools",
 ]
 makedepends = [
@@ -20,28 +20,26 @@ makedepends = [
     "glib-devel",
     "gtk+3-devel",
     "libnotify-devel",
-    "libwnck-devel",
     "libxfce4ui-devel",
     "libxfce4util-devel",
+    "libxfce4windowing-devel",
+    "libyaml-devel",
     "thunar-devel",
     "xfconf-devel",
 ]
-# We patch the default wallpaper to Chimera's, see
-# patches/default-to-chimera-wallpaper.patch
 depends = ["chimera-artwork"]
 pkgdesc = "Xfce desktop manager"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/xfce/xfdesktop/start"
 source = f"$(XFCE_SITE)/xfce/xfdesktop/{pkgver[:-2]}/xfdesktop-{pkgver}.tar.bz2"
-sha256 = "ef9268190c25877e22a9ff5aa31cc8ede120239cb0dfca080c174e7eed4ff756"
+sha256 = "227041ba80c7f3eb9c99dec817f1132b35d8aec7a4335703f61ba1735cd65632"
 
 
 @subpackage("xfce4-backgrounds")
 def _(self):
     self.pkgdesc = "Backgrounds for the Xfce desktop"
     self.install_if = [self.parent]
-    # TODO: https://gitlab.xfce.org/xfce/xfdesktop/-/issues/298
     self.license = "CC-BY-SA-4.0"
 
     return ["usr/share/backgrounds"]
