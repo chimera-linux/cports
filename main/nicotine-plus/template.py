18c813ab01d6334b0974034235909d9d83a97818
Erica Z <zerica@callcc.eu>
2024-12-17 21:21:41 +0100

    main/nicotine-plus: update to 3.3.7

--- main/nicotine-plus/template.py
+++ main/nicotine-plus/template.py
@@ -1,5 +1,5 @@
 pkgname = "nicotine-plus"
-pkgver = "3.3.6"
+pkgver = "3.3.7"
 pkgrel = 0
 build_style = "python_pep517"
 # test_update_check: no networking + patched
@@ -36,4 +36,4 @@ url = "https://nicotine-plus.github.io/nicotine-plus"
 source = (
     f"https://github.com/Nicotine-Plus/nicotine-plus/archive/{pkgver}.tar.gz"
 )
-sha256 = "3911c178f96da1fdd48dedd98ac2d2ad6a59da388f091f91bb5722871cca91da"
+sha256 = "ea92ef27f4da7c41ce01f6419523345e1496f2a4348a7e1355f1bf87f9201274"
