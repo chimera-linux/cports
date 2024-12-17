c07ad7b10ab26e7170a2882d96bba719c8bf3d4b
theuae <theuae@nanachi.network>
2024-12-17 10:23:00 -0500

    user/spc2it: new package

--- user/spc2it/template.py
+++ user/spc2it/template.py
@@ -6,15 +6,12 @@ pkgdesc = "Utility for converting SPC700 music files to Impulse Tracker modules"
 maintainer = "theuae <theuae@nanachi.network>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/uyjulian/spc2it"
-
 source = f"https://github.com/uyjulian/spc2it/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "5f30b0d88a8120ad30cc3a0052441c3d605ee9f165d863bcbeef7c915562a9fa"
-
 hostmakedepends = [
     "meson",
     "ninja"
 ]
-
 def post_install(self):
     self.install_license("doc/LICENSE_SNEESE")
     self.install_bin("build/spc2it")
