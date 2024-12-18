c6bcdd69ac9a031da1209a5c507c35b1fd825820
theuae <theuae@nanachi.network>
2024-12-18 10:27:51 -0500

    user/spc2it: remove accidental newlines

--- user/spc2it/template.py
+++ user/spc2it/template.py
@@ -12,7 +12,3 @@ sha256 = "5f30b0d88a8120ad30cc3a0052441c3d605ee9f165d863bcbeef7c915562a9fa"
 def post_install(self):
     self.install_license("doc/LICENSE_SNEESE")
     self.install_bin("build/spc2it")
-
-
-    
-
