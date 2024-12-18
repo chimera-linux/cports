6fad8867eebe9c0a145ab46c2866cef6ec86886d
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-12-18 21:31:12 +0545

    user/monolith: new package

---
+++ user/monolith/template.py
@@ -0,0 +1,21 @@
+pkgname = "monolith"
+pkgver = "2.8.3"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["pkgconf"]
+makedepends = [
+    "rust-std",
+    "cargo-auditable",
+    "openssl-devel",
+]
+
+env = {
+    "OPENSSL_NO_VENDOR": "1",
+}
+
+pkgdesc = "CLI tool for saving complete web pages as a single HTML file"
+maintainer = "Sudan <sudanchapagain@proton.me>"
+license = "CC0-1.0"
+url = "https://github.com/Y2Z/monolith"
+source = f"{url}/archive/v{pkgver}/monolith-{pkgver}.tar.gz"
+sha256 = "51769e6505d5708ac296e5d93e280c9fefa7873452d471c5106aaeb7c3667f9f"
