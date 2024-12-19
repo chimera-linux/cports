bf1ecf6b9c917c3a9e1f82234d6f41e6bc3f06f7
ttyyls <contact@behri.org>
2024-12-19 21:44:46 +0100

    main/docker-cli-buildx: update to 0.19.3

--- main/docker-cli-buildx/template.py
+++ main/docker-cli-buildx/template.py
@@ -1,5 +1,5 @@
 pkgname = "docker-cli-buildx"
-pkgver = "0.19.2"
+pkgver = "0.19.3"
 pkgrel = 0
 build_style = "go"
 make_build_args = [
@@ -15,7 +15,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://github.com/docker/buildx"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f45cb0d465ef0bdcac5136764db33626280fb8720dbae5f9565102e1af58f3c4"
+sha256 = "981a17d5763d9583c6c73ac194d4978de6fb632e3818fa6d983aefbc0c02f844"
 # some tests rely on network
 options = ["!check"]
 
