5d5f40ecb317b37d430aa533f98b32aa69ec3e81
q66 <q66@chimera-linux.org>
2024-12-19 20:09:19 +0100

    main/base-files: update to 0.1.20241206
    This comes with a big rework moving most /etc files outside,
    leaving only passwd and group skeletons for the time being,
    until we figure out sysusers interactions.

--- main/base-files/files/profile.d/locale.sh
+++ main/base-files/files/profile.d/locale.sh
@@ -1,7 +1,11 @@
 # Sets up locale system settings from /etc/locale.conf.
 #
-if [ -s /etc/locale.conf ]; then
-	. /etc/locale.conf
+if [ -e /etc/locale.conf ]; then
+    . /etc/locale.conf
+else
+    # defaults
+    LANG=C.UTF-8
+    LC_COLLATE=C
 fi
 
 export LANG LANGUAGE LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY
