c597fc351f4c1ffae88c30b0a808b9bf5f36fa96
q66 <q66@chimera-linux.org>
2024-12-20 11:57:01 +0100

    main/ngtcp2: update to 1.10.0

--- main/ngtcp2/template.py
+++ main/ngtcp2/template.py
@@ -1,5 +1,5 @@
 pkgname = "ngtcp2"
-pkgver = "1.9.1"
+pkgver = "1.10.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DENABLE_GNUTLS=ON", "-DENABLE_OPENSSL=OFF"]
@@ -15,7 +15,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/ngtcp2/ngtcp2"
 source = f"{url}/releases/download/v{pkgver}/ngtcp2-{pkgver}.tar.xz"
-sha256 = "f9617895c15d5d2985282aa948889d3ea032bf8a7db351d20cb9569352d64087"
+sha256 = "4f8dc1d61957205d01c3d6aa6f1c96c7b2bac1feea71fdaf972d86db5f6465df"
 
 
 def post_install(self):
