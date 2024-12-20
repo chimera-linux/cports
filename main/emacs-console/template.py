4c558cae1e422c034495bea86c62f4b2250b98f1
q66 <q66@chimera-linux.org>
2024-12-20 11:51:19 +0100

    main/*: rebuild for tree-sitter

--- main/emacs-console/template.py
+++ main/emacs-console/template.py
@@ -1,6 +1,6 @@
 pkgname = "emacs-console"
 pkgver = "29.4"
-pkgrel = 5
+pkgrel = 6
 build_style = "gnu_configure"
 configure_args = [
     "--with-gameuser=:_games",
--- main/emacs-gtk3/template.py
+++ main/emacs-gtk3/template.py
@@ -1,6 +1,6 @@
 pkgname = "emacs-gtk3"
 pkgver = "29.4"
-pkgrel = 5
+pkgrel = 6
 build_style = "gnu_configure"
 configure_args = [
     "--with-gameuser=:_games",
