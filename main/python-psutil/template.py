09f8d1888bd980bc1344d7f01d2875521d1d2e62
q66 <q66@chimera-linux.org>
2024-12-20 11:52:42 +0100

    main/python-psutil: update to 6.1.1

--- main/python-psutil/template.py
+++ main/python-psutil/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-psutil"
-pkgver = "6.1.0"
+pkgver = "6.1.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -16,7 +16,7 @@ maintainer = "yopito <pierre.bourgin@free.fr>"
 license = "BSD-3-Clause"
 url = "https://github.com/giampaolo/psutil"
 source = f"$(PYPI_SITE)/p/psutil/psutil-{pkgver}.tar.gz"
-sha256 = "353815f59a7f64cdaca1c0307ee13558a0512f6db064e92fe833784f08539c7a"
+sha256 = "cf8496728c18f2d0b45198f06895be52f36611711746b7f30c464b422b50e2f5"
 # cwd import memes
 options = ["!check"]
 
