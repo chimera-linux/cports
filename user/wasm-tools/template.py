e3e78178b689eff92983035f1e633f8c09cada5d
q66 <q66@chimera-linux.org>
2024-12-21 15:54:38 +0100

    user/wasm-tools: update to 1.222.0

--- user/wasm-tools/template.py
+++ user/wasm-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "wasm-tools"
-pkgver = "1.221.2"
+pkgver = "1.222.0"
 pkgrel = 0
 # wasmtime
 archs = ["aarch64", "riscv64", "x86_64"]
@@ -14,7 +14,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "Apache-2.0 WITH LLVM-exception OR Apache-2.0 OR MIT"
 url = "https://github.com/bytecodealliance/wasm-tools"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "9940c19aa878f500a031282f13401fb6177517a238220246bbcea0d642ee8934"
+sha256 = "50681e7e6d8822ce31cd741e9369011ef771f81a7151a02777c9558df47712c9"
 # needs checked out submodules and whatever
 options = ["!check"]
 
