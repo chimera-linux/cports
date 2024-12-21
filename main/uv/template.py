06a560fd7824ee035f68c972b65a9d9886c41145
q66 <q66@chimera-linux.org>
2024-12-21 15:49:59 +0100

    main/uv: update to 0.5.11

--- main/uv/template.py
+++ main/uv/template.py
@@ -1,5 +1,5 @@
 pkgname = "uv"
-pkgver = "0.5.9"
+pkgver = "0.5.11"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -19,7 +19,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/astral-sh/uv"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "1a58411374da0391a7954327bd9e0ebabb54ca581da89524cf6664c0fac033d5"
+sha256 = "4f581e0903285e3228f55967f7094f3311a06aa3ec4e4ea3bf7438702fb776d6"
 # too many of them need net
 # completions with host bin
 options = ["!check", "!cross"]
