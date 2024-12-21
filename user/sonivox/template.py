9db8f507c9b5022f778f7d62342ea27baf75b338
q66 <q66@chimera-linux.org>
2024-12-21 15:53:54 +0100

    user/sonivox: update to 3.6.15

--- user/sonivox/template.py
+++ user/sonivox/template.py
@@ -1,8 +1,8 @@
 pkgname = "sonivox"
-pkgver = "3.6.14"
+pkgver = "3.6.15"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DBUILD_SONIVOX_STATIC=OFF"]
+configure_args = ["-DBUILD_SONIVOX_STATIC=OFF", "-DBUILD_TESTING=OFF"]
 hostmakedepends = [
     "cmake",
     "ninja",
@@ -14,7 +14,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/pedrolcl/sonivox"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "216171c5bbe4f5a9524d73a2ee9e7e597c71a040e5d7c9e35b92c9dc459c2985"
+sha256 = "3e54cf11ecdc7ffd9fb24f0ba3319b6e0fe3df56f5e3082f2847666a31be3ff3"
 
 
 @subpackage("sonivox-devel")
