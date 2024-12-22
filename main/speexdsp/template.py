4872914d320e34073ac82102fb2f9770ffd95c40
q66 <q66@chimera-linux.org>
2024-12-22 12:52:57 +0100

    main/speexdsp: disable cfi

--- main/speexdsp/template.py
+++ main/speexdsp/template.py
@@ -1,6 +1,6 @@
 pkgname = "speexdsp"
 pkgver = "1.2.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 hostmakedepends = [
     "automake",
@@ -13,7 +13,6 @@ license = "BSD-3-Clause"
 url = "https://www.speex.org"
 source = f"https://gitlab.xiph.org/xiph/speexdsp/-/archive/SpeexDSP-{pkgver}/speexdsp-SpeexDSP-{pkgver}.tar.bz2"
 sha256 = "b36d4f16e42b7103b7fc3e4a8f98b6bf889dd1f70f65c2365af07be82844db29"
-hardening = ["vis", "cfi"]
 
 
 def post_install(self):
