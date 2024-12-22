b67117de0087e9b3dc800f4fee6b66067645500c
q66 <q66@chimera-linux.org>
2024-12-22 12:40:33 +0100

    main/libaec: disable cfi

--- main/libaec/template.py
+++ main/libaec/template.py
@@ -1,6 +1,6 @@
 pkgname = "libaec"
 pkgver = "1.1.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -12,7 +12,6 @@ license = "BSD-2-Clause"
 url = "https://gitlab.dkrz.de/k202009/libaec"
 source = f"{url}/-/archive/v{pkgver}.tar.gz"
 sha256 = "453de44eb6ea2500843a4cf4d2e97d1be251d2df7beae6c2ebe374edcb11e378"
-hardening = ["vis", "cfi"]
 
 
 def post_install(self):
