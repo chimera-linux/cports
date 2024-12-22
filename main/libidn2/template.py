c72f2c57cc7d4feef8669a909c74be11c9945a29
q66 <q66@chimera-linux.org>
2024-12-22 12:41:30 +0100

    main/libidn2: disable cfi

--- main/libidn2/template.py
+++ main/libidn2/template.py
@@ -1,6 +1,6 @@
 pkgname = "libidn2"
 pkgver = "2.3.7"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 # defines this to nothing and yields #if invalid syntax for some reason
 make_build_args = ["GNULIBHEADERS_OVERRIDE_WINT_T=0"]
@@ -18,7 +18,6 @@ license = "LGPL-3.0-or-later AND GPL-3.0-or-later"
 url = "https://www.gnu.org/software/libidn#libidn2"
 source = f"$(GNU_SITE)/libidn/libidn2-{pkgver}.tar.gz"
 sha256 = "4c21a791b610b9519b9d0e12b8097bf2f359b12f8dd92647611a929e6bfd7d64"
-hardening = ["vis", "cfi"]
 
 
 @subpackage("libidn2-devel")
