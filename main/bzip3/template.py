7d5179c8a611c70b91a8e5df86b82125d9a41d64
q66 <q66@chimera-linux.org>
2024-12-22 12:35:29 +0100

    main/bzip3: disable cfi and fix naming convention

--- main/bzip3/template.py
+++ main/bzip3/template.py
@@ -1,6 +1,6 @@
 pkgname = "bzip3"
 pkgver = "1.5.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 hostmakedepends = [
     "automake",
@@ -13,14 +13,17 @@ license = "LGPL-3.0-or-later AND Apache-2.0"
 url = "https://github.com/kspalaiologos/bzip3"
 source = f"{url}/releases/download/{pkgver}/bzip3-{pkgver}.tar.zst"
 sha256 = "6223fc69a0e983712242d225930b212879454b81eb2712f5f8d9cda3a69b08e1"
-hardening = ["vis", "cfi"]
 
 
-@subpackage("libbzip3")
+@subpackage("bzip3-libs")
 def _(self):
+    # transitional
+    self.provides = [self.with_pkgver("libbzip3")]
     return self.default_libs()
 
 
-@subpackage("libbzip3-devel")
+@subpackage("bzip3-devel")
 def _(self):
+    # transitional
+    self.provides = [self.with_pkgver("libbzip3-devel")]
     return self.default_devel()
