c3ad5228dce8ac47884ccc51806d8693c045bedc
q66 <q66@chimera-linux.org>
2024-12-22 12:52:35 +0100

    main/rnnoise: disable cfi

--- main/rnnoise/template.py
+++ main/rnnoise/template.py
@@ -1,6 +1,6 @@
 pkgname = "rnnoise"
 pkgver = "0.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 hostmakedepends = [
     "automake",
@@ -13,7 +13,6 @@ license = "BSD-3-Clause"
 url = "https://jmvalin.ca/demo/rnnoise"
 source = f"https://github.com/xiph/rnnoise/releases/download/v{pkgver}/rnnoise-{pkgver}.tar.gz"
 sha256 = "90fce4b00b9ff24c08dbfe31b82ffd43bae383d85c5535676d28b0a2b11c0d37"
-hardening = ["vis", "cfi"]
 
 if self.profile().arch == "x86_64":
     configure_args = ["--enable-x86-rtcd"]
