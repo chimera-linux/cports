2224e440137ecf024e2c3c320a7a70910fe2a295
q66 <q66@chimera-linux.org>
2024-12-22 12:50:04 +0100

    main/libtasn1: disable cfi and regen autohell

--- main/libtasn1/template.py
+++ main/libtasn1/template.py
@@ -1,15 +1,14 @@
 pkgname = "libtasn1"
 pkgver = "4.19.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf", "perl"]
+hostmakedepends = ["automake", "gtk-doc-tools", "pkgconf", "perl", "slibtool"]
 pkgdesc = "ASN.1 structure parser library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://www.gnu.org/software/libtasn1"
 source = f"$(GNU_SITE)/libtasn1/libtasn1-{pkgver}.tar.gz"
 sha256 = "1613f0ac1cf484d6ec0ce3b8c06d56263cc7242f1c23b30d82d23de345a63f7a"
-hardening = ["vis", "cfi"]
 
 
 @subpackage("libtasn1-devel")
@@ -20,6 +19,3 @@ def _(self):
 @subpackage("libtasn1-progs")
 def _(self):
     return self.default_progs()
-
-
-configure_gen = []
