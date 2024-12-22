7858f0bd618fa32e3004ab0a7e8fd26765e0d7b3
q66 <q66@chimera-linux.org>
2024-12-22 12:40:56 +0100

    main/libdvdcss: disable cfi and regen autohell

--- main/libdvdcss/template.py
+++ main/libdvdcss/template.py
@@ -1,8 +1,8 @@
 pkgname = "libdvdcss"
 pkgver = "1.4.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["automake", "pkgconf", "slibtool"]
 makedepends = ["linux-headers"]
 pkgdesc = "Library for accessing DVDs"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -10,12 +10,8 @@ license = "GPL-2.0-or-later"
 url = "https://www.videolan.org/developers/libdvdcss.html"
 source = f"https://download.videolan.org/pub/libdvdcss/{pkgver}/libdvdcss-{pkgver}.tar.bz2"
 sha256 = "233cc92f5dc01c5d3a96f5b3582be7d5cee5a35a52d3a08158745d3d86070079"
-hardening = ["vis", "cfi"]
 
 
 @subpackage("libdvdcss-devel")
 def _(self):
     return self.default_devel()
-
-
-configure_gen = []
