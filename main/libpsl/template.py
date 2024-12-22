b65a785f89efa10e525a9ff9a0628a6586ba0b66
q66 <q66@chimera-linux.org>
2024-12-22 12:49:24 +0100

    main/libpsl: disable cfi

--- main/libpsl/template.py
+++ main/libpsl/template.py
@@ -1,6 +1,6 @@
 pkgname = "libpsl"
 pkgver = "0.21.5"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 hostmakedepends = ["meson", "ninja", "pkgconf"]
 makedepends = ["icu-devel", "libidn2-devel", "libunistring-devel"]
@@ -10,7 +10,6 @@ license = "MIT"
 url = "https://rockdaboot.github.io/libpsl"
 source = f"https://github.com/rockdaboot/libpsl/releases/download/{pkgver}/libpsl-{pkgver}.tar.gz"
 sha256 = "1dcc9ceae8b128f3c0b3f654decd0e1e891afc6ff81098f227ef260449dae208"
-hardening = ["vis", "cfi"]
 
 
 def post_install(self):
