3b6747c4d91ebba8b48a33489638bc90c7dbdadb
q66 <q66@chimera-linux.org>
2024-12-22 12:40:15 +0100

    main/libaacs: disable cfi

--- main/libaacs/template.py
+++ main/libaacs/template.py
@@ -1,6 +1,6 @@
 pkgname = "libaacs"
 pkgver = "0.11.1"
-pkgrel = 2
+pkgrel = 3
 build_style = "gnu_configure"
 configure_args = ["--disable-optimizations"]
 hostmakedepends = [
@@ -17,7 +17,6 @@ license = "LGPL-2.1-or-later"
 url = "https://www.videolan.org/developers/libaacs.html"
 source = f"https://download.videolan.org/pub/videolan/libaacs/{pkgver}/libaacs-{pkgver}.tar.bz2"
 sha256 = "a88aa0ebe4c98a77f7aeffd92ab3ef64ac548c6b822e8248a8b926725bea0a39"
-hardening = ["cfi", "vis"]
 
 
 @subpackage("libaacs-devel")
