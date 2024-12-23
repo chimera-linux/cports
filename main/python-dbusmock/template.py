f4ee6d6921e8baffeb35d94c00b8e3e03e72756d
triallax <triallax@tutanota.com>
2024-12-23 15:11:39 +0000

    main/python-dbusmock: update to 0.34.1

--- main/python-dbusmock/template.py
+++ main/python-dbusmock/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-dbusmock"
-pkgver = "0.33.0"
+pkgver = "0.34.1"
 pkgrel = 0
 build_style = "python_pep517"
 # needs upower
@@ -11,10 +11,24 @@ hostmakedepends = [
     "python-wheel",
 ]
 depends = ["python-dbus"]
-checkdepends = ["dbus", "python-gobject", "python-pytest", *depends]
+checkdepends = [
+    "bluez",
+    "dbus",
+    "elogind",
+    "libnotify",
+    "modemmanager",
+    "networkmanager",
+    "polkit",
+    "python-gobject",
+    "python-pytest",
+    # Cycle
+    # "power-profiles-daemon",
+    # "upower",
+    *depends,
+]
 pkgdesc = "D-Bus object mocks for python"
 maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "LGPL-3.0-or-later"
 url = "https://github.com/martinpitt/python-dbusmock"
-source = f"https://github.com/martinpitt/python-dbusmock/releases/download/{pkgver}/dist.python-dbusmock-{pkgver}.tar.gz"
-sha256 = "04efd311dd1063ac2b8f7baa79a026b5c0aa3ed4ef18cd9226f52a273fa6193a"
+source = f"https://github.com/martinpitt/python-dbusmock/releases/download/{pkgver}/dist.python_dbusmock-{pkgver}.tar.gz"
+sha256 = "81a0ef601995889842a4549747cf017f058db2c6193d8dd8fbdc4ee57bea4941"
