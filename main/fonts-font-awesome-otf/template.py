b10d80c6e08594038c4f389d188bc8b0930f3990
triallax <triallax@tutanota.com>
2024-12-23 14:05:30 +0000

    main/fonts-font-awesome-otf: update to 6.7.2

--- main/fonts-font-awesome-otf/template.py
+++ main/fonts-font-awesome-otf/template.py
@@ -1,12 +1,12 @@
 pkgname = "fonts-font-awesome-otf"
-pkgver = "6.7.1"
+pkgver = "6.7.2"
 pkgrel = 0
 pkgdesc = "Iconic font set"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "OFL-1.1"
 url = "https://fontawesome.com"
 source = f"https://github.com/FortAwesome/Font-Awesome/releases/download/{pkgver}/fontawesome-free-{pkgver}-desktop.zip"
-sha256 = "3118838d8d0aa88b8c9a5e132f8a195a3f1b23895ae66c61dc6746f9ceef80da"
+sha256 = "22ff7898b429b997a45e1cf89bb869ed3abcc65333d90289181ba5363c8fd19b"
 
 
 def install(self):
