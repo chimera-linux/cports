957ba798d484a5c48fffad72268ce9e02233a03f
q66 <q66@chimera-linux.org>
2024-12-24 15:26:27 +0100

    user/cargo-crev: update to 0.26.2 and move from main

--- user/cargo-crev/template.py
+++ user/cargo-crev/template.py
@@ -1,10 +1,8 @@
 pkgname = "cargo-crev"
-pkgver = "0.25.9"
-pkgrel = 1
+pkgver = "0.26.2"
+pkgrel = 0
 build_wrksrc = "cargo-crev"
 build_style = "cargo"
-# remove this once updated to new cargo crate that has new libgit2
-make_env = {"LIBGIT2_NO_VENDOR": "0"}
 hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = ["openssl-devel", "libgit2-devel", "rust-std", "sqlite-devel"]
 pkgdesc = "Cryptographically verifiable code review system for cargo"
@@ -12,7 +10,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "MPL-2.0 OR MIT OR Apache-2.0"
 url = "https://github.com/crev-dev/cargo-crev"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7ebc63b272f09730d44c469d39413e3208538e885cf977bf4a61d768948700a2"
+sha256 = "b2204510acf65667418980dc6e93580167e738376ee888f810064542fa04ef92"
 # takes forever to run literally 2 unittests
 options = ["!check"]
 
