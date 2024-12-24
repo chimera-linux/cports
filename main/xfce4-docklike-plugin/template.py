478b348bac07b945aa1a69deb5b019ad951b4b18
triallax <triallax@tutanota.com>
2024-12-24 20:55:47 +0000

    main/xfce4-docklike-plugin: update to 0.4.3

--- main/xfce4-docklike-plugin/template.py
+++ main/xfce4-docklike-plugin/template.py
@@ -1,22 +1,22 @@
 pkgname = "xfce4-docklike-plugin"
-pkgver = "0.4.2"
+pkgver = "0.4.3"
 pkgrel = 0
 build_style = "gnu_configure"
-make_dir = "."
 hostmakedepends = [
     "automake",
     "gettext-devel",
-    "intltool",
     "libtool",
     "pkgconf",
     "xfce4-dev-tools",
 ]
 makedepends = [
+    "cairo-devel",
     "gtk+3-devel",
-    "libwnck-devel",
+    "gtk-layer-shell-devel",
     "libx11-devel",
     "libxfce4ui-devel",
     "libxfce4util-devel",
+    "libxfce4windowing-devel",
     "xfce4-panel-devel",
 ]
 pkgdesc = "Xfce taskbar panel plugin"
@@ -24,4 +24,4 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-3.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-docklike-plugin/start"
 source = f"$(XFCE_SITE)/panel-plugins/xfce4-docklike-plugin/{pkgver[:-2]}/xfce4-docklike-plugin-{pkgver}.tar.bz2"
-sha256 = "b6a40b976a78f2abb1bec057c48d45bfb317e00b12e05a7dfcbea4d183f8db71"
+sha256 = "e81e16b4ab1c655a3202473d78cc81617bb4829e5dd102eecabf9addd7668a9d"
