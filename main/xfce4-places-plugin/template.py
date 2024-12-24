d26dc2b9001594cad01ef3a8bf563c962b386f31
triallax <triallax@tutanota.com>
2024-12-24 20:57:58 +0000

    main/xfce4-places-plugin: update to 1.8.4

--- main/xfce4-places-plugin/template.py
+++ main/xfce4-places-plugin/template.py
@@ -1,14 +1,12 @@
 pkgname = "xfce4-places-plugin"
-pkgver = "1.8.3"
+pkgver = "1.8.4"
 pkgrel = 0
 build_style = "gnu_configure"
-make_dir = "."
 hostmakedepends = [
     "automake",
     "gettext-devel",
-    "intltool",
-    "libtool",
     "pkgconf",
+    "slibtool",
     "xfce4-dev-tools",
 ]
 makedepends = [
@@ -16,7 +14,6 @@ makedepends = [
     "gtk+3-devel",
     "libnotify-devel",
     "libwnck-devel",
-    "libx11-devel",
     "libxfce4ui-devel",
     "libxfce4util-devel",
     "xfce4-panel-devel",
@@ -27,4 +24,4 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-places-plugin/start"
 source = f"$(XFCE_SITE)/panel-plugins/xfce4-places-plugin/{pkgver[:-2]}/xfce4-places-plugin-{pkgver}.tar.bz2"
-sha256 = "f11d0e6d03f22ab02c2e6b507d365b5a918532e8819e50647ee1860eca60c743"
+sha256 = "ba766a5d31580fad043fbd1fd66b811cbda706229473d24a734a590d49ef710e"
