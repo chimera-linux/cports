32c6738b7b873900c5b6f1ab3b5d39c1be757796
triallax <triallax@tutanota.com>
2024-12-24 20:51:06 +0000

    main/xfce4-cpufreq-plugin: update to 1.2.9

--- main/xfce4-cpufreq-plugin/template.py
+++ main/xfce4-cpufreq-plugin/template.py
@@ -1,12 +1,10 @@
 pkgname = "xfce4-cpufreq-plugin"
-pkgver = "1.2.8"
+pkgver = "1.2.9"
 pkgrel = 0
 build_style = "gnu_configure"
-make_dir = "."
 hostmakedepends = [
     "automake",
     "gettext-devel",
-    "intltool",
     "libtool",
     "pkgconf",
     "xfce4-dev-tools",
@@ -22,4 +20,4 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-cpufreq-plugin/start"
 source = f"$(XFCE_SITE)/panel-plugins/xfce4-cpufreq-plugin/{pkgver[:-2]}/xfce4-cpufreq-plugin-{pkgver}.tar.bz2"
-sha256 = "07e458d9f4725e572001fb7eb66b9e931792311146e0f75ad5d87b9ae19573e9"
+sha256 = "d0714720d588c649457590e5de3d95859b922a98d5fa9d0d1416f36a76bd3ef9"
