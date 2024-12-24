71ea45138af766c50c07e525ed34039d681a716b
triallax <triallax@tutanota.com>
2024-12-24 20:56:20 +0000

    main/xfce4-fsguard-plugin: update to 1.1.4

--- main/xfce4-fsguard-plugin/template.py
+++ main/xfce4-fsguard-plugin/template.py
@@ -1,14 +1,12 @@
 pkgname = "xfce4-fsguard-plugin"
-pkgver = "1.1.3"
+pkgver = "1.1.4"
 pkgrel = 0
 build_style = "gnu_configure"
-make_dir = "."
 hostmakedepends = [
     "automake",
     "gettext-devel",
-    "intltool",
-    "libtool",
     "pkgconf",
+    "slibtool",
     "xfce4-dev-tools",
 ]
 makedepends = [
@@ -23,7 +21,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "BSD-2-Clause"
 url = "https://docs.xfce.org/panel-plugins/xfce4-fsguard-plugin/start"
 source = f"$(XFCE_SITE)/panel-plugins/xfce4-fsguard-plugin/{pkgver[:-2]}/xfce4-fsguard-plugin-{pkgver}.tar.bz2"
-sha256 = "84ef8bb4752292d64c0ef101badf7b14448790bfa0a85de644dbfa22986ec258"
+sha256 = "24b3789be6de92678e7036678530c4da4b3838aa3cda428439aa8d140704a4a6"
 
 
 def post_install(self):
