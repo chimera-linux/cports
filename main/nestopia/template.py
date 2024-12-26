50e93dea27c61ea55f887cfcb1db1492431164d5
Erica Z <zerica@callcc.eu>
2024-12-26 16:08:27 +0100

    main/nestopia: update to 1.53.0

--- main/nestopia/template.py
+++ main/nestopia/template.py
@@ -1,12 +1,13 @@
 pkgname = "nestopia"
-pkgver = "1.52.1"
-pkgrel = 1
+pkgver = "1.53.0"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["autoconf-archive", "automake", "pkgconf"]
 makedepends = [
     "fltk-devel",
     "fontconfig-devel",
     "libarchive-devel",
+    "libepoxy-devel",
     "libjpeg-turbo-devel",
     "libpng-devel",
     "libxft-devel",
@@ -18,5 +19,5 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-2.0-or-later"
 url = "http://0ldsk00l.ca/nestopia"
 source = f"https://github.com/0ldsk00l/nestopia/archive/{pkgver}.tar.gz"
-sha256 = "c9c0bce673eb3b625b538b462e49c00ed1ee1ded1e0bad09be780076880968b5"
+sha256 = "27a26a6fd92e6acc2093bbd6c1e3ab7f2fff419d9ed6de13bc43349b52e1f705"
 tool_flags = {"CXXFLAGS": ["-Wno-c++11-narrowing"]}
