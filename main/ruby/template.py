0e388b1206e757aa5cc83a77cdd746b8c2c716ef
q66 <q66@chimera-linux.org>
2024-12-26 15:27:05 +0100

    main/ruby: update to 3.4.1

--- main/ruby/template.py
+++ main/ruby/template.py
@@ -1,5 +1,5 @@
 pkgname = "ruby"
-pkgver = "3.3.6"
+pkgver = "3.4.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -31,7 +31,7 @@ url = "https://www.ruby-lang.org/en"
 source = (
     f"https://cache.ruby-lang.org/pub/ruby/{pkgver[:-2]}/ruby-{pkgver}.tar.xz"
 )
-sha256 = "540975969d1af42190d26ff629bc93b1c3f4bffff4ab253e245e125085e66266"
+sha256 = "018d59ffb52be3c0a6d847e22d3fd7a2c52d0ddfee249d3517a0c8c6dbfa70af"
 # until verified; gonna need removing arch prefix from compiler name
 # tests mostly pass but there are some portability issues in the test
 # suite (stat usage) + chown not working in the sandbox + locale issues
