f62fd11f669406f5d8e9ec575d6d6047bf65e30d
Matthias Nyman <natthias@proton.me>
2024-12-26 09:04:35 +0100

    main/regreet: update to 0.1.3

--- main/regreet/template.py
+++ main/regreet/template.py
@@ -1,5 +1,5 @@
 pkgname = "regreet"
-pkgver = "0.1.2"
+pkgver = "0.1.3"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = [
@@ -19,7 +19,7 @@ maintainer = "natthias <natthias@proton.me>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/rharish101/ReGreet"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "7358f5536d1fbbf36e0d9ba152a393d85d733ea5946bdc0c720f460de2c5e8e4"
+sha256 = "94663c856aa748a0ab973a77fb0851a1cd8afcea7c5c273d4671eca4ba8ad3c3"
 
 
 def post_install(self):
