39d80bd618cd4c5a99da962047d26d77ab84e958
ttyyls <contact@behri.org>
2024-12-26 12:10:25 +0100

    user/gcli: new package

---
+++ user/gcli/template.py
@@ -0,0 +1,24 @@
+pkgname = "gcli"
+pkgver = "2.6.1"
+pkgrel = 0
+build_style = "configure"
+configure_args = ["--prefix=/usr"]
+hostmakedepends = ["byacc", "flex", "kyua", "pkgconf"]
+makedepends = [
+    "atf-devel",
+    "curl-devel",
+    "libedit-devel",
+    "lowdown-devel",
+    "musl-bsd-headers",
+    "openssl3-devel",
+]
+pkgdesc = "CLI tool for interacting with Git(Hub|Lab|Tea)"
+maintainer = "ttyyls <contact@behri.org>"
+license = "BSD-2-Clause"
+url = "https://herrhotzenplotz.de/gcli"
+source = f"{url}/releases/gcli-{pkgver}/gcli-{pkgver}.tar.xz"
+sha256 = "ce91365eb6220056a1326cfa2afe287b7bb1708de9af12141dab21a00965c79a"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
