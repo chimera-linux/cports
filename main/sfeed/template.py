89eda9e994e7afd199a9fe825310f490a442bb80
ttyyls <contact@behri.org>
2024-12-27 22:46:41 +0100

    main/sfeed: update to 2.1

--- main/sfeed/template.py
+++ main/sfeed/template.py
@@ -1,6 +1,6 @@
 pkgname = "sfeed"
-pkgver = "2.0"
-pkgrel = 1
+pkgver = "2.1"
+pkgrel = 0
 build_style = "makefile"
 make_install_args = [
     "COMPATOBJ=",
@@ -14,7 +14,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "ISC"
 url = "https://codemadness.org/sfeed.html"
 source = f"https://codemadness.org/releases/sfeed/sfeed-{pkgver}.tar.gz"
-sha256 = "8e6a7e6e1d7e86034ae27035e37a8f7fc98bc25fe35120bd18ff07e04f18e91d"
+sha256 = "dd54c9b3ff8c47a67ceae64b8cd62b064ebbf2f11715386d89603ecd276e3705"
 hardening = ["vis", "cfi"]
 # no tests defined
 options = ["!check"]
