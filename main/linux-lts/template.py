0d2982dceccf599f3885667ad7c018b1d9fb765c
q66 <q66@chimera-linux.org>
2024-12-28 20:40:36 +0100

    main/linux-lts: update to 6.6.68

--- main/linux-lts/template.py
+++ main/linux-lts/template.py
@@ -1,6 +1,6 @@
 # update linux-lts-zfs-bin when bumping
 pkgname = "linux-lts"
-pkgver = "6.6.66"
+pkgver = "6.6.68"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]
 build_style = "linux-kernel"
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
-sha256 = "9d757937c4661c2f512c62641b74ef74eff9bb13dc5dbcbaaa108c21152f1e52"
+sha256 = "283ff410e3f352ceed161ae30c0020301326059db03e86efcb384d46ac5840e2"
 # no meaningful checking to be done
 options = [
     "!check",
