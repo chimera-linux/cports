0b126e137eb1837f7ddfb4d6e055c832da3a2242
breakgimme <adam@plock.com>
2024-12-29 22:05:17 +0100

    user/pods: new package

---
+++ user/pods/template.py
@@ -0,0 +1,50 @@
+pkgname = "pods"
+pkgver = "2.1.2"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "cargo-auditable",
+    "desktop-file-utils",
+    "gettext",
+    "glib-devel",
+    "gtk-update-icon-cache",
+    "meson",
+    "pkgconf",
+    "rust-clippy",
+]
+makedepends = [
+    "appstream-glib-devel",
+    "glib-devel",
+    "gtksourceview-devel",
+    "gtk4-devel",
+    "libadwaita-devel",
+    "rust-std",
+    "vte-gtk4-devel",
+]
+pkgdesc = "GTK frontend for podman"
+maintainer = "breakgimme <adam@plock.com>"
+license = "GPL-3.0-only"
+url = "https://github.com/marhkb/pods"
+source = f"https://github.com/marhkb/pods/releases/download/v{pkgver}/pods-v{pkgver}.tar.xz"
+sha256 = "259fe1830e1efcde5bea5afabd234907361ef6727c0b29c4f289427b1f1e5360"
+
+
+def post_extract(self):
+    self.rm(".cargo/config")
+
+
+def post_patch(self):
+    from cbuild.util import cargo
+
+    cargo.Cargo(self, wrksrc=".").vendor()
+
+
+def init_build(self):
+    from cbuild.util import cargo
+
+    renv = cargo.get_environment(self)
+    self.make_env.update(renv)
+
+
+def post_install(self):
+    self.install_bin(f"./build/src/{self.profile().triplet}/release/pods")
