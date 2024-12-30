485bdd05d0afe3d9c5031815add09f7d910309f6
ttyyls <contact@behri.org>
2024-12-30 13:08:41 +0100

    main/tinymist: update to 0.12.14, adopt

--- main/tinymist/template.py
+++ main/tinymist/template.py
@@ -1,17 +1,17 @@
 pkgname = "tinymist"
-pkgver = "0.12.12"
+pkgver = "0.12.14"
 pkgrel = 0
 build_style = "cargo"
-make_build_args = ["--bin", "tinymist", "--bin", "typlite"]
+make_build_args = ["--features=cli", "--bin", "tinymist", "--bin", "typlite"]
 hostmakedepends = ["cargo", "pkgconf"]
 makedepends = ["rust-std", "openssl-devel"]
 depends = ["typst"]
 pkgdesc = "Language server for Typst"
-maintainer = "Orphaned <orphaned@chimera-linux.org>"
+maintainer = "ttyyls <contact@behri.org>"
 license = "Apache-2.0"
 url = "https://myriad-dreamin.github.io/tinymist"
 source = f"https://github.com/Myriad-Dreamin/tinymist/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f9cb474364d2f1e42a51a0c409b03e7f2482787f260d7a2ab6df71dce27b4d47"
+sha256 = "24bd7b64487158f41ca980066fff3b9b95b2d5367d364c62b23974f6a82cfd32"
 # takes forever
 options = ["!check"]
 
