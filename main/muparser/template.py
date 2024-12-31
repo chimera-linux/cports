28e55b3e6f09b29fcfedb16a259d746b4c11b519
q66 <q66@chimera-linux.org>
2024-12-31 12:23:10 +0100

    main/muparser: make openmp optional

--- main/muparser/template.py
+++ main/muparser/template.py
@@ -2,20 +2,23 @@ pkgname = "muparser"
 pkgver = "2.3.4"
 pkgrel = 0
 build_style = "cmake"
+configure_args = []
 hostmakedepends = [
     "cmake",
     "ninja",
     "pkgconf",
 ]
-makedepends = [
-    "libomp-devel",
-]
+makedepends = []
 pkgdesc = "Qt implementation of freedesktop.org xdg specs"
 maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "BSD-2-Clause"
 url = "https://beltoforion.de/en/muparser"
 source = f"https://github.com/beltoforion/muparser/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "0c3fa54a3ebf36dda0ed3e7cd5451c964afbb15102bdbcba08aafb359a290121"
+
+if self.profile().arch in ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]:
+    configure_args += ["-DENABLE_OPENMP=OFF"]
+    makedepends += ["libomp-devel"]
 
 
 def post_install(self):
