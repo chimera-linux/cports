92e28b2538cbd6fe15b3e8f4d54c57e617c92c07
q66 <q66@chimera-linux.org>
2024-12-31 12:08:55 +0100

    main/openblas: make openmp optional

--- main/openblas/template.py
+++ main/openblas/template.py
@@ -3,10 +3,10 @@ pkgver = "0.3.28"
 pkgrel = 0
 archs = ["aarch64", "ppc", "ppc64", "ppc64le", "riscv64", "x86_64"]
 build_style = "cmake"
-configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DUSE_OPENMP=1"]
+configure_args = ["-DBUILD_SHARED_LIBS=ON"]
 # FIXME: flang support
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
-makedepends = ["libomp-devel", "linux-headers"]
+makedepends = ["linux-headers"]
 pkgdesc = "Optimized BLAS library"
 maintainer = "Erica Z <zerica@callcc.eu>"
 license = "BSD-3-Clause"
@@ -14,11 +14,14 @@ url = "https://www.openblas.net"
 source = f"https://github.com/OpenMathLib/OpenBLAS/releases/download/v{pkgver}/OpenBLAS-{pkgver}.tar.gz"
 sha256 = "f1003466ad074e9b0c8d421a204121100b0751c96fc6fcf3d1456bd12f8a00a1"
 
+_have_omp = True
+
 match self.profile().arch:
     case "aarch64":
         configure_args += ["-DTARGET=ARMV8"]
     case "ppc":
         configure_args += ["-DTARGET=PPCG4"]
+        _have_omp = False
     case "ppc64":
         configure_args += ["-DTARGET=PPC970MP"]
     case "ppc64le":
@@ -28,12 +31,16 @@ match self.profile().arch:
     case "x86_64":
         configure_args += ["-DTARGET=GENERIC"]
 
-if self.profile().arch in ["aarch64", "ppc64le", "riscv", "x86_64"]:
+if self.profile().arch in ["aarch64", "ppc64le", "riscv64", "x86_64"]:
     configure_args += ["-DDYNAMIC_ARCH=ON"]
 
 if self.profile().arch in ["ppc", "ppc64", "ppc64le"]:
     # needs GNU as
     hostmakedepends += [f"binutils-{self.profile().arch}"]
+
+if _have_omp:
+    makedepends += ["libomp-devel"]
+    configure_args += ["-DUSE_OPENMP=1"]
 
 
 def post_install(self):
