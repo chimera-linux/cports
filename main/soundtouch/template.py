4622a2edb097edea81237e22b440ff2f18b2ee80
q66 <q66@chimera-linux.org>
2024-12-31 12:21:13 +0100

    main/soundtouch: make openmp optional

--- main/soundtouch/template.py
+++ main/soundtouch/template.py
@@ -2,17 +2,21 @@ pkgname = "soundtouch"
 pkgver = "2.3.3"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_args = ["--enable-openmp"]
+configure_args = []
 configure_gen = ["./bootstrap"]
 make_build_args = ["V=1"]
 hostmakedepends = ["pkgconf", "automake", "libtool"]
-makedepends = ["libomp-devel"]
+makedepends = []
 pkgdesc = "SoundTouch audio processing library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-only"
 url = "https://www.surina.net/soundtouch"
 source = f"{url}/soundtouch-{pkgver}.tar.gz"
 sha256 = "43b23dfac2f64a3aff55d64be096ffc7b73842c3f5665caff44975633a975a99"
+
+if self.profile().arch in ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]:
+    configure_args += ["--enable-openmp"]
+    makedepends += ["libomp-devel"]
 
 
 @subpackage("soundtouch-devel")
