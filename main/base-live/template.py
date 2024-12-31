fa519aedb99d8f6735b727814b7616bc22de522f
q66 <q66@chimera-linux.org>
2024-12-31 22:42:33 +0100

    main/base-live: add back bootloader
    We probably want this because a live image should contain a
    bootloader for local installs without internet access. Without
    it, it's not possible to install it.
    
    While at it, add systemd-boot and limine for efi targets as well.

--- main/base-live/template.py
+++ main/base-live/template.py
@@ -1,6 +1,6 @@
 pkgname = "base-live"
 pkgver = "0.1"
-pkgrel = 1
+pkgrel = 2
 build_style = "meta"
 depends = [
     "cryptsetup-scripts",
@@ -12,3 +12,18 @@ pkgdesc = "Packages to be included in official live images"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:meta"
 url = "https://chimera-linux.org"
+
+# grub for local installations without net access
+match self.profile().arch:
+    case "aarch64":
+        depends += ["grub-arm64-efi"]
+    case "ppc" | "ppc64" | "ppc64le":
+        depends += ["grub-powerpc-ieee1275"]
+    case "riscv64":
+        depends += ["grub-riscv64-efi"]
+    case "x86_64":
+        depends += ["grub-i386-efi", "grub-i386-pc", "grub-x86_64-efi"]
+
+# extra bootloaders on efi targets, again for offline install
+if self.profile().arch in ["aarch64", "riscv64", "x86_64"]:
+    depends += ["limine", "systemd-boot"]
