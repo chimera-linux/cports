01b98bd703c4c0b730283e2f78c9530fe95b551c
q66 <q66@chimera-linux.org>
2024-12-31 12:23:55 +0100

    main/lensfun: make openmp optional

--- main/lensfun/template.py
+++ main/lensfun/template.py
@@ -4,7 +4,7 @@ pkgrel = 2
 build_style = "cmake"
 configure_args = ["-DINSTALL_HELPER_SCRIPTS=OFF", "-DBUILD_TESTS=ON"]
 hostmakedepends = ["cmake", "ninja", "pkgconf", "python-setuptools"]
-makedepends = ["glib-devel", "libomp-devel"]
+makedepends = ["glib-devel"]
 pkgdesc = "Photographic lens distortion library"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-3.0-only AND CC-BY-SA-3.0"
@@ -13,6 +13,9 @@ source = (
     f"https://github.com/lensfun/lensfun/archive/refs/tags/v{pkgver}.tar.gz"
 )
 sha256 = "dafb39c08ef24a0e2abd00d05d7341b1bf1f0c38bfcd5a4c69cf5f0ecb6db112"
+
+if self.profile().arch in ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]:
+    makedepends += ["libomp-devel"]
 
 
 @subpackage("lensfun-devel")
