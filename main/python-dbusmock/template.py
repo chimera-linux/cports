1a79684a736b6156425af6d6eb965ba95dd05f8e
triallax <triallax@tutanota.com>
2025-01-01 23:01:00 +0000

    main/python-dbusmock: update to 0.34.2

--- main/python-dbusmock/template.py
+++ main/python-dbusmock/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-dbusmock"
-pkgver = "0.34.1"
+pkgver = "0.34.2"
 pkgrel = 0
 build_style = "python_pep517"
 # needs upower
@@ -25,5 +25,7 @@ pkgdesc = "D-Bus object mocks for python"
 maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "LGPL-3.0-or-later"
 url = "https://github.com/martinpitt/python-dbusmock"
-source = f"https://github.com/martinpitt/python-dbusmock/releases/download/{pkgver}/dist.python_dbusmock-{pkgver}.tar.gz"
-sha256 = "81a0ef601995889842a4549747cf017f058db2c6193d8dd8fbdc4ee57bea4941"
+source = (
+    f"{url}/releases/download/{pkgver}/dist.python_dbusmock-{pkgver}.tar.gz"
+)
+sha256 = "69c3137f15d3397846fd4aeb2e7f41289aa4c7dc2a894502225186d1fcb396ec"
