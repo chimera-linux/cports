cb14d9c7195e5ef46411252ab0cf5c380d8c518b
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-01-01 18:04:00 +0100

    user/taskwarrior: update to 3.3.0

--- user/taskwarrior/template.py
+++ user/taskwarrior/template.py
@@ -1,17 +1,17 @@
 pkgname = "taskwarrior"
-pkgver = "3.1.0"
+pkgver = "3.3.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DSYSTEM_CORROSION=ON"]
 make_dir = "."
-hostmakedepends = ["cmake", "ninja", "cargo", "corrosion"]
+hostmakedepends = ["cmake", "ninja", "cargo", "corrosion", "cxxbridge"]
 makedepends = ["libuuid-devel", "rust-std", "sqlite-devel"]
 pkgdesc = "TODO list manager for the command line"
 maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "MIT"
 url = "https://taskwarrior.org"
 source = f"https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v{pkgver}/task-{pkgver}.tar.gz"
-sha256 = "1ae67c74b84067573a53095cf3cb6718245dd7dd808f19f9b3d85da445838b4f"
+sha256 = "7fd1e3571f673679758f001b5f44963eee59fd0d2cac887a5807cf2fd90856a1"
 
 if self.profile().wordsize == 32:
     broken = "atomic64 assumptions"
