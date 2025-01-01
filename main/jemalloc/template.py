4c3d858b990d7b31236b1033554cce3d6009b46c
q66 <q66@chimera-linux.org>
2025-01-01 20:35:42 +0100

    main/jemalloc: also use bigger pages on ppc

--- main/jemalloc/template.py
+++ main/jemalloc/template.py
@@ -17,7 +17,7 @@ source = f"https://github.com/jemalloc/jemalloc/releases/download/{pkgver}/jemal
 sha256 = "2db82d1e7119df3e71b7640219b6dfe84789bc0537983c3b7ac4f7189aecfeaa"
 
 match self.profile().arch:
-    case "aarch64" | "ppc64" | "ppc64le":
+    case "aarch64" | "ppc" | "ppc64" | "ppc64le":
         configure_args += ["--with-lg-page=16"]
     case _:
         configure_args += ["--with-lg-page=12"]
