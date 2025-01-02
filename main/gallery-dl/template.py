37af6bdace30733f798654f01b6632c25b34afc4
triallax <triallax@tutanota.com>
2025-01-02 00:22:54 +0000

    main/gallery-dl: update to 1.28.3

--- main/gallery-dl/template.py
+++ main/gallery-dl/template.py
@@ -1,5 +1,5 @@
 pkgname = "gallery-dl"
-pkgver = "1.28.1"
+pkgver = "1.28.3"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
@@ -20,7 +20,7 @@ maintainer = "Nasado <hi@nasado.name>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/mikf/gallery-dl"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "419377669f53f3815e1042a267373b6d5588c3ce7a2804c6841df19d433321d6"
+sha256 = "abd2dc28dba629da21238353c57151a1af37b44c66e5f0c72ef5ed9479554166"
 
 
 def pre_build(self):
