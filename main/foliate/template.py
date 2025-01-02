1f9608a4b2adb5ad52bebf2fc2b465d66b944dbb
triallax <triallax@tutanota.com>
2025-01-02 00:44:00 +0000

    main/foliate: update to 3.2.1

--- main/foliate/template.py
+++ main/foliate/template.py
@@ -1,5 +1,5 @@
 pkgname = "foliate"
-pkgver = "3.2.0"
+pkgver = "3.2.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -25,5 +25,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://johnfactotum.github.io/foliate"
 source = f"https://github.com/johnfactotum/foliate/releases/download/{pkgver}/com.github.johnfactotum.Foliate-{pkgver}.tar.xz"
-sha256 = "714d42d54e103f4a3c367bf18186a56401228b7ee1175224e760ae59f204e1e6"
+sha256 = "01c27a8c481a9ebc4a3237e2947c3e86ef36cc0fac2f4199e7a79e554d676ea4"
 options = ["!cross"]
