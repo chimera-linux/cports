5bcd8c75c7e5aa0b67db6fb334a2418efdbd9835
triallax <triallax@tutanota.com>
2025-01-02 00:18:45 +0000

    main/fonts-noto: update to 2025.01.01

--- main/fonts-noto/template.py
+++ main/fonts-noto/template.py
@@ -1,12 +1,12 @@
 pkgname = "fonts-noto"
-pkgver = "2024.12.01"
+pkgver = "2025.01.01"
 pkgrel = 0
 pkgdesc = "Google Noto fonts"
 maintainer = "GeopJr <evan@geopjr.dev>"
 license = "OFL-1.1"
 url = "https://github.com/notofonts/notofonts.github.io"
 source = f"{url}/archive/refs/tags/noto-monthly-release-{pkgver}.zip"
-sha256 = "04620d14d924dea9bb08c0090578a21d349ab0a76cdaa5126c60c60de66d2542"
+sha256 = "24d78988a21778f7630b3bf2858ab6b7341ecb47aca452bc218bf5a3003b96e4"
 
 
 def install(self):
