86da4281480c66a7d12ccdd7194719a051da71f5
q66 <q66@chimera-linux.org>
2025-01-02 18:41:31 +0100

    main/kanata: broken on 32bit

--- main/kanata/template.py
+++ main/kanata/template.py
@@ -10,3 +10,6 @@ license = "LGPL-3.0-only"
 url = "https://github.com/jtroo/kanata"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "eb7e11511f77558d72b5b3b0c9defb04b269637e5c8a4ad9b45d21382e9247d2"
+
+if self.profile().wordsize == 32:
+    broken = "needs atomic64"
