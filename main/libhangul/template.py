4675cd3f1109911fcb341a9742102331610a4638
q66 <q66@chimera-linux.org>
2025-01-02 13:12:36 +0100

    main/libhangul: fix build
    Using the bundled autogen mysteriously brings in an outdated
    po.m4 from the internet, breaking it again.

--- main/libhangul/template.py
+++ main/libhangul/template.py
@@ -2,7 +2,6 @@ pkgname = "libhangul"
 pkgver = "0.1.0"
 pkgrel = 0
 build_style = "gnu_configure"
-configure_gen = ["./autogen.sh"]
 hostmakedepends = ["automake", "gettext-devel", "libtool", "pkgconf"]
 pkgdesc = "Library to support hangul input and character classification"
 maintainer = "Isaac Freund <mail@isaacfreund.com>"
@@ -11,6 +10,10 @@ url = "https://github.com/libhangul/libhangul"
 source = f"{url}/archive/libhangul-{pkgver}.tar.gz"
 sha256 = "e2a81ef159ed098d3cc1a20377dba6204821b7ce2bc24cfb2f2543adf3bc5830"
 hardening = ["!vis", "!cfi"]
+
+
+def post_extract(self):
+    (self.cwd / "ChangeLog").touch()
 
 
 @subpackage("libhangul-devel")
