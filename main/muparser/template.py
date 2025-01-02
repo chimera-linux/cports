df5c7e711365f5595efa2a4b4c2b844183b58231
q66 <q66@chimera-linux.org>
2025-01-02 18:47:56 +0100

    main/muparser: fix omp disable

--- main/muparser/template.py
+++ main/muparser/template.py
@@ -17,8 +17,9 @@ source = f"https://github.com/beltoforion/muparser/archive/refs/tags/v{pkgver}.t
 sha256 = "0c3fa54a3ebf36dda0ed3e7cd5451c964afbb15102bdbcba08aafb359a290121"
 
 if self.profile().arch in ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]:
-    configure_args += ["-DENABLE_OPENMP=OFF"]
     makedepends += ["libomp-devel"]
+else:
+    configure_args += ["-DENABLE_OPENMP=OFF"]
 
 
 def post_install(self):
