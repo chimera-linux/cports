ea9140596c8707f9f05ec2b0be841477039b8759
q66 <q66@chimera-linux.org>
2025-01-03 18:00:44 +0100

    main/i3blocks: fix bashcomp

--- main/i3blocks/template.py
+++ main/i3blocks/template.py
@@ -1,6 +1,6 @@
 pkgname = "i3blocks"
 pkgver = "1.5"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_gen = ["./autogen.sh"]
 make_dir = "."
@@ -13,3 +13,8 @@ url = "https://github.com/vivien/i3blocks"
 source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "41764d771043d0c06c23d75b1e3ca6b2b94279191483d03f10c5e034d6722ebf"
 hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    # spurious file
+    self.uninstall("usr/share/bash-completion/completions/bash-completion")
