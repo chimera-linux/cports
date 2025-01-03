e9821aea42fdc0c02f06ebd33bc6bce193e3cfb9
q66 <q66@chimera-linux.org>
2025-01-03 11:11:15 +0100

    main/ra-multiplex: broken on 32bit

--- main/ra-multiplex/template.py
+++ main/ra-multiplex/template.py
@@ -12,6 +12,9 @@ url = "https://github.com/pr2502/ra-multiplex"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "c24a7e277adce9bbfb86641905d75f166e46459cf4e5b5f3aaa7456b052392dc"
 
+if self.profile().wordsize == 32:
+    broken = "needs atomic64"
+
 
 def post_install(self):
     self.install_license("LICENSE")
