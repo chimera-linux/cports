e76a5c38464fbaa8d23bf6e79415172f2b9f8769
q66 <q66@chimera-linux.org>
2025-01-04 19:14:16 +0100

    main/erofs-utils: update to 1.8.4

--- main/erofs-utils/template.py
+++ main/erofs-utils/template.py
@@ -1,5 +1,5 @@
 pkgname = "erofs-utils"
-pkgver = "1.8.3"
+pkgver = "1.8.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -30,5 +30,5 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "Apache-2.0 AND GPL-2.0-or-later"
 url = "https://github.com/erofs/erofs-utils"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "3c5cc03603ea08ba9ae5e0420eeaea5ff17ed29e2280685310356cbf25304e85"
+sha256 = "791587aa074bb9f9fa218c762cc1f60b015e2f51bcb2ce91f682f043856ab494"
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=2097152"]}
