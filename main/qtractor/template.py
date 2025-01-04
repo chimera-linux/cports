5f49754a23e3ef28c6f5f9414f801dc51fd20707
Erica Z <zerica@callcc.eu>
2025-01-04 20:19:00 +0100

    main/qtractor: update to 1.5.1

--- main/qtractor/template.py
+++ main/qtractor/template.py
@@ -1,5 +1,5 @@
 pkgname = "qtractor"
-pkgver = "1.5.0"
+pkgver = "1.5.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DCONFIG_WAYLAND=1"]
@@ -23,4 +23,4 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-2.0-or-later"
 url = "https://www.qtractor.org"
 source = f"$(SOURCEFORGE_SITE)/qtractor/qtractor-{pkgver}.tar.gz"
-sha256 = "a7616e69db36b93884217452eeca9ef056b75c42b05b5343fe93e75211652dd6"
+sha256 = "8c127caabb9605eba4b96ef3ce2f6b45ac892fe142d5eddff8eddc6fa41f7621"
