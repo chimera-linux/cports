683accad39b4d12182db20b81ff939a0ff4c5cac
q66 <q66@chimera-linux.org>
2025-01-04 02:50:44 +0100

    main/nano: update to 8.3

--- main/nano/template.py
+++ main/nano/template.py
@@ -1,5 +1,5 @@
 pkgname = "nano"
-pkgver = "8.2"
+pkgver = "8.3"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-utf8"]
@@ -11,7 +11,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://www.nano-editor.org"
 source = f"https://www.nano-editor.org/dist/v{pkgver[0]}/nano-{pkgver}.tar.xz"
-sha256 = "d5ad07dd862facae03051c54c6535e54c7ed7407318783fcad1ad2d7076fffeb"
+sha256 = "551b717b2e28f7e90f749323686a1b5bbbd84cfa1390604d854a3ca3778f111e"
 hardening = ["vis", "cfi"]
 
 
