422b9912d8a2b36dc833d3fd234c3a7d4c6d67d7
q66 <q66@chimera-linux.org>
2025-01-04 19:08:23 +0100

    main/musl-bsd-headers: bump
    It seems there may be something wrong with the ppc package?

--- main/musl-bsd-headers/template.py
+++ main/musl-bsd-headers/template.py
@@ -1,6 +1,6 @@
 pkgname = "musl-bsd-headers"
 pkgver = "0.1"
-pkgrel = 0
+pkgrel = 1
 pkgdesc = "BSD compatibility headers for musl"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause AND BSD-3-Clause"
