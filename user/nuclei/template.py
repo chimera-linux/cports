212d858e4e31771cf2f50ed1f4d83c6b1382a3f5
Gabriel M. Dutra <gabrieldmdutra@gmail.com>
2025-01-04 23:58:38 -0300

    user/nuclei: update to 3.3.7

--- user/nuclei/template.py
+++ user/nuclei/template.py
@@ -1,5 +1,5 @@
 pkgname = "nuclei"
-pkgver = "3.3.6"
+pkgver = "3.3.7"
 pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/nuclei"]
@@ -9,7 +9,7 @@ maintainer = "Gabriel M. Dutra <dmdutra@proton.me>"
 license = "MIT"
 url = "https://github.com/projectdiscovery/nuclei"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7fb42cf5c9c4fa8800d40a997466dbfeac9954e1cae8d98a7af25c19801eb113"
+sha256 = "3d12498f90b4babead541578f095c8f8aac7d08038073f0f239142356d3a4c79"
 # Nuclei tests requires network connection
 options = ["!check"]
 
