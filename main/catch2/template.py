72c0b11b00414d377713be9cf60cc2715d0c0934
triallax <triallax@tutanota.com>
2025-01-08 20:45:07 +0000

    main/catch2: update to 3.8.0

--- main/catch2/template.py
+++ main/catch2/template.py
@@ -1,5 +1,5 @@
 pkgname = "catch2"
-pkgver = "3.7.1"
+pkgver = "3.8.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -15,7 +15,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "BSL-1.0"
 url = "https://github.com/catchorg/Catch2"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "c991b247a1a0d7bb9c39aa35faf0fe9e19764213f28ffba3109388e62ee0269c"
+sha256 = "1ab2de20460d4641553addfdfe6acd4109d871d5531f8f519a52ea4926303087"
 # vis breaks symbols
 hardening = ["!vis"]
 
