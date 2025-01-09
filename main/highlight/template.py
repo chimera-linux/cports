f37772f6b1c2270e38bf22a5e40119f358dd9d1e
triallax <triallax@tutanota.com>
2025-01-09 20:44:53 +0000

    main/highlight: update to 4.15

--- main/highlight/template.py
+++ main/highlight/template.py
@@ -1,6 +1,6 @@
 pkgname = "highlight"
-pkgver = "4.14"
-pkgrel = 1
+pkgver = "4.15"
+pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["pkgconf"]
 makedepends = ["boost-devel", "lua5.4-devel"]
@@ -9,7 +9,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-3.0-or-later AND LGPL-3.0-or-later AND MIT AND BSD-2-Clause"
 url = "http://www.andre-simon.de/doku/highlight/en/highlight.html"
 source = f"http://www.andre-simon.de/zip/highlight-{pkgver}.tar.bz2"
-sha256 = "099e0eaf38709fe430108125ad9d35f7aacfa05d5d41ad20f405593b575627c5"
+sha256 = "68b3f8178c5c9d4b0a03f6948635cef1c8d06244f6b438eebf3a190c588337e9"
 hardening = ["vis", "cfi"]
 # No tests, can't be bothered to fix cross
 options = ["!check", "!distlicense", "!cross"]
