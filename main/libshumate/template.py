78568d3c9f69ae5fb0e47d437d2a4f35c308bf72
triallax <triallax@tutanota.com>
2025-01-09 20:55:32 +0000

    main/libshumate: update to 1.3.1

--- main/libshumate/template.py
+++ main/libshumate/template.py
@@ -1,5 +1,5 @@
 pkgname = "libshumate"
-pkgver = "1.3.0"
+pkgver = "1.3.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dgtk_doc=false"]
@@ -26,7 +26,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libshumate"
 source = f"$(GNOME_SITE)/libshumate/{pkgver[:-2]}/libshumate-{pkgver}.tar.xz"
-sha256 = "8227a6e8281cde12232894fef83760d44fa66b39ef033c61ed934a86c6dc75d4"
+sha256 = "6efe9352d91744822d41ead4714a2ab4b378481a86a2204b7bec4082dffc1b8b"
 options = ["!cross"]
 
 
