88ce5aafab4a9096e3fabcd778f1df4ebb4b744a
q66 <q66@chimera-linux.org>
2025-01-09 02:30:03 +0100

    main/byacc: update to 20241231

--- main/byacc/template.py
+++ main/byacc/template.py
@@ -1,5 +1,5 @@
 pkgname = "byacc"
-pkgver = "20240109"
+pkgver = "20241231"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--program-transform=s,^,b,"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "custom:byacc"
 url = "http://invisible-island.net/byacc"
 source = f"https://invisible-island.net/archives/byacc/byacc-{pkgver}.tgz"
-sha256 = "f2897779017189f1a94757705ef6f6e15dc9208ef079eea7f28abec577e08446"
+sha256 = "192c2fae048d4e7f514ba451627f9c4e612765099f819c19191f9fde3e609673"
 hardening = ["vis", "cfi"]
 
 
