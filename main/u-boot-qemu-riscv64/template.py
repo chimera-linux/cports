b9e35f101b2c4a2c78ec37c16a294fa363b23221
q66 <q66@chimera-linux.org>
2025-01-09 17:15:08 +0100

    main/u-boot-*: update to 2025.01

--- main/u-boot-qemu-riscv64/template.py
+++ main/u-boot-qemu-riscv64/template.py
@@ -1,5 +1,5 @@
 pkgname = "u-boot-qemu-riscv64"
-pkgver = "2024.10"
+pkgver = "2025.01"
 pkgrel = 0
 build_style = "u_boot"
 hostmakedepends = [
@@ -17,7 +17,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only AND BSD-3-Clause"
 url = "https://www.denx.de/wiki/U-Boot"
 source = f"https://ftp.denx.de/pub/u-boot/u-boot-{pkgver}.tar.bz2"
-sha256 = "b28daf4ac17e43156363078bf510297584137f6df50fced9b12df34f61a92fb0"
+sha256 = "cdef7d507c93f1bbd9f015ea9bc21fa074268481405501945abc6f854d5b686f"
 env = {
     "U_BOOT_TRIPLET": "riscv64-unknown-elf",
     "U_BOOT_TARGETS": "u-boot",
