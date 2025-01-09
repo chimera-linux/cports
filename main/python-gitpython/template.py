76384d22a172352c425cc4341f12dcfa29a8309d
triallax <triallax@tutanota.com>
2025-01-09 21:09:31 +0000

    main/python-gitpython: update to 3.1.44

--- main/python-gitpython/template.py
+++ main/python-gitpython/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-gitpython"
-pkgver = "3.1.43"
+pkgver = "3.1.44"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -12,8 +12,8 @@ pkgdesc = "Python library to interact with Git repos"
 maintainer = "triallax <triallax@tutanota.com>"
 license = "BSD-3-Clause"
 url = "https://gitpython.readthedocs.org"
-source = f"$(PYPI_SITE)/g/GitPython/GitPython-{pkgver}.tar.gz"
-sha256 = "35f314a9f878467f5453cc1fee295c3e18e52f1b99f10f6cf5b1682e968a9e7c"
+source = f"$(PYPI_SITE)/g/GitPython/gitpython-{pkgver}.tar.gz"
+sha256 = "c87e30b26253bf5418b01b0660f818967f3c503193838337fe5e573331249269"
 # TODO
 options = ["!check"]
 
