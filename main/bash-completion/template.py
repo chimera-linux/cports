c5ae88463adc8fd2e6f482b68e4b9d2dc0cbf197
q66 <q66@chimera-linux.org>
2025-01-09 02:29:43 +0100

    main/bash-completion: update to 2.16:0

--- main/bash-completion/template.py
+++ main/bash-completion/template.py
@@ -1,5 +1,5 @@
 pkgname = "bash-completion"
-pkgver = "2.15.0"
+pkgver = "2.16.0"
 pkgrel = 0
 build_style = "gnu_configure"
 make_install_args = [
@@ -13,6 +13,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/scop/bash-completion"
 source = f"{url}/releases/download/{pkgver}/bash-completion-{pkgver}.tar.xz"
-sha256 = "976a62ee6226970283cda85ecb9c7a4a88f62574c0a6f9e856126976decf1a06"
+sha256 = "3369bd5e418a75fb990863925aed5b420398acebb320ec4c0306b3eae23f107a"
 # what's the point (needs a truckload of checkdepends too)
 options = ["!check"]
