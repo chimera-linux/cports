cda61ad2c89c303976c43bb7079b5907d45afc3c
triallax <triallax@tutanota.com>
2025-01-09 21:50:13 +0000

    main/podman-compose: update to 1.3.0

--- main/podman-compose/template.py
+++ main/podman-compose/template.py
@@ -1,5 +1,5 @@
 pkgname = "podman-compose"
-pkgver = "1.2.0"
+pkgver = "1.3.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -18,7 +18,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://github.com/containers/podman-compose"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "211b235cf6ffe38f5f65fa188471e4b2706895c1ef4e800bb2a59a01f272674e"
+sha256 = "d2d641d23bead9cead06bdd2edb9d6a592a138fc8bb7ecb53f87e21e99d45af7"
 # tests need to run podman which isn't gonna work in bwrap without networking and so on
 options = ["!check"]
 
