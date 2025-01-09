2092259446f3f636982828eb6acc93711a2f30f0
triallax <triallax@tutanota.com>
2025-01-09 20:51:47 +0000

    main/orca: update to 47.3

--- main/orca/template.py
+++ main/orca/template.py
@@ -1,5 +1,5 @@
 pkgname = "orca"
-pkgver = "47.2"
+pkgver = "47.3"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -32,4 +32,4 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "LGPL-2.1-or-later"
 url = "https://orca.gnome.org"
 source = f"$(GNOME_SITE)/orca/{pkgver[:-2]}/orca-{pkgver}.tar.xz"
-sha256 = "5e67af357f7198ea00a44381cab4c4f94e6826d6ed800668f39416ce2aab8e5a"
+sha256 = "1b0b145bb685cd74eca3e28cb7b7097f98d13ee1cc58b71edeed3a8f9045231b"
