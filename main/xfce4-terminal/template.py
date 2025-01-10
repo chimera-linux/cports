b37786d8fb4991d7dc02bfcdf2d64f8e1924de77
triallax <triallax@tutanota.com>
2025-01-10 00:26:17 +0000

    main/xfce4-terminal: update to 1.1.4

--- main/xfce4-terminal/template.py
+++ main/xfce4-terminal/template.py
@@ -1,15 +1,13 @@
 pkgname = "xfce4-terminal"
-pkgver = "1.1.3"
-pkgrel = 1
+pkgver = "1.1.4"
+pkgrel = 0
 build_style = "gnu_configure"
-make_dir = "."
 hostmakedepends = [
     "automake",
     "gettext-devel",
     "gtk-doc-tools",
-    "intltool",
-    "libtool",
     "pkgconf",
+    "slibtool",
     "xfce4-dev-tools",
 ]
 makedepends = ["exo-devel", "libxfce4ui-devel", "vte-gtk3-devel"]
@@ -18,4 +16,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/apps/xfce4-terminal/start"
 source = f"$(XFCE_SITE)/apps/xfce4-terminal/{pkgver[:-2]}/xfce4-terminal-{pkgver}.tar.bz2"
-sha256 = "214dd588d441b69f816009682a3bb4652cc19bed7ea64b612a12f097417b3d45"
+sha256 = "873c921da1f4b986ffb459d4960789c9c063af98648c9f0ca146dc6f6f5b71b7"
