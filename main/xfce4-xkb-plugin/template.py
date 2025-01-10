4aad3662307233cb9af02d79e6f5d1cd68b6a962
triallax <triallax@tutanota.com>
2025-01-10 00:27:52 +0000

    main/xfce4-xkb-plugin: update to 0.8.5

--- main/xfce4-xkb-plugin/template.py
+++ main/xfce4-xkb-plugin/template.py
@@ -1,14 +1,12 @@
 pkgname = "xfce4-xkb-plugin"
-pkgver = "0.8.3"
+pkgver = "0.8.5"
 pkgrel = 0
 build_style = "gnu_configure"
-make_dir = "."
 hostmakedepends = [
     "automake",
     "gettext-devel",
-    "intltool",
-    "libtool",
     "pkgconf",
+    "slibtool",
     "xfce4-dev-tools",
 ]
 makedepends = [
@@ -29,4 +27,4 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-xkb-plugin/start"
 source = f"$(XFCE_SITE)/panel-plugins/xfce4-xkb-plugin/{pkgver[:-2]}/xfce4-xkb-plugin-{pkgver}.tar.bz2"
-sha256 = "f0bfe97875ef1ca0a3b6a6fac312663c9cada151cf1ac96071393d320cd04987"
+sha256 = "86ce4a194c47c506315cfded3a041067a72dedcb6d9cc2f9c99853203d332b19"
