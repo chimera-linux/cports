4b325eea93484134debcd0e687a0ea53c9cba4ca
triallax <triallax@tutanota.com>
2025-01-10 00:26:51 +0000

    main/xfce4-verve-plugin: update to 2.0.4

--- main/xfce4-verve-plugin/template.py
+++ main/xfce4-verve-plugin/template.py
@@ -1,14 +1,12 @@
 pkgname = "xfce4-verve-plugin"
-pkgver = "2.0.3"
+pkgver = "2.0.4"
 pkgrel = 0
 build_style = "gnu_configure"
-make_dir = "."
 hostmakedepends = [
     "automake",
     "gettext-devel",
-    "intltool",
-    "libtool",
     "pkgconf",
+    "slibtool",
     "xfce4-dev-tools",
 ]
 makedepends = [
@@ -22,4 +20,4 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-verve-plugin/start"
 source = f"$(XFCE_SITE)/panel-plugins/xfce4-verve-plugin/{pkgver[:-2]}/xfce4-verve-plugin-{pkgver}.tar.bz2"
-sha256 = "e1bf121f1bf9cf2a199bf5c0f3aa802f503df9bea50724741e7a92fe6d9fe09e"
+sha256 = "214a812dd7cc410bbe85d736e0dc76459ddd7861e5a1c60b67dd89dcd34e90a1"
