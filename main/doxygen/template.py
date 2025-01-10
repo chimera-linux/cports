acb244bd0feccf6935fa226d87ec3e788325597e
rietbergenm <mathijs.rietbergen@proton.me>
2025-01-10 23:57:18 +0100

    main/doxygen: update to 1.13.2

--- main/doxygen/template.py
+++ main/doxygen/template.py
@@ -1,5 +1,5 @@
 pkgname = "doxygen"
-pkgver = "1.13.1"
+pkgver = "1.13.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DCMAKE_BUILD_TYPE=Release"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-only"
 url = "https://doxygen.nl"
 source = f"{url}/files/doxygen-{pkgver}.src.tar.gz"
-sha256 = "b593a17e9f7dd00c253d5bb18f05b84632136e89753b87fe366c858ea63f6e62"
+sha256 = "3a25e3386c26ea5494c784e946327225debfbc5dbfa8b13549010a315aace66d"
 hardening = ["vis", "cfi"]
 
 
