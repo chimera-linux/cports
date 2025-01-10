d5c5cccffe38b91f32e1c4973953aeba1804b9eb
triallax <triallax@tutanota.com>
2025-01-10 00:26:30 +0000

    main/xfce4-time-out-plugin: update to 1.1.4

--- main/xfce4-time-out-plugin/template.py
+++ main/xfce4-time-out-plugin/template.py
@@ -1,14 +1,12 @@
 pkgname = "xfce4-time-out-plugin"
-pkgver = "1.1.3"
+pkgver = "1.1.4"
 pkgrel = 0
 build_style = "gnu_configure"
-make_dir = "."
 hostmakedepends = [
     "automake",
     "gettext-devel",
-    "intltool",
-    "libtool",
     "pkgconf",
+    "slibtool",
     "xfce4-dev-tools",
 ]
 makedepends = [
@@ -22,4 +20,4 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-time-out-plugin/start"
 source = f"$(XFCE_SITE)/panel-plugins/xfce4-time-out-plugin/{pkgver[:-2]}/xfce4-time-out-plugin-{pkgver}.tar.bz2"
-sha256 = "5a1ca36361e95ec718bbd887ea5be6a270ab458d1c2d672186721522a7228ee8"
+sha256 = "b6b708900d7fd0cc3d8a045514962db94b60d959c266049aa2cff768fc381726"
