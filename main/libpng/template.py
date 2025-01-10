f44fc9c23c570cce57ab2fdb9b29d4f65b09ffa4
triallax <triallax@tutanota.com>
2025-01-10 01:57:19 +0000

    main/libpng: update to 1.6.45

--- main/libpng/template.py
+++ main/libpng/template.py
@@ -1,15 +1,15 @@
 pkgname = "libpng"
-pkgver = "1.6.44"
+pkgver = "1.6.45"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["automake", "pkgconf", "libtool"]
+hostmakedepends = ["automake", "pkgconf", "slibtool"]
 makedepends = ["zlib-ng-compat-devel"]
 pkgdesc = "Library for manipulating PNG images"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "Libpng"
 url = "http://www.libpng.org/pub/png/libpng.html"
 source = f"$(SOURCEFORGE_SITE)/libpng/libpng-{pkgver}.tar.xz"
-sha256 = "60c4da1d5b7f0aa8d158da48e8f8afa9773c1c8baa5d21974df61f1886b8ce8e"
+sha256 = "926485350139ffb51ef69760db35f78846c805fef3d59bfdcb2fba704663f370"
 
 
 def post_install(self):
