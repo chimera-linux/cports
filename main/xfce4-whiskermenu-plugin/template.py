ce0eb75e2ec611edd3c8260485031242235a5999
triallax <triallax@tutanota.com>
2025-01-10 00:27:39 +0000

    main/xfce4-whiskermenu-plugin: update to 2.8.4

--- main/xfce4-whiskermenu-plugin/template.py
+++ main/xfce4-whiskermenu-plugin/template.py
@@ -1,5 +1,5 @@
 pkgname = "xfce4-whiskermenu-plugin"
-pkgver = "2.8.3"
+pkgver = "2.8.4"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -24,4 +24,4 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-whiskermenu-plugin/start"
 source = f"$(XFCE_SITE)/panel-plugins/xfce4-whiskermenu-plugin/{pkgver[:-2]}/xfce4-whiskermenu-plugin-{pkgver}.tar.bz2"
-sha256 = "e776c287658f98d0739447279522fe78766088438242cf2365a49c8973fc9cd0"
+sha256 = "ed918950e01dc97fe831e01c698b44247f1537992999b1262ab61c799272b3b7"
