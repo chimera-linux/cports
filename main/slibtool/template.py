cfb23d6e357e66dab8e992762bb730aad1ab5032
q66 <q66@chimera-linux.org>
2025-01-12 20:03:49 +0100

    main/slibtool: update to 0.6.1

--- main/slibtool/template.py
+++ main/slibtool/template.py
@@ -1,6 +1,6 @@
 pkgname = "slibtool"
-pkgver = "0.6.0"
-pkgrel = 2
+pkgver = "0.6.1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
 pkgdesc = "Alternative libtool implementation"
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://dev.midipix.org/cross/slibtool"
 source = f"https://dl.foss21.org/slibtool/slibtool-{pkgver}.tar.xz"
-sha256 = "10b0a12c074b10fa1fec6fe74937b4812c3a7b37f7cc45d0dca68495c2b45e6a"
+sha256 = "61b07f9f371ca05dc5b1259b27e73d8d7a2ef15e2a6adc9bac9816c5a4beacc4"
 # no tests?
 options = ["!check"]
 
