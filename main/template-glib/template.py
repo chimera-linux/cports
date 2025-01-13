2647dc7e08b60600fdf9d8628e7335fa2a6a059f
q66 <q66@chimera-linux.org>
2025-01-14 00:38:51 +0100

    main/template-glib: update to 3.36.3

--- main/template-glib/template.py
+++ main/template-glib/template.py
@@ -1,5 +1,5 @@
 pkgname = "template-glib"
-pkgver = "3.36.2"
+pkgver = "3.36.3"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -15,7 +15,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/template-glib"
 source = f"$(GNOME_SITE)/template-glib/{'.'.join(pkgver.rsplit('.')[:-1])}/template-glib-{pkgver}.tar.xz"
-sha256 = "0020f3a401888ce763b3a17508c2f58e91972a483a0c547afdb7ccbe25619948"
+sha256 = "d528b35b2cf90e07dae50e25e12fbadb0eb048f57fd5151cf9f6e98cce1df20e"
 # gobject-introspection
 options = ["!cross"]
 
