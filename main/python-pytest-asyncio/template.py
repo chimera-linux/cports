098f6b9a6234fe90ecdb1575e1dcf71169344d00
triallax <triallax@tutanota.com>
2025-01-13 16:44:15 +0000

    main/python-pytest-asyncio: update to 0.25.2

--- main/python-pytest-asyncio/template.py
+++ main/python-pytest-asyncio/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pytest-asyncio"
-pkgver = "0.25.0"
+pkgver = "0.25.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,6 +14,6 @@ maintainer = "firefly-cpp <iztok@iztok.space>"
 license = "Apache-2.0"
 url = "https://github.com/pytest-dev/pytest-asyncio"
 source = f"$(PYPI_SITE)/p/pytest-asyncio/pytest_asyncio-{pkgver}.tar.gz"
-sha256 = "8c0610303c9e0442a5db8604505fc0f545456ba1528824842b37b4a626cbf609"
+sha256 = "3f8ef9a98f45948ea91a0ed3dc4268b5326c0e7bce73892acc654df4262ad45f"
 # missing dependencies
 options = ["!check"]
