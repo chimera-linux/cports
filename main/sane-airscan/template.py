620217a248978dd2e59f86fd6d5a2b60bb9c9231
q66 <q66@chimera-linux.org>
2025-01-14 00:38:10 +0100

    main/sane-airscan: update to 0.99.31

--- main/sane-airscan/template.py
+++ main/sane-airscan/template.py
@@ -1,5 +1,5 @@
 pkgname = "sane-airscan"
-pkgver = "0.99.30"
+pkgver = "0.99.31"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -21,4 +21,4 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-2.0-or-later WITH SANE-exception"
 url = "https://github.com/alexpevzner/sane-airscan"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "89bef46839b5f07689f29e9550b694748d70263cbd66a34566e7d616ca228ad6"
+sha256 = "67782be6a4fd36e753fc4766b8989d75f806bc6d1d2e92f617ea686be2924c14"
