30e964fe09c338fbb229342134a03bb9d5ea4b48
q66 <q66@chimera-linux.org>
2025-01-14 00:41:20 +0100

    main/uacme: update to 1.7.6

--- main/uacme/template.py
+++ main/uacme/template.py
@@ -1,5 +1,5 @@
 pkgname = "uacme"
-pkgver = "1.7.5"
+pkgver = "1.7.6"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-openssl"]
@@ -19,5 +19,5 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/ndilieto/uacme"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f75a375d95567145625366058fc8ed5359c575f62ec89e6b06b060de4c669d25"
+sha256 = "484088abdcb8dd134f1334626b1a4df2d9c475e0e537cd498eb5c6dd621373c7"
 hardening = ["vis", "cfi"]
