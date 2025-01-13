28e00092827b4cf87c90b96fe36715f5875eee50
q66 <q66@chimera-linux.org>
2025-01-14 00:57:07 +0100

    main/libmikmod: update to 3.3.12

--- main/libmikmod/template.py
+++ main/libmikmod/template.py
@@ -1,6 +1,6 @@
 pkgname = "libmikmod"
-pkgver = "3.3.11.1"
-pkgrel = 1
+pkgver = "3.3.12"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "libtool", "pkgconf"]
 pkgdesc = "Mikmod module player and library"
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "http://mikmod.shlomifish.org"
 source = f"$(SOURCEFORGE_SITE)/mikmod/libmikmod-{pkgver}.tar.gz"
-sha256 = "ad9d64dfc8f83684876419ea7cd4ff4a41d8bcd8c23ef37ecb3a200a16b46d19"
+sha256 = "adef6214863516a4a5b44ebf2c71ef84ecdfeb3444973dacbac70911c9bc67e9"
 # CFI: crashes in sc2 ucm
 hardening = ["vis", "!cfi"]
 
