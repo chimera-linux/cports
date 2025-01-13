02a1b3060fc1c819cfe9191359ecb9bc8b0128f2
triallax <triallax@tutanota.com>
2025-01-13 17:09:07 +0000

    main/python-click: update to 8.1.8

--- main/python-click/template.py
+++ main/python-click/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-click"
-pkgver = "8.1.7"
-pkgrel = 1
+pkgver = "8.1.8"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
+    "python-flit_core",
     "python-installer",
-    "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python"]
 checkdepends = ["python-pytest"]
@@ -15,8 +14,8 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://palletsprojects.com/p/click"
 source = f"$(PYPI_SITE)/c/click/click-{pkgver}.tar.gz"
-sha256 = "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+sha256 = "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a"
 
 
 def post_install(self):
-    self.install_license("LICENSE.rst")
+    self.install_license("LICENSE.txt")
