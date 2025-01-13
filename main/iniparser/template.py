aafc765f4d428420a4850a1c6513265c70125839
q66 <q66@chimera-linux.org>
2025-01-14 00:33:50 +0100

    main/iniparser: update to 4.2.5

--- main/iniparser/template.py
+++ main/iniparser/template.py
@@ -1,5 +1,5 @@
 pkgname = "iniparser"
-pkgver = "4.2.4"
+pkgver = "4.2.5"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -14,7 +14,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "MIT"
 url = "https://gitlab.com/iniparser/iniparser"
 source = f"{url}/-/archive/v{pkgver}.tar.gz"
-sha256 = "cd4341a4dec5505e1f007cec643d064e8ad526569bd904f0e823c4a6ab10b8ba"
+sha256 = "c03f125259ff8fec7e3fc76277957a1ba070dcbfa6c05f29c08ad0216bbe99cf"
 # vis breaks symbols
 hardening = []
 
