81bed14d3bef7ac33016ef6df9629b15c5e24c92
q66 <q66@chimera-linux.org>
2025-01-14 00:58:42 +0100

    main/nettle: update to 3.10.1

--- main/nettle/template.py
+++ main/nettle/template.py
@@ -1,6 +1,6 @@
 pkgname = "nettle"
-pkgver = "3.10"
-pkgrel = 1
+pkgver = "3.10.1"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "texinfo", "automake", "libtool"]
 makedepends = ["gmp-devel", "linux-headers"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later OR LGPL-3.0-or-later"
 url = "https://www.lysator.liu.se/~nisse/nettle"
 source = f"$(GNU_SITE)/nettle/nettle-{pkgver}.tar.gz"
-sha256 = "b4c518adb174e484cb4acea54118f02380c7133771e7e9beb98a0787194ee47c"
+sha256 = "b0fcdd7fc0cdea6e80dcf1dd85ba794af0d5b4a57e26397eee3bc193272d9132"
 
 
 @subpackage("nettle-devel")
