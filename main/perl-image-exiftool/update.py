6a67580927e2c3a8e0de594808d0a01d56ac33b1
triallax <triallax@tutanota.com>
2025-01-13 16:14:21 +0000

    main/perl-image-exiftool: don't include dev releases in update check

--- main/perl-image-exiftool/update.py
+++ main/perl-image-exiftool/update.py
@@ -1,1 +1,4 @@
-pkgname = "Image-ExifTool"
+# https://exiftool.org includes development releases, but only production
+# releases are uploaded to CPAN, so check there instead
+url = "https://metacpan.org/dist/Image-ExifTool"
+pattern = r"Image-ExifTool-([\d.]+)[\"']"
