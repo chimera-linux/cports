8bacc97d6b73e1c896407c8b55550dacd4ff8864
triallax <triallax@tutanota.com>
2025-01-13 17:02:44 +0000

    main/python-charset-normalizer: update to 3.4.1

--- main/python-charset-normalizer/template.py
+++ main/python-charset-normalizer/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-charset-normalizer"
-pkgver = "3.4.0"
-pkgrel = 1
+pkgver = "3.4.1"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
-    "python-setuptools",
-    "python-wheel",
+    "python-setuptools_scm",
 ]
 checkdepends = ["python-pytest"]
 depends = ["python"]
@@ -15,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://charset-normalizer.readthedocs.io"
 source = f"https://github.com/Ousret/charset_normalizer/archive/refs/tags/{pkgver}.tar.gz>3.4.0-real.tar.gz"
-sha256 = "e8dfa606a7c3f4d4540e7f648a602338323ee0f5e458043accf59b4a0493783f"
+sha256 = "5b91677d4c790eead798f4ed3e5f546ed80d6fe8cf1d8120939960d593371855"
 # dependency of pytest
 options = ["!check"]
 
