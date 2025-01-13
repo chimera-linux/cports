5fc2cda4b0db7a2c2d725a2cc624eeb33105e2ac
q66 <q66@chimera-linux.org>
2025-01-14 00:33:32 +0100

    main/inferno: update to 0.12.1

--- main/inferno/template.py
+++ main/inferno/template.py
@@ -1,5 +1,5 @@
 pkgname = "inferno"
-pkgver = "0.12.0"
+pkgver = "0.12.1"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -9,6 +9,6 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "CDDL-1.0"
 url = "https://github.com/jonhoo/inferno"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7c045ad20db6284b0f1312a60e13438723bc6273e49ca2544000027794638396"
+sha256 = "913145c6087a5dd24c8ca976c079309a4fdd15983e392b1dbd13b8658daceb57"
 # some tests need an actual git checkout of a submodule (not tarball) and i cbf
 options = ["!check"]
