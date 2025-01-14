c1f197d5c6636940740b1405a84b250ddcbbb1ba
Erica Z <zerica@callcc.eu>
2025-01-14 21:33:39 +0100

    user/python-ssh2: update to 1.1.1

--- user/python-ssh2/template.py
+++ user/python-ssh2/template.py
@@ -1,9 +1,8 @@
 pkgname = "python-ssh2"
-pkgver = "1.0.0"
+pkgver = "1.1.1"
 pkgrel = 0
 build_style = "python_pep517"
 make_build_env = {
-    "SSH2_PYTHON_VERSION": pkgver,
     "SYSTEM_LIBSSH2": "1",
 }
 hostmakedepends = [
@@ -20,13 +19,14 @@ checkdepends = [
     "openssh",
     "python-jinja2",
     "python-pytest",
+    "python-pytest-rerunfailures",
 ]
 pkgdesc = "Python bindings for libssh2"
 maintainer = "Erica Z <zerica@callcc.eu>"
 license = "LGPL-2.1-only"
 url = "https://github.com/ParallelSSH/ssh2-python"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "70c6b6efd8ca9f8de9c2d77e7cb1d5859542588347ea426d6822b0ffd9889af3"
+sha256 = "33bc92de14341aab479ac49e33e747895be5117863b910e90f8a5fc23e8e64b8"
 
 
 # this is identical to the default check, we just have to change the pytest invocation
@@ -58,26 +58,5 @@ def check(self):
         "/usr/bin/pytest",
         # use installed ssh2 module
         "--import-mode=importlib",
-        "-k",
-        # these require an ssh agent
-        "not test_agent_get_identities"
-        + " and not test_agent_id_path"
-        + " and not test_agent"
-        + " and not test_failed_agent_auth"
-        # ssh2.exceptions.SocketRecvError
-        + " and not test_sftp_symlink_realpath_lstat"
-        + " and not test_sftp_write"
-        + " and not test_statvfs"
-        + " and not SessionTestCase"
-        # ssh2.exceptions.AuthenticationError
-        + " and not test_direct_tcpip"
-        + " and not ChannelTestCase"
-        + " and not KnownHostTestCase"
-        + " and not SFTPTestCase"
-        # ssh2.exceptions.SocketDisconnectError
-        + " and not test_non_blocking"
-        + " and not test_pubkey_auth"
-        + " and not test_scp_recv"
-        + " and not test_publickey_frommemory",
         path=[envpy.parent],
     )
