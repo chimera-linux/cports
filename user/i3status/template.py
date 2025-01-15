8c808be943cdd7d6d768f58d7bd92faa2f333291
shortcircuit <shortcircuitv@gmail.com>
2025-01-15 19:53:41 +0000

    user/i3status: new package

--- user/i3status/template.py
+++ user/i3status/template.py
@@ -2,14 +2,8 @@ pkgname = "i3status"
 pkgver = "2.15"
 pkgrel = 0
 build_style = "meson"
-configure_args = [
-    "-Dmans=true",
-    "-Ddocs=true"
-]
-hostmakedepends = [
-    "meson",
-    "pkgconf"
-]
+configure_args = ["-Dmans=true"]
+hostmakedepends = ["meson", "pkgconf"]
 makedepends = [
     "libconfuse-devel",
     "yajl-devel",
@@ -24,10 +18,12 @@ pkgdesc = "Generates status bar to use with i3bar, dzen2 or xmobar"
 maintainer = "shortcircuit <shortcircuitv@gmail.com>"
 license = "BSD-3-Clause"
 url = "https://github.com/i3/i3status"
-source = f"{url}/releases/tag/{pkgver}.tar.gz"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "25af0dd77a5325c13890d4ee53a9205827a11c8b90f54e8a7fe2654bd0273d4b"
 hardening = ["vis", "cfi"]
 # grep -P doesnt exist
 options = ["!check"]
+
+
 def post_install(self):
     self.install_license("LICENSE")
