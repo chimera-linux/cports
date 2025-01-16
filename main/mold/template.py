6a65b7c04b532de57bda098621013339aaabef2d
q66 <q66@chimera-linux.org>
2025-01-17 00:34:21 +0100

    main/mold: update to 2.36.0

--- main/mold/template.py
+++ main/mold/template.py
@@ -1,5 +1,5 @@
 pkgname = "mold"
-pkgver = "2.35.1"
+pkgver = "2.36.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -20,7 +20,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/rui314/mold"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "912b90afe7fde03e53db08d85a62c7b03a57417e54afc72c08e2fa07cab421ff"
+sha256 = "3f57fe75535500ecce7a80fa1ba33675830b7d7deb1e5ee9a737e2bc43cdb1c7"
 # TODO: a portion of the tests fail, for various reasons, such as assuming
 # presence of gcc, gnu grep, and various toolchain specifics; around 70%
 # of the tests pass right now, fix the rest later
