b29794fae76819fa81b39a2779027625652d1c47
q66 <q66@chimera-linux.org>
2025-01-17 00:36:42 +0100

    main/netavark: update to 1.13.1

--- main/netavark/template.py
+++ main/netavark/template.py
@@ -1,5 +1,5 @@
 pkgname = "netavark"
-pkgver = "1.13.0"
+pkgver = "1.13.1"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "go-md2man", "protoc"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/containers/netavark"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "34862383aee916677333b586f57d8b1d29f94676029da23c9a1ad1fcb509d1c1"
+sha256 = "b3698021677fb3b0fd1dc5f669fd62b49a7f4cf26bb70f977663f6d1a5046a31"
 
 
 def install(self):
