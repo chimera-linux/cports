85396305d1dee7fc32ffd43d69a6a9f173b01e2d
q66 <q66@chimera-linux.org>
2025-01-17 00:23:01 +0100

    main/rust-analyzer: update to 2025.01.13, add dep

--- main/rust-analyzer/template.py
+++ main/rust-analyzer/template.py
@@ -1,16 +1,17 @@
 pkgname = "rust-analyzer"
-pkgver = "2025.01.08"
+pkgver = "2025.01.13"
 pkgrel = 0
 build_style = "cargo"
 make_env = {"CARGO_PROFILE_RELEASE_PANIC": "unwind"}
 hostmakedepends = ["cargo-auditable"]
 makedepends = ["rust-std"]
+depends = ["rust-src"]
 pkgdesc = "Rust compiler LSP server"
 maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/rust-lang/rust-analyzer"
 source = f"{url}/archive/refs/tags/{pkgver.replace('.', '-')}.tar.gz"
-sha256 = "d95c925526139b7976c33fffe33c0f457ec82b31e37935b9cb0fc4f291eaa9e1"
+sha256 = "937198b4f678b64b935857e3302bb7c1f0e39fe948172463b7c38aab98a1d73a"
 # invokes rustfmt via rustup arg, also take longer to build than the actual
 # build..
 options = ["!check"]
