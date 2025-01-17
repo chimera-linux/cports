fdd94af5dfa9d85083038dee2511893e48a1c2fd
q66 <q66@chimera-linux.org>
2025-01-17 22:48:01 +0100

    user/nlopt: update to 2.9.1

--- user/nlopt/template.py
+++ user/nlopt/template.py
@@ -1,5 +1,5 @@
 pkgname = "nlopt"
-pkgver = "2.7.1"
+pkgver = "2.9.1"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://nlopt.readthedocs.io"
 source = f"https://github.com/stevengj/nlopt/archive/v{pkgver}.tar.gz"
-sha256 = "db88232fa5cef0ff6e39943fc63ab6074208831dc0031cf1545f6ecd31ae2a1a"
+sha256 = "1e6c33f8cbdc4138d525f3326c231f14ed50d99345561e85285638c49b64ee93"
 
 
 def post_install(self):
