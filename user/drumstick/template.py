720340c94e779c09038ff56c4975f82c62b71b3f
q66 <q66@chimera-linux.org>
2025-01-17 22:09:45 +0100

    user/drumstick: update to 2.10.0

--- user/drumstick/template.py
+++ user/drumstick/template.py
@@ -1,5 +1,5 @@
 pkgname = "drumstick"
-pkgver = "2.9.1"
+pkgver = "2.10.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -25,7 +25,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://drumstick.sourceforge.io"
 source = f"$(SOURCEFORGE_SITE)/drumstick/drumstick-{pkgver}.tar.gz"
-sha256 = "a7049333d5411faf4d91a81ae666746106b578897fc0713a84325f65fdd06ffb"
+sha256 = "92f5fc2a94b8c9067200897fd14027f707bf0103871ea942e388f9afe95e0f34"
 
 
 @subpackage("drumstick-devel")
