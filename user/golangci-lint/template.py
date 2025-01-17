379b76bc89b791d03384aacb026f95f835d1fa84
q66 <q66@chimera-linux.org>
2025-01-17 22:28:02 +0100

    user/golangci-lint: update to 1.63.4

--- user/golangci-lint/template.py
+++ user/golangci-lint/template.py
@@ -1,6 +1,6 @@
 pkgname = "golangci-lint"
-pkgver = "1.62.0"
-pkgrel = 1
+pkgver = "1.63.4"
+pkgrel = 0
 build_style = "go"
 make_dir = "build-cccc"
 make_build_args = ["./cmd/golangci-lint"]
@@ -10,6 +10,6 @@ maintainer = "Denis Strizhkin <strdenis02@gmail.com>"
 license = "GPL-3.0-or-later"
 url = "https://golangci-lint.run"
 source = f"https://github.com/golangci/golangci-lint/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "d3837b18bd70f6ab7e06a02d9759b5f4ad0bca9459bc27e6d27d4d105d3d0f88"
+sha256 = "4424d5e3437fdb723e912c3f4636a69d45155124568132d4440cde9a24be6dac"
 # some tests fail because of chroot and some need network
 options = ["!check"]
