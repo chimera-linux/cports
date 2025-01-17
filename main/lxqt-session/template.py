0b1f2556bf3626f1f20866dd0d678cb2c6dd95c5
q66 <q66@chimera-linux.org>
2025-01-17 01:51:17 +0100

    */*: rebuild for procps

--- main/lxqt-session/template.py
+++ main/lxqt-session/template.py
@@ -1,6 +1,6 @@
 pkgname = "lxqt-session"
 pkgver = "2.1.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
--- user/igt-gpu-tools/template.py
+++ user/igt-gpu-tools/template.py
@@ -1,6 +1,6 @@
 pkgname = "igt-gpu-tools"
 pkgver = "1.30"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
     "--libexecdir=/usr/lib",  # XXX drop libexec
