97e6a6f115c32ea1face05b0a086e8d88291c436
q66 <q66@chimera-linux.org>
2025-01-17 22:19:37 +0100

    user/vmpk: update to 0.9.1

--- user/vmpk/template.py
+++ user/vmpk/template.py
@@ -1,5 +1,5 @@
 pkgname = "vmpk"
-pkgver = "0.9.0"
+pkgver = "0.9.1"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -16,4 +16,4 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://sourceforge.net/projects/vmpk"
 source = f"$(SOURCEFORGE_SITE)/vmpk/vmpk-{pkgver}.tar.gz"
-sha256 = "3cfd900843b1e068fda9a4e075c204086447a2e233d4800d785d2124480ae2f5"
+sha256 = "3ac9cca97fcdbffd25605c5de88984e15a8b2dbeee885f626b1c5c499af80b51"
