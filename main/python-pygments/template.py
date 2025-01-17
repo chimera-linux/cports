dd16fd89b54d75dbc6c41ad69108074419166801
q66 <q66@chimera-linux.org>
2025-01-17 22:44:30 +0100

    main/python-pygments: update to 2.19.1

--- main/python-pygments/template.py
+++ main/python-pygments/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pygments"
-pkgver = "2.18.0"
+pkgver = "2.19.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-2-Clause"
 url = "https://pygments.org"
 source = f"$(PYPI_SITE)/p/pygments/pygments-{pkgver}.tar.gz"
-sha256 = "786ff802f32e91311bff3889f6e9a86e81505fe99f2735bb6d60ae0c5004f199"
+sha256 = "61c16d2a8576dc0649d9f39e089b5f02bcd27fba10d8fb4dcc28173f7a45151f"
 # dependency of pytest
 options = ["!check"]
 
