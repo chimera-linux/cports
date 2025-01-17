796b56b2dd6e47e9873ba0cea7fb22870e8599c0
q66 <q66@chimera-linux.org>
2025-01-17 21:55:29 +0100

    main/libdatachannel: update to 0.22.4

--- main/libdatachannel/template.py
+++ main/libdatachannel/template.py
@@ -1,5 +1,5 @@
 pkgname = "libdatachannel"
-pkgver = "0.22.3"
+pkgver = "0.22.4"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -25,7 +25,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "MPL-2.0"
 url = "https://libdatachannel.org"
 source = f"https://github.com/paullouisageneau/libdatachannel/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7aabb3bbf2672d50b6669cd95916e050a66a8c67a477e5c8fed82f526671426e"
+sha256 = "43eff11d71382aa1e0249aed9729cd744fa91e5cf2410364efa37e006a620454"
 hardening = ["cfi", "vis"]
 # tests seem to need network access (the tests themselves don't seem to work
 # with cfi either; library itself works just fine though)
