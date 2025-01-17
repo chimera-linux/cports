63e5748604f9bf6fc7ae73d1c8316687edc792f3
q66 <q66@chimera-linux.org>
2025-01-17 12:00:24 +0100

    user/lazygit: update to 0.45.0 and move from main

--- user/lazygit/template.py
+++ user/lazygit/template.py
@@ -1,6 +1,6 @@
 pkgname = "lazygit"
-pkgver = "0.44.1"
-pkgrel = 2
+pkgver = "0.45.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = [f"-ldflags=-X main.version=v{pkgver}"]
 hostmakedepends = ["go"]
@@ -10,7 +10,7 @@ maintainer = "jabuxas <jabuxas@proton.me>"
 license = "MIT"
 url = "https://github.com/jesseduffield/lazygit"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "02b67d38e07ae89b0ddd3b4917bd0cfcdfb5e158ed771566d3eb81f97f78cc26"
+sha256 = "11bb69916a32a22d29c90196f18af633bcf22bec2b84a675222edfb6c1f89a87"
 # need to be in git checkout
 options = ["!check"]
 
