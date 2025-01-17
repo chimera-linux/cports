8b7d2e00392419588480f59b61ed5c0bd83fe094
q66 <q66@chimera-linux.org>
2025-01-17 21:41:48 +0100

    main/bpftrace: update to 0.22.1

--- main/bpftrace/template.py
+++ main/bpftrace/template.py
@@ -1,5 +1,5 @@
 pkgname = "bpftrace"
-pkgver = "0.22.0"
+pkgver = "0.22.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -34,7 +34,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/bpftrace/bpftrace"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "a786f5582e57926a9124e2c93f81ad9d9d42ab79fd9e446c2c062c7e627005e5"
+sha256 = "31369abf18a42a970ad094e7e92af9547208a38a4e4a3bcb0ffa470fb4f50016"
 # bpftrace/bpftrace-aotrt binaries need keeping BEGIN/END_trigger syms
 # just skip strip for now until we can plumb through --keep-symbol to objcopy
 options = ["!strip"]
