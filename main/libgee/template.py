9b221f072cf29649c0ca8c65873e7745a3f3ef59
q66 <q66@chimera-linux.org>
2025-01-17 13:11:11 +0100

    main/libgee: update to 0.20.8

--- main/libgee/template.py
+++ main/libgee/template.py
@@ -1,5 +1,5 @@
 pkgname = "libgee"
-pkgver = "0.20.7"
+pkgver = "0.20.8"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-static"]
@@ -16,7 +16,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://wiki.gnome.org/Libgee"
 source = f"$(GNOME_SITE)/libgee/{pkgver[:-2]}/libgee-{pkgver}.tar.xz"
-sha256 = "0fdc3cd3d385b3beeb5955ff450752cae76176296ee123be2155d0810d0f89c7"
+sha256 = "189815ac143d89867193b0c52b7dc31f3aa108a15f04d6b5dca2b6adfad0b0ee"
 
 
 @subpackage("libgee-devel")
