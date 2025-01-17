eae94425707f024916553c9e0033e9a009292c49
q66 <q66@chimera-linux.org>
2025-01-17 22:33:46 +0100

    user/bulky: update to 3.6

--- user/bulky/template.py
+++ user/bulky/template.py
@@ -1,5 +1,5 @@
 pkgname = "bulky"
-pkgver = "3.5"
+pkgver = "3.6"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["gettext"]
@@ -15,7 +15,7 @@ maintainer = "Earldridge Jazzed Pineda <earldridgejazzedpineda@gmail.com>"
 license = "GPL-3.0-or-later"
 url = "https://projects.linuxmint.com/xapps"
 source = f"https://github.com/linuxmint/bulky/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "84d449535553f5f1857812d88c70252730473c8f85760e80be1381d53290028c"
+sha256 = "f98e412cf5f15839ee2d143c0bf733982a5cd12725cfffb94c1f66bb46a00874"
 # Test script requires write access to the cbuild container's root
 options = ["!check"]
 
