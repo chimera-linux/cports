0d56646bcca54e97f0ffa9671c2213fc841ca016
q66 <q66@chimera-linux.org>
2025-01-17 01:23:12 +0100

    main/gnome-color-manager: update to 3.36.2

--- main/gnome-color-manager/template.py
+++ main/gnome-color-manager/template.py
@@ -1,6 +1,6 @@
 pkgname = "gnome-color-manager"
-pkgver = "3.36.0"
-pkgrel = 1
+pkgver = "3.36.2"
+pkgrel = 0
 build_style = "meson"
 make_check_wrapper = ["wlheadless-run", "--"]
 hostmakedepends = ["gettext", "glib-devel", "itstool", "meson", "pkgconf"]
@@ -18,4 +18,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-color-manager"
 source = f"$(GNOME_SITE)/gnome-color-manager/{pkgver[:-2]}/gnome-color-manager-{pkgver}.tar.xz"
-sha256 = "9ddb9e6b6472e119801381f90905332ec1d4258981721bba831ca246ceb3ad3b"
+sha256 = "3904d42abb4ea566df0b880e82bf0b9f86386c692f15b318469a4c7be33a887f"
