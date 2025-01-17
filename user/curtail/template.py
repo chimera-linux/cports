83f22245ff87665b90cb2bb707d1d13f036c7ef1
q66 <q66@chimera-linux.org>
2025-01-17 22:09:22 +0100

    user/curtail: update to 1.12.0

--- user/curtail/template.py
+++ user/curtail/template.py
@@ -1,5 +1,5 @@
 pkgname = "curtail"
-pkgver = "1.11.1"
+pkgver = "1.12.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -24,4 +24,4 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/Huluti/Curtail"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "b5a33041ff64edbdcad293113d472a9de93f4b83be6efd99661cb2555050a068"
+sha256 = "7ca6f13012d7cb3ddc94b625c5cad5aefc9d535511dec67e55774c0429c43fb0"
