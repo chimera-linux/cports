8a0c917b9d40749e23ce1f3bcf8079e6991dba29
q66 <q66@chimera-linux.org>
2025-01-17 22:11:52 +0100

    user/libgbinder: update to 1.1.42

--- user/libgbinder/template.py
+++ user/libgbinder/template.py
@@ -1,5 +1,5 @@
 pkgname = "libgbinder"
-pkgver = "1.1.41"
+pkgver = "1.1.42"
 pkgrel = 0
 build_style = "makefile"
 make_install_target = "install-dev"
@@ -12,7 +12,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/mer-hybris/libgbinder"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "270a93a6e2168392cfee64520b17ac7d6347da070e3daa2391be3abc75d44ef2"
+sha256 = "32dcf31c5dc823af11558d180ed5fabb160fdfafe60f01d3212fd200a0842aed"
 
 
 def post_build(self):
