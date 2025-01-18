e8e23604688a514bcdb09992f57d50ffe75673ae
q66 <q66@chimera-linux.org>
2025-01-18 22:06:13 +0100

    main/libsixel: update to 1.10.5

--- main/libsixel/template.py
+++ main/libsixel/template.py
@@ -1,6 +1,6 @@
 pkgname = "libsixel"
-pkgver = "1.10.3"
-pkgrel = 1
+pkgver = "1.10.5"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "meson",
@@ -16,7 +16,7 @@ maintainer = "natthias <natthias@proton.me>"
 license = "MIT"
 url = "https://github.com/libsixel/libsixel"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "028552eb8f2a37c6effda88ee5e8f6d87b5d9601182ddec784a9728865f821e0"
+sha256 = "b6654928bd423f92e6da39eb1f40f10000ae2cc6247247fc1882dcff6acbdfc8"
 
 
 def post_install(self):
