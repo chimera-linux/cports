b3e2c60021df150ef33442ad921a39ffe5c9896b
Guilhem Baccialone <guilhem.baccialone@zaclys.net>
2025-01-18 18:18:11 +0100

    user/lshw: new package

---
+++ user/lshw/template.py
@@ -0,0 +1,50 @@
+pkgname = "lshw"
+pkgver = "02.20"
+pkgrel = 0
+build_style = "makefile"
+make_build_args = [
+    "all",
+    "gui",
+    f"VERSION={pkgver}",
+    "NO_VERSION_CHECK=1",
+    "ZLIB=1",
+]
+make_install_args = [
+    "install",
+    "install-gui",
+    f"VERSION={pkgver}",
+    "NO_VERSION_CHECK=1",
+    "ZLIB=1",
+]
+make_use_env = True
+hostmakedepends = ["gettext", "pkgconf"]
+makedepends = [
+    "gtk+3-devel",
+    "linux-headers",
+    "zlib-ng-compat-devel",
+]
+pkgdesc = "Hardware lister"
+maintainer = "Guilhem Baccialone <guilhem.baccialone@zaclys.net>"
+license = "GPL-2.0-only"
+url = "https://ezix.org/project/wiki/HardwareLiSter"
+source = f"https://www.ezix.org/software/files/lshw-B.{pkgver}.tar.gz"
+sha256 = "06d9cf122422220e5dc94e8ea5b01816a69bb6b59368f63d7f21fff31fc6922a"
+# no tests available
+options = ["!check"]
+
+
+@subpackage("lshw-gtk")
+def _(self):
+    self.subdesc = "GTK UI"
+    self.depends = [
+        self.parent,
+    ]
+    return [
+        "cmd:gtk-lshw",
+        "usr/share/lshw/artwork",
+        "usr/share/lshw/ui",
+        "usr/share/applications",
+        "usr/share/metainfo",
+        "usr/share/doc/lshw",
+        "usr/lib/pam.d",
+    ]
