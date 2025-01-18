4b69156e2ef12827fdd0363f939b82462d1185a8
q66 <q66@chimera-linux.org>
2025-01-18 21:21:23 +0100

    user/eza: update to 0.20.17 and move from main

--- user/eza/template.py
+++ user/eza/template.py
@@ -1,7 +1,8 @@
 pkgname = "eza"
-pkgver = "0.20.15"
+pkgver = "0.20.17"
 pkgrel = 0
 build_style = "cargo"
+prepare_after_patch = True
 hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = [
     "libgit2-devel",
@@ -15,7 +16,7 @@ url = "https://eza.rocks"
 source = (
     f"https://github.com/eza-community/eza/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "cbb50e61b35b06ccf487ee6cc88d3b624931093546194dd5a2bbd509ed1786d6"
+sha256 = "62bcf8f1b2d087fa0c2c8365e4427ad30ffa986330413191da47061aecfe20de"
 
 
 def post_install(self):
