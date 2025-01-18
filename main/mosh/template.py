5c0ce5f32fbaa1d34ef730dc223abb1f21e2340c
q66 <q66@chimera-linux.org>
2025-01-18 17:13:34 +0100

    */*: rebuild for protobuf

--- main/mosh/template.py
+++ main/mosh/template.py
@@ -1,6 +1,6 @@
 pkgname = "mosh"
 pkgver = "1.4.0"
-pkgrel = 19
+pkgrel = 20
 build_style = "gnu_configure"
 hostmakedepends = [
     "automake",
--- main/protobuf-c/template.py
+++ main/protobuf-c/template.py
@@ -1,6 +1,6 @@
 pkgname = "protobuf-c"
 pkgver = "1.5.0"
-pkgrel = 21
+pkgrel = 22
 build_style = "gnu_configure"
 configure_args = [
     "--enable-protoc",
