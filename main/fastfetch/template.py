77e8c4bc548d9a819cf57fba753ffc24d293a877
q66 <q66@chimera-linux.org>
2025-01-18 17:01:29 +0100

    main/fastfetch: update to 2.34.1

--- main/fastfetch/template.py
+++ main/fastfetch/template.py
@@ -1,5 +1,5 @@
 pkgname = "fastfetch"
-pkgver = "2.32.1"
+pkgver = "2.34.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_TESTS=ON", "-DENABLE_SYSTEM_YYJSON=ON"]
@@ -27,7 +27,7 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "MIT"
 url = "https://github.com/fastfetch-cli/fastfetch"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "f08efaaffa9f1c58b085105acfc41c65ed8f5721bdc7b1746b80700c727a4f60"
+sha256 = "856920a6484a324088bbbcaf608555c74168078fd34cdbde4d3e4af88bc5cb5f"
 tool_flags = {"CFLAGS": ["-DNDEBUG"]}
 # CFI: dies immediately (ffPlatformPathAddHome at FFlist.c:31:12)
 hardening = ["vis", "!cfi"]
