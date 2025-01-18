035d74f01a9f0097a832c1a345175d33e217b9ac
q66 <q66@chimera-linux.org>
2025-01-18 16:54:06 +0100

    main/blueprint-compiler: update to 0.16.0

--- main/blueprint-compiler/template.py
+++ main/blueprint-compiler/template.py
@@ -1,5 +1,5 @@
 pkgname = "blueprint-compiler"
-pkgver = "0.14.0"
+pkgver = "0.16.0"
 pkgrel = 0
 build_style = "meson"
 make_check_wrapper = ["wlheadless-run", "--"]
@@ -18,4 +18,4 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "LGPL-3.0-or-later"
 url = "https://jwestman.pages.gitlab.gnome.org/blueprint-compiler"
 source = f"https://gitlab.gnome.org/jwestman/blueprint-compiler/-/archive/v{pkgver}/blueprint-compiler-v{pkgver}.tar.gz"
-sha256 = "05faf3810cb76d4e2d2382c6a7e6c8096af27e144e2260635c97f6a173d67234"
+sha256 = "01feb8263fe7a450b0a9fed0fd54cf88947aaf00f86cc7da345f8b39a0e7bd30"
