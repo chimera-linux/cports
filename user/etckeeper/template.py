6c4d13e0a6aca59ab7fb151f958fe1ee03e0d187
q66 <q66@chimera-linux.org>
2025-01-19 23:38:37 +0100

    user/etckeeper: move from main and update to 1.18.22

--- user/etckeeper/template.py
+++ user/etckeeper/template.py
@@ -1,5 +1,5 @@
 pkgname = "etckeeper"
-pkgver = "1.18.21"
+pkgver = "1.18.22"
 pkgrel = 0
 build_style = "makefile"
 make_install_args = [
@@ -14,4 +14,4 @@ maintainer = "hge <h.gersen@gmail.com>"
 license = "GPL-2.0-or-later"
 url = "https://etckeeper.branchable.com"
 source = f"https://git.joeyh.name/index.cgi/etckeeper.git/snapshot/etckeeper-{pkgver}.tar.gz"
-sha256 = "a87c5e9c847c29f761da933c1cd907779545c7ddf92fb75de8ef692b90fc9e5d"
+sha256 = "ff0e95e3b6cf6f377b8a04f18f572b011e890eedc1a742b3c0e11ebc283f7a7e"
