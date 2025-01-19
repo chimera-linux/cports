7f08772c984c5c05755311dd4e2094c3a5ea0f8a
q66 <q66@chimera-linux.org>
2025-01-19 23:46:36 +0100

    main/lxqt-runner: update to 2.1.1

--- main/lxqt-runner/template.py
+++ main/lxqt-runner/template.py
@@ -1,5 +1,5 @@
 pkgname = "lxqt-runner"
-pkgver = "2.1.0"
+pkgver = "2.1.1"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -23,4 +23,4 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/lxqt-runner"
 source = f"{url}/releases/download/{pkgver}/lxqt-runner-{pkgver}.tar.xz"
-sha256 = "2b7f233792099d0ee2369576c5f2e0ebb3dce9137593e7cdf0a1ffe10feff6a6"
+sha256 = "bc764f027bcd3e40d5fa3c28b31db975aa8c1f5f75218384c5b1b3b37f8cbb5e"
