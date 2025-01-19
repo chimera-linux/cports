a931346f5bb0abd4bb8adea8ae6bdb7275bba220
Erica Z <zerica@callcc.eu>
2025-01-19 09:35:36 +0100

    main/python-numpy: update to 2.2.2

--- main/python-numpy/template.py
+++ main/python-numpy/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-numpy"
-pkgver = "2.2.1"
+pkgver = "2.2.2"
 pkgrel = 0
 build_style = "python_pep517"
 make_build_args = []
@@ -21,7 +21,7 @@ maintainer = "Erica Z <zerica@callcc.eu>"
 license = "BSD-3-Clause"
 url = "https://numpy.org"
 source = f"https://github.com/numpy/numpy/releases/download/v{pkgver}/numpy-{pkgver}.tar.gz"
-sha256 = "45681fd7128c8ad1c379f0ca0776a8b0c6583d2f69889ddac01559dfe4390918"
+sha256 = "ed6906f61834d687738d25988ae117683705636936cc605be0bb208b23df4d8f"
 hardening = ["!int"]
 
 if self.profile().arch == "aarch64":
