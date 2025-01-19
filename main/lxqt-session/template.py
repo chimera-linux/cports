816f8e01ae2e64ecc78336c7896d5b4668db94e8
q66 <q66@chimera-linux.org>
2025-01-19 23:47:26 +0100

    main/lxqt-session: update to 2.1.1

--- main/lxqt-session/template.py
+++ main/lxqt-session/template.py
@@ -1,6 +1,6 @@
 pkgname = "lxqt-session"
-pkgver = "2.1.0"
-pkgrel = 1
+pkgver = "2.1.1"
+pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -28,4 +28,4 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/lxqt-session"
 source = f"{url}/releases/download/{pkgver}/lxqt-session-{pkgver}.tar.xz"
-sha256 = "b745ef14afec4bed788aeb2d448861fbc0c6f738c1ce956f4a6450f6c745270b"
+sha256 = "312b0cd4106d0bca6812c7100750edee012e93e9fefc2b171731a782bc9bc819"
