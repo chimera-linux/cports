45758ad1a1fcf6ab15af28683383ffc85633e1d8
q66 <q66@chimera-linux.org>
2025-01-19 23:48:56 +0100

    main/muparser: update to 2.3.5

--- main/muparser/template.py
+++ main/muparser/template.py
@@ -1,5 +1,5 @@
 pkgname = "muparser"
-pkgver = "2.3.4"
+pkgver = "2.3.5"
 pkgrel = 0
 build_style = "cmake"
 configure_args = []
@@ -14,7 +14,7 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "BSD-2-Clause"
 url = "https://beltoforion.de/en/muparser"
 source = f"https://github.com/beltoforion/muparser/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0c3fa54a3ebf36dda0ed3e7cd5451c964afbb15102bdbcba08aafb359a290121"
+sha256 = "20b43cc68c655665db83711906f01b20c51909368973116dfc8d7b3c4ddb5dd4"
 
 if self.profile().arch in ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]:
     makedepends += ["libomp-devel"]
