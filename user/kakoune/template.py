665e4f63f6c68a5ebd8f1da0aa5bab95941f6e1d
q66 <q66@chimera-linux.org>
2025-01-19 22:11:15 +0100

    user/kakoune: disable test on aarch64

--- user/kakoune/template.py
+++ user/kakoune/template.py
@@ -12,3 +12,9 @@ url = "https://kakoune.org"
 source = f"https://github.com/mawww/kakoune/releases/download/v{pkgver}/kakoune-{pkgver}.tar.bz2"
 sha256 = "dae8ac2e61d21d9bcd10145aa70b421234309a7b0bc57fad91bc34dbae0cb9fa"
 hardening = ["vis", "cfi"]
+# check may be disabled
+options = []
+
+if self.profile().arch == "aarch64":
+    # fails kak_selection test
+    options += ["!check"]
