2e99bbdfc9eca6769ae058c37e7142a63c047479
Renato Botelho do Couto <renato@netgate.com>
2025-01-21 14:16:08 -0300

    main/libmaxminddb: update to 1.12.2

--- main/libmaxminddb/template.py
+++ main/libmaxminddb/template.py
@@ -1,5 +1,5 @@
 pkgname = "libmaxminddb"
-pkgver = "1.11.0"
+pkgver = "1.12.2"
 pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
@@ -9,7 +9,7 @@ maintainer = "Renato Botelho do Couto <renato@netgate.com>"
 license = "Apache-2.0"
 url = "https://github.com/maxmind/libmaxminddb"
 source = f"{url}/releases/download/{pkgver}/libmaxminddb-{pkgver}.tar.gz"
-sha256 = "b2eea79a96fed77ad4d6c39ec34fed83d45fcb75a31c58956813d58dcf30b19f"
+sha256 = "1bfbf8efba3ed6462e04e225906ad5ce5fe958aa3d626a1235b2a2253d600743"
 
 
 @subpackage("libmaxminddb-devel")
