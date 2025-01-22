c40468412bdd6d0ba56351b939e71b7fd9169b88
q66 <q66@chimera-linux.org>
2025-01-22 03:14:58 +0100

    main/sdl12-compat: build with sdl2-compat

--- main/sdl12-compat/template.py
+++ main/sdl12-compat/template.py
@@ -1,9 +1,9 @@
 pkgname = "sdl12-compat"
 pkgver = "1.2.68"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
-makedepends = ["sdl-devel"]
+makedepends = ["glu-devel", "sdl2-compat-devel"]
 # is dlopen'ed
 depends = ["so:libSDL2-2.0.so.0!sdl"]
 pkgdesc = "Compatibility layer for SDL 1.2"
@@ -16,4 +16,6 @@ sha256 = "63c6e4dcc1154299e6f363c872900be7f3dcb3e42b9f8f57e05442ec3d89d02d"
 
 @subpackage("sdl12-compat-devel")
 def _(self):
+    self.depends += makedepends
+
     return self.default_devel()
