ce4fac95c8ba0a0b387cf0300c187b0762b5a7ea
ttyyls <contact@behri.org>
2025-01-22 11:04:49 +0100

    user/kakoune-lsp: update to 18.1.2

--- user/kakoune-lsp/template.py
+++ user/kakoune-lsp/template.py
@@ -1,6 +1,6 @@
 pkgname = "kakoune-lsp"
-pkgver = "18.1.1"
-pkgrel = 1
+pkgver = "18.1.2"
+pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
 makedepends = ["rust-std"]
@@ -9,7 +9,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "MIT OR Unlicense"
 url = "https://github.com/kakoune-lsp/kakoune-lsp"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "cb0f8185dfd7952f213e3b11cbcea36a9420514710589c37c5db594f5144f3ac"
+sha256 = "d3087c0f78f42d3ad30ccaa64f192a88f646e40615b7e4986c53165019f34934"
 
 
 def install(self):
