0b789b267830d1c7dd0eeee4be4d4f596e05a1ac
q66 <q66@chimera-linux.org>
2025-01-22 02:43:23 +0100

    main/sdl2-compat: provide higher devel versions
    2 is not high enough as e.g. sdl_image and sdl_mixer devel pkgs
    require at least 2.0.9. Keep it still lower than actual sdl-devel
    for now though.

--- main/sdl2-compat/template.py
+++ main/sdl2-compat/template.py
@@ -1,6 +1,6 @@
 pkgname = "sdl2-compat"
 pkgver = "2.30.50"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = ["sdl3-devel", "sdl3-devel-static"]  # needs libSDL3_test.a
@@ -20,8 +20,8 @@ sha256 = "f65e369b45c4cf2981f446541b1754ccb4714a7ec62fad339d75c0176b8fa212"
 def _(self):
     # see above
     self.provides = [
-        "cmd:sdl2-config=2",
-        "pc:sdl2=2",
+        "cmd:sdl2-config=2.30.0",
+        "pc:sdl2=2.30.0",
     ]
 
     return self.default_devel()
