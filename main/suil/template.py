ed6cea30b6abce1713a603cbfb0c51ce28d853bd
q66 <q66@chimera-linux.org>
2025-01-23 01:52:52 +0100

    main/suil: update to 0.10.22

--- main/suil/template.py
+++ main/suil/template.py
@@ -1,5 +1,5 @@
 pkgname = "suil"
-pkgver = "0.10.20"
+pkgver = "0.10.22"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -21,8 +21,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "ISC"
 url = "https://drobilla.net/software/suil.html"
 source = f"https://download.drobilla.net/suil-{pkgver}.tar.xz"
-sha256 = "334a3ed3e73d5e17ff400b3db9801f63809155b0faa8b1b9046f9dd3ffef934e"
-hardening = ["vis", "cfi"]
+sha256 = "d720969e0f44a99d5fba35c733a43ed63a16b0dab867970777efca4b25387eb7"
 # no actual tests
 options = ["!check"]
 
