99dfec7d72549baab071f272184ed93aaff75cec
q66 <q66@chimera-linux.org>
2025-01-23 01:54:38 +0100

    main/xdg-desktop-portal-gnome: update to 47.2

--- main/xdg-desktop-portal-gnome/template.py
+++ main/xdg-desktop-portal-gnome/template.py
@@ -1,5 +1,5 @@
 pkgname = "xdg-desktop-portal-gnome"
-pkgver = "47.1"
+pkgver = "47.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dsystemduserunitdir=/tmp/delete_me"]
@@ -16,7 +16,7 @@ maintainer = "eater <=@eater.me>"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/xdg-desktop-portal-gnome"
 source = f"https://download.gnome.org/sources/xdg-desktop-portal-gnome/{pkgver.split('.')[0]}/xdg-desktop-portal-gnome-{pkgver}.tar.xz"
-sha256 = "d420ff72173b04dc9491d3d7dd8cc96204f7f3f27c4cbea6024361ea9839f769"
+sha256 = "4b5368aa19dc0aa62c8a84e481c8a26490b4d77c7900b8e5c9d432ce91f1274c"
 
 
 def post_install(self):
