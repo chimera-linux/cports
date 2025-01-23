a4287eaaf97b1a5252e8ec452674c2a176c14685
q66 <q66@chimera-linux.org>
2025-01-23 01:39:15 +0100

    main/brlaser: update to 6.2.7

--- main/brlaser/template.py
+++ main/brlaser/template.py
@@ -1,5 +1,5 @@
 pkgname = "brlaser"
-pkgver = "6.2.6"
+pkgver = "6.2.7"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja"]
@@ -9,5 +9,5 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/Owl-Maintain/brlaser"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "551f85a234c75c52c52bb76f379eb458ebc7bd7164d55763d97e197a0ad47f6d"
+sha256 = "e67c5726fc1fe53574c2e8b5f72634f1359d0f53586a555eb2489fafd7c81640"
 hardening = ["cfi", "vis"]
