64bc09f7f79a04b42c6ae38aa768ad846dfa29b4
Bin Jin <bjin@protonmail.com>
2025-01-24 17:43:58 +0800

    user/librime: new package

---
+++ user/librime/template.py
@@ -0,0 +1,33 @@
+pkgname = "librime"
+pkgver = "1.13.1"
+pkgrel = 0
+build_style = "cmake"
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = [
+    "boost-devel",
+    "glog-devel",
+    "leveldb-devel",
+    "marisa-trie-devel",
+    "opencc-devel",
+    "yaml-cpp-devel",
+]
+checkdepends = ["gtest-devel"]
+pkgdesc = "Rime Input Method Engine"
+license = "BSD-3-Clause"
+url = "https://github.com/rime/librime"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "ae7eb6335139c044e438299b2ab9a0f630e665e8f5fe1f30a9416a2d1325b84e"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("librime-devel")
+def _(self):
+    return self.default_devel()
+
+
+@subpackage("librime-progs")
+def _(self):
+    return self.default_progs()
