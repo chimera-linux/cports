46964f260dd3132e1951e61da230b7a9d412fae1
ttyyls <contact@behri.org>
2025-01-24 16:39:37 +0100

    user/zathura-pdf-mupdf: new package

---
+++ user/zathura-pdf-mupdf/template.py
@@ -0,0 +1,21 @@
+pkgname = "zathura-pdf-mupdf"
+pkgver = "0.4.4"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
+makedepends = [
+    "mupdf-devel",
+    "zathura-devel",
+]
+checkdepends = ["check-devel"]
+depends = ["zathura"]
+pkgdesc = "PDF support for zathura"
+maintainer = "ttyyls <contact@behri.org>"
+license = "Zlib"
+url = "https://pwmt.org/projects/zathura-pdf-mupdf"
+source = f"{url}/download/zathura-pdf-mupdf-{pkgver}.tar.xz"
+sha256 = "0125624901cabe3a2fe63315a46e7d966a323c028ff53890dfaf7856adb1f4fc"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
