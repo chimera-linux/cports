38f03e050c4855847e11f15b6fb03f9f0420311b
q66 <q66@chimera-linux.org>
2025-01-24 03:32:49 +0100

    user/erlang: update to 27.2.1

--- user/erlang/template.py
+++ user/erlang/template.py
@@ -1,6 +1,6 @@
 # TODO: split devel, maybe the libs too? (may not be worth it)
 pkgname = "erlang"
-pkgver = "27.2"
+pkgver = "27.2.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -21,7 +21,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://www.erlang.org"
 source = f"https://github.com/erlang/otp/archive/OTP-{pkgver}.tar.gz"
-sha256 = "0727cf869622544a2434a104109b31f5fadb8dc6b532287aea182fab95922ea8"
+sha256 = "5314d0b876fd8d99402d4646a5cf75e803763152b80e8fc2cbf0a4f1199c8ebc"
 # some staticlibs inside the runtime should be non-lto
 options = ["!cross", "!lto"]
 
