8993b9ee4efc92452124ec4e7f90c51d702d93c7
q66 <q66@chimera-linux.org>
2025-01-25 01:17:41 +0100

    main/kpartx: update to 0.11.0

--- main/kpartx/template.py
+++ main/kpartx/template.py
@@ -1,5 +1,5 @@
 pkgname = "kpartx"
-pkgver = "0.10.0"
+pkgver = "0.11.0"
 pkgrel = 0
 build_wrksrc = "kpartx"
 build_style = "makefile"
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "http://christophe.varoqui.free.fr"
 source = f"https://github.com/opensvc/multipath-tools/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "f2e67a1d2167f3945afab6f0697207a03678f5b2bd80f1f45958c6fa1dfb8eef"
+sha256 = "0e7564e4984806006ec96134ea8823cfc461455675ce8aa5f363d94f696412f6"
 hardening = ["vis", "cfi"]
 # no test suite
 options = ["!check"]
