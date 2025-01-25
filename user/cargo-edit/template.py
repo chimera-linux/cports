2a668436bc308d88f0b8fd96adfe3278e8814fdc
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-01-25 23:53:54 +0100

    user/cargo-edit: update to 0.13.1

--- user/cargo-edit/template.py
+++ user/cargo-edit/template.py
@@ -1,6 +1,6 @@
 pkgname = "cargo-edit"
-pkgver = "0.13.0"
-pkgrel = 1
+pkgver = "0.13.1"
+pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = ["rust-std"]
@@ -9,7 +9,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/killercup/cargo-edit"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "c81a73fb1ef4ffef722835baf473beed9868ce2c58ad98a27596f2cbabbfcba3"
+sha256 = "11a973bc77ef1562a599e8acc844bf763be4e9caf6e5a650239bc9c6d2077e5b"
 # Checks don't work with our cargo config overrides
 options = ["!check"]
 
