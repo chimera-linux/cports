a835a2bc0bcbf9cdf32c58eb37807d09673cab4a
q66 <q66@chimera-linux.org>
2025-01-26 23:33:35 +0100

    main/python-josepy: update to 1.15.0

--- main/python-josepy/template.py
+++ main/python-josepy/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-josepy"
-pkgver = "1.14.0"
+pkgver = "1.15.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,4 +14,4 @@ maintainer = "Duncan Bellamy <dunk@denkimushi.com>"
 license = "Apache-2.0"
 url = "https://josepy.readthedocs.io/en/latest"
 source = f"$(PYPI_SITE)/j/josepy/josepy-{pkgver}.tar.gz"
-sha256 = "308b3bf9ce825ad4d4bba76372cf19b5dc1c2ce96a9d298f9642975e64bd13dd"
+sha256 = "46c9b13d1a5104ffbfa5853e555805c915dcde71c2cd91ce5386e84211281223"
