01d15eb7af03065e8635fac2386c0c680edf2d0c
q66 <q66@chimera-linux.org>
2025-01-26 03:48:48 +0100

    user/arx-libertatis: bump up to latest git revision
    This is a low-traffic project (no changes since august last year)
    and the stable release seems to have crashes, e.g. during the
    first level i got a reproducible crash always at a specific point.
    
    This seems to be triggered by C++ stdlib assertions.

--- user/arx-libertatis/template.py
+++ user/arx-libertatis/template.py
@@ -1,6 +1,9 @@
 pkgname = "arx-libertatis"
-pkgver = "1.2.1"
+# latest stable version has known crashes, e.g. in the first
+# dungeon near the cobweb, due to c++ vector length-related bugs
+pkgver = "1.2.1_git20240822"
 pkgrel = 0
+_gitrev = "5b95e4c5ca9d583f1b11c085326979772645e0f3"
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "python"]
 makedepends = [
@@ -18,9 +21,20 @@ pkgdesc = "Improved open source engine for Arx Fatalis"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://arx-libertatis.org"
-source = f"{url}/files/arx-libertatis-{pkgver}.tar.xz"
-sha256 = "aafd8831ee2d187d7647ad671a03aabd2df3b7248b0bac0b3ac36ffeb441aedf"
+source = [
+    f"https://github.com/arx/ArxLibertatis/archive/{_gitrev}.tar.gz",
+    "https://arx-libertatis.org/files/data/arx-libertatis-data-1.tar.xz",
+]
+source_paths = [".", "data-icons"]
+sha256 = [
+    "976a46086f256d33aa26fda9e62f423302a71a978f4a06d73fbe6d8f18c000d9",
+    "79d943f060a48818e111fda658425d2879726ce53b02b6bb188a07b92aa88702",
+]
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=0x200000"]}
 hardening = ["!int"]
 # not particularly useful
 options = ["!check"]
+
+
+def post_extract(self):
+    self.mv("data-icons/icons/arx-libertatis-*", "data/icons", glob=True)
