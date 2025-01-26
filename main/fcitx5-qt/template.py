d0b69d810fac1344d4c93b36508d8855eb9d3752
q66 <q66@chimera-linux.org>
2025-01-26 23:27:12 +0100

    main/fcitx5-qt: update to 5.1.9

--- main/fcitx5-qt/template.py
+++ main/fcitx5-qt/template.py
@@ -1,6 +1,6 @@
 pkgname = "fcitx5-qt"
-pkgver = "5.1.8"
-pkgrel = 1
+pkgver = "5.1.9"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DBUILD_SHARED_LIBS=ON",
@@ -29,7 +29,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "BSD-3-Clause AND LGPL-2.1-or-later"
 url = "https://fcitx-im.org"
 source = f"https://github.com/fcitx/fcitx5-qt/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "a09fb653ab855424a5a92c6a634c3726fa19ae85ca7c24716784b3ddd136f201"
+sha256 = "4e691182f20984d11f8ea391a57b2711e362c43fda21236327e386c07f9c8998"
 hardening = ["vis", "cfi"]
 # fails
 options = ["!cross"]
