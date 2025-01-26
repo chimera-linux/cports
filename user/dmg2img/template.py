6b2dceb1fa16277a02a77089c3280df0f0dc792c
breakgimme <adam@plock.com>
2025-01-26 20:38:41 +0100

    user/dmg2img: new package

---
+++ user/dmg2img/template.py
@@ -0,0 +1,13 @@
+pkgname = "dmg2img"
+pkgver = "1.6.7"
+pkgrel = 0
+build_style = "makefile"
+makedepends = ["bzip2-devel", "openssl-devel", "zlib-ng-compat-devel"]
+pkgdesc = "Compressed dmg to standard image disk file convert tool"
+maintainer = "breakgimme <adam@plock.com>"
+license = "GPL-2.0-only"
+url = "http://vu1tur.eu.org/tools"
+source = f"http://vu1tur.eu.org/tools/dmg2img-{pkgver}.tar.gz"
+sha256 = "02aea6d05c5b810074913b954296ddffaa43497ed720ac0a671da4791ec4d018"
+# no tests
+options = ["!check"]
