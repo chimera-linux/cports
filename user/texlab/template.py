3849c4c2220c1a64fe351349bdbda2744e52cdae
ttyyls <contact@behri.org>
2025-01-28 00:21:53 +0100

    user/texlab: update to 5.22.0

--- user/texlab/template.py
+++ user/texlab/template.py
@@ -1,6 +1,6 @@
 pkgname = "texlab"
-pkgver = "5.21.0"
-pkgrel = 1
+pkgver = "5.22.0"
+pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
 makedepends = ["rust-std"]
@@ -9,7 +9,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/latex-lsp/texlab"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "723099514ffb7a6537f486de951ae2cfd97d2ae6420aa1ff8eb6ed4068ecb160"
+sha256 = "dfca5bc12419e771092b6bdaf9379fe04164848ff19054d4ec7c4bba0eef7021"
 
 
 def install(self):
