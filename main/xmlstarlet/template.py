17a4260e9ce512683f7ee06906cc2b0634577024
q66 <q66@chimera-linux.org>
2025-01-27 16:26:41 +0100

    main/xmlstarlet: fix tests

--- main/xmlstarlet/template.py
+++ main/xmlstarlet/template.py
@@ -4,7 +4,9 @@ pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-libxml-prefix=/usr", "--with-libxslt-prefix=/usr"]
 make_dir = "."
-hostmakedepends = ["pkgconf", "xsltproc"]
+# exclude bigxml tests that fail with latest libxml2
+make_check_target = "qcheck"
+hostmakedepends = ["automake", "pkgconf", "xsltproc"]
 makedepends = ["libxslt-devel", "libxml2-devel"]
 pkgdesc = "Command line utilities for XML manipulation"
 maintainer = "q66 <q66@chimera-linux.org>"
@@ -12,13 +14,9 @@ license = "MIT"
 url = "https://xmlstar.sourceforge.net"
 source = f"$(SOURCEFORGE_SITE)/xmlstar/xmlstarlet/{pkgver}/xmlstarlet-{pkgver}.tar.gz"
 sha256 = "15d838c4f3375332fd95554619179b69e4ec91418a3a5296e7c631b7ed19e7ca"
-options = ["!cross"]
-
 tool_flags = {"CFLAGS": ["-Wno-incompatible-function-pointer-types"]}
+options = ["!cross"]
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-configure_gen = []
