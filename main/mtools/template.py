b906c3e64172faae3e758f8bf5e063eb85457fd1
q66 <q66@chimera-linux.org>
2025-01-28 02:00:52 +0100

    main/mtools: update to 4.0.47

--- main/mtools/template.py
+++ main/mtools/template.py
@@ -1,5 +1,5 @@
 pkgname = "mtools"
-pkgver = "4.0.46"
+pkgver = "4.0.47"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "texinfo"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/mtools"
 source = f"$(GNU_SITE)/mtools/mtools-{pkgver}.tar.bz2"
-sha256 = "9aad8dd859f88fb7787924ec47590192d3abf7bad6c840509c854290d6bc16c0"
+sha256 = "31aa06078cc3f50591b95e71a909c56dd179d87e9cbdc07bf435e595bd7cc7ff"
 hardening = ["vis", "!cfi"]
 
 
