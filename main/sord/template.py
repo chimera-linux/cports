f5900b8ef112314ac7815d9ac33b3052dec9ce51
q66 <q66@chimera-linux.org>
2025-01-28 02:11:04 +0100

    main/sord: update to 0.16.18

--- main/sord/template.py
+++ main/sord/template.py
@@ -1,5 +1,5 @@
 pkgname = "sord"
-pkgver = "0.16.16"
+pkgver = "0.16.18"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "ISC"
 url = "https://drobilla.net/software/sord.html"
 source = f"https://download.drobilla.net/sord-{pkgver}.tar.xz"
-sha256 = "257f876d756143da02ee84c9260af93559d6249dd87f317e70ab5fffcc975fd0"
+sha256 = "4f398b635894491a4774b1498959805a08e11734c324f13d572dea695b13d3b3"
 hardening = ["vis", "!cfi"]
 
 
