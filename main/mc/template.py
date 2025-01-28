27bb8a870b4aef03476af106eaf9c9af3a087aad
q66 <q66@chimera-linux.org>
2025-01-28 02:17:00 +0100

    main/mc: update to 4.8.33

--- main/mc/template.py
+++ main/mc/template.py
@@ -1,6 +1,6 @@
 pkgname = "mc"
-pkgver = "4.8.32"
-pkgrel = 1
+pkgver = "4.8.33"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-screen=ncurses", "--without-x"]
 configure_gen = []  # broken m4
@@ -17,5 +17,5 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-3.0-or-later"
 url = "https://midnight-commander.org"
 source = f"{url}/downloads/mc-{pkgver}.tar.xz"
-sha256 = "4ddc83d1ede9af2363b3eab987f54b87cf6619324110ce2d3a0e70944d1359fe"
+sha256 = "cae149d42f844e5185d8c81d7db3913a8fa214c65f852200a9d896b468af164c"
 hardening = ["vis", "cfi"]
