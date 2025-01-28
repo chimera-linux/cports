d00139a9de95dee67b50a247cdf6d526a8d02bf4
q66 <q66@chimera-linux.org>
2025-01-28 01:58:49 +0100

    main/libpaper: update to 2.2.6

--- main/libpaper/template.py
+++ main/libpaper/template.py
@@ -1,6 +1,6 @@
 pkgname = "libpaper"
-pkgver = "2.2.5"
-pkgrel = 1
+pkgver = "2.2.6"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "libtool", "gettext"]
 pkgdesc = "Library for handling paper characteristics"
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT AND GPL-2.0-only AND GPL-3.0-or-later AND custom:none"
 url = "https://github.com/rrthomas/libpaper"
 source = f"{url}/releases/download/v{pkgver}/libpaper-{pkgver}.tar.gz"
-sha256 = "7be50974ce0df0c74e7587f10b04272cd53fd675cb6a1273ae1cc5c9cc9cab09"
+sha256 = "500d39dc58768ee09688738c8b5bfe07640ba2fd6c25a6dc78810eb69c719e93"
 
 
 def post_install(self):
