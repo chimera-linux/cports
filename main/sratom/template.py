8f59c83c1445e3abbdf70d24c6f309ba778de290
q66 <q66@chimera-linux.org>
2025-01-28 02:11:26 +0100

    main/sratom: update to 0.6.18

--- main/sratom/template.py
+++ main/sratom/template.py
@@ -1,5 +1,5 @@
 pkgname = "sratom"
-pkgver = "0.6.16"
+pkgver = "0.6.18"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -9,7 +9,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "ISC"
 url = "https://drobilla.net/software/sratom.html"
 source = f"https://download.drobilla.net/sratom-{pkgver}.tar.xz"
-sha256 = "71c157991183e53d0555393bb4271c75c9b5f5dab74a5ef22f208bb22de322c4"
+sha256 = "4c6a6d9e0b4d6c01cc06a8849910feceb92e666cb38779c614dd2404a9931e92"
 hardening = ["vis", "!cfi"]
 
 
