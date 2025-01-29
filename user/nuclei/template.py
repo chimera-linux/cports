f756f580dac5195ba5644d4730d7d975bbb924a5
q66 <q66@chimera-linux.org>
2025-01-29 01:35:19 +0100

    user/nuclei: update to 3.3.8

--- user/nuclei/template.py
+++ user/nuclei/template.py
@@ -1,5 +1,5 @@
 pkgname = "nuclei"
-pkgver = "3.3.7"
+pkgver = "3.3.8"
 pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/nuclei"]
@@ -9,7 +9,7 @@ maintainer = "Gabriel M. Dutra <dmdutra@proton.me>"
 license = "MIT"
 url = "https://github.com/projectdiscovery/nuclei"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "3d12498f90b4babead541578f095c8f8aac7d08038073f0f239142356d3a4c79"
+sha256 = "e03e36778ff9736882e52c43c19da8888443c9130cafd30a3305e42cbfb86467"
 # Nuclei tests requires network connection
 options = ["!check"]
 
