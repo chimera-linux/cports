1b6f87349ca4bfbbb53857d0070ea80ce40907ae
q66 <q66@chimera-linux.org>
2025-01-29 01:34:48 +0100

    user/k6: update to 0.56.0

--- user/k6/template.py
+++ user/k6/template.py
@@ -1,5 +1,5 @@
 pkgname = "k6"
-pkgver = "0.55.0"
+pkgver = "0.56.0"
 pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
@@ -8,7 +8,7 @@ maintainer = "Gabriel M. Dutra <dmdutra@proton.me>"
 license = "AGPL-3.0-only"
 url = "https://github.com/grafana/k6"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0b32dfbafc91719bba9ffd149ddfb849270beecf3830e99708b9b6c25bf1b677"
+sha256 = "a11776d57e9cf3fb306c4f0df9141f972c1b6506c47a750fd71d0f5b24945272"
 # k6 tests requires network connection
 options = ["!check"]
 
