e2ddac3b92169c078235d3b467e2fc7a5b168864
triallax <triallax@tutanota.com>
2025-02-01 19:07:05 +0000

    main/python-structlog: update to 25.1.0

--- main/python-structlog/template.py
+++ main/python-structlog/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-structlog"
-pkgver = "24.4.0"
+pkgver = "25.1.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -20,7 +20,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "Apache-2.0 OR MIT"
 url = "https://structlog.org"
 source = f"$(PYPI_SITE)/s/structlog/structlog-{pkgver}.tar.gz"
-sha256 = "b27bfecede327a6d2da5fbc96bd859f114ecc398a6389d664f62085ee7ae6fc4"
+sha256 = "2ef2a572e0e27f09664965d31a576afe64e46ac6084ef5cec3c2b8cd6e4e3ad3"
 
 
 def post_install(self):
