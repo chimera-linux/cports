4cad2b0592f6d9418f671fd3d48f30b4d2a9b176
triallax <triallax@tutanota.com>
2025-02-02 13:25:17 +0000

    main/libwacom: update to 2.14.0
    Closes https://github.com/chimera-linux/cports/pull/3549

--- main/libwacom/template.py
+++ main/libwacom/template.py
@@ -1,5 +1,5 @@
 pkgname = "libwacom"
-pkgver = "2.13.0"
+pkgver = "2.14.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocumentation=disabled", "-Dtests=enabled"]
@@ -18,7 +18,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/linuxwacom/libwacom"
 source = f"{url}/releases/download/libwacom-{pkgver}/libwacom-{pkgver}.tar.xz"
-sha256 = "acd18121441bbc00fc5c881fca08a33319ab814b798eac8d0be6354923f8fb08"
+sha256 = "5900b3ad3d780e1b864103ace99cace9470db727a162517e1648c86a1bdec0e3"
 
 
 def post_install(self):
