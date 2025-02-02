0b1aec47ff45310ce2fdf1e1d017de1665e47b64
triallax <triallax@tutanota.com>
2025-02-02 01:22:26 +0000

    main/python-fonttools: update to 4.55.8

--- main/python-fonttools/template.py
+++ main/python-fonttools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-fonttools"
-pkgver = "4.55.3"
+pkgver = "4.55.8"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -18,7 +18,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT AND OFL-1.1 AND BSD-3-Clause AND Apache-2.0"
 url = "https://github.com/fonttools/fonttools"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "c8c250586066a66ea8a26cc4b06bce5ec6fb9b2bf4afd7aff9d509ed7bfd5fcd"
+sha256 = "1fd1ca263ec5de41bca4c490e30ce614364910a8374ea971132f7c6f7ee3f7db"
 
 
 def init_check(self):
