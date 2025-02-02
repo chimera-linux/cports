d89144609817512ee3cd4aad02e6928c3fdea0ef
triallax <triallax@tutanota.com>
2025-02-02 12:58:27 +0000

    main/python-babel: update to 2.17.0

--- main/python-babel/template.py
+++ main/python-babel/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-babel"
-pkgver = "2.16.0"
-pkgrel = 1
+pkgver = "2.17.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python", "python-setuptools"]
 pkgdesc = "Tools for internationalizing Python applications"
@@ -14,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://babel.pocoo.org"
 source = f"$(PYPI_SITE)/B/Babel/babel-{pkgver}.tar.gz"
-sha256 = "d1f3554ca26605fe173f3de0c65f750f5a42f924499bf134de6423582298e316"
+sha256 = "0c54cffb19f690cdcc52a3b50bcbf71e07a808d1c80d549f2459b9d2cf0afb9d"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
 
