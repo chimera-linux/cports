d506b1d70efb34fa5c53b0349bbf54707bd18a83
triallax <triallax@tutanota.com>
2025-02-02 12:46:38 +0000

    main/spdlog: update to 1.15.1

--- main/spdlog/template.py
+++ main/spdlog/template.py
@@ -1,6 +1,6 @@
 pkgname = "spdlog"
-pkgver = "1.15.0"
-pkgrel = 1
+pkgver = "1.15.1"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DSPDLOG_FMT_EXTERNAL=ON",
@@ -15,7 +15,7 @@ maintainer = "avgwst <avgwst@tutanota.de>"
 license = "MIT"
 url = "https://github.com/gabime/spdlog"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "9962648c9b4f1a7bbc76fd8d9172555bad1871fdb14ff4f842ef87949682caa5"
+sha256 = "25c843860f039a1600f232c6eb9e01e6627f7d030a2ae5e232bdd3c9205d26cc"
 
 
 def post_install(self):
