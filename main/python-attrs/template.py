17ffda1b663991859194ce1186af981e6cf62842
triallax <triallax@tutanota.com>
2025-02-02 02:38:17 +0000

    main/python-attrs: update to 25.1.0

--- main/python-attrs/template.py
+++ main/python-attrs/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-attrs"
-pkgver = "24.3.0"
+pkgver = "25.1.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-installer", "python-hatch_vcs"]
@@ -10,7 +10,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://attrs.readthedocs.io"
 source = f"$(PYPI_SITE)/a/attrs/attrs-{pkgver}.tar.gz"
-sha256 = "8f5c07333d543103541ba7be0e2ce16eeee8130cb0b3f9238ab904ce1e85baff"
+sha256 = "1c97078a80c814273a76b2a298a932eb681c87415c11dee0a6921de7f1b02c3e"
 # dependency of pytest
 options = ["!check"]
 
