168801223f69615d7372ce43892688a0e4e1ed16
triallax <triallax@tutanota.com>
2025-02-02 14:32:39 +0000

    main/libjcat: don't install tests, switch libexecdir

--- main/libjcat/template.py
+++ main/libjcat/template.py
@@ -1,7 +1,10 @@
 pkgname = "libjcat"
 pkgver = "0.2.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
+configure_args = [
+    "--libexecdir=/usr/lib",  # XXX drop libexec
+]
 hostmakedepends = [
     "gnutls-progs",
     "gobject-introspection",
@@ -22,6 +25,11 @@ url = "https://github.com/hughsie/libjcat"
 source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "60fb1d30b16ba1a2dbf48998094d366bc94a082846b71decf9d6ac6bb6aa4800"
 options = ["!cross"]
+
+
+def post_install(self):
+    self.uninstall("usr/lib/installed-tests")
+    self.uninstall("usr/share/installed-tests")
 
 
 @subpackage("libjcat-devel")
