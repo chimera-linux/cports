9daa3241b3152907963777af29ce9432d8a68845
triallax <triallax@tutanota.com>
2025-02-02 00:00:48 +0000

    main/gst-plugins-bad: update to 1.24.12

--- main/gst-plugins-bad/template.py
+++ main/gst-plugins-bad/template.py
@@ -1,5 +1,5 @@
 pkgname = "gst-plugins-bad"
-pkgver = "1.24.11"
+pkgver = "1.24.12"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -143,7 +143,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "LGPL-2.1-or-later"
 url = "https://gstreamer.freedesktop.org"
 source = f"{url}/src/gst-plugins-bad/gst-plugins-bad-{pkgver}.tar.xz"
-sha256 = "9126c2b7053a5f059db958f43c6a5230d2e843ff9adff8c8c2477ea2457e3315"
+sha256 = "3d386af3d1dbd1a06c74a6251250c269b481e703f0e3255ba89ef6c1e063afea"
 # FIXME int
 hardening = ["!int"]
 # TODO: a few fails, debug later
