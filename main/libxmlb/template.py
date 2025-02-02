ff9969a2c594b89bf207f743b675a6bd8f31d513
triallax <triallax@tutanota.com>
2025-02-02 14:32:56 +0000

    main/libxmlb: don't install tests, switch libexecdir

--- main/libxmlb/template.py
+++ main/libxmlb/template.py
@@ -1,8 +1,11 @@
 pkgname = "libxmlb"
 pkgver = "0.3.21"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
-configure_args = ["-Dgtkdoc=false"]
+configure_args = [
+    "-Dgtkdoc=false",
+    "--libexecdir=/usr/lib",  # XXX drop libexec
+]
 hostmakedepends = ["pkgconf", "meson", "gobject-introspection"]
 makedepends = ["xz-devel", "zstd-devel"]
 checkdepends = ["shared-mime-info"]
@@ -14,6 +17,11 @@ url = "https://github.com/hughsie/libxmlb"
 source = f"https://github.com/hughsie/libxmlb/releases/download/{pkgver}/libxmlb-{pkgver}.tar.xz"
 sha256 = "642343c9b3eca5c234ef83d3d5f6307c78d024f2545f3cc2fa252c9e14e4efd1"
 options = ["!cross"]
+
+
+def post_install(self):
+    self.uninstall("usr/lib/installed-tests")
+    self.uninstall("usr/share/installed-tests")
 
 
 @subpackage("libxmlb-devel")
