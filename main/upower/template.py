374b0d7eed7db502b7c129f68ff4add52e37444b
triallax <triallax@tutanota.com>
2025-02-02 14:33:17 +0000

    main/upower: don't install tests, switch libexecdir

--- main/upower/template.py
+++ main/upower/template.py
@@ -1,8 +1,9 @@
 pkgname = "upower"
 pkgver = "1.90.7"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
+    "--libexecdir=/usr/lib",  # XXX drop libexec
     "-Ddefault_library=shared",
     "-Dos_backend=linux",
     "-Dsystemdsystemunitdir=no",
@@ -41,6 +42,13 @@ url = "https://upower.freedesktop.org"
 source = f"https://gitlab.freedesktop.org/upower/upower/-/archive/v{pkgver}/upower-v{pkgver}.tar.gz"
 sha256 = "7d616e529ddfd4a4ced9dcf701c1d77628ef12e242d0603f32bedfa1a2c7e3ed"
 options = ["!cross"]
+
+
+def post_install(self):
+    self.uninstall("usr/lib/upower/integration-test.py")
+    self.uninstall("usr/lib/upower/output_checker.py")
+    self.uninstall("usr/lib/upower/tests")
+    self.uninstall("usr/share/installed-tests")
 
 
 @subpackage("upower-devel")
