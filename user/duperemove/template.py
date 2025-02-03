5d97bb615021b640830ca73ee05b879e4a7f3ebd
Autumn on Tape <autumn@cyfox.net>
2025-02-03 12:39:12 -0800

    user/duperemove: move from main and update to 0.15

--- user/duperemove/template.py
+++ user/duperemove/template.py
@@ -1,25 +1,26 @@
 pkgname = "duperemove"
-pkgver = "0.14.1"
+pkgver = "0.15"
 pkgrel = 0
 build_style = "makefile"
 make_build_env = {
     "VERSION": f"v{pkgver}",
     "IS_RELEASE": "1",
 }
-make_install_args = ["SBINDIR=/usr/bin"]
 hostmakedepends = ["pkgconf"]
-makedepends = ["glib-devel", "sqlite-devel", "linux-headers"]
+makedepends = [
+    "glib-devel",
+    "musl-bsd-headers",
+    "sqlite-devel",
+    "xxhash-devel",
+    "linux-headers",
+]
 pkgdesc = "Tools for deduplicating extents in filesystems like Btrfs"
 maintainer = "autumnontape <autumn@cyfox.net>"
-license = "GPL-2.0-only AND BSD-2-Clause"
+license = "GPL-2.0-only"
 url = "https://github.com/markfasheh/duperemove"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5970a68e37c1b509448f6d82278ca21403cc7722f6267f7da27723b0749088ea"
-tool_flags = {"CFLAGS": ["-std=gnu2x"]}
+sha256 = "1dacde51f12ead1da6b067d5520731a83adee3301fbc36eb282cf8362b93d773"
+tool_flags = {"CFLAGS": ["-std=c23"]}
 hardening = ["vis", "cfi"]
 # no test suite exists
 options = ["!check"]
-
-
-def post_install(self):
-    self.install_license("LICENSE.xxhash")
