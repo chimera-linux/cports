4a6589e07124384af220c31cd49dd6ca2c2bb37a
Gnarwhal <git.aspect893@passmail.net>
2025-02-04 03:54:57 +0000

    user/python-dbus-fast: update to 2.32.0

--- user/python-dbus-fast/template.py
+++ user/python-dbus-fast/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-dbus-fast"
-pkgver = "2.28.0"
+pkgver = "2.32.0"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_wrapper = ["dbus-run-session"]
@@ -20,7 +20,7 @@ maintainer = "Gnarwhal <git.aspect893@passmail.net>"
 license = "MIT"
 url = "https://pypi.org/project/dbus-fast"
 source = f"https://github.com/Bluetooth-Devices/dbus-fast/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0973bedcd19c09fafa1dcb520ce99a3eae831fdb344ad72c2f705f2947f4e4fb"
+sha256 = "084e32b1eea578ca2c891470a75a36b1f9017a879b3bccc03f74c602e4fe0a27"
 
 
 def post_install(self):
