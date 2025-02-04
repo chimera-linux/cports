cd415abea3122190c92a6c65085f3c136a2ce42d
ttyyls <contact@behri.org>
2025-02-04 14:56:12 +0100

    user/sad: update to 0.4.32

--- user/sad/template.py
+++ user/sad/template.py
@@ -1,6 +1,6 @@
 pkgname = "sad"
-pkgver = "0.4.31"
-pkgrel = 1
+pkgver = "0.4.32"
+pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "python"]
 makedepends = ["rust-std"]
@@ -9,7 +9,7 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "MIT"
 url = "https://github.com/ms-jpq/sad"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "c717e54798e21356340271e32e43de5b05ba064ae64edf639fede27b1ed15ceb"
+sha256 = "a67902b9edb287861668ee3e39482c17b41c60e244ece62b3f8016250286294f"
 
 
 def post_install(self):
