88f0d04d868c701607b0205aa2e7aa4182a3a5f7
q66 <q66@chimera-linux.org>
2025-02-05 12:02:40 +0100

    main/fcix5-gtk: fix subpackages

--- main/fcitx5-gtk/template.py
+++ main/fcitx5-gtk/template.py
@@ -1,6 +1,6 @@
 pkgname = "fcitx5-gtk"
 pkgver = "5.1.3"
-pkgrel = 1
+pkgrel = 2
 build_style = "cmake"
 configure_args = [
     "-DENABLE_GTK2_IM_MODULE=OFF",
@@ -36,10 +36,12 @@ def _(self):
     return self.default_devel()
 
 
-@subpackage("fcitx5-gtk3")
+@subpackage("fcitx5-gtk-gtk3")
 def _(self):
     self.subdesc = "GTK+3 variant"
     self.install_if = [self.parent, "gtk+3"]
+    # transitional
+    self.provides = [self.with_pkgver("fcitx5-gtk3")]
 
     return [
         "usr/bin/fcitx5-gtk3-immodule-probing",
@@ -47,10 +49,12 @@ def _(self):
     ]
 
 
-@subpackage("fcitx5-gtk4")
+@subpackage("fcitx5-gtk-gtk4")
 def _(self):
     self.subdesc = "GTK4 variant"
     self.install_if = [self.parent, "gtk4"]
+    # transitional
+    self.provides = [self.with_pkgver("fcitx5-gtk4")]
 
     return [
         "usr/bin/fcitx5-gtk4-immodule-probing",
