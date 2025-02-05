9fc83be2fd083274cf64587cc3d14eb1e151d9b4
q66 <q66@chimera-linux.org>
2025-02-05 23:12:02 +0100

    main/xfdesktop: rename xfce4-backgrounds

--- main/xfdesktop/template.py
+++ main/xfdesktop/template.py
@@ -1,6 +1,6 @@
 pkgname = "xfdesktop"
 pkgver = "4.20.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = [
     "--with-default-backdrop-filename=/usr/share/backgrounds/chimera/bg-l.svg"
@@ -36,10 +36,12 @@ source = f"$(XFCE_SITE)/xfce/xfdesktop/{pkgver[:-2]}/xfdesktop-{pkgver}.tar.bz2"
 sha256 = "227041ba80c7f3eb9c99dec817f1132b35d8aec7a4335703f61ba1735cd65632"
 
 
-@subpackage("xfce4-backgrounds")
+@subpackage("xfdesktop-backgrounds")
 def _(self):
     self.pkgdesc = "Backgrounds for the Xfce desktop"
     self.install_if = [self.parent]
     self.license = "CC-BY-SA-4.0"
+    # transitional
+    self.provides = [self.with_pkgver("xfce4-backgrounds")]
 
     return ["usr/share/backgrounds"]
