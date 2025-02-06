cc40176dbf93a902402126019f479ec375c88c59
q66 <q66@chimera-linux.org>
2025-02-06 11:56:48 +0100

    main/chromium: update to 133.0.6943.53

--- main/chromium/template.py
+++ main/chromium/template.py
@@ -1,6 +1,6 @@
 pkgname = "chromium"
 # https://chromiumdash.appspot.com/releases?platform=Linux
-pkgver = "132.0.6834.159"
+pkgver = "133.0.6943.53"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "x86_64"]
 configure_args = [
@@ -139,7 +139,7 @@ license = "BSD-3-Clause"
 url = "https://www.chromium.org"
 # from gentoo
 source = f"https://chromium-tarballs.syd1.digitaloceanspaces.com/chromium-{pkgver}-linux.tar.xz"
-sha256 = "3c8ff6af8f639f497f92d7ac56c23ff1bb7652a6101a97bd4215313f43340f9e"
+sha256 = "dbd06314054f738ef6aa4e0f5d7e765602b90df9820e62a21efe068a869de7a3"
 debug_level = 1
 tool_flags = {
     "CFLAGS": [
@@ -229,7 +229,6 @@ def configure(self):
         "harfbuzz-ng",
         "highway",
         "icu",
-        "libevent",
         "libjpeg",
         "libpng",
         "libsecret",
