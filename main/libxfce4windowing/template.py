c6339c40a8a1fde55b1c04331fc74619b2a22f76
triallax <triallax@tutanota.com>
2025-02-07 11:32:34 +0000

    main/libxfce4windowing: update to 4.20.2

--- main/libxfce4windowing/template.py
+++ main/libxfce4windowing/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxfce4windowing"
-pkgver = "4.20.0"
+pkgver = "4.20.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-tests"]
@@ -27,7 +27,7 @@ maintainer = "triallax <triallax@tutanota.com>"
 license = "LGPL-2.1-or-later"
 url = "https://docs.xfce.org/xfce/libxfce4windowing/start"
 source = f"$(XFCE_SITE)/xfce/libxfce4windowing/{pkgver[:-2]}/libxfce4windowing-{pkgver}.tar.bz2"
-sha256 = "56f29b1d79606fb00a12c83ef4ece12877d2b22bf1acaaff89537fbe8e939f68"
+sha256 = "0b9b95aee8b868a2953920c2feafc026672ad19584976f19e89119e93ab1abc8"
 options = ["!cross"]
 
 
