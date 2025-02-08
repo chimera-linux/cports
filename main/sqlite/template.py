de2e57260ad34d2eac7831f8d8a7c4a52e615b5a
q66 <q66@chimera-linux.org>
2025-02-08 19:59:43 +0100

    main/sqlite: update to 3.49.0

--- main/sqlite/template.py
+++ main/sqlite/template.py
@@ -1,16 +1,19 @@
 pkgname = "sqlite"
-pkgver = "3.48.0"
-_amalg = "3480000"
+pkgver = "3.49.0"
+_amalg = "3490000"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
-    "--enable-dynamic-extensions",
     "--enable-fts3",
     "--enable-fts4",
     "--enable-fts5",
+    "--enable-shared",
     "--enable-threadsafe",
+    "--editline",
+    "--soname=legacy",
 ]
 configure_gen = []
+make_dir = "."
 hostmakedepends = ["pkgconf"]
 makedepends = ["libedit-devel", "zlib-ng-compat-devel"]
 pkgdesc = "SQL Database Engine in a C library"
@@ -18,8 +21,9 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "blessing"
 url = "https://sqlite.org"
 source = f"https://sqlite.org/2025/sqlite-autoconf-{_amalg}.tar.gz"
-sha256 = "ac992f7fca3989de7ed1fe99c16363f848794c8c32a158dafd4eb927a2e02fd5"
-options = ["!parallel"]
+sha256 = "4d8bfa0b55e36951f6e5a9fb8c99f3b58990ab785c57b4f84f37d163a0672759"
+# no tests
+options = ["!parallel", "!check"]
 
 _cflags = [
     "-DHAVE_FDATASYNC",
