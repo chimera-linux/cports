66c21e6d002ee5709af8c614bd1cc398a5585fad
Isaac Freund <mail@isaacfreund.com>
2025-02-09 18:01:50 +0100

    main/lxqt-runner: update to 2.1.2

--- main/lxqt-runner/template.py
+++ main/lxqt-runner/template.py
@@ -1,5 +1,5 @@
 pkgname = "lxqt-runner"
-pkgver = "2.1.1"
+pkgver = "2.1.2"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -23,4 +23,4 @@ maintainer = "Isaac Freund <mail@isaacfreund.com>"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/lxqt-runner"
 source = f"{url}/releases/download/{pkgver}/lxqt-runner-{pkgver}.tar.xz"
-sha256 = "bc764f027bcd3e40d5fa3c28b31db975aa8c1f5f75218384c5b1b3b37f8cbb5e"
+sha256 = "9d95ab07e57102c696b72be772c169c6ae260b04177cbc336e75e7578a4b2de2"
