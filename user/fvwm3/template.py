2ae2e298b30857815a17c49d48ae01e2cd81c3f3
q66 <q66@chimera-linux.org>
2025-02-09 20:47:18 +0100

    user/fvwm3: update to 1.1.2

--- user/fvwm3/template.py
+++ user/fvwm3/template.py
@@ -1,5 +1,5 @@
 pkgname = "fvwm3"
-pkgver = "1.1.1"
+pkgver = "1.1.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-mandoc"]
@@ -24,4 +24,4 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://github.com/fvwmorg/fvwm3"
 source = f"{url}/releases/download/{pkgver}/fvwm3-{pkgver}.tar.gz"
-sha256 = "ecdcf1099bac3e7b1198bdee7542150d378ee333a644c49095e974d819b8d1c3"
+sha256 = "fde449c21678e059d16278da3ac69f3786aed96cac90962163e72bf59e840421"
