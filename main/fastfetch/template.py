c8e252ea426586ad02d733b2df246440f9f63e82
Jami Kettunen <jami.kettunen@protonmail.com>
2025-02-10 11:05:07 +0200

    main/fastfetch: update to 2.36.0

--- main/fastfetch/template.py
+++ main/fastfetch/template.py
@@ -1,8 +1,12 @@
 pkgname = "fastfetch"
-pkgver = "2.34.1"
-pkgrel = 1
+pkgver = "2.36.0"
+pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DBUILD_TESTS=ON", "-DENABLE_SYSTEM_YYJSON=ON"]
+configure_args = [
+    "-DBUILD_FLASHFETCH=OFF",
+    "-DBUILD_TESTS=ON",
+    "-DENABLE_SYSTEM_YYJSON=ON",
+]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = [
     "chafa-devel",
@@ -27,7 +31,7 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "MIT"
 url = "https://github.com/fastfetch-cli/fastfetch"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "856920a6484a324088bbbcaf608555c74168078fd34cdbde4d3e4af88bc5cb5f"
+sha256 = "bdbe5c7672aeacfec15ec7539f718e666c5206b1a3de9bbf8bd1b3d3c9c997e7"
 tool_flags = {"CFLAGS": ["-DNDEBUG"]}
 # CFI: dies immediately (ffPlatformPathAddHome at FFlist.c:31:12)
 hardening = ["vis", "!cfi"]
