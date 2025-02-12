2e1854e1e45a4236863199667ab533f8061adf45
q66 <q66@chimera-linux.org>
2025-02-12 01:53:20 +0100

    main/mdds: update to 3.0.0

--- main/mdds/template.py
+++ main/mdds/template.py
@@ -1,6 +1,6 @@
 pkgname = "mdds"
-pkgver = "2.1.1"
-pkgrel = 5
+pkgver = "3.0.0"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "slibtool"]
 checkdepends = ["boost-devel"]
@@ -8,8 +8,8 @@ pkgdesc = "Collection of multi-dimensional data structures"
 maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://gitlab.com/mdds/mdds"
-source = f"http://kohei.us/files/mdds/src/mdds-{pkgver}.tar.bz2"
-sha256 = "8a3767f0a60c53261b5ebbaa717381446813aef8cf28fe9d0ea1371123bbe3f1"
+source = f"https://gitlab.com/api/v4/projects/mdds%2Fmdds/packages/generic/source/{pkgver}/mdds-{pkgver}.tar.gz"
+sha256 = "e8660f5510f099af1cc9bbcd2c1f12493b72b24f82616d70f9d71ab6ce1258ec"
 
 
 def post_install(self):
