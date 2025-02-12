a1fc0cad49428d94300002b518fed7917366e847
q66 <q66@chimera-linux.org>
2025-02-12 01:54:35 +0100

    main/ixion: update to 0.20.0

--- main/ixion/template.py
+++ main/ixion/template.py
@@ -1,12 +1,12 @@
 pkgname = "ixion"
-pkgver = "0.19.0"
-pkgrel = 5
+pkgver = "0.20.0"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
     "automake",
+    "libtool",
     "pkgconf",
     "python",
-    "slibtool",
 ]
 makedepends = ["boost-devel", "python-devel", "mdds"]
 checkdepends = ["bash"]
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MPL-2.0"
 url = "https://gitlab.com/ixion/ixion"
 source = f"{url}/-/archive/{pkgver}/ixion-{pkgver}.tar.gz"
-sha256 = "b5b67ea7fc631a0fda4fff3123f0cc2e3831849bdd8fbae8443be0766a77f243"
+sha256 = "4a6c2c480ad40b706ecf459dfca03f39351e12b48911c7c4803b75c823a1bcb1"
 
 
 @subpackage("ixion-libs")
