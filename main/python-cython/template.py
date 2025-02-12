d362862bc808d610933c860cee1e25dec085bee5
triallax <triallax@tutanota.com>
2025-02-12 15:26:02 +0000

    main/python-cython: update to 3.0.12

--- main/python-cython/template.py
+++ main/python-cython/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-cython"
-pkgver = "3.0.11"
-pkgrel = 1
+pkgver = "3.0.12"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 makedepends = ["python-devel"]
 depends = ["python"]
@@ -15,6 +14,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://cython.org"
 source = f"$(PYPI_SITE)/c/cython/cython-{pkgver}.tar.gz"
-sha256 = "7146dd2af8682b4ca61331851e6aebce9fe5158e75300343f80c07ca80b1faff"
+sha256 = "b988bb297ce76c671e28c97d017b95411010f7c77fa6623dd0bb47eed1aee1bc"
 # check: flaky tests
 options = ["!check"]
