2e931ece52f261a8624b129917302c692a27cc98
q66 <q66@chimera-linux.org>
2025-02-12 13:05:20 +0100

    main/intel-media-driver: update to 25.1.1

--- main/intel-media-driver/template.py
+++ main/intel-media-driver/template.py
@@ -1,5 +1,5 @@
 pkgname = "intel-media-driver"
-pkgver = "25.1.0"
+pkgver = "25.1.1"
 pkgrel = 0
 # doesn't build elsewhere
 archs = ["x86_64"]
@@ -25,7 +25,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://github.com/intel/media-driver"
 source = f"{url}/archive/refs/tags/intel-media-{pkgver}.tar.gz"
-sha256 = "94178add6dd545bd14747c063863d87d7b4f3cbcaab5d660d2ae5310cbfe4553"
+sha256 = "db5788341789099cbfce212f64f428eb79739f578eabfcc6056f03f3029d3708"
 # INT: crashes during certain vaapi decode (twitch.tv?)
 hardening = ["vis", "!cfi", "!int"]
 
