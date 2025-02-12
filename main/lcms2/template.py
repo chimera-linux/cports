a04e7a86b6adebfab86ae3ec11745588f3cb9b56
triallax <triallax@tutanota.com>
2025-02-12 15:30:42 +0000

    main/lcms2: update to 2.17

--- main/lcms2/template.py
+++ main/lcms2/template.py
@@ -1,5 +1,5 @@
 pkgname = "lcms2"
-pkgver = "2.16"
+pkgver = "2.17"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -14,7 +14,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://littlecms.com"
 source = f"https://github.com/mm2/Little-CMS/releases/download/lcms{pkgver}/lcms2-{pkgver}.tar.gz"
-sha256 = "d873d34ad8b9b4cea010631f1a6228d2087475e4dc5e763eb81acc23d9d45a51"
+sha256 = "d11af569e42a1baa1650d20ad61d12e41af4fead4aa7964a01f93b08b53ab074"
 hardening = ["vis", "!cfi"]
 
 
