911ac9f69786e627e3b355d8d671835255126241
triallax <triallax@tutanota.com>
2025-02-12 15:26:44 +0000

    main/python-pip: update to 25.0.1

--- main/python-pip/template.py
+++ main/python-pip/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pip"
-pkgver = "25.0"
+pkgver = "25.0.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://pip.pypa.io"
 source = f"$(PYPI_SITE)/p/pip/pip-{pkgver}.tar.gz"
-sha256 = "8e0a97f7b4c47ae4a494560da84775e9e2f671d415d8d828e052efefb206b30b"
+sha256 = "88f96547ea48b940a3a385494e181e29fb8637898f88d88737c5049780f196ea"
 # unpackaged dependencies
 options = ["!check"]
 
