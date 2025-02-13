9d5db0f077cb674d036a7bce5387b9c9714e5a5a
breakgimme <adam@plock.com>
2025-02-13 23:43:00 +0100

    user/hcloud: new package

---
+++ user/hcloud/template.py
@@ -0,0 +1,33 @@
+pkgname = "hcloud"
+pkgver = "1.50.0"
+pkgrel = 0
+build_style = "go"
+make_build_args = [
+    "-ldflags="
+    + f" -X github.com/hetznercloud/cli/internal/version.version={pkgver}"
+    + " -X github.com/hetznercloud/cli/internal/version.versionPrerelease=",
+    "./cmd/hcloud",
+]
+hostmakedepends = ["go"]
+pkgdesc = "Command-line interface for Hetzner Cloud"
+maintainer = "breakgimme <adam@plock.com>"
+license = "MIT"
+url = "https://github.com/hetznercloud/cli"
+source = (
+    f"https://github.com/hetznercloud/cli/archive/refs/tags/v{pkgver}.tar.gz"
+)
+sha256 = "61fb1823a176ec77c102c30b3a7de50c67df7eda6dd8a3bf1f17c882e8c78011"
+# generates completions with host binary
+options = ["!cross"]
+
+
+def post_build(self):
+    for shell in ["bash", "fish", "zsh"]:
+        with open(self.cwd / f"hcloud.{shell}", "w") as f:
+            self.do(f"{self.make_dir}/hcloud", "completion", shell, stdout=f)
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+    for shell in ["bash", "fish", "zsh"]:
+        self.install_completion(f"hcloud.{shell}", shell)
