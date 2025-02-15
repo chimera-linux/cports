eca8f5cbfd42ddeba42dc9bdc13a4968e1f98727
ttyyls <contact@behri.org>
2025-02-15 13:56:12 +0100

    main/luanti: update to 5.11.0

--- main/luanti/template.py
+++ main/luanti/template.py
@@ -1,5 +1,5 @@
 pkgname = "luanti"
-pkgver = "5.10.0"
+pkgver = "5.11.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -48,7 +48,7 @@ url = "https://www.luanti.org"
 source = (
     f"https://github.com/minetest/minetest/archive/refs/tags/{pkgver}.tar.gz"
 )
-sha256 = "2a3161c04e7389608006f01280eda30507f8bacfa1d6b64c2af1b820a62d2677"
+sha256 = "70e531d0776988ce6e579ea5490fdf6be3e349a4ade5281f5111aa4fdd8ee510"
 tool_flags = {"CFLAGS": ["-DNDEBUG"], "CXXFLAGS": ["-DNDEBUG"]}
 hardening = ["!int"]
 # see below
