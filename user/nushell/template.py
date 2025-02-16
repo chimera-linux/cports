2052882b14178db0b1d3088c3a05bcb1c88a3db1
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-02-16 17:19:45 +0100

    user/nushell: update to 0.102.0

--- user/nushell/template.py
+++ user/nushell/template.py
@@ -1,8 +1,14 @@
 pkgname = "nushell"
-pkgver = "0.101.0"
-pkgrel = 1
+pkgver = "0.102.0"
+pkgrel = 0
 build_style = "cargo"
 prepare_after_patch = True
+make_check_args = [
+    "--",
+    "--skip=shell::environment::env::env_shlvl_in_exec_repl",
+    "--skip=shell::environment::env::env_shlvl_in_repl",
+    "--skip=shell::environment::env::path_is_a_list_in_repl",
+]
 hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = [
     "libgit2-devel",
@@ -16,7 +22,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "MIT"
 url = "https://www.nushell.sh"
 source = f"https://github.com/nushell/nushell/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "43e4a123e86f0fb4754e40d0e2962b69a04f8c2d58470f47cb9be81daabab347"
+sha256 = "97faa3626be944d83b26c43d0b5c9e1ae14dfc55ef4465ac00fc1c64dceda7ce"
 
 
 def post_install(self):
