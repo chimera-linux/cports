cc6adcb4731d074e695bb6eeb6031adcc42d2a3e
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-02-16 16:15:13 +0100

    user/television: update to 0.10.6

--- user/television/template.py
+++ user/television/template.py
@@ -1,5 +1,5 @@
 pkgname = "television"
-pkgver = "0.10.0"
+pkgver = "0.10.6"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -10,7 +10,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "MIT"
 url = "https://github.com/alexpasmantier/television"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "62dbc15b4109be90ef7bda6993523223aa6f89f39333ea8d35d515edc4ccb98d"
+sha256 = "d316ddd82aa725e381f238aec190423c6dd1569b25697a3e318fef990205ade3"
 
 if self.profile().wordsize == 32:
     broken = "needs atomic64"
