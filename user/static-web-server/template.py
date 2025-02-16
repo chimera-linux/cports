cce53964ca7700b0d528ff052601309557ffb766
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-02-16 16:19:22 +0100

    user/static-web-server: update to 2.36.0

--- user/static-web-server/template.py
+++ user/static-web-server/template.py
@@ -1,6 +1,6 @@
 pkgname = "static-web-server"
-pkgver = "2.35.0"
-pkgrel = 1
+pkgver = "2.36.0"
+pkgrel = 0
 build_style = "cargo"
 # We patch Cargo.toml and Cargo.lock
 prepare_after_patch = True
@@ -11,7 +11,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "MIT OR Apache-2.0"
 url = "https://github.com/static-web-server/static-web-server"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "adf260f0aa3ccc18955f9f68ce11356c8d0e3fbb0d9b9446b137430427dafb3b"
+sha256 = "bb99fd25835050e9572ea4589f66b94a64d1724712a2f4881ab35f29d1d8f2a9"
 # generates completions using host binary
 options = ["!cross"]
 
