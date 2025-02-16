d33a2a1b9c153811d97785379c3bed63c08078e3
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-02-16 16:31:18 +0100

    user/sequoia-sq: update to 1.2.0

--- user/sequoia-sq/template.py
+++ user/sequoia-sq/template.py
@@ -1,5 +1,5 @@
 pkgname = "sequoia-sq"
-pkgver = "1.1.0"
+pkgver = "1.2.0"
 pkgrel = 0
 build_style = "cargo"
 make_build_env = {"ASSET_OUT_DIR": "assets"}
@@ -17,7 +17,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "LGPL-2.0-or-later"
 url = "https://sequoia-pgp.org/projects#sq"
 source = f"https://gitlab.com/sequoia-pgp/sequoia-sq/-/archive/v{pkgver}/sequoia-sq-v{pkgver}.tar.gz"
-sha256 = "3316902e1f52e8f01829b72014bda006ad9712ec3802703d395dbc6dbf50cb9d"
+sha256 = "7dc01df61daed42a9db3f8c6056070a450875ea65685e174a402b7729a7d8c5e"
 options = ["!cross"]
 
 
