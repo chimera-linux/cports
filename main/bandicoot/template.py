2b34707a80110c5df54ef29f00269b12016b46c6
q66 <q66@chimera-linux.org>
2025-02-16 23:23:35 +0100

    main/bandicoot: new package
    It works but is too early to be enabled by default, so provided
    mostly for testing for now.
    
    Install + enable service + reboot and enjoy autocaptured and
    compressed crash dumps.

---
+++ main/bandicoot/template.py
@@ -0,0 +1,19 @@
+pkgname = "bandicoot"
+pkgver = "0_git20250216"
+pkgrel = 0
+_gitrev = "84fccd824c32d4cea26161f10b52bf5ca324d5b1"
+build_style = "meson"
+configure_args = ["--libexecdir=/usr/lib"]  # XXX libexecdir
+hostmakedepends = ["meson", "pkgconf"]
+makedepends = ["linux-headers", "zstd-devel"]
+pkgdesc = "Crash dump handler"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "BSD-2-Clause"
+url = "https://github.com/chimera-linux/bandicoot"
+source = f"{url}/archive/{_gitrev}.tar.gz"
+sha256 = "599b6f69c8cbab835b59811298824e26f992d5989e9ea965fc9953b936d286e2"
+
+
+def post_install(self):
+    self.install_service(self.files_path / "bandicootd")
+    self.install_license("COPYING.md")
