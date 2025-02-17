f09b8b78d6b00ec1958dbcede4ffe1aee86b982b
q66 <q66@chimera-linux.org>
2025-02-18 00:45:49 +0100

    user/sysstat: move from main, update to 12.7.7

--- user/sysstat/template.py
+++ user/sysstat/template.py
@@ -1,5 +1,5 @@
 pkgname = "sysstat"
-pkgver = "12.7.6"
+pkgver = "12.7.7"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-compress-manpg"]
@@ -13,7 +13,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://sysstat.github.io"
 source = f"https://sysstat.github.io/sysstat-packages/sysstat-{pkgver}.tar.xz"
-sha256 = "474b2bbc89e47b22dc8e4832cc2c555e7fb52f1271b7913434290986a62b71f7"
+sha256 = "79bddfca14130797c02aa4d819528aaa243c879e5bbd1c404cd43c9953a8cdf9"
 hardening = ["vis", "cfi"]
 # dunno how to run these
 options = ["!check"]
