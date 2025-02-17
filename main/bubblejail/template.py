8f3eea3ee731d65c339d932cef157e48aa4ccbb0
ttyyls <contact@behri.org>
2025-02-17 08:05:50 +0100

    main/bubblejail: update to 0.9.5

--- main/bubblejail/template.py
+++ main/bubblejail/template.py
@@ -1,5 +1,5 @@
 pkgname = "bubblejail"
-pkgver = "0.9.4.1"
+pkgver = "0.9.5"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -27,4 +27,4 @@ maintainer = "ttyyls <contact@behri.org>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/igo95862/bubblejail"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "c86388bce72d3e7a8b973ba4f38ecdd8fa04f02e80e1cc8bfeeea9e560192efd"
+sha256 = "cafaca440bf40e7069978a9f9e2c3c3199d01b3c9fa1af080f23cf072b06d188"
