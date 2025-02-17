1d026967327e1fc7b27f98a80cf2b850822f022d
q66 <q66@chimera-linux.org>
2025-02-18 00:42:01 +0100

    main/python-cryptography-vectors: update to 44.0.1

--- main/python-cryptography-vectors/template.py
+++ main/python-cryptography-vectors/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-cryptography-vectors"
-pkgver = "44.0.0"
+pkgver = "44.0.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,7 +15,7 @@ url = "https://github.com/pyca/cryptography"
 source = (
     f"$(PYPI_SITE)/c/cryptography_vectors/cryptography_vectors-{pkgver}.tar.gz"
 )
-sha256 = "106228766c45b9a5ffd1a8b3e254f0560c821da79d5cd7fd113a17e3753580ab"
+sha256 = "5a9866b14465dcfaf12bcdfbc3392987bb559f37ac8b8a4c9b6359be7a3d7ea0"
 # vectors for python-cryptography tests
 options = ["!check"]
 
