c9f977ecb5a484dbd3deec58b1d3e4f1c2abdf92
q66 <q66@chimera-linux.org>
2025-02-18 00:52:01 +0100

    user/lf: update to 34

--- user/lf/template.py
+++ user/lf/template.py
@@ -1,6 +1,6 @@
 pkgname = "lf"
-pkgver = "33"
-pkgrel = 2
+pkgver = "34"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Terminal file manager"
@@ -8,7 +8,7 @@ maintainer = "Subhaditya Nath <sn03.general@gmail.com>"
 license = "MIT"
 url = "https://github.com/gokcehan/lf"
 source = f"{url}/archive/refs/tags/r{pkgver}.tar.gz"
-sha256 = "045565197a9c12a14514b85c153dae4ee1bcd3b4313d60aec5004239d8d785a0"
+sha256 = "9c78735fa88c0b77664d7de41e7edbbca99ace5410f522530307244a09839263"
 
 
 def post_install(self):
