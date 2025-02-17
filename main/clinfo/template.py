0be274484ddeacb9329a3af0ebb2a290abe8dc0f
q66 <q66@chimera-linux.org>
2025-02-18 00:53:37 +0100

    main/clinfo: update to 3.0.25.02.14

--- main/clinfo/template.py
+++ main/clinfo/template.py
@@ -1,5 +1,5 @@
 pkgname = "clinfo"
-pkgver = "3.0.23.01.25"
+pkgver = "3.0.25.02.14"
 pkgrel = 0
 build_style = "makefile"
 makedepends = ["ocl-icd-devel"]
@@ -8,7 +8,7 @@ maintainer = "eater <=@eater.me>"
 license = "CC0-1.0"
 url = "https://github.com/Oblomov/clinfo"
 source = f"https://github.com/Oblomov/clinfo/archive/{pkgver}.tar.gz"
-sha256 = "6dcdada6c115873db78c7ffc62b9fc1ee7a2d08854a3bccea396df312e7331e3"
+sha256 = "48b77dc33315e6f760791a2984f98ea4bff28504ff37d460d8291585f49fcd3a"
 # no tests available
 options = ["!check"]
 
