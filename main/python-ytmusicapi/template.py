ce526a834a6adcb379cf816697297902258604e3
q66 <q66@chimera-linux.org>
2025-02-18 00:44:18 +0100

    main/python-ytmusicapi: update to 1.10.1

--- main/python-ytmusicapi/template.py
+++ main/python-ytmusicapi/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-ytmusicapi"
-pkgver = "1.10.0"
+pkgver = "1.10.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,7 +14,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/sigma67/ytmusicapi"
 source = f"$(PYPI_SITE)/y/ytmusicapi/ytmusicapi-{pkgver}.tar.gz"
-sha256 = "7a5492b0b9b053f04e93e79fe0afaee46c2d47d9449450cbde4a5b079c7f5edf"
+sha256 = "812685451916e76bf47b96f61ef5d4f4db408ea154a44d0779757539f62d69f9"
 # like all tests need net
 options = ["!check"]
 
