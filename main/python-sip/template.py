4e67bf9a914457afef3d34155dd68bd8a4a1c5ca
q66 <q66@chimera-linux.org>
2025-02-18 00:43:24 +0100

    main/python-sip: update to 6.10.0

--- main/python-sip/template.py
+++ main/python-sip/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-sip"
-pkgver = "6.9.1"
+pkgver = "6.10.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -17,7 +17,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "custom:sip"
 url = "https://github.com/Python-SIP/sip"
 source = f"$(PYPI_SITE)/s/sip/sip-{pkgver}.tar.gz"
-sha256 = "7904be5190d7879952563b78a3af0e58fa27d9525af7f53f93eac7a83b433e7b"
+sha256 = "fa0515697d4c98dbe04d9e898d816de1427e5b9ae5d0e152169109fd21f5d29c"
 # no tests
 options = ["!check"]
 
