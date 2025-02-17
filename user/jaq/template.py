6e03fe592c6a7f993c333dc04fff1c8aa6643844
Wesley Moore <wes@wezm.net>
2025-02-17 13:55:35 +1000

    user/jaq: update to 2.1.0

--- user/jaq/template.py
+++ user/jaq/template.py
@@ -1,6 +1,6 @@
 pkgname = "jaq"
-pkgver = "2.0.1"
-pkgrel = 1
+pkgver = "2.1.0"
+pkgrel = 0
 build_style = "cargo"
 # disable the default mimalloc feature and just use the system allocator
 make_build_args = ["--no-default-features", "--bin", "jaq"]
@@ -13,7 +13,7 @@ maintainer = "Wesley Moore <wes@wezm.net>"
 license = "MIT"
 url = "https://github.com/01mf02/jaq"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "e554f375500d09813c9a2f4454217b8d12ae3be5dba56bc545b199ae0d4ab72e"
+sha256 = "59cd17e806a4797e28fa42073c6c8a4d6fb40e28efd7a63f3004d1d738d5be93"
 
 
 def install(self):
