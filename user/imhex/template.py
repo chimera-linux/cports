b5f5054291e2cd11f0635f24eaca85f5c71981a2
Erica Z <zerica@callcc.eu>
2025-02-19 09:51:09 +0100

    user/imhex: update to 1.37.3

--- user/imhex/template.py
+++ user/imhex/template.py
@@ -1,5 +1,5 @@
 pkgname = "imhex"
-pkgver = "1.36.2"
+pkgver = "1.37.3"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -26,19 +26,18 @@ makedepends = [
     "capstone-devel",
     # for llvm cmake detection to work
     "clang-tools-extra",
+    "curl-devel",
     "dbus-devel",
     "file-devel",
     "fmt-devel",
     "freetype-devel",
     "glfw-devel",
     "libarchive-devel",
-    "curl-devel",
     "libedit-devel",
     "llvm-devel",
     # LLVMdemangle is static only
     "llvm-devel-static",
     "mbedtls-devel",
-    "mesa-devel",
     "nlohmann-json",
     "xz-devel",
     "yara-devel",
@@ -47,22 +46,20 @@ pkgdesc = "Hex editor for reverse engineers"
 maintainer = "Erica Z <zerica@callcc.eu>"
 license = "GPL-2.0-or-later"
 url = "https://imhex.werwolv.net"
-_plutovg_ver = "0.0.10"
 source = [
     f"https://github.com/WerWolv/ImHex/releases/download/v{pkgver}/Full.Sources.tar.gz>src-{pkgver}.tar.gz",
     f"https://github.com/WerWolv/ImHex-Patterns/archive/refs/tags/ImHex-v{pkgver}.tar.gz",
-    f"https://github.com/sammycage/plutovg/archive/refs/tags/v{_plutovg_ver}.tar.gz",
 ]
 source_paths = [
     ".",
     "ImHex-Patterns",
-    "build/_deps/plutovg-src",
 ]
 sha256 = [
-    "29bed22dae537b83ac4cce905352b4a1c5342862a67cad5575c58ff41f5847f7",
-    "d1555d142347ddf576e5c0b1a19050ae168e8d174b206d269a9e84e07d2515c9",
-    "639ce28e8f12920ed19d96cd50809973ded904a4e17ba7f9986d6e88431e93c6",
+    "38f670293fcc1e5a06fccf7d35dd8c23a4be8c6ab4a5e3b2900f730d2a8ad2f5",
+    "dc009ad616e73209f8eed1a1a36ebea70e1288a038e756d4ad0c341991fcbfb3",
 ]
+# FIXME: drop with LLVM 20 where std::jthread & std::stop_token are deemed stable
+tool_flags = {"CXXFLAGS": ["-fexperimental-library"]}
 
 if self.profile().wordsize == 32:
     broken = "uses int128"
