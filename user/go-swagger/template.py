04665f518036f6de24181538fcbb433e784b7404
q66 <q66@chimera-linux.org>
2025-02-21 03:52:20 +0100

    user/go-swagger: new package

---
+++ user/go-swagger/template.py
@@ -0,0 +1,14 @@
+pkgname = "go-swagger"
+pkgver = "0.31.0"
+pkgrel = 0
+build_style = "go"
+make_build_args = ["./cmd/swagger"]
+hostmakedepends = ["go"]
+pkgdesc = "Swagger implementation for Go"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "Apache-2.0"
+url = "https://github.com/go-swagger/go-swagger"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "fd8d2a165f12cec1b4cd73392dc91955cc6ae378417c9e105fb67f6b29862c86"
+# needs network
+options = ["!check"]
