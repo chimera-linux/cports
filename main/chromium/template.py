09d3cf0b0797b61599744c96beab34013161c3cf
q66 <q66@chimera-linux.org>
2025-02-21 02:13:29 +0100

    main/chromium: update to 133.0.6943.126

--- main/chromium/template.py
+++ main/chromium/template.py
@@ -1,6 +1,6 @@
 pkgname = "chromium"
 # https://chromiumdash.appspot.com/releases?platform=Linux
-pkgver = "133.0.6943.98"
+pkgver = "133.0.6943.126"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "x86_64"]
 configure_args = [
@@ -139,7 +139,7 @@ license = "BSD-3-Clause"
 url = "https://www.chromium.org"
 # from gentoo
 source = f"https://chromium-tarballs.syd1.digitaloceanspaces.com/chromium-{pkgver}-linux.tar.xz"
-sha256 = "f0c2a519cb7794260ed30c8557f554e0e78edbae733900d89dc6a1d3e096dc96"
+sha256 = "4162194c056300ac4567656b8bae996538438d3553b4951209c887300ffc083d"
 debug_level = 1
 tool_flags = {
     "CFLAGS": [
