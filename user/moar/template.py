8de09e00d8e615726d619a0f9250f5da0c4b4837
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-21 09:45:55 +0000

    user/moar: update to 1.31.3

--- user/moar/template.py
+++ user/moar/template.py
@@ -1,6 +1,6 @@
 pkgname = "moar"
-pkgver = "1.31.2"
-pkgrel = 2
+pkgver = "1.31.3"
+pkgrel = 0
 build_style = "go"
 make_build_args = [f"-ldflags=-X main.versionString=v{pkgver}"]
 hostmakedepends = ["go"]
@@ -9,7 +9,7 @@ maintainer = "Biswapriyo Nath <nathbappai@gmail.com>"
 license = "BSD-2-Clause"
 url = "https://github.com/walles/moar"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f88f244c18ee1a8ba1428bc195f9140d53a551124cc8b54c0c7942ba6433e2d7"
+sha256 = "8fd565853844ce3a5da173d885406fb1cab7d894fc8617617dc4f6a4cfe08cec"
 
 
 def install(self):
