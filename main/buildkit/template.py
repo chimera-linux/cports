8d927057bdd81444b510eead26907c35aed61591
q66 <q66@chimera-linux.org>
2025-02-22 01:20:54 +0100

    main/buildkit: update to 0.20.0

--- main/buildkit/template.py
+++ main/buildkit/template.py
@@ -1,6 +1,6 @@
 pkgname = "buildkit"
-pkgver = "0.19.0"
-pkgrel = 2
+pkgver = "0.20.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/..."]
 hostmakedepends = ["go"]
@@ -10,7 +10,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/moby/buildkit"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "2bc1b4525e7b2beaeba76324c79ce529bf8d2c0ff37b7e2d594acac0a7f88645"
+sha256 = "4b582972aa915fd580481c0cbad8966aa6c18aaaf4503a9a508ed2e6a9c6283b"
 # cannot work in bwrap
 options = ["!check"]
 
