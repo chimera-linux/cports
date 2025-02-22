f691cb3275c75ecd3c43b2cbf8c3ebd8f222ca7b
q66 <q66@chimera-linux.org>
2025-02-22 01:20:35 +0100

    main/blake3: update to 1.6.0

--- main/blake3/template.py
+++ main/blake3/template.py
@@ -1,5 +1,5 @@
 pkgname = "blake3"
-pkgver = "1.5.5"
+pkgver = "1.6.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
@@ -14,7 +14,7 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "CC0-1.0 OR Apache-2.0"
 url = "https://github.com/BLAKE3-team/BLAKE3"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "6feba0750efc1a99a79fb9a495e2628b5cd1603e15f56a06b1d6cb13ac55c618"
+sha256 = "cc6839962144126bc6cc1cde89a50c3bb000b42a93d7e5295b1414d9bdf70c12"
 hardening = ["vis", "cfi"]
 
 
