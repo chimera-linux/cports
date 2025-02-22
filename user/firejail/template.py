f5012bab72a8d2a85e8f0416cecc8840321c2ef6
Herwig Bogaert <herwig.bogaert@posteo.net>
2025-02-22 17:23:50 +0100

    user/firejail: new package

---
+++ user/firejail/template.py
@@ -0,0 +1,27 @@
+pkgname = "firejail"
+pkgver = "0.9.72_git20250221"
+pkgrel = 0
+_commit = "733f9a9c487358ba73b06186ed348f086d8ac839"
+build_style = "gnu_configure"
+configure_args = ["--sysconfdir=/usr/share"]
+configure_gen = []
+make_dir = "."
+make_check_target = "test"
+makedepends = [
+    "gawk",
+    "base-kernel-devel"
+]
+pkgdesc = "Firejail SUID sandboxing tool"
+maintainer = "hbog <herwig.bogaert@posteo.net>"
+license = "GPL-2.0-only"
+url = "https://github.com/netblue30/firejail"
+source = f"{url}/archive/{_commit}.zip"
+sha256 = "2a05497ac0dc2053b5e3aca2ddf5c32ab5176f60be102498bc849b3042225bb3"
+file_modes = {
+    "usr/bin/firejail": ("root", "root", 0o4755)
+}
+hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    self.install_tmpfiles(self.files_path / "tmpfiles.conf")
