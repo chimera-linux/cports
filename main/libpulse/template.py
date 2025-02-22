4d90cecbcf0f77730e8d5fcdc682b2779b93c03e
q66 <q66@chimera-linux.org>
2025-02-22 02:11:16 +0100

    */*: rebuild for orc

--- main/libpulse/template.py
+++ main/libpulse/template.py
@@ -1,6 +1,6 @@
 pkgname = "libpulse"
 pkgver = "17.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
     "-Ddaemon=false",
--- main/libvidstab/template.py
+++ main/libvidstab/template.py
@@ -1,6 +1,6 @@
 pkgname = "libvidstab"
 pkgver = "1.1.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = ["orc-devel"]
