cf28ec27867ede87b568ebec7fd3ccc4e58d9fed
q66 <q66@chimera-linux.org>
2025-02-22 01:35:06 +0100

    main/python-hyperframe: update to 6.1.0

--- main/python-hyperframe/template.py
+++ main/python-hyperframe/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-hyperframe"
-pkgver = "6.0.1"
-pkgrel = 1
+pkgver = "6.1.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/python-hyper/hyperframe"
 source = f"$(PYPI_SITE)/h/hyperframe/hyperframe-{pkgver}.tar.gz"
-sha256 = "ae510046231dc8e9ecb1a6586f63d2347bf4c8905914aa84ba585ae85f28a914"
+sha256 = "f630908a00854a7adeabd6382b43923a4c4cd4b821fcb527e6ab9e15382a3b08"
 # fails to find itself?
 options = ["!check"]
 
