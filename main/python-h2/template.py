f2c07e75f439e8a12337bf5f81982462724fd92d
q66 <q66@chimera-linux.org>
2025-02-22 01:34:29 +0100

    main/python-h2: update to 4.2.0

--- main/python-h2/template.py
+++ main/python-h2/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-h2"
-pkgver = "4.1.0"
-pkgrel = 1
+pkgver = "4.2.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -15,7 +15,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "MIT"
 url = "https://github.com/python-hyper/h2"
 source = f"$(PYPI_SITE)/h/h2/h2-{pkgver}.tar.gz"
-sha256 = "a83aca08fbe7aacb79fec788c9c0bac936343560ed9ec18b82a13a12c28d2abb"
+sha256 = "c8a52129695e88b1a0578d8d2cc6842bbd79128ac685463b887ee278126ad01f"
 # unpackaged checkdepends
 options = ["!check"]
 
