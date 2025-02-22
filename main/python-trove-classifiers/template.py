be223a9e019cc4feda41b6faa6f38efc13f0fe3a
q66 <q66@chimera-linux.org>
2025-02-22 01:36:44 +0100

    main/python-trove-classifiers: update to 2025.2.18.16

--- main/python-trove-classifiers/template.py
+++ main/python-trove-classifiers/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-trove-classifiers"
-pkgver = "2025.1.15.22"
+pkgver = "2025.2.18.16"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,6 +15,6 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "Apache-2.0"
 url = "https://github.com/pypa/trove-classifiers"
 source = f"$(PYPI_SITE)/t/trove-classifiers/trove_classifiers-{pkgver}.tar.gz"
-sha256 = "90af74358d3a01b3532bc7b3c88d8c6a094c2fd50a563d13d9576179326d7ed9"
+sha256 = "b1ee2e1668589217d4edf506743e28b1834da128f8a122bad522c02d837006e1"
 # cycle
 options = ["!check"]
