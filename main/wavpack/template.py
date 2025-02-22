68fe57d45612a1c547eb60242300a257d1ef987d
q66 <q66@chimera-linux.org>
2025-02-22 01:38:36 +0100

    main/wavpack: update to 5.8.1

--- main/wavpack/template.py
+++ main/wavpack/template.py
@@ -1,5 +1,5 @@
 pkgname = "wavpack"
-pkgver = "5.7.0"
+pkgver = "5.8.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "gettext-devel", "libtool", "pkgconf"]
@@ -8,7 +8,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "https://www.wavpack.com"
 source = f"https://github.com/dbry/WavPack/releases/download/{pkgver}/wavpack-{pkgver}.tar.xz"
-sha256 = "e81510fd9ec5f309f58d5de83e9af6c95e267a13753d7e0bbfe7b91273a88bee"
+sha256 = "7322775498602c8850afcfc1ae38f99df4cbcd51386e873d6b0f8047e55c0c26"
 
 
 def post_install(self):
