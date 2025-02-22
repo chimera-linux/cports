8686e7f3864e6a2da80ed41d7fc68e4a87b20ebd
q66 <q66@chimera-linux.org>
2025-02-22 21:04:34 +0100

    main/base-quartzpro64: new package

---
+++ main/base-quartzpro64/template.py
@@ -0,0 +1,22 @@
+pkgname = "base-quartzpro64"
+pkgver = "0.1"
+pkgrel = 1
+archs = ["aarch64"]
+depends = [
+    "firmware-linux-rockchip",
+    "u-boot-quartzpro64-rk3588",
+    "u-boot-menu",
+]
+pkgdesc = "Chimera base package for QuartzPro64"
+maintainer = "q66 <q66@chimera-linux.org>"
+license = "custom:none"
+url = "https://chimera-linux.org"
+
+
+def install(self):
+    # u-boot-menu
+    self.install_file(self.files_path / "u-boot-device", "etc/default")
+    self.install_file(self.files_path / "u-boot-cmdline", "etc/default")
+    # agetty service customization
+    self.install_file(self.files_path / "agetty", "etc/default")
+    self.install_file(self.files_path / "agetty-ttyS2", "etc/default")
