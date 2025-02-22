6e2103488bbfcca93ffd280ffcfac37b5eedc0bc
q66 <q66@chimera-linux.org>
2025-02-22 01:39:51 +0100

    main/zstd: update to 1.5.7

--- main/zstd/template.py
+++ main/zstd/template.py
@@ -1,6 +1,6 @@
 pkgname = "zstd"
-pkgver = "1.5.6"
-pkgrel = 2
+pkgver = "1.5.7"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Db_ndebug=true",
@@ -20,7 +20,7 @@ maintainer = "q66 <q66@chimera-linux.org>"
 license = "BSD-3-Clause"
 url = "http://www.zstd.net"
 source = f"https://github.com/facebook/zstd/releases/download/v{pkgver}/zstd-{pkgver}.tar.gz"
-sha256 = "8c29e06cf42aacc1eafc4077ae2ec6c6fcb96a626157e0593d5e82a34fd403c1"
+sha256 = "eb33e51f49a15e023950cd7825ca74a4a2b43db8354825ac24fc1b7ee09e6fa3"
 compression = "deflate"
 hardening = ["!vis", "!cfi"]
 
