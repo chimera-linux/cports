ed79a97e3a46bfc2a560cab1ab9066278b7bfc80
Aaron John <unrealapex@disroot.org>
2025-02-23 04:59:53 -0800

    user/fnf: new package

---
+++ user/fnf/template.py
@@ -0,0 +1,13 @@
+pkgname = "fnf"
+pkgver = "0.1"
+pkgrel = 0
+build_style = "makefile"
+pkgdesc = "CLI fuzzy finder"
+license = "MIT"
+url = "https://github.com/leo-arch/fnf"
+source = f"{url}/archive/v{pkgver}.tar.gz"
+sha256 = "878eae406a5fdc9e8b88d28c264f772da258ddad8378c50b8e08cbc9ce0df07a"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
