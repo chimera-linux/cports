1465d85c24b938924575a7197fb66846567d023f
ttyyls <contact@behri.org>
2025-02-24 21:56:13 +0000

    user/xmpp-dns: new package

---
+++ user/xmpp-dns/template.py
@@ -0,0 +1,16 @@
+pkgname = "xmpp-dns"
+pkgver = "0.4.5"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go"]
+pkgdesc = "CLI tool to check XMPP SRV records"
+maintainer = "ttyyls <contact@behri.org>"
+license = "BSD-2-Clause"
+url = "https://salsa.debian.org/mdosch/xmpp-dns"
+source = f"{url}/-/archive/v{pkgver}/xmpp-dns-v{pkgver}.tar.gz"
+sha256 = "ce4ee08577f9a84828d234790af85e4031727eaf37900f0fccc4ceeb92616239"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+    self.install_man("man/xmpp-dns.1")
