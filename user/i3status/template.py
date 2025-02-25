3b6dc50b67ccde6105e539e75eb01f3513e4e5ac
shortcircuit <shortcircuitv@gmail.com>
2025-02-25 18:04:48 +0000

    user/i3status: new package
    Signed-off-by: shortcircuit <shortcircuitv@gmail.com>

---
+++ user/i3status/template.py
@@ -0,0 +1,23 @@
+pkgname = "i3status"
+pkgver = "2.15"
+pkgrel = 0
+build_style = "meson"
+configure_args = ["-Dmans=true"]
+hostmakedepends = ["asciidoc", "meson", "perl", "pkgconf", "xmlto"]
+makedepends = [
+    "alsa-lib-devel",
+    "libconfuse-devel",
+    "libnl-devel",
+    "libpulse-devel",
+    "yajl-devel",
+]
+pkgdesc = "Generates status bar to use with i3bar, dzen2 or xmobar"
+license = "BSD-3-Clause"
+url = "https://github.com/i3/i3status"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "25af0dd77a5325c13890d4ee53a9205827a11c8b90f54e8a7fe2654bd0273d4b"
+hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
