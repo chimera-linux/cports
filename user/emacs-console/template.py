2ebac405288f7a63dcde9f40bbb4477a11c6721f
Louis Hampton <louis@ecog.io>
2025-02-25 11:25:46 +0000

    user/emacs-*: move from main, adopt, update to 30.1

--- user/emacs-console/template.py
+++ user/emacs-console/template.py
@@ -1,57 +1,43 @@
-pkgname = "emacs-gtk3"
-pkgver = "29.4"
-pkgrel = 9
+pkgname = "emacs-console"
+pkgver = "30.1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--with-gameuser=:_games",
     "--with-gpm",
     "--with-json",
-    "--with-jpeg",
-    "--with-webp",
-    "--with-x-toolkit=gtk3",
-    "--with-xft",
-    "--without-tiff",
-    "--without-toolkit-scroll-bars",
+    "--without-file-notification",
+    "--without-sound",
+    "--without-x",
 ]
 hostmakedepends = [
     "automake",
     "gawk",
+    "gcc-jit-libs",
     "pkgconf",
     "texinfo",
 ]
 makedepends = [
     "acl-devel",
-    "alsa-lib-devel",
-    "fontconfig-devel",
-    "giflib-devel",
     "glib-devel",
+    "gcc-jit-devel",
     "gmp-devel",
     "gnutls-devel",
-    "gtk+3-devel",
-    "harfbuzz-devel",
     "jansson-devel",
     "lcms2-devel",
-    "libjpeg-turbo-devel",
-    "libpng-devel",
-    "librsvg-devel",
-    "libtiff-devel",
-    "libwebp-devel",
     "libxml2-devel",
-    "libxpm-devel",
     "linux-headers",
     "ncurses-devel",
-    "pango-devel",
-    "sqlite-devel",
     "tree-sitter-devel",
 ]
 provides = [f"emacs={pkgver}"]
-provider_priority = 10
+provider_priority = 0
 pkgdesc = "Extensible, customizable, self-documenting, real-time display editor"
-maintainer = "Orphaned <orphaned@chimera-linux.org>"
+maintainer = "roastveg <louis@hamptonsoftworks.com>"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/emacs/emacs.html"
 source = f"https://ftp.gnu.org/gnu/emacs/emacs-{pkgver}.tar.xz"
-sha256 = "ba897946f94c36600a7e7bb3501d27aa4112d791bfe1445c61ed28550daca235"
+sha256 = "6ccac1ae76e6af93c6de1df175e8eb406767c23da3dd2a16aa67e3124a6f138f"
 # CFI: breaks
 hardening = ["vis", "!cfi"]
 # no tests
