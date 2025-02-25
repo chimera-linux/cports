9ab05cda434a9bbad4cbf8bbda51b2770f2eeba0
q66 <q66@chimera-linux.org>
2025-02-25 01:48:44 +0100

    user/typstyle: broken on 32bit

--- user/typstyle/template.py
+++ user/typstyle/template.py
@@ -11,6 +11,9 @@ url = "https://github.com/Enter-tainer/typstyle"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "2a57d02df94d50e7b0e721ab0d3e6139a1ee892b51a5afb6e1ea8c62f637151f"
 
+if self.profile().wordsize == 32:
+    broken = "needs atomic64"
+
 
 def install(self):
     self.install_bin(f"target/{self.profile().triplet}/release/typstyle")
