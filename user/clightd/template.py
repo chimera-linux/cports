acd9c24e2bab06920e4b39547bbc8169399d6b73
Anthony <w732qq@gmail.com>
2025-02-27 01:40:11 +0300

    user/clightd: update to 5.9

--- user/clightd/template.py
+++ user/clightd/template.py
@@ -1,6 +1,5 @@
 pkgname = "clightd"
-# TODO: Can't build 5.9 yet as it requires libiio, which is missing
-pkgver = "5.8"
+pkgver = "5.9"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -11,26 +10,28 @@ configure_args = [
 ]
 hostmakedepends = [
     "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
     "dbus-devel",
     "elogind-devel",
     "libdrm-devel",
+    "libiio-devel",
     "libjpeg-turbo-devel",
     "libmodule-devel",
     "libxrandr-devel",
     "linux-headers",
-    "ninja",
-    "pkgconf",
     "polkit-devel",
     "udev-devel",
     "wayland-devel",
 ]
 pkgdesc = "Linux bus interface that lets you change screen brightness"
-# , compute captured webcam frames brightness and change screen temperature"
 maintainer = "Anthony <w732qq@gmail.com>"
 license = "GPL-3.0-or-later"
 url = "https://github.com/FedeDP/Clightd"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "89f0906bc2c1dd4f9bd62194499fd156197c211760c4bb1adcb149650f852684"
+sha256 = "76ae865b5eeb721c98c23b1d4d8531b2b6c10b71386d0396b14666b5650f3054"
 
 
 def post_install(self):
