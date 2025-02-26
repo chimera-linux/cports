5e6f9ff4269f413766b5ad2eb5fb24faa6a0d4b0
sewn <sewn@disroot.org>
2025-02-26 10:21:09 +0300

    user/soju: new package

---
+++ user/soju/template.py
@@ -0,0 +1,55 @@
+pkgname = "soju"
+pkgver = "0.8.2"
+_commit = "1f8f8520f0526dfb9814fae7b5cb7251d5ce4710"
+pkgrel = 0
+build_style = "go"
+make_build_args = [
+    "-ldflags="
+    " -X codeberg.org/emersion/soju/config.DefaultPath=/etc/soju/config"
+    " -X codeberg.org/emersion/soju/config.DefaultUnixAdminPath=/run/soju/admin",
+    "./contrib/...",
+    "./cmd/...",
+]
+hostmakedepends = ["go", "scdoc", "libcap-progs"]
+makedepends = ["sqlite-devel"]
+go_build_tags = ["libsqlite3"]
+pkgdesc = "IRC bouncer"
+license = "AGPL-3.0-only"
+url = "https://soju.im"
+source = f"https://codeberg.org/emersion/soju/archive/{_commit}.tar.gz"
+sha256 = "2613de3579143c2f83df2c9b210c306478e2cc684201925ccc3ab20d14076957"
+file_modes = {
+    "usr/bin/soju": ("root", "root", 0o755),
+}
+file_xattrs = {
+    "usr/bin/soju": {"security.capability": "cap_net_bind_service+ep"},
+}
+# no tests
+options = ["!check"]
+
+
+def post_build(self):
+    self.do("make", "doc/soju.1", "doc/sojuctl.1")
+
+
+def install(self):
+    for util in ["migrate-db", "migrate-logs", "znc-import"]:
+        self.install_bin(f"{self.make_dir}/{util}", name=f"soju-{util}")
+    self.install_bin(f"{self.make_dir}/soju*", glob=True)
+    self.install_file("contrib/casemap-logs.sh", "usr/lib/soju", mode=0o755)
+
+    self.install_man("doc/soju.1")
+    self.install_man("doc/sojuctl.1")
+    self.install_tmpfiles(self.files_path / "tmpfiles.conf")
+    self.install_sysusers(self.files_path / "sysusers.conf")
+    self.install_service(self.files_path / "soju")
+    self.install_license("LICENSE")
+
+
+@subpackage("soju-utils")
+def _(self):
+    self.depends = [f"{pkgname}={pkgver}-r{pkgrel}"]
+    return [
+        "usr/bin/soju-*",
+        "usr/lib/soju/casemap-logs.sh",
+    ]
