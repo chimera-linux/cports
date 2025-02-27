3f49495ad5cdb1e926b7a34d14eab115afb7909a
q66 <q66@chimera-linux.org>
2025-02-27 01:51:02 +0100

    user/ncmpc: update to 0.52

--- user/ncmpc/template.py
+++ user/ncmpc/template.py
@@ -1,5 +1,5 @@
 pkgname = "ncmpc"
-pkgver = "0.51"
+pkgver = "0.52"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -27,5 +27,5 @@ maintainer = "Caio Raposo <caioraposo@disroot.org>"
 license = "GPL-2.0-or-later"
 url = "https://www.musicpd.org/clients/ncmpc"
 source = f"https://www.musicpd.org/download/ncmpc/0/ncmpc-{pkgver}.tar.xz"
-sha256 = "e74be00e69bc3ed1268cafcc87274e78dfbde147f2480ab0aad8260881ec7271"
+sha256 = "3af225496fe363a8534a9780fb46ae1bd17baefd80cf4ba7430a19cddd73eb1a"
 hardening = ["vis", "cfi"]
