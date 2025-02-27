9722ab5b7e94647b689eeff67e3651b6f581e2d3
q66 <q66@chimera-linux.org>
2025-02-27 01:45:18 +0100

    main/qalculate-gtk: update to 5.5.1

--- main/qalculate-gtk/template.py
+++ main/qalculate-gtk/template.py
@@ -1,6 +1,6 @@
 pkgname = "qalculate-gtk"
 # match to libqalculate
-pkgver = "5.5.0"
+pkgver = "5.5.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -20,4 +20,4 @@ maintainer = "Orphaned <orphaned@chimera-linux.org>"
 license = "GPL-2.0-or-later"
 url = "https://qalculate.github.io"
 source = f"https://github.com/Qalculate/qalculate-gtk/releases/download/v{pkgver}/qalculate-gtk-{pkgver}.tar.gz"
-sha256 = "89840c16deba524b23512dc6d8d91f74f282c672a2ad001533f4b063d49171dd"
+sha256 = "dcf33e89ec2539c3e0bf9c5aee18b44680f6630b1e02cf23e2e9add6578450c7"
