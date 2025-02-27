1013d76152e9d19d6e85517b59286459e115cc2d
q66 <q66@chimera-linux.org>
2025-02-27 20:30:35 +0100

    user/fish: move from main and update to 4.0.0

---
+++ user/fish-shell/template.py
@@ -0,0 +1,30 @@
+pkgname = "fish-shell"
+pkgver = "4.0.0"
+pkgrel = 0
+build_style = "cmake"
+make_check_target = "fish_run_tests"
+hostmakedepends = ["cargo", "cmake", "ninja", "pkgconf", "gettext"]
+makedepends = ["pcre2-devel", "rust-std"]
+checkdepends = ["python", "procps"]
+pkgdesc = "Friendly interactive command line shell"
+license = "GPL-2.0-only"
+url = "https://fishshell.com"
+source = f"https://github.com/fish-shell/fish-shell/releases/download/{pkgver}/fish-{pkgver}.tar.xz"
+sha256 = "2fda5bd970357064d8d4c896e08285ba59965ca2a8c4829ca8a82bf3b89c69f3"
+
+
+def prepare(self):
+    from cbuild.util import cargo
+
+    cargo.Cargo(self, wrksrc=".").vendor()
+
+
+def init_build(self):
+    from cbuild.util import cargo
+
+    renv = cargo.get_environment(self)
+    self.make_env.update(renv)
+
+
+def post_install(self):
+    self.install_shell("/usr/bin/fish")
