225eda5b81d252b0a197e7326360e8a6702a1e15
ttyyls <contact@behri.org>
2025-02-27 11:27:31 +0000

    user/typst: update to 0.13.0

--- user/typst/template.py
+++ user/typst/template.py
@@ -1,6 +1,6 @@
 pkgname = "typst"
-pkgver = "0.12.0"
-pkgrel = 1
+pkgver = "0.13.0"
+pkgrel = 0
 build_style = "cargo"
 make_build_args = ["-p", "typst-cli"]
 make_build_env = {
@@ -15,7 +15,7 @@ maintainer = "Jan Christian Grünhage <jan.christian@gruenhage.xyz>"
 license = "Apache-2.0"
 url = "https://typst.app"
 source = f"https://github.com/typst/typst/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5e92463965c0cf6aa003a3bacd1c68591ef2dc0db59dcdccb8f7b084836a1266"
+sha256 = "5a7224e32a555ac647ff202667a183b80d35539b685b3ce64bedf5d4e5a1a286"
 # takes forever
 options = ["!check"]
 
