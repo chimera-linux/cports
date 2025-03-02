95c40d4e188c547b11fbd408fa62d2c698110855
q66 <q66@chimera-linux.org>
2025-03-02 21:12:35 +0100

    main/bubblewrap: disable loongarch seccomp test like aarch64

--- main/bubblewrap/template.py
+++ main/bubblewrap/template.py
@@ -12,6 +12,6 @@ source = f"{url}/releases/download/v{pkgver}/bubblewrap-{pkgver}.tar.xz"
 sha256 = "988fd6b232dafa04b8b8198723efeaccdb3c6aa9c1c7936219d5791a8b7a8646"
 hardening = ["vis", "cfi"]
 
-# seccomp tests fail on aarch64 with efault instead of econnrefused for various assertions
-if self.profile().arch != "aarch64":
+# efault instead of econnrefused for various assertions
+if self.profile().arch not in ["aarch64", "loongarch64"]:
     checkdepends += ["python-libseccomp"]
