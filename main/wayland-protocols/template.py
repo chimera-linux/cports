4e037f33718a7c3f80c01abf57b8240337366860
q66 <q66@chimera-linux.org>
2025-03-02 03:08:58 +0100

    main/wayland-protocols: disable tests on loongarch

--- main/wayland-protocols/template.py
+++ main/wayland-protocols/template.py
@@ -9,6 +9,12 @@ license = "MIT"
 url = "https://wayland.freedesktop.org"
 source = f"https://gitlab.freedesktop.org/wayland/wayland-protocols/-/archive/{pkgver}/wayland-protocols-{pkgver}.tar.gz"
 sha256 = "f25b0d00f3c610158b00b57b1b7b6e59c4bfd4d91aed46f24d9eba7acf220788"
+# check conditional
+options = []
+
+if self.profile().arch == "loongarch64":
+    # several pedantic tests complain about symbol not found
+    options += ["!check"]
 
 
 def post_install(self):
