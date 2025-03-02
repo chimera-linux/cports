7b8443f3fe45844218b415e2614a5fddc9fccfa0
q66 <q66@chimera-linux.org>
2025-03-02 21:46:31 +0100

    main/mozjs128: update libc due to loongarch fail

--- main/mozjs128/template.py
+++ main/mozjs128/template.py
@@ -22,8 +22,15 @@ makedepends = [
 pkgdesc = "Mozilla JavaScript interpreter and library, version 128.x"
 license = "MPL-2.0"
 url = "https://www.mozilla.org/firefox"
-source = f"$(MOZILLA_SITE)/firefox/releases/{pkgver}esr/source/firefox-{pkgver}esr.source.tar.xz"
-sha256 = "e0a7bf4eddfa33cbc8797d68ecea8fb4e6404ca197d3b2242cbc8daed03794f7"
+source = [
+    f"$(MOZILLA_SITE)/firefox/releases/{pkgver}esr/source/firefox-{pkgver}esr.source.tar.xz",
+    "https://github.com/rust-lang/libc/archive/refs/tags/0.2.170.tar.gz",
+]
+source_paths = [".", "libc"]
+sha256 = [
+    "e0a7bf4eddfa33cbc8797d68ecea8fb4e6404ca197d3b2242cbc8daed03794f7",
+    "ee5684d57baaec2cc08f5b9edb083627d6f7a9d545f9759acaed78a0575220e9",
+]
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=1048576"]}
 env = {
     "MACH_BUILD_PYTHON_NATIVE_PACKAGE_SOURCE": "system",
@@ -50,6 +57,20 @@ def init_configure(self):
     self.env["RUST_TARGET"] = self.profile().triplet
     # use all the cargo env vars we enforce
     self.env.update(cargo.get_environment(self))
+
+
+def post_extract(self):
+    from cbuild.util import cargo
+
+    self.rm("third_party/rust/libc", recursive=True)
+    self.mv("libc", "third_party/rust")
+
+    cargo.write_vendor_checksum(
+        self,
+        "libc",
+        "875b3680cb2f8f71bdcf9a30f38d48282f5d3c95cbf9b3fa57269bb5d5c06828",
+        vendor_dir="third_party/rust",
+    )
 
 
 def configure(self):
