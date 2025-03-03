274ff5f45cc1a720ea2bacc4aea7bda68340cd36
q66 <q66@chimera-linux.org>
2025-03-03 15:53:33 +0100

    main/python-cffi: add !int

--- main/python-cffi/template.py
+++ main/python-cffi/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-cffi"
 pkgver = "1.17.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "python_pep517"
 hostmakedepends = [
     "libffi8-devel",
@@ -16,6 +16,8 @@ license = "MIT"
 url = "https://cffi.readthedocs.io"
 source = f"$(PYPI_SITE)/c/cffi/cffi-{pkgver}.tar.gz"
 sha256 = "1c39c6016c32bc48dd54561950ebd6836e1670f2ae46128f67cf49e789c52824"
+# tests crash on loongarch64
+hardening = ["!int"]
 
 
 def post_install(self):
