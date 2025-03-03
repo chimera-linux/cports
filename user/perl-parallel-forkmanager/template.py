5b1d2d97e32ae4c9d9dffcc07ab5a34a0fb01bd6
ttyyls <contact@behri.org>
2025-03-03 22:19:04 +0000

    user/perl-parallel-forkmanager: new package

---
+++ user/perl-parallel-forkmanager/template.py
@@ -0,0 +1,16 @@
+pkgname = "perl-parallel-forkmanager"
+pkgver = "2.03"
+pkgrel = 0
+build_style = "perl_module"
+hostmakedepends = ["perl"]
+makedepends = ["perl"]
+depends = ["perl-moo"]
+checkdepends = [
+    *depends,
+    "perl-test-warn",
+]
+pkgdesc = "Parallel processing fork manager"
+license = "Artistic-1.0-Perl"
+url = "https://metacpan.org/pod/Parallel::ForkManager"
+source = f"$(CPAN_SITE)/Parallel/Parallel-ForkManager-{pkgver}.tar.gz"
+sha256 = "c0e0bead458224b9ac5bb32ed2b1fa088963b565521c1bb1a6a3566d522c2e35"
