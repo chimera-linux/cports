6e74613449e8c048ade1ff078a889a28c8c8e306
q66 <q66@chimera-linux.org>
2025-03-05 22:01:00 +0100

    user/tuigreet: bump up libc

--- user/tuigreet/template.py
+++ user/tuigreet/template.py
@@ -12,5 +12,18 @@ source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "14fd1fadeb84040eb31901da2b53a48aa55b0fdaccb36d96fa52ce2d2113667f"
 
 
+def pre_prepare(self):
+    # the version that is in there is busted on loongarch
+    self.do(
+        "cargo",
+        "update",
+        "--package",
+        "libc",
+        "--precise",
+        "0.2.170",
+        allow_network=True,
+    )
+
+
 def post_install(self):
     self.install_tmpfiles(self.files_path / "tmpfiles.conf")
