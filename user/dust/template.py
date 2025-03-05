f7b4c8576dc25a5756aa2d4696e96d144f54931e
q66 <q66@chimera-linux.org>
2025-03-06 00:22:51 +0100

    user/dust: bump up libc + tests may fail on some archs

--- user/dust/template.py
+++ user/dust/template.py
@@ -9,10 +9,29 @@ license = "Apache-2.0"
 url = "https://github.com/bootandy/dust"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "98cae3e4b32514e51fcc1ed07fdbe6929d4b80942925348cc6e57b308d9c4cb0"
+# tests may be disabled
+options = []
 
 
 if self.profile().wordsize == 32:
     broken = "requires atomic64"
+
+if self.profile().arch != "x86_64":
+    # tests will fail on kernels with larger pages due to "different sizes"
+    options += ["!check"]
+
+
+def pre_prepare(self):
+    # the version that is in there is busted on loongarch
+    self.do(
+        "cargo",
+        "update",
+        "--package",
+        "libc",
+        "--precise",
+        "0.2.170",
+        allow_network=True,
+    )
 
 
 def install(self):
