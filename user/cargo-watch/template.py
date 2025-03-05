6703e2559eee7664536cc47ed5590b1a8db2100d
q66 <q66@chimera-linux.org>
2025-03-06 00:35:03 +0100

    user/cargo-watch: broken on loongarch

--- user/cargo-watch/template.py
+++ user/cargo-watch/template.py
@@ -12,6 +12,9 @@ url = "https://github.com/watchexec/cargo-watch"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "1884674d19492727d762da91b9aebc05d29bdb34cdb1903cde36d81edbcc6514"
 
+if self.profile().arch in ["loongarch64"]:
+    broken = "old nix crate, can't update"
+
 
 def post_install(self):
     self.install_man("cargo-watch.1")
