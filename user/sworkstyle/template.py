b578116ed8a4422e3a55343cd5aa9dcb2ffb386b
q66 <q66@chimera-linux.org>
2025-03-05 22:49:24 +0100

    user/sworkstyle: bump up libc

--- user/sworkstyle/template.py
+++ user/sworkstyle/template.py
@@ -11,6 +11,19 @@ source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "cee6b56c85c15d80200a2ccac5522ec65127dad4f44c5c345b64e3cc9ebb9e15"
 
 
+def pre_prepare(self):
+    # the version that is in there is busted on loongarch
+    self.do(
+        "cargo",
+        "update",
+        "--package",
+        "libc",
+        "--precise",
+        "0.2.170",
+        allow_network=True,
+    )
+
+
 def post_install(self):
     self.install_license("LICENSE")
     self.install_service(self.files_path / "sworkstyle.user")
