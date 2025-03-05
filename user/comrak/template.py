9094513c6c0718d4a7da9e9bcd1b71f110030d79
q66 <q66@chimera-linux.org>
2025-03-06 00:30:53 +0100

    user/comrak: broken on loongarch

--- user/comrak/template.py
+++ user/comrak/template.py
@@ -10,6 +10,9 @@ url = "https://github.com/kivikakk/comrak"
 source = f"{url}/archive/v{pkgver}.tar.gz"
 sha256 = "64dc51f2adbf3761548d9f3ab608de874db14d723e8ca6f9fbd88ebf3bff3046"
 
+if self.profile().arch in ["loongarch64"]:
+    broken = "linux-raw-sys does not support, can't bump (semver)"
+
 
 def post_install(self):
     self.install_license("COPYING")
