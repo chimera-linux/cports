6eab3f3a5bd32451523b6b9cda2c62b2711d9183
q66 <q66@chimera-linux.org>
2025-03-06 00:39:22 +0100

    main/grass: bump up libc

--- main/grass/template.py
+++ main/grass/template.py
@@ -11,6 +11,19 @@ source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "577858cce48440d161c6036d83dbfb3c173058f9df297977b13b8646f88a4906"
 
 
+def pre_prepare(self):
+    # the version that is in there is busted on loongarch
+    self.do(
+        "cargo",
+        "update",
+        "--package",
+        "libc",
+        "--precise",
+        "0.2.170",
+        allow_network=True,
+    )
+
+
 def install(self):
     self.install_bin(f"target/{self.profile().triplet}/release/grass")
     self.install_license("LICENSE")
