5b2da0ec3d33345c994cbdc85a73a2a7e43a5440
q66 <q66@chimera-linux.org>
2025-03-05 22:33:37 +0100

    user/tectonic: broken on loongarch

--- user/tectonic/template.py
+++ user/tectonic/template.py
@@ -25,6 +25,9 @@ url = "https://tectonic-typesetting.github.io/en-US"
 source = f"https://github.com/tectonic-typesetting/tectonic/archive/refs/tags/tectonic@{pkgver}.tar.gz"
 sha256 = "3c13de312c4fe39ff905ad17e64a15a3a59d33ab65dacb0a8b9482c57e6bc6aa"
 
+if self.profile().arch in ["loongarch64"]:
+    broken = "outdated nix crate, can't update"
+
 
 def pre_prepare(self):
     # rust 1.80 type inference regression
