04acba800d06bf575b2d8175def1a1e843385511
q66 <q66@chimera-linux.org>
2025-03-07 22:57:43 +0100

    main/inxi: satisfy ruff format

--- main/inxi/template.py
+++ main/inxi/template.py
@@ -5,7 +5,7 @@ depends = ["perl"]
 pkgdesc = "Fully featured CLI system information tool"
 license = "GPL-3.0-or-later"
 url = "https://codeberg.org/smxi/inxi"
-source = f"{url}/archive/{pkgver[:pkgver.rfind('.')]}-{pkgver[pkgver.rfind('.') + 1:]}.tar.gz"
+source = f"{url}/archive/{pkgver[: pkgver.rfind('.')]}-{pkgver[pkgver.rfind('.') + 1 :]}.tar.gz"
 sha256 = "da730f84f4a2ca53bab471860a83995c9d498bb34c2518fbb7ff65ee705e048e"
 
 
