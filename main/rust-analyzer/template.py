5b36b01df59601a9759f4959268b13fddef33cc3
triallax <triallax@tutanota.com>
2025-03-08 10:07:20 +0000

    main/rust-analyzer: update to 2025.03.04

--- main/rust-analyzer/template.py
+++ main/rust-analyzer/template.py
@@ -1,16 +1,20 @@
 pkgname = "rust-analyzer"
-pkgver = "2025.02.24"
+pkgver = "2025.03.04"
+_pver = pkgver.replace(".", "-")
 pkgrel = 0
 build_style = "cargo"
-make_env = {"CARGO_PROFILE_RELEASE_PANIC": "unwind"}
+make_env = {
+    "CARGO_PROFILE_RELEASE_PANIC": "unwind",
+    "CFG_RELEASE": _pver,
+}
 hostmakedepends = ["cargo-auditable"]
 makedepends = ["rust-std"]
 depends = ["rust-src"]
 pkgdesc = "Rust compiler LSP server"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/rust-lang/rust-analyzer"
-source = f"{url}/archive/refs/tags/{pkgver.replace('.', '-')}.tar.gz"
-sha256 = "1d435dc75b0fd6eeaa186e6aef36c14bb31a158fc8adc312a170594741a440ee"
+source = f"{url}/archive/refs/tags/{_pver}.tar.gz"
+sha256 = "ea7dae7da0dc54fecd4d722545fff314cdefb9b8760df1b0d954bc02a42669ca"
 # invokes rustfmt via rustup arg, also take longer to build than the actual
 # build..
 options = ["!check"]
