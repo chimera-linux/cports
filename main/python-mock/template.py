8a36cef15dbe0bbca57df736182a4ab957da7dc3
triallax <triallax@tutanota.com>
2025-03-08 11:13:06 +0000

    main/python-mock: update to 5.2.0

--- main/python-mock/template.py
+++ main/python-mock/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-mock"
-pkgver = "5.1.0"
-pkgrel = 2
+pkgver = "5.2.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python"]
 checkdepends = ["python-pytest"]
@@ -14,7 +13,7 @@ pkgdesc = "Python mock library"
 license = "BSD-2-Clause"
 url = "https://mock.readthedocs.org"
 source = f"$(PYPI_SITE)/m/mock/mock-{pkgver}.tar.gz"
-sha256 = "5e96aad5ccda4718e0a229ed94b2024df75cc2d55575ba5762d31f5767b8767d"
+sha256 = "4e460e818629b4b173f32d08bf30d3af8123afbb8e04bb5707a1fd4799e503f0"
 
 
 def post_install(self):
