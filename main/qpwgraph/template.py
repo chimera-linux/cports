72578666a11c7a130a546694441d497c02ffd4b8
triallax <triallax@tutanota.com>
2025-03-08 11:42:47 +0000

    main/qpwgraph: update to 0.8.2

--- main/qpwgraph/template.py
+++ main/qpwgraph/template.py
@@ -1,5 +1,5 @@
 pkgname = "qpwgraph"
-pkgver = "0.8.1"
+pkgver = "0.8.2"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -17,4 +17,4 @@ pkgdesc = "Pipewire graph manager"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.freedesktop.org/rncbc/qpwgraph"
 source = f"{url}/-/archive/v{pkgver}/qpwgraph-v{pkgver}.tar.gz"
-sha256 = "fdfeb9e087e437ae6d10f02d31bfddd7d5acb7fc7255f167baee8808d99d38f8"
+sha256 = "a4e87d48f701235789c232c793de699e0ca6837af15775461dcc9a202ca2a95b"
