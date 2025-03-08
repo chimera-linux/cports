97710a9a8d9c2981babccf9d02e1f6af9d39f52f
triallax <triallax@tutanota.com>
2025-03-08 08:26:59 +0000

    main/xfce4-whiskermenu-plugin: update to 2.9.2

--- main/xfce4-whiskermenu-plugin/template.py
+++ main/xfce4-whiskermenu-plugin/template.py
@@ -1,5 +1,5 @@
 pkgname = "xfce4-whiskermenu-plugin"
-pkgver = "2.9.0"
+pkgver = "2.9.2"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -23,4 +23,4 @@ pkgdesc = "Xfce application launcher panel plugin"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-whiskermenu-plugin/start"
 source = f"$(XFCE_SITE)/panel-plugins/xfce4-whiskermenu-plugin/{pkgver[:-2]}/xfce4-whiskermenu-plugin-{pkgver}.tar.bz2"
-sha256 = "8892bb9cc6bbaa402288ad7e5ee76a1990cb8476fe35964a41ec8a9794f6f322"
+sha256 = "e2f28c066709bdcfe30236307026a562ec9aed790f9929a4e40aa411a5c7e3de"
