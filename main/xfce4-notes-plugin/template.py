76ca0c115e7e7a963f7c9acc7888129bef47418e
triallax <triallax@tutanota.com>
2025-03-08 08:27:28 +0000

    main/xfce4-notes-plugin: update to 1.11.2

--- main/xfce4-notes-plugin/template.py
+++ main/xfce4-notes-plugin/template.py
@@ -1,5 +1,5 @@
 pkgname = "xfce4-notes-plugin"
-pkgver = "1.11.1"
+pkgver = "1.11.2"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -21,4 +21,4 @@ pkgdesc = "Xfce notes panel plugin"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-notes-plugin/start"
 source = f"$(XFCE_SITE)/panel-plugins/xfce4-notes-plugin/{pkgver[:-2]}/xfce4-notes-plugin-{pkgver}.tar.bz2"
-sha256 = "ef410999c9dafc5cf13fdcfd62ae49728508c0d37510f006971d5291061524aa"
+sha256 = "8301fcd397bbc98a3def3d94f04de30cc128b4a35477024d2bcb2952a161a3b5"
