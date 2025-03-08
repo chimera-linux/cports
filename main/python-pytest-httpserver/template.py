dd687fc2dfd1b56b0b67b1da195366bd76d0ae35
triallax <triallax@tutanota.com>
2025-03-08 11:37:44 +0000

    main/python-pytest-httpserver: update to 1.1.2

--- main/python-pytest-httpserver/template.py
+++ main/python-pytest-httpserver/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pytest-httpserver"
-pkgver = "1.1.0"
-pkgrel = 1
+pkgver = "1.1.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,7 +13,7 @@ pkgdesc = "Pytest http server extension"
 license = "MIT"
 url = "https://github.com/csernazs/pytest-httpserver"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "4378ff64c5c305d7174d3f7aed9c00330c8bf6caa60ea0340885a9879aeee94d"
+sha256 = "b706af59bcf019d9d1e623b7934c316038529cb18137163289ab5387ba627d43"
 
 
 def post_install(self):
