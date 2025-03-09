fab4f7cf04a01fefd999046aeec0cd2700368214
q66 <q66@chimera-linux.org>
2025-03-09 20:34:59 +0100

    main/linux-lts: update to 6.12.18

--- main/linux-lts/template.py
+++ main/linux-lts/template.py
@@ -1,6 +1,6 @@
 # update linux-lts-zfs-bin when bumping
 pkgname = "linux-lts"
-pkgver = "6.12.16"
+pkgver = "6.12.18"
 pkgrel = 0
 archs = [
     "aarch64",
@@ -21,7 +21,7 @@ pkgdesc = f"Linux kernel {pkgver[0 : pkgver.rfind('.')]}.x"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
-sha256 = "5f81362a694f51520bff9faecb73f1cc9bc7bece6fdd10d5c27e348df39d7dc4"
+sha256 = "beb902a5f69d9e57710112203db38111dad6d30556ea8ce389284c8077fe944d"
 # no meaningful checking to be done
 options = [
     "!check",
