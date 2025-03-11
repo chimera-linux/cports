e87ea7b6beaf860d6be8c24aeda8eb57a20771de
q66 <q66@chimera-linux.org>
2025-03-11 20:42:00 +0100

    main/mtools: update to 4.0.48

--- main/mtools/template.py
+++ main/mtools/template.py
@@ -1,5 +1,5 @@
 pkgname = "mtools"
-pkgver = "4.0.47"
+pkgver = "4.0.48"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "texinfo"]
@@ -8,7 +8,7 @@ pkgdesc = "Utilities to access MS-DOS disks"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/mtools"
 source = f"$(GNU_SITE)/mtools/mtools-{pkgver}.tar.bz2"
-sha256 = "31aa06078cc3f50591b95e71a909c56dd179d87e9cbdc07bf435e595bd7cc7ff"
+sha256 = "03c29aac8735dd7154a989fbc29eaf2b506121ae1c3a35cd0bf2a02e94d271a9"
 hardening = ["vis", "!cfi"]
 
 
