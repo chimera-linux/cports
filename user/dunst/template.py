1b163fee42364de8612e0352d8c932abc25d52bb
ttyyls <contact@behri.org>
2025-03-11 20:15:19 +0000

    user/dunst: update to 1.12.2

--- user/dunst/template.py
+++ user/dunst/template.py
@@ -1,6 +1,6 @@
 pkgname = "dunst"
-pkgver = "1.12.1"
-pkgrel = 1
+pkgver = "1.12.2"
+pkgrel = 0
 build_style = "makefile"
 make_check_target = "test"
 make_use_env = True
@@ -26,7 +26,7 @@ url = "https://dunst-project.org"
 source = (
     f"https://github.com/dunst-project/dunst/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "340b10c38ee519a75b14040f65505d72817857358ce7a6fe23190ab68782f892"
+sha256 = "8f7664bd4e6083e9604e282145fe5b8dee7655fa0b099a5b682a2549e1f33d32"
 env = {"SYSCONFDIR": "/etc"}
 hardening = ["vis", "cfi"]
 
