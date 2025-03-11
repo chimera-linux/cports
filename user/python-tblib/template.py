1ae9356fa76ac7d2ed961df59921acf9d35f847d
ttyyls <contact@behri.org>
2025-03-11 04:56:46 +0000

    user/python-tblib: new package

---
+++ user/python-tblib/template.py
@@ -0,0 +1,25 @@
+pkgname = "python-tblib"
+pkgver = "3.0.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-wheel",
+    "python-setuptools",
+]
+checkdepends = [
+    "python-pytest",
+    "python-twisted",
+]
+pkgdesc = "Traceback serialization library"
+license = "BSD-2-Clause"
+url = "https://github.com/ionelmc/python-tblib"
+source = f"$(PYPI_SITE)/t/tblib/tblib-{pkgver}.tar.gz"
+sha256 = "93622790a0a29e04f0346458face1e144dc4d32f493714c6c3dff82a4adb77e6"
+# FIXME: failing tests
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
