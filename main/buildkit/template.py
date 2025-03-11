664e64ef3be031d679235778405e83415b40eefc
q66 <q66@chimera-linux.org>
2025-03-11 20:42:41 +0100

    main/buildkit: update to 0.20.1

--- main/buildkit/template.py
+++ main/buildkit/template.py
@@ -1,6 +1,6 @@
 pkgname = "buildkit"
-pkgver = "0.20.0"
-pkgrel = 1
+pkgver = "0.20.1"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/..."]
 hostmakedepends = ["go"]
@@ -9,7 +9,7 @@ pkgdesc = "Concurrent, cache-efficient, and Dockerfile-agnostic builder toolkit"
 license = "Apache-2.0"
 url = "https://github.com/moby/buildkit"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "4b582972aa915fd580481c0cbad8966aa6c18aaaf4503a9a508ed2e6a9c6283b"
+sha256 = "f188a443233a1c3f9c2a66fdebc0d7667442e17da34a4f05d227bbf73168e820"
 # cannot work in bwrap
 options = ["!check"]
 
