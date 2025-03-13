08b87679a1e5246b9d4750c7cd65471c6b54ade0
q66 <q66@chimera-linux.org>
2025-03-13 19:13:41 +0100

    user/tg_owt-static: new package

---
+++ user/tg_owt-static/template.py
@@ -0,0 +1,73 @@
+pkgname = "tg_owt-static"
+pkgver = "0_git20241203"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    "-DBUILD_SHARED_LIBS=OFF",
+]
+_gitrev = "be39b8c8d0db1f377118f813f0c4bd331d341d5e"
+_libyuv_gitrev = "04821d1e7d60845525e8db55c7bcd41ef5be9406"
+_crc32c_gitrev = "1c51f87c9ad8157b4461e2216b9272f13fd0be3b"
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+    "protobuf",
+    "python",
+]
+makedepends = [
+    "abseil-cpp-devel",
+    "alsa-lib-devel",
+    "ffmpeg-devel",
+    "glib-devel",
+    "libdrm-devel",
+    "libepoxy-devel",
+    "libevent-devel",
+    "libjpeg-turbo-devel",
+    "libpulse-devel",
+    "libsrtp-devel",
+    "libvpx-devel",
+    "libxcomposite-devel",
+    "libxdamage-devel",
+    "libxext-devel",
+    "libxfixes-devel",
+    "libxrandr-devel",
+    "libxrender-devel",
+    "libxtst-devel",
+    "mesa-devel",
+    "mesa-gbm-devel",
+    "musl-bsd-headers",
+    "openh264-devel",
+    "openssl3-devel",
+    "opus-devel",
+    "pipewire-devel",
+    "protobuf-devel",
+]
+depends = [*makedepends]
+pkgdesc = "Telegram's fork of WebRTC"
+license = "BSD-3-Clause"
+url = "https://github.com/desktop-app/tg_owt"
+source = [
+    f"{url}/archive/{_gitrev}.tar.gz",
+    f"https://github.com/google/crc32c/archive/{_crc32c_gitrev}.tar.gz",
+    f"https://github.com/klemensn/libyuv/archive/{_libyuv_gitrev}.tar.gz",
+]
+source_paths = [
+    ".",
+    "src/third_party/crc32c/src",
+    "src/third_party/libyuv",
+]
+sha256 = [
+    "b17301264d43272e7033b5434617ed9376020da9c4d118b16e0c3fcde2c89e03",
+    "b0397b85ddf0ee10be288687a017ad057f93bac5e1a28f30fcd67665d4271285",
+    "eadc1c7276135320f42a22599f23a2f55419e1d90a3c6a4c58cd1586f7b83bff",
+]
+# crashes
+hardening = ["!int"]
+
+if self.profile().endian == "big":
+    broken = "tdesktop deosn't work on this anyway etc."
+
+
+def post_install(self):
+    self.install_license("LICENSE")
