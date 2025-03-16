e07bb3555efa68ab2bd8359bb316a1ff3677252f
q66 <q66@chimera-linux.org>
2025-03-16 12:22:22 +0100

    main/chimera-repo-user: also enable for loongarch

--- main/chimera-repo-user/template.py
+++ main/chimera-repo-user/template.py
@@ -1,7 +1,15 @@
 pkgname = "chimera-repo-user"
 pkgver = "0.3"
 pkgrel = 0
-archs = ["aarch64", "ppc", "ppc64", "ppc64le", "riscv64", "x86_64"]
+archs = [
+    "aarch64",
+    "loongarch64",
+    "ppc",
+    "ppc64",
+    "ppc64le",
+    "riscv64",
+    "x86_64",
+]
 build_style = "meta"
 depends = ["chimera-repo-main"]
 pkgdesc = "Chimera user repository"
