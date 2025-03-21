ed07db5518c8aa2b8cb51b4014b5a33a045ab14d
triallax <triallax@tutanota.com>
2025-03-21 17:10:51 +0000

    main/blake3: update to 1.7.0

--- main/blake3/template.py
+++ main/blake3/template.py
@@ -1,19 +1,20 @@
 pkgname = "blake3"
-pkgver = "1.6.1"
+pkgver = "1.7.0"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DBUILD_SHARED_LIBS=ON"]
+configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DBLAKE3_USE_TBB=ON"]
 cmake_dir = "c"
 hostmakedepends = [
     "cmake",
     "ninja",
     "pkgconf",
 ]
+makedepends = ["onetbb-devel"]
 pkgdesc = "BLAKE3 cryptographic hash implementation"
 license = "CC0-1.0 OR Apache-2.0"
 url = "https://github.com/BLAKE3-team/BLAKE3"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "1f2fbd93790694f1ad66eef26e23c42260a1916927184d78d8395ff1a512d285"
+sha256 = "59bb6f42ecf1bd136b40eaffe40232fc76488b03954ef25cb588404b8d66a7e0"
 hardening = ["vis", "cfi"]
 
 
