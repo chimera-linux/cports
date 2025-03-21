69c937258cbd0734be45a35b0b69d7c3a9f52c72
Jami Kettunen <jami.kettunen@protonmail.com>
2025-03-22 01:11:52 +0200

    main/qca: update to 2.3.10

--- main/qca/template.py
+++ main/qca/template.py
@@ -1,6 +1,6 @@
 pkgname = "qca"
-pkgver = "2.3.9"
-pkgrel = 1
+pkgver = "2.3.10"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_WITH_QT6=ON"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -14,7 +14,7 @@ pkgdesc = "Qt6 cryptographic architecture"
 license = "LGPL-2.1-or-later"
 url = "https://api.kde.org/qca/html"
 source = f"$(KDE_SITE)/qca/{pkgver}/qca-{pkgver}.tar.xz"
-sha256 = "c555d5298cdd7b6bafe2b1f96106f30cfa543a23d459d50c8a91eac33c476e4e"
+sha256 = "1c5b722da93d559365719226bb121c726ec3c0dc4c67dea34f1e50e4e0d14a02"
 hardening = ["vis"]
 
 
