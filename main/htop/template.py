9f9f66b0113f50c65d495ccaa6eb5d64dd169b78
triallax <triallax@tutanota.com>
2025-03-21 16:50:14 +0000

    main/htop: update to 3.4.0

--- main/htop/template.py
+++ main/htop/template.py
@@ -1,6 +1,6 @@
 pkgname = "htop"
-pkgver = "3.3.0"
-pkgrel = 2
+pkgver = "3.4.0"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--enable-capabilities",
@@ -18,10 +18,16 @@ makedepends = [
     "lm-sensors-devel",
     "ncurses-devel",
 ]
+depends = [
+    # dlopened
+    "so:libnl-3.so!libnl",
+    "so:libnl-genl-3.so!libnl",
+    "so:libsensors.so.5!lm-sensors-libs",
+]
 pkgdesc = "Interactive process viewer"
 license = "GPL-2.0-only"
 url = "https://htop.dev"
 source = f"https://github.com/htop-dev/htop/releases/download/{pkgver}/htop-{pkgver}.tar.xz"
-sha256 = "a69acf9b42ff592c4861010fce7d8006805f0d6ef0e8ee647a6ee6e59b743d5c"
+sha256 = "feaabd2d31ca27c09c367a3b1b547ea9f96105fc41f4dfa799e2f49daad5de29"
 # CFI cannot work with libsensors dlsym() stuff
 hardening = ["vis", "!cfi"]
