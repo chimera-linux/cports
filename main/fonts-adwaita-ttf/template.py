f67831c1098c5c6de2f53a8af202c2300bb0c66a
q66 <q66@chimera-linux.org>
2025-03-22 21:36:49 +0100

    main/fonts-adwaita-ttf: reformat

--- main/fonts-adwaita-ttf/template.py
+++ main/fonts-adwaita-ttf/template.py
@@ -6,7 +6,9 @@ hostmakedepends = ["meson", "pkgconf"]
 pkgdesc = "Adwaita family of fonts"
 license = "OFL-1.1"
 url = "https://gitlab.gnome.org/GNOME/adwaita-fonts"
-source = f"$(GNOME_SITE)/adwaita-fonts/{pkgver[:-2]}/adwaita-fonts-{pkgver}.tar.xz"
+source = (
+    f"$(GNOME_SITE)/adwaita-fonts/{pkgver[:-2]}/adwaita-fonts-{pkgver}.tar.xz"
+)
 sha256 = "156f7e92f2f82e527fc73c309dbb237c0a4a5c3a95bc5ee94a5efb6947c553e0"
 
 
