b4b2815b6b0a559364ae48ddf113050427276d2c
q66 <q66@chimera-linux.org>
2025-03-22 17:39:23 +0100

    main/gnome-console: update to 48.0.1

--- main/gnome-console/template.py
+++ main/gnome-console/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-console"
-pkgver = "47.1"
+pkgver = "48.0.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -23,6 +23,6 @@ pkgdesc = "GNOME console"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/console"
 source = (
-    f"$(GNOME_SITE)/gnome-console/{pkgver[:-2]}/gnome-console-{pkgver}.tar.xz"
+    f"$(GNOME_SITE)/gnome-console/{pkgver[:-4]}/gnome-console-{pkgver}.tar.xz"
 )
-sha256 = "d3f600b45b51716691aee92870332e9c9a8ca89d553565f3131d9e28074725d0"
+sha256 = "018e908e4daebcfcb150d4f1bb28d98272aa9d14d6815eaf8da45f889db05c49"
