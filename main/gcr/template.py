67cc36e68eb3add858736333d3cbe3101bd55599
q66 <q66@chimera-linux.org>
2025-03-22 15:14:03 +0100

    main/gcr: update to 4.4.0.1

--- main/gcr/template.py
+++ main/gcr/template.py
@@ -1,5 +1,5 @@
 pkgname = "gcr"
-pkgver = "4.3.1"
+pkgver = "4.4.0.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -29,8 +29,8 @@ makedepends = [
 pkgdesc = "GNOME crypto package"
 license = "LGPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gcr"
-source = f"$(GNOME_SITE)/gcr/{pkgver[:-2]}/gcr-{pkgver}.tar.xz"
-sha256 = "b2f070fff1840eef70546a28be80235427c116aadc593b5b68ccc869be3aa09d"
+source = f"$(GNOME_SITE)/gcr/{pkgver[:-4]}/gcr-{pkgver}.tar.xz"
+sha256 = "0c3c341e49f9f4f2532a4884509804190a0c2663e6120360bb298c5d174a8098"
 # getpass
 tool_flags = {"CFLAGS": ["-D_GNU_SOURCE"]}
 # FIXME int (crashes gnome-keyring suite)
