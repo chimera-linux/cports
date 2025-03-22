f72449c5053da61b2571f2cd408eee3e244b7762
q66 <q66@chimera-linux.org>
2025-03-22 16:55:58 +0100

    main/gsettings-desktop-schemas: update to 48.0

--- main/gsettings-desktop-schemas/template.py
+++ main/gsettings-desktop-schemas/template.py
@@ -1,6 +1,6 @@
 pkgname = "gsettings-desktop-schemas"
-pkgver = "47.1"
-pkgrel = 1
+pkgver = "48.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=true"]
 hostmakedepends = [
@@ -14,14 +14,13 @@ makedepends = ["glib-devel"]
 depends = [
     "adwaita-icon-theme",
     "chimera-artwork",
-    "fonts-cantarell-otf",
-    "fonts-source-code-pro-otf",
+    "fonts-adwaita-ttf",
 ]
 pkgdesc = "Collection of GSettings schemas"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas"
 source = f"$(GNOME_SITE)/gsettings-desktop-schemas/{pkgver[:-2]}/gsettings-desktop-schemas-{pkgver}.tar.xz"
-sha256 = "a60204d9c9c0a1b264d6d0d134a38340ba5fc6076a34b84da945d8bfcc7a2815"
+sha256 = "e68f155813bf18f865a8b2c8e9d473588b6ccadcafbb666ab788857c6c2d1bd3"
 options = ["!cross"]
 
 
