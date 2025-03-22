a4e9553e2cba137758eb770bb1496e402f4578b9
q66 <q66@chimera-linux.org>
2025-03-22 16:57:58 +0100

    main/babl: update to 0.1.112

--- main/babl/template.py
+++ main/babl/template.py
@@ -1,5 +1,5 @@
 pkgname = "babl"
-pkgver = "0.1.110"
+pkgver = "0.1.112"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Denable-gir=true", "-Dwith-docs=false"]
@@ -17,7 +17,7 @@ url = "https://gegl.org/babl"
 source = (
     f"https://download.gimp.org/pub/babl/{pkgver[:-4]}/babl-{pkgver}.tar.xz"
 )
-sha256 = "bf47be7540d6275389f66431ef03064df5376315e243d0bab448c6aa713f5743"
+sha256 = "fb696682421787c8fecc83e8aab48121dec8ee38d119b65291cfcbe315028a79"
 # FIXME all tests fail
 hardening = ["!int"]
 
