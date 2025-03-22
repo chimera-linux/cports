154ebe78b86d6814aea9e85bcb0aa2d3df4bc4d2
q66 <q66@chimera-linux.org>
2025-03-22 15:45:09 +0100

    main/libmanette: update to 0.2.11

--- main/libmanette/template.py
+++ main/libmanette/template.py
@@ -1,11 +1,12 @@
 pkgname = "libmanette"
-pkgver = "0.2.9"
+pkgver = "0.2.11"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dgudev=enabled"]
 hostmakedepends = [
     "glib-devel",
     "gobject-introspection",
+    "hidapi-devel",
     "meson",
     "pkgconf",
     "vala",
@@ -14,8 +15,8 @@ makedepends = ["glib-devel", "libevdev-devel", "libgudev-devel"]
 pkgdesc = "Simple GObject game controller library"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libmanette"
-source = f"$(GNOME_SITE)/libmanette/{pkgver[:-2]}/libmanette-{pkgver}.tar.xz"
-sha256 = "29366be5452f60a74c65fc64ffe2d74eddd4e6e6824c2cefa567a43bd92b688f"
+source = f"$(GNOME_SITE)/libmanette/{pkgver[:-3]}/libmanette-{pkgver}.tar.xz"
+sha256 = "b812b94e08632ba62a30960a8de29217a73a2fff5da2f12acc8a5d4771a49a70"
 
 
 @subpackage("libmanette-devel")
