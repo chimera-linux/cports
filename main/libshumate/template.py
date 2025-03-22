0fe2241f7e95802ad9045f552a7f31555b6c238d
q66 <q66@chimera-linux.org>
2025-03-22 16:37:20 +0100

    main/libshumate: update to 1.4.0

--- main/libshumate/template.py
+++ main/libshumate/template.py
@@ -1,5 +1,5 @@
 pkgname = "libshumate"
-pkgver = "1.3.2"
+pkgver = "1.4.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dgtk_doc=false"]
@@ -25,7 +25,7 @@ pkgdesc = "GTK library to display maps"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libshumate"
 source = f"$(GNOME_SITE)/libshumate/{pkgver[:-2]}/libshumate-{pkgver}.tar.xz"
-sha256 = "f8762bbc6e296d78be1f8422f56da4c40bc8d12afc7002a324172a9198eeed5c"
+sha256 = "3984368e0259862b3810d1ddc86d2dadd6d372a2b32376ccf4aff7c2e48c6d30"
 options = ["!cross"]
 
 
