9287b0c392ec63e257025e153cc5862c2d3a8ae7
q66 <q66@chimera-linux.org>
2025-03-22 17:07:33 +0100

    main/xdg-desktop-portal: update to 1.20.0

--- main/xdg-desktop-portal/template.py
+++ main/xdg-desktop-portal/template.py
@@ -1,6 +1,6 @@
 pkgname = "xdg-desktop-portal"
-pkgver = "1.18.4"
-pkgrel = 1
+pkgver = "1.20.0"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "bubblewrap",
@@ -15,16 +15,27 @@ makedepends = [
     "fuse-devel",
     "gdk-pixbuf-devel",
     "geoclue-devel",
+    "gst-plugins-base-devel",
     "json-glib-devel",
     "libportal-devel",
     "pipewire-devel",
 ]
-checkdepends = ["bash", "dbus"]
+checkdepends = [
+    "bash",
+    "dbus",
+    "gstreamer",
+    "gst-plugins-good",
+    "python-dbus",
+    "python-dbusmock",
+    "python-gobject",
+    "python-pytest",
+    "umockdev-devel",
+]
 pkgdesc = "Desktop integration portal"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/flatpak/xdg-desktop-portal"
 source = f"https://github.com/flatpak/xdg-desktop-portal/releases/download/{pkgver}/xdg-desktop-portal-{pkgver}.tar.xz"
-sha256 = "b858aa1e74e80c862790dbb912906e6eab8b1e4db9339cd759473af62b461e65"
+sha256 = "33d666f169efdf3f3bedd531bdbd272edc8f471caf6ca6cf6752efbbab57523a"
 
 
 def post_install(self):
