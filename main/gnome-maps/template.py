ed1d1b735eea3a8fdb943b81b9aa1d5d590d2638
q66 <q66@chimera-linux.org>
2025-03-22 17:58:46 +0100

    main/gnome-maps: update to 48.1

--- main/gnome-maps/template.py
+++ main/gnome-maps/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-maps"
-pkgver = "47.4"
+pkgver = "48.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -44,5 +44,5 @@ pkgdesc = "GNOME maps"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later AND CC0-1.0"
 url = "https://apps.gnome.org/Maps"
 source = f"$(GNOME_SITE)/gnome-maps/{pkgver.split('.')[0]}/gnome-maps-{pkgver}.tar.xz"
-sha256 = "18c5e270fb522cfd3c003ec01093e065eb2f2b8d1b2780dfc279e0bf77735e0b"
+sha256 = "ab2264a76f85458ee0b243984afa22b0aa516ed468b6d65395941c3786d82e9b"
 options = ["!cross"]
