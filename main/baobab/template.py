b1cd4725d77933459d019ac245ff76d5d56406da
q66 <q66@chimera-linux.org>
2025-03-22 18:50:21 +0100

    main/baobab: update to 48.0

--- main/baobab/template.py
+++ main/baobab/template.py
@@ -1,5 +1,5 @@
 pkgname = "baobab"
-pkgver = "47.0"
+pkgver = "48.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -21,5 +21,5 @@ pkgdesc = "Graphical directory tree analyzer for GNOME"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/action/show/Apps/DiskUsageAnalyzer"
 source = f"$(GNOME_SITE)/baobab/{pkgver[:-2]}/baobab-{pkgver}.tar.xz"
-sha256 = "b88f74f9c052d3c2388f7062d228cf5e927545acf7408c56841df80ccd1f9c37"
+sha256 = "54592504d49d807f23591be7e7eef10c6c9dfcb7ac527b81c3acd58787b26fda"
 hardening = ["vis", "!cfi"]
