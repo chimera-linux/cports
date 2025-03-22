06e27f0314a01e693b54b0e25eebe8f4d9f7eba4
q66 <q66@chimera-linux.org>
2025-03-22 15:46:19 +0100

    main/libadwaita: update to 1.7.0

--- main/libadwaita/template.py
+++ main/libadwaita/template.py
@@ -1,5 +1,5 @@
 pkgname = "libadwaita"
-pkgver = "1.6.4"
+pkgver = "1.7.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -30,7 +30,7 @@ pkgdesc = "GTK4 building blocks for modern adaptive applications"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libadwaita"
 source = f"$(GNOME_SITE)/libadwaita/{pkgver[:-2]}/libadwaita-{pkgver}.tar.xz"
-sha256 = "1011a5a93dc3b87e82da19910d7fab01a3b9d7fcb0b09751babb476eedb3d9a3"
+sha256 = "58bf99b8a9f8b0171964de0ae741d01d5a09db3662134fa67541c99a8ed7dec0"
 options = ["!cross"]
 
 
