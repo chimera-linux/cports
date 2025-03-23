81c21174ec56df188e032ad73f9bfed4d94c8a68
fossdd <fossdd@pwned.life>
2025-03-23 15:19:44 +0100

    main/gsettings-desktop-schemas: use gsettings override instead of patch
    The override (20_...) will be prefered to other overrides, e.g. by Phosh
    (00_...) and therefore take precedence regarding the accent color and the
    background. It also the preferred method by upstream for distributors to
    apply own values.
    
    The Phosh issue was only visible in gdm since Phosh's overrides only
    apply if XDG_CURRENT_DESKTOP was set to "Phosh", which wasn't if run
    manually.

--- main/gsettings-desktop-schemas/template.py
+++ main/gsettings-desktop-schemas/template.py
@@ -1,6 +1,6 @@
 pkgname = "gsettings-desktop-schemas"
 pkgver = "48.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = ["-Dintrospection=true"]
 hostmakedepends = [
@@ -22,6 +22,13 @@ url = "https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas"
 source = f"$(GNOME_SITE)/gsettings-desktop-schemas/{pkgver[:-2]}/gsettings-desktop-schemas-{pkgver}.tar.xz"
 sha256 = "e68f155813bf18f865a8b2c8e9d473588b6ccadcafbb666ab788857c6c2d1bd3"
 options = ["!cross"]
+
+
+def post_install(self):
+    self.install_file(
+        self.files_path / "10_chimera_theme.gschema.override",
+        "usr/share/glib-2.0/schemas",
+    )
 
 
 @subpackage("gsettings-desktop-schemas-devel")
