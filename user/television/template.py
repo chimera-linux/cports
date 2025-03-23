1fc7e8d6df7a6b7ec23310ac0a7baae9c57e7b13
ttyyls <contact@behri.org>
2025-03-23 18:35:26 +0000

    user/television: update to 0.11.4

--- user/television/template.py
+++ user/television/template.py
@@ -1,5 +1,5 @@
 pkgname = "television"
-pkgver = "0.11.3"
+pkgver = "0.11.4"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -9,9 +9,7 @@ pkgdesc = "Fuzzy finder"
 license = "MIT"
 url = "https://github.com/alexpasmantier/television"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "3b589b6552fc741d8527d3b1a6a4b57ce08b50b2203e4baf4ab151f5dbf57cc4"
-# generates completions with host binary
-options = ["!cross"]
+sha256 = "c4e8a87d65135fb5a6006c7c08dd34cf6aeb24c2d59e9ea6772e9ff714bfed29"
 
 if self.profile().wordsize == 32:
     broken = "needs atomic64"
