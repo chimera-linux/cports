38231e766108ff227751c94ba62d44d81463bf7c
Erica Z <zerica@callcc.eu>
2025-03-25 18:25:20 +0100

    user/protontricks: restrict to x86_64

--- user/protontricks/template.py
+++ user/protontricks/template.py
@@ -1,6 +1,7 @@
 pkgname = "protontricks"
 pkgver = "1.12.1"
 pkgrel = 0
+archs = ["x86_64"]
 build_style = "python_pep517"
 make_build_env = {"SETUPTOOLS_SCM_PRETEND_VERSION": pkgver}
 hostmakedepends = [
