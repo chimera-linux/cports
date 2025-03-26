6cce0993986738f04f845eacbfcf9f37d1389aca
q66 <q66@chimera-linux.org>
2025-03-26 22:17:18 +0100

    user/kanata: update to 1.8.0

--- user/kanata/template.py
+++ user/kanata/template.py
@@ -1,6 +1,6 @@
 pkgname = "kanata"
-pkgver = "1.7.0"
-pkgrel = 1
+pkgver = "1.8.0"
+pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
 makedepends = ["rust-std"]
@@ -8,7 +8,7 @@ pkgdesc = "Software keyboard remapper"
 license = "LGPL-3.0-only"
 url = "https://github.com/jtroo/kanata"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "eb7e11511f77558d72b5b3b0c9defb04b269637e5c8a4ad9b45d21382e9247d2"
+sha256 = "396a379d7620001531b856913a2677baa56fa16c5c9d329f6937dfb57d3ac704"
 
 if self.profile().wordsize == 32:
     broken = "needs atomic64"
