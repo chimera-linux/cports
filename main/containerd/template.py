7598ff6d2fcf02c1b117c7a714ba808b88e14469
q66 <q66@chimera-linux.org>
2025-03-26 16:38:19 +0100

    main/containerd: update to 2.0.4

--- main/containerd/template.py
+++ main/containerd/template.py
@@ -1,6 +1,6 @@
 pkgname = "containerd"
-pkgver = "2.0.2"
-pkgrel = 4
+pkgver = "2.0.4"
+pkgrel = 0
 build_style = "makefile"
 make_build_args = [
     # invokes go, so slower in parallel since races for cache and overloads threads
@@ -38,7 +38,7 @@ source = [
     f"!https://raw.githubusercontent.com/containerd/nerdctl/refs/tags/v{pkgver}/extras/rootless/containerd-rootless.sh>containerd-rootless-{pkgver}",
 ]
 sha256 = [
-    "472747a7a6b360a0864bab0ee00a8a6f51da5795171e6a60ab17aa80cbd850a2",
+    "af0b18d125abf97fbe90d6d2cda53ffa0cd4cbb9e7d65fee61fc095bfb63cef5",
     "1f8101ce7680ce4533ced18b4e3e39bd300c08210c336d30f6969c8cb1727a7c",
 ]
 # can't run tests inside namespaces
