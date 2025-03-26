0873a4c8749424eee7565aeb6ff4131b204ebff2
q66 <q66@chimera-linux.org>
2025-03-26 17:27:03 +0100

    main/rootlesskit: update to 2.3.4

--- main/rootlesskit/template.py
+++ main/rootlesskit/template.py
@@ -1,6 +1,6 @@
 pkgname = "rootlesskit"
-pkgver = "2.3.2"
-pkgrel = 3
+pkgver = "2.3.4"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X github.com/rootless-containers/rootlesskit/pkg/version.Version={pkgver}",
@@ -12,6 +12,6 @@ pkgdesc = "Fake root for rootless containers"
 license = "Apache-2.0"
 url = "https://github.com/rootless-containers/rootlesskit"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "feb71a63864344f4cf72b6b230082e616aff4f4a43bd2cf13cebd4b04e277980"
+sha256 = "a4b28fdf7a712db2429b5b5c54161585f2a34d4fcb765ed8447e3466dc83b065"
 # no tests
 options = ["!check"]
