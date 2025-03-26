5def0c77177055224e9a2859ba8b5e841a5b6238
q66 <q66@chimera-linux.org>
2025-03-26 22:22:18 +0100

    user/taskwarrior: update to 3.4.1

--- user/taskwarrior/template.py
+++ user/taskwarrior/template.py
@@ -1,16 +1,23 @@
 pkgname = "taskwarrior"
-pkgver = "3.3.0"
+pkgver = "3.4.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DSYSTEM_CORROSION=ON"]
 make_dir = "."
-hostmakedepends = ["cmake", "ninja", "cargo", "corrosion", "cxxbridge"]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "cargo",
+    "corrosion",
+    "cxxbridge",
+    "rust-bindgen",
+]
 makedepends = ["util-linux-uuid-devel", "rust-std", "sqlite-devel"]
 pkgdesc = "TODO list manager for the command line"
 license = "MIT"
 url = "https://taskwarrior.org"
 source = f"https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v{pkgver}/task-{pkgver}.tar.gz"
-sha256 = "7fd1e3571f673679758f001b5f44963eee59fd0d2cac887a5807cf2fd90856a1"
+sha256 = "23eb60f73e42f16111cc3912b44ee12be6768860a2db2a9c6a47f8ac4786bac3"
 
 if self.profile().wordsize == 32:
     broken = "atomic64 assumptions"
@@ -18,6 +25,17 @@ if self.profile().wordsize == 32:
 
 def post_patch(self):
     from cbuild.util import cargo
+
+    # match packaged cxxbridge
+    self.do(
+        "cargo",
+        "update",
+        "--package",
+        "cxx",
+        "--precise",
+        "1.0.150",
+        allow_network=True,
+    )
 
     cargo.Cargo(self).vendor()
 
