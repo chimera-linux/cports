840266b24fa6ef43a7c6eb825dbd66b893a19ebe
q66 <q66@chimera-linux.org>
2025-03-26 23:30:01 +0100

    main/vulkan-tools: update to 1.4.311

--- main/vulkan-tools/template.py
+++ main/vulkan-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-tools"
-pkgver = "1.4.306"
+pkgver = "1.4.311"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -30,7 +30,7 @@ url = "https://www.khronos.org/vulkan"
 source = (
     f"https://github.com/KhronosGroup/Vulkan-Tools/archive/v{pkgver}.tar.gz"
 )
-sha256 = "8e3c3df74fa8e6408cbb772d20012c3fdf0bf1b3e363e18e909b6c99d5b489a2"
+sha256 = "7113bc0c746b45072e269fada0d684b4ae9de609c38d9e764b59793b14120a7b"
 # CFI: vkcube etc fail
 hardening = ["vis", "!cfi"]
 # no test suite
