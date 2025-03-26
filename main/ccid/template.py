ae877511ee0bf768b3daf0653c36aeb110756a7f
q66 <q66@chimera-linux.org>
2025-03-26 16:35:12 +0100

    main/ccid: update to 1.6.2

--- main/ccid/template.py
+++ main/ccid/template.py
@@ -1,5 +1,5 @@
 pkgname = "ccid"
-pkgver = "1.6.1"
+pkgver = "1.6.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -22,7 +22,7 @@ pkgdesc = "PC/SC driver to support CCID compliant readers"
 license = "LGPL-2.1-or-later"
 url = "https://ccid.apdu.fr"
 source = f"{url}/files/ccid-{pkgver}.tar.xz"
-sha256 = "2eca8fb07e8fe7c0d39daeaca7b97cd73c40ed9b72738a24ad3dcbdfc918e1ea"
+sha256 = "41958410950157e622f9d91c9e78c7b708db74e22f71190c581d24d20564d449"
 
 
 def post_install(self):
