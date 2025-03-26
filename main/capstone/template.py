6144e99eeadf1b1f188e1f70f98ed0b7f6fa6abd
q66 <q66@chimera-linux.org>
2025-03-26 16:34:16 +0100

    main/capstone: update to 5.0.6

--- main/capstone/template.py
+++ main/capstone/template.py
@@ -1,5 +1,5 @@
 pkgname = "capstone"
-pkgver = "5.0.5"
+pkgver = "5.0.6"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
@@ -13,7 +13,7 @@ pkgdesc = "Ultimate Disassembler"
 license = "BSD-3-Clause-Clear"
 url = "https://www.capstone-engine.org"
 source = f"https://github.com/capstone-engine/capstone/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "3bfd3e7085fbf0fab75fb1454067bf734bb0bebe9b670af7ce775192209348e9"
+sha256 = "240ebc834c51aae41ca9215d3190cc372fd132b9c5c8aa2d5f19ca0c325e28f9"
 hardening = ["vis", "!cfi"]
 
 
