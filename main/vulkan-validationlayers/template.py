3e449ab9065a069d01fa2fca1691ac98e70fba4b
q66 <q66@chimera-linux.org>
2025-03-26 23:33:05 +0100

    main/vulkan-validationlayers: update to 1.4.311

--- main/vulkan-validationlayers/template.py
+++ main/vulkan-validationlayers/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-validationlayers"
-pkgver = "1.4.306"
+pkgver = "1.4.311"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -26,7 +26,7 @@ license = "Apache-2.0"
 url = "https://vulkan.lunarg.com/doc/sdk/latest/linux/khronos_validation_layer.html"
 source = [
     f"https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/refs/tags/v{pkgver}.tar.gz",
-    "https://github.com/KhronosGroup/SPIRV-Headers/archive/2b2e05e088841c63c0b6fd4c9fb380d8688738d3.tar.gz",
+    "https://github.com/KhronosGroup/SPIRV-Headers/archive/ddd2c099be25e3fec7cdc14106c17aca99512082.tar.gz",
     "https://github.com/martinus/robin-hood-hashing/archive/7697343363af4cc3f42cab17be49e6af9ab181e2.tar.gz",
 ]
 source_paths = [
@@ -35,7 +35,7 @@ source_paths = [
     "layers/robinhood",
 ]
 sha256 = [
-    "9756a34e206e8d1fff4dfb951d46536ab2eb14e12bcdafbb79202155ec48c1a9",
-    "2e226ee953472e2e39724bf315433dce8cf119a397c451742dfda25bab7690af",
+    "d784393c9fd2b8c3f9ca71aa14cee5fd0690227e433c16895ea9c6db0e1b43a1",
+    "9c302616ec6c73b507d7ca61935b84484ab891b6467a8c9499ce7f1de33aad5e",
     "bce88bee05812abd863e8ae045757b66116fc9d7e880e649916d8eb71a10fa9f",
 ]
