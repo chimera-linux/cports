6656e093f885c7fae52eb1f58e0f22627ce761e8
q66 <q66@chimera-linux.org>
2025-03-26 16:34:41 +0100

    main/ccache: update to 4.11.2

--- main/ccache/template.py
+++ main/ccache/template.py
@@ -1,13 +1,17 @@
 pkgname = "ccache"
-pkgver = "4.10.2"
-pkgrel = 3
+pkgver = "4.11.2"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DENABLE_TESTING=ON",
     "-DREDIS_STORAGE_BACKEND=OFF",
 ]
 # these fail cause of running grep on .o and musl has no reg_startend so shit sucks
-make_check_args = ["-E", "(test.direct|test.debug_prefix_map)"]
+# test.debug_compilation_dir fails because llvm objdump has different options
+make_check_args = [
+    "-E",
+    "(test.direct|test.debug_prefix_map|test.debug_compilation_dir)",
+]
 hostmakedepends = [
     "cmake",
     "ninja",
@@ -23,7 +27,7 @@ pkgdesc = "Fast C/C++ compiler cache"
 license = "GPL-3.0-or-later"
 url = "https://ccache.dev"
 source = f"https://github.com/ccache/ccache/releases/download/v{pkgver}/ccache-{pkgver}.tar.xz"
-sha256 = "c0b85ddfc1a3e77b105ec9ada2d24aad617fa0b447c6a94d55890972810f0f5a"
+sha256 = "319390f276123968cfa565acc3da0b1e18414374b40ff25274230e6860352125"
 # cfi crashes in fmt template expansion
 hardening = ["vis", "!cfi"]
 
