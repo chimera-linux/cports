e84bb35c739915639e0f4c8b80b4da56b8796a2f
q66 <q66@chimera-linux.org>
2025-03-26 16:44:21 +0100

    main/faust: update to 2.79.3

--- main/faust/template.py
+++ main/faust/template.py
@@ -1,5 +1,5 @@
 pkgname = "faust"
-pkgver = "2.77.3"
+pkgver = "2.79.3"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "ppc64", "riscv64", "x86_64"]
 build_style = "makefile"
@@ -10,7 +10,7 @@ pkgdesc = "Functional programming language for real-time signal processing"
 license = "GPL-2.0-or-later"
 url = "https://faust.grame.fr"
 source = f"https://github.com/grame-cncm/faust/releases/download/{pkgver}/faust-{pkgver}.tar.gz"
-sha256 = "3685348ba2482547fc7675b345caea490ff380814c5dcabc8a5f772682617c0e"
+sha256 = "ca2171cb136f135960be10fee2c1728304865a5d5190e9a03cace88b4936c558"
 # skip execinfo
 tool_flags = {"CXXFLAGS": ["-DALPINE"]}
 # no check target
