dd25e83d6ed6b30b3acdb46d588608b58ac62b15
q66 <q66@chimera-linux.org>
2025-03-26 16:50:09 +0100

    main/icewm: update to 3.7.2

--- main/icewm/template.py
+++ main/icewm/template.py
@@ -1,5 +1,5 @@
 pkgname = "icewm"
-pkgver = "3.6.0"
+pkgver = "3.7.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -41,4 +41,4 @@ pkgdesc = "Window manager for X11"
 license = "GPL-2.0-or-later"
 url = "https://ice-wm.org"
 source = f"https://github.com/ice-wm/icewm/archive/{pkgver}.tar.gz"
-sha256 = "0fa15f252983d21b1bc434142c787b16bdfc347a83c3057586d0e0091f26617b"
+sha256 = "855e2503c16bdf6cb23c2032b9ab3b19a48ca882951d48af7c3258bb7c71214b"
