46bd08b73b2d25ba2f4beaadeb9ff820adc1a94b
q66 <q66@chimera-linux.org>
2025-03-26 16:43:41 +0100

    main/faad2: update to 2.11.2

--- main/faad2/template.py
+++ main/faad2/template.py
@@ -1,5 +1,5 @@
 pkgname = "faad2"
-pkgver = "2.11.1"
+pkgver = "2.11.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
@@ -12,7 +12,7 @@ pkgdesc = "Free AAC decoder"
 license = "GPL-2.0-or-later"
 url = "https://sourceforge.net/projects/faac"
 source = f"https://github.com/knik0/faad2/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "72dbc0494de9ee38d240f670eccf2b10ef715fd0508c305532ca3def3225bb06"
+sha256 = "3fcbd305e4abd34768c62050e18ca0986f7d9c5eca343fb98275418013065c0e"
 
 
 @subpackage("faad2-devel")
