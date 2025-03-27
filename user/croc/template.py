26e253b40aad021e5976ee72e46bea8e40e35da7
triallax <triallax@tutanota.com>
2025-03-27 10:55:41 +0000

    user/croc: update to 10.2.2

--- user/croc/template.py
+++ user/croc/template.py
@@ -1,13 +1,13 @@
 pkgname = "croc"
-pkgver = "10.2.1"
-pkgrel = 4
+pkgver = "10.2.2"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "File transfer tool"
 license = "MIT"
 url = "https://github.com/schollz/croc"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "78bf0efd00daa9002bcdeb460f4ddaf82dde4480e63862feab0958ed9ed54963"
+sha256 = "1d892bbf3f8dacd0f528f683ab6c3678483374b17076187da7d1af805326fa68"
 # check: needs network access
 options = ["!check"]
 
