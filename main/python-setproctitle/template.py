768a7f8adf2473503af5c04857ea6b95defeae11
triallax <triallax@tutanota.com>
2025-03-27 11:32:54 +0000

    main/python-setproctitle: update to 1.3.5

--- main/python-setproctitle/template.py
+++ main/python-setproctitle/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-setproctitle"
-pkgver = "1.3.4"
-pkgrel = 1
+pkgver = "1.3.5"
+pkgrel = 0
 build_style = "python_pep517"
 make_check_target = "tests"
 hostmakedepends = [
@@ -15,7 +15,7 @@ pkgdesc = "Python module for process title customization"
 license = "BSD-3-Clause"
 url = "https://github.com/dvarrazzo/py-setproctitle"
 source = f"$(PYPI_SITE)/s/setproctitle/setproctitle-{pkgver}.tar.gz"
-sha256 = "3b40d32a3e1f04e94231ed6dfee0da9e43b4f9c6b5450d53e6dd7754c34e0c50"
+sha256 = "1e6eaeaf8a734d428a95d8c104643b39af7d247d604f40a7bebcf3960a853c5e"
 # can't find itself
 options = ["!check"]
 
