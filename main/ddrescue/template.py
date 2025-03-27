205e7cf578ea441f8863d82d2f9d882be02de264
triallax <triallax@tutanota.com>
2025-03-27 12:05:04 +0000

    main/ddrescue: update to 1.29.1

--- main/ddrescue/template.py
+++ main/ddrescue/template.py
@@ -1,5 +1,5 @@
 pkgname = "ddrescue"
-pkgver = "1.29"
+pkgver = "1.29.1"
 pkgrel = 0
 build_style = "gnu_configure"
 # handrolled conf
@@ -8,7 +8,7 @@ pkgdesc = "Data recovery tool for failing block devices"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/ddrescue/ddrescue.html"
 source = f"$(GNU_SITE)/ddrescue/ddrescue-{pkgver}.tar.lz"
-sha256 = "01a414327853b39fba2fd0ece30f7bee2e9d8c8e8eb314318524adf5a60039a3"
+sha256 = "ddd7d45df026807835a2ec6ab9c365df2ef19e8de1a50ffe6886cd391e04dd75"
 hardening = ["vis", "cfi"]
 
 
