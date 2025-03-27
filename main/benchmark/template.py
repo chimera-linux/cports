ed8644f084edf65e36737df799fd5aea0d06b84f
triallax <triallax@tutanota.com>
2025-03-27 11:57:51 +0000

    main/benchmark: update to 1.9.2

--- main/benchmark/template.py
+++ main/benchmark/template.py
@@ -1,5 +1,5 @@
 pkgname = "benchmark"
-pkgver = "1.9.1"
+pkgver = "1.9.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -19,7 +19,7 @@ pkgdesc = "Microbenchmark support library"
 license = "Apache-2.0"
 url = "https://github.com/google/benchmark"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "32131c08ee31eeff2c8968d7e874f3cb648034377dfc32a4c377fa8796d84981"
+sha256 = "409075176168dc46bbb81b74c1b4b6900385b5d16bfc181d678afb060d928bd3"
 
 
 @subpackage("benchmark-devel")
