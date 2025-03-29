fdf98d377913bd4df22d1d5230a5b509d8d27f83
triallax <triallax@tutanota.com>
2025-03-29 18:41:59 +0000

    main/libexpat: update to 2.7.1

--- main/libexpat/template.py
+++ main/libexpat/template.py
@@ -1,5 +1,5 @@
 pkgname = "libexpat"
-pkgver = "2.7.0"
+pkgver = "2.7.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--without-examples"]
@@ -10,7 +10,7 @@ pkgdesc = "XML parser library written in C"
 license = "MIT"
 url = "https://libexpat.github.io"
 source = f"https://github.com/libexpat/libexpat/releases/download/R_{pkgver.replace('.', '_')}/expat-{pkgver}.tar.xz"
-sha256 = "25df13dd2819e85fb27a1ce0431772b7047d72af81ae78dc26b4c6e0805f48d1"
+sha256 = "354552544b8f99012e5062f7d570ec77f14b412a3ff5c7d8d0dae62c0d217c30"
 # CFI: crash reproducible e.g. with graphene build
 hardening = ["vis", "!cfi"]
 
