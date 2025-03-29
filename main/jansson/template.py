b05bf3d0b46abfecbe0bed984428c04722043a9d
triallax <triallax@tutanota.com>
2025-03-29 18:28:36 +0000

    main/jansson: update to 2.14.1

--- main/jansson/template.py
+++ main/jansson/template.py
@@ -1,5 +1,5 @@
 pkgname = "jansson"
-pkgver = "2.14"
+pkgver = "2.14.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf", "slibtool"]
@@ -7,7 +7,7 @@ pkgdesc = "Library for encoding, decoding and manipulating JSON data"
 license = "MIT"
 url = "https://www.digip.org/jansson"
 source = f"https://github.com/akheron/jansson/releases/download/v{pkgver}/jansson-{pkgver}.tar.gz"
-sha256 = "5798d010e41cf8d76b66236cfb2f2543c8d082181d16bc3085ab49538d4b9929"
+sha256 = "2521cd51a9641d7a4e457f7215a4cd5bb176f690bc11715ddeec483e85d9e2b3"
 
 
 def post_install(self):
