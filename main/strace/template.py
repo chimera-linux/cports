0b406a8b3d52b124dbed240569503491f2a638c7
triallax <triallax@tutanota.com>
2025-03-29 18:14:35 +0000

    main/strace: update to 6.14

--- main/strace/template.py
+++ main/strace/template.py
@@ -1,5 +1,5 @@
 pkgname = "strace"
-pkgver = "6.13"
+pkgver = "6.14"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -21,5 +21,5 @@ pkgdesc = "System call tracer"
 license = "LGPL-2.1-or-later"
 url = "https://strace.io"
 source = f"https://github.com/strace/strace/releases/download/v{pkgver}/strace-{pkgver}.tar.xz"
-sha256 = "e209daf0ee038ca5adcc4c277e9273b4d51f46a2ff86da575d36742ac3508a17"
+sha256 = "244f3b5c20a32854ca9b7ca7a3ee091dd3d4bd20933a171ecee8db486c77d3c9"
 hardening = ["vis", "cfi"]
