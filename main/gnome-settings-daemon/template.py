051ea0f534789277707bcf8326984431d6190dae
q66 <q66@chimera-linux.org>
2025-03-30 22:33:25 +0200

    main/gnome-settings-daemon: temporarily revert a few commits
    These commits broke the display backlight showing in the system
    menu in the top panel, so go back to a known good version.
    
    Ref https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/issues/867

--- main/gnome-settings-daemon/template.py
+++ main/gnome-settings-daemon/template.py
@@ -1,6 +1,8 @@
 pkgname = "gnome-settings-daemon"
 pkgver = "48.0"
-pkgrel = 0
+pkgrel = 1
+# temporary: https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/issues/867#note_2397625
+_gitrev = "90edb8f76e5acfeda5d17f8dd3e690c752283036"
 build_style = "meson"
 configure_args = [
     "-Dsystemd=false",
@@ -57,8 +59,16 @@ depends = ["iio-sensor-proxy-meta"]
 pkgdesc = "GNOME settings daemon"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-settings-daemon"
-source = f"$(GNOME_SITE)/gnome-settings-daemon/{pkgver[:-2]}/gnome-settings-daemon-{pkgver}.tar.xz"
-sha256 = "f3da20ab4a2ba56fd3e97b991f2bc267973d5619bd58bfa516410aa036305e45"
+# source = f"$(GNOME_SITE)/gnome-settings-daemon/{pkgver[:-2]}/gnome-settings-daemon-{pkgver}.tar.xz"
+source = [
+    f"{url}/-/archive/{_gitrev}.tar.gz",
+    "https://gitlab.gnome.org/GNOME/libgnome-volume-control/-/archive/91f3f41490666a526ed78af744507d7ee1134323.tar.gz",
+]
+source_paths = [".", "subprojects/gvc"]
+sha256 = [
+    "45d8d50acfdf6428346baaa9358c0bcec9dbf74b193c1b8ca85a50e667ffa9c0",
+    "302d1f97ada58b38168d7ce5d5d1df07fc0813e2700b6c69c6f1d7f705ccca29",
+]
 tool_flags = {"CFLAGS": ["-UG_DISABLE_ASSERT"]}
 # wants xvfb
 options = ["!check"]
