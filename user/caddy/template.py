be7549d97914fffce73ea2e508981aa1b0db2797
elivance <silentgaze@outlook.com>
2025-04-02 04:33:39 +0100

    user/caddy: new package

---
+++ user/caddy/template.py
@@ -0,0 +1,21 @@
+pkgname = "caddy"
+pkgver = "2.9.1"
+pkgrel = 0
+build_style = "go"
+make_build_args = [
+    f"-ldflags=-X github.com/caddyserver/caddy/v2.CustomVersion=v{pkgver}",
+    "./cmd/caddy",
+]
+make_check_args = ["-p", "1", "./..."]
+hostmakedepends = [ "go" ]
+depends = ["shared-mime-info"]
+pkgdesc = "Extensible HTTP server with automatic HTTPS"
+license = "Apache-2.0"
+url = "https://caddyserver.com"
+source = f"https://github.com/caddyserver/caddy/archive/v{pkgver}.tar.gz"
+sha256 = "beb52478dfb34ad29407003520d94ee0baccbf210d1af72cebf430d6d7dd7b63"
+
+def post_install(self):
+    self.install_sysusers(self.files_path / "sysusers.conf")
+    self.install_tmpfiles(self.files_path / "tmpfiles.conf")
+    self.install_service(self.files_path / "caddy")
