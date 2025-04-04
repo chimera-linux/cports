f08fe219cd1c12ee3b541a574ce659c34dacb70b
q66 <q66@chimera-linux.org>
2025-04-04 13:39:05 +0200

    main/txr: get rid of update check for now
    This stalls our nightly update check forever because it gets
    stuck in their honeypot.

--- main/txr/update.py
+++ main/txr/update.py
@@ -1,2 +1,4 @@
-url = "https://www.kylheku.com/cgit/txr/refs"
-pattern = r"/cgit/txr/snapshot/txr-(\d+)\.tar\.gz"
+# gets caught in honeypot
+# url = "https://www.kylheku.com/cgit/txr/refs"
+# pattern = r"/cgit/txr/snapshot/txr-(\d+)\.tar\.gz"
+ignore = True
