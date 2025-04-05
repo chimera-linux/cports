d490114d2a09613a971404e4093009f5dd35f985
Erica Z <zerica@callcc.eu>
2025-04-05 08:16:00 +0200

    main/spiped: update to 1.6.4

--- main/spiped/template.py
+++ main/spiped/template.py
@@ -1,5 +1,5 @@
 pkgname = "spiped"
-pkgver = "1.6.3"
+pkgver = "1.6.4"
 pkgrel = 0
 build_style = "makefile"
 make_check_target = "test"
@@ -9,7 +9,7 @@ pkgdesc = "Secure pipe daemon"
 license = "BSD-2-Clause"
 url = "https://www.tarsnap.com/spiped.html"
 source = f"https://www.tarsnap.com/spiped/spiped-{pkgver}.tgz"
-sha256 = "70c53070dbbb10d1442754aeafb01b08ec829203d41023647dbf1a1435ee4a65"
+sha256 = "424fb4d3769d912b04de43d21cc32748cdfd3121c4f1d26d549992a54678e06a"
 
 
 def init_install(self):
