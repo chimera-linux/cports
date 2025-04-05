1ecd6441d1bdcd584eda24e84a932bcdd416947f
breakgimme <adam@plock.com>
2025-04-05 23:31:12 +0200

    user/celluloid: update to 0.28

--- user/celluloid/template.py
+++ user/celluloid/template.py
@@ -1,5 +1,5 @@
 pkgname = "celluloid"
-pkgver = "0.27"
+pkgver = "0.28"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["glib-devel", "gettext", "meson", "pkgconf"]
@@ -15,4 +15,4 @@ pkgdesc = "GTK frontend for mpv"
 license = "GPL-3.0-or-later"
 url = "https://celluloid-player.github.io"
 source = f"https://github.com/celluloid-player/celluloid/releases/download/v{pkgver}/celluloid-{pkgver}.tar.xz"
-sha256 = "216656c4495bb3ca02dc4ad9cf3da8e8f15c8f80e870eeac8eb1eedab4c3788b"
+sha256 = "5b36fdf0dfff873d149655064350e370872dc54226dd2cbfcc02fa0c107e533a"
