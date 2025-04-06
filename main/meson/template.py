2ecba8dcac6747131863f584e16405d43f445a92
q66 <q66@chimera-linux.org>
2025-04-06 04:20:56 +0200

    main/meson: update to 1.7.2

--- main/meson/template.py
+++ main/meson/template.py
@@ -1,6 +1,6 @@
 pkgname = "meson"
-pkgver = "1.7.0"
-pkgrel = 1
+pkgver = "1.7.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,7 +13,7 @@ pkgdesc = "Meson build system"
 license = "Apache-2.0"
 url = "https://mesonbuild.com"
 source = f"https://github.com/mesonbuild/meson/releases/download/{pkgver}/meson-{pkgver}.tar.gz"
-sha256 = "08efbe84803eed07f863b05092d653a9d348f7038761d900412fddf56deb0284"
+sha256 = "4d40d63aa748a9c139cc41ab9bffe43edd113c5639d78bde81544ca955aea890"
 # meson is early in our bootstrap path but has a million checkdepends
 options = ["!check"]
 
