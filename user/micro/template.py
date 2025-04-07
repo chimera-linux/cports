d924765e699a3c8999e59eddc898aafc77fe47f5
Karol Kosek <krkk@serenityos.org>
2025-04-07 21:17:07 +0200

    user/micro: generate syntax highlighting files

--- user/micro/template.py
+++ user/micro/template.py
@@ -1,6 +1,6 @@
 pkgname = "micro"
 pkgver = "2.0.14"
-pkgrel = 7
+pkgrel = 8
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X github.com/zyedidia/micro/v2/internal/util.Version={pkgver}",
@@ -12,6 +12,12 @@ license = "MIT"
 url = "https://micro-editor.github.io"
 source = f"https://github.com/zyedidia/micro/archive/v{pkgver}.tar.gz"
 sha256 = "40177579beb3846461036387b649c629395584a4bbe970f61ba7591bd9c0185a"
+
+
+def pre_build(self):
+    from cbuild.util import golang
+
+    self.do("go", "generate", "./runtime", env=golang.get_go_env(self))
 
 
 def post_install(self):
