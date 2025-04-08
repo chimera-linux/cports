38cf7ca3d08f0386f9fa93ce5e6742a5f2f52c2d
q66 <q66@chimera-linux.org>
2025-04-08 03:29:56 +0200

    main/fdupes: update to 2.4.0

--- main/fdupes/template.py
+++ main/fdupes/template.py
@@ -1,5 +1,5 @@
 pkgname = "fdupes"
-pkgver = "2.3.2"
+pkgver = "2.4.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -8,7 +8,7 @@ pkgdesc = "Tool for identifying and/or deleting duplicate files"
 license = "MIT"
 url = "https://github.com/adrianlopezroche/fdupes"
 source = f"{url}/releases/download/v{pkgver}/fdupes-{pkgver}.tar.gz"
-sha256 = "808d8decbe7fa41cab407ae4b7c14bfc27b8cb62227540c3dcb6caf980592ac7"
+sha256 = "527b27a39d031dcbe1d29a220b3423228c28366c2412887eb72c25473d7b1736"
 hardening = ["vis", "cfi"]
 
 
