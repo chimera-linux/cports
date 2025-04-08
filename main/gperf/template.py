09c690f31f2e51c0d58f2d0533a780ef18eca186
q66 <q66@chimera-linux.org>
2025-04-08 03:32:42 +0200

    main/gperf: update to 3.2

--- main/gperf/template.py
+++ main/gperf/template.py
@@ -1,5 +1,5 @@
 pkgname = "gperf"
-pkgver = "3.1"
+pkgver = "3.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -7,6 +7,6 @@ pkgdesc = "Perfect hash function generator"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/gperf"
 source = f"$(GNU_SITE)/gperf/gperf-{pkgver}.tar.gz"
-sha256 = "588546b945bba4b70b6a3a616e80b4ab466e3f33024a352fc2198112cdbb3ae2"
+sha256 = "e0ddadebb396906a3e3e4cac2f697c8d6ab92dffa5d365a5bc23c7d41d30ef62"
 # FIXME
 hardening = ["vis", "!cfi", "!int"]
