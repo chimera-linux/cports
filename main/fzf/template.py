bfbf885f08a1adc99336a3e3b6cbb6afc6915edb
q66 <q66@chimera-linux.org>
2025-04-08 03:31:32 +0200

    main/fzf: update to 0.61.1

--- main/fzf/template.py
+++ main/fzf/template.py
@@ -1,6 +1,6 @@
 pkgname = "fzf"
-pkgver = "0.60.3"
-pkgrel = 1
+pkgver = "0.61.1"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 makedepends = ["ncurses-devel"]
@@ -8,7 +8,7 @@ pkgdesc = "Command-line fuzzy finder"
 license = "MIT"
 url = "https://github.com/junegunn/fzf"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "bdef337774050c26c6c4a6f38bc4ccb0901450854cd7f667cb3a330166a9ada5"
+sha256 = "702e1886dab359a1dab361b372c1ac05b2a1ed5d916aa0fbc08e8269a53a5171"
 
 
 def post_install(self):
