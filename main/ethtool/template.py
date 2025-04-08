3c1d319aa097245a75b166910e7b8430a54d1c0b
q66 <q66@chimera-linux.org>
2025-04-08 03:28:49 +0200

    main/ethtool: update to 6.14

--- main/ethtool/template.py
+++ main/ethtool/template.py
@@ -1,5 +1,5 @@
 pkgname = "ethtool"
-pkgver = "6.11"
+pkgver = "6.14"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "libtool"]
@@ -8,6 +8,6 @@ pkgdesc = "Utility for controlling network drivers and hardware"
 license = "GPL-2.0-only"
 url = "http://www.kernel.org/pub/software/network/ethtool"
 source = f"{url}/ethtool-{pkgver}.tar.xz"
-sha256 = "8d91f5c72ae3f25b7e88d4781279dcb320f71e30058914370b1c574c96b31202"
+sha256 = "9338bb00e492878d3bbe3cd2894e60db35813634c208db0b20f5c7ee84da69b1"
 # FIXME int
 hardening = ["vis", "cfi", "!int"]
