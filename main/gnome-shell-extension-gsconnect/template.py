7e3eb4237795c3e803335a5abfb1d3c9c60b0351
Erica Z <zerica@callcc.eu>
2025-04-08 12:33:13 +0200

    main/gnome-shell-extension-gsconnect: update to 62

--- main/gnome-shell-extension-gsconnect/template.py
+++ main/gnome-shell-extension-gsconnect/template.py
@@ -1,6 +1,6 @@
 pkgname = "gnome-shell-extension-gsconnect"
-pkgver = "58"
-pkgrel = 2
+pkgver = "62"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dinstalled_tests=false"]
 # Would've used weston-headless-run here instead of xvfb-run, but that runs
@@ -17,12 +17,13 @@ hostmakedepends = [
     "meson",
     "pkgconf",
 ]
+makedepends = ["dbus-devel"]
 depends = ["evolution-data-server", "gnome-shell", "gsound", "openssl3"]
 checkdepends = ["dbus", "gnome-shell", "xserver-xorg-xvfb"]
 pkgdesc = "KDE Connect implementation for GNOME"
 license = "GPL-2.0-or-later"
 url = "https://github.com/GSConnect/gnome-shell-extension-gsconnect"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "8142bb854fb3f2e5ba29e7f545af71aaf129ed26f5b32812a7ecb51b804919ef"
+sha256 = "34301389690937eabf4bb89703b2d050a8afd6664c965743df03fe52235ce13d"
 # All tests fail in latest release https://github.com/GSConnect/gnome-shell-extension-gsconnect/issues/1786
 options = ["!check"]
