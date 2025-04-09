7db46c6735b69d5dc2b8a148de559dffb8c81e63
Jens Reidel <adrian@travitia.xyz>
2025-04-09 20:04:31 +0200

    main/pv: fix build
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- main/pv/template.py
+++ main/pv/template.py
@@ -8,3 +8,7 @@ license = "GPL-3.0-or-later"
 url = "https://www.ivarch.com/programs/pv.shtml"
 source = f"https://www.ivarch.com/programs/sources/pv-{pkgver}.tar.gz"
 sha256 = "a35e92ec4ac0e8f380e8e840088167ae01014bfa008a3a9d6506b848079daedf"
+
+
+def post_extract(self):
+    self.rm("po/Makefile.in.in")
