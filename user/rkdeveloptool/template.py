c450e7cd59f30fc74616c84edf23842cb6bbb1fb
Jens Reidel <adrian@travitia.xyz>
2025-04-10 01:22:40 +0200

    user/rkdeveloptool: enforce minimum cmake version policy
    See 1cc54c89fe2b92d2c2192b3c52d8ebedea816a9c.
    
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- user/rkdeveloptool/template.py
+++ user/rkdeveloptool/template.py
@@ -2,6 +2,7 @@ pkgname = "rkdeveloptool"
 pkgver = "1.1.0"
 pkgrel = 0
 build_style = "cmake"
+configure_args = ["-DCMAKE_POLICY_VERSION_MINIMUM=3.5"]
 hostmakedepends = [
     "cmake",
     "ninja",
