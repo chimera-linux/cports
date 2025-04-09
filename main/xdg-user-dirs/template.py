7930c47fca7f99785fd62e7f59c741e999a8e179
Jens Reidel <adrian@travitia.xyz>
2025-04-09 19:47:45 +0200

    main/xdg-user-dirs: fix build
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- main/xdg-user-dirs/template.py
+++ main/xdg-user-dirs/template.py
@@ -17,3 +17,7 @@ source = (
     f"https://user-dirs.freedesktop.org/releases/xdg-user-dirs-{pkgver}.tar.gz"
 )
 sha256 = "ec6f06d7495cdba37a732039f9b5e1578bcb296576fde0da40edb2f52220df3c"
+
+
+def post_extract(self):
+    self.rm("po/Makefile.in.in")
