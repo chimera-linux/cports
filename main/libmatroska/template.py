1ecfc05b2d1ba329625735021543b3953d42148a
Jens Reidel <adrian@travitia.xyz>
2025-04-09 03:21:29 +0200

    main/libmatroska: enforce minimum cmake version policy
    See 1cc54c89fe2b92d2c2192b3c52d8ebedea816a9c.
    
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- main/libmatroska/template.py
+++ main/libmatroska/template.py
@@ -2,7 +2,10 @@ pkgname = "libmatroska"
 pkgver = "1.7.1"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DBUILD_SHARED_LIBS=ON"]
+configure_args = [
+    "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
+    "-DBUILD_SHARED_LIBS=ON",
+]
 hostmakedepends = [
     "cmake",
     "ninja",
