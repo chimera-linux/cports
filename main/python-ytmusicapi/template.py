06bd828bd3ef41a46994883aabcb2d1f4d28d54b
q66 <q66@chimera-linux.org>
2025-04-10 16:18:18 +0200

    main/python-ytmusicapi: update to 1.10.3

--- main/python-ytmusicapi/template.py
+++ main/python-ytmusicapi/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-ytmusicapi"
-pkgver = "1.10.2"
+pkgver = "1.10.3"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ pkgdesc = "Python library for the Youtube Music API"
 license = "MIT"
 url = "https://github.com/sigma67/ytmusicapi"
 source = f"$(PYPI_SITE)/y/ytmusicapi/ytmusicapi-{pkgver}.tar.gz"
-sha256 = "8fb4e63dcedd753aa0d755d7b1891df75e720e6ee9753d0e57a4dc015e411bf1"
+sha256 = "7235361ac9d5958d49a29f586eec55b1c83e90f7b063361e8a206e1cf4f76216"
 # like all tests need net
 options = ["!check"]
 
