1e8b9fbb11f9e647f2c66760387775eb5b2c7b78
q66 <q66@chimera-linux.org>
2025-04-10 16:54:12 +0200

    main/python-pebble: update to 5.1.1

--- main/python-pebble/template.py
+++ main/python-pebble/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pebble"
-pkgver = "5.1.0"
-pkgrel = 1
+pkgver = "5.1.1"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -16,4 +16,4 @@ pkgdesc = "Multi-threading and processing for python"
 license = "LGPL-3.0-or-later"
 url = "https://github.com/noxdafox/pebble"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "d4b9bd857fe34b287e522fd2f206e53b495b4650d57837fe4fa8c8c5b854c668"
+sha256 = "8e91255f28641da9b35c906c8dda6b0a5dd57c9adedf0271cee50312708aa03a"
