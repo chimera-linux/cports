1efd6d212dd36e0e73b83f3976b34d6bba32a2b0
q66 <q66@chimera-linux.org>
2025-04-10 16:28:19 +0200

    main/python-roman-numerals-py: new package

---
+++ main/python-roman-numerals-py/template.py
@@ -0,0 +1,20 @@
+pkgname = "python-roman-numerals-py"
+pkgver = "3.1.0"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-flit_core",
+    "python-installer",
+]
+depends = ["python"]
+checkdepends = ["python-pytest"]
+pkgdesc = "Manipulate Roman numerals"
+license = "0BSD OR CC0-1.0"
+url = "https://github.com/AA-Turner/roman-numerals"
+source = f"$(PYPI_SITE)/r/roman-numerals-py/roman_numerals_py-{pkgver}.tar.gz"
+sha256 = "be4bf804f083a4ce001b5eb7e3c0862479d10f94c936f6c4e5f250aa5ff5bd2d"
+
+
+def post_install(self):
+    self.install_license("LICENCE.rst")
