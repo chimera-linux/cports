b201cd2053d08638c28a13e6d9d44bf1b27e7453
q66 <q66@chimera-linux.org>
2025-04-10 16:51:48 +0200

    main/c-ares: update to 1.34.5

--- main/c-ares/template.py
+++ main/c-ares/template.py
@@ -1,5 +1,5 @@
 pkgname = "c-ares"
-pkgver = "1.34.4"
+pkgver = "1.34.5"
 pkgrel = 0
 build_style = "gnu_configure"
 # circular gtest
@@ -9,7 +9,7 @@ pkgdesc = "C library for asynchronous DNS requests"
 license = "MIT"
 url = "https://c-ares.haxx.se"
 source = f"https://github.com/c-ares/c-ares/releases/download/v{pkgver}/c-ares-{pkgver}.tar.gz"
-sha256 = "fa38dbed659ee4cc5a32df5e27deda575fa6852c79a72ba1af85de35a6ae222f"
+sha256 = "7d935790e9af081c25c495fd13c2cfcda4792983418e96358ef6e7320ee06346"
 hardening = ["vis", "!cfi"]
 # does not like the sandbox
 options = ["!check"]
