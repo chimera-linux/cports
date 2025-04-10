b5734371a2134de4331cc8998da9e0e3df5e6d57
q66 <q66@chimera-linux.org>
2025-04-10 16:51:13 +0200

    main/python-pycodestyle: update to 2.13.0

--- main/python-pycodestyle/template.py
+++ main/python-pycodestyle/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pycodestyle"
-pkgver = "2.12.1"
-pkgrel = 1
+pkgver = "2.13.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -14,7 +14,7 @@ pkgdesc = "Python style guide checker"
 license = "MIT"
 url = "https://github.com/PyCQA/pycodestyle"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "231f65fbf5558e342cbad275245accb8a988d637cbeaf66508dd890f3d2d60fa"
+sha256 = "b1a4db0d9b8285f6643bcdb41362be6d6c94b891b13ead09c57a2513c46b717b"
 
 
 def post_install(self):
