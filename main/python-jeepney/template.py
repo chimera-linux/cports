571b2dc51f3b4d49dd8c185b36d67f245272c7f9
q66 <q66@chimera-linux.org>
2025-04-10 17:10:00 +0200

    main/python-jeepney: update to 0.9.0

--- main/python-jeepney/template.py
+++ main/python-jeepney/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-jeepney"
-pkgver = "0.8.0"
-pkgrel = 1
+pkgver = "0.9.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -12,7 +12,7 @@ pkgdesc = "Python DBus protocol wrapper"
 license = "MIT"
 url = "https://gitlab.com/takluyver/jeepney"
 source = f"$(PYPI_SITE)/j/jeepney/jeepney-{pkgver}.tar.gz"
-sha256 = "5efe48d255973902f6badc3ce55e2aa6c5c3b3bc642059ef3a91247bcfcc5806"
+sha256 = "cf0e9e845622b81e4a28df94c40345400256ec608d0e55bb8a3feaa9163f5732"
 # bunch more deps
 options = ["!check"]
 
