c140d824433a579bc917c63d48396c7bea6d120d
q66 <q66@chimera-linux.org>
2025-04-10 17:59:32 +0200

    main/python-roman-numerals-py: disable check

--- main/python-roman-numerals-py/template.py
+++ main/python-roman-numerals-py/template.py
@@ -14,6 +14,8 @@ license = "0BSD OR CC0-1.0"
 url = "https://github.com/AA-Turner/roman-numerals"
 source = f"$(PYPI_SITE)/r/roman-numerals-py/roman_numerals_py-{pkgver}.tar.gz"
 sha256 = "be4bf804f083a4ce001b5eb7e3c0862479d10f94c936f6c4e5f250aa5ff5bd2d"
+# cyclic
+options = ["!check"]
 
 
 def post_install(self):
