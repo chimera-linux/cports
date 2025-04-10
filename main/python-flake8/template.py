40899bc70afdc740e4f89fc7d4de3da669cbf0d9
q66 <q66@chimera-linux.org>
2025-04-10 17:16:46 +0200

    main/python-flake8: update to 7.2.0

--- main/python-flake8/template.py
+++ main/python-flake8/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-flake8"
-pkgver = "7.1.2"
-pkgrel = 1
+pkgver = "7.2.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -14,7 +14,7 @@ pkgdesc = "Python tool for style guide enforcement"
 license = "MIT"
 url = "https://flake8.pycqa.org"
 source = f"https://github.com/PyCQA/flake8/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "60364d3593a8fd8a22f3ffcd751b29d0b61945e975754115bb9316bef157e03e"
+sha256 = "a4e94891bcada0adf8b5bdf6be9f8a81bd09b0951674b52ca3e84f21ba0fe651"
 # some failures in new testsuite
 options = ["!check"]
 
