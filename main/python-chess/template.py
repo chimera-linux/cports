10caad819fe78642de8988d9ae35a09773a90b00
q66 <q66@chimera-linux.org>
2025-04-10 17:50:39 +0200

    main/python-chess: update to 1.11.2

--- main/python-chess/template.py
+++ main/python-chess/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-chess"
-pkgver = "1.10.0"
-pkgrel = 1
+pkgver = "1.11.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -14,6 +14,6 @@ pkgdesc = "Pure Python chess library with move generation and validation"
 license = "GPL-3.0-or-later"
 url = "https://github.com/niklasf/python-chess"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "832fa7d589ffe916f5e84f08ece0d634dfb50568867932e525edcebb573041df"
+sha256 = "cd920994700e700c0354f56b73a2591f652a44fa2ff28552d89c975bd820c647"
 # tests not runnable >=3.13
 options = ["!check"]
