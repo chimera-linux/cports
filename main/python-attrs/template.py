0c61a93c071a51d22ab8e56bdd4c2e41227e4e51
q66 <q66@chimera-linux.org>
2025-04-10 17:55:45 +0200

    main/python-attrs: update to 25.3.0

--- main/python-attrs/template.py
+++ main/python-attrs/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-attrs"
-pkgver = "25.1.0"
-pkgrel = 1
+pkgver = "25.3.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-installer", "python-hatch_vcs"]
 checkdepends = ["python-pytest"]  # and other stuff, but does not matter
@@ -9,7 +9,7 @@ pkgdesc = "Attributes without boilerplate"
 license = "MIT"
 url = "https://attrs.readthedocs.io"
 source = f"$(PYPI_SITE)/a/attrs/attrs-{pkgver}.tar.gz"
-sha256 = "1c97078a80c814273a76b2a298a932eb681c87415c11dee0a6921de7f1b02c3e"
+sha256 = "75d7cefc7fb576747b2c81b4442d4d4a1ce0900973527c011d1030fd3bf4af1b"
 # dependency of pytest
 options = ["!check"]
 
