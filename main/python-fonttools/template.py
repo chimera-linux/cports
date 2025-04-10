23663aa6b2589e6280b6180b9dd3434bd30359f8
q66 <q66@chimera-linux.org>
2025-04-10 17:14:05 +0200

    main/python-fonttools: update to 4.57.0

--- main/python-fonttools/template.py
+++ main/python-fonttools/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-fonttools"
-pkgver = "4.56.0"
-pkgrel = 1
+pkgver = "4.57.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -17,7 +17,7 @@ pkgdesc = "Library to manipulate font files from Python"
 license = "MIT AND OFL-1.1 AND BSD-3-Clause AND Apache-2.0"
 url = "https://github.com/fonttools/fonttools"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "aa86d299fafd9b9c24c56559eac5739e96885cb411d86a8f318a169090a42cb0"
+sha256 = "97dd3259e8095328a14a64de63ce7d96533d453ceee32cbd96f991c421116219"
 
 
 def init_check(self):
