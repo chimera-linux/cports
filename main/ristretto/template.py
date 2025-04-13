e083a7bcb761164c278a94c56223e00d90d13550
triallax <triallax@tutanota.com>
2025-04-14 00:37:33 +0100

    main/ristretto: update to 0.13.4

--- main/ristretto/template.py
+++ main/ristretto/template.py
@@ -1,16 +1,16 @@
 pkgname = "ristretto"
-pkgver = "0.13.3"
+pkgver = "0.13.4"
 pkgrel = 0
-build_style = "gnu_configure"
+build_style = "meson"
 hostmakedepends = [
-    "automake",
-    "gettext-devel",
+    "gettext",
+    "glib-devel",
+    "meson",
     "pkgconf",
-    "slibtool",
-    "xfce4-dev-tools",
 ]
 makedepends = [
     "cairo-devel",
+    "exo-devel",
     "file-devel",
     "glib-devel",
     "gtk+3-devel",
@@ -23,5 +23,5 @@ makedepends = [
 pkgdesc = "Xfce image viewer"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/apps/ristretto/start"
-source = f"$(XFCE_SITE)/apps/ristretto/{pkgver[:-2]}/ristretto-{pkgver}.tar.bz2"
-sha256 = "5b9172ef704ae192a5338df6bee4e91a59edc65618c375bb4433ffb38e2126cb"
+source = f"$(XFCE_SITE)/apps/ristretto/{pkgver[:-2]}/ristretto-{pkgver}.tar.xz"
+sha256 = "a84ef8cb80638681d9b9ef09cddff86a5d7a0e028603b4a601cf0ff6c2869ce8"
