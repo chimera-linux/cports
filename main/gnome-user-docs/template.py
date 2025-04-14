57f265137f5300f75a971a854b02ae2d5be7cdcc
triallax <triallax@tutanota.com>
2025-04-14 02:44:03 +0100

    main/gnome-user-docs: update to 48.1

--- main/gnome-user-docs/template.py
+++ main/gnome-user-docs/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-user-docs"
-pkgver = "47.5"
+pkgver = "48.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -15,5 +15,5 @@ pkgdesc = "User documentation for GNOME"
 license = "CC-BY-3.0"
 url = "https://gitlab.gnome.org/GNOME/gnome-user-docs"
 source = f"$(GNOME_SITE)/gnome-user-docs/{pkgver[:-2]}/gnome-user-docs-{pkgver}.tar.xz"
-sha256 = "727cd30d80a801412be4085bfedc870f962a49fd057d904c6d42534bae1f2133"
+sha256 = "ac973d924e00545a1458d86a110d4773e6bbe37c372840d76ed640cb268c61fd"
 options = ["!splitdoc"]
