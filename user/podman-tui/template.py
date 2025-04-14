2c408fbae2d4c9ecf46fb5f6b92280b50d4b73b3
ttyyls <contact@behri.org>
2025-04-14 10:23:50 +0100

    user/podman-tui: update to 1.5.0

--- user/podman-tui/template.py
+++ user/podman-tui/template.py
@@ -1,6 +1,6 @@
 pkgname = "podman-tui"
-pkgver = "1.4.0"
-pkgrel = 2
+pkgver = "1.5.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 makedepends = [
@@ -25,7 +25,7 @@ pkgdesc = "Podman terminal UI"
 license = "Apache-2.0"
 url = "https://github.com/containers/podman-tui"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0b385f11b7cff1143dabe4972c0fe2032a583024660d837658626e9a8e703d2f"
+sha256 = "d9ba16d37f959d7ae5ca6650c3ccc7b0e1a726215791c99604f8f5955ee8f61d"
 
 
 def post_extract(self):
