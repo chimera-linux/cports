325e845a9366c40600fdcfbfe18c57d29d4e8a77
triallax <triallax@tutanota.com>
2025-04-14 02:38:37 +0100

    main/gnome-online-accounts: update to 3.54.2

--- main/gnome-online-accounts/template.py
+++ main/gnome-online-accounts/template.py
@@ -1,8 +1,9 @@
 pkgname = "gnome-online-accounts"
-pkgver = "3.54.1"
-pkgrel = 1
+pkgver = "3.54.2"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
+    "--libexecdir=/usr/lib",  # XXX drop libexec
     "-Ddocumentation=false",
     "-Dintrospection=true",
     "-Dvapi=true",
@@ -38,7 +39,7 @@ pkgdesc = "GNOME service to access online accounts"
 license = "LGPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-online-accounts"
 source = f"$(GNOME_SITE)/gnome-online-accounts/{pkgver[:-2]}/gnome-online-accounts-{pkgver}.tar.xz"
-sha256 = "9d058b3aac8b2d8b6b2ae7cb57c8a8abe539e73c88220c315b5b99f87ce6efd8"
+sha256 = "0f7f3d29c0666ffbcc503f3e0f3396362effdb9317d41664ce65df147ef35372"
 options = ["!cross"]
 
 
