69580a3b2faa84db687251d0e5931c6fc94c1421
triallax <triallax@tutanota.com>
2025-04-14 02:22:47 +0100

    main/intel-media-driver: update to 25.2.0

--- main/intel-media-driver/template.py
+++ main/intel-media-driver/template.py
@@ -1,10 +1,11 @@
 pkgname = "intel-media-driver"
-pkgver = "25.1.4"
+pkgver = "25.2.0"
 pkgrel = 0
 # doesn't build elsewhere
 archs = ["x86_64"]
 build_style = "cmake"
 configure_args = [
+    "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
     "-DINSTALL_DRIVER_SYSCONF=OFF",
     "-DMEDIA_BUILD_FATAL_WARNINGS=OFF",
 ]
@@ -24,7 +25,7 @@ pkgdesc = "Intel Media Driver for VAAPI"
 license = "BSD-3-Clause"
 url = "https://github.com/intel/media-driver"
 source = f"{url}/archive/refs/tags/intel-media-{pkgver}.tar.gz"
-sha256 = "6339bba3168102f5a608f447076c9efb2a8194e847a9dac219fbc13324192a76"
+sha256 = "be7a1aa9341c637a6adb26164adffeffcad859980246fab4df04f3d7d8e2114b"
 # INT: crashes during certain vaapi decode (twitch.tv?)
 hardening = ["vis", "!cfi", "!int"]
 
