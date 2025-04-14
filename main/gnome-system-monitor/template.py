830ed20496fc28d6ed5ea1bd488e488548e55e53
triallax <triallax@tutanota.com>
2025-04-14 01:59:07 +0100

    main/gnome-system-monitor: update to 48.1

--- main/gnome-system-monitor/template.py
+++ main/gnome-system-monitor/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-system-monitor"
-pkgver = "48.0"
+pkgver = "48.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -27,5 +27,5 @@ pkgdesc = "GNOME system monitor"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/SystemMonitor"
 source = f"$(GNOME_SITE)/gnome-system-monitor/{pkgver[0 : pkgver.find('.')]}/gnome-system-monitor-{pkgver}.tar.xz"
-sha256 = "e4e5b345fbd4d7dc2f40ad6c62305ae5c7cc2b465ce95988692a54af347532a3"
+sha256 = "133c3a6e286366e659fd2d80582409a7bd5edae456e63c4f69ccf66ed6fc6638"
 tool_flags = {"CFLAGS": ["-D_BSD_SOURCE"]}
