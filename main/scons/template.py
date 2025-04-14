08cbbe989a7b55876587981799b8c1ba58ffb915
triallax <triallax@tutanota.com>
2025-04-14 18:13:44 +0100

    main/scons: update to 4.9.1

--- main/scons/template.py
+++ main/scons/template.py
@@ -1,6 +1,6 @@
 pkgname = "scons"
-pkgver = "4.8.1"
-pkgrel = 1
+pkgver = "4.9.1"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,7 +13,7 @@ pkgdesc = "Build system nobody likes"
 license = "MIT"
 url = "https://scons.org"
 source = f"https://github.com/SCons/scons/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "9735ac24f345a11c76561991e3c3bdf943f97c772ded54156243e7f4ea5f4bc5"
+sha256 = "810c3f82c23758f8eaa23f7263363e1ac1822253dc8719ffa897ee77604bbe02"
 # uhhh, nah
 options = ["!check"]
 
