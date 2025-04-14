dfbc8ffbd7d75b302168831b36f642129f0d7cf9
triallax <triallax@tutanota.com>
2025-04-14 18:12:13 +0100

    main/sakura: update to 3.8.9

--- main/sakura/template.py
+++ main/sakura/template.py
@@ -1,5 +1,5 @@
 pkgname = "sakura"
-pkgver = "3.8.8"
+pkgver = "3.8.9"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "gettext", "ninja", "perl", "pkgconf"]
@@ -8,5 +8,5 @@ pkgdesc = "Libvte-based terminal emulator"
 license = "GPL-2.0-only"
 url = "https://launchpad.net/sakura"
 source = f"{url}/trunk/{pkgver}/+download/sakura-{pkgver}.tar.gz"
-sha256 = "9f7e6b4e00401279686ca3ff11bab988d817b2df37ddf42d16cd3c3090560c38"
+sha256 = "0bf1151b08c05e3d151e827ee3f8f68639133f5462e3e0761f82946aa3fe50df"
 hardening = ["cfi", "vis"]
