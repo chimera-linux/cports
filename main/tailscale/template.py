a947eb00e748e9b7ab048e47ee562af4586e52ca
Erica Z <zerica@callcc.eu>
2025-04-18 08:18:59 +0200

    main/tailscale: update to 1.82.5

--- main/tailscale/template.py
+++ main/tailscale/template.py
@@ -1,6 +1,6 @@
 pkgname = "tailscale"
-pkgver = "1.82.0"
-pkgrel = 1
+pkgver = "1.82.5"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     "-ldflags="
@@ -15,7 +15,7 @@ pkgdesc = "Mesh VPN daemon based on WireGuard"
 license = "BSD-3-Clause"
 url = "https://github.com/tailscale/tailscale"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "d846c1064dc9fb23559e260c1faf3d36b4bdda5457b1e069dceec3add8bb7e55"
+sha256 = "09685251057c96b7aa01e62b982a90824559ffd7113c8b69b9fc454c611f40a9"
 # check: needs network access
 # cross: completions with host bin
 options = ["!check", "!cross"]
