29fbbeb60f0596027f5c63c6e8ec15b181e95f59
Ayush Agarwal <ayush@ayushnix.com>
2025-04-21 17:29:22 +0530

    user/dnscrypt-proxy: update to 2.1.11
    The default configuration file, dnscrypt-proxy.toml, was removed in
    favor of patching the configuration file in builddir. A helper python
    script, generate-domains-blocklist.py, along with other default
    configuration files were added for a better out of the box experience.
    
    The dnscrypt-proxy service file was updated to the latest dinit service
    file syntax.

--- user/dnscrypt-proxy/template.py
+++ user/dnscrypt-proxy/template.py
@@ -1,13 +1,14 @@
 pkgname = "dnscrypt-proxy"
-pkgver = "2.1.7"
-pkgrel = 5
+pkgver = "2.1.11"
+pkgrel = 0
 build_style = "go"
+make_build_args = ["./dnscrypt-proxy"]
 hostmakedepends = ["go"]
 pkgdesc = "Tool for securing communications between a client and a DNS resolver"
 license = "ISC"
 url = "https://dnscrypt.info"
 source = f"https://github.com/DNSCrypt/dnscrypt-proxy/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "6394cd2d73dedca9317aeee498b6c2520b841cea042d83f398c3355a13c50f7c"
+sha256 = "3f17d952843f48d80f91198e2bde7a12bb98f857031ca92085451d33f6c149fa"
 # no tests included
 options = ["!check"]
 
@@ -17,15 +18,17 @@ def post_extract(self):
     self.rm("vendor", recursive=True)
 
 
-def build(self):
-    self.golang.build(wrksrc="dnscrypt-proxy")
-
-
 def post_install(self):
     self.install_sysusers(self.files_path / "sysusers.conf")
     self.install_tmpfiles(self.files_path / "tmpfiles.conf")
-    self.install_file(
-        self.files_path / "dnscrypt-proxy.toml", "etc/dnscrypt-proxy"
-    )
     self.install_service(self.files_path / "dnscrypt-proxy")
+    self.install_files(
+        "utils/generate-domains-blocklist", "usr/share/dnscrypt-proxy"
+    )
+    for f in (self.cwd / "dnscrypt-proxy").glob("example-*"):
+        self.install_file(
+            f"dnscrypt-proxy/{f.name}",
+            "usr/share/dnscrypt-proxy",
+            name=f"{f.name.removeprefix('example-')}",
+        )
     self.install_license("LICENSE")
