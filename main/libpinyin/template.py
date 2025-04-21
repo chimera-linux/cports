7f4bdfb5e227da50e63b9787935960b189876ad3
q66 <q66@chimera-linux.org>
2025-04-21 21:33:52 +0200

    main/libpinyin: update to 2.10.1

--- main/libpinyin/template.py
+++ main/libpinyin/template.py
@@ -1,6 +1,6 @@
 pkgname = "libpinyin"
-pkgver = "2.8.1"
-pkgrel = 1
+pkgver = "2.10.1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-dbm=KyotoCabinet", "--enable-libzhuyin"]
 hostmakedepends = ["pkgconf", "automake", "libtool", "gettext"]
@@ -9,7 +9,7 @@ pkgdesc = "Algorithms core for intelligent sentence-based Chinese pinyin IMEs"
 license = "GPL-3.0-or-later"
 url = "https://github.com/libpinyin/libpinyin"
 source = f"{url}/releases/download/{pkgver}/libpinyin-{pkgver}.tar.gz"
-sha256 = "353154f06d71dd0737b77ddcb27cb0dcaddb00f7ccd695bc0314bb42050e9050"
+sha256 = "bc5fd25724f9863bc329d2aa3d400dce28130efdb819bb96d42db52b0845d37f"
 
 
 @subpackage("libpinyin-devel")
