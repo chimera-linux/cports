77e0feb77039eec194c54fdf526375ac9d800add
q66 <q66@chimera-linux.org>
2025-04-21 18:03:59 +0200

    user/alsa-utils: update to 1.2.14

--- user/alsa-utils/template.py
+++ user/alsa-utils/template.py
@@ -1,5 +1,5 @@
 pkgname = "alsa-utils"
-pkgver = "1.2.13"
+pkgver = "1.2.14"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-udev-rules-dir=no"]
@@ -16,4 +16,4 @@ pkgdesc = "ALSA utilities"
 license = "LGPL-2.1-or-later"
 url = "https://www.alsa-project.org"
 source = f"{url}/files/pub/utils/alsa-utils-{pkgver}.tar.bz2"
-sha256 = "1702a6b1cdf9ba3e996ecbc1ddcf9171e6808f5961d503d0f27e80ee162f1daa"
+sha256 = "0794c74d33fed943e7c50609c13089e409312b6c403d6ae8984fc429c0960741"
