30a213a39613118759f363a385760137f50ca1ae
q66 <q66@chimera-linux.org>
2025-04-23 00:57:42 +0200

    user/wasm-tools: update to 1.229.0

--- user/wasm-tools/template.py
+++ user/wasm-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "wasm-tools"
-pkgver = "1.227.1"
+pkgver = "1.229.0"
 pkgrel = 0
 # wasmtime
 archs = ["aarch64", "riscv64", "x86_64"]
@@ -13,7 +13,7 @@ pkgdesc = "CLI for manipulation of wasm modules"
 license = "Apache-2.0 WITH LLVM-exception OR Apache-2.0 OR MIT"
 url = "https://github.com/bytecodealliance/wasm-tools"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "a806566b60fa59d76704b9a3493b9bb7f33a6c7430da0f94f924c41aea6684b4"
+sha256 = "ff3769834160b5bf1733daeeae14e6d74df85edcf49f300601927ef574790f9b"
 # needs checked out submodules and whatever
 options = ["!check"]
 
