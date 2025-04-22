47d5f8497d20c589c20c9a495293cb2750881c9e
q66 <q66@chimera-linux.org>
2025-04-23 01:09:14 +0200

    main/linux-stable: update to 6.13.3

--- main/linux-stable/template.py
+++ main/linux-stable/template.py
@@ -1,6 +1,6 @@
 # update linux-stable-zfs-bin when bumping
 pkgname = "linux-stable"
-pkgver = "6.14.2"
+pkgver = "6.14.3"
 pkgrel = 0
 archs = [
     "aarch64",
@@ -14,6 +14,7 @@ archs = [
 build_style = "linux-kernel"
 configure_args = ["FLAVOR=generic", f"RELEASE={pkgrel}"]
 make_dir = "build"
+make_install_env = {"ZSTD_CLEVEL": "9"}
 hostmakedepends = ["base-kernel-devel"]
 depends = ["base-kernel"]
 provides = ["linux"]
@@ -21,7 +22,7 @@ pkgdesc = f"Linux kernel {pkgver[0 : pkgver.rfind('.')]}.x"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
-sha256 = "c5c682a354ea3190139357a57d34a79e5c37221ace823a938e10116b577a2e1b"
+sha256 = "91c6c322c32568f09922a02ed35f46372ca8526714c6555b3115b9e9c156652a"
 # no meaningful checking to be done
 options = [
     "!check",
