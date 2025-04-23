2e03c837c98023e7675aae78e0609559bfc0cb85
q66 <q66@chimera-linux.org>
2025-04-24 00:12:28 +0200

    main/debian-archive-keyring: update to 2025.1

--- main/debian-archive-keyring/template.py
+++ main/debian-archive-keyring/template.py
@@ -1,11 +1,11 @@
 pkgname = "debian-archive-keyring"
-pkgver = "2023.4"
+pkgver = "2025.1"
 pkgrel = 0
 pkgdesc = "Debian archive keyring"
 license = "custom:none"
 url = "https://salsa.debian.org/release-team/debian-archive-keyring"
 source = f"$(DEBIAN_SITE)/main/d/debian-archive-keyring/debian-archive-keyring_{pkgver}_all.deb"
-sha256 = "6e93a87b9e50bd81518880ec07a62f95d7d8452f4aa703f5b0a3076439f1022c"
+sha256 = "9ea7778e443144ca490668737a8ab22dd3e748bb99e805e22ec055abeb3c7fac"
 
 
 def install(self):
