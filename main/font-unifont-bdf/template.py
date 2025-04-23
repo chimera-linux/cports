17cf5cfd7ea8c2b23ef81acf8a0b9ccec70353ba
q66 <q66@chimera-linux.org>
2025-04-24 00:24:18 +0200

    main/font-unifont-bdf: update to 16.0.03

--- main/font-unifont-bdf/template.py
+++ main/font-unifont-bdf/template.py
@@ -1,5 +1,5 @@
 pkgname = "font-unifont-bdf"
-pkgver = "16.0.02"
+pkgver = "16.0.03"
 pkgrel = 0
 depends = ["font-util"]
 pkgdesc = "GNU Unifont Glyphs"
@@ -7,7 +7,7 @@ subdesc = "BDF"
 license = "GPL-2.0-or-later"
 url = "http://unifoundry.com/unifont/index.html"
 source = f"http://unifoundry.com/pub/unifont/unifont-{pkgver}/font-builds/unifont-{pkgver}.bdf.gz"
-sha256 = "521f2b92e8b6bd4ea190cea049a039dde359a6e2cae9458e45d696008fa6997f"
+sha256 = "7f3d1664ac1c051f59a1a1360dd654f78d82c246a688c342e863cec7f6ba95d4"
 
 
 def install(self):
