04b7f4471ac178b1ad98cbe1359279873aa4e56d
q66 <q66@chimera-linux.org>
2025-04-23 23:51:52 +0200

    main/ansible: update to 11.5.0

--- main/ansible/template.py
+++ main/ansible/template.py
@@ -1,5 +1,5 @@
 pkgname = "ansible"
-pkgver = "11.4.0"
+pkgver = "11.5.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -18,7 +18,7 @@ pkgdesc = "Configuration management and multinode orchestration framework"
 license = "GPL-3.0-or-later"
 url = "https://ansible.com"
 source = f"$(PYPI_SITE)/a/ansible/ansible-{pkgver}.tar.gz"
-sha256 = "d25a7f26bf5821f8043bc806019822fd2810bd65e6b6bafb698bbeedadba72bf"
+sha256 = "18a3fc73120a49ade9a9a67eb8f9d4f5009d2106c34ffeb9663ad928b76ed59b"
 # many collections either require additional Python modules, or the tests
 # require dependencies that aren't in the tarball, or they're just broken
 # (possibly due to Python version compat issues)
