d1ae03e5ea2e78e24c6265c7cc473db16a316c83
q66 <q66@chimera-linux.org>
2025-04-24 00:31:24 +0200

    main/qemu: update to 10.0.0

--- main/qemu/template.py
+++ main/qemu/template.py
@@ -1,5 +1,5 @@
 pkgname = "qemu"
-pkgver = "9.2.3"
+pkgver = "10.0.0"
 pkgrel = 0
 build_style = "gnu_configure"
 # TODO vde
@@ -110,7 +110,7 @@ pkgdesc = "Generic machine emulator and virtualizer"
 license = "GPL-2.0-only AND LGPL-2.1-only"
 url = "https://qemu.org"
 source = f"https://download.qemu.org/qemu-{pkgver}.tar.xz"
-sha256 = "baed494270c361bf69816acc84512e3efed71c7a23f76691642b80bc3de7693e"
+sha256 = "22c075601fdcf8c7b2671a839ebdcef1d4f2973eb6735254fd2e1bd0f30b3896"
 tool_flags = {
     # see libbpf comment about bpf headers
     "CFLAGS": ["-I/usr/include/bpf/uapi"],
