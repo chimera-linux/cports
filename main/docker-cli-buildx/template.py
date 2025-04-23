28ad137a3a85bd25fa285d4533d84bc2e6aca89e
q66 <q66@chimera-linux.org>
2025-04-24 00:15:13 +0200

    main/docker-cli-buildx: update to 0.23.0

--- main/docker-cli-buildx/template.py
+++ main/docker-cli-buildx/template.py
@@ -1,6 +1,6 @@
 pkgname = "docker-cli-buildx"
-pkgver = "0.22.0"
-pkgrel = 1
+pkgver = "0.23.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X github.com/docker/buildx/version.Version=v{pkgver}",
@@ -14,7 +14,7 @@ pkgdesc = "Docker CLI plugin for extended build capabilities with BuildKit"
 license = "Apache-2.0"
 url = "https://github.com/docker/buildx"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "afcfea302f639f86299d41657f2ea6ca8df0d8cc3e243c299c8ada0ecfbb1fc4"
+sha256 = "a6c8d86c55a733ddd6aeac8da0624aa9d0cb966846e955399f5a075590499fd8"
 # some tests rely on network
 options = ["!check"]
 
