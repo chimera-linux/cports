c88a24a116c8cbc53bbe29d6ac8490be5346a726
ttyyls <contact@behri.org>
2025-04-24 23:45:36 +0100

    user/direnv: update to 2.36.0

--- user/direnv/template.py
+++ user/direnv/template.py
@@ -1,13 +1,13 @@
 pkgname = "direnv"
-pkgver = "2.35.0"
-pkgrel = 4
+pkgver = "2.36.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Environment variables loader"
 license = "MIT"
 url = "https://github.com/direnv/direnv"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "a7aaec49d1b305f0745dad364af967fb3dc9bb5befc9f29d268d528b5a474e57"
+sha256 = "edb89ca67ef46a792d4e20177dae9dbd229e26dcbcfb17baa9645c1ff7cc47b0"
 
 
 def post_install(self):
