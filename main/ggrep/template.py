cbdb20e1f3e8f9d5285205a6f59611076943511a
q66 <q66@chimera-linux.org>
2025-04-25 00:33:17 +0200

    main/ggrep: update to 3.12

--- main/ggrep/template.py
+++ main/ggrep/template.py
@@ -1,14 +1,16 @@
 pkgname = "ggrep"
-pkgver = "3.11"
-pkgrel = 1
+pkgver = "3.12"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--program-prefix=g"]
-hostmakedepends = ["automake", "libtool", "pkgconf", "gettext-devel"]
+# fails to regen due to gnulib junk
+configure_gen = []
+hostmakedepends = ["pkgconf"]
 makedepends = ["pcre2-devel"]
 checkdepends = ["perl"]
 pkgdesc = "GNU grep"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/grep"
 source = f"$(GNU_SITE)/grep/grep-{pkgver}.tar.xz"
-sha256 = "1db2aedde89d0dea42b16d9528f894c8d15dae4e190b59aecc78f5a951276eab"
+sha256 = "2649b27c0e90e632eadcd757be06c6e9a4f48d941de51e7c0f83ff76408a07b9"
 hardening = ["vis", "cfi"]
