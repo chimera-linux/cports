49a59439f863ee5b79ae12a767858061afb0975c
jbz <justin.berthault@zaclys.net>
2025-04-25 09:09:47 +0200

    user/lazygit: update to 0.49.0

--- user/lazygit/template.py
+++ user/lazygit/template.py
@@ -1,6 +1,6 @@
 pkgname = "lazygit"
-pkgver = "0.48.0"
-pkgrel = 2
+pkgver = "0.49.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = [f"-ldflags=-X main.version=v{pkgver}"]
 hostmakedepends = ["go"]
@@ -9,7 +9,7 @@ pkgdesc = "Terminal UI for git commands"
 license = "MIT"
 url = "https://github.com/jesseduffield/lazygit"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "b8507602e19a0ab7b1e2c9f26447df87d068be9bf362394106bad8a56ce25f82"
+sha256 = "5f74970a7303f22e5cb47a99b0480c213e280c10585edbf6ae16240fc4e55fa7"
 # need to be in git checkout
 options = ["!check"]
 
