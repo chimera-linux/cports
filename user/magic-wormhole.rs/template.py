c300db1de04d9bb10930ffc8db74876e89abc4bd
Erica Z <zerica@callcc.eu>
2025-04-27 14:18:18 +0200

    user/magic-wormhole.rs: update to 0.7.6

--- user/magic-wormhole.rs/template.py
+++ user/magic-wormhole.rs/template.py
@@ -1,5 +1,5 @@
 pkgname = "magic-wormhole.rs"
-pkgver = "0.7.5"
+pkgver = "0.7.6"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = ["--bins"]
@@ -9,13 +9,9 @@ pkgdesc = "Magic Wormhole CLI client"
 license = "EUPL-1.2"
 url = "https://github.com/magic-wormhole/magic-wormhole.rs"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "b0560c3310e7ab3c9361d6eae7a471658ed5b5ac991f22094b8e737c8f6f1a64"
+sha256 = "1d76e80108291f0a31e1a0e2e1d6199decb55bec73bc725baacb93ea0ae06e5e"
 # generates completions with host bin
 options = ["!check", "!cross"]
-
-
-def post_extract(self):
-    self.rm(".cargo/config")
 
 
 def post_build(self):
