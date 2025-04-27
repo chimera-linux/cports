3fd8ea65d58241cf9aa1a62300a805cbf64d3f49
cassiofb-dev <cassiofb.souza@gmail.com>
2025-04-27 20:07:12 -0300

    user/wgcf: update to 2.2.26

--- user/wgcf/template.py
+++ user/wgcf/template.py
@@ -1,13 +1,13 @@
 pkgname = "wgcf"
-pkgver = "2.2.25"
-pkgrel = 3
+pkgver = "2.2.26"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Unofficial CLI for Cloudflare Warp"
 license = "MIT"
 url = "https://github.com/ViRb3/wgcf"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1f994953aa4e9d6718dd7629957db9ee82f766e22975808cded1dcaf722734d8"
+sha256 = "386e8ec5985d86ab25588070a737f761a6687127162dcc990370bf77eb108c1d"
 
 
 def post_build(self):
