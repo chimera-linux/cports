51a11fe554d83c862ba47536bfe02b9ffbca2e7f
q66 <q66@chimera-linux.org>
2025-04-30 00:14:47 +0200

    main/libheif: update to 1.19.8

--- main/libheif/template.py
+++ main/libheif/template.py
@@ -1,5 +1,5 @@
 pkgname = "libheif"
-pkgver = "1.19.7"
+pkgver = "1.19.8"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -24,7 +24,7 @@ pkgdesc = "HEIF and AVIF file format decoder and encoder"
 license = "LGPL-3.0-or-later"
 url = "http://www.libheif.org"
 source = f"https://github.com/strukturag/libheif/archive/v{pkgver}.tar.gz"
-sha256 = "8334c7c418f82c30c9bec1f46e6abfd5a8d3c420a3210d5505eb1868696ce0cc"
+sha256 = "0d67481c2b3d855b27b162e21b39152100346098f75cb5da31db4003d9077680"
 hardening = ["!vis", "!cfi"]
 
 
