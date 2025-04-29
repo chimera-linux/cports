da89624eac0bad21a6733df93a30a1bf9d13e241
q66 <q66@chimera-linux.org>
2025-04-30 00:15:18 +0200

    main/libavif: update to 1.2.1

--- main/libavif/template.py
+++ main/libavif/template.py
@@ -1,5 +1,5 @@
 pkgname = "libavif"
-pkgver = "1.1.1"
+pkgver = "1.2.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -27,8 +27,15 @@ checkdepends = ["bash"]
 pkgdesc = "Library for encoding and decoding .avif files"
 license = "BSD-2-Clause"
 url = "https://github.com/AOMediaCodec/libavif"
-source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "914662e16245e062ed73f90112fbb4548241300843a7772d8d441bb6859de45b"
+source = [
+    f"{url}/archive/refs/tags/v{pkgver}.tar.gz",
+    "https://github.com/kmurray/libargparse/archive/ee74d1b53bd680748af14e737378de57e2a0a954.tar.gz",
+]
+source_paths = [".", "ext/libargparse"]
+sha256 = [
+    "9c859c7c12ccb0f407511bfe303e6a7247f5f6738f54852662c6df8048daddf4",
+    "7727b0498851e5b6a6fcd734eb667a8a231897e2c86a357aec51cc0664813060",
+]
 hardening = ["!vis", "!cfi"]
 # fails since libpng update, check again for 1.2
 options = ["!check"]
