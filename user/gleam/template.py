d1d304604e3e6ad96e80752fd0a33f54a4708620
JetpackJackson <jetpackjackson@katzenmue.de>
2025-04-30 16:55:50 -0400

    user/gleam: update to 1.10.0
    Signed-off-by: JetpackJackson <jetpackjackson@katzenmue.de>

--- user/gleam/template.py
+++ user/gleam/template.py
@@ -1,5 +1,5 @@
 pkgname = "gleam"
-pkgver = "1.9.1"
+pkgver = "1.10.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -11,7 +11,7 @@ url = "https://gleam.run"
 source = (
     f"https://github.com/gleam-lang/gleam/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "eacf88d2ce6f7ca06e9a0d6b8117c517a8a21593349233edb2506263d08a830f"
+sha256 = "4661bebc010209c5c3d180a8f7ad6c16b596655acf74bf459d3baf81af8589d5"
 
 
 def install(self):
