24081dcbebbe9561cdcacae6d854266b9d46f786
triallax <triallax@tutanota.com>
2025-05-01 22:15:45 +0100

    main/cpupower: update to 6.14.4

--- main/cpupower/template.py
+++ main/cpupower/template.py
@@ -1,5 +1,5 @@
 pkgname = "cpupower"
-pkgver = "6.14.3"
+pkgver = "6.14.4"
 pkgrel = 0
 build_style = "makefile"
 make_build_target = "cpupower"
@@ -33,7 +33,7 @@ pkgdesc = "Linux CPU power management tools"
 license = "GPL-2.0-only"
 url = "https://kernel.org"
 source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[: pkgver.find('.')]}.x/linux-{pkgver}.tar.xz"
-sha256 = "91c6c322c32568f09922a02ed35f46372ca8526714c6555b3115b9e9c156652a"
+sha256 = "9452f28d7a0051fba4886712395b484c4c7fcf9f85944a62fd3d97dc923f5339"
 # nope
 options = ["!check"]
 
