b94b6532505eff368a285600bf51449033400e25
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-05-02 23:44:05 +0200

    user/television: update to 0.11.9

--- user/television/template.py
+++ user/television/template.py
@@ -1,5 +1,5 @@
 pkgname = "television"
-pkgver = "0.11.7"
+pkgver = "0.11.9"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -9,7 +9,7 @@ pkgdesc = "Fuzzy finder"
 license = "MIT"
 url = "https://github.com/alexpasmantier/television"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "d3687a286b4813ab8534d7ce8653908dcf17ad291bc865f3f64c183163f0b650"
+sha256 = "be58e6011ce1228afa1c6ba15f33023484b1bc8673c466f186ac2a130bb25bdb"
 # generates completions with host binary
 options = ["!cross"]
 
