bc05ca1fa2e3ea7f4b29e9d82de8310f4d1aa27e
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-05-03 01:13:21 +0200

    main/pcsc-lite: update to 2.3.3

--- main/pcsc-lite/template.py
+++ main/pcsc-lite/template.py
@@ -1,5 +1,5 @@
 pkgname = "pcsc-lite"
-pkgver = "2.3.1"
+pkgver = "2.3.3"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -14,7 +14,7 @@ pkgdesc = "Middleware to access PC/SC smart cards using SCard API"
 license = "BSD-3-Clause"
 url = "https://pcsclite.apdu.fr"
 source = f"https://pcsclite.apdu.fr/files/pcsc-lite-{pkgver}.tar.xz"
-sha256 = "a641d44d57affe1edd8365dd75307afc307e7eefb4e7ad839f6f146baa41ed56"
+sha256 = "cdff7d7153a0b37aa74e26dfec89ec7dc5c5286aa21b91b903e38739d227e8e7"
 
 
 def post_install(self):
