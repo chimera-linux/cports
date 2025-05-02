1d23f85b1e554025546f6c3b7af9102c8fc97a44
triallax <triallax@tutanota.com>
2025-05-02 02:05:30 +0100

    main/gtest: update to 1.17.0

--- main/gtest/template.py
+++ main/gtest/template.py
@@ -1,6 +1,6 @@
 # nb: requires CXX14+ to use it
 pkgname = "gtest"
-pkgver = "1.16.0"
+pkgver = "1.17.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -14,7 +14,7 @@ pkgdesc = "Google's framework for writing C++ tests"
 license = "BSD-3-Clause"
 url = "https://github.com/google/googletest"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "78c676fc63881529bf97bf9d45948d905a66833fbfa5318ea2cd7478cb98f399"
+sha256 = "65fab701d9829d38cb77c14acdc431d2108bfdbf8979e40eb8ae567edf10b27c"
 
 
 def post_install(self):
