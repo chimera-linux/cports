f711d16f34bbd3f9343cce81640cd803c880b4e8
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-05-03 00:23:37 +0200

    user/gitoxide: update to 0.44.0

--- user/gitoxide/template.py
+++ user/gitoxide/template.py
@@ -1,10 +1,10 @@
 pkgname = "gitoxide"
-pkgver = "0.42.0"
+pkgver = "0.44.0"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = [
     "--no-default-features",
-    "--features=max-control,gix-features/zlib-stock,gitoxide-core-blocking-client,http-client-curl",
+    "--features=max-control,gitoxide-core-blocking-client,http-client-curl",
 ]
 make_install_args = [*make_build_args]
 make_check_args = [*make_install_args]
@@ -13,13 +13,12 @@ makedepends = [
     "curl-devel",
     "rust-std",
     "sqlite-devel",
-    "zlib-ng-compat-devel",
 ]
 pkgdesc = "Rust implementation of Git"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/Byron/gitoxide"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "4f7febd1bc45d96afc643142d26753ccb7fde7e69f68ca201f04953c1fc6ba7a"
+sha256 = "1166627cd41daf68eb4e97591cd5daaccf94aa75bb454f657b93766a9bf70da9"
 
 
 def post_install(self):
