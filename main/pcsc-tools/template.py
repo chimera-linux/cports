6fbba42e7baa9d9ee4b03e575728ed1cbff24db5
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-05-03 01:14:03 +0200

    main/pcsc-tools: update to 1.7.3

--- main/pcsc-tools/template.py
+++ main/pcsc-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "pcsc-tools"
-pkgver = "1.7.2"
+pkgver = "1.7.3"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -18,4 +18,4 @@ pkgdesc = "Tools for PCSC"
 license = "GPL-2.0-or-later"
 url = "https://pcsc-tools.apdu.fr"
 source = f"{url}/pcsc-tools-{pkgver}.tar.bz2"
-sha256 = "fd6fcc25e8140f613b3bf44b02eda4a8a429ee4939fb777f474e2c829f7b472c"
+sha256 = "c3b6d2b7a40bc066ebb8cd2512fb42e2b787a5c491a5715b8741743f4edb8cc2"
