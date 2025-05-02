4d393d0e95ff351745d55dc974e60062ac634f03
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-05-03 01:09:17 +0200

    user/atuin: update to 18.5.0

--- user/atuin/template.py
+++ user/atuin/template.py
@@ -1,6 +1,6 @@
 pkgname = "atuin"
-pkgver = "18.4.0"
-pkgrel = 2
+pkgver = "18.5.0"
+pkgrel = 0
 build_style = "cargo"
 # we patch Cargo.toml and Cargo.lock
 prepare_after_patch = True
@@ -11,7 +11,7 @@ pkgdesc = "Sync, search and backup tool for shell history"
 license = "MIT"
 url = "https://github.com/atuinsh/atuin"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "de6d2bcf10de4d757916c7e92a70f15929fc1dea75abc4df09b0baedf26a53b2"
+sha256 = "f3744e8dfee2c7089ac140cb8aafe01e5d77a2299097a2cc0a42db26d127340a"
 # A bunch of failures yet to be investigated
 # generates completions using host binary
 options = ["!check", "!cross"]
@@ -29,7 +29,7 @@ def build(self):
     with self.stamp("client"):
         self.cargo.build(["--features=client,sync,clipboard"])
 
-    for shell in ["bash", "fish", "zsh"]:
+    for shell in ["bash", "fish", "nushell", "zsh"]:
         with open(self.cwd / f"atuin.{shell}", "w") as outf:
             self.do(
                 f"target/{self.profile().triplet}/release/atuin",
@@ -44,7 +44,7 @@ def install(self):
     self.install_bin(f"target/{self.profile().triplet}/release/atuin")
     self.install_bin(f"target/{self.profile().triplet}/release/atuin-server")
 
-    for shell in ["bash", "fish", "zsh"]:
+    for shell in ["bash", "fish", "nushell", "zsh"]:
         self.install_completion(f"atuin.{shell}", shell)
 
     self.install_file(
