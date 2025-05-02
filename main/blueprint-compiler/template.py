1eb89cdc888787dcdcab2eef6b3c963c9ebfe218
triallax <triallax@tutanota.com>
2025-05-02 01:07:53 +0100

    main/blueprint-compiler: update URL, fix update check

--- main/blueprint-compiler/template.py
+++ main/blueprint-compiler/template.py
@@ -1,6 +1,6 @@
 pkgname = "blueprint-compiler"
 pkgver = "0.16.0"
-pkgrel = 1
+pkgrel = 2
 build_style = "meson"
 make_check_wrapper = ["wlheadless-run", "--"]
 hostmakedepends = [
@@ -15,6 +15,6 @@ depends = [
 checkdepends = ["xwayland-run", *depends]
 pkgdesc = "Markup language compiler for GTK user interfaces"
 license = "LGPL-3.0-or-later"
-url = "https://jwestman.pages.gitlab.gnome.org/blueprint-compiler"
-source = f"https://gitlab.gnome.org/jwestman/blueprint-compiler/-/archive/v{pkgver}/blueprint-compiler-v{pkgver}.tar.gz"
+url = "https://gnome.pages.gitlab.gnome.org/blueprint-compiler"
+source = f"https://gitlab.gnome.org/GNOME/blueprint-compiler/-/archive/v{pkgver}/blueprint-compiler-v{pkgver}.tar.gz"
 sha256 = "01feb8263fe7a450b0a9fed0fd54cf88947aaf00f86cc7da345f8b39a0e7bd30"
