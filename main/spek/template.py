fa254f9401cda63ec79198d9c3d556824087f390
q66 <q66@chimera-linux.org>
2025-05-04 00:44:15 +0200

    main/spek: fix build
    Closes https://github.com/chimera-linux/cports/pull/4009

--- main/spek/template.py
+++ main/spek/template.py
@@ -18,3 +18,7 @@ url = "https://github.com/alexkay/spek"
 source = f"{url}/releases/download/v{pkgver}/spek-{pkgver}.tar.xz"
 sha256 = "1bccf85a14a01af8f2f30476cbad004e8bf6031f500e562bbe5bbd1e5eb16c59"
 hardening = ["vis", "cfi"]
+
+
+def post_extract(self):
+    self.rm("po/Makefile.in.in")
