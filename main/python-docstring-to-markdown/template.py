4546d52aaec23efcaf20b01512e8caae0d8376d5
Gnarwhal <git.aspect893@passmail.net>
2025-05-03 15:50:33 +0000

    main/python-docstring-to-markdown: update to 0.17

--- main/python-docstring-to-markdown/template.py
+++ main/python-docstring-to-markdown/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-docstring-to-markdown"
-pkgver = "0.16"
-pkgrel = 1
+pkgver = "0.17"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -16,4 +16,4 @@ pkgdesc = "Python module for converting docstrings to markdown"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/python-lsp/docstring-to-markdown"
 source = f"$(PYPI_SITE)/d/docstring-to-markdown/docstring_to_markdown-{pkgver}.tar.gz"
-sha256 = "097bf502fdf040b0d019688a7cc1abb89b98196801448721740e8aa3e5075627"
+sha256 = "df72a112294c7492487c9da2451cae0faeee06e86008245c188c5761c9590ca3"
