3d5053615e46f57a7999af8909b28089d693b463
q66 <q66@chimera-linux.org>
2025-05-05 01:00:37 +0200

    main/kddockwidgets: update to 2.2.4

--- main/kddockwidgets/template.py
+++ main/kddockwidgets/template.py
@@ -1,6 +1,6 @@
 pkgname = "kddockwidgets"
-pkgver = "2.2.1"
-pkgrel = 2
+pkgver = "2.2.4"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DKDDockWidgets_QT6=ON",
@@ -13,15 +13,17 @@ hostmakedepends = [
     "pkgconf",
 ]
 makedepends = [
+    "fmt-devel",
     "nlohmann-json",
     "qt6-qtbase-private-devel",  # qhighdpiscaling_p.h/qquickitem_p.h
     "qt6-qtdeclarative-devel",
+    "spdlog-devel",
 ]
 pkgdesc = "Dock Widget Framework for Qt"
 license = "GPL-2.0-only OR GPL-3.0-only"
 url = "https://www.kdab.com/development-resources/qt-tools/kddockwidgets"
 source = f"https://github.com/KDAB/KDDockWidgets/releases/download/v{pkgver}/kddockwidgets-{pkgver}.tar.gz"
-sha256 = "8693e06abee0c88517d8480b22647702a51a0708f3c876ed5385d9a4e356e1a5"
+sha256 = "a1cf55a3cf267108ee495de8df9038c67f61da5ca324059cb32543d69877524e"
 
 
 @subpackage("kddockwidgets-devel")
