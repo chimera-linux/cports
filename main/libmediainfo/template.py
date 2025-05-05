26e4e8028e0c26f966ff8e543755eb544da581a9
q66 <q66@chimera-linux.org>
2025-05-05 23:43:15 +0200

    main/libmediainfo: update to 25.04

--- main/libmediainfo/template.py
+++ main/libmediainfo/template.py
@@ -1,10 +1,9 @@
 pkgname = "libmediainfo"
-pkgver = "25.03"
+pkgver = "25.04"
 pkgrel = 0
 build_wrksrc = "Project/CMake"
 build_style = "cmake"
 configure_args = [
-    "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
     "-DBUILD_SHARED_LIBS=ON",
 ]
 hostmakedepends = ["pkgconf", "cmake", "ninja"]
@@ -18,7 +17,7 @@ pkgdesc = "Shared library for mediainfo"
 license = "BSD-2-Clause"
 url = "https://mediaarea.net/en/MediaInfo"
 source = f"https://mediaarea.net/download/source/libmediainfo/{pkgver}/libmediainfo_{pkgver}.tar.bz2"
-sha256 = "5852567e90b0ad2f51ad7cc18a13ba1389c83250437e541f9a2bb749b24b60ba"
+sha256 = "a5c5ce1e21d40c6907c47a9459c3b5f36cd5c7a0e5800f87419da10b9267becd"
 
 
 def post_install(self):
