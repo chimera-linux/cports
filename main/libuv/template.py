b3e0f9e0e87a118a3826c17b931d264041970f26
q66 <q66@chimera-linux.org>
2025-05-06 00:07:39 +0200

    main/libuv: update to 1.51.0

--- main/libuv/template.py
+++ main/libuv/template.py
@@ -1,5 +1,5 @@
 pkgname = "libuv"
-pkgver = "1.50.0"
+pkgver = "1.51.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -8,7 +8,7 @@ pkgdesc = "Multi-platform support library with focus on asynchronous I/O"
 license = "MIT"
 url = "https://libuv.org"
 source = f"https://dist.libuv.org/dist/v{pkgver}/libuv-v{pkgver}-dist.tar.gz"
-sha256 = "c42c51d4e630f95dcefcafff95bf003ea52939e312d5e6584e7d9e102ead3e9e"
+sha256 = "2ceca1a7577633cf92794db5bf5512370f6cd45a5746d6e14f8c20aeab0a547b"
 hardening = ["vis", "!cfi"]
 
 
