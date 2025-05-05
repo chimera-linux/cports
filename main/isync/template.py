efea9ef9b6d5efc02ef6cf76d6141b15d602b429
q66 <q66@chimera-linux.org>
2025-05-05 18:27:39 +0200

    main/isync: update to 1.5.1

--- main/isync/template.py
+++ main/isync/template.py
@@ -1,5 +1,5 @@
 pkgname = "isync"
-pkgver = "1.5.0"
+pkgver = "1.5.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -16,7 +16,7 @@ pkgdesc = "IMAP and MailDir mailbox synchronizer"
 license = "GPL-2.0-or-later WITH custom:LicenseRef-isync-GPL-exception"
 url = "https://isync.sourceforge.io"
 source = f"$(SOURCEFORGE_SITE)/isync/isync/{pkgver}/isync-{pkgver}.tar.gz"
-sha256 = "a0c81e109387bf279da161453103399e77946afecf5c51f9413c5e773557f78d"
+sha256 = "28cc90288036aa5b6f5307bfc7178a397799003b96f7fd6e4bd2478265bb22fa"
 hardening = ["vis", "!cfi"]
 # missing perl modules
 options = ["!check"]
