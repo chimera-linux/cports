94dc1f62ff6026786da4286a255364d8bc1af0b3
q66 <q66@chimera-linux.org>
2025-05-06 01:04:34 +0200

    user/riff: bump libc version for loongarch

--- user/riff/template.py
+++ user/riff/template.py
@@ -11,5 +11,18 @@ source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "6db6ac7525f00c4a4cb45351b9a229e253b3e9053ab365d6f881c0144159f8da"
 
 
+def pre_prepare(self):
+    # the version that is in there is busted on loongarch
+    self.do(
+        "cargo",
+        "update",
+        "--package",
+        "libc",
+        "--precise",
+        "0.2.170",
+        allow_network=True,
+    )
+
+
 def post_install(self):
     self.install_license("LICENSE")
