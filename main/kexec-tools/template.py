49e37282e21bd9df49d3e529ca3ed4c41c14154a
q66 <q66@chimera-linux.org>
2025-05-05 18:29:05 +0200

    main/kexec-tools: update to 2.0.31

--- main/kexec-tools/template.py
+++ main/kexec-tools/template.py
@@ -1,15 +1,20 @@
 pkgname = "kexec-tools"
-pkgver = "2.0.30"
+pkgver = "2.0.31"
 pkgrel = 0
 archs = ["aarch64", "armhf", "armv7", "ppc64", "ppc64le", "x86_64"]
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "libtool"]
-makedepends = ["linux-headers", "xz-devel", "zlib-ng-compat-devel"]
+makedepends = [
+    "linux-headers",
+    "xz-devel",
+    "zlib-ng-compat-devel",
+    "zstd-devel",
+]
 pkgdesc = "Tools for kexec"
 license = "GPL-2.0-only"
 url = "https://kernel.org/pub/linux/utils/kernel/kexec"
 source = f"$(KERNEL_SITE)/utils/kernel/kexec/kexec-tools-{pkgver}.tar.xz"
-sha256 = "748a5fa7af27a8059d1de907bddfc32ac530c7189b5770c83e6f3d898d82124e"
+sha256 = "8a8f350ddc66e1c905a3ab525a7e9ba96c81e04e70ef69397b0155b67b922c31"
 hardening = ["vis", "cfi"]
 # no tests
 options = ["!check"]
