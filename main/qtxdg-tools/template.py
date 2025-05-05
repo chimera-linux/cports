b4e9d5a8fc1f4a7e452e12a29cc82e3027fc9b0b
q66 <q66@chimera-linux.org>
2025-05-05 23:33:34 +0200

    main/qtxdg-tools: update to 4.2.0

--- main/qtxdg-tools/template.py
+++ main/qtxdg-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "qtxdg-tools"
-pkgver = "4.1.0"
+pkgver = "4.2.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -15,4 +15,4 @@ pkgdesc = "User tools for libqtxdg"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/qtxdg-tools"
 source = f"{url}/releases/download/{pkgver}/qtxdg-tools-{pkgver}.tar.xz"
-sha256 = "dbd59b7641091a226fb58222e11b4aeb36e6e65dc235280897d066e59fa966b6"
+sha256 = "503a6e37792709340dddd642821eca13ef8d789cd26a06983cb7c5e06b8d63af"
