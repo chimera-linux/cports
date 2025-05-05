3f9615b4b7769e648812caed5ece49e08f947f6f
q66 <q66@chimera-linux.org>
2025-05-05 22:49:46 +0200

    main/libde265: update to 1.0.16

--- main/libde265/template.py
+++ main/libde265/template.py
@@ -1,5 +1,5 @@
 pkgname = "libde265"
-pkgver = "1.0.15"
+pkgver = "1.0.16"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-option-checking"]
@@ -9,14 +9,13 @@ pkgdesc = "Open H.265 codec implementation"
 license = "LGPL-3.0-or-later"
 url = "http://www.libde265.org"
 source = f"https://github.com/strukturag/libde265/archive/v{pkgver}.tar.gz"
-sha256 = "d4e55706dfc5b2c5c9702940b675ce2d3e7511025c6894eaddcdbaf0b15fd3f3"
+sha256 = "ed12c931759c1575848832f70db5071a001ac813db4e4f568ee08aef6e234d4e"
 hardening = ["!vis", "!cfi"]
 
 
 def post_install(self):
     # do not polute /usr/bin with junk
     for f in [
-        "acceleration_speed",
         "bjoentegaard",
         "block-rate-estim",
         "gen-enc-table",
