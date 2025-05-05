ed5abc12376e99afa35b81a933fff4957551c229
q66 <q66@chimera-linux.org>
2025-05-06 00:19:02 +0200

    main/libxft: update to 2.3.9

--- main/libxft/template.py
+++ main/libxft/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxft"
-pkgver = "2.3.8"
+pkgver = "2.3.9"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf", "slibtool", "xorg-util-macros"]
@@ -13,7 +13,7 @@ pkgdesc = "X font library"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXft-{pkgver}.tar.gz"
-sha256 = "32e48fe2d844422e64809e4e99b9d8aed26c1b541a5acf837c5037b8d9f278a8"
+sha256 = "47c157fb4d0308f8b9604b74c29bb902b019eb97031f8fbf5ab62aa9f147a104"
 # crashes
 hardening = ["!int"]
 
