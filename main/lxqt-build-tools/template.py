3abcb95f9ad8f8a6ee395a5683384a3d26dc2cb6
q66 <q66@chimera-linux.org>
2025-05-05 23:32:23 +0200

    main/lxqt-build-tools: update to 2.2.0

--- main/lxqt-build-tools/template.py
+++ main/lxqt-build-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "lxqt-build-tools"
-pkgver = "2.1.0"
+pkgver = "2.2.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -11,7 +11,7 @@ pkgdesc = "LXQt build and packaging tooling"
 license = "BSD-3-Clause"
 url = "https://github.com/lxqt/lxqt-build-tools"
 source = f"{url}/releases/download/{pkgver}/lxqt-build-tools-{pkgver}.tar.xz"
-sha256 = "2458b629936f5e1ff8850e9953e49d66b96ac419cb484fed0a4d28a711fd8ef9"
+sha256 = "1214e12ca06561ca724e67092ae1265fdd23128fde449e9d3b0275cb2a051d43"
 
 
 def post_install(self):
