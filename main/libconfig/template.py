4a64a8f71c77bd3748afa0e0104fac97014ed64b
q66 <q66@chimera-linux.org>
2025-05-05 22:47:28 +0200

    main/libconfig: update to 1.8

--- main/libconfig/template.py
+++ main/libconfig/template.py
@@ -1,5 +1,5 @@
 pkgname = "libconfig"
-pkgver = "1.7.3"
+pkgver = "1.8"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-examples"]
@@ -16,7 +16,7 @@ pkgdesc = "Configuration file library"
 license = "LGPL-2.1-or-later"
 url = "https://hyperrealm.com/libconfig/libconfig.html"
 source = f"https://github.com/hyperrealm/libconfig/archive/v{pkgver}.tar.gz"
-sha256 = "68757e37c567fd026330c8a8449aa5f9cac08a642f213f2687186b903bd7e94e"
+sha256 = "22e13253e652ec583ba0dd5b474bd9c7bd85dc724f2deb0d76a6299c421358ef"
 
 
 @subpackage("libconfig-devel")
