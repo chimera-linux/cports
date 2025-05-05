832c91e707db4542a6da983136d37bd8c8ccab32
q66 <q66@chimera-linux.org>
2025-05-05 21:36:27 +0200

    main/easyeffects: fix build

--- main/easyeffects/template.py
+++ main/easyeffects/template.py
@@ -2,7 +2,6 @@ pkgname = "easyeffects"
 pkgver = "7.2.3"
 pkgrel = 4
 build_style = "meson"
-configure_args = ["-Denable-libcpp-workarounds=true"]
 hostmakedepends = [
     "desktop-file-utils",
     "gettext",
@@ -44,6 +43,7 @@ license = "GPL-3.0-or-later"
 url = "https://github.com/wwmm/easyeffects"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "5d3afa96901c86c4fa065aa40db11800a7cdfd9d393f1de9b44bb126eee4b01e"
+tool_flags = {"CXXFLAGS": ["-fexperimental-library"]}
 
 
 def post_install(self):
