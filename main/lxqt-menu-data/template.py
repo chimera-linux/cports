b23bb288c97ba4655a1fd82c870bc20892f0f8cd
q66 <q66@chimera-linux.org>
2025-05-05 23:36:58 +0200

    main/lxqt-menu-data: update to 2.2.0

--- main/lxqt-menu-data/template.py
+++ main/lxqt-menu-data/template.py
@@ -1,5 +1,5 @@
 pkgname = "lxqt-menu-data"
-pkgver = "2.1.0"
+pkgver = "2.2.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -16,4 +16,4 @@ pkgdesc = "Menu files for LXQt components"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/lxqt/lxqt-menu-data"
 source = f"{url}/releases/download/{pkgver}/lxqt-menu-data-{pkgver}.tar.xz"
-sha256 = "d2d2187313c16fc435a7313e53b80ace7ccba0b5de6c0d9cd53bfdfa13d5eab4"
+sha256 = "a5a026fe3a8d279551233fa399b37139a668c70b07b53ec85c23e67249ae895b"
