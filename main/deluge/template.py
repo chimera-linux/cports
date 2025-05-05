2a36b44ff44086b2df5760634c80d3fe3a472516
q66 <q66@chimera-linux.org>
2025-05-05 16:56:16 +0200

    main/deluge: update to 2.2.0

--- main/deluge/template.py
+++ main/deluge/template.py
@@ -1,7 +1,7 @@
 # TODO: improve services
 pkgname = "deluge"
-pkgver = "2.1.1"
-pkgrel = 4
+pkgver = "2.2.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "intltool",
@@ -29,7 +29,7 @@ pkgdesc = "Portable BitTorrent client"
 license = "GPL-3.0-or-later"
 url = "https://deluge-torrent.org"
 source = f"https://ftp.osuosl.org/pub/deluge/source/{pkgver[:-2]}/deluge-{pkgver}.tar.xz"
-sha256 = "768dd319802e42437ab3794ebe75b497142e08ed5b0fb2503bad62cef442dff7"
+sha256 = "b9ba272b5ba42aaf1c694e6c29628ab816cc1a700a37bac08aacb52571606acd"
 # unpackaged checkdepends
 options = ["!check"]
 
@@ -38,7 +38,7 @@ def post_install(self):
     self.install_sysusers(self.files_path / "sysusers.conf")
     self.install_tmpfiles(self.files_path / "tmpfiles.conf")
     self.install_file(
-        "deluge/ui/data/share/appdata/deluge.appdata.xml", "usr/share/appdata"
+        "deluge/ui/data/share/metainfo/deluge.metainfo.xml", "usr/share/metainfo"
     )
     self.install_file(
         "deluge/ui/data/share/applications/deluge.desktop",
@@ -59,9 +59,9 @@ def _(self):
         "usr/bin/deluge",
         "usr/bin/deluge-gtk",
         "usr/lib/python3*/site-packages/deluge/ui/gtk3",
-        "usr/share/appdata",
         "usr/share/applications",
         "usr/share/icons",
+        "usr/share/metainfo",
         "usr/share/pixmaps",
         "usr/share/man/man1/deluge.1",
         "usr/share/man/man1/deluge-gtk.1",
