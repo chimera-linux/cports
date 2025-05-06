cbba43f4b25b2b78be4886ff94318757b8ee254b
triallax <triallax@tutanota.com>
2025-05-06 18:23:58 +0100

    main/libwacom: update to 2.15.0

--- main/libwacom/template.py
+++ main/libwacom/template.py
@@ -1,6 +1,6 @@
 pkgname = "libwacom"
-pkgver = "2.14.0"
-pkgrel = 1
+pkgver = "2.15.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocumentation=disabled", "-Dtests=enabled"]
 make_check_args = ["--timeout-multiplier", "4"]
@@ -17,7 +17,7 @@ pkgdesc = "Library to handle Wacom tablets"
 license = "MIT"
 url = "https://github.com/linuxwacom/libwacom"
 source = f"{url}/releases/download/libwacom-{pkgver}/libwacom-{pkgver}.tar.xz"
-sha256 = "5900b3ad3d780e1b864103ace99cace9470db727a162517e1648c86a1bdec0e3"
+sha256 = "20cd65b1693129c3a6c003db0fe7fff7eccaf19fb04e89aaad9c20eb2151515a"
 
 
 def post_install(self):
