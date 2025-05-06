8adac590695b380256c78d12775849abfc6a4ffb
triallax <triallax@tutanota.com>
2025-05-06 17:58:41 +0100

    main/libiscsi: update url

--- main/libiscsi/template.py
+++ main/libiscsi/template.py
@@ -1,6 +1,6 @@
 pkgname = "libiscsi"
 pkgver = "1.20.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 configure_args = ["--disable-werror"]
 configure_gen = ["./autogen.sh"]
@@ -9,8 +9,8 @@ hostmakedepends = ["automake", "libtool", "pkgconf"]
 makedepends = ["linux-headers", "libgcrypt-devel"]
 pkgdesc = "ISCSI client library and utilities"
 license = "LGPL-2.1-or-later AND GPL-2.0-or-later"
-url = "https://foo.software"
-source = f"https://github.com/sahlberg/libiscsi/archive/{pkgver}.tar.gz"
+url = "https://github.com/sahlberg/libiscsi"
+source = f"{url}/archive/{pkgver}.tar.gz"
 sha256 = "6bd6feef2904de1bb1869cec935b58995bc1311cad57184a2221e09ed6137eec"
 tool_flags = {"CFLAGS": ["-Wno-strict-prototypes"]}
 
