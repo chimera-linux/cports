88b67cf6c24aefea80371fbaa5a8c6673b57a0b3
triallax <triallax@tutanota.com>
2025-05-06 18:11:14 +0100

    main/intel-gmmlib: update to 22.7.2

--- main/intel-gmmlib/template.py
+++ main/intel-gmmlib/template.py
@@ -1,10 +1,11 @@
 pkgname = "intel-gmmlib"
-pkgver = "22.7.1"
+pkgver = "22.7.2"
 pkgrel = 0
 # aarch64 segfaults in tests
 # only needed for intel-media-driver anyway
 archs = ["x86_64"]
 build_style = "cmake"
+configure_args = ["-DCMAKE_POLICY_VERSION_MINIMUM=3.5"]
 hostmakedepends = [
     "cmake",
     "ninja",
@@ -14,7 +15,7 @@ pkgdesc = "Intel Graphics Memory Management Library"
 license = "MIT"
 url = "https://github.com/intel/gmmlib"
 source = f"{url}/archive/refs/tags/intel-gmmlib-{pkgver}.tar.gz"
-sha256 = "3b84d993950069f0a9b0cc373e2f4b026b194d8fdfb2a8856490d17dadd1b5a5"
+sha256 = "36f2ff2b59158d1253a33d735f4e35a1b5740c4751818bdaa222b8ddf96ee881"
 # CFI: testsuite sigill
 hardening = ["vis", "!cfi"]
 # check cross: testsuite runs as part of install(), disabling that also doesn't build it..
