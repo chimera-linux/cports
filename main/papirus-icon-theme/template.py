ec57c2aaa8cd0ec8054437f6b72d0f5c12d1b13e
triallax <triallax@tutanota.com>
2025-05-06 20:28:43 +0100

    main/papirus-icon-theme: update to 20250501

--- main/papirus-icon-theme/template.py
+++ main/papirus-icon-theme/template.py
@@ -1,11 +1,11 @@
 pkgname = "papirus-icon-theme"
-pkgver = "20250201"
+pkgver = "20250501"
 pkgrel = 0
 build_style = "makefile"
 pkgdesc = "Pixel perfect icon theme"
 license = "GPL-3.0-or-later"
 url = "https://github.com/PapirusDevelopmentTeam/papirus-icon-theme"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "01a7a07293db9e22437b96fae9d7fd8dad74c33c5460af8c86227973cb3a9846"
+sha256 = "3831a487f813479ad3224fdbfb0c7023f23056899bc78c93737f341aa655558e"
 # makes no sense since it's just icons
 options = ["!check"]
