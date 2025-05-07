c8b6d5a9768b35d1a35cc9f6e5efc66f50182e14
q66 <q66@chimera-linux.org>
2025-05-07 21:43:06 +0200

    main/nbfc-linux: update to 0.3.14

--- main/nbfc-linux/template.py
+++ main/nbfc-linux/template.py
@@ -1,14 +1,15 @@
 pkgname = "nbfc-linux"
-pkgver = "0.2.7"
-pkgrel = 2
+pkgver = "0.3.14"
+pkgrel = 0
 build_style = "makefile"
 make_build_args = ["confdir=/etc"]
 make_install_args = ["confdir=/etc"]
+makedepends = ["curl-devel", "linux-headers", "openssl-devel"]
 pkgdesc = "NoteBook FanControl for Linux"
 license = "GPL-3.0-or-later"
 url = "https://github.com/nbfc-linux/nbfc-linux"
 source = f"{url}/archive/{pkgver}/nbfc-linux-{pkgver}.tar.gz"
-sha256 = "5915af1fd3c23bdb8289cc9428fad825daf49dc187b50bbe759c317c9785bd35"
+sha256 = "f49eba952c31396f858a6b338d68ea8c66b2ebc0659a49296f2d851f284a9969"
 # recursive json parser
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=0x200000"]}
 # no tests
@@ -20,3 +21,4 @@ if self.profile().wordsize == 32:
 
 def post_install(self):
     self.install_service(self.files_path / "nbfc")
+    self.uninstall("usr/lib/systemd")
