f0c7866ebc89d5669978a6ec75eb695f590061b1
q66 <q66@chimera-linux.org>
2025-05-07 21:44:40 +0200

    main/ndctl: update to 81

--- main/ndctl/template.py
+++ main/ndctl/template.py
@@ -1,8 +1,9 @@
 pkgname = "ndctl"
-pkgver = "80"
+pkgver = "81"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
+    "-Ddefault_library=shared",
     "-Dmodprobedatadir=/usr/lib/modprobe.d",
     "-Dsystemd=disabled",
     f"-Dversion-tag={pkgver}",
@@ -28,7 +29,7 @@ pkgdesc = "Tools and libraries for NVDIMMs and other platform memory"
 license = "GPL-2.0-only AND LGPL-2.1-only AND MIT AND CC0-1.0"
 url = "https://github.com/pmem/ndctl"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "4bb7565feea486997a0f62b1878aeb6d4db9af128e0fa459359b3c02644325c5"
+sha256 = "e40784687ac2d0c176748c9ffb8bde541a12301b6fedb607a9a70c8fd1f94929"
 hardening = ["vis", "cfi"]
 # the MIT licence used doesn't have a copyright line in it
 options = ["!distlicense"]
