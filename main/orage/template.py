6cc052ddd5b76b5312f278605369809b86c3375a
q66 <q66@chimera-linux.org>
2025-05-07 22:06:04 +0200

    main/orage: update to 4.20.1

--- main/orage/template.py
+++ main/orage/template.py
@@ -1,5 +1,5 @@
 pkgname = "orage"
-pkgver = "4.20.0"
+pkgver = "4.20.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-x11-tray-icon"]
@@ -23,4 +23,4 @@ pkgdesc = "Xfce time-managing app"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/apps/orage/start"
 source = f"$(XFCE_SITE)/apps/orage/{pkgver[:-2]}/orage-{pkgver}.tar.bz2"
-sha256 = "ee07918a6f6033e6107e9c1b8e164c645ea590ffbdba0b58d071c0e491046a85"
+sha256 = "7e0331167ba438e494e0dc7fbdf03843fdfa4004910f4d5b113afea77b3eecc0"
