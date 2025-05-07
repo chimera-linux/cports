09af59a964d2d09981a8fcdfc0580c433e44ba84
q66 <q66@chimera-linux.org>
2025-05-07 21:50:56 +0200

    main/newt: update to 0.52.25

--- main/newt/template.py
+++ main/newt/template.py
@@ -1,6 +1,6 @@
 pkgname = "newt"
-pkgver = "0.52.24"
-pkgrel = 2
+pkgver = "0.52.25"
+pkgrel = 0
 build_style = "gnu_configure"
 # reconf breaks library soname stuff (???)
 configure_gen = []
@@ -11,7 +11,7 @@ pkgdesc = "Library for color text mode, widget based user interfaces"
 license = "LGPL-2.0-only"
 url = "https://pagure.io/newt"
 source = f"https://releases.pagure.org/newt/newt-{pkgver}.tar.gz"
-sha256 = "5ded7e221f85f642521c49b1826c8de19845aa372baf5d630a51774b544fbdbb"
+sha256 = "ef0ca9ee27850d1a5c863bb7ff9aa08096c9ed312ece9087b30f3a426828de82"
 # no proper check target
 options = ["!check"]
 
