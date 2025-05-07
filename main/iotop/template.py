e13452edbaa3971de9859296e3847477ab71c422
q66 <q66@chimera-linux.org>
2025-05-07 18:59:53 +0200

    main/iotop: update to 1.29

--- main/iotop/template.py
+++ main/iotop/template.py
@@ -1,5 +1,5 @@
 pkgname = "iotop"
-pkgver = "1.28"
+pkgver = "1.29"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = [
@@ -13,7 +13,7 @@ pkgdesc = "Top-like utility for IO"
 license = "GPL-2.0-or-later"
 url = "https://github.com/Tomas-M/iotop"
 source = f"https://github.com/Tomas-M/iotop/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "b83e5c7d82da3bfb3e46b74e91b24a52fc65d44ff268ace95b613972e27fc678"
+sha256 = "8afcd3f2a91a5246c4b187e39cb87fafdb5c2dd27a0386fb8381361c271f8f60"
 hardening = ["cfi", "vis"]
 # no tests
 options = ["!check"]
