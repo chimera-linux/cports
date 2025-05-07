5984b65a0628d55c72188cc8995ac5812d1095c7
q66 <q66@chimera-linux.org>
2025-05-07 21:47:55 +0200

    main/newsraft: update to 0.30

--- main/newsraft/template.py
+++ main/newsraft/template.py
@@ -1,5 +1,5 @@
 pkgname = "newsraft"
-pkgver = "0.29"
+pkgver = "0.30"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = [
@@ -12,13 +12,12 @@ makedepends = [
     "libexpat-devel",
     "ncurses-devel",
     "sqlite-devel",
-    "yajl-devel",
 ]
 pkgdesc = "Feed reader for terminal"
 license = "ISC"
 url = "https://codeberg.org/newsraft/newsraft"
 source = f"{url}/archive/newsraft-{pkgver}.tar.gz"
-sha256 = "71c29e98b71dd48e445449a689ad9a5b25e56eb294cd79c7ed5627d36eda6769"
+sha256 = "5ae782d7eb19042cd05e260c8ec0fe4d0544e51716885a4b1e96a673576bd998"
 hardening = ["vis", "cfi"]
 
 
