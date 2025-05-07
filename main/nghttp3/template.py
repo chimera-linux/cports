2c22b50dd61fd2fdd342791e1699ac210fbf0333
q66 <q66@chimera-linux.org>
2025-05-07 21:53:48 +0200

    main/nghttp3: update to 1.9.0

--- main/nghttp3/template.py
+++ main/nghttp3/template.py
@@ -1,5 +1,5 @@
 pkgname = "nghttp3"
-pkgver = "1.8.0"
+pkgver = "1.9.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -11,7 +11,7 @@ pkgdesc = "C HTTP/3 library"
 license = "MIT"
 url = "https://nghttp2.org/nghttp3"
 source = f"https://github.com/ngtcp2/nghttp3/releases/download/v{pkgver}/nghttp3-{pkgver}.tar.xz"
-sha256 = "a9dd28970977e6802a3eaf2cfaeae6d0fae60c8d2c0f2c4ce600036a7998ee9a"
+sha256 = "b1b531cf6ae5c9366abea2d3319e4ac932fd64c072c8aafd69cf6295733ce12a"
 
 
 def post_install(self):
