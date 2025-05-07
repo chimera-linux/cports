4f80c47d3d76cf5fbca3c809066e3bca4a2bc26f
q66 <q66@chimera-linux.org>
2025-05-07 21:16:27 +0200

    main/metee: update to 5.0.0

--- main/metee/template.py
+++ main/metee/template.py
@@ -1,5 +1,5 @@
 pkgname = "metee"
-pkgver = "4.3.1"
+pkgver = "5.0.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON"]
@@ -12,7 +12,7 @@ pkgdesc = "Intel CSME HECI interface access library"
 license = "Apache-2.0"
 url = "https://github.com/intel/metee"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "21c31c754421b0beb71e8f6a88fa4829c0c6d530f14c39f4a30d37cc216340b7"
+sha256 = "b18810875ab2e13ffe10afed54f99974ad75fbed536c680d7d1950bb90b20d68"
 
 
 @subpackage("metee-devel")
