b1d948e34edf9465a9415664330f91832248c2d5
q66 <q66@chimera-linux.org>
2025-05-07 22:00:16 +0200

    main/nvtop: update to 3.2.0

--- main/nvtop/template.py
+++ main/nvtop/template.py
@@ -1,6 +1,6 @@
 pkgname = "nvtop"
-pkgver = "3.1.0"
-pkgrel = 1
+pkgver = "3.2.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DBUILD_TESTING=ON",
@@ -19,5 +19,5 @@ pkgdesc = "GPU process monitor"
 license = "GPL-3.0-or-later"
 url = "https://github.com/Syllo/nvtop"
 source = f"https://github.com/Syllo/nvtop/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "9481c45c136163574f1f16d87789859430bc90a1dc62f181b269b5edd92f01f3"
+sha256 = "d26df685455023cedc4dda033862dcddb67402fbdb685da70da78492f73c41d0"
 hardening = ["vis", "!cfi"]
