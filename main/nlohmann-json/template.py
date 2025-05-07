5ae82f972be4d3e078b7684b6eab892db1289645
q66 <q66@chimera-linux.org>
2025-05-07 21:57:08 +0200

    main/nlohmann-json: update to 3.12.0

--- main/nlohmann-json/template.py
+++ main/nlohmann-json/template.py
@@ -1,6 +1,6 @@
 pkgname = "nlohmann-json"
-pkgver = "3.11.3"
-pkgrel = 1
+pkgver = "3.12.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DJSON_BuildTests=OFF"]
 hostmakedepends = [
@@ -12,7 +12,7 @@ pkgdesc = "JSON for modern C++"
 license = "MIT"
 url = "https://json.nlohmann.me"
 source = f"https://github.com/nlohmann/json/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0d8ef5af7f9794e3263480193c491549b2ba6cc74bb018906202ada498a79406"
+sha256 = "4b92eb0c06d10683f7447ce9406cb97cd4b453be18d7279320f7b2f025c10187"
 # header files
 options = ["!check"]
 
