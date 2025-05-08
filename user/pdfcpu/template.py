680473e9c1771acbec554a08825fa131ec40178b
Jens Reidel <adrian@travitia.xyz>
2025-05-08 02:49:38 +0200

    user/pdfcpu: disable checks on 32-bit targets
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- user/pdfcpu/template.py
+++ user/pdfcpu/template.py
@@ -10,3 +10,9 @@ license = "Apache-2.0"
 url = "https://pdfcpu.io"
 source = f"https://github.com/pdfcpu/pdfcpu/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "a07cc50446ef6526fa26d5fe2c9e207724971e0b6917f3d70680ec39cfc53aec"
+# check may be disabled
+options = []
+
+if self.profile().wordsize == 32:
+    # invalid RSA public exponent
+    options += ["!check"]
