b54cd4879fbfaf3d5a295da95209c4eee415f540
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-05-10 11:56:14 +0200

    user/uv: update to 0.7.3

--- user/uv/template.py
+++ user/uv/template.py
@@ -1,5 +1,5 @@
 pkgname = "uv"
-pkgver = "0.7.2"
+pkgver = "0.7.3"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -18,7 +18,7 @@ pkgdesc = "Python package installer"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/astral-sh/uv"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "bf42c698cff01e7f383351efce7f014af788810fa77d41a8f4a853e073582d1e"
+sha256 = "ad6dbd387c8627571580c81e9e127555d4b91d2bec4578876c5f2505a4cda29c"
 # too many of them need net
 # completions with host bin
 options = ["!check", "!cross"]
@@ -27,7 +27,7 @@ if self.profile().wordsize == 32:
     broken = "needs atomic64"
 
 
-def post_patch(self):
+def prepare(self):
     from cbuild.util import cargo
 
     cargo.Cargo(self).vendor()
@@ -41,21 +41,15 @@ def init_build(self):
 
 
 def post_build(self):
-    for shell in ["bash", "fish", "nushell", "zsh"]:
-        with open(self.cwd / f"uv.{shell}", "w") as cf:
-            self.do(
-                f"./target/{self.profile().triplet}/release/uv",
-                "--generate-shell-completion",
-                shell,
-                stdout=cf,
-            )
-        with open(self.cwd / f"uvx.{shell}", "w") as cf:
-            self.do(
-                f"./target/{self.profile().triplet}/release/uvx",
-                "--generate-shell-completion",
-                shell,
-                stdout=cf,
-            )
+    for cmd in ["uv", "uvx"]:
+        for shell in ["bash", "fish", "nushell", "zsh"]:
+            with open(self.cwd / f"{cmd}.{shell}", "w") as cf:
+                self.do(
+                    f"./target/{self.profile().triplet}/release/{cmd}",
+                    "--generate-shell-completion",
+                    shell,
+                    stdout=cf,
+                )
 
 
 def check(self):
@@ -66,6 +60,6 @@ def check(self):
 
 def post_install(self):
     self.install_license("LICENSE-MIT")
-    for shell in ["bash", "fish", "nushell", "zsh"]:
-        self.install_completion(f"uv.{shell}", shell)
-        self.install_completion(f"uvx.{shell}", shell, "uvx")
+    for cmd in ["uv", "uvx"]:
+        for shell in ["bash", "fish", "nushell", "zsh"]:
+            self.install_completion(f"{cmd}.{shell}", shell, cmd)
