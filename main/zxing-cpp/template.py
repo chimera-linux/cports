741855eeb8000aa38e7d9f7823da00c2cfee350e
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-10 06:05:51 +0000

    main/zxing-cpp: Build reader and write programs

--- main/zxing-cpp/template.py
+++ main/zxing-cpp/template.py
@@ -1,14 +1,14 @@
 pkgname = "zxing-cpp"
 pkgver = "2.3.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 configure_args = [
     "-DBUILD_UNIT_TESTS=ON",
-    "-DBUILD_EXAMPLES=OFF",
+    "-DBUILD_EXAMPLES=ON",
     "-DBUILD_BLACKBOX_TESTS=OFF",
     "-DBUILD_DEPENDENCIES=LOCAL",
 ]
-hostmakedepends = ["cmake", "ninja", "pkgconf"]
+hostmakedepends = ["cmake", "ninja", "pkgconf", "stb"]
 checkdepends = ["gtest-devel"]
 pkgdesc = "Multi-format 1D/2D barcode library"
 license = "Apache-2.0"
@@ -20,3 +20,8 @@ sha256 = "64e4139103fdbc57752698ee15b5f0b0f7af9a0331ecbdc492047e0772c417ba"
 @subpackage("zxing-cpp-devel")
 def _(self):
     return self.default_devel()
+
+
+@subpackage("zxing-cpp-progs")
+def _(self):
+    return self.default_progs()
