3b0b992aa5af4b99d42933517c7f156a3cb1caf2
Gabriel M. Dutra <nulldutra@gmail.com>
2025-05-11 03:18:35 -0300

    user/golangci-lint: update to 2.1.6

--- user/golangci-lint/template.py
+++ user/golangci-lint/template.py
@@ -1,6 +1,6 @@
 pkgname = "golangci-lint"
-pkgver = "2.1.5"
-pkgrel = 1
+pkgver = "2.1.6"
+pkgrel = 0
 build_style = "go"
 make_dir = "build-cccc"
 make_build_args = ["./cmd/golangci-lint"]
@@ -9,6 +9,6 @@ pkgdesc = "Linters runner for Go"
 license = "GPL-3.0-or-later"
 url = "https://golangci-lint.run"
 source = f"https://github.com/golangci/golangci-lint/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "57a29dfc7b9e81b5ee42b8f3e807d3c68f9e0e74ddc9e66649bdf625ebd92a12"
+sha256 = "0ae50c50dbc7603ed27a6f559dca194cb8851f030410c5635270866e78cb3400"
 # some tests fail because of chroot and some need network
 options = ["!check"]
