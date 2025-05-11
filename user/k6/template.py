bcc3e7625e9f83c30f6bb39250db9f94588b47c7
Gabriel M. Dutra <nulldutra@gmail.com>
2025-05-11 03:09:30 -0300

    user/k6: update to 1.0.0

--- user/k6/template.py
+++ user/k6/template.py
@@ -1,13 +1,13 @@
 pkgname = "k6"
-pkgver = "0.58.0"
-pkgrel = 2
+pkgver = "1.0.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Load testing tool"
 license = "AGPL-3.0-only"
 url = "https://github.com/grafana/k6"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "013c5deb43264afc2f17a2f059fa27a706464abb235af401acfda26bb45fd8e7"
+sha256 = "790e8a1d1171262095edbd5df5a74f18406d11ea88098560d0f18b7614c8b880"
 # k6 tests requires network connection
 options = ["!check"]
 
