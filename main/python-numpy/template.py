17fe51b021bf7558a7206a1c0d9a49c43fa3c010
Erica Z <zerica@callcc.eu>
2025-05-13 19:56:46 +0200

    main/python-numpy: install static libs, split devel

--- main/python-numpy/template.py
+++ main/python-numpy/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-numpy"
 pkgver = "2.3.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "python_pep517"
 make_build_args = []
 hostmakedepends = [
@@ -78,11 +78,24 @@ def check(self):
 
 def post_install(self):
     self.install_license("LICENSE.txt")
-    # remove static libs
-    self.uninstall(
-        f"usr/lib/python{self.python_version}/site-packages/numpy/*/lib/lib*.a",
-        glob=True,
-    )
+
+
+@subpackage("python-numpy-devel")
+def _(self):
+    self.depends = [self.parent]
+
+    def install():
+        self.take(
+            f"usr/lib/python{self.parent.python_version}/site-packages/numpy/*/include"
+        )
+        self.take(
+            f"usr/lib/python{self.parent.python_version}/site-packages/numpy/*/lib"
+        )
+        self.take(
+            f"usr/lib/python{self.parent.python_version}/site-packages/numpy/*/src"
+        )
+
+    return install
 
 
 @subpackage("python-numpy-tests")
