fb751b697844ae5baf6c2be8325e458b317d8d0e
Renato Botelho do Couto <renato@netgate.com>
2025-05-13 09:02:13 -0500

    user/libyang: new package

---
+++ user/libyang/template.py
@@ -0,0 +1,21 @@
+pkgname = "libyang"
+pkgver = "2.1.148"
+pkgrel = 0
+build_style = "cmake"
+configure_args = ["-DCMAKE_POLICY_VERSION_MINIMUM=3.5"]
+hostmakedepends = ["cmake", "ninja"]
+makedepends = ["pcre2-devel"]
+pkgdesc = "YANG data modelling language parser and toolkit"
+license = "BSD-3-Clause"
+url = "https://github.com/CESNET/libyang"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "77a0aaaeb3df720aeb70d6896e32e2c2be099d48df73e3cfb52567051af3e44b"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("libyang-devel")
+def _(self):
+    return self.default_devel()
