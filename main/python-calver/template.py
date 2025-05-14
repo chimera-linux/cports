0aec0cbe6f5014d35761f43d4053c813d9e99190
triallax <triallax@tutanota.com>
2025-05-14 22:27:51 +0100

    main/python-calver: update to 2025.4.17

--- main/python-calver/template.py
+++ main/python-calver/template.py
@@ -1,18 +1,17 @@
 pkgname = "python-calver"
-pkgver = "2025.4.2"
+pkgver = "2025.4.17"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python"]
 pkgdesc = "Setuptools extension for CalVer package versions"
 license = "Apache-2.0"
 url = "https://github.com/di/calver"
 source = f"$(PYPI_SITE)/c/calver/calver-{pkgver}.tar.gz"
-sha256 = "f854bb865a070da3d45f989f812b2fb6847a10c2d5a7490490ec16832617a463"
+sha256 = "460702737d620f5c3d4175450485180a1b7f7a422c5db0e6af3e655c7395ec7e"
 # no tests
 options = ["!check"]
