616d32dc4f96e323d6611ca0ba431fd792c7e2d5
triallax <triallax@tutanota.com>
2025-05-14 21:51:53 +0100

    main/python-setproctitle: update to 1.3.6
    without python-devel, building the c module fails with a "fatal error:
    'Python.h' file not found" yet the build succeeded anyway??? so calling
    setproctitle didn't do anything

--- main/python-setproctitle/template.py
+++ main/python-setproctitle/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-setproctitle"
-pkgver = "1.3.5"
+pkgver = "1.3.6"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_target = "tests"
@@ -9,13 +9,14 @@ hostmakedepends = [
     "python-setuptools",
     "python-wheel",
 ]
+makedepends = ["python-devel"]
 depends = ["python"]
 checkdepends = ["python-pytest"]
 pkgdesc = "Python module for process title customization"
 license = "BSD-3-Clause"
 url = "https://github.com/dvarrazzo/py-setproctitle"
 source = f"$(PYPI_SITE)/s/setproctitle/setproctitle-{pkgver}.tar.gz"
-sha256 = "1e6eaeaf8a734d428a95d8c104643b39af7d247d604f40a7bebcf3960a853c5e"
+sha256 = "c9f32b96c700bb384f33f7cf07954bb609d35dd82752cef57fb2ee0968409169"
 # can't find itself
 options = ["!check"]
 
