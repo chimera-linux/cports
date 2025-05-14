0ed1ac7a8ea084e9207ca7d832d76ceac2f5b7a1
triallax <triallax@tutanota.com>
2025-05-14 22:31:48 +0100

    main/python-certifi: update to 2025.4.26

--- main/python-certifi/template.py
+++ main/python-certifi/template.py
@@ -1,18 +1,17 @@
 pkgname = "python-certifi"
-pkgver = "2025.1.31"
-pkgrel = 1
+pkgver = "2025.4.26"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["ca-certificates", "python"]
 pkgdesc = "Python package for providing Mozilla's CA bundle"
 license = "MPL-2.0"
 url = "https://github.com/certifi/python-certifi"
 source = f"$(PYPI_SITE)/c/certifi/certifi-{pkgver}.tar.gz"
-sha256 = "3d5da6925056f6f18f119200434a4780a94263f10d1c21d032a6f6b2baa20651"
+sha256 = "0a816057ea3cdefcef70270d2c515e4506bbc954f417fa5ade2021213bb8f0c6"
 # no tests
 options = ["!check"]
