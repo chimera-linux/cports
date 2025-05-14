d1d3d7ddee78191bec9dcc6c4f2ddfae9e17edd9
triallax <triallax@tutanota.com>
2025-05-14 20:25:06 +0100

    main/pixman: update to 0.46.0

--- main/pixman/template.py
+++ main/pixman/template.py
@@ -1,5 +1,5 @@
 pkgname = "pixman"
-pkgver = "0.44.2"
+pkgver = "0.46.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -15,7 +15,7 @@ pkgdesc = "Library of low-level pixel manipulation routines"
 license = "MIT"
 url = "https://pixman.org"
 source = f"https://www.cairographics.org/releases/pixman-{pkgver}.tar.gz"
-sha256 = "6349061ce1a338ab6952b92194d1b0377472244208d47ff25bef86fc71973466"
+sha256 = "02d9ff7b8458ef61731c3d355f854bbf461fd0a4d3563c51f1c1c7b00638050d"
 # needs a lot larger stack than musl default
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=2097152"]}
 # FIXME int (test fails)
