10507794bf2502aeb5252cf3132da19230d75efe
triallax <triallax@tutanota.com>
2025-05-14 20:56:12 +0100

    main/xfce4-dict: update to 0.8.9

--- main/xfce4-dict/template.py
+++ main/xfce4-dict/template.py
@@ -1,5 +1,5 @@
 pkgname = "xfce4-dict"
-pkgver = "0.8.8"
+pkgver = "0.8.9"
 pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
@@ -14,7 +14,6 @@ hostmakedepends = [
 makedepends = [
     "glib-devel",
     "gtk+3-devel",
-    "libx11-devel",
     "libxfce4ui-devel",
     "libxfce4util-devel",
     "xfce4-panel-devel",
@@ -23,6 +22,6 @@ pkgdesc = "Xfce dictionary search app"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/apps/xfce4-dict/start"
 source = (
-    f"$(XFCE_SITE)/apps/xfce4-dict/{pkgver[:-2]}/xfce4-dict-{pkgver}.tar.bz2"
+    f"$(XFCE_SITE)/apps/xfce4-dict/{pkgver[:-2]}/xfce4-dict-{pkgver}.tar.xz"
 )
-sha256 = "0d0cb079d276d82c174a52c94ee4df12df490a525befc44650e261f8e5eae916"
+sha256 = "f1a81baad1e60496aeffc8f8ef1be6413c732b09e250d52d062d293ade001a8c"
