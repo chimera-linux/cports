44e4bab6cdfdf1a029e797272f24bee40eba0c3c
triallax <triallax@tutanota.com>
2025-05-16 01:41:35 +0100

    main/python-cryptography-vectors: update to 44.0.3

--- main/python-cryptography-vectors/template.py
+++ main/python-cryptography-vectors/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-cryptography-vectors"
-pkgver = "44.0.2"
+pkgver = "44.0.3"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,7 +14,7 @@ url = "https://github.com/pyca/cryptography"
 source = (
     f"$(PYPI_SITE)/c/cryptography_vectors/cryptography_vectors-{pkgver}.tar.gz"
 )
-sha256 = "ab32e156b6e7eaf6d8c727a32245967dcce049487301482fca38c0c5fdc84e4b"
+sha256 = "340e790dd7dbe81a0bbec128bfb4ab58e4e10081b05ecb24a8c69380909a3a4b"
 # vectors for python-cryptography tests
 options = ["!check"]
 
