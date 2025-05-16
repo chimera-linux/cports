db68a0250f383db865aa59065eb1ff267f307b16
triallax <triallax@tutanota.com>
2025-05-16 03:28:23 +0100

    main/python-scikit_build_core: update to 0.11.2

--- main/python-scikit_build_core/template.py
+++ main/python-scikit_build_core/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-scikit_build_core"
-pkgver = "0.11.1"
+pkgver = "0.11.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,6 +14,6 @@ pkgdesc = "Python CMake adaptor"
 license = "Apache-2.0"
 url = "https://scikit-build-core.readthedocs.io"
 source = f"$(PYPI_SITE)/s/scikit_build_core/scikit_build_core-{pkgver}.tar.gz"
-sha256 = "4e5988df5cd33f0bdb9967b72663ca99f50383c9bc21d8b24fa40c0661ae72b7"
+sha256 = "778fc71f0b8dd0736fa40a43f98603dd32e176977b00244b4e866d79d4564a2d"
 # needs virtualenv
 options = ["!check"]
