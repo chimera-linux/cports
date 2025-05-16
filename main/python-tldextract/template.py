835a4648111607e703fcccb41383d047320a39ac
triallax <triallax@tutanota.com>
2025-05-16 02:58:00 +0100

    main/python-tldextract: update to 5.3.0

--- main/python-tldextract/template.py
+++ main/python-tldextract/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-tldextract"
-pkgver = "5.2.0"
+pkgver = "5.3.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools_scm",
-    "python-wheel",
 ]
 depends = [
     "python-filelock",
@@ -28,7 +27,7 @@ pkgdesc = "Separates url's subdomain, domain, and public suffix"
 license = "BSD-3-Clause"
 url = "https://github.com/john-kurkowski/tldextract"
 source = f"$(PYPI_SITE)/t/tldextract/tldextract-{pkgver}.tar.gz"
-sha256 = "c3a8c4daf2c25a57f54d6ef6762aeac7eff5ac3da04cdb607130be757b8457ab"
+sha256 = "b3d2b70a1594a0ecfa6967d57251527d58e00bb5a91a74387baa0d87a0678609"
 
 
 def post_install(self):
