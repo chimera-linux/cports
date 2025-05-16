5a54d1327223d8935cb1864d4ffc3a3e3ec2d0d4
triallax <triallax@tutanota.com>
2025-05-16 02:32:59 +0100

    main/python-jsonschema-specifications: update to 2025.4.1

--- main/python-jsonschema-specifications/template.py
+++ main/python-jsonschema-specifications/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-jsonschema-specifications"
-pkgver = "2024.10.1"
-pkgrel = 1
+pkgver = "2025.4.1"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -17,7 +17,7 @@ pkgdesc = "Json Schema meta-schemas and vocabularies, exposed as a Registry"
 license = "MIT"
 url = "https://github.com/python-jsonschema/jsonschema-specifications"
 source = f"$(PYPI_SITE)/j/jsonschema_specifications/jsonschema_specifications-{pkgver}.tar.gz"
-sha256 = "0f38b83639958ce1152d02a7f062902c41c8fd20d558b0c34344292d417ae272"
+sha256 = "630159c9f4dbea161a6a2205c3011cc4f18ff381b189fff48bb39b9bf26ae608"
 
 
 def post_install(self):
