ff4d83e55fb145d5c7cbccd0f8aee6387521a88d
triallax <triallax@tutanota.com>
2025-05-16 02:48:27 +0100

    main/python-trio: update to 0.30.0

--- main/python-trio/template.py
+++ main/python-trio/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-trio"
-pkgver = "0.29.0"
+pkgver = "0.30.0"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
@@ -28,7 +28,7 @@ pkgdesc = "Python library for async concurrency"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/python-trio/trio"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "80da11933df9ec7fa8636966b2b4006e936dcf8127cdc97688af10bb1eb6cb24"
+sha256 = "eaa877a19e21ca500234b28b029ffb46a6b8cedcc6a6387568ee7f8f2b972602"
 
 
 def post_install(self):
