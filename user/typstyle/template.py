5f6101a77d6965f6c80de06689d709b81f0b07c4
ttyyls <contact@behri.org>
2025-05-17 22:40:01 +0100

    user/typstyle: update to 0.13.7

--- user/typstyle/template.py
+++ user/typstyle/template.py
@@ -1,5 +1,5 @@
 pkgname = "typstyle"
-pkgver = "0.13.6"
+pkgver = "0.13.7"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -8,7 +8,7 @@ pkgdesc = "Typst code formatter"
 license = "Apache-2.0"
 url = "https://github.com/Enter-tainer/typstyle"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "3ecac7ccd9e8e6d3ac5750b3036dc0253a4a228c9270c14e0a499c6ae677eb42"
+sha256 = "594e5f65d458250422b4eb05ad66dfbaaa377df97ed88a43f7bd0530c8de4ed2"
 
 if self.profile().wordsize == 32:
     broken = "needs atomic64"
