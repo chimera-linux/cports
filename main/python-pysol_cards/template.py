9eca817db786f6ebb1b8f14abb6809763e0bdb74
triallax <triallax@tutanota.com>
2025-05-18 01:40:31 +0100

    main/python-pysol_cards: update to 0.22.0

--- main/python-pysol_cards/template.py
+++ main/python-pysol_cards/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pysol_cards"
-pkgver = "0.18.1"
-pkgrel = 1
+pkgver = "0.22.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,7 +13,7 @@ pkgdesc = "Python module for dealing cards like various solitaire games"
 license = "MIT"
 url = "https://github.com/shlomif/pysol_cards"
 source = f"$(PYPI_SITE)/p/pysol_cards/pysol_cards-{pkgver}.tar.gz"
-sha256 = "103c7c0c319e72e836e099bbb47ff54be729d975e35c11ba74d4ac5e4286b8eb"
+sha256 = "c555ef5e05ad41774e742b603ce6e6ba76e5d013ddf4ae087a3d87c55279f142"
 
 
 def post_install(self):
