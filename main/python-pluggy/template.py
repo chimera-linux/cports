63385aa686683d67ef57f3f9d47e861e5e5091fb
triallax <triallax@tutanota.com>
2025-05-18 01:39:01 +0100

    main/python-pluggy: update to 1.6.0

--- main/python-pluggy/template.py
+++ main/python-pluggy/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-pluggy"
-pkgver = "1.5.0"
-pkgrel = 1
+pkgver = "1.6.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools_scm",
-    "python-wheel",
 ]
 checkdepends = ["python-pytest"]
 depends = ["python"]
@@ -14,7 +13,7 @@ pkgdesc = "Minimalist production ready plugin system"
 license = "MIT"
 url = "https://github.com/pytest-dev/pluggy"
 source = f"$(PYPI_SITE)/p/pluggy/pluggy-{pkgver}.tar.gz"
-sha256 = "2cffa88e94fdc978c4c574f15f9e59b7f4201d439195c3715ca9e2486f1d0cf1"
+sha256 = "7dcc130b76258d33b90f61b658791dede3486c3e6bfb003ee5c9bfb396dd22f3"
 # dependency of pytest
 options = ["!check"]
 
