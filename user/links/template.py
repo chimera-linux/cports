ead18d7cb4e4d1a45e4290aa8bcd183f85f41b38
unrealapex <unrealapex@disroot.org>
2025-05-19 02:34:52 -0700

    user/links: new package
    Co-authored-by: triallax <triallax@tutanota.com>

---
+++ user/links/template.py
@@ -0,0 +1,19 @@
+pkgname = "links"
+pkgver = "2.30"
+pkgrel = 0
+build_style = "gnu_configure"
+# TODO: configure fails with 'conditional "am__fastdepCXX" was never defined.'???
+configure_gen = []
+hostmakedepends = ["pkgconf"]
+makedepends = [
+    "libjpeg-turbo-devel",
+    "libtiff-devel",
+    "openssl3-devel",
+    "zlib-ng-compat-devel",
+]
+pkgdesc = "Lynx-like text WWW browser"
+license = "GPL-2.0-or-later"
+url = "http://links.twibright.com"
+source = f"{url}/download/links-{pkgver}.tar.gz"
+sha256 = "7f0d54f4f7d1f094c25c9cbd657f98bc998311122563b1d757c9aeb1d3423b9e"
+hardening = ["vis", "cfi"]
