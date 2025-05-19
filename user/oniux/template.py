fb683f49ec1ef3c1b73d81ab3a5e5e42388533d0
breakgimme <adam@plock.com>
2025-05-19 03:03:33 +0200

    user/oniux: new package
    Co-authored-by: JC Grünhage <jan.christian@gruenhage.xyz>
    Co-authored-by: q66 <q66@chimera-linux.org>
    
    Closes https://github.com/chimera-linux/cports/pull/5044

---
+++ user/oniux/template.py
@@ -0,0 +1,21 @@
+pkgname = "oniux"
+pkgver = "0.11.0"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable", "cmake", "pkgconf", "rust-bindgen"]
+makedepends = ["openssl3-devel", "rust-std", "sqlite-devel", "zstd-devel"]
+pkgdesc = "Kernel-level Tor isolation for Linux applications"
+license = "MIT OR Apache-2.0"
+url = "https://gitlab.torproject.org/tpo/core/oniux"
+source = f"{url}/-/archive/v{pkgver}/oniux-v{pkgver}.tar.gz"
+sha256 = "ea59ce1f2884c1cbdcd981fbdef06e7f3a6f12d25870dbd8cc8a213ea80737a8"
+# no tests
+options = ["!check"]
+
+if self.profile().wordsize == 32:
+    broken = "atomic64"
+
+
+def install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/oniux")
+    self.install_license("LICENSE-MIT")
---
+++ user/oniux/update.py
@@ -0,0 +1,2 @@
+url = "https://gitlab.torproject.org/tpo/core/oniux/-/tags?format=atom"
+pattern = r"/tags/v([\d.]+)"
