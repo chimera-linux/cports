beb9c535f593b5549fdad4aef549200486ac362e
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-19 06:57:39 +0000

    user/moar: update to 1.31.7

--- user/moar/template.py
+++ user/moar/template.py
@@ -1,5 +1,5 @@
 pkgname = "moar"
-pkgver = "1.31.5"
+pkgver = "1.31.7"
 pkgrel = 0
 build_style = "go"
 make_build_args = [f"-ldflags=-X main.versionString=v{pkgver}"]
@@ -8,7 +8,7 @@ pkgdesc = "Terminal pager program"
 license = "BSD-2-Clause"
 url = "https://github.com/walles/moar"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "2c03100e24f88163d808d52b7a5763c0fe731b29c46fd44a103a089c5429eb72"
+sha256 = "02e7f8c7f6163380eb444ae45bf353c644a260bb30b9b60a18fef4b028b847e4"
 
 
 def install(self):
