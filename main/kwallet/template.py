c33dc4e303d792764cf5356aa282fd3a8b149c3e
triallax <triallax@tutanota.com>
2025-05-20 16:46:00 +0100

    main/kwallet: update to 6.14.1

--- main/kwallet/template.py
+++ main/kwallet/template.py
@@ -1,5 +1,5 @@
 pkgname = "kwallet"
-pkgver = "6.14.0"
+pkgver = "6.14.1"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -31,7 +31,7 @@ pkgdesc = "KDE Safe desktop-wide storage for passwords"
 license = "LGPL-2.1-or-later"
 url = "https://api.kde.org/frameworks/kwallet/html"
 source = f"$(KDE_SITE)/frameworks/{pkgver[: pkgver.rfind('.')]}/kwallet-{pkgver}.tar.xz"
-sha256 = "a7477e4e5e7ccb43491c5a0eccadf6a33e962088d5af32c4b6b4aacb920f59e3"
+sha256 = "ef482be2d5211f711d578aa1d1f37f71e641a6a52fd37e11327e19915fd42127"
 hardening = ["vis"]
 
 
