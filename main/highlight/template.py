0335fbca128a8310a2519490b1ce693ddea9a3b0
triallax <triallax@tutanota.com>
2025-05-22 04:04:12 +0100

    main/highlight: update to 4.16

--- main/highlight/template.py
+++ main/highlight/template.py
@@ -1,6 +1,6 @@
 pkgname = "highlight"
-pkgver = "4.15"
-pkgrel = 1
+pkgver = "4.16"
+pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["pkgconf"]
 makedepends = ["boost-devel", "lua5.4-devel"]
@@ -8,7 +8,7 @@ pkgdesc = "Syntax highlighter program"
 license = "GPL-3.0-or-later AND LGPL-3.0-or-later AND MIT AND BSD-2-Clause"
 url = "http://www.andre-simon.de/doku/highlight/en/highlight.html"
 source = f"http://www.andre-simon.de/zip/highlight-{pkgver}.tar.bz2"
-sha256 = "68b3f8178c5c9d4b0a03f6948635cef1c8d06244f6b438eebf3a190c588337e9"
+sha256 = "92261ff5c27c73e7a5c85ab65ada2a2edf8aa3dbe9c9c3d8e82e062088e60e5a"
 hardening = ["vis", "cfi"]
 # No tests, can't be bothered to fix cross
 options = ["!check", "!distlicense", "!cross"]
