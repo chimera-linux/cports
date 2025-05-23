3a74c6d56cbc1d4cb309aa3b704f9703e1d00a27
triallax <triallax@tutanota.com>
2025-05-23 03:02:46 +0100

    main/xfce4-verve-plugin: update to 2.1.0

--- main/xfce4-verve-plugin/template.py
+++ main/xfce4-verve-plugin/template.py
@@ -1,13 +1,11 @@
 pkgname = "xfce4-verve-plugin"
-pkgver = "2.0.4"
+pkgver = "2.1.0"
 pkgrel = 0
-build_style = "gnu_configure"
+build_style = "meson"
 hostmakedepends = [
-    "automake",
-    "gettext-devel",
+    "gettext",
+    "meson",
     "pkgconf",
-    "slibtool",
-    "xfce4-dev-tools",
 ]
 makedepends = [
     "gtk+3-devel",
@@ -18,5 +16,5 @@ makedepends = [
 pkgdesc = "Xfce command line panel plugin"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-verve-plugin/start"
-source = f"$(XFCE_SITE)/panel-plugins/xfce4-verve-plugin/{pkgver[:-2]}/xfce4-verve-plugin-{pkgver}.tar.bz2"
-sha256 = "214a812dd7cc410bbe85d736e0dc76459ddd7861e5a1c60b67dd89dcd34e90a1"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-verve-plugin/{pkgver[:-2]}/xfce4-verve-plugin-{pkgver}.tar.xz"
+sha256 = "237e0da802cdc02e0ec0c3cdefecb6fa2992ade9f59ce2999779cc30d59c9f24"
