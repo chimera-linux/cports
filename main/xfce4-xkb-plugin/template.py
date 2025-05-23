6bed388f949a2bcae63581a27421df05217f0744
triallax <triallax@tutanota.com>
2025-05-23 03:00:23 +0100

    main/xfce4-xkb-plugin: update to 0.9.0

--- main/xfce4-xkb-plugin/template.py
+++ main/xfce4-xkb-plugin/template.py
@@ -1,13 +1,11 @@
 pkgname = "xfce4-xkb-plugin"
-pkgver = "0.8.5"
+pkgver = "0.9.0"
 pkgrel = 0
-build_style = "gnu_configure"
+build_style = "meson"
 hostmakedepends = [
-    "automake",
-    "gettext-devel",
+    "gettext",
+    "meson",
     "pkgconf",
-    "slibtool",
-    "xfce4-dev-tools",
 ]
 makedepends = [
     "garcon-devel",
@@ -25,5 +23,5 @@ makedepends = [
 pkgdesc = "Xfce keyboard layout panel plugin"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-xkb-plugin/start"
-source = f"$(XFCE_SITE)/panel-plugins/xfce4-xkb-plugin/{pkgver[:-2]}/xfce4-xkb-plugin-{pkgver}.tar.bz2"
-sha256 = "86ce4a194c47c506315cfded3a041067a72dedcb6d9cc2f9c99853203d332b19"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-xkb-plugin/{pkgver[:-2]}/xfce4-xkb-plugin-{pkgver}.tar.xz"
+sha256 = "7cd7f3626ef39dc4ce142b2f96ab7583cbea84b4c0352fbc9c9667faac0bdd12"
