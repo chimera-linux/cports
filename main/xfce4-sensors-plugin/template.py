114c93683941d743c597f23c5d336edd30fb77b6
triallax <triallax@tutanota.com>
2025-05-23 03:02:42 +0100

    main/xfce4-sensors-plugin: update to 1.5.0

--- main/xfce4-sensors-plugin/template.py
+++ main/xfce4-sensors-plugin/template.py
@@ -1,14 +1,12 @@
 pkgname = "xfce4-sensors-plugin"
-pkgver = "1.4.5"
+pkgver = "1.5.0"
 pkgrel = 0
-build_style = "gnu_configure"
-configure_args = ["--disable-procapi", "--enable-sysfsacpi"]
+build_style = "meson"
+configure_args = ["-Dprocacpi=disabled", "-Dsysfsacpi=enabled"]
 hostmakedepends = [
-    "automake",
-    "gettext-devel",
+    "gettext",
+    "meson",
     "pkgconf",
-    "slibtool",
-    "xfce4-dev-tools",
 ]
 makedepends = [
     "gtk+3-devel",
@@ -21,10 +19,5 @@ makedepends = [
 pkgdesc = "Xfce hardware sensors panel plugin"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-sensors-plugin/start"
-source = f"$(XFCE_SITE)/panel-plugins/xfce4-sensors-plugin/{pkgver[:-2]}/xfce4-sensors-plugin-{pkgver}.tar.bz2"
-sha256 = "f69fdf79b7f76d2a81724828124a6fce76803a9122a4c82de8f3dfa3efbb179a"
-
-
-@subpackage("xfce4-sensors-plugin-devel")
-def _(self):
-    return self.default_devel()
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-sensors-plugin/{pkgver[:-2]}/xfce4-sensors-plugin-{pkgver}.tar.xz"
+sha256 = "840442b87fdddcd8595bd9f83ea8b81f771fe296bb9d2abf0e1979e208727ae9"
