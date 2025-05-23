d493c6bc81665ca9e8a69779ddd235ca5933ef8d
triallax <triallax@tutanota.com>
2025-05-23 03:02:38 +0100

    main/xfce4-generic-slider: update to 1.1.0

--- main/xfce4-generic-slider/template.py
+++ main/xfce4-generic-slider/template.py
@@ -1,13 +1,11 @@
 pkgname = "xfce4-generic-slider"
-pkgver = "1.0.1"
+pkgver = "1.1.0"
 pkgrel = 0
-build_style = "gnu_configure"
+build_style = "meson"
 hostmakedepends = [
-    "automake",
-    "gettext-devel",
+    "gettext",
+    "meson",
     "pkgconf",
-    "slibtool",
-    "xfce4-dev-tools",
 ]
 makedepends = [
     "gtk+3-devel",
@@ -17,5 +15,5 @@ makedepends = [
 pkgdesc = "Xfce generic slider panel plugin"
 license = "GPL-3.0-only"
 url = "https://docs.xfce.org/panel-plugins/xfce4-generic-slider/start"
-source = f"$(XFCE_SITE)/panel-plugins/xfce4-generic-slider/{pkgver[:-2]}/xfce4-generic-slider-{pkgver}.tar.bz2"
-sha256 = "5f3f1da2fa1428ddc51dbbdcdf119f88b023260b605edd3d1d796261a047f0ec"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-generic-slider/{pkgver[:-2]}/xfce4-generic-slider-{pkgver}.tar.xz"
+sha256 = "0cef3174157621e14d123a9d246ee3b1d7c8ef89579377398305a4eb33636f5f"
