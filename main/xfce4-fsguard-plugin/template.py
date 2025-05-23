164d9404c18cd0a4dbb177402668e89dc4255e2b
triallax <triallax@tutanota.com>
2025-05-23 03:02:37 +0100

    main/xfce4-fsguard-plugin: update to 1.2.0

--- main/xfce4-fsguard-plugin/template.py
+++ main/xfce4-fsguard-plugin/template.py
@@ -1,13 +1,11 @@
 pkgname = "xfce4-fsguard-plugin"
-pkgver = "1.1.4"
+pkgver = "1.2.0"
 pkgrel = 0
-build_style = "gnu_configure"
+build_style = "meson"
 hostmakedepends = [
-    "automake",
-    "gettext-devel",
+    "gettext",
+    "meson",
     "pkgconf",
-    "slibtool",
-    "xfce4-dev-tools",
 ]
 makedepends = [
     "gtk+3-devel",
@@ -19,8 +17,8 @@ makedepends = [
 pkgdesc = "Xfce free space guard panel plugin"
 license = "BSD-2-Clause"
 url = "https://docs.xfce.org/panel-plugins/xfce4-fsguard-plugin/start"
-source = f"$(XFCE_SITE)/panel-plugins/xfce4-fsguard-plugin/{pkgver[:-2]}/xfce4-fsguard-plugin-{pkgver}.tar.bz2"
-sha256 = "24b3789be6de92678e7036678530c4da4b3838aa3cda428439aa8d140704a4a6"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-fsguard-plugin/{pkgver[:-2]}/xfce4-fsguard-plugin-{pkgver}.tar.xz"
+sha256 = "9e40cf3ce7b34e1c27d6b442f3a067886c35154b5d0c4d644a239038611da64f"
 
 
 def post_install(self):
