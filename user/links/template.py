fd58e71aed31e35f9f862cdd5b17938f66d56d66
unrealapex <unrealapex@proton.me>
2025-05-22 18:50:52 -0700

    Apply suggestions from code review
    Co-authored-by: triallax <triallax@tutanota.com>

--- user/links/template.py
+++ user/links/template.py
@@ -1,9 +1,10 @@
 pkgname = "links"
 pkgver = "2.30"
 pkgrel = 0
-build_style = "configure"
-configure_args = ["--prefix=/usr", "--mandir=PREFIX/share/man"]
-hostmakedepends = [
+build_style = "gnu_configure"
+# TODO: configure fails with 'conditional "am__fastdepCXX" was never defined.'???
+configure_gen = []
+makedepends = [
     "libjpeg-turbo-devel",
     "libtiff-devel",
     "openssl3-devel",
@@ -12,5 +13,5 @@ hostmakedepends = [
 pkgdesc = "Lynx-like text WWW browser"
 license = "GPL-2.0-or-later"
 url = "http://links.twibright.com"
-source = f"{url}/download/{pkgname}-{pkgver}.tar.gz"
+source = f"{url}/download/links-{pkgver}.tar.gz"
 sha256 = "7f0d54f4f7d1f094c25c9cbd657f98bc998311122563b1d757c9aeb1d3423b9e"
