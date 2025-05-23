1cb3eaf4e77849ddef536a8a732697049bdc74bd
Ayush Agarwal <ayush@ayushnix.com>
2025-05-23 18:52:50 +0530

    main/tree-sitter: update to 0.25.4
    main/tree-sitter-cli: update to 0.25.4
    main/tree-sitter-lua: update to 0.4.0
    main/tree-sitter-markdown: update to 0.5.0
    main/tree-sitter-query: update to 0.6.1
    main/tree-sitter-vimdoc: update to 4.0.0
    
    removed patches which were no longer necessary because they were
    already part of the sources
    
    bumped pkgrel for neovim, emacs-console, emacs-gtk3, and emacs-pgtk

--- main/tree-sitter/template.py
+++ main/tree-sitter/template.py
@@ -1,6 +1,6 @@
 pkgname = "tree-sitter"
 # match to tree-sitter-cli
-pkgver = "0.25.3"
+pkgver = "0.25.4"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["pkgconf"]
@@ -8,7 +8,7 @@ pkgdesc = "Incremental parsing library for language grammars"
 license = "MIT"
 url = "https://tree-sitter.github.io/tree-sitter"
 source = f"https://github.com/tree-sitter/tree-sitter/archive/v{pkgver}.tar.gz"
-sha256 = "862fac52653bc7bc9d2cd0630483e6bdf3d02bcd23da956ca32663c4798a93e3"
+sha256 = "87eadc505905c70a692917c821958a819903f808f8d244068b1d273a033dc728"
 # check requires cargo/fixture stuff (from remote repositories)
 options = ["!check"]
 
