f05e681481c4f1e32ab8c554eb7717b4c7dbe8b5
triallax <triallax@tutanota.com>
2025-05-23 03:02:32 +0100

    main/xfce4-battery-plugin: update to 1.2.0

--- main/xfce4-battery-plugin/template.py
+++ main/xfce4-battery-plugin/template.py
@@ -1,14 +1,8 @@
 pkgname = "xfce4-battery-plugin"
-pkgver = "1.1.6"
+pkgver = "1.2.0"
 pkgrel = 0
-build_style = "gnu_configure"
-hostmakedepends = [
-    "automake",
-    "gettext-devel",
-    "pkgconf",
-    "slibtool",
-    "xfce4-dev-tools",
-]
+build_style = "meson"
+hostmakedepends = ["meson", "gettext", "pkgconf"]
 makedepends = [
     "gtk+3-devel",
     "libxfce4ui-devel",
@@ -18,5 +12,5 @@ makedepends = [
 pkgdesc = "Xfce battery panel plugin"
 license = "GPL-2.0-or-later AND LGPL-2.0-or-later"
 url = "https://docs.xfce.org/panel-plugins/xfce4-battery-plugin/start"
-source = f"$(XFCE_SITE)/panel-plugins/xfce4-battery-plugin/{pkgver[:-2]}/xfce4-battery-plugin-{pkgver}.tar.bz2"
-sha256 = "327d7304dded8411cd1a48da4f46bac248b44d8b27ffbc2036e0268cb37c3676"
+source = f"$(XFCE_SITE)/panel-plugins/xfce4-battery-plugin/{pkgver[:-2]}/xfce4-battery-plugin-{pkgver}.tar.xz"
+sha256 = "1dba8f470d2b12517e7b86d83cd2ebf13eb57ff1a01a4f2d2d156cf5194d97b6"
