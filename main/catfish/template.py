92ff83342534549086f1713445a3bb72da316af9
triallax <triallax@tutanota.com>
2025-05-23 03:05:45 +0100

    main/catfish: update to 4.20.1

--- main/catfish/template.py
+++ main/catfish/template.py
@@ -1,6 +1,6 @@
 pkgname = "catfish"
-pkgver = "4.20.0"
-pkgrel = 1
+pkgver = "4.20.1"
+pkgrel = 0
 build_style = "meson"
 _deps = ["python-dbus", "python-gobject", "python-pexpect"]
 hostmakedepends = [
@@ -22,7 +22,7 @@ pkgdesc = "Xfce file search tool"
 # TODO: https://gitlab.xfce.org/apps/catfish/-/issues/106
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/apps/catfish/start"
-source = f"$(XFCE_SITE)/apps/catfish/{pkgver[:-2]}/catfish-{pkgver}.tar.bz2"
-sha256 = "3938a3cd5a9ecd75c9c7777f7204f8e4cfcb9960203ffd8c2df7d08d11a73a6e"
+source = f"$(XFCE_SITE)/apps/catfish/{pkgver[:-2]}/catfish-{pkgver}.tar.xz"
+sha256 = "fe00d45b163cf86b4c85ebdd23a73d53aa55bc97ba3f691a248ec403d4ade62b"
 # No tests
 options = ["!check"]
