5a9238a8841f205ee0112fd6a26513c28b312a95
triallax <triallax@tutanota.com>
2025-05-25 00:22:48 +0100

    main/python-click: update to 8.2.1

--- main/python-click/template.py
+++ main/python-click/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-click"
-pkgver = "8.2.0"
+pkgver = "8.2.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ pkgdesc = "Python module for command line interfaces"
 license = "BSD-3-Clause"
 url = "https://palletsprojects.com/p/click"
 source = f"$(PYPI_SITE)/c/click/click-{pkgver}.tar.gz"
-sha256 = "f5452aeddd9988eefa20f90f05ab66f17fce1ee2a36907fd30b05bbb5953814d"
+sha256 = "27c491cc05d968d271d5a1db13e3b5a184636d9d930f148c50b038f0d0646202"
 
 
 def post_install(self):
