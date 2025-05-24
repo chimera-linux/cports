7aa465dd9bdb7ce24214aefc3c2c2c6b580950cb
triallax <triallax@tutanota.com>
2025-05-24 16:09:01 +0100

    main/kddockwidgets: update to 2.2.5

--- main/kddockwidgets/template.py
+++ main/kddockwidgets/template.py
@@ -1,6 +1,6 @@
 pkgname = "kddockwidgets"
-pkgver = "2.2.4"
-pkgrel = 1
+pkgver = "2.2.5"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DKDDockWidgets_QT6=ON",
@@ -23,7 +23,7 @@ pkgdesc = "Dock Widget Framework for Qt"
 license = "GPL-2.0-only OR GPL-3.0-only"
 url = "https://www.kdab.com/development-resources/qt-tools/kddockwidgets"
 source = f"https://github.com/KDAB/KDDockWidgets/releases/download/v{pkgver}/kddockwidgets-{pkgver}.tar.gz"
-sha256 = "a1cf55a3cf267108ee495de8df9038c67f61da5ca324059cb32543d69877524e"
+sha256 = "1c202d03a0c7018aebcb249b09122d846b34298d88d0bc247a601f48c7513c89"
 
 
 @subpackage("kddockwidgets-devel")
