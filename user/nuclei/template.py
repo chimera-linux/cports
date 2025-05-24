083f5899fd989cdf68bd511b38645936dc25fa1c
Gabriel M. Dutra <nulldutra@gmail.com>
2025-05-24 17:41:38 +0000

    user/nuclei: update to 3.4.4

--- user/nuclei/template.py
+++ user/nuclei/template.py
@@ -1,6 +1,6 @@
 pkgname = "nuclei"
-pkgver = "3.4.2"
-pkgrel = 2
+pkgver = "3.4.4"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/nuclei"]
 hostmakedepends = ["go"]
@@ -8,7 +8,7 @@ pkgdesc = "Web vulnerability scanner"
 license = "MIT"
 url = "https://github.com/projectdiscovery/nuclei"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "9c7188baa161430942a751f2c95fa1c557d1afa0846110a8dcc08167c97c8399"
+sha256 = "977726c98398fc3f9348bc7ffbf3704b5eedcaa754f7f964a8de25d3c9ca6c59"
 # Nuclei tests requires network connection
 options = ["!check"]
 
