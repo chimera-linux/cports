d4f8b685aa996b4a42074eef18b38c7ec758de28
Gabriel M. Dutra <nulldutra@gmail.com>
2025-05-24 17:49:01 +0000

    user/btop: update to 1.4.3

--- user/btop/template.py
+++ user/btop/template.py
@@ -1,5 +1,5 @@
 pkgname = "btop"
-pkgver = "1.4.1"
+pkgver = "1.4.3"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBTOP_LTO=OFF", "-DBTOP_FORTIFY=OFF"]
@@ -9,7 +9,7 @@ pkgdesc = "TUI monitor of system resources"
 license = "Apache-2.0"
 url = "https://github.com/aristocratos/btop"
 source = f"{url}/archive/v{pkgver}/btop-{pkgver}.tar.gz"
-sha256 = "40f6c54d1bc952c674b677d81dd25f55b61e9c004883c27950dc30780c86f381"
+sha256 = "81b133e59699a7fd89c5c54806e16452232f6452be9c14b3a634122e3ebed592"
 hardening = ["cfi", "vis"]
 
 
