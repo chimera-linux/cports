46cf606fe4a6c59d58d5b38beb530edb6bab35e2
Ayush Agarwal <ayush@ayushnix.com>
2025-05-24 19:56:26 +0530

    user/featherpad: update to 1.6.2

--- user/featherpad/template.py
+++ user/featherpad/template.py
@@ -1,5 +1,5 @@
 pkgname = "featherpad"
-pkgver = "1.6.0"
+pkgver = "1.6.2"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -18,4 +18,4 @@ pkgdesc = "Qt Plain-Text editor"
 license = "GPL-3.0-or-later"
 url = "https://github.com/tsujan/FeatherPad"
 source = f"{url}/releases/download/V{pkgver}/FeatherPad-{pkgver}.tar.xz"
-sha256 = "246e7d72572a8f44a3e22bb1a9eba5672ff0e997053e19ba78abcfab5d5ca41d"
+sha256 = "0c6ad7687d933e48f5e64047dacb4d4611155210ef1557874fb708e805a50daa"
