dadac954772b660a7770e3100ff245f3c5ab03a8
triallax <triallax@tutanota.com>
2025-05-24 18:57:04 +0100

    main/tinysparql: update to 3.9.2

--- main/tinysparql/template.py
+++ main/tinysparql/template.py
@@ -1,6 +1,6 @@
 pkgname = "tinysparql"
-pkgver = "3.9.1"
-pkgrel = 2
+pkgver = "3.9.2"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     # TODO: user services with dinit?
@@ -11,51 +11,45 @@ configure_args = [
 ]
 make_check_wrapper = ["dbus-run-session"]
 hostmakedepends = [
+    "asciidoc",
+    "dbus",
+    "gettext",
+    "glib-devel",
+    "gobject-introspection",
+    "libxslt-progs",
     "meson",
     "pkgconf",
-    "glib-devel",
-    "gettext",
-    "vala",
-    "asciidoc",
-    "libxslt-progs",
-    "dbus",
-    "gobject-introspection",
     "python-gobject",
+    "vala",
 ]
 makedepends = [
-    "glib-devel",
+    "bash-completion",
     "dbus-devel",
+    "glib-devel",
     "icu-devel",
     "json-glib-devel",
+    "libsoup-devel",
     "libxml2-devel",
     "sqlite-devel",
-    "libsoup-devel",
-    "bash-completion",
 ]
 depends = ["shared-mime-info"]
 checkdepends = ["mandoc"]
-provides = [self.with_pkgver("tracker")]
+renames = ["tracker"]
 pkgdesc = "Search engine and triplestore for desktop, embedded and mobile"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://gnome.pages.gitlab.gnome.org/tinysparql"
 source = f"$(GNOME_SITE)/tinysparql/{pkgver[:-2]}/tinysparql-{pkgver}.tar.xz"
-sha256 = "98e8933b946d8c54a77eadabd7f1d14cddd88fa43eef0d0ace3c0fcce646722b"
-# check may be disabled
+sha256 = "14ce039024135e181022bcce4b1aad2e00377dd9c7d812b98391ccfc756dad8e"
 options = ["!cross"]
-
-
-if self.profile().arch == "ppc64le":
-    # https://gitlab.gnome.org/GNOME/tinysparql/-/issues/474
-    options += ["!check"]
 
 
 @subpackage("tinysparql-devel")
 def _(self):
-    self.provides = [self.with_pkgver("tracker-devel")]
+    self.renames = ["tracker-devel"]
     return self.default_devel()
 
 
 @subpackage("tinysparql-libs")
 def _(self):
-    self.provides = [self.with_pkgver("tracker-libs")]
+    self.renames = ["tracker-libs"]
     return self.default_libs()
