3bb9e44a285ac0301de92f963adc38a8d12fdcc0
triallax <triallax@tutanota.com>
2025-05-25 01:45:59 +0100

    main/evolution-data-server: update to 3.56.2

--- main/evolution-data-server/template.py
+++ main/evolution-data-server/template.py
@@ -1,5 +1,5 @@
 pkgname = "evolution-data-server"
-pkgver = "3.56.1"
+pkgver = "3.56.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -24,27 +24,27 @@ hostmakedepends = [
 ]
 makedepends = [
     "glib-devel",
-    "libcanberra-devel",
-    "libical-devel",
+    "gnome-online-accounts-devel",
     "heimdal-devel",
+    "json-glib-devel",
+    "libcanberra-devel",
+    "libgweather-devel",
+    "libical-devel",
+    "libsecret-devel",
+    "libsoup-devel",
+    "nspr-devel",
+    "nss-devel",
+    "sqlite-devel",
+    "vala-devel",
     "webkitgtk-devel",
     "webkitgtk4-devel",
-    "libsecret-devel",
-    "nss-devel",
-    "nspr-devel",
-    "gnome-online-accounts-devel",
-    "sqlite-devel",
-    "libgweather-devel",
-    "libsoup-devel",
-    "json-glib-devel",
-    "vala-devel",
 ]
 checkdepends = ["dbus"]
 pkgdesc = "Centralized access to appointments and contacts"
 license = "LGPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/evolution-data-server"
 source = f"$(GNOME_SITE)/evolution-data-server/{pkgver[:-2]}/evolution-data-server-{pkgver}.tar.xz"
-sha256 = "646cc0037da3f9f295794c637d95394ad76f8c9bee2268be2c4183e27720c137"
+sha256 = "df4ec29950f29a76eac6fbe0f814c48d2cef7d3fdb905002a4a883dd761ce93c"
 options = ["!cross"]
 
 
