d22714d5a04424040eb52d0941975b6c245d14cf
ttyyls <contact@behri.org>
2025-05-26 10:28:20 +0100

    user/luanti: move from main, update to 5.12.0

--- user/luanti/template.py
+++ user/luanti/template.py
@@ -1,6 +1,6 @@
 pkgname = "luanti"
-pkgver = "5.11.0"
-pkgrel = 1
+pkgver = "5.12.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DBUILD_SERVER=TRUE",
@@ -8,6 +8,7 @@ configure_args = [
     "-DENABLE_LTO=OFF",
     "-DENABLE_PROMETHEUS=ON",
     "-DENABLE_UPDATE_CHECKER=OFF",
+    "-DUSE_SDL2=ON",
 ]
 hostmakedepends = [
     "cmake",
@@ -19,35 +20,35 @@ hostmakedepends = [
 ]
 makedepends = [
     "bzip2-devel",
+    "curl-devel",
     "freetype-devel",
     "gettext-devel",
     "gmp-devel",
     "hiredis-devel",
     "jsoncpp-devel",
-    "curl-devel",
     "libjpeg-turbo-devel",
     "libogg-devel",
     "libpng-devel",
     "libvorbis-devel",
-    "libxi-devel",
     "luajit-devel",
     "mesa-devel",
     "ncurses-devel",
     "openal-soft-devel",
     "openssl3-devel",
     "prometheus-cpp-devel",
+    "sdl2-devel",
     "sqlite-devel",
     "zstd-devel",
 ]
 depends = [self.with_pkgver("luanti-common")]
-provides = [self.with_pkgver("minetest")]
+renames = ["minetest"]
 pkgdesc = "Voxel game creation platform"
 license = "LGPL-2.1-or-later"
 url = "https://www.luanti.org"
 source = (
-    f"https://github.com/minetest/minetest/archive/refs/tags/{pkgver}.tar.gz"
+    f"https://github.com/luanti-org/luanti/archive/refs/tags/{pkgver}.tar.gz"
 )
-sha256 = "70e531d0776988ce6e579ea5490fdf6be3e349a4ade5281f5111aa4fdd8ee510"
+sha256 = "876867ac874492f20968f2c2eb4e403231e8e9f29e0e06efa512200bd5152355"
 tool_flags = {"CFLAGS": ["-DNDEBUG"], "CXXFLAGS": ["-DNDEBUG"]}
 hardening = ["!int"]
 # see below
@@ -76,7 +77,7 @@ def post_install(self):
 @subpackage("luanti-common")
 def _(self):
     self.subdesc = "common files"
-    self.provides = [self.with_pkgver("minetest-common")]
+    self.renames = ["minetest-common"]
 
     return ["usr/share/luanti/builtin"]
 
@@ -85,6 +86,6 @@ def _(self):
 def _(self):
     self.subdesc = "server"
     self.depends = [self.with_pkgver("luanti-common")]
-    self.provides = [self.with_pkgver("minetest-server")]
+    self.renames = ["minetest-server"]
 
     return ["usr/bin/luantiserver"]
