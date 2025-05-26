990a5bcd122e9068097c3373de4b32f56072cd8f
triallax <triallax@tutanota.com>
2025-05-27 00:30:32 +0100

    main/python-freezegun: update to 1.5.2

--- main/python-freezegun/template.py
+++ main/python-freezegun/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-freezegun"
-pkgver = "1.5.1"
-pkgrel = 1
+pkgver = "1.5.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python-dateutil"]
 checkdepends = [
@@ -17,6 +16,4 @@ pkgdesc = "Let your python tests travel through time"
 license = "Apache-2.0"
 url = "https://github.com/spulec/freezegun"
 source = f"$(PYPI_SITE)/f/freezegun/freezegun-{pkgver}.tar.gz"
-sha256 = "b29dedfcda6d5e8e083ce71b2b542753ad48cfec44037b3fc79702e2980a89e9"
-# some date shenanigans
-options = ["!check"]
+sha256 = "a54ae1d2f9c02dbf42e02c18a3ab95ab4295818b549a34dac55592d72a905181"
