657d44745525030d2098144b32e40b4dcac00fb1
triallax <triallax@tutanota.com>
2025-05-26 18:04:38 +0100

    main/gexiv2: update to 0.14.5

--- main/gexiv2/template.py
+++ main/gexiv2/template.py
@@ -1,6 +1,6 @@
 pkgname = "gexiv2"
-pkgver = "0.14.3"
-pkgrel = 1
+pkgver = "0.14.5"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=true", "-Dvapi=true"]
 hostmakedepends = [
@@ -16,7 +16,7 @@ pkgdesc = "GObject wrapper for exiv2"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Projects/gexiv2"
 source = f"$(GNOME_SITE)/gexiv2/{pkgver[:-2]}/gexiv2-{pkgver}.tar.xz"
-sha256 = "21e64d2c56e9b333d44fef3f2a4b25653d922c419acd972fa96fab695217e2c8"
+sha256 = "0913c53daabab1f1ab586afd55bb55370796f2b8abcc6e37640ab7704ad99ce1"
 
 
 @subpackage("gexiv2-devel")
