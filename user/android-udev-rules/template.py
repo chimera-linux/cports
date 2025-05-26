66409825caa8005c619e9da99d22e8e886e6c0e2
triallax <triallax@tutanota.com>
2025-05-26 19:38:31 +0100

    user/android-udev-rules: update to 20250525

--- user/android-udev-rules/template.py
+++ user/android-udev-rules/template.py
@@ -1,11 +1,11 @@
 pkgname = "android-udev-rules"
-pkgver = "20250314"
+pkgver = "20250525"
 pkgrel = 0
 pkgdesc = "Comprehensive Android udev rules collection"
 license = "GPL-3.0-or-later"
 url = "https://github.com/M0Rf30/android-udev-rules"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "a1b3b6055cdb74a013fe3afcfe1e505bc6ca6339f05d64410660d37f1aca2c8d"
+sha256 = "582bf8daa23f318047e77ece4c101c8696fd9151c459f695dca56cf4a40a72a2"
 options = ["!splitudev"]
 
 
