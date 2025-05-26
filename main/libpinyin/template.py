54ad7ac25ef9c51d8d9419b92d0ca9bcd3f1bc6e
triallax <triallax@tutanota.com>
2025-05-26 18:45:18 +0100

    main/libpinyin: update to 2.10.2

--- main/libpinyin/template.py
+++ main/libpinyin/template.py
@@ -1,5 +1,5 @@
 pkgname = "libpinyin"
-pkgver = "2.10.1"
+pkgver = "2.10.2"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-dbm=KyotoCabinet", "--enable-libzhuyin"]
@@ -9,7 +9,7 @@ pkgdesc = "Algorithms core for intelligent sentence-based Chinese pinyin IMEs"
 license = "GPL-3.0-or-later"
 url = "https://github.com/libpinyin/libpinyin"
 source = f"{url}/releases/download/{pkgver}/libpinyin-{pkgver}.tar.gz"
-sha256 = "bc5fd25724f9863bc329d2aa3d400dce28130efdb819bb96d42db52b0845d37f"
+sha256 = "2c29d9dfd1e0dc521fa654b300cc5560bad82d6bed42e4b3ec9d71098fb26d80"
 
 
 @subpackage("libpinyin-devel")
