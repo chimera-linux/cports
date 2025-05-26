07cfb2b8ab1bd3cfe969f9d96899ce378b504e40
triallax <triallax@tutanota.com>
2025-05-26 19:52:29 +0100

    user/wob: move from main, update to 0.16

--- user/wob/template.py
+++ user/wob/template.py
@@ -1,5 +1,5 @@
 pkgname = "wob"
-pkgver = "0.15.1"
+pkgver = "0.16"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dseccomp=enabled"]
@@ -16,9 +16,12 @@ pkgdesc = "Wayland Overlay Bar"
 license = "ISC"
 url = "https://github.com/francma/wob"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "094f666e799a7fa0914192d041032f4e9a28a87486d024db80ade010988b218a"
+sha256 = "8a5850beec72b5b19be631a6eb21315a20082bf9135447080f9e9045f143938b"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("LICENSE")
+
+    # TODO: dinit service?
+    self.uninstall("usr/lib/systemd")
