1cdac32e2decb17046023f6e4bbb0fb9cd96040b
triallax <triallax@tutanota.com>
2025-05-26 18:37:48 +0100

    main/gnome-online-accounts: update to 3.54.3

--- main/gnome-online-accounts/template.py
+++ main/gnome-online-accounts/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-online-accounts"
-pkgver = "3.54.2"
+pkgver = "3.54.3"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -39,7 +39,7 @@ pkgdesc = "GNOME service to access online accounts"
 license = "LGPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-online-accounts"
 source = f"$(GNOME_SITE)/gnome-online-accounts/{pkgver[:-2]}/gnome-online-accounts-{pkgver}.tar.xz"
-sha256 = "0f7f3d29c0666ffbcc503f3e0f3396362effdb9317d41664ce65df147ef35372"
+sha256 = "bcf655dd1ddc22bc25793b6840da19f5cad7ba0b7227ff969ed9c252f036aac5"
 options = ["!cross"]
 
 
