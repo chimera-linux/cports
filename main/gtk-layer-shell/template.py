79965da5a26c94fca06d1096eb817d5e29a60525
triallax <triallax@tutanota.com>
2025-05-26 19:26:39 +0100

    main/gtk-layer-shell: update to 0.9.2

--- main/gtk-layer-shell/template.py
+++ main/gtk-layer-shell/template.py
@@ -1,5 +1,5 @@
 pkgname = "gtk-layer-shell"
-pkgver = "0.9.1"
+pkgver = "0.9.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -9,12 +9,12 @@ hostmakedepends = [
     "vala",
     "wayland-progs",
 ]
-makedepends = ["gtk+3-devel"]
+makedepends = ["gtk+3-devel", "wayland-protocols"]
 pkgdesc = "Library to create panels and other desktop components for Wayland"
 license = "LGPL-3.0-or-later AND MIT"
 url = "https://github.com/wmww/gtk-layer-shell"
-source = f"https://github.com/wmww/gtk-layer-shell/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "43e2165cf1a9aa8a317b081c2a583648e02389162f1fbbd33836ba27f9ca19fa"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "526dd95c083e2a73eafd8baa1f5d676a36cb80fc8e7b304cbe3efebd62f0600c"
 # vis breaks sumbols
 hardening = ["!vis"]
 # a few tests fail
