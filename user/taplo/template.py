3ae916091fa9398c9567d8f44addb03194017bbb
ttyyls <contact@behri.org>
2025-05-26 11:58:51 +0100

    user/taplo: update to 0.10.0

--- user/taplo/template.py
+++ user/taplo/template.py
@@ -1,13 +1,13 @@
 pkgname = "taplo"
-pkgver = "0.9.3"
-pkgrel = 1
+pkgver = "0.10.0"
+pkgrel = 0
 build_style = "cargo"
 make_build_args = [
     "-p",
     "taplo-cli",
     "--no-default-features",
     "--features",
-    "native-tls,lsp",
+    "completions,native-tls,lsp",
 ]
 make_check_args = [*make_build_args]
 hostmakedepends = [
@@ -22,7 +22,9 @@ pkgdesc = "CLI for TOML"
 license = "MIT"
 url = "https://taplo.tamasfe.dev"
 source = f"https://github.com/tamasfe/taplo/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "65510664071252541e66f603dc9aa04016c38d62299061419c95d3bffaa73125"
+sha256 = "c2f7b3234fc62000689a476b462784db4d1bb2be6edcc186654b211f691efaf8"
+# generates completions with host binary
+options = ["!cross"]
 
 
 def pre_prepare(self):
@@ -38,6 +40,19 @@ def pre_prepare(self):
     )
 
 
+def post_build(self):
+    for shell in ["bash", "fish", "zsh"]:
+        with open(self.cwd / f"taplo.{shell}", "w") as outf:
+            self.do(
+                f"target/{self.profile().triplet}/release/taplo",
+                "completions",
+                shell,
+                stdout=outf,
+            )
+
+
 def install(self):
-    self.install_license("LICENSE.md")
+    self.install_license("LICENSE")
     self.install_bin(f"target/{self.profile().triplet}/release/taplo")
+    for shell in ["bash", "fish", "zsh"]:
+        self.install_completion(f"taplo.{shell}", shell)
