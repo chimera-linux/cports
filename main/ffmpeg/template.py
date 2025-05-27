721a7d5e7d53795c593fcee83517a5e03e6f2293
triallax <triallax@tutanota.com>
2025-05-27 06:23:46 +0100

    main/rav1e: revert 0.8.0 bump
    seems to be broken with ffmpeg:
    ```
    thread '<unnamed>' panicked at src/api/context.rs:369:33:
    called `Result::unwrap()` on an `Err` value: Custom { kind: InvalidInput, error: "excessive value for bits written" }
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    ```
    
    rav1e cli doesn't seem to have this issue, might look into it later

--- main/ffmpeg/template.py
+++ main/ffmpeg/template.py
@@ -1,6 +1,6 @@
 pkgname = "ffmpeg"
 pkgver = "7.1.1"
-pkgrel = 7
+pkgrel = 8
 build_style = "configure"
 configure_args = [
     "--prefix=/usr",
--- main/rav1e/template.py
+++ main/rav1e/template.py
@@ -1,6 +1,6 @@
 pkgname = "rav1e"
-pkgver = "0.8.0"
-pkgrel = 0
+pkgver = "0.7.1"
+pkgrel = 1
 build_style = "cargo"
 make_build_args = [
     "--no-default-features",
@@ -19,8 +19,31 @@ makedepends = ["rust-std"]
 pkgdesc = "AV1 encoder"
 license = "BSD-2-Clause"
 url = "https://github.com/xiph/rav1e"
-source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "2580bb4b4efae50e0a228e8ba80db1f73805a0e6f6a8c22bee066c90afb35ba0"
+source = [
+    f"{url}/archive/refs/tags/v{pkgver}.tar.gz",
+    f"!{url}/releases/download/v{pkgver}/Cargo.lock>Cargo.lock.{pkgver}",
+]
+sha256 = [
+    "da7ae0df2b608e539de5d443c096e109442cdfa6c5e9b4014361211cf61d030c",
+    "4482976bfb7647d707f9a01fa1a3848366988f439924b5c8ac7ab085fba24240",
+]
+
+
+def post_extract(self):
+    self.cp(self.sources_path / f"Cargo.lock.{pkgver}", "Cargo.lock")
+
+
+def pre_prepare(self):
+    # the version that is in there is busted on loongarch
+    self.do(
+        "cargo",
+        "update",
+        "--package",
+        "libc",
+        "--precise",
+        "0.2.170",
+        allow_network=True,
+    )
 
 
 def post_build(self):
