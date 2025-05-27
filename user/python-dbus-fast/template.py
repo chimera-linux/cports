6ff6fdc8f5ec3b73fbb09c7097e2ced6ae827e6a
triallax <triallax@tutanota.com>
2025-05-27 03:11:40 +0100

    user/python-dbus-fast: deselect tests broken by pytest-asyncio 1.0.0, run glib tests

--- user/python-dbus-fast/template.py
+++ user/python-dbus-fast/template.py
@@ -2,6 +2,21 @@ pkgname = "python-dbus-fast"
 pkgver = "2.44.1"
 pkgrel = 0
 build_style = "python_pep517"
+make_check_args = [
+    # these fail since pytest-asyncio 1.0.0
+    "--deselect",
+    "tests/test_aio_low_level.py::test_sending_signals_between_buses",
+    "--deselect",
+    "tests/test_disconnect.py::test_bus_disconnect_before_reply",
+    "--deselect",
+    "tests/test_disconnect.py::test_unexpected_disconnect",
+    "--deselect",
+    "tests/test_fd_passing.py::test_high_level_service_fd_passing",
+    "--deselect",
+    "tests/test_fd_passing.py::test_sending_file_descriptor_with_proxy",
+    "--deselect",
+    "tests/test_tcp_address.py::test_tcp_connection_with_forwarding",
+]
 make_check_wrapper = ["dbus-run-session"]
 hostmakedepends = [
     "python-build",
@@ -14,7 +29,12 @@ makedepends = [
     "python-devel",
 ]
 depends = ["python"]
-checkdepends = ["dbus", "python-pytest-asyncio", "python-pytest-timeout"]
+checkdepends = [
+    "dbus",
+    "python-gobject",
+    "python-pytest-asyncio",
+    "python-pytest-timeout",
+]
 pkgdesc = "DBus library for python"
 license = "MIT"
 url = "https://pypi.org/project/dbus-fast"
@@ -24,7 +44,7 @@ sha256 = "1f25e21eb7c6f771b53e45caba9ea7ecd640b3d39fb1cb68b3cdc8bc8f9bc002"
 
 def post_prepare(self):
     # Requires pytest_codspeed
-    self.rm(self.srcdir / "tests/benchmarks", recursive=True)
+    self.rm("tests/benchmarks", recursive=True)
 
 
 def post_install(self):
