54fcad7c2ea4d1a6410117cfa29e6253145ce511
Erica Z <zerica@callcc.eu>
2025-05-27 16:44:07 +0200

    main/furnace: update to 0.6.8.2

--- main/furnace/template.py
+++ main/furnace/template.py
@@ -1,6 +1,6 @@
 pkgname = "furnace"
-pkgver = "0.6.8.1"
-pkgrel = 1
+pkgver = "0.6.8.2"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DSYSTEM_FFTW=ON",
@@ -36,6 +36,6 @@ source = [
 ]
 source_paths = [".", "extern/adpcm"]
 sha256 = [
-    "ce3c2a3a758596c73b3c4c4636722486f8ec51629019f8deda960c1d033093df",
+    "e821cbdd3f0c7ca7cca99a80eb2c90e6732d7bba2410702caa4694bbde89eaa2",
     "46da29342d2968ff222ba00e07c646e038b76af2e6c86de037c653059a056251",
 ]
