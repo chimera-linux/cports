7388139b9a254c6f3cc0e0090a752e300d1bc8a9
triallax <triallax@tutanota.com>
2025-05-27 01:40:22 +0100

    main/magic-wormhole: update to 0.18.0, fix update check

--- main/magic-wormhole/template.py
+++ main/magic-wormhole/template.py
@@ -1,11 +1,12 @@
 pkgname = "magic-wormhole"
-pkgver = "0.17.0"
-pkgrel = 1
+pkgver = "0.18.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
+    "python-versioneer",
 ]
 depends = [
     "python-attrs",
@@ -16,6 +17,7 @@ depends = [
     "python-humanize",
     "python-iterable-io",
     "python-pynacl",
+    "python-qrcode",
     "python-spake2",
     "python-tqdm",
     "python-twisted",
@@ -31,8 +33,8 @@ checkdepends = [
 pkgdesc = "Utility to transfer data between computers"
 license = "MIT"
 url = "https://magic-wormhole.readthedocs.io"
-source = f"$(PYPI_SITE)/m/magic-wormhole/magic_wormhole-{pkgver}.tar.gz"
-sha256 = "142c7a271684b0b04470792601848f6b0ade0d8bf54fbcb30c6259d75edd9d06"
+source = f"$(PYPI_SITE)/m/magic-wormhole/magic-wormhole-{pkgver}.tar.gz"
+sha256 = "66fdee0861ec63ab494560aa1c68ebe3b21e955d0cabd84eadf93013e332852c"
 
 
 def init_check(self):
@@ -41,8 +43,6 @@ def init_check(self):
         "--dist=worksteal",
         # needs some locale folder
         "--deselect=src/wormhole/test/test_cli.py",
-        # needs net
-        "--deselect=src/wormhole/test/dilate/test_record.py",
     ]
 
 
