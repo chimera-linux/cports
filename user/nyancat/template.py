c14b560edb5e8fc7acb76719c32ea6b7267450e8
unrealapex <unrealapex@disroot.org>
2025-05-27 20:44:14 -0700

    user/nyancat: new package

---
+++ user/nyancat/template.py
@@ -0,0 +1,10 @@
+pkgname = "nyancat"
+pkgver = "1.5.2"
+pkgrel = 0
+build_style = "makefile"
+pkgdesc = "Nyancat in your terminal, rendered through ANSI escape sequences"
+license = "NCSA"
+url = "https://nyancat.dakko.us"
+# TODO: fix the source url
+source = f"https://github.com/klange/nyancat/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "88cdcaa9c7134503dd0364a97fa860da3381a09cb555c3aae9918360827c2032"
