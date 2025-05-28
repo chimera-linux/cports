fd80e38644a76eb76a6fb3db96f83197c36a0ef1
triallax <triallax@tutanota.com>
2025-05-28 20:43:40 +0100

    main/python-pytest-mock: update to 3.14.1, fix update check

--- main/python-pytest-mock/template.py
+++ main/python-pytest-mock/template.py
@@ -1,20 +1,19 @@
 pkgname = "python-pytest-mock"
-pkgver = "3.14.0"
-pkgrel = 1
+pkgver = "3.14.1"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools_scm",
-    "python-wheel",
 ]
 depends = ["python-mock", "python-pytest"]
 checkdepends = ["python-pytest-asyncio", *depends]
 pkgdesc = "Thin-wrapper around the mock package for easier use with pytest"
 license = "MIT"
 url = "https://pytest-mock.readthedocs.io/en/latest/index.html"
-source = f"$(PYPI_SITE)/p/pytest-mock/pytest-mock-{pkgver}.tar.gz"
-sha256 = "2719255a1efeceadbc056d6bf3df3d1c5015530fb40cf347c0f9afac88410bd0"
+source = f"$(PYPI_SITE)/p/pytest-mock/pytest_mock-{pkgver}.tar.gz"
+sha256 = "159e9edac4c451ce77a5cdb9fc5d1100708d2dd4ba3c3df572f14097351af80e"
 
 
 def post_install(self):
