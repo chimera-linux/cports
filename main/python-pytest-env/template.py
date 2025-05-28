9077af109c0bafc95c04ee2f6fb237af50cb906c
triallax <triallax@tutanota.com>
2025-05-28 20:34:47 +0100

    main/python-pytest-env: update to 1.1.5, fix update check

--- main/python-pytest-env/template.py
+++ main/python-pytest-env/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pytest-env"
-pkgver = "1.1.3"
-pkgrel = 1
+pkgver = "1.1.5"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -12,7 +12,7 @@ depends = ["python-pytest"]
 checkdepends = [*depends]
 pkgdesc = "Pytest plugin for adding environment variables"
 license = "MIT"
-url = "https://github.com/MobileDynasty/pytest-env"
+url = "https://github.com/pytest-dev/pytest-env"
 source = f"$(PYPI_SITE)/p/pytest-env/pytest_env-{pkgver}.tar.gz"
 sha256 = "fcd7dc23bb71efd3d35632bde1bbe5ee8c8dc4489d6617fb010674880d96216b"
 
