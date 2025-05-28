6de32316ea99591601f18f893e38591ee4fb5a76
triallax <triallax@tutanota.com>
2025-05-28 02:20:59 +0100

    main/python-mako: update to 1.3.10, fix update check

--- main/python-mako/template.py
+++ main/python-mako/template.py
@@ -1,20 +1,19 @@
 pkgname = "python-mako"
-pkgver = "1.3.5"
-pkgrel = 1
+pkgver = "1.3.10"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 checkdepends = ["python-pytest", "python-setuptools", "python-markupsafe"]
 depends = ["python-setuptools", "python-markupsafe"]
 pkgdesc = "Fast and lightweight templating engine for Python"
 license = "MIT"
 url = "https://www.makotemplates.org"
-source = f"$(PYPI_SITE)/M/Mako/Mako-{pkgver}.tar.gz"
-sha256 = "48dbc20568c1d276a2698b36d968fa76161bf127194907ea6fc594fa81f943bc"
+source = f"$(PYPI_SITE)/M/Mako/mako-{pkgver}.tar.gz"
+sha256 = "99579a6f39583fa7e5630a28c3c1f440e4e97a414b80372649c0ce338da2ea28"
 
 
 def post_install(self):
