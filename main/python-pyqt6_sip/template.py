2ebe7080cf53922160908bb3f6c0599c76c11287
triallax <triallax@tutanota.com>
2025-05-28 04:07:56 +0100

    main/python-pyqt6_sip: update to 13.10.2, fix update check

--- main/python-pyqt6_sip/template.py
+++ main/python-pyqt6_sip/template.py
@@ -1,21 +1,19 @@
 pkgname = "python-pyqt6_sip"
-pkgver = "13.9.0"
-pkgrel = 1
+pkgver = "13.10.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
-    "python-devel",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 makedepends = ["python-devel"]
 depends = ["python"]
 pkgdesc = "PyQt6 support for python-sip"
 license = "custom:sip"
 url = "https://www.riverbankcomputing.com/software/sip"
-source = f"$(PYPI_SITE)/P/PyQt6_sip/PyQt6_sip-{pkgver}.tar.gz"
-sha256 = "5dc660f2242f6bd8c6bc5973a39f31a5b97f261f5ba69b4571912c7feae346bb"
+source = f"$(PYPI_SITE)/P/PyQt6_sip/pyqt6_sip-{pkgver}.tar.gz"
+sha256 = "464ad156bf526500ce6bd05cac7a82280af6309974d816739b4a9a627156fafe"
 # no tests
 options = ["!check"]
 
