f056c293227474e42aca9085f53b6368e445b51c
triallax <triallax@tutanota.com>
2025-05-28 03:24:25 +0100

    main/milou: update dead url

--- main/milou/template.py
+++ main/milou/template.py
@@ -1,6 +1,6 @@
 pkgname = "milou"
 pkgver = "6.3.5"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -19,7 +19,7 @@ makedepends = [
 ]
 pkgdesc = "KDE Dedicated search application"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
-url = "https://api.kde.org/plasma/milou/html"
+url = "https://invent.kde.org/plasma/milou"
 source = f"$(KDE_SITE)/plasma/{pkgver}/milou-{pkgver}.tar.xz"
 sha256 = "bcf4c73f623fa0510bd185e61e4e55b275fdc88a4e5dd31185e0b723666fc8fa"
 hardening = ["vis"]
