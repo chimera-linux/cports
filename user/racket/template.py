6655725696f22f3c1dcfff57b5960972b25f82d0
Erica Z <zerica@callcc.eu>
2025-05-30 11:21:51 +0200

    user/racket: build all targets with chez scheme

--- user/racket/template.py
+++ user/racket/template.py
@@ -1,12 +1,20 @@
 pkgname = "racket"
 pkgver = "8.17"
-pkgrel = 0
-archs = ["aarch64", "ppc64le", "riscv64", "x86_64"]
+pkgrel = 1
 build_wrksrc = "src"
 build_style = "gnu_configure"
-configure_args = ["--disable-strip"]
+configure_args = [
+    "--disable-strip",
+    "--enable-scheme=/usr/bin/scheme",
+    "--enable-csonly",
+    "--enable-csdefault",
+]
 configure_gen = []
-hostmakedepends = ["automake", "sqlite"]
+hostmakedepends = [
+    "automake",
+    "chez-scheme",
+    "sqlite",
+]
 makedepends = [
     "libffi8-devel",
     "lz4-devel",
@@ -18,19 +26,30 @@ license = "MIT OR Apache-2.0"
 url = "https://racket-lang.org"
 source = f"https://download.racket-lang.org/installers/{pkgver}/racket-{pkgver}-src-builtpkgs.tgz"
 sha256 = "bb98bd8b6d9eba56bd5107fe29be50ad8cd4fb7bb32fbd762c5bd259c15e706f"
-# no tests, cross requires external chezscheme
+# no tests, cross requires chezscheme version to match exactly
 options = ["!check", "!cross"]
 
+# same as main/chez-scheme
 match self.profile().arch:
-    case "aarch64" | "x86_64":
-        configure_args += ["--enable-csonly", "--enable-csdefault"]
-    case "ppc64le" | "riscv64":
-        configure_args += ["--enable-bconly", "--enable-bcdefault"]
-        makedepends += ["libucontext-devel"]
-        tool_flags = {
-            "CFLAGS": ["-D_GNU_SOURCE"],
-            "LDFLAGS": ["-lucontext"],
-        }
+    case "aarch64":
+        _machine = "tarm64le"
+    case "armhf" | "armv7":
+        _machine = "tarm32le"
+    case "loongarch64":
+        _machine = "tla64le"
+    case "ppc":
+        _machine = "tppc32le"
+    case "riscv64":
+        _machine = "trv64le"
+    case "x86_64":
+        _machine = "ta6le"
+    case _:
+        # portable bytecode
+        _machine = f"tpb{self.profile().wordsize}{self.profile().endian[0]}"
+
+configure_args += [
+    f"--enable-mach={_machine}",
+]
 
 
 def post_extract(self):
