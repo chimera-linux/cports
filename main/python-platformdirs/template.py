8d14aedeb741b3a7efc769e82801f667b5f245bd
q66 <q66@chimera-linux.org>
2025-06-02 11:23:26 +0200

    main/python-platformdirs: rebuild
    This had a corrupt package on loongarch.

--- main/python-platformdirs/template.py
+++ main/python-platformdirs/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-platformdirs"
 pkgver = "4.3.8"
-pkgrel = 0
+pkgrel = 1
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-installer", "python-hatch_vcs"]
 checkdepends = ["python-pytest", "python-appdirs", "python-pytest-mock"]
