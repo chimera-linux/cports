7ce84072b58d58645e248b5292a53259fb7b9adf
ttyyls <contact@behri.org>
2025-06-06 13:56:35 +0100

    main/pdfjs: update to 5.3.31

--- main/pdfjs/template.py
+++ main/pdfjs/template.py
@@ -1,11 +1,11 @@
 pkgname = "pdfjs"
-pkgver = "5.2.133"
+pkgver = "5.3.31"
 pkgrel = 0
 pkgdesc = "Platform for parsing and rendering PDFs"
 license = "Apache-2.0"
 url = "https://github.com/mozilla/pdf.js"
 source = f"{url}/releases/download/v{pkgver}/pdfjs-{pkgver}-legacy-dist.zip"
-sha256 = "2024d3ae9a35fb62de125219a8acb11190e93a5aa5a694cdbba792e4df871281"
+sha256 = "9d058813dd630fdf2201ee30243e62b3a066d2fedc8fd4103c1a34d12a6066c4"
 # no tests defined
 options = ["!check"]
 
