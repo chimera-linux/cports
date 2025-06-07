d0c333c957f87a594836ce9450e0bc3abcff12ec
triallax <triallax@tutanota.com>
2025-06-08 00:57:07 +0100

    main/bolt: update to 0.9.9

--- main/bolt/template.py
+++ main/bolt/template.py
@@ -1,6 +1,6 @@
 pkgname = "bolt"
-pkgver = "0.9.8"
-pkgrel = 5
+pkgver = "0.9.9"
+pkgrel = 0
 build_style = "meson"
 # XXX drop libexec
 configure_args = [
@@ -24,9 +24,9 @@ pkgdesc = "Thunderbolt device manager"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.freedesktop.org/bolt/bolt"
 source = f"{url}/-/archive/{pkgver}/bolt-{pkgver}.tar.gz"
-sha256 = "5a4306aa21ee398e1e9f2a5072748c9469c9360bf5edc7dcec2f12fc17be122e"
+sha256 = "d2b05e3ee1ffa9b4fc1c4a11138c69bf1f60acba86f07b1b4b40b4d456048936"
 
 
 def post_install(self):
-    self.install_service(self.files_path / "boltd")
-    self.install_tmpfiles(self.files_path / "tmpfiles.conf")
+    self.install_service("^/boltd")
+    self.install_tmpfiles("^/tmpfiles.conf")
