829cd9106b05275b38d90f0f875fb72c2d2ead29
triallax <triallax@tutanota.com>
2025-06-07 01:33:55 +0100

    user/ryzenadj: move from main, update to 0.17.0

--- user/ryzenadj/template.py
+++ user/ryzenadj/template.py
@@ -1,5 +1,5 @@
 pkgname = "ryzenadj"
-pkgver = "0.16.0"
+pkgver = "0.17.0"
 pkgrel = 0
 # only for ryzen cpus
 archs = ["x86_64"]
@@ -13,4 +13,4 @@ pkgdesc = "Adjust power management settings for Ryzen mobile processors"
 license = "LGPL-3.0-only"
 url = "https://github.com/FlyGoat/RyzenAdj"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7bef7dbde006afbe316091d8da8c8c551d5d7d43185d9e62281671959b7a3ca2"
+sha256 = "848ac9d86ff65d30f5e2c8600aac2613f0f10003b0d6f0e516a54761d7345d44"
