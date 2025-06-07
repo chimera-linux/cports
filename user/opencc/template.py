59a3231e3c803450e84e0126b558127c54f3023f
triallax <triallax@tutanota.com>
2025-06-07 05:18:20 +0100

    user/opencc: skip test that gets stuck on opencc

--- user/opencc/template.py
+++ user/opencc/template.py
@@ -9,6 +9,8 @@ configure_args = [
     "-DUSE_SYSTEM_RAPIDJSON=ON",
     "-DUSE_SYSTEM_TCLAP=ON",
 ]
+# conditionally appended to below
+make_check_args = []
 hostmakedepends = ["pkgconf", "cmake", "ninja", "python"]
 makedepends = ["gtest-devel", "marisa-trie-devel", "rapidjson", "tclap"]
 pkgdesc = "Open Chinese conversion library"
@@ -16,6 +18,10 @@ license = "Apache-2.0"
 url = "https://github.com/BYVoid/OpenCC"
 source = f"{url}/archive/refs/tags/ver.{pkgver}.tar.gz"
 sha256 = "ad4bcd8d87219a240a236d4a55c9decd2132a9436697d2882ead85c8939b0a99"
+
+if self.profile().arch == "ppc64":
+    # FIXME: hangs
+    make_check_args += ["-E", "ConfigTest"]
 
 
 @subpackage("opencc-devel")
