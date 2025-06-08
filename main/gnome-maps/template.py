ff38b11b7b6ba9a4c26f6b582c29d33ab7b436eb
triallax <triallax@tutanota.com>
2025-06-08 03:04:55 +0100

    main/gnome-maps: update to 48.4

--- main/gnome-maps/template.py
+++ main/gnome-maps/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-maps"
-pkgver = "48.3"
+pkgver = "48.4"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -44,5 +44,5 @@ pkgdesc = "GNOME maps"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later AND CC0-1.0"
 url = "https://apps.gnome.org/Maps"
 source = f"$(GNOME_SITE)/gnome-maps/{pkgver.split('.')[0]}/gnome-maps-{pkgver}.tar.xz"
-sha256 = "ad460a33cb72e520dc0674788af16173f8cc2794590ff8b762a21edb7dddc629"
+sha256 = "ae8716b8b0b3febd3ed22c17e9062eb80f6d6d25c50501c04d4230b97cee45ca"
 options = ["!cross"]
