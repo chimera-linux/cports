146724afc76c3984839d1fc5e79945a95be64e7c
triallax <triallax@tutanota.com>
2025-06-08 23:07:16 +0100

    main/newsraft: update to 0.31

--- main/newsraft/template.py
+++ main/newsraft/template.py
@@ -1,23 +1,19 @@
 pkgname = "newsraft"
-pkgver = "0.30"
+pkgver = "0.31"
 pkgrel = 0
 build_style = "makefile"
-hostmakedepends = [
-    "pkgconf",
-    "scdoc",
-]
+hostmakedepends = ["pkgconf"]
 makedepends = [
     "gumbo-parser-devel",
     "curl-devel",
     "libexpat-devel",
-    "ncurses-devel",
     "sqlite-devel",
 ]
 pkgdesc = "Feed reader for terminal"
 license = "ISC"
 url = "https://codeberg.org/newsraft/newsraft"
 source = f"{url}/archive/newsraft-{pkgver}.tar.gz"
-sha256 = "5ae782d7eb19042cd05e260c8ec0fe4d0544e51716885a4b1e96a673576bd998"
+sha256 = "de0d96664d9a276dbe58cf4b44a6861bc18b6fd4c0f41a97450c5b3509904ae8"
 hardening = ["vis", "cfi"]
 
 
