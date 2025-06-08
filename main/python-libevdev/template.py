4cf8f516214ac7c3aad5e4c1dd91478e9b0ddf53
triallax <triallax@tutanota.com>
2025-06-08 22:14:22 +0100

    main/python-libevdev: update to 0.12

--- main/python-libevdev/template.py
+++ main/python-libevdev/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-libevdev"
-pkgver = "0.11"
-pkgrel = 2
+pkgver = "0.12"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
 depends = ["python", "libevdev"]
 checkdepends = ["python-pytest", "libevdev"]
@@ -14,16 +13,8 @@ pkgdesc = "Python wrapper around libevdev"
 license = "MIT"
 url = "https://gitlab.freedesktop.org/libevdev/python-libevdev"
 source = f"{url}/-/archive/{pkgver}/python-libevdev-{pkgver}.tar.gz"
-sha256 = "60eebb58ff20be2d8443d716c3c299392720aac89db269fdb4b9de14fe313c24"
+sha256 = "9da0a5f686e0c68c0f2414f84313dc8ca7df0dea76abbd918b18c9abaf798316"
 
 
 def post_install(self):
     self.install_license("COPYING")
-
-
-def check(self):
-    self.do(
-        "pytest",
-        "-v",
-        *map(lambda p: f"test/{p.name}", (self.cwd / "test").glob("*.py")),
-    )
