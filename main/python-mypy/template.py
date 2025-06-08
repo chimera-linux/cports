388e7d73e5fae5c3f13fab9733dc171515684767
triallax <triallax@tutanota.com>
2025-06-08 22:25:43 +0100

    main/python-mypy: update to 1.16.0

--- main/python-mypy/template.py
+++ main/python-mypy/template.py
@@ -1,28 +1,30 @@
 pkgname = "python-mypy"
-pkgver = "1.15.0"
-pkgrel = 1
+pkgver = "1.16.0"
+pkgrel = 0
 build_style = "python_pep517"
 make_check_target = "mypy/test"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
-    "python-wheel",
 ]
-depends = ["python-mypy_extensions", "python-typing_extensions"]
+depends = [
+    "python-mypy_extensions",
+    "python-pathspec",
+    "python-typing_extensions",
+]
 checkdepends = [
-    *depends,
     "python-filelock",
     "python-lxml",
     "python-psutil",
-    "python-pytest",
     "python-pytest-xdist",
+    *depends,
 ]
 pkgdesc = "Optional static typing for Python"
 license = "MIT"
 url = "https://www.mypy-lang.org"
 source = f"$(PYPI_SITE)/m/mypy/mypy-{pkgver}.tar.gz"
-sha256 = "404534629d51d3efea5c800ee7c42b72a6554d6c400e6a79eafe15d11341fd43"
+sha256 = "84b94283f817e2aa6350a14b4a8fb2a35a53c286f97c9d30f53b63620e7af8ab"
 # they take ages, also there are like 3 failures
 options = ["!check"]
 
