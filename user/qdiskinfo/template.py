1742679d2ecf98e6b26f930cb5e8e0d6f3676e8b
triallax <triallax@tutanota.com>
2025-06-08 23:35:10 +0100

    user/qdiskinfo: move from main, update to 0.4

--- user/qdiskinfo/template.py
+++ user/qdiskinfo/template.py
@@ -1,10 +1,11 @@
 pkgname = "qdiskinfo"
-pkgver = "0.3"
-pkgrel = 1
+pkgver = "0.4"
+pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
     "ninja",
+    "pkgconf",
 ]
 makedepends = ["qt6-qtbase-devel"]
 depends = [
@@ -15,4 +16,4 @@ pkgdesc = "Frontend for smartctl to display SMART data"
 license = "GPL-3.0-only"
 url = "https://github.com/edisionnano/QDiskInfo"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "f9829a488ff08395e14f953d41a85dac9c91714fdd34bc9a76a46fe761511209"
+sha256 = "33762f494f2da4b59e770207ad5bacca4394774c76509c15d3e3fa23fbf76d33"
