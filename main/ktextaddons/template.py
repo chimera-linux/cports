3bf773ec4898ddbc516682f84925a303ebf6aac0
triallax <triallax@tutanota.com>
2025-06-08 03:42:45 +0100

    main/ktextaddons: update to 1.6.0

--- main/ktextaddons/template.py
+++ main/ktextaddons/template.py
@@ -1,5 +1,5 @@
 pkgname = "ktextaddons"
-pkgver = "1.5.4"
+pkgver = "1.6.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_WITH_QT6=ON"]
@@ -28,6 +28,7 @@ makedepends = [
     "kcoreaddons-devel",
     "ki18n-devel",
     "kio-devel",
+    "ktextwidgets-devel",
     "qt6-qtdeclarative-devel",
     "qt6-qtspeech-devel",
     "qt6-qttools-devel",
@@ -39,7 +40,7 @@ pkgdesc = "KDE text handling addons library"
 license = "LGPL-2.1-or-later"
 url = "https://invent.kde.org/libraries/ktextaddons"
 source = f"$(KDE_SITE)/ktextaddons/ktextaddons-{pkgver}.tar.xz"
-sha256 = "64b80602e84b25e9164620af3f6341fa865b85e826ab8f5e02061ae24a277b20"
+sha256 = "d9846505a52f97bda64ead193032bc29573ec31481f473be3c586f5b8321ecdc"
 
 
 @subpackage("ktextaddons-devel")
