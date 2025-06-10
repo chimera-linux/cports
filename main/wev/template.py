24b4045ab3def0281ffe2ac7116af83f0647eca6
ttyyls <contact@behri.org>
2025-06-10 16:06:45 +0100

    main/wev: update to 1.1.0

--- main/wev/template.py
+++ main/wev/template.py
@@ -1,5 +1,5 @@
 pkgname = "wev"
-pkgver = "1.0.0"
+pkgver = "1.1.0"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["pkgconf", "scdoc", "wayland-progs"]
@@ -13,7 +13,7 @@ pkgdesc = "Wayland event viewer"
 license = "MIT"
 url = "https://git.sr.ht/~sircmpwn/wev"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "613a1df1a4879d50ce72023de14aaf05be2e6f51346e84a69f50fc6d8502bbf4"
+sha256 = "89b8d9bc756631795ee91c99a0d5b0877c9f5c3acfe81f77a2b69ecfc15daf16"
 hardening = ["vis", "cfi"]
 # No tests
 options = ["!check"]
