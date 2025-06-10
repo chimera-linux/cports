9b686dd89e5f740f06aa11a45d01847f040b40bd
triallax <triallax@tutanota.com>
2025-06-10 22:58:57 +0100

    main/adwaita-icon-theme: update to 48.1

--- main/adwaita-icon-theme/template.py
+++ main/adwaita-icon-theme/template.py
@@ -1,5 +1,5 @@
 pkgname = "adwaita-icon-theme"
-pkgver = "48.0"
+pkgver = "48.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -7,5 +7,7 @@ depends = ["hicolor-icon-theme"]
 pkgdesc = "Icon theme for GTK+"
 license = "LGPL-3.0-or-later OR CC-BY-SA-3.0"
 url = "https://gitlab.gnome.org/GNOME/adwaita-icon-theme"
-source = f"$(GNOME_SITE)/adwaita-icon-theme/{pkgver[:-2]}/adwaita-icon-theme-{pkgver}.tar.xz"
-sha256 = "847068888650d9673115be6dbf2bfdc31a46aebc528a6a9db4420e60e656b8d4"
+# missing tarball
+# source = f"$(GNOME_SITE)/adwaita-icon-theme/{pkgver[:-2]}/adwaita-icon-theme-{pkgver}.tar.xz"
+source = f"https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/archive/{pkgver}/adwaita-icon-theme-{pkgver}.tar.gz"
+sha256 = "403f650189cc41ba8321417470cbb7e484425374df7a52a3289421b9ac30e792"
