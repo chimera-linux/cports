b5c5793aa381c7cecda9b56ae74000c308acecb2
triallax <triallax@tutanota.com>
2025-06-10 22:01:17 +0100

    main/osinfo-db: update to 20250606

--- main/osinfo-db/template.py
+++ main/osinfo-db/template.py
@@ -1,12 +1,12 @@
 pkgname = "osinfo-db"
-pkgver = "20250124"
+pkgver = "20250606"
 pkgrel = 0
 hostmakedepends = ["osinfo-db-tools"]
 pkgdesc = "Osinfo database of information about operating systems"
 license = "GPL-2.0-or-later"
 url = "https://libosinfo.org"
 source = f"!https://releases.pagure.org/libosinfo/osinfo-db-{pkgver}.tar.xz"
-sha256 = "7ca717f0975a798135a2b39eefdd1436a0b0682e29685c7fd01ef7f83a257250"
+sha256 = "9940aa47df298073c51dcf8a4dcc855f494ab864c24cdbda46bd897957357fe1"
 options = ["!cross"]
 
 
