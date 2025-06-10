42cd15b9d1375df7e1c8d481706d39a57b3c49b6
triallax <triallax@tutanota.com>
2025-06-10 23:11:20 +0100

    main/python-cython: update to 3.1.2

--- main/python-cython/template.py
+++ main/python-cython/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-cython"
-pkgver = "3.1.1"
+pkgver = "3.1.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,6 +13,6 @@ pkgdesc = "C extensions for Python"
 license = "Apache-2.0"
 url = "https://cython.org"
 source = f"$(PYPI_SITE)/c/cython/cython-{pkgver}.tar.gz"
-sha256 = "505ccd413669d5132a53834d792c707974248088c4f60c497deb1b416e366397"
+sha256 = "6bbf7a953fa6762dfecdec015e3b054ba51c0121a45ad851fa130f63f5331381"
 # check: flaky tests
 options = ["!check"]
