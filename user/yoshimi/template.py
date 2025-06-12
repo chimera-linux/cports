f08195e78ccf1d262cb879a12af7c5c30247a77c
Erica Z <zerica@callcc.eu>
2025-06-12 18:27:10 +0200

    user/yoshimi: update to 2.3.4.1

--- user/yoshimi/template.py
+++ user/yoshimi/template.py
@@ -1,5 +1,5 @@
 pkgname = "yoshimi"
-pkgver = "2.3.3.3"
+pkgver = "2.3.4.1"
 pkgrel = 0
 build_style = "cmake"
 cmake_dir = "src"
@@ -25,4 +25,4 @@ pkgdesc = "Software synthesizer"
 license = "GPL-2.0-or-later"
 url = "https://yoshimi.github.io"
 source = f"https://github.com/Yoshimi/yoshimi/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "10b70d6bf577de3f36883b0e066d5de0b7291d4e4de3dee3b56df68feaed9469"
+sha256 = "2ab49688c42038863a6e5807055d0911ac069ca1527afbbbb93eb50595820d40"
