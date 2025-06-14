76c9d86984f6ea364e0feb78627fe000385f54ea
ttyyls <contact@behri.org>
2025-06-14 20:18:57 +0100

    user/tea: update to 0.10.1

--- user/tea/template.py
+++ user/tea/template.py
@@ -1,17 +1,17 @@
 pkgname = "tea"
-pkgver = "0.9.2"
-pkgrel = 13
+pkgver = "0.10.1"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     "-ldflags",
-    f"-X main.Version={pkgver}",
+    f"-X code.gitea.io/tea/cmd.Version={pkgver}",
 ]
 hostmakedepends = ["go"]
 pkgdesc = "CLI tool to interact with Gitea servers"
 license = "MIT"
 url = "https://gitea.com/gitea/tea"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "b5a944de8db7d5af4aa87e9640261c925f094d2b6d26c4faf2701773acab219b"
+sha256 = "791b7f90eff9ade0d5ee5e3f0dfba128e35eaf83b5f8b8d5f5d6cc9a94ae9b03"
 
 
 def post_install(self):
