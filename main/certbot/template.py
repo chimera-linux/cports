98276d0c6fe7c86585adafc16ffee12607b20810
Duncan Bellamy <dunk@denkimushi.com>
2025-06-14 11:32:36 +0000

    main/certbot: update to 4.1.1

--- main/certbot/template.py
+++ main/certbot/template.py
@@ -1,5 +1,5 @@
 pkgname = "certbot"
-pkgver = "4.0.0"
+pkgver = "4.1.1"
 pkgrel = 0
 build_wrksrc = "certbot"
 build_style = "python_pep517"
@@ -46,7 +46,7 @@ pkgdesc = "Tool to obtain certs from Let's Encrypt"
 license = "Apache-2.0 AND MIT"
 url = "https://github.com/certbot/certbot"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "8c365355ca9b4860312cdb6d56eb2254fff26877a6677b293b5e3307554496d2"
+sha256 = "336f323e0b175f28f146eede7c8db3f2984ce7b542f621e334d2f5c3eb111ca5"
 
 
 def post_build(self):
