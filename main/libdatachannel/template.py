4a7503701fe9c5a6cca0278e95c82abb6bc2c4dc
triallax <triallax@tutanota.com>
2025-06-18 08:12:28 +0100

    main/libdatachannel: update to 0.23.0

--- main/libdatachannel/template.py
+++ main/libdatachannel/template.py
@@ -1,5 +1,5 @@
 pkgname = "libdatachannel"
-pkgver = "0.22.6"
+pkgver = "0.23.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -24,7 +24,7 @@ pkgdesc = "WebRTC network library"
 license = "MPL-2.0"
 url = "https://libdatachannel.org"
 source = f"https://github.com/paullouisageneau/libdatachannel/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "cfadd3155cea2546a0c4d3760cf1fec1753dc28f3b8ce703b05f33d5f129b1c5"
+sha256 = "540fe6927b5a91656e2eb1dfefb837213f5b7bb83203b7e82a954804f09244e1"
 hardening = ["cfi", "vis"]
 # tests seem to need network access (the tests themselves don't seem to work
 # with cfi either; library itself works just fine though)
