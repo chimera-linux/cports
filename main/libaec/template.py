33c90a7b23dc0e46d1047c310829f2647e441533
triallax <triallax@tutanota.com>
2025-06-18 09:55:53 +0100

    main/libaec: update to 1.1.4

--- main/libaec/template.py
+++ main/libaec/template.py
@@ -1,6 +1,6 @@
 pkgname = "libaec"
-pkgver = "1.1.3"
-pkgrel = 1
+pkgver = "1.1.4"
+pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -10,12 +10,11 @@ pkgdesc = "Adaptive entropy coding library"
 license = "BSD-2-Clause"
 url = "https://gitlab.dkrz.de/k202009/libaec"
 source = f"{url}/-/archive/v{pkgver}.tar.gz"
-sha256 = "453de44eb6ea2500843a4cf4d2e97d1be251d2df7beae6c2ebe374edcb11e378"
+sha256 = "95439e861968cb0638a10b0bbdb37c9a10df1b22c5ee0293902acdbc68140f53"
 
 
 def post_install(self):
     self.install_license("LICENSE.txt")
-    self.rename("usr/cmake", "lib/cmake")
 
 
 @subpackage("libaec-devel")
