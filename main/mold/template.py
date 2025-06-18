3a26a3075c41315bc34e6a43c385aa8b6f341823
triallax <triallax@tutanota.com>
2025-06-18 10:59:58 +0100

    main/mold: update to 2.40.1

--- main/mold/template.py
+++ main/mold/template.py
@@ -1,5 +1,5 @@
 pkgname = "mold"
-pkgver = "2.40.0"
+pkgver = "2.40.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -20,7 +20,7 @@ pkgdesc = "High-performance linker"
 license = "MIT"
 url = "https://github.com/rui314/mold"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "ee93ef0868cd54d1f30dfba1070dbacf04c1a35428bbea307a4b9242a883ad20"
+sha256 = "d1ce09a69941f8158604c3edcc96c7178231e7dba2da66b20f5ef6e112c443b7"
 # TODO: a portion of the tests fail, for various reasons, such as assuming
 # presence of gcc, gnu grep, and various toolchain specifics; around 70%
 # of the tests pass right now, fix the rest later
