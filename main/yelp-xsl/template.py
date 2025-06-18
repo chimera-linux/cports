cb0833d39d616d98389221991c3eb877467ca2a1
triallax <triallax@tutanota.com>
2025-06-18 15:43:09 +0100

    main/yelp-xsl: update to 42.4

--- main/yelp-xsl/template.py
+++ main/yelp-xsl/template.py
@@ -1,12 +1,20 @@
 pkgname = "yelp-xsl"
-pkgver = "42.1"
+pkgver = "42.4"
 pkgrel = 0
-build_style = "gnu_configure"
-hostmakedepends = ["automake", "gettext-devel", "itstool", "pkgconf"]
+build_style = "meson"
+hostmakedepends = [
+    "bash",
+    "gettext",
+    "itstool",
+    "libxml2-progs",
+    "libxslt-progs",
+    "meson",
+    "pkgconf",
+]
 makedepends = ["libxslt-devel"]
 pkgdesc = "Help browser for GNOME desktop"
 subdesc = "XSL and misc files"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/Yelp/Xsl"
 source = f"$(GNOME_SITE)/yelp-xsl/{pkgver[:-2]}/yelp-xsl-{pkgver}.tar.xz"
-sha256 = "238be150b1653080ce139971330fd36d3a26595e0d6a040a2c030bf3d2005bcd"
+sha256 = "fdebb07eb2e66a7fb7a0dce6ad8248ad29a4bbb134ba829128ca104f58abd7d1"
