6a85e85ca58bc8d430d2afa87679512f8916102f
triallax <triallax@tutanota.com>
2025-06-18 11:05:11 +0100

    main/mtools: update to 4.0.49

--- main/mtools/template.py
+++ main/mtools/template.py
@@ -1,5 +1,5 @@
 pkgname = "mtools"
-pkgver = "4.0.48"
+pkgver = "4.0.49"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "texinfo"]
@@ -8,7 +8,7 @@ pkgdesc = "Utilities to access MS-DOS disks"
 license = "GPL-3.0-or-later"
 url = "http://www.gnu.org/software/mtools"
 source = f"$(GNU_SITE)/mtools/mtools-{pkgver}.tar.bz2"
-sha256 = "03c29aac8735dd7154a989fbc29eaf2b506121ae1c3a35cd0bf2a02e94d271a9"
+sha256 = "6fe5193583d6e7c59da75e63d7234f76c0b07caf33b103894f46f66a871ffc9f"
 hardening = ["vis", "!cfi"]
 
 
