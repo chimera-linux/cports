8275f506aac8793e331e3ddda13ee2a0d27b7725
triallax <triallax@tutanota.com>
2025-06-19 13:45:06 +0100

    main/ninja: update to 1.13.0

--- main/ninja/template.py
+++ main/ninja/template.py
@@ -1,12 +1,12 @@
 pkgname = "ninja"
-pkgver = "1.12.1"
+pkgver = "1.13.0"
 pkgrel = 0
 hostmakedepends = ["python"]
 pkgdesc = "Small build system with a focus on speed"
 license = "Apache-2.0"
 url = "https://ninja-build.org"
 source = f"https://github.com/ninja-build/ninja/archive/v{pkgver}.tar.gz"
-sha256 = "821bdff48a3f683bc4bb3b6f0b5fe7b2d647cf65d52aeb63328c91a6c6df285a"
+sha256 = "f08641d00099a9e40d44ec0146f841c472ae58b7e6dd517bee3945cfd923cedf"
 # Cycle: ninja -> gtest -> ninja
 options = ["!check"]
 
@@ -21,11 +21,5 @@ def build(self):
 
 def install(self):
     self.install_bin("ninja")
-    self.install_file(
-        "misc/bash-completion",
-        "usr/share/bash-completion/completions",
-        name="ninja",
-    )
-    self.install_file(
-        "misc/zsh-completion", "usr/share/zsh/site-functions", name="_ninja"
-    )
+    for shell in ["bash", "zsh"]:
+        self.install_completion(f"misc/{shell}-completion", shell)
