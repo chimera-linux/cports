51de94e5d27c7a6bc5faaa9309e56a198907cb78
triallax <triallax@tutanota.com>
2025-06-19 13:45:24 +0100

    main/meson: update to 1.8.2

--- main/meson/template.py
+++ main/meson/template.py
@@ -1,5 +1,5 @@
 pkgname = "meson"
-pkgver = "1.8.1"
+pkgver = "1.8.2"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ pkgdesc = "Meson build system"
 license = "Apache-2.0"
 url = "https://mesonbuild.com"
 source = f"https://github.com/mesonbuild/meson/releases/download/{pkgver}/meson-{pkgver}.tar.gz"
-sha256 = "b4e3b80e8fa633555abf447a95a700aba1585419467b2710d5e5bf88df0a7011"
+sha256 = "c105816d8158c76b72adcb9ff60297719096da7d07f6b1f000fd8c013cd387af"
 # meson is early in our bootstrap path but has a million checkdepends
 options = ["!check"]
 
