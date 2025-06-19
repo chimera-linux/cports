f34345f61c3e118bd14c7ab5fcc54e1d9e3d6a00
triallax <triallax@tutanota.com>
2025-06-19 13:27:24 +0100

    main/pam_wrapper: update to 1.1.8

--- main/pam_wrapper/template.py
+++ main/pam_wrapper/template.py
@@ -1,6 +1,6 @@
 pkgname = "pam_wrapper"
-pkgver = "1.1.7"
-pkgrel = 1
+pkgver = "1.1.8"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DUNIT_TESTING=ON"]
 hostmakedepends = ["cmake", "ninja", "pkgconf", "python-setuptools"]
@@ -10,7 +10,7 @@ pkgdesc = "Tool to test PAM applications and modules"
 license = "GPL-3.0-or-later"
 url = "https://cwrap.org/pam_wrapper.html"
 source = f"https://ftp.samba.org/pub/cwrap/pam_wrapper-{pkgver}.tar.gz"
-sha256 = "d1c93e2b687d08834df0e09026dd63d2ce4f577701d406a013e9a8afe469bde1"
+sha256 = "6549c0b3e41d1ebe0c94a1be63c25eec918191462b602ab6f47d4a5fa709c3e4"
 
 
 def post_extract(self):
