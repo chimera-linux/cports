c021bc47562bba3cffb43089aa9282385c85ad7b
triallax <triallax@tutanota.com>
2025-06-19 11:01:07 +0100

    main/podman-compose: update to 1.4.1

--- main/podman-compose/template.py
+++ main/podman-compose/template.py
@@ -1,6 +1,6 @@
 pkgname = "podman-compose"
-pkgver = "1.3.0"
-pkgrel = 1
+pkgver = "1.4.1"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -17,7 +17,7 @@ pkgdesc = "Podman utility for docker-compose.yml support"
 license = "GPL-2.0-only"
 url = "https://github.com/containers/podman-compose"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "d2d641d23bead9cead06bdd2edb9d6a592a138fc8bb7ecb53f87e21e99d45af7"
+sha256 = "582c6021b03565d4fe71ddb5d12c5fbcdbc630ca24d2b29b02d568a2da304853"
 # tests need to run podman which isn't gonna work in bwrap without networking and so on
 options = ["!check"]
 
