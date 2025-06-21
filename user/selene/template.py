9b383afa4eb0d97458ee029bd85f2adc45151805
Jens Reidel <adrian@travitia.xyz>
2025-06-22 01:58:26 +0200

    user/selene: fix crash on 32-bit targets
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- user/selene/template.py
+++ user/selene/template.py
@@ -1,6 +1,6 @@
 pkgname = "selene"
 pkgver = "0.28.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cargo"
 prepare_after_patch = True
 hostmakedepends = ["cargo-auditable"]
@@ -10,6 +10,20 @@ license = "MPL-2.0"
 url = "https://github.com/Kampfkarren/selene"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "c51acf52e7c3136cd0b67b9a39a4a447c8f0257371b2b2acc7e77587260a377b"
+
+
+def pre_prepare(self):
+    # unsafe-libyaml 0.2.5 does not have the fix for
+    # https://github.com/dtolnay/unsafe-libyaml/issues/21 yet
+    self.do(
+        "cargo",
+        "update",
+        "--package",
+        "unsafe-libyaml",
+        "--precise",
+        "0.2.10",
+        allow_network=True,
+    )
 
 
 def install(self):
