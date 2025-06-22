c26f4de85f4c73a8483872995acade280f872c04
Matthias Nyman <natthias@proton.me>
2025-06-22 12:22:08 +0200

    user/yazi: update to 25.5.31

--- user/yazi/template.py
+++ user/yazi/template.py
@@ -1,5 +1,5 @@
 pkgname = "yazi"
-pkgver = "25.5.28"
+pkgver = "25.5.31"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = ["--bins"]
@@ -12,7 +12,7 @@ pkgdesc = "Terminal file manager"
 license = "MIT"
 url = "https://yazi-rs.github.io"
 source = f"https://github.com/sxyazi/yazi/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "6d6258d68f3e453be8b9ba966073d52af893149f04c790d6d8fe6f2597e26b4f"
+sha256 = "4d005e7c3f32b5574d51ab105597f3da3a4be2f7b5cd1bcb284143ad38253ed4"
 
 if self.profile().wordsize == 32:
     broken = "needs atomic64"
