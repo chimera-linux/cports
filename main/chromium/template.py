bce3f5a8ffc041a94869b6bcffef1a31be05477c
q66 <q66@chimera-linux.org>
2025-06-22 14:55:31 +0200

    main/chromium: update to 137.0.7151.119
    Also switch source back to the official because the tarball
    is not up yet in the other one.

--- main/chromium/template.py
+++ main/chromium/template.py
@@ -1,6 +1,6 @@
 pkgname = "chromium"
 # https://chromiumdash.appspot.com/releases?platform=Linux
-pkgver = "137.0.7151.103"
+pkgver = "137.0.7151.119"
 pkgrel = 0
 archs = ["aarch64", "ppc64le", "x86_64"]
 configure_args = [
@@ -136,9 +136,8 @@ depends = [
 pkgdesc = "Web browser"
 license = "BSD-3-Clause"
 url = "https://www.chromium.org"
-# from gentoo
-source = f"https://chromium-tarballs.syd1.digitaloceanspaces.com/chromium-{pkgver}-linux.tar.xz"
-sha256 = "f5f051a30c732b21ce9957cdd7fe0a083623e19078a15ee20d49b27a5cb857e6"
+source = f"https://commondatastorage.googleapis.com/chromium-browser-official/chromium-{pkgver}.tar.xz"
+sha256 = "0deb4e0a63ff9bf1594c303781d67f55fa5b0bb35ab84bc71aef89ccd0b7e052"
 debug_level = 1
 tool_flags = {
     "CFLAGS": [
