2bb1b60a91ca8e65d9ed6a8b04ff8156326964a4
Erica Z <zerica@callcc.eu>
2025-06-24 11:40:30 +0200

    user/fonts-tex-gyre-pagella-math-otf: new package

---
+++ user/fonts-tex-gyre-pagella-math-otf/template.py
@@ -0,0 +1,14 @@
+pkgname = "fonts-tex-gyre-pagella-math-otf"
+pkgver = "1632"
+pkgrel = 0
+pkgdesc = "Math companion for TeX Gyre Pagella"
+license = "LPPL-1.3c"
+url = "https://www.gust.org.pl/projects/e-foundry/tg-math"
+source = f"{url}/download/texgyrepagella-math-{pkgver}.zip"
+sha256 = "68a9c0ce195915334673960b13a281f24d31a8ae380454a0e35652dba506d474"
+
+
+def install(self):
+    self.install_file(
+        "opentype/texgyrepagella-math.otf", "usr/share/fonts/tex-gyre"
+    )
