8789fdd8cd0cd08555ae27fa4bd59072326e665e
triallax <triallax@tutanota.com>
2025-06-25 16:34:51 +0100

    main/python-flake8: update to 7.3.0

--- main/python-flake8/template.py
+++ main/python-flake8/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-flake8"
-pkgver = "7.2.0"
+pkgver = "7.3.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,7 +14,7 @@ pkgdesc = "Python tool for style guide enforcement"
 license = "MIT"
 url = "https://flake8.pycqa.org"
 source = f"https://github.com/PyCQA/flake8/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "a4e94891bcada0adf8b5bdf6be9f8a81bd09b0951674b52ca3e84f21ba0fe651"
+sha256 = "71a7d2ec4166f83c6fc66e6465a45c49b4565ee29b69f27b335366ce101d8c2b"
 # some failures in new testsuite
 options = ["!check"]
 
