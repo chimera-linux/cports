35ef7b18b887f4121b63a5d4491ab085d2ccd1f3
triallax <triallax@tutanota.com>
2025-06-25 12:26:31 +0100

    main/less: update to 679

--- main/less/template.py
+++ main/less/template.py
@@ -1,5 +1,5 @@
 pkgname = "less"
-pkgver = "678"
+pkgver = "679"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-regex=posix"]
@@ -10,16 +10,14 @@ pkgdesc = "Pager program similar to more(1)"
 license = "custom:less OR GPL-3.0-or-later"
 url = "https://www.greenwoodsoftware.com/less"
 source = f"https://www.greenwoodsoftware.com/less/less-{pkgver}.tar.gz"
-sha256 = "4c085364f3028290d34647df27f56018c365dc4c0092ab7de74ed8fe89014fe7"
+sha256 = "9b68820c34fa8a0af6b0e01b74f0298bcdd40a0489c61649b47058908a153d78"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
     self.install_license("LICENSE")
-    self.install_file(self.files_path / "lesspipe.sh", "usr/bin", mode=0o755)
-    self.install_file(
-        self.files_path / "zless.sh", "usr/bin", mode=0o755, name="zless"
-    )
+    self.install_file("^/lesspipe.sh", "usr/bin", mode=0o755)
+    self.install_file("^/zless.sh", "usr/bin", mode=0o755, name="zless")
     self.install_link("usr/bin/more", "less")
     self.install_link("usr/share/man/man1/more.1", "less.1")
     self.install_link("usr/bin/bzless", "zless")
