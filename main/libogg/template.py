ac85e38c7bbf694c15bbf2e061065705484912ed
triallax <triallax@tutanota.com>
2025-06-25 14:44:43 +0100

    main/libogg: update to 1.3.6

--- main/libogg/template.py
+++ main/libogg/template.py
@@ -1,5 +1,5 @@
 pkgname = "libogg"
-pkgver = "1.3.5"
+pkgver = "1.3.6"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf", "slibtool"]
@@ -7,7 +7,7 @@ pkgdesc = "Ogg bitstream file format library"
 license = "BSD-3-Clause"
 url = "https://www.xiph.org/ogg"
 source = f"https://downloads.xiph.org/releases/ogg/libogg-{pkgver}.tar.xz"
-sha256 = "c4d91be36fc8e54deae7575241e03f4211eb102afb3fc0775fbbc1b740016705"
+sha256 = "5c8253428e181840cd20d41f3ca16557a9cc04bad4a3d04cce84808677fa1061"
 # FIXME int
 hardening = ["!int"]
 
