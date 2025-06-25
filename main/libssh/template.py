8cb8348f976335c45582d701c6340da4f2e2b56b
triallax <triallax@tutanota.com>
2025-06-25 20:50:01 +0100

    main/libssh: update to 0.11.2

--- main/libssh/template.py
+++ main/libssh/template.py
@@ -1,5 +1,5 @@
 pkgname = "libssh"
-pkgver = "0.11.1"
+pkgver = "0.11.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -22,7 +22,7 @@ pkgdesc = "Library for accessing ssh client services through C libraries"
 license = "LGPL-2.1-or-later WITH custom:openssl-exception AND BSD-2-Clause"
 url = "https://www.libssh.org"
 source = f"https://www.libssh.org/files/{pkgver[: pkgver.rfind('.')]}/libssh-{pkgver}.tar.xz"
-sha256 = "14b7dcc72e91e08151c58b981a7b570ab2663f630e7d2837645d5a9c612c1b79"
+sha256 = "69529fc18f5b601f0baf0e5a4501a2bc26df5e2f116f5f8f07f19fafaa6d04e7"
 hardening = ["vis", "cfi"]
 
 
