134c3a57106a330035351cb46f1988e698897c6b
triallax <triallax@tutanota.com>
2025-06-25 08:28:01 +0100

    main/ndctl: update to 82

--- main/ndctl/template.py
+++ main/ndctl/template.py
@@ -1,5 +1,5 @@
 pkgname = "ndctl"
-pkgver = "81"
+pkgver = "82"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -29,7 +29,7 @@ pkgdesc = "Tools and libraries for NVDIMMs and other platform memory"
 license = "GPL-2.0-only AND LGPL-2.1-only AND MIT AND CC0-1.0"
 url = "https://github.com/pmem/ndctl"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "e40784687ac2d0c176748c9ffb8bde541a12301b6fedb607a9a70c8fd1f94929"
+sha256 = "8c12d8228e2df4e05db075aa3e9be86bbef41ef5249829bb89b79c4859b74ee5"
 hardening = ["vis", "cfi"]
 # the MIT licence used doesn't have a copyright line in it
 options = ["!distlicense"]
