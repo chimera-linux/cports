fcb641e6ce770899796c1e6836ba438a4123300e
triallax <triallax@tutanota.com>
2025-06-25 15:50:58 +0100

    main/colord: update to 1.4.8

--- main/colord/template.py
+++ main/colord/template.py
@@ -1,6 +1,6 @@
 pkgname = "colord"
-pkgver = "1.4.7"
-pkgrel = 4
+pkgver = "1.4.8"
+pkgrel = 0
 build_style = "meson"
 # manpages fail to generate
 configure_args = [
@@ -36,7 +36,7 @@ pkgdesc = "Color management daemon"
 license = "GPL-2.0-only"
 url = "https://www.freedesktop.org/software/colord"
 source = f"{url}/releases/colord-{pkgver}.tar.xz"
-sha256 = "de02d9910634ae159547585cec414e450f711c27235453b4f9b38a9f2361a653"
+sha256 = "21500bd68975312a7f0f3ce6019d9f75f42aacaa75ca7115ec720b5445406896"
 # FIXME int
 hardening = ["!int"]
 
@@ -45,15 +45,14 @@ if self.profile().cross:
 
 
 def post_install(self):
-    self.install_service(self.files_path / "colord")
-    self.install_sysusers(self.files_path / "sysusers.conf")
-    self.install_tmpfiles(self.files_path / "tmpfiles.conf")
+    self.install_service("^/colord")
+    self.install_sysusers("^/sysusers.conf")
+    self.install_tmpfiles("^/tmpfiles.conf")
 
 
 @subpackage("colord-libs")
 def _(self):
-    # transitional
-    self.provides = [self.with_pkgver("libcolord")]
+    self.renames = ["libcolord"]
 
     return self.default_libs()
 
