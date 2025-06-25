4ffd99eac950f60cf0220c2775b6dacfc2c42980
triallax <triallax@tutanota.com>
2025-06-25 11:49:14 +0100

    main/parallel: update to 20250622

--- main/parallel/template.py
+++ main/parallel/template.py
@@ -1,6 +1,6 @@
 pkgname = "parallel"
-pkgver = "20250522"
-pkgrel = 1
+pkgver = "20250622"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake"]
 depends = ["perl"]
@@ -8,4 +8,4 @@ pkgdesc = "Shell tool for executing jobs in parallel"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/parallel"
 source = f"https://ftp.gnu.org/gnu/parallel/parallel-{pkgver}.tar.bz2"
-sha256 = "b4b28f475f8cff8bb6ed4b03cc5a67041f18fc73fa256923b23181b56afdb2cb"
+sha256 = "69f578cf11f1b124ba3c2b673a16641debe63aeb3d2ac4cec5ad65f8a53d489b"
