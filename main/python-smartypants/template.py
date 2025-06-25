8900f074d0acc86f93c2845cc5f0d2f73f3c6e6b
triallax <triallax@tutanota.com>
2025-06-25 17:02:42 +0100

    main/python-smartypants: update to 2.0.2

--- main/python-smartypants/template.py
+++ main/python-smartypants/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-smartypants"
-pkgver = "2.0.1"
-pkgrel = 1
+pkgver = "2.0.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,7 +13,7 @@ pkgdesc = "Translate ASCII punctuation into HTML entities"
 license = "BSD-3-Clause"
 url = "https://github.com/leohemsted/smartypants.py"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "b98191911ff3b4144ef8ad53e776a2d0ad24bd508a905c6ce523597c40022773"
+sha256 = "560ae7485a27c288f5bd63131f5c3c7b8f0745f50e7c3f85f0bbc65d87799d19"
 
 
 def post_install(self):
