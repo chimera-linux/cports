90dea22d7c5893fb883a5f190d05174363b4c2c9
triallax <triallax@tutanota.com>
2025-06-25 12:34:25 +0100

    main/libnvme: update to 1.14

--- main/libnvme/template.py
+++ main/libnvme/template.py
@@ -1,5 +1,5 @@
 pkgname = "libnvme"
-pkgver = "1.13"
+pkgver = "1.14"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocs=false"]
@@ -16,7 +16,7 @@ pkgdesc = "C library for NVMe on Linux"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/linux-nvme/libnvme"
 source = f"{url}/archive/v{pkgver}/libnvme-v{pkgver}.tar.gz"
-sha256 = "f083c21f525644a2bcca6d367d96fab813de880cba9f1746a850f3a8692a5980"
+sha256 = "a7230d6d4959f26cf0c0ef6c9bb479bd94a8c0ec738bf6e164d66c3dc6397e66"
 
 
 @subpackage("libnvme-python")
