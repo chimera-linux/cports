30759a54888fea89e1797e3ea35ac0eb5d61d114
triallax <triallax@tutanota.com>
2025-06-25 20:47:03 +0100

    main/python-pyqt-builder: update to 1.18.2

--- main/python-pyqt-builder/template.py
+++ main/python-pyqt-builder/template.py
@@ -1,19 +1,18 @@
 pkgname = "python-pyqt-builder"
-pkgver = "1.18.1"
-pkgrel = 1
+pkgver = "1.18.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools_scm",
-    "python-wheel",
 ]
 depends = ["python"]
 pkgdesc = "PEP517 backend for PyQt projects"
 license = "custom:sip"
 url = "https://github.com/Python-PyQt/PyQt-builder"
 source = f"$(PYPI_SITE)/P/PyQt-builder/pyqt_builder-{pkgver}.tar.gz"
-sha256 = "3f7a3a2715947a293a97530a76fd59f1309fcb8e57a5830f45c79fe7249b3998"
+sha256 = "56dfea461484a87a8f0c8b0229190defc436d7ec5de71102e20b35e5639180bc"
 # no tests
 options = ["!check"]
 
