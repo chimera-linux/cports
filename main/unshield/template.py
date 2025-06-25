448a8c37613de3f2c5d175a82799ba88c92f32e9
triallax <triallax@tutanota.com>
2025-06-25 19:10:25 +0100

    main/unshield: update to 1.6.2

--- main/unshield/template.py
+++ main/unshield/template.py
@@ -1,15 +1,14 @@
 pkgname = "unshield"
-pkgver = "1.5.1"
+pkgver = "1.6.2"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DCMAKE_POLICY_VERSION_MINIMUM=3.5"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = ["zlib-ng-compat-devel"]
 pkgdesc = "Utility for InstallShield CAB archive extraction"
 license = "MIT"
 url = "https://github.com/twogood/unshield"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "34cd97ff1e6f764436d71676e3d6842dc7bd8e2dd5014068da5c560fe4661f60"
+sha256 = "a937ef596ad94d16e7ed2c8553ad7be305798dcdcfd65ae60210b1e54ab51a2f"
 # runner not integrated
 options = ["!check"]
 
