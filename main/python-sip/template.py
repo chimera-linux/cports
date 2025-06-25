0c1fbaecda0814b7432c81c33895efed3460449a
triallax <triallax@tutanota.com>
2025-06-25 20:46:53 +0100

    main/python-sip: update to 6.12.0

--- main/python-sip/template.py
+++ main/python-sip/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-sip"
-pkgver = "6.11.1"
+pkgver = "6.12.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -15,7 +15,7 @@ pkgdesc = "Tool for making python bindings for C/C++ libraries"
 license = "custom:sip"
 url = "https://github.com/Python-SIP/sip"
 source = f"$(PYPI_SITE)/s/sip/sip-{pkgver}.tar.gz"
-sha256 = "3dae4baaf9e9f781d84bf293e0e2938dc7f44b826837889026eba53bd36c81b5"
+sha256 = "083ced94f85315493231119a63970b2ba42b1d38b38e730a70e02a99191a89c6"
 # pypi tarball does not contain tests
 options = ["!check"]
 
