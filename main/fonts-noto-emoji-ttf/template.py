e44a968c36becea9f0f6a6fe2dd664fd8464089f
triallax <triallax@tutanota.com>
2025-06-25 15:40:51 +0100

    main/fonts-noto-emoji-ttf: update to 2.048

--- main/fonts-noto-emoji-ttf/template.py
+++ main/fonts-noto-emoji-ttf/template.py
@@ -1,18 +1,18 @@
 pkgname = "fonts-noto-emoji-ttf"
-pkgver = "2.047"
+pkgver = "2.048"
 pkgrel = 0
 pkgdesc = "Google Noto emoji fonts"
 license = "OFL-1.1"
 url = "https://github.com/googlefonts/noto-emoji"
 source = f"{url}/archive/v{pkgver}/font-noto-emoji-{pkgver}.tar.gz"
-sha256 = "2cfaf5a427eb26334cdb30d98e4a0c005b660504a339249dc54373e566f09b50"
+sha256 = "e6396642172e3d5031bef5f381cc047a007588e73b26209ba1c47b3d1f8faa60"
 # No copyright header in license text
 options = ["!distlicense"]
 
 
 def install(self):
     self.install_file(
-        self.files_path / "66-noto-color-emoji.conf",
+        "^/66-noto-color-emoji.conf",
         "usr/share/fontconfig/conf.avail",
     )
     self.install_file("fonts/NotoColorEmoji.ttf", "usr/share/fonts/noto")
