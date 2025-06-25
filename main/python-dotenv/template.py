89b909ff773453f10ef85d8d3d55262dc3ff550f
triallax <triallax@tutanota.com>
2025-06-25 16:31:46 +0100

    main/python-dotenv: update to 1.1.1

--- main/python-dotenv/template.py
+++ main/python-dotenv/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-dotenv"
-pkgver = "1.1.0"
+pkgver = "1.1.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -17,7 +17,7 @@ pkgdesc = "Python module that reads env vars from a .env file"
 license = "BSD-3-Clause"
 url = "https://saurabh-kumar.com/python-dotenv"
 source = f"$(PYPI_SITE)/p/python-dotenv/python_dotenv-{pkgver}.tar.gz"
-sha256 = "41f90bc6f5f177fb41f53e87666db362025010eb28f60a01c9143bfa33a2b2d5"
+sha256 = "a8a6399716257f45be6a007360200409fce5cda2661e3dec71d23dc15f6189ab"
 
 
 def post_install(self):
