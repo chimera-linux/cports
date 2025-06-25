dbdeea007c7474f1b9bb46e41a07131c7a16fe85
triallax <triallax@tutanota.com>
2025-06-25 16:44:18 +0100

    main/python-pytest-subtests: update to 0.14.2

--- main/python-pytest-subtests/template.py
+++ main/python-pytest-subtests/template.py
@@ -1,12 +1,11 @@
 pkgname = "python-pytest-subtests"
-pkgver = "0.14.1"
-pkgrel = 1
+pkgver = "0.14.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools_scm",
-    "python-wheel",
 ]
 depends = ["python-attrs", "python-pytest"]
 checkdepends = ["python-pytest-xdist", *depends]
@@ -14,7 +13,7 @@ pkgdesc = "Unittest subTest() and subtests fixture"
 license = "MIT"
 url = "https://github.com/pytest-dev/pytest-subtests"
 source = f"$(PYPI_SITE)/p/pytest-subtests/pytest_subtests-{pkgver}.tar.gz"
-sha256 = "350c00adc36c3aff676a66135c81aed9e2182e15f6c3ec8721366918bbbf7580"
+sha256 = "7154a8665fd528ee70a76d00216a44d139dc3c9c83521a0f779f7b0ad4f800de"
 
 
 def post_install(self):
