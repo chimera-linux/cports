9773d41c3f45ad7fdd15199900b2db0aecac0312
ttyyls <contact@behri.org>
2025-06-27 18:26:19 +0100

    user/texlab: update to 5.23.1

--- user/texlab/template.py
+++ user/texlab/template.py
@@ -1,5 +1,5 @@
 pkgname = "texlab"
-pkgver = "5.23.0"
+pkgver = "5.23.1"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -8,7 +8,7 @@ pkgdesc = "LaTeX LSP server"
 license = "GPL-3.0-or-later"
 url = "https://github.com/latex-lsp/texlab"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f14a3e100706cc217a6720057dea2e30b7c7a3a7297e6d28ea741a533500a1cf"
+sha256 = "32620d4a186222cef1140250c9c43b83ed873a4710d05a0075c7d8f6d1d4e1ec"
 
 
 def install(self):
