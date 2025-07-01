a4c04eaafbfb3416d8579c576107eb14771fc0f2
Erica Z <zerica@callcc.eu>
2025-07-01 21:37:16 +0200

    main/limine: update to 9.4.0

--- main/limine/template.py
+++ main/limine/template.py
@@ -1,5 +1,5 @@
 pkgname = "limine"
-pkgver = "9.2.3"
+pkgver = "9.4.0"
 pkgrel = 0
 # these targets implemented
 archs = ["aarch64", "loongarch64", "riscv64", "x86_64"]
@@ -10,7 +10,7 @@ pkgdesc = "Multiprotocol EFI bootloader"
 license = "BSD-2-Clause AND 0BSD"
 url = "https://limine-bootloader.org"
 source = f"https://github.com/limine-bootloader/limine/releases/download/v{pkgver}/limine-{pkgver}.tar.zst"
-sha256 = "a7ac32591fce9cc92ec3e951305f5380921da84634282f8494ce0a476ce54bb5"
+sha256 = "2d9b7e9dbc2d566ef54b66c8016bc525a2f8faf52edafded52a330027ed9799a"
 # no test suite
 options = ["!check"]
 
