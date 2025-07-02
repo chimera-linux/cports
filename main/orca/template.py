b7d71e15c473a2fff5396418675f22be3a45067f
triallax <triallax@tutanota.com>
2025-07-02 21:49:52 +0100

    main/orca: update to 48.6

--- main/orca/template.py
+++ main/orca/template.py
@@ -1,5 +1,5 @@
 pkgname = "orca"
-pkgver = "48.5"
+pkgver = "48.6"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -32,4 +32,4 @@ pkgdesc = "GNOME screen reader"
 license = "LGPL-2.1-or-later"
 url = "https://orca.gnome.org"
 source = f"$(GNOME_SITE)/orca/{pkgver[:-2]}/orca-{pkgver}.tar.xz"
-sha256 = "51529efdc07b15b9568eca0147f9a84ff915654046e2c021bfee6a04910dad66"
+sha256 = "edc50344e0dfd72476b5c14b3aa725ca268718ea6dd89bc4ee26f450b339d696"
