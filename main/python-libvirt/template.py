2fa3707f244f33ba312421729491b6c36cd3b511
triallax <triallax@tutanota.com>
2025-07-03 00:29:18 +0100

    main/python-libvirt: update to 11.5.0

--- main/python-libvirt/template.py
+++ main/python-libvirt/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-libvirt"
-pkgver = "11.2.0"
+pkgver = "11.5.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -21,4 +21,4 @@ pkgdesc = "Libvirt virtualization API Python3 binding"
 license = "LGPL-2.1-or-later"
 url = "https://pypi.org/project/libvirt-python"
 source = f"https://libvirt.org/sources/python/libvirt-python-{pkgver}.tar.gz"
-sha256 = "bb5f245f0e01579b0b48e8657f0488f440e609c6b6c3c7c7e7c4ae4e61d3c346"
+sha256 = "69aad89ec689526835bf7d2d224badafe52a2def0d719676166755a8eab7ac23"
