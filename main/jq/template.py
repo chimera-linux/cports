28ecfe7a97d0b0a5f7fe3cae632cd9c825d66805
triallax <triallax@tutanota.com>
2025-07-02 20:05:57 +0100

    main/jq: update to 1.8.1

--- main/jq/template.py
+++ main/jq/template.py
@@ -1,5 +1,5 @@
 pkgname = "jq"
-pkgver = "1.8.0"
+pkgver = "1.8.1"
 pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
@@ -13,7 +13,7 @@ pkgdesc = "Command-line JSON processor"
 license = "MIT"
 url = "https://github.com/jqlang/jq"
 source = f"{url}/releases/download/jq-{pkgver}/jq-{pkgver}.tar.gz"
-sha256 = "91811577f91d9a6195ff50c2bffec9b72c8429dc05ec3ea022fd95c06d2b319c"
+sha256 = "2be64e7129cecb11d5906290eba10af694fb9e3e7f9fc208a311dc33ca837eb0"
 # FIXME int: null meme in jqtest
 # FIXME vis: fails to link
 hardening = ["!int", "!vis", "!cfi"]
