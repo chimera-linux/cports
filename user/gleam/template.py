e534020457cd98f21da295490501a2deb113a754
Wesley Moore <wes@wezm.net>
2025-07-02 20:38:31 +1000

    user/gleam: update to 1.11.1

--- user/gleam/template.py
+++ user/gleam/template.py
@@ -1,5 +1,5 @@
 pkgname = "gleam"
-pkgver = "1.10.0"
+pkgver = "1.11.1"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -11,7 +11,7 @@ url = "https://gleam.run"
 source = (
     f"https://github.com/gleam-lang/gleam/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "4661bebc010209c5c3d180a8f7ad6c16b596655acf74bf459d3baf81af8589d5"
+sha256 = "34dfdc397835849bc56ac01bf45e68ee9cfc3c99609fb7b3ab02910930a8c40d"
 
 
 def install(self):
