8a807fe0914185b415c5f51cd350e2f3f155bf72
triallax <triallax@tutanota.com>
2025-07-02 14:35:08 +0100

    main/rust-analyzer: update to 2025.06.30

--- main/rust-analyzer/template.py
+++ main/rust-analyzer/template.py
@@ -1,5 +1,5 @@
 pkgname = "rust-analyzer"
-pkgver = "2025.06.02"
+pkgver = "2025.06.30"
 _pver = pkgver.replace(".", "-")
 pkgrel = 0
 build_style = "cargo"
@@ -14,7 +14,7 @@ pkgdesc = "Rust compiler LSP server"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/rust-lang/rust-analyzer"
 source = f"{url}/archive/refs/tags/{_pver}.tar.gz"
-sha256 = "c993bee4845acc400e6b6b499dadb50eafca02fab4e38001663fc6003ecec623"
+sha256 = "d7f41ab38b689df07e34a38cc923cffd9e8225a1b643b370f017437637129075"
 # invokes rustfmt via rustup arg, also take longer to build than the actual
 # build..
 options = ["!check"]
