57060aa622d61d655e3287ee9c2d2fc8ccb9bdd9
ttyyls <contact@behri.org>
2025-07-03 19:53:34 +0200

    user/re2c: update to 4.3

--- user/re2c/template.py
+++ user/re2c/template.py
@@ -1,5 +1,5 @@
 pkgname = "re2c"
-pkgver = "4.2"
+pkgver = "4.3"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = ["./autogen.sh"]
@@ -15,7 +15,7 @@ pkgdesc = "Regular Expressions to Code, lexer generator"
 license = "custom:none"  # Public Domain dedication
 url = "https://re2c.org"
 source = f"https://github.com/skvadrik/re2c/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "01b56c67ca2d5054b1aafc41ef5c15c50fbb6a7e760b1b2346e6116ef039525e"
+sha256 = "39cd7048a817cf3d7d0c2e58a52fb3597d6e1bc86b1df32b8a3cd755c458adfd"
 
 
 def post_install(self):
