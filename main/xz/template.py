28c453afcfd09577e161df2fd2f0d2085d810866
triallax <triallax@tutanota.com>
2025-07-04 15:23:16 +0100

    main/xz: update to 5.8.1

--- main/xz/template.py
+++ main/xz/template.py
@@ -1,15 +1,15 @@
 pkgname = "xz"
-pkgver = "5.6.4"
+pkgver = "5.8.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "libtool", "pkgconf"]
 makedepends = []
-provides = [self.with_pkgver("liblzma")]
+renames = ["liblzma"]
 pkgdesc = "XZ compression utilities"
 license = "0BSD"
 url = "https://tukaani.org/xz"
 source = f"https://github.com/tukaani-project/xz/releases/download/v{pkgver}/xz-{pkgver}.tar.gz"
-sha256 = "269e3f2e512cbd3314849982014dc199a7b2148cf5c91cedc6db629acdf5e09b"
+sha256 = "507825b599356c10dca1cd720c9d0d0c9d5400b9de300af00e4d1ea150795543"
 options = ["bootstrap"]
 
 
@@ -42,6 +42,6 @@ def post_install(self):
 
 @subpackage("xz-devel")
 def _(self):
-    self.provides = [self.with_pkgver("liblzma-devel")]
+    self.renames = ["liblzma-devel"]
 
     return self.default_devel()
