44181b625f1a168a1b243af2581a6295151166ac
Erica Z <zerica@callcc.eu>
2025-07-05 10:53:44 +0200

    main/python-setuptools: update to 80.9.0

--- main/python-setuptools/template.py
+++ main/python-setuptools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-setuptools"
-pkgver = "78.1.0"
+pkgver = "80.9.0"
 pkgrel = 0
 hostmakedepends = ["python-devel"]
 depends = ["python", "python-wheel"]
@@ -7,7 +7,7 @@ pkgdesc = "Easily build and distribute Python packages"
 license = "MIT"
 url = "https://github.com/pypa/setuptools"
 source = f"$(PYPI_SITE)/s/setuptools/setuptools-{pkgver}.tar.gz"
-sha256 = "18fd474d4a82a5f83dac888df697af65afa82dec7323d09c3e37d1f14288da54"
+sha256 = "f36b47402ecde768dbfafc46e8e4207b4360c654f1f3bb84475f0a28628fb19c"
 env = {
     "SETUPTOOLS_INSTALL_WINDOWS_SPECIFIC_FILES": "0",
     "SETUPTOOLS_DISABLE_VERSIONED_EASY_INSTALL_SCRIPT": "1",
