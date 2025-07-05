265137f01d45443d23bc182efa428abd8ae11daa
Erica Z <zerica@callcc.eu>
2025-07-05 10:58:58 +0200

    main/v4l-utils: update to 1.30.1
    also add a couple missing makedepends

--- main/v4l-utils/template.py
+++ main/v4l-utils/template.py
@@ -1,11 +1,12 @@
 pkgname = "v4l-utils"
-pkgver = "1.28.1"
-pkgrel = 1
+pkgver = "1.30.1"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dqv4l2=disabled", "-Dudevdir=/usr/lib/udev"]
 hostmakedepends = [
     "bash",
     "gettext-devel",
+    "gsed",
     "meson",
     "perl",
     "pkgconf",
@@ -13,6 +14,8 @@ hostmakedepends = [
 makedepends = [
     "argp-standalone",
     "glu-devel",
+    "json-c-devel",
+    "libbpf-devel",
     "libjpeg-turbo-devel",
     "libx11-devel",
     "mesa-devel",
@@ -22,8 +25,12 @@ pkgdesc = "Userspace tools and libraries for V4L"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://linuxtv.org/wiki/index.php/V4l-utils"
 source = f"https://linuxtv.org/downloads/v4l-utils/v4l-utils-{pkgver}.tar.xz"
-sha256 = "0fa075ce59b6618847af6ea191b6155565ccaa44de0504581ddfed795a328a82"
-tool_flags = {"LDFLAGS": ["-largp"]}
+sha256 = "c1cf549c2ec3cf39eb5ec7bf15731349e61b26a21b5e963922db422333bae197"
+tool_flags = {
+    # mmap64, open64
+    "CXXFLAGS": ["-D_LARGEFILE64_SOURCE"],
+    "LDFLAGS": ["-largp"],
+}
 
 
 @subpackage("v4l-utils-devel")
