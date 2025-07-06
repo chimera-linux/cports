ab3aef880cf524f6f4316e52c531f59f6631485f
triallax <triallax@tutanota.com>
2025-07-06 18:03:22 +0100

    main/python-pycodestyle: update to 2.14.0

--- main/python-pycodestyle/template.py
+++ main/python-pycodestyle/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pycodestyle"
-pkgver = "2.13.0"
+pkgver = "2.14.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,7 +14,7 @@ pkgdesc = "Python style guide checker"
 license = "MIT"
 url = "https://github.com/PyCQA/pycodestyle"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "b1a4db0d9b8285f6643bcdb41362be6d6c94b891b13ead09c57a2513c46b717b"
+sha256 = "ffcf4dc55f1e5fbdc6dd6acf5db0fd07ded534ae376eee23a742e1410b48d9ae"
 
 
 def post_install(self):
