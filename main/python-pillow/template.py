7f9d025aec9529ea25c48b4c68828dbdcfa7762d
triallax <triallax@tutanota.com>
2025-07-06 14:27:02 +0100

    main/python-pillow: update to 11.3.0

--- main/python-pillow/template.py
+++ main/python-pillow/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pillow"
-pkgver = "11.2.1"
+pkgver = "11.3.0"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_target = "Tests"
@@ -11,6 +11,7 @@ hostmakedepends = [
 makedepends = [
     "freetype-devel",
     "lcms2-devel",
+    "libavif-devel",
     "libjpeg-turbo-devel",
     "libtiff-devel",
     "libwebp-devel",
@@ -23,7 +24,7 @@ pkgdesc = "Python Imaging Library"
 license = "MIT-CMU"
 url = "https://python-pillow.org"
 source = f"$(PYPI_SITE)/p/pillow/pillow-{pkgver}.tar.gz"
-sha256 = "a64dd61998416367b7ef979b73d3a85853ba9bec4c2925f74e588879a58716b6"
+sha256 = "3828ee7586cd0b2091b6209e5ad53e20d0649bbe87164a459d0676e035e8f523"
 
 
 def post_install(self):
