9ca74e92801c150c937a7eb084d7af57d445465c
JetpackJackson <jetpackjackson@katzenmue.de>
2025-07-06 14:29:10 -0400

    user/listenbrainz-mpd: update to 2.3.9
    Signed-off-by: JetpackJackson <jetpackjackson@katzenmue.de>

--- user/listenbrainz-mpd/template.py
+++ user/listenbrainz-mpd/template.py
@@ -1,14 +1,14 @@
 pkgname = "listenbrainz-mpd"
-pkgver = "2.3.8"
+pkgver = "2.3.9"
 pkgrel = 0
 build_style = "cargo"
-hostmakedepends = ["cargo", "pkgconf"]
+hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = ["openssl3-devel", "rust-std", "sqlite-devel"]
 pkgdesc = "ListenBrainz submission client for MPD"
 license = "AGPL-3.0-only"
 url = "https://codeberg.org/elomatreb/listenbrainz-mpd"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "0a3456029c6eb73b391b1ad3b0f01795554c3b040f63998112a4a5fe8cbc7637"
+sha256 = "20f287d1561f9739ce0f61fba053f0e0cd8d48869ff33e5d2f14bcda40912a64"
 # no tests
 options = ["!check"]
 
