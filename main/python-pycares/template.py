ac55ed1a233fbac476e9a2240fbf5242f73002d7
triallax <triallax@tutanota.com>
2025-07-06 17:53:57 +0100

    main/python-pycares: update to 4.9.0

--- main/python-pycares/template.py
+++ main/python-pycares/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pycares"
-pkgver = "4.8.0"
+pkgver = "4.9.0"
 pkgrel = 0
 build_style = "python_pep517"
 make_build_env = {"PYCARES_USE_SYSTEM_LIB": "1"}
@@ -15,7 +15,7 @@ pkgdesc = "Python bindings for c-ares"
 license = "MIT"
 url = "https://github.com/saghul/pycares"
 source = f"$(PYPI_SITE)/p/pycares/pycares-{pkgver}.tar.gz"
-sha256 = "2fc2ebfab960f654b3e3cf08a732486950da99393a657f8b44618ad3ed2d39c1"
+sha256 = "8ee484ddb23dbec4d88d14ed5b6d592c1960d2e93c385d5e52b6fad564d82395"
 # tests requires internet access
 options = ["!check"]
 
