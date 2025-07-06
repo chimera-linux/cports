d394d2ffbfb807652ae1be3549ff117029d36a40
triallax <triallax@tutanota.com>
2025-07-06 16:21:51 +0100

    main/python-rpds-py: update to 0.26.0

--- main/python-rpds-py/template.py
+++ main/python-rpds-py/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-rpds-py"
-pkgver = "0.25.1"
+pkgver = "0.26.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -20,7 +20,7 @@ pkgdesc = "Python bindings to the Rust rpds crate"
 license = "MIT"
 url = "https://github.com/crate-py/rpds"
 source = f"$(PYPI_SITE)/r/rpds-py/rpds_py-{pkgver}.tar.gz"
-sha256 = "8960b6dac09b62dac26e75d7e2c4a22efb835d827a7278c34f72b2b84fa160e3"
+sha256 = "20dae58a859b0906f0685642e591056f1e787f3a8b39c8e8749a45dc7d26bdb0"
 
 
 def post_patch(self):
