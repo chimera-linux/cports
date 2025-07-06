0e6c08106decf94700e4342c9b3284ab408962f6
triallax <triallax@tutanota.com>
2025-07-06 03:17:39 +0100

    main/readline: update to 8.3

--- main/readline/template.py
+++ main/readline/template.py
@@ -3,9 +3,9 @@
 # really bad ones) and sometimes we cannot just replace it
 pkgname = "readline"
 # use a git revision so we don't have to deal with the stupid patches
-_gitrev = "037d85f199a8c6e5b16689a46c8bc31b586a0c94"
-pkgver = "8.2.013"
-pkgrel = 1
+_gitrev = "447b8290b3e2e2d117dc8e9cdb83b0dc6448a638"
+pkgver = "8.3"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--disable-static",
@@ -21,7 +21,7 @@ pkgdesc = "GNU Readline library"
 license = "GPL-3.0-or-later"
 url = "https://tiswww.cwru.edu/php/chet/readline/rltop.html"
 source = f"https://git.savannah.gnu.org/cgit/readline.git/snapshot/readline-{_gitrev}.tar.gz"
-sha256 = "86959fc06a4ad8a3852b68ce67f5bb2f98d7d95548bbfc5c87e806042a8567e1"
+sha256 = "0affa0b657cadd7c63109e99ad5801dff0ba4841395b824d55b4cd52396fff74"
 
 
 def post_install(self):
