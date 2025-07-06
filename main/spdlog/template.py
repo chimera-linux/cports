a700ac8098f740283788ffd7b5ad09c35de52095
triallax <triallax@tutanota.com>
2025-07-06 05:01:16 +0100

    main/spdlog: update to 1.15.3

--- main/spdlog/template.py
+++ main/spdlog/template.py
@@ -1,6 +1,6 @@
 pkgname = "spdlog"
-pkgver = "1.15.1"
-pkgrel = 1
+pkgver = "1.15.3"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DSPDLOG_FMT_EXTERNAL=ON",
@@ -14,7 +14,7 @@ pkgdesc = "C++ logging library"
 license = "MIT"
 url = "https://github.com/gabime/spdlog"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "25c843860f039a1600f232c6eb9e01e6627f7d030a2ae5e232bdd3c9205d26cc"
+sha256 = "15a04e69c222eb6c01094b5c7ff8a249b36bb22788d72519646fb85feb267e67"
 
 
 def post_install(self):
