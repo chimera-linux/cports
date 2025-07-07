42d7c5d6269803c7fbc1cd1a3a98fd41437a9656
q66 <q66@chimera-linux.org>
2025-07-07 16:37:46 +0200

    main/glslang: update to 15.4.0

--- main/glslang/template.py
+++ main/glslang/template.py
@@ -1,5 +1,5 @@
 pkgname = "glslang"
-pkgver = "15.3.0"
+pkgver = "15.4.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -14,7 +14,7 @@ pkgdesc = "Khronos reference front-end for GLSL/ESSL + sample SPIR-V generator"
 license = "BSD-3-Clause"
 url = "https://github.com/KhronosGroup/glslang"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "c6c21fe1873c37e639a6a9ac72d857ab63a5be6893a589f34e09a6c757174201"
+sha256 = "b16c78e7604b9be9f546ee35ad8b6db6f39bbbbfb19e8d038b6fe2ea5bba4ff4"
 # FIXME: tests reveal a suboverflow in TIntermConstantUnion::fold that should be fixed
 hardening = ["!int"]
 
