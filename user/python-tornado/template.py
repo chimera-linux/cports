d6faa423e5576f271d13638a945a25b60bb480a9
Renato Botelho do Couto <renato@netgate.com>
2025-07-09 05:51:12 -0500

    user/python-tornado: new package

---
+++ user/python-tornado/template.py
@@ -0,0 +1,65 @@
+pkgname = "python-tornado"
+pkgver = "6.5.1"
+pkgrel = 0
+build_style = "python_pep517"
+hostmakedepends = [
+    "python-build",
+    "python-installer",
+    "python-setuptools",
+]
+depends = [
+    "python-alabaster",
+    "python-babel",
+    "python-black",
+    "python-cachetools",
+    "python-certifi",
+    "python-chardet",
+    "python-charset-normalizer",
+    "python-click",
+    "python-colorama",
+    "python-distlib",
+    "python-docutils",
+    "python-filelock",
+    "python-flake8",
+    "python-idna",
+    "python-imagesize",
+    "python-jinja2",
+    "python-markupsafe",
+    "python-mccabe",
+    "python-mypy",
+    "python-packaging",
+    "python-pathspec",
+    "python-platformdirs",
+    "python-pluggy",
+    "python-pycodestyle",
+    "python-pyflakes",
+    "python-pygments",
+    "python-requests",
+    "python-snowballstemmer",
+    "python-sphinx",
+    "python-sphinxcontrib-applehelp",
+    "python-sphinxcontrib-devhelp",
+    "python-sphinxcontrib-htmlhelp",
+    "python-sphinxcontrib-jquery",
+    "python-sphinxcontrib-jsmath",
+    "python-sphinxcontrib-qthelp",
+    "python-sphinxcontrib-serializinghtml",
+    "python-urllib3",
+    "python-wheel",
+]
+checkdepends = ["python-pytest"]
+pkgdesc = "Python3 web framework and asynchronous networking library"
+license = "Apache-2.0"
+url = "https://www.tornadoweb.org"
+source = f"$(PYPI_SITE)/t/tornado/tornado-{pkgver}.tar.gz"
+sha256 = "84ceece391e8eb9b2b95578db65e920d2a61070260594819589609ba9bc6308c"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+def init_check(self):
+    self.make_check_args = [
+        "--ignore=tornado/test/iostream_test.py",
+    ]
