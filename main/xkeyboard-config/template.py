e69a2714f2ae9d6f7b36f85389a75fcb6c037725
q66 <q66@chimera-linux.org>
2025-07-09 15:44:19 +0200

    Revert "main/xkeyboard-config: update to 2.45"
    This reverts commit 7c0e52470d127464a6d613445488fa40b3a5ce2c.
    
    The newer version breaks macintosh keyboard layouts. Revert until
    that is fixed

--- main/xkeyboard-config/template.py
+++ main/xkeyboard-config/template.py
@@ -1,5 +1,6 @@
 pkgname = "xkeyboard-config"
-pkgver = "2.45"
+# 2.45 breaks macintosh layouts
+pkgver = "2.44"
 pkgrel = 1
 build_style = "meson"
 configure_args = ["-Dxorg-rules-symlinks=true", "-Dcompat-rules=true"]
@@ -11,15 +12,9 @@ pkgdesc = "X Keyboard Configuration Database"
 license = "MIT"
 url = "https://www.freedesktop.org/wiki/Software/XKeyboardConfig"
 source = f"$(XORG_SITE)/data/xkeyboard-config/xkeyboard-config-{pkgver}.tar.xz"
-sha256 = "169e075a92d957a57787c199e84e359df2931b7196c1c5b4a3d576ee6235a87c"
+sha256 = "54d2c33eeebb031d48fa590c543e54c9bcbd0f00386ebc6489b2f47a0da4342a"
 hardening = ["vis", "cfi"]
 
 
 def post_install(self):
-    # apk bug: can't migrate directory -> symlink
-    self.uninstall("usr/share/X11/xkb")
-    self.rename(
-        "usr/share/xkeyboard-config-2", "usr/share/X11/xkb", relative=False
-    )
-    self.install_link("usr/share/xkeyboard-config-2", "X11/xkb")
     self.install_license("COPYING")
