c467c3d70409c0071b6db17d75b5505e8a347238
q66 <q66@chimera-linux.org>
2025-07-10 01:37:28 +0200

    main/mygui: update to 3.4.3

--- main/mygui/template.py
+++ main/mygui/template.py
@@ -1,11 +1,11 @@
 pkgname = "mygui"
-pkgver = "3.4.2"
-pkgrel = 7
+pkgver = "3.4.3"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
-    "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
     "-DMYGUI_BUILD_DEMOS=OFF",
     "-DMYGUI_BUILD_TOOLS=OFF",
+    "-DMYGUI_DONT_USE_OBSOLETE=ON",
     "-DMYGUI_RENDERSYSTEM=1",
 ]
 hostmakedepends = ["cmake", "ninja", "pkgconf", "doxygen"]
@@ -21,7 +21,7 @@ pkgdesc = "Fast, flexible and simple GUI"
 license = "MIT"
 url = "https://github.com/MyGUI"
 source = f"{url}/mygui/archive/MyGUI{pkgver}.tar.gz"
-sha256 = "1cc45fb96c9438e3476778449af0378443d84794a458978a29c75306e45dd45a"
+sha256 = "33c91b531993047e77cace36d6fea73634b8c17bd0ed193d4cd12ac7c6328abd"
 # unit tests are off
 options = ["!check"]
 
