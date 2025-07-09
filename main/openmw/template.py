1357637b9b02cc60e0140d7cdd675536f166306d
q66 <q66@chimera-linux.org>
2025-07-10 01:43:04 +0200

    main/openmw: update to 0.49.0

--- main/openmw/template.py
+++ main/openmw/template.py
@@ -1,11 +1,10 @@
 pkgname = "openmw"
-pkgver = "0.47.0"
-pkgrel = 11
+pkgver = "0.49.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
-    # enable once we have proper qt6 support (0.49)
-    "-DBUILD_OPENCS=OFF",
+    "-DBUILD_OPENCS=ON",
     "-DOPENMW_USE_SYSTEM_BULLET=OFF",
     "-DOPENMW_LTO_BUILD=ON",
 ]
@@ -14,25 +13,28 @@ hostmakedepends = [
     "ninja",
     "pkgconf",
     "qt6-qtbase",
-    "qt6-qttools",
+    "qt6-qttools-devel",
 ]
 makedepends = [
     "boost-devel",
     "ffmpeg-devel",
     "libxt-devel",
     "lz4-devel",
+    "luajit-devel",
     "mygui-devel",
     "openal-soft-devel",
     "openscenegraph-devel",
     "qt6-qtbase-devel",
+    "qt6-qtsvg-devel",
     "sdl2-compat-devel",
     "unshield-devel",
+    "yaml-cpp-devel",
 ]
 pkgdesc = "Open implementation of Morrowind's engine"
 license = "GPL-3.0-or-later"
 url = "https://openmw.org"
 # matches the files openmw declares in cmake
-_recast_hash = "e75adf86f91eb3082220085e42dda62679f9a3ea"
+_recast_hash = "c393777d26d2ff6519ac23612abf8af42678c9dd"
 _bullet_hash = "3.17"
 source = [
     f"https://gitlab.com/OpenMW/openmw/-/archive/openmw-{pkgver}/openmw-openmw-{pkgver}.tar.gz",
@@ -45,8 +47,8 @@ source_paths = [
     "build/extern/fetched/bullet",
 ]
 sha256 = [
-    "bd7f77e1527c2180e9b0dfcbe401d6fb48f24dbb37701dac7747697873d6edb4",
-    "d3339aaea1d81307bcac2bece176c5359ed5f8c8f9721fc360d28f82f9119253",
+    "5f31741d61cf7c736bbe522a1a33342773c7e1b713e7e20f3717eb8da2b1733d",
+    "7d7fc38c9a52dbda0ba1dab70d1ed59da1178ab1c4fa209cdb7cc2dcfce8e4ea",
     "baa642c906576d4d98d041d0acb80d85dd6eff6e3c16a009b1abf1ccd2bc0a61",
 ]
 # unit tests are off
@@ -54,6 +56,19 @@ options = ["!check"]
 
 if self.profile().endian == "big":
     broken = "esm loader is not ready etc."
+
+
+@subpackage("openmw-cs")
+def _(self):
+    self.pkgdesc = "Open implementation of Morrowind Creation Set"
+    self.depends = [self.parent]
+
+    return [
+        "etc/openmw/defaults-cs.bin",
+        "usr/bin/openmw-cs",
+        "usr/share/applications/org.openmw.cs.desktop",
+        "usr/share/pixmaps/openmw-cs.png",
+    ]
 
 
 @subpackage("openmw-esmtool")
