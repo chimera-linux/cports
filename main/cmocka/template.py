11873cbce251544c6bb63344900e26909af560ff
q66 <q66@chimera-linux.org>
2025-07-20 01:10:22 +0200

    main/cmocka: update to 1.1.8

--- main/cmocka/template.py
+++ main/cmocka/template.py
@@ -1,6 +1,6 @@
 pkgname = "cmocka"
-pkgver = "1.1.7"
-pkgrel = 1
+pkgver = "1.1.8"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DUNIT_TESTING=ON"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -8,7 +8,7 @@ pkgdesc = "Unit testing framework in C"
 license = "Apache-2.0"
 url = "https://cmocka.org"
 source = f"{url}/files/{pkgver[:-2]}/cmocka-{pkgver}.tar.xz"
-sha256 = "810570eb0b8d64804331f82b29ff47c790ce9cd6b163e98d47a4807047ecad82"
+sha256 = "58435b558766d7f4c729ba163bdf3aec38bed3bc766dab684e3526ed0aa7c780"
 
 
 @subpackage("cmocka-devel")
