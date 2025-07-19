7604e4b2d549e0b18dfef87a174d55e5f13a6b90
q66 <q66@chimera-linux.org>
2025-07-20 01:25:08 +0200

    main/erofs-utils: update to 1.8.10

--- main/erofs-utils/template.py
+++ main/erofs-utils/template.py
@@ -1,6 +1,6 @@
 pkgname = "erofs-utils"
-pkgver = "1.8.6"
-pkgrel = 1
+pkgver = "1.8.10"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--enable-fuse",
@@ -29,7 +29,7 @@ pkgdesc = "Userspace tools for the EROFS filesystem"
 license = "Apache-2.0 AND GPL-2.0-or-later"
 url = "https://github.com/erofs/erofs-utils"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5b221dc3fd6d151425b30534ede46fb7a90dc233a8659cba0372796b0a066547"
+sha256 = "05eb4edebe11decce6ecb34e98d2f80c8cd283c2f2967d8ba7efd58418570514"
 tool_flags = {
     "CFLAGS": ["-D_LARGEFILE64_SOURCE"],
     "LDFLAGS": ["-Wl,-z,stack-size=2097152"],
