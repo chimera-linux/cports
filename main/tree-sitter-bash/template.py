80f4a966f90a16240b3e5a158cc9e51f10d74d28
Ayush Agarwal <ayush@ayushnix.com>
2025-07-21 20:29:15 +0530

    main/tree-sitter-bash: update to 0.25.0

--- main/tree-sitter-bash/template.py
+++ main/tree-sitter-bash/template.py
@@ -1,5 +1,5 @@
 pkgname = "tree-sitter-bash"
-pkgver = "0.23.3"
+pkgver = "0.25.0"
 pkgrel = 0
 build_style = "makefile"
 make_check_target = "test"
@@ -11,14 +11,14 @@ pkgdesc = "Bash grammar for tree-sitter"
 license = "MIT"
 url = "https://github.com/tree-sitter/tree-sitter-bash"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "c682b81d0fe953d19f6632db3ba6e4f2db1efe1784f7a28bc5fcf6355d67335b"
+sha256 = "9d6bad618e712b51ff060515b0ce6872e33727148f35becb8aa3ad80044c2348"
 
 
 def post_install(self):
     self.install_license("LICENSE")
     self.install_dir("usr/lib/tree-sitter")
     self.install_link(
-        "usr/lib/tree-sitter/bash.so", "../libtree-sitter-bash.so.14"
+        "usr/lib/tree-sitter/bash.so", "../libtree-sitter-bash.so.15"
     )
 
 
