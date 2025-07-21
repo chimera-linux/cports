50bbd17ffa77dd173e0332b2ed1f6bdf0b2e623a
Ayush Agarwal <ayush@ayushnix.com>
2025-07-21 20:29:23 +0530

    main/tree-sitter-query: update to 0.6.2

--- main/tree-sitter-query/template.py
+++ main/tree-sitter-query/template.py
@@ -1,5 +1,5 @@
 pkgname = "tree-sitter-query"
-pkgver = "0.6.1"
+pkgver = "0.6.2"
 pkgrel = 0
 build_style = "makefile"
 make_check_target = "test"
@@ -12,8 +12,8 @@ license = "Apache-2.0"
 url = "https://github.com/tree-sitter-grammars/tree-sitter-query"
 source = [
     f"{url}/archive/refs/tags/v{pkgver}.tar.gz",
-    "https://github.com/nvim-treesitter/nvim-treesitter/archive/30de5e7e9486fb1b1b8c2a1e71052b13f94f1cb0.tar.gz",
-    "https://github.com/nvim-treesitter/nvim-treesitter-textobjects/archive/5f9bf4b1ead7707e4e74e5319ee56bdc81fb73db.tar.gz",
+    "https://github.com/nvim-treesitter/nvim-treesitter/archive/9866036ec3c5db40700a9178494e0cfdcfe6ecfd.tar.gz",
+    "https://github.com/nvim-treesitter/nvim-treesitter-textobjects/archive/71385f191ec06ffc60e80e6b0c9a9d5daed4824c.tar.gz",
 ]
 source_paths = [
     ".",
@@ -21,9 +21,9 @@ source_paths = [
     ".tests/nvim-treesitter-textobjects",
 ]
 sha256 = [
-    "18e158092789e4391668760e9ad1b5b4135c8f483d29163912d29e5619518d40",
-    "f18e3705a55bc36ca43c8a8f64dcecf1fa04c9995046e2ed25639ee8c5e8f3c9",
-    "73dcba69ae5a005451a95f2155ceaefc3e32c7bcda3f41b35d69f97f234a3198",
+    "90682e128d048fbf2a2a17edca947db71e326fa0b3dba4136e041e096538b4eb",
+    "e5d345447a560d50e8e926a657c772060b17665cf34ba296d413af46e3411c00",
+    "ff6435187774f11f846420de3a982d754c105c86cbab0cb1bd76384eb209bbfd",
 ]
 
 
