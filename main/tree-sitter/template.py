24c40f2557f516ad7030b3cbc3b228b39fb413eb
Ayush Agarwal <ayush@ayushnix.com>
2025-07-21 20:29:25 +0530

    main/tree-sitter: update to 0.25.8

--- main/tree-sitter/template.py
+++ main/tree-sitter/template.py
@@ -1,6 +1,6 @@
 pkgname = "tree-sitter"
 # match to tree-sitter-cli
-pkgver = "0.25.4"
+pkgver = "0.25.8"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["pkgconf"]
@@ -8,7 +8,7 @@ pkgdesc = "Incremental parsing library for language grammars"
 license = "MIT"
 url = "https://tree-sitter.github.io/tree-sitter"
 source = f"https://github.com/tree-sitter/tree-sitter/archive/v{pkgver}.tar.gz"
-sha256 = "87eadc505905c70a692917c821958a819903f808f8d244068b1d273a033dc728"
+sha256 = "178b575244d967f4920a4642408dc4edf6de96948d37d7f06e5b78acee9c0b4e"
 # check requires cargo/fixture stuff (from remote repositories)
 options = ["!check"]
 
