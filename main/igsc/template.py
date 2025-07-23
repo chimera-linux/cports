50261572fe067b68f4d04626aab8e64f235ee441
triallax <triallax@tutanota.com>
2025-07-24 00:56:52 +0100

    main/igsc: update to 0.9.6

--- main/igsc/template.py
+++ main/igsc/template.py
@@ -1,6 +1,6 @@
 pkgname = "igsc"
-pkgver = "0.9.5"
-pkgrel = 3
+pkgver = "0.9.6"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DSYSLOG=OFF", "-DENABLE_WERROR=OFF"]
 hostmakedepends = [
@@ -16,7 +16,7 @@ pkgdesc = "Intel graphics system controller firmware update library"
 license = "Apache-2.0"
 url = "https://github.com/intel/igsc"
 source = f"{url}/archive/refs/tags/V{pkgver}.tar.gz"
-sha256 = "516a099239ad0541598be87025556c878c0d11c6d43e09115d2bb2e24508b6ec"
+sha256 = "2c6440cb459ca897dc84e405f32a7e120c9fa0eb9038ea04319cc929ef58ec5c"
 
 
 @subpackage("igsc-devel")
