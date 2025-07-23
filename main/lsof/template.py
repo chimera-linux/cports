8d457f3f509984328fed10e1259bc0252e9008a3
triallax <triallax@tutanota.com>
2025-07-23 21:55:42 +0100

    main/lsof: update to 4.99.5

--- main/lsof/template.py
+++ main/lsof/template.py
@@ -1,5 +1,5 @@
 pkgname = "lsof"
-pkgver = "4.99.4"
+pkgver = "4.99.5"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -14,7 +14,7 @@ pkgdesc = "List open files"
 license = "custom:lsof"
 url = "https://lsof.readthedocs.io/en/latest"
 source = f"https://github.com/lsof-org/lsof/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "90d02ae35cd14341bfb04ce80e0030767476b0fc414a0acb115d49e79b13d56c"
+sha256 = "3c591556c665196e0aada5982ff43c75e248187bad78bb1368d8fb9c1c527e6e"
 hardening = ["vis", "!cfi"]
 # FIXME: weird failures
 options = ["!check"]
