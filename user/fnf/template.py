251394e2a73bc8e9578dde9797335a3ba7df8f40
unrealapex <unrealapex@disroot.org>
2025-07-23 20:57:50 -0700

    user/fnf: update to 0.3.1

--- user/fnf/template.py
+++ user/fnf/template.py
@@ -1,12 +1,13 @@
 pkgname = "fnf"
-pkgver = "0.2"
-pkgrel = 1
+pkgver = "0.3.1"
+pkgrel = 0
 build_style = "makefile"
+makedepends = ["linux-headers"]
 pkgdesc = "CLI fuzzy finder"
 license = "MIT"
 url = "https://github.com/leo-arch/fnf"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "2b31b19d2eb71d20854ec34f26ae1258c22b7cba5b634fd85728c0a5f4e6dd68"
+sha256 = "3a65de45f68419528e5fa57d9857aa208f4802ba85de267a09734d7231b6d1eb"
 
 
 def post_install(self):
