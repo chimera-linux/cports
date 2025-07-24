07e25c0eac1bcdb2c08d5677913f31b44962f552
ItsHarper <10224994+ItsHarper@users.noreply.github.com>
2025-07-24 01:06:52 -0500

    user/nushell: update to 0.106.1

--- user/nushell/template.py
+++ user/nushell/template.py
@@ -1,5 +1,5 @@
 pkgname = "nushell"
-pkgver = "0.105.1"
+pkgver = "0.106.1"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = [
@@ -9,8 +9,6 @@ make_build_args = [
 make_install_args = [*make_build_args]
 make_check_args = [
     "--",
-    "--skip=shell::environment::env::env_shlvl_in_exec_repl",
-    "--skip=shell::environment::env::env_shlvl_in_repl",
     "--skip=shell::environment::env::path_is_a_list_in_repl",
 ]
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -25,8 +23,7 @@ pkgdesc = "Shell with a focus on structured data"
 license = "MIT"
 url = "https://www.nushell.sh"
 source = f"https://github.com/nushell/nushell/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "2c52ef5aef2ba1a3ae873e84bf72b52220f47c8fe47b99950b791e678a43d597"
-
+sha256 = "3e24044c354d050a850b69dc77c99cc503542c3d9d75fed0aef1c12fefdf380b"
 
 def post_install(self):
     self.install_shell("/usr/bin/nu")
