c85b3b79d33ad01662932614618dd68e8d2966a0
Ayush Agarwal <ayush@ayushnix.com>
2025-07-27 12:36:12 +0530

    user/selene: update to 0.29.0

--- user/selene/template.py
+++ user/selene/template.py
@@ -1,6 +1,6 @@
 pkgname = "selene"
-pkgver = "0.28.0"
-pkgrel = 1
+pkgver = "0.29.0"
+pkgrel = 0
 build_style = "cargo"
 prepare_after_patch = True
 hostmakedepends = ["cargo-auditable"]
@@ -9,7 +9,7 @@ pkgdesc = "Lua linter"
 license = "MPL-2.0"
 url = "https://github.com/Kampfkarren/selene"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "c51acf52e7c3136cd0b67b9a39a4a447c8f0257371b2b2acc7e77587260a377b"
+sha256 = "9882007e7b2d16023cd2c69d64d72afbee65dce7c3ab44a1527f5318667ed2a1"
 
 
 def pre_prepare(self):
