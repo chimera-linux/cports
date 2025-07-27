b7b96b28c914f25f6afb737dcc1dd78b3dddf4e2
triallax <triallax@tutanota.com>
2025-07-27 16:23:50 +0100

    main/xeyes: update to 1.3.1

--- main/xeyes/template.py
+++ main/xeyes/template.py
@@ -1,6 +1,6 @@
 pkgname = "xeyes"
-pkgver = "1.3.0"
-pkgrel = 1
+pkgver = "1.3.1"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf", "xorg-util-macros"]
 makedepends = ["libxmu-devel", "libxt-devel", "libxrender-devel", "libxi-devel"]
@@ -8,7 +8,7 @@ pkgdesc = "Follow the mouse"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/xeyes-{pkgver}.tar.gz"
-sha256 = "e2f0ab2fd48d12444e42e537e235dc6141bb1750f2206140fe380fda6d7e1196"
+sha256 = "d223283270488b1a6b408d0e97bd69a7704cf6a9596c0320035d807e63cdc084"
 hardening = ["vis", "cfi"]
 
 
