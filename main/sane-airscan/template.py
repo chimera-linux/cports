f458bc961b3f712e7caeefd2ea85f69c4761b83d
triallax <triallax@tutanota.com>
2025-07-27 16:54:32 +0100

    main/sane-airscan: update to 0.99.36

--- main/sane-airscan/template.py
+++ main/sane-airscan/template.py
@@ -1,5 +1,5 @@
 pkgname = "sane-airscan"
-pkgver = "0.99.35"
+pkgver = "0.99.36"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -20,4 +20,4 @@ pkgdesc = "SANE backend for AirScan (eSCL) and WSD document scanners"
 license = "GPL-2.0-or-later WITH SANE-exception"
 url = "https://github.com/alexpevzner/sane-airscan"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "c947246c0e3bf0e2c79b3d795a8b59f0159d4ac81650ba8328ba8d49545eb308"
+sha256 = "43d3436c0199496ee18aca4f875fe3926a40a0fae781bc280cdb96f7b5068ac0"
