38e66a4aa1234c1162369c4d6bfa9da330713133
ttyyls <contact@behri.org>
2025-07-28 11:20:14 +0100

    user/dnscontrol: update to 4.22.0

--- user/dnscontrol/template.py
+++ user/dnscontrol/template.py
@@ -1,5 +1,5 @@
 pkgname = "dnscontrol"
-pkgver = "4.21.0"
+pkgver = "4.22.0"
 pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
@@ -7,7 +7,7 @@ pkgdesc = "System for maintaining DNS zones"
 license = "MIT"
 url = "https://dnscontrol.org"
 source = f"https://github.com/StackExchange/dnscontrol/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "fb28227d0b94e0fa645989ea811dde9ec1ba272fb8ed5b1011788a710ffde3a8"
+sha256 = "3fac9a6e229d2c8d74f4b398f6d8bc2753df613a4d92010cede222333a295551"
 # tests rely on network
 # generates completions with host bins
 options = ["!check", "!cross"]
