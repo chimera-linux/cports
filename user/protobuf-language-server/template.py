7a6311f5927fa288b63a36b6158b6f9a120f2bfc
ttyyls <contact@behri.org>
2025-07-28 10:46:16 +0100

    user/protobuf-language-server: new package

---
+++ user/protobuf-language-server/template.py
@@ -0,0 +1,10 @@
+pkgname = "protobuf-language-server"
+pkgver = "0.1.1"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go"]
+pkgdesc = "Language server for protobuf"
+license = "Apache-2.0"
+url = "https://github.com/lasorda/protobuf-language-server"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "661d1b6b154ded725955c389b60ff757b9b42b619e9036b133507da973ab310c"
