ca679ee547d19529dbe7faf8fc1869dc7bf9b7f0
ItsHarper <10224994+ItsHarper@users.noreply.github.com>
2025-07-29 20:37:55 -0500

    user/nushell: include core plugins as subpackages
    Co-authored-by: Jan Christian Grünhage <jan.christian@gruenhage.xyz>

--- user/nushell/template.py
+++ user/nushell/template.py
@@ -5,8 +5,8 @@ build_style = "cargo"
 make_build_args = [
     "--no-default-features",
     "--features=plugin,trash-support,sqlite,native-tls",
+    "--workspace",
 ]
-make_install_args = [*make_build_args]
 make_check_args = [
     "--",
     "--skip=shell::environment::env::path_is_a_list_in_repl",
@@ -24,7 +24,46 @@ license = "MIT"
 url = "https://www.nushell.sh"
 source = f"https://github.com/nushell/nushell/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "3e24044c354d050a850b69dc77c99cc503542c3d9d75fed0aef1c12fefdf380b"
+_plugins = [
+    "polars",
+    "formats",
+    "gstat",
+    "query",
+    "inc",
+]
+
+
+def install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/nu")
+    nu_autoload_path = "usr/share/nushell/vendor/autoload"
+    self.install_dir(nu_autoload_path)
+    for _plugin in _plugins:
+        self.install_bin(
+            f"target/{self.profile().triplet}/release/nu_plugin_{_plugin}"
+        )
+        with open(
+            self.destdir / nu_autoload_path / f"enable_plugin_{_plugin}.nu", "w"
+        ) as ofile:
+            ofile.write(f"plugin add /usr/bin/nu_plugin_{_plugin}\n")
+
 
 def post_install(self):
     self.install_shell("/usr/bin/nu")
     self.install_license("LICENSE")
+
+
+def _genmod(pname):
+    @subpackage(f"nushell-plugin-{pname}")
+    def _(self):
+        self.subdesc = f"{pname} plugin"
+        # The scripts enabling the nushell plugins automatically are no completions
+        # and make no sense in a `-nucomp` package
+        self.options = ["!autosplit"]
+        return [
+            f"usr/bin/nu_plugin_{pname}",
+            f"usr/share/nushell/vendor/autoload/enable_plugin_{pname}.nu",
+        ]
+
+
+for _plugin in _plugins:
+    _genmod(_plugin)
