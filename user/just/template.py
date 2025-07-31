cb2cc5f4fcdfbea6de3073030b208544e454b2f6
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-07-31 22:12:33 +0200

    user/just: update to 1.42.4

--- user/just/template.py
+++ user/just/template.py
@@ -1,5 +1,5 @@
 pkgname = "just"
-pkgver = "1.41.0"
+pkgver = "1.42.4"
 pkgrel = 0
 build_style = "cargo"
 # skip tests that fail when run outside of git repo
@@ -10,19 +10,30 @@ checkdepends = ["bash", "python"]
 pkgdesc = "Save and run commands from justfile"
 license = "CC0-1.0"
 url = "https://github.com/casey/just"
-source = [
-    f"{url}/archive/{pkgver}.tar.gz",
-    f"{url}/releases/download/{pkgver}/just-{pkgver}-x86_64-unknown-linux-musl.tar.gz",
-]
-source_paths = [".", "docs-prebuilt"]
-sha256 = [
-    "4ab64ebeaf7d6cf90d2824fddb91f7a3a4cfbb5d016e99cc5039ded475c8a244",
-    "9d794c80727b28f549e9237ec0c01870794a36afeadea0864b5c2dbb32dd1fdb",
-]
+source = f"{url}/archive/{pkgver}.tar.gz"
+sha256 = "bd604ff72ecd8d8def79d39997499433e22fbffa03260e3a2c5fe5f84cc37f52"
+# generates completions and man page with host binary
+options = ["!cross"]
+
+
+def post_build(self):
+    for shell in ["bash", "fish", "zsh", "nushell"]:
+        with open(self.cwd / f"just.{shell}", "w") as f:
+            self.do(
+                f"./target/{self.profile().triplet}/release/just",
+                "--completions",
+                shell,
+                stdout=f,
+            )
+    with open(self.cwd / "just.1", "w") as f:
+        self.do(
+            f"./target/{self.profile().triplet}/release/just",
+            "--man",
+            stdout=f,
+        )
 
 
 def post_install(self):
-    self.install_man("docs-prebuilt/just.1")
-    self.install_completion("docs-prebuilt/completions/just.bash", "bash")
-    self.install_completion("docs-prebuilt/completions/just.zsh", "zsh")
-    self.install_completion("docs-prebuilt/completions/just.fish", "fish")
+    self.install_man("just.1")
+    for shell in ["bash", "fish", "zsh", "nushell"]:
+        self.install_completion(f"just.{shell}", shell)
