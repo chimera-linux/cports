daa17bc8e6a4fbd66534ef679303871996a89207
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-07-31 17:30:00 +0200

    user/cargo-edit: update to 0.13.6

--- user/cargo-edit/template.py
+++ user/cargo-edit/template.py
@@ -1,5 +1,5 @@
 pkgname = "cargo-edit"
-pkgver = "0.13.3"
+pkgver = "0.13.6"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -8,7 +8,7 @@ pkgdesc = "CLI utility for managing cargo dependencies"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/killercup/cargo-edit"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "df53ad2288cb9f9ee3ebc0eea389ec14e4e0fbf9cdefda75e5b0eedd0a550be1"
+sha256 = "325975345522decc9089635bb19b61c30942254a34b570925049fb56672d400d"
 # Checks don't work with our cargo config overrides
 options = ["!check"]
 
