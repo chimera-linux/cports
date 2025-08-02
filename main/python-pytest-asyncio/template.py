a2ea3d7f57e08a4cdc2f01c3eb2fbdb92c022a54
triallax <triallax@tutanota.com>
2025-08-02 15:57:19 +0100

    main/python-pytest-asyncio: update to 1.1.0

--- main/python-pytest-asyncio/template.py
+++ main/python-pytest-asyncio/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pytest-asyncio"
-pkgver = "1.0.0"
+pkgver = "1.1.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,4 +13,4 @@ pkgdesc = "Asyncio support for pytest"
 license = "Apache-2.0"
 url = "https://github.com/pytest-dev/pytest-asyncio"
 source = f"$(PYPI_SITE)/p/pytest-asyncio/pytest_asyncio-{pkgver}.tar.gz"
-sha256 = "d15463d13f4456e1ead2594520216b225a16f781e144f8fdf6c5bb4667c48b3f"
+sha256 = "796aa822981e01b68c12e4827b8697108f7205020f24b5793b3c41555dab68ea"
