063e3c14198df8a0c2dd2e7468a5880d95730d3b
triallax <triallax@tutanota.com>
2025-08-02 14:33:09 +0100

    user/libnetfilter_queue: fix ppc64* build

--- user/libnetfilter_queue/template.py
+++ user/libnetfilter_queue/template.py
@@ -17,6 +17,12 @@ license = "GPL-2.0-or-later"
 url = "https://netfilter.org/projects/libnetfilter_queue"
 source = f"{url}/files/libnetfilter_queue-{pkgver}.tar.bz2"
 sha256 = "f9ff3c11305d6e03d81405957bdc11aea18e0d315c3e3f48da53a24ba251b9f5"
+# modified below
+options = []
+
+if self.profile().arch in ["ppc64", "ppc64le"]:
+    # ld: error: relocation R_PPC64_REL16_LO cannot be used against symbol '.TOC.'; recompile with -fPIC
+    options += ["!lto"]
 
 
 @subpackage("libnetfilter_queue-devel")
