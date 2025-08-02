a4710f915c58784a67b9c216da08c99a96730a9d
Duncan Bellamy <dunk@denkimushi.com>
2025-08-02 17:17:13 +0000

    main/python-josepy: update to 2.1.0

--- main/python-josepy/template.py
+++ main/python-josepy/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-josepy"
-pkgver = "2.0.0"
+pkgver = "2.1.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,4 +13,4 @@ pkgdesc = "JOSE protocol implementation in Python"
 license = "Apache-2.0"
 url = "https://josepy.readthedocs.io/en/latest"
 source = f"$(PYPI_SITE)/j/josepy/josepy-{pkgver}.tar.gz"
-sha256 = "e7d7acd2fe77435cda76092abe4950bb47b597243a8fb733088615fa6de9ec40"
+sha256 = "9beafbaa107ec7128e6c21d86b2bc2aea2f590158e50aca972dca3753046091f"
