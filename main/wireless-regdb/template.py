9b15d24e0c49fbdad1f14b7d60825c10077f585a
triallax <triallax@tutanota.com>
2025-08-04 20:16:55 +0100

    main/wireless-regdb: update to 2025.07.10

--- main/wireless-regdb/template.py
+++ main/wireless-regdb/template.py
@@ -1,5 +1,5 @@
 pkgname = "wireless-regdb"
-pkgver = "2025.02.20"
+pkgver = "2025.07.10"
 pkgrel = 0
 build_style = "makefile"
 make_install_args = ["FIRMWARE_PATH=/usr/lib/firmware"]
@@ -7,7 +7,7 @@ pkgdesc = "Wireless CRDA database"
 license = "ISC"
 url = "https://wireless.wiki.kernel.org/en/developers/regulatory/wireless-regdb"
 source = f"https://mirrors.edge.kernel.org/pub/software/network/wireless-regdb/wireless-regdb-{pkgver}.tar.xz"
-sha256 = "57f8e7721cf5a880c13ae0c202edbb21092a060d45f9e9c59bcd2a8272bfa456"
+sha256 = "a8340bcdcd1b5db6c79149879d122b170f3bb075381718d4f429ad831a6fa28d"
 # just files
 options = ["!check"]
 
