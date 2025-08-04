ef43abb64b808eae5a6c164abb2e772a3ee821ad
triallax <triallax@tutanota.com>
2025-08-04 23:38:20 +0100

    main/geany-plugins: update to 2.1.0

--- main/geany-plugins/template.py
+++ main/geany-plugins/template.py
@@ -1,11 +1,9 @@
 pkgname = "geany-plugins"
-pkgver = "2.0.0"
-pkgrel = 6
+pkgver = "2.1.0"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--enable-all-plugins",
-    "--disable-geniuspaste",  # libsoup 2.x
-    "--disable-updatechecker",  # libsoup 2.x
 ]
 hostmakedepends = [
     "automake",
@@ -21,6 +19,7 @@ makedepends = [
     "geany-devel",
     "gpgme-devel",
     "libgit2-devel",
+    "libsoup-devel",
     "lua5.1-devel",
     "vte-gtk3-devel",
     "webkitgtk-devel",
@@ -30,5 +29,8 @@ depends = [f"geany~{pkgver}"]
 pkgdesc = "Geany IDE plugins"
 license = "GPL-2.0-or-later"
 url = "https://geany.org"
-source = f"https://github.com/geany/geany-plugins/releases/download/{pkgver}/geany-plugins-{pkgver[:-2]}.tar.gz"
-sha256 = "cd7d27f00aef4afe2040d7e5246a863234c340c8520ef698be9a15005ed8f57e"
+# source = f"https://github.com/geany/geany-plugins/releases/download/{pkgver}/geany-plugins-{pkgver[:-2]}.tar.gz"
+source = (
+    f"https://github.com/geany/geany-plugins/archive/refs/tags/{pkgver}.tar.gz"
+)
+sha256 = "9ca8412763c2f8a7141f6a1569166f4fabf95fc8aad5149a754265673ffce5bb"
