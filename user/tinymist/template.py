b9b77a033ea3dc02c1060272ca8feb60125830a2
ttyyls <contact@behri.org>
2025-08-04 13:40:22 +0100

    user/tinymist: update to 0.13.18

--- user/tinymist/template.py
+++ user/tinymist/template.py
@@ -1,5 +1,5 @@
 pkgname = "tinymist"
-pkgver = "0.12.20"
+pkgver = "0.13.18"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = ["--features=cli", "--bin", "tinymist", "--bin", "typlite"]
@@ -10,7 +10,7 @@ pkgdesc = "Language server for Typst"
 license = "Apache-2.0"
 url = "https://myriad-dreamin.github.io/tinymist"
 source = f"https://github.com/Myriad-Dreamin/tinymist/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5af73d3793d2c243f4e758951c5c62e39b539ba3a9ebe4b1af2d712de99e8f03"
+sha256 = "84ff7b3812dbe9c57a6cfb2844aea3c61282f94514767e956a1c3c65d1d66dac"
 # check: takes forever
 options = ["!check", "!cross"]
 
