7a44554541086e18fbce9d3e835dd8fe14be1c29
Erin John <unrealapex@disroot.org>
2025-08-05 13:50:45 -0700

    main/doasedit: use makefile to install

--- main/doasedit/template.py
+++ main/doasedit/template.py
@@ -1,14 +1,17 @@
 pkgname = "doasedit"
 pkgver = "1.0.8"
 pkgrel = 0
+build_style = "makefile"
+make_install_args = ["prefix=/usr"]
 depends = ["opendoas"]
 pkgdesc = "Shell script to edit files with doas"
 license = "MIT"
 url = "https://codeberg.org/TotallyLeGIT/doasedit"
 source = f"https://codeberg.org/TotallyLeGIT/doasedit/archive/{pkgver}.tar.gz"
 sha256 = "de7dfc450568d14f1ff473aa4a50a0d0505e188e117cc12cf3a2b0ea5da8af10"
+# no tests
+options = ["!check"]
 
 
-def install(self):
-    self.install_bin("doasedit")
+def post_install(self):
     self.install_license("LICENSE")
