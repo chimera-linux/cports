555741c953626a5aa1fad433bb38e0cd17e5b4df
triallax <triallax@tutanota.com>
2025-08-06 09:13:14 +0100

    main/python-pebble: update to 5.1.3

--- main/python-pebble/template.py
+++ main/python-pebble/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pebble"
-pkgver = "5.1.1"
+pkgver = "5.1.3"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -16,4 +16,4 @@ pkgdesc = "Multi-threading and processing for python"
 license = "LGPL-3.0-or-later"
 url = "https://github.com/noxdafox/pebble"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "8e91255f28641da9b35c906c8dda6b0a5dd57c9adedf0271cee50312708aa03a"
+sha256 = "8304af95d4d43d73c4f648403114be8cefcb9527d27488bb9e974ea6cde2661a"
