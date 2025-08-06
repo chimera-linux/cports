f936d938f35c8ca10ebfb59123628699ca291a2a
triallax <triallax@tutanota.com>
2025-08-06 09:12:00 +0100

    main/mediainfo: update to 25.07

--- main/mediainfo/template.py
+++ main/mediainfo/template.py
@@ -1,5 +1,5 @@
 pkgname = "mediainfo"
-pkgver = "25.04"
+pkgver = "25.07"
 pkgrel = 0
 build_wrksrc = "Project/GNU/CLI"
 build_style = "gnu_configure"
@@ -10,7 +10,7 @@ pkgdesc = "Display information about media files"
 license = "BSD-2-Clause"
 url = "https://mediaarea.net/en/MediaInfo"
 source = f"https://mediaarea.net/download/source/mediainfo/{pkgver}/mediainfo_{pkgver}.tar.bz2"
-sha256 = "157f7a2b0b6295ec2a411478d048a4431484a21abb028af901d5fc62f210518e"
+sha256 = "e1b72bba16b23e5a83693bcbedd5104c87671490a634d8c3a8b12638f547c06f"
 hardening = ["vis", "cfi"]
 
 
