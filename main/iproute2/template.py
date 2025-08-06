6bd082e41d78fbbd458505e6a644755be22f50ae
triallax <triallax@tutanota.com>
2025-08-06 09:26:33 +0100

    main/iproute2: update to 6.16.0

--- main/iproute2/template.py
+++ main/iproute2/template.py
@@ -1,5 +1,5 @@
 pkgname = "iproute2"
-pkgver = "6.15.0"
+pkgver = "6.16.0"
 pkgrel = 0
 build_style = "configure"
 configure_args = ["--color", "auto"]
@@ -23,7 +23,7 @@ pkgdesc = "IP routing utilities"
 license = "GPL-2.0-only"
 url = "https://wiki.linuxfoundation.org/networking/iproute2"
 source = f"$(KERNEL_SITE)/utils/net/iproute2/iproute2-{pkgver}.tar.xz"
-sha256 = "8041854a882583ad5263466736c9c8c68c74b1a35754ab770d23343f947528fb"
+sha256 = "5900ccc15f9ac3bf7b7eae81deb5937123df35e99347a7f11a22818482f0a8d0"
 # causes some part of the build to silently break which drops support for various features
 hardening = ["!vis"]
 
