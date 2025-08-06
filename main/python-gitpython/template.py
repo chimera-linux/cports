0a85f4fa2ee8521c5fe1ebf121522f5fe8a57fda
triallax <triallax@tutanota.com>
2025-08-06 08:44:25 +0100

    main/python-gitpython: update to 3.1.45

--- main/python-gitpython/template.py
+++ main/python-gitpython/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-gitpython"
-pkgver = "3.1.44"
-pkgrel = 1
+pkgver = "3.1.45"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -12,7 +12,7 @@ pkgdesc = "Python library to interact with Git repos"
 license = "BSD-3-Clause"
 url = "https://gitpython.readthedocs.org"
 source = f"$(PYPI_SITE)/g/GitPython/gitpython-{pkgver}.tar.gz"
-sha256 = "c87e30b26253bf5418b01b0660f818967f3c503193838337fe5e573331249269"
+sha256 = "85b0ee964ceddf211c41b9f27a49086010a190fd8132a24e21f362a4b36a791c"
 # TODO
 options = ["!check"]
 
