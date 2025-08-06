060df244f0ad17ca38f48e0f9f5f93c7fe942a8c
triallax <triallax@tutanota.com>
2025-08-06 04:32:40 +0100

    main/libheif: update to 1.20.2

--- main/libheif/template.py
+++ main/libheif/template.py
@@ -1,5 +1,5 @@
 pkgname = "libheif"
-pkgver = "1.20.1"
+pkgver = "1.20.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -24,7 +24,7 @@ pkgdesc = "HEIF and AVIF file format decoder and encoder"
 license = "LGPL-3.0-or-later"
 url = "http://www.libheif.org"
 source = f"https://github.com/strukturag/libheif/archive/v{pkgver}.tar.gz"
-sha256 = "9d3d601ec7a55281217aaa6c773cf6645757b062bc7e9680b664bbd8e481112d"
+sha256 = "b70340395d84184bb8dfc833dd51c95ae049435f7ff9abc7b505a08b5ee2bd2a"
 hardening = ["!vis", "!cfi"]
 
 
@@ -39,8 +39,7 @@ def _(self):
     self.install_if = [self.parent]
     self.renames = ["heif-thumbnailer"]
     return [
-        "usr/bin/heif-thumbnailer",
-        "usr/share/man/man1/heif-thumbnailer.1",
+        "cmd:heif-thumbnailer",
         "usr/share/thumbnailers",
     ]
 
