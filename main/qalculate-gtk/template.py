a5368b19f33ccd0c3e19e05d5a464c8eaaafa9db
triallax <triallax@tutanota.com>
2025-08-06 01:56:16 +0100

    main/qalculate-gtk: update to 5.7.0

--- main/qalculate-gtk/template.py
+++ main/qalculate-gtk/template.py
@@ -1,6 +1,6 @@
 pkgname = "qalculate-gtk"
 # match to libqalculate
-pkgver = "5.6.0"
+pkgver = "5.7.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -19,4 +19,4 @@ pkgdesc = "GTK+3 frontend for libqalculate"
 license = "GPL-2.0-or-later"
 url = "https://qalculate.github.io"
 source = f"https://github.com/Qalculate/qalculate-gtk/releases/download/v{pkgver}/qalculate-gtk-{pkgver}.tar.gz"
-sha256 = "3dabd3b1ed981222b7fe7d2c14d47926715b8ae13098b0a4bdaa9dce96eee36d"
+sha256 = "dcb3663b36abafdfe32e943644bf4fc64bd685c0225f944a3f1c4a85e70db3b5"
