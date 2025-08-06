5e058dcddad778535a328455b6e05f78632ef365
triallax <triallax@tutanota.com>
2025-08-06 03:26:19 +0100

    main/libdispatch: update to 6.1.2
    no changes but just get it done with anyway

--- main/libdispatch/template.py
+++ main/libdispatch/template.py
@@ -1,5 +1,5 @@
 pkgname = "libdispatch"
-pkgver = "6.1"
+pkgver = "6.1.2"
 pkgrel = 0
 build_style = "cmake"
 # these always fail on linux for some reason on musl
@@ -16,7 +16,7 @@ pkgdesc = "Apple's concurrent threading library"
 license = "Apache-2.0"
 url = "https://apple.github.io/swift-corelibs-libdispatch"
 source = f"https://github.com/apple/swift-corelibs-libdispatch/archive/refs/tags/swift-{pkgver}-RELEASE.tar.gz"
-sha256 = "5bba8d7442890f7dbd37a9245340c5bb0c4c924dee6180ba30385b24e3fdf121"
+sha256 = "26e8f6d661415502c10f909835961cac4edf56a0ab9512a9988489fe98601385"
 hardening = ["vis", "!cfi"]
 
 
