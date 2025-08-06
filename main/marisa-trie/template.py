51a8d4915c6497ca6c2c56ea7c4119f2097ec062
triallax <triallax@tutanota.com>
2025-08-06 02:39:18 +0100

    main/marisa-trie: update to 0.3.1

--- main/marisa-trie/template.py
+++ main/marisa-trie/template.py
@@ -1,6 +1,6 @@
 pkgname = "marisa-trie"
 # match to python-marisa
-pkgver = "0.3.0"
+pkgver = "0.3.1"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -16,7 +16,7 @@ pkgdesc = "Matching algorithm with recursively implemented storage"
 license = "BSD-2-Clause OR LGPL-2.1-or-later"
 url = "https://github.com/s-yata/marisa-trie"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "a3057d0c2da0a9a57f43eb8e07b73715bc5ff053467ee8349844d01da91b5efb"
+sha256 = "986ed5e2967435e3a3932a8c95980993ae5a196111e377721f0849cad4e807f3"
 
 
 def post_install(self):
