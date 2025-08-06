91b1bde1031f79c4a2ddb9463cd7edebb5fc6a07
triallax <triallax@tutanota.com>
2025-08-06 09:16:39 +0100

    main/pv: update to 1.9.34

--- main/pv/template.py
+++ main/pv/template.py
@@ -1,5 +1,5 @@
 pkgname = "pv"
-pkgver = "1.9.31"
+pkgver = "1.9.34"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "gettext-devel"]
@@ -7,7 +7,7 @@ pkgdesc = "Tool for monitoring the progress of data through a pipeline"
 license = "GPL-3.0-or-later"
 url = "https://www.ivarch.com/programs/pv.shtml"
 source = f"https://www.ivarch.com/programs/sources/pv-{pkgver}.tar.gz"
-sha256 = "a35e92ec4ac0e8f380e8e840088167ae01014bfa008a3a9d6506b848079daedf"
+sha256 = "c0626bed6cbef5006b53d3281e8e3ae4b2838729462b21eccf28140eefef6bb1"
 
 
 def post_extract(self):
