2e7dbe7710e0ba6e350c7bc7449e4de853ac8ab5
triallax <triallax@tutanota.com>
2025-08-06 02:06:31 +0100

    main/libphonenumber: update to 9.0.11

--- main/libphonenumber/template.py
+++ main/libphonenumber/template.py
@@ -1,5 +1,5 @@
 pkgname = "libphonenumber"
-pkgver = "9.0.7"
+pkgver = "9.0.11"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -25,7 +25,7 @@ pkgdesc = "Library for parsing, formatting, and validating phone numbers"
 license = "Apache-2.0"
 url = "https://github.com/google/libphonenumber"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "15b929c24e9071847893cc2bbeb29631eab819ec6561baaf51250852773348b0"
+sha256 = "80a53c5da67c6240e15ca9cbb2cf263e9875fd37415464892b5cd1a00b1e2dba"
 
 
 @subpackage("libphonenumber-devel")
