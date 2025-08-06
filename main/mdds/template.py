9260390085161d32d7b4ac25e620fef797fb470a
triallax <triallax@tutanota.com>
2025-08-06 05:07:59 +0100

    main/mdds: update to 3.1.0

--- main/mdds/template.py
+++ main/mdds/template.py
@@ -1,6 +1,6 @@
 pkgname = "mdds"
-pkgver = "3.0.0"
-pkgrel = 1
+pkgver = "3.1.0"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "slibtool"]
 checkdepends = ["boost-devel"]
@@ -8,7 +8,7 @@ pkgdesc = "Collection of multi-dimensional data structures"
 license = "MIT"
 url = "https://gitlab.com/mdds/mdds"
 source = f"https://gitlab.com/api/v4/projects/mdds%2Fmdds/packages/generic/source/{pkgver}/mdds-{pkgver}.tar.gz"
-sha256 = "e8660f5510f099af1cc9bbcd2c1f12493b72b24f82616d70f9d71ab6ce1258ec"
+sha256 = "53fdc421b11fb7dda26591eace89b52eb0bdbbb8c8e6f05b6a66dfb55a29a4b6"
 
 
 def post_install(self):
