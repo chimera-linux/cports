062d25f3ed2dec82b652969930bfac2f978c7155
triallax <triallax@tutanota.com>
2025-08-06 20:05:38 +0100

    main/lsp-plugins: update to 1.2.22

--- main/lsp-plugins/template.py
+++ main/lsp-plugins/template.py
@@ -1,5 +1,5 @@
 pkgname = "lsp-plugins"
-pkgver = "1.2.21"
+pkgver = "1.2.22"
 pkgrel = 0
 build_style = "makefile"
 make_use_env = True
@@ -20,7 +20,7 @@ pkgdesc = "Collection of free audio plugins"
 license = "LGPL-3.0-or-later"
 url = "https://lsp-plug.in"
 source = f"https://github.com/sadko4u/lsp-plugins/releases/download/{pkgver}/lsp-plugins-src-{pkgver}.tar.gz"
-sha256 = "ae2da1d0557ed645371d54a779061240a0295e399c2aa441c96fa236d76e12d9"
+sha256 = "bb97270482b04c1269643a8373b554255dde287b1c5ddbc567ac94de4815a75b"
 hardening = ["vis", "!cfi"]
 # no tests
 # cross broken because of dumb uname arch detection
