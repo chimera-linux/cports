3dab272dd4c76ee06840e25ceff6c59240c205f9
triallax <triallax@tutanota.com>
2025-08-06 08:04:51 +0100

    main/python-docutils: update to 0.22

--- main/python-docutils/template.py
+++ main/python-docutils/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-docutils"
-pkgver = "0.21.2"
-pkgrel = 1
+pkgver = "0.22"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,7 +13,7 @@ pkgdesc = "Python documentation utilities"
 license = "custom:none AND BSD-2-Clause AND GPL-3.0-or-later AND Python-2.0"
 url = "http://docutils.sourceforge.net"
 source = f"$(PYPI_SITE)/d/docutils/docutils-{pkgver}.tar.gz"
-sha256 = "3a6b18732edf182daa3cd12775bbb338cf5691468f91eeeb109deff6ebfa986f"
+sha256 = "ba9d57750e92331ebe7c08a1bbf7a7f8143b86c476acd51528b042216a6aad0f"
 
 
 def check(self):
@@ -21,4 +21,4 @@ def check(self):
 
 
 def post_install(self):
-    self.install_license("COPYING.txt")
+    self.install_license("COPYING.rst")
