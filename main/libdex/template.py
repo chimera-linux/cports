c79fde6a09bd5dbab6f4a2a210767597ad8aedff
triallax <triallax@tutanota.com>
2025-08-06 03:12:53 +0100

    main/libdex: update to 0.10.1

--- main/libdex/template.py
+++ main/libdex/template.py
@@ -1,5 +1,5 @@
 pkgname = "libdex"
-pkgver = "0.10.0"
+pkgver = "0.10.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -24,7 +24,7 @@ pkgdesc = "Future-based programming for GLib-based applications"
 license = "LGPL-2.1-or-later AND BSD-3-Clause"
 url = "https://gitlab.gnome.org/GNOME/libdex"
 source = f"$(GNOME_SITE)/libdex/{'.'.join(pkgver.rsplit('.')[:-1])}/libdex-{pkgver}.tar.xz"
-sha256 = "98a69626aa7646ad455bea7a7f92d2a1ffa47e4559a154a1bfe98c16fa711ee1"
+sha256 = "7472e88090db2b228307505edeba4483e872681340cb0404ae64ac3da5bed0a6"
 # for liburing
 tool_flags = {
     "CFLAGS": ["-D_GNU_SOURCE"],
