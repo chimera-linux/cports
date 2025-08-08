43d87cd41624f06d4da1ff4021a33063f089f209
triallax <triallax@tutanota.com>
2025-08-08 04:12:49 +0100

    main/python-btrfs: update to 15

--- main/python-btrfs/template.py
+++ main/python-btrfs/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-btrfs"
-pkgver = "14.1"
-pkgrel = 2
+pkgver = "15"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,6 +13,6 @@ pkgdesc = "Python module and utilities for interacting with btrfs filesystems"
 license = "LGPL-3.0-or-later"
 url = "https://github.com/knorrie/python-btrfs"
 source = f"$(PYPI_SITE)/b/btrfs/btrfs-{pkgver}.tar.gz"
-sha256 = "04f28fc13df78bc7d060991465b9c9f2740d6e62b0d03aba7a46fd9abedac046"
+sha256 = "1419914ff141dfe9e16fd0477d9548d4bea733ecd77588e8cb7255ce17ada10b"
 # no tests
 options = ["!check"]
