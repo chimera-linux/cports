dcba119d04c55e27990fd4c6c92c26a024a05874
triallax <triallax@tutanota.com>
2025-08-08 03:11:12 +0100

    main/iwd: update to 3.9

--- main/iwd/template.py
+++ main/iwd/template.py
@@ -1,5 +1,5 @@
 pkgname = "iwd"
-pkgver = "3.8"
+pkgver = "3.9"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -22,7 +22,7 @@ pkgdesc = "Wireless daemon that replaces wpa_supplicant"
 license = "LGPL-2.1-or-later"
 url = "https://iwd.wiki.kernel.org"
 source = f"$(KERNEL_SITE)/network/wireless/iwd-{pkgver}.tar.xz"
-sha256 = "c556a5a5376270af68940e04e26765026fbbbe4941668317c274c91042611cdf"
+sha256 = "0cd7dc9b32b9d6809a4a5e5d063b5c5fd279f5ad3a0bf03d7799da66df5cad45"
 tool_flags = {
     "CFLAGS": ["-Wno-unknown-warning-option", "-Wno-duplicate-decl-specifier"]
 }
