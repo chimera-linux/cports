d87273e5af74c8df7301eefd92d047f1bef3eadb
triallax <triallax@tutanota.com>
2025-08-08 18:24:46 +0100

    main/python-trove-classifiers: update to 2025.8.6.13

--- main/python-trove-classifiers/template.py
+++ main/python-trove-classifiers/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-trove-classifiers"
-pkgver = "2025.5.9.12"
+pkgver = "2025.8.6.13"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -14,6 +14,6 @@ pkgdesc = "Canonical source for classifiers on PyPI"
 license = "Apache-2.0"
 url = "https://github.com/pypa/trove-classifiers"
 source = f"$(PYPI_SITE)/t/trove-classifiers/trove_classifiers-{pkgver}.tar.gz"
-sha256 = "7ca7c8a7a76e2cd314468c677c69d12cc2357711fcab4a60f87994c1589e5cb5"
+sha256 = "5a0abad839d2ed810f213ab133d555d267124ddea29f1d8a50d6eca12a50ae6e"
 # cycle
 options = ["!check"]
