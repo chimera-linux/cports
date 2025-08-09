5bfcd5cafcd318dbb03f66771705545c74d33bca
Gnarwhal <git.aspect893@passmail.net>
2025-08-09 04:50:28 +0000

    user/python-desktop-notifier: update to 6.2.0

--- user/python-desktop-notifier/template.py
+++ user/python-desktop-notifier/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-desktop-notifier"
-pkgver = "6.1.1"
+pkgver = "6.2.0"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_wrapper = ["dbus-run-session"]
@@ -14,7 +14,7 @@ pkgdesc = "Cross-platform desktop notification library"
 license = "MIT"
 url = "https://pypi.org/project/desktop-notifier"
 source = f"https://github.com/samschott/desktop-notifier/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "60b1eef6edb645ce48eb89fa8ef955e6c0aa34787808247587acc16c281fe871"
+sha256 = "8953ff5e6cb1415e8d770cd84f4b35d912d2f935711c59abc3a0317925a5dbe4"
 # no org.freedesktop.Notifications in chroot
 options = ["!check"]
 
