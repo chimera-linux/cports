a6e6e0824094c7281b870a49e7a05d2d2e229df4
triallax <triallax@tutanota.com>
2025-08-10 03:41:38 +0100

    user/thinkfan: move from main, update to 2.0.0

--- user/thinkfan/template.py
+++ user/thinkfan/template.py
@@ -1,15 +1,14 @@
 pkgname = "thinkfan"
-pkgver = "1.3.1"
-pkgrel = 2
+pkgver = "2.0.0"
+pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DCMAKE_POLICY_VERSION_MINIMUM=3.5"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
-makedepends = ["yaml-cpp-devel"]
+makedepends = ["yaml-cpp-devel", "lm-sensors-devel"]
 pkgdesc = "Simple fan control program"
 license = "GPL-3.0-or-later"
 url = "https://github.com/vmatare/thinkfan"
 source = f"https://github.com/vmatare/thinkfan/archive/{pkgver}.tar.gz"
-sha256 = "9466c8c82b7c4333b280fa66445ab26185ffbb4aada6bcb4a164eed742f8d78c"
+sha256 = "0fc94eb378dcba8c889e91f41dab3a8d6eebc7324a59a0704cc39aa66551987e"
 hardening = ["vis"]
 
 
