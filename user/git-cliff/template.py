3b341263302e993460b4b70cf30a06d1827e4334
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-08-10 17:54:08 +0200

    user/git-cliff: update to 2.10.0

--- user/git-cliff/template.py
+++ user/git-cliff/template.py
@@ -1,5 +1,5 @@
 pkgname = "git-cliff"
-pkgver = "2.9.1"
+pkgver = "2.10.0"
 pkgrel = 0
 build_style = "cargo"
 make_check_args = [
@@ -22,7 +22,7 @@ pkgdesc = "Changelog generator for conventional commits"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/orhun/git-cliff"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "dd5a707fba4549ab4ea5e9e95ea7ed302852eb1916b93a1a369019f005ad4cc4"
+sha256 = "5983409377bb34337ee930af53afb522737d87b9ba15096eae30ced3484b12a2"
 # generates manpages/completions with host bins
 options = ["!cross"]
 
