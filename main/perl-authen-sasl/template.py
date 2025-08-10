7825e4ec7b07885cee12bae78fdd4e826a5906dc
triallax <triallax@tutanota.com>
2025-08-10 04:48:23 +0100

    Revert "main/perl-authen-sasl: update to 2.1900"
    This reverts commit 11841abeec709d8801026034e0fbff326bd40771.
    
    i thought i tested this but i didn't...

--- main/perl-authen-sasl/template.py
+++ main/perl-authen-sasl/template.py
@@ -1,6 +1,6 @@
 pkgname = "perl-authen-sasl"
-pkgver = "2.1900"
-pkgrel = 0
+pkgver = "2.1800"
+pkgrel = 1
 build_style = "perl_module"
 hostmakedepends = ["perl"]
 makedepends = ["perl", "perl-digest-hmac"]
@@ -9,4 +9,4 @@ pkgdesc = "SASL authentication framework"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/release/Authen-SASL"
 source = f"$(CPAN_SITE)/Authen/Authen-SASL-{pkgver}.tar.gz"
-sha256 = "be3533a6891b2e677150b479c1a0d4bf11c8bbeebed3e7b8eba34053e93923b0"
+sha256 = "0b03686bddbbf7d5c6548e468d079a4051c9b73851df740ae28cfd2db234e922"
