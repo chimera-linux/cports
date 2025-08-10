0f261012adbe0593d1cc2a5dadaea9a6f2666641
triallax <triallax@tutanota.com>
2025-08-10 04:21:00 +0100

    main/sbc: update to 2.1

--- main/sbc/template.py
+++ main/sbc/template.py
@@ -1,5 +1,5 @@
 pkgname = "sbc"
-pkgver = "2.0"
+pkgver = "2.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-pie"]
@@ -9,7 +9,7 @@ pkgdesc = "Bluetooth Subband Codec (SBC) library"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "http://www.bluez.org"
 source = f"$(KERNEL_SITE)/bluetooth/sbc-{pkgver}.tar.xz"
-sha256 = "8f12368e1dbbf55e14536520473cfb338c84b392939cc9b64298360fd4a07992"
+sha256 = "426633cabd7c798236443516dfa8335b47e004b0ef37ff107e0c7ead3299fcc2"
 
 
 @subpackage("sbc-devel")
