562ecf7a489ca90c40b797572f191838957b5b88
triallax <triallax@tutanota.com>
2025-08-10 03:17:30 +0100

    main/xfce4-screensaver: update to 4.20.0, switch libexecdir

--- main/xfce4-screensaver/template.py
+++ main/xfce4-screensaver/template.py
@@ -1,13 +1,16 @@
 pkgname = "xfce4-screensaver"
-pkgver = "4.18.4"
-pkgrel = 1
+pkgver = "4.20.0"
+pkgrel = 0
 # workaround for lack of gdbus port
-_dbus_gmain_rev = "93e8fced640e29bd6fbcc066a6c854a8dd74f8ab"
+_dbus_gmain_rev = "623a4f3386a675060335e20afd810c6e10ae2cc8"
 build_style = "gnu_configure"
-configure_args = ["--with-xf86gamma-ext"]
+configure_args = [
+    "--libexecdir=/usr/lib",  # XXX drop libexec
+]
 hostmakedepends = [
     "automake",
     "gettext-devel",
+    "glib-devel",
     "pkgconf",
     "slibtool",
     "xfce4-dev-tools",
@@ -16,7 +19,6 @@ makedepends = [
     "dbus-devel",
     "elogind-devel",
     "garcon-devel",
-    "glib-devel",
     "gtk+3-devel",
     "libwnck-devel",
     "libx11-devel",
@@ -24,9 +26,7 @@ makedepends = [
     "libxfce4ui-devel",
     "libxfce4util-devel",
     "libxklavier-devel",
-    "libxrandr",
     "libxscrnsaver-devel",
-    "libxxf86vm-devel",
     "linux-pam-devel",
     "mesa-devel",
     "shadow-devel",
@@ -38,19 +38,19 @@ pkgdesc = "Xfce screensaver"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/apps/xfce4-screensaver/start"
 source = [
-    f"$(XFCE_SITE)/apps/xfce4-screensaver/{pkgver[:-2]}/xfce4-screensaver-{pkgver}.tar.bz2",
+    f"$(XFCE_SITE)/apps/xfce4-screensaver/{pkgver[:-2]}/xfce4-screensaver-{pkgver}.tar.xz",
     f"https://gitlab.freedesktop.org/dbus/dbus-glib/-/archive/{_dbus_gmain_rev}/dbus-glib-{_dbus_gmain_rev}.tar.gz",
 ]
 source_paths = [".", "dbus-gmain"]
 sha256 = [
-    "cf717d032d2d0555978c479299da992af6dc3363ae7e758af9515c7166eac170",
-    "b05a1cab9564d5490df3f92d564cec8582e82cc264130a071b0e0a8d9350ec18",
+    "e370298d002848fdb2065fee254cb5b0efa0f2699b74299c234019c8d79b852e",
+    "c1206c06fd625e864e06dbf43bab8543837ac35cbefa03457eda152cdf60c9ba",
 ]
 
 
 def post_extract(self):
     self.mkdir("src/dbus-gmain")
-    self.cp("dbus-gmain/dbus-gmain.c", "src")
+    self.cp("dbus-gmain/dbus-gmain/dbus-gmain.c", "src")
     self.cp("dbus-gmain/dbus-gmain/dbus-gmain.h", "src/dbus-gmain")
 
 
