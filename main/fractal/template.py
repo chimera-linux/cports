b4e7bdee404334b8f5617d7e9eb3a2b3e7e0caa2
triallax <triallax@tutanota.com>
2025-08-16 10:35:32 +0100

    main/fractal: update to 12

--- main/fractal/template.py
+++ main/fractal/template.py
@@ -1,5 +1,5 @@
 pkgname = "fractal"
-pkgver = "11.2"
+pkgver = "12"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -30,7 +30,7 @@ pkgdesc = "GTK Matrix client"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.gnome.org/World/fractal"
 source = f"{url}/-/archive/{pkgver}/fractal-{pkgver}.tar.gz"
-sha256 = "c79cf5d66d8c90f5e33b27867a43686d8ab46fc9a47c7816078c51821708af68"
+sha256 = "2f0700ccd4ac0c6b1d918647c8aaf869a3b031836fe4a81d9488cc31c0fd0b83"
 # check: has few actual tests, not worth a time-consuming cargo rebuild
 # debug: quite massive, CARGO_PROFILE_RELEASE_DEBUG=line-tables-only in
 # env makes it better but it's still ~260M
