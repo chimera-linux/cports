6c9352a237711f4c1fb572faa30334dd9197f644
Duncan Bellamy <dunk@denkimushi.com>
2025-08-17 21:24:24 +0000

    main/python-time-machine: update to 2.17.0

--- main/python-time-machine/template.py
+++ main/python-time-machine/template.py
@@ -1,7 +1,9 @@
 pkgname = "python-time-machine"
-pkgver = "2.16.0"
-pkgrel = 1
+pkgver = "2.17.0"
+pkgrel = 0
 build_style = "python_pep517"
+# missing tokenize-rt
+make_check_args = ["--ignore=tests/test_cli.py"]
 hostmakedepends = [
     "python-build",
     "python-devel",
@@ -14,7 +16,7 @@ pkgdesc = "Python library for mocking the current time"
 license = "MIT"
 url = "https://github.com/adamchainz/time-machine"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "d2ed8ebef04133d69bce09114bbf66be0d404d725597874a644318af6e0b3e28"
+sha256 = "1cd8965cdd841a24d5f8e9c35a49c56f15a02e273186300360cfec621c0c5ef6"
 
 
 def post_install(self):
