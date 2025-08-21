0dcb293bd3494268b7427299e9c229cfd3858bf9
BarryLhm <BarryLhm@outlook.com>
2025-08-21 22:43:35 +0800

    user/fonts-monaspace-otf: update to 1.300

--- user/fonts-monaspace-otf/template.py
+++ user/fonts-monaspace-otf/template.py
@@ -1,13 +1,19 @@
 pkgname = "fonts-monaspace-otf"
-pkgver = "1.200"
+pkgver = "1.300"
 pkgrel = 0
 pkgdesc = "GitHub Next Monaspace fonts"
 license = "OFL-1.1"
 url = "https://github.com/githubnext/monaspace"
-source = f"{url}/archive/refs/tags/v{pkgver}.zip"
-sha256 = "e72ae4dacfa7268ef75abca32fba01cc92ec187897d4deb99ecb843c088d3307"
+source = [
+    f"{url}/releases/download/v{pkgver}/monaspace-static-v{pkgver}.zip",
+    f"!{url}/blob/v{pkgver}/LICENSE?raw=true",
+]
+sha256 = [
+    "b857609c97dcf8c55e3fc408c6bf3e62b50b53122d6b1cc6813554e39cbebccc",
+    "0e84e5f7dd6f05e74a00f2fb828ca43e489d954f5509ff0fa439ea18c0d35fe9",
+]
 
 
 def install(self):
-    self.install_file("fonts/otf/*.otf", "usr/share/fonts/monaspace", glob=True)
-    self.install_license("LICENSE")
+    self.install_file("*/*.otf", "usr/share/fonts/monaspace", glob=True)
+    self.install_license(self.sources_path / "LICENSE")
