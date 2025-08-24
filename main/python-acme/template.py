5cb862bec38bfc0f5729feae656ef172b60a80de
Duncan Bellamy <dunk@denkimushi.com>
2025-08-24 16:50:01 +0000

    main/python-acme: update to 4.2.0

--- main/python-acme/template.py
+++ main/python-acme/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-acme"
-pkgver = "4.1.1"
+pkgver = "4.2.0"
 pkgrel = 0
 build_wrksrc = "acme"
 build_style = "python_pep517"
@@ -23,7 +23,7 @@ url = "https://github.com/certbot/certbot/tree/master/acme"
 source = (
     f"https://github.com/certbot/certbot/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "336f323e0b175f28f146eede7c8db3f2984ce7b542f621e334d2f5c3eb111ca5"
+sha256 = "1bd900a0c6fb8f31575b0a525b67d2924c0905704c96f73d8702d61c66ede474"
 
 
 def init_check(self):
