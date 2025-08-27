f4acc2669da274e533d6c842b4d8fe31e5576c13
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-08-27 20:18:54 +0200

    user/cargo-deny: update to 0.18.4

--- user/cargo-deny/template.py
+++ user/cargo-deny/template.py
@@ -1,5 +1,5 @@
 pkgname = "cargo-deny"
-pkgver = "0.18.2"
+pkgver = "0.18.4"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = ["--no-default-features", "--features=native-certs"]
@@ -12,7 +12,7 @@ pkgdesc = "Cargo plugin for linting dependencies"
 license = "MIT OR Apache-2.0"
 url = "https://github.com/EmbarkStudios/cargo-deny"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "bb47741fada886c166e2a697a87fe93fca38ec083db489d404c73bcb0b9d7445"
+sha256 = "cd093a71d383988252428ebc08ee83f9db204e95074138264f9c8bd5fa06369a"
 # TODO
 options = ["!check"]
 
