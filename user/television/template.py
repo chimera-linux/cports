a1dd86b5d24de91f1f2b72afa4c371753d074e1d
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-08-27 22:33:32 +0200

    user/television: update to 0.13.3

--- user/television/template.py
+++ user/television/template.py
@@ -1,15 +1,18 @@
 pkgname = "television"
-pkgver = "0.11.9"
+pkgver = "0.13.3"
 pkgrel = 0
 build_style = "cargo"
+make_check_env = {
+    "TV_BIN_PATH": f"./target/{self.profile().triplet}/release/tv",
+}
 hostmakedepends = ["cargo-auditable", "pkgconf"]
-makedepends = ["oniguruma-devel"]
-depends = ["rust-std"]
+depends = ["bash", "fd", "bat", "rust-std"]
+checkdepends = [*depends]
 pkgdesc = "Fuzzy finder"
 license = "MIT"
 url = "https://github.com/alexpasmantier/television"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "be58e6011ce1228afa1c6ba15f33023484b1bc8673c466f186ac2a130bb25bdb"
+sha256 = "4d3f5475fd4040ac64abc08395f4c769ffd40c9071a9a560d8038b233277b0c6"
 # generates completions with host binary
 options = ["!cross"]
 
