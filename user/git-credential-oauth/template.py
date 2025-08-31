6357de799d99501161eaa35ed82da7dfc1e3ea14
libkurisu <libkurisu@tutamail.com>
2025-08-31 09:45:16 -0500

    user/git-credential-oauth: new package
    Closes https://github.com/chimera-linux/cports/pull/4728

---
+++ user/git-credential-oauth/template.py
@@ -0,0 +1,14 @@
+pkgname = "git-credential-oauth"
+pkgver = "0.17.2"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go"]
+pkgdesc = "Oauth credential manager for git"
+license = "Apache-2.0"
+url = "https://github.com/hickford/git-credential-oauth"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "23769afc87f82fe21b5519d059bb5ce56b2fad2c4abc7ecde9bff49a4e065ab6"
+
+
+def post_install(self):
+    self.install_man("git-credential-oauth.1")
