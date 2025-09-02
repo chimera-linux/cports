d6407ec0c36ec8e55ee608ea89b08df1acab9cea
sizeofnull <me@sizeofnull.xyz>
2025-09-02 17:14:03 +0000

    user/golangci-lint: update to 2.4.0

--- user/golangci-lint/template.py
+++ user/golangci-lint/template.py
@@ -1,6 +1,6 @@
 pkgname = "golangci-lint"
-pkgver = "2.2.2"
-pkgrel = 1
+pkgver = "2.4.0"
+pkgrel = 0
 build_style = "go"
 make_dir = "build-cccc"
 make_build_args = ["./cmd/golangci-lint"]
@@ -9,7 +9,7 @@ pkgdesc = "Linters runner for Go"
 license = "GPL-3.0-or-later"
 url = "https://golangci-lint.run"
 source = f"https://github.com/golangci/golangci-lint/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "807175564d9f641365677a62e5ea50b41c05e6d8204fc8225492a25a26551189"
+sha256 = "ac90b9f5343f4b18b1b477303c104fb1537d63260862e264d31a144a9685f81b"
 # cross: generates completions with host binary
 # some tests fail because of chroot and some need network
 options = ["!check", "!cross"]
