f3152773f3f302321fda242bb1bf746d26fa32fc
q66 <q66@chimera-linux.org>
2025-09-06 19:13:20 +0200

    main/adw-xfwm4: stop installing the pixmaps folder
    These are just the source .xcf (gimp) images, they are not used.

--- main/adw-xfwm4/template.py
+++ main/adw-xfwm4/template.py
@@ -1,7 +1,7 @@
 pkgname = "adw-xfwm4"
 pkgver = "0_git20230124"
 _gitrev = "b0b163bac7d74e5c2d69451d9b1315389bb3c361"
-pkgrel = 0
+pkgrel = 1
 pkgdesc = "Libadwaita theme for xfwm4"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/FabianOvrWrt/adw-xfwm4"
@@ -11,7 +11,6 @@ sha256 = "8d51102bbfa6a7e7784c760629b85196626621573fa6642a7b985c96728b0f55"
 
 def install(self):
     self.install_files("themes", "usr/share")
-    self.install_files("pixmaps", "usr/share")
 
     # No idea why these are there
     self.uninstall("**/themerc~", glob=True)
