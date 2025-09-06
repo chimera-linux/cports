a3dd0bbaa2876131bd7c90864245c74f4b05a2c3
triallax <triallax@tutanota.com>
2025-09-06 14:06:11 +0300

    main/mpvqt: drop spurious space prefix from license

--- main/mpvqt/template.py
+++ main/mpvqt/template.py
@@ -1,6 +1,6 @@
 pkgname = "mpvqt"
 pkgver = "1.1.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -12,7 +12,7 @@ makedepends = [
     "qt6-qtdeclarative-devel",
 ]
 pkgdesc = "QML libmpv wrapper"
-license = " LGPL-2.1-only OR LGPL-3.0-only"
+license = "LGPL-2.1-only OR LGPL-3.0-only"
 url = "https://invent.kde.org/libraries/mpvqt"
 source = f"$(KDE_SITE)/mpvqt/mpvqt-{pkgver}.tar.xz"
 sha256 = "bdd1ea69338cf3017f628a886218b8c185ca24e8257f03207a3cf1bbb51e3d32"
