5c29d7b61924eaae6c93ed6c35bf92ffc683daea
Gnarwhal <git.aspect893@passmail.net>
2025-09-06 12:26:03 +0000

    main/obs-pipewire-audio-capture: update to 1.2.1

--- main/obs-pipewire-audio-capture/template.py
+++ main/obs-pipewire-audio-capture/template.py
@@ -1,5 +1,5 @@
 pkgname = "obs-pipewire-audio-capture"
-pkgver = "1.2.0"
+pkgver = "1.2.1"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -15,5 +15,5 @@ pkgdesc = "OBS plugin for capturing device and application audio using pipewire"
 license = "GPL-2.0-or-later"
 url = "https://obsproject.com/forum/resources/pipewire-audio-capture.1458"
 source = f"https://github.com/dimtpap/obs-pipewire-audio-capture/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "ce5973187d637edaf5089ac443f364e5ade27b9612082c30c422ab5d8211d88a"
+sha256 = "70107cafd2020437fc0b663fdf6a94598b474942a0582b24719a0e76a9f73a50"
 hardening = ["vis", "!cfi"]
