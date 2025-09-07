97bb469607295c062bf4d76e6d9da136ab93af3b
Erica Z <zerica@callcc.eu>
2025-09-07 14:29:54 +0200

    user/hashcat: update to 7.1.2

--- user/hashcat/template.py
+++ user/hashcat/template.py
@@ -1,20 +1,27 @@
 pkgname = "hashcat"
-pkgver = "6.2.6"
+pkgver = "7.1.2"
 pkgrel = 0
 build_style = "makefile"
 make_env = {
     "PREFIX": "/usr",
     "SED": "/usr/bin/gsed",
-    "USE_SYSTEM_ZLIB": "1",
     "USE_SYSTEM_OPENCL": "1",
     "USE_SYSTEM_XXHASH": "1",
+    "USE_SYSTEM_ZLIB": "1",
+    # prevents -march=native from being passed
+    "MAINTAINER_MODE": "1",
 }
 make_use_env = True
-hostmakedepends = ["gsed"]
+hostmakedepends = [
+    "cargo",
+    "gsed",
+]
 makedepends = [
     "linux-headers",
     "minizip-devel",
     "opencl-headers",
+    "python-devel",
+    "rust-std",
     "xxhash-devel",
     "zlib-ng-compat-devel",
 ]
@@ -22,13 +29,27 @@ pkgdesc = "Password recovery tool"
 license = "MIT"
 url = "https://hashcat.net/hashcat"
 source = f"https://hashcat.net/files/hashcat-{pkgver}.tar.gz"
-sha256 = "b25e1077bcf34908cc8f18c1a69a2ec98b047b2cbcf0f51144dcf3ba1e0b7b2a"
+sha256 = "9546a6326d747530b44fcc079babad40304a87f32d3c9080016d58b39cfc8b96"
 # check: no obvious test suite
-options = ["!check"]
+options = ["!check", "!cross"]
 
 
 if self.profile().endian == "big":
     broken = "bug endian"
+
+
+def prepare(self):
+    from cbuild.util import cargo
+
+    cargo.Cargo(self).vendor(
+        ["--manifest-path", "Rust/generic_hash/Cargo.toml"]
+    )
+
+
+def init_build(self):
+    from cbuild.util import cargo
+
+    self.make_env.update(cargo.get_environment(self))
 
 
 def post_install(self):
