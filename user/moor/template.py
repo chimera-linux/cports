cc6dfa8c10935bd1e113153617fcc4e0981e15c7
Biswapriyo Nath <nathbappai@gmail.com>
2025-09-07 05:49:55 +0000

    user/moor: rename from moar, update to 2.1.0

---
+++ user/moor/template.py
@@ -0,0 +1,18 @@
+pkgname = "moor"
+pkgver = "2.1.0"
+pkgrel = 0
+build_style = "go"
+make_build_args = [f"-ldflags=-X main.versionString=v{pkgver}", "./cmd/moor"]
+hostmakedepends = ["go"]
+renames = ["moar"]
+pkgdesc = "Terminal pager program"
+license = "BSD-2-Clause"
+url = "https://github.com/walles/moor"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "3c09cd7a22524c77d6b7c5564688f2c8e446319eef27cbdfdcbe810a14e2ba0a"
+
+
+def install(self):
+    self.install_bin("build/moor")
+    self.install_license("LICENSE")
+    self.install_man("moor.1")
