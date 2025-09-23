904e5264ce1a1f9965427ae86903a5c9657325ac
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2025-09-23 15:43:19 +0545

    user/blanket: update to 0.8.0
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

--- user/blanket/template.py
+++ user/blanket/template.py
@@ -1,5 +1,5 @@
 pkgname = "blanket"
-pkgver = "0.7.0"
+pkgver = "0.8.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -20,7 +20,7 @@ pkgdesc = "Listen to different sounds"
 license = "GPL-3.0-or-later"
 url = "https://apps.gnome.org/Blanket"
 source = f"https://github.com/rafaelmardojai/blanket/archive/{pkgver}/blanket-{pkgver}.tar.gz"
-sha256 = "14445e5d2b56116ac741aef8bba9fed444ec52a089ebd175099539a87de10ab5"
+sha256 = "46bc401182a738d9b4986b7491dfe330127117a511c24b81309b49ed5b9e6e83"
 
 
 def post_install(self):
