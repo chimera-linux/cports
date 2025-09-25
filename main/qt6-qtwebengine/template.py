9ac9ed4d6f5042882f4640e8be282343f80ee9df
q66 <q66@chimera-linux.org>
2025-09-25 11:37:25 +0200

    main/qt6-qtwebengine: sec bump

--- main/qt6-qtwebengine/template.py
+++ main/qt6-qtwebengine/template.py
@@ -1,12 +1,12 @@
 pkgname = "qt6-qtwebengine"
 pkgver = "6.9.2"
-pkgrel = 2
+pkgrel = 3
 # latest from https://github.com/qt/qtwebengine-chromium/commits/130-based
 # check CHROMIUM_VERSION on qt majors
 # note that like half the chromium patches are probably unneeded but
 # they are taken directly from chromium patches/ for that major for
 # ease of maintenance
-_qtwebengine_gitrev = "40242ad8e04084da1a2a19d671f6e9f86a6c277f"
+_qtwebengine_gitrev = "94c3d2b54efc455a13017730afeb4c0cc9777124"
 archs = ["aarch64", "ppc64le", "x86_64"]
 build_style = "cmake"
 configure_args = [
@@ -96,7 +96,7 @@ source = [
 source_paths = [".", "3rdparty-chromium"]
 sha256 = [
     "99cb0792abc2e39b299d73d8e2aa076b9ebcd55c3f4a4b5fd514eef5a62d03ab",
-    "8914ef227259d25bd225e4ea25a8c8804f5b722b48eb113fe2f7bed373ba8490",
+    "47972869d80f0739876a389fa4a103a1e03d8d9c4003ff8541322abdc425861a",
 ]
 debug_level = 1  # defatten, especially with LTO
 tool_flags = {
