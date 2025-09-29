9929ada2659b14589da15829b270d47b6c2572e3
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-09-29 20:44:37 +0200

    user/just: update to 1.43.0

--- user/just/template.py
+++ user/just/template.py
@@ -1,5 +1,5 @@
 pkgname = "just"
-pkgver = "1.42.4"
+pkgver = "1.43.0"
 pkgrel = 0
 build_style = "cargo"
 # skip tests that fail when run outside of git repo
@@ -11,20 +11,12 @@ pkgdesc = "Save and run commands from justfile"
 license = "CC0-1.0"
 url = "https://github.com/casey/just"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "bd604ff72ecd8d8def79d39997499433e22fbffa03260e3a2c5fe5f84cc37f52"
+sha256 = "03904d6380344dbe10e25f04cd1677b441b439940257d3cc9d8c5f09d91e3065"
 # generates completions and man page with host binary
 options = ["!cross"]
 
 
 def post_build(self):
-    for shell in ["bash", "fish", "zsh", "nushell"]:
-        with open(self.cwd / f"just.{shell}", "w") as f:
-            self.do(
-                f"./target/{self.profile().triplet}/release/just",
-                "--completions",
-                shell,
-                stdout=f,
-            )
     with open(self.cwd / "just.1", "w") as f:
         self.do(
             f"./target/{self.profile().triplet}/release/just",
@@ -35,5 +27,6 @@ def post_build(self):
 
 def post_install(self):
     self.install_man("just.1")
-    for shell in ["bash", "fish", "zsh", "nushell"]:
-        self.install_completion(f"just.{shell}", shell)
+    for shell in ["bash", "fish", "zsh"]:
+        self.install_completion(f"completions/just.{shell}", shell)
+    self.install_completion("completions/just.nu", "nushell")
