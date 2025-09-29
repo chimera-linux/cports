afc4076cc37413e965e2e0332751ee17c463a902
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-09-29 20:58:12 +0200

    user/restic: update to 0.18.1

--- user/restic/template.py
+++ user/restic/template.py
@@ -1,6 +1,6 @@
 pkgname = "restic"
-pkgver = "0.18.0"
-pkgrel = 4
+pkgver = "0.18.1"
+pkgrel = 0
 build_style = "go"
 make_build_args = [f"-ldflags=-X main.version=v{pkgver}", "./cmd/restic"]
 hostmakedepends = ["go"]
@@ -8,7 +8,7 @@ pkgdesc = "Backup tool"
 license = "BSD-2-Clause"
 url = "https://restic.net"
 source = f"https://github.com/restic/restic/releases/download/v{pkgver}/restic-{pkgver}.tar.gz"
-sha256 = "fc068d7fdd80dd6a968b57128d736b8c6147aa23bcba584c925eb73832f6523e"
+sha256 = "4b8e2b6cb20e9707e14b9b9d92ddb6f2e913523754e1f123e2e6f3321e67f7ca"
 # fails in bwrap chroot
 options = ["!check"]
 
