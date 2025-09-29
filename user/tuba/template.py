e8c81e4ff8a03587e5643dafea6c6afdc6a0d409
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2025-09-29 07:44:07 +0545

    user/tuba: update to 0.10.2
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

--- user/tuba/template.py
+++ user/tuba/template.py
@@ -1,5 +1,5 @@
 pkgname = "tuba"
-pkgver = "0.10.0"
+pkgver = "0.10.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -31,4 +31,4 @@ pkgdesc = "GTK fediverse client"
 license = "GPL-3.0-or-later"
 url = "https://tuba.geopjr.dev"
 source = f"https://github.com/GeopJr/Tuba/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "da86f8fcfbc8d47ca0c9e393827c7948594bddb90e475527db3e079b179870a6"
+sha256 = "2adc7921ede1d6d1a8b3e6395e12235e8acbe5d9a1957e9419317101bd1c5b8a"
