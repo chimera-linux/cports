96df542b1a845453dff1ae0c0ef0f0f2334f3c9a
Guilhem Baccialone <guilhem.baccialone@zaclys.net>
2025-09-29 22:09:56 +0200

    user/refine: update to 0.6.0

--- user/refine/template.py
+++ user/refine/template.py
@@ -1,5 +1,5 @@
 pkgname = "refine"
-pkgver = "0.5.10"
+pkgver = "0.6.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -23,4 +23,4 @@ pkgdesc = "Tweak various aspects of GNOME"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.gnome.org/TheEvilSkeleton/Refine"
 source = f"{url}/-/archive/{pkgver}/Refine-{pkgver}.tar.bz2"
-sha256 = "97ce0c9cc3a8e751bd8c6cbe9b5669bea8a26eaf9ce308897ffd667d6a0bb925"
+sha256 = "0040d1c18b860cc6843c5479db80022cee5e3b99687d75266f28e3be8d794892"
