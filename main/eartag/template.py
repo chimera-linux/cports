e53fcf5c37e11329d9ef2ae85fb7de6baca275c1
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2025-10-01 21:01:50 +0545

    main/eartag: update to 1.0.2
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

--- main/eartag/template.py
+++ main/eartag/template.py
@@ -1,5 +1,5 @@
 pkgname = "eartag"
-pkgver = "0.6.5"
+pkgver = "1.0.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -12,17 +12,19 @@ depends = [
     "gtk4",
     "libadwaita",
     "mutagen",
+    "python-filetype",
     "python-gobject",
     "python-magic",
     "python-pillow",
     "python-pyacoustid",
 ]
-checkdepends = ["python-pytest", *depends]
 pkgdesc = "Edit audio file tags"
 license = "MIT"
 url = "https://apps.gnome.org/EarTag"
 source = f"https://gitlab.gnome.org/World/eartag/-/archive/{pkgver}/eartag-{pkgver}.tar.gz"
-sha256 = "122093d2e17cc6f242bd5effac9e9ac0d320b9124a98b7cb638f19cb324d8269"
+sha256 = "c11287ba47fb529b44163e0fbdd1950efd7b9fd970fdfe7027e86bb93decf1b9"
+# requires multiple unpackaged checkdepends
+options = ["!check"]
 
 
 def post_install(self):
