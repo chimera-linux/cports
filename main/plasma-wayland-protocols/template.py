a1e811c90aaca67b6e3beba6dbfb9b1f7545cad6
q66 <q66@chimera-linux.org>
2025-10-05 02:15:16 +0200

    main/plasma-wayland-protocols: update to 1.19.0

--- main/plasma-wayland-protocols/template.py
+++ main/plasma-wayland-protocols/template.py
@@ -1,5 +1,5 @@
 pkgname = "plasma-wayland-protocols"
-pkgver = "1.18.0"
+pkgver = "1.19.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -15,7 +15,7 @@ pkgdesc = "Plasma-specific wayland protocols"
 license = "MIT AND (LGPL-2.1-only OR LGPL-3.0-only)"
 url = "https://invent.kde.org/libraries/plasma-wayland-protocols"
 source = f"$(KDE_SITE)/plasma-wayland-protocols/plasma-wayland-protocols-{pkgver}.tar.xz"
-sha256 = "2567472671ad5d989f88b51baef9dd59353a5e7c3f2ed7e6b989755cb9004233"
+sha256 = "45679fe7a63c414f2c81793a62528fae6ccee584b6965719d7f9fc6dd48ba846"
 
 
 def post_install(self):
