78785508bc22b18ca5dbf0ad5cd85410af8960f1
q66 <q66@chimera-linux.org>
2025-10-06 12:35:16 +0200

    main/gtk-doc-tools: update to 1.35.1

--- main/gtk-doc-tools/template.py
+++ main/gtk-doc-tools/template.py
@@ -1,6 +1,6 @@
 pkgname = "gtk-doc-tools"
-pkgver = "1.34.0"
-pkgrel = 2
+pkgver = "1.35.1"
+pkgrel = 0
 build_style = "meson"
 # glib cyclic dep
 configure_args = ["-Dtests=false"]
@@ -25,7 +25,7 @@ pkgdesc = "Documentation tool for public library API"
 license = "GPL-2.0-or-later AND GFDL-1.1-or-later"
 url = "http://www.gtk.org/gtk-doc"
 source = f"$(GNOME_SITE)/gtk-doc/{pkgver[:-2]}/gtk-doc-{pkgver}.tar.xz"
-sha256 = "b20b72b32a80bc18c7f975c9d4c16460c2276566a0b50f87d6852dff3aa7861c"
+sha256 = "611c9f24edd6d88a8ae9a79d73ab0dc63c89b81e90ecc31d6b9005c5f05b25e2"
 options = ["!splitdoc"]
 
 
