345e0c532722c882b290a564acb4f027d8407cee
q66 <q66@chimera-linux.org>
2025-10-06 14:32:43 +0200

    user/papers: update to 49.0

--- user/papers/template.py
+++ user/papers/template.py
@@ -1,5 +1,5 @@
 pkgname = "papers"
-pkgver = "48.3"
+pkgver = "49.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -7,8 +7,10 @@ configure_args = [
     "-Ddocumentation=false",
     "-Dtiff=enabled",
     "-Ddjvu=enabled",
+    "-Dtests=disabled",
 ]
 hostmakedepends = [
+    "blueprint-compiler",
     "cargo-auditable",
     "desktop-file-utils",
     "gettext",
@@ -36,13 +38,16 @@ pkgdesc = "GNOME document viewer"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/Incubator/papers"
 source = f"{url}/-/archive/{pkgver}.tar.gz"
-sha256 = "2c9f00bb479b7305fca5261f871370be07997b7191db0c7ae1f61d5f1247d27d"
+sha256 = "6cb48dca9d938620ed9e0b5af1f3c7f95820fee997f63ba38674edd62e7091ea"
+# needs thumbnailer copied etc, whatever don't care
+options = ["!check"]
 
 
 def post_patch(self):
     from cbuild.util import cargo
 
     cargo.Cargo(self, wrksrc="shell").vendor()
+    cargo.Cargo(self, wrksrc="thumbnailer").vendor()
 
 
 def init_build(self):
@@ -55,6 +60,9 @@ def init_build(self):
 def post_install(self):
     self.install_bin(
         f"./build/shell/src/{self.profile().triplet}/release/papers"
+    )
+    self.install_bin(
+        f"./build/thumbnailer/{self.profile().triplet}/release/papers-thumbnailer"
     )
 
 
