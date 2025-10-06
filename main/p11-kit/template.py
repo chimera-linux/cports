9c8848d0fdeaa9efd52755edf9b0c00d3fec71e3
q66 <q66@chimera-linux.org>
2025-10-06 12:47:35 +0200

    main/p11-kit: update to 0.25.10

--- main/p11-kit/template.py
+++ main/p11-kit/template.py
@@ -1,5 +1,5 @@
 pkgname = "p11-kit"
-pkgver = "0.25.5"
+pkgver = "0.25.10"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -26,7 +26,7 @@ pkgdesc = "Provides a way to load and enumerate PKCS#11 modules"
 license = "BSD-3-Clause"
 url = "https://github.com/p11-glue/p11-kit"
 source = f"{url}/releases/download/{pkgver}/p11-kit-{pkgver}.tar.xz"
-sha256 = "04d0a86450cdb1be018f26af6699857171a188ac6d5b8c90786a60854e1198e5"
+sha256 = "a62a137a966fb3a9bbfa670b4422161e369ddea216be51425e3be0ab2096e408"
 
 
 def post_install(self):
