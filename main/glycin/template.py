df8e499b6adb125f2754e4e7dbe741ee96b623a9
q66 <q66@chimera-linux.org>
2025-10-06 11:21:52 +0200

    main/glycin: update to 2.0.0

--- main/glycin/template.py
+++ main/glycin/template.py
@@ -1,7 +1,9 @@
 pkgname = "glycin"
-pkgver = "1.2.3"
+# 2.0.2 needs close_range
+pkgver = "2.0.0"
 pkgrel = 0
 build_style = "meson"
+prepare_after_patch = True
 configure_args = [
     "--libexecdir=/usr/lib",  # XXX libexecdir
 ]
@@ -31,16 +33,16 @@ pkgdesc = "Sandboxed and extendable image decoding"
 license = "MPL-2.0 OR LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/glycin"
 source = f"$(GNOME_SITE)/glycin/{pkgver[:-2]}/glycin-{pkgver}.tar.xz"
-sha256 = "380aafe2bfb4eca0c45b42660ebff449600d00a43b609d5b1c83f795f5c8fb3c"
+sha256 = "e39c3ca4f5bd6905f19f090456940650c8de1f217d2edb1f46ee17e04e7ae502"
 # gobject-introspection
 # check: for some divine reason, it always passes locally and never on the builders (??)
 options = ["!cross", "!check"]
 
 
-def post_prepare(self):
+def prepare(self):
     from cbuild.util import cargo
 
-    cargo.clear_vendor_checksums(self, "zvariant")
+    cargo.Cargo(self, wrksrc=".").vendor()
 
 
 def init_build(self):
@@ -67,8 +69,8 @@ def _(self):
     # transitional
     self.provides = [self.with_pkgver("libglycin-gtk4")]
     return [
-        "lib:libglycin-gtk4-1.so.*",
-        "usr/lib/girepository-1.0/GlyGtk4-1.typelib",
+        "lib:libglycin-gtk4-2.so.*",
+        "usr/lib/girepository-1.0/GlyGtk4-2.typelib",
     ]
 
 
