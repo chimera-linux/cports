593010abffa16e4b2b22fafa99264ae3a4b52a57
q66 <q66@chimera-linux.org>
2025-10-06 12:35:31 +0200

    main/ostree: update to 2025.6

--- main/ostree/template.py
+++ main/ostree/template.py
@@ -1,5 +1,5 @@
 pkgname = "ostree"
-pkgver = "2025.4"
+pkgver = "2025.6"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -38,7 +38,7 @@ pkgdesc = "Operating system and container binary deployment and upgrades"
 license = "LGPL-2.0-or-later"
 url = "https://ostreedev.github.io/ostree"
 source = f"https://github.com/ostreedev/ostree/releases/download/v{pkgver}/libostree-{pkgver}.tar.xz"
-sha256 = "014d12d19ea664fae53fe147d3d2c0b7465a3c37e099c87d289df908adeb5669"
+sha256 = "6750627df600f28c8ef2c7f443a9d72aef7061a342cde234162ebdf850a7e575"
 # failing on their test harness, i will find motivation Soon
 options = ["!check"]
 
