23ace2324d6738530238f460fedf92163acdd490
q66 <q66@chimera-linux.org>
2025-10-06 12:41:43 +0200

    main/template-glib: update to 3.38.0

--- main/template-glib/template.py
+++ main/template-glib/template.py
@@ -1,5 +1,5 @@
 pkgname = "template-glib"
-pkgver = "3.36.3"
+pkgver = "3.38.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -14,7 +14,7 @@ pkgdesc = "Library for GObject template expansion"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/template-glib"
 source = f"$(GNOME_SITE)/template-glib/{'.'.join(pkgver.rsplit('.')[:-1])}/template-glib-{pkgver}.tar.xz"
-sha256 = "d528b35b2cf90e07dae50e25e12fbadb0eb048f57fd5151cf9f6e98cce1df20e"
+sha256 = "40d00dc223dcf2eb7f2ec422f7dec5a67373a0ca1101abca0f49c62f050cb312"
 # gobject-introspection
 options = ["!cross"]
 
