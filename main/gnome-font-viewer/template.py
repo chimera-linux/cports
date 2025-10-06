7a650915a3b824083487558c35e52919aa7aa5ed
q66 <q66@chimera-linux.org>
2025-10-06 14:14:14 +0200

    main/gnome-font-viewer: update to 49.0

--- main/gnome-font-viewer/template.py
+++ main/gnome-font-viewer/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-font-viewer"
-pkgver = "48.0"
+pkgver = "49.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -21,6 +21,6 @@ pkgdesc = "Font viewer for GNOME"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/gnome-font-viewer"
 source = f"$(GNOME_SITE)/gnome-font-viewer/{pkgver[:-2]}/gnome-font-viewer-{pkgver}.tar.xz"
-sha256 = "732624231b624ff5c7ac03a8ce71be12393daa53551d11550b20d7b0a3a872a7"
+sha256 = "7c018925c285771b55d7d1a6f15711c0c193d7450ed9871e20d44f2548562404"
 
 tool_flags = {"CFLAGS": ["-Wno-incompatible-function-pointer-types"]}
