cdba99516a495bf151ca39c43341c1f98a4c4546
q66 <q66@chimera-linux.org>
2025-10-06 13:19:15 +0200

    main/editorconfig: update to 0.12.10

--- main/editorconfig/template.py
+++ main/editorconfig/template.py
@@ -1,7 +1,7 @@
 pkgname = "editorconfig"
-pkgver = "0.12.9"
+pkgver = "0.12.10"
 _test_commit = "5ca0b296dc31124d0303895c163013450bd97958"
-pkgrel = 2
+pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = ["pcre2-devel"]
@@ -14,7 +14,7 @@ source = [
 ]
 source_paths = [".", "tests"]
 sha256 = [
-    "4aaa4e3883332aac7ec19c169dcf128f5f0f963f61d09beb299eb2bce5944e2c",
+    "ab9f897a90fb36cfc34e5b67221e55ab0e3119b3512de8e31029d376c6bab870",
     "a1ca71c8f099c6ffc4fc1c0214732d4e27168fb2a5fbf2da47b5bc50fb7b5e79",
 ]
 # a bunch of tests fail due to... cmake?
