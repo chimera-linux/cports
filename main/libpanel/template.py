6761ea56ffbb2b4d6a4f5c73509cdb4d65f3418b
q66 <q66@chimera-linux.org>
2025-10-06 12:15:07 +0200

    main/libpanel: update to 1.10.2

--- main/libpanel/template.py
+++ main/libpanel/template.py
@@ -1,5 +1,5 @@
 pkgname = "libpanel"
-pkgver = "1.10.0"
+pkgver = "1.10.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocs=disabled"]
@@ -14,7 +14,7 @@ pkgdesc = "Dock/panel library for GTK 4"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/libpanel"
 source = f"$(GNOME_SITE)/libpanel/{'.'.join(pkgver.rsplit('.')[:-1])}/libpanel-{pkgver}.tar.xz"
-sha256 = "578ce512278ff2bb5eeebb55099392c52537a5abd9bd0629567f102532b38b25"
+sha256 = "cc12e8e10f1e4977bd12ad3ffaedcd52ac176348b4af6fe5da686b96325bfe01"
 # gobject-introspection
 options = ["!cross"]
 
