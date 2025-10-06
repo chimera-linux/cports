6e89bf1a1780a3b72fb65e6665cfcef35af07fee
q66 <q66@chimera-linux.org>
2025-10-06 12:13:57 +0200

    main/gsettings-desktop-schemas: update to 49.0

--- main/gsettings-desktop-schemas/template.py
+++ main/gsettings-desktop-schemas/template.py
@@ -1,6 +1,6 @@
 pkgname = "gsettings-desktop-schemas"
-pkgver = "48.0"
-pkgrel = 1
+pkgver = "49.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=true"]
 hostmakedepends = [
@@ -20,7 +20,7 @@ pkgdesc = "Collection of GSettings schemas"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas"
 source = f"$(GNOME_SITE)/gsettings-desktop-schemas/{pkgver[:-2]}/gsettings-desktop-schemas-{pkgver}.tar.xz"
-sha256 = "e68f155813bf18f865a8b2c8e9d473588b6ccadcafbb666ab788857c6c2d1bd3"
+sha256 = "912905cc45382888a47702ed1101c6b08ebd0122a32a67d940ab8116a96c520d"
 options = ["!cross"]
 
 
