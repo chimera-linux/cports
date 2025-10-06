f8ee43ae220ece748c17330a71f0eeb7946f662e
q66 <q66@chimera-linux.org>
2025-10-06 10:20:10 +0200

    main/freetype: update to 2.14.1

--- main/freetype/template.py
+++ main/freetype/template.py
@@ -1,5 +1,5 @@
 pkgname = "freetype"
-pkgver = "2.13.3"
+pkgver = "2.14.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -24,7 +24,7 @@ pkgdesc = "Font rendering engine"
 license = "FTL OR GPL-2.0-or-later"
 url = "https://freetype.org"
 source = f"$(SOURCEFORGE_SITE)/freetype/freetype-{pkgver}.tar.xz"
-sha256 = "0550350666d427c74daeb85d5ac7bb353acba5f76956395995311a9c6f063289"
+sha256 = "32427e8c471ac095853212a37aef816c60b42052d4d9e48230bab3bdf2936ccc"
 hardening = ["!vis", "!cfi"]
 # data files missing
 options = ["!check"]
