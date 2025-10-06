655421d46bfe31ca526d1a8d51aca4e3d618ff53
q66 <q66@chimera-linux.org>
2025-10-06 12:12:37 +0200

    main/meson: update to 1.9.1

--- main/meson/template.py
+++ main/meson/template.py
@@ -1,5 +1,5 @@
 pkgname = "meson"
-pkgver = "1.8.2"
+pkgver = "1.9.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ pkgdesc = "Meson build system"
 license = "Apache-2.0"
 url = "https://mesonbuild.com"
 source = f"https://github.com/mesonbuild/meson/releases/download/{pkgver}/meson-{pkgver}.tar.gz"
-sha256 = "c105816d8158c76b72adcb9ff60297719096da7d07f6b1f000fd8c013cd387af"
+sha256 = "4e076606f2afff7881d195574bddcd8d89286f35a17b4977a216f535dc0c74ac"
 # meson is early in our bootstrap path but has a million checkdepends
 options = ["!check"]
 
