23545d22612821318e20fed9ec240345bf3fe443
q66 <q66@chimera-linux.org>
2025-10-06 13:17:49 +0200

    main/vte: update to 0.82.0

--- main/vte/template.py
+++ main/vte/template.py
@@ -1,5 +1,5 @@
 pkgname = "vte"
-pkgver = "0.80.3"
+pkgver = "0.82.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -37,15 +37,8 @@ pkgdesc = "Gtk terminal widget"
 subdesc = "common files"
 license = "LGPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/Terminal/VTE"
-source = [
-    f"$(GNOME_SITE)/vte/{pkgver[: pkgver.rfind('.')]}/vte-{pkgver}.tar.xz",
-    "https://github.com/fastfloat/fast_float/archive/refs/tags/v8.0.2.tar.gz",
-]
-source_paths = [".", "subprojects/fast_float"]
-sha256 = [
-    "2e596fd3fbeabb71531662224e71f6a2c37f684426136d62854627276ef4f699",
-    "e14a33089712b681d74d94e2a11362643bd7d769ae8f7e7caefe955f57f7eacd",
-]
+source = f"$(GNOME_SITE)/vte/{pkgver[: pkgver.rfind('.')]}/vte-{pkgver}.tar.xz"
+sha256 = "b0718db3254730701b43bf5e113cbf8cdb2c14715d32ee1e8a707dc6eb70535f"
 # assert in meson
 options = ["!lto", "!cross"]
 
@@ -58,13 +51,6 @@ tool_flags = {
         "-Wno-float-equal",
     ],
 }
-
-
-def post_extract(self):
-    self.mv(
-        "subprojects/packagefiles/fast_float/meson.build",
-        "subprojects/fast_float",
-    )
 
 
 @subpackage("vte-gtk3")
