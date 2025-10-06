f0081f6ae698ec55644baf58f9129ae88de40bc7
q66 <q66@chimera-linux.org>
2025-10-06 13:52:51 +0200

    main/gnome-backgrounds: update to 49.0

--- main/gnome-backgrounds/template.py
+++ main/gnome-backgrounds/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-backgrounds"
-pkgver = "48.2.1"
+pkgver = "49.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson"]
@@ -7,7 +7,7 @@ pkgdesc = "Backgrounds for GNOME desktop"
 license = "GPL-2.0-or-later AND CC-BY-2.0 AND CC-BY-SA-2.0 AND CC-BY-SA-3.0"
 url = "https://gitlab.gnome.org/GNOME/gnome-backgrounds"
 source = f"$(GNOME_SITE)/gnome-backgrounds/{pkgver[: pkgver.find('.')]}/gnome-backgrounds-{pkgver}.tar.xz"
-sha256 = "6a1c5b7b2e0d8f5ce977926d55f9c2d65dd180822cdea5e59150dcfb5abd1ed9"
+sha256 = "01fe260de58ba00e44aba589a998e33e8bc7aecc701df0bee64cf4cfba44f4b5"
 
 
 @subpackage("gnome-backgrounds-gnome")
