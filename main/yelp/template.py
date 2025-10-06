70ece73e6728638dea8334082ab6d3855463ce32
q66 <q66@chimera-linux.org>
2025-10-06 13:26:45 +0200

    main/yelp: update to 49.0

--- main/yelp/template.py
+++ main/yelp/template.py
@@ -1,10 +1,11 @@
 pkgname = "yelp"
-pkgver = "42.3"
+pkgver = "49.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddefault_library=shared"]
 hostmakedepends = [
     "bash",
+    "desktop-file-utils",
     "gettext",
     "glib-devel",
     "itstool",
@@ -13,12 +14,12 @@ hostmakedepends = [
 ]
 makedepends = [
     "glib-devel",
-    "gtk+3-devel",
-    "libhandy-devel",
+    "gtk4-devel",
+    "libadwaita-devel",
     "libxml2-devel",
     "libxslt-devel",
     "sqlite-devel",
-    "webkitgtk-devel",
+    "webkitgtk4-devel",
     "yelp-xsl",
 ]
 depends = ["dconf", "yelp-xsl"]
@@ -26,7 +27,7 @@ pkgdesc = "Help browser for GNOME desktop"
 license = "GPL-2.0-or-later"
 url = "https://wiki.gnome.org/Apps/Yelp"
 source = f"$(GNOME_SITE)/yelp/{pkgver[:-2]}/yelp-{pkgver}.tar.xz"
-sha256 = "26ccc422679a9e6a7a3aa083d90fcdb347f5f2300be3e01431a30d0cdd2a89a3"
+sha256 = "e6614e0b1f4ba5fe7b8d149bdd42673f030656fbdcd736ae9860719197c63457"
 
 
 @subpackage("yelp-devel")
