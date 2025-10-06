76d220356f54830de47fba7d13afac51a39fcb6e
q66 <q66@chimera-linux.org>
2025-10-06 11:58:09 +0200

    main/geoclue: update to 2.8.0

--- main/geoclue/template.py
+++ main/geoclue/template.py
@@ -1,5 +1,5 @@
 pkgname = "geoclue"
-pkgver = "2.7.2"
+pkgver = "2.8.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -29,7 +29,7 @@ pkgdesc = "D-Bus geoinformation service"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.freedesktop.org/geoclue/geoclue/wikis/home"
 source = f"https://gitlab.freedesktop.org/geoclue/geoclue/-/archive/{pkgver}/geoclue-{pkgver}.tar.bz2"
-sha256 = "01d24a44a036769049411899ae83c9a86f73d272cca3df6af5a03bfac32456d2"
+sha256 = "c07aeb35cccf959ec1dc2e8f9a71a9d8bdd643879ef0a8d37926499541da1685"
 
 
 def post_install(self):
