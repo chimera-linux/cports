f09b863b4f1cb8dcb5764e172211367da549f1df
triallax <triallax@tutanota.com>
2025-10-09 19:57:56 +0100

    user/tcsh: update to 6.24.16

--- user/tcsh/template.py
+++ user/tcsh/template.py
@@ -1,5 +1,5 @@
 pkgname = "tcsh"
-pkgver = "6.24.15"
+pkgver = "6.24.16"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-nls"]
@@ -9,7 +9,7 @@ pkgdesc = "Enhanced version of the Berkeley UNIX C shell"
 license = "BSD-2-Clause"
 url = "https://astron.com/pub/tcsh"
 source = f"{url}/tcsh-{pkgver}.tar.gz"
-sha256 = "d4d0b2a4df320f57a518e44c359ef36bbcf85d64f5146d0cb8ff34984e0d23fd"
+sha256 = "4208cf4630fb64d91d81987f854f9570a5a0e8a001a92827def37d0ed8f37364"
 
 
 def post_install(self):
