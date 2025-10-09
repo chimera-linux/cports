8ee6171a6014148e9f87dbca8324040d3253c7fb
geemili <opensource@geemili.xyz>
2025-10-09 12:33:27 -0600

    user/bcachefs-tools: add bcachefs-ckms submodule, update to 1.31.7

--- user/bcachefs-tools/template.py
+++ user/bcachefs-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "bcachefs-tools"
-pkgver = "1.31.6"
+pkgver = "1.31.7"
 pkgrel = 0
 build_style = "makefile"
 make_install_args = [
@@ -27,7 +27,7 @@ pkgdesc = "Bcachefs utilities"
 license = "GPL-2.0-only"
 url = "https://github.com/koverstreet/bcachefs-tools"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f8fd465e7d15bfc5c7d8f49b6e85dec4acec9293567b6763f6f2d47379c73bfe"
+sha256 = "e4b6b2af769575cc77496976c72e9aaaeec3a54af71d556f101b5e26aeb1ba85"
 # no tests
 options = ["!check"]
 
@@ -45,6 +45,31 @@ def init_build(self):
     self.make_build_args += [
         "EXTRA_CFLAGS=" + self.get_cflags(shell=True),
         "EXTRA_LDFLAGS=" + self.get_ldflags(shell=True),
+
+        # We need to set ROOT_SBINDIR to /usr/bin,
+        # or else bcachefs-tools' Makefile will generate an initramfs hook
+        # that tries to copy the `bcachefs` exectable from `/usr/local/sbin/`,
+        # instead of from `/usr/bin`.
+        "ROOT_SBINDIR=/usr/bin",
     ]
     # apply our rust stuff
     self.env.update(cargo.get_environment(self))
+
+def post_install(self):
+    # install ckms source tree
+    srcp = f"usr/src/bcachefs-{pkgver}"
+    self.install_dir(srcp)
+    self.install_file(
+        self.files_path / "ckms.ini", srcp, template={"VERSION": pkgver}
+    )
+
+@subpackage("bcachefs-ckms")
+def _(self):
+    self.subdesc = "kernel sources"
+    self.install_if = [self.parent, "ckms"]
+    self.depends = [
+        self.parent,
+        "ckms",
+    ]
+
+    return ["usr/src"]
