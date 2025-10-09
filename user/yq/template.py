e1b616a9c3d330230c0266dd04ef52e516ab7306
triallax <triallax@tutanota.com>
2025-10-09 19:57:35 +0100

    user/yq: update to 4.47.2

--- user/yq/template.py
+++ user/yq/template.py
@@ -1,6 +1,6 @@
 pkgname = "yq"
-pkgver = "4.47.1"
-pkgrel = 2
+pkgver = "4.47.2"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 checkdepends = ["bash", "tzdb"]
@@ -8,7 +8,7 @@ pkgdesc = "Command-line YAML processor"
 license = "MIT"
 url = "https://github.com/mikefarah/yq"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "48ef09b9ffdb80f26ee516fa2fc83f713c1b49f503cedef79a96435509d456af"
+sha256 = "b1ed327337be9e044d8222c41f1437313b148ca73ec83946b1ff26e4ff785964"
 # generates completions with host binary
 options = ["!cross"]
 
