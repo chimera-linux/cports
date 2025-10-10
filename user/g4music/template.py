0d72b6acee437b63cf7d3554eb9722cd26fbcaa1
triallax <triallax@tutanota.com>
2025-10-11 00:57:16 +0100

    user/g4music: update to 4.5

--- user/g4music/template.py
+++ user/g4music/template.py
@@ -1,5 +1,5 @@
 pkgname = "g4music"
-pkgver = "4.3.1"
+pkgver = "4.5"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -20,4 +20,4 @@ pkgdesc = "GTK4 music player with a fluent adaptive user interface"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.gnome.org/neithern/g4music"
 source = f"{url}/-/archive/v{pkgver}/g4music-v{pkgver}.tar.gz"
-sha256 = "a4aa9db0923ba04b362f45bbe2b777cd6a163a6f209c1bad0db3ea6727f450a0"
+sha256 = "0953a2c8d3adddbb8538b853b9d926f761a7ecd9eae1660ad0ed08545b2110ff"
