b65d7451ad74851e059656505b87ddf5b40ee12a
Renato Botelho do Couto <renato@netgate.com>
2025-10-10 07:46:32 -0500

    user/snort: update to 3.9.6.0

--- user/snort/template.py
+++ user/snort/template.py
@@ -1,5 +1,5 @@
 pkgname = "snort"
-pkgver = "3.9.5.0"
+pkgver = "3.9.6.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -26,7 +26,7 @@ pkgdesc = "Network intrusion prevention and detection system"
 license = "GPL-2.0-or-later"
 url = "https://www.snort.org"
 source = f"https://github.com/snort3/snort3/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "e2e36a8db2c4c26a6ff58ea58839339260319eba25d0eb901ddb7210f4fa4b4c"
+sha256 = "2335678bc5ff4f876dcdb6985407a5312b0f3bb470da29e2926f57f942ce3b94"
 
 
 @subpackage("snort-devel")
