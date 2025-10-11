9cc1de3ecb195c59e6e6691a9bc4272319686207
triallax <triallax@tutanota.com>
2025-10-11 01:02:14 +0100

    user/duperemove: update to 0.15.2

--- user/duperemove/template.py
+++ user/duperemove/template.py
@@ -1,5 +1,5 @@
 pkgname = "duperemove"
-pkgver = "0.15"
+pkgver = "0.15.2"
 pkgrel = 0
 build_style = "makefile"
 make_build_env = {
@@ -18,7 +18,7 @@ pkgdesc = "Tools for deduplicating extents in filesystems like Btrfs"
 license = "GPL-2.0-only"
 url = "https://github.com/markfasheh/duperemove"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1dacde51f12ead1da6b067d5520731a83adee3301fbc36eb282cf8362b93d773"
+sha256 = "27809aa91b7b9b7d0810e5329614bf80af2c48e917781e682a3fbcf61fa274da"
 tool_flags = {"CFLAGS": ["-std=c23"]}
 hardening = ["vis", "cfi"]
 # no test suite exists
