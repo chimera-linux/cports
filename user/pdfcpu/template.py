7ef93005cddcca83488c5b6f1753c825cc9a2850
triallax <triallax@tutanota.com>
2025-10-11 21:11:02 +0100

    user/pdfcpu: update to 0.11.0

--- user/pdfcpu/template.py
+++ user/pdfcpu/template.py
@@ -1,6 +1,6 @@
 pkgname = "pdfcpu"
-pkgver = "0.10.2"
-pkgrel = 4
+pkgver = "0.11.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["-ldflags", f"-X main.commit=v{pkgver}", "./cmd/pdfcpu"]
 make_check_args = ["-p", "1", "./..."]
@@ -9,7 +9,7 @@ pkgdesc = "PDF processor written in Go"
 license = "Apache-2.0"
 url = "https://pdfcpu.io"
 source = f"https://github.com/pdfcpu/pdfcpu/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "a07cc50446ef6526fa26d5fe2c9e207724971e0b6917f3d70680ec39cfc53aec"
+sha256 = "16e6e4fbcf809f9d737d8931c267220e5e4cb00fbce793eeaa4501193b954c55"
 # check may be disabled
 options = []
 
