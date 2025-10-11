319bd12624675c62e53e4e17de0ff55387636d0d
triallax <triallax@tutanota.com>
2025-10-11 18:23:26 +0100

    user/ov: update to 0.44.0

--- user/ov/template.py
+++ user/ov/template.py
@@ -1,13 +1,13 @@
 pkgname = "ov"
-pkgver = "0.42.1"
-pkgrel = 2
+pkgver = "0.44.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Terminal pager"
 license = "MIT"
 url = "https://noborus.github.io/ov"
 source = f"https://github.com/noborus/ov/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "94a712214125fd6de24f0235e7aa8aa83d9220213036c73065321f2cc9ff2483"
+sha256 = "87ee2f7d6477b02b5562d0da033e7a8f4f49c92fe7637c47985646c685474984"
 
 
 def post_install(self):
