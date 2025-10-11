af5a4efea79542b72d39720d3975eff19049751a
triallax <triallax@tutanota.com>
2025-10-11 16:30:02 +0100

    user/bacon: update to 3.18.0

--- user/bacon/template.py
+++ user/bacon/template.py
@@ -1,5 +1,5 @@
 pkgname = "bacon"
-pkgver = "3.16.0"
+pkgver = "3.18.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -8,7 +8,7 @@ pkgdesc = "Background rust code checker"
 license = "AGPL-3.0-only"
 url = "https://dystroy.org/bacon"
 source = f"https://github.com/Canop/bacon/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "cf7f3471883260f7cd56d1b2bcce713463082e64a830bb46489d7e94303b3ba0"
+sha256 = "2136e7604bf92b209c1363393142e5bb369bbe06a4f75c7d6cbe16d3437ad9a0"
 
 
 def post_install(self):
