757ae4592805c11dc80d0543c1af1d4ec71b1a10
triallax <triallax@tutanota.com>
2025-10-11 18:19:25 +0100

    user/nuclei: update to 3.4.10

--- user/nuclei/template.py
+++ user/nuclei/template.py
@@ -1,6 +1,6 @@
 pkgname = "nuclei"
-pkgver = "3.4.5"
-pkgrel = 2
+pkgver = "3.4.10"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/nuclei"]
 hostmakedepends = ["go"]
@@ -8,7 +8,7 @@ pkgdesc = "Web vulnerability scanner"
 license = "MIT"
 url = "https://github.com/projectdiscovery/nuclei"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5ea8c1f1fc932032328c1f864a85db65715b12b62a9f3ad0bb5b37d3363f2e1c"
+sha256 = "d5604ef47d31abba42814876b637105f0b1e9b9167b79206aeeeb2935f102102"
 # Nuclei tests requires network connection
 options = ["!check"]
 
