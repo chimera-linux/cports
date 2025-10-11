4a5b8bc35a29787ef871a1fd8570cbe46bcfa5e6
triallax <triallax@tutanota.com>
2025-10-11 22:28:37 +0100

    user/impala: update to 0.3.0

--- user/impala/template.py
+++ user/impala/template.py
@@ -1,13 +1,14 @@
 pkgname = "impala"
-pkgver = "0.2.4"
+pkgver = "0.3.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
+makedepends = ["rust-std"]
 pkgdesc = "TUI frontend for iwd"
 license = "GPL-3.0-only"
 url = "https://github.com/pythops/impala"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "550ce667588659af40ff2af6bddcca74fb1a990001ccba7cf16d3739717a70fc"
+sha256 = "6ebdfbd83f7c3874fd43a1b781a7c08b309a2dc5bef99e1c29c5736b616a0f33"
 # No tests are available
 options = ["!check"]
 
