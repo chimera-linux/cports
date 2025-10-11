28220ee36a25f8eb9b473366d1bff26c96d94ec5
triallax <triallax@tutanota.com>
2025-10-11 17:37:28 +0100

    user/xmpp-dns: update to 0.5.3

--- user/xmpp-dns/template.py
+++ user/xmpp-dns/template.py
@@ -1,13 +1,13 @@
 pkgname = "xmpp-dns"
-pkgver = "0.5.1"
-pkgrel = 1
+pkgver = "0.5.3"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "CLI tool to check XMPP SRV records"
 license = "BSD-2-Clause"
 url = "https://salsa.debian.org/mdosch/xmpp-dns"
 source = f"{url}/-/archive/v{pkgver}/xmpp-dns-v{pkgver}.tar.gz"
-sha256 = "f071924999071cabe49edfc9b33a54a1cfcc12c0e14d3ebc0a5a1c06a33d6049"
+sha256 = "a8bb29cde92aa80d7b4d753c225aac54a167e6324ce81717c335bdec657526e3"
 
 
 def post_install(self):
