38a8ccf9abc4322e585870c4f1e5db1ba57a01c5
triallax <triallax@tutanota.com>
2025-10-11 02:16:21 +0100

    user/tuba: update to 0.10.3

--- user/tuba/template.py
+++ user/tuba/template.py
@@ -1,6 +1,6 @@
 pkgname = "tuba"
-pkgver = "0.10.2"
-pkgrel = 1
+pkgver = "0.10.3"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Ddistro=true",
@@ -31,4 +31,4 @@ pkgdesc = "GTK fediverse client"
 license = "GPL-3.0-or-later"
 url = "https://tuba.geopjr.dev"
 source = f"https://github.com/GeopJr/Tuba/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "2adc7921ede1d6d1a8b3e6395e12235e8acbe5d9a1957e9419317101bd1c5b8a"
+sha256 = "397297bbd140a2b2c90024392dbc5f8a77bc6a10472308c7741c7913e82eb050"
