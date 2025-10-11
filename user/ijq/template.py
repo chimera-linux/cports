78156d146f782788998f326f2c95055db9d7b024
triallax <triallax@tutanota.com>
2025-10-11 02:06:43 +0100

    user/ijq: update to 1.2.0

--- user/ijq/template.py
+++ user/ijq/template.py
@@ -1,6 +1,6 @@
 pkgname = "ijq"
-pkgver = "1.1.2"
-pkgrel = 13
+pkgver = "1.2.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["-ldflags", f"-X main.Version={pkgver}"]
 hostmakedepends = ["go", "scdoc"]
@@ -9,7 +9,7 @@ pkgdesc = "Interactive jq repl to preview filters"
 license = "GPL-3.0-or-later"
 url = "https://git.sr.ht/~gpanders/ijq"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "dd5055e7a740c54a32043b744f061b1a00a4d2f97f6c6214c2109fd22491b9f3"
+sha256 = "11ac7f233bac6dd8fa97399c90bcf4ffb311367eb31a95ddcf5f36708561a0f0"
 
 
 def post_build(self):
