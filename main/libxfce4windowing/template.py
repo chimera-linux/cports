188566520a1ba8bc386fe6d36b2effcde903be0a
triallax <triallax@tutanota.com>
2025-10-11 20:13:09 +0100

    main/libxfce4windowing: update to 4.20.4

--- main/libxfce4windowing/template.py
+++ main/libxfce4windowing/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxfce4windowing"
-pkgver = "4.20.3"
+pkgver = "4.20.4"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-tests"]
@@ -26,7 +26,7 @@ pkgdesc = "Xfce windowing abstraction library"
 license = "LGPL-2.1-or-later"
 url = "https://docs.xfce.org/xfce/libxfce4windowing/start"
 source = f"$(XFCE_SITE)/xfce/libxfce4windowing/{pkgver[:-2]}/libxfce4windowing-{pkgver}.tar.bz2"
-sha256 = "df2419a6bd960c0bfac3307eff593050857524642597eb35a26fb4f8261a017b"
+sha256 = "db467f9ac4bac8f1c4e82667902841fc0957af835c29603d6659a57440b6f8cb"
 options = ["!cross"]
 
 
