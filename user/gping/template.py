8ece2ea3961e5ca6cd1d90a642e65f43936aeafd
triallax <triallax@tutanota.com>
2025-10-11 20:51:36 +0100

    user/gping: update to 1.20.1

--- user/gping/template.py
+++ user/gping/template.py
@@ -1,5 +1,5 @@
 pkgname = "gping"
-pkgver = "1.19.0"
+pkgver = "1.20.1"
 pkgrel = 0
 build_style = "cargo"
 make_check_args = [
@@ -7,12 +7,13 @@ make_check_args = [
     "--skip=test_integration",
     "--skip=linux::tests::test_linux_detection",
 ]
-hostmakedepends = ["cargo-auditable"]
+hostmakedepends = ["cargo-auditable", "pkgconf"]
+makedepends = ["rust-std", "libgit2-devel"]
 pkgdesc = "TUI ping tool with a graph"
 license = "MIT"
 url = "https://github.com/orf/gping"
 source = f"{url}/archive/refs/tags/gping-v{pkgver}.tar.gz"
-sha256 = "a979c9a8c7a1a540bb48a1e90bb7ad294560bddc16ca977bc8475fb14f20155d"
+sha256 = "0df965111429d5fcef832a4ff23b452a1ec8f683d51ed31ce9b10902c0a18a9c"
 
 
 def install(self):
