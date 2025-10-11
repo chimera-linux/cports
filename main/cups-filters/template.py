c826dda09b1503d3f4f920e3c78818cbc0d40538
ava <ava@systemausfall.org>
2025-10-11 09:55:59 +0200

    main/cups-filters: install_if brlaser
    brlaser requires cups-filters to be useful.
    The driver will technically do it's job but cups will fail to execute
    basically anything, since the document format isn't supported without
    the filters.

--- main/cups-filters/template.py
+++ main/cups-filters/template.py
@@ -22,13 +22,13 @@ makedepends = [
     "linux-headers",
 ]
 depends = ["cups"]
+install_if = [self.with_pkgver("brlaser")]
 pkgdesc = "Filters, backends, utilities for CUPS"
 license = "Apache-2.0 AND custom:gpl-exception"
 url = "https://github.com/OpenPrinting/cups-filters"
 source = f"https://github.com/OpenPrinting/cups-filters/releases/download/{pkgver}/cups-filters-{pkgver}.tar.xz"
 sha256 = "39e71de3ce06762b342749f1dc7cba6817738f7bf4d322c1bb9ab10b8569ab80"
 hardening = ["vis", "cfi"]
-
 
 def post_install(self):
     self.install_license("COPYING")
