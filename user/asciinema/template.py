c401d6fa723d4b1ab987dcf4d96c23cf9ecf4f07
triallax <triallax@tutanota.com>
2025-10-11 16:39:34 +0100

    user/asciinema: update to 3.0.0

--- user/asciinema/template.py
+++ user/asciinema/template.py
@@ -1,16 +1,12 @@
 pkgname = "asciinema"
-pkgver = "2.4.0"
-pkgrel = 1
-build_style = "python_pep517"
-hostmakedepends = [
-    "python-build",
-    "python-installer",
-    "python-setuptools",
-]
-depends = ["python"]
-checkdepends = ["python-pytest"]
+pkgver = "3.0.0"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable"]
+makedepends = ["rust-std"]
+checkdepends = ["python"]
 pkgdesc = "Terminal session recorder"
-license = "GPL-3.0-only"
+license = "GPL-3.0-or-later"
 url = "https://github.com/asciinema/asciinema"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "b0e05f0b5ae7ae4e7186c6bd824e6d670203bb24f1c89ee52fc8fae7254e6091"
+sha256 = "f44feaa1bc150e7964635dc4714fd86089a968587fed81dccf860ee7b64617ca"
