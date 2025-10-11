a3b8a7ccc15216fa3dee389a6fde8ab3e62583b1
triallax <triallax@tutanota.com>
2025-10-11 02:26:12 +0100

    user/tailspin: update to 5.5.0

--- user/tailspin/template.py
+++ user/tailspin/template.py
@@ -1,5 +1,5 @@
 pkgname = "tailspin"
-pkgver = "5.4.5"
+pkgver = "5.5.0"
 pkgrel = 0
 build_style = "cargo"
 make_check_args = ["--bins"]  # disable integration tests
@@ -9,7 +9,7 @@ pkgdesc = "Log file highlighter"
 license = "MIT"
 url = "https://github.com/bensadeh/tailspin"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "8dec5333c8f3e9cb6f29b4823482634b3c10e317aae12d0844d73ccfb1307a13"
+sha256 = "e9d7cefb865585bb048a2ccbfcc1f900ae344134d71132a7c48ee0d5af09cdaf"
 
 
 def post_install(self):
