2072db817ed963c2643aab9c8cd265155d61549e
q66 <q66@chimera-linux.org>
2025-10-12 19:25:52 +0200

    main/gnome-weather: update to 49.0

--- main/gnome-weather/template.py
+++ main/gnome-weather/template.py
@@ -1,18 +1,20 @@
 pkgname = "gnome-weather"
-pkgver = "48.0"
+pkgver = "49.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "desktop-file-utils",
     "gettext",
-    "glib-devel",
+    "gobject-introspection",
     "gtk+3-update-icon-cache",
     "meson",
     "pkgconf",
+    "typescript",
 ]
 makedepends = [
     "geoclue-devel",
     "gjs-devel",
+    "gobject-introspection-devel",
     "libadwaita-devel",
     "libgweather-devel",
 ]
@@ -23,4 +25,4 @@ url = "https://wiki.gnome.org/Apps/Weather"
 source = (
     f"$(GNOME_SITE)/gnome-weather/{pkgver[:-2]}/gnome-weather-{pkgver}.tar.xz"
 )
-sha256 = "4c0569b3d815ae2f9416d47134cbd3056340640ff186d31a94c865813b4bdbb5"
+sha256 = "ee1f76b85eba9db0c8d5c02041a9d85ecdd22abb5ec6bb3ff32225929c0fce5f"
