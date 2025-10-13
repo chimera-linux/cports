dbaf8e7c63f20b08300730e2e1760d47eec5cf45
q66 <q66@chimera-linux.org>
2025-10-13 15:12:51 +0200

    main/python-zstandard: update to 0.25.0

--- main/python-zstandard/template.py
+++ main/python-zstandard/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-zstandard"
-pkgver = "0.23.0"
-pkgrel = 1
+pkgver = "0.25.0"
+pkgrel = 0
 build_style = "python_pep517"
 make_build_args = ["--skip-dependency-check"]
 hostmakedepends = [
@@ -17,7 +17,7 @@ pkgdesc = "Python bindings to the Zstandard compression library"
 license = "BSD-3-Clause"
 url = "https://github.com/indygreg/python-zstandard"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "f29233338bcef11f233737eb58aba85074f0fd3163bec1a20303de1270e6fb16"
+sha256 = "851846ffe25681f7936ab2fd89130acf62b214a77b8c54e4319824f3510ab395"
 # tests fail to find internal modules due to cwd
 options = ["!check"]
 
