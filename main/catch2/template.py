b96c92b5991a09885881afbc8b080a02d4210379
q66 <q66@chimera-linux.org>
2025-10-13 17:12:42 +0200

    main/catch2: update to 3.11.0

--- main/catch2/template.py
+++ main/catch2/template.py
@@ -1,5 +1,5 @@
 pkgname = "catch2"
-pkgver = "3.8.1"
+pkgver = "3.11.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -8,13 +8,15 @@ configure_args = [
     "-DCATCH_DEVELOPMENT_BUILD=ON",
     "-DCATCH_ENABLE_WERROR=OFF",
 ]
+# differing outputs due to the failed test build patch
+make_check_args = ["-E", "(ApprovalTests)"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 checkdepends = ["python"]
 pkgdesc = "C++-based test framework"
 license = "BSL-1.0"
 url = "https://github.com/catchorg/Catch2"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "18b3f70ac80fccc340d8c6ff0f339b2ae64944782f8d2fca2bd705cf47cadb79"
+sha256 = "82fa1cb59dc28bab220935923f7469b997b259eb192fb9355db62da03c2a3137"
 # vis breaks symbols
 hardening = ["!vis"]
 
