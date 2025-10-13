39a850e1e7930e48f05cbd2d27b3c83896597374
q66 <q66@chimera-linux.org>
2025-10-13 17:00:52 +0200

    main/cairomm: update to 1.19.0

--- main/cairomm/template.py
+++ main/cairomm/template.py
@@ -1,6 +1,6 @@
 pkgname = "cairomm"
-pkgver = "1.18.0"
-pkgrel = 5
+pkgver = "1.19.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dboost-shared=true"]
 hostmakedepends = ["meson", "pkgconf"]
@@ -10,7 +10,7 @@ pkgdesc = "C++ bindings to Cairo graphics library"
 license = "LGPL-2.1-or-later"
 url = "https://www.cairographics.org/cairomm"
 source = f"https://cairographics.org/releases/cairomm-{pkgver}.tar.xz"
-sha256 = "b81255394e3ea8e8aa887276d22afa8985fc8daef60692eb2407d23049f03cfb"
+sha256 = "8b14f03a0e5178c7ff8f7b288cb342a61711c84c9fbed6e663442cfcc873ce5b"
 
 
 @subpackage("cairomm-devel")
