fb573cba5df6eb1949dc65d41a9f67d12b255b83
q66 <q66@chimera-linux.org>
2025-10-13 17:14:00 +0200

    main/ccid: update to 1.7.0

--- main/ccid/template.py
+++ main/ccid/template.py
@@ -1,5 +1,5 @@
 pkgname = "ccid"
-pkgver = "1.6.2"
+pkgver = "1.7.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -22,10 +22,4 @@ pkgdesc = "PC/SC driver to support CCID compliant readers"
 license = "LGPL-2.1-or-later"
 url = "https://ccid.apdu.fr"
 source = f"{url}/files/ccid-{pkgver}.tar.xz"
-sha256 = "41958410950157e622f9d91c9e78c7b708db74e22f71190c581d24d20564d449"
-
-
-def post_install(self):
-    self.install_file(
-        "src/92_pcscd_ccid.rules", "usr/lib/udev/rules.d", mode=0o644
-    )
+sha256 = "2a040141109c1652caf06b41d9ed580b3ce706d478ebafd323b9085eb04e45a1"
