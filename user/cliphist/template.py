747b9ee6eecfcf39934ad925b1ca058bbff144f9
Matthias Nyman <natthias@proton.me>
2025-10-13 20:40:59 +0200

    user/cliphist: update to 0.7.0, enable tests

--- user/cliphist/template.py
+++ user/cliphist/template.py
@@ -1,6 +1,6 @@
 pkgname = "cliphist"
-pkgver = "0.6.1"
-pkgrel = 10
+pkgver = "0.7.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 depends = ["wl-clipboard", "xdg-utils"]
@@ -8,6 +8,4 @@ pkgdesc = "Wayland clipboard manager"
 license = "GPL-3.0-only"
 url = "https://github.com/sentriz/cliphist"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "db0f25ba3e9173dcb257d66cb1293e277a8bdfed11cc8d6b38b7473b0947781d"
-# cannot use nopTestdeps{} blabla ...
-options = ["!check"]
+sha256 = "8d7dc7b4495e5812b5613274c250ba5d3900933d78888ce7921c01247f191cc8"
