cbcbd73f0544b17142effe07da0b6303946a122f
q66 <q66@chimera-linux.org>
2025-10-13 17:00:17 +0200

    main/cage: update to 0.2.1

--- main/cage/template.py
+++ main/cage/template.py
@@ -1,14 +1,14 @@
 pkgname = "cage"
-pkgver = "0.2.0"
+pkgver = "0.2.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "scdoc", "wayland-progs"]
-makedepends = ["wayland-protocols", "wlroots0.18-devel"]
+makedepends = ["wayland-protocols", "wlroots0.19-devel"]
 pkgdesc = "Kiosk compositor for Wayland"
 license = "MIT"
 url = "https://www.hjdskes.nl/projects/cage"
 source = f"https://github.com/cage-kiosk/cage/releases/download/v{pkgver}/cage-{pkgver}.tar.gz"
-sha256 = "557c194d18af7202a9ec2e8be6dd7129f6c16d0f4528f4079ba26ccd57b6ef88"
+sha256 = "fc1238e3aa5b82787a95d49cb3e1bac0671e4d3a40090087848f43f3e1f63a98"
 hardening = ["vis", "cfi"]
 
 
