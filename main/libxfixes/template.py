023d4116a3948669e56b71853cc51406bca98c82
q66 <q66@chimera-linux.org>
2025-10-13 14:37:18 +0200

    main/libxfixes: update to 6.0.2

--- main/libxfixes/template.py
+++ main/libxfixes/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxfixes"
-pkgver = "6.0.1"
+pkgver = "6.0.2"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf", "slibtool", "xorg-util-macros"]
@@ -8,7 +8,7 @@ pkgdesc = "Xfixes library and extension of X RandR"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXfixes-{pkgver}.tar.gz"
-sha256 = "e69eaa321173c748ba6e2f15c7cf8da87f911d3ea1b6af4b547974aef6366bec"
+sha256 = "041331b8e6e36038b3bf836785b6b175ec8515f964c9e4e3316b3bfed0f53ac7"
 
 
 def post_install(self):
