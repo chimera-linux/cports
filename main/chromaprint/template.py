ccbbbc143b526faf526c75e095c763bbe7a73a67
q66 <q66@chimera-linux.org>
2025-10-13 17:16:55 +0200

    main/chromaprint: update to 1.6.0

--- main/chromaprint/template.py
+++ main/chromaprint/template.py
@@ -1,20 +1,18 @@
 pkgname = "chromaprint"
-pkgver = "1.5.1"
-pkgrel = 2
+pkgver = "1.6.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
     "-DBUILD_TOOLS=ON",
 ]
-make_check_target = "check"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = ["fftw-devel", "ffmpeg-devel"]
 pkgdesc = "Library that extracts fingerprints from any audio source"
 license = "MIT AND LGPL-2.1-only"
 url = "https://acoustid.org/chromaprint"
 source = f"https://github.com/acoustid/chromaprint/releases/download/v{pkgver}/chromaprint-{pkgver}.tar.gz"
-sha256 = "a1aad8fa3b8b18b78d3755b3767faff9abb67242e01b478ec9a64e190f335e1c"
-hardening = ["vis", "cfi"]
+sha256 = "9d33482e56a1389a37a0d6742c376139fa43e3b8a63d29003222b93db2cb40da"
 
 
 def post_install(self):
