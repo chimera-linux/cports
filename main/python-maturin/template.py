1404840c79bedcaeadb77fae8293f483e1bba274
q66 <q66@chimera-linux.org>
2025-10-13 15:18:16 +0200

    main/python-maturin: update to 1.9.6

--- main/python-maturin/template.py
+++ main/python-maturin/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-maturin"
-pkgver = "1.9.3"
+pkgver = "1.9.6"
 pkgrel = 0
 build_style = "python_pep517"
 make_build_env = {
@@ -21,7 +21,7 @@ pkgdesc = "Tool for building and publishing Rust-based Python packages"
 license = "Apache-2.0 OR MIT"
 url = "https://www.maturin.rs"
 source = f"https://github.com/PyO3/maturin/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1a4a87224a34a97a4322bd123487e9c6f2d2091bac4fe469618b92a06aad3492"
+sha256 = "c8aef8af6cd3d5b3331191b21191ec92d7b4ee0633e0799351a01af1c5ea2a6c"
 # yeah no
 options = ["!check"]
 
