90fd618cf29534a9977295490a081ee19706153b
q66 <q66@chimera-linux.org>
2025-10-13 15:23:21 +0200

    main/crispy-doom: update to 7.1

--- main/crispy-doom/template.py
+++ main/crispy-doom/template.py
@@ -1,5 +1,5 @@
 pkgname = "crispy-doom"
-pkgver = "7.0"
+pkgver = "7.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf"]
@@ -14,5 +14,5 @@ pkgdesc = "Limit-removing enhanced-resolution Doom source port"
 license = "GPL-2.0-or-later"
 url = "https://github.com/fabiangreffrath/crispy-doom"
 source = f"{url}/archive/crispy-doom-{pkgver}.tar.gz"
-sha256 = "25eea88fdbe1320ad0d1a3e0ed66ae8d985c39b79e442beab5fc36d9d5ddfc42"
+sha256 = "f0eb02afb81780165ddc81583ed5648cbee8b3205bcc27e181b3f61eb26f8416"
 hardening = ["vis", "!cfi", "!int"]
