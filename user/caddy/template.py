826b795f01fa5a1fd8d7af74893488b569b828ff
elivance <silentgaze@outlook.com>
2025-10-15 00:29:08 +0100

    user/caddy: add completions for bash/fish/zsh

--- user/caddy/template.py
+++ user/caddy/template.py
@@ -1,6 +1,6 @@
 pkgname = "caddy"
 pkgver = "2.10.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X github.com/caddyserver/caddy/v2.CustomVersion=v{pkgver}",
@@ -15,9 +15,25 @@ license = "Apache-2.0"
 url = "https://caddyserver.com"
 source = f"https://github.com/caddyserver/caddy/archive/v{pkgver}.tar.gz"
 sha256 = "f63f46b7ae68ced0a5c2e31df1b6dfc7656117d162a1bc7fed4bd4afd14ddc8f"
+# generates completions with host binary
+options = ["!check"]
+
+
+def post_build(self):
+    for shell in ["bash", "fish", "zsh"]:
+        with open(self.cwd / f"caddy.{shell}", "w") as outf:
+            self.do(
+                "build/caddy",
+                "completion",
+                shell,
+                stdout=outf,
+            )
 
 
 def post_install(self):
     self.install_sysusers(self.files_path / "sysusers.conf")
     self.install_tmpfiles(self.files_path / "tmpfiles.conf")
     self.install_service(self.files_path / "caddy")
+
+    for shell in ["bash", "fish", "zsh"]:
+        self.install_completion(f"caddy.{shell}", shell)
