dcdb1a868c6b8c9cc9f72a3a3ce28a19399e79bc
JC Grünhage <jan.christian@gruenhage.xyz>
2025-10-15 17:29:49 +0200

    user/k9s: update to 0.50.15

--- user/k9s/template.py
+++ user/k9s/template.py
@@ -1,6 +1,6 @@
 pkgname = "k9s"
-pkgver = "0.50.13"
-pkgrel = 1
+pkgver = "0.50.15"
+pkgrel = 0
 build_style = "go"
 make_build_args = [f"-ldflags= -X github.com/derailed/k9s/cmd.version={pkgver}"]
 hostmakedepends = ["go"]
@@ -8,7 +8,7 @@ pkgdesc = "Terminal UI to interact with Kubernetes clusters"
 license = "Apache-2.0"
 url = "https://k9scli.io"
 source = f"https://github.com/derailed/k9s/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5aa5b3142bb66a0a73fe0154cbe54a0eeead46d9406dca3c06835549cc05b3ca"
+sha256 = "f97774984c31bf9b64224b0ab5003072734420007d37c1ad2c92f1361de4f23d"
 # cross: generates completions with host binary
 options = ["!cross"]
 
