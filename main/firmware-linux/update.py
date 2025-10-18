1dcc69c5fa84297a00d9b0668844c5bbbbff8531
q66 <q66@chimera-linux.org>
2025-10-18 17:34:44 +0200

    main/firmware-linux: fix update check

--- main/firmware-linux/update.py
+++ main/firmware-linux/update.py
@@ -1,2 +1,2 @@
-url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/refs/tags"
-pattern = r">linux-firmware-([\d]+).tar.gz<"
+url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/info/refs"
+pattern = r"refs/tags/([\d]+)(?!^)"
