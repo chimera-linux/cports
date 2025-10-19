06bf3ce128cb75838a5a00d0a3dbad7ca0f8495c
triallax <triallax@tutanota.com>
2025-10-19 21:07:47 +0100

    user/perl-parallel-forkmanager: update to 2.04

--- user/perl-parallel-forkmanager/template.py
+++ user/perl-parallel-forkmanager/template.py
@@ -1,5 +1,5 @@
 pkgname = "perl-parallel-forkmanager"
-pkgver = "2.03"
+pkgver = "2.04"
 pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["perl"]
@@ -13,4 +13,4 @@ pkgdesc = "Parallel processing fork manager"
 license = "Artistic-1.0-Perl"
 url = "https://metacpan.org/pod/Parallel::ForkManager"
 source = f"$(CPAN_SITE)/Parallel/Parallel-ForkManager-{pkgver}.tar.gz"
-sha256 = "c0e0bead458224b9ac5bb32ed2b1fa088963b565521c1bb1a6a3566d522c2e35"
+sha256 = "606894fc2e9f7cd13d9ec099aaac103a8f0943d1d80c2c486bae14730a39b7fc"
