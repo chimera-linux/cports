9785b01e767512b688d5293892d1f72986ad880e
triallax <triallax@tutanota.com>
2025-10-19 03:38:09 +0100

    user/moor: update to 2.6.1

--- user/moor/template.py
+++ user/moor/template.py
@@ -1,5 +1,5 @@
 pkgname = "moor"
-pkgver = "2.5.0"
+pkgver = "2.6.1"
 pkgrel = 0
 build_style = "go"
 make_build_args = [f"-ldflags=-X main.versionString=v{pkgver}", "./cmd/moor"]
@@ -9,7 +9,7 @@ pkgdesc = "Terminal pager program"
 license = "BSD-2-Clause"
 url = "https://github.com/walles/moor"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1255f622811554d81c285deeeb9de5b4eef867014ebef55d7c076b6dfaf8c00e"
+sha256 = "19067bcb17c65ae6da8f80c3525e0859a388155025174ac65bc55d247e3b1dd4"
 
 
 def install(self):
