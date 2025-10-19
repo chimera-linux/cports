e0e596504ae83a83c942df3fc979089185ec0633
triallax <triallax@tutanota.com>
2025-10-19 21:13:13 +0100

    user/python-emoji: update to 2.15.0

--- user/python-emoji/template.py
+++ user/python-emoji/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-emoji"
-pkgver = "2.14.1"
-pkgrel = 1
+pkgver = "2.15.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,7 +13,7 @@ pkgdesc = "Python library for emoji support"
 license = "BSD-3-Clause"
 url = "https://github.com/carpedm20/emoji"
 source = f"$(PYPI_SITE)/e/emoji/emoji-{pkgver}.tar.gz"
-sha256 = "f8c50043d79a2c1410ebfae833ae1868d5941a67a6cd4d18377e2eb0bd79346b"
+sha256 = "eae4ab7d86456a70a00a985125a03263a5eac54cd55e51d7e184b1ed3b6757e4"
 
 
 def post_install(self):
