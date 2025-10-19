66ad78de23a328e351d09695f67e7cd66ce96ba4
triallax <triallax@tutanota.com>
2025-10-19 22:23:24 +0100

    user/python-cachetools: update to 6.2.1

--- user/python-cachetools/template.py
+++ user/python-cachetools/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-cachetools"
-pkgver = "6.2.0"
+pkgver = "6.2.1"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -12,7 +12,7 @@ pkgdesc = "Extensible memoizing collections and decorators"
 license = "MIT"
 url = "https://github.com/tkem/cachetools"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f2b8a896891276a3b3dda4f78d49c93e5dbdb08757c02098d4757c8d6cec2fc1"
+sha256 = "2d5ddb99ac024a693d729bbbcf86a6a811686813fe1ed9c3f83f6385beda063e"
 
 
 def post_install(self):
