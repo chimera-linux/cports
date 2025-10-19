8762359625db68f32db5160557e49d537b51a089
triallax <triallax@tutanota.com>
2025-10-19 04:17:37 +0100

    user/bacon: update to 3.19.0

--- user/bacon/template.py
+++ user/bacon/template.py
@@ -1,5 +1,5 @@
 pkgname = "bacon"
-pkgver = "3.18.0"
+pkgver = "3.19.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -8,7 +8,7 @@ pkgdesc = "Background rust code checker"
 license = "AGPL-3.0-only"
 url = "https://dystroy.org/bacon"
 source = f"https://github.com/Canop/bacon/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "2136e7604bf92b209c1363393142e5bb369bbe06a4f75c7d6cbe16d3437ad9a0"
+sha256 = "2c49ca02687391d425f2cc9a19cae8227338def2d689d55ff5970cb70fd2b7f6"
 
 if self.profile().arch == "loongarch64":
     broken = "busted rustix"
