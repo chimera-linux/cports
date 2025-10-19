3e30c7ebf2130975faee920f7e89a137f1b61cb6
triallax <triallax@tutanota.com>
2025-10-19 04:23:22 +0100

    user/bluetuith: update to 0.2.5

--- user/bluetuith/template.py
+++ user/bluetuith/template.py
@@ -1,6 +1,6 @@
 pkgname = "bluetuith"
-pkgver = "0.2.3"
-pkgrel = 13
+pkgver = "0.2.5"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 depends = ["bluez"]
@@ -8,7 +8,7 @@ pkgdesc = "TUI bluetooth manager"
 license = "MIT"
 url = "https://github.com/darkhz/bluetuith"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "ff4ca6e5fda87b33c472b703c81b4000d1df4efb2bdc0af1762e4a3e3c507228"
+sha256 = "9e48728843d1e50c8199e532a714c989681f7ab041fb64a7cb093530383e86b0"
 
 
 def post_install(self):
