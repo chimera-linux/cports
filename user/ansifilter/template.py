56e780c23c63b01b249e58472dca1c32bd3b6720
triallax <triallax@tutanota.com>
2025-10-22 00:46:04 +0100

    user/ansifilter: update to 2.22

--- user/ansifilter/template.py
+++ user/ansifilter/template.py
@@ -1,6 +1,6 @@
 pkgname = "ansifilter"
-pkgver = "2.21"
-pkgrel = 3
+pkgver = "2.22"
+pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja"]
 makedepends = ["boost-devel", "qt6-qtbase-devel"]
@@ -8,7 +8,7 @@ pkgdesc = "ANSI escape codes parser"
 license = "GPL-3.0-only"
 url = "https://gitlab.com/saalen/ansifilter"
 source = f"{url}/-/archive/{pkgver}/ansifilter-{pkgver}.tar.gz"
-sha256 = "d3dd7503044c91c70e8b4c99489cb222cf831974bb7edc6b52acbd0a21742f50"
+sha256 = "cf5b95564d95d398e78071f147ee3cbf850e6dc8226a86ecff2de4356f19ff66"
 hardening = ["vis", "cfi"]
 
 
