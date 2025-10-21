23529af69ae9287c12a7eaec8f8fffe909af11a9
triallax <triallax@tutanota.com>
2025-10-22 00:53:53 +0100

    user/iwmenu: update to 0.3.0

--- user/iwmenu/template.py
+++ user/iwmenu/template.py
@@ -1,5 +1,5 @@
 pkgname = "iwmenu"
-pkgver = "0.2.0"
+pkgver = "0.3.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -9,7 +9,7 @@ pkgdesc = "Menu-driven Wi-Fi management interface"
 license = "GPL-3.0-or-later"
 url = "https://github.com/e-tho/iwmenu"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "bc178eb9bdb63b4221a539fc8a2b3710528623f3b4ba87c54b21f0bf6132ba0e"
+sha256 = "9537bf39032a229558e34f7e98700bb0f1924d818aa4bc55ee92c7ddf4bd73b9"
 # no tests defined
 options = ["!check"]
 
