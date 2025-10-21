756c785d78ebbf94bf571af92652bd6412e4774a
q66 <q66@chimera-linux.org>
2025-10-21 10:01:46 +0200

    main/rust-bootstrap: update to 1.89.0

--- main/rust-bootstrap/template.py
+++ main/rust-bootstrap/template.py
@@ -1,5 +1,5 @@
 pkgname = "rust-bootstrap"
-pkgver = "1.88.0"
+pkgver = "1.89.0"
 pkgrel = 0
 # satisfy revdeps
 makedepends = ["zlib-ng-compat", "ncurses-libs", "zstd"]
@@ -18,38 +18,38 @@ options = ["!strip"]
 match self.profile().arch:
     case "aarch64":
         sha256 = [
-            "b4d100f5cdf86f9fd6571b9cec7df7358b8886da76f4a4fbce2fa7ec418776ab",
-            "8177b2cb1ff75a882cba27ce9edead78b907696ac0c602d93d7484cc34f10f5b",
+            "b34bccb954ca2e518f04a1c2aef84ac42911ca2e4eb715e43e5a64e9ded7ca21",
+            "ee5eedc6362ab92b7203188f96ed2d784beed700ff8740f432538c3e5d285abb",
         ]
     case "loongarch64":
         sha256 = [
-            "ea416c46066562f3d2558e8bcb8104a6ebc8829583f5af9f5de940d2d88be6d8",
-            "b157fd911fcd8b24eadfe6ecbecdde17eda9776e67fcec34b44179eefc08df6a",
+            "809d6f9d030f859740ed78d330284346f47538a3ecd0549450c6898f2dbae3d3",
+            "e703dbb9daa4e9eff0b1b47903d31963a0385972c824704e50b5333b317e9182",
         ]
     case "ppc64le":
         sha256 = [
-            "8cdfd3e4cc8510cb09cbd013373f26af0a5ca22c6690ff26072dcc90144027fd",
-            "c4a8aa092b0dced816779cf3eceb0a41f9590d657a70c442c722418a4abaf966",
+            "81d680c7bda50183b43fdd7d3c4130a621a25235a3deca758761eb745ddceab7",
+            "99db29eab63fa04781eb780618acdb9337b85d7a818b7e361b714748821cf53e",
         ]
     case "ppc64":
         sha256 = [
-            "8d592d9aaef29ca141f08a19ac759bef437df97180314f5c20ac7afb48ac7ed9",
-            "4dc20d21a64d8e58a241e133bdd4b842f3ec8af3267440ddeb34c1c0f563325e",
+            "b21468e8b8454529b41f2937443d3ac81fbbdbceccedd0f8d4338556709c1f7f",
+            "e4174d8d05f1b74ad172f3e1c356b410b80091421628d42f01909489187a0a5e",
         ]
     case "ppc":
         sha256 = [
-            "b7ac9ddbbd809da7cfa0c23dae4a14806bc4f62349c81f143ace2473d89f8e3b",
-            "12dd0563682a72b336964f4fb2793ffbb5bd71f4809cdced44be1b5a7bc973b1",
+            "ec108c1f1cc7db9bc181f9e7aefb1d6222e8bfe28b2c3c4839764821eec1f803",
+            "44aab5420889a76d521300ef6e55fdcf81331caf9fa19706b23c95cca6bdcfec",
         ]
     case "riscv64":
         sha256 = [
-            "9fa7bb602f4a9bd30702059936487338040b1a5319d3d3efbe7925eab5b19437",
-            "dcb0bf6ddb3d82142933e10ffd106e585ccda908638836d36feda5c592cd052d",
+            "b410dcc66bcf48dea39c1067d01816098a1c1c2201a05f61dbb30d8f0e09d97c",
+            "5e64c9fcd1a8ade90601ade976a715b8b9e31ee340e891d0803a34a849a004de",
         ]
     case "x86_64":
         sha256 = [
-            "c6e5815140ae9328307ad87dd35da41e2f053bb2d446b421a3fd1731581d71f7",
-            "f0b663f73c32d625028d41bfb61e24b7ccf56d3bc2816aaa88bc34dd46f9f3b4",
+            "65e1658b81eac1dfccdd1bcf39c113a7e0d84e854a0f00413f892b8c2ae90a61",
+            "329729cc92c7aa47b2edef14606a53df78cf200bce15715426dc92673208d9a3",
         ]
     case _:
         broken = f"not yet built for {self.profile().arch}"
