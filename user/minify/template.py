286757db3212e370601b712e0e16badf41aba3fb
triallax <triallax@tutanota.com>
2025-10-26 00:20:24 +0100

    user/minify: update to 2.24.5

--- user/minify/template.py
+++ user/minify/template.py
@@ -1,5 +1,5 @@
 pkgname = "minify"
-pkgver = "2.24.4"
+pkgver = "2.24.5"
 pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/minify"]
@@ -8,7 +8,7 @@ pkgdesc = "Minifier for web formats"
 license = "MIT"
 url = "https://github.com/tdewolff/minify"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "0e5d728bbbe594389598d35c351c787619dc47fd23a01268e38b5d75c1a1c721"
+sha256 = "4f384f6d7fd9509026f582b3a1e4afb30c1d8855efbc607a15a9943d9a73e362"
 
 
 def post_install(self):
