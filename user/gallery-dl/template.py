e9be2692a677f5ccc13ee68e2115628311d7f0ad
triallax <triallax@tutanota.com>
2025-10-26 00:36:32 +0100

    user/gallery-dl: update to 1.30.10

--- user/gallery-dl/template.py
+++ user/gallery-dl/template.py
@@ -1,5 +1,5 @@
 pkgname = "gallery-dl"
-pkgver = "1.30.9"
+pkgver = "1.30.10"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
@@ -19,7 +19,7 @@ pkgdesc = "CLI program to download image galleries/collections"
 license = "GPL-2.0-or-later"
 url = "https://github.com/mikf/gallery-dl"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "cfe5bbfc19aae100cea58bc3a7c83edabf6f5c5585d7b2228ebd074f867db2b0"
+sha256 = "e5f6c1bbf1f6478909c291042f4d763f4ba92f695d29c920bf7266fc0d65d9dc"
 
 
 def pre_build(self):
