ac8cd4177296ae5345858e292328d3d2b3971e4b
triallax <triallax@tutanota.com>
2025-10-26 00:18:09 +0100

    user/gitoxide: update to 0.46.0, auditable build

--- user/gitoxide/template.py
+++ user/gitoxide/template.py
@@ -1,5 +1,5 @@
 pkgname = "gitoxide"
-pkgver = "0.45.0"
+pkgver = "0.46.0"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = [
@@ -8,7 +8,7 @@ make_build_args = [
 ]
 make_install_args = [*make_build_args]
 make_check_args = [*make_install_args]
-hostmakedepends = ["cargo", "pkgconf"]
+hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = [
     "curl-devel",
     "rust-std",
@@ -18,7 +18,7 @@ pkgdesc = "Rust implementation of Git"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/Byron/gitoxide"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1914573c9efa69a3a8b8ef97483a7756a4d5ab433a93e43502fb18332f77b623"
+sha256 = "68a60cae90e0882cb3e1e699bc1c7e64902b632cc30209f60444c8ca8b2d820e"
 
 
 def post_install(self):
