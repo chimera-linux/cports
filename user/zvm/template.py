1805df6e5cf1833f04ab6626448a4c3a03a70761
triallax <triallax@tutanota.com>
2025-10-25 22:02:08 +0100

    user/zvm: update to 0.8.10

--- user/zvm/template.py
+++ user/zvm/template.py
@@ -1,13 +1,13 @@
 pkgname = "zvm"
-pkgver = "0.8.8"
-pkgrel = 1
+pkgver = "0.8.10"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Zig version manager"
 license = "MIT"
 url = "https://github.com/tristanisham/zvm"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "aafeb40122acd549817f106d072a96910a5440feec41d0af9f99474d3e498a09"
+sha256 = "96d46c820cd2624b78ccbe49217e56c9c4e3a4242c4f9102fe5c59165777c9a3"
 
 
 def post_install(self):
