86d1de83cc1e62d5cca2ecd304f4fd8e7081f758
triallax <triallax@tutanota.com>
2025-10-26 01:11:41 +0000

    user/supertuxkart: update to 1.5

--- user/supertuxkart/template.py
+++ user/supertuxkart/template.py
@@ -1,8 +1,7 @@
 pkgname = "supertuxkart"
-pkgver = "1.4"
-pkgrel = 5
+pkgver = "1.5"
+pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DCMAKE_POLICY_VERSION_MINIMUM=3.5"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = [
     "bluez-devel",
@@ -25,8 +24,8 @@ depends = [self.with_pkgver("supertuxkart-data")]
 pkgdesc = "Kart racing game"
 license = "GPL-3.0-or-later"
 url = "https://supertuxkart.net"
-source = f"https://github.com/supertuxkart/stk-code/releases/download/{pkgver}/SuperTuxKart-{pkgver}-src.tar.xz"
-sha256 = "9890392419baf4715313f14d5ad60746f276eed36eb580636caf44e2532c0f03"
+source = f"https://github.com/supertuxkart/stk-code/releases/download/{pkgver}/SuperTuxKart-{pkgver}-src.tar.gz"
+sha256 = "33cf8841e4ff4082d80b9248014295bbbea61d14683e86dff100e3ab8f7b27cb"
 # breaks bullet
 hardening = ["!int"]
 
