bf2ea890d0361ac68dacf9dd5756f181e13e0eb9
triallax <triallax@tutanota.com>
2025-10-26 01:05:59 +0100

    user/thin-provisioning-tools: update to 1.3.0

--- user/thin-provisioning-tools/template.py
+++ user/thin-provisioning-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "thin-provisioning-tools"
-pkgver = "1.2.2"
+pkgver = "1.3.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = [
@@ -18,7 +18,7 @@ pkgdesc = "Tools for manipulating the metadata of dm-thin targets"
 license = "GPL-3.0-or-later"
 url = "https://github.com/jthornber/thin-provisioning-tools"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "8ac15a068e6a6aec0cf0343d3d76f88c397d5fb2d6bd6202e9f13a490a9f3e22"
+sha256 = "64b0fcc7960b2ea37a4e5c162aed604337541142c65a5674146b29c706e08671"
 # too long
 options = ["!check"]
 
