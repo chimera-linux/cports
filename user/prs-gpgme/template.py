f971bdcbc771a0b5abf375d608b0964d7160c756
JC Grünhage <jan.christian@gruenhage.xyz>
2025-10-27 00:11:12 +0100

    user/prs-gpgme: new package

---
+++ user/prs-gpgme/template.py
@@ -0,0 +1,52 @@
+# Keep prs-gnupg-bin, prs-gpgme and prs-rpgpie in sync
+pkgname = "prs-gpgme"
+pkgver = "0.5.7"
+pkgrel = 0
+build_wrksrc = "cli"
+build_style = "cargo"
+prepare_after_patch = True
+make_build_args = [
+    "--no-default-features",
+    "--features="
+    + ",".join(
+        [
+            "alias",
+            "backend-gpgme",
+            "clipboard",
+            "notify",
+            "select-fzf-bin",
+            "select-skim",
+            "tomb",
+            "totp",
+        ]
+    ),
+]
+hostmakedepends = ["cargo-auditable", "pkgconf"]
+makedepends = ["gpgme-devel", "libgpg-error-devel", "rust-std"]
+pkgdesc = "Password manager CLI inspired by pass"
+subdesc = "GPGME backend"
+license = "GPL-3.0-only"
+url = "https://timvisee.com/projects/prs"
+source = (
+    f"https://gitlab.com/timvisee/prs/-/archive/v{pkgver}/prs-v{pkgver}.tar.gz"
+)
+sha256 = "0ad41003ab48a5309e3f2df0d6fe723babfe66007c980d3d0abf72c0a4d47f59"
+# Host binary for completion generation
+options = ["!cross"]
+
+
+def post_build(self):
+    for shell in ["bash", "fish", "zsh", "nushell"]:
+        with open(self.cwd / f"prs.{shell}", "w") as f:
+            self.do(
+                f"../target/{self.profile().triplet}/release/prs",
+                "internal",
+                "completions",
+                shell,
+                stdout=f,
+            )
+
+
+def post_install(self):
+    for shell in ["bash", "fish", "zsh", "nushell"]:
+        self.install_completion(f"prs.{shell}", shell, name="prs")
