ad76dc5207f9ada34af61972e8d3b29ef707a121
triallax <triallax@tutanota.com>
2025-10-26 22:00:28 +0000

    user/carla: update to 2.5.10

--- user/carla/template.py
+++ user/carla/template.py
@@ -1,6 +1,6 @@
 pkgname = "carla"
-pkgver = "2.5.9"
-pkgrel = 1
+pkgver = "2.5.10"
+pkgrel = 0
 build_style = "makefile"
 make_check_target = "tests"
 make_check_args = ["PEDANTIC_CFLAGS=", "PEDANTIC_CXXFLAGS="]
@@ -19,7 +19,7 @@ pkgdesc = "Audio plugin host"
 license = "GPL-2.0-or-later"
 url = "https://kx.studio/Applications:Carla"
 source = f"https://github.com/falkTX/Carla/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "226fb5d646b7541b82035080190e7440df1f92372fb798b4ad49289570e5ad81"
+sha256 = "ae2835b12081f7271a6b0b25d34b87d36b022c40370028ca4a10f90fcedfa661"
 tool_flags = {"LDFLAGS": ["-lfts"]}
 # needs to exist, even if empty
 file_modes = {"+usr/share/carla/resources": ("root", "root", 0o755, True)}
