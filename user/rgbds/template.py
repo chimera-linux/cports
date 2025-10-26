bdabaa1f05a5e31c6002cc1ba18784451f0e0c63
triallax <triallax@tutanota.com>
2025-10-26 01:54:03 +0100

    user/rgbds: update to 0.9.4

--- user/rgbds/template.py
+++ user/rgbds/template.py
@@ -1,5 +1,5 @@
 pkgname = "rgbds"
-pkgver = "0.9.3"
+pkgver = "0.9.4"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -15,7 +15,7 @@ pkgdesc = "Assembler/linker for the Game Boy and Game Boy Color"
 license = "MIT"
 url = "https://rgbds.gbdev.io"
 source = f"https://github.com/gbdev/rgbds/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "e4db822494e438f4a3619a0043280fec5a16596ac1dc7756e7c8bf1c57ab0376"
+sha256 = "29a0bdea2c07ae7e7af9f313de5deaa3ab0557e0251eac9b1e418cc18ebc0ba4"
 # No check or test targets
 options = ["!check"]
 
