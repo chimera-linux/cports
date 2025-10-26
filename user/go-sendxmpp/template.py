06a1a672787e2b0c2cd71a159e07c71b1cc6dbf8
triallax <triallax@tutanota.com>
2025-10-26 05:07:27 +0000

    user/go-sendxmpp: update to 0.15.1

--- user/go-sendxmpp/template.py
+++ user/go-sendxmpp/template.py
@@ -1,13 +1,13 @@
 pkgname = "go-sendxmpp"
-pkgver = "0.14.1"
-pkgrel = 6
+pkgver = "0.15.1"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Tool to send messages or files over XMPP"
 license = "BSD-2-Clause"
 url = "https://salsa.debian.org/mdosch/go-sendxmpp"
 source = f"{url}/-/archive/v{pkgver}/go-sendxmpp-v{pkgver}.tar.gz"
-sha256 = "3221a8a563153d1dfab367e7588e0e1991ceb6b3cf41e2e525351d476f12d887"
+sha256 = "35efac1a27740888dbb047056bb1a159659e910f11e2637e06c1768e5047c222"
 
 
 def post_install(self):
