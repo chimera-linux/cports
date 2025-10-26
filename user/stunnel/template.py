dcf49ef07bbfc164068e39e5e83971b6edaa61ed
triallax <triallax@tutanota.com>
2025-10-26 01:13:51 +0000

    user/stunnel: update to 5.76

--- user/stunnel/template.py
+++ user/stunnel/template.py
@@ -1,5 +1,5 @@
 pkgname = "stunnel"
-pkgver = "5.75"
+pkgver = "5.76"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["autoconf-archive", "automake", "libtool"]
@@ -9,7 +9,7 @@ pkgdesc = "TLS proxy to add TLS encryption to existing clients and servers"
 license = "GPL-2.0-or-later"
 url = "https://stunnel.org"
 source = f"{url}/downloads/stunnel-{pkgver}.tar.gz"
-sha256 = "0c1ef0ed85240974dccb94fe74fb92d6383474c7c0d10e8796d1f781a3ba5683"
+sha256 = "cda37eb4d0fb1e129718ed27ad77b5735e899394ce040bb2be28bbb937fd79e1"
 
 
 def post_install(self):
