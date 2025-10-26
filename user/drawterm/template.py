d4bab3ee6d84dd3b964ae19b3e70ef7903085934
triallax <triallax@tutanota.com>
2025-10-26 23:46:36 +0000

    user/drawterm: update to 0_git20251011, drop unused dep

--- user/drawterm/template.py
+++ user/drawterm/template.py
@@ -1,6 +1,6 @@
 pkgname = "drawterm"
-_commit = "903bcd8dba9cb9dfc70707a28089c469e5302539"
-pkgver = "0_git20250629"
+_commit = "48d53278a8273bb39ca295e8f163563ab04b3530"
+pkgver = "0_git20251011"
 pkgrel = 0
 build_style = "makefile"
 make_env = {"CONF": "linux"}
@@ -16,23 +16,11 @@ makedepends = [
 pkgdesc = "Interface to Plan 9 systems"
 license = "MIT"
 url = "https://drawterm.9front.org"
-_wlr_protocols_commit = "2ec67ebd26b73bada12f3fa6afdd51563b656722"
-source = [
-    f"https://git.9front.org/git/plan9front/drawterm/{_commit}/snap.tar.gz>snap-{_commit}.tar.gz",
-    f"!https://gitlab.freedesktop.org/wlroots/wlr-protocols/-/raw/{_wlr_protocols_commit}/unstable/wlr-virtual-pointer-unstable-v1.xml>{_wlr_protocols_commit}.xml",
-]
-sha256 = [
-    "d281e1397e6d63be9db1040245c0d506d8f70ff9522587871b531e2e892bc80d",
-    "3ff6d540be0bc5228195bf072bde42117ea17945a5c2061add5d3cf97d6bb524",
-]
+source = f"https://git.9front.org/git/plan9front/drawterm/{_commit}/snap.tar.gz>snap-{_commit}.tar.gz"
+sha256 = "9b6e719705c84d3c744524dd39fc96d2fcf34c142c2bd529dad9ad0180f566bc"
+
 # no tests
 options = ["!check", "!cross"]
-
-
-def init_build(self):
-    self.make_build_args += [
-        f"WLR_VIRTUAL_POINTER={self.chroot_sources_path}/{_wlr_protocols_commit}.xml"
-    ]
 
 
 def install(self):
