be3a10860293ff56989f5fda6974cf9d28c128f8
q66 <q66@chimera-linux.org>
2025-10-27 10:42:48 +0100

    user/bacon: drop loongarch libc downgrade
    This should not be needed? It uses rustix version that should be
    fixed along with a new enough libc... let's see how it fails

--- user/bacon/template.py
+++ user/bacon/template.py
@@ -11,18 +11,5 @@ source = f"https://github.com/Canop/bacon/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "2c49ca02687391d425f2cc9a19cae8227338def2d689d55ff5970cb70fd2b7f6"
 
 
-def pre_prepare(self):
-    # rustix loongarch64
-    self.do(
-        "cargo",
-        "update",
-        "--package",
-        "libc",
-        "--precise",
-        "0.2.174",
-        allow_network=True,
-    )
-
-
 def post_install(self):
     self.install_license("LICENSE")
