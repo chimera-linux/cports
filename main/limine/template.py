6662d64b1309ab72e3692c4a8b748c3c3f3a52bf
q66 <q66@chimera-linux.org>
2025-10-27 13:29:59 +0100

    main/limine: update to 10.2.0

--- main/limine/template.py
+++ main/limine/template.py
@@ -1,5 +1,5 @@
 pkgname = "limine"
-pkgver = "9.6.5"
+pkgver = "10.2.0"
 pkgrel = 0
 # these targets implemented
 archs = ["aarch64", "loongarch64", "riscv64", "x86_64"]
@@ -10,7 +10,7 @@ pkgdesc = "Multiprotocol EFI bootloader"
 license = "BSD-2-Clause AND 0BSD"
 url = "https://limine-bootloader.org"
 source = f"https://codeberg.org/Limine/Limine/releases/download/v{pkgver}/limine-{pkgver}.tar.zst"
-sha256 = "8751f0418d2ed6c1a99874a8925491f53b963b008ef4a51ecd0725dd812dfff3"
+sha256 = "0de794cfb061bb42b9a9f5e8a5beac9c8f1f05783d8cb811741a7659c1aeaae0"
 # no test suite
 options = ["!check"]
 
