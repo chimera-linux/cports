4e3de4e63d0ef0638103dea1e23d193b398d482c
triallax <triallax@tutanota.com>
2025-10-28 22:44:27 +0000

    main/gumbo-parser: update to 0.13.2

--- main/gumbo-parser/template.py
+++ main/gumbo-parser/template.py
@@ -1,5 +1,5 @@
 pkgname = "gumbo-parser"
-pkgver = "0.13.1"
+pkgver = "0.13.2"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -8,7 +8,7 @@ pkgdesc = "HTML5 parsing library in pure C99"
 license = "Apache-2.0"
 url = "https://codeberg.org/gumbo-parser/gumbo-parser"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "1a054d1e53d556641a6666537247411a77b0c18ef6ad5df23e30d2131676ef81"
+sha256 = "dbdc159dc8e5c6f3f254e50bce689dd9e439064ff06c165d5653410a5714ab66"
 
 
 @subpackage("gumbo-parser-devel")
