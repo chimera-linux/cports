183a0fc1c06d5691ee3e1ae2ff6d0f5da76bb410
triallax <triallax@tutanota.com>
2025-10-29 00:04:24 +0000

    main/openjpeg: update to 2.5.4

--- main/openjpeg/template.py
+++ main/openjpeg/template.py
@@ -1,5 +1,5 @@
 pkgname = "openjpeg"
-pkgver = "2.5.3"
+pkgver = "2.5.4"
 pkgrel = 0
 build_style = "cmake"
 # we skip static libs or they get referenced in cmake devel files
@@ -10,7 +10,7 @@ pkgdesc = "Open-source JPEG 2000 codec written in C"
 license = "BSD-2-Clause"
 url = "https://www.openjpeg.org"
 source = f"https://github.com/uclouvain/openjpeg/archive/v{pkgver}.tar.gz"
-sha256 = "368fe0468228e767433c9ebdea82ad9d801a3ad1e4234421f352c8b06e7aa707"
+sha256 = "a695fbe19c0165f295a8531b1e4e855cd94d0875d2f88ec4b61080677e27188a"
 hardening = ["!vis", "!cfi"]
 # missing test data
 options = ["!check"]
