16ad87564927c9862a84cad40006de12fa4eeef5
q66 <q66@chimera-linux.org>
2025-10-29 14:53:06 +0100

    main/musl: rebuild with llvm21
    This is not necessary on most archs but on loongarch it rebuilds
    the libc with medium code model and we want that.

--- main/musl-cross/template.py
+++ main/musl-cross/template.py
@@ -1,6 +1,6 @@
 pkgname = "musl-cross"
 pkgver = "1.2.5_git20240705"
-pkgrel = 2
+pkgrel = 3
 _commit = "dd1e63c3638d5f9afb857fccf6ce1415ca5f1b8b"
 _mimalloc_ver = "2.1.7"
 build_style = "gnu_configure"
