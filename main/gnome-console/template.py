d9918b01bc4e2a6c27b1e9a5ca833311cfc16c8d
triallax <triallax@tutanota.com>
2025-11-01 22:22:10 +0000

    main/gnome-console: update to 49.1

--- main/gnome-console/template.py
+++ main/gnome-console/template.py
@@ -1,6 +1,6 @@
 pkgname = "gnome-console"
-pkgver = "49.0"
-pkgrel = 2
+pkgver = "49.1"
+pkgrel = 0
 build_style = "meson"
 make_check_wrapper = ["wlheadless-run", "--"]
 hostmakedepends = [
@@ -27,6 +27,6 @@ url = "https://gitlab.gnome.org/GNOME/console"
 source = (
     f"$(GNOME_SITE)/gnome-console/{pkgver[:-2]}/gnome-console-{pkgver}.tar.xz"
 )
-sha256 = "9e8e9646f473d01f4b4a7bce2c47ad226b4ae83fabf24cbbb4ac94f6ac5d5cc2"
+sha256 = "fca39fd041e05ee7ca6d2a5c82001937d02ae1513f3f3651bd37ae0e2ef66e3e"
 # tries to open gpu
 options = ["!check"]
