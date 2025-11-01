acd719886343ffdfb80b1d4b14ede6d4807ace76
triallax <triallax@tutanota.com>
2025-11-01 23:18:51 +0000

    main/gsettings-desktop-schemas: update to 49.1

--- main/gsettings-desktop-schemas/template.py
+++ main/gsettings-desktop-schemas/template.py
@@ -1,5 +1,5 @@
 pkgname = "gsettings-desktop-schemas"
-pkgver = "49.0"
+pkgver = "49.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=true"]
@@ -20,7 +20,7 @@ pkgdesc = "Collection of GSettings schemas"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas"
 source = f"$(GNOME_SITE)/gsettings-desktop-schemas/{pkgver[:-2]}/gsettings-desktop-schemas-{pkgver}.tar.xz"
-sha256 = "912905cc45382888a47702ed1101c6b08ebd0122a32a67d940ab8116a96c520d"
+sha256 = "777a7f83d5e5a8076b9bf809cb24101b1b1ba9c230235e3c3de8e13968ed0e63"
 options = ["!cross"]
 
 
