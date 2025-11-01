c727ca30193510cb4cb8d77895a7f8d6adca996f
triallax <triallax@tutanota.com>
2025-11-01 22:36:48 +0000

    main/simple-scan: update to 49.1

--- main/simple-scan/template.py
+++ main/simple-scan/template.py
@@ -1,5 +1,5 @@
 pkgname = "simple-scan"
-pkgver = "49.0.1"
+pkgver = "49.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -29,5 +29,5 @@ url = "https://gitlab.gnome.org/GNOME/simple-scan"
 # tarball is missing on GNOME_SITE
 # source = f"$(GNOME_SITE)/simple-scan/{pkgver[:-4]}/simple-scan-{pkgver}.tar.xz"
 source = f"https://gitlab.gnome.org/GNOME/simple-scan/-/archive/{pkgver}/simple-scan-{pkgver}.tar.gz"
-sha256 = "f123e0e3c319381a4749e01f51a5e3764028fc9aa8a6afb2696426ce96706315"
+sha256 = "a27e0412b36fb7c03b810532d220eb7a606898c06131fb1757af4745587abcbb"
 hardening = ["vis", "!cfi"]
