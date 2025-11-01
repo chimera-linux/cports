cafe77a98bcbad451316f2145059089b20bad4be
triallax <triallax@tutanota.com>
2025-11-01 16:03:40 +0000

    main/inxi: update to 3.3.39.1

--- main/inxi/template.py
+++ main/inxi/template.py
@@ -1,12 +1,12 @@
 pkgname = "inxi"
-pkgver = "3.3.38.1"
+pkgver = "3.3.39.1"
 pkgrel = 0
 depends = ["perl"]
 pkgdesc = "Fully featured CLI system information tool"
 license = "GPL-3.0-or-later"
 url = "https://codeberg.org/smxi/inxi"
 source = f"{url}/archive/{pkgver[: pkgver.rfind('.')]}-{pkgver[pkgver.rfind('.') + 1 :]}.tar.gz"
-sha256 = "9601b5d6287a2508a2e3c2652ce44190636dfe48371dc658e48ffc74af500b1b"
+sha256 = "c0441f21dc5ea365a6d63466070d00e6858aed3b3c42276a1bf18ab3c57c013c"
 
 
 def install(self):
