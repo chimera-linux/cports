ab0e7cf9e56101db14eab21d69da42b2eae6422c
triallax <triallax@tutanota.com>
2025-11-01 22:31:15 +0000

    main/doasedit: update to 1.0.9

--- main/doasedit/template.py
+++ main/doasedit/template.py
@@ -1,5 +1,5 @@
 pkgname = "doasedit"
-pkgver = "1.0.8"
+pkgver = "1.0.9"
 pkgrel = 0
 build_style = "makefile"
 make_install_args = ["prefix=/usr"]
@@ -8,7 +8,7 @@ pkgdesc = "Shell script to edit files with doas"
 license = "MIT"
 url = "https://codeberg.org/TotallyLeGIT/doasedit"
 source = f"https://codeberg.org/TotallyLeGIT/doasedit/archive/{pkgver}.tar.gz"
-sha256 = "de7dfc450568d14f1ff473aa4a50a0d0505e188e117cc12cf3a2b0ea5da8af10"
+sha256 = "bb02ab7a86b44f9128f6216946c8d21b41a1a4cc9acc06055d4774105a2239ee"
 # no tests
 options = ["!check"]
 
