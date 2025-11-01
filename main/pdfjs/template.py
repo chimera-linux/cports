e6f12e06635295e6d1556e52a4e98f0bbb7b5849
triallax <triallax@tutanota.com>
2025-11-01 17:56:40 +0000

    main/pdfjs: update to 5.4.296

--- main/pdfjs/template.py
+++ main/pdfjs/template.py
@@ -1,11 +1,11 @@
 pkgname = "pdfjs"
-pkgver = "5.4.54"
+pkgver = "5.4.296"
 pkgrel = 0
 pkgdesc = "Platform for parsing and rendering PDFs"
 license = "Apache-2.0"
 url = "https://github.com/mozilla/pdf.js"
 source = f"{url}/releases/download/v{pkgver}/pdfjs-{pkgver}-legacy-dist.zip"
-sha256 = "cebbae3903847088be128ddcdd7243eae2985fdc9b553e5b4519282624443df9"
+sha256 = "9085d6a3f5b494341aed440aeed61c61304b6acacbf04ddd90639765d51c7949"
 # no tests defined
 options = ["!check"]
 
