1d2720ba5d57395eca579d3b750bd7938c5eef84
triallax <triallax@tutanota.com>
2025-11-02 19:24:47 +0000

    main/copyq: update to 13.0.0

--- main/copyq/template.py
+++ main/copyq/template.py
@@ -1,5 +1,5 @@
 pkgname = "copyq"
-pkgver = "12.0.1"
+pkgver = "13.0.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -13,20 +13,19 @@ hostmakedepends = [
     "pkgconf",
 ]
 makedepends = [
+    "kguiaddons-devel",
     "libxfixes-devel",
     "libxtst-devel",
     "qt6-qtbase-private-devel",  # qtx11extras_p.h
     "qt6-qtdeclarative-devel",
     "qt6-qtsvg-devel",
     "qt6-qttools-devel",
-    "qt6-qtwayland-devel",
-    "wayland-devel",
 ]
 pkgdesc = "Clipboard manager with advanced features"
 license = "GPL-3.0-or-later"
 url = "https://hluk.github.io/CopyQ"
 source = f"https://github.com/hluk/CopyQ/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "452cd2acbe9d70e0b93bef413620dfece363f0595b19788d71035c8455d6097f"
+sha256 = "c7e0c9aa95b5653dee547f168d58cf5aae4f66ad5f94856a61741ebe9527ad72"
 # CFI: loading plugins fail with cfi enabled
 hardening = ["vis", "!cfi"]
 # requires building project a second time in debug mode
