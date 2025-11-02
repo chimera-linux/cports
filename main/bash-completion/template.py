8bc77a180ab692865fae5ba3d930ab41516ac19c
triallax <triallax@tutanota.com>
2025-11-02 02:31:29 +0000

    main/bash-completion: update to 2.17.0

--- main/bash-completion/template.py
+++ main/bash-completion/template.py
@@ -1,6 +1,6 @@
 pkgname = "bash-completion"
-pkgver = "2.16.0"
-pkgrel = 1
+pkgver = "2.17.0"
+pkgrel = 0
 build_style = "gnu_configure"
 make_install_args = [
     "profiledir=/usr/share/bash/bashrc.d",
@@ -12,6 +12,6 @@ pkgdesc = "Programmable completion functions for bash"
 license = "GPL-2.0-or-later"
 url = "https://github.com/scop/bash-completion"
 source = f"{url}/releases/download/{pkgver}/bash-completion-{pkgver}.tar.xz"
-sha256 = "3369bd5e418a75fb990863925aed5b420398acebb320ec4c0306b3eae23f107a"
+sha256 = "dd9d825e496435fb3beba3ae7bea9f77e821e894667d07431d1d4c8c570b9e58"
 # what's the point (needs a truckload of checkdepends too)
 options = ["!check"]
