844de511ddc96d5bc5eb343255d35637049bf56c
triallax <triallax@tutanota.com>
2025-11-02 00:17:19 +0000

    main/jpegoptim: update to 1.5.6

--- main/jpegoptim/template.py
+++ main/jpegoptim/template.py
@@ -1,5 +1,5 @@
 pkgname = "jpegoptim"
-pkgver = "1.5.5"
+pkgver = "1.5.6"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-arith"]
@@ -11,7 +11,7 @@ pkgdesc = "Utility for optimising jpeg files"
 license = "GPL-3.0-or-later"
 url = "https://github.com/tjko/jpegoptim"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "90a309d1c092de358bb411d702281ac3039b489d03adb0bc3c4ef04cf0067d38"
+sha256 = "661a808dfffa933d78c6beb47a2937d572b9f03e94cbaaab3d4c0d72f410e9be"
 hardening = ["vis", "!cfi"]
 # no tests
 options = ["!check"]
