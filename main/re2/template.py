c547af782563f571c70a00210a87e6695588b3a4
triallax <triallax@tutanota.com>
2025-11-02 02:20:27 +0000

    main/re2: update to 2025.08.12

--- main/re2/template.py
+++ main/re2/template.py
@@ -1,6 +1,6 @@
 pkgname = "re2"
-pkgver = "2025.08.05"
-pkgrel = 1
+pkgver = "2025.08.12"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DBUILD_SHARED_LIBS=ON",
@@ -24,7 +24,7 @@ pkgdesc = "C++ regular expression library"
 license = "BSD-3-Clause"
 url = "https://github.com/google/re2"
 source = f"{url}/archive/refs/tags/{pkgver.replace('.', '-')}.tar.gz"
-sha256 = "b5708d8388110624c85f300e7e9b39c4ed5469891eb1127dd7f9d61272d04907"
+sha256 = "2f3bec634c3e51ea1faf0d441e0a8718b73ef758d7020175ed7e352df3f6ae12"
 tool_flags = {"CXXFLAGS": ["-DNDEBUG"]}
 
 
