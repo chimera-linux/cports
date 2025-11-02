925fcf10ea10f3b59838ca9489134dec6b5665f8
triallax <triallax@tutanota.com>
2025-11-02 18:59:16 +0000

    main/brlaser: update to 6.2.8

--- main/brlaser/template.py
+++ main/brlaser/template.py
@@ -1,13 +1,12 @@
 pkgname = "brlaser"
-pkgver = "6.2.7"
+pkgver = "6.2.8"
 pkgrel = 0
 build_style = "cmake"
-configure_args = ["-DCMAKE_POLICY_VERSION_MINIMUM=3.5"]
 hostmakedepends = ["cmake", "ninja"]
 makedepends = ["cups-devel"]
 pkgdesc = "CUPS driver for monochrome Brother laser printers"
 license = "GPL-2.0-or-later"
 url = "https://github.com/Owl-Maintain/brlaser"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "e67c5726fc1fe53574c2e8b5f72634f1359d0f53586a555eb2489fafd7c81640"
+sha256 = "16dae855aa7fff0eef0c05398fab37678243d7d610fa5f9af0d3a2cc9bf08cb0"
 hardening = ["cfi", "vis"]
