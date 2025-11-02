740a2ca8eeee2068fcb8ffe341b4fdce295adf92
triallax <triallax@tutanota.com>
2025-11-02 03:19:02 +0000

    main/brotli: split progs into subpkg
    it's tiny but we tend to do this consistently for e.g. compression libs
    anyway so might as well

--- main/brotli/template.py
+++ main/brotli/template.py
@@ -1,7 +1,7 @@
 # update main/python-brotli alongside this
 pkgname = "brotli"
 pkgver = "1.1.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 pkgdesc = "General-purpose lossless compression algorithm"
@@ -13,6 +13,11 @@ sha256 = "e720a6ca29428b803f4ad165371771f5398faba397edf6778837a18599ea13ff"
 
 def post_install(self):
     self.install_license("LICENSE")
+
+
+@subpackage("brotli-progs")
+def _(self):
+    return self.default_progs()
 
 
 @subpackage("brotli-devel")
