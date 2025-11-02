31158ed9f41ebceb074d5c668c8ec0422fa04930
triallax <triallax@tutanota.com>
2025-11-02 04:08:06 +0000

    main/librsvg: update to 2.61.2

--- main/librsvg/template.py
+++ main/librsvg/template.py
@@ -1,5 +1,5 @@
 pkgname = "librsvg"
-pkgver = "2.61.1"
+pkgver = "2.61.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -41,7 +41,7 @@ pkgdesc = "SVG library for GNOME"
 license = "GPL-2.0-or-later AND LGPL-2.0-or-later"
 url = "https://wiki.gnome.org/Projects/LibRsvg"
 source = f"$(GNOME_SITE)/librsvg/{pkgver[:-2]}/librsvg-{pkgver}.tar.xz"
-sha256 = "bc1bbcd419120b098db28bea55335d9de2470d4e6a9f6ee97207b410fc15867d"
+sha256 = "4644d83623dd61cc4479c2b3c372e1da2b281552ebc90035c8d1ac502eb1dc00"
 # check: sample files may differ based on pango/freetype/harfbuzz version
 # cross: no introspection in cross
 options = ["!check", "!cross"]
