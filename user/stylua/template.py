34172ad82f04e0828ff7e3d633034963aa573c23
Ayush Agarwal <ayush@ayushnix.com>
2025-11-05 23:34:52 +0530

    user/stylua: update to 2.3.1

--- user/stylua/template.py
+++ user/stylua/template.py
@@ -1,5 +1,5 @@
 pkgname = "stylua"
-pkgver = "2.3.0"
+pkgver = "2.3.1"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = [
@@ -14,7 +14,7 @@ pkgdesc = "Lua formatter"
 license = "MPL-2.0"
 url = "https://github.com/JohnnyMorganz/StyLua"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "e1dfdae2fcbeeae60d1e25102d1845a09501e0afa98a7d31f1e8a4f636695adc"
+sha256 = "aba628d721380290a334ae899eff1aec9d3d14302d2af336f67d8d7af72d35e3"
 
 
 def install(self):
