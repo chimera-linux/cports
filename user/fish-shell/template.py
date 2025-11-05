f2537e2bdc5eb0b80c16f42b0d64dc8d256cfd5d
Ayush Agarwal <ayush@ayushnix.com>
2025-11-05 14:18:01 +0530

    user/fish-shell: update to 4.2.1
    Closes #4834
    Closes #4922

--- user/fish-shell/template.py
+++ user/fish-shell/template.py
@@ -1,18 +1,16 @@
 pkgname = "fish-shell"
-pkgver = "4.0.8"
+pkgver = "4.2.1"
 pkgrel = 0
 build_style = "cmake"
 make_check_target = "fish_run_tests"
-hostmakedepends = ["cargo-auditable", "cmake", "ninja", "pkgconf", "gettext"]
+hostmakedepends = ["cargo-auditable", "cmake", "gettext", "pkgconf", "ninja"]
 makedepends = ["pcre2-devel", "rust-std"]
-checkdepends = ["python", "procps"]
+checkdepends = ["procps", "python"]
 pkgdesc = "Friendly interactive command line shell"
 license = "GPL-2.0-only"
 url = "https://fishshell.com"
 source = f"https://github.com/fish-shell/fish-shell/releases/download/{pkgver}/fish-{pkgver}.tar.xz"
-sha256 = "7f779d13aa55d2fa3afc17364c61ab9edc16faa1eac5851badeffb4e73692240"
-# FIXME lintpixmaps
-options = ["!lintpixmaps"]
+sha256 = "0f99222a3063377c91fbf78d9850edab7a0b91bdbed201cf79da48ea3a41f393"
 
 
 def prepare(self):
@@ -30,3 +28,7 @@ def init_build(self):
 
 def post_install(self):
     self.install_shell("/usr/bin/fish")
+    for fishbin in ["fish_indent", "fish_key_reader"]:
+        self.uninstall(f"usr/bin/{fishbin}")
+        self.install_link(f"usr/bin/{fishbin}", "fish")
+    self.uninstall("etc/fish")
