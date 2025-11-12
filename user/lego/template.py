a0faabea3c094f149ffbae6817abcec4b5f23ca1
Wesley Moore <wes@wezm.net>
2025-11-12 18:51:53 +1000

    user/lego: update to 4.28.1

--- user/lego/template.py
+++ user/lego/template.py
@@ -1,5 +1,5 @@
 pkgname = "lego"
-pkgver = "4.27.0"
+pkgver = "4.28.1"
 pkgrel = 0
 build_style = "go"
 make_build_args = ["-ldflags", f"-X main.version={pkgver}", "./cmd/lego"]
@@ -8,7 +8,7 @@ pkgdesc = "Let's Encrypt/ACME client"
 license = "MIT"
 url = "https://github.com/go-acme/lego"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "bd5a07b0cc111e6848d4065581e1da46f1c474db2e32b5e71d9c3f9f7753f303"
+sha256 = "d0a2d032a82d1ce2ee0b13a532725382b06d71fe35bd86f9f78d9989d3aa2816"
 # check: tests need network access: https://github.com/go-acme/lego/issues/560
 options = ["!check"]
 
