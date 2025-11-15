f9c1b8084b391792c6f3bda11621fe17ed895744
Ayush Agarwal <ayush@ayushnix.com>
2025-11-15 18:14:24 +0530

    main/qbittorrent: update to 5.1.3

--- main/qbittorrent/template.py
+++ main/qbittorrent/template.py
@@ -1,6 +1,6 @@
 pkgname = "qbittorrent"
-pkgver = "5.1.2"
-pkgrel = 4
+pkgver = "5.1.3"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DSTACKTRACE=OFF"]
 hostmakedepends = [
@@ -23,7 +23,7 @@ pkgdesc = "QT-based torrent client"
 license = "GPL-2.0-or-later"
 url = "https://www.qbittorrent.org"
 source = f"https://github.com/qbittorrent/qBittorrent/archive/refs/tags/release-{pkgver}.tar.gz"
-sha256 = "a35448f3c8cb57d033bd3c4bd66c63417b0ca793ae7e9c5c5053960e2229ad9e"
+sha256 = "9c6c6018239a461ef02b67534db30232027cb8a431827c1f393f59cb8adc01e9"
 # CFI: BitTorrent::SessionImpl::SessionImpl crash
 hardening = ["vis", "!cfi"]
 # don't build
