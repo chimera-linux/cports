2f6de6c21e944ab0550ea7344158b83ee9dc5f89
q66 <q66@chimera-linux.org>
2025-11-15 22:08:27 +0100

    main/xmlsec1: update to 1.3.9

--- main/xmlsec1/template.py
+++ main/xmlsec1/template.py
@@ -1,5 +1,5 @@
 pkgname = "xmlsec1"
-pkgver = "1.3.8"
+pkgver = "1.3.9"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-docs", "--enable-http"]
@@ -17,13 +17,14 @@ pkgdesc = "XML Security Library"
 license = "MIT"
 url = "https://www.aleksey.com/xmlsec"
 source = f"{url}/download/xmlsec1-{pkgver}.tar.gz"
-sha256 = "d0180916ae71be28415a6fa919a0684433ec9ec3ba1cc0866910b02e5e13f5bd"
+sha256 = "a631c8cd7a6b86e6adb9f5b935d45a9cf9768b3cb090d461e8eb9d043cf9b62f"
 # broken tests build + tests reach internet
 options = ["!cross", "!check"]
 
 
 def post_install(self):
     self.install_license("COPYING")
+    self.uninstall("usr/bin/xmlsec_unit_tests")
 
 
 @subpackage("xmlsec1-devel")
