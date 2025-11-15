d7e9195a4cd15cc695fa70e9d6ed169f178baa15
Ayush Agarwal <ayush@ayushnix.com>
2025-11-15 19:37:44 +0530

    main/haproxy: update to 3.2.8
    Merges #4956
    
    Co-authored-by: JC Grünhage <jan.christian@gruenhage.xyz>

--- main/haproxy/template.py
+++ main/haproxy/template.py
@@ -1,5 +1,5 @@
 pkgname = "haproxy"
-pkgver = "3.2.7"
+pkgver = "3.2.9"
 pkgrel = 0
 build_style = "makefile"
 make_build_args = [
@@ -41,7 +41,7 @@ url = "https://www.haproxy.org"
 source = (
     f"{url}/download/{pkgver[: pkgver.rfind('.')]}/src/haproxy-{pkgver}.tar.gz"
 )
-sha256 = "1f0ae9dfb0b319e2d5cb6e4cdf931a0877ad88e0090c46cf16faf008fbf54278"
+sha256 = "e660d141b29019f4d198785b0834cc3e9c96efceeb807c2fff2fc935bd3354c2"
 hardening = ["!vis", "!cfi", "!int"]
 # hard depends on vtest which doesn't have releases
 options = ["!check"]
