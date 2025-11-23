365d0709c5dcde4fccb4671cde52a3d864b081ff
Duncan Bellamy <dunk@denkimushi.com>
2025-11-23 10:52:49 +0000

    main/python-pyproject-metadata: update to 0.10.0

--- main/python-pyproject-metadata/template.py
+++ main/python-pyproject-metadata/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pyproject-metadata"
-pkgver = "0.9.1"
+pkgver = "0.10.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ pkgdesc = "PEP 621 metadata parsing"
 license = "MIT"
 url = "https://github.com/pypa/pyproject-metadata"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "95213045cf74cb32642034ccb4b3223c2a4337fa675fa886cbd48be6780241c7"
+sha256 = "f0e5d416d0851b89198cb112ff568adbb75da7d4941ca23f5a16690163785277"
 
 
 def post_install(self):
