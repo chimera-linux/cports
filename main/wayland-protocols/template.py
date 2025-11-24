cf57d6aa5bf9974fe30f04fdca258fa6466822e5
q66 <q66@chimera-linux.org>
2025-11-24 11:46:34 +0100

    main/wayland-protocols: update to 1.46

--- main/wayland-protocols/template.py
+++ main/wayland-protocols/template.py
@@ -1,5 +1,5 @@
 pkgname = "wayland-protocols"
-pkgver = "1.45"
+pkgver = "1.46"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "wayland-progs"]
@@ -8,7 +8,7 @@ pkgdesc = "Wayland compositor protocols"
 license = "MIT"
 url = "https://wayland.freedesktop.org"
 source = f"https://gitlab.freedesktop.org/wayland/wayland-protocols/-/archive/{pkgver}/wayland-protocols-{pkgver}.tar.gz"
-sha256 = "460dad72a6c84b2a7d80745bee43d96ba117f4e3dfc4d7c45f83f66469ea27df"
+sha256 = "83afedde1e63751578ba51b5f6c36052802f85634ed68be41ecfcc3a515ab03d"
 # check conditional
 options = []
 
