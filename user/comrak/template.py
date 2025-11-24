590aa111f2f28f5613455dfa38bb1d9b75c272a0
JC Grünhage <jan.christian@gruenhage.xyz>
2025-11-24 09:00:50 +0100

    user/comrak: update to 0.48.0

--- user/comrak/template.py
+++ user/comrak/template.py
@@ -1,5 +1,5 @@
 pkgname = "comrak"
-pkgver = "0.45.0"
+pkgver = "0.48.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -8,7 +8,7 @@ pkgdesc = "CommonMark compatible GitHub Flavored Markdown parser and formatter"
 license = "BSD-2-Clause"
 url = "https://github.com/kivikakk/comrak"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "0fe58e74bbc47705192a25352e131cdf1de1f868b614e56c9b28b714db010500"
+sha256 = "91d136008082a5019df88255bef198e21f177cf7234895be4957ffeb92bd886e"
 
 if self.profile().arch in ["loongarch64"]:
     broken = "linux-raw-sys does not support, can't bump (semver)"
