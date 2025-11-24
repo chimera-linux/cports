ffd4870641c4075ecc76ca2fc6b8767c632c686a
JC Grünhage <jan.christian@gruenhage.xyz>
2025-11-24 09:36:04 +0100

    user/television: update to 0.13.11

--- user/television/template.py
+++ user/television/template.py
@@ -1,5 +1,5 @@
 pkgname = "television"
-pkgver = "0.13.7"
+pkgver = "0.13.11"
 pkgrel = 0
 build_style = "cargo"
 make_check_args = [
@@ -17,7 +17,7 @@ pkgdesc = "Fuzzy finder"
 license = "MIT"
 url = "https://github.com/alexpasmantier/television"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "aa2009f5df526bb8ae0fdd4eb644fc51bf50eeaf093a8609c4dd7453fd043a54"
+sha256 = "a524e0cb07224794df7fda729a0aa90d77d7dfbb87a1a46a9b3b1a3c838532d5"
 # generates completions with host binary
 options = ["!cross"]
 
