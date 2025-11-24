6faed20c301ba6f946ed8682db814f76d3d5c844
JC Grünhage <jan.christian@gruenhage.xyz>
2025-11-24 08:22:03 +0100

    user/uv: update to 0.9.11

--- user/uv/template.py
+++ user/uv/template.py
@@ -1,5 +1,5 @@
 pkgname = "uv"
-pkgver = "0.9.5"
+pkgver = "0.9.11"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -18,7 +18,7 @@ pkgdesc = "Python package installer"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/astral-sh/uv"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "9fd1dd030b37b51dcf79b582ea77a911eeb4015a00669bd3047d3b6adea37ba8"
+sha256 = "bd3306812c8bcfd8d43a9b32f0a2af240aeab4c1e561055bede91183802a306e"
 # too many of them need net
 # completions with host bin
 options = ["!check", "!cross"]
