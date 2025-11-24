ea55ef9e15e4d72cbaaab0f396b44fa8256616d6
JC Grünhage <jan.christian@gruenhage.xyz>
2025-11-24 08:39:18 +0100

    main/ruff: update to 0.14.6

--- main/ruff/template.py
+++ main/ruff/template.py
@@ -1,5 +1,5 @@
 pkgname = "ruff"
-pkgver = "0.14.3"
+pkgver = "0.14.6"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -18,7 +18,7 @@ pkgdesc = "Python formatter and linter"
 license = "MIT"
 url = "https://docs.astral.sh/ruff"
 source = f"https://github.com/astral-sh/ruff/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "e44ce7ab8c6c0894ae4afcaad086624ee1279b7c3e1e2a7dfc5face14800d80d"
+sha256 = "58ebb8ec4479e8b307c5364fcf562f94d1debf65a0f9821c153f2b3aa019243c"
 # generates completions with host bin
 # tests are mostly a waste of time
 options = ["!cross", "!check"]
