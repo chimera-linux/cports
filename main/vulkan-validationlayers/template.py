18285bb9449823fdc807a4c33a7321276d120e63
q66 <q66@chimera-linux.org>
2025-11-24 20:30:14 +0100

    main/vulkan-validationlayers: update to 1.4.334

--- main/vulkan-validationlayers/template.py
+++ main/vulkan-validationlayers/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-validationlayers"
-pkgver = "1.4.326"
+pkgver = "1.4.334"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -25,13 +25,13 @@ license = "Apache-2.0"
 url = "https://vulkan.lunarg.com/doc/sdk/latest/linux/khronos_validation_layer.html"
 source = [
     f"https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/refs/tags/v{pkgver}.tar.gz",
-    "https://github.com/KhronosGroup/SPIRV-Headers/archive/e6d5e88c07cc66a798b668945e7fb29ec1cfee27.tar.gz",
+    "https://github.com/KhronosGroup/SPIRV-Headers/archive/f2e4bd213104fe323a01e935df56557328d37ac8.tar.gz",
 ]
 source_paths = [
     ".",
     "spirv-headers",
 ]
 sha256 = [
-    "41b0a3d5b8a0a1ed395650adfc453b9711ee02c27abdc27845dc58c683d31268",
-    "fac301cb7156dbe747fa6fc6700a8a9265519c0d15a592884cc1515cc2852c9f",
+    "8a730695f9e0181febf66847181c14830d2f7d64cb32006fb9e273a1bb86b76c",
+    "4e26fecd4142ca178df6b8f24485d2215ef0621de534cd277faeb33d3e06d897",
 ]
