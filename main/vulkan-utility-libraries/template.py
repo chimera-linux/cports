3c70ef9b8c4c9eda7bc7919515672295d346156d
q66 <q66@chimera-linux.org>
2025-11-24 20:29:00 +0100

    main/vulkan-utility-libraries: update to 1.4.334

--- main/vulkan-utility-libraries/template.py
+++ main/vulkan-utility-libraries/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-utility-libraries"
-pkgver = "1.4.326"
+pkgver = "1.4.334"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_TESTS=ON"]
@@ -17,7 +17,7 @@ pkgdesc = "Utility libraries for Vulkan"
 license = "Apache-2.0"
 url = "https://github.com/KhronosGroup/Vulkan-Utility-Libraries"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "74bc0be35045bc4f3e7dd2b52fbf8141cda7329ab9d4f14c988442bd74f201c8"
+sha256 = "444fa580290bc7e3bb2d098648478a79ee85b4a1ca5f852e58fd64798a36c498"
 # broken cmake files
 tool_flags = {"CXXFLAGS": ["-I/usr/include/magic_enum"]}
 # static-only library, so just keep it as one package
