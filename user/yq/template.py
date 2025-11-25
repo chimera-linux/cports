4041018a97adb6afc08acf0ebe36a21882544896
Wesley Moore <wes@wezm.net>
2025-11-25 21:24:28 +1000

    user/yq: update to 4.49.2

--- user/yq/template.py
+++ user/yq/template.py
@@ -1,14 +1,21 @@
 pkgname = "yq"
-pkgver = "4.48.1"
-pkgrel = 1
+pkgver = "4.49.2"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 checkdepends = ["bash", "tzdb"]
 pkgdesc = "Command-line YAML processor"
 license = "MIT"
 url = "https://github.com/mikefarah/yq"
-source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "591158368f8155421bd8821754a67b4478ee2cde205b7abfbf2d50f90769cf0e"
+source = [
+    f"{url}/archive/v{pkgver}.tar.gz",
+    f"{url}/releases/download/v{pkgver}/yq_man_page_only.tar.gz",
+]
+source_paths = [".", "manpage"]
+sha256 = [
+    "648d96cc490a4e08edb6bf8ff9498360b405263e202663cd9c92322b3aa557ef",
+    "74f01170c0d866cf787660d3ba2be16d47c447889ddc558958789a02c99d2623",
+]
 # generates completions with host binary
 options = ["!cross"]
 
@@ -26,5 +33,6 @@ def post_build(self):
 
 def post_install(self):
     self.install_license("LICENSE")
+    self.install_man("manpage/yq.1")
     for shell in ["bash", "fish", "zsh"]:
         self.install_completion(f"yq.{shell}", shell)
