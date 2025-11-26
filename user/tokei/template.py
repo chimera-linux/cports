bca34b9b487feba1d1afa1cff07631c880fe5371
JC Grünhage <jan.christian@gruenhage.xyz>
2025-11-26 23:32:08 +0100

    user/tokei: update to 13.0.0

--- user/tokei/template.py
+++ user/tokei/template.py
@@ -1,29 +1,16 @@
 pkgname = "tokei"
-pkgver = "12.1.2"
-pkgrel = 1
+pkgver = "13.0.0"
+pkgrel = 0
 build_style = "cargo"
 # we patch lockfile
 prepare_after_patch = True
-hostmakedepends = ["cargo-auditable"]
-makedepends = ["rust-std"]
+hostmakedepends = ["cargo-auditable", "pkgconf"]
+makedepends = ["rust-std", "libgit2-devel"]
 pkgdesc = "CLI for counting lines of code with stats per language"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/XAMPPRocky/tokei"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "81ef14ab8eaa70a68249a299f26f26eba22f342fb8e22fca463b08080f436e50"
-
-
-def pre_prepare(self):
-    # the version that is in there is busted on loongarch
-    self.do(
-        "cargo",
-        "update",
-        "--package",
-        "libc",
-        "--precise",
-        "0.2.170",
-        allow_network=True,
-    )
+sha256 = "b426ab03b8eedf4fe3ea70ca8379a2355981b0e9ca1d0083a66e623858e7e481"
 
 
 def post_install(self):
