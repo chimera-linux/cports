dc614fb94777710cd24aeefb6b46acd4641a0352
Erica Z <zerica@callcc.eu>
2025-11-30 14:16:21 +0100

    user/fish-shell: fix loginctl completions
    elogind's version of the command does not understand this flag

--- user/fish-shell/template.py
+++ user/fish-shell/template.py
@@ -1,6 +1,6 @@
 pkgname = "fish-shell"
 pkgver = "4.2.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 make_check_target = "fish_run_tests"
 hostmakedepends = ["cargo-auditable", "cmake", "gettext", "pkgconf", "ninja"]
