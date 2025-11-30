532e834b073e7f3c7cecebbed95489ccabade9e4
Ayush Agarwal <ayush@ayushnix.com>
2025-11-30 12:39:15 +0530

    main/haproxy: update to 3.4.2

--- main/haproxy/template.py
+++ main/haproxy/template.py
@@ -1,11 +1,12 @@
 pkgname = "haproxy"
-pkgver = "3.2.9"
+pkgver = "3.4.2"
 pkgrel = 0
 build_style = "makefile"
 make_build_args = [
     "EXTRA=admin/halog/halog",
     "TARGET=linux-musl",
     "USE_GETADDRINFO=1",
+    "USE_KTLS=1",
     "USE_LUA=1",
     "USE_NS=1",
     "USE_OPENSSL=1",
@@ -14,23 +15,19 @@ make_build_args = [
     "USE_PROMEX=1",
     "USE_PTHREAD_EMULATION=1",
     "USE_QUIC=1",
-    "USE_QUIC_OPENSSL_COMPAT=1",
+    "USE_SHM_OPEN=1",
     "USE_ZLIB=1",
     "V=1",
 ]
 make_install_args = [
     "EXTRA=admin/halog/halog",
     "SBINDIR=/usr/bin",
-    "DOCDIR=/usr/share/doc/haproxy",
 ]
-make_check_target = "reg-tests"
-hostmakedepends = [
-    "pkgconf",
-]
+hostmakedepends = ["pkgconf"]
 makedepends = [
     "dinit-chimera",
     "linux-headers",
-    "lua5.4-devel",
+    "lua5.5-devel",
     "openssl3-devel",
     "pcre2-devel",
     "zlib-ng-compat-devel",
@@ -41,7 +38,8 @@ url = "https://www.haproxy.org"
 source = (
     f"{url}/download/{pkgver[: pkgver.rfind('.')]}/src/haproxy-{pkgver}.tar.gz"
 )
-sha256 = "e660d141b29019f4d198785b0834cc3e9c96efceeb807c2fff2fc935bd3354c2"
+sha256 = "b1330dbb0d6e6bc4a72c4708a6a9e585579cd1156dfe5763c26305105bc12907"
+# builds successfully but fails to run if enabled
 hardening = ["!vis", "!cfi", "!int"]
 # hard depends on vtest which doesn't have releases
 options = ["etcfiles", "!check"]
@@ -54,8 +52,16 @@ def init_build(self):
 def post_install(self):
     self.install_file(self.files_path / "haproxy.cfg", "etc/haproxy")
     self.install_files("examples", "usr/share/haproxy")
-    self.install_files("doc", "usr/share/haproxy")
+    self.uninstall("usr/doc/haproxy")
     self.install_sysusers(self.files_path / "sysusers.conf")
     self.install_tmpfiles(self.files_path / "tmpfiles.conf")
     self.install_service(self.files_path / "haproxy")
     self.install_license("LICENSE")
+
+
+@subpackage("haproxy-cli-scripts")
+def _(self):
+    self.subdesc = "administration bash scripts"
+    self.depends = [self.parent, "bash"]
+    self.install_if = [self.parent, "bash"]
+    return ["cmd:haproxy-dump-certs", "cmd:haproxy-reload"]
