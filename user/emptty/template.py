35000fa2239f6730497b529e3df747a52a1f855e
Michal Tvrznik <emporeor@gmail.com>
2025-12-03 11:03:07 +0100

    user/emptty: update to 0.15.0

--- user/emptty/template.py
+++ user/emptty/template.py
@@ -1,6 +1,6 @@
 pkgname = "emptty"
-pkgver = "0.14.0"
-pkgrel = 5
+pkgver = "0.15.0"
+pkgrel = 0
 build_style = "go"
 make_env = {"CGO_ENABLED": "1"}
 hostmakedepends = ["go"]
@@ -9,7 +9,7 @@ pkgdesc = "TTY display manager"
 license = "MIT"
 url = "https://github.com/tvrzna/emptty"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "f4b95a8b6d7d09bd4c80fb16e33ac8295344d81186f6f5c2601f237795e93859"
+sha256 = "fae7c04afeeb9ef3dcbb9bca67e9a2fa940e99a91872ebc0775e10253972c7f3"
 
 
 def post_install(self):
