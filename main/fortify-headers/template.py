af21acb506ca7d35509bbc6f484139bea9ae561f
Julien Voisin <jvoisin@users.noreply.github.com>
2025-12-14 23:17:41 +0100

    main/fortify-headers: update to 3.0.1

--- main/fortify-headers/template.py
+++ main/fortify-headers/template.py
@@ -1,5 +1,5 @@
 pkgname = "fortify-headers"
-pkgver = "2.3.3"
+pkgver = "3.0.1"
 pkgrel = 0
 build_style = "makefile"
 make_check_target = "clang"
@@ -9,7 +9,7 @@ pkgdesc = "Standalone fortify implementation"
 license = "0BSD"
 url = "https://git.2f30.org/fortify-headers"
 source = f"https://github.com/jvoisin/fortify-headers/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "26d81fc55b8ce3db22c7a697616392aeba928e921d975053a3f00221d1a33c08"
+sha256 = "576ac1dc2db9a503d8458c1e9f4a6ce8b0437cfc3c507859d7601a3b8d8ac0e6"
 tool_flags = {"CFLAGS": ["-Wno-macro-redefined"]}
 options = ["bootstrap"]
 
