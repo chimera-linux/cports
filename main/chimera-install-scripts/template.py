b1690e5eedc40c7307ce45a9a73d4346a06e7b79
q66 <q66@chimera-linux.org>
2025-12-20 17:33:34 +0100

    main/chimera-install-scripts: update to 0.6
    This brings some minor improvements to the bootstrap scripts
    and adds a rudimentary TUI installer that is for now experimental
    and probably expected to be broken in subtle cases

--- main/chimera-install-scripts/template.py
+++ main/chimera-install-scripts/template.py
@@ -1,11 +1,12 @@
 pkgname = "chimera-install-scripts"
-pkgver = "0.5"
+pkgver = "0.6"
 pkgrel = 0
-_commit = "437f08305db5b71926f90265fb0279ad143c5e27"
+_commit = "dc0984a59bd94e1f6cc3b31255bad4edafb69d6a"
 build_style = "makefile"
 depends = [
     "cmd:apk!apk-tools",
     "cmd:chroot!chimerautils",
+    "cmd:dialog!dialog",
     "cmd:findmnt!util-linux-mount",
     "cmd:id!chimerautils",
     "cmd:mount!util-linux-mount",
@@ -17,7 +18,7 @@ pkgdesc = "Scripts to aid Chimera system installation"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/chimera-install-scripts"
 source = f"{url}/archive/{_commit}.tar.gz"
-sha256 = "6479e7f3f567f1ff50de09fa214e03f7a402163e44668991115c7b8772d5a726"
+sha256 = "d41919c919d347352b08257489d77531ab1cd214ea858628ae6f4e418c5616a4"
 # no test suite
 options = ["!check"]
 
