d88d3886b60f5c57c7f59cb871d252f41fcf485c
JC Grünhage <jan.christian@gruenhage.xyz>
2025-12-21 00:01:35 +0100

    user/syft: update to 1.38.2

--- user/syft/template.py
+++ user/syft/template.py
@@ -1,6 +1,6 @@
 pkgname = "syft"
-pkgver = "1.38.0"
-pkgrel = 1
+pkgver = "1.38.2"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags= -X main.version={pkgver}",
@@ -11,7 +11,7 @@ pkgdesc = "SBOM generator CLI for container images, filesystems and binaries"
 license = "Apache-2.0"
 url = "https://github.com/anchore/syft"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "56131747bc5cd01475ceced9289f6fb527ccd40bc0cb5ff9f528bb04604638eb"
+sha256 = "5a6969b5e7b172d09049fc728e8e3b99371941bfb9bde4a7c88af1590841a17f"
 # Test suite depends on docker
 # generates manpages/completions with host bins
 options = ["!check", "!cross"]
