95d5ea7424d618c75a087d787ee1c9948035c49b
q66 <q66@chimera-linux.org>
2025-12-20 17:37:14 +0100

    main/chimera-install-scripts: actually install the installer

--- main/chimera-install-scripts/template.py
+++ main/chimera-install-scripts/template.py
@@ -1,7 +1,7 @@
 pkgname = "chimera-install-scripts"
-pkgver = "0.6"
+pkgver = "0.6.1"
 pkgrel = 0
-_commit = "dc0984a59bd94e1f6cc3b31255bad4edafb69d6a"
+_commit = "43b0a7d2c86fa51c85a3fdc532ac5ebf9ece83b1"
 build_style = "makefile"
 depends = [
     "cmd:apk!apk-tools",
@@ -18,7 +18,7 @@ pkgdesc = "Scripts to aid Chimera system installation"
 license = "BSD-2-Clause"
 url = "https://github.com/chimera-linux/chimera-install-scripts"
 source = f"{url}/archive/{_commit}.tar.gz"
-sha256 = "d41919c919d347352b08257489d77531ab1cd214ea858628ae6f4e418c5616a4"
+sha256 = "a2f9ca5c91249c77e039574f174b1695bc6afe79ad09678310eb646989f42e10"
 # no test suite
 options = ["!check"]
 
