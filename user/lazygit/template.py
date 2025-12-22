65f3a83c3aeddcc1c84f4d17113aaacc5c5e9206
Saksham <voidisnull@duck.com>
2025-12-22 15:44:01 +0530

    user/lazygit: update to 0.57.0

--- user/lazygit/template.py
+++ user/lazygit/template.py
@@ -1,6 +1,6 @@
 pkgname = "lazygit"
-pkgver = "0.55.1"
-pkgrel = 1
+pkgver = "0.58.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = [f"-ldflags=-X main.version=v{pkgver}"]
 hostmakedepends = ["go"]
@@ -9,7 +9,7 @@ pkgdesc = "Terminal UI for git commands"
 license = "MIT"
 url = "https://github.com/jesseduffield/lazygit"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "6c11d02c61767aea1b3f2956797f9b4b3fccc526668a3f211d4e6071195ceb2c"
+sha256 = "9f714b3d35fa7cae5a2193cf11a2e9bc97bd0a9bbf52ccaea432765bf8f63f3c"
 # need to be in git checkout
 options = ["!check"]
 
