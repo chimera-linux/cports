33b4f45d389df6a85a95360f7e16f64fe818923c
Ayush Agarwal <ayush@ayushnix.com>
2025-12-24 10:19:11 +0530

    main/fennel: update to 1.6.0

--- main/fennel/template.py
+++ main/fennel/template.py
@@ -1,5 +1,5 @@
 pkgname = "fennel"
-pkgver = "1.5.3"
+pkgver = "1.6.0"
 pkgrel = 0
 build_style = "makefile"
 make_check_target = "test"
@@ -9,7 +9,7 @@ pkgdesc = "Programming language built on top of Lua"
 license = "MIT"
 url = "https://fennel-lang.org"
 source = f"https://git.sr.ht/~technomancy/fennel/archive/{pkgver}.tar.gz"
-sha256 = "0ad230b4919f234e114ae763d179ff9bda8fa55c8833b97b99a1b596f98536ff"
+sha256 = "42942d90bbd68656b6025144bb0527c1ae5a5d55e22a53c7e820325230185bf5"
 
 
 def post_install(self):
