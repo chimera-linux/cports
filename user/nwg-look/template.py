e52ad811eb1dedc3e67c56a276ded15356a60dbb
aniruddhgutta <aniruddhgutta@disroot.org>
2025-12-26 21:29:01 +0530

    user/nwg-look: new package

---
+++ user/nwg-look/template.py
@@ -0,0 +1,30 @@
+pkgname = "nwg-look"
+pkgver = "1.0.6"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = [
+    "go",
+    "pkgconf",
+]
+makedepends = [
+    "gtk+3-devel",
+]
+depends = ["gsettings-desktop-schemas", "xcur2png"]
+pkgdesc = "GTK3 settings editor adapted to work in the wlroots environment"
+license = "MIT"
+url = "https://github.com/nwg-piotr/nwg-look"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "ddaba674253fbbf8f3ee7392315e51408af445adb85ed4b0f70e50301801720d"
+hardening = ["vis", "cfi"]
+
+
+def install(self):
+    self.install_bin(f"build/{pkgname}")
+    self.install_license("LICENSE")
+    self.install_file("stuff/main.glade", f"usr/share/{pkgname}")
+    self.install_files("langs", f"usr/share/{pkgname}", name="langs")
+    self.install_file(f"stuff/{pkgname}.desktop", "usr/share/applications")
+    self.install_file(
+        f"stuff/{pkgname}.svg",
+        "usr/share/icons/hicolor/scalable/apps",
+    )
