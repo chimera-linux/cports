32ab392423e959af9b0953972e66e0ab9a29cbc0
aniruddhgutta <aniruddhgutta@disroot.org>
2025-12-30 15:33:43 +0530

    user/mpdris2-rs: new package
    Closes github.com/chimera-linux/cports/pull/5061

---
+++ user/mpdris2-rs/template.py
@@ -0,0 +1,22 @@
+pkgname = "mpdris2-rs"
+pkgver = "1.1.2"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable"]
+makedepends = [
+    "dinit-chimera",
+    "dinit-dbus",
+    "mpd",
+    "rust-std",
+]
+pkgdesc = "Exposing MPRIS V2.2 D-Bus interface for mpd"
+license = "GPL-3.0-or-later"
+url = "https://github.com/szclsya/mpdris2-rs"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "50ffaa10c47122921ca382beaffa399cb11fa67b42a25f2a117001121ff76662"
+hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    self.install_license("COPYING")
+    self.install_service(self.files_path / "mpdris2-rs.user")
