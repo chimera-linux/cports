763e6ac48ce50cb589a54465165cfe887d42f67a
Ayush Agarwal <ayush@ayushnix.com>
2026-01-04 12:27:58 +0530

    user/age: update to 1.3.1

--- user/age/template.py
+++ user/age/template.py
@@ -1,14 +1,18 @@
 pkgname = "age"
-pkgver = "1.2.1"
-pkgrel = 10
+pkgver = "1.3.1"
+pkgrel = 0
 build_style = "go"
-make_build_args = [f"-ldflags=-X main.Version={pkgver}", "./cmd/..."]
+make_build_args = [
+    f"-ldflags=-X main.Version={pkgver}",
+    "./cmd/...",
+    "./extra/...",
+]
 hostmakedepends = ["go"]
 pkgdesc = "File encryption tool"
 license = "BSD-3-Clause"
 url = "https://github.com/FiloSottile/age"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "93bd89a16c74949ee7c69ef580d8e4cf5ce03e7d9c461b68cf1ace3e4017eef5"
+sha256 = "396007bc0bc53de253391493bda1252757ba63af1a19db86cfb60a35cb9d290a"
 
 
 def post_install(self):
