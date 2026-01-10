19266438f62d0d1c2ff9363967954d54097caca7
Saksham <voidisnull@duck.com>
2026-01-10 22:34:05 +0530

    user/television: update to 0.14.4

--- user/television/template.py
+++ user/television/template.py
@@ -1,5 +1,5 @@
 pkgname = "television"
-pkgver = "0.13.11"
+pkgver = "0.14.4"
 pkgrel = 0
 build_style = "cargo"
 make_check_args = [
@@ -17,7 +17,7 @@ pkgdesc = "Fuzzy finder"
 license = "MIT"
 url = "https://github.com/alexpasmantier/television"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "a524e0cb07224794df7fda729a0aa90d77d7dfbb87a1a46a9b3b1a3c838532d5"
+sha256 = "039d554569117c665e1b4336c427747a118cca781ddbffaf701c4b5f01c7f3e1"
 # generates completions with host binary
 options = ["!cross"]
 
