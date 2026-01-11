c40068080b0c0c3ee008af06130cd1a68d670157
q66 <q66@chimera-linux.org>
2026-01-11 19:56:00 +0100

    main/perl-sub-quote: update to 2.006009

--- main/perl-sub-quote/template.py
+++ main/perl-sub-quote/template.py
@@ -1,6 +1,6 @@
 pkgname = "perl-sub-quote"
-pkgver = "2.006008"
-pkgrel = 1
+pkgver = "2.006009"
+pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = [
     "perl",
@@ -11,4 +11,4 @@ pkgdesc = "Perl module for eval-based subroutine generation"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/pod/Sub::Quote"
 source = f"$(CPAN_SITE)/Sub/Sub-Quote-{pkgver}.tar.gz"
-sha256 = "94bebd500af55762e83ea2f2bc594d87af828072370c7110c60c238a800d15b2"
+sha256 = "967282d54d2d51b198c67935594f93e4dea3e54d1e5bced158c94e29be868a4b"
