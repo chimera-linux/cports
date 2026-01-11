824efac70b164cadb9d65cd16d96d501c7cf0f8f
q66 <q66@chimera-linux.org>
2026-01-11 19:55:04 +0100

    main/perl-image-exiftool: update to 13.45

--- main/perl-image-exiftool/template.py
+++ main/perl-image-exiftool/template.py
@@ -1,6 +1,6 @@
 pkgname = "perl-image-exiftool"
-pkgver = "13.30"
-pkgrel = 1
+pkgver = "13.45"
+pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["perl"]
 makedepends = ["perl"]
@@ -9,7 +9,7 @@ pkgdesc = "Perl module for editing exif metadata"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://exiftool.org"
 source = f"{url}/Image-ExifTool-{pkgver}.tar.gz"
-sha256 = "885afd06c4efcc60d1df703cc88ba7ddc3bb6fed854cfbaa9e6cd72adfbe8da9"
+sha256 = "c2328f14b86be36c624332f93d387fbe3fb37f1ff3c1d26e7e6eaf65e0a9047b"
 
 
 @subpackage("perl-image-exiftool-progs")
