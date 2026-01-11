be9937fd434a792334ec8126d1ba09e02270a55c
q66 <q66@chimera-linux.org>
2026-01-11 19:56:22 +0100

    main/perl-uri: update to 5.34

--- main/perl-uri/template.py
+++ main/perl-uri/template.py
@@ -1,6 +1,6 @@
 pkgname = "perl-uri"
-pkgver = "5.32"
-pkgrel = 1
+pkgver = "5.34"
+pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["perl"]
 makedepends = ["perl"]
@@ -9,6 +9,6 @@ pkgdesc = "Perl Uniform Resource Identifiers module"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/pod/URI"
 source = f"$(CPAN_SITE)/URI/URI-{pkgver}.tar.gz"
-sha256 = "9632067d34e14e0dae2da94631c4f25a387fcc48d06fa29330e8b3c04c4e913d"
+sha256 = "de64c779a212ff1821896c5ca2bb69e74767d2674cee411e777deea7a22604a8"
 # missing checkdepends
 options = ["!check"]
