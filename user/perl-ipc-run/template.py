606fff818ac43af172b8de16b1eacbe98e94ad03
q66 <q66@chimera-linux.org>
2026-01-11 19:56:58 +0100

    user/perl-ipc-run: update to 20250809.0

--- user/perl-ipc-run/template.py
+++ user/perl-ipc-run/template.py
@@ -1,6 +1,6 @@
 pkgname = "perl-ipc-run"
-pkgver = "20231003.0"
-pkgrel = 1
+pkgver = "20250809.0"
+pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = ["perl"]
 depends = ["perl-io-tty"]
@@ -8,4 +8,4 @@ pkgdesc = "Perl system() and background procs w/ piping, redirs, ptys"
 license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
 url = "https://metacpan.org/dist/IPC-Run"
 source = f"$(CPAN_SITE)/IPC/IPC-Run-{pkgver}.tar.gz"
-sha256 = "eb25bbdf5913d291797ef1bfe998f15130b455d3ed02aacde6856f0b25e4fe57"
+sha256 = "b1e85a30405786ed8378b68dd57159315ad7ddc0a55e432aa9eeca6166ca53fe"
