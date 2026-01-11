b7f42612d99c3fa6cdf7f34496ba1165b389a49d
q66 <q66@chimera-linux.org>
2026-01-11 20:11:52 +0100

    main/perl-crypt-urandom: new package

---
+++ main/perl-crypt-urandom/template.py
@@ -0,0 +1,12 @@
+pkgname = "perl-crypt-urandom"
+pkgver = "0.54"
+pkgrel = 0
+build_style = "perl_module"
+hostmakedepends = ["perl"]
+makedepends = ["perl"]
+depends = ["perl"]
+pkgdesc = "Perl module for non-blocking randomness"
+license = "Artistic-1.0-Perl OR GPL-1.0-or-later"
+url = "https://metacpan.org/dist/Crypt-URandom"
+source = f"$(CPAN_SITE)/Crypt/Crypt-URandom-{pkgver}.tar.gz"
+sha256 = "4a73cd394933328da484aaeb8645d735b35465df60109e559e0a28b066053a57"
