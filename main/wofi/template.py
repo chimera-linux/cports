410f2c48f13ab1a75d96b895e9d5d4ebfd586d5f
Saksham <voidisnull@duck.com>
2026-01-12 22:53:02 +0530

    main/wofi: update to 1.5.3

--- main/wofi/template.py
+++ main/wofi/template.py
@@ -1,6 +1,6 @@
 pkgname = "wofi"
-pkgver = "1.4.1"
-pkgrel = 1
+pkgver = "1.5.3"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
 makedepends = ["gtk+3-devel", "wayland-devel"]
@@ -8,7 +8,7 @@ pkgdesc = "Launcher/menu program for wlroots-based wayland compositors"
 license = "GPL-3.0-or-later"
 url = "https://sr.ht/~scoopta/wofi"
 source = f"https://hg.sr.ht/~scoopta/wofi/archive/v{pkgver}.tar.gz"
-sha256 = "e95e35c03551c39178c16ad6213a88e3883a236e942d7f2666c780d934c270bb"
+sha256 = "6216dc14d93cdb6170f89c1ca3aaacdeaa44862fbc9be947d614be266a9c49f6"
 # vis breaks all modes
 hardening = ["!vis"]
 # no check
