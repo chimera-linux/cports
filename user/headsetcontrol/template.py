901415483e4ea61aa78caa1885d39603414ffed6
q66 <q66@chimera-linux.org>
2026-01-17 16:37:54 +0100

    user/headsetcontrol: update to 3.1.0_git20260114
    The stable tag is missing support for a bunch of devices, we can
    converge on a tag again once they tag one

--- user/headsetcontrol/template.py
+++ user/headsetcontrol/template.py
@@ -1,7 +1,8 @@
 pkgname = "headsetcontrol"
-pkgver = "3.1.0"
+pkgver = "3.1.0_git20260114"
 pkgrel = 0
 build_style = "cmake"
+configure_args = [f"-DGIT_VERSION={pkgver}"]
 hostmakedepends = [
     "cmake",
     "ninja",
@@ -11,5 +12,12 @@ makedepends = ["hidapi-devel"]
 pkgdesc = "Tool to control gaming headsets"
 license = "GPL-3.0-or-later"
 url = "https://github.com/Sapd/HeadsetControl"
-source = f"{url}/archive/refs/tags/3.1.0.tar.gz"
-sha256 = "caba01afa69477f0a4fce1f8608a0c5e85e7032c350c1239bb4e1ecdfb171359"
+# source = f"{url}/archive/refs/tags/3.1.0.tar.gz"
+source = f"{url}/archive/6fe0cec4f8baeae5e6441489df02c395e39c6ae2.tar.gz"
+sha256 = "9ee16cd2b54bd6630fd3cb34f5b25bd1004205babc9a2dfea7bb839980952183"
+
+
+def post_install(self):
+    # library not yet necessary
+    self.uninstall("usr/include")
+    self.uninstall("usr/lib/*.a", glob=True)
