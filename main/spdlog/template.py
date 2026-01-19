f2e39bbc3d75abdc2fd7cdf2f9ca39e57472ced4
Matthias Nyman <natthias@proton.me>
2026-01-20 00:29:48 +0100

    main/spdlog: update to 1.17.0

--- main/spdlog/template.py
+++ main/spdlog/template.py
@@ -1,5 +1,5 @@
 pkgname = "spdlog"
-pkgver = "1.16.0"
+pkgver = "1.17.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -10,11 +10,12 @@ configure_args = [
 ]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = ["catch2-devel", "elogind-devel", "fmt-devel"]
+checkdepends = ["tzdb"]
 pkgdesc = "C++ logging library"
 license = "MIT"
 url = "https://github.com/gabime/spdlog"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "8741753e488a78dd0d0024c980e1fb5b5c85888447e309d9cb9d949bdb52aa3e"
+sha256 = "d8862955c6d74e5846b3f580b1605d2428b11d97a410d86e2fb13e857cd3a744"
 
 
 def post_install(self):
