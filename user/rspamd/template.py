1e7f644be9b410f5a0cbb39bcaaab9b7a5900e64
Matthias Nyman <natthias@proton.me>
2026-01-20 01:28:43 +0100

    user/rspamd: update to 3.14.3

--- user/rspamd/template.py
+++ user/rspamd/template.py
@@ -1,6 +1,6 @@
 pkgname = "rspamd"
-pkgver = "3.14.2"
-pkgrel = 1
+pkgver = "3.14.3"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DCONFDIR=/etc/rspamd",
@@ -41,7 +41,7 @@ pkgdesc = "Spam filtering system"
 license = "Apache-2.0 AND BSD-2-Clause AND BSD-3-Clause AND BSL-1.0 AND CC0-1.0 AND LGPL-3.0-only AND MIT AND Zlib"
 url = "https://rspamd.com"
 source = f"https://github.com/rspamd/rspamd/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "b8402b639b2480b793795907d53609f375fd19b99501f777ff49f2e410edfb5b"
+sha256 = "ac9123f38174f1909856f4d7c104b0cf600ee1ad51958b486dfde1275af7c01a"
 
 
 match self.profile().arch:
