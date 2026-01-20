4c098d876f2f79fd5ed0c69722b201cf7f00419a
Matthias Nyman <natthias@proton.me>
2026-01-20 17:37:51 +0100

    */*: rebuild for spdlog

--- main/mtxclient/template.py
+++ main/mtxclient/template.py
@@ -1,6 +1,6 @@
 pkgname = "mtxclient"
 pkgver = "0.10.1"
-pkgrel = 1
+pkgrel = 2
 build_style = "cmake"
 configure_args = ["-DBUILD_LIB_EXAMPLES=OFF"]
 make_check_args = [
--- main/nheko/template.py
+++ main/nheko/template.py
@@ -1,6 +1,6 @@
 pkgname = "nheko"
 pkgver = "0.12.1"
-pkgrel = 1
+pkgrel = 2
 build_style = "cmake"
 hostmakedepends = [
     "asciidoctor",
