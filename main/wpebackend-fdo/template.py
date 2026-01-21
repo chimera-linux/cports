58d341dce705c9599a86b828192b5ed7d089e4e3
q66 <q66@chimera-linux.org>
2026-01-21 15:49:53 +0100

    main/wpebackend-fdo: update to 1.16.1

--- main/wpebackend-fdo/template.py
+++ main/wpebackend-fdo/template.py
@@ -1,5 +1,5 @@
 pkgname = "wpebackend-fdo"
-pkgver = "1.16.0"
+pkgver = "1.16.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "wayland-progs"]
@@ -8,7 +8,7 @@ pkgdesc = "Wayland backend for WPE WebKit"
 license = "BSD-2-Clause"
 url = "https://wpewebkit.org"
 source = f"{url}/releases/wpebackend-fdo-{pkgver}.tar.xz"
-sha256 = "beddf321232d5bd08106c179dbc600f8ce88eb3620b4a59a6329063b78f64635"
+sha256 = "544ae14012f8e7e426b8cb522eb0aaaac831ad7c35601d1cf31d37670e0ebb3b"
 
 
 def post_install(self):
