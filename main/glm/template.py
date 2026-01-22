57d2f8ec13945a62621565b136de117f658198cb
q66 <q66@chimera-linux.org>
2026-01-22 15:02:29 +0100

    main/glm: update to 1.0.3

--- main/glm/template.py
+++ main/glm/template.py
@@ -1,5 +1,5 @@
 pkgname = "glm"
-pkgver = "1.0.2"
+pkgver = "1.0.3"
 pkgrel = 0
 build_style = "cmake"
 # unversioned library built out of all the headers combined
@@ -13,7 +13,7 @@ pkgdesc = "OpenGL Mathematics"
 license = "custom:Happy-Bunny-License"
 url = "https://github.com/g-truc/glm"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "19edf2e860297efab1c74950e6076bf4dad9de483826bc95e2e0f2c758a43f65"
+sha256 = "6775e47231a446fd086d660ecc18bcd076531cfedd912fbd66e576b118607001"
 hardening = ["!int"]
 
 
