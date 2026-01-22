745416f803fdedd9aa73ea84d1d9687d1ceb0fcf
q66 <q66@chimera-linux.org>
2026-01-22 15:21:27 +0100

    main/md4c: move from user

--- main/md4c/template.py
+++ main/md4c/template.py
@@ -1,6 +1,6 @@
 pkgname = "md4c"
 pkgver = "0.5.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 pkgdesc = "Fast CommonMark compliant markdown parser"
