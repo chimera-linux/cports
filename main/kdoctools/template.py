36b195826f0b2858305af77ad0459b42e3a45d21
q66 <q66@chimera-linux.org>
2026-01-23 22:01:57 +0100

    main/kdoctools: unsplit stylesheets/common files
    We don't want to put those in -doc package as they are necessary
    by docs viewable through this framework.

--- main/kdoctools/template.py
+++ main/kdoctools/template.py
@@ -1,6 +1,6 @@
 pkgname = "kdoctools"
 pkgver = "6.22.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -19,12 +19,16 @@ makedepends = [
     "qt6-qttools-devel",
 ]
 depends = ["docbook-xsl", "libxml2-progs"]
+provides = [self.with_pkgver("kdoctools-doc")]
 pkgdesc = "KDE Documentation generation from docbook"
 license = "LGPL-2.1-or-later"
 url = "https://api.kde.org/frameworks/kdoctools/html"
 source = f"$(KDE_SITE)/frameworks/{pkgver[: pkgver.rfind('.')]}/kdoctools-{pkgver}.tar.xz"
 sha256 = "d893f2758f9e1d9a529d1b573b3fc413d4c3e0cf4b8573ecee5c99c8d66a3f11"
 hardening = ["vis"]
+# the "docs" are really common stylesheets that are needed
+# by things using kdoctools so make sure they get installed
+options = ["!splitdoc"]
 
 
 @subpackage("kdoctools-devel")
