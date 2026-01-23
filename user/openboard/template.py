b8741be2c420ca593af5f9392bc3160beaf4ba9e
breakgimme <adam@plock.com>
2026-01-23 02:29:05 +0100

    user/openboard: update to 1.7.4

--- user/openboard/template.py
+++ user/openboard/template.py
@@ -1,6 +1,6 @@
 pkgname = "openboard"
-pkgver = "1.7.3"
-pkgrel = 13
+pkgver = "1.7.4"
+pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DCMAKE_CXX_STANDARD=20"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -20,4 +20,4 @@ pkgdesc = "Interactive whiteboard application"
 license = "GPL-3.0-or-later"
 url = "https://openboard.ch"
 source = f"https://github.com/OpenBoard-org/OpenBoard/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "55532df042e3a5b36e1f6f1e29916d3bbd01796d920782fa1f8a03438dcddd9c"
+sha256 = "9948a2fec3920679087422d5c1c1b18839e2a95cee5b485d7584bb972b54a13c"
