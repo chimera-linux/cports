bf54af565b69730c22ba6d723fc1dae57593919b
Gabriel M. Dutra <gabrielmdutra@pm.me>
2026-01-26 03:58:16 +0000

    user/opentofu: update to 1.11.4

--- user/opentofu/template.py
+++ user/opentofu/template.py
@@ -1,5 +1,5 @@
 pkgname = "opentofu"
-pkgver = "1.10.7"
+pkgver = "1.11.4"
 pkgrel = 0
 build_style = "go"
 make_build_args = [
@@ -11,4 +11,4 @@ pkgdesc = "Tool for building, changing and versioning infrastructure"
 license = "MPL-2.0"
 url = "https://github.com/opentofu/opentofu"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0038ca3064d90ac1c390a168539e731331f0ac80eaffd8fcaecac586790cf306"
+sha256 = "89dd393aeae8fe3cbcc8c4813354fb4d06f8ec67c824b85f86b7b24222eb632f"
