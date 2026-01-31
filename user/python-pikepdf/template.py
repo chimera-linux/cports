d5114d803692d4822dde6bbb7c2d24db139ddd1f
James Adam Armstrong <gwenio@live.com>
2026-01-31 11:01:53 -0600

    user/python-pikepdf: update to 10.3.0

--- user/python-pikepdf/template.py
+++ user/python-pikepdf/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-pikepdf"
-pkgver = "10.2.0"
+pkgver = "10.3.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -19,7 +19,7 @@ pkgdesc = "Lossless conversion of raster images to PDF"
 license = "MPL-2.0"
 url = "https://github.com/pikepdf/pikepdf"
 source = f"https://github.com/pikepdf/pikepdf/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "749dc71cb3dd2180a77a2a40832d53260fcf39a1b1ece9fe780aff103ea37ac5"
+sha256 = "bc792cf00acc39ea38243b6279b4cd9014909e6e8567d33c7ec84fe259dad4cc"
 # uses invalid options for pytest in pyproject.toml
 options = ["!check"]
 
