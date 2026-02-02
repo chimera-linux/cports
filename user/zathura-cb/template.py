e2de5585b9320fae2a8f045dd557a621e79a0aec
Ayush Agarwal <ayush@ayushnix.com>
2026-02-02 18:15:57 +0530

    user/zathura-cb: update to 2026.02.03

--- user/zathura-cb/template.py
+++ user/zathura-cb/template.py
@@ -1,6 +1,6 @@
 pkgname = "zathura-cb"
-pkgver = "0.1.12"
-pkgrel = 1
+pkgver = "2026.02.03"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "meson",
@@ -15,7 +15,7 @@ pkgdesc = "Comic book support for zathura"
 license = "Zlib"
 url = "https://pwmt.org/projects/zathura-cb"
 source = f"{url}/download/zathura-cb-{pkgver}.tar.xz"
-sha256 = "bc62dec4d04d51419192d370ecdf2afa66ba10554c2518abecee0dfce2aac96e"
+sha256 = "743e37b16b8095c54996afb6a11e4713f0ed3988ed3e1b0bdb8126a2eee83c5c"
 
 
 def post_install(self):
