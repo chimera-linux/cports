fed9feba32998b645b8de79579608f7ced28f83e
flukey <flukey@vapourmail.eu>
2026-02-03 15:06:25 +0000

    main/ffmpeg: update to 8.0.1

--- main/ffmpeg/template.py
+++ main/ffmpeg/template.py
@@ -1,5 +1,5 @@
 pkgname = "ffmpeg"
-pkgver = "8.0"
+pkgver = "8.0.1"
 pkgrel = 0
 build_style = "configure"
 configure_args = [
@@ -142,7 +142,7 @@ pkgdesc = "Decoding, encoding and streaming software"
 license = "GPL-3.0-or-later"
 url = "https://ffmpeg.org"
 source = f"{url}/releases/ffmpeg-{pkgver}.tar.xz"
-sha256 = "b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e"
+sha256 = "05ee0b03119b45c0bdb4df654b96802e909e0a752f72e4fe3794f487229e5a41"
 # some conf checks like for some pthread functions don't detect interfaces
 # without it
 tool_flags = {"CFLAGS": ["-D_GNU_SOURCE"]}
