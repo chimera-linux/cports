c7311516f5220ef56e7a6a58ba85c4feead2241b
aniruddhgutta <aniruddhgutta@disroot.org>
2026-02-05 18:59:55 +0530

    user/nwg-look: new package
    Closes https://github.com/chimera-linux/cports/pull/5176

---
+++ user/nwg-look/template.py
@@ -0,0 +1,24 @@
+pkgname = "nwg-look"
+pkgver = "1.1.1"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go", "pkgconf"]
+makedepends = ["gtk+3-devel"]
+depends = ["xcur2png"]
+pkgdesc = "GTK settings editor for wlroots"
+license = "MIT"
+url = "https://github.com/nwg-piotr/nwg-look"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "568c5efe443892d74ffce6cf8ac7db2aea6071be70d97d3ba7c5efd8b351e601"
+
+
+def install(self):
+    self.install_bin("build/nwg-look")
+    self.install_license("LICENSE")
+    self.install_file("stuff/main.glade", "usr/share/nwg-look")
+    self.install_files("langs", "usr/share/nwg-look")
+    self.install_file("stuff/nwg-look.desktop", "usr/share/applications")
+    self.install_file(
+        "stuff/nwg-look.svg",
+        "usr/share/icons/hicolor/scalable/apps",
+    )
