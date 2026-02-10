edd9e610e1a1d281f5a449dd53ab1b5dc4169e7b
q66 <q66@chimera-linux.org>
2026-02-11 00:44:11 +0100

    user/nuclei: update to 3.7.0

--- user/nuclei/template.py
+++ user/nuclei/template.py
@@ -1,14 +1,17 @@
 pkgname = "nuclei"
-pkgver = "3.4.10"
-pkgrel = 2
+pkgver = "3.7.0"
+pkgrel = 0
 build_style = "go"
+prepare_after_patch = True
 make_build_args = ["./cmd/nuclei"]
 hostmakedepends = ["go"]
 pkgdesc = "Web vulnerability scanner"
 license = "MIT"
 url = "https://github.com/projectdiscovery/nuclei"
-source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "d5604ef47d31abba42814876b637105f0b1e9b9167b79206aeeeb2935f102102"
+# source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+# temporary while the release can't support go 1.26
+source = f"{url}/archive/8aa427a6ea8d5e1faf4f109ccc94156431018582.tar.gz"
+sha256 = "a5a97b4953f053c08d8ecdd7bdb50bb45aa98b8204fc00a7dcb788befa85a24f"
 # Nuclei tests requires network connection
 options = ["!check"]
 
