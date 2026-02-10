1f2ef23e45f99ada43633171b5c59baa371c2912
q66 <q66@chimera-linux.org>
2026-02-10 23:54:03 +0100

    main/go-bootstrap: update to 1.25.4

--- main/go-bootstrap/template.py
+++ main/go-bootstrap/template.py
@@ -1,5 +1,5 @@
 pkgname = "go-bootstrap"
-pkgver = "1.24.0"
+pkgver = "1.25.4"
 pkgrel = 0
 # just in case
 depends = ["!go"]
@@ -11,23 +11,23 @@ options = ["!strip", "!scanrundeps", "!lintstatic", "foreignelf", "execstack"]
 match self.profile().goarch:
     case "arm64":
         sha256 = (
-            "a694eb1a1e1c45f435f673040bfe5f5f0b6f028eb796fb9c50de7f72217fc50f"
+            "97cc9292ef0e2cfe44d46cb9d7b4fca92d3e636ebe21141745fea3d334a4acbd"
         )
     case "loong64":
         sha256 = (
-            "dc709c3762393fdb6ca28c23449b51568c59947bbebd1f3cd43633a76d361d02"
+            "dd39af03da38b40d01b640dd85c96932b119747fe8d5f5896c9b423ee7c56f4d"
         )
     case "ppc64le":
         sha256 = (
-            "158f4045f8a6ae0926d488e71582cfad962a1683d9ef6b2789e15ad5020f17a0"
+            "a47c93c24670600b8e23ac1070f22f85d8178b6faf253d90cab6a1b213ba10d6"
         )
     case "riscv64":
         sha256 = (
-            "263f04498e2e2479488728b3c1da7609b06cd18d8033f925538fd43b617fb318"
+            "821c690b1cc237ea5960560d846c740f0549b39d6d9ad7d7660fd6801f831ecc"
         )
     case "amd64":
         sha256 = (
-            "8f6ba7f20231f7c60ef1100fccda81ed49102f8fe9d32eef091df2d14d540604"
+            "b6ceb7a6f33b5e209beeaa8ddd63e6cacae5d6cf2ccd21e080019c6edff953a2"
         )
     case _:
         broken = f"not yet built for {self.profile().arch}"
