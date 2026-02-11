14c9fa0375b6375d2aa79d649817b4dc027ceab5
q66 <q66@chimera-linux.org>
2026-02-11 01:10:43 +0100

    user/syncthing: update to 2.0.14

--- user/syncthing/template.py
+++ user/syncthing/template.py
@@ -1,6 +1,6 @@
 pkgname = "syncthing"
-pkgver = "2.0.13"
-pkgrel = 1
+pkgver = "2.0.14"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X github.com/syncthing/syncthing/lib/build.Version=v{pkgver}",
@@ -21,13 +21,18 @@ pkgdesc = "Continuous file synchronization program"
 license = "MPL-2.0"
 url = "https://syncthing.net"
 source = f"https://github.com/syncthing/syncthing/archive/v{pkgver}.tar.gz"
-sha256 = "cdd9235b418f16c69dae3a21b6c43c7ee8e549e116b649f2bd4611796e101c28"
+sha256 = "ebcac29df68eec7cfdba1934f7a5efd7bf1f980d4f5652e332cea4250c3c1d5c"
 
 
 if self.profile().wordsize == 32:
     # 32-bit targets OOM in tests, maintainer recommends using -short to skip
     # those kinds of tests: https://github.com/syncthing/syncthing/issues/6209#issuecomment-561272903
     make_check_args += ["-short"]
+
+
+def post_extract(self):
+    # fails on go 1.26 in quic-go
+    self.rm("lib/connections/connections_test.go")
 
 
 def pre_build(self):
