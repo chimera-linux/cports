3d5cfd1a9acfacf3d216b7f843029471d136b191
xanadu3 <63268320+xanadu3@users.noreply.github.com>
2026-02-20 00:12:03 +0100

    Update template.py
    Since version 4.0, Inter font provides a TTC file of its static fonts, consolidating redundant data.

--- user/fonts-inter/template.py
+++ user/fonts-inter/template.py
@@ -1,6 +1,6 @@
 pkgname = "fonts-inter"
 pkgver = "4.1"
-pkgrel = 0
+pkgrel = 1
 pkgdesc = "Inter typeface family"
 license = "OFL-1.1"
 url = "https://rsms.me/inter"
@@ -13,13 +13,14 @@ def install(self):
     self.install_file("*.ttf", "usr/share/fonts/inter", glob=True)
     self.install_file("extras/ttf/*.ttf", "usr/share/fonts/inter", glob=True)
     self.install_file("extras/otf/*.otf", "usr/share/fonts/inter", glob=True)
+    self.install_file("*.ttc", "usr/share/fonts/inter", glob=True)
     self.install_license("LICENSE.txt")
 
 
 @subpackage("fonts-inter-otf")
 def _(self):
-    self.subdesc = "OpenType"
-    self.depends = [self.parent, "!fonts-inter-ttf"]
+    self.subdesc = "OpenType (OTF): static CFF outlines; one SFNT file per font face."
+    self.depends = [self.parent, "!fonts-inter-ttf", "!fonts-inter-ttc"]
     self.install_if = [self.parent]
 
     return ["usr/share/fonts/inter/*.otf"]
@@ -27,7 +28,17 @@ def _(self):
 
 @subpackage("fonts-inter-ttf")
 def _(self):
-    self.subdesc = "TrueType"
-    self.depends = [self.parent, "!fonts-inter-otf"]
+    self.subdesc = "TrueType (TTF): static glyf outlines; one SFNT file per font face."
+    self.depends = [self.parent, "!fonts-inter-otf", "!fonts-inter-ttc"]
+    self.install_if = [self.parent]
 
     return ["usr/share/fonts/inter/*.ttf"]
+
+
+@subpackage("fonts-inter-ttc")
+def _(self):
+    self.subdesc = "TrueType Collection (TTC): all static glyf outline tables in one SFNT file; redundancies among faces deduplicated."
+    self.depends = [self.parent, "!fonts-inter-otf", "!fonts-inter-ttf"]
+    self.install_if = [self.parent]
+
+    return ["usr/share/fonts/inter/*.ttc"] 
