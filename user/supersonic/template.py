c73b7d8b2cd2b30f250dc2b96e60b281e361a935
crispy-caesus <crispy@crispy-caesus.eu>
2026-02-23 00:42:15 +0100

    user/supersonic: new package

---
+++ user/supersonic/template.py
@@ -0,0 +1,32 @@
+pkgname = "supersonic"
+pkgver = "0.20.1"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go", "pkgconf"]
+makedepends = [
+    "libxcursor-devel",
+    "libxi-devel",
+    "libxinerama-devel",
+    "mpv-devel",
+]
+go_build_tags = ["migrated_fynedo"]
+pkgdesc = "Client for Subsonic and Jellyfin music servers"
+license = "GPL-3.0-or-later AND BSD-3-Clause AND MIT"
+url = "https://github.com/dweymouth/supersonic"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "6a51404e2388959058a6d42c069c858f15c224f6543c7b2b3de60deb5e32da78"
+
+
+def post_install(self):
+    self.rename("usr/bin/supersonic", "supersonic-desktop")
+    self.install_license("LICENSE")
+    for res in [128, 256, 512]:
+        self.install_file(
+            f"res/appicon-{res}.png",
+            f"usr/share/icons/hicolor/{res}x{res}/apps",
+            name="supersonic-desktop.png",
+        )
+    self.install_file(
+        "res/supersonic-desktop.desktop",
+        "usr/share/applications",
+    )
