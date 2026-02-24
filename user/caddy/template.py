334372aba02fccb1802e2e2639b0c6fdcbee41f7
nune <145225213+gigirassy@users.noreply.github.com>
2026-02-23 20:50:44 -0500

    user/caddy: update to 2.11.1

--- user/caddy/template.py
+++ user/caddy/template.py
@@ -1,6 +1,6 @@
 pkgname = "caddy"
-pkgver = "2.10.2"
-pkgrel = 3
+pkgver = "2.11.1"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X github.com/caddyserver/caddy/v2.CustomVersion=v{pkgver}",
@@ -14,7 +14,7 @@ pkgdesc = "Extensible HTTP server with automatic HTTPS"
 license = "Apache-2.0"
 url = "https://caddyserver.com"
 source = f"https://github.com/caddyserver/caddy/archive/v{pkgver}.tar.gz"
-sha256 = "f63f46b7ae68ced0a5c2e31df1b6dfc7656117d162a1bc7fed4bd4afd14ddc8f"
+sha256 = "6d0a9e90b3a7ea0b385a66003f493c1a7317f47d57faadd0ee7d82798b7d89e3"
 # generates completions with host binary
 options = ["!cross"]
 
