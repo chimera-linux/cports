ffc828ca1b3ebc340fa4bfd517d7266d64d02f38
q66 <q66@chimera-linux.org>
2026-02-26 15:09:00 +0100

    main/libphonenumber: update to 9.0.25

--- main/libphonenumber/template.py
+++ main/libphonenumber/template.py
@@ -1,5 +1,5 @@
 pkgname = "libphonenumber"
-pkgver = "9.0.22"
+pkgver = "9.0.25"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -27,7 +27,7 @@ pkgdesc = "Library for parsing, formatting, and validating phone numbers"
 license = "Apache-2.0"
 url = "https://github.com/google/libphonenumber"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "2a5bd5ea96a497cb917511f521b638b1e953212efbd3c601653df07ebd99289d"
+sha256 = "de4cfebe1e1da4ecfb687c7bdf312d93b37548737f5e52444d2d29d0b36d2512"
 
 
 @subpackage("libphonenumber-devel")
