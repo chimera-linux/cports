034940cffe05b3c8d1c7da0d3003ec0a556ea3be
nune <145225213+gigirassy@users.noreply.github.com>
2026-03-06 08:17:06 -0500

    user/kind: update to 0.31.0

--- user/kind/template.py
+++ user/kind/template.py
@@ -1,6 +1,6 @@
 pkgname = "kind"
-pkgver = "0.29.0"
-pkgrel = 3
+pkgver = "0.31.0"
+pkgrel = 0
 build_style = "go"
 make_check_args = ["-skip", "TestIntegrationEnsureNetworkConcurrent"]
 hostmakedepends = ["go"]
@@ -8,7 +8,7 @@ pkgdesc = "Containerized Kubernetes Environment in Docker"
 license = "Apache-2.0"
 url = "https://kind.sigs.k8s.io"
 source = f"https://github.com/kubernetes-sigs/kind/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "32fcd55671f241b7a782400e1bf1c762f9729526850e7eda08f56451f12268ea"
+sha256 = "f4aaa1f572f9965eea3f7513d166f545f41b61ab5efeed953048bdcb13c51032"
 # cross: uses host binary to generate completions
 options = ["!cross"]
 
