5be793b53c54b6b4edd382a26cbcfd4cfd1fc200
nune <145225213+gigirassy@users.noreply.github.com>
2026-03-06 20:22:20 -0500

    user/opentofu: update to 1.11.5
    Closes #5278

--- user/opentofu/template.py
+++ user/opentofu/template.py
@@ -1,14 +1,15 @@
 pkgname = "opentofu"
-pkgver = "1.10.7"
-pkgrel = 1
+pkgver = "1.11.5"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X main.version={pkgver} -X github.com/opentofu/opentofu/version.dev=no",
     "./cmd/tofu",
 ]
 hostmakedepends = ["go"]
+checkdepends = ["bash"]
 pkgdesc = "Tool for building, changing and versioning infrastructure"
 license = "MPL-2.0"
 url = "https://github.com/opentofu/opentofu"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0038ca3064d90ac1c390a168539e731331f0ac80eaffd8fcaecac586790cf306"
+sha256 = "450f962f262d9f484ad1fb73454650740cdce0d83a854ad8b6c183cc5822eb09"
