f28e91e04d87ae9fb7eb68acfadadf385f0a4384
breakgimme <adam@plock.com>
2026-03-12 19:00:46 +0100

    main/docker-cli-buildx: update to 0.32.1

--- main/docker-cli-buildx/template.py
+++ main/docker-cli-buildx/template.py
@@ -1,6 +1,6 @@
 pkgname = "docker-cli-buildx"
-pkgver = "0.25.0"
-pkgrel = 4
+pkgver = "0.32.1"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X github.com/docker/buildx/version.Version=v{pkgver}",
@@ -14,7 +14,7 @@ pkgdesc = "Docker CLI plugin for extended build capabilities with BuildKit"
 license = "Apache-2.0"
 url = "https://github.com/docker/buildx"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "e5a7573a5995c0f12c86d35a8148b2a10a6f1b11d1cf8c6977bf03ac281e6959"
+sha256 = "fc4b82f21f766a704fb26239550d37dfed3936b6e76580cddb00407f13f48f6d"
 # some tests rely on network
 options = ["!check"]
 
