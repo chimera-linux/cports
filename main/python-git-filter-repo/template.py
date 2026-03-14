77a4df3ddcaa1a4d1a0ea419d81e0fb3d03361ef
yiyitiantian <140891108+yiyitiantian@users.noreply.github.com>
2026-03-14 09:22:44 +0800

    main/python-git-filter-repo: rebuild to enable tests
    update pkgdesc

--- main/python-git-filter-repo/template.py
+++ main/python-git-filter-repo/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-git-filter-repo"
 pkgver = "2.47.0"
-pkgrel = 1
+pkgrel = 2
 build_style = "python_pep517"
 make_build_env = {"SETUPTOOLS_SCM_PRETEND_VERSION": pkgver}
 hostmakedepends = [
@@ -10,18 +10,16 @@ hostmakedepends = [
 ]
 depends = ["git", "python"]
 checkdepends = ["bash", "rsync", *depends]
-pkgdesc = "Tool for rewriting git history"
+pkgdesc = "Quickly rewrite git repository history"
 license = "MIT"
 url = "https://github.com/newren/git-filter-repo"
 # pypi tarball doesn't contain man page
 source = f"{url}/releases/download/v{pkgver}/git-filter-repo-{pkgver}.tar.xz"
 sha256 = "4662cbe5918196a9f1b5b3e1211a32e61cff1812419c21df4f47c5439f09e902"
-# TODO: figure out why some of the tests fail
-options = ["!check"]
 
 
 def check(self):
-    self.do("../t/run_tests")
+    self.do("./t/run_tests")
 
 
 def post_install(self):
