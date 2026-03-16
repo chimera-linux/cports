537fc5a6eafce6b5520e99864be0d232de68e994
q66 <q66@chimera-linux.org>
2026-03-16 17:07:15 +0100

    main/flashrom: drop fmap-verify support on big endian for now
    Better than disabling the whole CLI, which is the part that
    everyone actually uses.

--- main/flashrom/template.py
+++ main/flashrom/template.py
@@ -1,6 +1,6 @@
 pkgname = "flashrom"
 pkgver = "1.7.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = ["-Duse_internal_dmi=false"]
 hostmakedepends = ["meson", "pkgconf", "python-sphinx"]
