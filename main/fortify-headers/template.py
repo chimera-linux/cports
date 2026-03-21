59a561bb0576205c191ba8ccecd1c781bc371cc0
q66 <q66@chimera-linux.org>
2026-03-21 22:55:39 +0100

    main/fortify-headers: update to 3.0.1

--- main/fortify-headers/template.py
+++ main/fortify-headers/template.py
@@ -1,16 +1,17 @@
 pkgname = "fortify-headers"
-pkgver = "2.3.3"
+pkgver = "3.0.1"
 pkgrel = 0
+_commit = "fa01a693ae41beda27dbf0948bd30bc8b57d90fc"
 build_style = "makefile"
 make_check_target = "clang"
 make_check_args = ["-C", "tests", "run"]
 make_use_env = True
 pkgdesc = "Standalone fortify implementation"
 license = "0BSD"
-url = "https://git.2f30.org/fortify-headers"
-source = f"https://github.com/jvoisin/fortify-headers/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "26d81fc55b8ce3db22c7a697616392aeba928e921d975053a3f00221d1a33c08"
-tool_flags = {"CFLAGS": ["-Wno-macro-redefined"]}
+url = "https://github.com/jvoisin/fortify-headers"
+source = f"{url}/archive/{_commit}.tar.gz"
+sha256 = "3479b38ed29ca917cb61050fde60d2096b7a63cf911bdb92e31745cbf51dd48e"
+# tool_flags = {"CFLAGS": ["-Wno-macro-redefined"]}
 options = ["bootstrap"]
 
 
