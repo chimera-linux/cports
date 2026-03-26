79a9683035881428265143ea8bc336e6b3b1394d
breakgimme <adam@plock.com>
2026-03-27 00:48:36 +0100

    main/evtest: update to 1.36

--- main/evtest/template.py
+++ main/evtest/template.py
@@ -1,5 +1,5 @@
 pkgname = "evtest"
-pkgver = "1.35"
+pkgver = "1.36"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["asciidoc", "automake", "xmlto"]
@@ -8,7 +8,7 @@ pkgdesc = "Command line tool to display device input information"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.freedesktop.org/libevdev/evtest"
 source = f"{url}/-/archive/evtest-{pkgver}/evtest-evtest-{pkgver}.tar.gz"
-sha256 = "06dfe6b9760b78f3f73aca2120cbcb79339b33e59d5c79a49b4bd5d34844b054"
+sha256 = "3b9a66c92e48b0cd13b689530b5729c031bc1bcbfe9d19c258f9245e2f8d2a0f"
 # silence 10k lines of spam
 tool_flags = {"CFLAGS": ["-Wno-initializer-overrides"]}
 hardening = ["vis", "cfi"]
