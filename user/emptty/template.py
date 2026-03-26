be8b8d9dda641c5edceb0227a170a6ddf46a126a
Michal Tvrznik <emporeor@gmail.com>
2026-03-26 14:18:27 +0100

    user/emptty: update to 0.16.1

--- user/emptty/template.py
+++ user/emptty/template.py
@@ -1,15 +1,15 @@
 pkgname = "emptty"
-pkgver = "0.15.0"
-pkgrel = 1
+pkgver = "0.16.1"
+pkgrel = 0
 build_style = "go"
 make_env = {"CGO_ENABLED": "1"}
 hostmakedepends = ["go"]
-makedepends = ["dinit-chimera", "libx11-devel", "linux-pam-devel"]
+makedepends = ["dinit-chimera", "linux-pam-devel"]
 pkgdesc = "TTY display manager"
 license = "MIT"
 url = "https://github.com/tvrzna/emptty"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "fae7c04afeeb9ef3dcbb9bca67e9a2fa940e99a91872ebc0775e10253972c7f3"
+sha256 = "e85d0658fd793ad97be90af241451a9374b299d9525d2aeb57a6f83f10ad4931"
 
 
 def post_install(self):
