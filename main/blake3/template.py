e4ef910c992462898045aa2abc08d26a90d0dd00
nune <145225213+gigirassy@users.noreply.github.com>
2026-03-27 20:56:45 -0400

    main/blake3: update to 1.8.4
    Closes #5375

--- main/blake3/template.py
+++ main/blake3/template.py
@@ -1,5 +1,5 @@
 pkgname = "blake3"
-pkgver = "1.8.2"
+pkgver = "1.8.4"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DBLAKE3_USE_TBB=ON"]
@@ -14,7 +14,7 @@ pkgdesc = "BLAKE3 cryptographic hash implementation"
 license = "CC0-1.0 OR Apache-2.0"
 url = "https://github.com/BLAKE3-team/BLAKE3"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "6b51aefe515969785da02e87befafc7fdc7a065cd3458cf1141f29267749e81f"
+sha256 = "b5ee5f5c5e025eb2733ae3af8d4c0e53bb66dff35095decfd377f1083e8ac9be"
 hardening = ["vis", "cfi"]
 
 
