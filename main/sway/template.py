0fcb3965da094b9b665ebf62f848bf8e85352cdd
Ayush Agarwal <ayush@ayushnix.com>
2026-03-29 00:23:58 +0530

    main/sway: update to 1.12_rc3

--- main/sway/template.py
+++ main/sway/template.py
@@ -1,6 +1,6 @@
 pkgname = "sway"
-pkgver = "1.11"
-pkgrel = 1
+pkgver = "1.12_rc3"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "libcap-progs",
@@ -16,14 +16,14 @@ makedepends = [
     "pcre2-devel",
     "wayland-devel",
     "wayland-protocols",
-    "wlroots0.19-devel",
+    "wlroots0.20-devel",
 ]
 depends = ["xwayland"]
 pkgdesc = "Wayland compositor compatible with i3"
 license = "MIT"
 url = "https://swaywm.org"
-source = f"https://github.com/swaywm/sway/releases/download/{pkgver}/sway-{pkgver}.tar.gz"
-sha256 = "0e37a55b7c3379230e97e1ad982542b75016a0c7d6676198604e557f9b373dae"
+source = f"https://github.com/swaywm/sway/releases/download/{pkgver.replace('_', '-')}/sway-{pkgver.replace('_', '-')}.tar.gz"
+sha256 = "ab393b85eefb9d410e8740cc1339d594209b39360547e9ac61b297a7acb8f934"
 file_modes = {
     "usr/bin/sway": ("root", "root", 0o755),
 }
