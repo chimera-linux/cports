ce02bbd17d3c1b4b24d6f145a692ee7e22b7d7f1
flukey <flukey@vapourmail.eu>
2026-04-03 09:49:48 +0100

    user/yash: update to 2.61

--- user/yash/template.py
+++ user/yash/template.py
@@ -1,5 +1,5 @@
 pkgname = "yash"
-pkgver = "2.60"
+pkgver = "2.61"
 pkgrel = 0
 build_style = "configure"
 configure_args = [
@@ -21,7 +21,7 @@ pkgdesc = "Yet another shell"
 license = "GPL-2.0-only"
 url = "https://github.com/magicant/yash"
 source = f"{url}/releases/download/{pkgver}/yash-{pkgver}.tar.xz"
-sha256 = "cc152f65221916c508e3ec013edda40028d1f77a9ca3dcf2489ee825fd746f75"
+sha256 = "a214966f4ff8b293aa5521a4d3ef6e87d707579eee616aa2f8218edaa920d447"
 
 
 def post_install(self):
