3a1c06ccf605cd1762060fe04321deee69324d23
Ayush Agarwal <ayush@ayushnix.com>
2026-04-03 11:41:07 +0530

    user/gdu: update to 5.35.0

--- user/gdu/template.py
+++ user/gdu/template.py
@@ -1,5 +1,5 @@
 pkgname = "gdu"
-pkgver = "5.34.0"
+pkgver = "5.35.0"
 pkgrel = 0
 build_style = "go"
 make_build_args = [
@@ -18,7 +18,7 @@ pkgdesc = "Disk usage analyzer"
 license = "MIT"
 url = "https://github.com/dundee/gdu"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "e7ff370d682563b71c2da0ad3162ecdb17db988cb2d2b5c1708405d31e63e816"
+sha256 = "2c0e4fe412a828e1c0f414f7c230b994e44356c4753c3546c67e8178db500535"
 # check may be disabled
 options = []
 
