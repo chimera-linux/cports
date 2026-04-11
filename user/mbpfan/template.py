06c1803a4b1e0bc696177383f67e9bc7a60ec957
Marc Becher <email@marcbecher.com>
2026-04-11 09:43:48 +0200

    user/mbpfan: Applied review suggestions

--- user/mbpfan/template.py
+++ user/mbpfan/template.py
@@ -4,15 +4,14 @@ pkgrel = 0
 build_style = "makefile"
 makedepends = ["gmake", "dinit-chimera", "linux-headers"]
 pkgdesc = "Daemon to control fan speed on MacBook/MacBook Pro"
-subdesc = "The kernel modules applesmc and coretemp are required to be loaded"
 license = "GPL-3.0-or-later"
 url = "https://github.com/linux-on-mac/mbpfan"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
 sha256 = "e1cdcffaba52be215ae40a8568949190866d500d6ae2a1e96b71ab5372f3580b"
 hardening = ["vis", "cfi"]
-# didn't want to, they make no sense here either
+# tests require a MacBook to run
 options = ["!check"]
 
 
 def post_install(self):
-    self.install_service(self.files_path / "mbpfan")
+    self.install_service("^/mbpfan")
