74b82e081d9355fe9a4af0ac332be11e63db2f44
Biswapriyo Nath <nathbappai@gmail.com>
2026-04-16 19:20:39 +0000

    user/celluloid: update to 0.30
    https://github.com/celluloid-player/celluloid/releases/tag/v0.30

--- user/celluloid/template.py
+++ user/celluloid/template.py
@@ -1,5 +1,5 @@
 pkgname = "celluloid"
-pkgver = "0.29"
+pkgver = "0.30"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["glib-devel", "gettext", "meson", "pkgconf"]
@@ -15,4 +15,4 @@ pkgdesc = "GTK frontend for mpv"
 license = "GPL-3.0-or-later"
 url = "https://celluloid-player.github.io"
 source = f"https://github.com/celluloid-player/celluloid/releases/download/v{pkgver}/celluloid-{pkgver}.tar.xz"
-sha256 = "5b9991557cc2764a8281a24aa726a645287eb075cde0f0ae7c737965264a119c"
+sha256 = "7fef96431842c24e5ae78a9c42bc6523818a6c45213f23ceb146d037d6ec8559"
