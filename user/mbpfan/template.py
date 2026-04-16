56389efda8bb512c6d9df8315b392c79316897bf
Marc Becher <email@marcbecher.com>
2026-04-16 05:59:01 +0200

    user/mbpfan: new package
    Closes https://github.com/chimera-linux/cports/pull/5416

---
+++ user/mbpfan/template.py
@@ -0,0 +1,25 @@
+pkgname = "mbpfan"
+pkgver = "2.4.0"
+pkgrel = 0
+archs = ["x86_64"]
+build_style = "makefile"
+hostmakedepends = ["dos2unix"]
+makedepends = ["dinit-chimera", "linux-headers"]
+pkgdesc = "Daemon to control fan speed on MacBook/MacBook Pro"
+license = "GPL-3.0-or-later"
+url = "https://github.com/linux-on-mac/mbpfan"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "e1cdcffaba52be215ae40a8568949190866d500d6ae2a1e96b71ab5372f3580b"
+hardening = ["vis", "cfi"]
+# tests require a MacBook to run
+options = ["etcfiles", "!check"]
+
+
+def post_extract(self):
+    # some patched files have windows line endings
+    self.do("dos2unix", "src/settings.h")
+    self.do("dos2unix", "src/settings.c")
+
+
+def post_install(self):
+    self.install_service(self.files_path / "mbpfan")
