d6fd47865b2ae192e89b52aff2eed03c7ac7151b
nune <145225213+gigirassy@users.noreply.github.com>
2026-04-17 22:31:29 -0400

    user/tea: update to 0.14.0

--- user/tea/template.py
+++ user/tea/template.py
@@ -1,17 +1,18 @@
 pkgname = "tea"
-pkgver = "0.11.1"
-pkgrel = 2
+pkgver = "0.14.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     "-ldflags",
     f"-X code.gitea.io/tea/cmd.Version={pkgver}",
 ]
 hostmakedepends = ["go"]
+checkdepends = ["git"]
 pkgdesc = "CLI tool to interact with Gitea servers"
 license = "MIT"
 url = "https://gitea.com/gitea/tea"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "1da6b6d2534bd6ffb0931400014bbdef26242cf4d35d4ba44c24928811825805"
+sha256 = "f509de217ac0e57491ffdab2750516e8c505780881529ee703b9d0c86cc652a3"
 
 
 def post_install(self):
