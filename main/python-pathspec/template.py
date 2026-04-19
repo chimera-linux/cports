7beac6c97e1319dcb73dca07a748cb7fabef5edc
nune <145225213+gigirassy@users.noreply.github.com>
2026-04-19 15:46:18 -0400

    main/python-pathspec: update to 1.0.4

--- main/python-pathspec/template.py
+++ main/python-pathspec/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pathspec"
-pkgver = "0.12.1"
-pkgrel = 2
+pkgver = "1.0.4"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,6 +13,6 @@ pkgdesc = "Python utility library for pattern matching of file paths"
 license = "MPL-2.0"
 url = "https://github.com/cpburnz/python-pathspec"
 source = f"$(PYPI_SITE)/p/pathspec/pathspec-{pkgver}.tar.gz"
-sha256 = "a482d51503a1ab33b1c67a6c3813a26953dbdc71c31dacaef9a838c4e29f5712"
+sha256 = "0210e2ae8a21a9137c0d470578cb0e595af87edaa6ebf12ff176f14a02e0e645"
 # cycle
 options = ["!check"]
