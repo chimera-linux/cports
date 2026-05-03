56a34679a517542ea83b16a6eca798896fc0d8b9
q66 <q66@chimera-linux.org>
2026-05-03 23:18:00 +0200

    main/glibmm: update to 2.88.0

--- main/glibmm/template.py
+++ main/glibmm/template.py
@@ -1,5 +1,5 @@
 pkgname = "glibmm"
-pkgver = "2.86.0"
+pkgver = "2.88.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "glib-devel", "perl", "pkgconf"]
@@ -9,7 +9,7 @@ pkgdesc = "C++ bindings for GLib"
 license = "LGPL-2.1-or-later"
 url = "https://www.gtkmm.org"
 source = f"$(GNOME_SITE)/glibmm/{pkgver[:-2]}/glibmm-{pkgver}.tar.xz"
-sha256 = "39c0e9f6da046d679390774efdb9ad564436236736dc2f7825e614b2d4087826"
+sha256 = "a6549da3a6c43de83b8717dae5413c57a60d92f6ecc624615c612d0bb0ad0fe2"
 
 
 @subpackage("glibmm-devel")
