94f79e7102c23665ee89c78ee3c0926d0debbb37
q66 <q66@chimera-linux.org>
2026-05-03 23:29:45 +0200

    main/vulkan-loader: update to 1.4.350

--- main/vulkan-loader/template.py
+++ main/vulkan-loader/template.py
@@ -1,5 +1,5 @@
 pkgname = "vulkan-loader"
-pkgver = "1.4.334"
+pkgver = "1.4.350"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -26,7 +26,7 @@ url = "https://www.khronos.org/vulkan"
 source = (
     f"https://github.com/KhronosGroup/Vulkan-Loader/archive/v{pkgver}.tar.gz"
 )
-sha256 = "c2020c76f24c8f1139fe76f06d4d15bad615895de4ec32f9034cb321514824dd"
+sha256 = "fe472f15c49b1915137c065d997dbce86e31750f5bfb56c5c9a3b5b4919e44eb"
 hardening = ["vis", "!cfi"]
 # tests disabled
 options = ["!check"]
