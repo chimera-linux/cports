eaee057e2e9eb84780bfdf5494ce5c74f31d62e4
cyberia <cyberia@donk.systmes>
2026-05-04 20:37:27 +0100

    user/tinymist: update to 0.14.18

--- user/tinymist/template.py
+++ user/tinymist/template.py
@@ -1,5 +1,5 @@
 pkgname = "tinymist"
-pkgver = "0.14.10"
+pkgver = "0.14.18"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = ["--features=cli", "--bin", "tinymist", "--bin", "typlite"]
@@ -11,7 +11,7 @@ pkgdesc = "Language server for Typst"
 license = "Apache-2.0"
 url = "https://myriad-dreamin.github.io/tinymist"
 source = f"https://github.com/Myriad-Dreamin/tinymist/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "215c08d8a10ff51e15711f0684eafc85d119dc98db57f4f47ec7bf5987ea681e"
+sha256 = "92491d5bcd7ba2a5fe66c3c5e4c3728c2dce19a01a7f755010a905d31ccd0d04"
 # check: takes forever
 options = ["!check", "!cross"]
 
