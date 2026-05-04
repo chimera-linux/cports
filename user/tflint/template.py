86b0a50ec7a159c9d8b59e2ad74e5dfa85f112e7
nune <145225213+gigirassy@users.noreply.github.com>
2026-05-03 20:25:48 -0400

    user/tflint: update to 0.62.0

--- user/tflint/template.py
+++ user/tflint/template.py
@@ -1,12 +1,12 @@
 pkgname = "tflint"
-pkgver = "0.59.1"
-pkgrel = 3
+pkgver = "0.62.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Terraform Linter"
 license = "MPL-2.0"
 url = "https://github.com/terraform-linters/tflint"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "9b45910e897fd2028d748387abc781f13c57127bacde97b083aed2198c7b105d"
+sha256 = "e3736da82afb3a77037f72b9a01f2d5a60560664ed927426627ef7cfa3356fdd"
 # Tests requires network connection
 options = ["!check"]
