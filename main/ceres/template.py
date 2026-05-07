89260e746d867bfdfa719cca6501c4cdc20f0332
q66 <q66@chimera-linux.org>
2026-05-07 13:35:24 +0200

    main/ceres: don't vectorize eigen code on big endian ppc*

--- main/ceres/template.py
+++ main/ceres/template.py
@@ -16,8 +16,14 @@ license = "BSD-3-Clause AND Apache-2.0 AND MIT"
 url = "http://ceres-solver.org"
 source = f"https://github.com/ceres-solver/ceres-solver/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "12efacfadbfdc1bbfa203c236e96f4d3c210bed96994288b3ff0c8e7c6f350d4"
+tool_flags = {"CXXFLAGS": []}
 # fails some tests
 hardening = ["!int"]
+
+match self.profile().arch:
+    case "ppc64" | "ppc":
+        # vsx assumptions in altivec code
+        tool_flags["CXXFLAGS"] += ["-DEIGEN_DONT_VECTORIZE"]
 
 
 def post_install(self):
