f0f0a9be67c25325b36c32491596f490bda24228
q66 <q66@chimera-linux.org>
2026-05-11 22:44:34 +0200

    main/audiotube: format

--- main/audiotube/template.py
+++ main/audiotube/template.py
@@ -39,6 +39,9 @@ url = "https://apps.kde.org/audiotube"
 source = f"$(KDE_SITE)/release-service/{pkgver}/src/audiotube-{pkgver}.tar.xz"
 sha256 = "78952113ae9b553a64f1fde1ed532f33871046b1dd40f62688638c1f14bb2216"
 # ??? since pybind or audiotube update
-tool_flags = {"CXXFLAGS": ["-I/usr/include/python3.13"], "LDFLAGS": ["-lpython3.13"]}
+tool_flags = {
+    "CXXFLAGS": ["-I/usr/include/python3.13"],
+    "LDFLAGS": ["-lpython3.13"],
+}
 # only test needs net
 options = ["!check"]
