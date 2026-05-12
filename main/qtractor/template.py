b57825c79f7b99df8a4b6c8f96307a926beeef4b
breakgimme <adam@plock.com>
2026-05-12 15:11:37 +0200

    main/qtractor: update to 1.6.0

--- main/qtractor/template.py
+++ main/qtractor/template.py
@@ -1,5 +1,5 @@
 pkgname = "qtractor"
-pkgver = "1.5.6"
+pkgver = "1.6.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DCONFIG_WAYLAND=1"]
@@ -22,4 +22,4 @@ pkgdesc = "Audio/MIDI multi-track sequencer"
 license = "GPL-2.0-or-later"
 url = "https://www.qtractor.org"
 source = f"$(SOURCEFORGE_SITE)/qtractor/qtractor-{pkgver}.tar.gz"
-sha256 = "f490be90b3271bb173b62d11025a3f946b6ebba585e2f3789f371904dfa4b992"
+sha256 = "d22315756963456c32a7361343eb64fb2e900baf2e62021d05ff88ba31a5b794"
