2f5c67fdf5f5a40441afa80fa3dc96080b32b4ad
q66 <q66@chimera-linux.org>
2026-05-13 22:09:13 +0200

    main/firefox: update to 150.0.3

--- main/firefox/template.py
+++ main/firefox/template.py
@@ -1,5 +1,5 @@
 pkgname = "firefox"
-pkgver = "150.0.2"
+pkgver = "150.0.3"
 pkgrel = 0
 hostmakedepends = [
     "automake",
@@ -63,7 +63,7 @@ pkgdesc = "Mozilla Firefox web browser"
 license = "GPL-3.0-only AND LGPL-2.1-only AND LGPL-3.0-only AND MPL-2.0"
 url = "https://www.mozilla.org/firefox"
 source = f"$(MOZILLA_SITE)/firefox/releases/{pkgver}/source/firefox-{pkgver}.source.tar.xz"
-sha256 = "e3830b20cdf660a9cdec6e4d2326d7ced07333d9746df028cfc00c8216acbec9"
+sha256 = "0a0a539ccc96d928fec78af511884b9b89d0aec87f0d5a8852d9298b2d9332da"
 debug_level = 1  # defatten, especially with LTO
 tool_flags = {
     "LDFLAGS": ["-Wl,-rpath=/usr/lib/firefox", "-Wl,-z,stack-size=2097152"]
