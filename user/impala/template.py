e6d8ad6fbcfcb0bd45390fd7af9000c4b769ee3f
crispy-caesus <crispy@crispy-caesus.eu>
2026-05-14 14:40:37 +0200

    user/impala: update to 0.7.4

--- user/impala/template.py
+++ user/impala/template.py
@@ -1,16 +1,15 @@
 pkgname = "impala"
-pkgver = "0.4.1"
+pkgver = "0.7.4"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
 makedepends = ["rust-std"]
+depends = ["iwd"]
 pkgdesc = "TUI frontend for iwd"
 license = "GPL-3.0-only"
 url = "https://github.com/pythops/impala"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "cada25ce7f56762f76be232968b878a16dd046939a138d2bd32976b714033fda"
-# No tests are available
-options = ["!check"]
+sha256 = "948b9b46ee7b8c06016430bc0e9fef8a23ecf9768cfe11ec1f9fd48dde249bb2"
 
 
 def post_install(self):
