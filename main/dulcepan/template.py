83be33b987a1ba5b241cb90a64382099a72c54f0
breakgimme <adam@plock.com>
2026-05-14 20:45:30 +0200

    main/dulcepan: update to 1.0.3

--- main/dulcepan/template.py
+++ main/dulcepan/template.py
@@ -1,5 +1,5 @@
 pkgname = "dulcepan"
-pkgver = "1.0.2"
+pkgver = "1.0.3"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -21,5 +21,5 @@ pkgdesc = "Screenshot tool for wlroots compositors"
 license = "GPL-3.0-only"
 url = "https://codeberg.org/vyivel/dulcepan"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "9d21d8cef45ea965a9a3039af3e3f887808c30d423c5dc3159c40aedab7da870"
+sha256 = "022a57335326b89b9ccc1efb98f043c7ad50fc3dcc14e1d0a220fae8d5efdf6d"
 hardening = ["vis", "cfi"]
