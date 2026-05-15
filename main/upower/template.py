0fe69540528b65cbf076cbe4cbf7a252e41be98d
q66 <q66@chimera-linux.org>
2026-05-15 11:24:43 +0200

    main/upower: update to 1.91.2

--- main/upower/template.py
+++ main/upower/template.py
@@ -1,5 +1,5 @@
 pkgname = "upower"
-pkgver = "1.90.9"
+pkgver = "1.91.2"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -39,7 +39,7 @@ pkgdesc = "Abstraction for enumerating power devices"
 license = "GPL-2.0-or-later"
 url = "https://upower.freedesktop.org"
 source = f"https://gitlab.freedesktop.org/upower/upower/-/archive/v{pkgver}/upower-v{pkgver}.tar.gz"
-sha256 = "fffd45c50d29ed73ecdcfb11c3a7bb042ef14e84b14b0c2d5fdb78c7b4435d6c"
+sha256 = "cd9a3a3f5e0aa21eac131253228757bbdc90ecd39768f46c325754e32daa3083"
 options = ["!cross"]
 
 
