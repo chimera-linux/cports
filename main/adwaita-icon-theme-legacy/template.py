ccbbc79690b79cdf1d997fbb0f5ad7fa06fd5bf3
q66 <q66@chimera-linux.org>
2026-05-15 14:48:51 +0200

    main/adwaita-icon-theme-legacy: rename licenses path

--- main/adwaita-icon-theme-legacy/template.py
+++ main/adwaita-icon-theme-legacy/template.py
@@ -1,6 +1,6 @@
 pkgname = "adwaita-icon-theme-legacy"
 pkgver = "46.2"
-pkgrel = 2
+pkgrel = 3
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
 depends = ["hicolor-icon-theme"]
@@ -9,3 +9,9 @@ license = "LGPL-3.0-or-later OR CC-BY-SA-3.0"
 url = "https://gitlab.gnome.org/GNOME/adwaita-icon-theme-legacy"
 source = f"$(GNOME_SITE)/adwaita-icon-theme-legacy/{pkgver[:-2]}/adwaita-icon-theme-legacy-{pkgver}.tar.xz"
 sha256 = "548480f58589a54b72d18833b755b15ffbd567e3187249d74e2e1f8f99f22fb4"
+
+
+def post_install(self):
+    self.rename(
+        "usr/share/licenses/adwaita-icon-theme", "adwaita-icon-theme-legacy"
+    )
