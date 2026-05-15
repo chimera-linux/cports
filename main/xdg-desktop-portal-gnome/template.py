1edbe718379b352f7fd5a90561e8b82bf5ef8836
q66 <q66@chimera-linux.org>
2026-05-15 11:39:01 +0200

    main/xdg-desktop-portal-gnome: update to 50.0

--- main/xdg-desktop-portal-gnome/template.py
+++ main/xdg-desktop-portal-gnome/template.py
@@ -1,5 +1,5 @@
 pkgname = "xdg-desktop-portal-gnome"
-pkgver = "48.0"
+pkgver = "50.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dsystemduserunitdir=/tmp/delete_me"]
@@ -15,7 +15,7 @@ pkgdesc = "Backend implementation for xdg-desktop-portal for GNOME"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/xdg-desktop-portal-gnome"
 source = f"https://download.gnome.org/sources/xdg-desktop-portal-gnome/{pkgver.split('.')[0]}/xdg-desktop-portal-gnome-{pkgver}.tar.xz"
-sha256 = "cd15aca2e1364da308eb3796bb8ae4a577e60ca4fe12006b315232cfd19c8861"
+sha256 = "ceeef2fb68b34b3f66a3def0a332a22a70af272641fb6c50065b7a2fde3d5759"
 
 
 def post_install(self):
