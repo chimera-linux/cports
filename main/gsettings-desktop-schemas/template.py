2640f20c5b8ce10e1e7158d310a9df8637fa84b6
q66 <q66@chimera-linux.org>
2026-05-15 11:07:26 +0200

    main/gsettings-desktop-schemas: update to 50.1

--- main/gsettings-desktop-schemas/template.py
+++ main/gsettings-desktop-schemas/template.py
@@ -1,5 +1,5 @@
 pkgname = "gsettings-desktop-schemas"
-pkgver = "49.1"
+pkgver = "50.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dintrospection=true"]
@@ -20,7 +20,7 @@ pkgdesc = "Collection of GSettings schemas"
 license = "LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas"
 source = f"$(GNOME_SITE)/gsettings-desktop-schemas/{pkgver[:-2]}/gsettings-desktop-schemas-{pkgver}.tar.xz"
-sha256 = "777a7f83d5e5a8076b9bf809cb24101b1b1ba9c230235e3c3de8e13968ed0e63"
+sha256 = "0a2aa25082672585d16fcdab61c7b0e33f035fb87476505c794f29565afa485b"
 options = ["!cross"]
 
 
