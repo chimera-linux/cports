2eedecdd73a71381c01f05db8c421cae7b23c8e6
q66 <q66@chimera-linux.org>
2026-05-15 11:05:31 +0200

    main/adwaita-icon-theme: update to 50.0

--- main/adwaita-icon-theme/template.py
+++ main/adwaita-icon-theme/template.py
@@ -1,13 +1,11 @@
 pkgname = "adwaita-icon-theme"
-pkgver = "49.0"
+pkgver = "50.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
-depends = ["hicolor-icon-theme"]
+depends = ["adwaita-icon-theme-legacy"]
 pkgdesc = "Icon theme for GTK+"
 license = "LGPL-3.0-or-later OR CC-BY-SA-3.0"
 url = "https://gitlab.gnome.org/GNOME/adwaita-icon-theme"
-# missing tarball
-# source = f"$(GNOME_SITE)/adwaita-icon-theme/{pkgver[:-2]}/adwaita-icon-theme-{pkgver}.tar.xz"
-source = f"https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/archive/{pkgver}/adwaita-icon-theme-{pkgver}.tar.gz"
-sha256 = "0702ce73eb9316d48f074b183ba71d87e9f9f76a399020de70f3eea824db8b5e"
+source = f"$(GNOME_SITE)/adwaita-icon-theme/{pkgver[:-2]}/adwaita-icon-theme-{pkgver}.tar.xz"
+sha256 = "fac6e0401fca714780561a081b8f7e27c3bc1db34ebda4da175081f26b24d460"
