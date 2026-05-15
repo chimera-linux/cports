f3fc5a647d0c1a7982278089110fcf6066402253
Gnarwhal <git.aspect893@passmail.net>
2026-05-15 14:32:47 +0000

    user/python-dbus-fast: update to 5.0.16

--- user/python-dbus-fast/template.py
+++ user/python-dbus-fast/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-dbus-fast"
-pkgver = "4.0.5"
+pkgver = "5.0.16"
 pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
@@ -39,7 +39,7 @@ pkgdesc = "DBus library for python"
 license = "MIT"
 url = "https://pypi.org/project/dbus-fast"
 source = f"https://github.com/Bluetooth-Devices/dbus-fast/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0de71ec2f24d04fdcf22f82bda19deea02e249b179f4d30c589c0e8f509500db"
+sha256 = "813a8a9ac6418e6c1661cfa2a21d151fe2434f4838b8a8fb12150affd68d5cba"
 # tests fail since pygobject update
 options = ["!check"]
 
