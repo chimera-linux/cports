6058c1e204b7d4f2b06b2f14a1c3ba648df77e87
breakgimme <adam@plock.com>
2026-05-16 14:36:37 +0200

    user/f2: update to 2.2.2

--- user/f2/template.py
+++ user/f2/template.py
@@ -1,6 +1,6 @@
 pkgname = "f2"
-pkgver = "2.2.1"
-pkgrel = 4
+pkgver = "2.2.2"
+pkgrel = 0
 build_style = "go"
 make_build_args = ["./cmd/f2"]
 hostmakedepends = ["go"]
@@ -9,7 +9,7 @@ pkgdesc = "Command-line tool for batch renaming files and directories"
 license = "MIT"
 url = "https://github.com/ayoisaiah/f2"
 source = f"https://github.com/ayoisaiah/f2/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "69e60baeb8e15644088713d7b2fb1e7d23131a92ef5fa61ed4c2c18160078ff1"
+sha256 = "0785e40b1fd2adb55165f668dc2635d47559fd7534b0f1da33849f155c4e539b"
 
 
 def post_install(self):
