39e1dacd9d214142ebd6626715fb055fd58882bd
breakgimme <adam@plock.com>
2026-05-16 14:46:14 +0200

    user/scrcpy: update to 4.0

--- user/scrcpy/template.py
+++ user/scrcpy/template.py
@@ -1,6 +1,6 @@
 pkgname = "scrcpy"
-pkgver = "3.3.3"
-pkgrel = 1
+pkgver = "4.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Dcompile_server=false",
@@ -15,7 +15,7 @@ hostmakedepends = [
 makedepends = [
     "ffmpeg-devel",
     "libusb-devel",
-    "sdl2-compat-devel",
+    "sdl3-devel",
 ]
 depends = ["android-tools"]
 pkgdesc = "Display and control an Android device"
@@ -26,8 +26,8 @@ source = [
     f"!{url}/releases/download/v{pkgver}/scrcpy-server-v{pkgver}",
 ]
 sha256 = [
-    "87fcd360a6bb6ca070ffd217bd33b33fb808b0a1572b464da51dde3fd3f6f60e",
-    "7e70323ba7f259649dd4acce97ac4fefbae8102b2c6d91e2e7be613fd5354be0",
+    "a62bc2639e1d56b3e7ebaa20d8deb4947dd02954b3362bdebe2ef9f7eae41b00",
+    "84924bd564a1eb6089c872c7521f968058977f91f5ff02514a8c74aff3210f3a",
 ]
 
 
