c7b69e967385410f54c425dafe907e8a13f6eef8
breakgimme <adam@plock.com>
2026-05-16 14:39:55 +0200

    user/cloc: update to 2.08

--- user/cloc/template.py
+++ user/cloc/template.py
@@ -1,5 +1,5 @@
 pkgname = "cloc"
-pkgver = "2.06"
+pkgver = "2.08"
 pkgrel = 0
 build_style = "makefile"
 make_dir = "Unix"
@@ -21,4 +21,4 @@ pkgdesc = "Count lines of source code"
 license = "GPL-2.0-or-later"
 url = "https://github.com/AlDanial/cloc"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "10d78427fda66aaa10ec733adb03d910c49376fe9068aacebb17aa657a7a3a05"
+sha256 = "8099b6275c124f662690f2db3581cd2ad4e9ad4e08332288719838ded00d1da5"
