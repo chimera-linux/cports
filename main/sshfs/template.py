4430d77c747c107163283b2537c347548c16a7c6
breakgimme <adam@plock.com>
2026-05-16 14:53:07 +0200

    main/sshfs: update to 3.7.5

--- main/sshfs/template.py
+++ main/sshfs/template.py
@@ -1,6 +1,6 @@
 pkgname = "sshfs"
-pkgver = "3.7.3"
-pkgrel = 1
+pkgver = "3.7.5"
+pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "meson",
@@ -12,12 +12,13 @@ hostmakedepends = [
 makedepends = [
     "fuse-devel",
     "glib-devel",
+    "linux-headers",
 ]
 pkgdesc = "FUSE client for SSH"
 license = "GPL-2.0-or-later"
 url = "https://github.com/libfuse/sshfs"
 source = f"{url}/releases/download/sshfs-{pkgver}/sshfs-{pkgver}.tar.xz"
-sha256 = "5218ce7bdd2ce0a34137a0d7798e0f6d09f0e6d21b1e98ee730a18b0699c2e99"
+sha256 = "0e45db63c2d00919db3174134fa234c6e0682d6fe573c46312d1d53d1d61a8bb"
 # CFI: shitty struct buffer -> struct readdir_handle cast
 hardening = ["vis", "!cfi"]
 # requires fuse kernel module
