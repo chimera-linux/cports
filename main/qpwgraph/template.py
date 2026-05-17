c7d767bbf6652d18100ae246125cb16e2af75d14
breakgimme <adam@plock.com>
2026-05-17 19:32:49 +0200

    main/qpwgraph: update to 1.0.1

--- main/qpwgraph/template.py
+++ main/qpwgraph/template.py
@@ -1,5 +1,5 @@
 pkgname = "qpwgraph"
-pkgver = "0.9.6"
+pkgver = "1.0.1"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -17,4 +17,4 @@ pkgdesc = "Pipewire graph manager"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.freedesktop.org/rncbc/qpwgraph"
 source = f"{url}/-/archive/v{pkgver}/qpwgraph-v{pkgver}.tar.gz"
-sha256 = "14faac587e58043f52fb0de20328e7439e3a7ffee898c54109e0557dfe018a12"
+sha256 = "3640c12833e32917054578b249caac73beefdef3113c834175f20bf582285925"
