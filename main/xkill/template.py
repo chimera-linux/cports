e4db09f9e552251469fef425a2573832dca52226
breakgimme <adam@plock.com>
2026-05-18 13:38:29 +0200

    main/xkill: update to 1.0.7

--- main/xkill/template.py
+++ main/xkill/template.py
@@ -1,6 +1,6 @@
 pkgname = "xkill"
-pkgver = "1.0.6"
-pkgrel = 1
+pkgver = "1.0.7"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "pkgconf", "xorg-util-macros"]
 makedepends = ["libx11-devel", "libxmu-devel"]
@@ -8,7 +8,7 @@ pkgdesc = "Kill an X client by its X resource"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/xkill-{pkgver}.tar.gz"
-sha256 = "3b35a2f4b67dda1e98b6541488cd7f7343eb6e3dbe613aeff3d5a5a4c4c64b58"
+sha256 = "aec5674d48d1749742e87dfbff30f1abacabe5e2a10c0679c3996882444c9f6d"
 hardening = ["vis", "cfi"]
 
 
