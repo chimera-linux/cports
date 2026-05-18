cc5b19f39500f53f9b8604948139eb1e825752c5
breakgimme <adam@plock.com>
2026-05-18 13:46:23 +0200

    main/wl-mirror: update to 0.18.5

--- main/wl-mirror/template.py
+++ main/wl-mirror/template.py
@@ -1,5 +1,5 @@
 pkgname = "wl-mirror"
-pkgver = "0.18.3"
+pkgver = "0.18.5"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -12,4 +12,4 @@ pkgdesc = "Wayland output mirror client"
 license = "GPL-3.0-or-later"
 url = "https://github.com/Ferdi265/wl-mirror"
 source = f"{url}/releases/download/v{pkgver}/wl-mirror-{pkgver}.tar.gz"
-sha256 = "52c15744ed2aee1aadb690bce91af49312cae0ed2f514d23bfbfaf0548dbac44"
+sha256 = "2b3e70374a229cdf49b421c75ce8974f1f666ba8c6a546b0cf88f550f118ba60"
