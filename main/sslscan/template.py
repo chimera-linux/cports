e611b0b5c142725884e18d8686ce435d46bea58b
breakgimme <adam@plock.com>
2026-05-18 13:13:17 +0200

    main/sslscan: update to 2.2.2

--- main/sslscan/template.py
+++ main/sslscan/template.py
@@ -1,5 +1,5 @@
 pkgname = "sslscan"
-pkgver = "2.2.0"
+pkgver = "2.2.2"
 pkgrel = 0
 build_style = "makefile"
 make_build_args = [f"GIT_VERSION={pkgver}"]
@@ -8,7 +8,7 @@ pkgdesc = "List supported ciphers in TLS servers"
 license = "GPL-3.0-or-later WITH custom:OpenSSL-exception"
 url = "https://github.com/rbsec/sslscan"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "17c6fe4a7822e1949bc8975feea59fcf042c4a46d62d9f5acffe59afc466cc1c"
+sha256 = "f6095c01163eef04ff9b3540913f20d899f54e27b1194afd409c5fc807eacb46"
 hardening = ["vis", "cfi"]
 # no tests
 options = ["!check"]
