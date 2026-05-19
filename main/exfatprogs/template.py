52e432db516355c45f25dc15c4ee218f83f1940d
breakgimme <adam@plock.com>
2026-05-19 16:52:35 +0200

    main/exfatprogs: update to 1.3.2

--- main/exfatprogs/template.py
+++ main/exfatprogs/template.py
@@ -1,5 +1,5 @@
 pkgname = "exfatprogs"
-pkgver = "1.2.9"
+pkgver = "1.3.2"
 pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
@@ -12,4 +12,4 @@ pkgdesc = "ExFAT filesystem utilities"
 license = "GPL-2.0-only"
 url = "https://github.com/exfatprogs/exfatprogs"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "a9d2722a4190a6d4080df6a447498886cc566ae22bb721e3405bb82f423d51d1"
+sha256 = "4b3e198a2b879da52254f9d4a68accff8001b7b4e5c1860d47ad232e03a2a2d0"
