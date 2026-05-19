8855e87a9bec9e63d4abd8ee0840e7c509bfc819
breakgimme <adam@plock.com>
2026-05-19 16:42:11 +0200

    main/cage: update to 0.3.0

--- main/cage/template.py
+++ main/cage/template.py
@@ -1,14 +1,14 @@
 pkgname = "cage"
-pkgver = "0.2.1"
+pkgver = "0.3.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "scdoc", "wayland-progs"]
-makedepends = ["wayland-protocols", "wlroots0.19-devel"]
+makedepends = ["wayland-protocols", "wlroots0.20-devel"]
 pkgdesc = "Kiosk compositor for Wayland"
 license = "MIT"
 url = "https://www.hjdskes.nl/projects/cage"
 source = f"https://github.com/cage-kiosk/cage/releases/download/v{pkgver}/cage-{pkgver}.tar.gz"
-sha256 = "fc1238e3aa5b82787a95d49cb3e1bac0671e4d3a40090087848f43f3e1f63a98"
+sha256 = "cd2510f83bef3e08e660d99492ca7761b218ecb53ee01cdbbeee3d6aabc7734e"
 hardening = ["vis", "cfi"]
 
 
