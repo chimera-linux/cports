c1b0865163f477c14a98a7492471658b702a9e4a
breakgimme <adam@plock.com>
2026-05-19 17:01:06 +0200

    main/colordiff: update to 1.0.22

--- main/colordiff/template.py
+++ main/colordiff/template.py
@@ -1,6 +1,6 @@
 pkgname = "colordiff"
-pkgver = "1.0.21"
-pkgrel = 1
+pkgver = "1.0.22"
+pkgrel = 0
 build_style = "makefile"
 make_install_args = ["INSTALL_DIR=/usr/bin", "MAN_DIR=/usr/share/man/man1"]
 hostmakedepends = ["xmlto", "lynx", "perl"]
@@ -9,7 +9,7 @@ pkgdesc = "Tool to colorize diff output"
 license = "GPL-2.0-or-later"
 url = "https://www.colordiff.org"
 source = f"https://www.colordiff.org/colordiff-{pkgver}.tar.gz"
-sha256 = "9b30f4257ef0f0806dea5a27c9ad8edc3f7999f05ddaff6f0627064dc927e615"
+sha256 = "f96f73c54521c53f14dc164d5a3920c9ca21a0e5f8e9613f43812a98af3e22af"
 # no testsuite
 options = ["!check"]
 
