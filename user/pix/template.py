9a7f72d7c91fc5c03353d592b58c8d86163d6408
breakgimme <adam@plock.com>
2026-05-20 14:11:19 +0200

    user/pix: update to 3.4.10

--- user/pix/template.py
+++ user/pix/template.py
@@ -1,5 +1,5 @@
 pkgname = "pix"
-pkgver = "3.4.7"
+pkgver = "3.4.10"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dlibbrasero=false", "-Dwebservices=false"]
@@ -23,12 +23,12 @@ makedepends = [
     "xapp-devel",
     "zlib-ng-compat-devel",
 ]
-depends = ["xapp-progs"]
+depends = ["xapp-progs", "xapp-symbolic-icons"]
 pkgdesc = "Image management application"
 license = "GPL-2.0-or-later"
 url = "https://projects.linuxmint.com/xapps"
 source = f"https://github.com/linuxmint/pix/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "6bb240ebc8b058aa3e165d6e128dbced3248e561860ab50b7f958a4b20c5ec29"
+sha256 = "d765e779ee6e7d8220fe556c010c181ae69be059d53192d628f5bec5fdc9082b"
 
 
 @subpackage("pix-devel")
