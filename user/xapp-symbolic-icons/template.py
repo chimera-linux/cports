a78714952659ffe1191c48f641d0e6317b0b2b76
breakgimme <adam@plock.com>
2026-05-20 14:11:16 +0200

    user/xapp-symbolic-icons: new package

---
+++ user/xapp-symbolic-icons/template.py
@@ -0,0 +1,17 @@
+pkgname = "xapp-symbolic-icons"
+pkgver = "1.1.0"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = ["meson", "pkgconf"]
+pkgdesc = "Set of symbolic icons for Gtk applications and projects"
+license = "LGPL-3.0-or-later"
+url = "https://github.com/xapp-project/xapp-symbolic-icons"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "fa96ac6a1a85c0cb16dccab5c03ae1e651a3482cde4a93f792956977bc19108d"
+
+
+@subpackage("xapp-symbolic-icons-progs")
+def _(self):
+    self.depends = [self.parent, "python"]
+
+    return ["usr/bin"]
