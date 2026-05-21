a69f153a1539769c3b38c6a190770e7e4757883b
Wesley Moore <wes@wezm.net>
2026-05-21 19:02:00 +1000

    user/flash-watcher: update to 0.2.0

--- user/flash-watcher/template.py
+++ user/flash-watcher/template.py
@@ -1,5 +1,5 @@
 pkgname = "flash-watcher"
-pkgver = "0.1.2"
+pkgver = "0.2.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -8,7 +8,7 @@ pkgdesc = "File watcher that executes commands when files change"
 license = "MIT"
 url = "https://github.com/sage-scm/Flash"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "dd9604dabbf0c864e59ad41ce24b91293a436b76ecb9a1c18a8cc53878c7753c"
+sha256 = "3ee5e18457f29919c01aad879ce964348f5ee0b549800dedf8eb20009313a235"
 
 
 def post_install(self):
