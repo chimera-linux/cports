281556638606e517a95854b3fce44f5e940d443d
Wesley Moore <wes@wezm.net>
2026-05-21 20:39:40 +1000

    main/lm-sensors: update to 3.6.2

--- main/lm-sensors/template.py
+++ main/lm-sensors/template.py
@@ -1,6 +1,6 @@
 pkgname = "lm-sensors"
-pkgver = "3.6.0"
-pkgrel = 2
+pkgver = "3.6.2"
+pkgrel = 0
 build_style = "makefile"
 make_build_args = [f"MACHINE={self.profile().arch}"]
 make_install_args = ["SBINDIR=/usr/bin", "MANDIR=/usr/share/man"]
@@ -10,7 +10,7 @@ pkgdesc = "Sensor reading utilities"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://github.com/lm-sensors/lm-sensors"
 source = f"{url}/archive/V{pkgver.replace('.', '-')}.tar.gz"
-sha256 = "0591f9fa0339f0d15e75326d0365871c2d4e2ed8aa1ff759b3a55d3734b7d197"
+sha256 = "c6a0587e565778a40d88891928bf8943f27d353f382d5b745a997d635978a8f0"
 # no test suite
 options = ["!check"]
 
