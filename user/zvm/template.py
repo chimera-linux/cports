a7ecfcb9439124edfa9d1ac83808ca835350100e
breakgimme <adam@plock.com>
2026-05-21 15:56:28 +0200

    user/zvm: update to 0.8.20

--- user/zvm/template.py
+++ user/zvm/template.py
@@ -1,14 +1,25 @@
 pkgname = "zvm"
-pkgver = "0.8.8"
-pkgrel = 6
+pkgver = "0.8.20"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Zig version manager"
 license = "MIT"
 url = "https://github.com/tristanisham/zvm"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "aafeb40122acd549817f106d072a96910a5440feec41d0af9f99474d3e498a09"
+sha256 = "dfeadf7a54bb14e2cc1819dc1e3b533e34dbe01b0747be55ed0c8615fa767616"
+# generates completions with host binary
+options = ["!cross"]
+
+
+# add fish completions after https://github.com/tristanisham/zvm/issues/161 gets fixed
+def post_build(self):
+    for shell in ["bash", "zsh"]:
+        with open(self.cwd / f"zvm.{shell}", "w") as f:
+            self.do(f"{self.make_dir}/zvm", "completion", shell, stdout=f)
 
 
 def post_install(self):
     self.install_license("LICENSE")
+    for shell in ["bash", "zsh"]:
+        self.install_completion(f"zvm.{shell}", shell)
