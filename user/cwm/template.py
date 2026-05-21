31313a325f7b5f8d0e94014ec905477c15f5fa41
breakgimme <adam@plock.com>
2026-05-21 15:32:13 +0200

    user/cwm: update to 7.9

--- user/cwm/template.py
+++ user/cwm/template.py
@@ -1,6 +1,6 @@
 pkgname = "cwm"
-pkgver = "7.4"
-pkgrel = 1
+pkgver = "7.9"
+pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["byacc", "pkgconf"]
 makedepends = [
@@ -11,7 +11,7 @@ pkgdesc = "Lightweight and efficient window manager for X11"
 license = "ISC"
 url = "https://github.com/leahneukirchen/cwm"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "b4f275143c8c716d7df1cfbb230f888c72aa861708e144d1749858f1cc6fcac0"
+sha256 = "ec75056a5f57980d9f57076c4ae7d25a1e6903ea4e793c0a04b98e25edb0fc90"
 hardening = ["vis", "cfi"]
 # no tests
 options = ["!check"]
