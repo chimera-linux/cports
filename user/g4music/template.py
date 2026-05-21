a7a6e12e1e88e7a38c3a216a63d26bbe1e09eb65
breakgimme <adam@plock.com>
2026-05-21 15:27:00 +0200

    user/g4music: update to 4.6

--- user/g4music/template.py
+++ user/g4music/template.py
@@ -1,5 +1,5 @@
 pkgname = "g4music"
-pkgver = "4.5"
+pkgver = "4.6"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -20,4 +20,4 @@ pkgdesc = "GTK4 music player with a fluent adaptive user interface"
 license = "GPL-3.0-or-later"
 url = "https://gitlab.gnome.org/neithern/g4music"
 source = f"{url}/-/archive/v{pkgver}/g4music-v{pkgver}.tar.gz"
-sha256 = "0953a2c8d3adddbb8538b853b9d926f761a7ecd9eae1660ad0ed08545b2110ff"
+sha256 = "7c20f17011b38b72748f58d81493620a441dbfbde5f4c0576a36586ebd81dc17"
