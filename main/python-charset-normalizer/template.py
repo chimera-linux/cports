b7c07a7775d06d0c10b83552cd34d0d840e0205b
q66 <q66@chimera-linux.org>
2026-05-28 14:09:32 +0200

    main/python-charset-normalizer: update to 3.4.7

--- main/python-charset-normalizer/template.py
+++ main/python-charset-normalizer/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-charset-normalizer"
-pkgver = "3.4.3"
-pkgrel = 1
+pkgver = "3.4.7"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,7 +13,7 @@ pkgdesc = "Encoding and language detection"
 license = "MIT"
 url = "https://charset-normalizer.readthedocs.io"
 source = f"https://github.com/Ousret/charset_normalizer/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "a8e48df4c97e52bc516e27edb301cd837684bb7266ce44a3532a9eed429c9e1c"
+sha256 = "7cf0421a9ac9f5b0894764a6756053e01f1f68641ed6377dfb21c776ca7031ca"
 # dependency of pytest
 options = ["!check"]
 
