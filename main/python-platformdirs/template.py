bcb658a78c35fcc6926b0cb1e5ecefabf2c9582c
q66 <q66@chimera-linux.org>
2026-05-28 13:58:16 +0200

    main/python-platformdirs: update to 4.10.0

--- main/python-platformdirs/template.py
+++ main/python-platformdirs/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-platformdirs"
-pkgver = "4.3.8"
-pkgrel = 2
+pkgver = "4.10.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = ["python-build", "python-installer", "python-hatch_vcs"]
 checkdepends = ["python-pytest", "python-appdirs", "python-pytest-mock"]
@@ -9,7 +9,7 @@ pkgdesc = "Platform-specific system directory library"
 license = "MIT"
 url = "https://platformdirs.readthedocs.io"
 source = f"$(PYPI_SITE)/p/platformdirs/platformdirs-{pkgver}.tar.gz"
-sha256 = "3d512d96e16bcb959a814c9f348431070822a6496326a4be0911c40b5a74c2bc"
+sha256 = "31e761a6a0ca04faf7353ea759bdba55652be214725111e5aac52dfa29d4bef7"
 # unpackaged checkdepends
 options = ["!check"]
 
