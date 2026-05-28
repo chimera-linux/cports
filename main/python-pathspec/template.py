fc4017649ebef7cc5a06f2bb51123078a0e621f4
q66 <q66@chimera-linux.org>
2026-05-28 13:50:39 +0200

    main/python-pathspec: update to 1.1.1

--- main/python-pathspec/template.py
+++ main/python-pathspec/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pathspec"
-pkgver = "0.12.1"
-pkgrel = 3
+pkgver = "1.1.1"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,6 +13,6 @@ pkgdesc = "Python utility library for pattern matching of file paths"
 license = "MPL-2.0"
 url = "https://github.com/cpburnz/python-pathspec"
 source = f"$(PYPI_SITE)/p/pathspec/pathspec-{pkgver}.tar.gz"
-sha256 = "a482d51503a1ab33b1c67a6c3813a26953dbdc71c31dacaef9a838c4e29f5712"
+sha256 = "17db5ecd524104a120e173814c90367a96a98d07c45b2e10c2f3919fff91bf5a"
 # cycle
 options = ["!check"]
