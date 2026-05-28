0893c3a052b989615a5f7ad9444e2aa840c1b08f
q66 <q66@chimera-linux.org>
2026-05-28 13:49:50 +0200

    main/python-click: update to 8.4.1

--- main/python-click/template.py
+++ main/python-click/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-click"
-pkgver = "8.2.2"
-pkgrel = 1
+pkgver = "8.4.1"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -13,7 +13,7 @@ pkgdesc = "Python module for command line interfaces"
 license = "BSD-3-Clause"
 url = "https://palletsprojects.com/p/click"
 source = f"$(PYPI_SITE)/c/click/click-{pkgver}.tar.gz"
-sha256 = "068616e6ef9705a07b6db727cb9c248f4eb9dae437a30239f56fa94b18b852ef"
+sha256 = "918b5633eddf6b41c32d4f454bf0de810065c74e3f7dbf8ee5452f8be88d3e96"
 
 
 def post_install(self):
