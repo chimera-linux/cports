ab8b145116a054b507a3e9cf06f83c4a60b134a7
q66 <q66@chimera-linux.org>
2026-05-28 14:15:44 +0200

    main/python-requests: update to 2.34.2

--- main/python-requests/template.py
+++ main/python-requests/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-requests"
-pkgver = "2.32.4"
-pkgrel = 1
+pkgver = "2.34.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -19,6 +19,6 @@ pkgdesc = "Python HTTP library for human beings"
 license = "Apache-2.0"
 url = "https://requests.readthedocs.io"
 source = f"$(PYPI_SITE)/r/requests/requests-{pkgver}.tar.gz"
-sha256 = "27d0316682c8a29834d3264820024b62a36942083d52caf2f14c0591336d3422"
+sha256 = "f288924cae4e29463698d6d60bc6a4da69c89185ad1e0bcc4104f584e960b9ed"
 # needs pytest, is a dependency of pytest
 options = ["!check"]
