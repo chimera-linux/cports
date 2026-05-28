7a73c7868aeec51b244989da0cf365c29396a899
q66 <q66@chimera-linux.org>
2026-05-28 14:02:05 +0200

    user/python-cachetools: update to 7.1.4

--- user/python-cachetools/template.py
+++ user/python-cachetools/template.py
@@ -1,18 +1,19 @@
 pkgname = "python-cachetools"
-pkgver = "6.2.1"
-pkgrel = 1
+pkgver = "7.1.4"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-installer",
     "python-setuptools",
+    "python-setuptools_scm",
 ]
 checkdepends = ["python-pytest"]
 pkgdesc = "Extensible memoizing collections and decorators"
 license = "MIT"
 url = "https://github.com/tkem/cachetools"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "2d5ddb99ac024a693d729bbbcf86a6a811686813fe1ed9c3f83f6385beda063e"
+sha256 = "9e7593c977cc71dc0a9bfcabce97c03ef8fdef970244f0367331920b3af931f2"
 
 
 def post_install(self):
