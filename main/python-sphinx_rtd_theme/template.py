9cad1b6b37a1cf57577fde4019387a4454e65015
q66 <q66@chimera-linux.org>
2026-05-28 13:48:46 +0200

    main/python-sphinx_rtd_theme: update to 3.1.0

--- main/python-sphinx_rtd_theme/template.py
+++ main/python-sphinx_rtd_theme/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-sphinx_rtd_theme"
-pkgver = "3.0.2"
-pkgrel = 2
+pkgver = "3.1.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -12,7 +12,7 @@ pkgdesc = "Sphinx theme for readthedocs.org"
 license = "MIT"
 url = "https://github.com/readthedocs/sphinx_rtd_theme"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "6d8e0435e525afe04cb3a10e9429cff37febef612a376576b69b63178f289aba"
+sha256 = "f1755f1069616adacf1e00c0814e6b16192b7930ba4a3522993c3e096001b9b5"
 # No tests available
 options = ["!check"]
 
