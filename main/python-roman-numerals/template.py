3ba2047548adaf75e569855206e0d431dbf763d6
q66 <q66@chimera-linux.org>
2026-05-28 13:46:28 +0200

    main/python-roman-numerals: rename and update to 4.1.0

--- main/python-roman-numerals/template.py
+++ main/python-roman-numerals/template.py
@@ -1,19 +1,21 @@
-pkgname = "python-roman-numerals-py"
-pkgver = "3.1.0"
-pkgrel = 1
+pkgname = "python-roman-numerals"
+pkgver = "4.1.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
     "python-flit_core",
     "python-installer",
+    "python-setuptools",
 ]
 depends = ["python"]
 checkdepends = ["python-pytest"]
+renames = ["python-roman-numerals-py"]
 pkgdesc = "Manipulate Roman numerals"
 license = "0BSD OR CC0-1.0"
 url = "https://github.com/AA-Turner/roman-numerals"
-source = f"$(PYPI_SITE)/r/roman-numerals-py/roman_numerals_py-{pkgver}.tar.gz"
-sha256 = "be4bf804f083a4ce001b5eb7e3c0862479d10f94c936f6c4e5f250aa5ff5bd2d"
+source = f"$(PYPI_SITE)/r/roman-numerals/roman_numerals-{pkgver}.tar.gz"
+sha256 = "1af8b147eb1405d5839e78aeb93131690495fe9da5c91856cb33ad55a7f1e5b2"
 # cyclic
 options = ["!check"]
 
