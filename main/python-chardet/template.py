4341eb534bca29045d466012e0031f47aae9eba7
q66 <q66@chimera-linux.org>
2026-05-28 14:09:00 +0200

    main/python-chardet: update to 6.0.0

--- main/python-chardet/template.py
+++ main/python-chardet/template.py
@@ -1,9 +1,10 @@
 pkgname = "python-chardet"
-pkgver = "5.2.0"
-pkgrel = 3
+pkgver = "6.0.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
+    "python-hatch_vcs",
     "python-installer",
     "python-setuptools",
     "python-wheel",
@@ -14,6 +15,6 @@ pkgdesc = "Universal encoding detector for Python"
 license = "LGPL-2.1-only"
 url = "https://github.com/chardet/chardet"
 source = f"$(PYPI_SITE)/c/chardet/chardet-{pkgver}.tar.gz"
-sha256 = "1b3b6ff479a8c414bc3fa2c0852995695c4a026dcd6d0633b2dd092ca39c1cf7"
+sha256 = "aaa00ede13dd39a582de2b1254221a1f3e1c77e7738036431b6cb7e6a05b4f19"
 # dependency of pytest
 options = ["!check"]
