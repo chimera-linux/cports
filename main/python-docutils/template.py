4d3b692e8cfc6febf5d417faead2a1e42eaf70b0
q66 <q66@chimera-linux.org>
2026-05-28 13:42:56 +0200

    main/python-docutils: update to 0.23

--- main/python-docutils/template.py
+++ main/python-docutils/template.py
@@ -1,7 +1,7 @@
 # test if sphinx doesn't break on updates (e.g. qemu build)
 pkgname = "python-docutils"
-pkgver = "0.22.2"
-pkgrel = 1
+pkgver = "0.23"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -14,7 +14,7 @@ pkgdesc = "Python documentation utilities"
 license = "custom:none AND BSD-2-Clause AND GPL-3.0-or-later AND Python-2.0"
 url = "http://docutils.sourceforge.net"
 source = f"$(PYPI_SITE)/d/docutils/docutils-{pkgver}.tar.gz"
-sha256 = "9fdb771707c8784c8f2728b67cb2c691305933d68137ef95a75db5f4dfbc213d"
+sha256 = "746f5060322511280a1e50eb76846ed6bf2342984b2ac04dc42caa1a8d78799e"
 
 
 def check(self):
