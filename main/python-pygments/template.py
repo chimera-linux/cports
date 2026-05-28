a38c29b51c374fadade842077a066e33e94f0f0e
q66 <q66@chimera-linux.org>
2026-05-28 13:45:52 +0200

    main/python-pygments: update to 2.20.0

--- main/python-pygments/template.py
+++ main/python-pygments/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-pygments"
-pkgver = "2.19.2"
-pkgrel = 1
+pkgver = "2.20.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -12,7 +12,7 @@ pkgdesc = "Generic syntax highlighter written in Python"
 license = "BSD-2-Clause"
 url = "https://pygments.org"
 source = f"$(PYPI_SITE)/p/pygments/pygments-{pkgver}.tar.gz"
-sha256 = "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
+sha256 = "6757cd03768053ff99f3039c1a36d6c0aa0b263438fcab17520b30a303a82b5f"
 # dependency of pytest
 options = ["!check"]
 
