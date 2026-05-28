4482e4aebf84f29d075ab16904c0a3c384a6067a
q66 <q66@chimera-linux.org>
2026-05-28 14:10:33 +0200

    main/python-idna: update to 3.16

--- main/python-idna/template.py
+++ main/python-idna/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-idna"
-pkgver = "3.10"
-pkgrel = 2
+pkgver = "3.16"
+pkgrel = 0
 build_style = "python_pep517"
 make_check_target = "tests"
 hostmakedepends = [
@@ -14,7 +14,7 @@ pkgdesc = "Internationalized Domain Names in Applications (IDNA) for Python"
 license = "BSD-3-Clause"
 url = "https://github.com/kjd/idna"
 source = f"$(PYPI_SITE)/i/idna/idna-{pkgver}.tar.gz"
-sha256 = "12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9"
+sha256 = "d7a6da03db833450fca25d2358ac9ff06cd624577a4aea3a596d5c0f77b8e03d"
 # dep cycle with pytest
 options = ["!check"]
 
