ec89fa58f1d772e2d2f453b9ea9648e9e4e86d18
q66 <q66@chimera-linux.org>
2026-06-01 13:44:10 +0200

    main/geoclue: switch libexec dir

--- main/geoclue/template.py
+++ main/geoclue/template.py
@@ -1,9 +1,8 @@
 pkgname = "geoclue"
 pkgver = "2.8.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
-    "--libexecdir=/usr/libexec",  # TODO switch libexec
     "-Ddbus-srv-user=_geoclue",
     "-Dgtk-doc=false",
     "-Dintrospection=true",
