43f289098e6a0f0948eb90b790f395235b3a50fc
q66 <q66@chimera-linux.org>
2026-06-03 16:54:27 +0200

    main/libcamera: switch libexec path

--- main/libcamera/template.py
+++ main/libcamera/template.py
@@ -1,9 +1,8 @@
 pkgname = "libcamera"
 pkgver = "0.5.2"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
-    "--libexecdir=/usr/libexec",  # TODO switch libexec
     "-Dtest=true",
 ]
 hostmakedepends = [
