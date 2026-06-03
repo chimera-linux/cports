c2aa522839c17c9bb582293c4c8bf6024a171dd2
q66 <q66@chimera-linux.org>
2026-06-03 22:00:53 +0200

    main/bats: update to 1.13.0

--- main/bats/template.py
+++ main/bats/template.py
@@ -1,5 +1,5 @@
 pkgname = "bats"
-pkgver = "1.12.0"
+pkgver = "1.13.0"
 pkgrel = 0
 hostmakedepends = ["bash"]
 checkdepends = ["bash", "procps"]
@@ -8,7 +8,17 @@ pkgdesc = "Bash Automated Testing System"
 license = "MIT"
 url = "https://github.com/bats-core/bats-core"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "e36b020436228262731e3319ed013d84fcd7c4bd97a1b34dee33d170e9ae6bab"
+sha256 = "a85e12b8828271a152b338ca8109aa23493b57950987c8e6dff97ba492772ff3"
+
+
+def post_extract(self):
+    for f in (self.cwd / "libexec/bats-core").iterdir():
+        self.mv(f, "lib/bats-core")
+    # for tests
+    self.rm("libexec", recursive=True)
+    self.ln_s("lib", "libexec")
+    # cba to patch
+    self.rm("test/install.bats")
 
 
 def check(self):
