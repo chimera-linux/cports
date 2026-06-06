a1e42e4f4cd54e4d7dde31162e145cd536da9d81
q66 <q66@chimera-linux.org>
2026-06-06 23:55:57 +0200

    main/gnome-settings-daemon: switch libexec path

--- main/gnome-settings-daemon/template.py
+++ main/gnome-settings-daemon/template.py
@@ -1,9 +1,8 @@
 pkgname = "gnome-settings-daemon"
 pkgver = "50.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
-    "--libexecdir=/usr/libexec",  # TODO switch libexec
     "-Delogind=true",
     "-Dsystemd=false",
     "-Dsystemd-units=false",
