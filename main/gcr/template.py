d6a3c239b5d871e6e61bbea6fa07be0330f31f85
q66 <q66@chimera-linux.org>
2026-06-06 23:55:13 +0200

    main/gcr: switch libexec path

--- main/gcr/template.py
+++ main/gcr/template.py
@@ -1,9 +1,8 @@
 pkgname = "gcr"
 pkgver = "4.4.0.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
-    "--libexecdir=/usr/libexec",  # TODO switch libexec
     "-Dgpg_path=/usr/bin/gpg",
     "-Dsystemd=disabled",
     "-Dssh_agent=false",
