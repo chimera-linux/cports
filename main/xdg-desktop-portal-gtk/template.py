267a4e6652a6c92ad652804fbe87ca210159172d
q66 <q66@chimera-linux.org>
2026-06-06 23:46:31 +0200

    main/xdg-desktop-portal-gtk: switch libexec dir

--- main/xdg-desktop-portal-gtk/template.py
+++ main/xdg-desktop-portal-gtk/template.py
@@ -1,9 +1,8 @@
 pkgname = "xdg-desktop-portal-gtk"
 pkgver = "1.15.3"
-pkgrel = 0
+pkgrel = 1
 build_style = "meson"
 configure_args = [
-    "--libexecdir=/usr/libexec",  # TODO switch libexec
     # pulls gnome-desktop
     "-Dwallpaper=disabled",
 ]
