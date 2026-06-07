ee4031f41ba606a09ccf1c9155b93433bdfd71ac
Alois Wohlschlager <alois1@gmx-topmail.de>
2026-06-07 11:48:31 +0300

    main/mkvtoolnix: update to 100.0

--- main/mkvtoolnix/template.py
+++ main/mkvtoolnix/template.py
@@ -1,13 +1,11 @@
 pkgname = "mkvtoolnix"
-pkgver = "97.0"
-pkgrel = 2
+pkgver = "100.0"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--disable-optimization",
     "--disable-update-check",
     "--with-docbook-xsl-root=/usr/share/xsl-nons/docbook",
-    # boost 1.89: work around custom build macros
-    "--with-boost-system=c",
 ]
 make_cmd = "rake"
 make_dir = "."
@@ -45,7 +43,7 @@ pkgdesc = "Tooling for editing and inspecting Matroska files"
 license = "GPL-2.0-only"
 url = "https://mkvtoolnix.download"
 source = f"{url}/sources/mkvtoolnix-{pkgver}.tar.xz"
-sha256 = "5d43bf66e011ff5af09516a2dba2fb717b1631791a3a7498fcf74849a86929d3"
+sha256 = "74480d07a261beeaa8baf898248e668ecc56335e2527bbffa841ef056dc028a1"
 
 
 @subpackage("mkvtoolnix-gui")
