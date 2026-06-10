adb7e8fccbad47446966a10fe84734005cbcaa6d
Marvin Friedrich <contact@marvinf.com>
2026-06-10 16:12:06 +0200

    main/limine: update to 12.3.3

--- main/limine/template.py
+++ main/limine/template.py
@@ -1,5 +1,5 @@
 pkgname = "limine"
-pkgver = "12.2.0"
+pkgver = "12.3.3"
 pkgrel = 0
 # these targets implemented
 archs = ["aarch64", "loongarch64", "riscv64", "x86_64"]
@@ -10,7 +10,7 @@ pkgdesc = "Multiprotocol EFI bootloader"
 license = "BSD-2-Clause AND 0BSD"
 url = "https://limine-bootloader.org"
 source = f"https://github.com/Limine-Bootloader/Limine/releases/download/v{pkgver}/limine-{pkgver}.tar.gz"
-sha256 = "db8a119878cfeead63c0a78236c577c40539c5759496950ea0ed32a6cf567865"
+sha256 = "f1a529da5cd50a5ca37ba5873133a7b8e72584b127d7331fe94e554e5e6012f7"
 # no test suite
 options = ["!check"]
 
