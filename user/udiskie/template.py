2b560874288e7fd47354d3f1ca521fc0a860fdf9
Matthias Nyman <mail@natthias.dev>
2026-06-16 14:28:56 +0200

    user/udiskie: update to 2.6.2, add dinit user service

--- user/udiskie/template.py
+++ user/udiskie/template.py
@@ -1,6 +1,6 @@
 pkgname = "udiskie"
-pkgver = "2.5.8"
-pkgrel = 1
+pkgver = "2.6.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "gettext",
@@ -8,6 +8,7 @@ hostmakedepends = [
     "python-installer",
     "python-setuptools",
 ]
+makedepends = ["turnstile"]
 depends = [
     "python-docopt",
     "python-gobject",
@@ -18,7 +19,7 @@ pkgdesc = "Automounter for removable media"
 license = "MIT"
 url = "https://github.com/coldfix/udiskie"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "ade0b67392fe5cfbd3a84c502c1e76bc2edb66e3c7e1d0ccbe2e62421f699674"
+sha256 = "9d758efd4e3706ce824e693708cce1e0a840dae9aa5b130e3592d3588da8279c"
 # usr/share/zsh/site-functions/_udiskie-canonical_paths has no matching command
 options = ["!lintcomp"]
 
@@ -30,3 +31,4 @@ def pre_check(self):
 
 def post_install(self):
     self.install_license("COPYING")
+    self.install_service(self.files_path / "udiskie.user")
