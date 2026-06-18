0acd931a6038f791064faea320e44ce1ca070166
Alois Wohlschlager <alois1@gmx-topmail.de>
2026-06-18 20:13:29 +0300

    main/oxygen-icons: update to 6.27.0
    The source URL changes too since it's a proper part of KDE frameworks again.

--- main/oxygen-icons/template.py
+++ main/oxygen-icons/template.py
@@ -1,5 +1,5 @@
 pkgname = "oxygen-icons"
-pkgver = "6.2.0"
+pkgver = "6.27.0"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -17,5 +17,5 @@ checkdepends = [
 pkgdesc = "Oxygen icon themes"
 license = "GPL-2.0-or-later"
 url = "https://api.kde.org/frameworks/oxygen-icons/html"
-source = f"$(KDE_SITE)/oxygen-icons/oxygen-icons-{pkgver}.tar.xz"
-sha256 = "61fd2ef56e7afbbbab034052017264ad00d074c7be06f0855d4c805a5cbacfdd"
+source = f"$(KDE_SITE)/frameworks/{pkgver[: pkgver.rfind('.')]}/oxygen-icons-{pkgver}.tar.xz"
+sha256 = "f36a0884e6b9e554721c0f8cbd40c20ed4ff1a1b7d7e293c67d3d11d0b72cf9a"
