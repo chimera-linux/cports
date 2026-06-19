ac108589796a7e9641735b6c15fe8966a4213756
q66 <q66@chimera-linux.org>
2026-06-19 15:30:41 +0200

    main/ngtcp2: update to 1.23.0

--- main/ngtcp2/template.py
+++ main/ngtcp2/template.py
@@ -1,20 +1,19 @@
 pkgname = "ngtcp2"
-pkgver = "1.14.0"
+pkgver = "1.23.0"
 pkgrel = 0
-build_style = "cmake"
-configure_args = ["-DENABLE_GNUTLS=ON", "-DENABLE_OPENSSL=OFF"]
-make_check_target = "check"
+build_style = "gnu_configure"
+configure_args = ["--with-gnutls", "--with-openssl"]
 hostmakedepends = [
-    "cmake",
-    "ninja",
+    "automake",
     "pkgconf",
+    "slibtool",
 ]
-makedepends = ["gnutls-devel"]
+makedepends = ["gnutls-bootstrap", "openssl3-devel"]
 pkgdesc = "C IETF QUIC protocol implementation"
 license = "MIT"
 url = "https://github.com/ngtcp2/ngtcp2"
 source = f"{url}/releases/download/v{pkgver}/ngtcp2-{pkgver}.tar.xz"
-sha256 = "d1fbf9eae92921bfd33154dab2574bc4b7d7936f486396d6c78bfff90ed5b35d"
+sha256 = "59d5b4211e96970f2d3d5e6876f73dce03414800ba04aa56835b132fce8de730"
 
 
 def post_install(self):
@@ -24,3 +23,19 @@ def post_install(self):
 @subpackage("ngtcp2-devel")
 def _(self):
     return self.default_devel()
+
+
+# projects explicitly link against crypto helpers of their choice
+# so no install_if shenanigans or anything like that
+@subpackage("ngtcp2-crypto-gnutls")
+def _(self):
+    self.subdesc = "GnuTLS helper"
+
+    return ["usr/lib/libngtcp2_crypto_gnutls.so.*"]
+
+
+@subpackage("ngtcp2-crypto-ossl")
+def _(self):
+    self.subdesc = "OpenSSL helper"
+
+    return ["usr/lib/libngtcp2_crypto_ossl.so.*"]
