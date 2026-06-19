20a128bd8b35bfae0f02942d2522101d8ba6bc20
flukey <flukey@vapourmail.eu>
2026-06-19 08:44:25 +0100

    user/senpai: update to 0.5.0

--- user/senpai/template.py
+++ user/senpai/template.py
@@ -1,14 +1,18 @@
 pkgname = "senpai"
-pkgver = "0.4.1"
-pkgrel = 9
+pkgver = "0.5.0"
+pkgrel = 1
 build_style = "go"
-make_build_args = ["./cmd/senpai"]
+make_build_args = [
+    "-ldflags",
+    f"-X git.sr.ht/~delthas/senpai.version={pkgver}",
+    "./cmd/senpai",
+]
 hostmakedepends = ["go", "scdoc"]
 pkgdesc = "IRC client that works best with bouncers"
 license = "ISC"
 url = "https://git.sr.ht/~delthas/senpai"
 source = f"https://git.sr.ht/~delthas/senpai/archive/v{pkgver}.tar.gz"
-sha256 = "ab786b7b3cffce69d080c3b58061e14792d9065ba8831f745838c850acfeab24"
+sha256 = "1793259dca5321f1365cae9d24316d5d4cd01df648d895eaa03eacb49f433db8"
 
 
 def post_build(self):
