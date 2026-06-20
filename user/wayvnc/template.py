febfbd3619e088a026fd6b54e09f2106dc8bd685
Matthias Nyman <mail@natthias.dev>
2026-06-20 14:41:55 +0200

    user/wayvnc: update to 0.10.0

--- user/wayvnc/template.py
+++ user/wayvnc/template.py
@@ -1,5 +1,5 @@
 pkgname = "wayvnc"
-pkgver = "0.9.1"
+pkgver = "0.10.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "wayland-progs"]
@@ -17,7 +17,7 @@ pkgdesc = "VNC server for wlroots-based Wayland compositors"
 license = "ISC"
 url = "https://github.com/any1/wayvnc"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "aaaca02d36e54ec6ecf457dc266251946d895ac91521fbabb3470c3c09b3753c"
+sha256 = "fcfda018d0e07ec00a80071420c8cc2a75885dc6d5e55bb50a9b12353754338f"
 hardening = ["vis", "cfi"]
 
 
