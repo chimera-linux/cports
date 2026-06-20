a750b144c6eaacb27030576b50f8c81b2c5c0128
q66 <q66@chimera-linux.org>
2026-06-20 13:40:01 +0200

    main/nghttp3: update to 1.16.0

--- main/nghttp3/template.py
+++ main/nghttp3/template.py
@@ -1,5 +1,5 @@
 pkgname = "nghttp3"
-pkgver = "1.15.0"
+pkgver = "1.16.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -11,7 +11,7 @@ pkgdesc = "C HTTP/3 library"
 license = "MIT"
 url = "https://nghttp2.org/nghttp3"
 source = f"https://github.com/ngtcp2/nghttp3/releases/download/v{pkgver}/nghttp3-{pkgver}.tar.xz"
-sha256 = "6da0cd06b428d32a54c58137838505d9dc0371a900bb8070a46b29e1ceaf2e0f"
+sha256 = "776f59a99905c9a348846807b2e5ac9bb3485fc0f8c0250ba803018d5238a16e"
 
 
 def post_install(self):
