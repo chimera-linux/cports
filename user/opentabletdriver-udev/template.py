d87e2868e10e437cc4626742fa1121f6102ce254
fiore <fiore@grrlz.net>
2026-06-20 16:55:58 +0200

    user/opentabletdriver-udev: upgrade to 0.6.7

--- user/opentabletdriver-udev/template.py
+++ user/opentabletdriver-udev/template.py
@@ -1,12 +1,12 @@
 pkgname = "opentabletdriver-udev"
-pkgver = "0.6.6.2"
+pkgver = "0.6.7"
 pkgrel = 0
 hostmakedepends = ["bash", "jq"]
 pkgdesc = "Udev rules for OpenTabletDriver"
 license = "LGPL-3.0-or-later"
 url = "https://opentabletdriver.net"
 source = f"https://github.com/OpenTabletDriver/OpenTabletDriver/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "1602f3291bad333be56671933e5ba6fb17144432729c10b2a0b40626e8c9dd28"
+sha256 = "95e8fe646ccfcee97da5477f2432ce3265ec6c0c5356a23b1b2f41a0b738796f"
 
 
 def build(self):
@@ -17,6 +17,6 @@ def build(self):
 def install(self):
     self.install_file("99-opentabletdriver.rules", "usr/lib/udev/rules.d")
     self.install_file(
-        "eng/linux/Generic/usr/lib/modprobe.d/99-opentabletdriver.conf",
+        "eng/bash/Generic/usr/lib/modprobe.d/99-opentabletdriver.conf",
         "usr/lib/modprobe.d",
     )
