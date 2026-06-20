42bb5ed82dc7c0461f4e6ded3e58cff7c61e507b
q66 <q66@chimera-linux.org>
2026-06-20 13:40:09 +0200

    main/nghttp2: update to 1.69.0

--- main/nghttp2/template.py
+++ main/nghttp2/template.py
@@ -1,7 +1,8 @@
 pkgname = "nghttp2"
-pkgver = "1.68.0"
+pkgver = "1.69.0"
 pkgrel = 0
 build_style = "gnu_configure"
+configure_args = ["--enable-http3"]
 configure_gen = []
 hostmakedepends = ["pkgconf"]
 makedepends = [
@@ -10,6 +11,8 @@ makedepends = [
     "libev-devel",
     "libevent-devel",
     "libxml2-devel",
+    "nghttp3-devel",
+    "ngtcp2-devel",
     "openssl3-devel",
     "zlib-ng-compat-devel",
 ]
@@ -18,7 +21,7 @@ pkgdesc = "HTTP/2 C Library"
 license = "MIT"
 url = "https://nghttp2.org"
 source = f"https://github.com/tatsuhiro-t/nghttp2/releases/download/v{pkgver}/nghttp2-{pkgver}.tar.xz"
-sha256 = "5511d3128850e01b5b26ec92bf39df15381c767a63441438b25ad6235def902c"
+sha256 = "1fb324b6ec2c56f6bde0658f4139ffd8209fa9e77ce98fd7a5f63af8d0e508ad"
 # CFI; reproduces in e.g. libsoup
 hardening = ["vis", "!cfi"]
 
