0bdb40fb9d61eead1cc96ee32f5a7f69b25be293
Alois Wohlschlager <alois1@gmx-topmail.de>
2026-06-20 13:43:03 +0300

    main/passt: update to 2026.06.11
    At the same time, disable some bullshit (although accidentally mostly harmless)
    CFLAGS for the AVX2 build.

--- main/passt/template.py
+++ main/passt/template.py
@@ -1,5 +1,5 @@
 pkgname = "passt"
-_pkgver = "2025_02_17.a1e48a0"
+_pkgver = "2026_06_11.a9c61ff"
 # yeardate only
 pkgver = _pkgver.split(".")[0].replace("_", ".")
 pkgrel = 0
@@ -12,9 +12,9 @@ pkgdesc = "User-mode network translation layer between Layer 2 and 4"
 license = "BSD-3-Clause AND GPL-2.0-or-later"
 url = "https://passt.top/passt/about"
 source = f"https://passt.top/passt/snapshot/passt-{_pkgver}.tar.zst"
-sha256 = "51ebc11c6e0c49e9905ef8821cb97720d4831d5d2f9cf161277e1fe076fb8d60"
-# tries to pass this via __attribute(optimise) for some stuff but that is ignored
-tool_flags = {"CFLAGS": ["-fno-strict-aliasing"]}
+sha256 = "9df3815aaf17c4cbc9862115e8bc15664e2794b54a3a8d584bba6b9021748a02"
+# tries to pass no-strict-aliasing via __attribute(optimise) for some stuff but that is ignored
+tool_flags = {"CFLAGS": ["-fno-strict-aliasing", "-std=c23"]}
 hardening = ["vis", "cfi"]
 # needs qemu and some other unpackaged tooling
 options = ["!check"]
