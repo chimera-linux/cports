0c570e434f6dd17710d0e5a9fea9d489660886b0
flukey <flukey@vapourmail.eu>
2026-06-22 10:14:45 +0100

    main/notmuch: update to 0.40

--- main/notmuch/template.py
+++ main/notmuch/template.py
@@ -1,15 +1,17 @@
 pkgname = "notmuch"
-pkgver = "0.39"
+pkgver = "0.40"
 pkgrel = 0
 build_style = "configure"
 configure_args = ["--prefix=/usr"]
 hostmakedepends = [
+    "bash-completion",
     "pkgconf",
     "python-sphinx",
 ]
 makedepends = [
     "gmime-devel",
     "linux-headers",
+    "python-devel",
     "talloc-devel",
     "xapian-core-devel",
     "zlib-ng-compat-devel",
@@ -18,7 +20,7 @@ pkgdesc = "Fast, global-search, tag-based email system"
 license = "GPL-3.0-or-later"
 url = "https://notmuchmail.org"
 source = f"{url}/releases/notmuch-{pkgver}.tar.xz"
-sha256 = "b88bb02a76c46bad8d313fd2bb4f8e39298b51f66fcbeb304d9f80c3eef704e3"
+sha256 = "4b4314bbf1c2029fdf793637e6c7bb15c1b1730d22be9aa04803c98c5bbc446f"
 hardening = ["vis", "!cfi"]
 # FIXME: they hang forever, after failing a test for -v not silencing output in harness
 # zsh completions don't match command
