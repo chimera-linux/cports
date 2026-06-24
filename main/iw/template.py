afcd938ec0016ae77248174673e927a7ba844fdf
flukey <flukey@vapourmail.eu>
2026-06-24 15:43:45 +0100

    main/iw: update to 6.17

--- main/iw/template.py
+++ main/iw/template.py
@@ -1,5 +1,5 @@
 pkgname = "iw"
-pkgver = "6.9"
+pkgver = "6.17"
 pkgrel = 0
 build_style = "makefile"
 make_install_args = ["SBINDIR=/usr/bin"]
@@ -10,7 +10,7 @@ pkgdesc = "Utility for nl80211 based CLI configuration of wireless devices"
 license = "ISC"
 url = "https://wireless.kernel.org/en/users/Documentation/iw"
 source = f"https://www.kernel.org/pub/software/network/iw/iw-{pkgver}.tar.xz"
-sha256 = "3f2db22ad41c675242b98ae3942dbf3112548c60a42ff739210f2de4e98e4894"
+sha256 = "7d182e498289ab39b257da6780d562e415377107f50358ee5b55b8cfe40b1e33"
 hardening = ["vis", "cfi"]
 
 
