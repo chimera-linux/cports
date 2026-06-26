3b0a53bc3facc121c852ea2c3504e4fba6c3dfa3
flukey <flukey@vapourmail.eu>
2026-06-26 11:15:07 +0100

    main/xdg-dbus-proxy: update to 0.1.7

--- main/xdg-dbus-proxy/template.py
+++ main/xdg-dbus-proxy/template.py
@@ -1,5 +1,5 @@
 pkgname = "xdg-dbus-proxy"
-pkgver = "0.1.6"
+pkgver = "0.1.7"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dman=enabled"]
@@ -16,4 +16,4 @@ pkgdesc = "Filtering proxy for D-Bus connections"
 license = "GPL-3.0-or-later"
 url = "https://github.com/flatpak/xdg-dbus-proxy"
 source = f"{url}/releases/download/{pkgver}/xdg-dbus-proxy-{pkgver}.tar.xz"
-sha256 = "131bf59fce7c7ee7ecbc5d9106d6750f4f597bfe609966573240f7e4952973a1"
+sha256 = "3ad3d27ba574e178acb5e4d438ba36ace25e3564f899c36f31c56f82c7adbbe7"
