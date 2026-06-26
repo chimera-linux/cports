87377ce6357a317397b65220cac9b65840db2bb9
Segen Stoutamire <segen@stoutamire.com>
2026-06-25 20:13:01 -0700

    user/raylib: new package
    Closes https://github.com/chimera-linux/cports/pull/5723

---
+++ user/raylib/template.py
@@ -0,0 +1,22 @@
+pkgname = "raylib"
+pkgver = "6.0"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    "-DOPENGL_VERSION=ES 2.0",  # universally supported by mesa
+    "-DUSE_EXTERNAL_GLFW=ON",
+    "-DBUILD_SHARED_LIBS=ON",
+    "-DBUILD_EXAMPLES=OFF",
+]
+hostmakedepends = ["cmake", "ninja", "pkgconf"]
+makedepends = ["glfw-devel"]
+pkgdesc = "Game programming library"
+license = "Zlib"
+url = "https://github.com/raysan5/raylib"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "2b3ee1e2120c7a0796b33062c7e9a694dd8a8caa56a96319ac8c8ecf54a90d0b"
+
+
+@subpackage("raylib-devel")
+def _(self):
+    return self.default_devel()
