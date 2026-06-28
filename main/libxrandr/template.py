e1e4bdc97143638f7da455b86e34a27994df337c
flukey <flukey@vapourmail.eu>
2026-06-28 16:56:13 +0100

    main/libxrandr: update to 1.5.5

--- main/libxrandr/template.py
+++ main/libxrandr/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxrandr"
-pkgver = "1.5.4"
+pkgver = "1.5.5"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--enable-malloc0returnsnull"]
@@ -9,7 +9,7 @@ pkgdesc = "X RandR Library from X.org"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXrandr-{pkgver}.tar.gz"
-sha256 = "c72c94dc3373512ceb67f578952c5d10915b38cc9ebb0fd176a49857b8048e22"
+sha256 = "23faedab4675890ba579b8103399132a139527306b18b500c6fe28e090e2a056"
 
 
 def post_install(self):
