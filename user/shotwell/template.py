8abb3d547b74412f85fbe4cb1076d1a6a319b1af
Wesley Moore <wes@wezm.net>
2026-06-29 11:54:09 +1000

    user/shotwell: update to 0.32.17

--- user/shotwell/template.py
+++ user/shotwell/template.py
@@ -1,6 +1,6 @@
 pkgname = "shotwell"
-pkgver = "0.32.15"
-pkgrel = 1
+pkgver = "0.32.17"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Ddefault_library=shared",
@@ -33,4 +33,4 @@ pkgdesc = "Digital photo organizer"
 license = "CC-BY-SA-3.0 AND LGPL-2.1-or-later"
 url = "https://gitlab.gnome.org/GNOME/shotwell"
 source = f"$(GNOME_SITE)/shotwell/{'.'.join(pkgver.split('.')[:2])}/shotwell-{pkgver}.tar.xz"
-sha256 = "ac10260f382e1a3193bbb0dac8143163f1ca35546fa76133d542e390af742f3a"
+sha256 = "0a56684e98817c3103f54a648fe9400427c76a25a7b111457fc1d860c3167672"
