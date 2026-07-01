051f2121de7b31f4901d243e1265e48b3c9824ce
fiore <fiore@grrlz.net>
2026-07-01 10:47:36 +0200

    main/labwc: upgrade to 0.20.1

--- main/labwc/template.py
+++ main/labwc/template.py
@@ -1,5 +1,5 @@
 pkgname = "labwc"
-pkgver = "0.9.7"
+pkgver = "0.20.1"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -24,7 +24,7 @@ makedepends = [
     "pango-devel",
     "wayland-devel",
     "wayland-protocols",
-    "wlroots0.19-devel",
+    "wlroots0.20-devel",
     "xcb-util-wm-devel",
     "xwayland-devel",
 ]
@@ -33,4 +33,4 @@ pkgdesc = "Stacking wayland compositor"
 license = "GPL-2.0-only"
 url = "https://github.com/labwc/labwc"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "4ad4e5e7f29e0d0704fadb4a072037173d850b46f12122b79168879b922e0f43"
+sha256 = "2c95d8c19cc50ce0dd5cf3e412932ebb4d3353bc4a5d11e2405d124e6c77dcd2"
