f9fb98454075694f8005fa1759ec0884337cb1b6
q66 <q66@chimera-linux.org>
2026-07-01 16:08:56 +0200

    main/hyphen: update to 2.8.9

--- main/hyphen/template.py
+++ main/hyphen/template.py
@@ -1,5 +1,5 @@
 pkgname = "hyphen"
-pkgver = "2.8.8"
+pkgver = "2.8.9"
 pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."  # fails tests otherwise
@@ -7,9 +7,9 @@ hostmakedepends = ["automake", "pkgconf", "perl", "gawk", "libtool"]
 checkdepends = ["bash"]
 pkgdesc = "Hyphenation library"
 license = "GPL-2.0-or-later OR LGPL-2.1-or-later OR MPL-1.1"
-url = "http://sourceforge.net/projects/hunspell/files/Hyphen"
-source = f"$(SOURCEFORGE_SITE)/hunspell/hyphen-{pkgver}.tar.gz"
-sha256 = "304636d4eccd81a14b6914d07b84c79ebb815288c76fe027b9ebff6ff24d5705"
+url = "https://hunspell.github.io"
+source = f"https://github.com/hunspell/hyphen/releases/download/v{pkgver}/hyphen-{pkgver}.tar.gz"
+sha256 = "783743daf477de8c4d16e3c74b4d2827377017718d8e17e2d9440210246f6abe"
 
 
 def post_install(self):
