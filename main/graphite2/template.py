20780989a5665b3ee5738ed1c145620085f4efcb
q66 <q66@chimera-linux.org>
2026-07-01 15:58:24 +0200

    main/graphite2: update to 1.3.15

--- main/graphite2/template.py
+++ main/graphite2/template.py
@@ -1,5 +1,5 @@
 pkgname = "graphite2"
-pkgver = "1.3.14"
+pkgver = "1.3.15"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -7,11 +7,12 @@ configure_args = [
     "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
 ]
 hostmakedepends = ["cmake", "ninja", "pkgconf", "python"]
+checkdepends = ["python-fonttools"]
 pkgdesc = "Reimplementation of the SIL Graphite text processing engine"
 license = "LGPL-2.1-or-later AND GPL-2.0-or-later"
 url = "https://github.com/silnrsi/graphite"
 source = f"{url}/releases/download/{pkgver}/graphite2-{pkgver}.tgz"
-sha256 = "f99d1c13aa5fa296898a181dff9b82fb25f6cc0933dbaa7a475d8109bd54209d"
+sha256 = "c6bc8b4252724665297f7cad0c55897285c673f9b8e6db3522ace833593fe0b1"
 # FIXME int
 hardening = ["!int"]
 
