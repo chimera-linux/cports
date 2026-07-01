81ad550ad0023db3ab5c6f40359d85f88497829c
q66 <q66@chimera-linux.org>
2026-07-01 15:59:10 +0200

    main/hunspell: update to 1.7.3

--- main/hunspell/template.py
+++ main/hunspell/template.py
@@ -1,6 +1,6 @@
 pkgname = "hunspell"
-pkgver = "1.7.2"
-pkgrel = 1
+pkgver = "1.7.3"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--with-ui", "--with-readline"]
 hostmakedepends = ["pkgconf", "automake", "libtool"]
@@ -10,7 +10,7 @@ pkgdesc = "Spell checker and morphological analyzer"
 license = "GPL-2.0-or-later OR LGPL-2.1-or-later OR MPL-1.1"
 url = "https://hunspell.github.io"
 source = f"https://github.com/hunspell/hunspell/releases/download/v{pkgver}/hunspell-{pkgver}.tar.gz"
-sha256 = "11ddfa39afe28c28539fe65fc4f1592d410c1e9b6dd7d8a91ca25d85e9ec65b8"
+sha256 = "433274dac0619cb00c2e18b43a3dd3a9d50da5b5613fa9b5c21781e35dd76bc1"
 
 
 @subpackage("hunspell-libs")
