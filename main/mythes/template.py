98a70f6169d497003d18d9883de6336361fd4108
q66 <q66@chimera-linux.org>
2026-07-01 16:18:11 +0200

    main/mythes: update to 1.2.6

--- main/mythes/template.py
+++ main/mythes/template.py
@@ -1,14 +1,14 @@
 pkgname = "mythes"
-pkgver = "1.2.4"
-pkgrel = 1
+pkgver = "1.2.6"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["pkgconf", "automake", "libtool"]
 makedepends = ["hunspell-devel"]
 pkgdesc = "Simple thesaurus that uses structured text data"
 license = "BSD-3-Clause"
-url = "http://sourceforge.net/projects/hunspell/files/MyThes"
-source = f"$(SOURCEFORGE_SITE)/hunspell/mythes-{pkgver}.tar.gz"
-sha256 = "1e81f395d8c851c3e4e75b568e20fa2fa549354e75ab397f9de4b0e0790a305f"
+url = "https://hunspell.github.io"
+source = f"https://github.com/hunspell/mythes/releases/download/v{pkgver}/mythes-{pkgver}.tar.xz"
+sha256 = "97bd2ba5738aebdba1bc31f30f1be42f1404386c94105ad5990d839a1311c8a5"
 
 
 def post_extract(self):
