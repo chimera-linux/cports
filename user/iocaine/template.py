a7f5c82053c784c9efe248bded7868b74322a388
Ayush Agarwal <ayush@ayushnix.com>
2026-07-01 11:25:17 +0530

    user/iocaine: new package

---
+++ user/iocaine/template.py
@@ -0,0 +1,30 @@
+pkgname = "iocaine"
+pkgver = "3.5.0"
+pkgrel = 0
+build_style = "cargo"
+prepare_after_patch = True
+hostmakedepends = ["cargo-auditable", "pkgconf"]
+makedepends = [
+    "dinit-chimera",
+    "gmp-devel",
+    "iptables-devel",
+    "jansson-devel",
+    "libmnl-devel",
+    "libnftnl-devel",
+    "nftables-devel",
+    "rust-std",
+    "zstd-devel",
+]
+pkgdesc = "LLM crawler abuse defense mechanism"
+license = "MIT"
+url = "https://iocaine.madhouse-project.org"
+source = f"https://git.madhouse-project.org/iocaine/iocaine/archive/iocaine-{pkgver}.tar.gz"
+sha256 = "d0acb7019238c4b7cb163a999dacbe4919ab0cc1380c1c39e79b6b7e108d6f1b"
+
+
+def install(self):
+    self.install_license("LICENSES/MIT.txt")
+    self.install_bin(f"target/{self.profile().triplet}/release/iocaine")
+    self.install_sysusers(self.files_path / "sysusers.conf")
+    self.install_tmpfiles(self.files_path / "tmpfiles.conf")
+    self.install_service(self.files_path / "iocaine")
