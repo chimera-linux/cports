508b0d11a3d68ba8e397f6a9737568369ea5e834
q66 <q66@chimera-linux.org>
2026-07-01 15:50:41 +0200

    main/libabw: update to 0.1.4

--- main/libabw/template.py
+++ main/libabw/template.py
@@ -1,6 +1,6 @@
 pkgname = "libabw"
-pkgver = "0.1.3"
-pkgrel = 9
+pkgver = "0.1.4"
+pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
 hostmakedepends = [
@@ -15,7 +15,7 @@ pkgdesc = "Library for AbiWord document format"
 license = "MPL-2.0"
 url = "https://wiki.documentfoundation.org/DLP/Libraries/libabw"
 source = f"https://dev-www.libreoffice.org/src/libabw/libabw-{pkgver}.tar.xz"
-sha256 = "e763a9dc21c3d2667402d66e202e3f8ef4db51b34b79ef41f56cacb86dcd6eed"
+sha256 = "fa2685a3440da6e03a66a778480d93cb95f6064e4541e58e37397680760fd6a0"
 
 
 @subpackage("libabw-progs")
