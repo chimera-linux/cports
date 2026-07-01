364a31552862be0455a5930d18106ea1ca7b069a
q66 <q66@chimera-linux.org>
2026-07-01 16:18:52 +0200

    main/libqxp: update to 0.0.3

--- main/libqxp/template.py
+++ main/libqxp/template.py
@@ -1,6 +1,6 @@
 pkgname = "libqxp"
-pkgver = "0.0.2"
-pkgrel = 14
+pkgver = "0.0.3"
+pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
 hostmakedepends = ["pkgconf", "automake", "slibtool"]
@@ -9,7 +9,7 @@ pkgdesc = "Library for QuarkXPress format"
 license = "MPL-2.0"
 url = "https://wiki.documentfoundation.org/DLP/Libraries/libqxp"
 source = f"https://dev-www.libreoffice.org/src/libqxp/libqxp-{pkgver}.tar.xz"
-sha256 = "e137b6b110120a52c98edd02ebdc4095ee08d0d5295a94316a981750095a945c"
+sha256 = "4687a9cc96c32d7406e5072c4da150fca696c563e5cde62b024f82fa53d32332"
 
 
 @subpackage("libqxp-progs")
