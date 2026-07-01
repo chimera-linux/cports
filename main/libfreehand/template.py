808623630c1456855c6ba0eac8d7fc6612fed02e
q66 <q66@chimera-linux.org>
2026-07-01 15:53:11 +0200

    main/libfreehand: update to 0.1.3

--- main/libfreehand/template.py
+++ main/libfreehand/template.py
@@ -1,6 +1,6 @@
 pkgname = "libfreehand"
-pkgver = "0.1.2"
-pkgrel = 9
+pkgver = "0.1.3"
+pkgrel = 0
 build_style = "gnu_configure"
 make_dir = "."
 hostmakedepends = [
@@ -20,7 +20,7 @@ pkgdesc = "Parser for Aldus/Macromedia/Adobe FreeHand documents"
 license = "MPL-2.0"
 url = "https://wiki.documentfoundation.org/DLP/Libraries/libfreehand"
 source = f"http://dev-www.libreoffice.org/src/libfreehand/libfreehand-{pkgver}.tar.xz"
-sha256 = "0e422d1564a6dbf22a9af598535425271e583514c0f7ba7d9091676420de34ac"
+sha256 = "a431d78767e5aa27ade7c6d1b7a11a9f1848cb4b9260bf0a6a44689553ecccfe"
 
 
 @subpackage("libfreehand-progs")
