aca6e3d42aabde3e6104b233979de6cbec64bd77
flukey <flukey@vapourmail.eu>
2026-07-02 18:04:44 +0100

    main/tree-sitter-cli: update to 0.26.10
    Revert patch was absorbed upstream.
    
    Built and ran tests on tree-sitter-c successfully.

--- main/tree-sitter-cli/template.py
+++ main/tree-sitter-cli/template.py
@@ -1,6 +1,6 @@
 pkgname = "tree-sitter-cli"
 # match to tree-sitter
-pkgver = "0.26.8"
+pkgver = "0.26.10"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = ["-p", "tree-sitter-cli"]
@@ -11,19 +11,13 @@ pkgdesc = "Parser generator tool for tree-sitter bindings"
 license = "MIT"
 url = "https://tree-sitter.github.io/tree-sitter"
 source = f"https://github.com/tree-sitter/tree-sitter/archive/v{pkgver}.tar.gz"
-sha256 = "e6826b7533ec3a885aba598377a6d20b5a6321ff3db76968e960c2352d3a5077"
+sha256 = "450cb85fd1af34111eb162e931e0e9e4d4dbf23fc09b9cb56f6299a1a80483b6"
 # requires fetching fixtures
 options = ["!check"]
 
 if self.profile().arch in ["aarch64", "x86_64"]:
     make_build_args += ["--features", "wasm"]
     make_check_args += ["--features", "wasm"]
-
-
-def post_patch(self):
-    from cbuild.util import cargo
-
-    cargo.clear_vendor_checksums(self, "libc-0.2.180")
 
 
 def install(self):
