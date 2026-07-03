da0cb4751b595d74945910f3c4f3c26591d03e1a
flukey <flukey@vapourmail.eu>
2026-07-03 16:05:24 +0100

    user/mpv-mpris: update to 1.2
    Closes https://github.com/chimera-linux/cports/pull/5765

--- user/mpv-mpris/template.py
+++ user/mpv-mpris/template.py
@@ -1,6 +1,6 @@
 pkgname = "mpv-mpris"
-pkgver = "1.1"
-pkgrel = 3
+pkgver = "1.2"
+pkgrel = 0
 build_style = "makefile"
 make_check_target = "test"
 make_use_env = True
@@ -22,10 +22,15 @@ pkgdesc = "MPRIS plugin for mpv"
 license = "MIT"
 url = "https://github.com/hoyon/mpv-mpris"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "71008aa181bccf4bc7b2b5b9673e9993b1d1f5b7e2c189dc3724ab23ef1f6ebb"
+sha256 = "ecdc66f0182a38164b8bdc79502c575df3d2c4453bae5bff225c4e5ce9dbef6c"
 options = ["etcfiles"]
 
 
 def install(self):
-    self.install_file("mpris.so", "etc/mpv/scripts", 0o755)
+    self.install_file("mpris.so", "usr/lib/mpv-mpris", 0o755)
+    self.install_dir("etc/mpv/scripts")
+    self.install_link(
+        "etc/mpv/scripts/mpris.so",
+        "../../../usr/lib/mpv-mpris/mpris.so",
+    )
     self.install_license("LICENSE")
