3596c8e61a25120c76040b0eafedc618ef99e0af
resverie <resvery@proton.me>
2026-07-04 17:23:31 +0000

    main/sfeed: update to 2.4

--- main/sfeed/template.py
+++ main/sfeed/template.py
@@ -1,5 +1,5 @@
 pkgname = "sfeed"
-pkgver = "2.2"
+pkgver = "2.4"
 pkgrel = 0
 build_style = "makefile"
 make_install_args = [
@@ -13,7 +13,7 @@ pkgdesc = "RSS and Atom feed fetcher"
 license = "ISC"
 url = "https://codemadness.org/sfeed.html"
 source = f"https://codemadness.org/releases/sfeed/sfeed-{pkgver}.tar.gz"
-sha256 = "4270389c3cfa474caa3892271c3171a751490328cc52e502d8435de3c2e41cc5"
+sha256 = "f9503fe9205a8136f76a9b753c6007abad33e2a516807e416a986723db06e879"
 hardening = ["vis", "cfi"]
 # no tests defined
 options = ["!check"]
