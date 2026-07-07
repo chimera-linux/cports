6edbad99a1e06f68f5e8cb9413a860afd41f19b6
Biswapriyo Nath <nathbappai@gmail.com>
2026-07-07 09:51:48 +0000

    user/android-tools: update to 36.0.1
    * Revert some changes from 31556b06e78e8f3f780afec6706cc1ab907da03d commit
      because this release can be used with system provided libusb library.
    
    * udev-devel is required for libusb. It is removed too.
    
    * protobuf30.patch was fixed in upstream.
    
    Closes https://github.com/chimera-linux/cports/pull/5770

--- user/android-tools/template.py
+++ user/android-tools/template.py
@@ -1,14 +1,9 @@
 pkgname = "android-tools"
-pkgver = "35.0.2"
-pkgrel = 32
+pkgver = "36.0.1"
+pkgrel = 0
 # only supports specific little-endian archs, particularly in boringssl
-archs = ["x86_64", "aarch64", "ppc64le", "riscv64"]
+archs = ["x86_64", "aarch64", "loongarch64", "ppc64le", "riscv64"]
 build_style = "cmake"
-configure_args = [
-    # until next libusb
-    "-DANDROID_TOOLS_USE_BUNDLED_LIBUSB=ON",
-    "-DANDROID_TOOLS_LIBUSB_ENABLE_UDEV=ON",
-]
 hostmakedepends = [
     "cmake",
     "go",
@@ -26,7 +21,6 @@ makedepends = [
     "lz4-devel",
     "pcre2-devel",
     "protobuf-devel",
-    "udev-devel",
     "zstd-devel",
 ]
 depends = ["android-udev-rules", "python"]
@@ -34,7 +28,7 @@ pkgdesc = "Android platform tools, such as adb and fastboot"
 license = "Apache-2.0 AND ISC AND GPL-2.0-only AND MIT"
 url = "https://github.com/nmeum/android-tools"
 source = f"{url}/releases/download/{pkgver}/android-tools-{pkgver}.tar.xz"
-sha256 = "d2c3222280315f36d8bfa5c02d7632b47e365bfe2e77e99a3564fb6576f04097"
+sha256 = "38e8a84b739480141de0836bf6d581b3339ac7d53d0f7ce8c044a3368c8c2f8f"
 tool_flags = {"CXXFLAGS": ["-D_LARGEFILE64_SOURCE"]}
 hardening = ["vis", "cfi"]
 
