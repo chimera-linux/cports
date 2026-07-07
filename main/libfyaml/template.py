5ec63d614cb78aef0c85c81b73bece66d6b64138
flukey <flukey@vapourmail.eu>
2026-07-07 22:12:18 +0100

    main/libfyaml: update to 0.9.6

--- main/libfyaml/template.py
+++ main/libfyaml/template.py
@@ -1,17 +1,16 @@
 pkgname = "libfyaml"
-pkgver = "0.9"
+pkgver = "0.9.6"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-network"]
-configure_gen = []
-hostmakedepends = ["pkgconf"]
+hostmakedepends = ["autoconf", "automake", "pkgconf", "libtool"]
 makedepends = ["linux-headers"]
 checkdepends = ["bash", "check-devel", "git"]
 pkgdesc = "YAML parser and emitter"
 license = "MIT"
 url = "https://github.com/pantoniou/libfyaml"
 source = f"{url}/releases/download/v{pkgver}/libfyaml-{pkgver}.tar.gz"
-sha256 = "7731edc5dfcc345d5c5c9f6ce597133991a689dabede393cd77bae89b327cd6d"
+sha256 = "a59cc3331e2eb903ec36933ad52a45888041cac31e44f553a00511131242c483"
 
 
 def post_install(self):
