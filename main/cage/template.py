f08a38dd1e5110a226b30aa180c8784a32ad24e4
Wesley Moore <wes@wezm.net>
2026-07-08 20:15:27 +1000

    main/cage: update to 0.3.1

--- main/cage/template.py
+++ main/cage/template.py
@@ -1,5 +1,5 @@
 pkgname = "cage"
-pkgver = "0.3.0"
+pkgver = "0.3.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf", "scdoc", "wayland-progs"]
@@ -8,7 +8,7 @@ pkgdesc = "Kiosk compositor for Wayland"
 license = "MIT"
 url = "https://www.hjdskes.nl/projects/cage"
 source = f"https://github.com/cage-kiosk/cage/releases/download/v{pkgver}/cage-{pkgver}.tar.gz"
-sha256 = "cd2510f83bef3e08e660d99492ca7761b218ecb53ee01cdbbeee3d6aabc7734e"
+sha256 = "edafc173b6f56a3e1564933e272a54aaa9fe158d330e6a415d57b44ab880fe2b"
 hardening = ["vis", "cfi"]
 
 
