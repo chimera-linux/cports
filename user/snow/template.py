7eb1b95549af1bd35cb252823d858acd1de1e999
Wesley Moore <wes@wezm.net>
2026-07-08 20:18:06 +1000

    user/snow: update to 1.5.0

--- user/snow/template.py
+++ user/snow/template.py
@@ -1,5 +1,5 @@
 pkgname = "snow"
-pkgver = "1.4.1"
+pkgver = "1.5.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = [
@@ -16,7 +16,7 @@ pkgdesc = "Classic Macintosh emulator"
 license = "MIT"
 url = "https://snowemu.com"
 source = f"https://github.com/twvd/snow/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "c980c9ac4d952ddc21da63fe28a411852ff49463a6e62c647502b541e3d0b229"
+sha256 = "00ee7515a8ed5e977e46b5f87a02ce964049613a5b909203b77b2feb277193a0"
 
 if self.profile().wordsize == 32:
     broken = "needs atomic64"
