673b0465a70ef9f7dee52e90fd3021461ac59353
aniruddhgutta <aniruddhgutta@disroot.org>
2026-07-09 17:55:14 +0530

    user/rebos: new package

---
+++ user/rebos/template.py
@@ -0,0 +1,15 @@
+pkgname = "rebos"
+pkgver = "3.5.2"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable"]
+makedepends = ["rust-std"]
+pkgdesc = "NixOS-style repeatability for any Linux distribution"
+license = "GPL-3.0-or-later"
+url = "https://gitlab.com/Oglo12/rebos"
+source = f"{url}/-/archive/v{pkgver}/rebos-v{pkgver}.tar.gz"
+sha256 = "fb5bd9c9d11712187bc0a4fcd8b438228d73951f920f08d35f23cb52dd051696"
+
+
+def install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/rebos")
