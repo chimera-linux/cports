24b7b5cfe2d86b1ef1c9bd10ab413e61a804ef9e
Erica Z <zerica@callcc.eu>
2026-07-10 11:45:00 +0200

    main/easyeffects: update to 8.2.7

--- main/easyeffects/template.py
+++ main/easyeffects/template.py
@@ -1,51 +1,59 @@
 pkgname = "easyeffects"
-pkgver = "7.2.5"
-pkgrel = 1
-build_style = "meson"
+pkgver = "8.2.7"
+pkgrel = 0
+build_style = "cmake"
 hostmakedepends = [
-    "desktop-file-utils",
+    "cmake",
+    "extra-cmake-modules",
     "gettext",
-    "glib-devel",
-    "gtk+3-update-icon-cache",
-    "itstool",
-    "meson",
+    "ninja",
     "pkgconf",
 ]
 makedepends = [
-    "appstream-glib-devel",
     "dinit-chimera",
     "dinit-dbus",
     "fftw-devel",
-    "fmt-devel",
-    "glib-devel",
     "gsl-devel",
-    "gtk4-devel",
+    "kcolorscheme-devel",
+    "kconfig-devel",
+    "kconfigwidgets-devel",
+    "kcoreaddons-devel",
+    "ki18n-devel",
+    "kiconthemes-devel",
+    "kirigami-addons-devel",
+    "kirigami-devel",
     "ladspa-sdk",
-    "libadwaita-devel",
     "libbs2b-devel",
     "libebur128-devel",
-    "libsamplerate-devel",
-    "libsigc++-devel",
+    "libmysofa-devel",
+    "libportal-qt6-devel",
     "libsndfile-devel",
     "lilv-devel",
     "lv2",
     "nlohmann-json",
     "onetbb-devel",
     "pipewire-devel",
+    "qqc2-desktop-style-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtgraphs-devel",
     "rnnoise-devel",
     "soundtouch-devel",
     "speexdsp-devel",
     "turnstile",
+    "webrtc-audio-processing-devel",
     "zita-convolver-devel",
 ]
-# most plugins are from here and it can crash without them (and at least prints
-# 9 million warnings), so just always pull it
-depends = ["dinit-dbus", "lsp-plugins-lv2"]
+depends = [
+    # most plugins are from here and it can crash without them (and at least prints
+    # 9 million warnings), so just always pull it
+    "lsp-plugins-lv2",
+    "qt6-qtgraphs",
+]
 pkgdesc = "PipeWire audio plugins"
 license = "GPL-3.0-or-later"
 url = "https://github.com/wwmm/easyeffects"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "d86877b12648a24b3b21a56d16d5680ee2585d575878ecdcea1b9bd9bb428191"
+sha256 = "b6374830282c34b90ace1a2c684d3675e5e00f8f73dffc4778105232f5482e13"
 tool_flags = {"CXXFLAGS": ["-fexperimental-library"]}
 
 
