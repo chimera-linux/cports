427a15ddcca1046a48ef041d63ae38270bda080f
q66 <q66@chimera-linux.org>
2026-07-10 14:52:41 +0200

    user/nilfs-utils: update to 2.3.1
    Closes https://github.com/chimera-linux/cports/pull/4923

--- user/nilfs-utils/template.py
+++ user/nilfs-utils/template.py
@@ -1,5 +1,5 @@
 pkgname = "nilfs-utils"
-pkgver = "2.2.11"
+pkgver = "2.3.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--without-selinux"]
@@ -9,6 +9,7 @@ hostmakedepends = [
     "slibtool",
 ]
 makedepends = [
+    "gnu-getopt",
     "linux-headers",
     "util-linux-blkid-devel",
     "util-linux-mount-devel",
@@ -18,7 +19,11 @@ pkgdesc = "Userspace utilities for the NILFS filesystem"
 license = "GPL-2.0-or-later AND LGPL-2.1-or-later"
 url = "https://nilfs.sourceforge.io/en/index.html"
 source = f"https://github.com/nilfs-dev/nilfs-utils/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "5172adef1f4a66add0e4e2e733aef82c5b1fc2405473bcd335e516814b5f634f"
+sha256 = "f4488f2978b3054fd97f87d206032cbf376d98479759d2855b57bc24a8d8efb9"
+tool_flags = {
+    "CFLAGS": ["-Dgetopt=gnu_getopt"],
+    "LDFLAGS": ["-lgnu_getopt"],
+}
 options = ["etcfiles"]
 
 
