61f94e82770c535b582296e54d8de597bdca6149
Erica Z <zerica@callcc.eu>
2026-07-10 13:43:09 +0200

    user/tectonic: update to 0.16.9

--- user/tectonic/template.py
+++ user/tectonic/template.py
@@ -1,6 +1,6 @@
 pkgname = "tectonic"
-pkgver = "0.15.0"
-pkgrel = 5
+pkgver = "0.16.9"
+pkgrel = 0
 build_style = "cargo"
 make_build_args = [
     "--no-default-features",
@@ -8,7 +8,14 @@ make_build_args = [
     "geturl-curl,serialization,external-harfbuzz",
 ]
 make_install_args = make_build_args
-make_check_args = ["--features", "external-harfbuzz"]
+make_check_args = [
+    "--features",
+    "external-harfbuzz",
+    "--",
+    "--skip",
+    # internet access
+    "no_segfault_after_failed_compilation",
+]
 hostmakedepends = ["cargo-auditable", "pkgconf"]
 makedepends = [
     "curl-devel",
@@ -23,23 +30,10 @@ pkgdesc = "Modernized LaTeX engine"
 license = "MIT"
 url = "https://tectonic-typesetting.github.io/en-US"
 source = f"https://github.com/tectonic-typesetting/tectonic/archive/refs/tags/tectonic@{pkgver}.tar.gz"
-sha256 = "3c13de312c4fe39ff905ad17e64a15a3a59d33ab65dacb0a8b9482c57e6bc6aa"
+sha256 = "9861d4d4230b987d8560f1b84fe6c8a550738401be65b9425b0c7d0466178f2b"
 
 if self.profile().arch in ["loongarch64"]:
     broken = "outdated nix crate, can't update"
-
-
-def pre_prepare(self):
-    # rust 1.80 type inference regression
-    self.do(
-        "cargo",
-        "update",
-        "--package",
-        "time",
-        "--precise",
-        "0.3.36",
-        allow_network=True,
-    )
 
 
 def post_install(self):
