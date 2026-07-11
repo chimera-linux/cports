36995488723c789e1cddc50e9221ee786f776edf
flukey <flukey@vapourmail.eu>
2026-07-11 11:57:49 +0100

    main/aardvark-dns: update to 2.0.0

--- main/aardvark-dns/template.py
+++ main/aardvark-dns/template.py
@@ -1,5 +1,5 @@
 pkgname = "aardvark-dns"
-pkgver = "1.16.0"
+pkgver = "2.0.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -8,7 +8,7 @@ pkgdesc = "Authoritative DNS server for A/AAAA container records"
 license = "Apache-2.0"
 url = "https://github.com/containers/aardvark-dns"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "6c84a3371087d6af95407b0d3de26cdc1e720ae8cd983a9bdaec8883e2216959"
+sha256 = "d3f5d6b3be3c2d80e8257fb9467e34ff104f299474427979454034dca6dc88cc"
 
 
 def install(self):
