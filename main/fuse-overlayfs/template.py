d96b47f41ce6a69376d603b17d234cabf2cc7956
flukey <flukey@vapourmail.eu>
2026-07-11 11:57:49 +0100

    main/fuse-overlayfs: update to 1.17

--- main/fuse-overlayfs/template.py
+++ main/fuse-overlayfs/template.py
@@ -1,5 +1,5 @@
 pkgname = "fuse-overlayfs"
-pkgver = "1.16"
+pkgver = "1.17"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -14,5 +14,5 @@ pkgdesc = "FUSE implementation for overlayfs"
 license = "GPL-2.0-or-later"
 url = "https://github.com/containers/fuse-overlayfs"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "45968517603389ead067222d234bc8d8ed33e4b4f8ba16216bdd3e6aedcccea9"
+sha256 = "cefffecfbb001b2784f19af344f27eae07b31a4faa38d345b738af96b2bec59e"
 hardening = ["vis", "cfi"]
