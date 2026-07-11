fdb801ebb76adad6592d92287722edcce05d286b
flukey <flukey@vapourmail.eu>
2026-07-11 11:57:49 +0100

    main/skopeo: update to 1.23.0

--- main/skopeo/template.py
+++ main/skopeo/template.py
@@ -1,6 +1,6 @@
 pkgname = "skopeo"
-pkgver = "1.20.0"
-pkgrel = 5
+pkgver = "1.23.0"
+pkgrel = 0
 build_style = "go"
 # for compatibility with Makefile targets
 make_dir = "bin"
@@ -21,9 +21,9 @@ depends = ["containers-common"]
 go_build_tags = ["libsqlite3"]
 pkgdesc = "OCI image and repo manipulation tool"
 license = "Apache-2.0"
-url = "https://github.com/containers/skopeo"
+url = "https://github.com/podman-container-tools/skopeo"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0c19fe51b2cd8d1bd5e38c03b97421e318fc08153bdf5ef2f816a29889eacdef"
+sha256 = "de96bfc2bb523c852af675ffdadd934484812ce190aa8620e1d5fd6c51442e25"
 
 
 def post_build(self):
