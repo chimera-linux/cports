31f3581cc6c730ddc1d589955b8a10305fa71f7e
flukey <flukey@vapourmail.eu>
2026-07-11 11:57:49 +0100

    main/containers-common: update to 0.68.1

--- main/containers-common/template.py
+++ main/containers-common/template.py
@@ -1,33 +1,23 @@
 pkgname = "containers-common"
-pkgver = "0.64.1"
+pkgver = "0.68.1"
 pkgrel = 0
 make_build_args = ["-C", "docs"]
 make_install_args = [*make_build_args]
 hostmakedepends = ["go-md2man"]
 pkgdesc = "Shared docs and configs for Containers"
 license = "Apache-2.0"
-url = "https://github.com/containers/common"
-_base_url = url.removesuffix("/common")
-_common_ver = pkgver
-_storage_ver = "1.59.1"
-_image_ver = "5.36.1"
+url = "https://github.com/containers/container-libs"
 _shortnames_ver = "2025.03.19"
 source = [
-    f"{_base_url}/common/archive/v{_common_ver}.tar.gz",
-    f"{_base_url}/storage/archive/v{_storage_ver}.tar.gz",
-    f"{_base_url}/image/archive/v{_image_ver}.tar.gz",
-    f"{_base_url}/shortnames/archive/v{_shortnames_ver}.tar.gz",
+    f"{url}/archive/refs/tags/common/v{pkgver}.tar.gz",
+    f"{url.removesuffix('container-libs')}/shortnames/archive/refs/tags/v{_shortnames_ver}.tar.gz",
 ]
 source_paths = [
-    "common",
-    "storage",
-    "image",
+    ".",
     "shortnames",
 ]
 sha256 = [
-    "414def665a172a4d79366dc594e5313d43d672ba19009aa2a3dd78272e277506",
-    "2d4b0e5f66c83c776c6dab81fd52bee2aac72832ef3af4e6a1e081aaf1f87f30",
-    "8ea547fe0f2dcfaa458f9e2d584eaacd504572bdb33ce0e98e70fffbc851c519",
+    "6cfc76c4db34c30e560ab1bf5f297964fc755e8448a71f4b1ed8a68026431788",
     "1a2db4dca75b04d54623087972888459363392b9c4f64b6d0ac2f4b78cba3e45",
 ]
 # no tests
