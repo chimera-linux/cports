28dc953ba3f3a0f84d710cb7aae95943bb2914af
Wesley Moore <wes@wezm.net>
2026-07-13 19:47:02 +1000

    user/sequoia-sq: update to 1.4.0

--- user/sequoia-sq/template.py
+++ user/sequoia-sq/template.py
@@ -1,11 +1,15 @@
 pkgname = "sequoia-sq"
-pkgver = "1.3.1"
+pkgver = "1.4.0"
 pkgrel = 0
 build_style = "cargo"
 make_build_env = {"ASSET_OUT_DIR": "assets"}
+# Tries to rename a file to /dev/null, passes outside bldroot
+make_check_args = [
+    "--",
+    "--skip=integration::sq_key_generate::sq_key_generate_dev_null",
+]
 hostmakedepends = ["cargo-auditable", "pkgconf", "capnproto"]
 makedepends = [
-    "bzip2-devel",
     "capnproto-devel",
     "nettle-devel",
     "openssl3-devel",
@@ -16,7 +20,7 @@ pkgdesc = "Command-line frontend for Sequoia, a new OpenPGP implementation"
 license = "LGPL-2.0-or-later"
 url = "https://sequoia-pgp.org/projects#sq"
 source = f"https://gitlab.com/sequoia-pgp/sequoia-sq/-/archive/v{pkgver}/sequoia-sq-v{pkgver}.tar.gz"
-sha256 = "9f112096f413e195ec737c81abb5649604f16e1f6dbe64a8accc5bb3ad39e239"
+sha256 = "c856bfb0f0c94a1b8f4b72a04a6eff1e1d3d24c377cb0b1e495688e9aad8467a"
 options = ["!cross"]
 
 
