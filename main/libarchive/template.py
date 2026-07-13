5bbe4f5796048e4e108a4976bae9b1fdea4a5f88
Wesley Moore <wes@wezm.net>
2026-07-13 20:11:36 +1000

    main/libarchive: update to 3.8.8

--- main/libarchive/template.py
+++ main/libarchive/template.py
@@ -1,5 +1,5 @@
 pkgname = "libarchive"
-pkgver = "3.8.7"
+pkgver = "3.8.8"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
@@ -25,7 +25,7 @@ pkgdesc = "Library to read/write several different streaming archive formats"
 license = "BSD-2-Clause"
 url = "https://www.libarchive.org"
 source = f"https://github.com/libarchive/libarchive/releases/download/v{pkgver}/libarchive-{pkgver}.tar.gz"
-sha256 = "4b787cca6697a95c7725e45293c973c208cbdc71ae2279f30ef09f52472b9166"
+sha256 = "038918ea315cdd446cc63acfe880d6011832bbe1711c887de5de5441b306c190"
 # encoding failures on musl; harmless
 options = ["bootstrap", "!check"]
 
