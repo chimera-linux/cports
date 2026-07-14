9050b9184b22c9ef7c2b24bcbb7b344b72a8ba7e
Erica Z <zerica@callcc.eu>
2026-07-14 10:02:49 +0200

    main/webrtc-audio-processing: update to 2.1

--- main/webrtc-audio-processing/template.py
+++ main/webrtc-audio-processing/template.py
@@ -1,6 +1,6 @@
 pkgname = "webrtc-audio-processing"
-pkgver = "1.3"
-pkgrel = 3
+pkgver = "2.1"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dcpp_std=c++17"]
 hostmakedepends = ["meson", "pkgconf", "cmake"]
@@ -9,7 +9,7 @@ pkgdesc = "Packaging-friendly copy of WebRTC AudioProcessing"
 license = "BSD-3-Clause"
 url = "https://freedesktop.org/software/pulseaudio/webrtc-audio-processing"
 source = f"$(FREEDESKTOP_SITE)/pulseaudio/webrtc-audio-processing/webrtc-audio-processing-{pkgver}.tar.gz"
-sha256 = "95552fc17faa0202133707bbb3727e8c2cf64d4266fe31bfdb2298d769c1db75"
+sha256 = "35e86b986d02ea15f3d04741a1a5a735ba399bc0fac0ee089c39480e35fc3253"
 
 if self.profile().endian == "big":
     broken = "does not support BE"
