fabd73718b3a14dfb4b36a7d1fe120236e0ae9b7
Wesley Moore <wes@wezm.net>
2026-07-17 21:30:19 +1000

    user/just: update to 1.58.0

--- user/just/template.py
+++ user/just/template.py
@@ -1,5 +1,5 @@
 pkgname = "just"
-pkgver = "1.55.1"
+pkgver = "1.58.0"
 pkgrel = 0
 build_style = "cargo"
 # skip tests that fail when run outside of git repo
@@ -12,7 +12,7 @@ pkgdesc = "Save and run commands from justfile"
 license = "CC0-1.0"
 url = "https://github.com/casey/just"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "40a2d3725480523ffebb762669cafe2b0135a00383946eec3d47adf5e9be6345"
+sha256 = "c8a36e6e9397f2fdfcb0cc246fcdb790b52a784f3c8cabc0d8baeb031852a148"
 # generates completions and man page with host binary
 options = ["!cross"]
 
