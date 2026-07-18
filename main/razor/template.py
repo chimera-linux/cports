fe5d148d55d9bd5d1cb83d5079ab1101b9df6f23
Duncan Bellamy <dunk@denkimushi.com>
2026-07-18 21:09:53 +0000

    main/razor: update to 2.88

--- main/razor/template.py
+++ main/razor/template.py
@@ -1,14 +1,15 @@
 pkgname = "razor"
-pkgver = "2.86"
-pkgrel = 2
+pkgver = "2.88"
+pkgrel = 0
 build_style = "perl_module"
 hostmakedepends = [
     "perl",
 ]
 makedepends = ["perl"]
 depends = ["perl", "perl-uri"]
+checkdepends = ["perl-uri"]
 pkgdesc = "Distributed, collaborative spam detection and filtering network"
 license = "Artistic-2.0"
 url = "https://sourceforge.net/projects/razor"
 source = f"https://cpan.metacpan.org/authors/id/T/TO/TODDR/Razor2-Client-Agent-{pkgver}.tar.gz"
-sha256 = "5e062e02ebb65e24b708e7eefa5300c43d6f657bf20d08fec4ca8a0a3b94845f"
+sha256 = "1586f1ef8759d6d52de9b1cd43056c3866161f8fd0db0048b25d46473658fdaa"
