4b940b975f46108f1dd20ebb3677da8da126b483
breakgimme <adam@plock.com>
2026-07-20 02:07:05 +0200

    user/b43-fwcutter: update to 020

--- user/b43-fwcutter/template.py
+++ user/b43-fwcutter/template.py
@@ -1,12 +1,12 @@
 pkgname = "b43-fwcutter"
-pkgver = "019"
+pkgver = "020"
 pkgrel = 0
 build_style = "makefile"
 pkgdesc = "Tool to extract firmware for Broadcom 43xx wireless chips"
 license = "BSD-2-Clause"
 url = "https://wireless.docs.kernel.org/en/latest/en/users/drivers/b43.html"
-source = f"https://bues.ch/b43/fwcutter/b43-fwcutter-{pkgver}.tar.bz2"
-sha256 = "d6ea85310df6ae08e7f7e46d8b975e17fc867145ee249307413cfbe15d7121ce"
+source = f"https://bues.ch/b43/fwcutter/b43-fwcutter-{pkgver}.tar.xz"
+sha256 = "bae58321c0926827b99afd4fddaebbc934c781d8e010fe62e1ddc4af83046214"
 # no tests
 options = ["!check"]
 
