a122f0d23dadd529c9ce4c56a6d8bb3cdbd7b06d
q66 <q66@chimera-linux.org>
2026-07-20 16:29:33 +0200

    main/dracut-install: update to 111
    Closes https://github.com/chimera-linux/cports/pull/5818

--- main/dracut-install/template.py
+++ main/dracut-install/template.py
@@ -1,5 +1,5 @@
 pkgname = "dracut-install"
-pkgver = "107"
+pkgver = "111"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-dracut-cpio"]
@@ -11,9 +11,9 @@ makedepends = ["chimerautils-devel", "kmod-devel"]
 checkdepends = ["asciidoc"]
 pkgdesc = "Dracut-install command from dracut"
 license = "GPL-2.0-or-later"
-url = "https://github.com/dracut-ng/dracut-ng"
+url = "https://github.com/dracut-ng/dracut"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "b39d0d1cd35ff43aba8771c5367d8c6c59bb432c0cac62f49601f21c0d634895"
+sha256 = "ca949190692e91611ef16ea3642c0f764f63948860f3f742524310728c991493"
 hardening = ["vis", "cfi"]
 # assumes rw filesystem
 options = ["!check"]
