76c3690cf7ad5708e14f15d9b3a66ee267340121
q66 <q66@chimera-linux.org>
2026-07-20 22:04:25 +0200

    main/libnetfilter_conntrack: update to 1.1.1

--- main/libnetfilter_conntrack/template.py
+++ main/libnetfilter_conntrack/template.py
@@ -1,6 +1,6 @@
 pkgname = "libnetfilter_conntrack"
-pkgver = "1.1.0"
-pkgrel = 1
+pkgver = "1.1.1"
+pkgrel = 0
 build_style = "gnu_configure"
 # reconf breaks ppc build
 configure_gen = []
@@ -10,7 +10,7 @@ pkgdesc = "Library providing an API to the in-kernel connection tracking table"
 license = "GPL-2.0-only"
 url = "https://www.netfilter.org/projects/libnetfilter_conntrack"
 source = f"{url}/files/libnetfilter_conntrack-{pkgver}.tar.xz"
-sha256 = "67edcb4eb826c2f8dc98af08dabff68f3b3d0fe6fb7d9d0ac1ee7ecce0fe694e"
+sha256 = "769d3eaf57fa4fbdb05dd12873b6cb9a5be7844d8937e222b647381d44284820"
 
 
 @subpackage("libnetfilter_conntrack-devel")
