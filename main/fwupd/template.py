c500606da92f9ec4c7abf90462e6923e885e633e
q66 <q66@chimera-linux.org>
2026-07-20 22:36:14 +0200

    main/fwupd: update to 2.1.6
    Closes https://github.com/chimera-linux/cports/pull/5077

--- main/fwupd/template.py
+++ main/fwupd/template.py
@@ -1,12 +1,12 @@
 pkgname = "fwupd"
-pkgver = "2.0.13"
+pkgver = "2.1.6"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Ddefault_library=shared",
     "-Ddocs=disabled",
     "-Defi_binary=false",
-    "-Delogind=enabled",
+    "-Dlogind=enabled",
     "-Dintrospection=enabled",
     "-Dsupported_build=enabled",
     "-Dsystemd=disabled",
@@ -32,11 +32,7 @@ makedepends = [
     "flashrom-devel",
     "gcab-devel",
     "gnutls-devel",
-    "json-glib-devel",
-    "libarchive-devel",
-    "libcbor-devel",
     "libdrm-devel",
-    "libjcat-devel",
     "libmbim-devel",
     "libqmi-devel",
     "libusb-devel",
@@ -45,17 +41,21 @@ makedepends = [
     "modemmanager-devel",
     "pango-devel",
     "polkit-devel",
-    "protobuf-c-devel",
     "sqlite-devel",
     "tpm2-tss-devel",
 ]
-depends = ["hwdata-usb", "shared-mime-info", "udisks"]
+depends = ["hwdata-usb", "polkit", "shared-mime-info", "udisks"]
 pkgdesc = "Firmware updater"
 license = "LGPL-2.1-or-later"
 url = "https://github.com/fwupd/fwupd"
 source = f"{url}/archive/{pkgver}.tar.gz"
-sha256 = "07b09f1d37792b5194840bc3e213c3cbf21d3d652f85489b2a028be47c1c9b84"
+sha256 = "03f45f637a7178839f9f4894f86f4cb0ff4e2fc8d04a9a4da5af3157404ac369"
 options = ["etcfiles", "!cross"]
+
+if self.profile().arch == "x86_64":
+    configure_args += ["-Dhsi=enabled"]
+else:
+    configure_args += ["-Dhsi=disabled"]
 
 _have_uefi = False
 
