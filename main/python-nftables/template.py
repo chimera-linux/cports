5e9113686945285f6f64ab6adb7035ea03e3de37
q66 <q66@chimera-linux.org>
2026-07-20 22:01:41 +0200

    main/python-nftables: update to 1.1.6

--- main/python-nftables/template.py
+++ main/python-nftables/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-nftables"
-pkgver = "1.1.3"
-pkgrel = 1
+pkgver = "1.1.6"
+pkgrel = 0
 build_wrksrc = "py"
 build_style = "python_pep517"
 hostmakedepends = [
@@ -13,7 +13,7 @@ pkgdesc = "Python bindings for nftables"
 license = "GPL-2.0-only AND GPL-2.0-or-later"
 url = "https://netfilter.org/projects/nftables"
 source = f"{url}/files/nftables-{pkgver}.tar.xz"
-sha256 = "9c8a64b59c90b0825e540a9b8fcb9d2d942c636f81ba50199f068fde44f34ed8"
+sha256 = "372931bda8556b310636a2f9020adc710f9bab66f47efe0ce90bff800ac2530c"
 # no tests
 options = ["!check"]
 
