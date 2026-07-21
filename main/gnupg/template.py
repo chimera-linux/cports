b2f509a76d1a356cca6a5fd2a71839620d720461
q66 <q66@chimera-linux.org>
2026-07-21 17:52:07 +0200

    main/gnupg: build with freepg patchset
    We could just fetch the whole patched tarball from freepg too
    but then we'd have to regenerate autotools and gnupg's autotools
    really dislikes being generated without maintainer-mode. So just
    take the canonical tarball and slap the whole patchset on it.
    
    Closes https://github.com/chimera-linux/cports/pull/5199

--- main/gnupg/template.py
+++ main/gnupg/template.py
@@ -1,13 +1,15 @@
 pkgname = "gnupg"
 pkgver = "2.4.9"
-pkgrel = 0
+pkgrel = 1
+_freepg_rel = 1
 build_style = "gnu_configure"
 configure_args = [
     "--enable-all-tests",
+    "--enable-large-secmem",
 ]
 configure_gen = []
 make_check_env = {"TESTFLAGS": f"--parallel={self.conf_jobs}"}
-hostmakedepends = ["pkgconf", "libgpg-error-progs"]
+hostmakedepends = ["pkgconf", "libgpg-error-progs", "texinfo"]
 # TODO: switch to libedit once it gains missing features
 makedepends = [
     "bzip2-devel",
@@ -26,5 +28,27 @@ depends = ["pinentry"]
 pkgdesc = "GNU Privacy Guard 2.x"
 license = "GPL-3.0-or-later"
 url = "https://www.gnupg.org"
-source = f"https://gnupg.org/ftp/gcrypt/gnupg/gnupg-{pkgver}.tar.bz2"
-sha256 = "dd17ab2e9a04fd79d39d853f599cbc852062ddb9ab52a4ddeb4176fd8b302964"
+source = [
+    f"https://gnupg.org/ftp/gcrypt/gnupg/gnupg-{pkgver}.tar.bz2",
+    f"https://gitlab.com/freepg/gnupg/-/archive/source-{pkgver}-freepg-{_freepg_rel}/gnupg-source-{pkgver}-freepg-{_freepg_rel}.tar.gz",
+]
+source_paths = [".", "freepg"]
+sha256 = [
+    "dd17ab2e9a04fd79d39d853f599cbc852062ddb9ab52a4ddeb4176fd8b302964",
+    "9d971136172c1370731586563b96596770d3ad5b9f40b22aaeeb61da8ba900ff",
+]
+
+
+def post_extract(self):
+    from cbuild.util import patch
+
+    plist = sorted(
+        (
+            self.cwd
+            / f"freepg/STABLE-BRANCH-{pkgver[:-2].replace('.', '-')}-freepg"
+        ).glob("*.patch")
+    )
+    if len(plist) == 0:
+        self.error("no freepg patches found")
+
+    patch.patch(self, plist, stamp=True)
