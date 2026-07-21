51ecc01e42397916681a92b382e8f0868f560ec9
q66 <q66@chimera-linux.org>
2026-07-21 19:41:24 +0200

    user/rebos: fix loongarch build

--- user/rebos/template.py
+++ user/rebos/template.py
@@ -11,5 +11,18 @@ source = f"{url}/-/archive/v{pkgver}/rebos-v{pkgver}.tar.gz"
 sha256 = "fb5bd9c9d11712187bc0a4fcd8b438228d73951f920f08d35f23cb52dd051696"
 
 
+def pre_prepare(self):
+    # the version that is in there is busted on loongarch
+    self.do(
+        "cargo",
+        "update",
+        "--package",
+        "libc",
+        "--precise",
+        "0.2.170",
+        allow_network=True,
+    )
+
+
 def install(self):
     self.install_bin(f"target/{self.profile().triplet}/release/rebos")
