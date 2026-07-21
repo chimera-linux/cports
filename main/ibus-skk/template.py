11073a651b05d18417f2660e04310cdb186b2959
Erica Z <zerica@callcc.eu>
2026-07-21 11:39:51 +0200

    main/ibus-skk: update to 1.4.4

--- main/ibus-skk/template.py
+++ main/ibus-skk/template.py
@@ -1,6 +1,6 @@
 pkgname = "ibus-skk"
-pkgver = "1.4.3"
-pkgrel = 1
+pkgver = "1.4.4"
+pkgrel = 0
 build_style = "gnu_configure"
 # old and doesn't reconf
 configure_gen = []
@@ -18,6 +18,6 @@ pkgdesc = "Japanese SKK engine for IBus"
 license = "GPL-2.0-or-later"
 url = "https://github.com/ueno/ibus-skk"
 source = f"{url}/releases/download/ibus-skk-{pkgver}/ibus-skk-{pkgver}.tar.xz"
-sha256 = "6661bd9d0bd7f8320d6041765a4bd307ec09b02e12d4191d035b4b698d39655d"
+sha256 = "2d98069104a3907a6deaca56d08da68ab0a511617c818b392c6a85a21e012e59"
 # valol
 tool_flags = {"CFLAGS": ["-Wno-incompatible-pointer-types"]}
