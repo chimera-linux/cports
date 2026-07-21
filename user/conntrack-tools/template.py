a7799370d4743ba45db2d88657b4a4217ab38c0c
q66 <q66@chimera-linux.org>
2026-07-21 16:28:31 +0200

    user/conntrack-tools: update to 1.4.9

--- user/conntrack-tools/template.py
+++ user/conntrack-tools/template.py
@@ -1,5 +1,5 @@
 pkgname = "conntrack-tools"
-pkgver = "1.4.8"
+pkgver = "1.4.9"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -22,7 +22,7 @@ pkgdesc = "Connection tracking userspace tools"
 license = "GPL-2.0-only"
 url = "https://www.netfilter.org/projects/conntrack-tools"
 source = f"{url}/files/conntrack-tools-{pkgver}.tar.xz"
-sha256 = "067677f4c5f6564819e78ed3a9d4a8980935ea9273f3abb22a420ea30ab5ded6"
+sha256 = "c15afe488a8d408c9d6d61e97dbd19f3c591942f62c13df6453a961ca4231cae"
 
 
 def post_install(self):
---
+++ user/conntrack-tools/update.py
@@ -0,0 +1,4 @@
+url = "https://git.netfilter.org/conntrack-tools/info/refs"
+pattern = r"refs/tags/conntrack-tools-([\d.]+)"
+# because netfilter is being annoying with anubis
+agent_name = "git"
