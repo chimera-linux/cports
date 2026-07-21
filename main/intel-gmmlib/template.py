b086170b2c6ac9da32d3b976b41fa7490c77b06a
Erica Z <zerica@callcc.eu>
2026-07-21 11:45:10 +0200

    main/intel-gmmlib: update to 22.10.0

--- main/intel-gmmlib/template.py
+++ main/intel-gmmlib/template.py
@@ -1,5 +1,5 @@
 pkgname = "intel-gmmlib"
-pkgver = "22.8.2"
+pkgver = "22.10.0"
 pkgrel = 0
 # aarch64 segfaults in tests
 # only needed for intel-media-driver anyway
@@ -14,7 +14,7 @@ pkgdesc = "Intel Graphics Memory Management Library"
 license = "MIT"
 url = "https://github.com/intel/gmmlib"
 source = f"{url}/archive/refs/tags/intel-gmmlib-{pkgver}.tar.gz"
-sha256 = "2e43e538a002574f45d480a24e02297c960963dc7914b7328791d9836832ff43"
+sha256 = "893e3c5a24b408ac312a0318b70f38f427f297646e323f31e79d3a06a92c1543"
 # CFI: testsuite sigill
 hardening = ["vis", "!cfi"]
 # check cross: testsuite runs as part of install(), disabling that also doesn't build it..
