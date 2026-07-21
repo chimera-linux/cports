66a9e96065232978ba657bbec901b5f3d23e1023
q66 <q66@chimera-linux.org>
2026-07-21 18:34:38 +0200

    main/python-poetry-core: update to 2.4.1

--- main/python-poetry-core/template.py
+++ main/python-poetry-core/template.py
@@ -1,6 +1,6 @@
 pkgname = "python-poetry-core"
-pkgver = "2.1.3"
-pkgrel = 1
+pkgver = "2.4.1"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -23,7 +23,7 @@ pkgdesc = "PEP517 build backend for Poetry"
 license = "MIT"
 url = "https://github.com/python-poetry/poetry-core"
 source = f"$(PYPI_SITE)/p/poetry_core/poetry_core-{pkgver}.tar.gz"
-sha256 = "0522a015477ed622c89aad56a477a57813cace0c8e7ff2a2906b7ef4a2e296a4"
+sha256 = "89dceb6c10e9c6d8650a16183400e3c9ff9ddee13b0a81023b5575334a2b3744"
 
 
 def post_install(self):
