dbd2a402b64d057a09bfeeb4889a8ee69e2f8dfa
Erica Z <zerica@callcc.eu>
2026-07-21 11:38:57 +0200

    main/fcitx5-skk: update to 5.1.10

--- main/fcitx5-skk/template.py
+++ main/fcitx5-skk/template.py
@@ -1,5 +1,5 @@
 pkgname = "fcitx5-skk"
-pkgver = "5.1.7"
+pkgver = "5.1.10"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
@@ -22,7 +22,7 @@ url = "https://fcitx-im.org"
 source = (
     f"https://github.com/fcitx/fcitx5-skk/archive/refs/tags/{pkgver}.tar.gz"
 )
-sha256 = "2794e3fbe072a04742407b7d637b59407915c3bb820f74a006019d2e44b59161"
+sha256 = "ac8a2c75284c6d7ba81145df43bf531c08af4616d45703490a2e8e7bfbab731d"
 hardening = ["vis", "cfi"]
 # fails
 options = ["!cross"]
