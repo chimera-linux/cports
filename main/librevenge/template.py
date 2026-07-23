aff428d89f2d4ffd6a573044f6b4a1d97815598b
flukey <flukey@vapourmail.eu>
2026-07-23 21:09:06 +0100

    main/librevenge: update to 0.0.6

--- main/librevenge/template.py
+++ main/librevenge/template.py
@@ -1,6 +1,6 @@
 pkgname = "librevenge"
-pkgver = "0.0.5"
-pkgrel = 10
+pkgver = "0.0.6"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--disable-werror", "--disable-static"]
 make_dir = "."
@@ -10,7 +10,7 @@ pkgdesc = "Library for reverse engineered format filters"
 license = "MPL-2.0"
 url = "https://sourceforge.net/p/libwpd/librevenge"
 source = f"$(SOURCEFORGE_SITE)/libwpd/librevenge-{pkgver}.tar.xz"
-sha256 = "106d0c44bb6408b1348b9e0465666fa83b816177665a22cd017e886c1aaeeb34"
+sha256 = "19eacf5ce55d7fe6a990a45142589cdf7da0c7b68701797f133482cb44f189fa"
 
 
 @subpackage("librevenge-devel")
