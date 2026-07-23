08eba80b49c2d475e93f822b14a673b8e14c1145
flukey <flukey@vapourmail.eu>
2026-07-23 17:08:39 +0100

    main/libxfont2: update to 2.0.8

--- main/libxfont2/template.py
+++ main/libxfont2/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxfont2"
-pkgver = "2.0.7"
+pkgver = "2.0.8"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -18,7 +18,7 @@ pkgdesc = "X font 2 library"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXfont2-{pkgver}.tar.xz"
-sha256 = "8b7b82fdeba48769b69433e8e3fbb984a5f6bf368b0d5f47abeec49de3e58efb"
+sha256 = "f556c0e1093a4e6911cc90bc4b106d201902ee187fd74af206ff162f7e6a24d5"
 # FIXME int (e.g. xorg fails check)
 hardening = ["!int"]
 
