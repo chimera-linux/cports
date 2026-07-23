f7a41c1cc650bb0bee815001416b2c26ff53c73b
flukey <flukey@vapourmail.eu>
2026-07-23 21:11:50 +0100

    main/libusb: update to 1.0.30

--- main/libusb/template.py
+++ main/libusb/template.py
@@ -1,5 +1,5 @@
 pkgname = "libusb"
-pkgver = "1.0.29"
+pkgver = "1.0.30"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "libtool", "pkgconf"]
@@ -8,7 +8,7 @@ pkgdesc = "Library for userspace USB device access"
 license = "LGPL-2.1-or-later"
 url = "https://libusb.info"
 source = f"https://github.com/libusb/libusb/releases/download/v{pkgver}/libusb-{pkgver}.tar.bz2"
-sha256 = "5977fc950f8d1395ccea9bd48c06b3f808fd3c2c961b44b0c2e6e29fc3a70a85"
+sha256 = "fea36f34f9156400209595e300840767ab1a385ede1dc7ee893015aea9c6dbaf"
 
 
 @subpackage("libusb-devel")
