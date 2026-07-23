911d1a818a3c6df506fcc899cc02f0a02da7c065
nune <145225213+gigirassy@users.noreply.github.com>
2026-07-23 18:24:25 -0400

    user/ani-cli: update to 4.15

--- user/ani-cli/template.py
+++ user/ani-cli/template.py
@@ -1,12 +1,12 @@
 pkgname = "ani-cli"
-pkgver = "4.12"
+pkgver = "4.15"
 pkgrel = 0
-depends = ["aria2", "curl", "fzf", "mpv", "yt-dlp"]
+depends = ["aria2", "botan", "curl", "fzf", "mpv", "yt-dlp"]
 pkgdesc = "CLI to browse and watch anime"
 license = "GPL-3.0-or-later"
 url = "https://github.com/pystardust/ani-cli"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "a247878b8a95d35c5ec6f28abe0594bb3aac29dbd1861531af4a2b909b6b4bed"
+sha256 = "7ede3794978dc2eec87475e0ea96449a604a2589e940c1eab6bfbddb8529f973"
 
 
 def install(self):
