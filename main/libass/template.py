0984104178739e3bafee0494f96100617c82e6fa
flukey <flukey@vapourmail.eu>
2026-07-23 21:13:19 +0100

    main/libass: update to 0.17.5

--- main/libass/template.py
+++ main/libass/template.py
@@ -1,5 +1,5 @@
 pkgname = "libass"
-pkgver = "0.17.4"
+pkgver = "0.17.5"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -18,7 +18,7 @@ pkgdesc = "Portable library for SSA/ASS subtitle rendering"
 license = "MIT"
 url = "https://github.com/libass/libass"
 source = f"{url}/releases/download/{pkgver}/libass-{pkgver}.tar.gz"
-sha256 = "a886b3b80867f437bc55cff3280a652bfa0d37b43d2aff39ddf3c4f288b8c5a8"
+sha256 = "caab4b993dd7be6187c55623b789ed75dddefea6e65938af134637c732fe094a"
 
 
 def post_install(self):
