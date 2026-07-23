2377fb5bb95e852c502256b6b9db55ca109782fe
Duncan Bellamy <dunk@denkimushi.com>
2026-07-23 19:39:59 +0000

    user/rspamd: update to 4.1.2

--- user/rspamd/template.py
+++ user/rspamd/template.py
@@ -1,5 +1,5 @@
 pkgname = "rspamd"
-pkgver = "4.1.1"
+pkgver = "4.1.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -38,7 +38,7 @@ pkgdesc = "Spam filtering system"
 license = "Apache-2.0 AND BSD-2-Clause AND BSD-3-Clause AND BSL-1.0 AND CC0-1.0 AND LGPL-3.0-only AND MIT AND Zlib"
 url = "https://rspamd.com"
 source = f"https://github.com/rspamd/rspamd/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "9039f2d85797f9fab2346954b32f9672e21f9dca8eede0ecc6d9beefec6b9208"
+sha256 = "f0a4cf9f036cb3bbef495f4eb50fe54ee1c1599f58896a1b6a727ec8d63b52ef"
 # check may be disabled
 options = ["etcfiles"]
 
