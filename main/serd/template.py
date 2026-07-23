e24eef3f2d954f21d532e14f10ab220c0ff4aac7
flukey <flukey@vapourmail.eu>
2026-07-23 21:05:03 +0100

    main/serd: update to 0.32.10

--- main/serd/template.py
+++ main/serd/template.py
@@ -1,5 +1,5 @@
 pkgname = "serd"
-pkgver = "0.32.4"
+pkgver = "0.32.10"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -7,7 +7,7 @@ pkgdesc = "C library for RDF syntax"
 license = "ISC"
 url = "https://drobilla.net/software/serd.html"
 source = f"https://download.drobilla.net/serd-{pkgver}.tar.xz"
-sha256 = "cbefb569e8db686be8c69cb3866a9538c7cb055e8f24217dd6a4471effa7d349"
+sha256 = "b0e93b49e52f01a049475b7886ef140407115a32d3b1e5dc5f95141c88275d1c"
 hardening = ["vis", "!cfi"]
 
 
