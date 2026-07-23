0d609a459bb64530f1077ec8b0c951b89a8dc3c5
flukey <flukey@vapourmail.eu>
2026-07-23 17:09:49 +0100

    main/tree-sitter: update to 0.26.11

--- main/tree-sitter/template.py
+++ main/tree-sitter/template.py
@@ -1,6 +1,6 @@
 pkgname = "tree-sitter"
 # match to tree-sitter-cli
-pkgver = "0.26.10"
+pkgver = "0.26.11"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["pkgconf"]
@@ -8,7 +8,7 @@ pkgdesc = "Incremental parsing library for language grammars"
 license = "MIT"
 url = "https://tree-sitter.github.io/tree-sitter"
 source = f"https://github.com/tree-sitter/tree-sitter/archive/v{pkgver}.tar.gz"
-sha256 = "450cb85fd1af34111eb162e931e0e9e4d4dbf23fc09b9cb56f6299a1a80483b6"
+sha256 = "1bab01ed21464f3272665b9c60e39ee79f68da1333e80b23f2c9356569d06971"
 # check requires cargo/fixture stuff (from remote repositories)
 options = ["!check"]
 
