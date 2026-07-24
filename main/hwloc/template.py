2a5bf6baffa240ff6b15a9498e3926c1b1c28bf8
q66 <q66@chimera-linux.org>
2026-07-24 18:47:40 +0200

    main/hwloc: split progs
    Closes https://github.com/chimera-linux/cports/pull/5918

--- main/hwloc/template.py
+++ main/hwloc/template.py
@@ -1,6 +1,6 @@
 pkgname = "hwloc"
 pkgver = "2.12.1"
-pkgrel = 0
+pkgrel = 1
 build_style = "gnu_configure"
 hostmakedepends = [
     "automake",
@@ -19,12 +19,21 @@ url = "https://www.open-mpi.org/projects/hwloc"
 source = f"https://www.open-mpi.org/software/hwloc/v{'.'.join(pkgver.split('.')[0:2])}/downloads/hwloc-{pkgver}.tar.bz2"
 sha256 = "38a90328bb86259f9bb2fe1dc57fd841e111d1e6358012bef23dfd95d21dc66b"
 # can't run them in bwrap with no sysfs
-# lintcomp: eager preload under different name
-options = ["!check", "!lintcomp"]
+options = ["!check"]
 
 
 def post_install(self):
     self.install_license("COPYING")
+
+
+@subpackage("hwloc-progs")
+def _(self):
+    # lintcomp: eager preload under different name
+    self.options = ["!lintcomp"]
+
+    return self.default_progs(
+        extra=["usr/share/applications", "usr/share/hwloc"]
+    )
 
 
 @subpackage("hwloc-devel")
