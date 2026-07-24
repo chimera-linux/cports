70ce9601f0816e7362ca048b19670933842efaa2
Erica Z <zerica@callcc.eu>
2026-07-24 12:12:50 +0200

    user/sfml: update to 3.1.0

--- user/sfml/template.py
+++ user/sfml/template.py
@@ -1,5 +1,5 @@
 pkgname = "sfml"
-pkgver = "3.0.2"
+pkgver = "3.1.0"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -16,18 +16,31 @@ makedepends = [
     "flac-devel",
     "freetype-devel",
     "libogg-devel",
+    "libssh2-devel",
     "libvorbis-devel",
     "libx11-devel",
     "libxcursor-devel",
     "libxi-devel",
     "libxrandr-devel",
+    "mbedtls-devel",
     "mesa-devel",
 ]
 pkgdesc = "C++ multimedia Library"
 license = "Zlib"
 url = "https://www.sfml-dev.org"
-source = f"https://github.com/SFML/SFML/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "0034e05f95509e5d3fb81b1625713e06da7b068f210288ce3fd67106f8f46995"
+_sheenbidi_ver = "3.0.0"
+source = [
+    f"https://github.com/SFML/SFML/archive/refs/tags/{pkgver}.tar.gz",
+    f"https://github.com/Tehreer/SheenBidi/archive/refs/tags/v{_sheenbidi_ver}.tar.gz",
+]
+source_paths = [
+    ".",
+    "build/_deps/sheenbidi-src",
+]
+sha256 = [
+    "91209a112c2bd0bc6f4ce0d5f3e413cfb48b57c0de59f5507dc81f71b1ad7a5c",
+    "86c56014034739ba39a24c23eb00323b0bf6f737354f665786015fca842af786",
+]
 
 
 @subpackage("sfml-devel")
