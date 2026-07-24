fee92a42d0c051533a63334bec57e5eb71259956
flukey <flukey@vapourmail.eu>
2026-07-24 13:23:30 +0100

    main/c-ares: update to 1.34.8
    Fixes: CVE-2026-33630, GHSA-pjmc-gx33-gc76, GHSA-jv8r-gqr9-68wj
    
    Many bugfixes.
    
    https://github.com/c-ares/c-ares/releases/tag/v1.34.7
    https://github.com/c-ares/c-ares/releases/tag/v1.34.8

--- main/c-ares/template.py
+++ main/c-ares/template.py
@@ -1,5 +1,5 @@
 pkgname = "c-ares"
-pkgver = "1.34.6"
+pkgver = "1.34.8"
 pkgrel = 0
 build_style = "gnu_configure"
 # circular gtest
@@ -9,7 +9,7 @@ pkgdesc = "C library for asynchronous DNS requests"
 license = "MIT"
 url = "https://c-ares.haxx.se"
 source = f"https://github.com/c-ares/c-ares/releases/download/v{pkgver}/c-ares-{pkgver}.tar.gz"
-sha256 = "912dd7cc3b3e8a79c52fd7fb9c0f4ecf0aaa73e45efda880266a2d6e26b84ef5"
+sha256 = "c222b6d681096f9444d2c4863d2c1174019e27cacca0a4a5c114d36dd7d7bf78"
 hardening = ["vis", "!cfi"]
 # does not like the sandbox
 options = ["!check"]
