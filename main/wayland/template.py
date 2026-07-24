31041a44c273b95b6458cc2fb0d3b7e74a58a88e
Erica Z <zerica@callcc.eu>
2026-07-24 11:20:16 +0200

    main/wayland: update to 1.26.0

--- main/wayland/template.py
+++ main/wayland/template.py
@@ -1,5 +1,5 @@
 pkgname = "wayland"
-pkgver = "1.25.0"
+pkgver = "1.26.0"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddocumentation=false"]
@@ -16,7 +16,7 @@ url = "https://wayland.freedesktop.org"
 source = (
     f"https://gitlab.freedesktop.org/wayland/wayland/-/archive/{pkgver}.tar.gz"
 )
-sha256 = "cb5a85adac2d0009214949bce3b87088ccec48c58109300bb6273182a1201198"
+sha256 = "5d92753c046c6dbd528a6f47edbab599a017d415aed8088875d8c36f39189b60"
 
 if self.profile().cross:
     hostmakedepends += ["wayland-progs"]
