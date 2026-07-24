48c2d52bf019e16b541ae4b9c4a0617b8ee43466
Erica Z <zerica@callcc.eu>
2026-07-24 18:38:15 +0200

    main/hwloc: update to 2.14.0

--- main/hwloc/template.py
+++ main/hwloc/template.py
@@ -1,5 +1,5 @@
 pkgname = "hwloc"
-pkgver = "2.12.1"
+pkgver = "2.14.0"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -17,7 +17,7 @@ pkgdesc = "Hardware resource locality library"
 license = "BSD-3-Clause"
 url = "https://www.open-mpi.org/projects/hwloc"
 source = f"https://www.open-mpi.org/software/hwloc/v{'.'.join(pkgver.split('.')[0:2])}/downloads/hwloc-{pkgver}.tar.bz2"
-sha256 = "38a90328bb86259f9bb2fe1dc57fd841e111d1e6358012bef23dfd95d21dc66b"
+sha256 = "966b9bb3e9f29f8d65ce8d106779e457f40e246a645e584b100772a42f9ae94b"
 # can't run them in bwrap with no sysfs
 # lintcomp: eager preload under different name
 options = ["!check", "!lintcomp"]
