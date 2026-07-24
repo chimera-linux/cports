23807c3cdffb9189f3dcdf1c2aa99010ac9f4c4c
nune <145225213+gigirassy@users.noreply.github.com>
2026-07-24 17:28:07 -0400

    user/mergiraf: update to 0.18.0

--- user/mergiraf/template.py
+++ user/mergiraf/template.py
@@ -1,11 +1,11 @@
 pkgname = "mergiraf"
-pkgver = "0.17.0"
+pkgver = "0.18.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
-checkdepends = ["git"]
+checkdepends = ["git", "jj"]
 pkgdesc = "Syntax-aware git merge driver"
 license = "GPL-3.0-only"
 url = "https://mergiraf.org"
 source = f"https://codeberg.org/mergiraf/mergiraf/archive/v{pkgver}.tar.gz"
-sha256 = "88f8fa352835a8c1d5eded304993fdfba7bc515c2e71f8331f1e43c9454cd62c"
+sha256 = "28b5187a1cd201c96aee6732dda9084406ad3001ed93fcff4e9fc3b740dbe471"
