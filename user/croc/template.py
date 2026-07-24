5fa0f8e028915f225aec2712967b177d346edea5
Erica Z <zerica@callcc.eu>
2026-07-24 10:02:46 +0200

    user/croc: update to 10.6.0

--- user/croc/template.py
+++ user/croc/template.py
@@ -1,13 +1,13 @@
 pkgname = "croc"
-pkgver = "10.4.2"
-pkgrel = 2
+pkgver = "10.6.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "File transfer tool"
 license = "MIT"
 url = "https://github.com/schollz/croc"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "9ad752a5e87152c15698bac0f4157bcfa56918d49bc3947f3318e39e08be4f21"
+sha256 = "d9ee32d93e8353fd4330d71ee2683f08e22f4a58b2f3f5a73c1c9d622ffd4598"
 # check: needs network access
 options = ["!check"]
 
