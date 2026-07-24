8213e581aa986cb2e719ebfa61ca89b8ec483fcd
breakgimme <adam@plock.com>
2026-07-24 05:45:31 +0200

    user/kotofetch: update to 0.2.22

--- user/kotofetch/template.py
+++ user/kotofetch/template.py
@@ -1,5 +1,5 @@
 pkgname = "kotofetch"
-pkgver = "0.2.12"
+pkgver = "0.2.22"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -8,7 +8,7 @@ pkgdesc = "Displays Japanese quotes in the terminal"
 license = "MIT"
 url = "https://github.com/hxpe-dev/kotofetch"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "80b143e5a087b2633d34012691c1446218b7ac8145a6df0ca66aba8514f9b2c9"
+sha256 = "0e04bedde86fcdd05b41e15211aa8459d24d347b3e45cf030383f5b650c01bf4"
 # no tests
 options = ["!check"]
 
