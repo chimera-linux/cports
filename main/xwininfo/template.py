b6dd96b8a7d80845715d70bd12b0ec60c107c55f
breakgimme <adam@plock.com>
2026-07-24 06:32:04 +0200

    main/xwininfo: update to 1.1.7

--- main/xwininfo/template.py
+++ main/xwininfo/template.py
@@ -1,6 +1,6 @@
 pkgname = "xwininfo"
-pkgver = "1.1.6"
-pkgrel = 1
+pkgver = "1.1.7"
+pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake", "gettext-devel", "pkgconf", "xorg-util-macros"]
 makedepends = ["libx11-devel"]
@@ -8,7 +8,7 @@ pkgdesc = "Query information about X windows"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/app/xwininfo-{pkgver}.tar.gz"
-sha256 = "2d52151de9d2808343c715c480e7d37f88958c8b7fcd090178b097436d987c2b"
+sha256 = "5484f0059a8ffab6f9493cc9f508735ebb4f0ae801fe759dd270ccb4b7f11d29"
 hardening = ["vis", "cfi"]
 
 
