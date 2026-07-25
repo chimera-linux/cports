8ddf94470f74a15662f84dd29d91f198ced97a95
Erica Z <zerica@callcc.eu>
2026-07-25 07:33:07 +0200

    user/python-urwid: update to 4.0.7

--- user/python-urwid/template.py
+++ user/python-urwid/template.py
@@ -1,20 +1,7 @@
 pkgname = "python-urwid"
-pkgver = "3.0.3"
-pkgrel = 1
+pkgver = "4.0.7"
+pkgrel = 0
 build_style = "python_pep517"
-make_check_args = [
-    # wrong os silly
-    "--ignore=urwid/display/_win32.py",
-    "--ignore=urwid/display/_win32_raw_display.py",
-    # missing checkdep: python-tornado
-    "--ignore=urwid/event_loop/tornado_loop.py",
-    # missing checkdep: python-zmq
-    "--ignore=urwid/event_loop/zmq_loop.py",
-    # TypeError: Can't instantiate abstract class Screen without an
-    # implementation for abstract methods '_read_raw_input', '_start',
-    # '_stop', 'hook_event_loop', 'unhook_event_loop'
-    "--ignore=urwid/display/_raw_display_base.py",
-]
 hostmakedepends = [
     "python-build",
     "python-installer",
@@ -34,4 +21,4 @@ pkgdesc = "Console UI library"
 license = "LGPL-2.1-or-later"
 url = "https://urwid.org"
 source = f"$(PYPI_SITE)/u/urwid/urwid-{pkgver}.tar.gz"
-sha256 = "300804dd568cda5aa1c5b204227bd0cfe7a62cef2d00987c5eb2e4e64294ed9b"
+sha256 = "c31aff787741428028b20bac3b0774fbe4f10de00ae00fe07722244d09462248"
