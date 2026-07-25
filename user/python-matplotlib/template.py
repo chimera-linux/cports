9189f7f62371c3575bd427e75f05e8964053c1f5
Erica Z <zerica@callcc.eu>
2026-07-25 07:25:55 +0200

    user/python-matplotlib: update to 3.11.1

--- user/python-matplotlib/template.py
+++ user/python-matplotlib/template.py
@@ -1,9 +1,10 @@
 pkgname = "python-matplotlib"
-pkgver = "3.10.7"
-pkgrel = 1
+pkgver = "3.11.1"
+pkgrel = 0
 build_style = "python_pep517"
 make_build_args = [
     "-Csetup-args=-Dsystem-freetype=true",
+    "-Csetup-args=-Dsystem-libraqm=true",
     "-Csetup-args=-Dsystem-qhull=true",
 ]
 hostmakedepends = [
@@ -18,6 +19,7 @@ makedepends = [
     "python-devel",
     "python-pybind11-devel",
     "qhull-devel",
+    "raqm-devel",
 ]
 depends = [
     "python-contourpy",
@@ -35,7 +37,7 @@ pkgdesc = "Python plotting library"
 license = "PSF-2.0"
 url = "https://matplotlib.org"
 source = f"https://github.com/matplotlib/matplotlib/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "0678f04e55c839c543a3803a7a13ab427f488ff396d85ffbad7d427f6fdcbbc3"
+sha256 = "b8a1eae79e86021624b43484bd07cb318ee83aa5f4ed4c3044dcfdcea63b07fe"
 # check: ImportError: cannot import name '_c_internal_utils' from 'matplotlib'
 options = ["!check"]
 
