b1678a5f4883b97a5da0d419b16674bc6aac54f9
Erica Z <zerica@callcc.eu>
2026-07-25 07:27:25 +0200

    user/python-wcwidth: update to 0.8.2

--- user/python-wcwidth/template.py
+++ user/python-wcwidth/template.py
@@ -1,11 +1,11 @@
 pkgname = "python-wcwidth"
-pkgver = "0.2.14"
-pkgrel = 1
+pkgver = "0.8.2"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
+    "python-hatchling",
     "python-installer",
-    "python-setuptools",
 ]
 depends = ["python"]
 checkdepends = ["python-pytest"]
@@ -13,7 +13,7 @@ pkgdesc = "Measure display width of unicode strings in a terminal"
 license = "MIT"
 url = "https://github.com/jquast/wcwidth"
 source = f"$(PYPI_SITE)/w/wcwidth/wcwidth-{pkgver}.tar.gz"
-sha256 = "4d478375d31bc5395a3c55c40ccdf3354688364cd61c4f6adacaa9215d0b3605"
+sha256 = "91fbef97204b96a3d4d421609b80340b760cf33e26da123ff243d76b1fda8dda"
 
 
 def post_install(self):
