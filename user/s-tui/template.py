b74575e485d8add5af491e35c63db348081d5879
Erica Z <zerica@callcc.eu>
2026-07-25 07:20:56 +0200

    user/s-tui: update to 1.4.0

--- user/s-tui/template.py
+++ user/s-tui/template.py
@@ -1,6 +1,6 @@
 pkgname = "s-tui"
-pkgver = "1.2.0"
-pkgrel = 1
+pkgver = "1.4.0"
+pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
     "python-build",
@@ -10,12 +10,11 @@ hostmakedepends = [
 depends = [
     "python-psutil",
     "python-urwid",
-    "stress-ng",
 ]
 pkgdesc = "CPU performance monitor"
 license = "GPL-2.0-or-later"
 url = "https://github.com/amanusk/s-tui"
 source = f"$(PYPI_SITE)/s/s-tui/s_tui-{pkgver}.tar.gz"
-sha256 = "82097f463cc8a6977bedfa574ba8b2fe2dfdcaa3322a6e16041e2392ee4a74b8"
+sha256 = "5153f1def01bf1eae62aaf37fe9562a804d09232644d1e2482c7d52e18c6653a"
 # check: no tests
 options = ["!check"]
