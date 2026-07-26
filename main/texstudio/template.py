2685f54f58fa6726b59d7cdd526710fd5cf87287
Erica Z <zerica@callcc.eu>
2026-07-26 20:10:39 +0200

    main/texstudio: update to 4.9.6

--- main/texstudio/template.py
+++ main/texstudio/template.py
@@ -1,6 +1,6 @@
 pkgname = "texstudio"
-pkgver = "4.8.7"
-pkgrel = 1
+pkgver = "4.9.6"
+pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = [
@@ -16,4 +16,4 @@ pkgdesc = "Integrated writing environment for creating LaTeX documents"
 license = "GPL-3.0-or-later"
 url = "https://www.texstudio.org"
 source = f"https://github.com/texstudio-org/texstudio/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "b8272bc6a067b7132805f8877aad9bee077e80970728cdb889edef2bb23e3b70"
+sha256 = "a57a39b63c07d02f0c67b151841f8218be9843ac6035616ae354903d94c145d2"
