86ef29a06136ef21f0af564cd23b00dc00667080
nune <145225213+gigirassy@users.noreply.github.com>
2026-07-26 16:23:34 -0400

    user/jjui: new package

---
+++ user/jjui/template.py
@@ -0,0 +1,16 @@
+pkgname = "jjui"
+pkgver = "0.10.9"
+pkgrel = 0
+build_style = "go"
+make_build_args = ["./cmd/jjui"]
+hostmakedepends = ["go"]
+depends = ["jj"]
+pkgdesc = "TUI for Jujutsu VCS framework"
+license = "MIT"
+url = "https://github.com/idursun/jjui"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "1e6f74b3e00bb652f533331a15e4e0b9d6139a0db6f3f0f1e5b348ce547f72d0"
+
+
+def post_install(self):
+    self.install_license("LICENSE")
