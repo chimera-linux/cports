673640ba02253edd7403b84b0e5c9037bfe94fb3
nune <145225213+gigirassy@users.noreply.github.com>
2026-07-27 15:56:38 -0400

    user/packemon: new package

---
+++ user/packemon/template.py
@@ -0,0 +1,18 @@
+pkgname = "packemon"
+pkgver = "1.8.26"
+pkgrel = 0
+build_style = "go"
+make_build_args = ["./cmd/packemon"]
+hostmakedepends = ["go"]
+depends = ["iproute2"]
+pkgdesc = "TUI network packet generator and monitor"
+license = "BSD-2-Clause"
+url = "https://github.com/ddddddO/packemon"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "dabfcb37057090dbe54f12a9700f86504c059f4e4d8a28eab44c5a90fe679801"
+# check: needs network
+options = ["!check"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
