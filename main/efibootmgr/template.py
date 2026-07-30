1eb72fd15432ff0384f95fa4105a06236a52043b
q66 <q66@chimera-linux.org>
2026-07-30 12:54:19 +0200

    main/efibootmgr: remove kernel hook
    This was never supported by the distro, documented anywhere, or
    even functioned properly in a lot of cases. Since I have no
    intention to do anything with it, don't just let it rot

--- main/efibootmgr/template.py
+++ main/efibootmgr/template.py
@@ -1,6 +1,6 @@
 pkgname = "efibootmgr"
 pkgver = "18"
-pkgrel = 3
+pkgrel = 4
 build_style = "makefile"
 hostmakedepends = ["pkgconf"]
 makedepends = ["efivar-devel", "popt-devel", "linux-headers"]
@@ -45,12 +45,3 @@ def install(self):
     self.install_man("src/efibootdump.8")
     self.install_bin("src/efibootmgr")
     self.install_man("src/efibootmgr.8")
-
-    # hook config file
-    self.install_file(self.files_path / "efibootmgr-hook", "etc/default")
-    # kernel hook
-    self.install_file(
-        self.files_path / "99-efibootmgr-hook.sh",
-        "usr/lib/kernel.d",
-        mode=0o755,
-    )
