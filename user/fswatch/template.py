ccfd4c1a87762441b959631b307069d3f9b8efe5
Alec Stewart <alec-stewart@pm.me>
2026-07-30 06:04:04 -0500

    user/fswatch: 1.21.0 -> 1.22.0

--- user/fswatch/template.py
+++ user/fswatch/template.py
@@ -1,14 +1,14 @@
 pkgname = "fswatch"
-pkgver = "1.21.0"
+pkgver = "1.22.0"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
-hostmakedepends = ["pkgconf", "automake", "gettext-devel", "slibtool"]
+hostmakedepends = ["automake", "pkgconf", "gettext-devel", "slibtool"]
 pkgdesc = "Cross-platform file change monitor"
 license = "GPL-3.0-or-later"
 url = "https://github.com/emcrisostomo/fswatch"
 source = f"{url}/releases/download/{pkgver}/fswatch-{pkgver}.tar.gz"
-sha256 = "881945bbe218d057c465e0cb0d8fe682df088918ee047295159616d700e67a2f"
+sha256 = "fa6e2becba0a629964b466b39c5997e72d8a6da40d82b88190aae7359065c758"
 # integration tests need a live inotify/fanotify environment
 options = ["!check"]
 
