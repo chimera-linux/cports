46737e66db071a29ab01534384924e1d6c0e9f03
Biswapriyo Nath <nathbappai@gmail.com>
2026-08-01 14:38:58 +0000

    user/android-tools: update to 37.0.0
    Closes https://github.com/chimera-linux/cports/pull/5965

--- user/android-tools/template.py
+++ user/android-tools/template.py
@@ -1,14 +1,12 @@
 pkgname = "android-tools"
-pkgver = "36.0.1"
-pkgrel = 1
+pkgver = "37.0.0"
+pkgrel = 0
 # only supports specific little-endian archs, particularly in boringssl
 archs = ["x86_64", "aarch64", "loongarch64", "ppc64le", "riscv64"]
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
-    "go",
     "ninja",
-    "perl",
     "pkgconf",
     "protobuf",
 ]
@@ -19,6 +17,7 @@ makedepends = [
     "libusb-devel",
     "linux-headers",
     "lz4-devel",
+    "musl-bsd-headers",
     "pcre2-devel",
     "protobuf-devel",
     "zstd-devel",
@@ -28,7 +27,7 @@ pkgdesc = "Android platform tools, such as adb and fastboot"
 license = "Apache-2.0 AND ISC AND GPL-2.0-only AND MIT"
 url = "https://github.com/nmeum/android-tools"
 source = f"{url}/releases/download/{pkgver}/android-tools-{pkgver}.tar.xz"
-sha256 = "38e8a84b739480141de0836bf6d581b3339ac7d53d0f7ce8c044a3368c8c2f8f"
+sha256 = "2725d09f892a3a38e534429f47a321f58ecf6a3169caa42c915fb2cb7d46be0e"
 tool_flags = {"CXXFLAGS": ["-D_LARGEFILE64_SOURCE"]}
 hardening = ["vis", "cfi"]
 
