a9c58d9b6fadb671316dbb9986f5b04ac458983d
Erica Z <zerica@callcc.eu>
2026-08-05 08:57:46 +0200

    main/varlink: update to 24.0.1

--- main/varlink/template.py
+++ main/varlink/template.py
@@ -1,5 +1,5 @@
 pkgname = "varlink"
-pkgver = "24"
+pkgver = "24.0.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -8,9 +8,9 @@ pkgdesc = "C implementation of the varlink protocol"
 license = "Apache-2.0"
 url = "https://github.com/varlink/libvarlink"
 source = (
-    f"https://github.com/varlink/libvarlink/archive/refs/tags/{pkgver}.tar.gz"
+    f"https://github.com/varlink/libvarlink/archive/refs/tags/v{pkgver}.tar.gz"
 )
-sha256 = "0586263ab8f4e8e26a2f2f54830f8f92e403326663b10e14fcf1a6c95e9eab95"
+sha256 = "ca3ecd13005309e0322bc64a26f2960e613f2a9a9cedee845865c2d042f73b3c"
 # CFI: fails in tests
 hardening = ["vis", "!cfi"]
 
