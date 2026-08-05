c5d13687d7068938f7bf8885318a2927d23853f5
flukey <flukey@vapourmail.eu>
2026-08-05 18:25:46 +0100

    main/openresolv: update to 3.17.4
    Mostly security hardening (quoting/escaping), and early boot fixes.

--- main/openresolv/template.py
+++ main/openresolv/template.py
@@ -1,6 +1,6 @@
 pkgname = "openresolv"
-pkgver = "3.16.5"
-pkgrel = 1
+pkgver = "3.17.4"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = ["--libexecdir=/usr/lib/resolvconf"]
 configure_gen = []
@@ -9,7 +9,7 @@ pkgdesc = "Management framework for resolv.conf"
 license = "BSD-2-Clause"
 url = "https://roy.marples.name/projects/openresolv"
 source = f"https://github.com/NetworkConfiguration/openresolv/releases/download/v{pkgver}/openresolv-{pkgver}.tar.xz"
-sha256 = "389cb94b57dca1e39a501c95efd448452adffbf311bb8a3bebd39f7f202ac284"
+sha256 = "901d83a15520b80e117aeca8cb6f5b70ceb205ba050a638d45cebcf304df729b"
 hardening = ["vis", "cfi"]
 # no test suite
 options = ["etcfiles", "!check"]
