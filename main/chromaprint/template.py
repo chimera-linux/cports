b243a71bcdf4e7f414f710064b6f728a5987a4cd
flukey <flukey@vapourmail.eu>
2026-08-06 10:45:46 +0100

    main/chromaprint: update to 1.6.1
    Bug and heap buffer overflow fixes.

--- main/chromaprint/template.py
+++ main/chromaprint/template.py
@@ -1,6 +1,6 @@
 pkgname = "chromaprint"
-pkgver = "1.6.0"
-pkgrel = 1
+pkgver = "1.6.1"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
@@ -12,7 +12,7 @@ pkgdesc = "Library that extracts fingerprints from any audio source"
 license = "MIT AND LGPL-2.1-only"
 url = "https://acoustid.org/chromaprint"
 source = f"https://github.com/acoustid/chromaprint/releases/download/v{pkgver}/chromaprint-{pkgver}.tar.gz"
-sha256 = "9d33482e56a1389a37a0d6742c376139fa43e3b8a63d29003222b93db2cb40da"
+sha256 = "3368805af0ee47b9df74df10b5001a44569e01df2844dab520031720dde9ad23"
 
 
 def post_install(self):
