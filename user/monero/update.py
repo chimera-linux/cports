48c09c3102c8c3c2a346afe17739b4cea32468b9
q66 <q66@chimera-linux.org>
2026-08-07 14:24:44 +0200

    user/monero: format update.py sigh

--- user/monero/update.py
+++ user/monero/update.py
@@ -1,2 +1,4 @@
-url = "https://github.com/monero-project/monero/info/refs?service=git-upload-pack"
+url = (
+    "https://github.com/monero-project/monero/info/refs?service=git-upload-pack"
+)
 pattern = r"refs/tags/v([\d.]+)(?!^)"
