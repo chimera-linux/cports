93327a933dbef683a92f4c560588594e43fb998a
breakgimme <adam@plock.com>
2026-08-07 20:09:26 +0200

    user/pavucontrol-qt: update to 2.4.0

--- user/pavucontrol-qt/template.py
+++ user/pavucontrol-qt/template.py
@@ -1,6 +1,6 @@
 pkgname = "pavucontrol-qt"
-pkgver = "2.2.0"
-pkgrel = 1
+pkgver = "2.4.0"
+pkgrel = 0
 build_style = "cmake"
 hostmakedepends = [
     "cmake",
@@ -18,4 +18,4 @@ pkgdesc = "Qt Pulseaudio mixer"
 license = "GPL-2.0-or-later"
 url = "https://github.com/lxqt/pavucontrol-qt"
 source = f"{url}/releases/download/{pkgver}/pavucontrol-qt-{pkgver}.tar.xz"
-sha256 = "f1d213426ac1033d23a8239732dc7f5fdeb393b064f11c9582e01b0c8310aee9"
+sha256 = "7f813d3029e9f1b66020cf3da143d07196dd8bce01a95bd754489287ca5b6380"
