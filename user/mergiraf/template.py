5c888b00daff7540a7c9e66f4ab2c661ab8f0903
Wesley Moore <wes@wezm.net>
2026-08-07 19:08:08 +1000

    user/mergiraf: enable tests on loongarch

--- user/mergiraf/template.py
+++ user/mergiraf/template.py
@@ -9,9 +9,3 @@ license = "GPL-3.0-only"
 url = "https://mergiraf.org"
 source = f"https://codeberg.org/mergiraf/mergiraf/archive/v{pkgver}.tar.gz"
 sha256 = "28b5187a1cd201c96aee6732dda9084406ad3001ed93fcff4e9fc3b740dbe471"
-# checks may be disabled
-options = []
-
-if self.profile().arch in ["loongarch64"]:
-    # checkdepends can't be installed
-    options += ["!check"]
