87a44e6e8ba4d632204299da21652be95f75ffa3
q66 <q66@chimera-linux.org>
2026-08-12 16:44:10 +0200

    main/linux-steamdeck: remove
    This should nowadays work fine enough with regular kernels.
    Also this is outdated and the "upstream" hasn't updated in a while.

--- main/linux-steamdeck/update.py
+++
@@ -1,8 +0,0 @@
-_vver = 24
-
-pattern = r"/tags/([\d.]+-valve\d+)\""
-pkgver = f"{self.pkgver}.{_vver}"
-
-
-def fetch_versions(self, src):
-    return map(lambda v: v.replace("-valve", "."), self.fetch_versions(src))
