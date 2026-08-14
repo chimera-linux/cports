5237f23d1c6e7a5b50abcc5f74f60d47c565e2bd
Duncan Bellamy <dunk@denkimushi.com>
2026-08-14 20:46:25 +0000

    main/ragel: update to 7.0.4

--- main/ragel/template.py
+++ main/ragel/template.py
@@ -1,13 +1,23 @@
 pkgname = "ragel"
-pkgver = "6.10"
+pkgver = "7.0.4"
 pkgrel = 0
 build_style = "gnu_configure"
-hostmakedepends = ["automake"]
+configure_args = [
+    "--with-colm=/usr",
+    "--disable-manual",
+    "--disable-static",
+]
+make_dir = "."
+hostmakedepends = ["automake", "libtool", "colm"]
+makedepends = ["colm-devel"]
+checkdepends = ["bash"]
 pkgdesc = "Finite state machine compiler"
 license = "GPL-2.0-or-later"
 url = "https://www.colm.net/open-source/ragel/index.html"
 source = f"https://www.colm.net/files/ragel/ragel-{pkgver}.tar.gz"
-sha256 = "5f156edb65d20b856d638dd9ee2dfb43285914d9aa2b6ec779dac0270cd56c3f"
-tool_flags = {"CXXFLAGS": ["-std=gnu++98"]}
-# tests need txl which is not open source http://www.txl.ca/
-options = ["!check"]
+sha256 = "84b1493efe967e85070c69e78b04dc55edc5c5718f9d6b77929762cb2abed278"
+
+
+@subpackage("ragel-devel")
+def _(self):
+    return self.default_devel()
