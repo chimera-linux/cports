cdcbde237d7a044026374c06ccd23b2346ff41fc
Ayush Agarwal <ayush@ayushnix.com>
2026-08-20 18:50:42 +0530

    user/tuigreet: update to 0.11.1
    broken on loongarch because it depends on the nix crate which pulls in
    utmp-rs and utmp-raw which do not support loongarch

--- user/tuigreet/template.py
+++ user/tuigreet/template.py
@@ -1,29 +1,32 @@
 pkgname = "tuigreet"
-pkgver = "0.9.1"
-pkgrel = 2
+pkgver = "0.11.1"
+pkgrel = 0
 build_style = "cargo"
-hostmakedepends = ["cargo-auditable"]
+hostmakedepends = ["cargo-auditable", "scdoc"]
 makedepends = ["rust-std"]
 depends = ["greetd"]
 pkgdesc = "Console greeter for greetd"
 license = "GPL-3.0-or-later"
 url = "https://github.com/apognu/tuigreet"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "14fd1fadeb84040eb31901da2b53a48aa55b0fdaccb36d96fa52ce2d2113667f"
+sha256 = "7d643ba224c40c6a63f9462a826630543071aea08e732ccd2e880bcd80d939e8"
+
+if self.profile().arch == "loongarch64":
+    broken = "nix crate issues"
 
 
-def pre_prepare(self):
-    # the version that is in there is busted on loongarch
-    self.do(
-        "cargo",
-        "update",
-        "--package",
-        "libc",
-        "--precise",
-        "0.2.170",
-        allow_network=True,
-    )
+def post_build(self):
+    with open(self.cwd / "contrib" / "man" / "tuigreet-1.scd", "rb") as i:
+        with open(self.cwd / "contrib" / "man" / "tuigreet.1", "w") as o:
+            self.do("scdoc", input=i.read(), stdout=o)
 
 
-def post_install(self):
+def install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/tuigreet")
+    self.install_man("contrib/man/tuigreet.1")
     self.install_tmpfiles(self.files_path / "tmpfiles.conf")
+    self.install_file(
+        "contrib/example.config.toml",
+        "usr/share/examples/tuigreet",
+        name="config.toml",
+    )
