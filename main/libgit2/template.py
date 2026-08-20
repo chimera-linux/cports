72948d78e297e6ae36f6e59710565d2d9018f638
q66 <q66@chimera-linux.org>
2026-08-20 20:30:56 +0200

    main/libgit2: update to 1.9.7

--- main/libgit2/template.py
+++ main/libgit2/template.py
@@ -1,5 +1,5 @@
 pkgname = "libgit2"
-pkgver = "1.9.6"
+pkgver = "1.9.7"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -27,7 +27,7 @@ pkgdesc = "Linkable library for using git"
 license = "GPL-2.0-only WITH GCC-exception-2.0"
 url = "https://libgit2.org"
 source = f"https://github.com/libgit2/libgit2/archive/v{pkgver}.tar.gz"
-sha256 = "a88a42a4ea9bdab7aa8686eead3bf7d9c6dd74529caca16ab22eaa92433d31d9"
+sha256 = "1a4fbe7589e814777ae76b64734ad80f4ecad22cd33a22682a2aaea4ae5375e7"
 
 
 def post_extract(self):
