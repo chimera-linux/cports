3693f7e06b31a098279369be52a2f1aef3db67d5
eris <nerd@users.noreply.github.com>
2026-08-22 03:24:59 -0400

    user/github-cli: update to 2.98.0

--- user/github-cli/template.py
+++ user/github-cli/template.py
@@ -1,6 +1,6 @@
 pkgname = "github-cli"
-pkgver = "2.94.0"
-pkgrel = 2
+pkgver = "2.98.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     f"-ldflags=-X github.com/cli/cli/v2/internal/build.Version=v{pkgver}",
@@ -14,7 +14,7 @@ pkgdesc = "GitHub CLI tool"
 license = "MIT"
 url = "https://cli.github.com"
 source = f"https://github.com/cli/cli/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "cf0b72ca760cc4262d5c7f57fe813dbfe33028e63026c3b6660f9b5e4954d3b7"
+sha256 = "abada9e8b550547ac93f99250f3ad4d90ad623fa245cb54cb058f78030a6a5f6"
 # cross: uses native binary to generate completions
 # check: needs network access
 options = ["!cross", "!check"]
