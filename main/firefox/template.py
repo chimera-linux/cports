b54fb21348208d3b98851266f5c3075f0bbf82f6
q66 <q66@chimera-linux.org>
2026-08-24 10:25:26 +0200

    main/firefox: update to 154.0

--- main/firefox/template.py
+++ main/firefox/template.py
@@ -1,5 +1,5 @@
 pkgname = "firefox"
-pkgver = "153.0.4"
+pkgver = "154.0"
 pkgrel = 0
 hostmakedepends = [
     "automake",
@@ -65,7 +65,7 @@ pkgdesc = "Mozilla Firefox web browser"
 license = "GPL-3.0-only AND LGPL-2.1-only AND LGPL-3.0-only AND MPL-2.0"
 url = "https://www.mozilla.org/firefox"
 source = f"$(MOZILLA_SITE)/firefox/releases/{pkgver}/source/firefox-{pkgver}.source.tar.xz"
-sha256 = "f7aa83924c66bb3b04cf139b3b00612d388a9f024c92fe7834161553a6028a48"
+sha256 = "36cec5b3688a60f78a6d20dcaee15b598f84e03c66f6587056aead6cb498b99a"
 debug_level = 1  # defatten, especially with LTO
 tool_flags = {
     "LDFLAGS": ["-Wl,-rpath=/usr/lib/firefox", "-Wl,-z,stack-size=2097152"]
