bf65e09f60a1200ff2ba6e916e22f0d5c19b1e9e
Xerydia <xerydian@gmail.com>
2026-08-25 05:55:45 +0200

    user/gitoxide: update to 0.58.0

--- user/gitoxide/template.py
+++ user/gitoxide/template.py
@@ -1,10 +1,10 @@
 pkgname = "gitoxide"
-pkgver = "0.51.0"
+pkgver = "0.58.0"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = [
     "--no-default-features",
-    "--features=max-control,gitoxide-core-blocking-client,http-client-curl",
+    "--features=max-control,gitoxide-core-blocking-client,http-client-curl,sha1",
 ]
 make_install_args = [*make_build_args]
 make_check_args = [*make_install_args]
@@ -18,7 +18,7 @@ pkgdesc = "Rust implementation of Git"
 license = "Apache-2.0 OR MIT"
 url = "https://github.com/Byron/gitoxide"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "22da356497d22eabb598233cfba61db3674e234792df1def55212ea7d2793e5d"
+sha256 = "af8210e01903e0995aa5899d8c8cd3f4f2b115ec38a924fa7bf3c7d8ba949077"
 
 
 def post_install(self):
