4d89704fd7195201d12131fae6b0bdc0c3e49f91
flukey <flukey@vapourmail.eu>
2026-08-26 22:25:15 +0100

    main/libuv: update to 1.52.1
    - thread_priority test failed for me

--- main/libuv/template.py
+++ main/libuv/template.py
@@ -1,14 +1,15 @@
 pkgname = "libuv"
-pkgver = "1.51.0"
+pkgver = "1.52.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
 hostmakedepends = ["pkgconf"]
+makedepends = ["linux-headers"]
 pkgdesc = "Multi-platform support library with focus on asynchronous I/O"
 license = "MIT"
 url = "https://libuv.org"
 source = f"https://dist.libuv.org/dist/v{pkgver}/libuv-v{pkgver}-dist.tar.gz"
-sha256 = "2ceca1a7577633cf92794db5bf5512370f6cd45a5746d6e14f8c20aeab0a547b"
+sha256 = "c5f14915e2fa7b83b6111c3bc477920559499e10d95f852707420c8725b82d6a"
 hardening = ["vis", "!cfi"]
 
 
