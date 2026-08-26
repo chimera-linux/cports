974c77a511fe80a2e1583f9e172e7f4552d7c2a5
flukey <flukey@vapourmail.eu>
2026-08-26 22:46:31 +0100

    main/nvme-cli: update to 2.16
    2.x is now in maintenance mode, v3.0 is coming soon (currently -rc2).
    
    - lc-measurement.patch dropped because of https://github.com/linux-nvme/nvme-cli/pull/2925/changes

--- main/nvme-cli/template.py
+++ main/nvme-cli/template.py
@@ -1,5 +1,5 @@
 pkgname = "nvme-cli"
-pkgver = "2.15"
+pkgver = "2.16"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -18,7 +18,7 @@ pkgdesc = "NVMe management command line interface"
 license = "GPL-2.0-or-later"
 url = "https://github.com/linux-nvme/nvme-cli"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "93282c426f22dd1ea6d172dec8af043c4e9ff80189becfbbb5378fe1ca0a74ad"
+sha256 = "989682ed7b250a2c7a8127e362ffc5d29f5c370127abe405be09c73216da2b97"
 hardening = ["vis", "cfi"]
 # require /dev nvme device
 options = ["etcfiles", "!check"]
