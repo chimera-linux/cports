bbe130b9c73064c37a18aa3e6f255cea303c965c
flukey <flukey@vapourmail.eu>
2026-08-26 22:22:04 +0100

    main/libpciaccess: update to 0.19
    - New tool scanpci disabled by default

--- main/libpciaccess/template.py
+++ main/libpciaccess/template.py
@@ -1,13 +1,14 @@
 pkgname = "libpciaccess"
-pkgver = "0.18.1"
+pkgver = "0.19"
 pkgrel = 0
 build_style = "meson"
+configure_args = ["-Dinstall-scanpci=false"]
 hostmakedepends = ["meson", "pkgconf"]
 pkgdesc = "X11 PCI access library"
 license = "MIT"
 url = "http://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libpciaccess-{pkgver}.tar.xz"
-sha256 = "4af43444b38adb5545d0ed1c2ce46d9608cc47b31c2387fc5181656765a6fa76"
+sha256 = "3c55aa86c82e54a4e3109786f0463530d53b36b6d1cfd14616454f985dd2aa43"
 
 
 def post_install(self):
