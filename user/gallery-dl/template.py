ee7c3b1d48d8470eea441b58472d9d88c6393197
Xerydia <xerydian@gmail.com>
2026-08-26 19:59:42 +0200

    user/gallery-dl: update to 1.32.9

--- user/gallery-dl/template.py
+++ user/gallery-dl/template.py
@@ -1,6 +1,6 @@
 pkgname = "gallery-dl"
-pkgver = "1.32.1"
-pkgrel = 1
+pkgver = "1.32.9"
+pkgrel = 0
 build_style = "python_pep517"
 make_check_args = [
     # needs internet
@@ -17,9 +17,9 @@ depends = ["python-requests"]
 checkdepends = ["python-pytest", *depends]
 pkgdesc = "CLI program to download image galleries/collections"
 license = "GPL-2.0-or-later"
-url = "https://github.com/mikf/gallery-dl"
+url = "https://codeberg.org/mikf/gallery-dl"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "95e69cae478cfbd407eb8451dbab9d42d65d89b861e6ee8c20888cb4b090c921"
+sha256 = "5a69dcbf5aeb3a32db8de5c70b0dd2e29e6633707ebe2bbeeeca5b632be78a75"
 
 
 def pre_build(self):
