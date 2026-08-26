10509675f7e57d4ff22bb3676328a7e10feee42d
JetpackJackson <jetpackjackson@katzenmue.de>
2026-08-26 09:32:12 -0400

    user/listenbrainz-mpd: update to 2.6.0

--- user/listenbrainz-mpd/template.py
+++ user/listenbrainz-mpd/template.py
@@ -1,5 +1,5 @@
 pkgname = "listenbrainz-mpd"
-pkgver = "2.5.1"
+pkgver = "2.6.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -8,7 +8,7 @@ pkgdesc = "ListenBrainz submission client for MPD"
 license = "AGPL-3.0-only"
 url = "https://codeberg.org/elomatreb/listenbrainz-mpd"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "1b2b586459e0b3720ca87aaf2bcaaa67c9bc28f7997d6798a2e0c7d4e51fdbbc"
+sha256 = "4d93ff68bb2f53093561d701594f1e53fa3a4409d540f5f2247e98c9ef1b276b"
 # no tests
 options = ["!check"]
 
