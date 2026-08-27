e84c412970a8c833d7bcbbc94ecdd0d0ae56abef
flukey <flukey@vapourmail.eu>
2026-08-27 17:21:10 +0100

    main/bash-completion: update to 2.18.0

--- main/bash-completion/template.py
+++ main/bash-completion/template.py
@@ -1,5 +1,5 @@
 pkgname = "bash-completion"
-pkgver = "2.17.0"
+pkgver = "2.18.0"
 pkgrel = 0
 build_style = "gnu_configure"
 make_install_args = [
@@ -12,6 +12,6 @@ pkgdesc = "Programmable completion functions for bash"
 license = "GPL-2.0-or-later"
 url = "https://github.com/scop/bash-completion"
 source = f"{url}/releases/download/{pkgver}/bash-completion-{pkgver}.tar.xz"
-sha256 = "dd9d825e496435fb3beba3ae7bea9f77e821e894667d07431d1d4c8c570b9e58"
+sha256 = "88bcf85124f77f74f2f2f8bcd16ac4382d807a827ede742a64940c7116aea33f"
 # what's the point (needs a truckload of checkdepends too)
 options = ["etcfiles", "!check"]
