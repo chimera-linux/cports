6e5bf4d4715d941215c943bcbb216dc06905959f
q66 <q66@chimera-linux.org>
2026-08-27 18:34:35 +0200

    main/zxing-cpp: update to 3.1.1

--- main/zxing-cpp/template.py
+++ main/zxing-cpp/template.py
@@ -1,12 +1,13 @@
 pkgname = "zxing-cpp"
-pkgver = "2.3.0"
-pkgrel = 3
+pkgver = "3.1.1"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
     "-DZXING_UNIT_TESTS=ON",
     "-DZXING_BLACKBOX_TESTS=OFF",
     "-DZXING_EXAMPLES=ON",
     "-DZXING_DEPENDENCIES=LOCAL",
+    "-DZXING_WRITERS=BOTH",
 ]
 # racey
 make_check_args = ["-j1"]
@@ -15,8 +16,8 @@ makedepends = ["gtest-devel", "stb"]
 pkgdesc = "Multi-format 1D/2D barcode library"
 license = "Apache-2.0"
 url = "https://github.com/nu-book/zxing-cpp"
-source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "64e4139103fdbc57752698ee15b5f0b0f7af9a0331ecbdc492047e0772c417ba"
+source = f"{url}/releases/download/v{pkgver}/zxing-cpp-{pkgver}.tar.gz"
+sha256 = "c3c02c29c0b519de7bd4e25b376e606e87f0761befd1282815642a2246613d14"
 
 
 @subpackage("zxing-cpp-devel")
