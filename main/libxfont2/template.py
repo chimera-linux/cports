c2113ae6d1ffddd6ad2de7adcbba3a57815ad899
flukey <flukey@vapourmail.eu>
2026-08-28 12:27:21 +0100

    main/libxfont2: update to 2.0.9
    Fixes CVE-2026-59679, CVE-2026-44950.

--- main/libxfont2/template.py
+++ main/libxfont2/template.py
@@ -1,5 +1,5 @@
 pkgname = "libxfont2"
-pkgver = "2.0.8"
+pkgver = "2.0.9"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -18,7 +18,7 @@ pkgdesc = "X font 2 library"
 license = "MIT"
 url = "https://xorg.freedesktop.org"
 source = f"$(XORG_SITE)/lib/libXfont2-{pkgver}.tar.xz"
-sha256 = "f556c0e1093a4e6911cc90bc4b106d201902ee187fd74af206ff162f7e6a24d5"
+sha256 = "f042a370666815e7b941e9b7019024755bd1c6c2954afbfa515af378251799e2"
 # FIXME int (e.g. xorg fails check)
 hardening = ["!int"]
 
