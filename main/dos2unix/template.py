edd868e4f81df333946af62c335fab7ab2d9430f
Xerydia <xerydian@gmail.com>
2026-08-28 06:27:32 +0200

    main/dos2unix: update to 7.5.7

--- main/dos2unix/template.py
+++ main/dos2unix/template.py
@@ -1,14 +1,14 @@
 pkgname = "dos2unix"
-pkgver = "7.5.5"
+pkgver = "7.5.7"
 pkgrel = 0
 build_style = "makefile"
 hostmakedepends = ["pkgconf", "gettext"]
 checkdepends = ["perl"]
 pkgdesc = "Line ending converter"
 license = "BSD-2-Clause"
-url = "https://waterlan.home.xs4all.nl/dos2unix.html"
-source = f"https://waterlan.home.xs4all.nl/dos2unix/dos2unix-{pkgver}.tar.gz"
-sha256 = "75f692b8484c8c24579a2ffd87df16b9c9428ed95497e3393a21d1ba0697ac33"
+url = "https://waterlander.net/dos2unix"
+source = f"{url}/files/dos2unix-{pkgver}.tar.gz"
+sha256 = "669ee27120ae71589f638fe3a167d6ea54f8633f5ab1b282551bd7a7c9510dfa"
 
 
 def post_install(self):
