2d0174cc68a8d36937c1c8499b67343d8ba7a563
flukey <flukey@vapourmail.eu>
2026-08-30 09:51:14 +0100

    main/blake3: update to 1.8.7
    - 1.8.7: Remove the arrayref dependency. Unfortunately the crates.io
      owner was compromised

--- main/blake3/template.py
+++ main/blake3/template.py
@@ -1,5 +1,5 @@
 pkgname = "blake3"
-pkgver = "1.8.5"
+pkgver = "1.8.7"
 pkgrel = 0
 build_style = "cmake"
 configure_args = ["-DBUILD_SHARED_LIBS=ON", "-DBLAKE3_USE_TBB=ON"]
@@ -14,7 +14,7 @@ pkgdesc = "BLAKE3 cryptographic hash implementation"
 license = "CC0-1.0 OR Apache-2.0"
 url = "https://github.com/BLAKE3-team/BLAKE3"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "220bd81286e2a0585beac66d41ac3f4c2c33ae8a4e339fc88cf22d5e00514fe9"
+sha256 = "c6782a28842b1c0478524ac06a4f2ede784038ee298d6e2162c0b089c4306a3c"
 hardening = ["vis", "cfi"]
 
 
