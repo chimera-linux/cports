a059f7d0d3b7e11e34e333f6674b69d9c4a2a505
flukey <flukey@vapourmail.eu>
2026-08-30 09:50:21 +0100

    main/at-spi2-core: update to 2.60.6

--- main/at-spi2-core/template.py
+++ main/at-spi2-core/template.py
@@ -1,5 +1,5 @@
 pkgname = "at-spi2-core"
-pkgver = "2.60.5"
+pkgver = "2.60.6"
 pkgrel = 0
 build_style = "meson"
 configure_args = [
@@ -29,7 +29,7 @@ url = "https://gitlab.gnome.org/GNOME/at-spi2-core"
 source = (
     f"$(GNOME_SITE)/at-spi2-core/{pkgver[:-2]}/at-spi2-core-{pkgver}.tar.xz"
 )
-sha256 = "6059a77d507438ff6c8d6d06025f8f9f5774fa0f8eabe9c9b059b1cc41e1bbc0"
+sha256 = "a89b64a8b217a8042bdf0e35cbfab629ceee35640dba75df578afde9aa789d57"
 # non-trivial dbus setup
 options = ["etcfiles", "!check", "!cross"]
 
