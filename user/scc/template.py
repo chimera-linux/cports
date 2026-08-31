50857aa088214ea7a2ed00bc7fec01260ab0db52
Wesley Moore <wes@wezm.net>
2026-08-31 10:48:40 +1000

    user/scc: skip tests failing on loongarch
    The tests are failing due to differing sign on 0.0 in generated SVG
    paths.

--- user/scc/template.py
+++ user/scc/template.py
@@ -2,12 +2,20 @@ pkgname = "scc"
 pkgver = "4.0.0"
 pkgrel = 0
 build_style = "go"
+make_check_args = []
 hostmakedepends = ["go"]
 pkgdesc = "Fast and accurate code counter"
 license = "MIT OR Unlicense"
 url = "https://github.com/boyter/scc"
 source = f"{url}/archive/v{pkgver}.tar.gz"
 sha256 = "7e0418d7b6dfa881b2673e50d32da81e9abc34475a305b612b57600d85801abc"
+
+if self.profile().arch in ["loongarch64"]:
+    # These fail with differing sign of 0.0 on loongarch: -0.0 vs. 0.0
+    make_check_args += [
+        "-skip",
+        "TestSparklinePath|TestRenderReport_Golden",
+    ]
 
 
 def post_install(self):
