3fe0fc99672f6f47cd4a670ea7c39026288754e5
flukey <flukey@vapourmail.eu>
2026-08-31 23:13:44 +0100

    main/libksba: update to 1.8.1
    Fix memory safety issues, parser infinite loop.

--- main/libksba/template.py
+++ main/libksba/template.py
@@ -1,5 +1,5 @@
 pkgname = "libksba"
-pkgver = "1.6.7"
+pkgver = "1.8.1"
 pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = []
@@ -9,7 +9,7 @@ pkgdesc = "CMS and X.509 access library"
 license = "GPL-2.0-or-later OR LGPL-3.0-or-later"
 url = "https://gnupg.org/software/libksba/index.html"
 source = f"https://gnupg.org/ftp/gcrypt/libksba/libksba-{pkgver}.tar.bz2"
-sha256 = "cf72510b8ebb4eb6693eef765749d83677a03c79291a311040a5bfd79baab763"
+sha256 = "c2f84393011827219ae117131dba8e7684c2bed0961eed11b0642c2acba440b5"
 
 
 @subpackage("libksba-devel")
