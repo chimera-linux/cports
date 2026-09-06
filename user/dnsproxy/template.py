60dfb73d790cbafba966255cdbf2e6d9ba2b05b2
Simon <cmooon@disroot.org>
2026-09-06 21:45:51 +0200

    user/dnsproxy: update to 0.84.2
    From the dnsproxy release:
    Changed:
      - The version of Go updated to 1.26.8.
    Fixed:
      - DoQ 0-RTT opcode handling.

--- user/dnsproxy/template.py
+++ user/dnsproxy/template.py
@@ -1,5 +1,5 @@
 pkgname = "dnsproxy"
-pkgver = "0.83.0"
+pkgver = "0.84.2"
 pkgrel = 1
 build_style = "go"
 hostmakedepends = ["go"]
@@ -8,7 +8,7 @@ pkgdesc = "DNS proxy server"
 license = "Apache-2.0"
 url = "https://github.com/AdguardTeam/dnsproxy"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "53a586f02ca9c5e3e39ccf70c8e8dade7302b3b17800ade171357dc08fc4e89c"
+sha256 = "a7a6fe89dd1ec34e94fecf78a980696ae1b5f95424e2b9cf62602bf8fee89a2d"
 # uses network
 options = ["etcfiles", "!check"]
 
