ee1ff1d0482bde2197b0c8ac07c64dc3dd18fcab
Xerydia <xerydian@gmail.com>
2026-09-08 06:43:21 +0200

    main/blueprint-compiler: update to 0.22.2

--- main/blueprint-compiler/template.py
+++ main/blueprint-compiler/template.py
@@ -1,5 +1,5 @@
 pkgname = "blueprint-compiler"
-pkgver = "0.20.4"
+pkgver = "0.22.2"
 pkgrel = 1
 build_style = "meson"
 make_check_args = ["--timeout-multiplier", "3"]
@@ -18,4 +18,4 @@ pkgdesc = "Markup language compiler for GTK user interfaces"
 license = "LGPL-3.0-or-later"
 url = "https://gnome.pages.gitlab.gnome.org/blueprint-compiler"
 source = f"https://gitlab.gnome.org/GNOME/blueprint-compiler/-/archive/v{pkgver}/blueprint-compiler-v{pkgver}.tar.gz"
-sha256 = "6f0f72fbce0919045047f30b56590e71cd82c66b890c37fe765a40494fd34feb"
+sha256 = "ba4dddeb22a1929ed5987e74cb69c592e22bc74f0c04cfdb9028b907cf688af6"
