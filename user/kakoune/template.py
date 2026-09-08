15fe7dbdfeded1d9d9a5b306dd432bb856f916a3
Xerydia <xerydian@gmail.com>
2026-09-09 01:11:59 +0200

    user/kakoune: update to 2026.05.21

--- user/kakoune/template.py
+++ user/kakoune/template.py
@@ -1,5 +1,5 @@
 pkgname = "kakoune"
-pkgver = "2026.04.12"
+pkgver = "2026.05.21"
 pkgrel = 1
 build_style = "makefile"
 make_install_args = ["gzip_man=no"]
@@ -9,7 +9,7 @@ pkgdesc = "Modal code editor inspired by vim"
 license = "Unlicense"
 url = "https://kakoune.org"
 source = f"https://github.com/mawww/kakoune/releases/download/v{pkgver}/kakoune-{pkgver}.tar.bz2"
-sha256 = "ce67adc8af7b20550463332c38e389cacfdd80f709e14b9940c127091aab0681"
+sha256 = "be1deb3fe9808a0733ab1057309da380bb757307e8fdbb22dc478b674b6bad34"
 hardening = ["vis", "cfi"]
 # check may be disabled
 options = []
