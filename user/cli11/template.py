aa11046102414f96d2162c8776d31d41582de2f3
Jose Arellano <jose.4rellano@gmail.com>
2026-09-09 15:01:38 -0300

    user/cli11: update to 2.7.2

--- user/cli11/template.py
+++ user/cli11/template.py
@@ -1,5 +1,5 @@
 pkgname = "cli11"
-pkgver = "2.6.2"
+pkgver = "2.7.2"
 pkgrel = 0
 build_style = "cmake"
 configure_args = [
@@ -12,8 +12,13 @@ pkgdesc = "Command line parser for C++11"
 license = "BSD-3-Clause"
 url = "https://github.com/CLIUtils/CLI11"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "c6ea6b2e5608b3ea8617999bd5f47420c71b2ebdb8dc4767c1034d1da5785711"
+sha256 = "46eef3101da70852ec7af026e09d485ccee81813331c8c6052d39344443b83da"
 tool_flags = {"CXXFLAGS": ["-Wno-c2y-extensions"]}
+
+if self.profile().cross:
+    # checkdepends aren't installed at configure time; CLI11 falls back to
+    # downloading a legacy Catch header, no network in cross.
+    configure_args += ["-DCLI11_BUILD_TESTS=OFF"]
 
 
 def post_install(self):
