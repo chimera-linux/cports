9bbc39c5668bb07b5ea5786e7a0b2d62a1e743c1
flukey <flukey@vapourmail.eu>
2026-09-10 14:35:38 +0100

    main/lsof: update to 4.99.7
    UAF and bug fixes.

--- main/lsof/template.py
+++ main/lsof/template.py
@@ -1,5 +1,5 @@
 pkgname = "lsof"
-pkgver = "4.99.6"
+pkgver = "4.99.7"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = [
@@ -14,7 +14,7 @@ pkgdesc = "List open files"
 license = "custom:lsof"
 url = "https://lsof.readthedocs.io/en/latest"
 source = f"https://github.com/lsof-org/lsof/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "2ce65158694e9c44dfc54916f5b843d887763c03128e0a1c77d62ae106537009"
+sha256 = "bac1b0acbc50aede42fc97dffaa0b0475e97973e36a6351de5f349c6155afc68"
 hardening = ["vis", "!cfi"]
 # FIXME: weird failures
 options = ["!check"]
