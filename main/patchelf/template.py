ffc23123ef0940b3f2d82b75adffbc5368ae3239
Xerydia <xerydian@gmail.com>
2026-09-12 06:03:43 +0200

    main/patchelf: update to 0.19.1

--- main/patchelf/template.py
+++ main/patchelf/template.py
@@ -1,13 +1,13 @@
 pkgname = "patchelf"
-pkgver = "0.18.0"
+pkgver = "0.19.1"
 pkgrel = 0
 build_style = "gnu_configure"
 hostmakedepends = ["automake"]
-pkgdesc = "Utility tomodify the dynamic linker and RPATH of ELF executables"
+pkgdesc = "Utility to modify the dynamic linker and RPATH of ELF executables"
 license = "GPL-3.0-or-later"
 url = "https://github.com/NixOS/patchelf"
-source = f"https://github.com/NixOS/patchelf/releases/download/{pkgver}/patchelf-{pkgver}.tar.bz2"
-sha256 = "1952b2a782ba576279c211ee942e341748fdb44997f704dd53def46cd055470b"
+source = f"{url}/releases/download/{pkgver}/patchelf-{pkgver}.tar.bz2"
+sha256 = "2cce01de93653829f6ab68a20c2ec275e1c00a946110704a27e928d2e6e88716"
 hardening = ["vis", "cfi"]
 # don't run for some reason
 options = ["!check"]
