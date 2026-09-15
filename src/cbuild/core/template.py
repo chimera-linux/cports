952683174a5eec9434cb46a8d97ebad121fa7a6c
q66 <q66@chimera-linux.org>
2026-09-15 14:51:51 +0200

    cbuild: update GNU_SITE ftpmirror url
    Following recent changes it seems they broke all the formerly
    working URLs.
    
    Fixes https://github.com/chimera-linux/cports/issues/6169

--- src/cbuild/core/template.py
+++ src/cbuild/core/template.py
@@ -573,7 +573,7 @@ sites = {
     "xorg": "https://www.x.org/releases/individual",
     "cpan": "https://www.cpan.org/modules/by-module",
     "pypi": "https://files.pythonhosted.org/packages/source",
-    "gnu": "https://ftpmirror.gnu.org/gnu",
+    "gnu": "https://ftpmirror.gnu.org",
     "kde": "https://download.kde.org/stable",
     "xfce": "https://archive.xfce.org/src",
 }
