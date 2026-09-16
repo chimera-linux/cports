cf5ff7c741a7aa4c1fd9ee344797ba553fdc4f21
q66 <q66@chimera-linux.org>
2026-09-16 22:19:09 +0200

    main/python-gobject: update to 3.58.0

--- main/python-gobject/template.py
+++ main/python-gobject/template.py
@@ -1,5 +1,5 @@
 pkgname = "python-gobject"
-pkgver = "3.56.3"
+pkgver = "3.58.0"
 pkgrel = 0
 build_style = "meson"
 make_check_env = {"PYGI_TEST_VERBOSE": "1"}
@@ -27,7 +27,7 @@ pkgdesc = "Python bindings for GObject"
 license = "LGPL-2.1-or-later"
 url = "https://pygobject.readthedocs.io"
 source = f"$(GNOME_SITE)/pygobject/{pkgver[:-2]}/pygobject-{pkgver}.tar.gz"
-sha256 = "12760e4a0e3d04b6eb95e06f7a27e362c826d567ea613373a92c003b6c70d2d6"
+sha256 = "45068697de3ffe46840ca369705f23118b34db4f7deb63f6eff079a6734ddcca"
 # cyclic + gtk3 does not handle seatless displays
 options = ["!check"]
 
