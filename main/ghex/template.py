86284901f5c4daa9d08007489c6893baa84d31de
q66 <q66@chimera-linux.org>
2026-09-16 23:14:56 +0200

    main/ghex: update to 50.4

--- main/ghex/template.py
+++ main/ghex/template.py
@@ -1,5 +1,5 @@
 pkgname = "ghex"
-pkgver = "50.1"
+pkgver = "50.4"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -22,7 +22,7 @@ pkgdesc = "Hex editor for GNOME"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/ghex"
 source = f"$(GNOME_SITE)/ghex/{pkgver[:-2]}/ghex-{pkgver}.tar.xz"
-sha256 = "eb270b35b41b8f78a830ec83e1b89b7caeabed032922035b9e129edd95598178"
+sha256 = "e2bcf62438edaf04ca961aef8dafd35a2d208aa45f00ee557000b37214c0972e"
 options = ["!cross"]
 
 
