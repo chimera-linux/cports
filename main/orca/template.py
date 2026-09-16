a29048b8af016a79eab54795fd61cb50e05ad656
q66 <q66@chimera-linux.org>
2026-09-16 23:19:11 +0200

    main/orca: update to 51.0

--- main/orca/template.py
+++ main/orca/template.py
@@ -1,7 +1,9 @@
 pkgname = "orca"
-pkgver = "50.1.2"
-pkgrel = 1
+pkgver = "51.0"
+pkgrel = 0
 build_style = "meson"
+# needs rust
+configure_args = ["-Dmathcat=false"]
 hostmakedepends = [
     "gettext",
     # checks for gtk+3 by running python program in host :/
@@ -32,8 +34,8 @@ depends = [
 pkgdesc = "GNOME screen reader"
 license = "LGPL-2.1-or-later"
 url = "https://orca.gnome.org"
-source = f"$(GNOME_SITE)/orca/{pkgver[:-4]}/orca-{pkgver}.tar.xz"
-sha256 = "8592b53df84239ea75ddaa8dec6792c84d2b99fb7b47a5fd9022c6af8ca657a8"
+source = f"$(GNOME_SITE)/orca/{pkgver[:-2]}/orca-{pkgver}.tar.xz"
+sha256 = "8bc3e44bc5b7b66ec7e0cc5c82695c0075661922745d25724f5fc84a25602108"
 options = ["etcfiles"]
 
 
