6facf0c60fd44a918c5343582c84092428508120
q66 <q66@chimera-linux.org>
2026-09-16 21:53:09 +0200

    main/glibmm2.4: update to 2.66.10

--- main/glibmm2.4/template.py
+++ main/glibmm2.4/template.py
@@ -1,5 +1,5 @@
 pkgname = "glibmm2.4"
-pkgver = "2.66.8"
+pkgver = "2.66.10"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "glib-devel", "perl", "pkgconf"]
@@ -8,8 +8,8 @@ checkdepends = ["glib-networking"]
 pkgdesc = "C++ bindings for GLib, API version 2.4"
 license = "LGPL-2.1-or-later"
 url = "https://www.gtkmm.org"
-source = f"$(GNOME_SITE)/glibmm/{pkgver[:-2]}/glibmm-{pkgver}.tar.xz"
-sha256 = "64f11d3b95a24e2a8d4166ecff518730f79ecc27222ef41faf7c7e0340fc9329"
+source = f"$(GNOME_SITE)/glibmm/{pkgver[:-3]}/glibmm-{pkgver}.tar.xz"
+sha256 = "2b61780203aed98e701d3ea57c8f353e7c8ada9706a79be782f6c5153dd035c0"
 
 
 @subpackage("glibmm2.4-devel")
--- main/glibmm2.4/update.py
+++ main/glibmm2.4/update.py
@@ -1,3 +1,3 @@
 pkgname = "glibmm"
 url = "https://gitlab.gnome.org/GNOME/glibmm/-/tags"
-ignore = ["2.68*", "2.7*", "2.8*"]
+ignore = ["2.68*", "2.7*", "2.8*", "2.9*"]
