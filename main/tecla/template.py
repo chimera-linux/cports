6427767125e487a251e81094063f5319360afd48
q66 <q66@chimera-linux.org>
2026-09-16 22:23:43 +0200

    main/tecla: update to 51.0

--- main/tecla/template.py
+++ main/tecla/template.py
@@ -1,5 +1,5 @@
 pkgname = "tecla"
-pkgver = "50.0"
+pkgver = "51.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -16,4 +16,4 @@ pkgdesc = "GNOME keyboard layout viewer"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/tecla"
 source = f"$(GNOME_SITE)/tecla/{pkgver.split('.')[0]}/tecla-{pkgver}.tar.xz"
-sha256 = "2542acb24850082e0ccf6aa17af9651db71d22f0e233fdbf5ed0cffe2e45bc06"
+sha256 = "ddf1a8555044171d0d84b580d6d541c3c621ae77996f6bc5edde598de2f8625b"
