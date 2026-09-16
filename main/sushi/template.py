c29c86a63768eaeae0ab95a6e88a5ddb944e4d5d
q66 <q66@chimera-linux.org>
2026-09-16 23:28:54 +0200

    main/sushi: update to 51.0

--- main/sushi/template.py
+++ main/sushi/template.py
@@ -1,8 +1,9 @@
 pkgname = "sushi"
-pkgver = "50.0"
+pkgver = "51.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
+    "blueprint-compiler",
     "gettext",
     "gjs",
     "glib-devel",
@@ -11,23 +12,23 @@ hostmakedepends = [
     "pkgconf",
 ]
 makedepends = [
-    "evince-devel",
     "freetype-devel",
-    "gdk-pixbuf-devel",
     "glib-devel",
+    "glycin-gtk4-devel",
     "gst-plugins-base-devel",
     "gstreamer-devel",
-    "gtk+3-devel",
-    "gtksourceview4-devel",
+    "gtk4-devel",
+    "gtksourceview-devel",
     "harfbuzz-devel",
     "libepoxy-devel",
-    "webkitgtk-devel",
+    "papers-devel",
+    "webkitgtk4-devel",
 ]
 depends = ["evince", "gtksourceview4", "nautilus", "webkitgtk"]
 pkgdesc = "File previewer for GNOME"
 license = "GPL-2.0-or-later"
 url = "https://gitlab.gnome.org/GNOME/sushi"
 source = f"$(GNOME_SITE)/sushi/{pkgver[:-2]}/sushi-{pkgver}.tar.xz"
-sha256 = "ab25177908d5ccc58568769a81eb9b4f32306786e6c73618193ebf61a127ee00"
+sha256 = "d41b1b9e640fd949895bf0e871143249eb7baa38ed4ba666dbb2427fbbc9e9a5"
 # gir
 options = ["!cross"]
