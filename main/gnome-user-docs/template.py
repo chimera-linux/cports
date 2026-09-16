138e91d2a949e7e36190ad41cc9778d67d7fca9f
q66 <q66@chimera-linux.org>
2026-09-16 22:55:59 +0200

    main/gnome-user-docs: update to 51.0

--- main/gnome-user-docs/template.py
+++ main/gnome-user-docs/template.py
@@ -1,13 +1,15 @@
 pkgname = "gnome-user-docs"
-pkgver = "50.0"
+pkgver = "51.0"
 pkgrel = 0
-build_style = "gnu_configure"
-configure_gen = []
+build_style = "meson"
 hostmakedepends = [
     "gettext",
     "itstool",
     "libxml2-progs",
+    "meson",
+    "ninja",
     "pkgconf",
+    "yelp-tools",
 ]
 makedepends = ["yelp"]
 depends = ["yelp"]
@@ -15,5 +17,6 @@ pkgdesc = "User documentation for GNOME"
 license = "CC-BY-3.0"
 url = "https://gitlab.gnome.org/GNOME/gnome-user-docs"
 source = f"$(GNOME_SITE)/gnome-user-docs/{pkgver[:-2]}/gnome-user-docs-{pkgver}.tar.xz"
-sha256 = "e8e23324184c7e985c504f05f6d9c63420c9a9d1f64efde5da2a811236072f78"
-options = ["!splitdoc"]
+sha256 = "bf3ebd7fa1da70617884b07392de8d605305cfef71c2e883749cb08ff0d6a7d1"
+# meh
+options = ["!splitdoc", "!check"]
