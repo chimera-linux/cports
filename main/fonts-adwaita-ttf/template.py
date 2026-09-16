77fd64f15db4c16d226174203a5a0d4da78c6692
q66 <q66@chimera-linux.org>
2026-09-16 22:26:34 +0200

    main/fonts-adwaita-ttf: update to 51.0

--- main/fonts-adwaita-ttf/template.py
+++ main/fonts-adwaita-ttf/template.py
@@ -1,5 +1,5 @@
 pkgname = "fonts-adwaita-ttf"
-pkgver = "50.0"
+pkgver = "51.0"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = ["meson", "pkgconf"]
@@ -9,7 +9,7 @@ url = "https://gitlab.gnome.org/GNOME/adwaita-fonts"
 source = (
     f"$(GNOME_SITE)/adwaita-fonts/{pkgver[:-2]}/adwaita-fonts-{pkgver}.tar.xz"
 )
-sha256 = "4c927fbfeec1c503801ba510c2c94e0054c82c522cf7ba0d3be5d4d41fcf5c86"
+sha256 = "fa104ae2c1b96580d322f563ffe8b2dfca5296b0eca4971efd2d8011405123d2"
 
 
 def post_install(self):
