5aff876f73ddb2ecfe20e52131ebba836a0b03c8
q66 <q66@chimera-linux.org>
2026-09-17 14:54:02 +0200

    main/gnome-shell-extension-gsconnect: fix for gnome 51

--- main/gnome-shell-extension-gsconnect/template.py
+++ main/gnome-shell-extension-gsconnect/template.py
@@ -1,6 +1,8 @@
 pkgname = "gnome-shell-extension-gsconnect"
 pkgver = "72"
-pkgrel = 0
+pkgrel = 1
+# until release with gnome 51
+_commit = "0be3a14d8a6db8bbd287d694e3397ce9f9888f0f"
 build_style = "meson"
 configure_args = ["-Dinstalled_tests=false"]
 make_check_wrapper = ["dbus-run-session", "--", "wlheadless-run", "--"]
@@ -20,7 +22,8 @@ checkdepends = ["dbus", "gnome-shell", "xwayland-run"]
 pkgdesc = "KDE Connect implementation for GNOME"
 license = "GPL-2.0-or-later"
 url = "https://github.com/GSConnect/gnome-shell-extension-gsconnect"
-source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "8147e9230823364cde716aaeea3b81953a2f7ebf0697568c32ac67363870201a"
+source = f"{url}/archive/{_commit}.tar.gz"
+# source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "88ae39996b69f3a7e7d4f90bd1cec37991c01692eb82bb5c889fb49355280356"
 # All tests fail in latest release https://github.com/GSConnect/gnome-shell-extension-gsconnect/issues/1786
 options = ["etcfiles", "!check"]
