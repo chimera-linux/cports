049a2db90a39db04cd60b6c3c8d7953506ddd7a3
q66 <q66@chimera-linux.org>
2026-09-17 19:55:53 +0200

    main/webkitgtk*: bring back system malloc for riscv
    Webkit upstream defaults to mimalloc now on some platforms incl
    riscv. We already use mimalloc in the system and webkit installs
    mimalloc lib/headers/pc file when enabled so avoid that.

--- main/webkitgtk/template.py
+++ main/webkitgtk/template.py
@@ -1,6 +1,6 @@
 pkgname = "webkitgtk"
 pkgver = "2.54.0"
-pkgrel = 0
+pkgrel = 1
 build_style = "cmake"
 configure_args = [
     "-DPORT=GTK",
@@ -14,6 +14,7 @@ configure_args = [
     "-DUSE_WOFF2=ON",
     "-DUSE_FLITE=OFF",
     "-DUSE_SPIEL=ON",
+    "-DUSE_MIMALLOC=OFF",
     # -DENABLE_*
     "-DENABLE_BUBBLEWRAP_SANDBOX=ON",
     "-DENABLE_DOCUMENTATION=OFF",
@@ -128,6 +129,10 @@ match self.profile.arch:
 
 if self.profile.arch == "loongarch64":
     tool_flags["CXXFLAGS"] += ["-DSIMDE_FLOAT16_API=SIMDE_FLOAT16_API_PORTABLE"]
+
+if self.profile.arch == "riscv64":
+    # libpas/bmalloc link errors
+    configure_args += ["-DUSE_SYSTEM_MALLOC=ON"]
 
 # LTO broken on aarch64 (JIT segfault)
 if self.has_lto(force=True) and self.profile.arch != "aarch64":
