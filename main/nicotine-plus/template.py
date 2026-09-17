b53b0198139348ce1780edfdc28abdbd4ffaca2d
flukey <flukey@vapourmail.eu>
2026-09-17 10:32:26 +0100

    main/nicotine-plus: update to 3.3.11
    Important security/hardening fixes. Some behavioural changes, e.g.
    remove "everyone" from manual upload perms, fixed number of upload slots
    by default.
    
    https://github.com/nicotine-plus/nicotine-plus/releases/tag/3.3.11

--- main/nicotine-plus/template.py
+++ main/nicotine-plus/template.py
@@ -1,6 +1,6 @@
 pkgname = "nicotine-plus"
-pkgver = "3.3.10"
-pkgrel = 1
+pkgver = "3.3.11"
+pkgrel = 0
 build_style = "python_pep517"
 # test_update_check: no networking + patched
 # FIXME test_gui_startup: for some reason there's a sigill in python somewhere
@@ -35,4 +35,4 @@ url = "https://nicotine-plus.github.io/nicotine-plus"
 source = (
     f"https://github.com/Nicotine-Plus/nicotine-plus/archive/{pkgver}.tar.gz"
 )
-sha256 = "3917ebc562f2d6a6b26b3d815d7cbdf1d11c058d994b1f47794bbb850489b35e"
+sha256 = "0021c20f6ee8a151a4eda78c38a0735cddbcedebf5ef834f4d0769384e76556b"
