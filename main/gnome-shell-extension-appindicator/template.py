39607b41ae2c09d18f974eb4d8a6ce617e694148
q66 <q66@chimera-linux.org>
2026-09-17 14:55:16 +0200

    main/gnome-shell-extension-appindicator: update to 65

--- main/gnome-shell-extension-appindicator/template.py
+++ main/gnome-shell-extension-appindicator/template.py
@@ -1,5 +1,5 @@
 pkgname = "gnome-shell-extension-appindicator"
-pkgver = "64"
+pkgver = "65"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
@@ -12,4 +12,4 @@ pkgdesc = "AppIndicator/KStatusNotifierItem support for GNOME"
 license = "GPL-2.0-or-later"
 url = "https://github.com/ubuntu/gnome-shell-extension-appindicator"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "e8a9136a134dc52c76b75ae4b89e71cee6fbe2061dd318d0f818f088f8253411"
+sha256 = "a55eb3ffc8882f7d1f974fbdce2c2f72c1e14b1bf9ba72af2a99622be022287f"
