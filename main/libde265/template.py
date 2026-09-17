66e2d524929aa84503dea567f3a48970de4090d8
flukey <flukey@vapourmail.eu>
2026-09-17 10:20:34 +0100

    main/libde265: update to 1.1.3

--- main/libde265/template.py
+++ main/libde265/template.py
@@ -1,5 +1,5 @@
 pkgname = "libde265"
-pkgver = "1.1.2"
+pkgver = "1.1.3"
 pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
@@ -7,7 +7,7 @@ pkgdesc = "Open H.265 codec implementation"
 license = "LGPL-3.0-or-later"
 url = "http://www.libde265.org"
 source = f"https://github.com/strukturag/libde265/archive/v{pkgver}.tar.gz"
-sha256 = "982f7838cc25aa6bda7fd33b9b3a05621d0f9b8456dc495d5fb4977fed6dcdbc"
+sha256 = "189baa08fd6d2dd34db099de411bd6b2e6bd5eb88e81236a3d0fa3826b9715c4"
 # no tests
 options = ["!check"]
 
