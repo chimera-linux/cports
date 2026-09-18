1814f0e91b474eb2925b1271d0c79cbd172de462
fiore <fiore@grrlz.net>
2026-09-18 16:34:56 +0200

    user/ripdrag: update to 0.4.13

--- user/ripdrag/template.py
+++ user/ripdrag/template.py
@@ -1,5 +1,5 @@
 pkgname = "ripdrag"
-pkgver = "0.4.12"
+pkgver = "0.4.13"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable", "pkgconf"]
@@ -11,7 +11,7 @@ pkgdesc = "Drag and drop utilty"
 license = "GPL-3.0-only"
 url = "https://github.com/nik012003/ripdrag"
 source = [f"{url}/archive/refs/tags/v{pkgver}.tar.gz"]
-sha256 = ["ffa685c42e84558cc47d8bd5713f8a68f8cd8e313be55a111a0bc43bf1e220de"]
+sha256 = ["f3d546233b8ba979f5ea4778f61267968a088e5b3b3035dd391ac9e6cc96162b"]
 
 
 def pre_prepare(self):
