3396e5338471dfb9c4ccb4fa2ba9f370bc4f2781
Tilman List <listentolist@posteo.de>
2026-09-20 13:15:12 +0200

    user/clifm: update to 1.29

--- user/clifm/template.py
+++ user/clifm/template.py
@@ -1,5 +1,5 @@
 pkgname = "clifm"
-pkgver = "1.28"
+pkgver = "1.29"
 pkgrel = 0
 build_style = "makefile"
 makedepends = [
@@ -13,6 +13,6 @@ pkgdesc = "Shell-like, command line terminal file manager"
 license = "GPL-2.0-or-later"
 url = "https://github.com/leo-arch/clifm"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "65ac33825fb55d6388c1044572e464a50ad367b607448774fb396d850b7c4420"
+sha256 = "dfdc0f339437345d9d5d8c2cb4bd43294c05821ebc8d5f0c9abfa4eec8f6c905"
 # no tests
 options = ["!check"]
