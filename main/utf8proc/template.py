41c191c97461bbb7d2258a519bf7918fb4c46234
flukey <flukey@vapourmail.eu>
2026-09-21 15:53:39 +0100

    main/utf8proc: update to 2.11.3
    Unicode 17 support and security fixes.

--- main/utf8proc/template.py
+++ main/utf8proc/template.py
@@ -1,5 +1,5 @@
 pkgname = "utf8proc"
-pkgver = "2.10.0"
+pkgver = "2.11.3"
 pkgrel = 0
 build_style = "makefile"
 make_install_args = ["prefix=/usr"]
@@ -8,7 +8,7 @@ pkgdesc = "Clean C library for processing UTF-8 Unicode data"
 license = "MIT"
 url = "https://github.com/JuliaStrings/utf8proc"
 source = f"{url}/archive/v{pkgver}/utf8proc-{pkgver}.tar.gz"
-sha256 = "6f4f1b639daa6dca9f80bc5db1233e9cbaa31a67790887106160b33ef743f136"
+sha256 = "abfed50b6d4da51345713661370290f4f4747263ee73dc90356299dfc7990c78"
 hardening = ["vis", "cfi"]
 # cannot run check because Julia isn't packaged
 options = ["!check"]
