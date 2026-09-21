b0e5432e760a2c8690df150a98d1999851db4512
shtayerc <david.murko@mailbox.org>
2026-09-21 15:58:54 +0200

    main/stockfish: update to 19

--- main/stockfish/template.py
+++ main/stockfish/template.py
@@ -1,5 +1,5 @@
 pkgname = "stockfish"
-pkgver = "18"
+pkgver = "19"
 pkgrel = 0
 build_wrksrc = "src"
 build_style = "makefile"
@@ -8,17 +8,14 @@ make_build_args = ["optimize=no", "SUPPORTED_ARCH=true", "arch=any"]
 pkgdesc = "Free UCI chess engine derived from Glaurung"
 license = "GPL-3.0-or-later"
 url = "https://stockfishchess.org"
-_net_file_big = "nn-c288c895ea92.nnue"
-_net_file_small = "nn-37f18f62d772.nnue"
+_net_file = "nn-1a298aa575a0.nnue"
 source = [
     f"https://github.com/official-stockfish/Stockfish/archive/sf_{pkgver}.tar.gz",
-    f"!https://tests.stockfishchess.org/api/nn/{_net_file_big}",
-    f"!https://tests.stockfishchess.org/api/nn/{_net_file_small}",
+    f"!https://tests.stockfishchess.org/api/nn/{_net_file}",
 ]
 sha256 = [
-    "22a195567e3493e7c9ca8bf8fa2339f4ffc876384849ac8a417ff4b919607e7b",
-    "c288c895ea924429ea9092e3f36b2b3c1f00f2a3a4c759ff7e57e79e3b43e4a7",
-    "37f18f62d772f3107e1d6aaca3898c130c3c86f2ab63e6555fbbca20635a899d",
+    "519b653d0d1ffb96531d982ccbe5c6a19425e8388e0e3c2f70f34b424ab32d76",
+    "1a298aa575a085434d29027978dc36867fe9c5bcea9376654b7a8eba1e52dfc2",
 ]
 tool_flags = {"LDFLAGS": ["-Wl,-z,stack-size=2097152"]}
 # no check target
@@ -26,8 +23,7 @@ options = ["!check"]
 
 
 def post_extract(self):
-    self.cp(self.sources_path / _net_file_big, self.build_wrksrc)
-    self.cp(self.sources_path / _net_file_small, self.build_wrksrc)
+    self.cp(self.sources_path / _net_file, self.build_wrksrc)
 
 
 def install(self):
