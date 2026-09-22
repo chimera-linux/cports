f6d5e50d6b55516bedd7d9e96667b9bf852f0212
Erica Z <zerica@callcc.eu>
2026-09-22 09:08:03 +0200

    main/snooze: update to 0.6

--- main/snooze/template.py
+++ main/snooze/template.py
@@ -1,12 +1,12 @@
 pkgname = "snooze"
-pkgver = "0.5.1"
+pkgver = "0.6"
 pkgrel = 0
 build_style = "makefile"
 pkgdesc = "Run a command at a particular time"
 license = "CC0-1.0"
 url = "https://github.com/leahneukirchen/snooze"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "abb0df288e8fe03ae25453d5f0b723b03a03bcc7afa41b9bec540a7a11a9f93e"
+sha256 = "3a4a2f3f00d42e30647d9af79c8e417990ced6c3f0565474b1ca717938b1e2ab"
 hardening = ["vis", "cfi"]
 # no tests
 options = ["!check"]
