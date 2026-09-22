6c476f8fe48d4346bb0a8d3d8ba85964e698464a
Erica Z <zerica@callcc.eu>
2026-09-22 08:15:13 +0200

    main/brotli: update to 1.2.0

--- main/brotli/template.py
+++ main/brotli/template.py
@@ -1,14 +1,14 @@
 # update main/python-brotli alongside this
 pkgname = "brotli"
-pkgver = "1.1.0"
-pkgrel = 2
+pkgver = "1.2.0"
+pkgrel = 0
 build_style = "cmake"
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 pkgdesc = "General-purpose lossless compression algorithm"
 license = "MIT"
 url = "https://github.com/google/brotli"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "e720a6ca29428b803f4ad165371771f5398faba397edf6778837a18599ea13ff"
+sha256 = "816c96e8e8f193b40151dad7e8ff37b1221d019dbcb9c35cd3fadbfe6477dfec"
 
 
 def post_install(self):
