0f0884a3307a159038fb628324080ae611cab9c4
Wesley Moore <wes@wezm.net>
2026-09-22 19:54:13 +1000

    user/scc: update to 4.1.0

--- user/scc/template.py
+++ user/scc/template.py
@@ -1,21 +1,13 @@
 pkgname = "scc"
-pkgver = "4.0.0"
+pkgver = "4.1.0"
 pkgrel = 0
 build_style = "go"
-make_check_args = []
 hostmakedepends = ["go"]
 pkgdesc = "Fast and accurate code counter"
 license = "MIT OR Unlicense"
 url = "https://github.com/boyter/scc"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "7e0418d7b6dfa881b2673e50d32da81e9abc34475a305b612b57600d85801abc"
-
-if self.profile.arch in ["loongarch64"]:
-    # These fail with differing sign of 0.0 on loongarch: -0.0 vs. 0.0
-    make_check_args += [
-        "-skip",
-        "TestSparklinePath|TestRenderReport_Golden",
-    ]
+sha256 = "4f3cf36010c542b10d5582afb91c668b26889160b184deee21b4319347030a7c"
 
 
 def post_install(self):
