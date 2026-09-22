c7ade6be95dce885b28f49c64203385bb98c00f3
q66 <q66@chimera-linux.org>
2026-09-22 11:13:23 +0200

    user/ripdrag: drop unnecessary pre_prepare

--- user/ripdrag/template.py
+++ user/ripdrag/template.py
@@ -12,16 +12,3 @@ license = "GPL-3.0-only"
 url = "https://github.com/nik012003/ripdrag"
 source = [f"{url}/archive/refs/tags/v{pkgver}.tar.gz"]
 sha256 = ["f3d546233b8ba979f5ea4778f61267968a088e5b3b3035dd391ac9e6cc96162b"]
-
-
-def pre_prepare(self):
-    # libc loongarch64
-    self.do(
-        "cargo",
-        "update",
-        "--package",
-        "libc",
-        "--precise",
-        "0.2.180",
-        allow_network=True,
-    )
