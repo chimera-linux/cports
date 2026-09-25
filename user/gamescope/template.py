63e9f71e4882c08ee2c0671af6d3f4b8cf09469f
flukey <flukey@vapourmail.eu>
2026-09-25 14:20:09 +0100

    user/gamescope: update to 3.16.30

--- user/gamescope/template.py
+++ user/gamescope/template.py
@@ -1,11 +1,12 @@
 pkgname = "gamescope"
-pkgver = "3.16.25"
-pkgrel = 1
+pkgver = "3.16.30"
+pkgrel = 0
 build_style = "meson"
 configure_args = [
     "-Ddefault_library=static",
     "-Davif_screenshots=enabled",
     "-Ddrm_backend=enabled",
+    "-Denable_zenity=false",
     "-Dinput_emulation=enabled",
     "-Dpipewire=enabled",
     "-Drt_cap=enabled",
@@ -49,7 +50,7 @@ makedepends = [
     "vulkan-headers",
     "vulkan-loader-devel",
     "wayland-protocols",
-    "wlroots0.19-devel",
+    "wlroots0.20-devel",
     "xcb-util-wm-devel",
     "xwayland-devel",
 ]
@@ -58,10 +59,10 @@ license = "BSD-2-Clause"
 url = "https://github.com/ValveSoftware/gamescope"
 source = [
     f"{url}/archive/refs/tags/{pkgver}.tar.gz",
-    "https://github.com/ValveSoftware/openvr/archive/ff87f683f41fe26cc9353dd9d9d7028357fd8e1a.tar.gz",
-    "https://github.com/Joshua-Ashton/vkroots/archive/5106d8a0df95de66cc58dc1ea37e69c99afc9540.tar.gz",
+    "https://github.com/ValveSoftware/openvr/archive/0924064316de3effbcd1acf1e309182a2deb1c05.tar.gz",
+    "https://github.com/misyltoad/vkroots/archive/5106d8a0df95de66cc58dc1ea37e69c99afc9540.tar.gz",
     "https://gitlab.freedesktop.org/emersion/libliftoff/-/archive/8b08dc1c14fd019cc90ddabe34ad16596b0691f4.tar.gz",
-    "https://github.com/Joshua-Ashton/reshade/archive/696b14cd6006ae9ca174e6164450619ace043283.tar.gz",
+    "https://github.com/misyltoad/reshade/archive/4245743a8c41abbe3dc73980c1810fe449359bf1.tar.gz",
 ]
 source_paths = [
     ".",
@@ -71,11 +72,11 @@ source_paths = [
     "src/reshade",
 ]
 sha256 = [
-    "4ebb6df36b89c43892e9bebf24b983bc444e125a7e05cb0c9e0238d4bcc9ac85",
-    "22ea84da76f0f37f15d3433aef5f202ad6f8df12c280da7caa47e0475eeb22fd",
+    "e85b06cfa0fce1f7609bb3a691911029b1014bc6014f5d20dc9fbeac80a3d628",
+    "95006a99a7fd26158055fbbeb03ae27421db7fa5fa863b3b9b76ffb55f84c3b1",
     "37b77586e91f7ebee70380dcddd73bf01ae4acef1053e6be41d0485ede022422",
     "8de28aee6f90f47b7fc7037dcd2360166197c0b5d2033f3afdbd34f2ea1bf216",
-    "3aa6feda7773cc8ffa8fb012fe95e6207c776101e29198d0e0d34a0c5e339f6a",
+    "a85d1e4c4546afd23fc4324654bcb08b281d8583820e74c178c4e983c81cb802",
 ]
 file_modes = {
     "usr/bin/gamescope": ("root", "root", 0o755),
