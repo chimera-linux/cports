bb088f3cafb86f1cd3a612d1ea8ab1a2403ac7d8
breakgimme <adam@plock.com>
2026-09-26 00:29:33 +0200

    main/inxi: update to 3.3.41.1

--- main/inxi/template.py
+++ main/inxi/template.py
@@ -1,12 +1,12 @@
 pkgname = "inxi"
-pkgver = "3.3.40.1"
+pkgver = "3.3.41.1"
 pkgrel = 0
 depends = ["perl"]
 pkgdesc = "Fully featured CLI system information tool"
 license = "GPL-3.0-or-later"
 url = "https://codeberg.org/smxi/inxi"
 source = f"{url}/archive/{pkgver[: pkgver.rfind('.')]}-{pkgver[pkgver.rfind('.') + 1 :]}.tar.gz"
-sha256 = "b3f307f06c3b969bd65151d39729b97a767af42fddd3d9bab971135c0e7cd873"
+sha256 = "e08d92550a0f10890e722cc7db1e6d6cbde7e9fb47e61a8c6cec51f54a0b63d8"
 
 
 def install(self):
