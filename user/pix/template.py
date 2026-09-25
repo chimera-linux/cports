542c491618132dc8b5ebe93f1c6376d71a8e2419
breakgimme <adam@plock.com>
2026-09-26 00:38:44 +0200

    user/pix: update to 3.4.11

--- user/pix/template.py
+++ user/pix/template.py
@@ -1,5 +1,5 @@
 pkgname = "pix"
-pkgver = "3.4.10"
+pkgver = "3.4.11"
 pkgrel = 0
 build_style = "meson"
 configure_args = ["-Dlibbrasero=false", "-Dwebservices=false"]
@@ -28,7 +28,7 @@ pkgdesc = "Image management application"
 license = "GPL-2.0-or-later"
 url = "https://projects.linuxmint.com/xapps"
 source = f"https://github.com/linuxmint/pix/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "d765e779ee6e7d8220fe556c010c181ae69be059d53192d628f5bec5fdc9082b"
+sha256 = "47ac74d19399fdde5d0e2e5d1c2b4a29c3281a549ca7881a1fb8457b1bf5c5eb"
 
 
 @subpackage("pix-devel")
