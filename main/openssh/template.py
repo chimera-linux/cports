173852a83d64e31f1ab8fe48bea31aaca9f89117
q66 <q66@chimera-linux.org>
2026-09-25 20:39:55 +0200

    main/openssh: update to 10.5_p1
    This does halfway rework of the sshd_config handling which no
    longer contains any uncommented values (including e.g. the
    subsystem stuff which normally has a hardcoded executable path).
    
    We still need to rework the sshd_config.d stuff which I need to
    figure out a transition path for.

--- main/openssh/template.py
+++ main/openssh/template.py
@@ -1,6 +1,6 @@
 pkgname = "openssh"
-pkgver = "10.3_p1"
-pkgrel = 1
+pkgver = "10.5_p1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--datadir=/usr/share/openssh",
@@ -43,7 +43,7 @@ pkgdesc = "OpenSSH free Secure Shell (SSH) client and server implementation"
 license = "SSH-OpenSSH"
 url = "https://www.openssh.com"
 source = f"https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-{pkgver.replace('_', '')}.tar.gz"
-sha256 = "56682a36bb92dcf4b4f016fd8ec8e74059b79a8de25c15d670d731e7d18e45f4"
+sha256 = "d44d28a839ea9daf969cc69150fde59910b2b39361dad81a3bd6cbd19218db11"
 file_modes = {"usr/lib/ssh-keysign": ("root", "root", 0o4755)}
 # CFI: does not work; maybe make testsuite work first
 hardening = ["vis", "!cfi"]
