149d3a61e550f889f32b04d3230eb77655d8dc39
breakgimme <adam@plock.com>
2026-09-26 00:44:26 +0200

    user/s-tui: update to 1.5.0

--- user/s-tui/template.py
+++ user/s-tui/template.py
@@ -1,5 +1,5 @@
 pkgname = "s-tui"
-pkgver = "1.4.0"
+pkgver = "1.5.0"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -9,12 +9,13 @@ hostmakedepends = [
 ]
 depends = [
     "python-psutil",
+    "python-typing_extensions",
     "python-urwid",
 ]
 pkgdesc = "CPU performance monitor"
 license = "GPL-2.0-or-later"
 url = "https://github.com/amanusk/s-tui"
 source = f"$(PYPI_SITE)/s/s-tui/s_tui-{pkgver}.tar.gz"
-sha256 = "5153f1def01bf1eae62aaf37fe9562a804d09232644d1e2482c7d52e18c6653a"
+sha256 = "93d7a1da2399d6af88228fc945986c14e21edc442fde9582cd8693224c20aa84"
 # check: no tests
 options = ["!check"]
