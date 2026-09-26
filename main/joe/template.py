4bf89ed41304e0c491e080b7c24580b7dff58c73
breakgimme <adam@plock.com>
2026-09-26 03:02:49 +0200

    main/joe: update to 4.8

--- main/joe/template.py
+++ main/joe/template.py
@@ -1,6 +1,6 @@
 pkgname = "joe"
-pkgver = "4.6"
-pkgrel = 1
+pkgver = "4.8"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_gen = ["./autojoe"]
 make_dir = "."
@@ -10,7 +10,7 @@ pkgdesc = "Curses-based text editor"
 license = "GPL-2.0-or-later"
 url = "https://joe-editor.sourceforge.io"
 source = f"$(SOURCEFORGE_SITE)/joe-editor/JOE%20sources/joe-{pkgver}/joe-{pkgver}.tar.gz"
-sha256 = "495a0a61f26404070fe8a719d80406dc7f337623788e445b92a9f6de512ab9de"
+sha256 = "6995b28ee20dcdbbcb5a45a4c110642dc96d67748aea27450c74cdb4dd07cc20"
 # FIXME
 hardening = ["!int", "vis", "cfi"]
 options = ["etcfiles"]
