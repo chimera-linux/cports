298c3433286ac749017a6ced60c1512912fc45c8
flukey <flukey@vapourmail.eu>
2026-09-26 13:33:29 +0100

    user/difftastic: update to 0.71.0
    Added missing man page, HTML docs.

--- user/difftastic/template.py
+++ user/difftastic/template.py
@@ -1,10 +1,11 @@
 pkgname = "difftastic"
-pkgver = "0.69.0"
+pkgver = "0.71.0"
 pkgrel = 0
 build_style = "cargo"
 prepare_after_patch = True
 hostmakedepends = [
     "cargo-auditable",
+    "mdbook",
 ]
 makedepends = [
     "rust-std",
@@ -15,8 +16,21 @@ url = "https://difftastic.wilfred.me.uk"
 source = (
     f"https://github.com/Wilfred/difftastic/archive/refs/tags/{pkgver}.tar.gz"
 )
-sha256 = "49d722fb80a0324ea99fe11907f796cde635443084d15cc6f1afd9e0de54bde0"
+sha256 = "d6afd26103c6492a91307dc6779c7dd0ca4d4c85499f81d7dc53fdfa5107331d"
+
+
+def post_build(self):
+    with self.pushd("manual"):
+        self.do(
+            "sed",
+            "-ie",
+            f"s/DFT_VERSION_HERE/{pkgver}/g",
+            "src/introduction.md",
+        )
+        self.do("mdbook", "build")
 
 
 def post_install(self):
     self.install_license("LICENSE")
+    self.install_man("difft.1")
+    self.install_files("manual/book", "usr/share/doc", name="difftastic")
