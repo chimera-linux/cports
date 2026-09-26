f3976d3540cc6ee85177b5fe8364e10e52a1539e
breakgimme <adam@plock.com>
2026-09-26 05:48:31 +0200

    main/socat: update to 1.8.1.3

--- main/socat/template.py
+++ main/socat/template.py
@@ -1,5 +1,5 @@
 pkgname = "socat"
-pkgver = "1.8.1.1"
+pkgver = "1.8.1.3"
 pkgrel = 0
 build_style = "gnu_configure"
 # broken
@@ -16,7 +16,7 @@ pkgdesc = "Multipurpose relay for binary protocols"
 license = "GPL-2.0-only"
 url = "http://www.dest-unreach.org/socat"
 source = f"{url}/download/socat-{pkgver}.tar.gz"
-sha256 = "f68b602c80e94b4b7498d74ec408785536fe33534b39467977a82ab2f7f01ddb"
+sha256 = "06602ffd591e98c75b3dc1d66f0f19136cc666b0b2d95caad987d6ab2cb28097"
 hardening = ["vis", "cfi"]
 # tests have heisenbugs
 options = ["!check"]
