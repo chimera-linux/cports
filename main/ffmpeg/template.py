831c6fd43629458758f67dd0d6e5ba0848d65d45
q66 <q66@chimera-linux.org>
2026-09-26 22:56:23 +0200

    main/ffmpeg: update to 8.1.3
    Closes https://github.com/chimera-linux/cports/pull/6241

--- main/ffmpeg/template.py
+++ main/ffmpeg/template.py
@@ -1,6 +1,6 @@
 pkgname = "ffmpeg"
-pkgver = "8.1.2"
-pkgrel = 4
+pkgver = "8.1.3"
+pkgrel = 0
 build_style = "configure"
 configure_args = [
     "--prefix=/usr",
@@ -142,7 +142,7 @@ pkgdesc = "Decoding, encoding and streaming software"
 license = "GPL-3.0-or-later"
 url = "https://ffmpeg.org"
 source = f"{url}/releases/ffmpeg-{pkgver}.tar.xz"
-sha256 = "464beb5e7bf0c311e68b45ae2f04e9cc2af88851abb4082231742a74d97b524c"
+sha256 = "7138d28c96d9d3e3af4ee3d8cad72741f8ffb40da90c1112235dea3ecd3178a3"
 # some conf checks like for some pthread functions don't detect interfaces
 # without it
 tool_flags = {
