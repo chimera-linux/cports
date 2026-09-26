bb4ef6995a6d98a4bf66e83916ce299917df57d7
breakgimme <adam@plock.com>
2026-09-26 05:33:11 +0200

    user/podman-tui: update to 2.0.0

--- user/podman-tui/template.py
+++ user/podman-tui/template.py
@@ -1,6 +1,6 @@
 pkgname = "podman-tui"
-pkgver = "1.9.0"
-pkgrel = 6
+pkgver = "2.0.0"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 makedepends = [
@@ -25,7 +25,7 @@ pkgdesc = "Podman terminal UI"
 license = "Apache-2.0"
 url = "https://github.com/containers/podman-tui"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7a0e89d71a18527f01be061c8d449823770cff768b6d716cef96b979f3672de7"
+sha256 = "32c8ffced718cda2d5b4bcedddb71299aaa035f035de6886100ab0f6469ca3dd"
 
 
 def post_extract(self):
