254fae1e219da5c73c1a5b7f841b3e15c7bb75e7
breakgimme <adam@plock.com>
2026-09-26 19:08:16 +0200

    main/pipx: update to 1.17.6

--- main/pipx/template.py
+++ main/pipx/template.py
@@ -1,5 +1,5 @@
 pkgname = "pipx"
-pkgver = "1.17.5"
+pkgver = "1.17.6"
 pkgrel = 0
 build_style = "python_pep517"
 hostmakedepends = [
@@ -23,7 +23,7 @@ pkgdesc = "Python tool for installing binaries to venvs"
 license = "MIT"
 url = "https://github.com/pypa/pipx"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
-sha256 = "05653f34729d6971c6e4726221c13cac4ba1752cbb2e214159f79dd3bc06eb9a"
+sha256 = "d31f0043cfdaf236ddd6d338bb3aa86d0f200a7e2e2f3b4f6d6e4e8118cc563b"
 # missing some unknown deps
 options = ["!check"]
 
