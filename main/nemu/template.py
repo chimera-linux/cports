d2e4c4ea0aa00081ae01a1e2c51d5f63683dd400
breakgimme <adam@plock.com>
2026-09-26 19:22:54 +0200

    main/nemu: update to 3.5.0

--- main/nemu/template.py
+++ main/nemu/template.py
@@ -1,9 +1,8 @@
 pkgname = "nemu"
-pkgver = "3.3.1"
-pkgrel = 3
+pkgver = "3.5.0"
+pkgrel = 0
 build_style = "cmake"
 configure_args = [
-    "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
     "-DNM_WITH_DBUS=ON",
     "-DNM_WITH_NETWORK_MAP=ON",
     "-DNM_WITH_REMOTE=ON",
@@ -30,7 +29,7 @@ pkgdesc = "Ncurses UI for QEMU"
 license = "BSD-2-Clause"
 url = "https://github.com/nemuTUI/nemu"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "7cdb27cbf5df1957d0f0a258fc334f15d9e2d06a450a982bb796094efc3960c0"
+sha256 = "dc251c6c478d60734a964324c028904e9da97dfeded7e5c6855f65e594e9a065"
 tool_flags = {"CFLAGS": ["-Wno-strict-prototypes"]}
 hardening = ["vis", "cfi"]
 
