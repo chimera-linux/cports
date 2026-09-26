2ddbe64d4df25604db4d77bd677e43fbeb6118ed
breakgimme <adam@plock.com>
2026-09-26 05:27:08 +0200

    user/mousepad: update to 0.7.0

--- user/mousepad/template.py
+++ user/mousepad/template.py
@@ -1,6 +1,6 @@
 pkgname = "mousepad"
-pkgver = "0.6.5"
-pkgrel = 1
+pkgver = "0.7.0"
+pkgrel = 0
 build_style = "meson"
 configure_args = ["-Ddefault_library=shared"]
 hostmakedepends = [
@@ -21,4 +21,4 @@ pkgdesc = "Xfce text editor"
 license = "GPL-2.0-or-later"
 url = "https://docs.xfce.org/apps/mousepad/start"
 source = f"$(XFCE_SITE)/apps/mousepad/{pkgver[:-2]}/mousepad-{pkgver}.tar.xz"
-sha256 = "21762bc8c3c4f120a4a509ce39f4a5a58dbc10e3f0da66cdc6d9a8c735fff2ac"
+sha256 = "e86c59feb08126d4cace368432c16b2dee8e519aaca8a9d2b409ae1cdd200802"
